cf0502930c30ea19da90650f75bb601facef446c
Samar Abu Eid <Samar@logitudeworld.com>
2018-10-21 13:20:54 +0300

    #43478

---
+++ Logitude/AngularModules/AngularModules/SharedLogistics/Components/SharedLogisticsPartnersPermissiosComponent.ts
@@ -0,0 +1,217 @@
+import { Component, OnInit } from '@angular/core';
+import { SessionLocator } from '../../Infrastructure/Utilities/SessionLocator';
+import { ServiceResponse } from '../../Infrastructure/DataContracts/ServiceResponse';
+import { SharedLogisticsSettingPM } from '../../Infrastructure/EntityPMs/SharedLogisticsSettingPM';
+import { SharedLogisticsSettingPMService } from '../../Infrastructure/Services/StandardPMs/SharedLogisticsSettingPMService';
+import { EntityResourceService } from '../../Infrastructure/Services/EntityResourceService';
+import { ObjectsLocator } from '../../Infrastructure/Locators/ObjectsLocator';
+
+@Component({
+    moduleId: module.id,
+    templateUrl: './SharedLogisticsPartnersPermissiosComponent.html',
+})
+
+export class SharedLogisticsPartnersPermissiosComponent implements OnInit {
+    public PartnersList: PartnerItem[];
+    public CarriersList: PartnerItem[];
+
+    public EntityPM: SharedLogisticsSettingPM;
+    public TenantZeroEntity: SharedLogisticsSettingPM;
+    private myService: SharedLogisticsSettingPMService;
+    public IsResourcesReady: boolean = false;
+
+    constructor(public entityResourceService: EntityResourceService) {
+        this.myService = new SharedLogisticsSettingPMService();
+    }
+
+    ngOnInit() {
+        this.entityResourceService.getEntityResourceByTableName("SharedLogisticsSetting").subscribe((res: any) => {
+            var tenantZeroId: number = 0;
+            this.myService.get(tenantZeroId.toString()).subscribe((myResponse: ServiceResponse) => {
+                if (!myResponse.HasError) {
+
+                    if (myResponse.Result) {
+                        this.TenantZeroEntity = myResponse.Result;
+                    }
+
+                    else {
+                        this.TenantZeroEntity = new SharedLogisticsSettingPM();
+                    }
+
+                    this.LoadMyTenantData();
+                }
+            });           
+        });
+    }
+
+    private LoadMyTenantData() {
+        this.myService.get(SessionLocator.Tenant.toString()).subscribe((myResponse: ServiceResponse) => {
+            if (!myResponse.HasError) {
+
+                if (myResponse.Result) {
+                    this.EntityPM = myResponse.Result;
+                }
+
+                else {
+                    this.EntityPM = new SharedLogisticsSettingPM();
+                    this.EntityPM.IsDirty = false;
+                }
+
+                if (this.EntityPM != null && this.TenantZeroEntity != null) {
+                    this.BuildData();
+                }
+
+                this.IsResourcesReady = true;
+            }
+        });
+    }
+
+    private BuildData() {
+        this.PartnersList = [];
+        this.CarriersList = [];
+
+        var item1: PartnerItem = new PartnerItem();
+        item1.PartnerName = "Shipper";
+        item1.SuggestedIsChecked = true;
+        item1.ChooseIsChecked = true;
+
+        var item2: PartnerItem = new PartnerItem();
+        item2.PartnerName = "Consignee";
+        item2.SuggestedIsChecked = true;
+        item2.ChooseIsChecked = true;
+
+        var item3: PartnerItem = new PartnerItem();
+        item3.PartnerName = "Agent";
+        item3.SuggestedIsChecked = this.TenantZeroEntity.IsAgentShared;
+        item3.ChooseIsChecked = this.EntityPM.IsAgentShared;
+
+        var item4: PartnerItem = new PartnerItem();
+        item4.PartnerName = "Shipper Not Exporter";
+        item4.SuggestedIsChecked = this.TenantZeroEntity.IsShipperNotExporterShared;
+        item4.ChooseIsChecked = this.EntityPM.IsShipperNotExporterShared;
+
+        var item5: PartnerItem = new PartnerItem();
+        item5.PartnerName = "Consignee Not Importer";
+        item5.SuggestedIsChecked = this.TenantZeroEntity.IsConsigneeNotImporterShared;
+        item5.ChooseIsChecked = this.EntityPM.IsConsigneeNotImporterShared;
+
+        var item6: PartnerItem = new PartnerItem();
+        item6.PartnerName = "Notify 1";
+        item6.SuggestedIsChecked = this.TenantZeroEntity.IsNotify1Shared;
+        item6.ChooseIsChecked = this.EntityPM.IsNotify1Shared;
+
+        var item7: PartnerItem = new PartnerItem();
+        item7.PartnerName = "Notify 2";
+        item7.SuggestedIsChecked = this.TenantZeroEntity.IsNotify2Shared;
+        item7.ChooseIsChecked = this.EntityPM.IsNotify2Shared;
+
+        var item8: PartnerItem = new PartnerItem();
+        item8.PartnerName = "Freight Forwarder";
+        item8.SuggestedIsChecked = this.TenantZeroEntity.IsFreightForwarderShared;
+        item8.ChooseIsChecked = this.EntityPM.IsFreightForwarderShared;
+
+        var item9: PartnerItem = new PartnerItem();
+        item9.PartnerName = "Coloader";
+        item9.SuggestedIsChecked = this.TenantZeroEntity.IsColoaderShared;
+        item9.ChooseIsChecked = this.EntityPM.IsColoaderShared;
+
+        var item10: PartnerItem = new PartnerItem();
+        item10.PartnerName = "Pickup and Deliveries Carriers";
+        item10.SuggestedIsChecked = this.TenantZeroEntity.IsPickDelivCarriesShared;
+        item10.ChooseIsChecked = this.EntityPM.IsPickDelivCarriesShared;
+
+        var item11: PartnerItem = new PartnerItem();
+        item11.PartnerName = "Main Carriage Carrier";
+        item11.SuggestedIsChecked = this.TenantZeroEntity.IsMainCarrierShared;
+        item11.ChooseIsChecked = this.EntityPM.IsMainCarrierShared;
+
+        this.PartnersList.push(item1);
+        this.PartnersList.push(item2);
+        this.PartnersList.push(item3);
+        this.PartnersList.push(item4);
+        this.PartnersList.push(item5);
+        this.PartnersList.push(item6);
+        this.PartnersList.push(item7);
+        this.PartnersList.push(item8);
+        this.PartnersList.push(item9);
+
+        this.CarriersList.push(item10);
+        this.CarriersList.push(item11);
+    }
+
+    private mySearchText: string;
+    onSearchTextChangeEvent(searchText) {
+        if (!searchText) searchText = "";
+
+        this.mySearchText = searchText;
+        this.BuildData();
+    }
+    
+    CancelButtonClicked() {
+        SessionLocator.CurrentSession.CloseCurrentWindow();
+    }
+
+    public ValidationErrorsList: string[] = [];
+    OkButtonClicked() {
+        this.ValidationErrorsList = [];
+
+        if (this.ValidationErrorsList.length == 0) {
+            if (this.EntityPM.IsDirty) {
+                SessionLocator.CurrentSession.StartBusyIndicatorSaving();
+
+                if (this.EntityPM.Tenant == null) {
+                    this.EntityPM.Tenant = SessionLocator.Tenant;
+                    this.myService.insert(this.EntityPM).subscribe((myRespone: ServiceResponse) => {
+                        SessionLocator.CurrentSession.StopBusyIndicator();
+
+                        if (!myRespone.HasError) {
+                            ObjectsLocator.SharedLogisticsSettingPM = this.EntityPM;
+                            SessionLocator.CurrentSession.CloseCurrentWindowEmit("OK");
+                        }
+
+                        else {
+                            this.ValidationErrorsList = myRespone.ErrorsArray;
+                        }
+                    });
+                }
+
+                else {
+                    this.myService.update(this.EntityPM).subscribe((myRespone: ServiceResponse) => {
+                        SessionLocator.CurrentSession.StopBusyIndicator();
+
+                        if (!myRespone.HasError) {
+                            ObjectsLocator.SharedLogisticsSettingPM = this.EntityPM;
+                            SessionLocator.CurrentSession.CloseCurrentWindowEmit("OK");
+                        }
+
+                        else {
+                            this.ValidationErrorsList = myRespone.ErrorsArray;
+                        }
+                    });
+                }
+            }
+
+            else {
+                SessionLocator.CurrentSession.CloseCurrentWindow();
+            }
+        }
+    }
+}
+
+export class PartnerItem {
+
+    constructor() {
+
+    }
+
+    public PartnerName: string;
+    public SuggestedIsChecked: boolean;
+
+    private chooseIsChecked: boolean;
+    public get ChooseIsChecked() { return this.chooseIsChecked; }
+    public set ChooseIsChecked(value: boolean) {
+        if (this.chooseIsChecked != null) {
+            this.chooseIsChecked = value;
+        }
+    }
+}
