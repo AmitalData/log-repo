27d05e2574944f4b48c2222eae5b32a158a8a9c7
Abed <abed@logitudeworld.com>
2018-10-23 12:17:33 +0300

    43919

---
+++ Logitude/General/Main Branch/2018/10 - 2018/Day 23 [Abed]QuoteVariableReplacementInDocumentTemplate.sql
@@ -0,0 +1,34 @@
+
+begin
+
+
+declare @ObjectTableId as varchar(15)
+
+set @ObjectTableId = (select id from ObjectTables where Name = 'Quote')
+
+declare @Tenant as int
+
+	 DECLARE TenantCursor CURSOR READ_ONLY
+	FOR
+	SELECT Id
+	From Tenants
+	 OPEN TenantCursor FETCH NEXT FROM TenantCursor INTO @Tenant
+	 WHILE @@FETCH_STATUS = 0
+	BEGIN
+
+
+update DocumentTypeTemplates set  TemplateBodyHtml = convert(varbinary(max),REPLACE(convert(varchar(max),TemplateBodyHtml ,0) , '[OrderNumberOfPackages]','[NumberOfPackages]') ,0) where tenant = @Tenant and EditorTool = 'R'   and DocumentTypeId =(select id from DocumentTypes where ObjectTableId = @ObjectTableId and id =DocumentTypeId) and  TemplateBodyHtml is not null and convert(varchar(max),TemplateBodyHtml ,0) like '%[OrderNumberOfPackages]%'
+
+
+
+
+
+print @Tenant
+
+
+	FETCH NEXT FROM TenantCursor     INTO @Tenant
+	END
+	 CLOSE TenantCursor
+	 DEALLOCATE TenantCursor
+END
+
