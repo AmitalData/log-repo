51a32aae143849b769e9fe14c18ee96bdbb8a1bf
itzik M <itzik@amital.co.il>
2018-10-25 09:16:37 +0300

    im-conn kill

--- Logitude/Logitude.CustomsMessaging/MessagingServices/MessagingServiceBase.ReqSheet.cs
+++ Logitude/Logitude.CustomsMessaging/MessagingServices/MessagingServiceBase.ReqSheet.cs
@@ -911,7 +911,11 @@ namespace Logitude.CustomsMessaging.MessagingServices
             {
 
                 var res = AnalyzeCore(requestParams, customsResponse, false);
-                CustomsRequestsSheetDomainModelUtil.ReleaseConcurrentKey(requestParams);
+                bool tryConcurrentKiller = ConfigurationManager.AppSettings["20180718.ConcurrentKiller"] == "1";
+                if (tryConcurrentKiller)
+                {
+                    CustomsRequestsSheetDomainModelUtil.ReleaseConcurrentKey(requestParams);
+                }
                 ContextObjectTag = res.ContextObjectTag;
                 return res
                 ;
