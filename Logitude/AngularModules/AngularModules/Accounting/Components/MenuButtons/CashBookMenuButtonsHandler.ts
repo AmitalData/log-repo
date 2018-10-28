cb05d2c396fc7ed00c21726ac6ab743c32a824ab
Ayman1 <Ayman@logitudeworld.com>
2018-10-28 11:11:32 +0200

    43472
    Related Work Items: #43472

--- Logitude/AngularModules/AngularModules/Accounting/Components/MenuButtons/CashBookMenuButtonsHandler.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/MenuButtons/CashBookMenuButtonsHandler.ts
@@ -1,4 +1,3 @@
-﻿import { SessionLocator } from './../../../Infrastructure/Utilities/SessionLocator';
 declare var window: any;
 import {CashBookPM} from '../../EntityPMs/CashBookPM';
 import {MenuButtonPM} from '../../../Infrastructure/EntityPMs/MenuButtonPM'
