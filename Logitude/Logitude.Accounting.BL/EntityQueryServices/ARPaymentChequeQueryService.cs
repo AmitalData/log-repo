0d4048d87c7b06b1d7616b82b361f1a385648ce1
alaa <alaa.jamal.h@live.com>
2018-11-05 14:15:17 +0200

    43675
    Related Work Items: #43675

--- Logitude/Logitude.Accounting.BL/EntityQueryServices/ARPaymentChequeQueryService.cs
+++ Logitude/Logitude.Accounting.BL/EntityQueryServices/ARPaymentChequeQueryService.cs
@@ -45,6 +45,11 @@ namespace Logitude.Accounting.BL.EntityQueryServices
                                                           BankBranch = a.BankBranch,
                                                           BankAccount = a.BankAccount,
                                                           StatusName = a.ARPaymentChequeStatus != null ? a.ARPaymentChequeStatus.EnglishName : "",
+                                                          PaymentId = paymentId,
+                                                          ExchangeRate = a.ExchangeRate,
+                                                          StatusCode = a.StatusCode,
+                                                          CurrencyId = a.CurrencyId,
+                                                         
 
                                                       }).ToList();
             return paymentCheques;
