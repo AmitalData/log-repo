f5231737c8b646238d3316aa8347bd0727bf0500
itzik M <itzik@amital.co.il>
2018-11-05 07:58:27 +0200

    im-CREATE UNIQUE INDEX IX_CPF_PIT ON CUSTOMSPARTNERFTPS (PARTNERCODE ASC, INTERFACENAME ASC, TYPECODE ASC)

--- Logitude/Logitude.OracleDatabaseMigration/Migrations/201811040823109_CustomsPartnerFtp.cs
+++ Logitude/Logitude.OracleDatabaseMigration/Migrations/201811040823109_CustomsPartnerFtp.cs
@@ -7,6 +7,8 @@ namespace Logitude.OracleDatabaseMigration.Migrations
     {
         public override void Up()
         {
+
+            
             CreateTable(
                 "Customs.CustomsPartnerFtps",
                 c => new
@@ -23,15 +25,17 @@ namespace Logitude.OracleDatabaseMigration.Migrations
                 .PrimaryKey(t => t.Id)
                 .ForeignKey("dbo.FTPDetails", t => t.FtpDetailsId)
                 .Index(t => t.FtpDetailsId);
-            
+
             //AlterColumn("dbo.JournalLines", "Reference1", c => c.String(maxLength: 30));
             //AlterColumn("dbo.JournalLines", "Reference2", c => c.String(maxLength: 30));
             //AlterColumn("dbo.JournalLines", "Reference3", c => c.String(maxLength: 30));
             //AlterColumn("dbo.LedgerTransactions", "Reference1", c => c.String(maxLength: 30));
             //AlterColumn("dbo.LedgerTransactions", "Reference2", c => c.String(maxLength: 30));
             //AlterColumn("dbo.LedgerTransactions", "Reference3", c => c.String(maxLength: 30));
+
+            ///CREATE UNIQUE INDEX IX_CPF_PIT ON CUSTOMSPARTNERFTPS (PARTNERCODE ASC, INTERFACENAME ASC, TYPECODE ASC) 
         }
-        
+
         public override void Down()
         {
             DropForeignKey("Customs.CustomsPartnerFtps", "FtpDetailsId", "dbo.FTPDetails");
@@ -43,6 +47,8 @@ namespace Logitude.OracleDatabaseMigration.Migrations
             //AlterColumn("dbo.JournalLines", "Reference2", c => c.String(maxLength: 30, unicode: false));
             //AlterColumn("dbo.JournalLines", "Reference1", c => c.String(maxLength: 30, unicode: false));
             DropTable("Customs.CustomsPartnerFtps");
+
+            //DROP INDEX IX_CPF_PIT;
         }
     }
 }
