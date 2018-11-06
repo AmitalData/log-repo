87bfe5e28c2a50a12666c00a8d3160d79cde4ee3
Mohammad Masoud <mohammad@logitudeworld.com>
2018-11-06 16:41:46 +0200

    required
    Related Work Items: #44571

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/LogTextBoxComponent.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/LogTextBoxComponent.ts
@@ -1397,7 +1397,7 @@ export class LogTextBoxComponent implements OnInit, AfterViewInit, OnDestroy {
     }
 
     ValidateField(emitPropertyChanged: boolean = true) {
-
+        var suppressValidateion: boolean = false;
         switch (this.InputType && this.InputType.toLowerCase()) {
             case 'text':
             case 'ntext':
@@ -1407,15 +1407,17 @@ export class LogTextBoxComponent implements OnInit, AfterViewInit, OnDestroy {
             default: {
                 if (isNaN(Number(this.TextValue))) {
                     this.SetValidity(false, TextCodeTranslator.Translate("General.O.InvalidInput"));
+                    suppressValidateion = true;
                 }
                 else {
                     this.SetValidity(true, null);
+                    suppressValidateion = false;
                 }
             }
 
         }
 
-        if (!this.NoValidation && this.uiProperty != null && this.uiProperty.ValidValue) {
+        if (!this.NoValidation && this.uiProperty != null && !suppressValidateion) {
             var errors = null;
             var table = window.ObjectTables.filter(d => d.Name === this.uiProperty.ObjectTableName)[0];
             if (table) {
