0c0f4a36a1a73d96e1b7f3c6683ed0be9b62764d
Abdullah Dawoud <abdullah@logitudeworld.com>
2018-11-11 12:04:00 +0200

    new cashbook: gla custom filter
    Related Work Items: #44589

--- Logitude/Logitude.Accounting.Data/CustomFilters/GLAccountCustomFilter.cs
+++ Logitude/Logitude.Accounting.Data/CustomFilters/GLAccountCustomFilter.cs
@@ -66,6 +66,15 @@ namespace Logitude.Accounting.Data.CustomFilters
                                        
                         }
                     }
+                    if (item.FieldName == "SingleAndMultiCurrencyAccount")
+                    {
+                        string value = item.FieldValue as string;
+
+                        if (!string.IsNullOrEmpty(value))
+                        {
+                            queryableData = queryableData.Where(d => d.CurrencyId == value || d.IsMultiCurrency == true);
+                        }
+                    }
                 }
             }
 
