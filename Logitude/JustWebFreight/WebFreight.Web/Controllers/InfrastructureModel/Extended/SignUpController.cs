6e7d3876be61a04a9a5732a69fd8f5cff33de357
Abed <abed@logitudeworld.com>
2018-11-13 13:35:13 +0200

    44781 44779 44778 44780 43920
    Related Work Items: #43920, #44778, #44779, #44780, #44781

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/InfrastructureModel/Extended/SignUpController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/InfrastructureModel/Extended/SignUpController.cs
@@ -34,12 +34,9 @@ namespace WebFreight.Web.Controllers.InfrastructureModel.Extended
 
                     using (TransactionScope scope = TransactionFactory.GetTransaction())
                     {
-                        if (!signupInfo.IsWithOutValidation)
-                        {
-                            string token = HttpContext.Current.Request.Headers["Token"];
-                            AuthenticationToken authToken = AuthenticationTokenRepository.GetSingleTokenFromCache(token);
-                            SecurityUtility.AuthenticationOnTenant(authToken.Tenant);
-                        }
+                        string token = HttpContext.Current.Request.Headers["Token"];
+                        AuthenticationToken authToken = AuthenticationTokenRepository.GetSingleTokenFromCache(token);
+                        SecurityUtility.AuthenticationOnTenant(authToken.Tenant);
 
                         FilterSerializer serializer = new FilterSerializer();
                         MemoryStream memstream = new MemoryStream();
@@ -50,8 +47,8 @@ namespace WebFreight.Web.Controllers.InfrastructureModel.Extended
                         string content = reader.ReadToEnd();
                         byte[] bytearray = memstream.ToArray();
 
-                        SignUpService signUpService = new SignUpService();
-                        signUpService.SendMessageToQueue(bytearray);
+                        SignUpHelper signUpHelper = new SignUpHelper();
+                        signUpHelper.SendMessageToQueue(bytearray);
 
                         scope.Complete();
 
