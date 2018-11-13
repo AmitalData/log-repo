6678bf58685ed0777b188922311c28c9c2216e05
itzik M <itzik@amital.co.il>
2018-11-13 07:49:17 +0200

    SendWebAPI2MamanGWMessageECTHRDataWR

--- Logitude/CustomsWorkerRole/SendWebAPI2MamanGWMessageECTHRDataWR.cs
+++ Logitude/CustomsWorkerRole/SendWebAPI2MamanGWMessageECTHRDataWR.cs
@@ -316,17 +316,19 @@ namespace CustomsWorkerRole
         }
         const string relativeUriToken = "Token";
         const string BEARER_TOKEN = "Bearer";
+        const string agent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36";
         public string PostIt(string host,string user,string pass, string dataJson)
         {
             string myResultString = "";
-            
 
+            string access_token = "";
+            string token_type = "";
             using (var client = new HttpClient())
             {
                 //var GetURI = URI + "ImporterShipmentDocuments/GetIfNew?id=" + DocumentFilingPM.CustomerDocumentId + "&tenant=" + importerTenant;// +"&importertenant=" + importerTenant;
 
                 var ADD = "User-Agent: Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36";
-                client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36");
+                client.DefaultRequestHeaders.Add("User-Agent", agent);
                 string webApiURI = host;//POST 
                 //var tokenUri = new Uri(new Uri(host), relativeUriToken);
                 webApiURI = @"https://maman.wsfreeze.co.il/WebAPIExt/Token"; //HTTP/1.1;
@@ -337,7 +339,11 @@ namespace CustomsWorkerRole
                 var task = client.PostAsync(webApiURI, content);
                 Wait4Finsh(task, 1);
                 myResultString = task.Result.Content.ReadAsStringAsync().Result;
-
+                //{"access_token":"zkKDt-XnqqM5uoyDwrPxDPHb_vM5hplsUKr7sT5GA2w8Vpsl_HT5eBidUriiyw3Gn-Mne0NIq2LQO7MMT525GdrFutDzIQpRKR6c7oz2GbdSdGdEY3S3nfP0W7svtmShEeUx23SbW8ysLkyAnFP-IdQhvMs2lzxzHIDrnDqm_agwq54x9UiiDa5-9ZkEWBUrN83U4B5qddiYTU0whODGvrxEE9wyrQKoygG3Gi48gwv2_TI4H9yrd2Uys9l_jBivOsRRm1oXtyGsyIq9DwDn7pmcoxUjz-yNwm_hp18Y1qi4aXk1Z8IjeKRQl_8FMUg-","token_type":"bearer","expires_in":35999,"UserName":"F_unitedf","role":"General",".issued":"Mon, 12 Nov 2018 14:48:43 GMT",".expires":"Tue, 13 Nov 2018 00:48:43 GMT"}
+                dynamic d= JsonConvert.DeserializeObject(myResultString);
+                access_token = d.access_token;
+                token_type = d.token_type;
+                //token_type=bearer
             }
 
             using (var client = new HttpClient())
@@ -346,14 +352,14 @@ namespace CustomsWorkerRole
 
                 string webApiURI = host;//URI + "APIAuthentication";
                 webApiURI = "https://maman.wsfreeze.co.il/WebAPIExt/api/baldar/CreateECTHRMessgae";
-
+                client.DefaultRequestHeaders.Add("User-Agent", agent);
                 var content = new StringContent(dataJson, Encoding.UTF8, "application/json");
                 //Authorization: <type> <credentials>
                 
                 string credentials = "";
                 //Authorization: Bearer O5GRnBFMruLRIdRJAI_CQNLzXanWBQ0FO4zQGR6gkluiYOWTaop-p_UkEfq0NaoIuFC_kLfJjABjJdN5HW0_aC-kTMS63nHKUb9yiCxOOiv5UmrCvd1XLgFbBxCLwdDcCnwiCgdM_CTkhM_cFX5KWsNyWAD9i85wyk06lV-iROw2itvXo3Vir-19fMiTZnFbe_OffXJWfl2lF89zXT_MYzlOJdCqDRYELSwAPjBcPzLva5-EN4Pi2Jyu-nZs7DxW5NcEDM6JJUDk66C7VXxqz5s3Q4D4Knr14lmYMmetdAY
-                credentials = "O5GRnBFMruLRIdRJAI_CQNLzXanWBQ0FO4zQGR6gkluiYOWTaop-p_UkEfq0NaoIuFC_kLfJjABjJdN5HW0_aC-kTMS63nHKUb9yiCxOOiv5UmrCvd1XLgFbBxCLwdDcCnwiCgdM_CTkhM_cFX5KWsNyWAD9i85wyk06lV-iROw2itvXo3Vir-19fMiTZnFbe_OffXJWfl2lF89zXT_MYzlOJdCqDRYELSwAPjBcPzLva5-EN4Pi2Jyu-nZs7DxW5NcEDM6JJUDk66C7VXxqz5s3Q4D4Knr14lmYMmetdAY";
-                client.DefaultRequestHeaders.Add("Authorization", $"{BEARER_TOKEN} {credentials}");
+                //credentials = "O5GRnBFMruLRIdRJAI_CQNLzXanWBQ0FO4zQGR6gkluiYOWTaop-p_UkEfq0NaoIuFC_kLfJjABjJdN5HW0_aC-kTMS63nHKUb9yiCxOOiv5UmrCvd1XLgFbBxCLwdDcCnwiCgdM_CTkhM_cFX5KWsNyWAD9i85wyk06lV-iROw2itvXo3Vir-19fMiTZnFbe_OffXJWfl2lF89zXT_MYzlOJdCqDRYELSwAPjBcPzLva5-EN4Pi2Jyu-nZs7DxW5NcEDM6JJUDk66C7VXxqz5s3Q4D4Knr14lmYMmetdAY";
+                client.DefaultRequestHeaders.Add("Authorization", $"{token_type} {access_token}");
                 var task = client.PostAsync(webApiURI, content);
                 Wait4Finsh(task, 1);
                 myResultString = task.Result.Content.ReadAsStringAsync().Result;
