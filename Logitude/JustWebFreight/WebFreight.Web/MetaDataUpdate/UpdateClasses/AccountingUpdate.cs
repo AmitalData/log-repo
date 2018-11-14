2aa636b2bae71abde1d455da31446f8ddaf758a6
alaa <alaa.jamal.h@live.com>
2018-11-14 11:25:45 +0200

    44680
    Related Work Items: #44680

--- Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/UpdateClasses/AccountingUpdate.cs
+++ Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/UpdateClasses/AccountingUpdate.cs
@@ -3865,40 +3865,40 @@ namespace WebFreight.Web.MetaDataUpdate.GeneratedUpdate
 
         public void CreateCounters(int tenant)
         {
-            objectContext = WebFreightContext.GetContext(tenant);
-            CounterRepository CounterRepository = new CounterRepository(objectContext);
-            CounterDefinitionRepository = new CounterDefinitionRepository(objectContext);
-            List<Counter> zeroCounters = CounterRepository.GetCounters(0).ToList();
-            ObjectTable taxDeductionReportObject = objectContext.ObjectTables.Where(d => d.Name == "TaxDeductionReport" && d.Tenant == 0).FirstOrDefault();
-            #region Tax Deduction Report Counters
+            //objectContext = WebFreightContext.GetContext(tenant);
+            //CounterRepository CounterRepository = new CounterRepository(objectContext);
+            //CounterDefinitionRepository = new CounterDefinitionRepository(objectContext);
+            //List<Counter> zeroCounters = CounterRepository.GetCounters(0).ToList();
+            //ObjectTable taxDeductionReportObject = objectContext.ObjectTables.Where(d => d.Name == "TaxDeductionReport" && d.Tenant == 0).FirstOrDefault();
+            //#region Tax Deduction Report Counters
 
-            if (!zeroCounters.Where(c => c.Code == "TXDC" && c.Tenant == 0).Any())
-            {
-                Counter taxDeductionReportCounter = new Counter()
-                {
-                    Id = IdCounter.GetNumber("Counter", 0).ToString(),
-                    ObjectTableId = taxDeductionReportObject.Id,
-                    Code = "TXDC",
-                    Tenant = 0,
-                    Name = "Tax Deduction Report",
-                };
+            //if (!zeroCounters.Where(c => c.Code == "TXDC" && c.Tenant == 0).Any())
+            //{
+            //    Counter taxDeductionReportCounter = new Counter()
+            //    {
+            //        Id = IdCounter.GetNumber("Counter", 0).ToString(),
+            //        ObjectTableId = taxDeductionReportObject.Id,
+            //        Code = "TXDC",
+            //        Tenant = 0,
+            //        Name = "Tax Deduction Report",
+            //    };
 
-                CounterDefinition taxDeductionReportCounter_CounterDef = new CounterDefinition()
-                {
-                    Id = IdCounter.GetNumber("CounterDefinition", 0).ToString(),
-                    CounterId = taxDeductionReportCounter.Id,
-                    Tenant = 0,
-                    StartNumber = 1000,
-                    Parameter1 = "TX",
-                };
+            //    CounterDefinition taxDeductionReportCounter_CounterDef = new CounterDefinition()
+            //    {
+            //        Id = IdCounter.GetNumber("CounterDefinition", 0).ToString(),
+            //        CounterId = taxDeductionReportCounter.Id,
+            //        Tenant = 0,
+            //        StartNumber = 1000,
+            //        Parameter1 = "TX",
+            //    };
 
-                CounterRepository.Add(taxDeductionReportCounter);
-                CounterDefinitionRepository.Add(taxDeductionReportCounter_CounterDef);
+            //    CounterRepository.Add(taxDeductionReportCounter);
+            //    CounterDefinitionRepository.Add(taxDeductionReportCounter_CounterDef);
 
-            }
-            #endregion
+            //}
+            //#endregion
 
-            this.objectContext.SaveChanges();
+            //this.objectContext.SaveChanges();
         }
     }
 }
