491a9834565a6e46fcda46e23d38af1217347a88
itzik M <itzik@amital.co.il>
2018-11-20 08:50:45 +0200

    REMARKS

--- Logitude/Logitude.Accounting.BL/CoreBL/Reports/TrailReportBase.cs
+++ Logitude/Logitude.Accounting.BL/CoreBL/Reports/TrailReportBase.cs
@@ -36,10 +36,16 @@ namespace Logitude.Accounting.BL.CoreBL.Reports
 
         protected IQueryable<Data.EntityPOCOs.GLAccountTotalByMonth> QBaseGLAccountTotalByMonthsFrom0BCTilNotIncludeStartOfMonthFromDate;
 
+
+        // מצטברים מחודש כולל ועד חודש לא כולל
         protected IQueryable<Data.EntityPOCOs.GLAccountTotalByMonth> QBaseTotalsFromStartOfMonthFromTilStartOfMonthTo;
 
+        // תנעות מכולל תחילת החודש  של מתאריך עד למתאריך -לא כולל    
         protected IQueryable<Data.EntityPOCOs.LedgerTransaction> QBaseTranactionBeginOfMonthFromTillFromDateNotInclude;
 
+
+
+        // תנעות מתחילת חודש אחרון כולל עד  תאריך הסיום + 1 לא כולל
         protected IQueryable<Data.EntityPOCOs.LedgerTransaction> QBaseTranactionBeginOfMonthToDateTillToDateInculde;
 
         protected IQueryable<ChartOfAccount5LevelM> QBaseAllCardsAndDetialsAccTypeBy5LevelHierarchy;
@@ -131,7 +137,7 @@ namespace Logitude.Accounting.BL.CoreBL.Reports
                  select tot
                  );
 
-
+            // תנעות מכולל תחילת החודש  של מתאריך עד למתאריך -לא כולל    
             QBaseTranactionBeginOfMonthFromTillFromDateNotInclude = (
                 from trans in _AccountingContext.LedgerTransactions
                 where trans.Tenant == _TrailReportParam.Tenant
@@ -142,7 +148,7 @@ namespace Logitude.Accounting.BL.CoreBL.Reports
                 select trans
                    );
             var toDateAdd1Day = _TrailReportParam.ToDate.AddDays(1);//INclude //
-            QBaseTranactionBeginOfMonthToDateTillToDateInculde =
+            QBaseTranactionBeginOfMonthToDateTillToDateInculde =// תנעות מתחילת חודש אחרון כולל עד  תאריך הסיום + 1 לא כולל
                 (
                 from trans in _AccountingContext.LedgerTransactions
                 where trans.Tenant == _TrailReportParam.Tenant
