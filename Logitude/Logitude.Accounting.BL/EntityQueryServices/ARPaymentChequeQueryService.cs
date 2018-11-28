864cfa2e889105b0919fb6949d381d9d00d842ce
alaa <alaa.jamal.h@live.com>
2018-11-28 15:08:21 +0200

    45195
    Related Work Items: #45195

--- Logitude/Logitude.Accounting.BL/EntityQueryServices/ARPaymentChequeQueryService.cs
+++ Logitude/Logitude.Accounting.BL/EntityQueryServices/ARPaymentChequeQueryService.cs
@@ -77,6 +77,17 @@ namespace Logitude.Accounting.BL.EntityQueryServices
                                                       }).FirstOrDefault();
             return paymentCheques;
         }
+
+
+
+        public ARPaymentChequePM GetPaymentChequeByChequeNo(string chequeNumber, int tenant)
+        {
+            ARPaymentCheque poco = (from a in context.ARPaymentCheques
+                                  where a.ChequeNumber == chequeNumber && a.Tenant == tenant
+                                  select a).FirstOrDefault();
+
+            return this.GetEntityPM(poco);
+        }
     }
 }
 
