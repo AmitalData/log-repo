f831300e011a693b6cf5e1eceb3d70a9cc10392a
Abdullah Dawoud <abdullah@logitudeworld.com>
2018-11-28 08:38:25 +0200

    change agin method
    Related Work Items: #44316

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/AccountingModel/GLAccountViewsController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/AccountingModel/GLAccountViewsController.cs
@@ -350,7 +350,7 @@ namespace WebFreight.Web.App_Code.AngularJS_App_Code.Generated
                     Aging4AccountTypeCode = isCustomer == true ? AgingReportParam.Aging4AccountTypeCodeEnum.Customer2 : AgingReportParam.Aging4AccountTypeCodeEnum.ControlAccountOnly1,
 
                     GroupByDate = groupByDate == "AccountingDate" ? AgingReportParam.DateEnum.AccountingDate : AgingReportParam.DateEnum.DueDate,
-                    AgingMethod = AgingReportParam.MethodEnum.TotalByMonthFIFOMethod.ToString(),
+                    AgingMethod = AgingReportParam.MethodEnum.ReconcileOpenBalanceMethod.ToString(),
 
                     AgingMethod_Options = Enum.GetNames(typeof(AgingReportParam.MethodEnum)).ToList().Aggregate((b4, aftr) => string.Concat(b4, ";", aftr)),
                     GroupByDate_Options = Enum.GetNames(typeof(AgingReportParam.DateEnum)).ToList().Aggregate((b4, aftr) => string.Concat(b4, ";", aftr)),
