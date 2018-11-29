762b7f6e97a0a7244c6607b1a828f1a339afe0b6
Islam Shaheen <Islam@logitudeworld.com>
2018-11-29 10:47:00 +0200

    metadata
    Related Work Items: #43992

--- Logitude/MeatadataGeneratorTool/MeatadataGeneratorTool/MeatadataGeneratorTool/ObjectTableControl.xaml.cs
+++ Logitude/MeatadataGeneratorTool/MeatadataGeneratorTool/MeatadataGeneratorTool/ObjectTableControl.xaml.cs
@@ -30,6 +30,9 @@ namespace MeatadataGeneratorTool
 
         void ObjectTableControl_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
+
+            System.Windows.Application.Current.Shutdown();
+            Environment.Exit(0);
             //ObjectTableViewModel viewModel = this.DataContext as ObjectTableViewModel;
             //MessageBoxResult result = MessageBox.Show("Do you want to save your changes?", "Save Changes", MessageBoxButton.YesNo);
             //if (result == MessageBoxResult.Yes)
