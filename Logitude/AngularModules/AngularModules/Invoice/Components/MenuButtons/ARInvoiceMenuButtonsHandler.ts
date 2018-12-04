defbcf254f5dfde40f9d26b0c8d7d500b01ba716
Maheera Jazi <Maheera@logitudeworld.com>
2018-12-04 08:03:56 +0200

    wi: 45755
    Related Work Items: #45755

--- Logitude/AngularModules/AngularModules/Invoice/Components/MenuButtons/ARInvoiceMenuButtonsHandler.ts
+++ Logitude/AngularModules/AngularModules/Invoice/Components/MenuButtons/ARInvoiceMenuButtonsHandler.ts
@@ -913,7 +913,7 @@ export class ARInvoiceMenuButtonsHandler {
             myReference = !AppTool.IsNullOrEmpty(this.EntityPM.InvoiceNumber) ? this.EntityPM.InvoiceNumber : "Draft: " + this.EntityPM.DraftNumber;
             this.StartPrinting(myEntityId, myChildEntityId, myObjectTableName, mychildObjectTableId, myDocumentTypeCode, myReference);
         }
-        else if (this.EntityPM.IsConsolidationInvoice) {
+        else if (this.EntityPM.IsGeneralInvoice) {
             myEntityId = this.EntityPM.Id;
             myChildEntityId = null;
             mychildObjectTableId = null;
