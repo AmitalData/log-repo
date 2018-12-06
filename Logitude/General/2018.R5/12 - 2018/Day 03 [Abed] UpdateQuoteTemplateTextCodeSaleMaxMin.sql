95bc0acb18cdf5e2154a546cefbb13d52bc3fbc5
Samar Abu Eid <Samar@logitudeworld.com>
2018-12-06 14:34:28 +0200

    #45756

---
+++ Logitude/General/2018.R5/12 - 2018/Day 03 [Abed] UpdateQuoteTemplateTextCodeSaleMaxMin.sql
@@ -0,0 +1,3 @@
+
+
+update QuoteTemplateTextCodes set EnglishName ='Min/Max', OriginalEnglishName = 'Min/Max', LocalName = 'Min/Max', OriginalLocalName = 'Min/Max' Where TextCode = 'SALEMINMAXPACKAGES' or TextCode = 'SALEMINMAXCONTAINERS'
