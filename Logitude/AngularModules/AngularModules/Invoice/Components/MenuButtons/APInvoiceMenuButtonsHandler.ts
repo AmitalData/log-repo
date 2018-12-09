8b858dd02c639a491b9f6017df22b905c19b1106
Khalid Khatib <khalid@logitudeworld.com>
2018-12-09 13:00:14 +0200

    QBO AP Invoice/Payments resending
    Related Work Items: #45174

--- Logitude/AngularModules/AngularModules/Invoice/Components/MenuButtons/APInvoiceMenuButtonsHandler.ts
+++ Logitude/AngularModules/AngularModules/Invoice/Components/MenuButtons/APInvoiceMenuButtonsHandler.ts
@@ -119,6 +119,32 @@ export class APInvoiceMenuButtonsHandler {
                                 }
                                 break;
                             }
+
+
+                        case "SendToQBO":
+                            {
+                                if (SessionLocator.AccountingSettingPM.AccountingSystemCode == "QBO" || SessionLocator.AccountingSettingPM.AccountingSystemCode == "QBOG") {
+                                    button.IsHidden = false;
+                                }
+                                else {
+                                    button.IsHidden = true;
+                                }
+                                if (this.EntityPM.TransferStatusCode == "RD" || this.EntityPM.TransferStatusCode == "NR") {
+                                    button.DisplayText = "Send to QBO";
+                                }
+                                else if (this.EntityPM.TransferStatusCode == "TR" || this.EntityPM.TransferStatusCode == "ET" || this.EntityPM.TransferStatusCode == "IP") {
+                                    button.LabelTextCodeCode = null;
+                                    button.DisplayText = "Resend to QBO";
+                                }
+                                if (this.EntityPM.ApprovedDate == null) {
+                                    myButtonIsDisabled = true;
+                                }
+                                else {
+                                    myButtonIsDisabled = false;
+                                }
+
+                                break;
+                            }
                     }
 
                     button.IsDisabled = myButtonIsDisabled;
@@ -173,6 +199,12 @@ export class APInvoiceMenuButtonsHandler {
                         break;
                     }
 
+                case "SendToQBO":
+                    {
+                        this.SendToQBO();
+                        break;
+                    }
+
                 default: {
                     this.StopFlags();
                     break;
@@ -181,6 +213,37 @@ export class APInvoiceMenuButtonsHandler {
 
         }
     }
+
+
+    SendToQBO() {
+        if (this.EntityPM.TransferStatusCode == "TR" || this.EntityPM.TransferStatusCode == "ET" || this.EntityPM.TransferStatusCode == "IP") {
+            var myConfirmWindow = new ConfirmWindow();
+            myConfirmWindow.Width = 400;
+            myConfirmWindow.Show("Resend this invoice to QBO?");
+            myConfirmWindow.WindowClosed.subscribe(s => {
+                this.StopFlags();
+                if (myConfirmWindow.Yes) {
+                    this.SendToQBOApproved("Resending Invoice to QBO");
+
+                }
+            });
+        }
+
+        else {
+            this.SendToQBOApproved("Sending Invoice to QBO");
+            this.StopFlags();
+        }
+    }
+
+
+    SendToQBOApproved(Text: string) {
+        this.EntityPM.SetReSendQBO = true;
+        this.EntityPM.SetVoided = false;
+        this.EntityPM.SetApproved = false;
+        this.EntityPM.SetReTransfer = false;
+        this.entityArgs.EditComponent.SaveChanges(Text);
+    }
+
 
     isValid: boolean = false;
     isButtonClicked: boolean = false;
@@ -493,6 +556,7 @@ export class APInvoiceMenuButtonsHandler {
                     this.EntityPM.SetApproved = false;
                     this.EntityPM.SetReTransfer = false;
                     this.EntityPM.SetCancelApproval = false;
+                    this.EntityPM.SetReSendQBO = false;
 
                     this.entityArgs.EditComponent.SaveChanges("Voiding...");
                 }
@@ -512,6 +576,7 @@ export class APInvoiceMenuButtonsHandler {
             this.EntityPM.SetApproved = false;
             this.EntityPM.SetReTransfer = true;
             this.EntityPM.SetCancelApproval = false;
+            this.EntityPM.SetReSendQBO = false;
 
             this.entityArgs.EditComponent.SaveChanges();
         }
--- Logitude/AngularModules/AngularModules/Invoice/EntityPMs/APInvoicePM.ts
+++ Logitude/AngularModules/AngularModules/Invoice/EntityPMs/APInvoicePM.ts
@@ -71,7 +71,11 @@ export class APInvoicePM {
     private vATNumber: string;
     public get VATNumber() { return this.vATNumber; }
     public set VATNumber(newValue: string) { if (this.vATNumber != newValue) { this.vATNumber = newValue; this.MarkAsDirty("VATNumber"); } }
-       
+
+    private setReSendQBO: boolean;
+    public get SetReSendQBO() { return this.setReSendQBO; }
+    public set SetReSendQBO(newValue: boolean) { if (this.setReSendQBO != newValue) { this.setReSendQBO = newValue; this.MarkAsDirty("SetReSendQBO"); } }
+
 	 
     private invoiceDate: Date;
     public get InvoiceDate() { return this.invoiceDate; }
