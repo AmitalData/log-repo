259db621e0868dddcfcf272cd0c9bb41af6307bb
itzik M <itzik@amital.co.il>
2018-12-09 09:56:26 +0200

    44826 NEXT DUE DATE - In some cases, 1900-01-01 appears, should be NULL
    Related Work Items: #44826

--- Logitude/Logitude.Accounting.BL/CoreBL/DueLocalBalanceService.cs
+++ Logitude/Logitude.Accounting.BL/CoreBL/DueLocalBalanceService.cs
@@ -190,7 +190,7 @@ namespace Logitude.Accounting.BL.CoreBL
 
 #endif
 
-        public void ReBuild(int tenant,string AcountId)
+        public void ReBuild(int tenant,string AcountId,bool fastRun= false)
         {
             int clientAndVendorTypeGLAccountIdsCount = -1;
             List<DueLocalBalanceM> myDueLocalBalanceListToUpdate;
@@ -202,8 +202,16 @@ namespace Logitude.Accounting.BL.CoreBL
                 using (var scope = TransactionFactory.GetNewTransaction(TimeSpan.FromMinutes(5)))
                 {
                     var accountingContext = AccountingContext.GetContext(tenant);
-                    //InitDueLocalBalanceListToUpdate(accountingContext, tenant, true,true);
-                    InitDueLocalBalanceListToUpdate(accountingContext, tenant,"",false, false);// WHY I CHANGE TO FALSE FALSE (FROM TRUE*2) 1 NO TIME 2 THE REVERSE DUE DATE RETURN LISt
+                    if (fastRun)
+                    {
+                        InitDueLocalBalanceListToUpdate(accountingContext, tenant, "",true,true);
+                    }
+                    else
+                    {
+                        InitDueLocalBalanceListToUpdate(accountingContext, tenant, "", false, false);// WHY I CHANGE TO FALSE FALSE (FROM TRUE*2) 1 NO TIME 2 THE REVERSE DUE DATE RETURN LISt
+                        
+                    }
+
                     myDueLocalBalanceListToUpdate = _QDueLocalBalanceListToUpdate.ToList();
                     
                     myClientAndVendorTypeGLAccountIds = _QClientAndVendorTypeGLAccountIds.ToList();
@@ -508,7 +516,7 @@ namespace Logitude.Accounting.BL.CoreBL
             {
                 try
                 {
-                    this.ReBuild(tenant,"");
+                    this.ReBuild(tenant,"",true);
                 }
                 catch (Exception e)
                 {
