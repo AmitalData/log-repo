9a0eaa740e9b0e87723cc209852b782c1abb8a01
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2018-12-11 08:56:53 +0200

    QueryBuilder Root Filter
    Related Work Items: #44985

--- Logitude/Simplog.Data/InfrastructureModel/EntityPOCOs/DWObjectTable.cs
+++ Logitude/Simplog.Data/InfrastructureModel/EntityPOCOs/DWObjectTable.cs
@@ -16,5 +16,6 @@ namespace Simplog.Data.InfrastructureModel.EntityPOCOs
         public string Name { get; set; }
         public string TypeCode { get; set; }
         public bool IsClosed { get; set; }
+        public string DefaultFilterBy { get; set; }
     }
 }
