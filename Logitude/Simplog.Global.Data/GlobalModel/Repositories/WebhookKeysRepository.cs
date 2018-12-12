6d961aead74a610c97aeba1b4b9f58c4c18fa47c
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2018-12-12 10:31:54 +0200

    45661
    Related Work Items: #45661

---
+++ Logitude/Simplog.Global.Data/GlobalModel/Repositories/WebhookKeysRepository.cs
@@ -0,0 +1,89 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+using Simplog.Global.Data.GlobalModel.EntityPOCOs;
+using Simplog.Global.Data.GlobalModel;
+using Simplog.Server.Infrastructure;
+
+namespace Simplog.Global.Data.GlobalModel.Repositories
+{
+    public class WebhookKeysRepository : IRepository<WebhookKeys>
+    {
+        IGlobalContext globalContext;
+        public WebhookKeysRepository()
+        {
+            globalContext = GlobalContext.GetContext();
+        }
+
+        public WebhookKeysRepository(IGlobalContext context)
+        {
+            globalContext = context;
+        }
+
+        public WebhookKeys GetSingleWebhookKeysById(int id)
+        {
+            WebhookKeys item = context.WebhookKeys.Where(d => d.Id == id).FirstOrDefault();
+            return item;
+        }
+
+        public WebhookKeys GetSingleWebhookKeyByAccessKey(string key)
+        {
+            WebhookKeys item = context.WebhookKeys.Where(d => d.AccessKey == key).FirstOrDefault();
+            return item;
+        }
+
+
+
+        public IQueryable<WebhookKeys> GetAllWebhookKeyss()
+        {
+            return from a in context.WebhookKeys
+                   select a;
+        }
+
+
+        public void Add(WebhookKeys entity)
+        {
+            context.WebhookKeys.Add(entity);
+        }
+
+        public void Remove(WebhookKeys entity)
+        {
+            context.WebhookKeys.Attach(entity);
+            context.WebhookKeys.Remove(entity);
+        }
+
+        public void Update(WebhookKeys entity)
+        {
+            context.WebhookKeys.Attach(entity);
+            context.SetAsModified(entity);
+        }
+
+        public List<WebhookKeys> All()
+        {
+            return context.WebhookKeys.ToList();
+        }
+
+        public IGlobalContext context
+        {
+            get { return globalContext; }
+        }
+
+        public void SubmitChanges()
+        {
+            context.SaveChanges();
+        }
+
+
+        public List<WebhookKeys> GetMulti(Simplog.Server.Infrastructure.EntityKeyFields entityKeys)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public WebhookKeys GetSingle(Simplog.Server.Infrastructure.EntityKeyFields entityKeys)
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}
