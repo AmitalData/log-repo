e8254768a21f3a91e61d284a5fdb3a8ace57f5fb
mirit <Mirit@amital.co.il>
2018-12-12 23:13:18 +0200

    45924
    Related Work Items: #45924

--- Logitude/Logitude.OracleDatabaseMigration/LogitudeModel/LogitudeMigrationContext.cs
+++ Logitude/Logitude.OracleDatabaseMigration/LogitudeModel/LogitudeMigrationContext.cs
@@ -899,6 +899,7 @@ namespace Logitude.OracleDatabaseMigration.LogitudeModel
         public IDbSet<ClaimsRelatedEntitiesRefund> ClaimsRelatedEntitiesRefund { get; set; }
         public IDbSet<SeizureFactorType> SeizureFactorType { get; set; }
         public IDbSet<MamanSpecialAction> MamanSpecialAction { get; set; }
+        public IDbSet<MamanSpecialActionStatus> MamanSpecialActionStatus { get; set; }
 
         #endregion
 
@@ -3901,6 +3902,7 @@ namespace Logitude.OracleDatabaseMigration.LogitudeModel
             modelBuilder.Configurations.Add(new ClaimsRelatedEntitiesRefundMap());
             modelBuilder.Configurations.Add(new SeizureFactorTypeMap());
             modelBuilder.Configurations.Add(new MamanSpecialActionMap());
+            modelBuilder.Configurations.Add(new MamanSpecialActionStatusMap());
 
             #endregion
 
