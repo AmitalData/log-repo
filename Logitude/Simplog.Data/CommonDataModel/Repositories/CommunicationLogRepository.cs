53f9e0ec79e5519f6e4eba9b2f0f5c97f056d68b
itzik M <itzik@amital.co.il>
2018-12-19 09:32:49 +0200

    CommunicationLogRepository.cs

--- Logitude/Simplog.Data/CommonDataModel/Repositories/CommunicationLogRepository.cs
+++ Logitude/Simplog.Data/CommonDataModel/Repositories/CommunicationLogRepository.cs
@@ -29,6 +29,12 @@ namespace Simplog.Data.CommonDataModel.Repositories
 
         public CommunicationLog GetSingleCommunicationLog(string id,int tenant)
         {
+            var q = (from a in context.CommunicationLogs.Include("CommunicationLogType").Include("CommunicationStatusType").Include("CreatedByUser").Include("CreatedByUser.Contact").Include("ObjectTable").Include("InternalDocument").Include("ExternalDocument").Include("Document").Include("CurrentTenant")
+                     where a.Id == id
+                     select a);
+            var log= q.FirstOrDefault();
+            return log;
+
             return (from a in context.CommunicationLogs.Include("CommunicationLogType").Include("CommunicationStatusType").Include("CreatedByUser").Include("CreatedByUser.Contact").Include("ObjectTable").Include("InternalDocument").Include("ExternalDocument").Include("Document").Include("CurrentTenant")
                     where a.Id == id
                     select a).FirstOrDefault();
