6e62b38cb20a089f7fd4b6984bc074d8045ec6aa
itzik M <itzik@amital.co.il>
2018-12-19 07:55:31 +0200

    config aot

--- Logitude/AngularModules/AngularModules/webpack.config-prod-Cust.js
+++ Logitude/AngularModules/AngularModules/webpack.config-prod-Cust.js
@@ -549,8 +549,8 @@ module.exports = {
           ],
           "perChunkOutput": false,
           //"outputTemplate": "C:\\source\\log-repo\\Logitude\\AngularModules\\AngularModules\\node_modules\\license-webpack-plugin\\output.template.ejs",
-          "outputTemplate": "D:\\Customs\\Logitude\\AngularModules\\AngularModules\\node_modules\\license-webpack-plugin\\output.template.ejs",
-            //"outputTemplate": "c:\\log\\Logitude\\AngularModules\\AngularModules\\node_modules\\license-webpack-plugin\\output.template.ejs",
+          //"outputTemplate": "D:\\Customs\\Logitude\\AngularModules\\AngularModules\\node_modules\\license-webpack-plugin\\output.template.ejs",
+          "outputTemplate": "c:\\log\\Logitude\\AngularModules\\AngularModules\\node_modules\\license-webpack-plugin\\output.template.ejs",
           
           "outputFilename": "3rdpartylicenses.txt",
           "suppressErrors": true,
