9c2ada351ff3ad08bf8f3efcad069e1d6214b10a
alaa <alaa.jamal.h@live.com>
2018-12-23 22:02:25 +0200

    45766
    Related Work Items: #45766

--- Logitude/AngularModules/AngularModules/Accounting/Components/Others/CancelChequeComponent.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/Others/CancelChequeComponent.ts
@@ -1,4 +1,4 @@
-﻿import {BaseComponent} from '../../../Infrastructure/Components/LogitudeComponents/BaseComponent';
+import {BaseComponent} from '../../../Infrastructure/Components/LogitudeComponents/BaseComponent';
 import {Component}  from '@angular/core';
 import {PaymentChequePM} from '../../EntityPMs/PaymentChequePM';
 import {SessionLocator} from '../../../Infrastructure/Utilities/SessionLocator';
@@ -6,6 +6,7 @@ import {PaymentChequePMService} from '../../Services/StandardPMs/PaymentChequePM
 import {ServiceResponse} from '../../../Infrastructure/DataContracts/ServiceResponse';
 import {AppTool} from '../../../Infrastructure/Tools';
 import {TextCodeTranslator} from '../../../Infrastructure/Utilities/TextCodeTranslator';
+import { JournalExtendedPMService } from '../../Services/ExtendedPMs/JournalExtendedPMService';
 
 
 
@@ -76,9 +77,21 @@ export class CancelChequeComponent extends BaseComponent {
                 var mm: ServiceResponse = myResult;
                 if (!mm.HasError) {
                     var entity = mm.Result;
-                    SessionLocator.CurrentSession.CloseCurrentWindowEmit("ok");
-                    SessionLocator.CurrentSession.StopBusyIndicator();
+                   
+                    let myJournalExtendedPMService: JournalExtendedPMService = new JournalExtendedPMService();
+                    myJournalExtendedPMService
+                        .VoidJournal(this.entityPM.Tenant, this.entityPM.JournalId, "", "", "")
+                        .subscribe((res: ServiceResponse) => {
+                           // SessionLocator.CurrentSession.CloseCurrentWindowEmit("ok");
+                            SessionLocator.CurrentSession.StopBusyIndicator();
 
+                            if (res.HasError) {
+                                this.ValidationErrorsList = res.ErrorsArray;
+
+                            }
+                            
+                        });
+                  
 
                 }
                 else {
