6b41d31c28fd4e714944c77f96d2d2ccfbc23f7d
Mohammad Masoud <mohammad@logitudeworld.com>
2018-12-31 10:18:54 +0200

    for the last commit
    Related Work Items: #46407

--- Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/DetailClasses/ObjectFieldsDetails.cs
+++ Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/DetailClasses/ObjectFieldsDetails.cs
@@ -25,6 +25,7 @@ namespace WebFreight.Web.MetaDataUpdate.DetailClasses
         public string ControlField2 { get; set; }
         public string ControlField3 { get; set; }
         public bool DisplayOnLookUp { get; set; }
+        public bool DisplayOnLookUpLocal { get; set; }
         public bool Isoveridden { get; set; }
         public int Tenant { get; set; }
         public string ObjectTableName { get; set; }
