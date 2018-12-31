1db2bd2561512be0cdd39ba0794319fc4eec334a
Samar Abu Eid <Samar@logitudeworld.com>
2018-12-31 09:09:44 +0200

    46698 46677
    Related Work Items: #46677, #46698

--- Logitude/AngularModules/AngularModules/InfrastructureModules/InfrastructureDocuments/Components/DocumentComponent/PrintDocumentComponent.ts
+++ Logitude/AngularModules/AngularModules/InfrastructureModules/InfrastructureDocuments/Components/DocumentComponent/PrintDocumentComponent.ts
@@ -1,4 +1,4 @@
-﻿declare var System: any;
+declare var System: any;
 declare var window: any;
 import {TextCodeTranslator} from '../../../../Infrastructure/Utilities/TextCodeTranslator';
 import {BaseComponent} from '../../../../Infrastructure/Components/LogitudeComponents/BaseComponent';
@@ -299,6 +299,7 @@ export class PrintDocumentComponent extends BaseComponent implements OnInit {
                 case "CRCCB":
                 case "OMBC":
                 case "WHL":
+                case "DESCH":
                     return true;
 
                 default:
