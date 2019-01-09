77e6d85235b3f604b4859a33294d91790eafc7ff
mirit <Mirit@amital.co.il>
2019-01-09 12:20:14 +0200

    47143
    Related Work Items: #47143

---
+++ Logitude/Logitude.OracleDatabaseMigration/Migrations/201901091013411_AddMigrationRefundCustomerActivityType.cs
@@ -0,0 +1,16 @@
+namespace Logitude.OracleDatabaseMigration.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMigrationRefundCustomerActivityType : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
