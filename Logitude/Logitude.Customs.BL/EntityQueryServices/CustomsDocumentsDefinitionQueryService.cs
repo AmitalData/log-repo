ab8b0fa562026900a3bffc3c232d7980b6fb3568
itzik M <itzik@amital.co.il>
2019-01-15 13:17:40 +0200

    hdesk call

--- Logitude/Logitude.Customs.BL/EntityQueryServices/CustomsDocumentsDefinitionQueryService.cs
+++ Logitude/Logitude.Customs.BL/EntityQueryServices/CustomsDocumentsDefinitionQueryService.cs
@@ -32,7 +32,7 @@ namespace Logitude.Customs.BL.EntityQueryServices
         {
             if(String.IsNullOrWhiteSpace(cargoTypeCode) || String.IsNullOrWhiteSpace(processTypeCode) || String.IsNullOrWhiteSpace(transportTypeCode))
             {
-                return null;
+                return new List<CustomsDocumentsDefinitionPM>();
             }
             
             List <CustomsDocumentsDefinition> CustomsDocumentsDefinitions = repository.GetCustomsDocumentsDefinitionsForDeclaration(cargoTypeCode, processTypeCode, transportTypeCode, tenant);
