e28e14fb30f90cea1668de5868def553655b6fce
Maheera Jazi <Maheera@logitudeworld.com>
2019-01-19 21:04:08 +0200

    #47328

--- Logitude/AngularModules/AngularModules/InfrastructureModules/InfrastructureBIReport/Components/Workspaces/BIReportPreviewComponent.ts
+++ Logitude/AngularModules/AngularModules/InfrastructureModules/InfrastructureBIReport/Components/Workspaces/BIReportPreviewComponent.ts
@@ -260,7 +260,7 @@ export class BIReportPreviewComponent implements OnInit {
             confirmWindow.YesButtonText = "Save ";
             confirmWindow.CancelButtonText = "Cancel";
             confirmWindow.Title = TextCodeTranslator.Translate("General.O.UnSavedChanges");
-            confirmWindow.Show("This Email has unsaved changes. Do you want to save it?");
+            confirmWindow.Show("This Report has unsaved changes. Do you want to save it?");
             confirmWindow.WindowClosed.subscribe((event: any) => {
                 if (confirmWindow.Yes) {
                     // save
