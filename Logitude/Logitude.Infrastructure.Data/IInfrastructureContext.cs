8cdc249ee81cadcbe496381a208a638546035a30
Samar Abu Eid <Samar@logitudeworld.com>
2019-01-27 09:57:05 +0200

    46338
    Related Work Items: #46338

--- Logitude/Logitude.Infrastructure.Data/IInfrastructureContext.cs
+++ Logitude/Logitude.Infrastructure.Data/IInfrastructureContext.cs
@@ -23,10 +23,12 @@ namespace Logitude.Infrastructure.Data
 		 IDbSet<BIReportsType> BIReportsTypes { get; }
 		 IDbSet<BusinessProcessQueue> BusinessProcessQueues { get; }
 		 IDbSet<BusinessRole> BusinessRoles { get; }
+		 IDbSet<FeatureToggle> FeatureToggles { get; }
 		 IDbSet<LBPTeamMember> LBPTeamMembers { get; }
 		 IDbSet<SharedLogisticsSetting> SharedLogisticsSettings { get; }
 		 IDbSet<Team> Teams { get; }
 		 IDbSet<TeamMemberBusinessRole> TeamMemberBusinessRoles { get; }
+		 IDbSet<Toggle> Toggles { get; }
 	 
          void SetAsModified(object entity);
          void DetectChanges();
