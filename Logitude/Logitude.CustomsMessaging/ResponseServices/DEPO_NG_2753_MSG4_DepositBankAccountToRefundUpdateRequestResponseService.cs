65457b7a17d9a9b6e5d488d51ae170b9b9b427b4
moran <moran@amital.co.il>
2019-01-28 17:57:15 +0200

    ms 47066
    Related Work Items: #47066

--- Logitude/Logitude.CustomsMessaging/ResponseServices/DEPO_NG_2753_MSG4_DepositBankAccountToRefundUpdateRequestResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/DEPO_NG_2753_MSG4_DepositBankAccountToRefundUpdateRequestResponseService.cs
@@ -151,7 +151,7 @@ namespace Logitude.CustomsMessaging.ResponseServices
             newNotificationPM.AssigneToNotificationTypeCode = typeCode;
             newNotificationPM.EntityId = _MyDepositPM.Id;
             newNotificationPM.ObjectTableId = ObjectTableRepository.GetObjectTableByName("Customs.Deposit");
-            if(!String.IsNullOrWhiteSpace(_MyDepositPM.TapagNumber)) newNotificationPM.Reference2Number = _MyDepositPM.Id;
+            if(!String.IsNullOrWhiteSpace(_MyDepositPM.TapagID)) newNotificationPM.Reference2Number = _MyDepositPM.TapagID;
             string customerId = null;
             string referentUserId = null;
             if (connectedDeclarationPM != null)
