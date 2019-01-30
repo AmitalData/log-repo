ba8436c216fa2dc0fe2c971a1c44fbfe60e34028
mirit <Mirit@amital.co.il>
2019-01-30 12:23:06 +0200

    47157
    Related Work Items: #47157

--- Logitude/Logitude.Customs.BL/Validators/CustomsRequiredFieldsValidator.cs
+++ Logitude/Logitude.Customs.BL/Validators/CustomsRequiredFieldsValidator.cs
@@ -1123,6 +1123,15 @@ namespace Logitude.Customs.BL.Validators
 
                         }
                     }
+                    if (info.Name == "GatewayPortCode")
+                    {
+                        if (info.GetValue(courierMaster) == null)
+                        {
+
+                            requiredErrors.RequiredFields.Add(new CustomsRequiredFieldsErrorItem() { FieldName = info.Name, TableName = "Customs.CourierMaster" });
+
+                        }
+                    }
                 }
             }
 
@@ -1236,7 +1245,7 @@ namespace Logitude.Customs.BL.Validators
                 foreach (PropertyInfo info in ConsignmentProperties)
                 {
                     //if(info.Name == "StorageSiteCode" || info.Name == "LoadingPortCode" || info.Name == "ThirdCargoID"|| info.Name == "ManifestNumber" || info.Name== "UnloadDate" || info.Name== "CargoDescription" || info.Name == "DeliveryPlaceName")
-                    if (info.Name == "StorageSiteCode" || info.Name == "LoadingPortCode" || info.Name == "ThirdCargoID" || info.Name == "ManifestNumber" || info.Name == "UnloadDate" || info.Name == "CargoDescription")//task 46459
+                    if (info.Name == "StorageSiteCode"  || info.Name == "ThirdCargoID" || info.Name == "ManifestNumber" || info.Name == "CargoDescription")//task 46459 // task 47157
                     {
                         if (info.GetValue(Consignment) == null)
                         {
