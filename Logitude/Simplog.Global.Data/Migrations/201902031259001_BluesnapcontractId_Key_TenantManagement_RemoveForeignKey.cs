ed650d5697a7c256e3ee1d8548b28ecba307cb64
Lana Daoud <Lana@logitudeworld.com>
2019-02-04 08:58:25 +0200

    khalid // fix

--- Logitude/Simplog.Global.Data/Migrations/201902031259001_BluesnapcontractId_Key_TenantManagement_RemoveForeignKey.cs
+++ Logitude/Simplog.Global.Data/Migrations/201902031259001_BluesnapcontractId_Key_TenantManagement_RemoveForeignKey.cs
@@ -8,7 +8,9 @@ namespace Simplog.Global.Data.Migrations
         public override void Up()
         {
             Sql("declare @ID as varchar(15) declare @Code as varchar(15) BEGIN DECLARE Bluesnapcontract CURSOR READ_ONLY FOR SELECT Code, Id FROM Bluesnapcontracts OPEN Bluesnapcontract FETCH NEXT FROM Bluesnapcontract INTO @Code, @Id WHILE @@FETCH_STATUS = 0 BEGIN if  exists(select BluesnapContractId from TenantManagements where BluesnapContractId = @Code) begin print(@Code) update TenantManagements set BluesnapContractId = @Id where BluesnapContractId = @Code end FETCH NEXT FROM Bluesnapcontract INTO @Code ,@Id END CLOSE Bluesnapcontract DEALLOCATE Bluesnapcontract END");
-
+              DropForeignKey("dbo.TenantManagements", "BluesnapContractId", "dbo.BluesnapContracts");
+              DropIndex("dbo.TenantManagements", new[] { "BluesnapContractId" });
+            AlterColumn("dbo.TenantManagements", "BluesnapContractId", c => c.String(maxLength: 15, unicode: false));
             DropPrimaryKey("dbo.BluesnapContracts");
             AddPrimaryKey("dbo.BluesnapContracts", "Id");
             CreateIndex("dbo.TenantManagements", "BluesnapContractId");
