6f8f88127a48b03ce5e654f7a08764b5957ceff4
alaa <alaa.jamal.h@live.com>
2019-02-11 11:28:42 +0200

    46384
    Related Work Items: #46384

---
+++ Logitude/AngularModules/AngularModules/Accounting/Components/EditTabs/TaxDeductionReport/TaxDeductionReportGeneralTabComponent.ts
@@ -0,0 +1,61 @@
+import { Component, Output, EventEmitter, OnInit, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import { BaseComponent } from '../../../../Infrastructure/Components/LogitudeComponents/BaseComponent';
+import { TaxDeductionReportPM } from '../../../EntityPMs/TaxDeductionReportPM';
+import { EntityArgs } from '../../../../Infrastructure/DataContracts/EntityArgs';
+import { ObjectsLocator } from '../../../../Infrastructure/Locators/ObjectsLocator';
+import { SessionLocator } from '../../../../Infrastructure/Utilities/SessionLocator';
+import { TaxDeductionReportExtendedPMService } from '../../../Services/ExtendedPMs/TaxDeductionReportExtendedPMService';
+import { ServiceResponse } from '../../../../Infrastructure/DataContracts/ServiceResponse';
+import { BatchTaskExecutionListService } from '../../../../Infrastructure/Services/StandardLists/BatchTaskExecutionListService';
+
+
+@Component({
+    moduleId: module.id,
+    templateUrl: './TaxDeductionReportGeneralTabComponent.html'
+})
+
+export class TaxDeductionReportGeneralTabComponent extends BaseComponent {
+
+    public DataContext: any = this;
+    ObjectTableName: string = "TaxDeductionReport";
+    entityPM: TaxDeductionReportPM;
+    isRTL: boolean = false;
+    showLocals: boolean = false;
+    taxDeductionReportExtendedPMService: TaxDeductionReportExtendedPMService = new TaxDeductionReportExtendedPMService();
+    _BatchTaskExecutionListService: BatchTaskExecutionListService = new BatchTaskExecutionListService();
+    Faild: boolean = false;
+
+    
+
+    constructor(private entityArgs: EntityArgs) {
+        super();
+        this.entityPM = entityArgs.EntityPM;
+        if (ObjectsLocator.GlobalSetting) this.isRTL = (ObjectsLocator.GlobalSetting.LayoutDirection == "rtl");
+        this.showLocals = !SessionLocator.LoggedUserPM.DontShowLocal;
+        this.UIProperties.SetEnabled("IsAdditionalReportExist", "TaxDeductionReport", false);
+        this.UIProperties.SetEnabled("Email", "TaxDeductionReport", false);
+
+        if (this.entityPM.StatusTypeCode == "4") {
+            this.Faild = true;
+        }
+    }
+
+    get IsAdditionalReportExist() { return this.entityPM.IsAdditionalReportExist; }
+
+    get Email() { return this.entityPM.Email; }
+    RunService() {
+
+        this.taxDeductionReportExtendedPMService.DownloadTaxDeduction856FileInBatch(this.entityPM).subscribe(myResult => {
+            var mm: ServiceResponse = myResult;
+            var entity = mm.Result;
+    
+            
+
+        });
+
+
+    }
+
+  
+}
+
--- Logitude/CommunicationWorkerRole/ThreadedRoleEntryPoint.cs
+++ Logitude/CommunicationWorkerRole/ThreadedRoleEntryPoint.cs
@@ -280,7 +280,7 @@ namespace CommunicationWorkerRole
             var tst = false;
             if (tst)
             {
-                BatchServicesDefinitions = BatchServicesDefinitions.Where(r => r.ClassName == "SchedularWorkerRole").ToList();
+                BatchServicesDefinitions = BatchServicesDefinitions.Where(r => r.ClassName == "BatchTaskExecutionWR").ToList();
             }
             foreach (var Service in BatchServicesDefinitions)
             {
