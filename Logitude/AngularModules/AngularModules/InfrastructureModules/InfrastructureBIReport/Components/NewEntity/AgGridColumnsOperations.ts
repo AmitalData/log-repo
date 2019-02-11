091397f9e149fbd0afda92c0a86bb99a3c76aeb7
Maheera Jazi <Maheera@logitudeworld.com>
2019-02-11 17:05:10 +0200

    #47733

--- Logitude/AngularModules/AngularModules/InfrastructureModules/InfrastructureBIReport/Components/NewEntity/AgGridColumnsOperations.ts
+++ Logitude/AngularModules/AngularModules/InfrastructureModules/InfrastructureBIReport/Components/NewEntity/AgGridColumnsOperations.ts
@@ -14,7 +14,7 @@ export class AgGridColumnsOperations extends BaseComponent {
     public ItemsSource: any[];
     private father: BIReportPreviewComponent;
     public IsAll = false;
-    private itemSource_Unsaved: Column[] = []; 
+    private itemSource_Unsaved: Array<Column>; 
 
     constructor() {
         super();
@@ -28,7 +28,7 @@ export class AgGridColumnsOperations extends BaseComponent {
     }
     SetWindowArgs(args: any) {
         this.father = args.father;
-        this.itemSource_Unsaved = [];
+        this.itemSource_Unsaved = new Array();
         this.father.BIReportXMLData.BITabularViewSettings.Columns.forEach(item => {
             this.itemSource_Unsaved.push(item);
         });
