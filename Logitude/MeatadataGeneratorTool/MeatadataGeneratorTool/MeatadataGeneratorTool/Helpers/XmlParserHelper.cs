343cc1fbb314d6906ac455559eac760ea692bcd5
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2019-02-13 10:32:38 +0200

    48271
    Related Work Items: #48271

--- Logitude/MeatadataGeneratorTool/MeatadataGeneratorTool/MeatadataGeneratorTool/Helpers/XmlParserHelper.cs
+++ Logitude/MeatadataGeneratorTool/MeatadataGeneratorTool/MeatadataGeneratorTool/Helpers/XmlParserHelper.cs
@@ -871,6 +871,17 @@ namespace MeatadataGeneratorTool.Helpers
                 DCField.IsCompositKey = false;
 
             }
+            try
+            {
+                DCField.CloseTableCode = GetAttributeStringValue(fieldNode.Attributes["CloseTableCode"]);
+
+            }
+            catch (Exception)
+            {
+                //DCField.CloseTableCode = false;
+
+            }
+
 
             return DCField;
         }
