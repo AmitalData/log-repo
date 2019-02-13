f2f47c7b1a5319e8a9703656a9eed245c7d45702
Abdullah Dawoud <abdullah@logitudeworld.com>
2019-02-13 14:52:12 +0200

    ARPayment field and new payemnt mod
    Related Work Items: #48056, #48057

--- Logitude/Logitude.BL/InvoiceModel/Tools/DataMapping/ARPaymentMapping.cs
+++ Logitude/Logitude.BL/InvoiceModel/Tools/DataMapping/ARPaymentMapping.cs
@@ -89,6 +89,7 @@ namespace Logitude.BL.InvoiceModel.Tools.DataMapping
             entity.ApprovedDate = entityPM.ApprovedDate;
             entity.ApprovedByUserId = entityPM.ApprovedByUserId;
             entity.FirstApproveDate = entityPM.FirstApproveDate;
+            entity.IsFullAccounting = entityPM.IsFullAccounting;
 
             if (entityPM.StatusCode == "AD" && entityPM.OpenAmount == 0)
             {
