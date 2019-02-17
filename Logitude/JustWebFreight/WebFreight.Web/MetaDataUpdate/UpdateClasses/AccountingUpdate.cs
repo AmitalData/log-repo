3fd23692b5e5995e31475e0217714b040df098d1
alaa <alaa.jamal.h@live.com>
2019-02-17 23:15:10 +0200

    48680
    Related Work Items: #48680

--- Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/UpdateClasses/AccountingUpdate.cs
+++ Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/UpdateClasses/AccountingUpdate.cs
@@ -1752,7 +1752,10 @@ namespace WebFreight.Web.MetaDataUpdate.GeneratedUpdate
             //AddTextCodes.AddTextCode(new TextCodeDetails() { Code = "Accounting.O.ARP.xxxxxx", DefaultText = "xxxxxxxx", LocalDefaultText = "yyyyyyyy", ObjectTableId = objectTable.Id, Tenant = 0, TextCodeTypeCode = "O", }, textCodeRepository, textcodes);
 
             AddTextCodes.AddTextCode(new TextCodeDetails() { Code = "Accounting.O.DocumentTypeNotFound", DefaultText = "Document type X not found", LocalDefaultText = "סוג מסמך X לא נמצא", ObjectTableId = objectTable.Id, Tenant = 0, TextCodeTypeCode = "O", }, textCodeRepository, textcodes);
+            AddTextCodes.AddTextCode(new TextCodeDetails() { Code = "Accounting.O.DeductionFileNumberNotFound", DefaultText = "Deduction file number not found", LocalDefaultText = "לא הוקלד מספר תיק ניכויים", ObjectTableId = objectTable.Id, Tenant = 0, TextCodeTypeCode = "O", }, textCodeRepository, textcodes);
 
+
+            
             #region MainMenu
 
 
