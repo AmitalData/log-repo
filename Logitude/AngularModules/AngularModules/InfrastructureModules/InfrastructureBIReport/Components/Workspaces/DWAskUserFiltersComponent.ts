f5bd261154aa214cb258752c379167019d66d801
Maheera Jazi <Maheera@logitudeworld.com>
2019-02-18 11:12:52 +0200

    #bi report

--- Logitude/AngularModules/AngularModules/InfrastructureModules/InfrastructureBIReport/Components/Workspaces/DWAskUserFiltersComponent.ts
+++ Logitude/AngularModules/AngularModules/InfrastructureModules/InfrastructureBIReport/Components/Workspaces/DWAskUserFiltersComponent.ts
@@ -81,10 +81,9 @@ export class DWAskUserFiltersComponent implements OnInit {
             var QueryData = new DWQueryData();
             QueryData.Columns = this.DWQueryData.Columns;
             QueryData.Filters = this.SelectedFiltersDataSource[0];
-
-
+            
             QueryData.PageIndex = this.DWQueryData.PageIndex;
-            QueryData.PageSize = 2;
+            QueryData.PageSize = this.DWQueryData.PageSize;
 
             QueryData.ColumnsSort = this.DWQueryData.ColumnsSort;
 
