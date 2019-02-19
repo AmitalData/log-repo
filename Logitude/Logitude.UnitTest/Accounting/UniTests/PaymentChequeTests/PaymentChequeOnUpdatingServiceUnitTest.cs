d19caee2cf9f49bc612d0c46edf38148350c8a28
alaa <alaa.jamal.h@live.com>
2019-02-19 11:44:49 +0200

    46014
    Related Work Items: #46014

--- Logitude/Logitude.UnitTest/Accounting/UniTests/PaymentChequeTests/PaymentChequeOnUpdatingServiceUnitTest.cs
+++ Logitude/Logitude.UnitTest/Accounting/UniTests/PaymentChequeTests/PaymentChequeOnUpdatingServiceUnitTest.cs
@@ -14,7 +14,7 @@ using Logitude.Accounting.BL.EntityUpdateServices;
 namespace Logitude.UnitTest.Accounting.UniTests.PaymentChequeTests
 {
     [TestClass]
-    public class PaymentChequeOnUpdatingServiceUnitTest
+    public class PaymentChequeOnUpdatingServiceUnitTest :TestBase
     {
 
         [TestMethod]
@@ -53,7 +53,65 @@ namespace Logitude.UnitTest.Accounting.UniTests.PaymentChequeTests
 
         }
 
-       
+
+
+        [TestMethod]
+        public void OnUpdating_CreatingJournal_Success()
+        {
+            int tenant = 1;
+            var expcted_IdCounter = "1-1";
+            var expcted_Number = "1000";
+            var expcted_LogId = GetLoggedContactInstance().Id;
+
+            //arrange
+            var PaymentChequePM = new PaymentChequePM()
+            {
+                Tenant = tenant,
+                CreatedByUserId = expcted_LogId,
+                Id = expcted_IdCounter,
+                InternalNumber = expcted_Number,
+                ChequeNumber ="11",
+                UpdatedByUserId = expcted_LogId,
+                PaymentChequeStatusCode="2",
+                BankAccountId ="1-1"
+
+            };
+
+            JournalPM journalPM = new JournalPM()
+            {
+                Tenant = 1,
+                AccountingEntityReference = PaymentChequePM.ChequeNumber,           
+                UpdatedByUserId = PaymentChequePM.UpdatedByUserId,
+            
+            };
+
+            BankAccountPM bankAccountPM = new BankAccountPM()
+            {
+                Tenant = 1,
+               ChequeCounter =1,
+                BankId = PaymentChequePM.BankAccountId,
+            };
+
+
+            var PaymentChequeUpdateService = A.Fake<PaymentChequeOnUpdatingService>(option => option.CallsBaseMethods());
+
+           // A.CallTo(() => PaymentChequeUpdateService.GetLogContactId(PaymentChequePM.Tenant)).Returns(expcted_LogId);
+            A.CallTo(() => PaymentChequeUpdateService.GetCurrentDateTime(PaymentChequePM.Tenant)).Returns(DateTime.Now);
+            A.CallTo(() => PaymentChequeUpdateService.CreateJournalPM(PaymentChequePM)).Returns(journalPM);
+            A.CallTo(() => PaymentChequeUpdateService.GetSingleBankAccountPM(PaymentChequePM)).Returns(bankAccountPM);
+
+            //act
+            PaymentChequeUpdateService.OnUpdating(PaymentChequePM, null);
+
+            ///check
+            Assert.AreEqual(journalPM.TypeCode, "0", "does not exist");
+            Assert.AreEqual(expcted_IdCounter, PaymentChequePM.Id);
+            Assert.AreEqual(expcted_Number.ToString(), PaymentChequePM.InternalNumber, "does not exist");
+
+
+        }
+
+
 
         private ContactPM GetLoggedContactInstance()
         {
