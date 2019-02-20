9756cd3d3bd6b40769230509b040ee88f3f90ea4
Abdullah Dawoud <abdullah@logitudeworld.com>
2019-02-20 13:59:54 +0200

    Related Work Items: #48840

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/LogTextBoxComponent.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/LogTextBoxComponent.ts
@@ -263,6 +263,20 @@ export class LogTextBoxComponent implements OnInit, AfterViewInit, OnDestroy {
             }, 1);
 
         }
+
+        //check rowscount
+        if(this.IsMultiline){
+            var _element = document.getElementById(this.InputId)
+            var elHeight = _element.clientHeight;
+            var calculatedRowsCount = (elHeight / 18);
+            var ___roundedCalculatedRowsCountHaha = Math.trunc(calculatedRowsCount);
+
+            if(this.RowsCount != ___roundedCalculatedRowsCountHaha)
+                this.RowsCount = ___roundedCalculatedRowsCountHaha;
+
+
+        }
+
     }
 
     RunComponent() {
