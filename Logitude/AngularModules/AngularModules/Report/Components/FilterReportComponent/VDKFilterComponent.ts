a111ed684f509573d927a711caeb2d968111d0ae
Khalid Khatib <khalid@logitudeworld.com>
2019-02-21 13:28:46 +0200

    VDK Report issue
    Related Work Items: #48886, #48912

--- Logitude/AngularModules/AngularModules/Report/Components/FilterReportComponent/VDKFilterComponent.ts
+++ Logitude/AngularModules/AngularModules/Report/Components/FilterReportComponent/VDKFilterComponent.ts
@@ -5,6 +5,7 @@ import { ReportFliter } from '../../Components/Filters/ReportFliter';
 import { QueryFilterItem } from '../../Components/Filters/QueryFilterItem';
 import { Component } from '@angular/core';
 import { DateTool } from '../../../Infrastructure/Tools';
+import { SessionLocator } from '../../../Infrastructure/Utilities/SessionLocator';
 
 @Component({
     moduleId: module.id,
@@ -21,7 +22,7 @@ export class VDKFilterComponent extends BaseComponent {
     public ValidationErrorsList: string[] = [];
     queryFilterItems: QueryFilterItem[];
     queryFilterItem: QueryFilterItem;
-
+    public SupplierValues: string = "CS";
     ToDate: Date;
     FromDate: Date;
     private shipmentCustomerTypeCode: string;
@@ -72,6 +73,12 @@ export class VDKFilterComponent extends BaseComponent {
 
     constructor() {
         super();
+        if (SessionLocator.TenantPM.AllowAgentInCustomersLOV) {
+            this.SupplierValues = "CS,AG";
+        }
+        else {
+            this.SupplierValues = "CS";
+        }
     }
 
     InitializeComponent(myReportsPreview: ReportsPreviewComponent) {
