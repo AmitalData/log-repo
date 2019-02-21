847f1cede0afbb6f3a929ac6031a56be5771819f
Alexander Khitrik <alexanderh@amital.co.il>
2019-02-21 15:02:45 +0200

    Alexander: Year transfer journal amounts
    Related Work Items: #48463

--- Logitude/Logitude.Accounting.BL/CoreBL/YearTransferService.cs
+++ Logitude/Logitude.Accounting.BL/CoreBL/YearTransferService.cs
@@ -124,7 +124,7 @@ namespace Logitude.Accounting.BL.CoreBL
                                           .OrderBy(r => r.CurrencyId)
                                           join glAcc in allRevenueExpenseCards.OrderBy(r => r.RevenueExpenseType)
                                           on myCurrencySum.AccountId equals glAcc.Id
-                                          select GetJournalLine(GetMyEnum( glAcc.RevenueExpenseType), myCurrencySum, journal, RevenueExpenseGLAccountId));
+                                          select GetJournalLine(GetMyEnum( glAcc.RevenueExpenseType), myCurrencySum, journal, RevenueExpenseGLAccountId, glAcc.RevenueExpenseType));
                                           //select GetJournalLine(MyJournalActionTypeEnum.Credit, myCurrencySum, journal, RevenueExpenseGLAccountId));
 
             journal.JournalLines.AddRange(journalLinesOfChildAcc);
@@ -152,9 +152,9 @@ namespace Logitude.Accounting.BL.CoreBL
             }
         }
 
-        private void CreateJLinesAganistMainREGLAcc(string RevenueExpenseGLAccountId, List<GLAccountAndMoreDTO> allRevenueExpenseCards, List<CurrencySum> totalBalance, JournalPM journal, string RevenueExpenseType,MyJournalActionTypeEnum journalActionTypeEnum)
+        private void CreateJLinesAganistMainREGLAcc(string RevenueExpenseGLAccountId, List<GLAccountAndMoreDTO> allRevenueExpenseCards, List<CurrencySum> totalBalance, JournalPM journal, string revenueExpenseType,MyJournalActionTypeEnum journalActionTypeEnum)
         {
-            var Type1Ids = allRevenueExpenseCards.Where(r => r.RevenueExpenseType == RevenueExpenseType).Select(r => r.Id).ToList();
+            var Type1Ids = allRevenueExpenseCards.Where(r => r.RevenueExpenseType == revenueExpenseType).Select(r => r.Id).ToList();
 
             var TypeJL
                 =
@@ -170,7 +170,7 @@ namespace Logitude.Accounting.BL.CoreBL
                      LocalAmountDebit = g.Sum(r => r.LocalAmountDebit),
 
                  }
-                 select GetJournalLine(journalActionTypeEnum, myCurrencySumGroup1, journal, "")
+                 select GetJournalLine(journalActionTypeEnum, myCurrencySumGroup1, journal, "", revenueExpenseType)
                  );
             //
             journal.JournalLines.AddRange(TypeJL);
@@ -183,7 +183,7 @@ namespace Logitude.Accounting.BL.CoreBL
         }
 
 
-        private JournalLinePM GetJournalLine(MyJournalActionTypeEnum journalActionTypeEnum, CurrencySum myCurrencySum, JournalPM journal, string RevenueExpenseGLAccountId)
+        private JournalLinePM GetJournalLine(MyJournalActionTypeEnum journalActionTypeEnum, CurrencySum myCurrencySum, JournalPM journal, string RevenueExpenseGLAccountId, string revenueExpenseType)
         {
             int tenant = journal.Tenant;
             bool useLocal = true;
@@ -202,6 +202,11 @@ namespace Logitude.Accounting.BL.CoreBL
                 Notes = TranslateTextsClassTranslate("General.MC.ACC.YearTransfer", 0, useLocal),
 
             };
+            if (revenueExpenseType == RevenueType)
+            {
+                journalLine.LocalAmount = -journalLine.LocalAmount;
+                journalLine.ForeignAmount = -journalLine.ForeignAmount;
+            }
             switch (journalActionTypeEnum)
             {
                 
