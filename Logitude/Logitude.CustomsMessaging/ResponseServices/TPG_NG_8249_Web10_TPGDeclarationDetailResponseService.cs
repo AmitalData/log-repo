ab3d8a99b43b87765fbc022ecc657f5ff7404890
mirit <Mirit@amital.co.il>
2019-02-27 15:40:58 +0200

    47772
    Related Work Items: #47772

--- Logitude/Logitude.CustomsMessaging/ResponseServices/TPG_NG_8249_Web10_TPGDeclarationDetailResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/TPG_NG_8249_Web10_TPGDeclarationDetailResponseService.cs
@@ -146,21 +146,22 @@ namespace Logitude.CustomsMessaging.ResponseServices
 
             LogMessagingUtil.Instance.AppendLine("Analyze Manifest Status Query response " + requestParams.DeclarationNumber);
 
-            if (string.IsNullOrWhiteSpace(requestParams.DeclarationId))
-            {
-                var text = "Can not find declaration";
-                if (!string.IsNullOrWhiteSpace(text))
-                {
-                    LogMessagingUtil.Instance.AppendLine(text);
+            //if (string.IsNullOrWhiteSpace(requestParams.DeclarationId))
+            //{
+            //    var text = "Can not find declaration";
+            //    if (!string.IsNullOrWhiteSpace(text))
+            //    {
+            //        LogMessagingUtil.Instance.AppendLine(text);
 
-                    LogMessagingUtil.Instance.AppendLine(text);
-                    this.MyResponseData.Succeeded = true;
-                    this.MyResponseData.HasException = false;
-                    this.MyResponseData.ResponseStatusXML = "";// GetDummyXml(text, customResponse.Cargo);
-                    return;
-                }
-            }
-            else
+            //        LogMessagingUtil.Instance.AppendLine(text);
+            //        this.MyResponseData.Succeeded = true;
+            //        this.MyResponseData.HasException = false;
+            //        this.MyResponseData.ResponseStatusXML = "";// GetDummyXml(text, customResponse.Cargo);
+            //        return;
+            //    }
+            //}
+            //else
+            if (!string.IsNullOrWhiteSpace(requestParams.DeclarationId))
             {
                 this._MyDeclarationPM = myDeclarationQueryService.GetSingle(requestParams.DeclarationId, true, false);
 
@@ -193,104 +194,21 @@ namespace Logitude.CustomsMessaging.ResponseServices
 
                 }
             }
-
-
-            /////////////////////////////////////////////////////////////////////////////////// FILL DATA FOR TESTING
-            //customResponse.GeneralDetails = new TPG_NG_8249_Web10_TPGDeclarationDetailGeneralDetails()
-            //{
-            //    customOfficeName = "customOfficeName",
-            //    customOfficeNumber = 11111,
-            //    declerationStatus = 22222,
-            //    externalID = 33333,
-            //    name = "name",
-            //    statusName = "statusName",
-            //};
-
-            //List<TPG_NG_8249_Web10_TPGDeclarationDetailFileList> claimList = new List<TPG_NG_8249_Web10_TPGDeclarationDetailFileList>();
-            //TPG_NG_8249_Web10_TPGDeclarationDetailFileList claim1 = new TPG_NG_8249_Web10_TPGDeclarationDetailFileList()
-            //{
-            //    agentExternalID = 11111,
-            //    agentName = "agentName",
-            //    claimAmount = 2222,
-            //    closeDate = DateTime.Now,
-            //    createDate = DateTime.Now,
-            //    displayFileNumber = "displayFileNumber",
-            //    fileNumber = "fileNumber",
-            //    Numeral = 1,
-            //    status = 55555,
-            //    statusName = "statusName",
-            //    totalRefundAmount = 66666,
-            //};
-            //claimList.Add(claim1);
-            //claim1 = new TPG_NG_8249_Web10_TPGDeclarationDetailFileList()
-            //{
-            //    agentExternalID = 55,
-            //    agentName = "agentName",
-            //    claimAmount = 66,
-            //    closeDate = DateTime.Now,
-            //    createDate = DateTime.Now,
-            //    displayFileNumber = "displayFileNumber",
-            //    fileNumber = "fileNumber",
-            //    Numeral = 1,
-            //    status = 55555,
-            //    statusName = "statusName",
-            //    totalRefundAmount = 66666,
-            //};
-            //claimList.Add(claim1);
-            //customResponse.Claim = claimList.ToArray();
-
-            //List<TPG_NG_8249_Web10_TPGDeclarationDetailFileList1> deficitList = new List<TPG_NG_8249_Web10_TPGDeclarationDetailFileList1>();
-            //TPG_NG_8249_Web10_TPGDeclarationDetailFileList1 deficit1 = new TPG_NG_8249_Web10_TPGDeclarationDetailFileList1()
-            //{
-            //    agentExternalID = 11111,
-            //    agentName = "agentName",
-            //    closeDate = DateTime.Now,
-            //    displayFileNumber = "displayFileNumber",
-            //    estimatedBalance = 222222,
-            //    fileNumber = "fileNumber",
-            //    Numeral = 3,
-            //    productionDate = DateTime.Now,
-            //    status = 44,
-            //    statusName = "statusName",
-            //    totalRefundAmount = 555,
-            //};
-            //deficitList.Add(deficit1);
-            //customResponse.Deficit = deficitList.ToArray();
-
-
-            //List<TPG_NG_8249_Web10_TPGDeclarationDetailFileList2> guaranteeList = new List<TPG_NG_8249_Web10_TPGDeclarationDetailFileList2>();
-            //TPG_NG_8249_Web10_TPGDeclarationDetailFileList2 guarantee1 = new TPG_NG_8249_Web10_TPGDeclarationDetailFileList2()
-            //{
-            //    agentExternalID = 11111,
-            //    agentName = "agentName",
-            //    Amount = 22222,
-            //    displayFileNumber = "displayFileNumber",
-            //    fileNumber = "fileNumber",
-            //    fileType = 33,
-            //    fileTypeName = "fileTypeName",
-            //    guaranteeStatus = 44,
-            //    guaranteeStatusName = "guaranteeStatusName",
-            //    Numeral = 5,
-            //    validity = DateTime.Now,
-
-            //};
-            //guaranteeList.Add(guarantee1);
-            //customResponse.Guarantee = guaranteeList.ToArray();
-            /////////////////////////////////////////////////////////////////////////////////// FILL DATA FOR TESTING
-            
-            
             
             if (customResponse.GeneralDetails != null)
             {
                 MyResponseData.GeneralDetailsData = new DeclarationFilterResponseData.GeneralDetails()
                 {
                     customOfficeName = customResponse.GeneralDetails.customOfficeName,
-                    customOfficeNumber = customResponse.GeneralDetails.customOfficeNumber,
-                    declerationStatus = customResponse.GeneralDetails.declerationStatus,
-                    externalID = customResponse.GeneralDetails.externalID,
+                    customOfficeNumber = customResponse.GeneralDetails.customOfficeNumber.ToString(),
+                    declerationStatus = customResponse.GeneralDetails.declerationStatus.ToString(),
                     name = customResponse.GeneralDetails.name,
                     statusName = customResponse.GeneralDetails.statusName,
                 };
+                if (customResponse.GeneralDetails.externalIDSpecified == true)
+                {
+                    MyResponseData.GeneralDetailsData.externalID = customResponse.GeneralDetails.externalID.ToString();
+                }
             }
 
             if (customResponse.Claim != null)
@@ -358,7 +276,7 @@ namespace Logitude.CustomsMessaging.ResponseServices
                         {
                             newDeficit.totalRefundAmount = deficit.totalRefundAmount.Value.ToString("N2");
                         }
-                        if (deficit.productionDateSpecified == true)
+                        if (deficit.closeDateSpecified == true)
                         {
                             newDeficit.closeDate = deficit.closeDate.Value.ToString("dd/MM/yyyy");
                         }
