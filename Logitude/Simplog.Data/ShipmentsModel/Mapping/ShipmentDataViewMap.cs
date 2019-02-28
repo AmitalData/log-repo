6bb00ae8c6c3a8ad3d6ab8d75f3422a35a607c1d
Lana Daoud <Lana@logitudeworld.com>
2019-02-28 10:32:36 +0200

    23423

--- Logitude/Simplog.Data/ShipmentsModel/Mapping/ShipmentDataViewMap.cs
+++ Logitude/Simplog.Data/ShipmentsModel/Mapping/ShipmentDataViewMap.cs
@@ -868,6 +868,8 @@ namespace Simplog.Data.ShipmentModel.Mapping
             this.Property(t => t.From).HasColumnName("From");
             this.Property(t => t.To).HasColumnName("To");
 
+
+
             this.Property(t => t.Origin).HasColumnName("Origin");
         }
     }
