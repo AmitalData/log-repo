7c2e8a00171049b24463589794b9e491df9dca58
Samar Abu Eid <Samar@logitudeworld.com>
2019-03-03 13:41:22 +0200

    49186
    Related Work Items: #49186

--- Logitude/Logitude.Infrastructure.Data/EntityListQueryServices/Generated/BIReportFolderListQueryService.Generated.cs
+++ Logitude/Logitude.Infrastructure.Data/EntityListQueryServices/Generated/BIReportFolderListQueryService.Generated.cs
@@ -105,7 +105,7 @@ namespace Logitude.Infrastructure.Data.EntityListQueryServices
                             }
                         default:
                             {
-                                query2 = query2.OrderBy(d => d.Index);
+                                query2 = query2.OrderBy(d => d.Name);
                                 break;
                             }
                     }
@@ -114,7 +114,7 @@ namespace Logitude.Infrastructure.Data.EntityListQueryServices
             }
 		    else
             {
-                query2 = query2.OrderBy(d => d.Index);
+                query2 = query2.OrderBy(d => d.Name);
             }
 			if(!queryOperations.GetAll)
 			{
