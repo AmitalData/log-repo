465ecbf309593adb7ceac89f0aa9445987964c4f
Samar Abu Eid <Samar@logitudeworld.com>
2019-03-03 12:39:28 +0200

    49140
    Related Work Items: #49140

--- Logitude/Logitude.DatabaseMigration/Migrations/201903030807448_AddIsVeichleToPackageType_Samar.cs
+++ Logitude/Logitude.DatabaseMigration/Migrations/201903030807448_AddIsVeichleToPackageType_Samar.cs
@@ -8,6 +8,9 @@ namespace Logitude.DatabaseMigration.Migrations
         public override void Up()
         {
             AddColumn("dbo.PackageTypes", "IsVehicle", c => c.Boolean(nullable: false));
+
+            Sql(@"update PackageTypes set IsVehicle = 1, IsContainer = 0, IsRefrigerated = 0
+                where Code = 'E5' or Code = 'E6' or Code = 'H6'");
         }
         
         public override void Down()
