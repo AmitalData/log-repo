7577591b17e33061222e3e1839b0ec28e4dc6a6a
Abed <abed@logitudeworld.com>
2019-03-10 16:51:48 +0200

    49596
    Related Work Items: #49596

---
+++ Logitude/JustWebFreight/WebFreight.Web/DataContracts/AutomationArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebFreight.Web.DataContracts
+{
+    public class AutomationArgs
+    {
+
+        [Key]
+        public string Id { get; set; }
+        public int Tenant { get; set; }
+        public string AutomationsId { get; set; }
+        public string RecipientValue { get; set; }
+        public string RecipientType { get; set; }
+        public int Order { get; set; }
+    }
+}
