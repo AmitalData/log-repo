b9d241d6aeda93fb0dc92a94a95c0bef35c2efd7
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2019-03-12 12:55:33 +0200

    Fixing and Loging Auth. Problem

--- Logitude/JustWebFreight/WebFreight.Web/WcfApi/FeatureWcfService.svc.cs
+++ Logitude/JustWebFreight/WebFreight.Web/WcfApi/FeatureWcfService.svc.cs
@@ -1,4 +1,8 @@
 ﻿using Logitude.Server.Tools;
+using Logitude.SystemLogs;
+using Simplog.Data.CommonDataModel;
+using Simplog.Data.CommonDataModel.EntityPOCOs;
+using Simplog.Data.CommonDataModel.Repositories;
 using Simplog.Server.Infrastructure;
 using System;
 using System.Collections.Generic;
@@ -20,12 +24,40 @@ namespace WebFreight.Web.WcfApi
     {
         public List<FeatureAccessInfo> GetActiveFeaturesForUser(List<FeatureAccessInfo> featuresList, int tenant, ref Response response)
         {
-            SecurityUtility.AuthenticationOnTenant(tenant);
+            var myEmail = HttpContext.Current.User.Identity.Name;
+            if (string.IsNullOrEmpty(myEmail))
+            {
+                AzureLog.SaveLogsInStorage("( Tenant : " + tenant + " ) => HttpContext.Current.User.Identity.Name is null or empty ", "P", DateTime.Now, "", "", 0, "", "FeatureWcfService", null);
+                string token = HttpContext.Current.Request.Headers["Token"];
+                if (!string.IsNullOrEmpty(token))
+                {
+                    ICommonDataContext context = CommonDataContext.GetContext(0);
+                    AuthenticationTokenRepository tokenRep = new AuthenticationTokenRepository(context);
+                    AuthenticationToken authToken = AuthenticationTokenRepository.GetSingleTokenFromCache(token);
+                    if (authToken != null)
+                    {
+                        myEmail = authToken.Email;
+                        if (string.IsNullOrEmpty(myEmail))
+                        {
+                            AzureLog.SaveLogsInStorage("( Tenant : " + tenant + " ) => authToken.Email is null or empty ", "P", DateTime.Now, "", "", 0, "", "FeatureWcfService", null);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                AzureLog.SaveLogsInStorage("( Tenant : " + tenant + " ) => authToken.Email is " + myEmail + " AuthOnTenant is Running", "P", DateTime.Now, "", "", 0, "", "FeatureWcfService", null);
+                SecurityUtility.AuthenticationOnTenant(tenant);
+            }
+
             try
             {
                 if (featuresList != null)
                 {
-                    SecurityUtility.CheckContactTableFeatures(featuresList, HttpContext.Current.User.Identity.Name, tenant);
+                    SecurityUtility.CheckContactTableFeatures(featuresList, myEmail, tenant);
+
+                    AzureLog.SaveLogsInStorage("( Tenant : " + tenant + " ) => CheckContactTableFeatures Done with No Problems. for Email => " + myEmail, "P", DateTime.Now, "", "", 0, "", "FeatureWcfService", null);
+
                     //SecurityUtility.CheckCustomContactTableFeatures(featuresList, HttpContext.Current.User.Identity.Name, tenant);//
 
                 }
@@ -41,6 +73,9 @@ namespace WebFreight.Web.WcfApi
                     response.ErrorMessage += Environment.NewLine + ex.StackTrace;
                 }
 
+                AzureLog.SaveLogsInStorage("( Tenant : " + tenant + " ) => Exception " + response.ErrorMessage , "P", DateTime.Now, "", "", 0, "", "FeatureWcfService", null);
+
+
                 return null;
 
             }
@@ -49,21 +84,21 @@ namespace WebFreight.Web.WcfApi
 
         public bool CheckOutlookVersion(string Version)
         {
-                var CurrentVersionarr = Version.Split('.');
-                var MinVersionarr = LogitudeSettings.MinimumOutlookVersion.Split('.');
-                int Curversion = 0;
-                int Minversion = 0;
-                bool CheckResult = true;
-                for (int i = 0; i < MinVersionarr.Length; i++)
+            var CurrentVersionarr = Version.Split('.');
+            var MinVersionarr = LogitudeSettings.MinimumOutlookVersion.Split('.');
+            int Curversion = 0;
+            int Minversion = 0;
+            bool CheckResult = true;
+            for (int i = 0; i < MinVersionarr.Length; i++)
+            {
+                int.TryParse(CurrentVersionarr[i], out Curversion);
+                int.TryParse(MinVersionarr[i], out Minversion);
+                if (Curversion < Minversion)
                 {
-                    int.TryParse(CurrentVersionarr[i], out Curversion);
-                    int.TryParse(MinVersionarr[i], out Minversion);
-                    if (Curversion < Minversion)
-                    {
-                        CheckResult = false;
-                    }
+                    CheckResult = false;
                 }
-                return CheckResult;
+            }
+            return CheckResult;
         }
     }
 }
