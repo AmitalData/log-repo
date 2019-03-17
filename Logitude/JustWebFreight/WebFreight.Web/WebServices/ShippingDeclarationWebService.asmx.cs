6943e5cfae653270cd031bffbba4036f084c5b18
Ayman1 <Ayman@logitudeworld.com>
2019-03-17 10:00:10 +0200

    50069
    Related Work Items: #50069

--- Logitude/JustWebFreight/WebFreight.Web/WebServices/ShippingDeclarationWebService.asmx.cs
+++ Logitude/JustWebFreight/WebFreight.Web/WebServices/ShippingDeclarationWebService.asmx.cs
@@ -742,6 +742,17 @@ namespace WebFreight.Web.WebServices
                 }
                 #endregion
 
+                #region IssuingCarrier
+                if (!string.IsNullOrEmpty(shipment.IssuingCarrierAgentId))
+                {
+                    Card myIssuingCarrier = (from a in commonContext.Cards where a.Id == shipment.IssuingCarrierAgentId select a).FirstOrDefault();
+                    if(myIssuingCarrier != null)
+                    {
+                        myDataProvider.IssuingCarrierAgentName = myIssuingCarrier.EnglishName;
+                    }
+                }
+                #endregion
+
                 #region Custom Agent Import|Broker
                 if (!string.IsNullOrEmpty(shipment.CustomAgentImportId))
                 {
