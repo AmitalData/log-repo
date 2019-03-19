03e09cc4c1d8ad57f28c2561d23e73f05b12f16b
itzik M <itzik@amital.co.il>
2019-03-19 09:06:48 +0200

    50052 שידור הצהרה ממשוב של מצהר - עדכון תאריך חישוב מיסים
    Related Work Items: #50052

--- Logitude/Logitude.CustomsMessaging/ResponseServices/MN_MSG4_SendManifestFeedBack_MessageResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/MN_MSG4_SendManifestFeedBack_MessageResponseService.cs
@@ -386,11 +386,13 @@ namespace Logitude.CustomsMessaging.ResponseServices
             {
                 if (myEventContextTagModel.EventCode == "MNC")//ODELIA SAID MNC its success !
                 {
+                    _MyDeclarationPM.TaxationDateTime = DateTime.Now.Date;//לפני השליחה יש לעדכן את תאריך חישוב המיסים לתאריך נוכחי על מנת להמנע מטיוטה שגויה
                     using (var trans = TransactionFactory.GetNewTransaction())// I PREFERRED WITHOUT TRANS BUT  (TO 1345- 1415). .
                     {
                         OnSucceededSendDeclarationDelay1Min(requestParams);
                         trans.Complete();
                     }
+
                 }
 
                 string loggingUserId = AuthenticationUtil.ResolveUserId(_MyDeclarationPM.Tenant);
