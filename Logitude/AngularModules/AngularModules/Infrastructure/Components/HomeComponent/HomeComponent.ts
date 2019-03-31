484522ed7f5e8ee22e2c2b8dbcd66129e1c2c0a1
Ayman1 <Ayman@logitudeworld.com>
2019-03-31 14:46:00 +0300

    50559
    Related Work Items: #50559

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/HomeComponent/HomeComponent.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/HomeComponent/HomeComponent.ts
@@ -860,6 +860,7 @@ private BluesnapContractService: BluesnapContractPMService= new BluesnapContract
                 this.SelectionChanged(tabItem);
 
                 var confirmWindow = new ConfirmWindow();
+                confirmWindow.IsOverAll = true;
                 confirmWindow.Width = 450;
                 confirmWindow.Height = 190;
                 confirmWindow.ShowCancelButton = true;
