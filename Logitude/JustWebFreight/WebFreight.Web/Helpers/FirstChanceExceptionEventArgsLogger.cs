afefc5151fc83a8e12d7646646abef3c27c181f8
Islam Shaheen <Islam@logitudeworld.com>
2019-04-04 09:21:26 +0300

    fix

--- Logitude/JustWebFreight/WebFreight.Web/Helpers/FirstChanceExceptionEventArgsLogger.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Helpers/FirstChanceExceptionEventArgsLogger.cs
@@ -11,14 +11,19 @@ namespace WebFreight.Web.Helpers
 {
 	public class FirstChanceExceptionEventArgsLogger
 	{
+		private static string filePath = HttpContext.Current.Server.MapPath(".") + "\\bin\\" + "exceptionslogfile.txt";
 		public static void LogException(FirstChanceExceptionEventArgs args)
 		{
 			try
 			{
-				if (args.Exception != null)
+				if (args.Exception != null && args.Exception.Message != null && !args.Exception.Message.Contains("exceptionslogfile.txt"))
 				{
-					string WorkingDir = Path.GetDirectoryName(Application.ExecutablePath);
-					using (StreamWriter w = File.AppendText(WorkingDir + @"\exceptionslog.txt"))
+					//string WorkingDir = Path.GetDirectoryName(Application.ExecutablePath);
+					
+					//filePath += "\\bin\\";
+					//string fileName = "exceptionslogfile.txt";
+					//filePath += fileName;
+					using (StreamWriter w = File.AppendText(filePath))
 					{
 
 						string message = args.Exception.Source + " " + args.Exception.Message + " " + args.Exception.StackTrace;
@@ -34,11 +39,13 @@ namespace WebFreight.Web.Helpers
 
 		private static void Log(string logMessage, TextWriter w)
 		{
-			w.Write("\r\nLog Entry : ");
-			w.WriteLine($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}");
-			//w.WriteLine("  :");
-			w.WriteLine($"  :{logMessage}");
-			w.WriteLine("-------------------------------");
+			string logEntry = DateTime.Now.ToString()+ " "+ logMessage;
+			w.WriteLine(logEntry);
+			//w.Write("\r\nLog Entry : ");
+			//w.WriteLine($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}");
+			////w.WriteLine("  :");
+			//w.WriteLine($"  :{logMessage}");
+			//w.WriteLine("-------------------------------");
 		}
 	}
 }
