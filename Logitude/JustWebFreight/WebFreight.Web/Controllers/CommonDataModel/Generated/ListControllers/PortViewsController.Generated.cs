54df1776be8b994414d3bc489fe55bf9e3b85506
Islam Shaheen <Islam@logitudeworld.com>
2019-04-07 13:27:04 +0300

    Can't Add New Currencies
    Related Work Items: #50809

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/CommonDataModel/Generated/ListControllers/PortViewsController.Generated.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/CommonDataModel/Generated/ListControllers/PortViewsController.Generated.cs
@@ -128,7 +128,8 @@ namespace WebFreight.Web.Controllers.CommonDataModel.Generated.ListControllers
                 AuthenticationToken authToken = AuthenticationTokenRepository.GetSingleTokenFromCache(token);
                 SecurityUtility.AuthenticationOnTenant(authToken.Tenant);
 				int tenant = authToken.Tenant;
-           
+            if(filters.Tenant != null)
+	                tenant = filters.Tenant.Value;
 					                
 				SecurityUtility.CheckContactFeature("Port", "READ", authToken.Tenant);
 	
