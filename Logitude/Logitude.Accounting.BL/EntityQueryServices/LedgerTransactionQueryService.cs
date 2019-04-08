47e689786569bcbd240d3b25d0ada9c3718d9cfe
Abdullah Dawoud <abdullah@logitudeworld.com>
2019-04-08 09:52:18 +0300

    Related Work Items: #50862

--- Logitude/Logitude.Accounting.BL/EntityQueryServices/LedgerTransactionQueryService.cs
+++ Logitude/Logitude.Accounting.BL/EntityQueryServices/LedgerTransactionQueryService.cs
@@ -576,7 +576,7 @@ namespace Logitude.Accounting.BL.EntityQueryServices
             IQueryable<LedgerTransactionPM> transactions = GetTransactionsJoinedWithJounrals();
 
             // get payment transaction
-            LedgerTransactionPM paymentTransaction = transactions.Where(t => t.SourceId == arpaymentId).FirstOrDefault();
+            LedgerTransactionPM paymentTransaction = transactions.Where(t => t.SourceId == arpaymentId && t.SourceTypeCode == AccountingEntityValues.ARInvoice).FirstOrDefault();
             if (paymentTransaction != null)
             {
                 string paymentTransactionId = paymentTransaction.Id;
