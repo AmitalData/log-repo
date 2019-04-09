4d42882b435d226a86c8e5ebb16199e8145949d7
Maheera Jazi <Maheera@logitudeworld.com>
2019-04-09 16:09:13 +0300

    51066

--- Logitude/Logitude.TimeManagement.Data/Repositories/TMProjectRepository.cs
+++ Logitude/Logitude.TimeManagement.Data/Repositories/TMProjectRepository.cs
@@ -23,7 +23,7 @@ namespace Logitude.TimeManagement.Data.Repositories
 
         public string GetTMProjectByNumber(string number, int tenant)
         {
-            var projectId = "";
+            string projectId = null;
             var project = (from d in context.TMProjects
                           where d.Tenant == tenant
                           && d.ProjectNumber == number
