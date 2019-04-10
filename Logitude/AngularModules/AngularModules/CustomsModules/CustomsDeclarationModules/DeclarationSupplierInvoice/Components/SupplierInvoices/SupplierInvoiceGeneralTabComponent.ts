f53afcb25dd9c683f1a29649266d51b0728742d0
mirit <Mirit@amital.co.il>
2019-04-10 16:30:09 +0300

    51094

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationSupplierInvoice/Components/SupplierInvoices/SupplierInvoiceGeneralTabComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationSupplierInvoice/Components/SupplierInvoices/SupplierInvoiceGeneralTabComponent.ts
@@ -52,6 +52,7 @@ import { CustomsSettingListService } from '../../../../../Customs/Services/Stand
 import { CustomsCountryListService } from '../../../../../Customs/Services/StandardLists/CustomsCountryListService';
 
 import { GITITEMCacheService } from '../../../../../Customs/Services/Others/GITITEMCacheService';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
     moduleId: module.id,
@@ -2092,8 +2093,8 @@ export class SupplierInvoiceGeneralTabComponent extends BaseComponent
             var amountInNIS: number;
             var rate: CustomsExchangeRatePM;
             if (item.CurrencyTypeCode == "ILS") {
-                amountInNIS = item.Amount;
-                totalFreightInNIS = totalFreightInNIS + amountInNIS;
+                amountInNIS = AppTool.ToNumber(item.Amount);              
+                totalFreightInNIS = +totalFreightInNIS + +amountInNIS;
                 //if (totalFreightInNIS != null) {
                 //    totalFreightInNIS = Math.round(totalFreightInNIS);
                 //}
@@ -2105,7 +2106,7 @@ export class SupplierInvoiceGeneralTabComponent extends BaseComponent
                     rate = this.customsExchangeRates.filter(d => d.CurrencyTypeCode == item.CurrencyTypeCode)[0];
                     if (rate != null) {
 
-                        amountInNIS = item.Amount * rate.ExchangeRate;
+                        amountInNIS = AppTool.ToNumber(item.Amount) * rate.ExchangeRate;
                         totalFreightInNIS = totalFreightInNIS + amountInNIS;
                         //if (totalFreightInNIS != null) {
                         //    totalFreightInNIS = Math.round(totalFreightInNIS);
