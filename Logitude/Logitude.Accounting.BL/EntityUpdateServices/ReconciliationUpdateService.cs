8f8a11f26d53b8aea468c874210effce80f0e186
Abdullah Dawoud <abdullah@logitudeworld.com>
2019-04-14 18:03:20 +0300

    Related Work Items: #51135

--- Logitude/Logitude.Accounting.BL/EntityUpdateServices/ReconciliationUpdateService.cs
+++ Logitude/Logitude.Accounting.BL/EntityUpdateServices/ReconciliationUpdateService.cs
@@ -369,11 +369,11 @@ namespace Logitude.Accounting.BL.EntityUpdateServices
         protected override void AfterUpdating(ReconciliationPM entityPM, EntityPM entityParentPM)
         {
             // Draft reconciliation
-            if (SuppressResetDraftOpenReconciliation)
-                return;
-            var repoLedger = new LedgerTransactionRepository(MainContext as IAccountingContext);
-            repoLedger.ResetDraftOpenReconciliation(entityPM.AccountId, entityPM.Tenant);
-
+            if (!SuppressResetDraftOpenReconciliation)
+            {
+                var repoLedger = new LedgerTransactionRepository(MainContext as IAccountingContext);
+                repoLedger.ResetDraftOpenReconciliation(entityPM.AccountId, entityPM.Tenant);
+            }
 
             // update connected ARPayment 
             ARPaymentReconciliationService arpRecoService = new ARPaymentReconciliationService(entityPM.Tenant);
