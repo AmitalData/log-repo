fcdcca570dccf04eef37f0ca0a4805484348199f
alaa <alaa.jamal.h@live.com>
2019-04-21 10:38:46 +0300

    48647
    Related Work Items: #48647

--- Logitude/Logitude.Accounting.BL/CoreBL/Batch/BatchFixIntegrityCheckErrorsService.cs
+++ Logitude/Logitude.Accounting.BL/CoreBL/Batch/BatchFixIntegrityCheckErrorsService.cs
@@ -22,6 +22,8 @@ namespace Logitude.Accounting.BL.CoreBL.Batch
    public class BatchFixIntegrityCheckErrorsService : BatchTaskExecutionsService
     {
         AccountingIntegrityCheckPM entityPM;
+        AccountingIntegrityResult AccountingIntegrityResult;
+        AccountingIntegrityService accountingIntegrityService;
         public BatchFixIntegrityCheckErrorsService(BatchTaskExecutionPM batchTaskExecution) : base(batchTaskExecution)
         {
 
@@ -32,7 +34,7 @@ namespace Logitude.Accounting.BL.CoreBL.Batch
            
             string xmlParameters = BatchTaskExecution.PrametersXml;
             System.IO.StringReader stringReader = new System.IO.StringReader(xmlParameters);
-            XmlSerializer serializer = new XmlSerializer(typeof(IntegrityCheckArgs)); // AccountingIntegrityInParam
+            XmlSerializer serializer = new XmlSerializer(typeof(IntegrityCheckArgs)); 
             IntegrityCheckArgs parameterArgs = serializer.Deserialize(stringReader) as IntegrityCheckArgs;
 
          
@@ -46,92 +48,24 @@ namespace Logitude.Accounting.BL.CoreBL.Batch
             IAccountingContext MyContext = AccountingContext.GetContext(args.Tenant);
             AccountingIntegrityCheckUpdateService updateService = new AccountingIntegrityCheckUpdateService(MyContext, new Dictionary<string, IContext>(), args.Tenant);
             GetIntegrityCheckPM(args);
-            entityPM.ChangeSetOp = ChangeSetOperation.Update;
+            DeserializeAccountingIntegrityResult(entityPM.ResultXML);
 
-            // Deserilaize parameters
-            string xmlParameters = entityPM.ParametersXML;
-            if (xmlParameters == null)
-            {
-                // update status
-                entityPM.StatusCode = "5"; // Failed
-                entityPM.HasException = true;
-                entityPM.DoneDateTimeUTC = DateTime.UtcNow;
-                entityPM.ResultXML = "Service Error: " + "Dates are not selected";
-
-                updateService.Update(entityPM, true);
-
-                throw new ApplicationException("Dates are not selected");
-            }
-
-            System.IO.StringReader stringReader = new System.IO.StringReader(xmlParameters);
-            XmlSerializer serializer = new XmlSerializer(typeof(AccountingIntegrityInParam));
-            AccountingIntegrityInParam _params = serializer.Deserialize(stringReader) as AccountingIntegrityInParam;
-
-
-            // 2- update status
-            entityPM.StatusCode = "2"; // In Progress
-            updateService.Update(entityPM, true);
-
-            // 3- check parameters
-            AccountingIntegrityService accountingIntegrityService = new AccountingIntegrityService();
-            string errorMessage = accountingIntegrityService.CheckParams(_params);
-            if (!string.IsNullOrEmpty(errorMessage))
-            {
-                // update status
-                entityPM.StatusCode = "5"; // Failed
-                entityPM.HasException = true;
-                entityPM.DoneDateTimeUTC = DateTime.UtcNow;
-                entityPM.ResultXML = "Service Error: " + errorMessage;
-                updateService.Update(entityPM, true);
-
-                throw new ApplicationException(errorMessage);
-            }
-
-            // 4- run service
-            AccountingIntegrityResult res;
+           
             try
             {
-                res = accountingIntegrityService.CheckIntegrity(_params);
+                FixingIntegrityCheck(args.Tenant, AccountingIntegrityResult);
+                
+                UpdateIntegrityCheck(entityPM);
             }
             catch (Exception ex)
             {
-                // update status
-                entityPM.StatusCode = "5"; // Failed
-                entityPM.HasException = true;
-                entityPM.DoneDateTimeUTC = DateTime.UtcNow;
-                entityPM.ResultXML = "Service Error: " + ex.Message;
-
-                updateService.Update(entityPM, true);
-
+                SetIntegrityCheckFaid(entityPM);
+                
+                UpdateIntegrityCheck(entityPM);
                 throw new ApplicationException(ex.Message);
             }
 
-            // 5- store resultXML
-            if (res != null)
-            {
-                // Check rows if have exception message
-                List<AccountingIntegrityStep> integritySteps = res.MyAccountingIntegrityStep;
-                foreach (AccountingIntegrityStep step in integritySteps)
-                {
-                    if (step.BadRows > 0 && step.ShouldFix == true)
-                    //if (!string.IsNullOrWhiteSpace(step.ExceptionMessage))
-                    {
-                        entityPM.HasException = true;
-                        break;
-                    }
-                }
-
-                // serialize resultXML
-                string stringXML = LogitudeXmlSerializer.SerializeObjectToXmlString<AccountingIntegrityResult>(res);
-
-                // update
-                entityPM.ResultXML = stringXML;
-                entityPM.StatusCode = "3"; // Check Completed
-                entityPM.DoneDateTimeUTC = DateTime.UtcNow;
-
-                // save 
-                updateService.Update(entityPM, true);
-            }
+         
 
         }
 
@@ -141,5 +75,50 @@ namespace Logitude.Accounting.BL.CoreBL.Batch
             AccountingIntegrityCheckQueryService query = new AccountingIntegrityCheckQueryService(args.Tenant);
             entityPM = query.GetSingle(args.EntityId, false, false);
         }
+
+        public void DeserializeAccountingIntegrityResult(string resultXml)
+        {
+
+            System.IO.StringReader stringReader = new System.IO.StringReader(entityPM.ResultXML);
+            XmlSerializer serializer = new XmlSerializer(typeof(AccountingIntegrityResult)); 
+            AccountingIntegrityResult = serializer.Deserialize(stringReader) as AccountingIntegrityResult;
+
+        }
+
+        public void UpdateIntegrityCheck(AccountingIntegrityCheckPM entityPM)
+        {
+            IAccountingContext MyContext = AccountingContext.GetContext(entityPM.Tenant);
+            AccountingIntegrityCheckUpdateService updateService = new AccountingIntegrityCheckUpdateService(MyContext, new Dictionary<string, IContext>(), entityPM.Tenant);
+            entityPM.ChangeSetOp = ChangeSetOperation.Update;
+            updateService.Update(entityPM, true);
+
+
+        }
+
+        public void SetIntegrityCheckFaid(AccountingIntegrityCheckPM entityPM)
+        {
+            entityPM.StatusCode = "5";
+        }
+        public void SetIntegrityCheckFixCompleted(AccountingIntegrityCheckPM entityPM)
+        {
+            entityPM.StatusCode = "4";
+        }
+
+        public void FixingIntegrityCheck(int tenant, AccountingIntegrityResult AccountingIntegrityResult)
+        {
+            accountingIntegrityService = new AccountingIntegrityService();
+            accountingIntegrityService.FixDBIntegrity(tenant, AccountingIntegrityResult.MyAccountingIntegrityStep);
+            var itemWithException = AccountingIntegrityResult.MyAccountingIntegrityStep.Where(d => d.ExceptionMessage != null);
+            if (itemWithException != null)
+            {
+                SetIntegrityCheckFaid(entityPM);
+            }
+            else
+            {
+                SetIntegrityCheckFixCompleted(entityPM);
+            }
+
+        }
+
     }
 }
