b7b5a43c48808f09e33ca47c7768d3848355ea44
Ayman1 <Ayman@logitudeworld.com>
2019-04-24 14:02:50 +0300

    fix

---
+++ Logitude/Simplog.Global.Data/Migrations/201904240914504_AddIsBlockingLoginFlag.cs
@@ -0,0 +1,17 @@
+namespace Simplog.Global.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddIsBlockingLoginFlag : DbMigration
+    {
+        public override void Up()
+        {
+            
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
