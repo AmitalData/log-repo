67a8d9c3e5481bc029b8f7a33d9c85dda45af3d3
Abdullah Dawoud <abdullah@logitudeworld.com>
2019-04-24 20:01:27 +0300

    tax report status
    Related Work Items: #50117

--- Logitude/Logitude.Accounting.BL/EntityUpdateServices/TaxReportUpdateService.cs
+++ Logitude/Logitude.Accounting.BL/EntityUpdateServices/TaxReportUpdateService.cs
@@ -164,7 +164,7 @@ namespace Logitude.Accounting.BL.EntityUpdateServices
             List<TaxReportLineList> lines = reportLineListQueryService.GetReportLines(taxReportPM.Id, taxReportPM.Tenant).ToList();
 
             bool hasErrors = lines.Any(d => d.StatusCode != "6"); // 6- Ready for transmit
-            if (hasErrors)
+            if (hasErrors && taxReportPM.StatusCode != VatReportStatusValues.Error)
             {
                 taxReportPM.ChangeSetOp = ChangeSetOperation.Update;
                 taxReportPM.StatusCode = VatReportStatusValues.Error;
