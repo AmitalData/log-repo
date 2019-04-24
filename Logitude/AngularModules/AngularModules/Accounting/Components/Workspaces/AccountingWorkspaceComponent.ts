94487dc8b64653f8073dde92da3b38047a0e2292
alaa <alaa.jamal.h@live.com>
2019-04-24 12:31:10 +0300

    48637
    Related Work Items: #48637

--- Logitude/AngularModules/AngularModules/Accounting/Components/Workspaces/AccountingWorkspaceComponent.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/Workspaces/AccountingWorkspaceComponent.ts
@@ -1,8 +1,10 @@
-﻿import {Component, ViewChildren, QueryList} from '@angular/core';
+import {Component, ViewChildren, QueryList} from '@angular/core';
 import {LocationDirective} from '../../../Infrastructure/Utilities/LocationDirective';
 import {SessionLocator} from '../../../Infrastructure/Utilities/SessionLocator';
 import {EntityResourceService} from '../../../Infrastructure/Services/EntityResourceService';
 import {ObjectsLocator} from '../../../Infrastructure/Locators/ObjectsLocator';
+import { FeatureLocator } from '../../../Infrastructure/Utilities/FeatureLocator';
+declare var window: any;
 
 @Component({
     selector: 'FullAccountingComponent',
@@ -14,7 +16,12 @@ export class AccountingWorkspaceComponent {
     @ViewChildren(LocationDirective) public AllLocations: QueryList<LocationDirective>;
 
     public isRTL: boolean = false;
-
+    public IsMainTabVisibile: boolean = false;
+    public IsReceivablesTabVisibile: boolean = false;
+    public IsPayablesTabVisibile: boolean = false;
+    public IsBanksTabVisibile: boolean = false;
+    public IsGLAccountsTabVisibile: boolean = false;
+    public IsMiscTabVisibile: boolean = false;
 
     constructor(private _entityResourceService: EntityResourceService) {
         this.RunComponent();
@@ -32,7 +39,31 @@ export class AccountingWorkspaceComponent {
         this._entityResourceService.getEntityResourceByTableName("AccountingPeriod").subscribe((response: any) => { });
 
         if (ObjectsLocator.GlobalSetting) this.isRTL = (ObjectsLocator.GlobalSetting.LayoutDirection == "rtl");
-
+        var table = window.ObjectTables.filter(d => d.Name === 'General')[0];
+        var mainTabFeature = FeatureLocator.Features.filter(f => (f.Code == "ACCMAIN") && f.ObjectTableId == table.Id)[0];
+        if (mainTabFeature) {
+            this.IsMainTabVisibile = true;
+        }
+        var ReceivablesTabFeature = FeatureLocator.Features.filter(f => (f.Code == "ACCReceivables") && f.ObjectTableId == table.Id)[0];
+        if (ReceivablesTabFeature) {
+            this.IsReceivablesTabVisibile = true;
+        }
+        var PayablesTabFeature = FeatureLocator.Features.filter(f => (f.Code == "ACCPayables") && f.ObjectTableId == table.Id)[0];
+        if (PayablesTabFeature) {
+            this.IsPayablesTabVisibile = true;
+        }
+        var BanksTabFeature = FeatureLocator.Features.filter(f => (f.Code == "ACCBanks") && f.ObjectTableId == table.Id)[0];
+        if (BanksTabFeature) {
+            this.IsBanksTabVisibile = true;
+        }
+        var GLAccountsTabFeature = FeatureLocator.Features.filter(f => (f.Code == "ACCGLAccounts") && f.ObjectTableId == table.Id)[0];
+        if (GLAccountsTabFeature) {
+            this.IsGLAccountsTabVisibile = true;
+        }
+        var MiscTabFeature = FeatureLocator.Features.filter(f => (f.Code == "ACCMisc") && f.ObjectTableId == table.Id)[0];
+        if (MiscTabFeature) {
+            this.IsMiscTabVisibile = true;
+        }
     }
 
     private isLoaderReady: boolean = false;
@@ -45,7 +76,29 @@ export class AccountingWorkspaceComponent {
 
             else {
                 this.isLoaderReady = true;
-                this.SelectedItem = "Main";
+                if (this.IsMainTabVisibile) {
+                    this.SelectedItem = "Main";
+                }
+                else if (this.IsReceivablesTabVisibile) {
+                    this.SelectedItem = "RCV";
+
+                }
+                else if (this.IsPayablesTabVisibile) {
+                    this.SelectedItem = "PAY";
+
+                }
+                else if (this.IsBanksTabVisibile) {
+                    this.SelectedItem = "BNKS";
+
+                }
+                else if (this.IsGLAccountsTabVisibile) {
+                    this.SelectedItem = "GLAccounts";
+
+                }
+                else if (this.IsMiscTabVisibile) {
+                    this.SelectedItem = "MISC";
+
+                }
             }
         }
 
