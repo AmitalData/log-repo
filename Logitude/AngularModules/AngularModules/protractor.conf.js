d20ddfd23138b5454fe9b7eaed7add452ab63f00
Razan Jararah <razan@logitudeworld.com>
2019-04-24 15:41:42 +0300

    Protractor

--- Logitude/AngularModules/AngularModules/protractor.conf.js
+++ Logitude/AngularModules/AngularModules/protractor.conf.js
@@ -31,9 +31,9 @@ exports.config = {
   suites: {
     // ********************* Login **********************************
      login: 'e2e/Login/**/Login.e2e-spec.ts',
-     CRM: 'e2e/CRM/**/CRMModule-spec.ts',
+    //  CRM: 'e2e/CRM/**/CRMModule-spec.ts',
 
-    //  NewShipment: 'e2e/Operations/Shipments/NewEntity/**/Operations.e2e-spec.ts',
+     NewShipment: 'e2e/Operations/Shipments/NewEntity/**/Operations.e2e-spec.ts',
     //  Contact: 'e2e/Contacts/**/Contacts-spec.ts',
 
      //EditTabs: 'e2e/Operations/Shipments/EditEntity/**/EditShipmentTabs.e2e-spec.ts',
