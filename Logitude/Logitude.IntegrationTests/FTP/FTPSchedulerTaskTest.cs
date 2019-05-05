8812c855fb663dd18d17dbd850e58f65a58aac0f
Islam Shaheen <Islam@logitudeworld.com>
2019-05-05 12:07:26 +0300

    FTP Scheduler : Integration Test

--- Logitude/Logitude.IntegrationTests/FTP/FTPSchedulerTaskTest.cs
+++ Logitude/Logitude.IntegrationTests/FTP/FTPSchedulerTaskTest.cs
@@ -6,6 +6,7 @@ using Simplog.Global.Data.GlobalModel.Repositories;
 using Simplog.Server.Infrastructure.Helpers;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Transactions;
 
 namespace Logitude.IntegrationTests.FTP
@@ -14,7 +15,7 @@ namespace Logitude.IntegrationTests.FTP
     public class FTPSchedulerTaskTest
     {
         [TestMethod]
-        public void Upload_DownloadFTPFiles_Prefix_Test()
+        public void Upload_Download_FTPFiles_Pdf_Extension_Test()
         {
 
 
@@ -27,21 +28,26 @@ namespace Logitude.IntegrationTests.FTP
                     From = "My FTP Server",
                     Host = "192.168.1.26",
                     Password = "!I123456",
-                    Prefix = "test",
+                    //Prefix = "test",
                     Subject = "test",
                     Suffix = "",
                     UserName = "Islam",
+                    Extension = "pdf",
                 }
             };
 
             //private static string filePath = HttpContext.Current.Server.MapPath(".") + "\\bin\\" + "exceptionslogfile.txt";
             FTPService ftpService = new FTPService(schedulerDetails.FTPDetails.Host, schedulerDetails.FTPDetails.UserName, schedulerDetails.FTPDetails.Password);
-            string startupPath = Environment.CurrentDirectory.Replace(@"bin\Debug", "FTPFiles");
+            string folderPath = Environment.CurrentDirectory.Replace(@"bin\Debug", "FTPFiles");
             //string filePath = HttpContext.Current.Server.MapPath(".") + "\\bin\\" + "exceptionslogfile.txt";
-            //foreach (string file in Directory.EnumerateFiles(folderPath, "*.xml"))
-            //{
-            //    string contents = File.ReadAllText(file);
-            //}
+            foreach (string file in Directory.EnumerateFiles(folderPath))
+            {
+                
+                byte[] contents = File.ReadAllBytes(file);
+                string p_message;
+                string fileName = Path.GetFileName(file);
+                ftpService.Upload(fileName, schedulerDetails.FTPDetails.Folder, contents, out p_message);
+            }
 
             //ftpService.Upload()
 
@@ -49,7 +55,13 @@ namespace Logitude.IntegrationTests.FTP
             FTPSchedulerTaskService fTPSchedulerTaskService = new FTPSchedulerTaskService();
             List<string> fileNames = fTPSchedulerTaskService.GetFilteredDirectoryFileNamesByFTPDetails(schedulerDetails);
 
-            fTPSchedulerTaskService.ReadFTPFilesBySchedulerDetailsToAnalyzeQueue(schedulerDetails);
+            var mExist = fileNames.Exists(f => (!string.IsNullOrEmpty(f) &&
+                Path.GetExtension(f).TrimStart('.').ToLower() != schedulerDetails.FTPDetails.Extension.Trim('.')));
+
+             
+            Assert.IsFalse(mExist, "Expected pdf files but get different extensions");
+
+            //fTPSchedulerTaskService.ReadFTPFilesBySchedulerDetailsToAnalyzeQueue(schedulerDetails);
 
             //using (TransactionScope scope = TransactionFactory.GetNewTransaction())
             //{
@@ -158,6 +170,27 @@ namespace Logitude.IntegrationTests.FTP
         [TestMethod]
         public void DownloadFTPFiles_Failure_Test()
         {
+            SchedulerDetails schedulerDetails = new SchedulerDetails()
+            {
+                Tenant = 1,
+                FTPDetails = new FTPSchedulerDetails()
+                {
+                    Folder = "DocumentsBackup",
+                    From = "My FTP Server",
+                    Host = "192.168.1.26",
+                    Password = "!I123456",
+                    //Prefix = "test",
+                    Subject = "fail test",
+                    Suffix = "",
+                    UserName = "Islam",
+                    Extension = "xml",
+                }
+            };
+
+            FTPSchedulerTaskService fTPSchedulerTaskService = new FTPSchedulerTaskService();
+            List<string> fileNames = fTPSchedulerTaskService.GetFilteredDirectoryFileNamesByFTPDetails(schedulerDetails);
+
+            fTPSchedulerTaskService.ReadFTPFilesBySchedulerDetailsToAnalyzeQueue(schedulerDetails);
         }
     }
 }
