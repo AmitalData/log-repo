8fdb1548fef6757c325d25c03d3385d6060bf203
Khawla Ata <khawla@logitudeworld.com>
2019-05-06 21:46:05 +0300

    related to #52029

--- Logitude/Logitude.Accounting.BL/CoreBL/OpenFormatReportService.cs
+++ Logitude/Logitude.Accounting.BL/CoreBL/OpenFormatReportService.cs
@@ -956,17 +956,17 @@ namespace Logitude.Accounting.BL.CoreBL
                         else
                         {
                             myStringBuilder.Append(a);
-                            myStringBuilder.Append('0', 15);
+                            myStringBuilder.Append(' ', 15);
                         }
                       
                     }
                     else
                     {
                         myStringBuilder.Append(a);
-                        myStringBuilder.Append('0', 15);
+                        myStringBuilder.Append(' ', 15);
                     }
 
-                    item.TotalDebitInForeignCurrency = trailReportM != null ? trailReportM.Select(d => d.ForeignDebit).Sum() : null;
+                   /* item.TotalDebitInForeignCurrency = trailReportM != null ? trailReportM.Select(d => d.ForeignDebit).Sum() : null;
                     if (item.TotalDebitInForeignCurrency != null)
                     {
                         string TotalDebitInForeignCurrency = Format(item.TotalDebitInForeignCurrency.Value); // Math.Abs((decimal) item.TotalDebitInForeignCurrency).ToString();
@@ -1027,10 +1027,10 @@ namespace Logitude.Accounting.BL.CoreBL
                     {
                         myStringBuilder.Append(a);
                         myStringBuilder.Append('0', 15);
-                    }
+                    }*/
                     if (item.CurrencyCode != null)
                     {
-                        if (item.CurrencyCode.Length > 3) { item.CurrencyCode.Substring(0, 3); }
+                        if (item.CurrencyCode.Length > 3) { item.CurrencyCode= item.CurrencyCode.Substring(0, 3); }
                         myStringBuilder.Append(a + item.CurrencyCode.PadLeft(3, ' '));
                     }
                     else
@@ -1042,7 +1042,7 @@ namespace Logitude.Accounting.BL.CoreBL
                 else
                 {
                     myStringBuilder.Append(a);
-                    myStringBuilder.Append('0', 45);
+                    myStringBuilder.Append(' ', 15);
                     myStringBuilder.Append(a);
                     myStringBuilder.Append(' ', 3);
                 }
