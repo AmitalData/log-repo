4bd1d752ccfac55ab23c063152b1f1136347021a
Samar Abu Eid <Samar@logitudeworld.com>
2019-05-12 13:16:51 +0300

    52121
    Related Work Items: #52121

---
+++ Logitude/Logitude.TariffModule.Data/EntityLists/Generated/TariffVersionList.Generated.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization; 
+
+namespace Logitude.TariffModule.Data.EntityLists
+{
+   [DataContract]
+   public partial class TariffVersionList
+   {
+   
+       [Key]
+       [DataMember]
+       public string TariffId  { get; set; }
+       [DataMember]
+       public int Tenant  { get; set; }
+       [DataMember]
+       public string CreatedByUserId  { get; set; }
+       [DataMember]
+       public string SearchFields  { get; set; }
+       [DataMember]
+       public DateTime? StartDate  { get; set; }
+       [DataMember]
+       public DateTime? ExpirationDate  { get; set; }
+
+       [Key]
+       [DataMember]
+       public int Version  { get; set; }
+       [DataMember]
+       public bool IsDraft  { get; set; }
+       [DataMember]
+       public DateTime? ApproveDate  { get; set; }
+   }
+
+}
+	 
