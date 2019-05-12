b59d2ac6889aabd54564be6a0b5468f94b7ce012
Samar Abu Eid <Samar@logitudeworld.com>
2019-05-12 12:49:31 +0300

    52121
    Related Work Items: #52121

--- Logitude/AngularModules/AngularModules/TariffModule/Services/TariffDomainService.ts
+++ Logitude/AngularModules/AngularModules/TariffModule/Services/TariffDomainService.ts
@@ -192,6 +192,8 @@ export class TariffFilterParameter {
     Tenant: number;
     FileData: string;
     PriceSteps: string;
+    TariffId: string;
+    Version: number;
 }
 
 export class ExcelTariffLines {
