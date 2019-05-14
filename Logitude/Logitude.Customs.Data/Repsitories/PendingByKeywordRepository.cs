3040923827265ca1da0da483e98cd422580ad461
moran <moran@amital.co.il>
2019-05-14 11:43:17 +0300

    51895 ms
    Related Work Items: #51895

--- Logitude/Logitude.Customs.Data/Repsitories/PendingByKeywordRepository.cs
+++ Logitude/Logitude.Customs.Data/Repsitories/PendingByKeywordRepository.cs
@@ -21,7 +21,32 @@ namespace Logitude.Customs.Data.Repsitories
 			throw new NotImplementedException();
         }
 
-   }
+        public List<string> GetCourierPendingReasonCodeBykeyWords(string keyWordsList, int tenant)
+        {
+            keyWordsList = keyWordsList.ToLower();
+            keyWordsList.Replace(" ", ",");
+            char[] BAD_CHARS = new char[] { '!', '@', '#', '$', '%', '_' , ')' , '(' , '-' , '*', '&', '^', '~', '.', '"', ';', '\'', '\\', '/', '<', '>', '{', '}', '[', ']' };
+            keyWordsList = string.Concat(keyWordsList.Split(BAD_CHARS, StringSplitOptions.RemoveEmptyEntries));
+            while (keyWordsList.Contains(",,"))
+            {
+                keyWordsList.Replace(",,", ",");
+            }
+            List<string>  CourierPendingReasonCodeList = null;
+            List<string> keyWordsList2 = keyWordsList.Split(',').ToList();
+            foreach (string word in keyWordsList2)
+            {
+
+
+                string wordtemp = "," + word + ",";
+                PendingByKeyword pendingByKeyword = (from a in context.PendingByKeywords
+                                                     where a.Tenant == tenant && a.KeywordsList.Contains(wordtemp)
+                                                     select a).FirstOrDefault();
+                CourierPendingReasonCodeList.Add(pendingByKeyword.CourierPendingReasonCode);
+            }
+            return CourierPendingReasonCodeList;
+        }
+
+    }
 
 }
    
