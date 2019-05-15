27f695cbc9ed0b6683f4d90d91312a87fc9ca84e
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2019-05-15 15:47:20 +0300

    fix Jenkins

--- Logitude/Logitude.Accounting.BL/CoreBL/System1000FlatFileAnalyser.cs
+++ Logitude/Logitude.Accounting.BL/CoreBL/System1000FlatFileAnalyser.cs
@@ -98,7 +98,7 @@ namespace Logitude.Accounting.BL.CoreBL
             GLAccountWithholdingTaxUpdateService gLAccountWithholdingTaxUpdateService = new GLAccountWithholdingTaxUpdateService(MyContext, new Dictionary<string, IContext>(), tenant);
 
             CardGLAccountDataView oneVendor = _AllVendorGLAccountCards.Where(p => p.DisplayNumber.Replace(" ", "").PadLeft(15, '0').Substring(0, 15) == vendorLineDTO.VendorCode && (p.VatNumber.Replace(" ", "").PadLeft(9, '0').Substring(0, 9) == vendorLineDTO.SentVATNum || p.VatNumber.Replace(" ", "").PadLeft(9, '0').Substring(0, 9) == vendorLineDTO.LocatedVATNum)).FirstOrDefault();
-            if (oneVendor is null)
+            if (oneVendor == null)
             {
                 MyResultLoadFlatFile.ValidateVendorLineAgaintDBErrors.Add($"Vendor Number {vendorLineDTO.VendorCode} not found ");
                 return;
