2d42e8b10f327db0a22d690053e89060ecbf85c5
Samar Abu Eid <Samar@logitudeworld.com>
2019-05-20 11:32:45 +0300

    52573
    Related Work Items: #52573

--- Logitude/AngularModules/AngularModules/TariffModule/Components/EditTabs/Tariff/VersionTabComponent.ts
+++ Logitude/AngularModules/AngularModules/TariffModule/Components/EditTabs/Tariff/VersionTabComponent.ts
@@ -648,15 +648,17 @@ export class VersionTabComponent extends BaseComponent implements OnDestroy {
         this.isCopyButtonClicked = true;
 
         this.EntityPM.LastVersion = this.EntityPM.LastVersion + 1;
+        this.EntityPM.LastStartDate = this.StartDate;
+        this.EntityPM.LastExpirationDate = this.ExpirationDate;
 
         var copiedVersion: TariffVersionPM = new TariffVersionPM(this.EntityPM);
         copiedVersion.TariffId = this.CurrentVersion.TariffId;
         copiedVersion.Version = this.EntityPM.LastVersion;
         copiedVersion.CreateDate = DateTool.GetCurrentDateAsUtc();
         copiedVersion.CreatedByUserId = SessionInfo.LoggedUserId;
-        copiedVersion.ExpirationDate = this.CurrentVersion.ExpirationDate;
+        copiedVersion.ExpirationDate = this.ExpirationDate;
         copiedVersion.IsDraft = true;
-        copiedVersion.StartDate = this.CurrentVersion.StartDate;
+        copiedVersion.StartDate = this.StartDate;
         copiedVersion.Tenant = SessionInfo.LoggedUserTenant;
         copiedVersion.ParentVersionNumber = this.CurrentVersion.Version;
 
