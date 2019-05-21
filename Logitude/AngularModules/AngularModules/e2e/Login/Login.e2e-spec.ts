88894c3ebb8e53fefc203182ece4b267e9557e9f
Razan Jararah <razan@logitudeworld.com>
2019-05-21 11:20:15 +0300

    Integration: update shipmentVars & Inland-domestic protractor
    Related Work Items: #52083

--- Logitude/AngularModules/AngularModules/e2e/Login/Login.e2e-spec.ts
+++ Logitude/AngularModules/AngularModules/e2e/Login/Login.e2e-spec.ts
@@ -11,7 +11,7 @@ describe('Login Module', () => {
 
   it('Login Success', function () {
     browser.ignoreSynchronization = true;
-    page.navigateTo('http://pre.logitudeworld.com');
+    page.navigateTo('https://system.logitudeworld.com');
     page.DoLogin('razantest@protractor.com', '!R123j456');
   });
 });
