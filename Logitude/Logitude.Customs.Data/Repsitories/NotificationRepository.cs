c3f0e0e45738b3cfa0ac11506079ddeb13c28a4c
mirit <Mirit@amital.co.il>
2019-05-22 08:53:06 +0300

    52305
    Related Work Items: #52305

--- Logitude/Logitude.Customs.Data/Repsitories/NotificationRepository.cs
+++ Logitude/Logitude.Customs.Data/Repsitories/NotificationRepository.cs
@@ -81,7 +81,7 @@ namespace Logitude.Customs.Data.Repsitories
 
            foreach(string id in ids)
            {
-             Notification notification = ( from a in context.Notifications.Include("AssigneTo.Contact").Include("AssigneTo").Include("NotificationDefinition")
+             Notification notification = ( from a in context.Notifications.Include("AssigneTo.Contact").Include("AssigneTo").Include("NotificationDefinition").Include("Card")
                        where a.Tenant == tenant && a.Id == id
 
                        select a).FirstOrDefault();
