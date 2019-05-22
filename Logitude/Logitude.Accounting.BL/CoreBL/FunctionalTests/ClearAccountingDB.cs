265cc1e1766c861359f692339df4eb9d0d9fd8bf
itzik M <itzik@amital.co.il>
2019-05-22 08:55:02 +0300

    52267 תוכנית בדיקה - שלב 1 - מחיקת הדייר שנבחר
    Related Work Items: #52267

---
+++ Logitude/Logitude.Accounting.BL/CoreBL/FunctionalTests/ClearAccountingDB.cs
@@ -0,0 +1,113 @@
+﻿using Logitude.Accounting.Data;
+using Logitude.Accounting.Data.EntityPOCOs;
+using Simplog.Data.InvoiceModel.EntityPOCOs;
+using Simplog.Server.Infrastructure;
+using Simplog.Server.Infrastructure.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logitude.Accounting.BL.CoreBL.FunctionalTests
+{
+    class ClearAccountingDB
+    {
+        public void ClearDB(int tenant)
+        {
+            if (tenant!= 1148)
+            {
+                throw new Exception("tenant!= 1148");
+            }
+            using (var scope = TransactionFactory.GetNewTransaction())
+            {
+
+
+                var accountingContext = AccountingContext.GetContext(tenant);
+
+
+                (accountingContext as DbContextBase)
+                    .DeleteWhere<ReconciliationLine>(rec => rec.Tenant == tenant);
+
+                (accountingContext as DbContextBase)
+        .DeleteWhere<Reconciliation>(rec => rec.Tenant == tenant);
+                (accountingContext as DbContextBase)
+        .DeleteWhere<JournalReconcile>(rec => rec.Tenant == tenant);
+                (accountingContext as DbContextBase)
+        .DeleteWhere<ExternalReconciliationLine>(rec => rec.Tenant == tenant);
+
+                (accountingContext as DbContextBase)
+        .DeleteWhere<ExternalReconciliation>(rec => rec.Tenant == tenant);
+
+                (accountingContext as DbContextBase)
+    .DeleteWhere<LedgerTransaction>(rec => rec.Tenant == tenant);
+                (accountingContext as DbContextBase)
+    .DeleteWhere<JournalLine>(rec => rec.Tenant == tenant);
+                (accountingContext as DbContextBase)
+    .DeleteWhere<JournalMoreData>(rec => rec.Tenant == tenant);
+
+                (accountingContext as DbContextBase)
+    .DeleteWhere<JournalAdditionalData>(rec => rec.Tenant == tenant);
+
+                (accountingContext as DbContextBase)
+    .DeleteWhere<Journal>(rec => rec.Tenant == tenant);
+
+                (accountingContext as DbContextBase)
+    .DeleteWhere<GLAccountTotalByMonth>(rec => rec.Tenant == tenant);
+
+                (accountingContext as DbContextBase)
+    .DeleteWhere<ReconcileExternalPageLine>(rec => rec.Tenant == tenant);
+
+                (accountingContext as DbContextBase)
+    .DeleteWhere<ReconcileExternalPage>(rec => rec.Tenant == tenant);
+
+                
+                //            (accountingContext as DbContextBase)
+                //.DeleteWhere<AccountingPeriod>(rec => rec.Tenant == tenant);
+
+                var exec = $"update GLAccountMoreDatas set BalanceInLocalCurrency=0.00,LocalBalanceInDue=0.00,NextDueDate='',TotalOpenChequesInLocalCur=0.00,TotFutureOpenChequesInLocalCur=0.00 where Tenant ={tenant}";
+                (accountingContext as DbContextBase)
+    .DeleteWhere<BankDepositLine>(rec => rec.Tenant == tenant);
+                (accountingContext as DbContextBase)
+    .DeleteWhere<BankDeposit>(rec => rec.Tenant == tenant);
+
+
+
+                (accountingContext as DbContextBase)
+    .DeleteWhere<ARPaymentCheque>(rec => rec.Tenant == tenant);
+
+                (accountingContext as DbContextBase)
+    .DeleteWhere<ARInvoicePayment>(rec => rec.Tenant == tenant);
+
+
+                (accountingContext as DbContextBase)
+    .DeleteWhere<ARPayment>(rec => rec.Tenant == tenant);
+
+                (accountingContext as DbContextBase)
+    .DeleteWhere<ARInvoiceLine>(rec => rec.Tenant == tenant);
+
+                (accountingContext as DbContextBase)
+    .DeleteWhere<APInvoicePayment>(rec => rec.Tenant == tenant);
+
+                (accountingContext as DbContextBase)
+    .DeleteWhere<ARInvoiceTotalVAT>(rec => rec.Tenant == tenant);
+
+                (accountingContext as DbContextBase)
+    .DeleteWhere<ARInvoice>(rec => rec.Tenant == tenant);
+
+
+                (accountingContext as DbContextBase)
+    .DeleteWhere<APInvoiceLine>(rec => rec.Tenant == tenant);
+
+                (accountingContext as DbContextBase)
+    .DeleteWhere<APInvoiceTotalVAT>(rec => rec.Tenant == tenant);
+
+                (accountingContext as DbContextBase)
+    .DeleteWhere<APInvoice>(rec => rec.Tenant == tenant);
+                scope.Complete();
+            }
+        }
+
+
+    }
+}
