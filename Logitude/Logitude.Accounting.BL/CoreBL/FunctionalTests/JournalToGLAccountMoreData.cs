19ee8d08ad0a6a8f186745bebaf15a3e1959a660
itzik M <itzik@amital.co.il>
2019-05-26 14:06:14 +0300

    52266 מסך לקליטת קובץ אקסל עם נתוני בדיקה + תוצאות
    Related Work Items: #52266

--- Logitude/Logitude.Accounting.BL/CoreBL/FunctionalTests/JournalToGLAccountMoreData.cs
+++ Logitude/Logitude.Accounting.BL/CoreBL/FunctionalTests/JournalToGLAccountMoreData.cs
@@ -1,6 +1,9 @@
 ﻿
 using Logitude.Accounting.BL.EntityQueryServices;
+using Logitude.Accounting.BL.EntityUpdateServices;
+using Logitude.Accounting.Data;
 using Logitude.Accounting.Def.EntityPMs;
+using Simplog.Server.Infrastructure;
 using Simplog.Server.Infrastructure.Helpers;
 using System;
 using System.Collections.Generic;
@@ -22,7 +25,7 @@ namespace Logitude.Accounting.BL.CoreBL.FunctionalTests
             var util = new XLSUtil();
             DataTable XLSTable = util.GetDataTableFromWorkSheet(byteArrayXLS, "Journals");
 
-            
+
             List<DataRow> list = XLSTable.Rows.Cast<DataRow>().ToList();
             var JournalInputRows = list
                 .Where(r => !String.IsNullOrWhiteSpace(r.Field<String>("Counter"))).ToList();
@@ -30,12 +33,64 @@ namespace Logitude.Accounting.BL.CoreBL.FunctionalTests
             ;
             var JournalInput = JournalInputRows.GroupBy(r => r.Field<String>("Counter")).Select(g => GetJournalPM(g)).ToList();
 
+            bool @buildJournals = false;
+            if (@buildJournals)
+            {
+                BuildJournals(tenant, JournalInput);
+            }
+            
             var GLAccountOutputRows = list
                 .Where(r => !String.IsNullOrWhiteSpace(r.Field<String>("GLA")))
+                .ToList()
                 ;
 
+            List<string> res = GLAccountOutputRows.Select(gLAccountOutput => CheckGLAccount(gLAccountOutput)).ToList();
+            
+        }
+
+        private string CheckGLAccount(DataRow gLAccountOutput)
+        {
+            var DisplayNumber  = gLAccountOutput.Field<string>("GLA");
+            var BalanceNIS=toDecimal(gLAccountOutput.Field<string>("BalanceNIS"));
+            var BalanceNISDue = toDecimal(gLAccountOutput.Field<string>("BalanceNISDue"));
+            
+            var qs = new GLAccountQueryService(_Tenant);
+            var glAccountPM = qs.GetSinglePMByDisplayNumber(DisplayNumber, _Tenant); ;
+            if (glAccountPM == null)
+            {
+                throw new Exception($"GetGLAccountByDisplayNumber({DisplayNumber}, {_Tenant}) return null");
+            }
+            string res = null;
+            if (glAccountPM.LocalBalanceInDue!= BalanceNISDue)
+            {
+                res = $"LocalBalanceInDue{glAccountPM.LocalBalanceInDue}!= BalanceNISDue{BalanceNISDue}";
+            }
+            if (glAccountPM.BalanceInLocalCurrency != BalanceNIS)
+            {
+                res = $"BalanceInLocalCurrency{glAccountPM.BalanceInLocalCurrency}!= BalanceNIS{BalanceNIS}";
+            }
+            return $"Card {DisplayNumber}:" + (res ?? " Totals r equal");
+        }
+
+        private static void BuildJournals(int tenant, List<JournalPM> JournalInput)
+        {
+            using (var scope = TransactionFactory.GetTransaction())
+            {
+                foreach (var entityPM in JournalInput)
+                {
+                    var MyContext = AccountingContext.GetContext(tenant);
+                    JournalQueryService mappingService = new JournalQueryService(tenant);
 
 
+
+                    //entityPM.ChangeSetOp = Simplog.Server.Infrastructure.ChangeSetOperation.Update;
+                    JournalUpdateService service = new JournalUpdateService(MyContext, new Dictionary<string, IContext>(), entityPM.Tenant);
+                    service.Update(entityPM, true);
+
+                }
+                scope.Complete();
+
+            }
         }
 
         private JournalPM GetJournalPM(IGrouping<string, DataRow> g)
@@ -69,10 +124,13 @@ namespace Logitude.Accounting.BL.CoreBL.FunctionalTests
                 DueDate = ToDate(r.Field<String>("ValueDate")),
                 DocumentDate = ToDate(r.Field<String>("AccountingDate")),
 
-
                 LocalAmount = toDecimal(r.Field<string>("AmountNIS")),
+                CurrencyCode = r.Field<string>("Currency"),
+                ForeignAmount = toDecimal(r.Field<string>("AmountForeign")),
 
                 Reference1 = r.Field<string>("Reference1"),
+                Reference2 = r.Field<string>("Reference2"),
+                Reference3 = r.Field<string>("Reference3"),
 
             };
             return line;
