b4a110b7bb4dd7042444116aa733ec101a28dd91
moran <moran@amital.co.il>
2019-05-27 16:52:40 +0300

    52254 ms
    Related Work Items: #52254

--- Logitude/Logitude.Customs.BL/EntityUpdateServices/CustomsDocumentUpdateService.cs
+++ Logitude/Logitude.Customs.BL/EntityUpdateServices/CustomsDocumentUpdateService.cs
@@ -630,8 +630,7 @@ namespace Logitude.Customs.BL.EntityUpdateServices
                     }
                     else if (myCustomsRequestsSheetServiceException.Where == CustomsRequestsSheetDomainModelServiceException.WhereEnum.NoAvailableSignServer)
                     {
-                        Logitude.Server.Tools.Helpers.LogMessagingUtil.Instance.AppendLine("2715 RequestInProgress stop create a new one !! " + myCustomsRequestsSheetServiceException.Message);
-                        return send;
+                        
                     }
                     throw;
                 }
