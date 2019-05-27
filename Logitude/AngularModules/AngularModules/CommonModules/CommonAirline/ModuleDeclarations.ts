1ba26fe9f43dd0fd75c5cc0bc4b8a7861b29e21d
Samar Abu Eid <Samar@logitudeworld.com>
2019-05-27 09:33:22 +0300

    52662
    Related Work Items: #52662

--- Logitude/AngularModules/AngularModules/CommonModules/CommonAirline/ModuleDeclarations.ts
+++ Logitude/AngularModules/AngularModules/CommonModules/CommonAirline/ModuleDeclarations.ts
@@ -8,6 +8,7 @@ import {AddEditAirlineMessagingRuleComponent} from './Components/AddEdit/AddEdit
 import {AddEditTarrifHeaderComponent} from './Components/AddEdit/AddEditTarrifHeaderComponent';
 import {AddEditTariffChargeComponent} from './Components/AddEdit/AddEditTariffChargeComponent';
 import { AirlineDocsInTabComponent } from './Components/EditTabs/AirlineDocsInTabComponent';
+import { AirlineGeneralTabComponent } from './Components/EditTabs/AirlineGeneralTabComponent';
 
 export const Components =
     [
@@ -21,6 +22,7 @@ export const Components =
         AddEditTarrifHeaderComponent,
         AddEditTariffChargeComponent,
         AirlineDocsInTabComponent,
+        AirlineGeneralTabComponent,
     ];
 
 export class ModuleDeclarations {
@@ -39,6 +41,7 @@ export class ModuleDeclarations {
             case "AddEditTarrifHeaderComponent": { myResult = AddEditTarrifHeaderComponent; break; }
             case "AddEditTariffChargeComponent": { myResult = AddEditTariffChargeComponent; break; }
             case "AirlineDocsInTabComponent": { myResult = AirlineDocsInTabComponent; break; }
+            case "AirlineGeneralTabComponent": { myResult = AirlineGeneralTabComponent; break; }
         }
 
         return myResult;
