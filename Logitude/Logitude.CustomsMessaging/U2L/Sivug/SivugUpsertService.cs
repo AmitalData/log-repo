f0a4c3805173d29eee9d23ca94998a38a9696d94
moran <moran@amital.co.il>
2019-05-27 16:00:56 +0300

    52746 ms
    Related Work Items: #52746

--- Logitude/Logitude.CustomsMessaging/U2L/Sivug/SivugUpsertService.cs
+++ Logitude/Logitude.CustomsMessaging/U2L/Sivug/SivugUpsertService.cs
@@ -798,6 +798,11 @@ namespace Logitude.CustomsMessaging.U2L.Sivug
                 {
                     SupplierInvoiceItemPM.InvoiceQuantityType = TranslateMeasurmentUnit(invoiceItem.UNIT_ID);
                 }
+                if (string.IsNullOrWhiteSpace(SupplierInvoiceItemPM.StatisticQuantityType) && !string.IsNullOrWhiteSpace(invoiceItem.StatisticQuantityType))
+                {
+                    SupplierInvoiceItemPM.StatisticQuantityType = TranslateMeasurmentUnit(invoiceItem.StatisticQuantityType);
+                }
+                
                 if (string.IsNullOrWhiteSpace(SupplierInvoiceItemPM.TaxExemptCode) && !string.IsNullOrWhiteSpace(invoiceItem.TAXEXEMPTCODE))
                 {
                     SupplierInvoiceItemPM.TaxExemptCode = invoiceItem.TAXEXEMPTCODE; //TranslateTaxExemptCode(invoiceItem.TAXEXEMPTCODE);
