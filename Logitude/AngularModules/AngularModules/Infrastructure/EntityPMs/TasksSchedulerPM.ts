5e5269e1b3d0517b6f7b7576ce6d2a8f77c4b761
Abdullah Dawoud <abdullah@logitudeworld.com>
2019-06-02 11:50:34 +0300

    Related Work Items: #48787

--- Logitude/AngularModules/AngularModules/Infrastructure/EntityPMs/TasksSchedulerPM.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/EntityPMs/TasksSchedulerPM.ts
@@ -205,9 +205,9 @@ export class TasksSchedulerPM {
     public set LastRunStartTime(newValue: Date) { if (this.lastRunStartTime != newValue) { this.lastRunStartTime = newValue; this.MarkAsDirty("LastRunStartTime"); } }
        
 	 
-    private schedulerDetailsData: any;
+    private schedulerDetailsData: string;
     public get SchedulerDetailsData() { return this.schedulerDetailsData; }
-    public set SchedulerDetailsData(newValue: any) { if (this.schedulerDetailsData != newValue) { this.schedulerDetailsData = newValue; this.MarkAsDirty("SchedulerDetailsData"); } }
+    public set SchedulerDetailsData(newValue: string) { if (this.schedulerDetailsData != newValue) { this.schedulerDetailsData = newValue; this.MarkAsDirty("SchedulerDetailsData"); } }
        
 	 
 
