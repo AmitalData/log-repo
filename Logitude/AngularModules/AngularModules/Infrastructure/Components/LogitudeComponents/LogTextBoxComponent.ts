e85f5440cfa8985db6ccd77df26a5b14411df036
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2019-06-02 21:23:29 +0300

    fix

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/LogTextBoxComponent.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/LogTextBoxComponent.ts
@@ -32,7 +32,7 @@ type NgxInstance = BeforeOnDestroy & Object;
 type Descriptor = TypedPropertyDescriptor<Function>;
 type Key = string | symbol;
 
-function BeforeOnDestroy(target: NgxInstance, key: Key, descriptor: Descriptor) {
+export function BeforeOnDestroy(target: NgxInstance, key: Key, descriptor: Descriptor) {
     return {
         value: async function (...args: any[]) {
             await target.ngxBeforeOnDestroy();
