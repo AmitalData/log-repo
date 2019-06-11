24bdbf5094e44fad97255b846e003ee83b350224
Razan Jararah <razan@logitudeworld.com>
2019-06-11 10:40:11 +0300

    Domestic Houses - protractor
    Related Work Items: #53121

--- Logitude/AngularModules/AngularModules/e2e/Operations/Shipments/NewEntity/HouseShipment.ts
+++ Logitude/AngularModules/AngularModules/e2e/Operations/Shipments/NewEntity/HouseShipment.ts
@@ -1,30 +1,30 @@
 import { browser, by, element, WebDriver, protractor } from 'protractor';
 import { FieldsHelper } from '../../../Helpers/FieldsHelper';
-import {ShipmentHelper} from '../ShipmentHelper';
+import { ShipmentHelper } from '../ShipmentHelper';
 
 export class HouseShipment {
   private Helper: FieldsHelper;
-  private ShipmentModes:ShipmentHelper;
+  private ShipmentModes: ShipmentHelper;
 
 
   constructor() {
     this.Helper = new FieldsHelper();
-    this.ShipmentModes=new ShipmentHelper();
+    this.ShipmentModes = new ShipmentHelper();
   }
 
 
-  public CreateHouseShipment(ReferenceNumber: string,LogitudeShipType:string, Direction:any, TransportMode:any, ShipmentType:any) {
+  public CreateHouseShipment(ReferenceNumber: string, LogitudeShipType: string, Direction: any, TransportMode: any, ShipmentType: any) {
 
     var AWBToggle = this.Helper.WaitByIdAndClick('NEWSHIP');
     this.Helper.WaitByIdAndClick('NEWHOUSE');
     this.ShipmentModes.SelectDicrctionTransportMode(Direction, TransportMode, ShipmentType);
 
-    this.FillHouseShipmentFields(ReferenceNumber);
+    this.FillHouseShipmentFields(ReferenceNumber, Direction);
     this.Helper.WaitByIdAndClick('ShipmentCreatebtn');
   }
 
 
-  FillHouseShipmentFields(ShipperRef: string) {
+  FillHouseShipmentFields(ShipperRef: string, Direction: string) {
     this.Helper.WaitByIdAndFill('Shipment_ShipperId', 'TestShipper');
     this.Helper.WaitByCssAndClick_FromTagInsideList('.DropDownListItem', 0);
 
@@ -33,14 +33,21 @@ export class HouseShipment {
     this.Helper.WaitByIdAndFill('Shipment_ConsigneeId', 'TestConsignee');
     this.Helper.WaitByCssAndClick_FromTagInsideList('.DropDownListItem', 0);
 
-    if(ShipperRef!='CreatedFromMaster'){
+    if (ShipperRef != 'CreatedFromMaster') {
+      if (Direction == 'Domestic') {
+        this.Helper.WaitByIdAndFill('Shipment_MainCarriageFromPortId', 'LAS');
+        this.Helper.WaitByCssAndClick_FromTagInsideList('.DropDownListItem', 0);
 
-    this.Helper.WaitByIdAndFill('Shipment_MainCarriageFromPortId', 'eze');
-    this.Helper.WaitByCssAndClick_FromTagInsideList('.DropDownListItem', 0);
+        this.Helper.WaitByIdAndFill('Shipment_MainCarriageToPortId', 'MIA');
+        this.Helper.WaitByCssAndClick_FromTagInsideList('.DropDownListItem', 0);
+      } else {
+        this.Helper.WaitByIdAndFill('Shipment_MainCarriageFromPortId', 'eze');
+        this.Helper.WaitByCssAndClick_FromTagInsideList('.DropDownListItem', 0);
 
-    this.Helper.WaitByIdAndFill('Shipment_MainCarriageToPortId', 'mvd');
-    this.Helper.WaitByCssAndClick_FromTagInsideList('.DropDownListItem', 0);
-  }
+        this.Helper.WaitByIdAndFill('Shipment_MainCarriageToPortId', 'mvd');
+        this.Helper.WaitByCssAndClick_FromTagInsideList('.DropDownListItem', 0);
+      }
+    }
     this.Helper.WaitByIdAndFill('Shipment_IncotermId', 'LDE');
     this.Helper.WaitByCssAndClick_FromTagInsideList('.DropDownListItem', 0);
 
