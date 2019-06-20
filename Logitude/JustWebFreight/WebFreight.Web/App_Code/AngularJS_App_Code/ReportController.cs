659dc496feb0e4ec3937821d2053fe8029f3c1a9
Abed <abed@logitudeworld.com>
2019-06-20 10:23:31 +0300

    53274

--- Logitude/JustWebFreight/WebFreight.Web/App_Code/AngularJS_App_Code/ReportController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/App_Code/AngularJS_App_Code/ReportController.cs
@@ -120,7 +120,7 @@ namespace WebFreight.Web.App_Code.AngularJS_App_Code
                     {
                         ReportHelper reportHelper = new ReportHelper();
                         byte[] filters = reportHelper.GetReportFilters(reportFliter.QueryFilterItemLists);
-                        Thread.Sleep(new TimeSpan(0,0,55));
+                   
                         LogitudeReportsWebService logitudeReportsWebService = new LogitudeReportsWebService();
 
                         byte[] dataProvider = reportHelper.BuildReportDataProvider(reportFliter, filters);
