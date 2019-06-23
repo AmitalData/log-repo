3de948d8609bc5b86325274f53ddf386b5121332
alaa <alaa.jamal.h@live.com>
2019-06-23 14:41:17 +0300

    53989
    Related Work Items: #53989

--- Logitude/AngularModules/AngularModules/Accounting/Components/EditTabs/PaymentCheque/PaymentChequeGeneralTabComponent.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/EditTabs/PaymentCheque/PaymentChequeGeneralTabComponent.ts
@@ -185,9 +185,16 @@ export class PaymentChequeGeneralTabComponent extends BaseComponent implements  
             this.account = value;
             if (value != null) {
              
-                    if (AppTool.IsNullOrEmpty(this.PayToName)) {
-                        this.PayToName = value.LocalName;
-                    }
+                if (value.AccountTypeCode == "3") {
+                  
+                    this.UIProperties.SetValidity("PayToGLAccountId", this.ObjectTableName, false, TextCodeTranslator.Translate("Accounting.General.O.VendorsGLAccount"));
+                }
+                else {
+                    this.UIProperties.SetValidity("PayToGLAccountId", this.ObjectTableName, true, null);
+
+                 
+                    this.PayToName = value.LocalName;
+                }
                 
             }
         }
