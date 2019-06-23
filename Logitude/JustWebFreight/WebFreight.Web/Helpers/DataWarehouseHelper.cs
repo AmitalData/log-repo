7a52e3532ef9183d6a9e7215dea4f6974cf2a4c5
Abed <abed@logitudeworld.com>
2019-06-23 23:20:33 +0300

    53027
    Related Work Items: #53027

--- Logitude/JustWebFreight/WebFreight.Web/Helpers/DataWarehouseHelper.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Helpers/DataWarehouseHelper.cs
@@ -52,21 +52,23 @@ namespace WebFreight.Web.Helpers
 
                 betweenDateValue1 = string.Format("{0:yyyy-MM-dd}", DateTime.Parse(valuesBetweenDateArray[0]));
                 betweenDateValue2 = valuesBetweenDateArray.Length > 1 ? string.Format("{0:yyyy-MM-dd}", DateTime.Parse(valuesBetweenDateArray[1])) : null;
-            
-                if(!string.IsNullOrEmpty(betweenDateValue1) && !string.IsNullOrEmpty(betweenDateValue2))
+
+
+                if (!isSample)
                 {
-                    result = fieldName + ">= '" + betweenDateValue1 + "' and " + fieldName + "<= '" + betweenDateValue2 + "'" ;
+                    if (!string.IsNullOrEmpty(betweenDateValue1) && !string.IsNullOrEmpty(betweenDateValue2))
+                    {
+                        result = fieldName + ">= '" + betweenDateValue1 + "' and " + fieldName + "<= '" + betweenDateValue2 + "'";
+                    }
+                    else
+                    {
+                        string betweenDateValue = !string.IsNullOrEmpty(betweenDateValue1) ? betweenDateValue1 : betweenDateValue2;
+                        result = fieldName + ">= '" + betweenDateValue + "' and " + fieldName + "<'" + string.Format("{0:yyyy-MM-dd}", DateTime.Parse(betweenDateValue).AddDays(1)) + "'";
+                    }
                 }
                 else
                 {
-                    string betweenDateValue = !string.IsNullOrEmpty(betweenDateValue1) ? betweenDateValue1 : betweenDateValue2;
-                    result = fieldName + ">= '" + string.Format("{0:yyyy-MM-dd}", DateTime.Parse(betweenDateValue)) + "' and " + fieldName + "<'" + string.Format("{0:yyyy-MM-dd}", DateTime.Parse(betweenDateValue).AddDays(1)) + "'";
-                }
-
-                if (isSample)
-                {
-                    result = betweenDateValue1 + "    -     " + betweenDateValue2;
-
+                    result = FormatDate(DateTime.Parse(betweenDateValue1), tenant) + " - " + (!string.IsNullOrEmpty(betweenDateValue2) ? FormatDate(DateTime.Parse(betweenDateValue2), tenant) : null);
                 }
 
             }
