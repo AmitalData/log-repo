a10ea4c0c2fa4f3218bd82d7e6881fec9f3ae994
Abed <abed@logitudeworld.com>
2019-06-24 15:59:31 +0300

    Report Worker Role

--- Logitude/CommunicationWorkerRole/ReportExecutionLogWorkerRole.cs
+++ Logitude/CommunicationWorkerRole/ReportExecutionLogWorkerRole.cs
@@ -182,6 +182,7 @@ namespace CommunicationWorkerRole
             }
         }
 
+
         private void ConnectClient()
         {
             try
@@ -212,9 +213,11 @@ namespace CommunicationWorkerRole
                 {
                     ExceptionHandler.HandleException(ex, DateTime.Now, 0, null, "Report Execution Log Queue worker role start", null, null);
                     this.UpdateReportExecutionLog(ex, reportExecutionLog, reportExecutionLogRepository, "F");
-                    queueservice.CompleteAsFailed();
+ 
 
                 }
+
+                queueservice.CompleteAsFailed();
             }
         }
 
