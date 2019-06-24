c1a8b966bcc68e4ec7064b75a43170fa0377b897
Razan Jararah <razan@logitudeworld.com>
2019-06-24 13:59:24 +0300

    Protractor - Ocean/Inland Receivables Payables
    Related Work Items: #53865

--- Logitude/AngularModules/AngularModules/protractor.conf.js
+++ Logitude/AngularModules/AngularModules/protractor.conf.js
@@ -39,8 +39,8 @@ exports.config = {
     });
     if (browser.params.Env == "prod") {
       browser.params.Link = "https://system.logitudeworld.com";
-      browser.params.Login.Email = "razantest@protractor.com";
-      browser.params.Login.Password = "!R123j456";
+      browser.params.Login.Email = "protractor@test.com";
+      browser.params.Login.Password = "!P123t456";
     }
     else if (browser.params.Env == "staging") {
       browser.params.Link = "https://staging.logitudeworld.com";
@@ -83,8 +83,8 @@ exports.config = {
     //  CRM: 'e2e/CRM/**/CRMModule-spec.ts',
     //  'e2e/LogBox/Login/**/Login.e2e-spec.ts',
 
-    //NewShipment: 'e2e/Operations/Shipments/NewEntity/**/Operations.e2e-spec.ts',
-      NewShipmentlogbox: 'e2e/LogBox/Shipments/**/ShipmentSearch.e2e-spec.ts',
+    NewShipment: 'e2e/Operations/Shipments/NewEntity/**/Operations.e2e-spec.ts',
+      // NewShipmentlogbox: 'e2e/LogBox/Shipments/**/ShipmentSearch.e2e-spec.ts',
     //  Contact: 'e2e/Contacts/**/Contacts-spec.ts',
 
     //EditTabs: 'e2e/Operations/Shipments/EditEntity/**/EditShipmentTabs.e2e-spec.ts',
