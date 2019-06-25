93665e7cf35274a415681fb2115f5ceb39c1a640
Khalid Khatib <khalid@logitudeworld.com>
2019-06-25 14:45:22 +0300

    Tariff: airlines areas
    Related Work Items: #53771

--- Logitude/AngularModules/AngularModules/CommonModules/CommonAirline/Components/AddEdit/AddEditAirlineAreaComponent.ts
+++ Logitude/AngularModules/AngularModules/CommonModules/CommonAirline/Components/AddEdit/AddEditAirlineAreaComponent.ts
@@ -18,9 +18,8 @@ import {CommodityPMService} from '../../../../Common/Services/StandardPMs/Commod
 })
 
 export class AddEditAirlineAreaComponent extends BaseComponent {
-    public AirlinePM: AirlinePM;
-    public EntityPM;
-    public ObjectTableName: string;
+    public EntityPM: AirlinePM;
+    public ObjectTableName: string="AirlineAreasPort";
     public DataContext: AddEditAirlineAreaComponent = this;
     public IsNew: boolean;
     public ValidationErrorsList: string[] = [];
@@ -31,90 +30,18 @@ export class AddEditAirlineAreaComponent extends BaseComponent {
 
     public IsResourcesReady: boolean = false;
     SetWindowArgs(windowArgs: any) {
-        this.AirlinePM = windowArgs['AirlinePM'];
         this.EntityPM = windowArgs['EntityPM'];
         this.IsNew = windowArgs['IsNew'];
-        this.ObjectTableName = windowArgs['ObjectTableName'];
-        this.SetUIProperties();
-        this.Clone();
 
         this.IsResourcesReady = true;
     }
 
-    SetUIProperties() {
-        this.UIProperties.SetVisibility("InActive", this.ObjectTableName, !this.IsNew);
-    }
 
-    get Code() { return this.EntityPM.Code; }
-    set Code(value: string) {
-        if (this.EntityPM.Code != value) {
-            this.EntityPM.Code = value;
-        }
-    }
-
-    get Name() { return this.EntityPM.Name; }
-    set Name(value: string) {
-        if (this.EntityPM.Name != value) {
-            this.EntityPM.Name = value;
-        }
-    }
-
-    get InActive() { return this.EntityPM.InActive; }
-    set InActive(value: boolean) {
-        if (this.EntityPM.InActive != value) {
-            this.EntityPM.InActive = value;
-        }
-    }
-
-    CancelButtonClicked() {
-        this.RejectChanges();
-        this.CurrentSession.CloseCurrentWindow();
-    }
+   
 
     private myService;
-    OkButtonClicked() {
-        var errors: string[] = [];
-
-        Validator.TryValidateObject(this.EntityPM, this.ObjectTableName, errors);
-        this.ValidationErrorsList = errors;
-
-        if (this.ValidationErrorsList.length == 0) {
-            this.SetService();
-
-            if (this.IsNew) {
-                this.CurrentSession.StartBusyIndicatorSaving();
-                this.myService.insert(this.EntityPM).subscribe(Result => {
-
-                    var mm: ServiceResponse = Result;
-                    if (!mm.HasError) {
-                        CachedDataManager.RefreshTableData(this.ObjectTableName, true);
-                        CachedDataManager.RefreshCompleted.subscribe(($event: any) => this.Close($event));
-                    }
-
-                    else {
-                        this.ValidationErrorsList = mm.ErrorsArray;
-                        this.CurrentSession.StopBusyIndicator();
-                    }
-                });
-            }
-
-            else {
-                this.CurrentSession.StartBusyIndicatorSaving();
-                this.myService.update(this.EntityPM).subscribe(Result => {
-
-                    var mm: ServiceResponse = Result;
-                    if (!mm.HasError) {
-                        CachedDataManager.RefreshTableData(this.ObjectTableName, true);
-                        CachedDataManager.RefreshCompleted.subscribe(($event: any) => this.Close($event));
-                    }
-
-                    else {
-                        this.ValidationErrorsList = mm.ErrorsArray;
-                        this.CurrentSession.StopBusyIndicator();
-                    }
-                });
-            }
-        }
+    CloseButtonClicked() {
+        this.CurrentSession.CloseCurrentWindow();
     }
 
     private Close(event: any) {
@@ -122,40 +49,7 @@ export class AddEditAirlineAreaComponent extends BaseComponent {
         this.CurrentSession.CloseCurrentWindowEmit("ok");
     }
 
-    private SetService() {
-        switch (this.ObjectTableName) {
-            case "AWBSpecialHandlingCode": {
-                this.myService = new AWBSpecialHandlingCodeExtendedPMService();
-                break;
-            }
+  
 
-            case "Commodity": {
-                this.myService = new CommodityPMService();
-                break;
-            }
 
-            case "IATACode": {
-                this.myService = new IATACodeExtendedPMService();
-                break;
-            }
-
-            case "BookingProduct": {
-                this.myService = new BookingProductExtendedPMService();
-                break;
-            }
-        }
-    }
-
-    private myCloner: Cloner;
-    private Clone() {
-        this.myCloner = new Cloner(this.DataContext);
-        this.myCloner.AddField('Name');
-        this.myCloner.AddField('Code');
-        this.myCloner.AddField('InActive');
-        this.myCloner.AddEntity(this.EntityPM);
-    }
-
-    private RejectChanges() {
-        this.myCloner.RejectChanges();
-    }
 }
