0759af8ade41997c30f72370778772c8a6a78639
Ayman1 <Ayman@logitudeworld.com>
2019-06-25 10:49:29 +0300

    fix

--- Logitude/AngularModules/AngularModules/TariffModule/EntityPMs/TariffPM.ts
+++ Logitude/AngularModules/AngularModules/TariffModule/EntityPMs/TariffPM.ts
@@ -8,7 +8,6 @@
 //------------------------------------------------------------------------------
 
 import {TariffVersionPM} from './TariffVersionPM';
-import {TariffVersionPM} from './TariffVersionPM';
 import {UIProperties, UIProperty} from '../../Infrastructure/Components/LogitudeComponents/UIProperties';
 import {ServiceHelper} from '../../Infrastructure/Utilities/ServiceHelper';
 import {ServiceLocator} from '../../Infrastructure/Locators/ServiceLocator';
