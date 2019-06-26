5fcd7c0fa803151142a5b91b85b99fc79b9ec53d
Khalid Khatib <khalid@logitudeworld.com>
2019-06-26 10:00:21 +0300

    Tariff: airlines areas
    Related Work Items: #53771

--- Logitude/AngularModules/AngularModules/CommonModules/CommonAirline/Components/AddEdit/AddEditAirlineAreaComponent.ts
+++ Logitude/AngularModules/AngularModules/CommonModules/CommonAirline/Components/AddEdit/AddEditAirlineAreaComponent.ts
@@ -1,16 +1,15 @@
 import {Component, Output, EventEmitter} from '@angular/core';
-import {AirlinePM} from '../../../../Common/EntityPMs/AirlinePM';
+import { AirlinePM } from '../../../../Common/EntityPMs/AirlinePM';
+import { AirlineAreaPM } from '../../../../Common/EntityPMs/AirlineAreaPM';
+
 import {BaseComponent} from '../../../../Infrastructure/Components/LogitudeComponents/BaseComponent';
-import {SessionLocator} from '../../../../Infrastructure/Utilities/SessionLocator';
-import {AppTool} from '../../../../Infrastructure/Tools';
+import { SessionLocator } from '../../../../Infrastructure/Utilities/SessionLocator';
+import { SessionInfo } from '../../../../Infrastructure/Utilities/SessionInfo';
+import {AppTool, DateTool} from '../../../../Infrastructure/Tools';
 import {Cloner} from '../../../../Infrastructure/Utilities/Cloner';
 import {Validator} from '../../../../Infrastructure/Validators/Validator';
 import {ServiceResponse} from '../../../../Infrastructure/DataContracts/ServiceResponse';
 import { CachedDataManager } from '../../../../Infrastructure/Utilities/CachedDataManager';
-import {AWBSpecialHandlingCodeExtendedPMService} from '../../../../Shipment/Services/ExtendedPMs/AWBSpecialHandlingCodeExtendedPMService';
-import {IATACodeExtendedPMService} from '../../../../Infrastructure/Services/ExtendedPMs/IATACodeExtendedPMService';
-import {BookingProductExtendedPMService} from '../../../../Booking/Services/ExtendedPMs/BookingProductExtendedPMService';
-import {CommodityPMService} from '../../../../Common/Services/StandardPMs/CommodityPMService';
 
 @Component({
     moduleId: module.id,
@@ -18,29 +17,53 @@ import {CommodityPMService} from '../../../../Common/Services/StandardPMs/Commod
 })
 
 export class AddEditAirlineAreaComponent extends BaseComponent {
-    public EntityPM: AirlinePM;
-    public ObjectTableName: string="AirlineAreasPort";
+    public EntityPM: AirlineAreaPM;
+    public AirlinePM: AirlinePM;
+
+    public ObjectTableName: string="AirlineArea";
     public DataContext: AddEditAirlineAreaComponent = this;
     public IsNew: boolean;
+    public ItemList = [];
     public ValidationErrorsList: string[] = [];
     private CurrentSession = SessionLocator.SelectedSession;
+    
+
     constructor() {
         super();
     }
 
+    public get Name() { return this.EntityPM.Name; }
+    public set Name(value: string) { this.EntityPM.Name = value; }
+
+
+    public get Description() { return this.EntityPM.Description; }
+    public set Description(value: string) { this.EntityPM.Description = value; }
+    
     public IsResourcesReady: boolean = false;
     SetWindowArgs(windowArgs: any) {
-        this.EntityPM = windowArgs['EntityPM'];
+        this.AirlinePM = windowArgs['EntityPM'];
         this.IsNew = windowArgs['IsNew'];
-
+        if (this.IsNew) {
+            this.EntityPM = new AirlineAreaPM(this.AirlinePM);
+        }
         this.IsResourcesReady = true;
     }
 
 
    
 
-    private myService;
     CloseButtonClicked() {
+        if (this.IsNew) {
+            this.EntityPM.CreatedByUserName = SessionInfo.LoggedUserPM.EnglishName;
+            this.EntityPM.UpdatedByUserName = SessionInfo.LoggedUserPM.EnglishName;
+            this.EntityPM.CreateDate = DateTool.GetCurrentDateTimeAsUtc();
+            this.EntityPM.UpdateDate = DateTool.GetCurrentDateTimeAsUtc();
+
+            this.AirlinePM.AddAirlineAreaPM(this.EntityPM);
+        }
+        else {
+
+        }
         this.CurrentSession.CloseCurrentWindow();
     }
 
