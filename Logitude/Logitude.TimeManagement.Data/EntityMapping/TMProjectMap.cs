470fba3d96b7f3944ba9b4c1f07b810addc1e8b4
Maheera Jazi <Maheera@logitudeworld.com>
2019-06-26 09:18:11 +0300

    53793
    Related Work Items: #53793

--- Logitude/Logitude.TimeManagement.Data/EntityMapping/TMProjectMap.cs
+++ Logitude/Logitude.TimeManagement.Data/EntityMapping/TMProjectMap.cs
@@ -57,7 +57,7 @@ namespace Logitude.TimeManagement.Data.EntityMapping
 
             this.Property(t => t.IsProrated).HasColumnName("IsProrated");
 
-            this.Property(t => t.ExternalProjectNumber).HasColumnName("ExternalProjectNumber").HasMaxLength(10).IsUnicode(false);
+            this.Property(t => t.ExternalProjectNumber).HasColumnName("ExternalProjectNumber").HasMaxLength(25).IsUnicode(false);
 
             this.Property(t => t.ExcludeFromProrating).HasColumnName("ExcludeFromProrating");
 
