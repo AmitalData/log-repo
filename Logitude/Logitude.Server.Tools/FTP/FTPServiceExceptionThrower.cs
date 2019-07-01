029df4106210247467d2dd5dea6e89209e26a64b
Islam Shaheen <islam@logitudeworld.com>
2019-07-01 12:29:10 +0300

    fix

--- Logitude/Logitude.Server.Tools/FTP/FTPServiceExceptionThrower.cs
+++ Logitude/Logitude.Server.Tools/FTP/FTPServiceExceptionThrower.cs
@@ -14,19 +14,31 @@ namespace Logitude.Server.Tools.FTP
 
 			string errorMessage = "";
 			var ftpResponse = ex.Response as FtpWebResponse;
-			switch (ftpResponse.StatusCode)
-			{
-				case FtpStatusCode.NotLoggedIn:
-					errorMessage += "Failed to perform 'Logon' to Host:'" + host + "' ,User:'" + user + Environment.NewLine + ftpResponse.StatusDescription;
-					break;
-				case FtpStatusCode.ActionNotTakenFileUnavailable:
-					errorMessage += "Failed to " + operation + " " + remoteFile + " file" + Environment.NewLine + ftpResponse.StatusDescription;
-					break;
-				default:
-					errorMessage += "Failed to " + operation + " " + (!string.IsNullOrEmpty(remoteFile) ? remoteFile + " file" : "") + Environment.NewLine + ftpResponse.StatusDescription;
-					break;
-			}
+            if (ftpResponse != null)
+            {
+                switch (ftpResponse.StatusCode)
+                {
+                    case FtpStatusCode.NotLoggedIn:
+                        errorMessage += "Failed to perform 'Logon' to Host:'" + host + "' ,User:'" + user + Environment.NewLine + ftpResponse.StatusDescription;
+                        break;
 
+
+                    case FtpStatusCode.ActionNotTakenFileUnavailable:
+                        errorMessage += "Failed to " + operation + " " + remoteFile + " file" + Environment.NewLine + ftpResponse.StatusDescription;
+                        break;
+                    default:
+                        errorMessage += "Failed to " + operation + " " + (!string.IsNullOrEmpty(remoteFile) ? remoteFile + " file" : "") + Environment.NewLine + ftpResponse.StatusDescription;
+                        break;
+                }
+            }
+            else
+            {
+                errorMessage += "Failed to " + operation + " " + (!string.IsNullOrEmpty(remoteFile) ? remoteFile + " file" : "") + Environment.NewLine + ex.Message;
+                if (ex.InnerException != null && !string.IsNullOrEmpty(ex.InnerException.Message))
+                {
+                    errorMessage += Environment.NewLine + ex.InnerException.Message;
+                }
+            }
 			errorMessage = DateTime.Now.ToString() + " : " + errorMessage;
 			throw new FTPServiceException(errorMessage);
 			//return errorMessage;
