a5e4a7fa435f3a2d0b78b34a2dcb7d6f442b085f
Khalid Khatib <khalid@logitudeworld.com>
2019-07-08 16:19:38 +0300

    API: inside packages
    Related Work Items: #54679

--- Logitude/JustWebFreight/WebFreight.Web/ExternalAPIs/V1/DirectController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/ExternalAPIs/V1/DirectController.cs
@@ -105,7 +105,36 @@ namespace WebFreight.Web.ExternalAPIs.V1
                                 {
                                     foreach (OceanOrInlandPackage item in entity.OceanOrInlandPackages)
                                     {
-                                        if (item.PackageType == null)
+
+                                        if (item.InsidePackages != null && item.InsidePackages.Count > 0)
+                                        {
+                                            foreach (InsidePackage itemInside in item.InsidePackages)
+                                            {
+                                                if (itemInside.PackageType != null)
+                                                {
+                                                    Logitude.BL.CommonDataModel.APIDataContract.ApiV1.PackageTypeQueryService PackageTypeService0 = new Logitude.BL.CommonDataModel.APIDataContract.ApiV1.PackageTypeQueryService(authToken.Tenant);
+                                                    PackageTypePM PackageTypePM = PackageTypeService0.PackageTypeDataMappingAndValidatin(itemInside.PackageType, authToken.Tenant);
+                                                    if (PackageTypePM != null)
+                                                    {
+                                                        if (PackageTypePM.IsContainer == true)
+                                                        {
+                                                            throw new ApplicationException("Invalid Inside Package Type Code") ;
+                                                        }
+                                                    }
+                                                    else
+                                                    {
+                                                        itemInside.PackageType = null;
+                                                    }
+                                                }
+
+                                            }
+
+                                        }
+                                             
+                                        
+
+
+                                            if (item.PackageType == null)
                                         {
                                             string message = entity.ShipmentType.Code.Contains("LCL") ? "Package Type is required" : "Container Type is required";
                                             throw new ApplicationException(message);
@@ -420,6 +449,7 @@ namespace WebFreight.Web.ExternalAPIs.V1
                                             {
                                                 throw new ApplicationException("Inside Packages Quantity is required");
                                             }
+                                            inside.Volume = ComputeHelper.ComputeInsideVolume(inside, entityPM);
 
                                             inside.VolumetricWeight = ComputeHelper.ComputeInsideVolumetricWeight(inside, entityPM);
 
