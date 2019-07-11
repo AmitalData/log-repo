b842f6ea2d943b81f81b092e529f9a10564f6594
Khawla Ata <khawla@logitudeworld.com>
2019-07-11 15:29:16 +0300

    fix by abdullah
    Related Work Items: #54670

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/AccountingModel/ReconciliationOpController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/AccountingModel/ReconciliationOpController.cs
@@ -170,7 +170,7 @@ namespace WebFreight.Web.Controllers.AccountingModel //AccountingPeriodViewsCont
             {
 
                 agg.OpenReconciliationDraft= qs.OpenReconciliationDraft(gLAccountId, tenant);
-                myQueryOperations.CallBack = qs.GetReconciliationFilterCallBack(myQueryOperations, gLAccountId, 
+                myQueryOperations.CallBack = qs.GetOpenReconciliationFilterCallBack(myQueryOperations, gLAccountId, 
 tenant);
             }
             var openReconciliation = qs.GetOpenReconciliationFilterList(myQueryOperations, myQueryOperations.CallBack, gLAccountId, tenant);
@@ -248,7 +248,7 @@ tenant);
 
                 LedgerTransactionListQueryService transactionQuery = new LedgerTransactionListQueryService(AccountingContext.GetContext(tenant));
 
-                GenericCallBack callback = transactionQuery.GetReconciliationFilterCallBack(queryOperations, gLAccountId, tenant);
+                GenericCallBack callback = transactionQuery.GetOpenReconciliationFilterCallBack(queryOperations, gLAccountId, tenant);
 
                 List<LedgerTransactionList> openReconciliation = transactionQuery.GetOpenReconciliationFilterList(queryOperations, callback, gLAccountId, tenant);
 
@@ -284,7 +284,7 @@ tenant);
 
                 LedgerTransactionListQueryService transactionQuery = new LedgerTransactionListQueryService(AccountingContext.GetContext(tenant));
 
-                GenericCallBack callback = transactionQuery.GetReconciliationFilterCallBack(queryOperations, gLAccountId, tenant,false);
+                GenericCallBack callback = transactionQuery.GetReconciliationFilterCallBack(queryOperations, gLAccountId, tenant);
 
                 List<LedgerTransactionList> openReconciliation = transactionQuery.GetReconciliationFilterList(queryOperations, callback, gLAccountId, tenant);
 
