4808359a69d61caf3af0df9ea9383225ab9722c7
Ahmad Baker <ahmadb@logitudeworld.com>
2019-07-14 13:50:27 +0300

    Nawras

--- Logitude/AngularModules/AngularModules/InfrastructureModules/InfrastructureBatchService/Components/TaskScheduler/TaskSchedulerComponent.ts
+++ Logitude/AngularModules/AngularModules/InfrastructureModules/InfrastructureBatchService/Components/TaskScheduler/TaskSchedulerComponent.ts
@@ -129,7 +129,7 @@ export class TaskSchedulerComponent implements OnInit  {
         }
 
         else {
-            //this.LoadHistoryList();
+           // this.LoadHistoryList();
             this.LoadTaskHistories();
         }
     }
@@ -308,19 +308,19 @@ export class TaskSchedulerComponent implements OnInit  {
         sortingCol = "StartDateTimeUTC";
         sortingDir = "descending"; 
         if (!this.SelectedRow) {
-        //    if (filters.AdditionalFilters.filter(a => a.FieldName == "TaskId").length > 0) {
-        //        filters.AdditionalFilters = filters.AdditionalFilters.filter(a => a.FieldName != "TaskId");
-        //    }
-        //    filters.addAdditionalFilter("TaskId", "0-0", null, null, "Equals", false, false, false, "String");
-        //}
-        //else {
-        //    if (!AppTool.IsNullOrEmpty(this.SelectedRow.Id)) {
-        //        if (filters.AdditionalFilters.filter(a => a.FieldName == "TaskId").length > 0) {
-        //            filters.AdditionalFilters = filters.AdditionalFilters.filter(a => a.FieldName != "TaskId");
-        //        }
-        //        filters.addAdditionalFilter("TaskId", this.SelectedRow.Id, null, null, "Equals", false, false, false, "String");
-        //    }
-            return
+            //if (filters.AdditionalFilters.filter(a => a.FieldName == "TaskId").length > 0) {
+            //    filters.AdditionalFilters = filters.AdditionalFilters.filter(a => a.FieldName != "TaskId");
+            //}
+            //filters.addAdditionalFilter("TaskId", "0-0", null, null, "Equals", false, false, false, "String");
+        }
+        else {
+            if (!AppTool.IsNullOrEmpty(this.SelectedRow.Id)) {
+                if (filters.AdditionalFilters.filter(a => a.FieldName == "TaskId").length > 0) {
+                    filters.AdditionalFilters = filters.AdditionalFilters.filter(a => a.FieldName != "TaskId");
+                }
+                filters.addAdditionalFilter("TaskId", this.SelectedRow.Id, null, null, "Equals", false, false, false, "String");
+            }
+            //return
         }
 
         filters.GetCount = getCount;
