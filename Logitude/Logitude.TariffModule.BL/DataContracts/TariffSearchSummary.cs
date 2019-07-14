d55ebe50742e5754c672ca3b63e86fad50501cfc
Khalid Khatib <khalid@logitudeworld.com>
2019-07-14 23:30:21 +0300

    Price check: calculating Surcharges Price Check: shipment measurements
    Related Work Items: #54449, #54959

--- Logitude/Logitude.TariffModule.BL/DataContracts/TariffSearchSummary.cs
+++ Logitude/Logitude.TariffModule.BL/DataContracts/TariffSearchSummary.cs
@@ -19,6 +19,8 @@ namespace Logitude.TariffModule.BL.DataContracts
         public decimal? decimalprice { get; set; }
         public string Currency { get; set; }
         public string VersionId { get; set; }
+        public string TotalSurcharge { get; set; }
+        public string WholePrice { get; set; }
 
 
     }
