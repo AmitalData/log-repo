8d401cb55776d935416841774bfd95e4edcc2727
Abdullah Dawoud <abdullah@logitudeworld.com>
2019-07-17 18:08:37 +0300

    change invoice amount calc
    Related Work Items: #54239

--- Logitude/Logitude.Accounting.BL/CoreBL/TaxReportService.cs
+++ Logitude/Logitude.Accounting.BL/CoreBL/TaxReportService.cs
@@ -199,45 +199,32 @@ namespace Logitude.Accounting.BL.CoreBL
                     Simplog.Data.CommonDataModel.EntityPOCOs.Card card = cards.Where(d => d.GLAccountId == a.OppositGLAccount).FirstOrDefault();
 
                    
-                    if (a.AccountingEntity == "4")
+                    if (a.AccountingEntity == AccountingEntityValues.APInvoice)
                     {
-
                         aPInvoice = aPInvoices.Where(d => d.Id == a.AccountingEntityId).FirstOrDefault();
-
-                   
-
                         if (aPInvoice != null)
                         {
                             aPInvoice.TotalVATs = totalvats.Where(d => d.APInvoiceId == aPInvoice.Id).ToList();
                             VatNumber = aPInvoice.VATNumber;
                             InputVatAmount = (decimal?)aPInvoice.TotalVATs.Sum(d => d.LocalVATAmount);
-                            InputInvoiceAmount = aPInvoice.AmountInLocalCurrency != null ? (decimal?)aPInvoice.AmountInLocalCurrency : 0;
+                            InputInvoiceAmount = (decimal?)aPInvoice.SubTotalInLocalCurrency ?? 0;
                         }
-
-
-
-
                     }
                     else
                     {
                         InputVatAmount = a.LocalAmountDebit;
 
                         if (card != null && card.PartnerTypeId == PartnerTypeValues.Vendor)
-                        {
                             VatNumber = card.VatNumber;
-                        }
                         else
-                        {
                             VatNumber = "000000000";
-                        }
 
-                        InputInvoiceAmount = ledgerTransactons.Where(d => d.JournalId == a.JournalId && d.Reference == a.Reference).Sum(d => d.LocalAmountCredit);
+                        var transactionSum = ledgerTransactons.Where(d => d.JournalId == a.JournalId && d.Reference == a.Reference).Sum(d => d.LocalAmountCredit);
+                        InputInvoiceAmount = transactionSum - InputVatAmount;
                     }
 
                     if (VatNumber == null)
-                    {
                         VatNumber = "000000000";
-                    }
 
                     GLAccountPM gLAccountPM = glAccounts.Where(d => d.Id == a.OppositGLAccount).FirstOrDefault();
                     if (gLAccountPM != null)
