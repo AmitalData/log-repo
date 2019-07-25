e98ef9449799be5655d8cc65abdf050cea623f4d
Raghad Sous <raghad@logitudeworld.com>
2019-07-25 10:18:09 +0300

    e2e

--- Logitude/AngularModules/AngularModules/e2e/Operations/ShipmentView/ShipmentView-spec.ts
+++ Logitude/AngularModules/AngularModules/e2e/Operations/ShipmentView/ShipmentView-spec.ts
@@ -1,6 +1,6 @@
 import { browser, by, element } from 'protractor';
-import {ShipmentViewScenario} from './ShipmentViewScenario';
-import{ShipmentView}from'./ShipmentView';
+import { ShipmentViewScenario } from './ShipmentViewScenario';
+import { ShipmentView } from './ShipmentView';
 import { LoginComp } from '../../Login/Login.po';
 import { FieldsHelper } from '../../Helpers/FieldsHelper';
 
@@ -8,12 +8,12 @@ import { FieldsHelper } from '../../Helpers/FieldsHelper';
 
 describe('ShipmentView', () => {
 
-    let ShipmentScenario: ShipmentViewScenario= new ShipmentViewScenario();
-    let CreatView:ShipmentView = new ShipmentView();
-  
-  
+    let ShipmentScenario: ShipmentViewScenario = new ShipmentViewScenario();
+    let CreatView: ShipmentView = new ShipmentView();
+
+
     beforeEach(() => {
-  
+
     });
     browser.ignoreSynchronization = true;
 
@@ -21,16 +21,38 @@ describe('ShipmentView', () => {
     it('OpenShipmentView', function () {
 
         CreatView.OpenShipmentView();
-     
-      });
-    
+
+   });
 
 
-      it('CreatShipmentView', function () {
 
-       CreatView.CreatNewView();
-     
-    });
+   it('CreatShipmentView', function () {
+
+      CreatView.CreatNewView();
+
+   });
+
+ /*  it('DeleteNewView', function () {
+
+    CreatView.DeleteNewView();
+});
+
+
+ it('EditNewView', function () {
+
+       CreatView.EditNewView();
+   });
+
+
+   it('DeleteNewView', function () {
+
+    CreatView.DeleteNewView();
+});
+
+  it('DisplayView', function () {
+
+    CreatView.DisplayView();
+});*/
 
 
 });
