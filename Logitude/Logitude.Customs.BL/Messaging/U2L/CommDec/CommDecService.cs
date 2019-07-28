7eb9bf48c98570c38381aac49c8b52d1f10a8fa8
itzik M <itzik@amital.co.il>
2019-07-28 16:52:39 +0300

    55217 בעיות בעדכון הצהרות בקבלת מסר עדכון מניפסט - כל התקשורות נכשלות מסוג UWL
    Related Work Items: #55217

--- Logitude/Logitude.Customs.BL/Messaging/U2L/CommDec/CommDecService.cs
+++ Logitude/Logitude.Customs.BL/Messaging/U2L/CommDec/CommDecService.cs
@@ -132,7 +132,10 @@ namespace Logitude.Customs.BL.Messaging.U2L.CommDec
                 _LogitudeCommDecFile.Id = existId;
             }
 
+            /////////////////////////////////////////////////////////////
+            _context = CustomContext.GetContext(ResolvedTenant());
             MyGenericResponseObj.Stage = "GetSingle";
+            myQueryService = new DeclarationQueryService(_context);
             this._MyDeclarationPM = myQueryService.GetSingle(this._LogitudeCommDecFile.Id, true, false);
             if (this._MyDeclarationPM == null)
             {
@@ -423,6 +426,11 @@ namespace Logitude.Customs.BL.Messaging.U2L.CommDec
             }
 
             _MyDeclarationPM.CurrentContextTag = UpsertActionConst; // moran 28.7.16 - Task 22249
+
+
+
+            declarationUpdateService = new DeclarationUpdateService(_context, new Dictionary<string, IContext>(), ResolvedTenant());
+
             declarationUpdateService.Update(this._MyDeclarationPM, true);
             AppendLogLine("declarationUpdat:Took:" + _Stopwatch.Elapsed.ToString()); _Stopwatch.Restart();
             string val = "";
