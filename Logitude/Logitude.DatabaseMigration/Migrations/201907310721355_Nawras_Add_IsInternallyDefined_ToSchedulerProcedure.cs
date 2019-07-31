976d031dfef6b9212d21d29048ba59c29c1c343c
Ahmad Baker <ahmadb@logitudeworld.com>
2019-07-31 10:25:49 +0300

    Nawras: Procedure Codes: modifications to the UI____Part1_Migration
    Related Work Items: #55608

---
+++ Logitude/Logitude.DatabaseMigration/Migrations/201907310721355_Nawras_Add_IsInternallyDefined_ToSchedulerProcedure.cs
@@ -0,0 +1,18 @@
+namespace Logitude.DatabaseMigration.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Nawras_Add_IsInternallyDefined_ToSchedulerProcedure : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.TariffVersions", "InitialEnddate", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.TariffVersions", "InitialEnddate");
+        }
+    }
+}
