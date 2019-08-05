e59ba08cbbad390371d998f625ac07a20f12ca83
Abed <abed@logitudeworld.com>
2019-08-05 15:25:28 +0300

    55177
    Related Work Items: #55177

--- Logitude/Logitude.WarehouseLib.Data/EntityLists/Generated/WarehouseEntryList.Generated.cs
+++ Logitude/Logitude.WarehouseLib.Data/EntityLists/Generated/WarehouseEntryList.Generated.cs
@@ -146,6 +146,8 @@ namespace Logitude.WarehouseLib.Data.EntityLists
        public string MasterHouse  { get; set; }
        [DataMember]
        public string EntryReferencesAndDate  { get; set; }
+       [DataMember]
+       public double? Ratio  { get; set; }
    }
 
 }
