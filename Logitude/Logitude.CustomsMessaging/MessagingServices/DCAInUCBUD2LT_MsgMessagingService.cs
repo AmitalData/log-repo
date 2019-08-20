b4c2759db0190d3d11c69d432c763a76f0d55fd4
itzik M <itzik@amital.co.il>
2019-08-20 15:12:41 +0300

    56095 יצירת טיקט לבלדרות כחלק מהתיוק ולא כ WS נפרד
    Related Work Items: #56095

--- Logitude/Logitude.CustomsMessaging/MessagingServices/DCAInUCBUD2LT_MsgMessagingService.cs
+++ Logitude/Logitude.CustomsMessaging/MessagingServices/DCAInUCBUD2LT_MsgMessagingService.cs
@@ -286,6 +286,12 @@ namespace Logitude.CustomsMessaging.MessagingServices
                     return;
                 }
 
+                if (TicketalreadyExistforthisDocument())
+                {
+                    Debug.WriteLine("TicketalreadyExistforthisDocument");
+                    return;
+
+                }
                 Debug.WriteLine("CreateCRS");
 
                 string loggingUserId = AuthenticationUtil.ResolveUserId(tenant);
@@ -305,7 +311,29 @@ namespace Logitude.CustomsMessaging.MessagingServices
 
         }
 
-
+        private bool TicketalreadyExistforthisDocument()
+        {
+            CustomsDocumentsTicketQueryService myCustomsDocumentsTicketQueryService = new CustomsDocumentsTicketQueryService(_DocumentsFilingPM.Tenant);
+            List<CustomsDocumentsTicketPM> myCustomsDocumentsTicketPMList = myCustomsDocumentsTicketQueryService.GetCustomsDocumentsTicketsByDocumentsFilingId(_DocumentsFilingPM.Id, _DocumentsFilingPM.Tenant);
+            if (myCustomsDocumentsTicketPMList != null && myCustomsDocumentsTicketPMList.Count() > 0)
+            {
+                List<string> ticketdIds = myCustomsDocumentsTicketPMList.Select(r => r.Id).ToList();
+                if (ticketdIds != null && ticketdIds.Count() > 0)
+                {
+                    CustomsDocumentPointerQueryService myCustomsDocumentPointerQueryService = new CustomsDocumentPointerQueryService(_DocumentsFilingPM.Tenant);
+                    List<CustomsDocumentPointerPM> myCustomsDocumentPointerPMList = myCustomsDocumentPointerQueryService.GetPointersForMultipleTickets(ticketdIds, _DocumentsFilingPM.Tenant);
+                    if (myCustomsDocumentPointerPMList != null && myCustomsDocumentPointerPMList.Count() > 0)
+                    {
+                        var myCustomsDocumentPointerPMListforDec = myCustomsDocumentPointerPMList.Where(o => o.ParentEntityCode == "Declaration" && o.ParentEntityId == _DocumentsFilingPM.Id);
+                        if (myCustomsDocumentPointerPMListforDec != null && myCustomsDocumentPointerPMListforDec.Count() > 0)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
 
         private bool CourierENV()
         {
