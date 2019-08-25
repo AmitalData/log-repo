1360af3d62a1eddbb262cbdefc00f3499ab1a530
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2019-08-25 14:14:47 +0300

    e2e

--- Logitude/AngularModules/AngularModules/protractor.conf.js
+++ Logitude/AngularModules/AngularModules/protractor.conf.js
@@ -134,36 +134,40 @@ exports.config = {
 
         // ********************* Login **********************************
         login: 'e2e/Login/**/Login.e2e-spec.ts',
-        NewQuote: 'e2e/CRM/Quotes/NewEntity/**/NewQuote-spec.ts',
+    //    NewQuote: 'e2e/CRM/Quotes/NewEntity/**/NewQuote-spec.ts',
 
-        FullAccProcess: 'e2e/FullAccounting/**/FullAccScenarios-spec.ts',
-        ARInvoice: 'e2e/FullAccounting/ARInvoice/**/ARInvoice-spec.ts',
-        CRM: 'e2e/CRM/**/CRMModule-spec.ts',
+     //   FullAccProcess: 'e2e/FullAccounting/**/FullAccScenarios-spec.ts',
+      //  ARInvoice: 'e2e/FullAccounting/ARInvoice/**/ARInvoice-spec.ts',
+      //  CRM: 'e2e/CRM/**/CRMModule-spec.ts',
        //  'e2e/LogBox/Login/**/Login.e2e-spec.ts',
 
-        NewShipment: 'e2e/Operations/Shipments/NewEntity/**/Operations.e2e-spec.ts',
-        NewShipmentlogbox: 'e2e/LogBox/Shipments/**/ShipmentSearch.e2e-spec.ts',
-        Contact: 'e2e/Contacts/**/Contacts-spec.ts',
-        EditTabs: 'e2e/Operations/Shipments/EditEntity/**/EditShipmentTabs.e2e-spec.ts',
-        ShipmentSearch: 'e2e/Operations/**/ShipmentSearch.e2e-spec.ts',
+        //NewShipment: 'e2e/Operations/Shipments/NewEntity/**/Operations.e2e-spec.ts',
+       // NewShipmentlogbox: 'e2e/LogBox/Shipments/**/ShipmentSearch.e2e-spec.ts',
+      //  Contact: 'e2e/Contacts/**/Contacts-spec.ts',
+      //  EditTabs: 'e2e/Operations/Shipments/EditEntity/**/EditShipmentTabs.e2e-spec.ts',
+      //  ShipmentSearch: 'e2e/Operations/**/ShipmentSearch.e2e-spec.ts',
 
         // ********************* FullAccounting **********************************
-        NewChartOfAccount: 'e2e/FullAccounting/ChartOfAccount/**/ChartOfAccount-spec.ts',
-        vendorgla: 'FullAccounting/GlAccounts/**/VendorGLAccount-spec.ts',
-        APInvoice: 'e2e/FullAccounting/APInvoice/**/APInvoice-spec.ts',
-        GLAccount: 'FullAccounting/**/GLAccounts/GlAccount-spec.ts',
+     //   NewChartOfAccount: 'e2e/FullAccounting/ChartOfAccount/**/ChartOfAccount-spec.ts',
+        //vendorgla: 'FullAccounting/GlAccounts/**/VendorGLAccount-spec.ts',
+       // APInvoice: 'e2e/FullAccounting/APInvoice/**/APInvoice-spec.ts',
+       // GLAccount: 'FullAccounting/**/GLAccounts/GlAccount-spec.ts',
 
         //*************Report********************
-        Reports: 'e2e/Report/**/Report-spec.ts',
+      //  Reports: 'e2e/Report/**/Report-spec.ts',
 
         //*************ShipmentView********************
-        ShipmentView: 'e2e/**/ShipmentView-spec.ts',
+      //  ShipmentView: 'e2e/**/ShipmentView-spec.ts',
 
      //*************CompanyAddressSetting********************
-     CompanyAddressSetting:'e2e/Maintenance/**/CompanyAddressSetting-spec.ts',
+    // CompanyAddressSetting:'e2e/Maintenance/**/CompanyAddressSetting-spec.ts',
+
+          //*************NewAgent***************
+   NewAgent: 'e2e/Maintenance/**/Agent-spec.ts',
+
 
         //*************DocOutTab***************
-        DocOut: 'e2e/**/DocsOut.e2e-spec.ts'
+    //    DocOut: 'e2e/**/DocsOut.e2e-spec.ts'
 
 
     },
