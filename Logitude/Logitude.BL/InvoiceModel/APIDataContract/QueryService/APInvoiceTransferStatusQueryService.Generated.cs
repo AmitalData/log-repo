f49c2197d56b51858d62a055161887f64e870a1f
Abed <abed@logitudeworld.com>
2019-08-25 09:58:11 +0300

    55980

--- Logitude/Logitude.BL/InvoiceModel/APIDataContract/QueryService/APInvoiceTransferStatusQueryService.Generated.cs
+++ Logitude/Logitude.BL/InvoiceModel/APIDataContract/QueryService/APInvoiceTransferStatusQueryService.Generated.cs
@@ -15,6 +15,7 @@ using Logitude.BL.ShipmentsModel.EntityPMs;
 using Logitude.BL.InfrastructureModel.EntityQueries;
 using Logitude.BL.InfrastructureModel.APIDataContract.ApiV1;
 using Logitude.BL.ShipmentsModel.APIDataContract.ApiV1;
+
 using Logitude.BL.Helpers;
 using Logitude.BL.InvoiceModel.EntityPMs;
 using Logitude.BL.InvoiceModel.Tools.EntityService;
