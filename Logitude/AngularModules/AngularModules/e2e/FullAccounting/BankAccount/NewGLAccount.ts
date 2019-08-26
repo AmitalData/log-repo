a901e756efb91afa91ce3588fa977d065a243ecf
Khawla Ata <khawla@logitudeworld.com>
2019-08-26 14:32:33 +0300

    e2e fixes

---
+++ Logitude/AngularModules/AngularModules/e2e/FullAccounting/BankAccount/NewGLAccount.ts
@@ -0,0 +1,71 @@
+import { FieldsHelper } from '../../Helpers/FieldsHelper';
+import { GeneralFunctions } from '../../Helpers/GeneralFunctions';
+import { browser, by, element } from 'protractor';
+
+
+
+export class NewGLAccount {
+  private Helper = new FieldsHelper();
+  private gn = new GeneralFunctions();
+  constructor() {
+
+
+  }
+
+  CreateNewGLAccount(LocalName: string) {
+    browser.ignoreSynchronization = true;
+
+    
+    this.Helper.WaitByIdAndClick('NewGLAccount');
+   
+
+    this.Helper.WaitByIdAndFill('GLAccount_ChartOfAccountsTypeCode', 'Banks');
+    this.Helper.WaitByCssAndClick_SelectItemFromList('.DropDownList', 0);
+    this.Helper.WaitBusyIndicator();
+    this.Helper.WaitByIdAndFill('GLAccount_ChartOfAccountsId', 'בנקים');
+      this.Helper.WaitByCssAndClick_FromTagInsideList('.DropDownListItem', 0);
+      this.Helper.WaitBusyIndicator();
+      this.Helper.WaitByIdAndFill('GLAccount_LocalName', LocalName);
+      this.Helper.WaitByIdAndFill('GLAccount_CurrencyId', 'NIS');
+      this.Helper.WaitByCssAndClick_FromTagInsideList('.DropDownListItem', 0); 
+    //this.Helper.WaitByIdAndFill('GLAccount_DisplayNumber', DisplayNumber);
+    this.Helper.WaitBusyIndicator();
+    this.Helper.WaitByIdAndFill('GLAccount_RevenueExpenseType', 'other');
+    this.Helper.WaitByCssAndClick_FromTagInsideList('.DropDownListItem', 0); 
+    this.Helper.WaitByIdAndClick('Ok-AddGLAccount');
+    this.Helper.WaitBusyIndicator();
+    this.Helper.WaitWindowClosed();
+
+  }
+
+  CreateNewGLAccountandmove(LocalName: string) {
+
+    browser.ignoreSynchronization = true;
+    this.Helper.WaitByIdAndClick('NewGLAccount');
+    this.Helper.WaitByIdAndFill('GLAccount_ChartOfAccountsTypeCode', 'Banks');
+    this.Helper.WaitByCssAndClick_SelectItemFromList('.DropDownList', 0);
+    this.Helper.WaitByIdAndFill('GLAccount_ChartOfAccountsId', 'בנקים');
+      this.Helper.WaitByCssAndClick_FromTagInsideList('.DropDownListItem', 0);
+      this.Helper.WaitBusyIndicator();
+      this.Helper.WaitByIdAndFill('GLAccount_LocalName', LocalName);
+      this.Helper.WaitByIdAndFill('GLAccount_CurrencyId', 'NIS');
+      this.Helper.WaitByCssAndClick_FromTagInsideList('.DropDownListItem', 0); 
+    //this.Helper.WaitByIdAndFill('GLAccount_DisplayNumber', DisplayNumber);
+    this.Helper.WaitBusyIndicator();
+    this.Helper.WaitByIdAndFill('GLAccount_RevenueExpenseType', 'other');
+    this.Helper.WaitByCssAndClick_FromTagInsideList('.DropDownListItem', 0); 
+    this.Helper.WaitByIdAndClick('Ok-AddGLAccount');
+    this.Helper.WaitBusyIndicator();
+    this.Helper.WaitWindowClosed();
+    this.Helper.WaitByIdAndClick('FABNKS');
+    
+  }
+
+
+
+}
+
+
+
+
+
