94ef15aaefa94358d9f0d83c39530a5b46cd5059
mirit <Mirit@amital.co.il>
2019-08-26 13:37:21 +0300

    55884
    Related Work Items: #55884

--- Logitude/Logitude.CustomsMessaging/ResponseServices/DE_NG_5108_DecisionMessageResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/DE_NG_5108_DecisionMessageResponseService.cs
@@ -45,6 +45,11 @@ namespace Logitude.CustomsMessaging.ResponseServices
             foreach (var deficitFileItem in customResponse.DeficitFile)
             {
                 TapagConnectionTablePM tapagConnectionTablePM = tapagConnectionTableQueryService.GetTapagConnectionByFileAndNumeral(deficitFileItem.TapagIdentifier.fileNumber, deficitFileItem.TapagIdentifier.numeral, requestParams.Tenant);
+                if (tapagConnectionTablePM == null)
+                {
+                    this.MyResponseData.HasException = true;
+                    this.MyResponseData.UserMessage = "לא נמצא תיק תפג " + deficitFileItem.TapagIdentifier.fileNumber;
+                }
                 DeficitPM deficitPM = deficitQueryService.GetDeficitByPaymentOrderNumberOrTapagId(null, tapagConnectionTablePM.TapagId, requestParams.Tenant);
                 if (deficitPM != null)
                 {
