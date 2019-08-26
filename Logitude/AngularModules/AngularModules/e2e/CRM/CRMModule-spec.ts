40f687fe19b41a903b1e087a63bd685a2348e718
Razan Jararah <razan@logitudeworld.com>
2019-08-26 08:13:17 +0300

    Add/Edit phone call and tasks, e2e & integration #56032

--- Logitude/AngularModules/AngularModules/e2e/CRM/CRMModule-spec.ts
+++ Logitude/AngularModules/AngularModules/e2e/CRM/CRMModule-spec.ts
@@ -1,31 +1,31 @@
 
 import { browser, by, element } from 'protractor';
-import {CRMComp} from './CRMModule'
+import { CRMComp } from './CRMModule'
 
 
 describe('CRM Module', () => {
-    let CRMPage : CRMComp;
+  let CRMPage: CRMComp;
   beforeEach(() => {
-      CRMPage=new CRMComp();
+    CRMPage = new CRMComp();
   });
   afterEach(() => {
   })
 
   it('Operations Success', function () {
     browser.ignoreSynchronization = true;
-    if(browser.params.CRM.CRMType=='overview'){
+    if (browser.params.CRM.CRMType == 'overview') {
       CRMPage.DoCRM('Overview');
     }
-    else if(browser.params.CRM.CRMType=='customer'){
+    else if (browser.params.CRM.CRMType == 'customer') {
       CRMPage.DoCRM('Customers');
     }
-    else if(browser.params.CRM.CRMType=='quote'){
+    else if (browser.params.CRM.CRMType == 'quote') {
       CRMPage.DoCRM('Quotes');
     }
-    else if(browser.params.CRM.CRMType=='activity'){
+    else if (browser.params.CRM.CRMType == 'activity') {
       CRMPage.DoCRM('Activities');
     }
-    else if(browser.params.CRM.CRMType=='opportunity'){
+    else if (browser.params.CRM.CRMType == 'opportunity') {
       CRMPage.DoCRM('Opportunities');
     }
   });
