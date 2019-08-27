09e2fda0091035920c11a516dcddad481cf1b5a0
moran <moran@amital.co.il>
2019-08-27 13:30:51 +0300

    55488 ms
    Related Work Items: #55488

--- Logitude/Logitude.Customs.BL/Messaging/U2L/Entry/EntryService.cs
+++ Logitude/Logitude.Customs.BL/Messaging/U2L/Entry/EntryService.cs
@@ -312,18 +312,18 @@ namespace Logitude.Customs.BL.Messaging.U2L.Entry
                 return ("");
             }
 
-            if (_GTRTRANQueryService == null)
+            using (_AmitalContext = AmitalContext.GetContext(_MyDeclarationPM.Tenant))
             {
                 _GTRTRANQueryService = new GTRTRANQueryService(_AmitalContext);
+                var myGTRTRANPM = _GTRTRANQueryService.GetSingle(partnerID, tableID, partnerCode, null, true);
+
+                if (myGTRTRANPM == null)
+                {
+                    return ("");
+                }
+                return (myGTRTRANPM.LOCALCODE);
             }
 
-            var myGTRTRANPM = _GTRTRANQueryService.GetSingle(partnerID, tableID, partnerCode, null, true);
-
-            if (myGTRTRANPM == null)
-            {
-                return ("");
-            }
-            return (myGTRTRANPM.LOCALCODE);
         }
 
         private void DeserilazeObject(string xmlLOGIENTRY)
