e2d023ee162ceb9284aafaf899ed2426f71623ed
Razan Jararah <razan@logitudeworld.com>
2019-09-10 21:37:28 +0300

    Master shipment - protractor

--- Logitude/AngularModules/AngularModules/e2e/Operations/Shipments/EditEntity/ShipmentsTab.ts
+++ Logitude/AngularModules/AngularModules/e2e/Operations/Shipments/EditEntity/ShipmentsTab.ts
@@ -15,11 +15,12 @@ export class ShipmentsTabComponent {
     public ShipmentsTab() {
         this.Helper.WaitByIdAndClick('Shipment.TH.Consolidation');
         this.Helper.WaitBusyIndicator();
+        this.Helper.WaitEditComponentBusyIndicator();
 
         this.Helper.WaitByIdAndClick('NewHouseBtn')
         this.Helper.WaitBusyIndicator();
 
-      //  this.CreateHouseShipment.FillHouseShipmentFields('CreatedFromMaster','');
+        this.CreateHouseShipment.FillHouseShipmentFields('CreatedFromMaster','','');
         this.Helper.WaitByIdAndClick('ShipmentCreatebtn');
         this.Helper.WaitWindowClosed();
         this.Helper.WaitBusyIndicator();
