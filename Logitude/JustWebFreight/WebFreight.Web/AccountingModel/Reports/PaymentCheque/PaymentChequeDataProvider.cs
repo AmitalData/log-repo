89a6b27e1bbf086c69136f04e730709a5d6fe97b
alaa <alaa.jamal.h@live.com>
2019-09-12 09:45:36 +0300

    56895
    Related Work Items: #56895

--- Logitude/JustWebFreight/WebFreight.Web/AccountingModel/Reports/PaymentCheque/PaymentChequeDataProvider.cs
+++ Logitude/JustWebFreight/WebFreight.Web/AccountingModel/Reports/PaymentCheque/PaymentChequeDataProvider.cs
@@ -36,7 +36,7 @@ namespace WebFreight.Web.AccountingModel.Reports.PaymentCheque
         public decimal? TaxDeductionLocalAmount { get; set; }
         public int? TaxDeductionPercentage { get; set; }
         public double? AmountInLocalCurrency { get; set; }
-        public string InternalNotes { get; set; }
+        public string PrintNotes { get; set; }
     }
 
     
