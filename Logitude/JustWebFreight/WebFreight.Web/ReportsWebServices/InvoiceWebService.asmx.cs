dcd66b494017729efa9e0e3d9bd712b859ab727b
alaa <alaa.jamal.h@live.com>
2019-09-15 16:17:04 +0300

    56568
    Related Work Items: #56568

--- Logitude/JustWebFreight/WebFreight.Web/ReportsWebServices/InvoiceWebService.asmx.cs
+++ Logitude/JustWebFreight/WebFreight.Web/ReportsWebServices/InvoiceWebService.asmx.cs
@@ -299,8 +299,14 @@ namespace WebFreight.Web.ReportsWebServices
                 {
                     if (invoicetype.Code == "CD")
                     {
-                        
-                        invoicedataprovider.InvoiceType_labelHebrew = "הודעת זיכוי";
+                        if (tenantSettings.AccountingActivated)
+                        {
+                            invoicedataprovider.InvoiceType_labelHebrew = "חשבונית זיכוי";
+                        }
+                       
+                        else{
+                            invoicedataprovider.InvoiceType_labelHebrew = "הודעת זיכוי";
+                        }
                         invoicedataprovider.InvoiceType_label_Spanish = "Nota de Credito";
                     }
                     else if (invoicetype.Code == "IN")
