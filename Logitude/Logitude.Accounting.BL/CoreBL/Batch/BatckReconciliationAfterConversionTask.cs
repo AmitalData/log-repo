aa622f531cffa243d0a2e7849720b6a3417e1a45
itzik M <itzik@amital.co.il>
2019-09-16 16:51:41 +0300

    56736 Reconciliations after conversion --move the service to batch
    Related Work Items: #56736

---
+++ Logitude/Logitude.Accounting.BL/CoreBL/Batch/BatckReconciliationAfterConversionTask.cs
@@ -0,0 +1,57 @@
+﻿using Logitude.Accounting.BL.Utils;
+using Logitude.Infrastructure.BL.EntityPMs;
+using Logitude.Infrastructure.BL.ExtendedServices;
+using Simplog.Server.Infrastructure;
+using Simplog.Server.Infrastructure.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Logitude.Accounting.BL.CoreBL.Batch
+{
+    public class BatckReconciliationAfterConversionTask : BatchTaskExecutionsService
+    {
+        public BatckReconciliationAfterConversionTask(BatchTaskExecutionPM batchTaskExecution) : base(batchTaskExecution)
+        {
+        }
+        public override void RunCode()
+        {
+            string xmlParameters = BatchTaskExecution.PrametersXml;
+            System.IO.StringReader stringReader = new System.IO.StringReader(xmlParameters);
+            XmlSerializer serializer = new XmlSerializer(typeof(ReconciliationAfterConversionArg));
+            var parameterArgs = serializer.Deserialize(stringReader) as ReconciliationAfterConversionArg;
+            try
+            {
+
+                
+                using (var scope = TransactionFactory.GetTransaction(TimeSpan.FromMinutes(10)))
+                {
+
+
+
+                    //entityPM.ChangeSetOp = Simplog.Server.Infrastructure.ChangeSetOperation.Update;
+                    //service.Update(entityPM, true);
+
+                    ReconciliationAfterConversionBatch reconciliationAfterConversionBatch = new ReconciliationAfterConversionBatch();
+                    reconciliationAfterConversionBatch.RunReconciliationAfterConversion(parameterArgs.Tenant, parameterArgs.FromExtNum, parameterArgs.ToExtNum);
+                    string responseText = reconciliationAfterConversionBatch.ResponseText();
+                    ChangeStatus("D", null, responseText);
+
+                    scope.Complete();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                LogitudeSettings.HandleLogMe(ex.ToString(), true, "BatckReconciliationAfterConversionTask", new DateTime(2019, 10, 1));
+                throw;
+
+            }
+        }
+    }
+}
