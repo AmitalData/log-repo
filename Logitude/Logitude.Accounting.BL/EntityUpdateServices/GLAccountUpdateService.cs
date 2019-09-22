e6622534051dd359d7e61da7464b67cc42de2ec4
alaa <alaa.jamal.h@live.com>
2019-09-22 16:02:11 +0300

    57078
    Related Work Items: #57078

--- Logitude/Logitude.Accounting.BL/EntityUpdateServices/GLAccountUpdateService.cs
+++ Logitude/Logitude.Accounting.BL/EntityUpdateServices/GLAccountUpdateService.cs
@@ -528,7 +528,7 @@ namespace Logitude.Accounting.BL.EntityUpdateServices
 
         private void SendHybridTask(GLAccountPM glaccounPM)
         {
-            if (glaccounPM.AccountTypeCode != "4" && glaccounPM.AccountTypeCode != "5")
+            if (glaccounPM.AccountTypeCode != "4" && glaccounPM.AccountTypeCode != "5" && glaccounPM.IsControlAccount==false)
             {
                 FillGLAccountCurrencyCode(glaccounPM);
 
