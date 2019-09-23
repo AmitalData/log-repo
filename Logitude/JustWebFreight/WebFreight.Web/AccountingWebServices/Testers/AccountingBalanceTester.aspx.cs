193baa51b6eb4081156c095eb1305956635feb4f
itzik M <itzik@amital.co.il>
2019-09-23 13:50:54 +0300

    56937 Integrity Check - בדיקת תקינות סכום פתוח בתנועות
    Related Work Items: #56937

--- Logitude/JustWebFreight/WebFreight.Web/AccountingWebServices/Testers/AccountingBalanceTester.aspx.cs
+++ Logitude/JustWebFreight/WebFreight.Web/AccountingWebServices/Testers/AccountingBalanceTester.aspx.cs
@@ -96,8 +96,8 @@ namespace WebFreight.Web.AccountingWebServices.Testers
             //testIt.GetDataTableFromWorkSheet(null,"");
             //var s = new ClearAccountingDB();
             //s.ClearDB(1148);
-            var a = new ReconcileOpenAmountService();
-            var l = a.CheckReconciliationOpenAmount(1106, 2019);
+            //var a = new ReconcileOpenAmountService();
+            //var l = a.GetLedgerOpenAmountDiff(1106, 2019);
 
 
             try
