657bb04fa0a810f3f26e66bda585ee0a19f97245
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2019-09-29 12:07:48 +0300

    e2e/rs

--- Logitude/AngularModules/AngularModules/e2e/Report/ReportSearch.ts
+++ Logitude/AngularModules/AngularModules/e2e/Report/ReportSearch.ts
@@ -13,16 +13,15 @@ export class ReportSearch {
 
   QuickSearch(reportName) {
     this.logitudeTab.GoToMainMenu('General.MH.Reports');
-   // this.UseSearchBox('null_Search', 'Unpaid Invoices');
-    this.UseSearchBox('null_Search',reportName);
+    this.UseSearchBox('null_Search', reportName);
 
   }
 
   UseSearchBox(searchFeildId: string, searchByRef: string) {
     this.Helper.WaitByIdAndFill(searchFeildId, searchByRef);
-   // this.Helper.WaitBusyIndicator();
+    this.Helper.waitByCss('.HyperlinkQueryButtonControl')
     this.Helper.WaitByCssAndClick_FromTagInsideList('.HyperlinkQueryButtonControl', 0);
-    
+
   }
 }
 
