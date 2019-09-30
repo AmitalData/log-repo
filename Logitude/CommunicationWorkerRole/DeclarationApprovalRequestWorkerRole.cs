3b80677624cd715b9ab69b03fb977535f89b5c3d
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2019-09-30 14:33:39 +0300

    DA Task

--- Logitude/CommunicationWorkerRole/DeclarationApprovalRequestWorkerRole.cs
+++ Logitude/CommunicationWorkerRole/DeclarationApprovalRequestWorkerRole.cs
@@ -189,73 +189,10 @@ namespace CommunicationWorkerRole
                                     var content = new StringContent(serializedObject, Encoding.UTF8, "application/json");
                                     var result = await client.PutAsync(URI + "DeclarationApprovalRequest", content);
                                     if (result.StatusCode == System.Net.HttpStatusCode.OK)
-                                    {
-                                        CustomerTenantAccessQuery customerTenantAccessQuery = new CustomerTenantAccessQuery(tenant);
-                                        HybridPartnerQuery hybridPartnerQuery = new HybridPartnerQuery(tenant);
-                                        CustomerTenantAccessPM temp = customerTenantAccessQuery.GetCustomerTenantAccessPMsByTenantCustomerTenant(tenant, CustomerTenant);
-                                        var hybridPartner = hybridPartnerQuery.GetSinglePMByPartnerTenant(tenant);
-                                        ContactRepository contactRepository = new ContactRepository(tenant);
-                                        Contact User = contactRepository.GetSingleContactByEmail("system@tenant" + tenant + ".com", tenant, true);
-                                        if (temp != null)
-                                        {
-                                            //if (IsPrivateLabelCustomer)
-                                            //{
-
-                                            //}
-                                            TenantRepository TenantRep = new TenantRepository(tenant);
-                                            LogBoxTenantSettingRepository LBTenantRep = new LogBoxTenantSettingRepository(tenant);
-                                            var MainTenant = TenantRep.GetSingleTenant(tenant);
-                                            var LBTenant = LBTenantRep.GetSingleLBTenant(tenant);
-                                            Contact LogBoxUser = contactRepository.GetSingleContact(LBTenant.LogBoxAdminUserId, tenant);
-                                            var domain = "@logbox.co.il";
-                                            if (!string.IsNullOrEmpty(LogBoxUser.Email))
-                                            {
-                                                var EmailParts = LogBoxUser.Email.Split('@');
-                                                if (EmailParts.Length == 2)
-                                                {
-                                                    domain = "@" + EmailParts[1];
-                                                }
-                                            }
-
-                                            var NoReplyemail = "no-reply" + domain;
-                                            var SalesEmail = "sales" + domain;
-                                            string emailMessage = "";
-                                            StringBuilder HtmlTemplate = new StringBuilder();
-                                            HtmlTemplate.Append("<p style='text-align:left'>");
-                                            HtmlTemplate.Append("<b>Hi " + temp.ContactName + "</b>");
-                                            HtmlTemplate.Append("<br />");
-                                            HtmlTemplate.Append("<b>" + (hybridPartner != null ? hybridPartner.Name : "") + " approved your request. You will start receiving your shipments and documents data into your account </b>");
-                                            HtmlTemplate.Append("<br />");
-                                            HtmlTemplate.Append("<b>Have a great day ! </b>");
-                                            HtmlTemplate.Append("<br />");
-                                            HtmlTemplate.Append("<b>" + MainTenant.Company + "</b>");
-                                            emailMessage = "The agent < " + (hybridPartner != null ? hybridPartner.Name : "") + " > approve the request for < " + temp.CompanyName + " > - < " + temp.CustomerTenant + " >";//HtmlTemplate.ToString();
-                                            var subject = "Approved request for < " + temp.CompanyName + " > - < " + temp.CustomerTenant + " >";//"Your request for data has been approved by " + (hybridPartner != null ? hybridPartner.Name : "");
-                                            EmailCommunicationParams emailParams = new EmailCommunicationParams()
-                                            {
-                                                From = NoReplyemail,
-                                                To = "sales@logbox.co.il",//temp.CompanyEmail,
-                                                //BCC = SalesEmail,
-                                                Subject = subject,
-                                                EmailBody = emailMessage,
-                                                LoggingUserId = User.Id,
-                                                Tenant = tenant,
-                                            };
-                                            if (hybridPartner.Name.ToLower() != "dsv")
-                                            {
-                                                Communications.AddEmailCommunicationLogQueue(emailParams, tenant);
-                                            }
-                                            
-                                        }
-                                        queue.Complete();
-                                        CustomerTenantAccessRepository repo = new CustomerTenantAccessRepository(tenant);
-                                        var CTA = repo.GetSingleCustomerTenantAccess(Id, tenant);
-                                        CTA.Status = "A";
-                                        repo.Update(CTA);
-                                        repo.SubmitChanges();
-                                        LogPM.Status = "D";
+                                    { 
+                                        // VDK Logic
                                         var ResponseData = result.Content.ReadAsStringAsync().Result;
-                                        var Donemsg = "Response Sent To Importer Successfully " + DateTime.Now;
+                                        var Donemsg = "Declaration Approval Request Sent To Importer Successfully " + DateTime.Now;
                                         APILogsUtility.UpdateAPILogStatus(LogPM.Id, tenant, "D", response.RetryNumber + 1, DateTime.Now, DateTime.UtcNow, Donemsg, null, ResponseData, null, "");
                                     }
                                     else //if (result.StatusCode == System.Net.HttpStatusCode.BadRequest)
@@ -263,7 +200,7 @@ namespace CommunicationWorkerRole
                                         APIException EXC = JsonConvert.DeserializeObject<APIException>(result.Content.ReadAsStringAsync().Result);
                                         if (EXC != null)
                                         {
-                                            var Failmsg = EXC.ErrorType + " Fail To Send Response To Importer Tenant " + DateTime.Now;
+                                            var Failmsg = EXC.ErrorType + " Fail To Send Declaration Approval Request To Importer Tenant " + DateTime.Now;
                                             APILogsUtility.UpdateAPILogStatus(LogPM.Id, tenant, "F", response.RetryNumber + 1, DateTime.Now, DateTime.UtcNow, Failmsg, null, LogitudeXmlSerializer.SerializeObjectToXmlString(EXC), null, "");
                                             throw new Exception(EXC.ErrorType, new Exception(EXC.ErrorMessage));
                                         }
@@ -336,7 +273,7 @@ namespace CommunicationWorkerRole
             catch (Exception ex)
             {
                 ConnectClient();
-                ExceptionHandler.HandleException(ex, DateTime.Now, 0, null, "importer shipments worker role start", null, null);
+                ExceptionHandler.HandleException(ex, DateTime.Now, 0, null, "Declaration Approval Request worker role start", null, null);
                 Thread.Sleep(10000);
             }
            
@@ -346,12 +283,12 @@ namespace CommunicationWorkerRole
             try
             {
                 queue = new DbQueueService();
-                queue.InitializeQueue("CustomerTenantAccessQueue", 0);
+                queue.InitializeQueue("DeclarationApprovalRequestWorkerRole", 0);
 
             }
             catch (Exception ex)
             {
-                ExceptionHandler.HandleException(ex, DateTime.Now, 0, null, "email worker role start", null, null);
+                ExceptionHandler.HandleException(ex, DateTime.Now, 0, null, "Declaration Approval Request worker role start", null, null);
             }
         }
 
---
+++ Logitude/JustWebFreight/WebFreight.Web/App_Code/DeclarationApprovalRequestController.cs
@@ -0,0 +1,140 @@
+﻿using Logitude.BL.CommonDataModel.EntityAMs;
+using Logitude.BL.CommonDataModel.EntityPMs;
+using Logitude.BL.CommonDataModel.EntityQueries;
+using Logitude.BL.CommonDataModel.Tools.EntityService;
+using Logitude.BL.Helpers;
+using Logitude.BL.InfrastructureModel.EntityPMs;
+using Logitude.BL.InfrastructureModel.Tools.EntityService;
+using Logitude.BL.ShipmentsModel.EntityPMs;
+using Logitude.Server.Tools;
+using Logitude.Server.Tools.Counters;
+using Newtonsoft.Json;
+using Simplog.Data.CommonDataModel;
+using Simplog.Data.InfrastructureModel;
+using Simplog.Data.InfrastructureModel.EntityPOCOs;
+using Simplog.Data.InfrastructureModel.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WebFreight.Web.Helpers;
+using WebFreight.Web.Security;
+
+namespace WebFreight.Web.App_Code
+{
+    public class DeclarationApprovalRequestController : ApiController
+    {
+        
+        public HttpResponseMessage Put(DeclarationApprovalRequestPM ApprovalRequest)
+        {
+            try
+            {
+                bool IsNewLog = false;
+                string CorrelationId = HttpContext.Current.Request.Headers["CorrelationId"];
+                IWebFreightContext webFreightContext = WebFreightContext.GetContext(ApprovalRequest.Tenant);
+                APILogsService apiLogsService = new APILogsService(webFreightContext, ApprovalRequest.Tenant);
+                ObjectTableRepository objectTabelRepository = new ObjectTableRepository(ApprovalRequest.Tenant);
+
+                var aPILogsRepository = new APILogsRepository(webFreightContext);
+                APILogs Log = aPILogsRepository.GetSingleAPILogsByCorrelationId(CorrelationId, ApprovalRequest.Tenant);
+                APILogsPM LogPM;
+                if (Log == null)
+                {
+                    IsNewLog = true;
+                    var Objecttable = objectTabelRepository.GetObjectTableByName("Shipment", ApprovalRequest.Tenant, true);
+                    LogPM = new APILogsPM()
+                    {
+                        Id = IdCounter.GetNumber("APILogs", ApprovalRequest.Tenant),
+                        CorrelationId = CorrelationId,
+                        CreateDate = DateTime.Now,
+                        CreateDateUTC = DateTime.UtcNow,
+                        Direction = "I",
+                        LastUpdateDate = DateTime.Now,
+                        LastUpdateDateUTC = DateTime.UtcNow,
+                        NumberOfRetries = 1,
+                        ObjectTableId = Objecttable.Id,
+                        ExpirationDate = DateTime.Now.AddDays(90),
+                        Status = "I",
+                        Tenant = ApprovalRequest.Tenant
+                    };
+                }
+                else
+                {
+                    IsNewLog = false;
+                    var Objecttable = objectTabelRepository.GetObjectTableByName("Shipment", ApprovalRequest.Tenant, true);
+                    LogPM = new APILogsPM()
+                    {
+                        Id = Log.Id,
+                        CorrelationId = Log.CorrelationId,
+                        CreateDate = Log.CreateDate,
+                        CreateDateUTC = Log.CreateDateUTC,
+                        Direction = Log.Direction,
+                        LastUpdateDate = Log.LastUpdateDate,
+                        LastUpdateDateUTC = Log.LastUpdateDateUTC,
+                        NumberOfRetries = Log.NumberOfRetries++,
+                        ObjectTableId = Log.ObjectTableId,
+                        ExpirationDate = Log.ExpirationDate,
+                        Status = "I",
+                        Tenant = Log.Tenant,
+
+                    };
+                }
+                LogPM.Subject = "Add Declaration Approval Request To Importer Tenant";
+                if (IsNewLog)
+                {
+                    apiLogsService.Create(LogPM);
+                }
+                try
+                {
+                    SecurityUtility.AuthenticationOnTenant(ApprovalRequest.Tenant);
+                    ////HybridPartnerQuery hybridPartnerQuery = new HybridPartnerQuery(ApprovalRequest.Tenant);
+                    ////var hybridPartner = hybridPartnerQuery.GetSinglePMByPartnerTenant(ApprovalRequest.Tenant);
+                    ////CustomerTenantAccessRequestQuery customerTenantAccessRequestQuery = new CustomerTenantAccessRequestQuery(entityAM.CustomerTenant);
+                    ////var customerTenantAccessRequest = customerTenantAccessRequestQuery.GetSinglePMByCustomerTenantAndPartnerTenant(hybridPartner.Id, entityAM.CustomerTenant);
+                    ////customerTenantAccessRequest.RequestStatus = "A";
+
+
+                    //var msg = "Start Sending Response To Importer Tenant " + DateTime.Now;
+                    //APILogsUtility.UpdateAPILogStatus(LogPM.Id, LogPM.Tenant, LogPM.Status, 1, DateTime.Now, DateTime.UtcNow, msg, LogitudeXmlSerializer.SerializeObjectToXmlString(customerTenantAccessRequest), null, null, "");
+                    //ICommonDataContext objectContext = CommonDataContext.GetContext(customerTenantAccessRequest.Tenant);
+
+                    //var SystemUser = "system@tenant" + customerTenantAccessRequest.Tenant + ".com";
+                    //CustomerTenantAccessRequestService service = new CustomerTenantAccessRequestService(objectContext, customerTenantAccessRequest.Tenant);
+                    //service.Update(customerTenantAccessRequest);
+                    //var ResponseData = JsonConvert.SerializeObject(customerTenantAccessRequest.Id);
+                    //var Donemsg = "Response Sent To Importer Tenant Successfully " + DateTime.Now;
+                    //APILogsUtility.UpdateAPILogStatus(LogPM.Id, LogPM.Tenant, "D", 1, DateTime.Now, DateTime.UtcNow, Donemsg, null, null, null, "");
+                    //IWebFreightContext webfreightcontext = WebFreightContext.GetContext(customerTenantAccessRequest.Tenant);
+                    //TableLastUpdateClass.UpdateTableHistory(customerTenantAccessRequest.Tenant, "CustomerTenantAccessRequest", webfreightcontext);
+                    return Request.CreateResponse(HttpStatusCode.OK, "OK");
+
+                }
+                catch (Exception ex)
+                {
+                    string errorMessage = ex.Message + Environment.NewLine;
+
+                    if (ex.InnerException != null)
+                    {
+
+                        errorMessage = errorMessage + " (" + (ex.InnerException.InnerException != null ? ex.InnerException.InnerException.Message : ex.InnerException.Message) + ")" + Environment.NewLine;
+
+                    }
+
+                    errorMessage = errorMessage + ex.StackTrace + Environment.NewLine;
+                    APILogsUtility.UpdateAPILogStatus(LogPM.Id, LogPM.Tenant, "F", 1, DateTime.Now, DateTime.UtcNow, ex.Message, null, null, errorMessage, (errorMessage.Length >= 250 ? errorMessage.Substring(0, 249) : errorMessage));
+                    APIException Responce = new APIException();
+                    Responce.ErrorType = ex.GetType().Name;
+                    Responce.ErrorMessage = errorMessage;
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, Responce);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ApiExceptionBuilder.BuildException(ex));
+            }  
+        } 
+    }
+}
