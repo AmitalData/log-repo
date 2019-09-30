75096f85b8141373c645de7ed01d92dfa1ec3d85
Abdullah Dawoud <abdullah@logitudeworld.com>
2019-09-30 14:17:33 +0300

    glaccount: more data
    Related Work Items: #57501

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/AccountingModel/GLAccountsController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/AccountingModel/GLAccountsController.cs
@@ -36,6 +36,7 @@ using Logitude.Accounting.Data.EntityListQueryServices;
 using Logitude.Accounting.BL.EntityQueryServices;
 using System.ComponentModel.DataAnnotations;
 using Logitude.Accounting.Def.EntityPMs;
+using Logitude.BL.CommonDataModel.EntityLists;
 
 namespace WebFreight.Web.App_Code.AngularJS_App_Code.Generated
 {
@@ -220,6 +221,27 @@ namespace WebFreight.Web.App_Code.AngularJS_App_Code.Generated
             catch (Exception ex)
             {
                  return Request.CreateResponse(HttpStatusCode.BadRequest, ApiExceptionBuilder.BuildException(ex));
+            }
+        }
+
+        public HttpResponseMessage GetConnectedCardsForGLAccount(string accountId)
+        {
+            try
+            {
+                string token = HttpContext.Current.Request.Headers["Token"];
+                AuthenticationToken authToken = AuthenticationTokenRepository.GetSingleTokenFromCache(token);
+                SecurityUtility.AuthenticationOnTenant(authToken.Tenant);
+                SecurityUtility.CheckContactFeature("GLAccount", "READ", authToken.Tenant);
+
+                IAccountingContext MyContext = AccountingContext.GetContext(authToken.Tenant);
+                GLAccountQueryService query = new GLAccountQueryService(MyContext);
+                List<CardList> connectedCards = query.GetConnectedCards(accountId, authToken.Tenant);
+
+                return Request.CreateResponse(HttpStatusCode.OK, connectedCards);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ApiExceptionBuilder.BuildException(ex));
             }
         }
 
