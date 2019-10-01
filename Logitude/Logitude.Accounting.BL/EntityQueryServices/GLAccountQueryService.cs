1d0483c5485eed101624c418357bfd6d131c89b4
Abdullah Dawoud <abdullah@logitudeworld.com>
2019-10-01 10:59:42 +0300

    clean code

--- Logitude/Logitude.Accounting.BL/EntityQueryServices/GLAccountQueryService.cs
+++ Logitude/Logitude.Accounting.BL/EntityQueryServices/GLAccountQueryService.cs
@@ -1052,14 +1052,14 @@ namespace Logitude.Accounting.BL.EntityQueryServices
                                    }).FirstOrDefault();
         }
 
-        public void ConnectCardToGLAccount(string accountId, string cardId,bool skipConnectedCardsValidation, int tenant)
+        public void ConnectCardToGLAccount(CardGLAccountConnectionArgs args)
         {
-            CardPM cardPM = GetCardById(cardId, tenant);
+            CardPM cardPM = GetCardById(args.CardId, args.Tenant);
 
-            if (!skipConnectedCardsValidation)
-                CheckConnectCards(accountId, cardId, tenant);
+            if (!args.SkipConnectedCardsValidation)
+                CheckConnectCards(args.AccountId, args.CardId, args.Tenant);
 
-            cardPM.GLAccountId = accountId;
+            cardPM.GLAccountId = args.AccountId;
             SubmitCard(cardPM);
 
 
@@ -1150,6 +1150,14 @@ namespace Logitude.Accounting.BL.EntityQueryServices
         public string CurrencyId { get; set; }
         public string AccountId { get; set; }
         //public string LocalName { get; set; }
+    }
+
+    public class CardGLAccountConnectionArgs
+    {
+        public string AccountId { get; set; }
+        public string CardId { get; set; }
+        public int Tenant { get; set; }
+        public bool SkipConnectedCardsValidation { get; set; }
     }
 
     public interface IGLAccountQueryService
