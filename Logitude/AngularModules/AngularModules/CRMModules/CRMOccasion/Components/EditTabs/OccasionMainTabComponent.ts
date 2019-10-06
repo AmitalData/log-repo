8ba4530b2cb7eeb336aaac8860cf3c8d64515be2
Maheera Jazi <Maheera@logitudeworld.com>
2019-10-06 13:32:01 +0300

    56955
    Related Work Items: #56955

--- Logitude/AngularModules/AngularModules/CRMModules/CRMOccasion/Components/EditTabs/OccasionMainTabComponent.ts
+++ Logitude/AngularModules/AngularModules/CRMModules/CRMOccasion/Components/EditTabs/OccasionMainTabComponent.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 import { BaseComponent } from '../../../../Infrastructure/Components/LogitudeComponents/BaseComponent';
 import { EntityArgs } from '../../../../Infrastructure/DataContracts/EntityArgs';
 import { OccasionPM } from '../../../../CRM/EntityPMs/OccasionPM';
-
+import { ApiQueryFilters } from '../../../../Infrastructure/DataContracts/ApiQueryFilters';
+import { ListComponentArgs } from '../../../../Infrastructure/Args';
+import { EntityResourceService } from '../../../../Infrastructure/Services/EntityResourceService';
+import { SessionLocator } from '../../../../Infrastructure/Utilities/SessionLocator';
 
 @Component({
     selector: 'OccasionMainTabComponent',
@@ -16,6 +19,10 @@ export class OccasionMainTabComponent extends BaseComponent {
     public EntityId: string;
     public IsCheckedAllContacts: false;
     public DataContext: OccasionMainTabComponent = this;
+    public ObjectTableName = "Occasion";
+    public _entityResourceService: EntityResourceService = new EntityResourceService();
+    private CurrentSession = SessionLocator.SelectedSession;
+    public OccasionLinesList = [];
 
     constructor(public entityArgs: EntityArgs) {
         super();
@@ -23,13 +30,71 @@ export class OccasionMainTabComponent extends BaseComponent {
         if (this.EntityPM) {
             this.EntityId = this.EntityPM.Id;
         }
-        
+
     }
 
+    LoadLinesData() {
+
+
+    }
 
     AddContacts() {
 
 
+    }
+
+    // Search
+    private SearchText: string = "";
+    SearchTextKeyUp(args: any) {
+        this.SearchText = args;
+        this.LoadLinesData();
+    }
+
+
+    // Statistics
+    public NumberAllContacts: number = 0;
+    public NumberInvitedContacts: number = 0;
+    public NumberOfParticipatedContacts: number = 0;
+    public NumberAllCustomers: number = 0;
+    public NumberInvitedCustomers: number = 0;
+    public NumberOfParticipatedCustomers: number = 0;
+
+    ViewAllData(arg) {
+        var objectTableName = "";
+        var queryCode = "";
+        var filterAgrs = new ApiQueryFilters();
+        var listArgs = new ListComponentArgs();
+
+        switch (arg) {
+            case "AllContacts":
+            case "InvitedContacts":
+            case "ParticipatedContacts":
+                {
+                    objectTableName = "Contact";
+                    queryCode = "Contacts";
+                    break;
+                }
+            case "AllContacts":
+            case "InvitedContacts":
+            case "ParticipatedContacts":
+                {
+                    objectTableName = "Customer";
+                    queryCode = "Customers";
+                    break;
+                }
+        }
+
+        listArgs.Filters = filterAgrs;
+        listArgs.QueryCode = queryCode;
+        listArgs.ObjectTableName = objectTableName;
+        listArgs.BackButtonTitle = "Occasions";
+        this._entityResourceService.getEntityResourceByTableName(listArgs.ObjectTableName, 0).subscribe(response => {
+            SessionLocator.DynamicLoader.Load('./Infrastructure/Components/ListComponent/ListComponent', this.CurrentSession.SessionLocation.viewContainerRef)
+                .then(cmpRef => {
+                    cmpRef.instance.ComponentRef = cmpRef;
+                    cmpRef.instance.Run(listArgs);
+                });
+        });
     }
 
 }
