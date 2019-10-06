4b32ec7fd23bc2f70dce6778e6913ae7d40b4a7a
Samar Abu Eid <Samar@logitudeworld.com>
2019-10-06 13:47:46 +0300

    56957
    Related Work Items: #56957

---
+++ Logitude/Logitude.CRM.BL/EntityUpdateServices/OccasionInviteeUpdateService.cs
@@ -0,0 +1,22 @@
+﻿using Logitude.Server.Tools.Counters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logitude.CRM.BL.EntityUpdateServices
+{
+    public partial class OccasionInviteeUpdateService
+    {
+        protected override void OnCreating(EntityPMs.OccasionInviteePM entityPM, EntityPMs.OccasionPM entityParentPM)
+        {
+            if (entityPM.ChangeSetOp == Simplog.Server.Infrastructure.ChangeSetOperation.Insert)
+            {
+                entityPM.Id = IdCounter.GetNumber("OccasionInvitee", entityPM.Tenant);
+            }
+
+            entityPM.OccasionId = entityParentPM.Id;
+        }
+    }
+}
