7cc5ba4a806bb9313adebc7ccd6cebcb1d1410ab
Sumaya Ghazi <sumaya@logitudeworld.com>
2019-10-08 13:33:56 +0300

    57776

--- Logitude/Logitude.BL/InvoiceModel/APIDataContract/QueryService/APInvoiceQueryService.cs
+++ Logitude/Logitude.BL/InvoiceModel/APIDataContract/QueryService/APInvoiceQueryService.cs
@@ -269,7 +269,7 @@ namespace Logitude.BL.InvoiceModel.APIDataContract.ApiV1
 
         private static TenantPM GetTenantPM(int tenant)
         {
-            TenantQuery tenantQuery = new TenantQuery();
+            //TenantQuery tenantQuery = new TenantQuery();
             TenantPM tenantPM = TenantQuery.GetSingleTenantPM(tenant);
             return tenantPM;
         }
