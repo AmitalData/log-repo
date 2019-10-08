4edc7f9ad0c07399047afc77844da5f745486e31
Sumaya Ghazi <sumaya@logitudeworld.com>
2019-10-08 16:02:25 +0300

    fullaccounting e2e

--- Logitude/AngularModules/AngularModules/e2e/FullAccounting/ARPayment/NewARPayment.ts
+++ Logitude/AngularModules/AngularModules/e2e/FullAccounting/ARPayment/NewARPayment.ts
@@ -1,6 +1,6 @@
 import { FieldsHelper } from '../../Helpers/FieldsHelper';
 import { GeneralFunctions } from '../../Helpers/GeneralFunctions';
-import { browser, by, element } from 'protractor';
+import { browser, by, element, WebDriver, protractor, $ } from 'protractor';
 
 
 
@@ -18,6 +18,8 @@ export class NewARPayment {
         browser.ignoreSynchronization = true;
         this.Helper.WaitByIdAndClick('NewARPayment');
         this.Helper.WaitBusyIndicator();
+        this.Helper.ItemsVisibility('ARPayment_BillToId');
+        this.Helper.ItemsPresent('ARPayment_BillToId');
         this.Helper.WaitByIdAndFill('ARPayment_BillToId', BillToName);
         this.Helper.WaitByCssAndClick_FromTagInsideList('.DropDownListItem', 0);
         this.Helper.WaitBusyIndicator();
@@ -27,27 +29,23 @@ export class NewARPayment {
         this.Helper.WaitByIdAndFill('ARPayment_AmountInPaymentCurrency', '10000');
         this.Helper.WaitByIdAndFill('ARPayment_AccountingPaymentMethodId', 'Cash');
         this.Helper.WaitByCssAndClick_SelectItemFromList('.DropDownList', 0),
-        this.Helper.WaitBusyIndicator();
+            this.Helper.WaitBusyIndicator();
 
-       
+
         this.Helper.WaitByIdAndClick('ok-AddARPayment');
         this.Helper.WaitWindowClosed();
         this.Helper.WaitBusyIndicator();
         this.Helper.WaitBusyIndicator();
         this.Helper.WaitByIdAndFill('ARPayment_BranchId', 'Main Office');
+        this.Helper.WaitBusyIndicator();
         this.Helper.WaitByCssAndClick_SelectItemFromList('.DropDownList', 0);
         this.Helper.WaitBusyIndicator();
-        this.Helper.ItemsVisibility('row0');
-//        this.Helper.WaitWindowClosed();
-
+        var ec = protractor.ExpectedConditions;
+        browser.wait(ec.invisibilityOf(element(by.id("ARPaymentSpinner"))), 100000);
+        this.Helper.WaitBusyIndicator();
         this.Helper.WaitByIdAndClick('ARPayment.B.Approve');
         this.Helper.WaitBusyIndicator();
-      //  browser.sleep(6000);
-       // this.Helper.WaitByIdAndClick('EditBackbutton');
-
-
-       // browser.sleep(6000);
-
+      
 
 
 
