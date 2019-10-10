380baaca658f5f839adaa70efb2a26f19721a5d9
Lana Daoud <Lana@logitudeworld.com>
2019-10-10 13:31:10 +0300

    Automation (Payable) e2e

--- Logitude/AngularModules/AngularModules/e2e/Operations/Shipments/EditEntity/PayablesTab.ts
+++ Logitude/AngularModules/AngularModules/e2e/Operations/Shipments/EditEntity/PayablesTab.ts
@@ -10,31 +10,39 @@ export class PayablesTabComponent {
     private amount1: any;
     private amount2: any;
     public PayablesTab(shipperRef1: string, ShipmentType: string) {
+
         this.Helper.WaitEditComponentBusyIndicator();
         this.Helper.WaitByIdAndClick('Shipment.TH.Payables');
         this.Helper.WaitEditComponentBusyIndicator();
-
         this.Helper.ItemsVisibility('ATDSPayable-payable');
 
-        //var InvoiceAmount: any;
+        this.AddPayableLines(shipperRef1,ShipmentType);
+        this.CreatAPInvoice(shipperRef1);
+        this.EditAPInvoice(shipperRef1);
+
+
+
+    }
+
+    AddPayableLines(shipperRef1: string, ShipmentType: string){
         if (ShipmentType == '') {
-            this.AddPayables('Air Frei', '10', '10');
-            this.AddPayables('Order', '10', '20');
+            this.AddPayables('Air Frei', '10', '10','Shipment');
+            this.AddPayables('Order', '10', '20','Shipment');
 
         } else if (ShipmentType == 'FCL' || ShipmentType == 'LCL') {
-            this.AddPayables('ocean', '10', '10');
-            this.AddPayables('Order', '10', '20');
+            this.AddPayables('ocean', '10', '10','Shipment');
+            this.AddPayables('Order', '10', '20','Shipment');
         } else {
-            this.AddPayables('Inland', '10', '10');
-            this.AddPayables('Order', '10', '20');
+            this.AddPayables('Inland', '10', '10','Shipment');
+            this.AddPayables('Order', '10', '20','Shipment');
         }
-        //InvoiceAmount = this.amount1 + this.amount2
-        this.AddAPInvoice(shipperRef1);
     }
-    AddPayables(ChargeType: string, quantity: any, unitPrice: any) {
-        var amount: any = 0;
-        this.Helper.WaitByIdAndClick('AddPayable');
+    AddPayables(ChargeType: string, quantity: any, unitPrice: any, Source :String) {
+       // var amount: any = 0;
 
+       if(Source == "Shipment"){
+        this.Helper.WaitByIdAndClick('AddPayable');
+        
         this.Helper.WaitByIdAndFill('ShipmentPayable_ChargesTypeId', ChargeType);
         this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'ShipmentPayable_ChargesTypeId', ChargeType);
 
@@ -46,11 +54,22 @@ export class PayablesTabComponent {
         this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'ShipmentPayable_CurrencyId', 'EU');
 
         this.Helper.WaitByIdAndClick('Ok-AddPayableBtn');
+    }
+    else {
+        this.Helper.WaitByIdAndClick('AddInvoiceLine');
+        this.Helper.WaitByIdAndFill('APInvoiceLine_ChargesTypeId', 'order');
+        this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'APInvoiceLine_ChargesTypeId', 'order');
+        this.Helper.WaitByIdAndFill('APInvoiceLine_VatTypeId','zero');
+        this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'APInvoiceLine_VatTypeId','zero');
+        this.Helper.WaitByIdAndFill('APInvoiceLine_InvoiceCurrencyAmount_2','100');
+        this.Helper.WaitByIdAndClick('Ok-AddInvoiceLine');
+    }
         // return amount;
     }
-    AddAPInvoice(shipperRef1: string) {
+    CreatAPInvoice(shipperRef1: string) {
         this.Helper.WaitBusyIndicator();
         this.Helper.WaitByIdAndClick('ReceiveInvoice');
+
 
         this.Helper.WaitByIdAndFill('APInvoice_VendorId', 'TestAgentExport1');
         this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'APInvoice_VendorId', 'TestAgentExport1');
@@ -70,24 +89,18 @@ export class PayablesTabComponent {
         this.Helper.WaitByIdAndFill('APInvoice_VATNumber', 'Vat Number ');
         this.Helper.WaitByIdAndClick('Ok-CreateAPInvoice');
         this.Helper.WaitBusyIndicator();
+    }
 
-
-
-        //this.Helper.WaitByIdAndClick('VATApplyToAll');
-
+        EditAPInvoice(shipperRef1: string){
         this.Helper.WaitByIdAndFill('APInvoice_VatTypeId', 'Zero');
         this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'APInvoice_VatTypeId', 'Zero');
         this.Helper.WaitByIdAndClick('VATApplyToAll');
 
         this.Helper.WaitByIdAndFill('APInvoiceLine_InvoiceCurrencyAmount', '100');
 
-        this.Helper.WaitByIdAndClick('AddInvoiceLine');
-        this.Helper.WaitByIdAndFill('APInvoiceLine_ChargesTypeId', 'order');
-        this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'APInvoiceLine_ChargesTypeId', 'order');
-        this.Helper.WaitByIdAndFill('APInvoiceLine_VatTypeId','zero');
-        this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'APInvoiceLine_VatTypeId','zero');
-        this.Helper.WaitByIdAndFill('APInvoiceLine_InvoiceCurrencyAmount_2','100');
-        this.Helper.WaitByIdAndClick('Ok-AddInvoiceLine');
+        this.AddPayables('Order', '10', '20','Invoice');
+
+        /**/
         this.Helper.WaitByIdAndClick('CheckBox_0_5_LBL');
         this.Helper.WaitEditComponentBusyIndicator();
         this.Helper.WaitByIdAndFill('APInvoice_VATNumber','Lana');
@@ -98,22 +111,29 @@ export class PayablesTabComponent {
         
         
         this.Helper.WaitByIdAndClick('APInvoice.B.Approve');
-        this.Helper.WaitShowEditComponentBusyIndicator();
-        this.Helper.WaitEditComponentBusyIndicator();
+        this.WaitBusyIndicatorToShowandHide();
+        this.WaitBusyIndicatorToShowandHide();        
         
 
         this.Helper.WaitByIdAndClick('MenuButtons_1');
         this.Helper.WaitByIdAndClick('APInvoice.B.CancelApproval');
-        this.Helper.WaitEditComponentBusyIndicator();
-
+        this.WaitBusyIndicatorToShowandHide();
         this.Helper.WaitByIdAndClick('MenuButtons_1');
         this.Helper.WaitByIdAndClick('APInvoice.B.Void');
         this.Helper.WaitByIdAndClick('ConfirmWindow_Yes_0');
-         this.Helper.WaitEditComponentBusyIndicator();
+        this.WaitBusyIndicatorToShowandHide();
         
-        
+
+        this.Helper.WaitByIdAndClick('APInvoice.TH.General');
+
         this.Helper.WaitByIdAndClick('EditBackbutton_1');
-        this.Helper.WaitEditComponentBusyIndicator();
+        this.WaitBusyIndicatorToShowandHide();
         // this.Helper.WaitBusyIndicator();
+        
     }
+    WaitBusyIndicatorToShowandHide(){
+    this.Helper.WaitShowEditComponentBusyIndicator();
+    this.Helper.WaitEditComponentBusyIndicator();
+   }
+    
 }  
