d54ba269f138b22eecf5b34648340073cdf76f90
Mohammad Masoud <mohammad@logitudeworld.com>
2019-10-10 08:42:56 +0300

    log text box alt+f10 problem

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/LogTextBoxComponent.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/LogTextBoxComponent.ts
@@ -545,13 +545,14 @@ export class LogTextBoxComponent implements BeforeOnDestroy,OnInit, AfterViewIni
     public ngxBeforeOnDestroy() {
         //console.log('1. BEFORE ONDESTROY INVOKE METHOD (await 2 sec)');
         return new Promise((resolve) => {
-            setTimeout(() => this.WaitFunction(resolve), 2000);
+            setTimeout(() => this.WaitFunction(resolve), 100);
         });
     }
 
 
-    @BeforeOnDestroy
-    ngOnDestroy() {
+    //@BeforeOnDestroy
+    async ngOnDestroy() {
+        await this.ngxBeforeOnDestroy();
         console.log("LogTextBox:ngOnDestroy");
         this.cd = null;
         if (this._debounceTimeSub) {
@@ -566,7 +567,7 @@ export class LogTextBoxComponent implements BeforeOnDestroy,OnInit, AfterViewIni
     private WaitFunction(resolve) {
         //console.log('2. EXECUTE HEAVY FUNCTION (3 sec)');
 
-        const sourcef = timer(3000)
+        const sourcef = timer(100)
             .pipe(take(1))
             .subscribe(() => {
                 resolve();
