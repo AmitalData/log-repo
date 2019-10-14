16a2fd9571df1311342e7d99c27b3d59b39b9752
Samar Abu Eid <Samar@logitudeworld.com>
2019-10-14 13:18:09 +0300

    55424
    Related Work Items: #55424

---
+++ Logitude/Logitude.BL/ShipmentsModel/Tools/DataMapping/CustomsTransferHeaderMapping.cs
@@ -0,0 +1,44 @@
+﻿using Logitude.BL.ShipmentsModel.EntityPMs;
+using Simplog.Data.ShipmentsModel.EntityPOCOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logitude.BL.ShipmentsModel.Tools.DataMapping
+{
+    public class CustomsTransferHeaderMapping
+    {
+        public static void MapEntity(CustomsTransferHeaderPM entityPM, CustomsTransferHeader entity, bool isNewState)
+        {
+            if (isNewState)
+            {
+                entity.Id = entityPM.Id;
+                entity.Tenant = entityPM.Tenant;
+                entity.CustomsTransferTypeCode = entityPM.CustomsTransferTypeCode;
+            }
+
+            entity.TransferNumber = entityPM.TransferNumber;
+            entity.TransferDate = entityPM.TransferDate;
+            entity.FileName = entityPM.FileName;
+            entity.CreatedByUserId = entityPM.CreatedByUserId;
+            entity.Notes = entityPM.Notes;
+            entity.SearchFields = entityPM.SearchFields;
+        }
+
+        public static void MapLine(CustomsTransferLinePM entityPM, CustomsTransferLine entity, bool isNewState)
+        {
+            if (isNewState)
+            {
+                entity.Id = entityPM.Id;
+                entity.Tenant = entityPM.Tenant;
+                entity.CustomsTransferHeaderId = entityPM.CustomsTransferHeaderId;
+            }
+
+            entity.ShipmentId = entityPM.ShipmentId;
+            entity.ShipmentNumber = entityPM.ShipmentNumber;
+            entity.SearchFields = entityPM.SearchFields;
+        }
+    }
+}
