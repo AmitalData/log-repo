7786fd43fe499a4ae0c7b9bb856956d50dcb348a
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2019-10-16 16:04:27 +0300

    e2e/rs

--- Logitude/AngularModules/AngularModules/e2e/Operations/PrintDocOut/PrintDocOut.ts
+++ Logitude/AngularModules/AngularModules/e2e/Operations/PrintDocOut/PrintDocOut.ts
@@ -10,15 +10,16 @@ export class PrintDocOut {
     }
 
     isPrintingCompleted(expectedId, closePopup) {
-        //   this.helper.WaitBusyIndicator();
+        // this.helper.WaitBusyIndicator();
         this.helper.WaitEditComponentBusyIndicator();
         this.helper.ItemsPresent(expectedId);
         if (closePopup) {
             this.helper.WaitByCssStringAndClick('.Button', 'Close');
-            this.helper.WaitBusyIndicator();
+            // this.helper.WaitBusyIndicator();
+            this.helper.WaitEditComponentBusyIndicator();
             this.helper.WaitWindowClosed();
         } else {
-            
+
             this.helper.WaitByIdAndClick('MessageWindow_Ok_0');
         }
     }
