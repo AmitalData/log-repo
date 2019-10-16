9e2770cdfb06d7ba83be3a0e9c3d04abe733f12c
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2019-10-16 13:12:58 +0300

    53586 - Logbox: Multi archive busy indicator and process time
    Related Work Items: #53586

--- Logitude/AngularModules/AngularModules/Invoice/Components/MenuButtons/APPaymentMenuButtonsHandler.ts
+++ Logitude/AngularModules/AngularModules/Invoice/Components/MenuButtons/APPaymentMenuButtonsHandler.ts
@@ -408,47 +408,7 @@ export class APPaymentMenuButtonsHandler {
         });
 
     }
-    OpenEditPaymentChequeScreen() {
-        this.CurrentSession.StartBusyIndicatorLoading();
-   
-       
-        var windowTitle = TextCodeTranslator.Translate("PaymentCheque");
-        var logWindow = new LogitudeWindow();
-        var windowArgs: any = {};
-        windowArgs = this.SetPaymentChequeWindowArgs(windowArgs);
-        logWindow.WindowArgs = windowArgs;
-        logWindow.Width = 520;
-        logWindow.Height = 450;
-        logWindow.Title = windowTitle;
-        logWindow.ShowCloseButton = true;
-
-        logWindow.WindowClosed.subscribe(($event: any) => this.ContinueSaving($event));
-        logWindow.Show('./InvoiceModules/APPayment/Components/EditTabs/EditPaymentChequeComponent');
-        this.CurrentSession.StopBusyIndicator();
-
-    }
-
-    public GetFullAccountingSettings() {
-        this.CurrentSession.StartBusyIndicatorLoading();
-        this.fullAccountingSettingPMService.get(SessionLocator.TenantPM.Id.toString()).subscribe(myResult => {
-            var myResponse: ServiceResponse = myResult;
-            this.CurrentSession.StopBusyIndicator();
-
-            if (myResponse != null) {
-
-                var res = myResponse.Result;
-                var fullAccountingSetting: FullAccountingSettingPM = res;
-                if (fullAccountingSetting.IsPaymentChequesActivated && this.EntityPM.ExcludeFromDeductionReport && this.EntityPM.PaymentMethodCode == "CH") {
-                    this.OpenEditPaymentChequeScreen();
-                } else {
-                    this.ContinueSaving(null);
-                }
-            }
-
-        });
-
-    }
-
+    
     SetPaymentChequeWindowArgs(windowArgs: any) {
         windowArgs.PayToGLAccountId = this.EntityPM.VendorGLAccountId;
         windowArgs.BankAccountId = this.EntityPM.BankAccountId;
