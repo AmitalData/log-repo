09219ce8d90f35a160bdb5cc707ec2ca76776e12
Mohammad Masoud <mohammad@logitudeworld.com>
2019-10-22 10:31:53 +0300

    lost code retreival

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/LogDatePickerComponent.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/LogDatePickerComponent.ts
@@ -2485,7 +2485,7 @@ export class LogDatePickerComponent
                 this.uiProperty.ValidValue = validValue;
                 this.uiProperty.ValidationError = errorMessage;
                 if (!validValue) {
-                    this.DataContext[this.ObjectFieldName] = null;
+                    
                     this.DatePickerInputDivStyle = {
                         border: "1px solid #ff0000"
                     };
