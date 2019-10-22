f051b66cf44668f2bbd9d08c345607b43479e23e
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2019-10-22 15:31:09 +0300

    e2e/rs

--- Logitude/AngularModules/AngularModules/e2e/Testing Scenarios/Production/Team Islam/RunTestCasesProd.bat
+++ Logitude/AngularModules/AngularModules/e2e/Testing Scenarios/Production/Team Islam/RunTestCasesProd.bat
@@ -37,8 +37,8 @@ cmd /c call npm run e2e -- --params.Env="prod_1" --params.Team="islam" --suite=l
  CALL :CheckError "NewUser"
  
 --NewShipper--
-cmd /c call npm run e2e -- --params.Env="prod_1" --params.Team="islam" --suite=login,NewShipper>D:\E2ETeamIslamReport\Report.log
- CALL :CheckError "NewShipper"
+rem cmd /c call npm run e2e -- --params.Env="prod_1" --params.Team="islam" --suite=login,NewShipper>D:\E2ETeamIslamReport\Report.log
+rem  CALL :CheckError "NewShipper"
  
 
 )
