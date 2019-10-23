2bf10c10129ee34ba00968d13d2c7d6a04629d39
itzik M <itzik@amital.co.il>
2019-10-23 09:43:15 +0300

    58384 שטר מטען בלדר יבוא - UCBUD2LT  - שליחה אוטומטית
    Related Work Items: #58384

--- Logitude/Logitude.Customs.BL/EntityUpdateServices/CustomsDocumentUpdateService.cs
+++ Logitude/Logitude.Customs.BL/EntityUpdateServices/CustomsDocumentUpdateService.cs
@@ -67,6 +67,7 @@ namespace Logitude.Customs.BL.EntityUpdateServices
                 }).ToList();
 
                 AutoSetOriginalDocumentTrue(entityPM);
+                this._AddPerfectCustomsDocumentMetaDataValues_IsMetaDataReady = true;
             }
         }
         protected override void UpdateComposition(CustomsDocumentPM entityPM)
@@ -294,7 +295,14 @@ namespace Logitude.Customs.BL.EntityUpdateServices
                     }
                 }
             }
-            if (ready)
+            if (_AddPerfectCustomsDocumentMetaDataValues_IsMetaDataReady)
+            {
+                if (requireddocumentTypeMetaDatas.Count == 0)
+                {
+                    ready = true;
+                }
+            }
+                if (ready)
             {
                 entityPM.IsMetaDataReady = true;
 
@@ -407,6 +415,8 @@ namespace Logitude.Customs.BL.EntityUpdateServices
         //protected override void AfterUpdating(CustomsDocumentPM entityPM, EntityPM entityParentPM)
 
         public bool IgnoreSendFailure = false;
+        private bool _AddPerfectCustomsDocumentMetaDataValues_IsMetaDataReady;
+
         void TrySendMessageToQueue(CustomsDocumentPM entityPM, bool forceDueLoadTest = false)
         {
             var send = false;
