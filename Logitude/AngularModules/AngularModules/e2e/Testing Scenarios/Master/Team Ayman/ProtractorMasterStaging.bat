539448cf6f30d6facb5605367e50f45d2e4a3b0f
Razan Jararah <razan@logitudeworld.com>
2019-10-27 07:19:14 +0200

    shipment actions #58181

--- Logitude/AngularModules/AngularModules/e2e/Testing Scenarios/Master/Team Ayman/ProtractorMasterStaging.bat
+++ Logitude/AngularModules/AngularModules/e2e/Testing Scenarios/Master/Team Ayman/ProtractorMasterStaging.bat
@@ -11,23 +11,23 @@ cd C:\Program Files (x86)\Jenkins\workspace\LogitudeTestDevOps\Logitude\AngularM
 FOR /L %%A IN (1,1,1) DO (  
     --------------------------------------------------Direct-------------------------------------------------------------------------------------------------
 
-    cmd /c call npm run e2e -- --params.Env="test" --params.Team="ayman" --params.ShipParams.ShipmentLevelCode="D" --params.ShipParams.Direction="Export" --params.ShipParams.TransportMode="A" --params.ShipParams.ShipmentType="" --suite=login,NewShipment >D:\E2ETeamAyman\prot.log 2>&1
+    cmd /c call npm run e2e -- --params.Env="test_staging" --params.Team="ayman" --params.ShipParams.ShipmentLevelCode="D" --params.ShipParams.Direction="Export" --params.ShipParams.TransportMode="A" --params.ShipParams.ShipmentType="" --suite=login,NewShipment >D:\E2ETeamAyman\prot.log 2>&1
     CALL :CheckError "Export-Air-Direct"
 
     --------------------------------------------------Houses-------------------------------------------------------------------------------------------------
-    cmd /c call npm run e2e -- --params.Env="test"  --params.Team="ayman" --params.ShipParams.ShipmentLevelCode="H" --params.ShipParams.Direction="Import" --params.ShipParams.TransportMode="O" --params.ShipParams.ShipmentType="FCL" --suite=login,NewShipment> D:\E2ETeamAyman\prot.log 2>&1
+    cmd /c call npm run e2e -- --params.Env="test_staging"  --params.Team="ayman" --params.ShipParams.ShipmentLevelCode="H" --params.ShipParams.Direction="Import" --params.ShipParams.TransportMode="O" --params.ShipParams.ShipmentType="FCL" --suite=login,NewShipment> D:\E2ETeamAyman\prot.log 2>&1
     CALL :CheckError "Import-Ocean-FCL-House"
 
     --------------------------------------------------Master-------------------------------------------------------------------------------------------------
-    cmd /c call npm run e2e -- --params.Env="test" --params.Team="ayman" --params.ShipParams.ShipmentLevelCode="M" --params.ShipParams.Direction="Domestic" --params.ShipParams.TransportMode="O" --params.ShipParams.ShipmentType="LCL" --suite=login,NewShipment> D:\E2ETeamAyman\prot.log 2>&1
+    cmd /c call npm run e2e -- --params.Env="test_staging" --params.Team="ayman" --params.ShipParams.ShipmentLevelCode="M" --params.ShipParams.Direction="Domestic" --params.ShipParams.TransportMode="O" --params.ShipParams.ShipmentType="LCL" --suite=login,NewShipment> D:\E2ETeamAyman\prot.log 2>&1
     CALL :CheckError "Domestic-Ocean-LCL-Master"
 
     --------------------------------------------------SpotRate-------------------------------------------------------------------------------------------------
-    cmd /c call npm run e2e -- --params.Env="test" --params.Team="ayman"  --params.QuoteParams.Direction="Export" --params.QuoteParams.TransportMode="A"  --params.QuoteParams.ShipmentType="" --params.QuoteParams.QuoteType="SpotRate" --suite=login,NewQuote > D:\E2ETeamAyman\prot.log 2>&1
+    cmd /c call npm run e2e -- --params.Env="test_staging" --params.Team="ayman"  --params.QuoteParams.Direction="Export" --params.QuoteParams.TransportMode="A"  --params.QuoteParams.ShipmentType="" --params.QuoteParams.QuoteType="SpotRate" --suite=login,NewQuote > D:\E2ETeamAyman\prot.log 2>&1
     CALL :CheckError "Export-Air-SpotRate"
 
     --------------------------------------------------RoutingRate-------------------------------------------------------------------------------------------------
-    cmd /c call npm run e2e -- --params.Env="test" --params.Team="ayman" --params.QuoteParams.Direction="Domestic" --params.QuoteParams.TransportMode="A"  --params.QuoteParams.ShipmentType="" --params.QuoteParams.QuoteType="RoutingRate" --suite=login,NewQuote > D:\E2ETeamAyman\prot.log 2>&1
+    cmd /c call npm run e2e -- --params.Env="test_staging" --params.Team="ayman" --params.QuoteParams.Direction="Domestic" --params.QuoteParams.TransportMode="A"  --params.QuoteParams.ShipmentType="" --params.QuoteParams.QuoteType="RoutingRate" --suite=login,NewQuote > D:\E2ETeamAyman\prot.log 2>&1
     CALL :CheckError "Domestic-Air-RoutingRate" 
 )
 cd /
