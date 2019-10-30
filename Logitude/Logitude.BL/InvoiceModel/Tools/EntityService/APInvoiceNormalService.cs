f8676cc1301fffb9e573d9d5ee678a2b0353a763
alaa <alaa.jamal.h@live.com>
2019-10-30 15:40:33 +0200

    58478
    Related Work Items: #58478

--- Logitude/Logitude.BL/InvoiceModel/Tools/EntityService/APInvoiceNormalService.cs
+++ Logitude/Logitude.BL/InvoiceModel/Tools/EntityService/APInvoiceNormalService.cs
@@ -2018,6 +2018,7 @@ namespace Logitude.BL.InvoiceModel.Tools.EntityService
                 JournalEntity journal = rep.GetJournalByAccountingEntityId(entityPM.Id, tenant);
                 if (journal != null)
                 {
+                    entityPM.JournalId = journal.JournalId;
                     entityPM.JournalNumber = journal.JournalNumber;
                 }
             }
