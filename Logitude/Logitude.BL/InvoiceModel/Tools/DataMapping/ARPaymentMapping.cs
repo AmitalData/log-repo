7693075dca2106f825e11549e4f0bb9b81f3d6f6
Maheera Jazi <Maheera@logitudeworld.com>
2019-11-03 12:34:36 +0200

    57905
    Related Work Items: #57905

--- Logitude/Logitude.BL/InvoiceModel/Tools/DataMapping/ARPaymentMapping.cs
+++ Logitude/Logitude.BL/InvoiceModel/Tools/DataMapping/ARPaymentMapping.cs
@@ -29,6 +29,7 @@ namespace Logitude.BL.InvoiceModel.Tools.DataMapping
                         entityPM.CreatedByUserId = loggedContact.Id;
                     }
                     entityPM.OpenAmount = entityPM.AmountInPaymentCurrency;
+                    entityPM.OpenAmountInLocalCurrency = entityPM.AmountInLocalCurrency;
 
                     entity.Id = entityPM.Id;
                     entity.PaymentNo = entityPM.PaymentNo;
