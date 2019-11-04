f92e7726f7f45dea3217325441af56784609d27b
Abdullah Dawoud <abdullah@logitudeworld.com>
2019-11-04 10:26:38 +0200

    Related Work Items: #58623

--- Logitude/AngularModules/AngularModules/Accounting/Components/Others/ExternalReconcileComponent.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/Others/ExternalReconcileComponent.ts
@@ -329,7 +329,7 @@ export class ExternalReconcileComponent extends BaseComponent implements OnInit,
                             });
                     });
                 });
-                    
+
             }
         });
 
@@ -559,7 +559,7 @@ export class ExternalReconcileComponent extends BaseComponent implements OnInit,
         var glaccountId;
         if (this.ObjectTableName == "GLAccount")
             glaccountId = this.EntityPM.Id;
-        else if (this.ObjectTableName == "GLAccount")
+        else if (this.ObjectTableName == "BankAccount")
             glaccountId = this.BankAccountPM.GLAccountId;
         return glaccountId;
     }
