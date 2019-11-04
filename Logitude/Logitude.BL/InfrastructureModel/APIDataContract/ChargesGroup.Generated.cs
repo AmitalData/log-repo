e5d6787d7581d78355efc3dbca3b1b236f7a8194
alaa <alaa.jamal.h@live.com>
2019-11-04 11:48:45 +0200

    58671
    Related Work Items: #58671

--- Logitude/Logitude.BL/InfrastructureModel/APIDataContract/ChargesGroup.Generated.cs
+++ Logitude/Logitude.BL/InfrastructureModel/APIDataContract/ChargesGroup.Generated.cs
@@ -17,7 +17,7 @@ using Simplog.Data.QuoteModel.EntityPOCOs;
 namespace Logitude.BL.InfrastructureModel.APIDataContract.ApiV1
 {
    
-    public class ChargesGroup
+    public partial class ChargesGroup
     {
 
 	    
--- Logitude/Logitude.BL/InvoiceModel/APIDataContract/APInvoice.Generated.cs
+++ Logitude/Logitude.BL/InvoiceModel/APIDataContract/APInvoice.Generated.cs
@@ -17,7 +17,7 @@ using Simplog.Data.QuoteModel.EntityPOCOs;
 namespace Logitude.BL.InvoiceModel.APIDataContract.ApiV1
 {
    
-    public class APInvoice
+    public partial class APInvoice
     {
 
 	    
