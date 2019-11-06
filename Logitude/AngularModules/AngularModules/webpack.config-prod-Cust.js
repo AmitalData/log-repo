a2a0eecfce2b10d3019a9022834405035105b0ca
elisheva <elisheva@amital.co.il>
2019-11-06 13:55:11 +0200

    local path

--- Logitude/AngularModules/AngularModules/webpack.config-prod-Cust.js
+++ Logitude/AngularModules/AngularModules/webpack.config-prod-Cust.js
@@ -564,6 +564,8 @@ module.exports = {
           //"outputTemplate": "C:\\GIT\\log-repo\\Logitude\\AngularModules\\AngularModules\\node_modules\\license-webpack-plugin\\output.template.ejs",
           ///moran 
           //"outputTemplate": "C:\\LW\\log-repo\\Logitude\\AngularModules\\AngularModules\\node_modules\\license-webpack-plugin\\output.template.ejs",
+         ///elisheva 
+          //"outputTemplate": "C:\\LWC\\Logitude\\AngularModules\\AngularModules\\node_modules\\license-webpack-plugin\\output.template.ejs",
 
           "outputFilename": "3rdpartylicenses.txt",
           "suppressErrors": true,
