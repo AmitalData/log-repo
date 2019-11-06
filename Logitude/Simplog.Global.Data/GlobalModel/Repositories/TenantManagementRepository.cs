7bcd0b090ba2c778c903d48bd1eab0eb4ba27219
Maheera Jazi <Maheera@logitudeworld.com>
2019-11-06 13:37:58 +0200

    59031
    Related Work Items: #59031

--- Logitude/Simplog.Global.Data/GlobalModel/Repositories/TenantManagementRepository.cs
+++ Logitude/Simplog.Global.Data/GlobalModel/Repositories/TenantManagementRepository.cs
@@ -164,6 +164,7 @@ namespace Simplog.Global.Data.GlobalModel.Repositories
 
         public TenantManagement GetSingleTenantManagementPMByListOfEmails(List<string> emails)
         {
+
             TenantManagement myTenant = new TenantManagement();
             List<TenantManagement> tenants = new List<TenantManagement>();
             if (emails.Count > 0)
