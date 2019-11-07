7d9f581bd4bc2fdac8064077f9c87fc2005e2469
Abd Al-Rahman Malakh <abd@logitudeworld.com>
2019-11-07 15:41:12 +0200

    58997 59001 59003
    Related Work Items: #58997, #59001, #59003

--- Logitude/Logitude.DBMigrations/Program.cs
+++ Logitude/Logitude.DBMigrations/Program.cs
@@ -1,62 +1,17 @@
 ﻿using Logitude.DBMigrations.Helpers;
-using Logitude.DBMigrations.Models;
-using System;
-using System.Configuration;
-using System.Data.SqlClient;
-using System.IO;
 
 namespace Logitude.DBMigrations
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-
-            var arguments = Array.ConvertAll(args, a => a.ToLower());
-
-            string projectDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
-            string DXMLFilesPath = Path.Combine(projectDirectory, @"EntityFiles");
-            string[] DXMLFiles = Directory.GetFiles(DXMLFilesPath, "*.dxml");
-
-            string generatedScript = "";
-
-            foreach (var DXMLFile in DXMLFiles)
+            string generatedScript = AppHelper.GenerateScriptFromDXMLFiles();
+            AppHelper.SaveGeneratedScript(generatedScript);
+            if (AppHelper.CheckAppArguments(args, "-exe"))
             {
-                var fileName = Path.GetFileName(DXMLFile).Split('.')[0];
-                Console.WriteLine("Generating Script For " + fileName + " Entity ...");
-                string xmlString = File.ReadAllText(DXMLFile);
-                TableDefinition DXMLTable = xmlString.ParseXML<TableDefinition>();
-                TableMigrations tableMigrations = new TableMigrations(DXMLTable);
-                generatedScript += tableMigrations.GetScript();
-                generatedScript += "\n-------------------------------------------------------\n";
-
+                AppHelper.ExecuteGeneratedScript(generatedScript);
             }
-
-            Console.WriteLine("Saving The Generated Script ...");
-            string generatedScriptFilePath = Path.Combine(projectDirectory, @"GeneratedScript\Script.sql");
-            File.WriteAllText(generatedScriptFilePath, generatedScript);
-            Console.WriteLine("The Generated Script Saved Successfully To GeneratedScript/Script.sql");
-
-            if (Array.IndexOf(arguments, "-exe") != -1)
-            {
-                Console.WriteLine("Executing The Generated Script To The Database ...");
-                try
-                {
-                    string connectionString = ConfigurationManager.AppSettings["ConnectionString"];
-                    SqlConnection mySqlConnection = new SqlConnection(connectionString);
-                    SqlCommand mySqlCommand = mySqlConnection.CreateCommand();
-                    mySqlCommand.CommandText = generatedScript;
-                    mySqlConnection.Open();
-                    mySqlCommand.ExecuteNonQuery();
-                    Console.WriteLine("The Generated Script Executed Successfully");
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Error While Executing The Generated Script:");
-                    Console.WriteLine(e.ToString());
-                }
-            }
-
         }
     }
 }
