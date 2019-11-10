f7f5456429e0203633c4890a05c55657cb7edeab
Abed <abed@logitudeworld.com>
2019-11-10 15:10:02 +0200

    58575
    Related Work Items: #58575

--- Logitude/MeatadataGeneratorTool/MeatadataGeneratorTool/MeatadataGeneratorTool/ObjectFieldsViewModel.cs
+++ Logitude/MeatadataGeneratorTool/MeatadataGeneratorTool/MeatadataGeneratorTool/ObjectFieldsViewModel.cs
@@ -1038,6 +1038,42 @@ namespace MeatadataGeneratorTool
             }
         }
 
+
+        private bool displayInAutomationAsEnitity;
+        public bool DisplayInAutomationAsEnitity
+        {
+            get
+            {
+                return displayInAutomationAsEnitity;
+            }
+            set
+            {
+                displayInAutomationAsEnitity = value;
+                FirePropertyChanged("DisplayInAutomationAsEnitity");
+            }
+        }
+
+
+        private string recordType;
+        public string RecordType
+        {
+            get
+            {
+                return recordType;
+            }
+            set
+            {
+                recordType = value;
+                FirePropertyChanged("RecordType");
+            }
+        }
+
+
+
+
+
+
+
         string hTMLListComponentURL;
         public string HtmlListComponentUrl
         {
