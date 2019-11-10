43f41b11070d38425513da7201a0b2f87ae58d9a
alaa <alaa.jamal.h@live.com>
2019-11-10 13:47:47 +0200

    57540
    Related Work Items: #57540

--- Logitude/Logitude.Accounting.BL/CoreBL/JournalLineOnUpdate.cs
+++ Logitude/Logitude.Accounting.BL/CoreBL/JournalLineOnUpdate.cs
@@ -50,7 +50,7 @@ namespace Logitude.Accounting.BL.CoreBL
             journalLinePM.CreditControlAccountId = creditIVerifyGLAccountManager.ControlAccountId;
 
 
-
+            FullAccountingSettingPM accountingSettings = getFullAccountingSettings(journalPM.Tenant);
 
             var debitIVerifyGLAccountManager = GetIVerifyGLAccountManager();
             debitIVerifyGLAccountManager.Verify(
@@ -65,8 +65,11 @@ namespace Logitude.Accounting.BL.CoreBL
                 haveChange = (journalLinePM.DebitAccountId != debitIVerifyGLAccountManager.AccountId ||
                     journalLinePM.DebitControlAccountId != debitIVerifyGLAccountManager.ControlAccountId);
             }
-            journalLinePM.DebitAccountId = debitIVerifyGLAccountManager.AccountId;
-            journalLinePM.DebitControlAccountId= debitIVerifyGLAccountManager.ControlAccountId;
+            if (accountingSettings.VATOutputGLAccountId != journalLinePM.CreditAccountId)
+            {
+                journalLinePM.DebitAccountId = debitIVerifyGLAccountManager.AccountId;
+                journalLinePM.DebitControlAccountId = debitIVerifyGLAccountManager.ControlAccountId;
+            }
 
 
 
@@ -122,7 +125,13 @@ namespace Logitude.Accounting.BL.CoreBL
 
 
 
-
+        private FullAccountingSettingPM getFullAccountingSettings(int tenant)
+        {
+            FullAccountingSettingPM accountingSettings;
+            FullAccountingSettingQueryService query = new FullAccountingSettingQueryService(tenant);
+            accountingSettings = query.GetSingleFullAccountingSetting(tenant);
+            return accountingSettings;
+        }
 
 
 
