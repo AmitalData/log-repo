cd37cd7da5b08e327cf2363cb7f3df1ec7a91b3d
alaa <alaa.jamal.h@live.com>
2019-11-12 20:56:45 +0200

    47565
    Related Work Items: #47565

--- Logitude/JustWebFreight/WebFreight.Web/DataProviders/RevenueExpenseDataProvider.cs
+++ Logitude/JustWebFreight/WebFreight.Web/DataProviders/RevenueExpenseDataProvider.cs
@@ -33,8 +33,8 @@ namespace WebFreight.Web.DataProviders
 
         public decimal? TotalForeignCloseBalance { get; set; }
         public bool CurrencyDetailed { get; set; }
-        public bool DetailedVendorsAccounts { get; set; }
-        public bool DetailedCustomersAccounts { get; set; }
+        public string DetailedVendorsAccounts { get; set; }
+        public string DetailedCustomersAccounts { get; set; }
         public string Category { get; set; }
         public bool UseZeroFilter { get; set; }
         public DateTime? FromDate { get; set; }
