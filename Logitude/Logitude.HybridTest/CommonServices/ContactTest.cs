81d8f9cbe8683c8610d1a72128b2caae347ad935
Wesam <Wesam@WESAM>
2019-11-14 14:36:24 +0200

    Hybrid Integration Testing
    Related Work Items: #58784

--- Logitude/Logitude.HybridTest/CommonServices/ContactTest.cs
+++ Logitude/Logitude.HybridTest/CommonServices/ContactTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Logitude.Server.Tools;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Logitude.HybridTest.CommonServices
@@ -10,31 +11,87 @@ namespace Logitude.HybridTest.CommonServices
         public void Test_Contact_UPSERT()
         {
             LoginService.GetLoginTokenByCredentials();
-            Server.Tools.Response serviceResponse = CallContactUpsert();
-            Assert.AreEqual(serviceResponse.HasError, false, serviceResponse.ErrorMessage);
+            Response serviceResponse = CallContactUpsert();
+            Assert.IsFalse(serviceResponse.HasError, "Upsert Failed! " + serviceResponse.ErrorMessage);
             Assert.IsNotNull(serviceResponse.Result, "Upsert Failed! " + serviceResponse.ErrorMessage);
          }
 
-        public static Server.Tools.Response CallContactUpsert()
+        public static Response CallContactUpsert()
         {
-
             ContactServiceReference.ContactWcfServiceClient serviceClient = new ContactServiceReference.ContactWcfServiceClient();
             string serviceAddress = serviceClient.Endpoint.Address.ToString().Replace("http://localhost:9996", TestEnvironmentGlobalParameters.ServerURL);
             serviceClient.Endpoint.Address = new System.ServiceModel.EndpointAddress(serviceAddress);
-            using (new System.ServiceModel.OperationContextScope((System.ServiceModel.IClientChannel)serviceClient.InnerChannel))
+            using (new System.ServiceModel.OperationContextScope(serviceClient.InnerChannel))
             {
-
                 System.ServiceModel.Web.WebOperationContext.Current.OutgoingRequest.Headers.Add("Token", TestEnvironmentGlobalParameters.Token);
                 ContactServiceReference.ContactPM entityPM = new ContactServiceReference.ContactPM()
                 {
                     EnglishName = "Hybrid Contact",
                     LocalName = "Hybrid Contact",
+                    Email = "HybridContact@logitudeworld.com",
                     ExternalId = HybridCodes.ContactCode,
                     Tenant = TestEnvironmentGlobalParameters.Tenant,
-
                 };
-                Logitude.Server.Tools.Response serviceResponse = serviceClient.Upsert(entityPM, false);
+                Response serviceResponse = serviceClient.Upsert(entityPM, false);
                 return serviceResponse;
+            }
+        }
+
+        [TestMethod]
+        public void Test_Contact_GetContactPMByEmail()
+        {
+            Test_Contact_UPSERT();
+            ContactServiceReference.ContactWcfServiceClient serviceClient = new ContactServiceReference.ContactWcfServiceClient();
+            string serviceAddress = serviceClient.Endpoint.Address.ToString().Replace("http://localhost:9996", TestEnvironmentGlobalParameters.ServerURL);
+            serviceClient.Endpoint.Address = new System.ServiceModel.EndpointAddress(serviceAddress);
+            using (new System.ServiceModel.OperationContextScope(serviceClient.InnerChannel))
+            {
+                System.ServiceModel.Web.WebOperationContext.Current.OutgoingRequest.Headers.Add("Token", TestEnvironmentGlobalParameters.Token);
+                Response serviceResponse = new Response();
+                ContactServiceReference.ContactPM entityPM = serviceClient.GetContactPMByEmail("HybridContact@logitudeworld.com", TestEnvironmentGlobalParameters.Tenant, ref serviceResponse);
+                Assert.AreEqual(entityPM.EnglishName,"Hybrid Contact", "Get Contact PM By Email Failed! " + serviceResponse.ErrorMessage);
+                Assert.IsFalse(serviceResponse.HasError, "Get Contact PM By Email Failed! " + serviceResponse.ErrorMessage);
+                Assert.IsNull(serviceResponse.Result, "Get Contact PM By Email Failed! " + serviceResponse.ErrorMessage);
+            }
+        }
+
+        [TestMethod]
+        public void Test_Contact_GetContactList()
+        {
+            Test_Contact_UPSERT();
+            ContactServiceReference.ContactWcfServiceClient serviceClient = new ContactServiceReference.ContactWcfServiceClient();
+            string serviceAddress = serviceClient.Endpoint.Address.ToString().Replace("http://localhost:9996", TestEnvironmentGlobalParameters.ServerURL);
+            serviceClient.Endpoint.Address = new System.ServiceModel.EndpointAddress(serviceAddress);
+            using (new System.ServiceModel.OperationContextScope(serviceClient.InnerChannel))
+            {
+                System.ServiceModel.Web.WebOperationContext.Current.OutgoingRequest.Headers.Add("Token", TestEnvironmentGlobalParameters.Token);
+                Response serviceResponse = new Response();
+                ContactServiceReference.ContactApiFilters filters = new ContactServiceReference.ContactApiFilters();
+                filters.Take = 10;
+                filters.ByCode = true;
+                filters.SearchFields = HybridCodes.ContactCode;
+                ContactServiceReference.ContactList[] serviceResult = serviceClient.GetContactList(filters, TestEnvironmentGlobalParameters.Tenant, ref serviceResponse);
+                Assert.AreEqual(serviceResult[0].EnglishName, "Hybrid Contact", "Get Contact PM By Email Failed! " + serviceResponse.ErrorMessage);
+                Assert.IsFalse(serviceResponse.HasError, "Get Contact PM By Email Failed! " + serviceResponse.ErrorMessage);
+                Assert.IsNull(serviceResponse.Result, "Get Contact PM By Email Failed! " + serviceResponse.ErrorMessage);
+            }
+        }
+
+        [TestMethod]
+        public void Test_Contact_GetContactByExternalId()
+        {
+            Test_Contact_UPSERT();
+            ContactServiceReference.ContactWcfServiceClient serviceClient = new ContactServiceReference.ContactWcfServiceClient();
+            string serviceAddress = serviceClient.Endpoint.Address.ToString().Replace("http://localhost:9996", TestEnvironmentGlobalParameters.ServerURL);
+            serviceClient.Endpoint.Address = new System.ServiceModel.EndpointAddress(serviceAddress);
+            using (new System.ServiceModel.OperationContextScope(serviceClient.InnerChannel))
+            {
+                System.ServiceModel.Web.WebOperationContext.Current.OutgoingRequest.Headers.Add("Token", TestEnvironmentGlobalParameters.Token);
+                Response serviceResponse = new Response();
+                ContactServiceReference.ContactPM entityPM = serviceClient.GetContactByExternalId("HybridContact@logitudeworld.com", TestEnvironmentGlobalParameters.Tenant, ref serviceResponse);
+                Assert.AreEqual(entityPM.EnglishName, "Hybrid Contact", "Get Contact By External Id Failed! " + serviceResponse.ErrorMessage);
+                Assert.IsFalse(serviceResponse.HasError, "Get Contact By External Id Failed! " + serviceResponse.ErrorMessage);
+                Assert.IsNull(serviceResponse.Result, "Get Contact By External Id Failed! " + serviceResponse.ErrorMessage);
             }
         }
     }
