077222c3d7a390c81b9fc6090719004d86fc85c8
elisheva <elisheva@amital.co.il>
2019-11-17 11:24:02 +0200

    Change calc PRAT_MEHES_LIST
    Related Work Items: #53872

--- Logitude/Logitude.Customs.BL/EntityUpdateServices/UnifreightDeclarationUpdateService.cs
+++ Logitude/Logitude.Customs.BL/EntityUpdateServices/UnifreightDeclarationUpdateService.cs
@@ -2257,7 +2257,7 @@ namespace Logitude.Customs.BL.EntityUpdateServices
 
             _CCUFILEMPM.NOOFINVOICES = _DirtyDeclarationPM.SupplierInvoices.Count();
             _CCUFILEMPM.TOTALINVOICELINESNO = GetCountSupplierInvoicesItems();
-         //   _CCUFILEMPM.PRATMEHESLIST = GetAllPratMehesList(3);
+            _CCUFILEMPM.PRATMEHESLIST = GetAllPratMehesList(3);
             _CCUFILEMPM.ALLPRATMEHESLIST = GetAllPratMehesList();
 
 
@@ -2288,7 +2288,7 @@ namespace Logitude.Customs.BL.EntityUpdateServices
             List<string> list = new List<string>();
             foreach (var invoice in _DirtyDeclarationPM.SupplierInvoices)
             {
-                list.AddRange(invoice.SupplierInvoiceItems.Select(x=>x.ClassificationCode));
+                list.AddRange(invoice.SupplierInvoiceItems.Select(x=>x.ClassificationCode.Substring(0, Math.Min(8, x.ClassificationCode.Length)) + x.ClassificationCode.Substring(Math.Min(11, x.ClassificationCode.Length - 1), 1)));
             }
             list = list.Where(x => x != null).OrderBy(x => x).Distinct().ToList();
             if (top!=0 && top < list.Count())
