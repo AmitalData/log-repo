6b8406eff2146cb155e5292d373e7bb986a4ad0e
Maheera Jazi <Maheera@logitudeworld.com>
2019-11-20 15:21:43 +0200

    59062
    Related Work Items: #59062

--- Logitude/Logitude.BL/InvoiceModel/APIDataContract/QueryService/APInvoiceQueryService.cs
+++ Logitude/Logitude.BL/InvoiceModel/APIDataContract/QueryService/APInvoiceQueryService.cs
@@ -416,6 +416,7 @@ namespace Logitude.BL.InvoiceModel.APIDataContract.ApiV1
                                             {
                                                 throw new ApplicationException("Line Container Type should be Ocean");
                                             }
+                                            line.ContainerTypeCode = packageType.Code;
                                         }
                                     }
                                 }
