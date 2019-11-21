177861b0610abe6b418946bbc1736418c54d2589
Khalid Khatib <khalid@logitudeworld.com>
2019-11-21 13:05:07 +0200

    QB - OAuth 2 Migration [5]
    Related Work Items: #59576

--- Logitude/CommunicationWorkerRole/QuckbooksOnlineWorkerRole.cs
+++ Logitude/CommunicationWorkerRole/QuckbooksOnlineWorkerRole.cs
@@ -1219,7 +1219,7 @@ namespace CommunicationWorkerRole
         {
             OAuth2RequestValidator oauthValidator = new OAuth2RequestValidator(GetAccessToken(tenant, entityPM, mySetting));
             ServiceContext serviceContext = new ServiceContext(entityPM.QBOrealMeID, IntuitServicesType.QBO, oauthValidator);
-            serviceContext.IppConfiguration.BaseUrl.Qbo = "https://sandbox-quickbooks.api.intuit.com/";
+            serviceContext.IppConfiguration.BaseUrl.Qbo = "https://quickbooks.api.intuit.com/";
 
             return serviceContext;
 
