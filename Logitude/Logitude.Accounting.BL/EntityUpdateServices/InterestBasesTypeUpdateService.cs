f0b6c60333dfae32ac5367864e06675c42d7d5ef
Mutaz Zuhairi <Mutaz@logitudeworld.com>
2019-11-24 13:15:42 +0200

    59720
    Related Work Items: #59720

--- Logitude/Logitude.Accounting.BL/EntityUpdateServices/InterestBasesTypeUpdateService.cs
+++ Logitude/Logitude.Accounting.BL/EntityUpdateServices/InterestBasesTypeUpdateService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Logitude.Accounting.Data.EntityPOCOs;
+using Logitude.Accounting.Def.EntityPMs;
+using Logitude.Server.Tools;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,20 @@ using System.Threading.Tasks;
 
 namespace Logitude.Accounting.BL.EntityUpdateServices
 {
-    class InterestBasesTypeUpdateService
+    public partial class InterestBasesTypeUpdateService
     {
+        protected override void OnCreating(InterestBasesTypePM entityPM, EntityPM entityParentPM)
+        {
+          
+        }
+
+        protected override void OnUpdating(InterestBasesTypePM entityPM, InterestBasesType entityPOCO)
+        {
+        }
+
+        protected override void UpdateComposition(InterestBasesTypePM entityPM)
+        {
+  
+        }
     }
 }
