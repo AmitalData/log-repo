5f455df291e930c0a61cfd13673afd113a66369c
Wesam <Wesam@WESAM>
2019-11-26 10:06:56 +0200

    test pull request

--- Logitude/Logitude.HybridTest/CommonServices/ShipmentTest.cs
+++ Logitude/Logitude.HybridTest/CommonServices/ShipmentTest.cs
@@ -40,7 +40,7 @@ namespace Logitude.HybridTest.CommonServices
                 ServiceOperation = "Cancel",
                 ServiceResponseIndex = 0,
                 ServiceType = null,
-                ServiceFilterType = null,
+                ServiceFilterType = null,//test puol request
             };
 
             Response serviceResponse = new Response();
