f23d36b3febc9b5199e33c32996a4006b3709dca
Sumaya Ghazi <sumaya@logitudeworld.com>
2019-11-26 08:17:18 +0200

    E2EARInvoice

--- Logitude/AngularModules/AngularModules/e2e/FullAccounting/ARInvoice/NewARInvoice.ts
+++ Logitude/AngularModules/AngularModules/e2e/FullAccounting/ARInvoice/NewARInvoice.ts
@@ -13,20 +13,16 @@ export class NewARInvoice {
         this.Helper.ItemsVisibility('NewInvoice');
         this.Helper.ItemsPresent('NewInvoice');
         this.Helper.WaitByIdAndClick('NewInvoice');
-
-        //  this.Helper.WaitByIdAndClick('NewGeneralARInvoice');
-        //this.Helper.WaitByCssButtonClick('.ToggleButtonMenu','New Invoice');
         this.Helper.WaitByIdAndClick('NewGeneralInvoice');
+        this.Helper.WaitBusyIndicator();
+        this.Helper.WaitByIdAndFill('ARInvoice_InvoiceCurrencyId', 'NIS');
+        this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'ARInvoice_InvoiceCurrencyId', 'NIS');
         this.Helper.WaitBusyIndicator();
 
         this.Helper.WaitByIdAndFill('ARInvoice_BillToId', BillToName);
         this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'ARInvoice_BillToId', BillToName);
         this.Helper.WaitBusyIndicator();
-        this.Helper.WaitByIdAndFill('ARInvoice_CustomerRef', 'Refrence');
-        browser.sleep(5000);
-        this.Helper.WaitByIdAndFill('ARInvoice_InvoiceCurrencyId', 'NIS');
-        this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'ARInvoice_InvoiceCurrencyId', 'NIS');
-        this.Helper.WaitBusyIndicator();
+      
         this.Helper.WaitByIdAndFill('ARInvoice_VatNumber', '0');
         this.Helper.WaitBusyIndicator();
        // this.Helper.WaitBusyIndicator(); // closed month
@@ -36,30 +32,33 @@ export class NewARInvoice {
         this.Helper.WaitByIdAndClick('ok-addArInvoice');
         this.Helper.WaitBusyIndicator();
         this.Helper.WaitWindowClosed();
-        // this.Helper.WaitBusyIndicator();
-        //this.Helper.WaitBusyIndicatorToShow();
-        //this.Helper.WaitBusyIndicator();
+       
         this.WaitBusyIndicatorToShowandHide();
         this.Helper.WaitByIdAndClick('Add');
         this.Helper.WaitBusyIndicator();
-        //this.WaitBusyIndicatorToShowandHide();
+       
         this.Helper.WaitByIdAndFill('ARInvoiceLine_ChargesTypeId', 'Air Freight');
         this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'ARInvoiceLine_ChargesTypeId', 'Air Freight');
         this.Helper.WaitBusyIndicator();
         this.Helper.WaitByIdAndFill('ARInvoiceLine_VatTypeId', 'Zero');
         this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'ARInvoiceLine_VatTypeId', 'Zero');
         this.Helper.WaitBusyIndicator();
+        this.Helper.WaitByIdAndFill('ARInvoiceLine_ForiegnCurrencyId', 'NIS');
+        this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'ARInvoiceLine_ForiegnCurrencyId', 'NIS');
+        this.Helper.WaitBusyIndicator();
         this.Helper.WaitByIdAndFill('ARInvoiceLine_Quantity', '5');
         this.Helper.WaitByIdAndFill('ARInvoiceLine_UnitPrice', '10');
         this.Helper.WaitByIdAndClick('ok-addArInvoiceline');
         this.Helper.WaitWindowClosed();
-        this.WaitBusyIndicatorToShowandHide();
+        this.Helper.WaitBusyIndicator();
+       
         this.Helper.WaitByIdAndClick('ARInvoice.B.SaveAsDraft');
+        
+        this.WaitBusyIndicatorToShowandHide();
         this.WaitBusyIndicatorToShowandHide();
         this.Helper.WaitByIdAndClick('ARInvoice.B.Approve');
-
-
-        this.WaitBusyIndicatorToShowandHide();
+        this.Helper.WaitBusyIndicator();
+       
 
 
 
