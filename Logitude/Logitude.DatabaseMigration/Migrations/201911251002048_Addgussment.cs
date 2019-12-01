b6cf3ebd6881337c70c165a6f7f85fb7770783de
Samar Abu Eid <Samar@logitudeworld.com>
2019-12-01 10:50:37 +0200

    fix migration

--- Logitude/Logitude.DatabaseMigration/Migrations/201911251002048_Addgussment.cs
+++ Logitude/Logitude.DatabaseMigration/Migrations/201911251002048_Addgussment.cs
@@ -7,14 +7,14 @@ namespace Logitude.DatabaseMigration.Migrations
     {
         public override void Up()
         {
-            AddColumn("dbo.QuoteSettings", "AutomaticallyCloseDays", c => c.Int(nullable: false));
+            //AddColumn("dbo.QuoteSettings", "AutomaticallyCloseDays", c => c.Int(nullable: false));
             AlterColumn("dbo.InterestBasesPeriods", "InterestRate", c => c.Decimal(nullable: false, precision: 4, scale: 2));
         }
         
         public override void Down()
         {
             AlterColumn("dbo.InterestBasesPeriods", "InterestRate", c => c.Decimal(nullable: false, precision: 2, scale: 2));
-            DropColumn("dbo.QuoteSettings", "AutomaticallyCloseDays");
+            //DropColumn("dbo.QuoteSettings", "AutomaticallyCloseDays");
         }
     }
 }
