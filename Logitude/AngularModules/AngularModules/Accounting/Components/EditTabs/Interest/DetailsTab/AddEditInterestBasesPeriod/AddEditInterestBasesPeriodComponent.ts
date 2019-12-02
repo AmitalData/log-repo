6f8bb5e7c3192e6c70337bfed3710fa22fd0f7d0
Mutaz Zuhairi <Mutaz@logitudeworld.com>
2019-12-02 12:14:50 +0200

    Related to 59998
    Related Work Items: #59998

--- Logitude/AngularModules/AngularModules/Accounting/Components/EditTabs/Interest/DetailsTab/AddEditInterestBasesPeriod/AddEditInterestBasesPeriodComponent.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/EditTabs/Interest/DetailsTab/AddEditInterestBasesPeriod/AddEditInterestBasesPeriodComponent.ts
@@ -26,26 +26,30 @@ export class AddEditInterestBasesPeriodComponent extends BaseComponent {
     SetDataContext(dataContext: InterestBasesPeriodItem) {
         this.DataContext = dataContext;
         this.EntityPM = dataContext.EntityPM;
-    }
+        this.EntityPM.CloneMe();
+        this.EntityPM.OldEntityPM = this.EntityPM;
+     }
 
     CancelButtonClicked() {
         this.CurrentSession.CloseCurrentWindow();
+        this.EntityPM.RejectChanges();
+        if (this.OldInterestBaseStartDate)
+        this.EntityPM.InterestBaseStartDate = this.OldInterestBaseStartDate;
     }
 
     OkButtonClicked() {
         var errors: string[] = [];
-        Validator.TryValidateObject(this.DataContext.EntityPM, this.DataContext.ObjectTableName, errors);
+        Validator.TryValidateObject(this.EntityPM, this.ObjectTableName, errors);
         this.ValidationErrorsList = errors;
-        if (this.DataContext.EntityPM.InterestRate && !this.CheckInterestRateValid(this.DataContext.EntityPM.InterestRate))
+        if (this.EntityPM.InterestRate && !this.CheckInterestRateValid(this.EntityPM.InterestRate))
             this.ValidationErrorsList.push(TextCodeTranslator.Translate("Accounting.General.O.Theratepercentageshouldbeformattedas00.00"));
-        if (this.DataContext.EntityPM.InterestBaseStartDate && !this.CheckInterestBaseStartDateExist(this.DataContext.EntityPM.InterestBaseStartDate, this.DataContext.EntityPM.CreateDate))
+        if (this.EntityPM.InterestBaseStartDate && !this.CheckInterestBaseStartDateExist(this.EntityPM.InterestBaseStartDate, this.DataContext.EntityPM.CreateDate))
             this.ValidationErrorsList.push(TextCodeTranslator.Translate("Accounting.General.O.AbaseperiodwiththesamestartdateisalreadyAdded"));
         if (this.ValidationErrorsList.length == 0) {
             if (this.DataContext.IsNewEntity)
                 if (this.DataContext.InterestBasesTypePM.InterestBasesPeriods.indexOf(this.EntityPM) == -1) {
-                    this.DataContext.CreateDate = DateTool.GetCurrentDateTimeAsUtc();
-                    this.DataContext.fatherComponent.InterestBasesPeriodsList.Insert(this.DataContext);
                     this.EntityPM.CreateDate = DateTool.GetCurrentDateTimeAsUtc();
+                    this.DataContext.fatherComponent.InterestBasesPeriodsList.Insert(this.DataContext);
                     this.DataContext.InterestBasesTypePM.AddInterestBasesPeriod(this.EntityPM);
                     this.DataContext.fatherComponent.BuildData();
                 }
@@ -74,6 +78,5 @@ export class AddEditInterestBasesPeriodComponent extends BaseComponent {
         }
         return true;
     }
-
-
+    OldInterestBaseStartDate: Date;
 }
