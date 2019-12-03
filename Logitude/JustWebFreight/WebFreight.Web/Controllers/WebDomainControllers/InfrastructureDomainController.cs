0dc7e05fbc7736b80f8b4d7b649b2457e373ffa5
Wesam <Wesam@WESAM>
2019-12-03 12:31:26 +0200

    BI reports history: Last Run Date & User
    Related Work Items: #59258

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/WebDomainControllers/InfrastructureDomainController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/WebDomainControllers/InfrastructureDomainController.cs
@@ -1837,9 +1837,11 @@ namespace WebFreight.Web.Controllers.WebDomainControllers
                 SecurityUtility.AuthenticationOnTenant(authToken.Tenant);
                 var ColumnsXML = LogitudeXmlSerializer.SerializeObjectToXmlString(QueryData.BITabularViewSettings);
 
+                ICommonDataContext commonContext = CommonDataContext.GetContext(authToken.Tenant);
                 IInfrastructureContext objectContext = InfrastructureContext.GetContext(authToken.Tenant);
                 BIReportRepository repository = new BIReportRepository(objectContext);
                 BIReportXMLData QueryData_Updated = new BIReportXMLData();
+                UserService userService = new UserService(commonContext, authToken.Tenant);
 
                 var entityPM = QueryData.BIReportPM;
                 var entityPOCO = repository.GetSingle(entityPM.Id, entityPM.Tenant);
@@ -1848,8 +1850,12 @@ namespace WebFreight.Web.Controllers.WebDomainControllers
                     entityPM.AGGridOptionsXML = ColumnsXML;
                     entityPM.ChangeSetOp = Simplog.Server.Infrastructure.ChangeSetOperation.Update;
                     entityPOCO.AGGridOptionsXML = entityPM.AGGridOptionsXML;
-                    entityPOCO.LastRunDate = entityPM.LastRunDate;
-                    entityPOCO.LastRunByUserId = entityPM.LastRunByUserId;
+                    bool isCustomerCare = userService.CheckIsUserCustomerCareById(entityPM.LastRunByUserId);
+                    if (!isCustomerCare)
+                    {
+                        entityPOCO.LastRunDate = entityPM.LastRunDate;
+                        entityPOCO.LastRunByUserId = entityPM.LastRunByUserId;
+                    }
                     repository.Update(entityPOCO);
                     repository.SubmitChanges();
 
