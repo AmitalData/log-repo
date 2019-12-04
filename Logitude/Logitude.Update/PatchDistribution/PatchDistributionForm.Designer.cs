758f0e81c78f016c897a430f26b3881dca2c048f
logitudo <logitudo@live.com>
2019-12-04 09:16:16 +0200

    form patch

---
+++ Logitude/Logitude.Update/PatchDistribution/PatchDistributionForm.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Logitude.Update.PatchDistribution
+{
+    partial class PatchDistributionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this._TBTenant = new System.Windows.Forms.TextBox();
+            this.textBoxLogger = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.doItToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(150, 6);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(65, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "LogSQL";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tenant";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.checkBox1);
+            this.splitContainer1.Panel1.Controls.Add(this._TBTenant);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.textBoxLogger);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 426);
+            this.splitContainer1.SplitterDistance = 68;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // _TBTenant
+            // 
+            this._TBTenant.Location = new System.Drawing.Point(72, 4);
+            this._TBTenant.Name = "_TBTenant";
+            this._TBTenant.Size = new System.Drawing.Size(72, 20);
+            this._TBTenant.TabIndex = 1;
+            this._TBTenant.Text = "1";
+            // 
+            // textBoxLogger
+            // 
+            this.textBoxLogger.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxLogger.Location = new System.Drawing.Point(0, 0);
+            this.textBoxLogger.Multiline = true;
+            this.textBoxLogger.Name = "textBoxLogger";
+            this.textBoxLogger.Size = new System.Drawing.Size(800, 354);
+            this.textBoxLogger.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.doItToolStripMenuItem});
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.startToolStripMenuItem.Text = "Start";
+            // 
+            // doItToolStripMenuItem
+            // 
+            this.doItToolStripMenuItem.Name = "doItToolStripMenuItem";
+            this.doItToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.doItToolStripMenuItem.Text = "DoIt";
+            this.doItToolStripMenuItem.Click += new System.EventHandler(this.doItToolStripMenuItem_Click);
+            // 
+            // PatchDistributionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "PatchDistributionForm";
+            this.Text = "PatchDistributionForm";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox _TBTenant;
+        private System.Windows.Forms.TextBox textBoxLogger;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem doItToolStripMenuItem;
+    }
+}
---
+++ Logitude/Logitude.Update/PatchDistribution/PatchDistributionForm.cs
@@ -0,0 +1,84 @@
+﻿using Devart.Data.Oracle;
+using Logitude.Customs.BL.PatchDistribution;
+using Logitude.Customs.BL.PatchDistribution.Patches;
+using Logitude.Customs.Data;
+using Simplog.Server.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Logitude.Update.PatchDistribution
+{
+    public partial class PatchDistributionForm : Form
+    {
+        public PatchDistributionForm()
+        {
+            InitializeComponent();
+            //DbContextBaseUtil.ToLog = checkBox1.Checked = true;
+            TraceListener debugListener = new MyTraceListener(this.textBoxLogger);
+            Debug.Listeners.Add(debugListener);
+            var myP19R03_0000_PatchDist = new P19R03_0000_PatchDist( );
+            myP19R03_0000_PatchDist.CreateSeedDbMigrateTable();
+        }
+
+       
+
+        private void doItToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+      
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            DbContextBaseUtil.ToLog = checkBox1.Checked;
+        }
+    }
+
+    public class MyTraceListener : TraceListener
+    {
+        private TextBoxBase output;
+
+        public MyTraceListener(TextBoxBase output)
+        {
+            this.Name = "Trace";
+            this.output = output;
+        }
+
+
+        public override void Write(string message)
+        {
+
+            Action append = delegate ()
+            {
+                output.AppendText(string.Format("[{0}] ", DateTime.Now.ToString()));
+                output.AppendText(message);
+            };
+            if (output.InvokeRequired)
+            {
+                output.BeginInvoke(append);
+            }
+            else
+            {
+                append();
+            }
+
+        }
+
+        public override void WriteLine(string message)
+        {
+            Write(message + Environment.NewLine);
+        }
+    }
+
+}
