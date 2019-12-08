7691f05dd7c80bc5e8301e9538a7e12a13a54192
Ahmad Baker <ahmadb@logitudeworld.com>
2019-12-08 15:25:09 +0200

    update test cases

--- Logitude/AngularModules/AngularModules/protractor.conf.js
+++ Logitude/AngularModules/AngularModules/protractor.conf.js
@@ -170,6 +170,12 @@ exports.config = {
             browser.params.Login.Password = "12La34Na56!";
         }
 
+        else if (browser.params.Env == "test_staging1") {
+            browser.params.Link = "https://test.logitudeworld.com/staging";
+            browser.params.Login.Email = "protractor2@test.com";
+            browser.params.Login.Password = "!P123p456";
+        }
+
         //------------------------------------- Reporter --------------------------------
         if (browser.params.Team == "ayman") {
             jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
