aa5e28847a20c9c63d1db24ed3c026a1757722c6
Samar Abu Eid <Samar@logitudeworld.com>
2019-12-09 08:44:20 +0200

    60127
    Related Work Items: #60127

--- Logitude/Logitude.BL/InvoiceModel/APIDataContract/QueryService/APInvoiceQueryService.cs
+++ Logitude/Logitude.BL/InvoiceModel/APIDataContract/QueryService/APInvoiceQueryService.cs
@@ -382,6 +382,11 @@ namespace Logitude.BL.InvoiceModel.APIDataContract.ApiV1
                     {
                         if (!string.IsNullOrEmpty(chargesType.ContainerMeasurementId))
                         {
+                            if(!chargesType.IsPayable)
+                            {
+                                throw new ApplicationException(chargesType.Code + " should be marked as Payable");
+                            }
+
                             Simplog.Data.CommonDataModel.EntityPOCOs.Measurement measurement = measurementRepository.GetSingleMeasurement(chargesType.ContainerMeasurementId, tenant);
                             if (measurement != null)
                             {
