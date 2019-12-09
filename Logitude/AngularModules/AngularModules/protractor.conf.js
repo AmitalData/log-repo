dd455e75654e888d2e2590057cab63fe8839d533
Ahmad Baker <ahmadb@logitudeworld.com>
2019-12-09 10:21:01 +0200

    Add test live parameter

--- Logitude/AngularModules/AngularModules/protractor.conf.js
+++ Logitude/AngularModules/AngularModules/protractor.conf.js
@@ -182,6 +182,11 @@ exports.config = {
             browser.params.Login.Password = "!P123p456";
         }
 
+        else if (browser.params.Env == "test_live1") {
+            browser.params.Link = "https://test.logitudeworld.com/test";
+            browser.params.Login.Email = "protractor2@test.com";
+            browser.params.Login.Password = "!P123p456";
+        }
         //------------------------------------- Reporter --------------------------------
         if (browser.params.Team == "ayman") {
             jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
