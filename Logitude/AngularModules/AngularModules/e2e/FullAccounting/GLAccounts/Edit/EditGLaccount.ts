0ff165dee56885dea8b3f314555c6fa0a38ebece
Lana Daoud <Lana@logitudeworld.com>
2019-12-11 21:02:53 +0200

    EditGLaccount Fix - Lana

--- Logitude/AngularModules/AngularModules/e2e/FullAccounting/GLAccounts/Edit/EditGLaccount.ts
+++ Logitude/AngularModules/AngularModules/e2e/FullAccounting/GLAccounts/Edit/EditGLaccount.ts
@@ -16,19 +16,16 @@ export class EditGLAccount {
 
 
         this.Helper.WaitByCssAndClick_FromTagInsideList('.ListBoxItem', 0);
-        this.Helper.WaitBusyIndicator();
+        this.Helper.WaitEditComponentBusyIndicator();
         this.Helper.WaitByIdAndClick('GLAccount.TH.General');
-      //  this.Helper.WaitShowEditComponentBusyIndicator();      //  
-       // this.Helper.WaitEditComponentBusyIndicator();
-       // this.WaitBusyIndicatorToShowandHide();
-        this.Helper.WaitBusyIndicator();
+
         this.Helper.WaitByIdAndFill('GLAccount_LocalName', 'Updated Local Name')
-      //  this.Helper.WaitBusyIndicator();
+        this.WaitBusyIndicatorToShowandHide();
         this.Helper.ItemsVisibility('GLAccount-Save');
         this.Helper.ItemsPresent('GLAccount-Save');
         this.Helper.WaitByIdAndClick('GLAccount-Save');
-      //  this.WaitBusyIndicatorToShowandHide();
-     //   this.Helper.WaitEditComponentBusyIndicator();
+        //  this.WaitBusyIndicatorToShowandHide();
+        //   this.Helper.WaitEditComponentBusyIndicator();
         this.Helper.WaitBusyIndicator();
 
 
