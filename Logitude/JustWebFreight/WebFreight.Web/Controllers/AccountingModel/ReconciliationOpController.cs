e819c2f76402e2ba6d2b868a4d1555ebe17f2f67
Abdullah Dawoud <abdullah@logitudeworld.com>
2019-12-15 13:55:48 +0000

    Conflict resolutions for !201

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/AccountingModel/ReconciliationOpController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/AccountingModel/ReconciliationOpController.cs
@@ -254,17 +254,19 @@ tenant);
 
                 GenericCallBack callback = transactionQuery.GetReconciliationFilterCallBack(queryOperations, gLAccountId, tenant, true);
 
-                List<LedgerTransactionList> openTransactions = transactionQuery.GetOpenReconciliationFilterList(queryOperations, callback, gLAccountId, tenant);
 
-                LedgerTransactionSorterArgs args = new LedgerTransactionSorterArgs()
-                {
-                    Tenant = tenant,
-                    AccountId = gLAccountId,
-                    QueryOperations = queryOperations,
-                    Transactions = openTransactions,
-                };
-                LedgerTransactionsSorter transactionsSorter = new LedgerTransactionsSorter(args);
-                openTransactions = transactionsSorter.SortQuery();
+
+                List<LedgerTransactionList> openReconciliation = transactionQuery.GetOpenReconciliationFilterList(queryOperations, callback, gLAccountId, tenant);
+
+                //LedgerTransactionSorterArgs args = new LedgerTransactionSorterArgs()
+                //{
+                //    Tenant = tenant,
+                //    AccountId = gLAccountId,
+                //    QueryOperations = queryOperations,
+                //    Transactions = openTransactions,
+                //};
+                //LedgerTransactionsSorter transactionsSorter = new LedgerTransactionsSorter(args);
+                //openTransactions = transactionsSorter.SortQuery();
 
                 ServiceResponse response = new ServiceResponse();
                 if (filters.GetCount)
