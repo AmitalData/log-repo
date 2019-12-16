6c93c312a5aeb490268e9617194f5f3349df8030
mirit <Mirit@amital.co.il>
2019-12-16 14:57:40 +0200

    59201
    Related Work Items: #59201

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsCourier/Components/CourierWorkspaces/CourierDeclarationWorkspaceComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsCourier/Components/CourierWorkspaces/CourierDeclarationWorkspaceComponent.ts
@@ -258,9 +258,7 @@ export class CourierDeclarationWorkspaceComponent implements AfterViewInit {
 
     BuildFiltersForQuery(filters: ApiQueryFilters = null) {
 
-        if (filters == null) {
-            filters = new ApiQueryFilters();
-        }
+        filters = new ApiQueryFilters();
         //filters.addAdditionalFilter("CourierMasterId", this.entityPM.Id, null, null, "Equals", false, false, false, "string");
         filters.addAdditionalFilter("Tenant", SessionLocator.Tenant, null, null, "Equals", false, false, false, "number");
 
@@ -380,10 +378,7 @@ export class CourierDeclarationWorkspaceComponent implements AfterViewInit {
     filterAgrs: ApiQueryFilters;
     getRows(skip, take, sortingCol, sortingDir, getCount: boolean, searchfields?: string, filters: ApiQueryFilters = null) {
 
-        if (filters == null) {
-            filters = new ApiQueryFilters();
-        }
-
+        filters = new ApiQueryFilters();
         filters.PageSize = take;
         filters.PageIndex = skip;
         filters.GetAll = false;
