7bfb969b309f56727f99b476388e0c854d8f4ac5
Maheera Jazi <Maheera@logitudeworld.com>
2019-12-16 16:23:38 +0200

    fix

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/InfrastructureModel/Generated/PMControllers/PriceStepController.Generated.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/InfrastructureModel/Generated/PMControllers/PriceStepController.Generated.cs
@@ -47,7 +47,7 @@ namespace WebFreight.Web.App_Code.AngularJS_App_Code.Generated
 { 
 
     
-    public partial class PriceStepController : ApiController
+    public partial class PriceStepsController : ApiController
     {
 	  
        
