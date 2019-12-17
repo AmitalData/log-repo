95866e74f0486e12e0f5ce615978b1b6aa079242
elisheva <elisheva@amital.co.il>
2019-12-17 13:48:00 +0200

    merge

--- Logitude/Logitude.Customs.Data/EntityListQueryServices/CustomsCollateralsAnswerListQueryService.cs
+++ Logitude/Logitude.Customs.Data/EntityListQueryServices/CustomsCollateralsAnswerListQueryService.cs
@@ -13,81 +13,42 @@ using System.Xml.Serialization;
 
 using Logitude.Customs.Data.EntityPOCOs;
 using Logitude.Customs.Data.EntityLists;
-using Simplog.Data.CommonDataModel.EntityPOCOs;
-using Logitude.Customs.Data.Utils;
 
 namespace Logitude.Customs.Data.EntityListQueryServices
 {
 
-    public partial class CustomsCollateralListQueryService
+    public partial class CustomsCollateralsAnswerListQueryService
     {
-        private IQueryable<CustomsCollateralList> GetIqueryableList(IQueryable<CustomsCollateral> iQueryable)
+        private IQueryable<CustomsCollateralsAnswerList> GetIqueryableList(IQueryable<CustomsCollateralsAnswer> iQueryable)
         {
-            CustomsCollateralsAnswerListQueryService customsCollateralsAnswerListQueryService = new CustomsCollateralsAnswerListQueryService(context);
+            IQueryable<CustomsCollateralsAnswerList> query = (from a in iQueryable
+                                                              select new CustomsCollateralsAnswerList()
+                                                              {
 
-
-            IQueryable<CustomsCollateralList> query = (from a in iQueryable.Include("CollateralRequestStatus").Include("EntityTypeLookup").Include("RequestedCollateralType")
-                                                       select new CustomsCollateralList()
-                                                       {
-                                                           DeclarationId = a.DeclarationId,
-                                                           CollateralRequestNumber = a.CollateralRequestNumber,
-                                                           CollateralRequestStatusCode = a.CollateralRequestStatusCode,
-                                                           CollateralValidityDate = a.CollateralValidityDate,
-                                                           CustomsEntityTypeCode = a.CustomsEntityTypeCode,
-                                                           CustomsHouseTypeCode = a.CustomsHouseTypeCode,
-                                                           EntityIdKey1 = a.EntityIdKey1,
-                                                           EntityIdKey2 = a.EntityIdKey2,
-                                                           EntityIdKey3 = a.EntityIdKey3,
-                                                           FileNo = a.FileNo,
-                                                           Id = a.Id,
-                                                           OrganizationUnitTypeCode = a.OrganizationUnitTypeCode,
-                                                           Remarks = a.Remarks,
-                                                           RequestedCollateralTypeCode = a.RequestedCollateralTypeCode,
-                                                           RequestValidityDate = a.RequestValidityDate,
-                                                           WorkerName = a.WorkerName,
-                                                           Tenant = a.Tenant,
-                                                           IncludingThirdPartyGuarantee = a.IncludingThirdPartyGuarantee,
-                                                           SearchFields = a.SearchFields,
-                                                           CollateralRequestStatusName = a.CollateralRequestStatus.LocalName,
-                                                           CustomsEntityTypeName = a.EntityTypeLookup.LocalName,
-                                                           RequestedCollateralTypeName = a.RequestedCollateralType.LocalName,
-                                                           OrganizationUnitTypeName = a.OrganizationUnitType.LocalName,
-                                                           CustomsHouseTypeName = a.CustomsHouseType.LocalName,
-                                                           IsClosed = a.IsClosed,
-                                                           CreateDateTime = a.CreateDateTime,
-                                                           CustomerName = a.Customer != null ? a.Customer.Card.LocalName : null,
-                                                           //IsAnswer= test.Where(x=>x.CustomsCollateralId== a.CollateralRequestNumber).Any()
-                                                       });
-
-            if (query.Count() > 0)
-            {
-                var query2 = query.ToList();
-
-                var customsCollateralsAnswerList = customsCollateralsAnswerListQueryService.GetList(query.First().Tenant);
-
-                foreach (var item in query2)
-                {
-                    item.IsAnswer = customsCollateralsAnswerList.Where(x => x.CustomsCollateralId == item.Id).Any();
-                }
-                query = query2.AsQueryable();
-            }
+                                                                  Remarks = a.Remarks,
+                                                                  AllocatedAmount = a.AllocatedAmount,
+                                                                  AnswerEntityTypeCode = a.AnswerEntityTypeCode,
+                                                                  AnswerForCollateralStatusCode = a.AnswerForCollateralStatusCode,
+                                                                  CustomsCollateralId = a.CustomsCollateralId,
+                                                                  CustomsNumeral = a.CustomsNumeral,
+                                                                  CustomsTapgFile = a.CustomsTapgFile,
+                                                                  Errors = a.Errors,
+                                                                  LineNumber = a.LineNumber,
+                                                                  Tenant = a.Tenant,
+                                                                  TapagId = a.TapagId,
+                                                                  RequestFileAmount = a.RequestFileAmount,
+                                                                  NewFileRequest = a.NewFileRequest,
+                                                                  RequestFileTypeCode = a.RequestFileTypeCode,
+                                                                  IsClosed = a.IsClosed,
+                                                                  RequestedTapagNumeral = a.RequestedTapagNumeral
+                                                              });
             return query;
         }
 
-        private IQueryable<CustomsCollateral> ApplyCustomFilters(QueryOperations queryOperations, IQueryable<CustomsCollateral> iQueryable, int tenant)
+        private IQueryable<CustomsCollateralsAnswer> ApplyCustomFilters(QueryOperations queryOperations, IQueryable<CustomsCollateralsAnswer> iQueryable, int tenant)
         {
-            // Freelancer filterting
-            FreelancerCustomersUtil frlUtil = new FreelancerCustomersUtil(tenant);
-            if (frlUtil.user.IsFreelancer)
-            {
-                List<string> customersIds = frlUtil.GetConnectedCustomersIds(tenant);
-                if (customersIds.Count > 0)
-                {
-                    iQueryable = iQueryable.Where(d => customersIds.Contains(d.CustomerId));
-                }
-            }
-
             return iQueryable;
+
         }
     }
 
