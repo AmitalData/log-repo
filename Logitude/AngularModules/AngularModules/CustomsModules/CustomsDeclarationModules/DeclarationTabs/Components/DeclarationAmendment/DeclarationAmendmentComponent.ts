c0c81bf2c9da528156984dd5bf97bcfa3271d8fa
elisheva <elisheva@amital.co.il>
2019-12-19 08:09:37 +0200

    Declaration Amendment

---
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationTabs/Components/DeclarationAmendment/DeclarationAmendmentComponent.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { BaseComponent } from "../../../../../Infrastructure/Components/LogitudeComponents/BaseComponent";
+
+@Component({
+    moduleId: module.id,
+    templateUrl: './DeclarationAmendmentComponent.html',
+})
+
+export class DeclarationAmendmentComponent extends BaseComponent   {
+}
