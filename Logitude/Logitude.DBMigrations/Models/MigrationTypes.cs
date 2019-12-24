ac5695692fd4219d40d232b481d3a01fd7f9329d
Abd Al-Rahman Malakh <Abd@logitudeworld.com>
2019-12-24 16:35:05 +0200

    60894
    Related Work Items: #60894

--- Logitude/Logitude.DBMigrations/Models/MigrationTypes.cs
+++ Logitude/Logitude.DBMigrations/Models/MigrationTypes.cs
@@ -18,7 +18,5 @@ namespace Logitude.DBMigrations.Models
         public const int SETNULLABLE = 7;
         public const int UNSETNULLABLE = 8;
         public const int ALTERPRECISIONANDSCALE = 9;
-        public const int ADDFOREIGNKEY = 10;
-        public const int DROPFOREIGNKEY = 11;
     }
 }
