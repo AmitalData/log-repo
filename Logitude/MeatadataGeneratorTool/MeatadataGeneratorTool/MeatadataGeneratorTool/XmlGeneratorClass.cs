96d33fe5af2a0e9c1564ec682f6796c464d70d2a
Islam Shaheen <islam@logitudeworld.com>
2019-12-24 08:12:46 +0200

    fix

--- Logitude/MeatadataGeneratorTool/MeatadataGeneratorTool/MeatadataGeneratorTool/XmlGeneratorClass.cs
+++ Logitude/MeatadataGeneratorTool/MeatadataGeneratorTool/MeatadataGeneratorTool/XmlGeneratorClass.cs
@@ -1835,7 +1835,7 @@ namespace MeatadataGeneratorTool
 
               
                 FileStream fileStream = new FileStream(App.DirectOpenPath, FileMode.Truncate, FileAccess.Write);
-                XmlWriterSettings settings = new XmlWriterSettings() { Indent = true, NewLineOnAttributes = true , OmitXmlDeclaration = true, WriteEndDocumentOnClose = true };//, WriteEndDocumentOnClose = true, OmitXmlDeclaration = true
+                XmlWriterSettings settings = new XmlWriterSettings() { Indent = true, NewLineOnAttributes = true , OmitXmlDeclaration = false, WriteEndDocumentOnClose = false };//, WriteEndDocumentOnClose = true, OmitXmlDeclaration = true
                 XmlWriter xmlWriter = XmlWriter.Create(fileStream, settings);
                  
                 doc.Save(xmlWriter);
