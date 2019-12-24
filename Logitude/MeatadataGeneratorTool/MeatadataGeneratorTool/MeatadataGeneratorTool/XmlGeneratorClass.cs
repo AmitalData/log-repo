4b4e86e16dba6d8ef7ecc28fa874154415766df4
Islam Shaheen <islam@logitudeworld.com>
2019-12-24 12:07:37 +0000

    Conflict resolutions for !417

--- Logitude/MeatadataGeneratorTool/MeatadataGeneratorTool/MeatadataGeneratorTool/XmlGeneratorClass.cs
+++ Logitude/MeatadataGeneratorTool/MeatadataGeneratorTool/MeatadataGeneratorTool/XmlGeneratorClass.cs
@@ -1833,16 +1833,15 @@ namespace MeatadataGeneratorTool
             if (!string.IsNullOrEmpty(App.DirectOpenPath))
             {
 
+              
                 FileStream fileStream = new FileStream(App.DirectOpenPath, FileMode.Truncate, FileAccess.Write);
-                XmlWriterSettings settings = new XmlWriterSettings() { Indent = true, NewLineOnAttributes = true, OmitXmlDeclaration = false, WriteEndDocumentOnClose = false };//, WriteEndDocumentOnClose = true, OmitXmlDeclaration = true
+                XmlWriterSettings settings = new XmlWriterSettings() { Indent = true, NewLineOnAttributes = true , OmitXmlDeclaration = false, WriteEndDocumentOnClose = false };//, WriteEndDocumentOnClose = true, OmitXmlDeclaration = true
                 XmlWriter xmlWriter = XmlWriter.Create(fileStream, settings);
-
+                 
                 doc.Save(xmlWriter);
                 xmlWriter.Close();
                 xmlWriter.Dispose();
                 //doc.Save(App.DirectOpenPath);
-
-
 
             }
             else
