ae93a8085cd762d29ac3a592d448cf1dae14517d
itzik <itzik@amital.co.il>
2019-12-25 16:40:16 +0200

    Revert "'ChangeOSVersion:1.D.19.03.6'"
    This reverts commit 8eb8ecf789b62de3b482ba1001087a9d38adde0e.

--- Logitude/AmitalCustomsWindowsService/Properties/AssemblyInfo.cs
+++ Logitude/AmitalCustomsWindowsService/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.D.19.03.6")]
+[assembly: AssemblyFileVersion("1.P.19.03.0")]
