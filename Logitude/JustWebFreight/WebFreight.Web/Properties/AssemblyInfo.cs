0f101f2c4d076d4af16a906deb2333d1ac72863d
itzik M <itzik@amital.co.il>
2019-12-25 15:58:44 +0000

    Conflict resolutions for !433

--- Logitude/JustWebFreight/WebFreight.Web/Properties/AssemblyInfo.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Properties/AssemblyInfo.cs
@@ -32,4 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< 19R03P
 [assembly: AssemblyFileVersion("1.P.19.03.6")]
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 19R03D
--- Logitude/Unifreight.Data/Properties/AssemblyInfo.cs
+++ Logitude/Unifreight.Data/Properties/AssemblyInfo.cs
@@ -33,4 +33,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< 19R03P
 [assembly: AssemblyFileVersion("1.P.19.03.6")]
+=======================================================================
