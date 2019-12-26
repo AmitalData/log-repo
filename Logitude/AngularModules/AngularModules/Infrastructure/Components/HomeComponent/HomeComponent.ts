430c2ceae5febae9090c93e839fc0f557d912c87
itzik <itzik@amital.co.il>
2019-12-26 10:22:55 +0200

    60649 תשתית לניהול הפצת תיקונים
    Related Work Items: #60649

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/HomeComponent/HomeComponent.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/HomeComponent/HomeComponent.ts
@@ -150,13 +150,6 @@ export class HomeComponent implements OnDestroy{
         this.Company = SessionLocator.TenantPM.Company;
         if (!AppTool.IsNullOrEmpty(ObjectsLocator.GlobalSetting) && !AppTool.IsNullOrEmpty(ObjectsLocator.GlobalSetting.ProductInfo)) {
             this.ProductInfo = ObjectsLocator.GlobalSetting.ProductInfo;
-            if (!AppTool.IsNullOrEmpty(ObjectsLocator.GlobalSetting.ProductMessage)) {
-                let myMessageWindow = new MessageWindow();
-                myMessageWindow.ShowErrorIcon = true;
-                myMessageWindow.Title = "Please Call Amital";
-                myMessageWindow.Show(ObjectsLocator.GlobalSetting.ProductMessage);
-
-            }
         } else {
             this.ProductInfo = "";
         }
@@ -807,7 +800,18 @@ export class HomeComponent implements OnDestroy{
                                     });
                                 }
 
-                                else{
+                                else {
+                                    if (!AppTool.IsNullOrEmpty(ObjectsLocator.GlobalSetting.ProductMessage)) {
+                                        cmpRef.instance.SessionInitialize.subscribe(s => {
+                                            let myMessageWindow = new MessageWindow();
+                                            myMessageWindow.ShowErrorIcon = true;
+                                            myMessageWindow.Title = "Please Call Amital";
+                                            myMessageWindow.Show(ObjectsLocator.GlobalSetting.ProductMessage);
+
+                                        });
+
+                                    }
+
                                     this.AccountAndTenantExpiration();
                                 }
                             }                            
