3ba2b4d242a1329a585065dd4978b9fc95e5e4cb
itzik <itzik@amital.co.il>
2019-12-26 08:52:07 +0200

    conflict  - merge problem

--- Logitude/Unifreight.Data/Properties/AssemblyInfo.cs
+++ Logitude/Unifreight.Data/Properties/AssemblyInfo.cs
@@ -33,6 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< 19R03P
-[assembly: AssemblyFileVersion("1.P.19.03.6")]
-=======================================================================
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
