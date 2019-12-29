e3f57392d34e4679464f906c4d871bac5d35c541
itzik <itzik@amital.co.il>
2019-12-29 08:15:05 +0200

    60649 תשתית לניהול הפצת תיקונים
    Related Work Items: #60649

--- Logitude/JustWebFreight/WebFreight.Web/Global.asax.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Global.asax.cs
@@ -379,6 +379,10 @@ namespace WebFreight.Web
                     var myP19R03_0000_PatchDist = new P19R03_0001_PatchDist();
                     myP19R03_0000_PatchDist.Enshure_SeedDbMigrateTable();
 
+                    if (!LogitudeSettings.IsCostomsDeploy)
+                    {
+                        return;
+                    }
 
                     bool supressAlertProductMessage = !String.IsNullOrWhiteSpace( System.Configuration.ConfigurationManager.AppSettings.Get("supressAlertProductMessage"));
                     if (!supressAlertProductMessage)
@@ -386,6 +390,10 @@ namespace WebFreight.Web
                         var assemblyVersion = assemblyUtil.GetVersion(LogitudeSettings.ProductInfo);
                         var patchDistributionMatch = new PatchDistributionMatch();
                         var patchDistributionMatchModel = patchDistributionMatch.GetPatchDistributionMatchModel(assemblyVersion);
+                        if (patchDistributionMatchModel.MyAssemblyDBMigrationModel== null)
+                        {
+                            return;
+                        }
                         if (patchDistributionMatchModel.MajorVersionMatch == PatchDistributionMatch.MajorVersionMatchEnum.OldDB ||
                             patchDistributionMatchModel.MajorVersionMatch == PatchDistributionMatch.MajorVersionMatchEnum.OldSource)
                         {
