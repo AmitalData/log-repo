0155e2f62e8f4880d2fcaa3d003f1b094fbc3775
Lana Daoud <lana@logitudeworld.com>
2019-12-29 09:55:33 +0000

    Conflict resolutions for !457

--- Logitude/AngularModules/AngularModules/CRMModules/CRMOccasion/Components/AddEdit/AddEditOccasionContactComponent.ts
+++ Logitude/AngularModules/AngularModules/CRMModules/CRMOccasion/Components/AddEdit/AddEditOccasionContactComponent.ts
@@ -33,14 +33,15 @@ export class AddEditOccasionContactComponent extends BaseComponent implements On
     @Output() SearchFieldChangeEvent = new EventEmitter();
     private selectedItems: ObservableCollection;
     private selectedItemsCount: number = 0;
+    private timerToken: any;
     private IsSavedAll: boolean = false;
     private itemsCount;
-
     constructor() {
         super();
         this.crmService = new CRMDomainService();
         this.selectedItems = new ObservableCollection([]);
 
+        this.RunComponentTimer();
         this.Listen();
     }
 
@@ -56,6 +57,12 @@ export class AddEditOccasionContactComponent extends BaseComponent implements On
             }
             this.OnLinesSelected();
         });
+    }
+
+    private RunComponentTimer() {
+        if (this.timerToken) {
+            clearTimeout(this.timerToken);
+        }
     }
 
     ngOnDestroy() {
@@ -348,11 +355,16 @@ export class AddEditOccasionContactComponent extends BaseComponent implements On
  
         }
     }
+    private searchText: string;
+    get SearchText() { return this.searchText; }
+    set SearchText(value: string) {
+        if (this.searchText != value) {
+            this.searchText = value;
+        }
+    }
 
-    public SearchText: string = null;
-    SearchMethod(text: string) {
+    OnSearchTextChangeEvent(text: string) {        
         this.SearchText = text;
-
         this.BrowseClicked();
     }
     
