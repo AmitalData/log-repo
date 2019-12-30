b18dfb661b2cd59303fa752382c8b2e149f273ae
Bisan AbdulJawwad <Bisan@logitudeworld.com>
2019-12-30 12:09:31 +0200

    61160
    Related Work Items: #61160

--- Logitude/AngularModules/AngularModules/TariffModule/Components/Workspaces/CostWorkspaceComponent.ts
+++ Logitude/AngularModules/AngularModules/TariffModule/Components/Workspaces/CostWorkspaceComponent.ts
@@ -154,17 +154,17 @@ export class CostWorkspaceComponent implements OnInit {
                 break;
             }
             case "OSC": {
-                windowTitle = "New " + TextCodeTranslator.Translate("Tariff.Q.Ocean.LCL.Surcharges.Cost");
+                windowTitle = "New " + TextCodeTranslator.Translate("Tariff.Q.Ocean.LCL.Surcharges.Cost") + " Surcharges Cost";
                 typeCode = "OSC";
                 break;
             }
             case "OFC": {
-                windowTitle = "New " + TextCodeTranslator.Translate("Tariff.Q.OceanFCLFreightCost");
+                windowTitle = "New " + TextCodeTranslator.Translate("Tariff.Q.OceanFCLFreightCost") + " Freight Cost";
                 typeCode = "OFC";
                 break;
             }
             case "OFS": {
-                windowTitle = "New " + TextCodeTranslator.Translate("Tariff.Q.OceanFCLSurchargesCost");
+                windowTitle = "New " + TextCodeTranslator.Translate("Tariff.Q.OceanFCLSurchargesCost") + " Surcharges Cost";
                 typeCode = "OFS";
                 break;
             }
