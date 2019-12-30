c4de2d5d90fe6535a58945db0aabd72cad2d2035
alaa <alaa.jamal.h@live.com>
2019-12-30 11:35:36 +0200

    Fix Maigration

--- Logitude/Logitude.DatabaseMigration/Migrations/201912221352074_Mutaz.cs
+++ Logitude/Logitude.DatabaseMigration/Migrations/201912221352074_Mutaz.cs
@@ -7,8 +7,8 @@ namespace Logitude.DatabaseMigration.Migrations
     {
         public override void Up()
         {
-            Sql("IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[DF__GLAccount__Minim__43836F81]') AND type = 'D') BEGIN ALTER TABLE[dbo].[GLAccounts] DROP CONSTRAINT[DF__GLAccount__Minim__43836F81] END");
-            AlterColumn("dbo.GLAccounts", "MinimumInterestInvoiceBilling", c => c.Int());
+            //Sql("IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[DF__GLAccount__Minim__43836F81]') AND type = 'D') BEGIN ALTER TABLE[dbo].[GLAccounts] DROP CONSTRAINT[DF__GLAccount__Minim__43836F81] END");
+            //AlterColumn("dbo.GLAccounts", "MinimumInterestInvoiceBilling", c => c.Int());
 
         }
 
