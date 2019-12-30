d32fc06297963dd0afc3438cb1cd4b48bbea48b0
mirit <Mirit@amital.co.il>
2019-12-30 14:29:02 +0200

    60589
    Related Work Items: #60589

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsClaim/Components/EditTabs/General/ClaimGeneralTabComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsClaim/Components/EditTabs/General/ClaimGeneralTabComponent.ts
@@ -603,7 +603,7 @@ export class ClaimGeneralTabComponent extends BaseComponent {
                 this.IsClientPassportEnabled = false;
                 this.UIProperties.SetEnabled("ClientId", "Customs.Claim", true);
             }
-            //this.RefreshEntity(); 
+            this.BuildClienAddressesList();
         }
 
     }
