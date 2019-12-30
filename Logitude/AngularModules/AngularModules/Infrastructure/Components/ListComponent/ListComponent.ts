95d5a5d017e7547ae9810441bc5c1b68332fb208
Mutaz Zuhairi <Mutaz@logitudeworld.com>
2019-12-30 12:16:19 +0200

    Related to 61272
    Related Work Items: #61272

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/ListComponent/ListComponent.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/ListComponent/ListComponent.ts
@@ -2561,7 +2561,7 @@ else{ this.View = TextCodeTranslator.Translate("General.O.View");}
                     }
                 case "InterestReport":
                     {
-                        logWindow.Width = 420;
+                        logWindow.Width = 320;
                         logWindow.Height = 200;
                         break;
                     }
