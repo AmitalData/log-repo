e6977fe892301aaf3d961a00921ab6cd580504b7
Mutaz Zuhairi <Mutaz@logitudeworld.com>
2019-12-30 10:44:57 +0200

    Related to 60798
    Related Work Items: #60798

--- Logitude/Logitude.Accounting.BL/EntityUpdateServices/InterestReportUpdateService.cs
+++ Logitude/Logitude.Accounting.BL/EntityUpdateServices/InterestReportUpdateService.cs
@@ -3,14 +3,19 @@ using Logitude.Accounting.Data.Repositories;
 using Logitude.Accounting.Def.EntityPMs;
 using Logitude.BL.CommonDataModel.EntityPMs;
 using Logitude.BL.Resolvers;
+using Logitude.Infrastructure.BL.EntityPMs;
+using Logitude.Infrastructure.BL.EntityUpdateServices;
+using Logitude.Infrastructure.Data;
 using Logitude.Server.Tools;
 using Logitude.Server.Tools.Counters;
 using Logitude.Server.Tools.Helpers;
+using Logitude.Server.Tools.QueueService;
 using Simplog.Data.CommonDataModel.EntityPOCOs;
 using Simplog.Data.CommonDataModel.Repositories;
 using Simplog.Server.Infrastructure;
 using System;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 
 namespace Logitude.Accounting.BL.EntityUpdateServices
 {
@@ -30,6 +35,7 @@ namespace Logitude.Accounting.BL.EntityUpdateServices
             {
                 throw new Exception(TextCodesTranslator.TranslateText("InterestReport.O.Customerisnotdefined", entityPM.Tenant, showLocals));
             }
+            createBTE(entityPM);
         }
 
         protected override void OnUpdating(InterestReportPM entityPM, InterestReport entityPOCO)
@@ -48,5 +54,51 @@ namespace Logitude.Accounting.BL.EntityUpdateServices
             ContactPM loggedcontact = LoggedContactResolver.GetLoggedContact(tenant);
             return loggedcontact;
         }
+
+        private void createBTE(InterestReportPM entityPM)
+        {
+            // 1- create BTE record
+            BatchTaskExecutionPM taskExe;
+
+            InterestReportArgs args = new InterestReportArgs() { Tenant = entityPM.Tenant, InterestReportId = entityPM.Id};
+            var stringwriter = new System.IO.StringWriter();
+            var serializer = new XmlSerializer(typeof(InterestReportArgs));
+            serializer.Serialize(stringwriter, args);
+            string xmlParameters = stringwriter.ToString();
+
+
+            taskExe = new BatchTaskExecutionPM()
+            {
+                Subject = "Create Interest Report",
+                Tenant = entityPM.Tenant,
+                ChangeSetOp = ChangeSetOperation.Insert,
+                ClassName = "Logitude.Accounting.BL.CoreBL.Batch.BatchInterestReportService,Logitude.Accounting.BL",
+                CreateDate = DateTime.Now,
+                PrametersXml = xmlParameters,
+                StatusCode = "C",
+
+            };
+
+
+            IInfrastructureContext MyContext = InfrastructureContext.GetContext(entityPM.Tenant);
+            BatchTaskExecutionUpdateService bteUpdateService = new BatchTaskExecutionUpdateService(MyContext, new Dictionary<string, IContext>(), entityPM.Tenant);
+            bteUpdateService.Update(taskExe, true);
+
+            // 2- Send to queue
+            IQueueService queueservice = new DbQueueService();
+            queueservice.InitializeQueue("batchtaskexecutionqueue", 0);
+            queueservice.Send(new Dictionary<string, string>()
+                {
+                    { "BatchTaskExecutionId", taskExe.Id },
+                    { "Tenant", entityPM.Tenant.ToString() }
+                });
+        }
+
+
     }
+}
+public class InterestReportArgs
+{
+    public string InterestReportId { get; set; }
+    public int Tenant { get; set; }
 }
