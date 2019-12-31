f69be6d6cb9527ab93cfc9d125dcb34f8f4cf358
Razan Jararah <razan@logitudeworld.com>
2019-12-31 16:04:56 +0200

    #61408

---
+++ Logitude/Logitude.IntegrationTest.Shipment/Initialize.cs
@@ -0,0 +1,29 @@
+﻿using Logitude.IntegrationTest.Core;
+using Logitude.IntegrationTest.Core.Login;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logitude.IntegrationTest.Shipmen
+{
+    [TestClass]
+    public class Initialize
+    {
+        [AssemblyInitialize]
+        public static async Task AssemblyInitialize(TestContext context)
+        {
+            await LoginService.GetLoginTokenByUserEmailAndTenant();
+        }
+
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+            Console.WriteLine("AssemblyCleanup");
+        }
+    }
+}
+
+
