61436e498fc694e79b128976d56c98c863935069
itzik <itzik@amital.co.il>
2019-12-31 17:05:36 +0200

    Revert "Added dynamic function -  FilterTabs"
    This reverts commit f2cad836b69ba493047940c93fee63547d2b0fb2.

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/EditComponent/EditComponent.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/EditComponent/EditComponent.ts
@@ -691,7 +691,8 @@ export class EditComponent implements OnDestroy {
         allTabs = window.ObjectTableTabs.filter(d => d.ObjectTableId === this.ObjectTableId);
         allTabs = this.FilterTabs(allTabs);
         allTabs = allTabs.sort((a, b) => { return a.IndexOrder - b.IndexOrder });
-         for (var i = 0; i < allTabs.length; i++) {
+
+        for (var i = 0; i < allTabs.length; i++) {
             var tab = allTabs[i];
 
             if (tab.ControlPath != null) {
@@ -708,8 +709,6 @@ export class EditComponent implements OnDestroy {
                         continue;
                     }
                 }
-
-
 
 
                 //if (tab.ControlPath.indexOf("WarehouseConnectionsTabComponent") != -1) {
@@ -888,7 +887,16 @@ export class EditComponent implements OnDestroy {
                 break;
             }
 
-          
+            case "Customs.Declaration": {
+                //CustomsSettingList customsSetting = DataProvider.GetCachedList<CustomsSettingList>("Customs.CustomsSetting").FirstOrDefault();
+                //if (customsSetting != null) {
+                //    if (customsSetting.IsConnectedToUniFreight) {
+                //        tabItem = objectTableTabs.Where(t => t.Code == "DCMF").FirstOrDefault();
+                //        objectTableTabs.Remove(tabItem);
+                //    }
+                //}
+                break;
+            }
             case "ARPayment": {
 
                 if (this.EntityPM.IsFullAccounting) {
@@ -904,10 +912,7 @@ export class EditComponent implements OnDestroy {
                 break;
             }
         }
-        this.EditComponentController.FilterTabs(allTabs);
-          
-          
-          
+
         return allTabs;
     }
     private OnEntityCreated() {
@@ -1819,10 +1824,6 @@ export class EditComponentDefaultController implements IEditComponentController 
     IsDisabled(itemTabCode: string): boolean {
         return false;
     }
-    FilterTabs(allTabs: any[]) {
-
-    }
-
 }
 export interface IEditComponentController {
     OnFirstTimeAfterSingleDataLoaded(CurrentEntity): Promise<boolean>;
@@ -1837,6 +1838,5 @@ export interface IEditComponentController {
     IsInBatchRequest: boolean;
     ResetMustRefresh(): void;
     IsDisabled(itemTabCode: string): boolean;
-    FilterTabs(allTabs: any[]);
 }
 
