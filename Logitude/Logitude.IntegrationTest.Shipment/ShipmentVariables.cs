37a5d1f4d15c51171f54bc6a42ccf1cd1688865a
Razan Jararah <razan@logitudeworld.com>
2020-01-02 14:03:14 +0200

    #61408

---
+++ Logitude/Logitude.IntegrationTest.Shipment/ShipmentVariables.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logitude.IntegrationTest.Shipment
+{
+    public class ShipmentVariables
+    {
+        public static string CurrencyEURId { get; set; }
+        public static string IncotermLDEId { get; set; }
+        public static string MeasurmentGRWTId { get; set; }
+        public static string ChargeGroupCOMMId { get; set; }
+        public static string ChargeGroupCOMMCode { get; set; }
+        public static string ChargeTypeAFTId { get; set; }
+        public static string PortLHRId { get; set; }
+        public static string PortLASId { get; set; }
+        public static string PortMIAId { get; set; }
+        public static string PortJFKId { get; set; }
+        public static string PortSOUId { get; set; }
+        public static string PortNYCId { get; set; }
+        public static string PortLONId { get; set; }
+        public static string PortMANId { get; set; }
+        public static string GlobalZoneEUId { get; set; }
+        public static string CountryGBId { get; set; }
+        public static string CountryUSId { get; set; }
+        public static string StateAKId { get; set; }
+
+
+
+    }
+}
