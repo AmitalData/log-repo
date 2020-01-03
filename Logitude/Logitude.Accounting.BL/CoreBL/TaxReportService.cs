5a618c47d6077ccb44d447cd9cfc9fa316d3d2d2
alaa <alaa.jamal.h@live.com>
2020-01-03 20:35:42 +0200

    61323
    Related Work Items: #61323

--- Logitude/Logitude.Accounting.BL/CoreBL/TaxReportService.cs
+++ Logitude/Logitude.Accounting.BL/CoreBL/TaxReportService.cs
@@ -372,12 +372,12 @@ namespace Logitude.Accounting.BL.CoreBL
             VatNumber = aPInvoice.VATNumber;
             if (aPInvoice.StatusCode == "AC")
             {
-                InputVatAmount = (decimal?)aPInvoice.TotalVATs.Sum(d => d.LocalVATAmount)*-1;
+                InputVatAmount = (decimal?)aPInvoice.TotalVATs.Sum(d => d.LocalVatableAmount)*-1;
                 InputInvoiceAmount = (decimal?)aPInvoice.SubTotalInLocalCurrency*-1 ?? 0;
             }
             else if (aPInvoice.StatusCode == "AD")
             {
-                InputVatAmount = (decimal?)aPInvoice.TotalVATs.Sum(d => d.LocalVATAmount);
+                InputVatAmount = (decimal?)aPInvoice.TotalVATs.Sum(d => d.LocalVatableAmount);
                 InputInvoiceAmount = (decimal?)aPInvoice.SubTotalInLocalCurrency ?? 0;
             }
         }
