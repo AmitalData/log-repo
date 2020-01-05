6e5233dba70c5d43fffd2a0fda04afeee9a5c0a2
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2020-01-05 14:58:46 +0200

    61203,61204
    Related Work Items: #61203, #61204

--- Logitude/Simplog.Data/CommonDataModel/Mapping/RoleFeatureMap.cs
+++ Logitude/Simplog.Data/CommonDataModel/Mapping/RoleFeatureMap.cs
@@ -15,6 +15,8 @@ namespace Simplog.Data.CommonDataModel.Mapping
             this.Property(t => t.FeatureId).IsRequired().HasMaxLength(15).IsUnicode(false);
             this.Property(t => t.FeatureAccessLevelCode).IsRequired().HasMaxLength(2).IsUnicode(false);
             this.Property(t => t.IsDeleted).IsRequired();
+            this.Property(t => t.FeatureUniqeCode).HasMaxLength(120).IsUnicode(false);
+
 
             // Table & Column Mappings
             this.ToTable("RoleFeatures");
@@ -24,9 +26,11 @@ namespace Simplog.Data.CommonDataModel.Mapping
             this.Property(t => t.FeatureId).HasColumnName("FeatureId");
             this.Property(t => t.FeatureAccessLevelCode).HasColumnName("FeatureAccessLevelCode");
             this.Property(t => t.IsDeleted).HasColumnName("IsDeleted");
+            this.Property(t => t.FeatureUniqeCode).HasColumnName("FeatureUniqeCode");
+
 
             // Relationships
-            this.HasRequired(t => t.Feature).WithMany().HasForeignKey(d => d.FeatureId);
+            //this.HasRequired(t => t.Feature).WithMany().HasForeignKey(d => d.FeatureId);
             this.HasRequired(t => t.Role).WithMany().HasForeignKey(d => d.RoleId);
             this.HasRequired(t => t.FeatureAccessLevel).WithMany().HasForeignKey(d => d.FeatureAccessLevelCode);
         }
