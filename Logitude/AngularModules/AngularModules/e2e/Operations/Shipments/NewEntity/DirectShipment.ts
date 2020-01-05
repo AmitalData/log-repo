9577caebb1ab6ee3dd401d8e3d6b1c7154662ac7
Lana Daoud <Lana@logitudeworld.com>
2020-01-05 09:04:40 +0200

    fix Stale element issue

--- Logitude/AngularModules/AngularModules/e2e/Operations/Shipments/NewEntity/DirectShipment.ts
+++ Logitude/AngularModules/AngularModules/e2e/Operations/Shipments/NewEntity/DirectShipment.ts
@@ -17,6 +17,7 @@ export class DirectShipment {
         this.Helper.WaitByIdAndClick('NEWDIRECT');
         this.ShipmentModes.SelectDicrctionTransportMode(Direction, TransportMode, ShipmentType);
         this.FillDirectShipmentFields(ReferenceNumber, TransportMode, Direction);
+        this.Helper.ItemsPresent('ShipmentCreatebtn');
         this.Helper.WaitByIdAndClick('ShipmentCreatebtn');
     }
 
@@ -58,6 +59,7 @@ export class DirectShipment {
                 this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'Shipment_MainCarriageFromPortId', 'eze');
                 this.Helper.WaitByIdAndFill('Shipment_IncotermId', 'CIF');
                 this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'Shipment_IncotermId', 'CIF');
+               
                 this.Helper.WaitByIdAndFill('Shipment_MainCarriageToPortId', 'mvd');
                 this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'Shipment_MainCarriageToPortId', 'mvd');
             }
@@ -67,7 +69,7 @@ export class DirectShipment {
             this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'Shipment_MainCarriageCarrierId', 'BA');
 
             this.Helper.WaitByIdAndFill('Shipment_MainCarriageCarrierNumber', '115');
-
+            this.Helper.ItemsPresent('Shipment_MoveTypeId');
             this.Helper.WaitByIdAndFill('Shipment_MoveTypeId', 'TestMoveTypeIdAirMTA');
             this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'Shipment_MoveTypeId', 'TestMoveTypeIdAirMTA');
 
