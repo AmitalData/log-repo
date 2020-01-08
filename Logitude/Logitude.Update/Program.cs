d16233a2d419fe189b5b2ef1855d0f7b8b367af9
itzik <itzik@amital.co.il>
2020-01-08 08:37:46 +0200

    /JenkinsCustomUpdate

--- Logitude/Logitude.Update/Program.cs
+++ Logitude/Logitude.Update/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,6 +9,10 @@ namespace Logitude.Update
 {
     static class Program
     {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -16,15 +21,35 @@ namespace Logitude.Update
         {
             if (System.Environment.CommandLine.EndsWith("/JenkinsCustomUpdate", StringComparison.OrdinalIgnoreCase))
             {
-                Form1.LoadLogitudeSettings();
-                WebFreight.Web.MetaDataUpdate.TenantsUpdateClass.UpdateDataForTenant(0, "customs");
-                WebFreight.Web.MetaDataUpdate.TenantsUpdateClass.BuildObjectTablesZipFilesData(false, true);
+                JenkinsCustomUpdate();
                 return;
             }
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+
+        private static void JenkinsCustomUpdate()
+        {
+            try
+            {
+
+                AllocConsole();
+                Console.WriteLine(System.Environment.CommandLine);
+                Form1.LoadLogitudeSettings();
+                Console.WriteLine("UpdateDataForTenant");
+                WebFreight.Web.MetaDataUpdate.TenantsUpdateClass.UpdateDataForTenant(0, "customs");
+                Console.WriteLine("BuildObjectTablesZipFilesData");
+                WebFreight.Web.MetaDataUpdate.TenantsUpdateClass.BuildObjectTablesZipFilesData(false, true);
+                Console.WriteLine("End");
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.ToString());
+            }
+
         }
     }
 }
