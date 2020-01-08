263aebeea8a056b6c318b229cfa0ffa80b17491b
Lana Daoud <Lana@logitudeworld.com>
2020-01-08 11:55:59 +0200

    fix

--- Logitude/AngularModules/AngularModules/e2e/Operations/Shipments/NewEntity/DirectShipment.ts
+++ Logitude/AngularModules/AngularModules/e2e/Operations/Shipments/NewEntity/DirectShipment.ts
@@ -50,15 +50,13 @@ export class DirectShipment {
             if (Direction == 'Domestic') {
                 this.Helper.WaitByIdAndFill('Shipment_MainCarriageFromPortId', 'eze');
                 this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'Shipment_MainCarriageFromPortId', 'eze');
-                this.Helper.WaitByIdAndFill('Shipment_IncotermId', 'CIF');
-                this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'Shipment_IncotermId', 'CIF');
+                this.Helper.WaitByIdAndFill('Shipment_MainCarriageCarrierNumber', 'Flight555');
                 this.Helper.WaitByIdAndFill('Shipment_MainCarriageToPortId', 'eze');
                 this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'Shipment_MainCarriageToPortId', 'eze');
             } else {
                 this.Helper.WaitByIdAndFill('Shipment_MainCarriageFromPortId', 'eze');
                 this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'Shipment_MainCarriageFromPortId', 'eze');
-                this.Helper.WaitByIdAndFill('Shipment_IncotermId', 'CIF');
-                this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'Shipment_IncotermId', 'CIF');
+                this.Helper.WaitByIdAndFill('Shipment_MainCarriageCarrierNumber', 'Flight555');
                 this.Helper.ItemsPresent('Shipment_MainCarriageToPortId');
                 this.Helper.WaitByIdAndFill('Shipment_MainCarriageToPortId', 'mvd');
                 this.Helper.WaitByCssAndClick_FromTagInsideListWithCheck('.DropDownListItem', 0, 'Shipment_MainCarriageToPortId', 'mvd');
