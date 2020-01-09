f37b9c24555244a3bdfb8c0f24350e3df04b737a
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2020-01-09 10:17:11 +0200

    BIReport Fixes EMail from Fajr FW: BI report does not display data ôًééن - 34230

--- Logitude/AngularModules/AngularModules/InfrastructureModules/InfrastructureBIReport/Components/Workspaces/DWAskUserFiltersComponent.ts
+++ Logitude/AngularModules/AngularModules/InfrastructureModules/InfrastructureBIReport/Components/Workspaces/DWAskUserFiltersComponent.ts
@@ -152,9 +152,15 @@ export class DWAskUserFiltersComponent extends BaseComponent implements OnInit {
             QueryData.PageIndex = this.PageIndex;
             QueryData.PageSize = this.PageSize;
             QueryData.ColumnsSort = this.DWQueryData.ColumnsSort;
-            var myAndOr = this.DWQueryData.Filters.AndOr;
+            var myAndOr = "And";
+            if (this.DWQueryData.Filters) {
+                myAndOr = this.DWQueryData.Filters.AndOr;
+            }
             this.DWQueryData.Filters = this.SelectedFiltersDataSource[0];
-            this.DWQueryData.Filters.AndOr = myAndOr;
+            if (this.DWQueryData.Filters) {
+                this.DWQueryData.Filters.AndOr = myAndOr;
+            }
+           
             this.GetRowData(QueryData);
         }
         else {
