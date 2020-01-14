1abe18d4073794a46deccc237eab588a8e951843
sohaib <sohaib@amital.co.il>
2020-01-14 14:27:11 +0200

    #61212 add EndDate to PhysicalCheck PM

--- Logitude/AngularModules/AngularModules/Customs/EntityPMs/PhysicalCheckPM.ts
+++ Logitude/AngularModules/AngularModules/Customs/EntityPMs/PhysicalCheckPM.ts
@@ -239,6 +239,11 @@ export class PhysicalCheckPM {
     public set VehicleChassisNumber(newValue: string) { if (this.vehicleChassisNumber != newValue) { this.vehicleChassisNumber = newValue; this.MarkAsDirty("VehicleChassisNumber"); } }
        
 	 
+    private endDate: Date;
+    public get EndDate() { return this.endDate; }
+    public set EndDate(newValue: Date) { if (this.endDate != newValue) { this.endDate = newValue; this.MarkAsDirty("EndDate"); } }
+       
+	 
 
     public OldEntityPM: PhysicalCheckPM;
 		
