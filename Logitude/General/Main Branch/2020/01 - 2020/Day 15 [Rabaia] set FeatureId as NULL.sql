55604e3040a2c99436ced6b367d4efde2c4aca30
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2020-01-15 13:01:55 +0200

    61827 - Script to set foreign Ids as null
    Related Work Items: #61827

---
+++ Logitude/General/Main Branch/2020/01 - 2020/Day 15 [Rabaia] set FeatureId as NULL.sql
@@ -0,0 +1,8 @@
+update MenusTables set FeatureId = NULL
+update MenuButtons set  FeatureId = NULL
+update Queries set  FeatureId = NULL
+update ObjectTableTabs set  FeatureId = NULL
+update ObjectTableHelperControls set FeatureId = NULL
+update PackageFeatures set  FeatureId = NULL
+update RoleFeatures set  FeatureId = NULL
+update Reports set FeatureId = NULL
