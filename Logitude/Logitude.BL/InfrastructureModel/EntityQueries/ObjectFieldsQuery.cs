92f33acffb4a254aaf2ab80166f2aaa1fe809559
Abed <abed@logitudeworld.com>
2020-01-15 18:58:55 +0200

    61755
    Related Work Items: #61755

--- Logitude/Logitude.BL/InfrastructureModel/EntityQueries/ObjectFieldsQuery.cs
+++ Logitude/Logitude.BL/InfrastructureModel/EntityQueries/ObjectFieldsQuery.cs
@@ -2552,7 +2552,7 @@ namespace Logitude.BL.InfrastructureModel.EntityQueries
         public List<ObjectFieldList> GetObjectFieldsForAutomations()
         {
             return (from a in repository.context.ObjectFields.Include("ObjectTable_LookUpTable").Include("FullNameTextCode").Include("ShortNameTextCode").Include("ListTextCode").Include("HelpTextCode").Include("ObjectTable").Include("ObjectTable_MultiTable")
-                    where a.InActive == false && (a.AllowedinAutomationConditions == true || a.AutomationEmailRecipient == true || a.IsCustom || a.CanAutomateSetValue || a.DisplayInAutomationAsEnitity)
+                    where  (a.AllowedinAutomationConditions == true || a.DisplayInAutomationAsEnitity == true || a.AutomationEmailRecipient == true || a.IsCustom || a.FieldName == "DescriptionOfGoods" || a.FieldName == "MainCarriageFinalDestinationETA" || a.FieldName == "MainCarriageFinalDestinationATA" || a.FieldName == "MainCarriageETD" || a.FieldName == "MainCarriageATD")
                     select new ObjectFieldList()
                     {
                         Id = a.Id,
