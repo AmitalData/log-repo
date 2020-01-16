a9e561bcd73094271fe9ac19e3db5332e5d1d87c
Samar Abu Eid <Samar@logitudeworld.com>
2020-01-16 12:28:19 +0200

    61883
    Related Work Items: #61883

--- Logitude/Logitude.BL/ShipmentsModel/EntityLists/ShipmentComputedFieldsList.cs
+++ Logitude/Logitude.BL/ShipmentsModel/EntityLists/ShipmentComputedFieldsList.cs
@@ -18,15 +18,11 @@ namespace Logitude.BL.ShipmentsModel.EntityLists
         public int NumberOfHouses { get; set; }
         public Shipment Shipment { get; set; }
         public bool IsDigitalSignRequired { get; set; }
-
         public bool IsDepositionRequired { get; set; }
         public string ImporterDepositionRequestDetails { get; set; }
-
         public string Commodity { get; set; }
         public string FirstPickupLocation { get; set; }
         public string ContainersNumbers { get; set; }
-
-
         public DateTime? FirstPickupATD { get; set; }
         public DateTime? FirstPickupATA { get; set; }
         public DateTime? FinalDeliveryETD { get; set; }
@@ -34,5 +30,20 @@ namespace Logitude.BL.ShipmentsModel.EntityLists
         public DateTime? FinalDeliveryATD { get; set; }
         public DateTime? FinalDeliveryATA { get; set; }
 
+        public string OperationallyClosedByUserId { get; set; }
+        public int? NumberOfDeliveries { get; set; }
+        public DateTime? ImportDeclarationDate { get; set; }
+        public string ImportDeclarationNumber { get; set; }
+        public DateTime? LastPickupETA { get; set; }
+        public DateTime? LastPickupETD { get; set; }
+        public DateTime? LastPickupATA { get; set; }
+        public DateTime? LastPickupATD { get; set; }
+        public string DeliveryToCity { get; set; }
+        public string DeliveryToPortId { get; set; }
+        public bool ContainsDangerousGoods { get; set; }
+        public string DeliveryFrom { get; set; }
+        public string DeliveryTo { get; set; }
+        public string PickupFrom { get; set; }
+        public string PickupTo { get; set; }
     }
 }
