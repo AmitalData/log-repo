903e79198e2826220982f4806f80c1baa99db41f
Abdullah Dawoud <abdullah@logitudeworld.com>
2020-01-20 17:02:11 +0200

    add 12- Bank Adjustment accounting entity type
    Related Work Items: #62010

--- Logitude/Logitude.Accounting.BL/CloseTables/DetailsClassesGenerated/AccountingEntityDetails.Generated.cs
+++ Logitude/Logitude.Accounting.BL/CloseTables/DetailsClassesGenerated/AccountingEntityDetails.Generated.cs
@@ -97,6 +97,13 @@ namespace Logitude.Accounting.BL
                 LocalName = "העברת שנה", 
                 EnglishName = "Year Transfer", 
 			});
+			 
+            all.Add(new AccountingEntityDetails()
+            {    
+                Code = 12, 
+                LocalName = "התאמת בנק", 
+                EnglishName = "Bank Adjustment", 
+			});
 			
             return all;
        }
