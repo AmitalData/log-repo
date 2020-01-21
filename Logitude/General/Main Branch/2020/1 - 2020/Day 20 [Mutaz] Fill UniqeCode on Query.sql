668476b2eba309456e1d0d77e0aeeb8b36ee2095
Danya Jabr <danya@logitudeworld.com>
2020-01-21 07:50:40 +0200

    Mutaz_Fix_UniqueCode

--- Logitude/General/Main Branch/2020/1 - 2020/Day 20 [Mutaz] Fill UniqeCode on Query.sql
+++ Logitude/General/Main Branch/2020/1 - 2020/Day 20 [Mutaz] Fill UniqeCode on Query.sql
@@ -1,1 +1,3 @@
-update Queries set UniqeCode = (select ObjectTables.Name from ObjectTables where Id= Queries.ObjectTableId)+'.'+Queries.Code
+  update Queries set UniqueCode = (select ObjectTables.Name from ObjectTables where Id= Queries.ObjectTableId)+'.'+Queries.Code;
+
+  ALTER TABLE Queries ALTER COLUMN UniqueCode VARCHAR(200) NOT NULL;
