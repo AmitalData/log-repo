b5d62f7651b70b73146f3b5b81a43e2036290975
Islam Shaheen <islam@logitudeworld.com>
2020-01-27 08:48:24 +0200

    AR payment to SAT - Resend issue

--- Logitude/CommunicationWorkerRole/SATInterfaceWorkerRole.cs
+++ Logitude/CommunicationWorkerRole/SATInterfaceWorkerRole.cs
@@ -587,6 +587,13 @@ namespace CommunicationWorkerRole
 						byte[] xmlfile = encoding.GetBytes(resultadoConsulta.Xml);
 						CreateSATPaymentDocument(payment, xmlfile, true);
 
+						waitingCommLog.CommunicationStatusTypeCode = "D";
+						waitingCommLog.DoneDate = TenantServerConfigration.GetCurrentDateTime(waitingCommLog.Tenant);
+						waitingCommLog.DoneDateUTC = DateTime.UtcNow;
+						waitingCommLog.LastStatusDate = TenantServerConfigration.GetCurrentDateTime(waitingCommLog.Tenant);
+						waitingCommLog.LastStatusDateUTC = DateTime.UtcNow;
+						communicationLogRep.Update(waitingCommLog);
+						communicationLogRep.SubmitChanges();
 						EventTracer.CreateTraceEvent(new EventTracerArgs()
 						{
 							EntityId = waitingCommLog.EntityId,
