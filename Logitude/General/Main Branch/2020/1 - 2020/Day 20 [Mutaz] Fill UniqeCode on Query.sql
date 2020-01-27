9537d86833c1813a37df126bd87fd0624c1bf854
Mutaz Zuhairi <Mutaz@logitudeworld.com>
2020-01-27 14:08:52 +0200

    Related to 62485
    Related Work Items: #62485

--- Logitude/General/Main Branch/2020/1 - 2020/Day 20 [Mutaz] Fill UniqeCode on Query.sql
+++ Logitude/General/Main Branch/2020/1 - 2020/Day 20 [Mutaz] Fill UniqeCode on Query.sql
@@ -1,8 +1,8 @@
-  update Queries set UniqueCode = (select ObjectTables.Name from ObjectTables where Id= Queries.ObjectTableId)+'.'+Queries.Code;
-
+  update Queries set UniqueCode = (select ObjectTables.Name from ObjectTables where Id= Queries.ObjectTableId)+'.'+Queries.UserId+'.'+Queries.Code where Tenant != 0 ;
+  update Queries set UniqueCode = (select ObjectTables.Name from ObjectTables where Id= Queries.ObjectTableId)+'.'+Queries.Code where Tenant = 0 ;
   ALTER TABLE Queries ALTER COLUMN UniqueCode VARCHAR(200) NOT NULL;
+ 
+ --Aplay this only if you have same constrient
+ --Alter TABLE QueryColumns DROP CONSTRAINT  UQ_Tenant_QueryId_ObjectFieldId_UserId
 
-
-  
-Alter TABLE QueryColumns
- DROP CONSTRAINT  UQ_Tenant_QueryId_ObjectFieldId_UserId
+ 
