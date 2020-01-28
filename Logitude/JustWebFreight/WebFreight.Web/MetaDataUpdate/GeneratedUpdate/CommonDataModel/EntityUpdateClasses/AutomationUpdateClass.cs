ef8cb8f1e3e8a3670c686a086a3ce947293d2ed2
Wesam <Wesam@WESAM>
2020-01-28 16:48:02 +0200

    Automation : Missing TextCode
    Related Work Items: #62676

--- Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/GeneratedUpdate/CommonDataModel/EntityUpdateClasses/AutomationUpdateClass.cs
+++ Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/GeneratedUpdate/CommonDataModel/EntityUpdateClasses/AutomationUpdateClass.cs
@@ -425,13 +425,18 @@ namespace WebFreight.Web.MetaDataUpdate.GeneratedUpdate.CommonDataModel.EntityUp
 	    }
 
 	    public void AddTableTextCodes(TextCodeRepository TextCodeRepository,FeatureRepository FeaturesRepository,Dictionary<string, Feature> TenantFeatures,Dictionary<string, TextCode> TextCodes,IWebFreightContext ObjectContext)
-	    {     
-	    
-}
+	    {
+			//--------------> Additional TextCodes <--------------\\
 
-    
+			ObjectTable AutomationObjectTable = ObjectContext.ObjectTables.Where(d => d.Name == "Automation" && d.Tenant == 0).FirstOrDefault();
 
-   }
+			TextCode AutomationTextCode_AutomationOEvents = AddTextCodes.AddTextCode(new TextCodeDetails() { Code = "Automation.TH.Events", DefaultText = "Events", LocalDefaultText = null, ObjectTableId = AutomationObjectTable.Id, Tenant = 0, TextCodeTypeCode = "TH", IsSpellChecked = false }, TextCodeRepository, TextCodes);
+
+		}
+
+
+
+	}
     
 }
 	 
