4c4d7fb09145508834dcd2330f81322597357520
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2020-02-06 14:59:10 +0200

    E2e/ pre env /team islam

--- Logitude/AngularModules/AngularModules/protractor.conf.js
+++ Logitude/AngularModules/AngularModules/protractor.conf.js
@@ -193,6 +193,12 @@ exports.config = {
             browser.params.Login.Password = "!P123p456";
         }
 
+        else if (browser.params.Env == "Pre_production") {
+            browser.params.Link = "https://pre.logitudeworld.com/";
+            browser.params.Login.Email = "Raghad@protractor.com";
+            browser.params.Login.Password = "!RS123Rs";
+        }
+
  	 else if (browser.params.Env == "Prod_Staging") {
             browser.params.Link = "https://staging.logitudeworld.com/";
             browser.params.Login.Email = "protractor2@test.com";
