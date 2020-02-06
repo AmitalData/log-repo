9b261a8f57e22aaab805d1b7410701e02726a870
mirit <Mirit@amital.co.il>
2020-02-06 15:06:20 +0200

    62018

--- Logitude/AngularModules/AngularModules/Customs/Services/WebServices/DeclarationWebService.ts
+++ Logitude/AngularModules/AngularModules/Customs/Services/WebServices/DeclarationWebService.ts
@@ -1029,6 +1029,47 @@ export class DeclarationWebService {
         );
     }
 
+    GetDeclarationCargoSealLists(declarationId: string, tenant: number) {
+        return Observable.defer(() => {
+
+            var authHeader = new Headers();
+            authHeader.append('Token', SessionInfo.Token);
+            authHeader.append('Content-Type', 'application/json');
+
+            var serviceResponse: ServiceResponse = new ServiceResponse();
+
+            return this._http.get(this._apiUrl + "/GetDeclarationCargoSealLists/?declarationId=" + declarationId + "&tenant=" + tenant, {
+                headers: authHeader
+            }).map(response => {
+
+                var res = response.json();
+                serviceResponse.Result = res;
+                return serviceResponse;
+            }).catch(ServiceHelper.HandleServiceError);
+        }
+        );
+    }
+
+    GetAcceptDeclarationAmendment(declarationId: string) {
+        return Observable.defer(() => {
+
+            var authHeader = new Headers();
+            authHeader.append('Token', SessionInfo.Token);
+            authHeader.append('Content-Type', 'application/json');
+
+            var serviceResponse: ServiceResponse = new ServiceResponse();
+
+            return this._http.get(this._apiUrl + "/GetAcceptDeclarationAmendment/?declarationId=" + declarationId , {
+                headers: authHeader
+            }).map(response => {
+
+                var res = response.json();
+                serviceResponse.Result = res;
+                return serviceResponse;
+            }).catch(ServiceHelper.HandleServiceError);
+        }
+        );
+    }
 
     GetDeclarationCargoSplitByDeclarationIdList(declarationId: string, tenant: number) {
         return Observable.defer(() => {
