cfc77fea4fe7c8c5a2e480b75adafba48f61c927
Abd Al-Rahman Malakh <Abd@logitudeworld.com>
2020-02-06 15:37:30 +0200

    63035
    Related Work Items: #63035

--- Logitude/Logitude.DXMLGenerator/Program.cs
+++ Logitude/Logitude.DXMLGenerator/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,27 @@ namespace Logitude.DXMLGenerator
     {
         static void Main(string[] args)
         {
+            //////Delete all DXML files of type table before begin generating
+            Console.WriteLine("Deleting All DXML Files Of Type Table ...\n");
+            int deletedDxmlFilesCount = 0;
+            string root = ConfigurationManager.AppSettings["Root"];
+            string dxmlFilesRoot = Path.Combine(root);
+            string[] dxmlFiles = Directory.GetFiles(dxmlFilesRoot, "*.dxml", SearchOption.AllDirectories);
+
+            foreach(var dxmlFile in dxmlFiles)
+            {
+                string xmlString = File.ReadAllText(dxmlFile);
+                if (xmlString.EndsWith("</Table>"))
+                {
+                    File.Delete(dxmlFile);
+                    deletedDxmlFilesCount++;
+                }
+            }
+
+            Console.WriteLine(deletedDxmlFilesCount + " DXML Files Of Type Table Deleted Successfully\n");
+            //////
+            
+
             string globalConnectionString = ConfigurationManager.AppSettings["GlobalConnectionString"];
             string mainConnectionString = ConfigurationManager.AppSettings["MainConnectionString"];
             string systemLogsConnectionString = ConfigurationManager.AppSettings["SystemLogsConnectionString"];
