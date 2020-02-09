2f021e286f9fdcad55049f3ea7eb24891da3ae81
mirit <Mirit@amital.co.il>
2020-02-09 12:17:20 +0200

    62474

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsRequests/Components/DeclarationRequests/CargoSealsQueryComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsRequests/Components/DeclarationRequests/CargoSealsQueryComponent.ts
@@ -60,12 +60,6 @@ export class CargoSealsQueryComponent
         this.CargoSealObslist = new ObservableCollection([]);
         this.EntityResourceService.getEntityResourceByTableName("Customs.CargoSealIdentifier").subscribe(response => {
             this.EntityResourceService.getEntityResourceByTableName("Customs.CargoSeal").subscribe(response => {
-            //    debugger;
-            //    this.amendmentTypeListService.getAll().subscribe(
-            //        data => {
-            //            this.AmendmentTypes = data.Result;
-            //        });
- 
                 this.IsReady = true;
             });
         });
@@ -181,7 +175,10 @@ export class CargoSealsQueryComponent
             this._IsDisplayOnly = value;
         }
     }
-    
+
+    public get IsReady() { return this._IsReady; }
+    public set IsReady(newValue: boolean) { this._IsReady = newValue; }
+
     get UpdateDate() { return this.RequestParams.UpdateDate; }
     set UpdateDate(value: Date) {
         if (this.RequestParams.UpdateDate != value) {
