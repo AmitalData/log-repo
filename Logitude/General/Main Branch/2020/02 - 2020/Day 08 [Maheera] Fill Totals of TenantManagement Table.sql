4092759e3345bb0598759bbc6a4c1a0fa5ab492c
Maheera Jazi <Maheera@logitudeworld.com>
2020-02-10 13:04:53 +0200

    63129
    Related Work Items: #63129

--- Logitude/General/Main Branch/2020/02 - 2020/Day 08 [Maheera] Fill Totals of TenantManagement Table.sql
+++ Logitude/General/Main Branch/2020/02 - 2020/Day 08 [Maheera] Fill Totals of TenantManagement Table.sql
@@ -9,6 +9,8 @@ declare @NumberOfUsers as int
 declare @FreeUsers as int 
 declare @TotalPrice as decimal 
 declare @TotalPaymentamount_Packages as decimal 
+declare @TotalNumberOfUsers_Packages as int 
+declare @TotalFreeUsers_Packages as int 
 
 DECLARE TenantManagementsCursor CURSOR READ_ONLY
 	FOR	
@@ -18,27 +20,36 @@ DECLARE TenantManagementsCursor CURSOR READ_ONLY
 	WHILE @@FETCH_STATUS = 0
 	BEGIN
 
-	    set @TotalNumberOfUsers = (select SUM(isnull(NumberOfUsers,0)) from TenantManagementLicenses where Tenant = @TenantId);
-		set @TotalFreeUsers = (select SUM(isnull(FreeUsers,0)) from TenantManagementLicenses where Tenant = @TenantId);
+	    set @TotalNumberOfUsers_Packages = (select SUM(isnull(NumberOfUsers,0)) from TenantManagementLicenses where Tenant = @TenantId);
+		set @TotalFreeUsers_Packages = (select SUM(isnull(FreeUsers,0)) from TenantManagementLicenses where Tenant = @TenantId);
 		set @TotalPaymentamount_Packages = (select SUM(isnull(TotalPrice,0)) from TenantManagementLicenses where Tenant = @TenantId);
-		
+		set @AveragePrice = 0;
+
 		if (@IsMultiPackage = 0)
 		 begin
-			set @TotalNumberOfUsers += isnull(@NumberOfUsers,0);
-			set @TotalFreeUsers += isnull(@FreeUsers,0);
+			set @TotalNumberOfUsers = isnull(@NumberOfUsers,0);
+			set @TotalFreeUsers = isnull(@FreeUsers,0);
 			set @TotalPaymentamount = isnull(@TotalPrice,0);
 		 end
 		 else 
 		 begin
 			set @TotalPaymentamount = isnull(@TotalPaymentamount_Packages,0);
+			set @TotalFreeUsers = isnull(@TotalFreeUsers_Packages,0);
+			set @TotalNumberOfUsers = isnull(@TotalNumberOfUsers_Packages,0);
 		 end
 
 		 if (@MainAdditionalPackageApplied = 1)
 		  begin
 			set @TotalPaymentamount = isnull(@TotalPaymentamount_Packages,0) + isnull(@TotalPrice,0);
+			set @TotalFreeUsers = isnull(@TotalFreeUsers_Packages,0) + isnull(@NumberOfUsers,0);
+			set @TotalNumberOfUsers = isnull(@TotalNumberOfUsers_Packages,0) + isnull(@FreeUsers,0);
 		 end
 
-		set @AveragePrice = ROUND(@TotalPaymentamount/@TotalNumberOfUsers, 3);
+		 if (@TotalNumberOfUsers != 0)
+		  begin
+			 set @AveragePrice = ROUND(@TotalPaymentamount/@TotalNumberOfUsers, 3);
+		  end
+
 		update TenantManagements set TotalNumberOfUsers=@TotalNumberOfUsers, TotalFreeUsers = @TotalFreeUsers, AveragePrice = @AveragePrice, TotalPaymentamount =@TotalPaymentamount where Id=@TenantId
 
 	FETCH NEXT FROM TenantManagementsCursor INTO @TenantId, @IsMultiPackage,@MainAdditionalPackageApplied,@NumberOfUsers, @FreeUsers, @TotalPrice
