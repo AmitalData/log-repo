a31ea13ca038e20ac4db8eb23e3ffa08de36f422
Ayman1 <Ayman@logitudeworld.com>
2020-02-10 07:43:28 +0200

    62656
    Related Work Items: #62656

--- Logitude/AngularModules/AngularModules/QuoteModules/QuoteCharges/Components/AddEditLCLChargeComponent.ts
+++ Logitude/AngularModules/AngularModules/QuoteModules/QuoteCharges/Components/AddEditLCLChargeComponent.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {AppTool, DateTool} from '../../../Infrastructure/Tools';
+import {AppTool,} from '../../../Infrastructure/Tools';
 import {BaseComponent} from '../../../Infrastructure/Components/LogitudeComponents/BaseComponent';
 import {ApiQueryFilters} from '../../../Infrastructure/DataContracts/ApiQueryFilters';
 import {SessionLocator} from '../../../Infrastructure/Utilities/SessionLocator';
@@ -8,7 +8,6 @@ import {Validator} from '../../../Infrastructure/Validators/Validator';
 import {Cloner} from '../../../Infrastructure/Utilities/Cloner';
 import {QuoteChargeItem} from './LCLChargesComponent';
 import {ObservableCollection} from '../../../Infrastructure/Utilities/ObservableCollection';
-import {EntityResourceService} from '../../../Infrastructure/Services/EntityResourceService';
 import {QuoteChargePM} from '../../../Quote/EntityPMs/QuoteChargePM';
 import {QuotePriceStepsPM} from '../../../Quote/EntityPMs/QuotePriceStepsPM';
 import { LogitudeWindow } from '../../../Controls/Windows/LogitudeWindow';
@@ -16,6 +15,7 @@ import { MessageWindow } from '../../../Controls/Windows/MessageWindow';
 import {QuotePM} from '../../../Quote/EntityPMs/QuotePM';
 import {VatTypesValidator} from '../../../Infrastructure/Validators/VatTypesValidator';
 import { QuoteValidator } from '../../../Quote/Validators/QuoteValidator';
+import { PriceStepList } from '../../../Infrastructure/EntityLists/PriceStepList';
 import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
@@ -23,10 +23,11 @@ import { forEach } from '@angular/router/src/utils/collection';
     templateUrl: './AddEditLCLChargeComponent.html',
 })
 
-export class AddEditLCLChargeComponent {
+export class AddEditLCLChargeComponent extends BaseComponent {
     public QuotePM: QuotePM;
     public EntityPM: QuoteChargePM;
     public DataContext: QuoteChargeItem;
+    public DataContext2 = this;
     public Father: any;
     public IsAdhoc: boolean = false;
     public IsRoutingRate: boolean = false;
@@ -43,8 +44,9 @@ export class AddEditLCLChargeComponent {
     private IsHyprid: boolean;
     private ChargesTypeCode: string;
     
-
     constructor() {
+        super();
+
         this.ItemsSource = new ObservableCollection([]);
         this.StepsItemsSource = new ObservableCollection([]);
         this.CurrentSession.SessionEvent.subscribe((res) => {
@@ -74,12 +76,26 @@ export class AddEditLCLChargeComponent {
         this.IsVATVisible = this.IsAdhoc && this.QuotePM.IsChargesByVAT ? true : false;
         this.ChargesTypeCode = this.EntityPM.ChargesTypeCode;
 
+        this.SetUIProperties();
         this.DataContext.SetUIProperties();
         this.BuildItemsSource();
         this.BuildQueryFilters();
         this.BuildStepItemsSource();
         this.Clone();
         
+    }
+
+    public IsAddBreaksEnabled: boolean = false;
+    SetUIProperties() {
+        var isAddBreaksEnabled = false;
+
+        if (this.IsEditingEnabled) {
+            if (!AppTool.IsNullOrEmpty(this.SelectedPriceStepId)) {
+                isAddBreaksEnabled = true;
+            }
+        }
+
+        this.IsAddBreaksEnabled = isAddBreaksEnabled;
     }
 
     BuildItemsSource() {
@@ -392,31 +408,66 @@ export class AddEditLCLChargeComponent {
         this.myCloner.RejectChanges();
     }
 
-    SelectBreaksClicked() {
-        if (this.StepsItemsSource.Length > 0) {
-            var messageWindow = new MessageWindow();
-            messageWindow.Show("Can't use default breaks when you have added breaks, please delete first");
+    private selectedPriceStepId: string = null;
+    public get SelectedPriceStepId() { return this.selectedPriceStepId; }
+    public set SelectedPriceStepId(value: string) {
+        if (this.selectedPriceStepId != value) {
+            this.selectedPriceStepId = value;
+            this.SetUIProperties();
         }
+    }
 
-        else {
-            var logitudeWindow = new LogitudeWindow();
-            logitudeWindow.Title = "Select Price Breaks";
-            logitudeWindow.Show('./QuoteModules/QuoteCharges/Components/SelectBreaksComponent');
+    private selectedPriceStepList: PriceStepList = null;
+    public get SelectedPriceStepList() { return this.selectedPriceStepList; }
+    public set SelectedPriceStepList(value: PriceStepList) {
+        if (this.selectedPriceStepList != value) {
+            this.selectedPriceStepList = value;
+        }
+    }
 
-            logitudeWindow.WindowClosed.subscribe(s => {
-                if (s) {
-                    var steps: string[] = s.split(',');
+    AddBreaksClicked() {
+        if (this.SelectedPriceStepList) {
+            if (this.StepsItemsSource.Collection.filter(f => !AppTool.IsNullOrZero(f.CostUnitPrice) || !AppTool.IsNullOrZero(f.SaleUnitPrice)).length > 0) {
+                var messageWindow = new MessageWindow();
+                messageWindow.Show("Can't use default breaks when you have added breaks, please delete first");
+            }
 
-                    steps.forEach((step: string) => {
-                        var newItem: QuotePriceStepsPM = new QuotePriceStepsPM(null);
-                        newItem.Tenant = SessionLocator.Tenant;
-                        newItem.QuoteId = this.EntityPM.Id;
-                        newItem.Step = +step;
-                        newItem.QuoteChargeId = this.EntityPM.Id;
-                        this.StepsItemsSource.Insert(new QuoteStepItem(newItem, this, true));
-                    });
-                }
-            });
+            else {
+                this.StepsItemsSource.Clear();
+
+                var steps: string[] = this.SelectedPriceStepList.Steps.split(',');
+
+                steps.forEach((step: string) => {
+                    var newItem: QuotePriceStepsPM = new QuotePriceStepsPM(null);
+                    newItem.Tenant = SessionLocator.Tenant;
+                    newItem.QuoteId = this.EntityPM.Id;
+                    newItem.Step = +step;
+                    newItem.QuoteChargeId = this.EntityPM.Id;
+                    this.StepsItemsSource.Insert(new QuoteStepItem(newItem, this, true));
+                });
+
+                this.SelectedPriceStepId = null;
+                this.SelectedPriceStepList = null;
+
+                //var logitudeWindow = new LogitudeWindow();
+                //logitudeWindow.Title = "Select Price Breaks";
+                //logitudeWindow.Show('./QuoteModules/QuoteCharges/Components/SelectBreaksComponent');
+
+                //logitudeWindow.WindowClosed.subscribe(s => {
+                //    if (s) {
+                //        var steps: string[] = s.split(',');
+
+                //        steps.forEach((step: string) => {
+                //            var newItem: QuotePriceStepsPM = new QuotePriceStepsPM(null);
+                //            newItem.Tenant = SessionLocator.Tenant;
+                //            newItem.QuoteId = this.EntityPM.Id;
+                //            newItem.Step = +step;
+                //            newItem.QuoteChargeId = this.EntityPM.Id;
+                //            this.StepsItemsSource.Insert(new QuoteStepItem(newItem, this, true));
+                //        });
+                //    }
+                //});
+            }
         }
     }
 }
