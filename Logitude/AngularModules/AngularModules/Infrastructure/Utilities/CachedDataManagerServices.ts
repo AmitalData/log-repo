3339004910b0f30f4ac25b3acf9638d20bcfc020
Ayman1 <Ayman@logitudeworld.com>
2020-02-16 18:09:37 +0200

    63435
    Related Work Items: #63435

--- Logitude/AngularModules/AngularModules/Infrastructure/Utilities/CachedDataManagerServices.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Utilities/CachedDataManagerServices.ts
@@ -90,7 +90,7 @@ import { TariffTypeListService } from '../../TariffModule/Services/StandardLists
 //Occasions
 import { OccasionStatusListService } from '../../CRM/Services/StandardLists/OccasionStatusListService';
 import { OccasionTypeListService } from '../../CRM/Services/StandardLists/OccasionTypeListService';
-
+import { AWBAdditionalHandlingInfoListService } from '../../Shipment/Services/StandardLists/AWBAdditionalHandlingInfoListService';
 
 export class CachedDataManagerServices {
     public getAllFromCache(objectTableName: string, filters: ApiQueryFilters) {
@@ -193,6 +193,8 @@ export class CachedDataManagerServices {
             case "TariffListService": { myResult = new TariffListService(); break; }               
             case "TariffTypeListService": { myResult = new TariffTypeListService(); break; }
             case "OccasionTypeListService": { myResult = new OccasionTypeListService(); break; }
+            case "AWBAdditionalHandlingInfoListService": { myResult = new AWBAdditionalHandlingInfoListService(); break; }               
+
             default: {
                 alert(name + " is not declared in CachedDataManagerServices");
                 break;
