7aada2ee09bca8c29fa0e05ae819fe7d54c3e920
Maheera Jazi <Maheera@logitudeworld.com>
2020-02-17 15:53:09 +0200

    63326
    Related Work Items: #63326

--- Logitude/JustWebFreight/WebFreight.Web/DataProviders/ShippingDeclarationDataProvider.Shared.cs
+++ Logitude/JustWebFreight/WebFreight.Web/DataProviders/ShippingDeclarationDataProvider.Shared.cs
@@ -425,6 +425,7 @@ namespace WebFreight.Web.DataProviders
 
         public string NotifyContactDetails { get; set; }      
         public string FullPickupAddress { get; set; }
+        public string PickupFromPartnerName { get; set; }
         public double? ChargeableWeight { get; set; }
         public string ChargeableWeightUnitCode { get; set; }
 
