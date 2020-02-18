913369e24a15fb3427f5730525e92633803700e5
BUILDMACHINE$ <BUILDMACHINE$@amital.co.il>
2020-02-18 09:07:02 +0200

    'ChangeOSVersion:1.D.19.03.14'

--- Logitude/JustWebFreight/WebFreight.Web/Properties/AssemblyInfo.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 
-[assembly: AssemblyFileVersion("1.D.19.03.15")]
+[assembly: AssemblyFileVersion("1.D.19.03.14")]
 
