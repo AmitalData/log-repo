2a6fbb1cdfd1b7909f762e368aa42f6c9087dd6b
Alexander Khitrik <alexanderh@amital.co.il>
2020-02-19 11:53:06 +0200

    Alexander:  Reco after conv optimisation

--- Logitude/Logitude.Accounting.BL/CoreBL/Batch/BatckReconciliationAfterConversionTask.cs
+++ Logitude/Logitude.Accounting.BL/CoreBL/Batch/BatckReconciliationAfterConversionTask.cs
@@ -26,7 +26,7 @@ namespace Logitude.Accounting.BL.CoreBL.Batch
             try
             {
 
-                
+
                 using (var scope = TransactionFactory.GetTransaction(TimeSpan.FromMinutes(10)))
                 {
 
@@ -36,7 +36,7 @@ namespace Logitude.Accounting.BL.CoreBL.Batch
                     //service.Update(entityPM, true);
 
                     ReconciliationAfterConversionBatch reconciliationAfterConversionBatch = new ReconciliationAfterConversionBatch();
-                    reconciliationAfterConversionBatch.RunReconciliationAfterConversion(parameterArgs.Tenant, parameterArgs.FromExtNum, parameterArgs.ToExtNum);
+                    reconciliationAfterConversionBatch.RunReconciliationAfterConversion(parameterArgs);
                     string responseText = reconciliationAfterConversionBatch.ResponseText();
                     ChangeStatus("D", null, responseText);
 
