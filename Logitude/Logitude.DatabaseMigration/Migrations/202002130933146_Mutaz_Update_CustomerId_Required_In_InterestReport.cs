70416da28227d9ccb49bd4f8807428e1013712b5
Mutaz Zuhairi <Mutaz@logitudeworld.com>
2020-02-20 14:03:11 +0200

    Fix

--- Logitude/Logitude.DatabaseMigration/Migrations/202002130933146_Mutaz_Update_CustomerId_Required_In_InterestReport.cs
+++ Logitude/Logitude.DatabaseMigration/Migrations/202002130933146_Mutaz_Update_CustomerId_Required_In_InterestReport.cs
@@ -7,11 +7,11 @@ namespace Logitude.DatabaseMigration.Migrations
     {
         public override void Up()
         {
-            DropForeignKey("dbo.InterestReports", "CustomerId", "dbo.Cards");
-            DropIndex("dbo.InterestReports", new[] { "CustomerId" });
-            AlterColumn("dbo.InterestReports", "CustomerId", c => c.String(nullable: false, maxLength: 15, unicode: false));
-            CreateIndex("dbo.InterestReports", "CustomerId");
-            AddForeignKey("dbo.InterestReports", "CustomerId", "dbo.Cards", "Id");
+            //DropForeignKey("dbo.InterestReports", "CustomerId", "dbo.Cards");
+            //DropIndex("dbo.InterestReports", new[] { "CustomerId" });
+            //AlterColumn("dbo.InterestReports", "CustomerId", c => c.String(nullable: false, maxLength: 15, unicode: false));
+            //CreateIndex("dbo.InterestReports", "CustomerId");
+            //AddForeignKey("dbo.InterestReports", "CustomerId", "dbo.Cards", "Id");
         }
 
         public override void Down()
