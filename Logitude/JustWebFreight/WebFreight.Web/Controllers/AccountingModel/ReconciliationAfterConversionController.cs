f1a5193f4acf000d4a26ba7d83f5ca4490480701
Alexander Khitrik <alexanderh@amital.co.il>
2020-02-23 15:45:48 +0200

    Alexander: Reco after conversion No Batch

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/AccountingModel/ReconciliationAfterConversionController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/AccountingModel/ReconciliationAfterConversionController.cs
@@ -53,6 +53,41 @@ namespace WebFreight.Web.Controllers.AccountingModel
             }
         }
 
+
+        public HttpResponseMessage GetReconciliationAfterConversion(int tenant, int NoBatch)
+        {
+            string fromExtNum = "000000000000001";
+            string toExtNum = "999999999999999";
+            try
+            {
+                string token = HttpContext.Current.Request.Headers["Token"];
+                //AuthenticationToken authToken = AuthenticationTokenRepository.GetSingleTokenFromCache(token);
+                //SecurityUtility.AuthenticationOnTenant(authToken.Tenant);
+                //SecurityUtility.CheckContactFeature("TaxDeductionReport", "NEW", authToken.Tenant);
+
+                ReconciliationAfterConversionBatch reconciliationAfterConversionBatch = new ReconciliationAfterConversionBatch();
+                ReconciliationAfterConversionArg reconciliationAfterConversionArg = new ReconciliationAfterConversionArg()
+                {
+                    Tenant = tenant,
+                    FromExtNum = fromExtNum,
+                    ToExtNum = toExtNum,
+                };
+                reconciliationAfterConversionBatch.RunReconciliationAfterConversion(reconciliationAfterConversionArg);
+                string responseText = reconciliationAfterConversionBatch.ResponseText();
+                HttpStatusCode StatusCode = reconciliationAfterConversionBatch.StatusCode();
+                var res1 = new { Success = true, Message = responseText };
+
+                return Request.CreateResponse(StatusCode, res1);
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ApiExceptionBuilder.BuildException(ex));
+            }
+        }
+
+
+
         public HttpResponseMessage GetReconciliationAfterConversionFromTo(int tenant, string fromExtNum, string toExtNum)
         {
             try
@@ -82,6 +117,85 @@ namespace WebFreight.Web.Controllers.AccountingModel
                 //SecurityUtility.AuthenticationOnTenant(authToken.Tenant);
                 //SecurityUtility.CheckContactFeature("TaxDeductionReport", "NEW", authToken.Tenant);
                 bool batchIt = true;
+                if (batchIt)
+                {
+                    var accountingContext = AccountingContext.GetContext(tenant);
+
+                    var myBatckReconciliationAfterConversionTask = new BatckReconciliationAfterConversionTask(null);
+                    string subj = $"Reconciliation After Conversion({_myfromExtNum}- {_mytoExtNum})";
+                    var batchTaskId = myBatckReconciliationAfterConversionTask.CreateQBatchTaskExecution<ReconciliationAfterConversionArg>(
+                        new ReconciliationAfterConversionArg()
+                        {
+                            Tenant = tenant,
+                            FromExtNum = _myfromExtNum,
+                            ToExtNum = _mytoExtNum
+                        }, tenant, subj, false);
+
+
+                    var res1 = new { Success = true, Message = $"Send to Batch Task {batchTaskId}" };
+                    return Request.CreateResponse(HttpStatusCode.Accepted, res1);
+                }
+                else
+                {
+                    ReconciliationAfterConversionBatch reconciliationAfterConversionBatch = new ReconciliationAfterConversionBatch();
+                    ReconciliationAfterConversionArg reconciliationAfterConversionArg = new ReconciliationAfterConversionArg()
+                    {
+                        Tenant = tenant,
+                        FromExtNum = _myfromExtNum,
+                        ToExtNum = _mytoExtNum,
+                    };
+                    reconciliationAfterConversionBatch.RunReconciliationAfterConversion(reconciliationAfterConversionArg);
+                    string responseText = reconciliationAfterConversionBatch.ResponseText();
+                    HttpStatusCode StatusCode = reconciliationAfterConversionBatch.StatusCode();
+                    var res1 = new { Success = true, Message = responseText };
+                    return Request.CreateResponse(StatusCode, res1);
+                }
+
+
+
+            }
+
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ApiExceptionBuilder.BuildException(ex));
+            }
+
+        }
+
+
+
+
+
+        public HttpResponseMessage GetReconciliationAfterConversionFromToNoBatch(int tenant, string fromExtNum, string toExtNum, int noBatch)
+        {
+            try
+            {
+                string _myfromExtNum = "";
+                if (String.IsNullOrWhiteSpace(fromExtNum) || fromExtNum == "undefined")
+                {
+                    _myfromExtNum = "000000000000001";
+                }
+                else
+                {
+                    _myfromExtNum = Regex.Replace(fromExtNum, @"\d+", n => n.Value.PadLeft(15, '0'));
+                }
+
+                string _mytoExtNum = "";
+                if (String.IsNullOrWhiteSpace(toExtNum) || toExtNum == "undefined")
+                {
+                    _mytoExtNum = "999999999999999";
+                }
+                else
+                {
+                    _mytoExtNum = Regex.Replace(toExtNum, @"\d+", n => n.Value.PadLeft(15, '0'));
+                }
+
+                string token = HttpContext.Current.Request.Headers["Token"];
+                //AuthenticationToken authToken = AuthenticationTokenRepository.GetSingleTokenFromCache(token);
+                //SecurityUtility.AuthenticationOnTenant(authToken.Tenant);
+                //SecurityUtility.CheckContactFeature("TaxDeductionReport", "NEW", authToken.Tenant);
+                bool batchIt = true;
+                if (noBatch == 1) batchIt = false;
                 if (batchIt)
                 {
                     var accountingContext = AccountingContext.GetContext(tenant);
