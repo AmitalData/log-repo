b948f379bd68fdb022c6f4d2bda167a1b1e21626
alaa <alaa.jamal.h@live.com>
2020-02-24 10:06:36 +0200

    63295
    Related Work Items: #63295

--- Logitude/Logitude.DatabaseMigration/Migrations/202002130920422_Mutaz_Update_Field_CustomerId_In_InterestReport.cs
+++ Logitude/Logitude.DatabaseMigration/Migrations/202002130920422_Mutaz_Update_Field_CustomerId_In_InterestReport.cs
@@ -7,8 +7,8 @@ namespace Logitude.DatabaseMigration.Migrations
     {
         public override void Up()
         {
-            Sql("update InterestReports set CustomerId = (select Cards.Id from Cards where GLAccountId = InterestReports.GLAccountId)");
-            AlterColumn("dbo.InterestReports", "CustomerId", c => c.String(maxLength: 15, unicode: false));
+            //Sql("update InterestReports set CustomerId = (select Cards.Id from Cards where GLAccountId = InterestReports.GLAccountId)");
+            //AlterColumn("dbo.InterestReports", "CustomerId", c => c.String(maxLength: 15, unicode: false));
         }
 
         public override void Down()
