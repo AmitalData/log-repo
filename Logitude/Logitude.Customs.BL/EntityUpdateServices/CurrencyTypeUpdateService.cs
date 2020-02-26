0549f8042752d5a8a65aa4d67f8e525ccda62300
itzik <itzik@amital.co.il>
2020-02-26 08:34:03 +0200

    63841
    Related Work Items: #63841

---
+++ Logitude/Logitude.Customs.BL/EntityUpdateServices/CurrencyTypeUpdateService.cs
@@ -0,0 +1,21 @@
+﻿using Logitude.BL.Helpers;
+using Logitude.Customs.Data.EntityPOCOs;
+using Logitude.Customs.Def.EntityPMs;
+using Logitude.Server.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logitude.Customs.BL.EntityUpdateServices
+{
+    public partial class CurrencyTypeUpdateService : EntityUpdateService<CurrencyType, CurrencyTypePM, EntityPM>
+    {
+        protected override void OnUpdating(CurrencyTypePM entityPM)
+        {
+            TableLastUpdateClass.UpdateTableHistory(0, "Customs.CurrencyType");
+            base.OnUpdating(entityPM);
+        }
+    }
+}
