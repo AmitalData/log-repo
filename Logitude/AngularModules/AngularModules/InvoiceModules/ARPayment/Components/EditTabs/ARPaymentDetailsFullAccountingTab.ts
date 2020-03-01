26c663c0d93a191d6a7287e59b8a4ceb01158d16
alaa <alaa.jamal.h@live.com>
2020-03-01 14:19:13 +0200

    63234
    Related Work Items: #63234

--- Logitude/AngularModules/AngularModules/InvoiceModules/ARPayment/Components/EditTabs/ARPaymentDetailsFullAccountingTab.ts
+++ Logitude/AngularModules/AngularModules/InvoiceModules/ARPayment/Components/EditTabs/ARPaymentDetailsFullAccountingTab.ts
@@ -268,7 +268,7 @@ export class ARPaymentDetailsFullAccountingTab extends BaseComponent implements 
             // setTimeout(() => {
 
 
-                this._LedgerTransactionExtendedListService.getTransactionsForARPayment(this.EntityPM.Id, this.EntityPM.GLAccountId).subscribe(myResult => {
+            this._LedgerTransactionExtendedListService.getTransactionsForARPayment(this.EntityPM.Id, this.EntityPM.GLAccountId, this.EntityPM.PaymentCurrencyId).subscribe(myResult => {
                     this._loading = false;
 
                     var mm: ServiceResponse = myResult;
