810d57952d320ac0d6f19c9987b096aabc8ae1f9
Abd Al-Rahman Malakh <Abd@logitudeworld.com>
2020-03-10 17:06:26 +0200

    64345
    Related Work Items: #64345

--- Logitude/Logitude.ServerHealthService/LogitudeServerHealthService.cs
+++ Logitude/Logitude.ServerHealthService/LogitudeServerHealthService.cs
@@ -6,21 +6,15 @@ using System.Linq;
 using System.Net.Mail;
 using System.Text;
 using System.Net;
-using System.Configuration;
+using Logitude.ServerHealthService.Models;
 
 namespace Logitude.ServerHealthService
 {
     public partial class LogitudeServerHealthService : ServiceBase
     {
         protected Timer ServiceTimer = new Timer();
-        protected int PercentageOfTotalDriveSpace;
-        protected long ServiceTimerIntervalInSeconds;
-        protected string FromAddress;
-        protected string ToAddresses;
-        protected string SmtpClientHost;
-        protected int SmtpClientPort;
-        protected string SmtpClientUsername;
-        protected string SmtpClientPassword;
+        protected Settings Settings;
+        protected DateTime? LastEmailAlert = null;
 
         public LogitudeServerHealthService()
         {
@@ -29,87 +23,98 @@ namespace Logitude.ServerHealthService
 
         protected override void OnStart(string[] args)
         {
-            WriteToLogsFile("Service Is Started At " + GetCurrentDateTime(true));
-            ReadConfigurations();
+            WriteToLogsFile("Service Is Started");
+            ReadServiceSettings();
             ServiceTimer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
-            ServiceTimer.Interval = ServiceTimerIntervalInSeconds * 1000;
+            ServiceTimer.Interval = Settings.GeneralSettings.DrivesCheckTimer.IntervalInSeconds * 1000;
             ServiceTimer.Enabled = true;
         }
 
         protected override void OnStop()
         {
-            WriteToLogsFile("Service Is Stopped At " + GetCurrentDateTime(true));
+            WriteToLogsFile("Service Is Stopped");
         }
 
         protected void OnElapsedTime(object source, ElapsedEventArgs e)
         {
-            DriveInfo[] serverDrives = DriveInfo.GetDrives().Where(d => d.DriveType == DriveType.Fixed).ToArray();
-
-            foreach (DriveInfo drive in serverDrives)
+            try
             {
-                if (drive.IsReady)
-                {
-                    long driveTotalSize = drive.TotalSize;
-                    long driveTotalFreeSpace = drive.TotalFreeSpace;
-                    double percentageOfTotal = ((double)PercentageOfTotalDriveSpace / 100) * driveTotalSize;
+                DriveInfo[] serverDrives = DriveInfo.GetDrives().Where(d => d.DriveType == DriveType.Fixed).ToArray();
 
-                    if (driveTotalFreeSpace < percentageOfTotal)
+                foreach (DriveInfo drive in serverDrives)
+                {
+                    if (drive.IsReady)
                     {
-                        string driveName = drive.VolumeLabel + " (" + drive.Name.Replace(@"\", String.Empty) + ")";
-                        string warningMessage = BuildWarningMessage(driveName, driveTotalSize, driveTotalFreeSpace);
-                        string subject = "Logitude Server Health Warning";
-                        SendEmails(subject, warningMessage);
+                        bool isDriveSpaceLow = IsDriveSpaceLow(drive);
+                        bool shouldSendEmails = ShouldSendEmails();
+
+                        if (isDriveSpaceLow && shouldSendEmails)
+                        {
+                            string driveLabelWithName = drive.VolumeLabel + " (" + drive.Name.Replace(@"\", String.Empty) + ")";
+                            string warningMessageSubject = "Low Drive Space In " + Environment.MachineName;
+                            string warningMessageBody = BuildWarningMessageBody(driveLabelWithName, drive.TotalSize, drive.TotalFreeSpace);
+
+                            SendEmails(warningMessageSubject, warningMessageBody);
+                            LastEmailAlert = DateTime.Now;
+                        }
                     }
                 }
             }
+            catch(Exception exception)
+            {
+                WriteToLogsFile("Error While Checking Drives Spaces With Exception: " + exception.Message);
+            }
         }
 
-        protected void ReadConfigurations()
+        protected void ReadServiceSettings()
         {
-            PercentageOfTotalDriveSpace = Convert.ToInt32(ConfigurationManager.AppSettings["PercentageOfTotalDriveSpace"]);
-            ServiceTimerIntervalInSeconds = Convert.ToInt64(ConfigurationManager.AppSettings["ServiceTimerIntervalInSeconds"]);
-            FromAddress = ConfigurationManager.AppSettings["FromAddress"];
-            ToAddresses = ConfigurationManager.AppSettings["ToAddresses"];
-            SmtpClientHost = ConfigurationManager.AppSettings["SmtpClientHost"];
-            SmtpClientPort = Convert.ToInt32(ConfigurationManager.AppSettings["SmtpClientPort"]);
-            SmtpClientUsername = ConfigurationManager.AppSettings["SmtpClientUsername"];
-            SmtpClientPassword = ConfigurationManager.AppSettings["SmtpClientPassword"];
+            try
+            {
+                string serviceSettingsXmlFilePath = AppDomain.CurrentDomain.BaseDirectory + "\\Settings.xml";
+                string settingsXmlString = File.ReadAllText(serviceSettingsXmlFilePath);
+                Settings = settingsXmlString.ParseXML<Settings>();
+            }
+            catch (Exception exception)
+            {
+                WriteToLogsFile("Error While Reading Service Settings With Exception: " + exception.Message);
+                this.Stop();
+            }
         }
-
-        protected string BuildWarningMessage(string driveName, long driveTotalSize, long driveTotalFreeSpace)
+        
+        protected string BuildWarningMessageBody(string driveLabelWithName, long driveTotalSize, long driveTotalFreeSpace)
         {
-            string warningMessage = "The Drive " + driveName + " Have Free Space Less Than " + PercentageOfTotalDriveSpace.ToString() + "% Of It's Total Space.\n";
-            warningMessage += "Total Space: " + ConvertBytesToGigabytes(driveTotalSize).ToString() + " GB.\n";
-            warningMessage += "Total Free Space: " + ConvertBytesToGigabytes(driveTotalFreeSpace).ToString() + " GB.\n";
+            string warningMessage = "Low Free Space Was Detected In Drive " + driveLabelWithName + " That In Server " + Environment.MachineName + ".\n";
+            warningMessage += "Total Space: " + Math.Round(ConvertBytesToGigabytes(driveTotalSize), 2).ToString() + " GB.\n";
+            warningMessage += "Total Free Space: " + Math.Round(ConvertBytesToGigabytes(driveTotalFreeSpace), 2).ToString() + " GB.\n";
             warningMessage += "\n" + "From Logitude Server Health Service.";
 
             return warningMessage;
         }
 
-        protected void SendEmails(string subject, string message)
+        protected void SendEmails(string subject, string body)
         {
             try
             {
-                SmtpClient smtpClient = new SmtpClient(SmtpClientHost);
+                System.Net.Mail.SmtpClient smtpClient = new System.Net.Mail.SmtpClient(Settings.EmailSettings.SmtpClient.Host);
                 MailMessage mailMessage = new MailMessage();
 
-                mailMessage.From = new MailAddress(FromAddress);
-                foreach (var emailAddress in ToAddresses.Split(',').ToList())
+                mailMessage.From = new MailAddress(Settings.EmailSettings.From.Address);
+                foreach (var emailAddress in Settings.EmailSettings.To.Addresses.Split(',').ToList())
                 {
                     mailMessage.To.Add(emailAddress);
                 }
                 mailMessage.BodyEncoding = Encoding.UTF8;
                 mailMessage.Subject = subject;
-                mailMessage.Body = message;
-                smtpClient.Port = SmtpClientPort;
-                smtpClient.Credentials = new NetworkCredential(SmtpClientUsername, SmtpClientPassword);
+                mailMessage.Body = body;
+                smtpClient.Port = Settings.EmailSettings.SmtpClient.Port;
+                smtpClient.Credentials = new NetworkCredential(Settings.EmailSettings.SmtpClient.Username, Settings.EmailSettings.SmtpClient.Password);
                 smtpClient.Send(mailMessage);
 
-                WriteToLogsFile("Warning Message Sent Successfully To " + ToAddresses + " At " + GetCurrentDateTime(true));
+                WriteToLogsFile("Warning Message Sent Successfully To " + Settings.EmailSettings.To.Addresses);
             }
             catch (Exception exception)
             {
-                WriteToLogsFile("Error While Sending Warning Message At " + GetCurrentDateTime(true) + " With Exception: " + exception.Message);
+                WriteToLogsFile("Error While Sending Warning Message With Exception: " + exception.Message);
             }
         }
 
@@ -117,27 +122,24 @@ namespace Logitude.ServerHealthService
         {
             try
             {
-                string logsFileDirectory = "C:" + "\\Logitude.ServerHealthService.Logs";
-
-                if (!Directory.Exists(logsFileDirectory))
+                string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+                if (!Directory.Exists(path))
                 {
-                    Directory.CreateDirectory(logsFileDirectory);
+                    Directory.CreateDirectory(path);
                 }
-
-                string logsFilePath = logsFileDirectory + "\\ServerHealthServiceLogs_" + GetCurrentDateTime(false).Replace('/', '_') + ".txt";
-
-                if (!File.Exists(logsFilePath))
+                string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + GetCurrentDateTime(false).Replace('/', '_') + ".txt";
+                if (!File.Exists(filepath))
                 {
-                    using (StreamWriter streamWriter = File.CreateText(logsFilePath))
+                    using (StreamWriter streamWriter = File.CreateText(filepath))
                     {
-                        streamWriter.WriteLine(log);
+                        streamWriter.WriteLine(log.TrimEnd('.') + ". At " + GetCurrentDateTime(true));
                     }
                 }
                 else
                 {
-                    using (StreamWriter streamWriter = File.AppendText(logsFilePath))
+                    using (StreamWriter streamWriter = File.AppendText(filepath))
                     {
-                        streamWriter.WriteLine(log);
+                        streamWriter.WriteLine(log.TrimEnd('.') + ". At " + GetCurrentDateTime(true));
                     }
                 }
             }
@@ -158,10 +160,79 @@ namespace Logitude.ServerHealthService
             return DateTime.Now.ToString(dateTimeFormat);
         }
 
-        protected double ConvertBytesToGigabytes(long bytes)
+        protected double ConvertBytesToGigabytes(double bytes)
         {
             double gigabytes = bytes * 9.31 * Math.Pow(10, -10);
             return gigabytes;
+        }
+
+        protected double ConvertGigabytesToBytes(double gigabytes)
+        {
+            double bytes = gigabytes / (9.31 * Math.Pow(10, -10));
+            return bytes;
+        }
+
+        protected bool ShouldSendEmails()
+        {
+            if(LastEmailAlert == null)
+            {
+                return true;
+            }
+            else
+            {
+                if((DateTime.Now - LastEmailAlert.Value).TotalSeconds >= Settings.GeneralSettings.EmailAlertTimer.IntervalInSeconds)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        protected bool IsDriveSpaceLow(DriveInfo drive)
+        {
+            bool isDriveSpaceLow = false;
+            long driveTotalSize = drive.TotalSize;
+            long driveTotalFreeSpace = drive.TotalFreeSpace;
+
+            if (Settings.DrivesSettings.Drives.Count == 0)
+            {
+                double percentageOfTotal = ((double)10 / 100) * driveTotalSize;
+                if (driveTotalFreeSpace < percentageOfTotal)
+                {
+                    isDriveSpaceLow = true;
+                }
+            }
+            else
+            {
+                Drive driveToCheck = Settings.DrivesSettings.Drives.Where(d => drive.Name.ToLower().Contains(d.Name.ToLower())).FirstOrDefault();
+                if (driveToCheck != null)
+                {
+                    if (driveToCheck.MinimumFreeSpacePercent != 0 || driveToCheck.MinimumFreeSpaceGB != 0)
+                    {
+                        if (driveToCheck.MinimumFreeSpacePercent != 0)
+                        {
+                            double percentageOfTotal = (driveToCheck.MinimumFreeSpacePercent / 100) * driveTotalSize;
+                            if (driveTotalFreeSpace < percentageOfTotal)
+                            {
+                                isDriveSpaceLow = true;
+                            }
+                        }
+
+                        if (driveToCheck.MinimumFreeSpaceGB != 0)
+                        {
+                            if (driveTotalFreeSpace < ConvertGigabytesToBytes(driveToCheck.MinimumFreeSpaceGB))
+                            {
+                                isDriveSpaceLow = true;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return isDriveSpaceLow;
         }
     }
 }
