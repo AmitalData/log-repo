80907be558aafe82459e8f12f4a0cc11fb4236c7
Wesam <Wesam@WESAM>
2020-03-11 22:20:17 +0200

    Add card names to the Cards' filter search screens
    Related Work Items: #64466

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/DWLogSearchWindowComponent.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/DWLogSearchWindowComponent.ts
@@ -168,7 +168,9 @@ export class DWLogSearchWindowComponent extends BaseComponent implements OnInit,
    
 
         ComponentArgs.AddComponent(new ParameterComponentArgs(this.CurrentSession.Sessionkey + "DWLogSearchWindow", this));
-
+        if (args.ObjectTableName == "DIM_Partners" && args.DisplayFieldsFromList == "[Code]" && args.LOVAdditionalColumns == null) {
+            args.LOVAdditionalColumns = "[Name]"; //this is because cannot filter on name
+        }
         this.ObjectTableName = args.ObjectTableName; // lookup table
         this.ObjectFieldName = args.DisplayFieldsFromList;
         this.LOVAdditionalColumns = this.BuildAdditionalColumns(args.LOVAdditionalColumns);
