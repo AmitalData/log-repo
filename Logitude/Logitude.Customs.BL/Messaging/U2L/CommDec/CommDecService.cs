96324af43157802680728f190a344947209ab80b
moran <moran@amital.co.il>
2020-03-11 16:11:17 +0200

    64723 MS
    Related Work Items: #64723

--- Logitude/Logitude.Customs.BL/Messaging/U2L/CommDec/CommDecService.cs
+++ Logitude/Logitude.Customs.BL/Messaging/U2L/CommDec/CommDecService.cs
@@ -494,6 +494,11 @@ namespace Logitude.Customs.BL.Messaging.U2L.CommDec
                     LogMessagingUtil.Instance.AppendLine("Set Courier Pending Reason Code To " + declarationPendingCode);
                     if (currentDeclarationCourierStatusPM.ChangeSetOp != ChangeSetOperation.Update) currentDeclarationCourierStatusPM.ChangeSetOp = ChangeSetOperation.Update;
                 }
+                if (currentDeclarationCourierStatusPM.ChangeSetOp == ChangeSetOperation.Update)
+                {
+                    DeclarationCourierStatusUpdateService declarationCourierStatusUpdateService = new DeclarationCourierStatusUpdateService(_context, new Dictionary<string, IContext>(), _MyDeclarationPM.Tenant);
+                    declarationCourierStatusUpdateService.Update(currentDeclarationCourierStatusPM, true);
+                }
             }
         }
 
