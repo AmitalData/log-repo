86af8d654de4ecaf7d26181be63bf1453377f794
alaa <alaa.jamal.h@live.com>
2020-03-16 20:38:23 +0200

    64704
    Related Work Items: #64704

--- Logitude/Logitude.BL/InvoiceModel/Tools/Validating/ARPaymentValidator.cs
+++ Logitude/Logitude.BL/InvoiceModel/Tools/Validating/ARPaymentValidator.cs
@@ -413,7 +413,13 @@ namespace Logitude.BL.InvoiceModel.Tools.Validating
                         string rmsg = TranslateTextsClass.Translate("General.M.FieldIsRequired", tenant, showLocal);
                         errors += rmsg.Replace("%FieldName", TranslateTextsClass.Translate("ARPayment.F.Bank", tenant, useLocal)) + ";";
                     }
-                    if(code  == "CH" && valueDate != null && registerDate != null)
+                    if(code != "CA" && valueDate == null)
+                    {
+                        string rmsg = TranslateTextsClass.Translate("General.M.FieldIsRequired", tenant, showLocal);
+                        errors += rmsg.Replace("%FieldName", TranslateTextsClass.Translate("ARPayment.F.ValueDate", tenant, useLocal)) + ";";
+
+                    }
+                    if (code  == "CH" && valueDate != null && registerDate != null)
                     {
                         ValidateValueDate(valueDate, registerDate , tenant);
                     }
