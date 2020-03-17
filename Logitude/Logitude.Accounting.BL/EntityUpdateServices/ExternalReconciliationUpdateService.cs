d7a73e8fda1000a0eeb3b278f44e14d1776d5798
Abdullah Dawoud <abdullah@logitudeworld.com>
2020-03-17 15:57:32 +0200

    canot reconcile twoe transfer transactions at the same time
    Related Work Items: #64329

--- Logitude/Logitude.Accounting.BL/EntityUpdateServices/ExternalReconciliationUpdateService.cs
+++ Logitude/Logitude.Accounting.BL/EntityUpdateServices/ExternalReconciliationUpdateService.cs
@@ -244,12 +244,32 @@ namespace Logitude.Accounting.BL.EntityUpdateServices
 
             base.Trace(entityPM, entityPOCO, changesXml);
         }
-        protected override void Validate(ExternalReconciliationPM entityPM)
+        protected override void Validate(ExternalReconciliationPM externalRecoPM)
         {
 
-            CheckDifferenc(entityPM);
+            CheckDifferenc(externalRecoPM);
+            CheckTransferTransactions(externalRecoPM);
 
-            base.Validate(entityPM);
+            base.Validate(externalRecoPM);
+        }
+
+        private static void CheckTransferTransactions(ExternalReconciliationPM externalRecoPM)
+        {
+            BankAccountPM bankAccount = GetBankAccount(externalRecoPM);
+
+            if (externalRecoPM.ExternalReconciliationLines.Count(d => d.LedgerGLAccountId == bankAccount.TransferGLAcccountId) > 1)
+            {
+                var msg = TextCodesTranslator.TranslateText("ExternalReconciliation.O.CantReconcileTwoTransfer", 0,
+                    LoggedContactResolver.GetLoggedContactShowLocal(externalRecoPM.Tenant));
+                throw new ApplicationException(msg);
+            }
+        }
+
+        private static BankAccountPM GetBankAccount(ExternalReconciliationPM externalRecoPM)
+        {
+            BankAccountQueryService bankAccountQueryService = new BankAccountQueryService(externalRecoPM.Tenant);
+            BankAccountPM bankAccount = bankAccountQueryService.GetSingle(externalRecoPM.BankAccountId, false, false);
+            return bankAccount;
         }
 
         void CheckDifferenc(ExternalReconciliationPM entityPM)
