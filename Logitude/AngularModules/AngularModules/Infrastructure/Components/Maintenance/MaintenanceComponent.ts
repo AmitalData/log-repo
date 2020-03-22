c26ee8c79a8ed01c90294d498a27eab1664964be
elisheva <elisheva@amital.co.il>
2020-03-22 11:11:23 +0200

    added Re-analysis menu task 64482
    Related Work Items: #64482

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/Maintenance/MaintenanceComponent.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/Maintenance/MaintenanceComponent.ts
@@ -159,7 +159,7 @@ export class MaintenanceComponent {
             item2.ObjectTableName = "Terms of Use";
             this.AllMaintenanceMenu.push(new MaintenanceMenuItem(item2));
         }
-
+   
         if (FeatureLocator.HasFeaturePermession("General", "SYSTEMSETTINGS")) {
 
             if (FeatureLocator.HasFeaturePermession("General", "General.Features.CompanyAddress")) {
@@ -477,6 +477,17 @@ export class MaintenanceComponent {
             item.ObjectTableId = window.ObjectTables.filter(d => d.Name == "Customs.CustomsSetting")[0].Id
             this.AllMaintenanceMenu.push(new MaintenanceMenuItem(item));
         }
+
+       
+        var item = new MenusTablePM();
+        item.CategoryTypeCode = "CMS";
+        item.Icon = "Settings"
+        item.Code = "CSRA";
+        item.ObjectTableName = "Re-request Analysis";
+        this.AllMaintenanceMenu.push(new MaintenanceMenuItem(item));
+             
+
+
     }
     private BuildAccountingMenus() {
 
@@ -743,6 +754,38 @@ export class MaintenanceComponent {
                     logWindow.Title = windowTitle;
                     logWindow.IsShowCloseButton = true;
                     logWindow.Show('./CustomsModules/CustomsMaintenance/Components/Maintenance/CustomsSettingsComponent');
+                    break;
+                }
+                case "CSRA": {
+                    let test = true;
+                    let strict = true;
+                    if (test) {
+                        if (DateTool.GetCurrentDateAsUtc().valueOf() < new Date(2017, 7, 20).valueOf()) {
+                            strict = false;
+                        }
+                    }
+                    let LoggedUserPMCode = SessionLocator.LoggedUserPM.Code || "";
+                    LoggedUserPMCode = LoggedUserPMCode.toLowerCase();
+                    let allowed = false;
+                    allowed = (LoggedUserPMCode == "amital" || LoggedUserPMCode.startsWith("amital."));
+                    if (strict && !SessionLocator.LoggedUserPM.IsCustomerCare && allowed) {
+
+                        let messageWindow = new MessageWindow()
+                        messageWindow.Show("Logged User Is not Customer Care ");
+                        return;
+                    }
+
+                    //SessionLocator.DynamicLoader.Load('./CustomsModules/CustomsControls/Components/CustomsRequestsSheetsComponent', this.CurrentSession.SessionMenuLocation.viewContainerRef)
+                    //    .then(cmpRef => {
+                    //        cmpRef.instance.ComponentRef = cmpRef;
+                    //        cmpRef.instance.Run(listArgs);
+                    //        //this.CurrentSession.AddMenuReference(cmpRef);
+                    //    });
+                  
+                        
+                
+
+ 
                     break;
                 }
                 case "COAD": {
