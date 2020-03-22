ecde8ece7c23d895f905d82e814660ec85558e9a
Ahmad Baker <ahmadb@logitudeworld.com>
2020-03-22 22:03:23 +0200

    fix build communication woker role

--- Logitude/CommunicationWorkerRole/ForwarderShipmentDocumentsWorkerRole.cs
+++ Logitude/CommunicationWorkerRole/ForwarderShipmentDocumentsWorkerRole.cs
@@ -394,7 +394,7 @@ namespace CommunicationWorkerRole
                                                     result.Wait();
                                                     if (result.Result.StatusCode == System.Net.HttpStatusCode.OK)
                                                     {
-                                                        var temp1 = result.Content.ReadAsStringAsync().Result;
+                                                        var temp1 = result.Result.Content.ReadAsStringAsync().Result;
                                                         var Donemsg = "New Document Sent To Forwarder Successfully " + DateTime.Now;
                                                         APILogsUtility.UpdateAPILogStatus(LogPM.Id, tenant, "D", response.RetryNumber + 1, DateTime.Now, DateTime.UtcNow, Donemsg, null, temp1, null, "");
                                                         queueservice.Complete();
--- Logitude/CommunicationWorkerRole/ForwarderShipmentWorkerRole.cs
+++ Logitude/CommunicationWorkerRole/ForwarderShipmentWorkerRole.cs
@@ -365,7 +365,7 @@ namespace CommunicationWorkerRole
                                             result.Wait();
                                             if (result.Result.StatusCode == System.Net.HttpStatusCode.OK)
                                             {
-                                                var temp1 = result.Content.ReadAsStringAsync().Result;
+                                                var temp1 = result.Result.Content.ReadAsStringAsync().Result;
                                                 msg = "Shipment sent To Forwarder " + DateTime.Now;
                                                 APILogsUtility.UpdateAPILogStatus(LogPM.Id, tenant, "D", response.RetryNumber + 1, DateTime.Now, DateTime.UtcNow, msg, LogitudeXmlSerializer.SerializeObjectToXmlString(shipmentAM), temp1, null, "");
                                                 queue.Complete();
