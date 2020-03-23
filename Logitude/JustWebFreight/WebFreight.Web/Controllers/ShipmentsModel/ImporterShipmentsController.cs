e13fef1f593b90f0869e7070812452ee5a85e6ab
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2020-03-23 18:43:15 +0200

    fix

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/ShipmentsModel/ImporterShipmentsController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/ShipmentsModel/ImporterShipmentsController.cs
@@ -341,6 +341,10 @@ namespace WebFreight.Web.Controllers.ShipmentsModel
                     if (!string.IsNullOrEmpty(Shipment.CustomerShipmentNumber))
                     {
                         ImporterShipment = shipmentQuery.GetSingleShipmentPMByNumber(Shipment.CustomerShipmentNumber, Shipment.ImporterTenant);
+                        if (ImporterShipment == null)
+                        {
+                            ImporterShipment = shipmentQuery.GetSingleShipmentPMByForwarderNumber(Shipment.ForwarderShipmentNumber, Shipment.ImporterTenant, Shipment.Tenant); 
+                        }
                     }
                     else
                     {
@@ -551,7 +555,7 @@ namespace WebFreight.Web.Controllers.ShipmentsModel
         {
             APIException Responce = new APIException();
             //TenantQuery myTenantQuery = new TenantQuery();
-            TenantPM currentTenant = TenantQuery.GetSingleTenantPM(entityAM.ImporterTenant,false);
+            TenantPM currentTenant = TenantQuery.GetSingleTenantPM(entityAM.ImporterTenant, false);
             ICommonDataContext commoncontext = CommonDataContext.GetContext(entityAM.ImporterTenant);
             HybridPartnerRepository hybridPartnerRepository = new HybridPartnerRepository(commoncontext);
             HybridPartnerQuery HybridPartnerQuerey = new HybridPartnerQuery(hybridPartnerRepository);
@@ -881,7 +885,7 @@ namespace WebFreight.Web.Controllers.ShipmentsModel
             entityPM.OnCarriageATD = entityAM.OnCarriageATD;
             entityPM.PreCarriageATA = entityAM.PreCarriageATA;
             entityPM.PreCarriageATD = entityAM.PreCarriageATD;
-           
+
             entityPM.DimensionsUnitCode = entityAM.DimensionsUnitCode;
             entityPM.GrossWeightUnitCode = entityAM.GrossWeightUnitCode;
             entityPM.ChargeableWeightUnitCode = entityAM.ChargeableWeightUnitCode;
@@ -907,7 +911,7 @@ namespace WebFreight.Web.Controllers.ShipmentsModel
 
             }
             entityPM.IsImporterApprovalRequired = entityAM.IsImporterApprovalRequired;
-            
+
             if (currentTenant.AutoArchiveOnInvoice == true && entityAM.OriginalStatusCode == "INPR" && entityAM.CustomsClearanceDate != null && entityPM.IsOperationalClosed == false)
             {
                 entityPM.IsOperationalClosed = true;
@@ -933,7 +937,7 @@ namespace WebFreight.Web.Controllers.ShipmentsModel
                 entityPM.ApproveDateTime = TenantServerConfigration.GetCurrentDateTime(entityPM.Tenant);
                 entityPM.VersionApproved = entityAM.VersionApproved;
             }
-      
+
             entityPM.CustomsClearanceDate = entityAM.CustomsClearanceDate;
             if (Partner != null)
             {
