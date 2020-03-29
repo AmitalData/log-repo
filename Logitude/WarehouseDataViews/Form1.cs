6dae8d3e9a1602cef0696addb3bab6ffd7b9bc49
Abed <abed@logitudeworld.com>
2020-03-29 19:18:52 +0300

    64949 65041
    Related Work Items: #64949, #65041

--- Logitude/WarehouseDataViews/Form1.cs
+++ Logitude/WarehouseDataViews/Form1.cs
@@ -13,9 +13,9 @@ namespace WarehouseDataViews
 {
     public partial class Form1 : Form
     {
-        string dbSourceConnection = "Logitude2-5_Main,sa,Saas256,.";
-        string dbDestinationConnection = "Logitude2-5_Global,sa,Saas256,.";
-
+        private string dbSourceConnection = "Logitude2-5_Main,sa,Saas256,.";
+        private string dbDestinationConnection = "Logitude2-5_Global,sa,Saas256,.";
+        private int? tenant = 1;
         //string dbSourceConnection = "LogitudeMain,logitudemanager,!LO852456,ebup282itq.database.windows.net";
         //string dbDestinationConnection = "UnicargoDW, UnicargoDBUser,Y&P95et1,logitude-ep.database.windows.net";
 
@@ -24,6 +24,7 @@ namespace WarehouseDataViews
             InitializeComponent();
             this.SourceConnectionTextBox.Text = dbSourceConnection;
             this.DestinationConnectionTextBox.Text = dbDestinationConnection;
+            this.TenantTextBox.Text = tenant.ToString() ;
         }
 
         private void CreateViewsButton_Click(object sender, EventArgs e)
@@ -35,13 +36,13 @@ namespace WarehouseDataViews
             try
             {
                 ResultLabel.Text = "";
-                if (!string.IsNullOrEmpty(dbSourceConnection) && !string.IsNullOrEmpty(dbDestinationConnection))
+                if (!string.IsNullOrEmpty(dbSourceConnection) && !string.IsNullOrEmpty(dbDestinationConnection) && tenant !=null)
                 {
-                    WarehouseViewsService warehouseViewsService = new WarehouseViewsService();
+                    WarehouseViewsService warehouseViewsService = new WarehouseViewsService((int)tenant);
                     string sourceConnectionString = warehouseViewsService.BuildConnectionString(dbSourceConnection);
                     string destinationConnectionString = warehouseViewsService.BuildConnectionString(dbDestinationConnection);
-                    CreateDimensionViews(sourceConnectionString, destinationConnectionString);
-                    CreateFactViews(warehouseViewsService, sourceConnectionString, destinationConnectionString);
+                    warehouseViewsService.CreateAllDimensionViews(sourceConnectionString, destinationConnectionString);
+                    warehouseViewsService.CreateFactShipmentView(sourceConnectionString, destinationConnectionString);
                     SetResultLable(true);
                 }
                 else MessageBox.Show("Connection Problem");
@@ -51,38 +52,8 @@ namespace WarehouseDataViews
                 DisplayExceptionMessage(ex);
             }
         }
-        private void CreateDimensionViews(string sourceConnectionString, string destinationConnectionString)
-        {
-            WarehouseViewsService warehouseViewsService = new WarehouseViewsService();
-            DataTable dimensionDWobjectFieldOnFact = warehouseViewsService.GetDimensionDWobjectFieldOnFactShipment(sourceConnectionString);
-            foreach (DataRow row in dimensionDWobjectFieldOnFact.Rows)
-            {
-                string fieldCode=  row["Code"].ToString();
-                string dimensionTableCode = row["DimensionTableCode"].ToString();
-                if (!string.IsNullOrEmpty(fieldCode) && fieldCode!= "[Parent Tenant]" &&  !string.IsNullOrEmpty(dimensionTableCode))
-                {
-                    if (dimensionTableCode != "DIM_Dates")
-                    {
-                        if (fieldCode == "[Notify 1]") fieldCode = "[Notify One]";
-                        else if (fieldCode == "[Notify 2]") fieldCode = "[Notify Two]";
-                        warehouseViewsService.DropView(fieldCode, destinationConnectionString);
-                        warehouseViewsService.CreateDimensionView(fieldCode, dimensionTableCode, destinationConnectionString);
-                    }
-                  //  warehouseViewsService.GrantView(fieldCode, destinationConnectionString);
-                }
-            }
-        }
 
-
-
-        private  void CreateFactViews(WarehouseViewsService warehouseViewsService,string sourceConnection,  string destinationConnectionString)
-        {
-            warehouseViewsService.DropView("Shipment", destinationConnectionString);
-            warehouseViewsService.CreateFactView("Fact_Shipments", sourceConnection , destinationConnectionString);
-           // warehouseViewsService.GrantView("Shipment", destinationConnectionString);
-        }
-
-
+       
         private void DeleteViewsButton_Click(object sender, EventArgs e)
         {
             DeleteDataWarehouseViews();
@@ -92,13 +63,13 @@ namespace WarehouseDataViews
             try
             {
                 ResultLabel.Text = "";
-                if (!string.IsNullOrEmpty(dbSourceConnection) && !string.IsNullOrEmpty(dbDestinationConnection))
+                if (!string.IsNullOrEmpty(dbSourceConnection) && !string.IsNullOrEmpty(dbDestinationConnection) && tenant!=null)
                 {
-                    WarehouseViewsService warehouseViewsService = new WarehouseViewsService();
+                    WarehouseViewsService warehouseViewsService = new WarehouseViewsService((int)tenant);
                     string sourceConnectionString = warehouseViewsService.BuildConnectionString(dbSourceConnection);
                     string destinationConnectionString = warehouseViewsService.BuildConnectionString(dbDestinationConnection);
-                    DeleteDimensionViews(sourceConnectionString, destinationConnectionString);
-                    warehouseViewsService.DropView("Shipment", destinationConnectionString);
+                    warehouseViewsService.DeleteDimensionViews(sourceConnectionString, destinationConnectionString);
+                    warehouseViewsService.DropView("ShipmentView", destinationConnectionString);
                     SetResultLable(true);
                 }
                 else MessageBox.Show("Connection Problem");
@@ -110,17 +81,7 @@ namespace WarehouseDataViews
             }
 
         }
-        private  void DeleteDimensionViews( string sourceConnectionString, string destinationConnectionString)
-        {
-            WarehouseViewsService warehouseViewsService = new WarehouseViewsService();
-            DataTable dimensionDWobjectFieldOnFact = warehouseViewsService.GetDimensionDWobjectFieldOnFactShipment(sourceConnectionString);
-            foreach (DataRow row in dimensionDWobjectFieldOnFact.Rows)
-            {
-                string fieldCode  = row["Code"].ToString();
-                if (!string.IsNullOrEmpty(fieldCode)) warehouseViewsService.DropView(fieldCode, destinationConnectionString);
-            }
-        }
-
+      
 
         private void DisplayExceptionMessage(Exception ex)
         {
@@ -146,5 +107,13 @@ namespace WarehouseDataViews
             this.dbDestinationConnection = destinationConnectionTextBox.Text;
         }
 
+        private void TenantTextBox_TextChanged(object sender, EventArgs e)
+        {
+            TextBox destinationConnectionTextBox = sender as TextBox;
+            if (!string.IsNullOrEmpty(destinationConnectionTextBox.Text))
+            {
+                this.tenant = Int32.Parse(destinationConnectionTextBox.Text);
+            }
+        }
     }
 }
---
+++ Logitude/WarehouseDataViews/Service/GeneralWarehouseViewsService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseDataViews.Service
+{
+  public  class GeneralWarehouseViewsService
+    {
+        long timeOut = 10000000000000000;
+        public string BuildConnectionString(string dbSourceConnection)
+        {
+            var connectionStringArray = dbSourceConnection.Split(',');
+            string catalog = connectionStringArray[0];
+            string userName = connectionStringArray[1];
+            string password = connectionStringArray[2];
+            string server = connectionStringArray[3];
+            string result = "Data Source=" + server + ";Initial Catalog=" + catalog + ";Integrated Security=False;Persist Security Info=True;User ID=" + userName + ";Password= " + password + ";MultipleActiveResultSets=True;Connect Timeout=60";
+            return result;
+        }
+
+        public string GetViewName(string fieldCode)
+        {
+            string viewName = fieldCode;
+            viewName = viewName.Replace("[", "");
+            viewName = viewName.Replace("]", "");
+            viewName = viewName.Replace(" ", "");
+            viewName += "View";
+            return viewName;
+        }
+
+        public string ReadScriptFile(string fileName)
+        {
+            string result = string.Empty;
+            string path = System.IO.Path.GetDirectoryName(new System.Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).LocalPath);
+            if (path.Contains(@"\bin\Debug")) path = path.Replace(@"\bin\Debug", string.Empty);
+
+            string fileDirectory = Path.Combine(path, "WarehouseViewsScript\\", fileName + ".sql");
+            FileInfo file = new FileInfo(fileDirectory);
+            result = file.OpenText().ReadToEnd();
+            return result;
+        }
+
+        public void ExecuteSql(string sqlString, string connectionString)
+        {
+
+            if (!string.IsNullOrEmpty(sqlString))
+            {
+                using (SqlConnection cn = new SqlConnection(connectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(sqlString, cn);
+                    sqlCommand.CommandTimeout = (int)timeOut;
+                    cn.Open();
+                    sqlCommand.ExecuteNonQuery();
+                    cn.Close();
+                }
+            }
+        }
+
+        public string ConvertFieldsNameToCamelCase(string scriptstring)
+        {
+            string result = scriptstring;
+            string[] sqlArray = result.Split(new string[] { "SELECT" }, StringSplitOptions.None);
+            sqlArray = sqlArray[1].Split(new string[] { "FROM" }, StringSplitOptions.None);
+            var allFields = sqlArray[0].Split(',');
+            foreach (string fieldName in allFields)
+            {
+                if (!string.IsNullOrEmpty(fieldName))
+                {
+                    if (!fieldName.Contains("Key") && !fieldName.Contains("Id_Number") && !fieldName.Contains("@CustomFields"))
+                    {
+                        string fieldNameCamelCase = fieldName + "as " + fieldName.Replace(" ", "");
+                        result = result.Replace(fieldName, fieldNameCamelCase);
+                    }
+                }
+            }
+            return result;
+        }
+
+    }
+}
