9f71490cfe8a6d10bbb8b9b7d3e21618576a533f
elisheva <elisheva@amital.co.il>
2020-03-29 16:11:44 +0300

    bug 64853
    Related Work Items: #64853

--- Logitude/Logitude.Customs.BL/BL/CalculateDeclarationCourierStatus.cs
+++ Logitude/Logitude.Customs.BL/BL/CalculateDeclarationCourierStatus.cs
@@ -351,25 +351,25 @@ namespace Logitude.Customs.BL.BL
             //List<CustomsDocumentsTicketPM> customsDocumentsTicketPMList = myCustomsDocumentsTicketQueryService.GetCustomsDocumentsTicketPMsByEntityIdAndChilds(declarationPM.Id, "", "", "", myDeclarationCourierStatusPM.Tenant, "Declaration").Where(r => r.DocumentStatusCode != "1").ToList();
 
             List<CustomsDocumentsTicketPM> customsDocumentsTicketPMList = myCustomsDocumentsTicketQueryService.GetCustomsDocumentsTicketPMsByEntityIdAndChilds(declarationPM.Id, "", "", "", myDeclarationCourierStatusPM.Tenant, "Declaration");
-            List<CustomsDocumentsTicketPM> customsDocumentsTicketPMListNotSend = customsDocumentsTicketPMList.Where(r => r.DocumentStatusCode != "1").ToList();
-            if (customsDocumentsTicketPMListNotSend == null || customsDocumentsTicketPMListNotSend.Count() < 1)
-            {
-                foreach (CustomsDocumentsTicketPM customsDocumentsTicketPMItem in customsDocumentsTicketPMListNotSend)
-                {
-                    CustomDocumentTypePM docType = docTypeQuery.GetSingle(customsDocumentsTicketPMItem.DocumentTypeCode, false, false);
-                    if (docType.IsCourierManadatory)
-                    {
-                        return true;
-                    }                      
-                }
-            }
+            //List<CustomsDocumentsTicketPM> customsDocumentsTicketPMListNotSend = customsDocumentsTicketPMList.Where(r => r.DocumentStatusCode != "1").ToList();
+            //if (customsDocumentsTicketPMListNotSend != null && customsDocumentsTicketPMListNotSend.Count() >= 1)
+            //{
+            //    foreach (CustomsDocumentsTicketPM customsDocumentsTicketPMItem in customsDocumentsTicketPMListNotSend)
+            //    {
+            //        CustomDocumentTypePM docType = docTypeQuery.GetSingle(customsDocumentsTicketPMItem.DocumentTypeCode, false, false);
+            //        if (docType.IsCourierManadatory)
+            //        {
+            //            return true;
+            //        }                      
+            //    }
+            //}
 
             List<CustomDocumentTypePM> CustomDocumentTypePMList = docTypeQuery.GetMandatoryCustomDocumentTypesForCourier(declarationPM.Tenant);
             if (CustomDocumentTypePMList != null)
             {
                 foreach (CustomDocumentTypePM customDocumentTypePMItem in CustomDocumentTypePMList)
                 {
-                    CustomsDocumentsTicketPM customsDocumentsTicketPM = customsDocumentsTicketPMList.Where(d => d.DocumentTypeCode == customDocumentTypePMItem.Code).FirstOrDefault();
+                    CustomsDocumentsTicketPM customsDocumentsTicketPM = customsDocumentsTicketPMList.Where(d => d.DocumentTypeCode == customDocumentTypePMItem.Code && d.DocumentsFilingId != null && d.DocumentStatusCode=="1" ).FirstOrDefault();
                     if (customsDocumentsTicketPM == null)
                     {
                         return true;
