a46cb59d3e209222e9b3583c36a5100cc616a982
Ayman1 <Ayman@logitudeworld.com>
2020-03-31 20:07:20 +0300

    ayman

--- Logitude/AngularModules/AngularModules/SharedLogistics/Components/SharedLogisticsMainComponent.ts
+++ Logitude/AngularModules/AngularModules/SharedLogistics/Components/SharedLogisticsMainComponent.ts
@@ -144,7 +144,7 @@ export class SharedLogisticsMainComponent implements OnInit {
     }
 
     LoadCardData() {
-        this._sharedLogisticsService.getSharedLogisticsStatistics(SessionInfo.LoggedUserTenant).subscribe(res => {
+        this._sharedLogisticsService.getSharedLogisticsStatistics(SessionInfo.LoggedUserTenant).subscribe((res:any) => {
             var pmResponse: ServiceResponse = res;
             if (!pmResponse.HasError) {
                 var myResult = pmResponse.Result;
@@ -199,7 +199,7 @@ export class SharedLogisticsMainComponent implements OnInit {
     }
 
     LoadSharedLogisticsSummary() {
-        this._sharedLogisticsService.getSharedLogisticsSummaryData(SessionInfo.LoggedUserTenant).subscribe(res => {
+        this._sharedLogisticsService.getSharedLogisticsSummaryData(SessionInfo.LoggedUserTenant).subscribe((res: any) => {
             var pmResponse: ServiceResponse = res;
             if (!pmResponse.HasError) {
                 this.sharedLogisticsSummary = pmResponse.Result;
@@ -238,7 +238,7 @@ export class SharedLogisticsMainComponent implements OnInit {
     }
 
     LoadLastLoginPartners() {
-        this._sharedLogisticsService.getLastLoginPartners(SessionInfo.LoggedUserTenant).subscribe(res => {
+        this._sharedLogisticsService.getLastLoginPartners(SessionInfo.LoggedUserTenant).subscribe((res: any) => {
             var pmResponse: ServiceResponse = res;
             if (!pmResponse.HasError) {
                 this.LastPartnersList = pmResponse.Result;
