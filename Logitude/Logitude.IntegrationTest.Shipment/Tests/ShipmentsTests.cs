7a4a363c4af5ce18a4e129bfe40f7cec2f8e2b32
Lana Daoud <Lana@logitudeworld.com>
2020-03-31 13:33:38 +0300

    fix

--- Logitude/Logitude.IntegrationTest.Shipment/Tests/ShipmentsTests.cs
+++ Logitude/Logitude.IntegrationTest.Shipment/Tests/ShipmentsTests.cs
@@ -8,6 +8,7 @@ using Logitude.IntegrationTest.Core.Login;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Logitude.IntegrationTest.Shipment.Tests;
 
+
 namespace Logitude.IntegrationTest.Shipment
 {
     [TestClass]
@@ -15,28 +16,34 @@ namespace Logitude.IntegrationTest.Shipment
     {
 
         ShipmentPM shipmentPM = new ShipmentPM();
+        CreateShipment shipment = new CreateShipment();
         [TestMethod]
         public async Task TestDirectExportAirShipment()
         {
-            await PostShipment("D", "E", "A");
-            shipmentPM = await GetShipment(ShipmentVariables.ShipmentId);
+
+            //shipment.PostShipment
+            await shipment.PostShipment("D", "E", "A");
+            shipmentPM = await shipment.GetShipment(ShipmentVariables.ShipmentId);
             //ShipmentPM entityPM= UpdateShipmentAirExport(shipmentPM);
             //await TestReceivables(shipmentPM, quantity, unitPrice);
         }
-        [TestMethod] 
+        [TestMethod]
         public async Task CalculateShipmentTotalProfit()
         {
             int quantity = 1;
             int PayableUnitPrice = 50;
             int RecUnitPrice = 20;
-            await PostShipment("D", "E", "A");
-            shipmentPM = await GetShipment(ShipmentVariables.ShipmentId);
-            ShipmentPM entityPM = UpdateReceivables(shipmentPM, quantity, RecUnitPrice);
-            entityPM = UpdatePayables(entityPM, quantity, PayableUnitPrice);
-            await PutShipment(entityPM);
+            await shipment.PostShipment("D", "E", "A");
 
-          
-           // shipmentPM = await GetShipment(ShipmentVariables.ShipmentId);
+            shipmentPM = await shipment.GetShipment(ShipmentVariables.ShipmentId);
+            ShipmentPM entityPM = shipment.UpdateReceivables(shipmentPM, quantity, RecUnitPrice);
+            entityPM = shipment.UpdatePayables(entityPM, quantity, PayableUnitPrice);
+            await shipment.PutShipment(entityPM);
+
+
+
+
+            // shipmentPM = await GetShipment(ShipmentVariables.ShipmentId);
 
         }
 
@@ -80,103 +87,5 @@ namespace Logitude.IntegrationTest.Shipment
                  throw new Exception(Ex.Message);
              }
          }*/
-        public async Task<ShipmentPM> GetShipment(string shipmentId)
-        {
-            HttpResponseMessage response = await RestClientService.GetAsync("Shipment/GetSingle?id=" + shipmentId);
-            ShipmentPM shipment = RestClientService.ParseResponse<ShipmentPM>(response);
-            Assert.IsTrue(response.StatusCode.ToString() == "OK");
-            return shipment;
-        }
-        public async Task PostShipment(string shipmentLevelCode, string directionId, string transportModeId)
-        {
-            shipmentPM = CreateShipmentPM(shipmentLevelCode, directionId, transportModeId);
-            HttpResponseMessage response = await RestClientService.PostAsync(shipmentPM, "shipment");
-            shipmentPM = RestClientService.ParseResponse<ShipmentPM>(response);
-            ShipmentVariables.ShipmentId = shipmentPM.Id;
-        }
-        public async Task PutShipment(ShipmentPM shipmentPM)
-        {
-            HttpResponseMessage response = await RestClientService.PutAsync(shipmentPM, "shipment");
-            shipmentPM = RestClientService.ParseResponse<ShipmentPM>(response);
-            await GetShipment(ShipmentVariables.ShipmentId);
-        }
-
-        /*  public void EvaluateOpenReceivablesAmount(ShipmentReceivablePM[] receivables)
-          {
-              OpenAmountInLocal = 0;
-              OpenAmpuntInProfit = 0;
-              foreach(ShipmentReceivablePM item in receivables)
-              {
-                  OpenAmountInLocal += item.TotalAmountLocal != null ? (double) item.TotalAmountLocal : 0;
-                  OpenAmpuntInProfit += item.AmountInProfitCurrency != null ? (double) item.AmountInProfitCurrency : 0;
-              }
-          }
-          public void EvaluateOpenPayablesAmount(ShipmentPayablePM[] payables)
-          {
-              OpenAmountInLocal = 0;
-              OpenAmpuntInProfit = 0;
-              foreach (ShipmentPayablePM item in payables)
-              {
-                  OpenAmountInLocal += item.OpenAmountInLocalCurrency != null ? (double) item.OpenAmountInLocalCurrency : 0;
-                  OpenAmpuntInProfit += item.OpenAmountInProfitCurrency != null ? (double) item.OpenAmountInProfitCurrency : 0;
-              }
-          }*/
-
-        private ShipmentPM CreateShipmentPM(string shipmentLevelCode, string directionId, string transportModeId)
-        {
-            shipmentPM.Tenant = IntegrationTestLoginParameters.Tenant;
-            shipmentPM.CreatedByUserId = IntegrationTestLoginParameters.LoginUserId;
-            shipmentPM.BranchId = CorePreparationVariables.BranchId;
-            shipmentPM.DepartmentId = CorePreparationVariables.DepartmentId;
-            shipmentPM.ProfitCurrencyId = CorePreparationVariables.TenantPM.ProfitCurrencyId;
-            shipmentPM.VolumeUnitCode = CorePreparationVariables.TenantPM.VolumeUnitCode;
-            shipmentPM.DimensionsUnitCode = CorePreparationVariables.TenantPM.DimensionsUnitCode;
-            shipmentPM.GrossWeightUnitCode = CorePreparationVariables.TenantPM.GrossWeightUnitCode;
-            shipmentPM.ChargeableWeightUnitCode = CorePreparationVariables.TenantPM.ChargeableWeightUnitCode;
-            shipmentPM.ShipmentLevelCode = shipmentLevelCode;
-            shipmentPM.DirectionId = directionId;
-            shipmentPM.TransportModeId = transportModeId;
-            //shipmentPM.MainCarriageTransportModeId = "";
-            shipmentPM.FreightPrepaidCollectId = "C";
-            shipmentPM.OtherPrepaidCollectId = "C";
-            shipmentPM.CreatedByUserId = CorePreparationVariables.UserId;
-            shipmentPM.UpdatedByUserId = CorePreparationVariables.UserId;
-            shipmentPM.CustomerId = ShipmentVariables.ShipperExport1;
-            shipmentPM.ShipperId = ShipmentVariables.ShipperExport1;
-            shipmentPM.IssuingCarrierAgentId = ShipmentVariables.AgentId;
-            shipmentPM.AgentId = ShipmentVariables.AgentId;
-            shipmentPM.FromPortId = ShipmentVariables.PortLHRId;
-            shipmentPM.ToPortId = ShipmentVariables.PortJFKId;
-            shipmentPM.MainCarriageFromPortId = ShipmentVariables.PortLHRId; ;
-            shipmentPM.MainCarriageToPortId = ShipmentVariables.PortJFKId;
-            shipmentPM.OriginMainCarriageFromPortId = ShipmentVariables.PortLHRId;
-            shipmentPM.AWBCurrencyId = ShipmentVariables.CurrencyEURId;
-            shipmentPM.ValueOfGoodsCurrencyId = ShipmentVariables.CurrencyEURId;
-            shipmentPM.AccountManagerUserId = CorePreparationVariables.UserId;
-
-            ShipmentVariables.ConcurrencyGUID = shipmentPM.NewConcurrencyGUID = Guid.NewGuid().ToString();
-            return shipmentPM;
-        }
-        private ShipmentPM UpdateShipmentAirExport(ShipmentPM entityPM)
-        {
-            entityPM.ConcurrencyGUID = ShipmentVariables.ConcurrencyGUID;
-            entityPM.NewConcurrencyGUID = ShipmentVariables.ConcurrencyGUID;
-            entityPM.ShipmentPackages = IntegrationShipmentPackages.ShipmentPackages();
-            //entityPM.ShipmentReceivables = IntegrationShipmentReceivable.ShipmentReceivables();
-            //entityPM.ShipmentPayables = IntegrationShipmentPayable.ShipmentPayables();
-            entityPM.ShipmentPickUps = IntegrationShipmentPickUps.ShipmentPickUps();
-            entityPM.ShipmentDeliveries = IntegrationShipmentDeliveries.shipmentDelivey();
-            return entityPM;
-        }
-        private ShipmentPM UpdateReceivables(ShipmentPM shipmentPM, int quantity, int unitPrice)
-        {
-            shipmentPM.ShipmentReceivables = IntegrationShipmentReceivable.ShipmentReceivables(quantity, unitPrice);
-            return shipmentPM;
-        }
-        private ShipmentPM UpdatePayables(ShipmentPM shipmentPM, int quantity, int unitPrice)
-        {
-            shipmentPM.ShipmentPayables = IntegrationShipmentPayable.ShipmentPayables(quantity, unitPrice);
-            return shipmentPM;
-        }
     }
 }
