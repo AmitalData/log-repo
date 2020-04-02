579d12eceeffbb3e86159064cf6b70e936519927
alaa <alaa.jamal.h@live.com>
2020-04-02 16:44:18 +0300

    SharedLogisticsContactLastLogin

--- Logitude/Simplog.Data/CommonDataModel/EntityPOCOs/Contact.cs
+++ Logitude/Simplog.Data/CommonDataModel/EntityPOCOs/Contact.cs
@@ -42,7 +42,7 @@ namespace Simplog.Data.CommonDataModel.EntityPOCOs
 
         public virtual ContactLastLogin ContactLastLogin { get; set; }
 
-        public virtual SharedLogisticsContactLastLogin SharedLogisticsContactLastLogin { get; set; }
+        //public virtual SharedLogisticsContactLastLogin SharedLogisticsContactLastLogin { get; set; }
 
         [ForeignKey("ImageDetailId")]
         public ImageDetail ImageDetail { get; set; }
