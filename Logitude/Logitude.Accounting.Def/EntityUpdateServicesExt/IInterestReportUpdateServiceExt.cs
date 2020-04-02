b73070db25e7a1c00085bf0a63f669354020b9d7
Mutaz Zuhairi <Mutaz@logitudeworld.com>
2020-04-02 15:04:18 +0300

    Fix

--- Logitude/Logitude.Accounting.Def/EntityUpdateServicesExt/IInterestReportUpdateServiceExt.cs
+++ Logitude/Logitude.Accounting.Def/EntityUpdateServicesExt/IInterestReportUpdateServiceExt.cs
@@ -10,6 +10,6 @@ namespace Logitude.Accounting.Def.EntityUpdateServicesExt
 {
     public interface IInterestReportUpdateServiceExt
     {
-        void UpdateConfirmCreateInvoice(InterestReportPM interestReportPM, int Tenant, IAccountingContext MainContext);
+        void UpdateConfirmCreateInvoice(InterestReportPM interestReportPM, int Tenant, IAccountingContext MainContext,string ARInvoiceId=null, string InvoiceNumber=null, double? AmountInLocalCurrency=null, string InvoiceEntitiId=null);
     }
 }
