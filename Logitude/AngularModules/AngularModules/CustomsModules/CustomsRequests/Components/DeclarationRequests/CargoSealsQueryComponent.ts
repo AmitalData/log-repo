edc61c3a70cb2bcd39bf807cb6c33d3aa094aa3e
elisheva <elisheva@amital.co.il>
2020-04-02 15:58:13 +0300

    IsCourierManadatory

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsRequests/Components/DeclarationRequests/CargoSealsQueryComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsRequests/Components/DeclarationRequests/CargoSealsQueryComponent.ts
@@ -323,13 +323,7 @@ export class CargoSealsQueryComponent
         }
     }
 
-    get IsReady() { return this._IsReady; }
-    set IsReady(newValue: boolean) {
-        if (this._IsReady != newValue) {
-            this._IsReady = newValue;
-        }
-    }
-
+ 
     get ResponseMessage() { return this.ResponseData ? this.ResponseData.UserMessage : null; }
     set ResponseMessage(value: string) {
         if (this.ResponseData.UserMessage != value) {
