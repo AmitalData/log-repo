4621f332774b9ce84591418c5a8e2592ead47eee
sohaib <sohaib@amital.co.il>
2020-04-04 20:03:25 +0300

    fix jenkins fail

--- Logitude/AngularModules/AngularModules/Customs/EntityPMs/CustomDocumentTypePM.ts
+++ Logitude/AngularModules/AngularModules/Customs/EntityPMs/CustomDocumentTypePM.ts
@@ -74,9 +74,7 @@ export class CustomDocumentTypePM {
     public set IsDiamondManadatory(newValue: boolean) { if (this.isDiamondManadatory != newValue) { this.isDiamondManadatory = newValue; this.MarkAsDirty("IsDiamondManadatory"); } }
        
 	 
-    private isCourierManadatory: boolean;
-    public get IsCourierManadatory() { return this.isCourierManadatory; }
-    public set IsCourierManadatory(newValue: boolean) { if (this.isCourierManadatory != newValue) { this.isCourierManadatory = newValue; this.MarkAsDirty("IsCourierManadatory"); } }
+  
        
 	 
 
