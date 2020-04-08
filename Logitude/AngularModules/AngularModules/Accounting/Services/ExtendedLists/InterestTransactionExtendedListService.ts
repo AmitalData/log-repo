8edbb076da51a88fac81bb95351ef9715669f137
Ayman1 <Ayman@logitudeworld.com>
2020-04-08 09:51:09 +0300

    fix angular compile error

--- Logitude/AngularModules/AngularModules/Accounting/Services/ExtendedLists/InterestTransactionExtendedListService.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Services/ExtendedLists/InterestTransactionExtendedListService.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-//import { Http, Headers } from '@angular/http';
-import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { ServiceHelper } from '../../../Infrastructure/Utilities/ServiceHelper';
 import { ServiceResponse } from '../../../Infrastructure/DataContracts/ServiceResponse';
 import { SessionInfo } from '../../../Infrastructure/Utilities/SessionInfo';
@@ -65,7 +64,7 @@ export class InterestTransactionExtendedListService {
         
         var serviceResponse: ServiceResponse = new ServiceResponse();
         var url = this._apiUrl + "/GetCheckRecentReports?interestDate=" + interestDate + "&customerId=" + customerId;
-        return this.httpClient.get(url, httpOptions).pipe(
+        return this.httpClient.get(url, ServiceHelper.GetHttpHeaders()).pipe(
             map(response => {
                 serviceResponse.Result = response;
                 return serviceResponse;
