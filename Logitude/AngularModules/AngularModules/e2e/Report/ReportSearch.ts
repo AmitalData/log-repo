bd52f73a50fb2c00d7deb42f985ceac4a41683e5
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2020-04-19 09:39:44 +0300

    fix for automation by raghad

--- Logitude/AngularModules/AngularModules/e2e/Report/ReportSearch.ts
+++ Logitude/AngularModules/AngularModules/e2e/Report/ReportSearch.ts
@@ -12,13 +12,15 @@ export class ReportSearch {
   }
 
   QuickSearch(reportName) {
-    this.logitudeTab.GoToMainMenu2('General.MH.Reports');
-    this.UseSearchBox('null_Search', reportName);
+      this.logitudeTab.GoToMainMenu2('General.MH.Reports');
+      this.Helper.ItemsVisibility('Report')   
+      this.UseSearchBox('null_Search', reportName);
 
 
   }
 
-  UseSearchBox(searchFeildId: string, searchByRef: string) {
+    UseSearchBox(searchFeildId: string, searchByRef: string) {
+    
     this.Helper.WaitByIdAndFill(searchFeildId, searchByRef);
     this.Helper.WaitByIdAndClick("BI");
     this.Helper.WaitByIdAndClick("Report")
