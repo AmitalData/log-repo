f71e56e6d0aba5376f317b51dfb376a4f47fea66
Lana Daoud <Lana@logitudeworld.com>
2020-04-20 12:11:22 +0300

    fix

--- Logitude/AngularModules/AngularModules/e2e/Operations/Shipments/EditEntity/EditShipmentTabs.po.ts
+++ Logitude/AngularModules/AngularModules/e2e/Operations/Shipments/EditEntity/EditShipmentTabs.po.ts
@@ -55,7 +55,7 @@ export class EditTabsComponent {
             this.PartnersTabScenario.PartnersTab(ShipmentLevelCode);
             this.PackagesTabScenario.PackagesTab(ShipmentLevelCode, ShipmentType);
             this.RoutingTabScenario.RoutingTab(ShipmentLevelCode, ShipmentType, Direction);
-            this.PayablesTabScenario.PayablesTab(shipperRef1, ShipmentType, false);
+            //this.PayablesTabScenario.PayablesTab(shipperRef1, ShipmentType, false);
             //this.ReceivablesTabScenario.RecievablesTab(ShipmentLevelCode, ShipmentType);
 
             if (ShipmentLevelCode == 'M') {
