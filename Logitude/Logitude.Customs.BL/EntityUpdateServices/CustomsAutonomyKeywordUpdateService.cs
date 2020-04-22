962d38b774c8ef6bb9b0a891d886cede8a49ff22
sohaib <sohaib@amital.co.il>
2020-04-22 11:20:45 +0300

    bugg #65609

--- Logitude/Logitude.Customs.BL/EntityUpdateServices/CustomsAutonomyKeywordUpdateService.cs
+++ Logitude/Logitude.Customs.BL/EntityUpdateServices/CustomsAutonomyKeywordUpdateService.cs
@@ -45,7 +45,7 @@ namespace Logitude.Customs.BL.EntityUpdateServices
                 var poco = (this.Repository as CustomsAutonomyKeywordRepository).GetBykeywordList(entityPM.KeywordsList, entityPM.Tenant);
                 if (poco != null)
                 {
-                    throw new Exception($"Update {entityPM.KeywordsList} not allowed !! Due already exist (KeywordsList:{entityPM.KeywordsList})");
+                    throw new Exception($"מילת מפתח זו קיימת כבר- לא ניתן להזין מילת מפתח כפולה");
                 }
             }
             base.OnUpdating(entityPM, entityPOCO);
