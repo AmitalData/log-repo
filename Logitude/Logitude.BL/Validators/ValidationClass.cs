6d817865ea02656b53fb4b34d9c1f4c6841d7fe8
Maheera Jazi <Maheera@logitudeworld.com>
2020-04-26 12:55:46 +0300

    66284
    Related Work Items: #66284

--- Logitude/Logitude.BL/Validators/ValidationClass.cs
+++ Logitude/Logitude.BL/Validators/ValidationClass.cs
@@ -11,11 +11,11 @@ using Logitude.Server.Tools.Helpers;
 
 namespace Logitude.BL.Validators
 {
-    public partial class ValidationClass:IValidateContext
+    public partial class ValidationClass : IValidateContext
     {
         bool ready;
         List<ObjectField> objectFieldList;
-        private  string errorMessage = "";
+        private string errorMessage = "";
         int tenant = 0;
 
         public ValidationClass(string objectTableName, int tenant)
@@ -25,15 +25,15 @@ namespace Logitude.BL.Validators
             objectFieldList = ObjectFieldRepository.GetObjectFieldsByObjectTableName(objectTableName, tenant).ToList();
         }
 
-        public bool IsValid(object value,object instance,string propertyName)
+        public bool IsValid(object value, object instance, string propertyName)
         {
             bool valid = true;
             errorMessage = "";
             Type type = instance.GetType();
             object typeProp = null;
-            FieldValidator fieldValidator = new FieldValidator(tenant);            
+            FieldValidator fieldValidator = new FieldValidator(tenant);
             string objectType = "";
-            
+
             if (typeProp != null)
             {
                 objectType = typeProp.ToString();
@@ -51,13 +51,13 @@ namespace Logitude.BL.Validators
                 }
             }
 
-            
+
             ObjectField field = (from a in objectFieldList
                                  where a.ObjectTable.Name == objectType && a.FieldName == propertyName
                                  select a).FirstOrDefault();
 
             if (field != null)
-            {              
+            {
                 if (field.IsRequiered)
                 {
                     if (field.IsCustom && value != null)
@@ -96,22 +96,17 @@ namespace Logitude.BL.Validators
                     {
                         if (!field.IsMaxLength)
                         {
-                        if (valueString.Length > field.MaxLength || valueString.Length < field.MinLength)
-                        {
-                            valid = false;
+                            if (valueString.Length > field.MaxLength || valueString.Length < field.MinLength)
+                            {
+                                valid = false;
+                            }
                         }
                     }
                 }
-                }
 
-                if (field.DataTypeCode == "Decimal" && field.NumberOfDigits != 0) 
+                if (field.DataTypeCode == "Decimal" && field.NumberOfDigits != 0)
                 {
                     string valueString = value != null ? value.ToString() : "";
-                    if (!string.IsNullOrEmpty(valueString))
-                    {
-                        int beforepointlength = valueString.Substring(0, valueString.LastIndexOf(".")).Length;
-                        int afterpointlength = valueString.Substring(valueString.IndexOf(".") + 1).Length;
-                    }
                 }
 
                 if (value != null)
@@ -124,17 +119,17 @@ namespace Logitude.BL.Validators
                         errorMessage = result.ErrorMessage;
                     }
                 }
-            }         
+            }
 
-            return valid;            
+            return valid;
         }
-        
-        public string GetErrorMessage(object value,object instance, string propertyName)
+
+        public string GetErrorMessage(object value, object instance, string propertyName)
         {
             string error = "";
             Type type = instance.GetType();
             object typeProp = null;
-            
+
             string objectType = "";
             if (typeProp != null)
             {
@@ -152,7 +147,7 @@ namespace Logitude.BL.Validators
                     objectType = objectType.Substring(0, objectType.Length - 2);
                 }
             }
-                        
+
             ObjectField field = (from a in objectFieldList
                                  where a.ObjectTable.Name == objectType && a.FieldName == propertyName
                                  select a).FirstOrDefault();
@@ -163,7 +158,7 @@ namespace Logitude.BL.Validators
                 {
                     error = TranslateTextsClass.GetTranslation("General.M.FieldIsRequired", field.FullNameTextCode.Code, null, null, field.Tenant);
                 }
-                if (string.IsNullOrEmpty(error)&&value!=null)
+                if (string.IsNullOrEmpty(error) && value != null)
                 {
                     if (value is string)
                     {
@@ -179,11 +174,11 @@ namespace Logitude.BL.Validators
             if (field.DataTypeCode == "Text")
             {
 
-                    if (field.IsCustom && value != null)
-                    {
-                        CustomFieldClass fieldClass = value as CustomFieldClass;
-                        value = fieldClass.Value;
-                    }
+                if (field.IsCustom && value != null)
+                {
+                    CustomFieldClass fieldClass = value as CustomFieldClass;
+                    value = fieldClass.Value;
+                }
 
                 string valueString = value != null ? value.ToString() : "";
                 if (!string.IsNullOrEmpty(valueString))
@@ -194,19 +189,15 @@ namespace Logitude.BL.Validators
                         {
                             error = TranslateTextsClass.GetTranslation("General.M.MinMax", field.FullNameTextCode.Code, field.MinLength.ToString(), field.MaxLength.ToString(), field.Tenant);
                         }
-                        }
                     }
                 }
+            }
 
             if (field.DataTypeCode == "Decimal" && field.NumberOfDigits != 0)
             {
                 string valueString = value != null ? value.ToString() : "";
-                if (!string.IsNullOrEmpty(valueString))
-                {
-                    int beforepointlength = valueString.Substring(0, valueString.LastIndexOf(".")).Length;
-                    int afterpointlength = valueString.Substring(valueString.IndexOf(".") + 1).Length;
-                }
-                }
+
+            }
 
             if (!String.IsNullOrEmpty(errorMessage))
             {
