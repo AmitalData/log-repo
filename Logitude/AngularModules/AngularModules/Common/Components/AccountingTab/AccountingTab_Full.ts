829c0e79faad2a0476505d8b0e58ca8aebe0331a
alaa <alaa.jamal.h@live.com>
2020-04-29 16:34:32 +0300

    65953
    Related Work Items: #65953

--- Logitude/AngularModules/AngularModules/Common/Components/AccountingTab/AccountingTab_Full.ts
+++ Logitude/AngularModules/AngularModules/Common/Components/AccountingTab/AccountingTab_Full.ts
@@ -19,7 +19,7 @@ import { EntityResourceService } from '../../../Infrastructure/Services/EntityRe
 })
 
 export class AccountingTab_Full extends BaseComponent implements OnDestroy, OnInit {
-    @ViewChild("TabPlaceholder", { read: ViewContainerRef, static: true }) viewContainerRef: ViewContainerRef;
+    @ViewChild("TabPlaceholder", { read: ViewContainerRef, static: false }) viewContainerRef: ViewContainerRef;
     public EntityPM: any = null;
     public ObjectTableName: string;
     public DataContext = this;
