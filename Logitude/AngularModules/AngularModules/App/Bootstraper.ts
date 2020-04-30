51dd535c1a242b008e5cb2755fb608886aba7e63
Ayman1 <Ayman@logitudeworld.com>
2020-04-30 13:18:01 +0300

    pre-final

--- Logitude/AngularModules/AngularModules/App/Bootstraper.ts
+++ Logitude/AngularModules/AngularModules/App/Bootstraper.ts
@@ -2,7 +2,7 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 //import { AppModule } from './Module_APP';
 import { environment } from '../environments/environment';
-import { AppModule } from './Module_APP_CUST';
+//import { AppModule } from './Module_APP_CUST';
 
 if (environment.production) {
     enableProdMode();
@@ -10,9 +10,22 @@ if (environment.production) {
 
 //platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.log(err));
 
-//import('./Module_APP_CUST').then(m => {
-//  platformBrowserDynamic().bootstrapModule(m.AppModule).catch(err => console.log(err));
-//});
+if (environment.customs) {
+  console.log('111111111111111111: Cusotms');
 
-platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.log(err));
+  import('./Module_APP_CUST').then(m => {
+    platformBrowserDynamic().bootstrapModule(m.AppModule).catch(err => console.log(err));
+  });
+}
+
+else {
+  console.log('111111111111111111: Logitude');
+
+  import('./Module_APP').then(m => {
+    platformBrowserDynamic().bootstrapModule(m.AppModule).catch(err => console.log(err));
+  });
+}
+
+
+//platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.log(err));
 
