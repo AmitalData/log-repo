f2ea41bce4686b47d5c957fff80ea61da0f8e394
Mohammad Masoud <mohammad@logitudeworld.com>
2020-05-03 12:08:12 +0300

    fix for angular 9

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/MenuButtonsComponent/MenuButtonsComponentLoader.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/MenuButtonsComponent/MenuButtonsComponentLoader.ts
@@ -1,6 +1,6 @@
 ﻿declare var window: any;
 declare var System: any;
-import {Component, Output, EventEmitter, Input, OnInit, ViewChild, ViewChildren, ViewContainerRef} from '@angular/core';
+import {Component, Output, EventEmitter, Input, OnInit, ViewChild, ViewChildren, ViewContainerRef,AfterViewInit} from '@angular/core';
 import {ObjectTablePM} from '../../../EntityPMs/ObjectTablePM'
 import {MenuButtonPM} from '../../../EntityPMs/MenuButtonPM'
 import {MenuButtonGroupPM} from '../../../EntityPMs/MenuButtonGroupPM'
@@ -16,7 +16,7 @@ import {AppTool} from '../../../Tools'
     templateUrl: "./MenuButtonsComponentLoader.html", 
 })
 
-export class MenuButtonsComponentLoader implements OnInit {
+export class MenuButtonsComponentLoader implements OnInit,AfterViewInit {
 
     //-----------------------------inputs---------------------------------------//
     @Input() MenuButton: MenuButtonPM;
@@ -29,6 +29,9 @@ export class MenuButtonsComponentLoader implements OnInit {
     }
 
     ngOnInit() {
+        
+    }
+    ngAfterViewInit(){
         if (this.MenuButton.HtmlComponentPath) {
             SessionLocator.DynamicLoader.Load(this.MenuButton.HtmlComponentPath, this.ComponentViewContainerRef).then(cmpRef => {
                 cmpRef.instance.Run({ EntityPM: this.EntityPM, ObjectTable: this.ObjectTable });
