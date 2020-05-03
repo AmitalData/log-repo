ed3a0956505933e73e264e2283514b5800cf20dc
moran <moran@amital.co.il>
2020-05-03 12:33:35 +0300

    66274 ms
    Related Work Items: #66274

--- Logitude/Logitude.Customs.BL/EntityUpdateServices/UnifreightDeclarationPaymentUpdateService.cs
+++ Logitude/Logitude.Customs.BL/EntityUpdateServices/UnifreightDeclarationPaymentUpdateService.cs
@@ -222,6 +222,7 @@ namespace Logitude.Customs.BL.EntityUpdateServices
                     _CCUPAYHAND.OBJECTIONEXPLAIN = _CCUPAYHAND.OBJECTIONEXPLAIN + declarationPaymentProtests.CustomsAgentExplanation;
                 }
             }
+            if (_CCUPAYHAND.OBJECTIONEXPLAIN.Length > 75) _CCUPAYHAND.OBJECTIONEXPLAIN = _CCUPAYHAND.OBJECTIONEXPLAIN.Substring(0, 75);
             User myUser = userRepository.GetSingleUser(_DirtyDeclarationPaymentPM.CreatedByUserId, _DirtyDeclarationPaymentPM.Tenant, true);
             if (myUser != null)
             {
