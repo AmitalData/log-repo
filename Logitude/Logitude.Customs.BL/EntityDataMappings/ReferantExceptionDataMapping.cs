404bed13b22e7dd587d6a644a9b51abf83829ee8
elisheva <elisheva@amital.co.il>
2020-05-04 07:59:35 +0300

    65603
    Related Work Items: #65603

--- Logitude/Logitude.Customs.BL/EntityDataMappings/ReferantExceptionDataMapping.cs
+++ Logitude/Logitude.Customs.BL/EntityDataMappings/ReferantExceptionDataMapping.cs
@@ -6,28 +6,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
-using Logitude.Server.Tools; 
+using Logitude.Server.Tools;
 using Logitude.Customs.Data.EntityPOCOs;
-using Logitude.Customs.Def.EntityPMs; 
+using Logitude.Customs.Def.EntityPMs;
 using Logitude.Customs.Data;
 
 namespace Logitude.Customs.BL.EntityDataMappings
 {
-   
-   public partial class ReferantExceptionDataMapping: IMapping<ReferantExceptionPM, ReferantException>
-   {
+
+    public partial class ReferantExceptionDataMapping : IMapping<ReferantExceptionPM, ReferantException>
+    {
 
         public void CustomPMToPOCO(ReferantExceptionPM entityPM, ReferantException entityPOCO)
         {
-            //throw new NotImplementedException();
+            if (entityPM.ChangeSetOp == Simplog.Server.Infrastructure.ChangeSetOperation.Insert)
+            {
+                this.CustomMappedPOCOProperties.Add(POCOPropertyNames.Tenant);
+                this.CustomMappedPOCOProperties.Add(POCOPropertyNames.DeclarationId);
+                this.CustomMappedPOCOProperties.Add(POCOPropertyNames.ExceptionReasonsCode);
+                entityPOCO.DeclarationId = entityPM.DeclarationId;
+                entityPOCO.Tenant = entityPM.Tenant;
+                entityPOCO.ExceptionReasonsCode = entityPM.ExceptionReasonsCode;
+            }
+
+
         }
 
         public void CustomPOCOToPM(ReferantExceptionPM entityPM, ReferantException entityPOCO)
         {
             //throw new NotImplementedException();
         }
-   }
+    }
 
 
 }
-   
--- Logitude/Logitude.Customs.Data/EntityListQueryServices/ExceptionReasonListQueryService.cs
+++ Logitude/Logitude.Customs.Data/EntityListQueryServices/ExceptionReasonListQueryService.cs
@@ -1,4 +1,4 @@
-	using Simplog.Data.InfrastructureModel.EntityPOCOs;
+using Simplog.Data.InfrastructureModel.EntityPOCOs;
 using Simplog.Data.InfrastructureModel.Repositories;
 using Simplog.Server.Infrastructure.DataContracts;
 using Simplog.Server.Infrastructure.Helpers;
@@ -15,36 +15,33 @@ using Logitude.Customs.Data.EntityPOCOs;
 using Logitude.Customs.Data.EntityLists;
 
 namespace Logitude.Customs.Data.EntityListQueryServices
-{ 
+{
 
     public partial class ExceptionReasonListQueryService
     {
-	    private IQueryable<ExceptionReasonList> GetIqueryableList(IQueryable<ExceptionReason> iQueryable)
+        private IQueryable<ExceptionReasonList> GetIqueryableList(IQueryable<ExceptionReason> iQueryable)
         {
-		IQueryable<ExceptionReasonList> query = (from a in iQueryable
-                                            select new ExceptionReasonList()
-											{
-                     
-					                          Code = a.Code,
-					
-					                          EnglishName = a.EnglishName,
-					
-					                          LocalName = a.LocalName,
-					
-					                          IsActive = a.IsActive,
-					
-					                          SearchFields = a.SearchFields,
-					
-		                    	            });
-            return query;
-		}
+            IQueryable<ExceptionReasonList> query = (from a in iQueryable
+                                                     select new ExceptionReasonList()
+                                                     {
 
-		private IQueryable<ExceptionReason> ApplyCustomFilters(QueryOperations queryOperations,IQueryable<ExceptionReason> iQueryable, int tenant)
+                                                         Code = a.Code,
+
+                                                         EnglishName = a.EnglishName,
+
+                                                         LocalName = a.LocalName,
+
+                                                         IsActive = a.IsActive,
+
+                                                     });
+            return query;
+        }
+
+        private IQueryable<ExceptionReason> ApplyCustomFilters(QueryOperations queryOperations, IQueryable<ExceptionReason> iQueryable, int tenant)
         {
-			throw new NotImplementedException();
-		}
-			}
+            return iQueryable;
+        }
+    }
 
 
 }
-	
