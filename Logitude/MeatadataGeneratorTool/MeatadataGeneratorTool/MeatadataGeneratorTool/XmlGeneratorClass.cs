b2181e3b14b0559764b59765b33f802b238e20d6
Abd Al-Rahman Malakh <Abd@logitudeworld.com>
2020-05-06 14:20:39 +0300

    66317
    Related Work Items: #66317

--- Logitude/MeatadataGeneratorTool/MeatadataGeneratorTool/MeatadataGeneratorTool/XmlGeneratorClass.cs
+++ Logitude/MeatadataGeneratorTool/MeatadataGeneratorTool/MeatadataGeneratorTool/XmlGeneratorClass.cs
@@ -2046,9 +2046,7 @@ namespace MeatadataGeneratorTool
                         string dxmlColumnDataType = GetDataTypeForDXMLColumn(fieldDataType, fieldIsFixedLength);
                         bool dxmlColumnNullable;
 
-                        if((entityName.ToLower() == "Address".ToLower() && (fieldName.ToLower() == "City".ToLower() || fieldName.ToLower() == "CountryId".ToLower())) ||
-                           (entityName.ToLower() == "ARPayment".ToLower() && (fieldName.ToLower() == "ValueDate".ToLower()))
-                          )
+                        if((entityName.ToLower() == "Address".ToLower() && (fieldName.ToLower() == "City".ToLower() || fieldName.ToLower() == "CountryId".ToLower())))
                         {
                             dxmlColumnNullable = true;
                         }
