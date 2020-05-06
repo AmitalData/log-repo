a96f2c5370602da4472ede96024f0e18a212660a
Lana Daoud <Lana@logitudeworld.com>
2020-05-06 12:47:42 +0300

    fix

--- Logitude/AngularModules/AngularModules/e2e/Helpers/FieldsHelper.ts
+++ Logitude/AngularModules/AngularModules/e2e/Helpers/FieldsHelper.ts
@@ -37,10 +37,13 @@ export class FieldsHelper {
     WaitByIdAndClick(Id: string) {
         var EC = protractor.ExpectedConditions;
         this.ItemsPresent(Id);
+        console.log('Item Presents in Dom');
         this.ItemsVisibility(Id);
+        console.log('Item is Visible);
         browser.wait(EC.elementToBeClickable(element(by.id(Id))), 180000).then(a => {
             element(by.id(Id)).click();
-        });
+            });
+        console.log('Item is Clicked');
     }
     ItemsPresentWithOutClick(Id: string) {
         var EC = protractor.ExpectedConditions;
