72fdc4115041bf22eaf214ba0dd46e0cc5dae26d
Mutaz Zuhairi <Mutaz@logitudeworld.com>
2020-05-11 13:51:12 +0300

    Fix

--- Logitude/General/2020.R2/05/Day 10 [Mutaz] FixTotaVatableAmountForTaxReportInOldARInvoices.sql
+++ Logitude/General/2020.R2/05/Day 10 [Mutaz] FixTotaVatableAmountForTaxReportInOldARInvoices.sql
@@ -7,11 +7,11 @@ BEGIN
 		DECLARE DataCursor1 CURSOR READ_ONLY
 		FOR
 		SELECT Id, Tenant
-		FROM ARInvoices
+		FROM ARInvoices WHERE Tenant in (select Id from Tenants where AccountingActivated =1)
 		OPEN DataCursor1 FETCH NEXT FROM DataCursor1 INTO @InvoiceId, @Tenant
 		WHILE @@FETCH_STATUS = 0
 		BEGIN
-		set @TotaVatableAmountForTaxReportSum =  round((select sum(isnull(LocalCurrencyAmount,0)) from ARInvoiceLines where ARInvoiceId = @InvoiceId and Tenant = @Tenant and LineActionCode = '1' and VatPercentage <> 0 and Tenant in (select Id from Tenants where AccountingActivated =1)),2)
+		set @TotaVatableAmountForTaxReportSum =  round((select sum(isnull(LocalCurrencyAmount,0)) from ARInvoiceLines where ARInvoiceId = @InvoiceId and Tenant = @Tenant and LineActionCode = '1' and VatPercentage <> 0 ),2)
         update ARInvoices set  TotaVatableAmountForTaxReport = @TotaVatableAmountForTaxReportSum where Id = @InvoiceId and Tenant = @Tenant;
 		FETCH NEXT FROM DataCursor1 INTO @InvoiceId, @Tenant
 		END
