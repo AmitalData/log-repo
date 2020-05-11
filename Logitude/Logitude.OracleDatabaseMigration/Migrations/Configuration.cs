cda8b13cb1ea1c6cfd11ca20f1f721e5dcdb3cbb
itzik <itzik@amital.co.il>
2020-05-11 17:09:19 +0300

    LineNumber NUMBER(10) GENERATED ALWAYS AS IDENTITY NOT NULL,
    Related Work Items: #66687

--- Logitude/Logitude.OracleDatabaseMigration/Migrations/Configuration.cs
+++ Logitude/Logitude.OracleDatabaseMigration/Migrations/Configuration.cs
@@ -26,7 +26,8 @@
             ////"User Id=AMINET_MAIN;  Password=AMINET_MAIN;Direct=True;Data Source=Univ57;port=1521;sid=amital"  ///573 --מפסיקים הפצת customs ל 5.7
             ///       "User Id=AMINET_MAIN;  Password=AMINET_MAIN;Direct=True;Data Source=Univ58;port=1521;sid=amital"  ///583
             //** OLD ENV
-
+            config.DatabaseScript.Column.NumericIdentityBehavior = Devart.Data.Oracle.Entity.Configuration.NumericIdentityBehavior.None;
+            
 
             // Create a custom connection to specify the database and set a SQL generator for MySql.
             var connectionInfo =
