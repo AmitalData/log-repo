ac63a8868f543b347e1cdfb8c1f429ba8bff70b7
sohaib <sohaib@amital.co.il>
2020-05-21 17:06:34 +0300

    67434
    Related Work Items: #67434

--- Logitude/AngularModules/AngularModules/Customs/Components/Spotlight/ReferantSpotlightDataTemplate.ts
+++ Logitude/AngularModules/AngularModules/Customs/Components/Spotlight/ReferantSpotlightDataTemplate.ts
@@ -191,7 +191,7 @@ export class ReferantSpotlightDataTemplate
 
                     SessionLocator.SelectedSession.CurrentListComponent.OnBackFromEdit(this.EntityPM.DeclarationId, { rowIndex: this.RowIndex });
                     //SessionLocator.SelectedSession.CurrentListComponent.DoRefresh();
-
+                });
                 this.DeletedCodeList.forEach((item: string) => {
                     this._referantExceptionExtendedPMService.Delete(this.EntityPM.DeclarationId, item).subscribe((response: any) => {
                         this.DeletedCodeList.splice(this.DeletedCodeList.indexOf(item), 1);
