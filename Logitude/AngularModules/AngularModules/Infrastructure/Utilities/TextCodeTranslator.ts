1e25c0686bf7979632356781a9593d43d34ae3ef
Mutaz Zuhairi <Mutaz@logitudeworld.com>
2020-05-28 14:37:39 +0300

    Fix Popup With Email LogitudeWorld

--- Logitude/AngularModules/AngularModules/Infrastructure/Utilities/TextCodeTranslator.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Utilities/TextCodeTranslator.ts
@@ -104,7 +104,11 @@ export class TextCodeTranslator {
             }
             else {
                 if (this.ShowAlertMessage(value)) {
-                    alert("This Code '" + value + "' Not Found!");
+                    if (SessionLocator.LoggedUserPM.Email.includes("logitudeworld.com")) 
+                    {
+                       alert("This Code '" + value + "' Not Found!");
+                    }   
+    
                 }
             }
         }
