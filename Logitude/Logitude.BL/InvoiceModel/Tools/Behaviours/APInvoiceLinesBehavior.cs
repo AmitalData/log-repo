df8bccc3bdf2d7200698b3bb9cbd93f2d48415c0
Ayman1 <Ayman@logitudeworld.com>
2020-05-28 12:19:37 +0300

    67123
    Related Work Items: #67123

---
+++ Logitude/Logitude.BL/InvoiceModel/Tools/Behaviours/APInvoiceLinesBehavior.cs
@@ -0,0 +1,45 @@
+﻿using Logitude.BL.InvoiceModel.EntityPMs;
+using Logitude.BL.InvoiceModel.Tools.Initializers;
+using Simplog.Server.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logitude.BL.InvoiceModel.Tools.Behaviours
+{
+    public class APInvoiceLinesBehavior : IServiceBehaviour
+    {
+        private APInvoicePM entityPM;
+
+        private APInvoiceServiceInitializer initializer;
+
+        public void Handle(IServiceInitializer initializer)
+        {
+            this.initializer = (APInvoiceServiceInitializer)initializer;
+            this.entityPM = this.initializer.EntityPM;
+            //this.HandleBehaviour();
+        }
+
+        private void HandleBehaviour()
+        {
+            if (initializer.EntityPM.TotalVATOnly)
+            {
+                foreach (APInvoiceLinePM item in initializer.EntityPM.InvoiceLines)
+                {
+                    if (item.VatTypeId != null)
+                    {
+                        item.VatTypeId = null;
+                        item.VatPercentage = null;
+
+                        if (item.ChangeSetOp == Simplog.Server.Infrastructure.ChangeSetOperation.None)
+                        {
+                            item.ChangeSetOp = Simplog.Server.Infrastructure.ChangeSetOperation.Update;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
