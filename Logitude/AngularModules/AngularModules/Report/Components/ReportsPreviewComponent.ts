37f623e3a53d0e7310a8a6e83d3e038f79a16e0d
Abdullah Dawoud <abdullah@logitudeworld.com>
2020-05-28 15:46:26 +0300

    fix balance totals logic for multi currency account
    Related Work Items: #67525

--- Logitude/AngularModules/AngularModules/Report/Components/ReportsPreviewComponent.ts
+++ Logitude/AngularModules/AngularModules/Report/Components/ReportsPreviewComponent.ts
@@ -58,7 +58,7 @@ export class ReportsPreviewComponent implements AfterViewInit {
     public isRTL: boolean = false;
     private CurrentSession = SessionLocator.SelectedSession;
 
-    IsHaveRunReportViewWorkerRoleToggleFeature: boolean = true;
+    IsHaveRunReportViewWorkerRoleToggleFeature: boolean = false;
     constructor(public _reportService: ReportService, private cd: ChangeDetectorRef) {
         var idIndex = this.CurrentSession.GetNewId("ReportsPreviewComponent");
         this.ComponentId = "ReportsPreview_" + idIndex;
@@ -81,7 +81,7 @@ export class ReportsPreviewComponent implements AfterViewInit {
         this.ReportsTemplateLists = reportTemplateLists;
         this.Title = SessionLocator.LoggedUserPM.DontShowLocal ? ReportList.Name : ReportList.LocalName;
         this.FilterControlName = ReportList.FilterControlName;
-        this.ReportsRunUsingWR = true;
+        this.ReportsRunUsingWR = false;
         this.RunComponent();
     }
 
@@ -336,7 +336,7 @@ export class ReportsPreviewComponent implements AfterViewInit {
             this.IsUsedReportsRunUsingWR = false;
             this.IsRunReportSucceeded = true;
             this.SetReportData();
-       
+
         }
     }
 
@@ -448,7 +448,7 @@ export class ReportsPreviewComponent implements AfterViewInit {
 
 
     StartBuildStimulReportViaWorkerRole(filter: ReportFliter, isUsedWorkerRoleAlalways = false) {
-        filter.ReportsRunUsingWR = this.IsUsedReportsRunUsingWR = true;
+        filter.ReportsRunUsingWR = this.IsUsedReportsRunUsingWR = false;
 
         if (isUsedWorkerRoleAlalways) {
             this.StartBusyIndicator("Generating...");
