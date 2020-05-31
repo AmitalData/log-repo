5c99ef1e2a2f9eb43b414b37740c158a8a538f62
Khawla Ata <khawla@logitudeworld.com>
2020-05-31 12:30:42 +0300

    edit runusing worker role

--- Logitude/AngularModules/AngularModules/Report/Components/ReportsPreviewComponent.ts
+++ Logitude/AngularModules/AngularModules/Report/Components/ReportsPreviewComponent.ts
@@ -58,7 +58,7 @@ export class ReportsPreviewComponent implements AfterViewInit {
     public isRTL: boolean = false;
     private CurrentSession = SessionLocator.SelectedSession;
 
-    IsHaveRunReportViewWorkerRoleToggleFeature: boolean = false;
+    IsHaveRunReportViewWorkerRoleToggleFeature: boolean = true;
     constructor(public _reportService: ReportService, private cd: ChangeDetectorRef) {
         var idIndex = this.CurrentSession.GetNewId("ReportsPreviewComponent");
         this.ComponentId = "ReportsPreview_" + idIndex;
@@ -81,7 +81,7 @@ export class ReportsPreviewComponent implements AfterViewInit {
         this.ReportsTemplateLists = reportTemplateLists;
         this.Title = SessionLocator.LoggedUserPM.DontShowLocal ? ReportList.Name : ReportList.LocalName;
         this.FilterControlName = ReportList.FilterControlName;
-        this.ReportsRunUsingWR = false;
+        this.ReportsRunUsingWR = true;
         this.RunComponent();
     }
 
