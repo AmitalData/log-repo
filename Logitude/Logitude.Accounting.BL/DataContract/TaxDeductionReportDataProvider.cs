17798a3090fee9595c17666788060e6e66cc5e98
alaa <alaa.jamal.h@live.com>
2020-06-03 17:01:18 +0300

    67941
    Related Work Items: #67941

--- Logitude/Logitude.Accounting.BL/DataContract/TaxDeductionReportDataProvider.cs
+++ Logitude/Logitude.Accounting.BL/DataContract/TaxDeductionReportDataProvider.cs
@@ -526,7 +526,7 @@ namespace Logitude.Accounting.BL.DataContract
                 {
                     Month = month,
                     TotalVendors = deductionLines.Where(d => d.MonthOfRegisterDate ==month).GroupBy(d => d.VendorId).Count(),
-                    TotalPaymentsWithoutDivided = Math.Round(deductionLines.Where(d => d.MonthOfRegisterDate == month && d.DeductionType != "18").Sum(d => d.AmountInLocalCurrency+ (double?) d.TaxDeductionLocalAmount).Value, 0),
+                    TotalPaymentsWithoutDivided = Math.Round(deductionLines.Where(d => d.MonthOfRegisterDate == month && d.DeductionType != "18").Sum(d => d.AmountInLocalCurrency).Value, 0),
                     TotalDeductionsWithoutDivided = Math.Round(deductionLines.Where(d => d.MonthOfRegisterDate == month && d.DeductionType != "18").Sum(d => d.TaxDeductionLocalAmount).Value, 0),
                     TotalDivided = Math.Round(deductionLines.Where(d => d.MonthOfRegisterDate == month && d.DeductionType == "18").Sum(d => d.AmountInLocalCurrency+ (double?) d.TaxDeductionLocalAmount).Value, 0),
                     TotalDeductionsFromDivided = Math.Round(deductionLines.Where(d => d.MonthOfRegisterDate == month && d.DeductionType == "18").Sum(d => d.TaxDeductionLocalAmount).Value, 0),
