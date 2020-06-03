a4709a87edd35858fd4f86f1d6e7543bbd51d53d
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2020-06-03 15:57:36 +0300

    fixes

--- Logitude/AngularModules/AngularModules/cypress/integration/examples/test.spec.js
+++ Logitude/AngularModules/AngularModules/cypress/integration/examples/test.spec.js
@@ -5,50 +5,32 @@ context('Actions', () => {
     cy.visit('https://system.logitudeworld.com')
   })*/
 
-  // https://on.cypress.io/interacting-with-elements
-
-/*
-
-        
-       
-
-*/
-  it('redirect into logitude', () => {
-	   
+ 
+    it('Redirect into logitude', () => { 
 		//cy.visit('http://192.168.1.100/test') 
 		//cy.visit('https://test.logitudeworld.com/staging') 
-    //cy.request('http://localhost:9996/LinksGateway.aspx') 
-    cy.visit('http://localhost:4200')
-  })
-  // it('Login to the test system', () => {
-	// 	cy.get('#Email').type('protractor@test.com') 
-	// 	cy.get('#Password').type('!P123t456') 
-  //       cy.get('#cmdLogin').click().then(()=>{
-	// 		//cy.contains('#errorsList')
-  //          //cy.url().should('include', '/Angular')
-  //          cy.wait(30000);
-  //       })
-  // })
+        //cy.request('http://localhost:9996/LinksGateway.aspx') 
+          cy.visit('http://localhost:4200')
+    }) 
+    it('Login succeeded', () => {
+	  //cy.get('#Email').type('protractor@test.com') 
+	  //cy.get('#Password').type('!P123t456') 
+      cy.get('#cmdLogin').click();
+      cy.server();
+      cy.route('/api/ObjectTableLastUpdate/GetLastTableUpdateDate/*').as('DataLoaded')
+      cy.wait('@DataLoaded') 
+    })
 
-  it('Login to the Local system', () => {
-		//cy.get('#Email').type('angular@fnarsoft.com') 
-		//cy.get('#Password').type('1') 
-        cy.get('#cmdLogin').click().then(()=>{ 
-           var test = cy.get('#GeneralMHOperations',{timeout:15000}).click()
-           cy.get('#SHIP',{timeout:15000}).click()
-           cy.get('#HelperNotes_0_0',{timeout:15000}).click()
-           cy.get('#NEWDIRECT',{timeout:15000}).click({force:true})
+    it('Moving to Shipment Succeeded', () => {
+        cy.get('#GeneralMHOperations').click()
+        cy.get('#SHIP').click() 
+    })
 
-        })
-       
-  })
-
-  //it('Move To Accounting Menu', () => {
-	 
-          
-         
-          //test.click()
-         
-  //})
+    it('Create Shipment', () => {
+      
+        cy.get('#HelperNotes_0_0').click()
+        cy.get('#NEWDIRECT').click({ force: true })
+    })
+     
 
 })
