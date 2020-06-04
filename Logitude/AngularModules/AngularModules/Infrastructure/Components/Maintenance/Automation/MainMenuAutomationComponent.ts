fa662800bc777818d230a8b1a38a75016d2cc647
Abed <abed@logitudeworld.com>
2020-06-04 14:46:32 +0300

    66891
    Related Work Items: #66891

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/Maintenance/Automation/MainMenuAutomationComponent.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/Maintenance/Automation/MainMenuAutomationComponent.ts
@@ -36,7 +36,7 @@ export class MainMenuAutomationComponent implements OnInit {
         if (FeatureLocator.HasFeaturePermession("Ticket", "AUTOMATION")) this.AutomationEntityLists.push(new AutomationItemClass("Tickets", "Ticket", "Ticket"));
 
         if (FeatureLocator.HasFeaturePermession("LogitudeMessagesTransmissionLog", "AUTOMATION")) this.AutomationEntityLists.push(new AutomationItemClass("Transmission Logs", "LogitudeMessagesTransmissionLog","Transmission Log"));
-        if (FeatureLocator.HasFeaturePermession("ARInvoice", "AUTOMATION")) this.AutomationEntityLists.push(new AutomationItemClass("AR Invoices", "ARInvoice", "ARInvoice", "Receivables Invoices"));
+        if (FeatureLocator.HasFeaturePermession("ARInvoice", "AUTOMATION")) this.AutomationEntityLists.push(new AutomationItemClass("AR Invoices", "ARInvoice", "AR Invoice", "Receivables Invoices"));
 
        
 
