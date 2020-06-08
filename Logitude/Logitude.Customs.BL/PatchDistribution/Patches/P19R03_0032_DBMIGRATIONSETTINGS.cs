41be1832322a394ef7ab6d0ba6b3d7704862e24a
itzik <itzik@amital.co.il>
2020-06-08 14:53:05 +0300

    67944TK2
    Related Work Items: #67944

---
+++ Logitude/Logitude.Customs.BL/PatchDistribution/Patches/P19R03_0032_DBMIGRATIONSETTINGS.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logitude.Customs.BL.PatchDistribution.Patches
+{
+    class P19R03_0032_DBMIGRATIONSETTINGS : PatchDistributionBase
+    {
+        public P19R03_0032_DBMIGRATIONSETTINGS()
+            : base("DBMIGRATIONSETTINGS", new DateTime(2020, 05, 07))
+        {
+
+        }
+
+        public override void CreateDownScripts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateUpScripts()
+        {
+            /*
+CREATE TABLE "AMINETCST_MAIN"."DBMIGRATIONSETTINGS"
+(
+"MODE" VARCHAR2(10 CHAR) NOT NULL,
+"MODULESLIST" VARCHAR2(1000 CHAR) NOT NULL
+);
+/
+
+
+INSERT INTO "AMINETCST_MAIN"."DBMIGRATIONSETTINGS"("MODE", "MODULESLIST") VALUES('Include', 'Customs,Common,Infrastructure');
+/
+
+ *              */
+
+            this.AddUpSqlScript(@"CREATE TABLE DBMIGRATIONSETTINGS( MODE VARCHAR2(10 CHAR) NOT NULL,MODULESLIST VARCHAR2(1000 CHAR) NOT NULL)");
+            this.AddUpSqlScript(@"INSERT INTO  DBMIGRATIONSETTINGS( MODE, MODULESLIST) VALUES('Include', 'Customs,Common,Infrastructure')");
+        }
+    }
+}
