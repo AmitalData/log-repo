2b3cb6692a3a96e2340a626ef088b8e39d1df623
Alexander Khitrik <alexanderh@amital.co.il>
2020-06-14 11:38:28 +0300

    Alexander: 67990 Link AC Cards to GLA

--- Logitude/Logitude.Accounting.Data/Repositories/GLAccountRepository.cs
+++ Logitude/Logitude.Accounting.Data/Repositories/GLAccountRepository.cs
@@ -897,7 +897,7 @@ namespace Logitude.Accounting.Data.Repositories
 
 
                         join a in context.GLAccounts
-       .Where(r => r.AccountTypeCode == "1" && r.Tenant == tenant)
+       .Where(r => (r.AccountTypeCode == "1" || r.AccountTypeCode == "2" || r.AccountTypeCode == "3") && r.Tenant == tenant)
        on crm.PayablesAccountingCard equals a.DisplayNumber
 
                         select new CardDTO()
@@ -922,7 +922,7 @@ namespace Logitude.Accounting.Data.Repositories
 
                                                             
                                                             join a in context.GLAccounts
-                                           .Where(r => r.AccountTypeCode == "1" && r.Tenant == tenant)
+                                           .Where(r => (r.AccountTypeCode == "1" || r.AccountTypeCode == "2" || r.AccountTypeCode == "3") && r.Tenant == tenant)
                                            on crm.ReceivablesAccountingCard equals a.DisplayNumber
 
                                                             select new CardDTO()
