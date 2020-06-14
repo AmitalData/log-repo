4169be2607ef44cc74d23f238b2ec44aa92ea0c9
moran <moran@amital.co.il>
2020-06-14 18:54:59 +0300

    67900 ms
    Related Work Items: #67900

--- Logitude/Logitude.Customs.BL/Messaging/U2L/CommDecReferantData/CommDecReferantDataService.cs
+++ Logitude/Logitude.Customs.BL/Messaging/U2L/CommDecReferantData/CommDecReferantDataService.cs
@@ -151,7 +151,7 @@ namespace Logitude.Customs.BL.Messaging.U2L.CommDecReferantData
                 if (_DeclarationReferantDataPM.Tenant < 1) _DeclarationReferantDataPM.Tenant = ResolvedTenant();
                 //if (!string.IsNullOrWhiteSpace(_LogitudeDeclarationReferantData.ClassifiedUserId)) _DeclarationReferantDataPM.ClassifiedUserId = _LogitudeDeclarationReferantData.ClassifiedUserId;
                 //if (!string.IsNullOrWhiteSpace(_LogitudeDeclarationReferantData.ControllerUserId)) _DeclarationReferantDataPM.ControllerUserId = _LogitudeDeclarationReferantData.ControllerUserId;
-                //if (_LogitudeDeclarationReferantData.FileStatus == "OPT") _DeclarationReferantDataPM.NewFile = false;
+                if (_LogitudeDeclarationReferantData.FileStatus == "OPT") _DeclarationReferantDataPM.NewFile = false;
 
                 myDeclarationReferantDataUpdateService.Update(this._DeclarationReferantDataPM, true);
 
