1d88ba5dfdee229185ad3f28bd25ca79a8ea4c79
Maheera Jazi <Maheera@logitudeworld.com>
2020-06-17 23:56:34 +0300

    68469
    Related Work Items: #68469

--- Logitude/Simplog.Data/CommonDataModel/Repositories/CardExternalCodeByCurrencyRepository.cs
+++ Logitude/Simplog.Data/CommonDataModel/Repositories/CardExternalCodeByCurrencyRepository.cs
@@ -55,9 +55,9 @@ namespace Simplog.Data.CommonDataModel.Repositories
         {
             return (from record in context.CardExternalCodeByCurrencies.Include("Currency") where record.Id == id && record.Tenant == tenant select record).FirstOrDefault();          
         }
-        public CardExternalCodeByCurrency GetSingleCardExternalCodeByCurrencyAndTenant(string id, string currencyId, int tenant)
+        public CardExternalCodeByCurrency GetSingleCardExternalCodeByCurrencyAndTenant(string cardId, string currencyId, int tenant)
         {
-            return (from record in context.CardExternalCodeByCurrencies.Include("Currency") where record.Id == id && record.CurrencyId == currencyId && record.Tenant == tenant select record).FirstOrDefault();
+            return (from record in context.CardExternalCodeByCurrencies.Include("Currency") where record.CardId == cardId && record.CurrencyId == currencyId && record.Tenant == tenant select record).FirstOrDefault();
         }
 
 
