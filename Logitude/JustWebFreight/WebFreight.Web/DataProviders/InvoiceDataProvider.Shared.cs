459361355daabed0d87e68aca22956902605414c
Samar Abu Eid <Samar@logitudeworld.com>
2020-06-17 23:50:02 +0300

    68531
    Related Work Items: #68531

--- Logitude/JustWebFreight/WebFreight.Web/DataProviders/InvoiceDataProvider.Shared.cs
+++ Logitude/JustWebFreight/WebFreight.Web/DataProviders/InvoiceDataProvider.Shared.cs
@@ -535,6 +535,7 @@ namespace WebFreight.Web.DataProviders
         public string MainCarriageLastdestinationPortName { get; set; }
         public string MainCarriageLastdestinationPortCode { get; set; }
         public string BillToBillingAddress { get; set; }
+        public string TrailerNumber { get; set; }
     }
 
     public class SAT
