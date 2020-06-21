c5d1599c90e5eb33352bd5865b73aca8f76ed11d
Abd Al-Rahman Malakh <Abd@logitudeworld.com>
2020-06-21 12:41:52 +0300

    68203
    Related Work Items: #68203

---
+++ Logitude/Logitude.DBMigrations/Models/GeneralModels/ToolArguments.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logitude.DBMigrations.Models
+{
+    public static class ToolArguments
+    {
+        public const string ROOT = "-root";
+        public const string EXE = "-exe";
+        public const string DEPLOYMENT = "-deployment";
+        public const string BASIC = "-basic";
+        public const string IGNOREHASH = "-ignorehash";
+        public const string IGNORESETTINGSCHECK = "-ignoresettingscheck";
+    }
+}
