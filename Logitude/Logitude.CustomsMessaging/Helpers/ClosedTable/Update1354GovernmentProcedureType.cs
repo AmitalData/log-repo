62f9ddbf8a5f1681eb05a21022c975f9fa20dbd6
elisheva <elisheva@amital.co.il>
2020-06-22 08:55:07 +0300

    68559
    Related Work Items: #68559

--- Logitude/Logitude.CustomsMessaging/Helpers/ClosedTable/Update1354GovernmentProcedureType.cs
+++ Logitude/Logitude.CustomsMessaging/Helpers/ClosedTable/Update1354GovernmentProcedureType.cs
@@ -34,7 +34,7 @@ namespace Logitude.CustomsMessaging.Helpers.ClosedTable
         protected override bool IsEqual(SYSTBL_NG_9001_MSG_SystemTablesResponseTableDataExt mehesTableRow, GovernmentProcedureTypePM curDbPM)
         {
             var gov = mehesTableRow.MyGovernmentProcedureType ?? new GovernmentProcedureType();
-            return base.IsEqual(mehesTableRow, curDbPM);// && curDbPM.IsImport == gov.IsImport;
+            return base.IsEqual(mehesTableRow, curDbPM) && curDbPM.IsImport == gov.IsImport && curDbPM.IsExport == gov.IsExport;
         }
         protected override void SetOtherFields(SYSTBL_NG_9001_MSG_SystemTablesResponseTableDataExt mehesTableRow, GovernmentProcedureTypePM curDbPM)
         {
