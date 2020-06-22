70abe32853962fa97655eaa928cf248e5d3acd35
Ahmad Baker <ahmadb@logitudeworld.com>
2020-06-22 08:55:00 +0300

    Add mocha reports,

--- Logitude/AngularModules/AngularModules/cypress/Login/Login.po.ts
+++ Logitude/AngularModules/AngularModules/cypress/Login/Login.po.ts
@@ -10,17 +10,13 @@ export class LoginComp {
 
 
  //cy.visit('http://localhost:4200/')
- //cy.visit('http://13.80.78.95:91/')
-  cy.visit('https://test.logitudeworld.com/test')
- //cy.visit('https://system.logitudeworld.com')
+ 
+  //cy.visit('https://test.logitudeworld.com/test')
+  cy.visit(Cypress.env("URL"))
 
-  //cy.get('#Email').clear()
-//cy.get('#Email').type('ahmadb@test.com', { delay: 50 }).should('have.value', 'ahmad2@test.com')
+  cy.get('#Email').type(Cypress.env("Email"), { delay: 50 }).should('have.value', 'protractor2@test.com')
 
-  //cy.get('#Email').type('protractor2@test.com', { delay: 50 }).should('have.value', 'protractor2@test.com')
-  cy.get('#Email').type('protractor2@test.com', { delay: 50 }).should('have.value', 'protractor2@test.com')
-
-  cy.get('#Password').type('!P123p456')
+  cy.get('#Password').type(Cypress.env("Password"))
   cy.get('#cmdLogin').click()
 
 
--- Logitude/AngularModules/AngularModules/cypress/TeamIslam/BuildShipmentDocsIn.ts
+++ Logitude/AngularModules/AngularModules/cypress/TeamIslam/BuildShipmentDocsIn.ts
@@ -27,9 +27,9 @@ it('Successfully Upload File', function () {
     cy.get('#row0').click();
     cy.get('#UploadDocumentdbtn',{ multiple: true }).click();
     //cy.get('.Button').click()
-const fileName = 'Cypress.png'
-cy.fixture('Cypress.png').then(function(fileContent){
-cy.get('input.upload').attachFile({fileContent,fileName,mimetype:'application/png'})
+const fileName = 'dummy.pdf'
+cy.fixture('dummy.pdf').then(function(fileContent){
+cy.get('input.upload').attachFile({fileContent,fileName,mimetype:'application/pdf'})
 cy.get('#FileUploadedSuccessfully').should('be.visible')
 //cy.get('#').should('be.visible')
 //cy.get(popupContainerSelector).contains('File Uploaded Successfully	').should('be.visible')
