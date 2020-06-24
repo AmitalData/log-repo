9ccbfeb39444d73a4531f7d619c4c535e200b693
elisheva <elisheva@amital.co.il>
2020-06-24 13:40:09 +0300

    68685
    Related Work Items: #68685

--- Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_5117_ImportDeclerationAmendmentReplyResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_5117_ImportDeclerationAmendmentReplyResponseService.cs
@@ -1803,14 +1803,14 @@ namespace Logitude.CustomsMessaging.ResponseServices
                 Tenant = this._MyDeclarationPM.Tenant,
                 RequestName = "Declaration Print(5117)",
                 ResponseName = "Declaration Print(5117)",
-                LoggingEntityId = this._MyDeclarationPM.Id,
+                LoggingEntityId = this._MyDeclarationPMOrg != null? this._MyDeclarationPMOrg.Id: _MyDeclarationPM.Id,
                 RequestVIA = SendRequestVIA.WebServiceBatch,
 
 
                 LoggingUserId = requestParams.LoggingUserId ,//HD CALL#298426
-            };
+            }; 
 
-            var myRequestMessagingService = new DF_NG_8302_Web03_DeclarationPrintMessagingService();
+             var myRequestMessagingService = new DF_NG_8302_Web03_DeclarationPrintMessagingService();
             var resData = myRequestMessagingService.Send(searchParams);
             _SendDeclarationPrintResponse = resData;
             if (!resData.Succeeded)
