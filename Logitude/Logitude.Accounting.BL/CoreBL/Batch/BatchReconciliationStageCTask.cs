3185d103ced3e7aa6f148b33eb26319baae014ce
Alexander Khitrik <alexanderh@amital.co.il>
2020-06-25 12:23:47 +0300

    Alexander: 68534 Reconciliation Stage C

---
+++ Logitude/Logitude.Accounting.BL/CoreBL/Batch/BatchReconciliationStageCTask.cs
@@ -0,0 +1,52 @@
+﻿using Logitude.Accounting.BL.Utils;
+using Logitude.Infrastructure.BL.EntityPMs;
+using Logitude.Infrastructure.BL.ExtendedServices;
+using Simplog.Server.Infrastructure;
+using Simplog.Server.Infrastructure.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Logitude.Accounting.BL.CoreBL.Batch
+{
+    public class BatchReconciliationStageCTask : BatchTaskExecutionsService
+    {
+        public BatchReconciliationStageCTask(BatchTaskExecutionPM batchTaskExecution) : base(batchTaskExecution)
+        {
+        }
+        public override void RunCode()
+        {
+            string xmlParameters = BatchTaskExecution.PrametersXml;
+            System.IO.StringReader stringReader = new System.IO.StringReader(xmlParameters);
+            XmlSerializer serializer = new XmlSerializer(typeof(ReconciliationStageCArg));
+            var parameterArgs = serializer.Deserialize(stringReader) as ReconciliationStageCArg;
+            try
+            {
+
+
+                using (var scope = TransactionFactory.GetTransaction(TimeSpan.FromMinutes(600)))
+                {
+
+                    ReconciliationStageCBatch reconciliationStageCBatch = new ReconciliationStageCBatch();
+                    reconciliationStageCBatch.RunReconciliationStageC(parameterArgs);
+                    string responseText = reconciliationStageCBatch.ResponseText();
+                    ChangeStatus("D", null, responseText);
+
+                    scope.Complete();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                LogitudeSettings.HandleLogMe(ex.ToString(), true, "BatchReconciliationStageCTask", new DateTime(2019, 10, 1));
+                throw;
+
+            }
+        }
+    }
+}
