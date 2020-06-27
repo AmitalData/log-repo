381f350719d3dfea8485f2a5f222511e9a15dfb9
alaa <alaa.jamal.h@live.com>
2020-06-27 22:03:09 +0300

    68755
    Related Work Items: #68755

--- Logitude/Logitude.Accounting.BL/CoreBL/TaxReportService.cs
+++ Logitude/Logitude.Accounting.BL/CoreBL/TaxReportService.cs
@@ -411,16 +411,17 @@ namespace Logitude.Accounting.BL.CoreBL
                     reference = Reference;
                     referenceGroup = "0000";
                 }
-
+                if (reference.Length > 9)
+                {
+                    reference = reference.Substring(reference.Length - 9);
+                }
             }
             else
             {
                 referenceGroup = "0000";
+                reference = null;
             }
-            if (reference.Length > 9)
-            {
-                reference = reference.Substring(reference.Length - 9);
-            }
+            
         }
         private static string RemoveSomeChars(string reference)
         {
