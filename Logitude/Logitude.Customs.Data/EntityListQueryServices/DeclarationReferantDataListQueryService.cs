6e34c46cd15ecf7b3901bfb6c223dc37baaa40e7
elisheva <elisheva@amital.co.il>
2020-06-30 14:57:30 +0300

    --

--- Logitude/Logitude.Customs.Data/EntityListQueryServices/DeclarationReferantDataListQueryService.cs
+++ Logitude/Logitude.Customs.Data/EntityListQueryServices/DeclarationReferantDataListQueryService.cs
@@ -81,7 +81,8 @@ namespace Logitude.Customs.Data.EntityListQueryServices
                                                                  Favorite=a.Favorite,
                                                                  IsCustomerLogBoxActivated=e.Customer.LogBoxActivated,
                                                                  SortedColumns= (a.NewFile && a.Favorite ? 1 : ( a.NewFile ? 2 : ( a.Favorite ? 3 : 4 ))),
-
+                                                                 IsCancelled= d.IsCancelled
+                                                                
                                                              });
             return query;
         }
