02f78f1a9316f74dd270868b32fd3d0acdaa3163
Abd Al-Rahman Malakh <Abd@logitudeworld.com>
2020-06-30 11:42:04 +0300

    68959
    Related Work Items: #68959

--- Logitude/Logitude.DBMigrations/Models/GeneralModels/MigrationTool.cs
+++ Logitude/Logitude.DBMigrations/Models/GeneralModels/MigrationTool.cs
@@ -408,7 +408,7 @@ namespace Logitude.DBMigrations.Models
         {
             if (!String.IsNullOrEmpty(script))
             {
-                Console.WriteLine("Executing Script On " + dbType + " Database ...");
+                Console.WriteLine("Executing Scripts On " + dbType + " Database ...\n");
                 string result = ExecuteScriptOnDatabase(script, dbType);
                 if (!String.IsNullOrEmpty(result))
                 {
@@ -602,6 +602,7 @@ namespace Logitude.DBMigrations.Models
                         {
                             oracleCommand.CommandText = (command.ToUpper().EndsWith(" END") || command.ToUpper().EndsWith("\nEND")) ? (command + ";") : command;
                             currentCommandText = oracleCommand.CommandText;
+                            PrintExecutingScript(currentCommandText);
                             oracleCommand.ExecuteNonQuery();
                         }
                     }
@@ -637,6 +638,7 @@ namespace Logitude.DBMigrations.Models
                             sqlCommand.CommandText = command;
                             sqlCommand.CommandTimeout = 3600;
                             currentCommandText = sqlCommand.CommandText;
+                            PrintExecutingScript(currentCommandText);
                             sqlCommand.ExecuteNonQuery();
                         }
                     }
@@ -1959,6 +1961,14 @@ namespace Logitude.DBMigrations.Models
                 return (string)AssemblyVersion.ConstructorArguments[0].Value;
             }
             return "0.0";
+        }
+
+        private void PrintExecutingScript(string script)
+        {
+            if (!(script.ToLower().Contains("INSERT INTO".ToLower()) && script.ToLower().Contains("DBMigrationsHistory".ToLower())))
+            {
+                Console.WriteLine("Executing Script:\n" + script.TrimStart('\n').TrimEnd('\n') + "\n");
+            }
         }
 
         private void ExitTool(string message)
