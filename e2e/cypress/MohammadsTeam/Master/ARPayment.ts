164a642dbbbd275f367faf4d0ba213e7154e9126
Sumaya Ghazi <sumaya@logitudeworld.com>
2020-07-01 16:22:11 +0300

    arpayment

--- e2e/cypress/MohammadsTeam/Master/ARPayment.ts
+++ e2e/cypress/MohammadsTeam/Master/ARPayment.ts
@@ -5,14 +5,38 @@ export class ARPayment {
 
 
     }
-    CreateNewARPayment(Name: string) {
+    CreateNewARPayment(BillToName: string) {
         {
 
-           
-
+            
+            cy.get('li[id=FACS]').click();
+            cy.get('button[id=NewARPayment]').click();
+            cy.get('input[id=ARPayment_BillToId]').type(BillToName).should("have.value", BillToName)
+            cy.get('ul[id=mydatalist_ARPayment_BillToId]').contains(BillToName).then(a => {
+                a[0].click();
+            })
+            cy.get('input[id=ARPayment_AmountInPaymentCurrency]').type("1000").should("have.value","1000")
+            cy.get('input[id=ARPayment_AccountingPaymentMethodId]').type("Cash").should("have.value","Cash")
+            cy.get('ul[id=mydatalist_ARPayment_AccountingPaymentMethodId]').contains("Cash").then(a => {
+                a[0].click();
+            })
+            cy.get('input[id=ARPayment_BranchId]').type("Main Office").should("have.value","Main Office")
+            cy.get('ul[id=mydatalist_ARPayment_BranchId]').contains("Main Office").then(a => {
+                a[0].click();
+            })
+            cy.get('button[id=ok-AddARPayment]').click();
+            cy.get('#ARPaymentSpinner').should("not.be.visible");
+            cy.get('button[id=ARPaymentBApprove]').click();
+            cy.contains('Approved') 
+            cy.log('ARPayment Is Approved')
+            
         }
 
     }
   
 }
  
+
+
+
+    
