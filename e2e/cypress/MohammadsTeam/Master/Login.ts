12517de0bfc0644397daf46cd2dec1e4a47a1740
Sumaya Ghazi <sumaya@logitudeworld.com>
2020-07-01 14:07:41 +0300

    Cypress

---
+++ e2e/cypress/MohammadsTeam/Master/Login.ts
@@ -0,0 +1,30 @@
+/// <reference types="cypress"/>
+
+export class Login {
+      constructor() {
+
+
+ 
+  }
+  login(url: string, email: string, password: string) {
+ {
+ cy.visit(url);
+    cy.get('input[id=Email]').clear().type(email)
+    cy.get('input[id=Password]').clear().type(password)
+    cy.get('input[id=cmdLogin]').click()
+
+
+cy.server();
+   cy.route('**/ObjectTableLastUpdate/**').as('LoadDataCompleted');
+
+ 
+
+ cy.wait('@LoadDataCompleted');
+
+
+
+
+ })
+
+}
+
