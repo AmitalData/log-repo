5114e6016ba9881a7d5e267b9f74a59eec9fee58
Abed <abed@logitudeworld.com>
2020-07-05 21:10:53 +0300

    68576
    Related Work Items: #68576

--- Logitude/WarehouseData/WarehouseScript/BuildWarehouse/BuildFactShipmentTable.sql
+++ Logitude/WarehouseData/WarehouseScript/BuildWarehouse/BuildFactShipmentTable.sql
@@ -204,7 +204,7 @@
  declare @ExceptionResolvedDescription as nvarchar(500)
  declare @LastExceptionDescription as nvarchar(2000)
  declare @RegistryDate as datetime
- declare @GrossWeightPerTon as decimal
+ declare @GrossWeightPerTon as float
  declare @NextETA as datetime
  declare @NextETD as datetime
  declare @Commodity as nvarchar(15)
