d5c5b91146e2852a448ca18f49568ecb66416443
alaa <alaa.jamal.h@live.com>
2020-07-05 18:43:44 +0300

    68748
    Related Work Items: #68748

--- Logitude/JustWebFreight/WebFreight.Web/ReportsWebServices/LogitudeReportsWebService.asmx.cs
+++ Logitude/JustWebFreight/WebFreight.Web/ReportsWebServices/LogitudeReportsWebService.asmx.cs
@@ -11811,7 +11811,7 @@ namespace WebFreight.Web.ReportsWebServices
                 DoNotShowCardWithLocalCloseBalanceEqualZero = dontShowCardsWith0Balance,
                 IsRevenueExpenseReport = false,
                 //  Skip = true
-
+                Suppress_DoNotShowCardWithoutActivity =false,
 
             };
 
@@ -12377,6 +12377,7 @@ namespace WebFreight.Web.ReportsWebServices
                 trailReportParam.Category4 = category4;
                 trailReportParam.Category5 = category5;
                 trailReportParam.MyTrailReportLevel = ReportLevel.GLAccount;
+                trailReportParam.Suppress_DoNotShowCardWithoutActivity = false;
                 trailReportParam.DoNotShowCardWithLocalCloseBalanceEqualZero =dontShowCardsWith0Balance;
                 var servce = TrailReportFactory.CreateNew(trailReportParam);
 
