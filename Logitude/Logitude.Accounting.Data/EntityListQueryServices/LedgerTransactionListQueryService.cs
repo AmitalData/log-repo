5afbbe7436f64ff4f0a79acb72c6ec36f167d47b
Abdullah Dawoud <abdullah@logitudeworld.com>
2020-07-07 12:56:31 +0300

    Related Work Items: #69231

--- Logitude/Logitude.Accounting.Data/EntityListQueryServices/LedgerTransactionListQueryService.cs
+++ Logitude/Logitude.Accounting.Data/EntityListQueryServices/LedgerTransactionListQueryService.cs
@@ -434,6 +434,7 @@ namespace Logitude.Accounting.Data.EntityListQueryServices
         {
             query2 = query2
                 .Where(rec => rec.IsReconciled == false)
+                .Where(rec => rec.IsExternalReconcile == false)
                 .Where(rec => rec.InReconcileProgress == false)// Seee CreateJournalReconcileService!!!
                 .Where(rec => rec.AccountId == AccountId)
                 //.OrderBy(rec => rec.AccountingDate)
