3f4190884d408a6a8b7ab3713c59a163f52ad7f4
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2020-07-12 20:47:54 +0300

    e2e/raghad

--- e2e/cypress/IslamsTeam/Master/Report/Report.spec.ts
+++ e2e/cypress/IslamsTeam/Master/Report/Report.spec.ts
@@ -23,7 +23,7 @@ constructor() {
 
 
 it('Search For Report', () => {
-    //cy.wait(10000)
+    cy.wait(10000)
     cy.get('#GeneralMHReports').click();
     cy.get('#null_Search').type('Automation test report')
     cy.get('#ReportID').click()
