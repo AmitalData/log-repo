f2a78d3c03731b32a59faa5c70be975b3e1df146
Samar Abu Eid <Samar@logitudeworld.com>
2020-07-12 20:13:28 +0300

    69412
    Related Work Items: #69412

--- Logitude/Logitude.BL/ShipmentsModel/Tools/Validating/ShipmentValidating.cs
+++ Logitude/Logitude.BL/ShipmentsModel/Tools/Validating/ShipmentValidating.cs
@@ -1524,72 +1524,9 @@ namespace Logitude.BL.ShipmentsModel.Tools.Validating
 
                 if (subType != null)
                 {
-                    switch (entityPM.TransportModeId)
+                    if(entityPM.ShipmentTypeId != subType.ShipmentTypeCode)
                     {
-                        case "A":
-                            {
-                                if (subType.Code != "Air")
-                                {
-                                    throw new ApplicationException("Sub Type should be Air when Shipment Transport Mode is Airline");
-                                }
-                                break;
-                            }
-
-                        case "I":
-                            {
-                                if (entityPM.ShipmentTypeId == "FTL")
-                                {
-                                    if (subType.Code != "FTL")
-                                    {
-                                        throw new ApplicationException("Sub Type should be FTL when Shipment Type is FTL");
-                                    }
-                                }
-
-                                else if (entityPM.ShipmentTypeId == "LTL")
-                                {
-                                    if (subType.Code != "LTL")
-                                    {
-                                        throw new ApplicationException("Sub Type should be LTL when Shipment Type is LTL");
-                                    }
-                                }
-
-                                else if (entityPM.ShipmentTypeId == "MyGI")
-                                {
-                                    if (subType.Code != "MyGI")
-                                    {
-                                        throw new ApplicationException("Sub Type should be Groupage when Shipment Type is Groupage");
-                                    }
-                                }
-                                break;
-                            }
-
-                        case "O":
-                            {
-                                if (entityPM.ShipmentTypeId == "FCLD")
-                                {
-                                    if (subType.Code != "FCL")
-                                    {
-                                        throw new ApplicationException("Sub Type should be FCL when Shipment Type is FCL");
-                                    }
-                                }
-
-                                else if (entityPM.ShipmentTypeId == "LCLD")
-                                {
-                                    if (subType.Code != "LCL")
-                                    {
-                                        throw new ApplicationException("Sub Type should be LCL when Shipment Type is LCL");
-                                    }
-                                }
-
-                                else if (entityPM.ShipmentTypeId == "MyGO")
-                                {
-                                    if (subType.Code != "MyGO")
-                                    {
-                                        throw new ApplicationException("Sub Type should be Groupage when Shipment Type is Groupage");
-                                    }
-                                }
-                                break;
-                            }
+                        throw new ApplicationException("Sub Type is not allowed with this shipment type");
                     }
                 }
             }
