a9512582083d695dad9f02eb292f69513d450eda
Mutaz Zuhairi <Mutaz@logitudeworld.com>
2020-07-13 12:25:50 +0300

    FixCargoRequest

--- Logitude/AngularModules/CargoTrackingModules/src/CargoTracking/Utilities/ServiceHelper.ts
+++ Logitude/AngularModules/CargoTrackingModules/src/CargoTracking/Utilities/ServiceHelper.ts
@@ -6,7 +6,7 @@ export  class ServiceHelper{
         if (window.location.origin.indexOf('localhost') > -1)
             return 'http://localhost:9996/';
         else
-            return window.location.origin + "/";
+            return window.location.origin + "/test/";
     }
     public static GetHeaders(){
 
