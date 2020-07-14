95d1afb5725daf3308d605baacb6bd0b92227305
Wesam <Wesam@WESAM>
2020-07-14 09:49:53 +0300

    Close 'Total Per Container' parameter when Quote status is 'Sent'
    Related Work Items: #69476

--- Logitude/AngularModules/AngularModules/QuoteModules/QuoteCharges/Components/FCLChargesComponent.ts
+++ Logitude/AngularModules/AngularModules/QuoteModules/QuoteCharges/Components/FCLChargesComponent.ts
@@ -72,7 +72,7 @@ export class FCLChargesComponent extends BaseComponent implements OnDestroy {
         this.AllInMatchText = TextCodeTranslator.Translate("Quote.M.UnableToDoAllIn") + "\n" + TextCodeTranslator.Translate("Quote.M.IfMatchesFrieghtCharge");
         this.HideFCLAllIn = SessionLocator.TenantPM.HideFCLAllIn;
 
-       if( FeatureLocator.HasFeaturePermession("Quote", "TOTALPERCONTAINER")) {
+       if( !FeatureLocator.HasFeaturePermession("Quote", "TOTALPERCONTAINER")) {
             this.IsShowTotalPerContainer = true;
         }
 
@@ -469,6 +469,7 @@ export class FCLChargesComponent extends BaseComponent implements OnDestroy {
         this.UIProperties.SetEnabled("SaleCurrencyId", this.ObjectTableName, this.IsEditingEnabled);
         this.UIProperties.SetEnabled("ExchangeRate", this.ObjectTableName, isExchangeRateEnabled);
         this.UIProperties.SetEnabled("IsFixedPrice", this.ObjectTableName, this.IsEditingEnabled);
+        this.UIProperties.SetEnabled("TotalPerContainer", this.ObjectTableName, this.IsEditingEnabled);
     }
 
     public IsCostQuantityVisible: boolean = false;
