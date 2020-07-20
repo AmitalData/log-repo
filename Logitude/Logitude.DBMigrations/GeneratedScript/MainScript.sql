06e411d60e7b55b7b0393fa5e414be98b61404c9
sohaib <sohaib@amital.co.il>
2020-07-20 11:09:23 +0300

    #69603

--- Logitude/Logitude.DBMigrations/GeneratedScript/MainScript.sql
+++ Logitude/Logitude.DBMigrations/GeneratedScript/MainScript.sql
@@ -1,4 +1,4 @@
--- General Script From 202007031145_DeclarationCancellationAddOn.sxml File
+-- General Script From 202007201100_ReferantSearchfield.sxml File
 DECLARE
 StartTime TIMESTAMP;
 EndTime TIMESTAMP;
@@ -6,36 +6,32 @@ BEGIN
 SAVEPOINT ScriptSavePoint;
 StartTime := SYSTIMESTAMP;
 BEGIN
-declare
-n int := 0;
-begin
-select count(*) into n
-from PACKAGES
-where code='DECA';
-if n = 0 then
-Insert into PACKAGES (CODE,NAME,SEARCHFIELDS,INACTIVE,FEATUREPACKAGETYPECODE) values ('DECA','Declaration Cancellation','DECA,Declaration Cancellation',0,'BS');
-Insert into PACKAGES (CODE,NAME,SEARCHFIELDS,INACTIVE,FEATUREPACKAGETYPECODE) values ('DECAA','Declaration Cancellation AddOn','DECAA,Declaration Cancellation AddOn',0,'AD');
-Insert into PACKAGECONNECTEDPACKAGES (ID,PACKAGECODE,CONNECTEDPACKAGECODE) values ('G-23','DECA','DECAA');commit;
-end if;
-end;
+update declarationreferantdatas set
+declarationreferantdatas.SEARCHFIELDS=
+(select   to_char(d.DeclarationNumber)||',' ||to_char(d.CustomFileNo) ||',' || to_char(d.CustomerId) ||',' ||
+to_char(c.ManifestNumber )  ||',' || c.SecondCargoID  ||',' || c.ThirdCargoID as SEARCHFIELDS
+from  declarationreferantdatas r
+inner join
+declarations d on  r.DECLARATIONID= d.id
+inner join  CONSIGNMENTS c
+on d.id= c.DECLARATIONID  and c.CONSIGNMENTNUMBER=1
+where  d.id= declarationreferantdatas.DECLARATIONID);
 END;
 EndTime:= SYSTIMESTAMP;
 BEGIN
 DECLARE ScriptBody NCLOB;
 BEGIN
-ScriptBody := 'declare
-n int := 0;
-begin
-select count(*) into n
-from PACKAGES
-where code=''DECA'';
-if n = 0 then
-Insert into PACKAGES (CODE,NAME,SEARCHFIELDS,INACTIVE,FEATUREPACKAGETYPECODE) values (''DECA'',''Declaration Cancellation'',''DECA,Declaration Cancellation'',0,''BS'');
-Insert into PACKAGES (CODE,NAME,SEARCHFIELDS,INACTIVE,FEATUREPACKAGETYPECODE) values (''DECAA'',''Declaration Cancellation AddOn'',''DECAA,Declaration Cancellation AddOn'',0,''AD'');
-Insert into PACKAGECONNECTEDPACKAGES (ID,PACKAGECODE,CONNECTEDPACKAGECODE) values (''G-23'',''DECA'',''DECAA'');commit;
-end if;
-end;';
-INSERT INTO "DBSCRIPTSHISTORY"("SXMLFILENAME", "EXECUTIONDATE", "SCRIPTBODY", "ELAPSEDTIMEINMS", "HASHVALUE", "VERSION")VALUES('202007031145_DeclarationCancellationAddOn.sxml', SYSDATE, ScriptBody, EXTRACT(DAY FROM(EndTime - StartTime) * 24 * 60 * 60 * 1000), '584da0fff34716b45bb5f101fe923b74', 1);
+ScriptBody := 'update declarationreferantdatas set
+declarationreferantdatas.SEARCHFIELDS=
+(select   to_char(d.DeclarationNumber)||'','' ||to_char(d.CustomFileNo) ||'','' || to_char(d.CustomerId) ||'','' ||
+to_char(c.ManifestNumber )  ||'','' || c.SecondCargoID  ||'','' || c.ThirdCargoID as SEARCHFIELDS
+from  declarationreferantdatas r
+inner join
+declarations d on  r.DECLARATIONID= d.id
+inner join  CONSIGNMENTS c
+on d.id= c.DECLARATIONID  and c.CONSIGNMENTNUMBER=1
+where  d.id= declarationreferantdatas.DECLARATIONID);';
+INSERT INTO "DBSCRIPTSHISTORY"("SXMLFILENAME", "EXECUTIONDATE", "SCRIPTBODY", "ELAPSEDTIMEINMS", "HASHVALUE", "VERSION")VALUES('202007201100_ReferantSearchfield.sxml', SYSDATE, ScriptBody, EXTRACT(DAY FROM(EndTime - StartTime) * 24 * 60 * 60 * 1000), '24d87dde50c2bf9294d31e637e485ad6', 1);
 END;
 END;
 EXCEPTION
