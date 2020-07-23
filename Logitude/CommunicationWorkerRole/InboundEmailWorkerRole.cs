df630d5e4c223121f88da33ecc411155aa88ebae
Maheera Jazi <Maheera@logitudeworld.com>
2020-07-23 10:46:19 +0300

    fix

--- Logitude/CommunicationWorkerRole/InboundEmailWorkerRole.cs
+++ Logitude/CommunicationWorkerRole/InboundEmailWorkerRole.cs
@@ -19,7 +19,7 @@ using Simplog.Data.CommonDataModel;
 using Simplog.Data.CommonDataModel.EntityPOCOs;
 using Simplog.Data.CommonDataModel.Repositories;
 using Simplog.Data.Helpers;
-using Simplog.Data.InfrastructureModel.EntityPOCOs;
+using Simplog.Data.InfrastructureModel.EntityPOCOs; 
 using Simplog.Data.InfrastructureModel.Repositories;
 using Simplog.Global.Data.GlobalModel.EntityPOCOs;
 using Simplog.Global.Data.GlobalModel.Repositories;
