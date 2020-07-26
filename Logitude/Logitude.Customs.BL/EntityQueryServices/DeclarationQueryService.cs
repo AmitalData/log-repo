7bafc5144ff411ef83256068d5bf691405e830db
elisheva <elisheva@amital.co.il>
2020-07-26 09:05:43 +0300

    69773 , 69275
    Related Work Items: #69275, #69773

--- Logitude/Logitude.Customs.BL/EntityQueryServices/DeclarationQueryService.cs
+++ Logitude/Logitude.Customs.BL/EntityQueryServices/DeclarationQueryService.cs
@@ -1773,7 +1773,7 @@ namespace Logitude.Customs.BL.EntityQueryServices
             }
             if (orderById)
             {
-                declarationLists = declarationLists.OrderBy(x =>Convert.ToInt32(x.AmendmentRequestNumber)).ToList();
+                declarationLists = declarationLists.OrderBy(x =>x.Id).ToList();
                 declarationLists.ForEach(x => { x.AmendmentNumber = i; i++; });
 
                 return declarationLists.ToList();
