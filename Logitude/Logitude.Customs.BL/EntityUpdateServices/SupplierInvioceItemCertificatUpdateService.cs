8fb3dbce27274b8d3b1410452e9d96bbbfe07e97
sohaib <sohaib@amital.co.il>
2020-07-30 15:14:42 +0300

    #70265
    Related Work Items: #68675

--- Logitude/Logitude.Customs.BL/EntityUpdateServices/SupplierInvioceItemCertificatUpdateService.cs
+++ Logitude/Logitude.Customs.BL/EntityUpdateServices/SupplierInvioceItemCertificatUpdateService.cs
@@ -653,7 +653,10 @@ namespace Logitude.Customs.BL.EntityUpdateServices
             entity.ResConfirmationTypeCode = "2402";
             entity.ReqConfirmationTypeCode = "2402";
             entity.AttachmentTypeCode = "2";
-            entity.SequenceNumeric = supplierInvioceItemCertificatRepository.getNextSequenceNumber(decId, tenant,invoiceItem.LineNumber);
+            if(entity.SequenceNumeric == 0 )
+            {
+                entity.SequenceNumeric = supplierInvioceItemCertificatRepository.getNextSequenceNumber(decId, tenant, invoiceItem.LineNumber);
+            }
             entity.ApprovalRequestNumber = requestNumber;
             invoiceItem.SupplierInvioceItemCertificats.Add(entity);
             invoiceItem.ChangeSetOp = ChangeSetOperation.Update;
