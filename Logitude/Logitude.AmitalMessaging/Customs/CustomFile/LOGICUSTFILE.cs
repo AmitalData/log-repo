dbfda7f2f2552d12b00fe131d5360b8b4dd5c521
moran <moran@amital.co.il>
2020-08-04 18:04:39 +0300

    69452 ms
    Related Work Items: #69452

--- Logitude/Logitude.AmitalMessaging/Customs/CustomFile/LOGICUSTFILE.cs
+++ Logitude/Logitude.AmitalMessaging/Customs/CustomFile/LOGICUSTFILE.cs
@@ -170,6 +170,8 @@ public partial class LogitudeCustomsFile
 
     private string newFileField;
 
+    private string teamField;
+
     /// <remarks/>
     public string CustomFileNo
     {
@@ -934,6 +936,19 @@ public partial class LogitudeCustomsFile
         set
         {
             this.newFileField = value;
+        }
+    }
+
+    /// <remarks/>
+    public string Team
+    {
+        get
+        {
+            return this.teamField;
+        }
+        set
+        {
+            this.teamField = value;
         }
     }
 }
