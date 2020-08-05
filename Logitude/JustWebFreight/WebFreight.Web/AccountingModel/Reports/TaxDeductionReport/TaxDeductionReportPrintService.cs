a4b775b472023fbfdba4a4425901b3bf5c52c042
alaa <alaa.jamal.h@live.com>
2020-08-05 18:24:57 +0300

    #70397

--- Logitude/JustWebFreight/WebFreight.Web/AccountingModel/Reports/TaxDeductionReport/TaxDeductionReportPrintService.cs
+++ Logitude/JustWebFreight/WebFreight.Web/AccountingModel/Reports/TaxDeductionReport/TaxDeductionReportPrintService.cs
@@ -81,7 +81,7 @@ namespace WebFreight.Web.AccountingModel.Reports.TaxDeductionReport
             //TenantPM tenantPM = TenantQuery.GetSingleTenantPM(tenant, false);
             TaxDeductionReportDataProvider deductionReportDataProvider = new TaxDeductionReportDataProvider(taxDeductionReportPM, tenant);
             TaxDeductionReportData data = deductionReportDataProvider.GetTaxDeductionReportData();
-            data.TaxYear = taxDeductionReportPM.ByMonth ? taxDeductionReportPM.Month + "/" + taxDeductionReportPM.TaxYear : taxDeductionReportPM.TaxYear.ToString();
+            data.TaxYear = taxDeductionReportPM.ByMonth ? taxDeductionReportPM.Month.Value.Month + "/" + taxDeductionReportPM.TaxYear : taxDeductionReportPM.TaxYear.ToString();
             data.ByVendorList= data.ByVendorList.OrderBy(d => d.VendorLocalName).ToList();
             return data;
         }
