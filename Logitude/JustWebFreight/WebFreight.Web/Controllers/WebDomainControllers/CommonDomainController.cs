c55c498a037cee7fdfef9342061769a8b9b97e42
Maheera Jazi <Maheera@logitudeworld.com>
2020-08-05 20:12:14 +0300

    70161

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/WebDomainControllers/CommonDomainController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/WebDomainControllers/CommonDomainController.cs
@@ -179,16 +179,17 @@ namespace WebFreight.Web.Controllers.WebDomainControllers
             dataTable1.Columns.Add("Fax Number");
             dataTable1.Columns.Add("E-Mail");
             dataTable1.Columns.Add("Contact Per.");
-            dataTable1.Columns.Add("Account ID = External ID");
+            dataTable1.Columns.Add("Receivables Account ID = Receivables External ID");
+            dataTable1.Columns.Add("Payables Account ID = Payables External ID");
             dataTable1.Columns.Add("Code");
-            var range = "A1:P1";
+            var range = "A1:Q1";
             sheet1.Range["A2:A1001"].DataValidation.ListOfValues = partnersTypes_Sheet.Select(s => s.Code).ToArray();
             sheet1.Range["A2:A1001"].DataValidation.IsSuppressDropDownArrow = false;
             sheet1.Range[range].CellStyle.Font.Color = ExcelKnownColors.White;
             sheet1.Range[range].CellStyle.Color = System.Drawing.Color.Gray;
             sheet1.Range[range].CellStyle.HorizontalAlignment = ExcelHAlign.HAlignCenter;
             sheet1.Columns[1].ColumnWidth = sheet1.Columns[6].ColumnWidth = sheet1.Columns[9].ColumnWidth = sheet1.Columns[10].ColumnWidth = sheet1.Columns[11].ColumnWidth = sheet1.Columns[13].ColumnWidth = 14;
-            sheet1.Columns[14].ColumnWidth = 20;
+            sheet1.Columns[14].ColumnWidth = sheet1.Columns[15].ColumnWidth = 20;
             sheet1.ImportDataTable(dataTable1, true, 1, 1);
         }
         [ActionName("PostUploadPartnersExcelFile")]
