348080da60e866f6ba8118467050502c807b3d63
moran <moran@amital.co.il>
2020-08-11 17:57:44 +0300

    70343 ms
    Related Work Items: #70343

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationSupplierInvoice/Components/SupplierInvoices/DeclarationSupplierInvoiceTabComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationSupplierInvoice/Components/SupplierInvoices/DeclarationSupplierInvoiceTabComponent.ts
@@ -617,7 +617,7 @@ export class DeclarationSupplierInvoiceTabComponent extends BaseComponent implem
         }
     }
     accumulationFeature: any;
-    notToCheckFeature: boolean = true;
+    notToCheckFeature: boolean = false;
     NewInvoice() {
         var itemPM = new SupplierInvoicePM();
         itemPM.DeclarationId = this.EntityPM.Id;
