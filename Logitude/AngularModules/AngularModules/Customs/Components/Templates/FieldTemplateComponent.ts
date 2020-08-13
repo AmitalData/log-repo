07a58d64ccdf462d9962498e185705dbe61cb015
sohaib <sohaib@amital.co.il>
2020-08-13 15:59:26 +0300

    #70776

--- Logitude/AngularModules/AngularModules/Customs/Components/Templates/FieldTemplateComponent.ts
+++ Logitude/AngularModules/AngularModules/Customs/Components/Templates/FieldTemplateComponent.ts
@@ -5,7 +5,7 @@ import { ServiceResponse } from '../../../Infrastructure/DataContracts/ServiceRe
 import { LogitudeWindow } from '../../../Controls/Windows/LogitudeWindow';
 import { CustomsAutonomyKeywordExtendedPMService } from '../../Services/ExtendedPMs/CustomsAutonomyKeywordExtendedPMService';
 import { ListComponentArgs } from '../../../Infrastructure/Args';
-import { AppTool } from '../../../Infrastructure/Tools';
+import { AppTool, DateTool } from '../../../Infrastructure/Tools';
 import { ConfirmWindow } from '../../../Controls/Windows/ConfirmWindow';
 import { TextCodeTranslator } from '../../../Infrastructure/Utilities/TextCodeTranslator';
 import { DeclarationRemarksService } from '../../../Common/Services/ExtendedPMs/DeclarationRemarksService';
