0d042c1a9433f60d55d7a2b85d112d9cc029dbd8
logitudo <logitudo@live.com>
2020-08-16 11:16:56 +0300

    #70812 by Abdullah

--- Logitude/Logitude.Accounting.Data/EntityListQueryServices/GLAccountListQueryService.cs
+++ Logitude/Logitude.Accounting.Data/EntityListQueryServices/GLAccountListQueryService.cs
@@ -32,7 +32,8 @@ namespace Logitude.Accounting.Data.EntityListQueryServices
             GLAccountRepository repository = new GLAccountRepository(context);
             IQueryable<GLAccountList> query = (from a in iQueryable
                                                join md in context.GLAccountMoreDatas on a.Id equals md.AccountId
-                                               join card in context.Cards.DefaultIfEmpty() on a.Id equals card.GLAccountId
+                                               join card in context.Cards on a.Id equals card.GLAccountId into ca
+                                               from card in ca.DefaultIfEmpty()
                                                select new GLAccountList()
                                                     {
                                                         SalesmanUserId = card == null ? null : card.SalesmanUserId,
