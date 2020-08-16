d9ca8a60e1f91e80f45c853ef583a41f74e85a69
Raghad Sous <raghad@logitudeworld.com>
2020-08-16 14:40:59 +0300

    e2e

--- e2e/cypress/IslamsTeam/Master/ShimentView/ShipmentView.spec.ts
+++ e2e/cypress/IslamsTeam/Master/ShimentView/ShipmentView.spec.ts
@@ -35,7 +35,7 @@ it('OpenShipmentView', () => {
 
 it('CreateNewView ', () => {
     // cy.wait(1000)
-    cy.get('#QueryList_0_0').click()
+    cy.get('#QueryList_0_0').click({ force: true })
     cy.get('#NewViewId_0_0').click()
     cy.get('#NewViewTabchoose').click()
     cy.get('#ViewNameId').type('CypressTest View')
