3039a3edae42788fed1cd58c97e0ca2c365d0a8b
Sumaya Ghazi <sumaya@logitudeworld.com>
2020-08-23 11:08:52 +0300

    # Fix By Mutaz

--- Logitude/Logitude.Accounting.Data/EntityListQueryServices/GLAccountListQueryService.cs
+++ Logitude/Logitude.Accounting.Data/EntityListQueryServices/GLAccountListQueryService.cs
@@ -120,7 +120,7 @@ namespace Logitude.Accounting.Data.EntityListQueryServices
                                                         Category4Name = a.Category4.EnglishName,
                                                         Category5Name = a.Category5.EnglishName,
 
-                                                        InterestCreditLimit = a.InterestCreditLimit,
+                                                         
 
                                                         // Created & Updated
                                                         CreateDate = a.CreateDate,
