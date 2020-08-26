7f18cbd3de82546bfe206941edbc3664267607ef
Islam Shaheen <islam@logitudeworld.com>
2020-08-26 10:29:50 +0300

    adding the service

---
+++ Logitude/JustWebFreight/WebFreight.Web/WcfApi/IPaymentTermWcfService.cs
@@ -0,0 +1,20 @@
+﻿using Intuit.Ipp.Core.Configuration;
+using Logitude.BL.CommonDataModel.EntityLists;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WebFreight.Web.WcfApi
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IPaymentTermWcfService" in both code and config file together.
+    [ServiceContract]
+    public interface IPaymentTermWcfService
+    {
+
+        [OperationContract]
+        List<PaymentTermList> GetIncoterms(ref Response response, int tenant);
+    }
+}
---
+++ Logitude/JustWebFreight/WebFreight.Web/WcfApi/PaymentTermWcfService.svc.cs
@@ -0,0 +1,21 @@
+﻿using Intuit.Ipp.Core.Configuration;
+using Logitude.BL.CommonDataModel.EntityLists;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WebFreight.Web.WcfApi
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "PaymentTermWcfService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select PaymentTermWcfService.svc or PaymentTermWcfService.svc.cs at the Solution Explorer and start debugging.
+    public class PaymentTermWcfService : IPaymentTermWcfService
+    {
+        public List<PaymentTermList> GetIncoterms(ref Response response, int tenant)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
