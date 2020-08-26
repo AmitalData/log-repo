f3c0285d3159a416fe952d812ee6cdaf066994df
Islam Shaheen <islam@logitudeworld.com>
2020-08-26 06:50:03 +0000

    Merged PR 4709: Warehouse Web Service -R3
    Related work items: #71089

---
+++ LogitudeTools/HybridTest/HybridTest/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace HypredTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSendXml = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnThreads = new System.Windows.Forms.Button();
+            this.btnUploadQuoteDocument = new System.Windows.Forms.Button();
+            this.btnDocsIn = new System.Windows.Forms.Button();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnTestCloud = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnMultiTaskQueueTest = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btnCustomerWD = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.btnRunTest = new System.Windows.Forms.Button();
+            this.cmdServices = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnSendXml
+            // 
+            this.btnSendXml.Location = new System.Drawing.Point(61, 278);
+            this.btnSendXml.Name = "btnSendXml";
+            this.btnSendXml.Size = new System.Drawing.Size(144, 34);
+            this.btnSendXml.TabIndex = 0;
+            this.btnSendXml.Text = "Send Xml";
+            this.btnSendXml.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btnSendXml.UseVisualStyleBackColor = true;
+            this.btnSendXml.Click += new System.EventHandler(this.btnSendXml_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 240);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Test Service";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnThreads
+            // 
+            this.btnThreads.Location = new System.Drawing.Point(61, 328);
+            this.btnThreads.Name = "btnThreads";
+            this.btnThreads.Size = new System.Drawing.Size(144, 33);
+            this.btnThreads.TabIndex = 2;
+            this.btnThreads.Text = "Start Threadeds";
+            this.btnThreads.UseVisualStyleBackColor = true;
+            this.btnThreads.Click += new System.EventHandler(this.btnThreads_Click);
+            // 
+            // btnUploadQuoteDocument
+            // 
+            this.btnUploadQuoteDocument.Location = new System.Drawing.Point(61, 200);
+            this.btnUploadQuoteDocument.Name = "btnUploadQuoteDocument";
+            this.btnUploadQuoteDocument.Size = new System.Drawing.Size(144, 34);
+            this.btnUploadQuoteDocument.TabIndex = 3;
+            this.btnUploadQuoteDocument.Text = "Upload Xml File";
+            this.btnUploadQuoteDocument.UseVisualStyleBackColor = true;
+            this.btnUploadQuoteDocument.Click += new System.EventHandler(this.btnUploadQuoteDocument_Click);
+            // 
+            // btnDocsIn
+            // 
+            this.btnDocsIn.Location = new System.Drawing.Point(275, 200);
+            this.btnDocsIn.Name = "btnDocsIn";
+            this.btnDocsIn.Size = new System.Drawing.Size(161, 33);
+            this.btnDocsIn.TabIndex = 4;
+            this.btnDocsIn.Text = "Upload Docs In";
+            this.btnDocsIn.UseVisualStyleBackColor = true;
+            this.btnDocsIn.Click += new System.EventHandler(this.btnDocsIn_Click);
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(275, 239);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(161, 33);
+            this.btnTest.TabIndex = 5;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(248, 427);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 75);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Send Xml To Cloud";
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnTestCloud
+            // 
+            this.btnTestCloud.Location = new System.Drawing.Point(275, 288);
+            this.btnTestCloud.Name = "btnTestCloud";
+            this.btnTestCloud.Size = new System.Drawing.Size(161, 42);
+            this.btnTestCloud.TabIndex = 7;
+            this.btnTestCloud.Text = "Test Cloud";
+            this.btnTestCloud.UseVisualStyleBackColor = true;
+            this.btnTestCloud.Click += new System.EventHandler(this.btnTestCloud_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(515, 199);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(162, 73);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Test Customer Queue";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnMultiTaskQueueTest
+            // 
+            this.btnMultiTaskQueueTest.Location = new System.Drawing.Point(515, 288);
+            this.btnMultiTaskQueueTest.Name = "btnMultiTaskQueueTest";
+            this.btnMultiTaskQueueTest.Size = new System.Drawing.Size(162, 73);
+            this.btnMultiTaskQueueTest.TabIndex = 9;
+            this.btnMultiTaskQueueTest.Text = "Start MultiTask Queue Test";
+            this.btnMultiTaskQueueTest.UseVisualStyleBackColor = true;
+            this.btnMultiTaskQueueTest.Click += new System.EventHandler(this.btnMultiTaskQueueTest_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(553, 472);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(200, 30);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnCustomerWD
+            // 
+            this.btnCustomerWD.Location = new System.Drawing.Point(37, 441);
+            this.btnCustomerWD.Name = "btnCustomerWD";
+            this.btnCustomerWD.Size = new System.Drawing.Size(168, 61);
+            this.btnCustomerWD.TabIndex = 11;
+            this.btnCustomerWD.Text = "Customer WD";
+            this.btnCustomerWD.UseVisualStyleBackColor = true;
+            this.btnCustomerWD.Click += new System.EventHandler(this.btnCustomerWD_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(515, 388);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(191, 39);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "upload trace events";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // btnRunTest
+            // 
+            this.btnRunTest.Location = new System.Drawing.Point(360, 35);
+            this.btnRunTest.Name = "btnRunTest";
+            this.btnRunTest.Size = new System.Drawing.Size(153, 30);
+            this.btnRunTest.TabIndex = 13;
+            this.btnRunTest.Text = "Run Service Test";
+            this.btnRunTest.UseVisualStyleBackColor = true;
+            this.btnRunTest.Click += new System.EventHandler(this.btnRunTest_Click);
+            // 
+            // cmdServices
+            // 
+            this.cmdServices.FormattingEnabled = true;
+            this.cmdServices.Items.AddRange(new object[] {
+            "Warehouse"});
+            this.cmdServices.Location = new System.Drawing.Point(37, 41);
+            this.cmdServices.Name = "cmdServices";
+            this.cmdServices.Size = new System.Drawing.Size(298, 21);
+            this.cmdServices.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 571);
+            this.Controls.Add(this.cmdServices);
+            this.Controls.Add(this.btnRunTest);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.btnCustomerWD);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnMultiTaskQueueTest);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnTestCloud);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.btnDocsIn);
+            this.Controls.Add(this.btnUploadQuoteDocument);
+            this.Controls.Add(this.btnThreads);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSendXml);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSendXml;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnThreads;
+        private System.Windows.Forms.Button btnUploadQuoteDocument;
+        private System.Windows.Forms.Button btnDocsIn;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnTestCloud;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnMultiTaskQueueTest;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnCustomerWD;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btnRunTest;
+        private System.Windows.Forms.ComboBox cmdServices;
+    }
+}
+
