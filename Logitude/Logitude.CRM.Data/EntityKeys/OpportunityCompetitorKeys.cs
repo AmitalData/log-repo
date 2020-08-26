e4592cb4d8e8848dcdbb76e24c4dbd739c26d745
Mohammad Masoud <mohammad@logitudeworld.com>
2020-08-26 09:22:02 +0300

    booking + CRM keys and update classes #70145

--- Logitude/Logitude.CRM.Data/EntityKeys/OpportunityCompetitorKeys.cs
+++ Logitude/Logitude.CRM.Data/EntityKeys/OpportunityCompetitorKeys.cs
@@ -24,7 +24,8 @@ namespace Logitude.CRM.Data.EntityKeys
 	 
 	  public override string GetFullKey()
       {
-          return OpportunityId+'_'+CompetitorId;
+                 return OpportunityId+'_'+CompetitorId ;
+                 
       }
 
       public override string GetEntityPMName()
