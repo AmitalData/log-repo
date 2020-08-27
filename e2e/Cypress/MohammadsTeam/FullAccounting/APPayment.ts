5ed04198b35ab678f1f232c729a40cf660b9b52c
Sumaya Ghazi <sumaya@logitudeworld.com>
2020-08-27 12:08:33 +0300

    cypress fixes
    Related Work Items: #70577

--- e2e/Cypress/MohammadsTeam/FullAccounting/APPayment.ts
+++ e2e/Cypress/MohammadsTeam/FullAccounting/APPayment.ts
@@ -11,20 +11,21 @@ export class APPayment {
             
             cy.get('li[id=FAVND]').click();
             cy.get('button[id=NewAPPayment]').click();
-            cy.get('input[id=APPayment_VendorId]').type(BillToName).should("have.value", BillToName)
+            cy.get('input[id=APPayment_VendorId]').type(BillToName).should("have.value", BillToName).then (a => {
             cy.get('ul[id=mydatalist_APPayment_VendorId]').contains(BillToName).then(a => {
                 a[0].click();
-            })
+            })});
+        
             cy.get('input[id=APPayment_AccountingPaymentMethodId]').type("Cash")       
             cy.get('ul[id=mydatalist_APPayment_AccountingPaymentMethodId]').contains("Cash").then(a => {
                 a[0].click();
             })
             cy.get('input[id=APPayment_AmountInPaymentCurrency]').type("1200")
         
-            cy.get('input[id=APPayment_PaymentCurrencyId]').type("NIS")
+            cy.get('input[id=APPayment_PaymentCurrencyId]').type("NIS").then (a => {
             cy.get('ul[id=mydatalist_APPayment_PaymentCurrencyId]').contains("NIS").then(a => {
                 a[0].click();
-            })
+            })});
             cy.get('button[id=APPaymentBApprove]').click();          
             cy.contains('Approved') 
             cy.log('APPayment Is Approved')
