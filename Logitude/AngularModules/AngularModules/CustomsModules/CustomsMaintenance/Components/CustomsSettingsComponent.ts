e0989e9d9e32fdd28e095e5174f90961a64efb86
elisheva <elisheva@amital.co.il>
2020-08-30 11:22:05 +0300

    71052 #71052

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsMaintenance/Components/CustomsSettingsComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsMaintenance/Components/CustomsSettingsComponent.ts
@@ -231,6 +231,10 @@ export class CustomsSettingsComponent
     set UnfConnectionString(value) { this.entityPM.UnfConnectionString = value; }
 
 
+    get LastRunningDCAWS() { return this.entityPM != null ? this.entityPM.LastRunningDCAWS : null; }
+    set LastRunningDCAWS(value) { this.entityPM.LastRunningDCAWS = value; }
+
+
 
 
     
