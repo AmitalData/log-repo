783063dad71eb6d52400a0ccbf448c531982198b
Abed <abed@logitudeworld.com>
2020-08-30 18:29:58 +0300

    69482
    Related Work Items: #69482

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/Export2ExcelControl/Export2ExcelControl.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/Export2ExcelControl/Export2ExcelControl.ts
@@ -1,3 +1,4 @@
+
 import {Component, Output, EventEmitter} from '@angular/core';
 import {TextCodeTranslationPipe} from '../../../Controls/Pipes/TextCodeTranslationPipe';
 
@@ -11,6 +12,7 @@ import {AmitalGatewayUtil} from '../../../Infrastructure/Utilities/AmitalGateway
 import {ObjectsLocator} from '../../../Infrastructure/Locators/ObjectsLocator';
 import { ServiceResponse } from '../../DataContracts/ServiceResponse';
 import { HttpClient } from '@angular/common/http';
+import {LogboxShipmentExportExcelService} from '../../../Shipment/Services/Others/LogboxShipmentExportExcelService';
 
 @Component({
     
@@ -38,35 +40,56 @@ export class Export2ExcelControl {
     queryCode: string;
     queryId: string;
     userid: string;
+    QueryType: string;
+    ExportExcelArgs: any;
     SetWindowArgs(args: any) {
-        var myService: WebFreightDomainService = new WebFreightDomainService();
-        this.ObjectTableName = args.currentObjectTable;
-        this.tenant = args.tenant;
-        this.queryName = TextCodeTranslator.Translate(args.query.NameTextCodeCode);
-        this.queryId = args.query.Id;
-        this.queryCode = args.query.UniqueCode;
 
-        this.userid = args.userid;
-        this.Filters = args.Filters;
-        myService.getExcelData(this.Filters, this.queryCode, args.tenant, args.userid, args.currentObjectTable).subscribe((myResult: any) => {
-          if (myResult.body == "Faild") {
-                this.btnRetryVisibile = true;
-                this.busyExportingVisibile = false;
-                this.btnSaveToFileVisibile = false;
-            }
-            else {
-              this.FileName = myResult.body;
-                //var tempDate = new Date();
-                //var MyDate = tempDate.getDate() + "-" + (tempDate.getMonth() + 1) + "-" + tempDate.getFullYear();
-                //this.url = logitude_url + "WebPages/DawnLoadExcelPage.aspx?fileName=" + this.FileName + "&tempId=" + ServiceHelper.GetLDocumentDownloadToken() +  "&qname=" + this.queryName + "_" + MyDate;
+        this.QueryType = args.QueryType ? args.QueryType : "";
+        this.queryName = args.QueryName;
+        this.ExportExcelArgs = args.ExportExcelArgs;
 
-                this.btnRetryVisibile = false;
-                this.busyExportingVisibile = false;
-                this.btnSaveToFileVisibile = true;
-            }
+        if (this.QueryType != "LogBox") {
+            var myService: WebFreightDomainService = new WebFreightDomainService();
+            this.ObjectTableName = args.currentObjectTable;
+            this.tenant = args.tenant;
+            this.queryName = TextCodeTranslator.Translate(args.query.NameTextCodeCode);
+            this.queryId = args.query.Id;
+            this.queryCode = args.query.UniqueCode;
 
-        });
+            this.userid = args.userid;
+            this.Filters = args.Filters;
+            myService.getExcelData(this.Filters, this.queryCode, args.tenant, args.userid, args.currentObjectTable).subscribe((myResult: any) => {
+                this.CompleteExcelData(myResult.body);
+            });
+        }
+        else if (this.QueryType == "LogBox") {
+            var logboxShipmentExportExcelService: LogboxShipmentExportExcelService = new LogboxShipmentExportExcelService();
+            logboxShipmentExportExcelService.GetQueryToExcelData(this.ExportExcelArgs).subscribe((myResponse: ServiceResponse) => {
+                if (!myResponse.HasError) this.CompleteExcelData(myResponse.Result);
+                else this.CompleteExcelData("Faild");
+            });
+
+        }
     }
+
+    CompleteExcelData(myResult:any) {
+        if (myResult == "Faild") {
+            this.btnRetryVisibile = true;
+            this.busyExportingVisibile = false;
+            this.btnSaveToFileVisibile = false;
+        }
+        else {
+            this.FileName = myResult;
+            //var tempDate = new Date();
+            //var MyDate = tempDate.getDate() + "-" + (tempDate.getMonth() + 1) + "-" + tempDate.getFullYear();
+            //this.url = logitude_url + "WebPages/DawnLoadExcelPage.aspx?fileName=" + this.FileName + "&tempId=" + ServiceHelper.GetLDocumentDownloadToken() +  "&qname=" + this.queryName + "_" + MyDate;
+
+            this.btnRetryVisibile = false;
+            this.busyExportingVisibile = false;
+            this.btnSaveToFileVisibile = true;
+        }
+    }
+
 
     SaveExcelFile(tenant: number, FileName: string, OTName: string) {
         var tempDate = new Date();
@@ -86,24 +109,26 @@ export class Export2ExcelControl {
         this.SaveExcelFile(this.tenant, this.FileName, this.ObjectTableName);
     }
     RetryBtnClicked() {
+
         this.btnRetryVisibile = false;
         this.busyExportingVisibile = true;
         this.btnSaveToFileVisibile = false;
-        var myService: WebFreightDomainService = new WebFreightDomainService();
-        myService.getExcelData(this.Filters, this.queryCode, this.tenant, this.userid, this.ObjectTableName).subscribe((myResult: any) => {
-            if (myResult == "Faild") {
-                this.btnRetryVisibile = true;
-                this.busyExportingVisibile = false;
-                this.btnSaveToFileVisibile = false;
-            }
-            else {
-                this.FileName = myResult;
-                this.btnRetryVisibile = false;
-                this.busyExportingVisibile = false;
-                this.btnSaveToFileVisibile = true;
-            }
 
-        });
+        if (this.QueryType != "LogBox") {
+
+            var myService: WebFreightDomainService = new WebFreightDomainService();
+            myService.getExcelData(this.Filters, this.queryCode, this.tenant, this.userid, this.ObjectTableName).subscribe((myResult: any) => {
+                this.CompleteExcelData(myResult);
+
+            });
+        } else if (this.QueryType == "LogBox") {
+            var logboxShipmentExportExcelService: LogboxShipmentExportExcelService = new LogboxShipmentExportExcelService();
+            logboxShipmentExportExcelService.GetQueryToExcelData(this.ExportExcelArgs).subscribe((myResponse: ServiceResponse) => {
+                if (!myResponse.HasError) this.CompleteExcelData(myResponse.Result);
+                else this.CompleteExcelData("Faild");
+            });
+
+        }
     }
 
     CancelButtonClicked() {
