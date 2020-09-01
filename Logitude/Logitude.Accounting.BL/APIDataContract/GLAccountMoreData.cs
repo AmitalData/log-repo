392854cfa3b94f6c4838288412d0c9cb9365cbc0
Lana Daoud <Lana@logitudeworld.com>
2020-09-01 13:19:06 +0300

    #65761

---
+++ Logitude/Logitude.Accounting.BL/APIDataContract/GLAccountMoreData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logitude.Accounting.BL.APIDataContract.ApiV1
+{
+   public partial class GLAccountMoreData
+    {
+        public List<GLAccountTotalsByCurrency> GLAccountTotalsByCurrencies { get; set; }
+    }
+}
