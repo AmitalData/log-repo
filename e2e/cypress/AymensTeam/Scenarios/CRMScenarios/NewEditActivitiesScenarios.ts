ebe753e780efd8967f70858293932c62d0268051
Razan Jararah <razan@logitudeworld.com>
2020-09-02 13:02:15 +0300

    71250

--- e2e/cypress/AymensTeam/Scenarios/CRMScenarios/NewEditActivitiesScenarios.ts
+++ e2e/cypress/AymensTeam/Scenarios/CRMScenarios/NewEditActivitiesScenarios.ts
@@ -9,7 +9,7 @@ export class NewEditActivitiesScenarios {
         this.ActivitTypes = ActivitTypes;
         this.EntityNumber = Random.GetRandomNumber();
         this.OpenWizardWindow();
-        this.FillActivitTypes();
+        this.ActivityScenarios();
     }
 
     private OpenWizardWindow() {
@@ -23,42 +23,41 @@ export class NewEditActivitiesScenarios {
         Resolvers.WindowResolver.ShouldBeOpend();
     }
  
-    private FillActivitTypes() {
+    private ActivityScenarios() {
         if (this.ActivitTypes == "T") {
             this.CreateTask(this.EntityNumber);
+            this.SaveAndSearchActivity();
+            this.EditTask(this.EntityNumber);
         }
         if (this.ActivitTypes == "P") {
             this.CreatePhoneCall(this.EntityNumber);
+            this.SaveAndSearchActivity();
+            this.EditPhoneCall(this.EntityNumber);
         }
         if (this.ActivitTypes == "A") {
             this.CreateAppoinment(this.EntityNumber);
+            this.SaveAndSearchActivity();
+            this.EditAppoinment(this.EntityNumber);
         }
 
     }
-    private CreateTask(TaskNumber: string) {
-        cy.get('#Activity_Subject').type(TaskNumber, { force: true }).then(() => {
-            Resolvers.LOVResolver.Selector('#Activity_PriorityCode').SelectFirst();
-            this.Save();
-        })
-        this.SearchAboutActivity(TaskNumber);
-        this.EditTask(TaskNumber);
-    }
-    private CreatePhoneCall(TaskNumber: string) {
-        Resolvers.LOVResolver.Selector('#Activity_CallWithId').SelectFirst();
-        cy.get('#Activity_Subject').type(TaskNumber, { force: true }).then(() => {
+    private CreateTask(EntityNumber: string) {
+        Resolvers.TextBoxResolver.Selector('#Activity_Subject').Type('Task # : ' + EntityNumber);
+        Resolvers.TextBoxResolver.Selector('#Activity_Description').Type('Description for task # : ' + EntityNumber);
         Resolvers.LOVResolver.Selector('#Activity_PriorityCode').SelectFirst();
-            this.Save();
-        })
-        this.SearchAboutActivity(TaskNumber);
-        this.EditPhoneCall(TaskNumber);
     }
-    private CreateAppoinment(TaskNumber: string) {
-        cy.get('#Activity_Subject').type(TaskNumber, { force: true }).then(() => {
-            Resolvers.LOVResolver.Selector('#Activity_PriorityCode').SelectFirst();
-            this.Save();  
-        })
-        this.SearchAboutActivity(TaskNumber);
-        this.EditAppoinment(TaskNumber);
+    private CreatePhoneCall(EntityNumber: string) {
+        Resolvers.LOVResolver.Selector('#Activity_CustomerId').Type('Customer Activity');
+        Resolvers.LOVResolver.Selector('#Activity_CallWithId').SelectFirst();
+
+        Resolvers.TextBoxResolver.Selector('#Activity_Subject').Type('PhoneCall # : ' + EntityNumber);
+        Resolvers.TextBoxResolver.Selector('#Activity_Description').Type('Description for PhoneCall # : ' + EntityNumber);
+        Resolvers.LOVResolver.Selector('#Activity_PriorityCode').SelectFirst();
+    }
+    private CreateAppoinment(EntityNumber: string) {
+        Resolvers.TextBoxResolver.Selector('#Activity_Subject').Type('Appoinment # : ' + EntityNumber);
+        Resolvers.TextBoxResolver.Selector('#Activity_Description').Type('Description for Appoinment # : ' + EntityNumber);
+        Resolvers.LOVResolver.Selector('#Activity_PriorityCode').SelectFirst();
     }
     Save() {
         return new Cypress.Promise((resolve, reject) => {
@@ -69,44 +68,46 @@ export class NewEditActivitiesScenarios {
                 onResponse: (xhr) => {
                     expect(xhr.status).to.eq(200);
                 }
-            }).as('CreateTask')
-            cy.get('#Ok-AddActivity').click();
-            cy.wait('@CreateTask');
-            Resolvers.WindowResolver.ShouldBeClosed();
+            }).as('CreateTask');
+            Resolvers.ButtonResolver.Selector('#Ok-AddActivity').Click();
+            cy.wait('@CreateTask').its('responseBody').then((json) => {
+                resolve(json['Subject']);
+            });
         });
     }
-    SearchAboutActivity(TaskNumber: string) {
-        cy.get('quicksearchtextbox')
-            .find('.LogitudeQuickSearchTextBox')
-            .eq(0)
-            .within(() => {
-                cy.get('input').type(TaskNumber).then(() => {
-                    cy.get('ul > li').eq(0).click({ force: true });
+    SaveAndSearchActivity() {
+        this.Save().then((entityNumber: string) => {
+            cy.get('quicksearchtextbox')
+                .find('.LogitudeQuickSearchTextBox')
+                .within(() => {
+                    cy.get('input').type(entityNumber).then(() => {
+                            cy.get('ul > li').eq(0).click({ force: true });
+                    });
                 });
-            });
+        });
     }
-    EditAppoinment(TaskNumber: string) {
+    EditAppoinment(EntityNumber: string) {
         let today = new Date().toLocaleDateString();
         Resolvers.DatePickerResolver.Selector('#date_Activity_StartDateTime').Type(today);
         Resolvers.DatePickerResolver.Selector('#time_Activity_StartDateTime').Type('10:00 AM');
         Resolvers.LOVResolver.Selector("#Activity_CustomerId").SelectFirst();
-        Resolvers.TextBoxResolver.Selector('#Activity_Description').Type(TaskNumber);
+        Resolvers.TextBoxResolver.Selector('#Activity_Description').Type('Edit Appointment # : ' + EntityNumber);
         cy.get('#Activity-SaveClose').click();
     }
-    EditPhoneCall(TaskNumber: string) {
+    EditPhoneCall(EntityNumber: string) {
         let today = new Date().toLocaleDateString();
         Resolvers.DatePickerResolver.Selector('#date_Activity_DueDate').Type(today);
         Resolvers.DatePickerResolver.Selector('#time_Activity_DueDate').Type('10:00 AM');
         Resolvers.LOVResolver.Selector("#Activity_PriorityCode").SelectFirst();
-        Resolvers.TextBoxResolver.Selector('#Activity_Description').Type(TaskNumber);
+        Resolvers.TextBoxResolver.Selector('#Activity_Description').Type('Edit PhoneCall # : ' + EntityNumber);
         cy.get('#Activity-SaveClose').click();
     }
-    EditTask(TaskNumber: string) {   
+    EditTask(EntityNumber: string) {   
         let today = new Date().toLocaleDateString();
         Resolvers.DatePickerResolver.Selector('#date_Activity_StartDateTime').Type(today);
         Resolvers.DatePickerResolver.Selector('#time_Activity_StartDateTime').Type('10:00 AM');
         Resolvers.LOVResolver.Selector("#Activity_CustomerId").SelectFirst();
-        Resolvers.TextBoxResolver.Selector('#Activity_Description').Type(TaskNumber);
+        Resolvers.TextBoxResolver.Selector('#Activity_Description').Type('Edit Task # : ' + EntityNumber);
         cy.get('#Activity-SaveClose').click();
     }
 }
