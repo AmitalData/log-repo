8ca97dac91b6b6b03043a995efad6de1952b58d8
Mutaz Zuhairi <Mutaz@logitudeworld.com>
2020-09-05 11:06:35 +0300

    #70479

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/AccountingModel/InterestReportController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/AccountingModel/InterestReportController.cs
@@ -57,8 +57,7 @@ namespace WebFreight.Web.Controllers.AccountingModel
 {
     public class InterestReportController : ApiController
     {
-        public Stream _Stream;
-        byte[] datainByte;
+     
 
         [HttpGet]
         public HttpResponseMessage GetInterestReportsByFilters([FromUri] ApiQueryFilters filters)
@@ -112,61 +111,19 @@ namespace WebFreight.Web.Controllers.AccountingModel
             {
                 int tenant = AuthinticateTenant();
                 string email = HttpContext.Current.User.Identity.Name;
-                PdfDocument pdfDoc = new PdfDocument();
-                InterestReportQueryService interestReportQueryService = new InterestReportQueryService(tenant);
-                ARInvoiceQuery aRInvoiceQuery = new ARInvoiceQuery(tenant);
-
-                if (interestReportArgs.AllSelected)
-                {
-                    IQueryable<ARInvoice> ARInvoices = aRInvoiceQuery.GetAllInterestInvoices(interestReportArgs.FromDate, interestReportArgs.ToDate, interestReportArgs.ShowPrintedInvoice, tenant);
-                    if (interestReportArgs.ExcludedIds != null)
-                    {
-                        interestReportArgs.SelectedIds = (from a in ARInvoices
-                                                          where !interestReportArgs.ExcludedIds.Contains(a.Id)
-                                                          select a.Id).ToList();
-
-                    }
-
-                }
- 
-                interestReportArgs.SelectedIds = interestReportQueryService.GetInterestReprtsWithInvocies(tenant, interestReportArgs.SelectedIds);
-
-                for (int i = 0; i < interestReportArgs.SelectedIds.Count; i++)
-                {
-                    string [] EntitiesId = interestReportArgs.SelectedIds[i].Split(',') ;
-                    string InterestReportId = EntitiesId[0];
-                    string ARInvoieId  = EntitiesId[1];
-                    bool IsPrintARInvoice = PrintInvoicesPDF(email, tenant, ARInvoieId, pdfDoc,"999G",false);
-                    if (IsPrintARInvoice)
-                    {
-                        bool IsPrintInterestReport = PrintInvoicesPDF(email, tenant, InterestReportId, pdfDoc, "ITDT", false);
-                    }
- 
-                 }
-
-                MemoryStream memoryStream = new MemoryStream();
-                pdfDoc.Save(memoryStream);
-             
-
-                var dataBytes = memoryStream.ToArray();
-                var dataStream = new MemoryStream(dataBytes);
-
-          
-
+                InteretInvoiceBatchPrint interetInvoiceBatchPrint = new InteretInvoiceBatchPrint();
+                var dataStream = interetInvoiceBatchPrint.CheckValidCopiesForInvoicesAndPrint(interestReportArgs, tenant, email);
                 var response = new HttpResponseMessage
                 {
                     StatusCode = HttpStatusCode.OK,
                     Content = new StreamContent(dataStream),
                 };
-
                 response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
                 {
                     FileName = "InterestInvoices.pdf"
                 };
                 response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/pdf");
-
-
-              
+  
                 return response;
             }
             catch (Exception ex)
@@ -183,65 +140,8 @@ namespace WebFreight.Web.Controllers.AccountingModel
             {
                 int tenant = AuthinticateTenant();
                 string email = HttpContext.Current.User.Identity.Name;
-                PdfDocument pdfDoc = new PdfDocument();
-                InterestReportQueryService interestReportQueryService = new InterestReportQueryService(tenant);
-                ARInvoiceQuery aRInvoiceQuery = new ARInvoiceQuery(tenant);
-
-                if (interestReportArgs.AllSelected)
-                {
-                    IQueryable<ARInvoice> ARInvoices = aRInvoiceQuery.GetAllInterestInvoices(interestReportArgs.FromDate, interestReportArgs.ToDate, interestReportArgs.ShowPrintedInvoice, tenant);
-                    if (interestReportArgs.ExcludedIds != null)
-                    {
-                        interestReportArgs.SelectedIds = (from a in ARInvoices
-                                                          where !interestReportArgs.ExcludedIds.Contains(a.Id)
-                                                          select a.Id).ToList();
-
-                    }
-
-                }
-
-                interestReportArgs.SelectedIds = interestReportQueryService.GetInterestReprtsWithInvocies(tenant, interestReportArgs.SelectedIds);
-                List<string> ARInvoiceIdsNotPrinted = new List<string>();
-                List<string> InterestReportIdsNotPrinted = new List<string>();
-
-                for (int i = 0; i < interestReportArgs.SelectedIds.Count; i++)
-                {
-                    string[] EntitiesId = interestReportArgs.SelectedIds[i].Split(',');
-                    string InterestReportId = EntitiesId[0];
-                    string ARInvoieId = EntitiesId[1];
-                    bool IsPrintARInvoice = PrintInvoicesPDF(email, tenant, ARInvoieId, pdfDoc, "999G",true);
-                    if (!IsPrintARInvoice)
-                    {
-                        ARInvoiceIdsNotPrinted.Add(ARInvoieId);
-
-                    }
-                    bool IsPrintInterestReport = PrintInvoicesPDF(email, tenant, InterestReportId, pdfDoc, "ITDT", true);
-                    if (!IsPrintInterestReport)
-                    {
-                        InterestReportIdsNotPrinted.Add(InterestReportId);
-                    }
-      
-                }
-
- 
-                List<string> ARInvoiceNumbersNotPrinted = null;
-                List<string> InterestReportNumbersNotPrinted = null;
-                if (ARInvoiceIdsNotPrinted != null && ARInvoiceIdsNotPrinted.Count() > 0)
-                {
-                    ARInvoiceNumbersNotPrinted = aRInvoiceQuery.GetInterestInvoiceNumbersByIds(ARInvoiceIdsNotPrinted, tenant);
-
-                }
-                if (InterestReportIdsNotPrinted != null && InterestReportIdsNotPrinted.Count() > 0)
-                {
-                    InterestReportNumbersNotPrinted = interestReportQueryService.GetInterestReportNumbersByIds(InterestReportIdsNotPrinted, tenant);
-
-                }
- 
-                PDFDocumentInvoices pDFDocumentInvoices = new PDFDocumentInvoices();
-                pDFDocumentInvoices.ARInvoiceNumbersNotPrinted = ARInvoiceNumbersNotPrinted;
-                pDFDocumentInvoices.InterestReportNumbersNotPrinted = InterestReportNumbersNotPrinted;
- 
- 
+                InteretInvoiceBatchPrint interetInvoiceBatchPrint = new InteretInvoiceBatchPrint();
+                PDFDocumentInvoices pDFDocumentInvoices=interetInvoiceBatchPrint.GetNumberOfDocumentNotPrinted(interestReportArgs,tenant,email);
                 return Request.CreateResponse(HttpStatusCode.OK, pDFDocumentInvoices);
             }
             catch (Exception ex)
@@ -276,7 +176,6 @@ namespace WebFreight.Web.Controllers.AccountingModel
             {
                 int tenant = AuthinticateTenant();
                 string email = HttpContext.Current.User.Identity.Name;
-
                 InterestReportService interestReportService = new InterestReportService();
                 int NumberOfInterestReportsWithoutInvoice = interestReportService.CheckNumberOfInterestReportInvoicingWithoutInvoice(interestReportArgs, tenant, email);
 
@@ -341,206 +240,8 @@ namespace WebFreight.Web.Controllers.AccountingModel
 
             return queryOperations;
         }
-
-        private bool PrintInvoicesPDF(string Email, int? tenant, string SelectId, PdfDocument pdfDoc, string DocumentCode, bool IsForChecked)
-        {
-            try
-            {
-
-
-                string userId = "";
-                string documentOutId = null;
-                string email = Email;
-                bool IsPrinted = false;
-
-                DocumentOut doucmentOut = null;
-                ICommonDataContext commonContext = CommonDataContext.GetContext((tenant != null ? (int)tenant : 0));
-
-                DocumentTypeQuery documentTypeQuery = new DocumentTypeQuery((int)tenant);
-                string documentTypeId = documentTypeQuery.GetDocumentTypeListIdByCodeAndTenant(DocumentCode, (int)tenant);
-
-
-                DocumentOutQuery documentOutQuery = new DocumentOutQuery((int)tenant);
-
-
-                doucmentOut = (from a in commonContext.DocumentOuts
-                               join docFile in commonContext.DocumentsFilings on a.Id equals docFile.Id
-                               where docFile.EntityId == SelectId && docFile.DocumentTypeId == documentTypeId && a.Tenant == tenant
-                               select a).Include("DocumentsFiling").Include("DocumentsFiling.DocumentType").FirstOrDefault();
-
-                if (doucmentOut != null)
-                {
-                    bool includeInPrint = true;
-
-                    documentOutId = doucmentOut.Id;
-                    tenant = doucmentOut.Tenant;
-
-                    List<DocumentOutCopy> copies = (from a in commonContext.DocumentOutCopies
-                                                    where a.DocumentOutId == documentOutId && tenant == (int)tenant
-                                                    select a).OrderBy(d => d.DocumentTypeCopy.IndexOrder).ToList();
-
-                    Uploader up = new Uploader();
-
-                    foreach (DocumentOutCopy copy in copies)
-                    {
-                        includeInPrint = true;
-                        if (doucmentOut.DocumentsFiling.DocumentType.IsDocumentOneTimePrintLimited && doucmentOut.DocumentsFiling.DocumentType.LimitedPrintCopyId == copy.DocumentTypeCopyId && !string.IsNullOrEmpty(copy.LastPrintedByUserId))
-                        {
-                            includeInPrint = false;
-                        }
-
-                        if (includeInPrint)
-                        {
-                            IsPrinted = true;
-                            if (!IsForChecked)
-                            {
-
-                                string documentExtension = up.GetFileExtension(copy.DocumentId, (int)tenant);
-                                string documentId = copy.DocumentId;
-                                if (!string.IsNullOrEmpty(documentExtension))
-                                {
-                                    _Stream = DownloadFile(documentId, documentExtension, "", (int)tenant);
-                                    if (_Stream != null)
-                                    {
-                                        PdfDocumentBase.Merge(pdfDoc, _Stream);
-                                        if ((pdfDoc.Pages.Count % 2 == 1) && doucmentOut.DocumentsFiling.DocumentType.Code == "740")
-                                        {
-                                            pdfDoc.Pages.Add();
-                                        }
-
-                                        UserRepository userRep = new UserRepository((int)tenant);
-                                        User printedBy = null;
-                                        if (!string.IsNullOrEmpty(userId)) printedBy = userRep.GetSingleUser(userId, (int)tenant);
-                                        else printedBy = userRep.GetSingleUserByCodeOrEmailForTenant(null, email, (int)tenant, false);
-                                        if (printedBy == null)
-                                        {
-                                            printedBy = userRep.GetSingleUserByCodeOrEmailForTenant(null, email, 0, false);
-                                        }
-
-                                        DocumentOutCopyRepository myRep = new DocumentOutCopyRepository((int)tenant);
-                                        DocumentOutCopy documentoutCopy = myRep.GetSingleDocumentOutCopyByTenant(copy.Id, (int)tenant);
-                                        documentoutCopy.LastPrintDate = TenantServerConfigration.GetCurrentDateTime((int)tenant);
-                                        documentoutCopy.LastPrintedByUserId = printedBy.Id;
-                                        myRep.Update(documentoutCopy);
-                                        myRep.SubmitChanges();
-
-                                        if (DocumentCode == "999G")
-                                        {
-                                            ARInvoiceRepository aRInvoiceRepository = new ARInvoiceRepository((int)tenant);
-                                            ARInvoice aRInvoice = aRInvoiceRepository.GetInvoices().Where(s => s.Id == SelectId).FirstOrDefault();
-                                            aRInvoice.IsPrinted = true;
-                                            aRInvoiceRepository.Update(aRInvoice);
-                                            aRInvoiceRepository.SubmitChanges();
-                                        }
-
-                                        break;
-
-                                    }
-                                }
-                            }
-
-
-
-                        }
-
-                    }
-
-                }
-
-
-
-
-                return IsPrinted;
-
-
-
-            }
-            catch (Exception errorInfo)
-            {
-                string errorMessage = errorInfo.Message;
-
-                if (errorInfo.InnerException != null)
-                {
-                    errorMessage += Environment.NewLine + errorInfo.InnerException.Message;
-                }
-                errorMessage += Environment.NewLine + errorInfo.ToString();
-                if (!string.IsNullOrEmpty(errorInfo.StackTrace))
-                {
-                    errorMessage += Environment.NewLine + errorInfo.StackTrace;
-                }
-                throw new ApplicationException(errorMessage);
-            }
-        }
-        public Stream DownloadFile(string documentId, string documentExtension, string fileLocation, int tenant)
-        {
-            ICommonDataContext commonContext = CommonDataContext.GetContext(tenant);
-
-            Document document = (from doc in commonContext.Documents
-                                 where doc.Id == documentId
-                                 select doc).FirstOrDefault();
-            if (document != null)
-            {
-                //string filelocation = GetFileLocation(fileLocation);
-                try
-                {
-
-                    //else // In Azure
-                    //{
-
-                    //string filename = document.Id + "." + document.Extension;
-
-                    //blobContainer = StorageAcountDetails.GetCurrentContainer(tenant);
-
-                    BlobFileInfo fileInfo = new BlobFileInfo()
-                    {
-                        FileName = document.Id,
-                        FolderName = document.Folder,
-                        Extension = document.Extension,
-                        Tenant = document.Tenant,
-                        FileSize = document.FileSize,
-                    };
-                    IBlobService storageservice = ContainerAccessor.Container.Resolve(typeof(IBlobService), "StorageService", new ParameterOverride("", 1)) as IBlobService;
-                    datainByte = storageservice.Read(fileInfo);
-
-
-                    if (datainByte != null)
-                    {
-
-                        MemoryStream stream = new MemoryStream(datainByte);
-                        return stream;
-                    }
-                    else
-                        return null;
-
-                }
-                catch (Exception e)
-                {
-                    string ip = "";
-                    if (HttpContext.Current != null && HttpContext.Current.Request != null)
-                    {
-                        string currentIP = HttpContext.Current.Request.Headers["X-Real-IP"];
-                        if (string.IsNullOrEmpty(currentIP))
-                        {
-                            currentIP = HttpContext.Current.Request.UserHostAddress;
-                        }
-                        ip = currentIP;
-                    }
-                    ExceptionHandler.HandleException(e, DateTime.Now, tenant, User != null ? User.Identity.Name : "", User != null ? User.Identity.Name : "", "MergeAllPage : DownloadFile Method ", ip);
-                    return null;
-                }
-            }
-            return null;
-        }
-
-
+ 
     }
 
 
-
-    public class PDFDocumentInvoices
-    {
-        public StreamContent Document { set; get; }
-        public List<string> ARInvoiceNumbersNotPrinted { set; get; }
-        public  List<string> InterestReportNumbersNotPrinted { set; get; }
-    }
 }
