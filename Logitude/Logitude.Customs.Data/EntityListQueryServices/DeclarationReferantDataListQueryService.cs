b82eb1a10bad2544eabd151a6f38ce7784174fa3
elisheva <elisheva@amital.co.il>
2020-09-07 18:19:04 +0300

    referant

--- Logitude/Logitude.Customs.Data/EntityListQueryServices/DeclarationReferantDataListQueryService.cs
+++ Logitude/Logitude.Customs.Data/EntityListQueryServices/DeclarationReferantDataListQueryService.cs
@@ -25,9 +25,7 @@ namespace Logitude.Customs.Data.EntityListQueryServices
                                                              join d in context.Declarations.Include("CustomerCard").Include("DeclarationOffice").Include("DeclarationStatusType")
                                                              on a.DeclarationId equals d.Id
 
-                                                             join e in context.Cards.Include("CustomerCard")
-                                                             on d.CustomerId equals e.Customer.Id
-
+                                                           
                                                              join c in context.Clients.Include("Client")
                                                              on d.ImporterCode equals c.Code
                                                              select new DeclarationReferantDataList()
@@ -82,7 +80,7 @@ namespace Logitude.Customs.Data.EntityListQueryServices
  
                                                                  NewFile = a.NewFile,
                                                                  Favorite = a.Favorite,
-                                                                 IsCustomerLogBoxActivated = e.Customer.LogBoxActivated,
+                                                                  IsCustomerLogBoxActivated = d.CustomerCard.Customer.LogBoxActivated,
                                                                  SortedColumns = (a.NewFile && a.Favorite ? 1 : (a.NewFile ? 2 : (a.Favorite ? 3 : 4))),
                                                                  IsCancelled = d.IsCancelled,
                                                                  ClassifiedUserName = a.ClassifiedUser.Contact.LocalName,
