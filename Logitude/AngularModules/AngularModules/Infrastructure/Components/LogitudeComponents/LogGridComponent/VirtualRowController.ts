1d72c8907595588eeb23f0f533c4a6072a373b41
sohaib <sohaib@amital.co.il>
2020-09-09 10:01:01 +0300

    #71807

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/LogGridComponent/VirtualRowController.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/LogGridComponent/VirtualRowController.ts
@@ -86,7 +86,9 @@ export class VirtualRowController implements OnInit, OnChanges {
                 this.dataSource.getRows(pageIndex * PSize, PSize, sortingCol, sortingDir, getCount, searchfields, Filters).then(res => {
                     res.subscribe((viewResponse: ServiceResponse) => {
                         if (!viewResponse.HasError) {
-                            if (this.MyCallTime == null || viewResponse.CallTime > this.MyCallTime || SearchFieldChanged == false) {
+                            var ResponseTime = viewResponse.CallTime.getTime();
+                            var MyCallExactTime = this.MyCallTime ? this.MyCallTime.getTime() : 0;
+                            if (this.MyCallTime == null || ResponseTime > MyCallExactTime || SearchFieldChanged == false) {
                                 this.MyCallTime = viewResponse.CallTime;
                                 console.log("this.MyCallTime " + this.MyCallTime);
                                 this.RecievedDataCount = viewResponse.Result.length;
