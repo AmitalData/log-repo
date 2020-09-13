ca5ce6bf756310652f2774738f47b95dc92d5ada
Mutaz Zuhairi <Mutaz@logitudeworld.com>
2020-09-13 13:30:47 +0300

    Fix

--- Logitude/Logitude.Accounting.BL/CoreBL/Batch/BatchInterestReportInvoiceService.cs
+++ Logitude/Logitude.Accounting.BL/CoreBL/Batch/BatchInterestReportInvoiceService.cs
@@ -220,6 +220,7 @@ namespace Logitude.Accounting.BL.CoreBL.Batch
 
             InterestReportInvoiceMapping interestReportInvoiceMapping = new InterestReportInvoiceMapping();
             ARInvoicePM aRInvoicePM = interestReportInvoiceMapping.MapARInvoice(interestReportArgs, interestReport, tenantPM, userPM, cardPM);
+            aRInvoicePM.InvoiceDate = interestReportArgs.InvoiceDate;
             ARInvoiceEntityPM aRInvoiceEntityPM = interestReportInvoiceMapping.MapARInvoiceEntity(interestReportArgs, ObjectTableId);
             ARInvoiceLinePM aRInvoiceLinePM = interestReportInvoiceMapping.MapARInvoiceLine(interestReport, tenantPM, chargesType, vatTypePercentagePM);
 
