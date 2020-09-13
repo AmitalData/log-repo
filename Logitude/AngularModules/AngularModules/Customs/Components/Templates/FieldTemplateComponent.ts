9490c7c23a0549ba186452e0115996991f633f4e
sohaib <sohaib@amital.co.il>
2020-09-13 15:21:29 +0300

    #71829

--- Logitude/AngularModules/AngularModules/Customs/Components/Templates/FieldTemplateComponent.ts
+++ Logitude/AngularModules/AngularModules/Customs/Components/Templates/FieldTemplateComponent.ts
@@ -587,8 +587,8 @@ export class FieldTemplateComponent {
                     if (SessionLocator.SelectedSession != null && SessionLocator.SelectedSession.CurrentWindow != null) {
                         SessionLocator.SelectedSession.CurrentWindow.SuppressBusyIndicator = false;
                     }
-                    SessionLocator.SelectedSession.CurrentListComponent.OnBackFromEdit(this.Entity.DeclarationId, { rowIndex: this.RowIndex });
                 });
+                this.OnBackFromEdit(this.Entity.DeclarationId, event);
             });
     }
 
