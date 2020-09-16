6a57d03fb46391a080a4802e266afa57aaf8f021
Wesam <Wesam@WESAM>
2020-09-16 09:17:12 +0300

    Change Logic In Over Manifest /CrossDocks
    Related Work Items: #72201

--- Logitude/AngularModules/AngularModules/Warehouse/Components/NewWarehouseEntryComponent.ts
+++ Logitude/AngularModules/AngularModules/Warehouse/Components/NewWarehouseEntryComponent.ts
@@ -111,14 +111,14 @@ export class NewWarehouseEntryComponent extends BaseComponent implements OnInit 
             this.ValidationWarningsList = [];
             this.PageChild_WEPD.WarehouseEntryPackagesLists.forEach(entryPackage => {
                 if (entryPackage.OverManifest > 0)
-                    this.ValidationWarningsList.push(this.GetOverManifestWarningMessage(entryPackage.OverManifest, entryPackage.OldQuantity));
+                    this.ValidationWarningsList.push(this.GetOverManifestWarningMessage(entryPackage.OverManifest, entryPackage.Quantity));
             });
         }
     }
 
-    GetOverManifestWarningMessage(overManifest: number, oldQuantity: number) {
+    GetOverManifestWarningMessage(overManifest: number, quantity: number) {
         var isOrAre: string = overManifest == 1 ? "is" : "are";
-        var warningMessage: string = overManifest + " out of " + oldQuantity + " packages " + isOrAre + " over manifest";
+        var warningMessage: string = overManifest + " out of " + quantity + " packages " + isOrAre + " over manifest";
         return warningMessage;
     }
 
