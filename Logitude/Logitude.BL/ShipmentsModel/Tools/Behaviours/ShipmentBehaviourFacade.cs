8782349d4df843c85cf8a4740449af193db16a4b
Wesam <Wesam@WESAM>
2020-09-16 17:09:35 +0300

    Warehouse Events/Status
    Related Work Items: #71257

--- Logitude/Logitude.BL/ShipmentsModel/Tools/Behaviours/ShipmentBehaviourFacade.cs
+++ Logitude/Logitude.BL/ShipmentsModel/Tools/Behaviours/ShipmentBehaviourFacade.cs
@@ -1,5 +1,6 @@
 ﻿using Logitude.BL.ShipmentsModel.EntityPMs;
 using Logitude.BL.ShipmentsModel.Tools.Behaviour;
+using Logitude.BL.ShipmentsModel.Tools.TraceEvents;
 using Simplog.Data.ShipmentsModel;
 using Simplog.Data.ShipmentsModel.EntityPOCOs;
 using System;
@@ -26,6 +27,11 @@ namespace Logitude.BL.ShipmentsModel.Tools.Behaviours
         {
             updateShipmentComputedFields.Handle();
             updateCrossDocks.Handle();
+        }
+
+        public void Trace(ShipmentTracing shipmentTracing)
+        {
+            updateCrossDocks.Trace(shipmentTracing);
         }
 
 
