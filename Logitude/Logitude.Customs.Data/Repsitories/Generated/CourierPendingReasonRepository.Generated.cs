918bf9374887379fabf003b7c939a79961ae8d50
sohaib <sohaib@amital.co.il>
2020-09-17 12:37:56 +0300

    undo

--- Logitude/Logitude.Customs.Data/Repsitories/Generated/CourierPendingReasonRepository.Generated.cs
+++ Logitude/Logitude.Customs.Data/Repsitories/Generated/CourierPendingReasonRepository.Generated.cs
@@ -34,6 +34,12 @@ namespace Logitude.Customs.Data.Repsitories
                     where a.Id == id && a.Tenant == tenant
                     select a).FirstOrDefault();
         }
+        public CourierPendingReason GetByCode(string code, int tenant)
+        {
+            return (from a in context.CourierPendingReasons
+                    where a.Code == code && a.Tenant == tenant
+                    select a).FirstOrDefault();
+        }
 
         public IQueryable<CourierPendingReason> GetAll(int tenant)
         {
