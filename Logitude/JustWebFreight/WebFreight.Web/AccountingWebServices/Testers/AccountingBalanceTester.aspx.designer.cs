f08ff20f2f63185fd6b246450dba2ddf37709f5d
itzik <itzik@amital.co.il>
2020-09-23 11:55:25 +0300

    69453
    Related Work Items: #69453

--- Logitude/JustWebFreight/WebFreight.Web/AccountingWebServices/Testers/AccountingBalanceTester.aspx.designer.cs
+++ Logitude/JustWebFreight/WebFreight.Web/AccountingWebServices/Testers/AccountingBalanceTester.aspx.designer.cs
@@ -78,6 +78,15 @@ namespace WebFreight.Web.AccountingWebServices.Testers
         protected global::System.Web.UI.WebControls.Button _ButtonReverseTotalControl;
 
         /// <summary>
+        /// _ButtonReverseEngineerControlAccountAccumulateChild control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button _ButtonReverseEngineerControlAccountAccumulateChild;
+
+        /// <summary>
         /// _AccountingIntegrityService control.
         /// </summary>
         /// <remarks>
