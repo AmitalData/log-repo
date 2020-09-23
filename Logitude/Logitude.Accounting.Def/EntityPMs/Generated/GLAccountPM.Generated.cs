b789d568b85e50a4818372583e8425b69b8f547d
Mutaz Zuhairi <Mutaz@logitudeworld.com>
2020-09-23 11:25:25 +0300

    #72491

--- Logitude/Logitude.Accounting.Def/EntityPMs/Generated/GLAccountPM.Generated.cs
+++ Logitude/Logitude.Accounting.Def/EntityPMs/Generated/GLAccountPM.Generated.cs
@@ -2378,12 +2378,12 @@ namespace Logitude.Accounting.Def.EntityPMs
 			
 		 }
 	   }
-	  private bool? activeForInterest ;
+	  private bool activeForInterest ;
 	  	  
        
 	   [CustomValidation(typeof(AccountingValidationClass), "ValidateClass")]
 	   [DataMember]
-       public bool? ActiveForInterest  
+       public bool ActiveForInterest  
 	   {
 	    
 	     get
@@ -2394,7 +2394,7 @@ namespace Logitude.Accounting.Def.EntityPMs
 		 {
 		   if(activeForInterest != value)
 		  {
-		    NotifyPropertyChangeValues values=new NotifyPropertyChangeValues(){PropertyName="ActiveForInterest",OldValue=activeForInterest,NewValue=value,PropertyType="bool?"};
+		    NotifyPropertyChangeValues values=new NotifyPropertyChangeValues(){PropertyName="ActiveForInterest",OldValue=activeForInterest,NewValue=value,PropertyType="bool"};
 		    NotifyPropertyChanged(values);
 		   activeForInterest=value;
 		   }
@@ -2424,12 +2424,12 @@ namespace Logitude.Accounting.Def.EntityPMs
 			
 		 }
 	   }
-	  private bool? activeForInterestCreditInvoice ;
+	  private bool activeForInterestCreditInvoice ;
 	  	  
        
 	   [CustomValidation(typeof(AccountingValidationClass), "ValidateClass")]
 	   [DataMember]
-       public bool? ActiveForInterestCreditInvoice  
+       public bool ActiveForInterestCreditInvoice  
 	   {
 	    
 	     get
@@ -2440,7 +2440,7 @@ namespace Logitude.Accounting.Def.EntityPMs
 		 {
 		   if(activeForInterestCreditInvoice != value)
 		  {
-		    NotifyPropertyChangeValues values=new NotifyPropertyChangeValues(){PropertyName="ActiveForInterestCreditInvoice",OldValue=activeForInterestCreditInvoice,NewValue=value,PropertyType="bool?"};
+		    NotifyPropertyChangeValues values=new NotifyPropertyChangeValues(){PropertyName="ActiveForInterestCreditInvoice",OldValue=activeForInterestCreditInvoice,NewValue=value,PropertyType="bool"};
 		    NotifyPropertyChanged(values);
 		   activeForInterestCreditInvoice=value;
 		   }
