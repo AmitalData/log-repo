b777c9cb661c96574cc6dbd36590dcdb8003385b
elisheva <elisheva@amital.co.il>
2020-09-24 10:20:20 +0300

    72681

--- Logitude/Logitude.CustomsMessaging/ResponseServices/MN_NG_8241_CargoResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/MN_NG_8241_CargoResponseService.cs
@@ -219,9 +219,9 @@ namespace Logitude.CustomsMessaging.ResponseServices
                             if (_MyDeclarationPM.AvailabilityDate == null)
                             {
                                 _MyDeclarationPM.AvailabilityDate = DateTime.Now;
-                                myDeclarationUpdateService.Update(_MyDeclarationPM, true);
                                 IsAvailabilityDate2 = true;
                             }
+                        myDeclarationUpdateService.Update(_MyDeclarationPM, true);
 
                         if (_status == "SST" || _status == "SMG")
                             SendPayment(_MyDeclarationPM, context, requestParams, IsAvailabilityDate2);
@@ -361,8 +361,8 @@ namespace Logitude.CustomsMessaging.ResponseServices
                         {
                             IsAvailabilityDate = true;
                             _MyDeclarationPM.AvailabilityDate = DateTime.Now;
-                            myDeclarationUpdateService.Update(_MyDeclarationPM, true);
                         }
+                    myDeclarationUpdateService.Update(_MyDeclarationPM, true);
 
                     if (status=="SST" || status=="SMG")
                         SendPayment(_MyDeclarationPM, context, requestParams, IsAvailabilityDate);
