b51a3317024ff377080a99aab2f0d047752208d0
Alaa Jamal <Alaa@logitudeworld.com>
2020-09-30 14:10:31 +0300

    #72823

--- Logitude/AngularModules/AngularModules/Accounting/Components/Others/InterestInvoiceAutoCreditComponent.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/Others/InterestInvoiceAutoCreditComponent.ts
@@ -155,9 +155,7 @@ export class InterestInvoiceAutoCreditComponent extends BaseComponent {
     }
    
     CancelButtonClicked() {
-        //if (this.ValidationErrorsList.length > 0) {
-        //    this.CurrentSession.CurrentEditComponent.ReloadEntityPM();
-        //}
+  
         this.interestReportPM.IsDirty = false;
         this.CurrentSession.CurrentWindow.Close("Cancel");
     }
