dab20ba594b2e89818cb172b671474a79ad4fa06
Maheera Jazi <Maheera@logitudeworld.com>
2020-10-13 20:24:32 +0300

    73244
    Related Work Items: #73244

--- Logitude/Simplog.Global.Data/GlobalModel/Repositories/TenantManagementRepository.cs
+++ Logitude/Simplog.Global.Data/GlobalModel/Repositories/TenantManagementRepository.cs
@@ -4,6 +4,8 @@ using Simplog.Global.Data.GlobalModel.EntityPOCOs;
 using Simplog.Global.Data.GlobalModel;
 using Simplog.Server.Infrastructure.Helpers;
 using Simplog.Server.Infrastructure;
+using System.Security.Cryptography;
+
 namespace Simplog.Global.Data.GlobalModel.Repositories
 {
     public class TenantManagementRepository : IRepository<TenantManagement>
@@ -47,15 +49,27 @@ namespace Simplog.Global.Data.GlobalModel.Repositories
 
         public TenantManagement GetSingleTenantManagementByBluesnapAccountId(string bluesnapaccountId)
         {
-            var tenantManagement = (from a in context.TenantManagements.Include("GlobalTenant")
-                                    where a.BluesnapAccount == bluesnapaccountId  && a.GlobalTenant.IsActive
-                                    select a).FirstOrDefault();
+            TenantManagement tenantManagement = null;
+            var allTenantManagements = (from a in context.TenantManagements.Include("GlobalTenant")
+                                        where a.BluesnapAccount == bluesnapaccountId
+                                        select a);
+            var parentTenant = allTenantManagements.Where(a => a.IsParentTenant && a.NoPaymentForChildTenants).FirstOrDefault();
 
-            if(tenantManagement == null)
+            if (parentTenant != null)
             {
-                tenantManagement = (from a in context.TenantManagements.Include("GlobalTenant")
-                                    where a.BluesnapAccount == bluesnapaccountId && !a.GlobalTenant.IsActive
+                tenantManagement = parentTenant;
+            }
+            else
+            {
+                tenantManagement = (from a in allTenantManagements.Include("GlobalTenant")
+                                    where a.BluesnapAccount == bluesnapaccountId && a.GlobalTenant.IsActive
                                     select a).FirstOrDefault();
+                if (tenantManagement == null)
+                {
+                    tenantManagement = (from a in allTenantManagements.Include("GlobalTenant")
+                                        where a.BluesnapAccount == bluesnapaccountId && !a.GlobalTenant.IsActive
+                                        select a).FirstOrDefault();
+                }
             }
 
             return tenantManagement;
