b0a19c0fefba636c78d579bbd85aa72cfd5169ec
Islam Shaheen <Islam@logitudeworld.com>
2020-10-13 13:06:50 +0300

    Error When open task in task Scheduler
    Related Work Items: #73408

--- Logitude/AngularModules/AngularModules/Infrastructure/Services/ExtendedPMs/SchedulerExtendedPMService.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Services/ExtendedPMs/SchedulerExtendedPMService.ts
@@ -1,13 +1,14 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import { defer, of } from 'rxjs';
-import {ServiceResponse} from '../../DataContracts/ServiceResponse';
-import {ClassLevelValidator} from '../../Validators/ClassLevelValidator';
-import {InfraSettings} from '../../Utilities/InfraSettings';
-import {ServiceHelper} from '../../Utilities/ServiceHelper';
-import {TasksSchedulerPM} from '../../EntityPMs/TasksSchedulerPM';
+import { ServiceResponse } from '../../DataContracts/ServiceResponse';
+import { ClassLevelValidator } from '../../Validators/ClassLevelValidator';
+import { InfraSettings } from '../../Utilities/InfraSettings';
+import { ServiceHelper } from '../../Utilities/ServiceHelper';
+import { TasksSchedulerPM } from '../../EntityPMs/TasksSchedulerPM';
 import { HttpClient, HttpHeaders, HttpEvent, HttpResponse } from '@angular/common/http';
 import { map, catchError, tap } from 'rxjs/operators';
 import { PerformanceLogger } from '../../Utilities/PerformanceLogger';
+import { CustomFieldClass } from 'Infrastructure/DataContracts/CustomFieldClass';
 
 @Injectable()
 export class SchedulerExtendedPMService {
@@ -18,7 +19,7 @@ export class SchedulerExtendedPMService {
         this.apiUrl = ServiceHelper.GetLogitudeURL() + 'api/SchedulerExtended';
     }
 
-    GetSchedulerHistoryLogs(HistoryId: string)  {
+    GetSchedulerHistoryLogs(HistoryId: string) {
         var url = this.apiUrl + '/GetSchedulerHistoryLogs?' + 'historyId=' + HistoryId;
 
         return this.httpClient.get(url, ServiceHelper.GetHttpHeaders()).pipe(map(response => {
@@ -30,7 +31,7 @@ export class SchedulerExtendedPMService {
         }), catchError(ServiceHelper.HandleServiceError));
     }
 
-    GetSchedulerDetailsById(schedulerId: string)  {
+    GetSchedulerDetailsById(schedulerId: string) {
         var url = this.apiUrl + '/GetSchedulerDetailsById?' + 'schedulerId=' + schedulerId;
 
         return this.httpClient.get(url, ServiceHelper.GetHttpHeaders()).pipe(map(response => {
@@ -42,7 +43,7 @@ export class SchedulerExtendedPMService {
         }), catchError(ServiceHelper.HandleServiceError));
     }
 
-    insert(entityPM: TasksSchedulerPM)  {
+    insert(entityPM: TasksSchedulerPM) {
         var callTime = new Date();
         var url = this.apiUrl;
         var validator: ClassLevelValidator;
@@ -51,8 +52,11 @@ export class SchedulerExtendedPMService {
         var serviceResponse: ServiceResponse;
         serviceResponse = new ServiceResponse();
 
+        var mappedEntity: TasksSchedulerPM;
+        mappedEntity = this.MapJsonToEntityPM(entityPM, false);
+
         if (errorsArray.length == 0) {
-            return this.httpClient.post(url, entityPM, ServiceHelper.GetHttpFullHeaders()).pipe(map((response: HttpEvent<any>) => {
+            return this.httpClient.post(url, mappedEntity, ServiceHelper.GetHttpFullHeaders()).pipe(map((response: HttpEvent<any>) => {
                 if (response instanceof HttpResponse) {
                     serviceResponse.Result = response;
                     var servertime = response.headers.get('ServerExecutionTime');
@@ -70,7 +74,7 @@ export class SchedulerExtendedPMService {
         }
     }
 
-    update(entityPM: TasksSchedulerPM)  {
+    update(entityPM: TasksSchedulerPM) {
         var callTime = new Date();
         var url = this.apiUrl;
         var validator: ClassLevelValidator;
@@ -79,8 +83,11 @@ export class SchedulerExtendedPMService {
         var serviceResponse: ServiceResponse;
         serviceResponse = new ServiceResponse();
 
+        var mappedEntity: TasksSchedulerPM;
+        mappedEntity = this.MapJsonToEntityPM(entityPM, false);
+
         if (errorsArray.length == 0) {
-            return this.httpClient.put(url, entityPM, ServiceHelper.GetHttpFullHeaders()).pipe(map((response: HttpEvent<any>) => {
+            return this.httpClient.put(url, mappedEntity, ServiceHelper.GetHttpFullHeaders()).pipe(map((response: HttpEvent<any>) => {
                 if (response instanceof HttpResponse) {
                     serviceResponse.Result = response;
                     var servertime = response.headers.get('ServerExecutionTime');
@@ -97,6 +104,55 @@ export class SchedulerExtendedPMService {
             return of(serviceResponse);
         }
     }
+
+    MapJsonToEntityPM(jsonPM: any, mapParent: boolean = true, entityPM: TasksSchedulerPM = null) {
+
+
+        if (!entityPM) {
+
+            entityPM = new TasksSchedulerPM();
+        }
+
+        var customFields: Array<string> = [];
+        for (var i = 1; i < 11; i++) {
+            customFields.push("Field" + i);
+        }
+        var jsonPMKeys = Object.keys(jsonPM);
+
+        for (var key in jsonPMKeys) {
+            if (jsonPMKeys[key] === "UIProperties" || jsonPMKeys[key] === "PropertyChanged") {
+
+                continue;
+            }
+            var property = jsonPMKeys[key];
+
+            if (customFields.indexOf(property) > -1) {
+                if (jsonPM[property]) {
+                    var customFieldClass: CustomFieldClass = new CustomFieldClass(jsonPM[property].Value, jsonPM[property].FieldName, jsonPM[property].TableName);
+                    entityPM[property] = customFieldClass;
+                }
+            }
+            else {
+                entityPM[property] = jsonPM[property];
+            }
+
+        }
+
+
+
+
+        if (mapParent) {
+            entityPM.OldEntityPM = this.clone(entityPM);
+
+        }
+        else {
+
+            entityPM.OldEntityPM = null;
+        }
+        entityPM.IsDirty = false;
+        return entityPM;
+    }
+
 
     public clone(jsonPM: any) {
         var entityPM: any;
