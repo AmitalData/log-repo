b58153d427842a784c1cf5a0467db1636f31898e
logitudo <logitudo@live.com>
2020-10-14 14:03:41 +0300

    ++

--- Logitude/AngularModules/AngularModules/Invoice/Services/StandardPMs/ARPaymentPMService.ts
+++ Logitude/AngularModules/AngularModules/Invoice/Services/StandardPMs/ARPaymentPMService.ts
@@ -24,7 +24,8 @@ import {PerformanceLogger} from '../../../Infrastructure/Utilities/PerformanceLo
 import {ARPaymentPM} from '../../EntityPMs/ARPaymentPM';
 
 import {ARPaymentInvoicePM} from '../../EntityPMs/ARPaymentInvoicePM';
-import {LedgerTransactionPM} from '../../EntityPMs/LedgerTransactionPM';
+import { LedgerTransactionPM } from 'Accounting/EntityPMs/LedgerTransactionPM';
+
 import {ARPaymentChequeReplicaPM} from '../../EntityPMs/ARPaymentChequeReplicaPM';
 import {ARPaymentPMInitService} from '../../EntityPMInitServices/ARPaymentPMInitService';
 import {ARPaymentValidator} from '../../Validators/ARPaymentValidator';
