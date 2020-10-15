a7ef06575a5a648fb82a3e9327bf0ad37c24f89c
Abd Al-Rahman Malakh <Abd@logitudeworld.com>
2020-10-15 12:45:18 +0300

    71812
    Related Work Items: #71812

---
+++ Logitude/Logitude.DeploymentUtilities/Models/ToolArguments.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Logitude.DeploymentUtilities.Models
+{
+    public static class ToolArguments
+    {
+        public static string[] Arguments;
+
+        public static bool IsArgumentProvided(string argument)
+        {
+            string[] arguments = Array.ConvertAll(Arguments, a => a?.ToLower());
+            return (Array.IndexOf(arguments, argument?.ToLower()) != -1);
+        }
+
+        public static string GetArgumentValue(string argument)
+        {
+            string[] arguments = Array.ConvertAll(Arguments, a => a?.ToLower());
+            int indexOfArgumentValue = Array.IndexOf(arguments, argument?.ToLower()) + 1;
+            if (indexOfArgumentValue < Arguments.Length && indexOfArgumentValue >= 0)
+            {
+                string argumentValue = Arguments[indexOfArgumentValue];
+                return argumentValue;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
