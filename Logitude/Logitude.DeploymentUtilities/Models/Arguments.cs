09307241fab0bda2c394a950e6f2d365de58ca8d
Abd Al-Rahman Malakh <Abd@logitudeworld.com>
2020-10-15 12:45:18 +0300

    71812
    Related Work Items: #71812

---
+++ Logitude/Logitude.DeploymentUtilities/Models/Arguments.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Logitude.DeploymentUtilities.Models
+{
+    public static class Arguments
+    {
+        public const string ROLES = "-roles";
+        public const string FEATURES = "-features";
+        public const string HTMLVERSION = "-htmlversion";
+    }
+}
