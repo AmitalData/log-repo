00ce92e32a5a008014b014924f0906ae5340c940
Alexander Khitrik <alexanderh@amital.co.il>
2020-10-18 08:08:19 +0300

    Alexander: 72801 Journal from csv

--- Logitude/JustWebFreight/WebFreight.Web/AccountingWebServices/Testers/AccountingBalanceTester.aspx.designer.cs
+++ Logitude/JustWebFreight/WebFreight.Web/AccountingWebServices/Testers/AccountingBalanceTester.aspx.designer.cs
@@ -357,6 +357,15 @@ namespace WebFreight.Web.AccountingWebServices.Testers
         protected global::System.Web.UI.WebControls.Button ButtonLoadGLAccountsCSV;
 
         /// <summary>
+        /// ButtonLoadJournalsCSV control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonLoadJournalsCSV;
+
+        /// <summary>
         /// _MyLastAction control.
         /// </summary>
         /// <remarks>
