13a9f72113c7f06ee58fb02f5150908b890c86b0
Abdullah Dawoud <Abdullah@logitudeworld.com>
2020-10-19 17:37:52 +0300

    filters

--- Logitude/AngularModules/CargoTrackingModules/src/CargoTracking/Components/Dashboard/shipments/dashboard-shipments.component.ts
+++ Logitude/AngularModules/CargoTrackingModules/src/CargoTracking/Components/Dashboard/shipments/dashboard-shipments.component.ts
@@ -23,6 +23,7 @@ export class DashboardShipmentsComponent
     Shipments: CargoTrackingShipmentList[] = [];
     tenant;
     isLoading: boolean  = false;
+    isFiltersSideBarOpened: boolean  = false;
 
     constructor(private router: Router,
         private route: ActivatedRoute,
