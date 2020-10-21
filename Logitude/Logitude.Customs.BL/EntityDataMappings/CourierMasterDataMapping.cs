166d640b9d40c608a67995867d792f263d613eca
itzik <itzik@amital.co.il>
2020-10-21 16:51:57 +0300

    73094
    Related Work Items: #73094

--- Logitude/Logitude.Customs.BL/EntityDataMappings/CourierMasterDataMapping.cs
+++ Logitude/Logitude.Customs.BL/EntityDataMappings/CourierMasterDataMapping.cs
@@ -48,22 +48,22 @@ namespace Logitude.Customs.BL.EntityDataMappings
             this.CustomMappedPMProperties.Add(PMPropertyNames.IntegratorName);
             this.CustomMappedPMProperties.Add(PMPropertyNames.IntegratorNumber);
 
-            CustomsAirlineRepository rep = new CustomsAirlineRepository(entityPM.Tenant);
+            var rep = new CustomsAirlineQueryService(entityPM.Tenant);
             UserRepository userRep = new UserRepository(entityPM.Tenant);
-            CustomsAirline customsAirline = rep.GetSingle(entityPOCO.AirlineId, entityPOCO.Tenant);
+            var customsAirline = rep.GetSingle(entityPOCO.AirlineId, false,true);
             if (customsAirline != null)
             {
                 entityPM.AirlinePrefix = customsAirline.AirlinePrefix;
                 entityPM.AirlineName = customsAirline.LocalName;
             }
 
-            User user = userRep.GetSingleUser(entityPM.CreatedByUserId, entityPM.Tenant);
+            User user = userRep.GetSingleUser(entityPM.CreatedByUserId, entityPM.Tenant,true);
             if(user != null)
             {
                 entityPM.CreatedByUserName = user.Contact.LocalName != null ? user.Contact.LocalName : user.Contact.EnglishName;
             }
 
-            User UpdatedByUser = userRep.GetSingleUser(entityPM.UpdatedByUserId, entityPM.Tenant);
+            User UpdatedByUser = userRep.GetSingleUser(entityPM.UpdatedByUserId, entityPM.Tenant, true);
             if (UpdatedByUser != null)
             {
                 entityPM.UpdatedByUserName = UpdatedByUser.Contact.LocalName != null ? UpdatedByUser.Contact.LocalName : UpdatedByUser.Contact.EnglishName;
