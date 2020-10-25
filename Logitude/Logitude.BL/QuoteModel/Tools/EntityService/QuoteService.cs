6bb15b4da21f4bd05f469432cad7a5a85cf5444c
logitudo <logitudo@live.com>
2020-10-25 10:45:18 +0200

    #73814

--- Logitude/Logitude.BL/QuoteModel/Tools/EntityService/QuoteService.cs
+++ Logitude/Logitude.BL/QuoteModel/Tools/EntityService/QuoteService.cs
@@ -1109,7 +1109,9 @@ namespace Logitude.BL.QuoteModel.Tools.EntityService
         }
         private void CloseEntityAutomatically()
         {
-            entityPM.QuoteClosingReasonId = "1-591";
+            var quoteClosingReasonRepository = new QuoteClosingReasonRepository(tenant);
+            var quoteClosing = quoteClosingReasonRepository.GetSingleQuoteClosingReasonByCode("XQ", tenant); 
+            entityPM.QuoteClosingReasonId = quoteClosing.Id;
             entityPM.IsClosed = true;
             entityPM.QuoteClosingReasonCode = "XQ";
             entityPM.ActionType = "Decline";
