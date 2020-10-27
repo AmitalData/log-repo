1402889400d09765e3480f2927ea41beb05898e2
Mutaz Zuhairi <Mutaz@logitudeworld.com>
2020-10-27 09:19:42 +0200

    Fix

--- Logitude/Logitude.DBMigrations/GeneratedScript/MainScript.sql
+++ Logitude/Logitude.DBMigrations/GeneratedScript/MainScript.sql
@@ -1,28 +1,18 @@
--- General Script From 202009152200_FixHorseEventTypes.sxml File
-BEGIN TRAN
-BEGIN TRY
-DECLARE @StartTime datetime
-DECLARE @EndTime datetime
-SELECT @StartTime = GETDATE()
-declare @WrongObjectTableId as varchar(15)
-set @WrongObjectTableId = (select Id from ObjectTables where Name = 'QuoteClosingReason')
-delete from EventTypes where Code in ('HRIN', 'HRRC') and Tenant = 0 and ObjectTableId = @WrongObjectTableId
-declare @ObjectTableId as varchar(15)
-set @ObjectTableId = (select Id from ObjectTables where Name = 'Horse')
-update EventTypes set ObjectTableId = @ObjectTableId where Code = 'HRIN'
-update EventTypes set ObjectTableId = @ObjectTableId where Code = 'HRRC'
-SELECT @EndTime = GETDATE()
-INSERT INTO [dbo].[DBScriptsHistory]([SxmlFileName], [ExecutionDate], [ScriptBody], [ElapsedTimeInMs], [HashValue], [Version])VALUES('202009152200_FixHorseEventTypes.sxml', GETDATE(), 'declare @WrongObjectTableId as varchar(15)
-set @WrongObjectTableId = (select Id from ObjectTables where Name = ''QuoteClosingReason'')
-delete from EventTypes where Code in (''HRIN'', ''HRRC'') and Tenant = 0 and ObjectTableId = @WrongObjectTableId
-declare @ObjectTableId as varchar(15)
-set @ObjectTableId = (select Id from ObjectTables where Name = ''Horse'')
-update EventTypes set ObjectTableId = @ObjectTableId where Code = ''HRIN''
-update EventTypes set ObjectTableId = @ObjectTableId where Code = ''HRRC''', DATEDIFF(MS,@StartTime,@EndTime), '89ba7a8cbe3640c00cea8ae514286393', 2);
-COMMIT TRAN
-END TRY
-BEGIN CATCH
-IF @@TRANCOUNT > 0
-ROLLBACK TRAN
-END CATCH;
+-- Add New Column With Name ResultType
+ALTER TABLE [dbo].[TasksScheduler] ADD [ResultType] VARCHAR(10) NULL;
+
+INSERT INTO [dbo].[DBMigrationsHistory]([Id], [DxmlFileName], [TableName], [ColumnName], [MigrationType], [ExecutionDate], [MigrationScript])VALUES('75f0e48a-3cf8-4b18-9884-3c6b7194af8e', 'TasksScheduler.dxml', 'TasksScheduler', 'ResultType', 'Add Column', GETDATE(), '-- Add New Column With Name ResultTypeALTER TABLE [dbo].[TasksScheduler] ADD [ResultType] VARCHAR(10) NULL;');
+
+
+-- Add New Column With Name IsFromInterestBatchInvoice
+ALTER TABLE [dbo].[ARInvoices] ADD [IsFromInterestBatchInvoice] BIT DEFAULT(0) NOT NULL;
+
+INSERT INTO [dbo].[DBMigrationsHistory]([Id], [DxmlFileName], [TableName], [ColumnName], [MigrationType], [ExecutionDate], [MigrationScript])VALUES('78e41ec3-6041-4b88-969a-cf181d9d8bee', 'ARInvoice.dxml', 'ARInvoices', 'IsFromInterestBatchInvoice', 'Add Column', GETDATE(), '-- Add New Column With Name IsFromInterestBatchInvoiceALTER TABLE [dbo].[ARInvoices] ADD [IsFromInterestBatchInvoice] BIT DEFAULT(0) NOT NULL;');
+
+
+-- Change Size From 1000 To 2000 For Column ContainersNumbersandTypesArray
+ALTER TABLE [dbo].[ShipmentComputedFields] ALTER COLUMN [ContainersNumbersandTypesArray] NVARCHAR(2000);
+
+INSERT INTO [dbo].[DBMigrationsHistory]([Id], [DxmlFileName], [TableName], [ColumnName], [MigrationType], [ExecutionDate], [MigrationScript])VALUES('60ea5d62-f3cc-4d4e-91d9-0a76e7f1ce7d', 'ShipmentComputedFields.dxml', 'ShipmentComputedFields', 'ContainersNumbersandTypesArray', 'Alter Column Size', GETDATE(), '-- Change Size From 1000 To 2000 For Column ContainersNumbersandTypesArrayALTER TABLE [dbo].[ShipmentComputedFields] ALTER COLUMN [ContainersNumbersandTypesArray] NVARCHAR(2000);');
+
 
