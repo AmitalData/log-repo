1f2b7d55d97d1ceef7cd6d30660cb0f6ae1e3043
Wesam Amarneh <wesam@logitudeworld.com>
2020-10-27 14:11:37 +0200

    Source Tenant Issue

--- Logitude/AngularModules/AngularModules/CommonModules/CommonOthers/Components/DWQueryBuilder/DWQueryBuilderComponent.ts
+++ Logitude/AngularModules/AngularModules/CommonModules/CommonOthers/Components/DWQueryBuilder/DWQueryBuilderComponent.ts
@@ -2077,7 +2077,7 @@ export class DWObjectFieldsDetails extends BaseComponent {
         var parentfieldCode = !AppTool.IsNullOrEmpty(DWObjectField.FieldCode) ? (DWObjectField.FieldCode.replace("[", "").replace("]", "")) : DWObjectField.DisplayName;
         var selectedDimensionDWObjectFields = SelectedDWObjectFields;
         if (SelectedDWObjectFields == null)
-            selectedDimensionDWObjectFields = DWObjectField.MyParentClass.DWObjectFields.filter(d => d.DimensionTableCode == DWObjectField.DimensionTableCode && d.DimensionTableDisplayName == DWObjectField.DisplayName);
+            selectedDimensionDWObjectFields = DWObjectField.MyParentClass.DWObjectFields.filter(d => d.DimensionTableCode == DWObjectField.DimensionTableCode && d.DimensionTableDisplayName == DWObjectField.Name);
         
         selectedDimensionDWObjectFields.filter(d => AppTool.IsNullOrEmpty(d.RecordType) || (!AppTool.IsNullOrEmpty(d.RecordType) && d.RecordType.split(',').indexOf(parentfieldCode) != -1)).forEach((field) => {
             if (field.DisplayInQueryBuilder == true) {
