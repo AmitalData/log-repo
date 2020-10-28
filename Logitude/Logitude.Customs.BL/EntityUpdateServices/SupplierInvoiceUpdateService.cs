b030a0fabbd42f4fc7931d1e1e7c6a99a4789605
sohaib <sohaib@amital.co.il>
2020-10-28 12:01:14 +0200

    #74001

--- Logitude/Logitude.Customs.BL/EntityUpdateServices/SupplierInvoiceUpdateService.cs
+++ Logitude/Logitude.Customs.BL/EntityUpdateServices/SupplierInvoiceUpdateService.cs
@@ -189,7 +189,7 @@ namespace Logitude.Customs.BL.EntityUpdateServices
             string remarks = "";
             foreach (SupplierInvoiceItemPM itemPM in entityPM.SupplierInvoiceItems)
             {
-                if (itemPM.ClassificationCode != itemPM.ClassificationCodeSource)
+                if (itemPM.ClassificationCode != itemPM.ClassificationCodeSource && itemPM.ClassificationCodeSource != null)
                 {
                     if (remarks != "")
                     {
