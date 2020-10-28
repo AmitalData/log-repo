a6f459ea29d9a39fb5d00e61c54ed4b6df237cd1
Alaa Jamal <Alaa@logitudeworld.com>
2020-10-28 10:16:08 +0200

    #72381

--- Logitude/AngularModules/CargoTrackingModules/src/CargoTracking/Components/Shipment/shipment.component.ts
+++ Logitude/AngularModules/CargoTrackingModules/src/CargoTracking/Components/Shipment/shipment.component.ts
@@ -129,19 +129,22 @@ export class ShipmentComponent implements OnInit
                     this.Delivered = false;
                 }
                 this.SetMilestonesFields(result);
+              
             }
             
 
         });
     }
+    public MilstonesExist: boolean = false;
     public Date: Date;
     SetMilestonesFields(result: CargoTrackingShipmentWithMilestones) {
      
-        this.AllMilestoneFields = result.Milestones;
+        this.AllMilestoneFields = result.Milestones;      
         if(this.AllMilestoneFields){
             this.AllMilestoneFields.forEach(S=>{
                 
-                    if(S.IsEstimation){
+                if (S.IsEstimation) {
+                  
                          this.FuturesMilestoneFields.push(S);
                     }
                     else if(!S.IsCurrent){
@@ -160,6 +163,9 @@ export class ShipmentComponent implements OnInit
         else {
             this.Date = this.CurrentMilestoneField.Date;
 
+        }
+        if (this.CompletedMilestoneFields.length > 0 || this.FuturesMilestoneFields.filter(d => d.EstimationDate).length > 0 || this.Shipment.CurrentMilestoneCode) {
+            this.MilstonesExist = true;
         }
     }
 
