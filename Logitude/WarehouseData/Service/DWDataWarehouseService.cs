d9986ed2cd4699cd542172cf1441d63382a76d20
Abed <abed@logitudeworld.com>
2020-10-28 10:15:02 +0000

    Merged PR 6117: 73270
    73270
    
    Related Work Items: #73270
    
    Related work items: #73270

--- Logitude/WarehouseData/Service/DWDataWarehouseService.cs
+++ Logitude/WarehouseData/Service/DWDataWarehouseService.cs
@@ -180,7 +180,7 @@ namespace WarehouseData.Service
                         + " ALTER TABLE " + table.Dw_TableName + " ADD CONSTRAINT DF_" + table.DBTableName + "ConsigneeNotImporterId DEFAULT '-1' FOR ConsigneeNotImporterId"
                         + " ALTER TABLE " + table.Dw_TableName + " ADD CONSTRAINT DF_" + table.DBTableName + "IssuingCarrierAgentId DEFAULT '-1' FOR IssuingCarrierAgentId"
                         + " ALTER TABLE " + table.Dw_TableName + " ADD CONSTRAINT DF_" + table.DBTableName + "OnCarriageTransportModeId DEFAULT '1' FOR OnCarriageTransportModeId"
-                        + " ALTER TABLE " + table.Dw_TableName + " ADD CONSTRAINT DF_" + table.DBTableName + "ComputedStatusId DEFAULT '1' FOR ComputedStatusId;"
+                        + " ALTER TABLE " + table.Dw_TableName + " ADD CONSTRAINT DF_" + table.DBTableName + "ComputedStatusId DEFAULT '-1' FOR ComputedStatusId;"
 
 
                         ;
