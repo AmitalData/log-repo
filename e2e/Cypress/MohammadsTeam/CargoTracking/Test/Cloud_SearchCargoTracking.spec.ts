dca4050a982a16ea05ad534cd117c48653d27439
Mutaz Zuhairi <Mutaz@logitudeworld.com>
2020-11-01 14:09:37 +0200

    #73727

---
+++ e2e/Cypress/MohammadsTeam/CargoTracking/Test/Cloud_SearchCargoTracking.spec.ts
@@ -0,0 +1,31 @@
+
+
+
+
+ 
+export class Cloud_SearchCargoTracking {
+
+ 
+}
+describe('Search Cargo Tracking ', () => {
+ 
+ 
+  it('Search Cargo Tracking Successfully', function () {
+
+   
+	  var URL = Cypress.env("CloudStagingCargoTrackingURL");
+	  cy.visit(URL)
+	  cy.get('#CargoTracking_Search').clear();
+      cy.get('#CargoTracking_Search').type("Cargo Tracking Warme", { delay: 50 });
+      cy.get('#Button_Search').click();
+      cy.get('#CargoTracking_BusyIndicator').should('not.be.visible');
+      cy.get('#SearchResults_0').click();
+        
+      
+    });
+      
+ 
+});
+
+
+
