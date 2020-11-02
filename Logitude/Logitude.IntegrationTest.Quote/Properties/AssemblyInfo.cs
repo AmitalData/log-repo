0e2c17554cb3bfae77f3b63a6fb24459aa9357b7
Ayman <Ayman@logitudeworld.com>
2020-11-02 13:19:07 +0200

    73837
    Related Work Items: #73837

---
+++ Logitude/Logitude.IntegrationTest.Quote/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Logitude.IntegrationTest.Quote")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Logitude.IntegrationTest.Quote")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0700f932-be74-4d43-9441-5476c4faed35")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
