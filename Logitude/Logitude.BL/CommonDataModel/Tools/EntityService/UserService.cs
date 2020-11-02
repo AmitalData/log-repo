07edad591dcf86b8a200b7292888d3836229451d
Abed <abed@logitudeworld.com>
2020-11-02 13:10:01 +0200

    74241
    Related Work Items: #74241

--- Logitude/Logitude.BL/CommonDataModel/Tools/EntityService/UserService.cs
+++ Logitude/Logitude.BL/CommonDataModel/Tools/EntityService/UserService.cs
@@ -851,30 +851,33 @@ namespace Logitude.BL.CommonDataModel.Tools.EntityService
                 }
             }
 
-            bool hasRoles = true;
 
-            if (this.isNewEntity)
+            if (!entityPm.SignupRole)
             {
-                if (entityPm.Roles.Count == 0)
+                bool hasRoles = true;
+
+                if (this.isNewEntity)
                 {
-                    hasRoles = false;
+                    if (entityPm.Roles.Count == 0)
+                    {
+                        hasRoles = false;
+                    }
+                }
+
+                else
+                {
+                    if (string.IsNullOrEmpty(myResult))
+                    {
+                        hasRoles = false;
+                    }
+                }
+
+                if (!hasRoles)
+                {
+                    string message = TranslateTextsClass.Translate("User.M.AddRoleToUser", this.tenant);
+                    throw new ApplicationException(message);
                 }
             }
-
-            else
-            {
-                if (string.IsNullOrEmpty(myResult))
-                {
-                    hasRoles = false;
-                }
-            }
-
-            if (!hasRoles)
-            {
-                string message = TranslateTextsClass.Translate("User.M.AddRoleToUser", this.tenant);
-                throw new ApplicationException(message);
-            }
-
             return myResult;
         }
 
