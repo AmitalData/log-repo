be44b808bfda5aba09986b25630d9b482f934b13
Islam Shaheen <Islam@logitudeworld.com>
2020-11-04 19:41:43 +0200

    duplicate potential customers

--- Logitude/JustWebFreight/WebFreight.Web/WcfApi/CustomerWcfService.svc.cs
+++ Logitude/JustWebFreight/WebFreight.Web/WcfApi/CustomerWcfService.svc.cs
@@ -441,7 +441,13 @@ namespace WebFreight.Web.WcfApi
 
                     if (entity == null && !string.IsNullOrEmpty(entityPM.VatNumber))
                     {
-                        entity = GetCustomerByVatNumber(entityPM, customerRepository, countryRepository, tenantEntity);
+                        Customer customer = customerRepository.GetSingleCustomerByVatForHybrid(entityPM.VatNumber, entityPM.Tenant, false);
+                        if (customer != null && (customer.CustomerStatusCode == "WAC" || customer.CustomerStatusCode == "POT"))
+                        {
+                            entity = customer;
+                        }
+                        else
+                            entity = GetCustomerByVatNumber(entityPM, customerRepository, countryRepository, tenantEntity);
                     }
 
                     if (entity == null)
@@ -596,6 +602,7 @@ namespace WebFreight.Web.WcfApi
                 {
                     entity = GetCustomerByVatUniquePartnerType(entityPM, customerRepository, tenantEntity);
                     //entity = customerRepository.GetSingleCustomerByVatForHybrid(entityPM.VatNumber, entityPM.Tenant, false);
+
                     if (entity != null && (entity.Card.Code != entityPM.Code))
                     {
                         throw new ApplicationException("A customer with the same vat and different code already exists.");
@@ -603,6 +610,8 @@ namespace WebFreight.Web.WcfApi
                         //response.ErrorMessage = "A customer with the same vat and different code already exists.";
                         //return response;
                     }
+
+
                 }
             }
 
