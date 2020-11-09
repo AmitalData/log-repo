4e1200e69b3b91f8a4f05b8142d948d2f597a1e3
Wesam Amarneh <wesam@logitudeworld.com>
2020-11-09 14:35:12 +0200

    Automation Module| Integration Testing: Internal Meeting & Tasks Working
    Related Work Items: #74197

---
+++ Logitude/Logitude.IntegrationTest.Automation/Initializers/LoginInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Logitude.IntegrationTest.Core.Login;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[assembly: Parallelize(Workers = 0, Scope = ExecutionScope.MethodLevel)] //0 means use as many workers as possible
+
+namespace Logitude.IntegrationTest.Automation.Initializers
+{
+    [TestClass]
+    public class LoginInitializer
+    {
+        [AssemblyInitialize]
+        public static async Task PrepareAutomationTest(TestContext context)
+        {
+            string email = IntegrationTestLoginParameters.Email;
+            await LoginService.GetLoginTokenByUserEmailAndTenant();
+            string testToken = IntegrationTestLoginParameters.Token;
+        }
+    }
+}
---
+++ Logitude/Logitude.IntegrationTest.Automation/Tests/Automations/OnUpdate_Email.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Logitude.IntegrationTest.Core.Login;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Logitude.IntegrationTest.Automation.Tests.Shipments
+{
+    [TestClass]
+    public class OnUpdate_Email
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string testToken = IntegrationTestLoginParameters.Token;
+            Console.WriteLine(testToken);
+        }
+    }
+}
