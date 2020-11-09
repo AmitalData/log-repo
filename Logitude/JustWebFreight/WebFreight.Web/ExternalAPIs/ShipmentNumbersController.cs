a82c33e93bab885191dd98537eaef9fa2c81d73a
Maheera Jazi <Maheera@logitudeworld.com>
2020-11-09 08:35:20 +0200

    74366
    Related Work Items: #74366

---
+++ Logitude/JustWebFreight/WebFreight.Web/ExternalAPIs/ShipmentNumbersController.cs
@@ -0,0 +1,35 @@
+﻿using Simplog.Data.CommonDataModel.EntityPOCOs;
+using Simplog.Data.CommonDataModel.Repositories;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WebFreight.Web.Helpers.ExternalAPIHelpers;
+using WebFreight.Web.Security;
+
+namespace WebFreight.Web.ExternalAPIs
+{
+    public class ShipmentNumbersController : ApiController
+    {
+        public HttpResponseMessage GetDocumentByRef()
+        {
+            try
+            {
+                string token = HttpContext.Current.Request.Headers["Token"];
+                AuthenticationToken authToken = AuthenticationTokenRepository.GetSingleTokenFromCache(token);
+                int tenant = authToken.Tenant;
+                SecurityUtility.AuthenticateAPICall(authToken.Tenant);
+                
+
+                return Request.CreateResponse(HttpStatusCode.OK, Result);
+            }
+            catch (Exception ex)
+            {
+                var apiExceptionResult = ApiExceptionHandler.HandleException(ex);
+                return Request.CreateResponse(apiExceptionResult.StatusCode, apiExceptionResult.Exception);
+            }
+        }
+
+    }
+}
