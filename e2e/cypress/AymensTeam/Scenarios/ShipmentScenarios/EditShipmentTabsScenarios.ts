40c0d613a5e794e181e67db0a41bdfa218da5829
logitudo <logitudo@live.com>
2020-11-11 09:36:41 +0200

    fix

--- e2e/cypress/AymensTeam/Scenarios/ShipmentScenarios/EditShipmentTabsScenarios.ts
+++ e2e/cypress/AymensTeam/Scenarios/ShipmentScenarios/EditShipmentTabsScenarios.ts
@@ -15,13 +15,13 @@ export class EditShipmentTabsScenarios {
     public OrderTab: OrderTab = new OrderTab();
 
     public RunEditTabsScenarios(levelCode: string, transportMode: string, direction: string, shipmentType: string = null) {
-      //  this.FillGeneralTab();
-     //   this.OrderTab.RunOrderTabScenarios(shipmentType);
-      //  this.PartnersTab.RunPartnersTabScenarios(levelCode,direction, transportMode);
-      //  this.PackagesTab.RunPackagesTabsScenarios(shipmentType, transportMode);
-      //  this.RoutingTab.RunRoutingTabScenarios(levelCode,transportMode);
+        this.FillGeneralTab();
+        this.OrderTab.RunOrderTabScenarios(shipmentType);
+        this.PartnersTab.RunPartnersTabScenarios(levelCode,direction, transportMode);
+        this.PackagesTab.RunPackagesTabsScenarios(shipmentType, transportMode);
+        this.RoutingTab.RunRoutingTabScenarios(levelCode,transportMode);
         this.PayablesTab.RunPayablesTabScenarios();
-        this.ReceivablesTab.RunReceivablesTabScenarios();
+       // this.ReceivablesTab.RunReceivablesTabScenarios();
     }
     private FillGeneralTab() {
         cy.get('#ShipmentTHGeneral').click();
