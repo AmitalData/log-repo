673a08bf844309e789d3215aad2d21ac63f7c538
sohaib <sohaib@amital.co.il>
2020-11-11 09:42:00 +0200

    #74484

---
+++ Logitude/CommunicationWorkerRole/Tasks/CustomsSendDeclarationStatus.cs
@@ -0,0 +1,47 @@
+﻿using Logitude.Customs.BL.EntityQueryServices;
+using Logitude.Server.Tools;
+using Logitude.Server.Tools.Contracts;
+using Logitude.Server.Tools.Helpers;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationWorkerRole.Tasks
+{
+    public class CustomsSendDeclarationStatus : TaskManagerBase
+    {
+        private int _SeedDefaultTenant = 0;
+        private string _TaskId;
+
+        public CustomsSendDeclarationStatus(string Id, int tenant)
+            : base(Id, tenant)
+        {
+            _SeedDefaultTenant = tenant;
+            _TaskId = Id;
+        }
+        public override void StartTask()
+        {
+
+            LogMessagingUtil.Instance.Clear();
+            try
+            {
+                ICustomsSendDeclarationStatus myICustomsSendDeclarationStatus = ContainerAccessor.Container.Resolve(typeof(ICustomsSendDeclarationStatus), "SendDeclarationStatus", new ParameterOverride("", this._SeedDefaultTenant)) as ICustomsSendDeclarationStatus;
+                myICustomsSendDeclarationStatus.StartRun(_TaskId, _SeedDefaultTenant);
+            }
+            catch (Exception e)
+            {
+
+                LogException(e.ToString());
+            }
+            finally
+            {
+                this.LogInfoToDB(LogMessagingUtil.Instance.ToString());
+                LogInfo(LogMessagingUtil.Instance.ToString());
+
+            }
+        }
+    }
+}
---
+++ Logitude/Logitude.Server.Tools/Contracts/ICustomsSendDeclarationStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logitude.Server.Tools.Contracts
+{
+    public interface ICustomsSendDeclarationStatus
+    {
+        void StartRun(string taskId, int seedDefaultTenant);
+    }
+}
