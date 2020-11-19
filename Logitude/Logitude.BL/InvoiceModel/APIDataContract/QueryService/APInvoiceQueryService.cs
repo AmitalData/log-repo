afea9543ec28d47033dc73a8c4789218bd9deca7
Alaa Jamal <Alaa@logitudeworld.com>
2020-11-19 07:21:17 +0200

    #74875

--- Logitude/Logitude.BL/InvoiceModel/APIDataContract/QueryService/APInvoiceQueryService.cs
+++ Logitude/Logitude.BL/InvoiceModel/APIDataContract/QueryService/APInvoiceQueryService.cs
@@ -367,8 +367,9 @@ namespace Logitude.BL.InvoiceModel.APIDataContract.ApiV1
                 throw new ApplicationException("Payment Term is required");
             }
         }
-        private void InitAndValidateTotalVATsOnly()
+        public void InitAndValidateTotalVATsOnly()
         {
+
             if (this.aPInvoicePM.TotalVATOnly)
             {
                 foreach (APInvoiceLinePM line in this.aPInvoicePM.InvoiceLines)
@@ -862,6 +863,7 @@ namespace Logitude.BL.InvoiceModel.APIDataContract.ApiV1
 
         public void APInvoiceCustomDataMapping(APInvoice apinvoice, int tenant)
         {
+        
             apinvoice.Tenant = tenant;
             apinvoice.InvoiceExpectedAmount = Math.Round((double)apinvoice.AmountInInvoiceCurrency, 2);
             apinvoice.AmountInInvoiceCurrency= Math.Round((double)apinvoice.AmountInInvoiceCurrency, 2);
@@ -897,7 +899,7 @@ namespace Logitude.BL.InvoiceModel.APIDataContract.ApiV1
             {
                 apinvoice.AccountingDate = TenantServerConfigration.GetCurrentDateTime(tenant);
             }
-            this.InitAndValidateTotalVATsOnly();
+       
         }
 
         public void PaymentTermMapAndValidate(APInvoice apinvoice, APInvoicePM apinvoicePM, int tenant)
