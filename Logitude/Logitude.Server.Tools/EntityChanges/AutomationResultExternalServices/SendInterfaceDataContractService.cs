34b48d33969a1a071348e833c9690832409dffe1
Abed <abed@logitudeworld.com>
2020-11-26 08:53:16 +0200

    74605
    Related Work Items: #74605

--- Logitude/Logitude.Server.Tools/EntityChanges/AutomationResultExternalServices/SendInterfaceDataContractService.cs
+++ Logitude/Logitude.Server.Tools/EntityChanges/AutomationResultExternalServices/SendInterfaceDataContractService.cs
@@ -70,14 +70,15 @@ namespace Logitude.Server.Tools.EntityChanges.Service
 
         private object GetDataContractObject(object entityPM , string computingPartnerCode)
         {
+            string shipmentLevelName = GetShipmentLevelName(entityPM);
             object sendInterfaceDataContractObject = null;
             Assembly blAssembly = Assembly.Load("Logitude.BL");
-            string typePath = "Logitude.BL.ShipmentsModel.APIDataContract.ApiV1." + sendInterfaceDataContractFileName + "QueryService";
+            string typePath = "Logitude.BL.ShipmentsModel.APIDataContract.ApiV1." + shipmentLevelName + "QueryService";
             Type type = blAssembly.GetType(typePath);
             if (type != null)
             {
                 var queryService = Activator.CreateInstance(type, new object[] { tenant });
-                MethodInfo methodInfo = queryService.GetType().GetMethods().Where(d => d.Name == (sendInterfaceDataContractFileName + "DataMapping")).FirstOrDefault();
+                MethodInfo methodInfo = queryService.GetType().GetMethods().Where(d => d.Name == (shipmentLevelName + "DataMapping")).FirstOrDefault();
                 sendInterfaceDataContractObject = methodInfo.Invoke(queryService, new object[] { entityPM, tenant, computingPartnerCode });
             }
             return sendInterfaceDataContractObject;
@@ -85,10 +86,19 @@ namespace Logitude.Server.Tools.EntityChanges.Service
 
         private string GetDataContractFileName(object entityPM)
         {
-            string sendInterfaceDataContractFileName = GetPropertyValueFromObject("ShipmentLevelName", entityPM);
-            if (sendInterfaceDataContractFileName == "Consol") sendInterfaceDataContractFileName = "Master";
-            return sendInterfaceDataContractFileName;
+            string shipmentNumber = GetPropertyValueFromObject("ShipmentNumber", entityPM);
+            string transportModeId = GetPropertyValueFromObject("TransportModeId", entityPM);
+            string directionId = GetPropertyValueFromObject("DirectionId", entityPM);
+            return GetShipmentLevelName(entityPM) + " " + transportModeId+ directionId +" "+ shipmentNumber;
         }
+        private string GetShipmentLevelName(object entityPM)
+        {
+            string shipmentLevelName = GetPropertyValueFromObject("ShipmentLevelName", entityPM);
+
+            return (shipmentLevelName == "Consol" ? "Master" : shipmentLevelName);
+        }
+
+
 
         private Document CreateDocument(byte[] fileData, string extension)
         {
