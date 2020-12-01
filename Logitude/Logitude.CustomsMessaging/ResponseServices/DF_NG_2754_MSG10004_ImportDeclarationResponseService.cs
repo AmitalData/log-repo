f9d5177340dda54cbb363e106c0a3cc6331c76e0
elisheva <elisheva@amital.co.il>
2020-12-01 11:26:01 +0200

    75521
    Related Work Items: #75521

--- Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_2754_MSG10004_ImportDeclarationResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_2754_MSG10004_ImportDeclarationResponseService.cs
@@ -626,12 +626,12 @@ namespace Logitude.CustomsMessaging.ResponseServices
             string paymentStatusCode = null; //Yuval Chalup 10.08.2015 TASK-15472
             if (customResponse.DeclarationPaymentDetails != null)
             {
-                if (customResponse.DeclarationPaymentDetails.PaymentOrderNumber != null)
+                if (customResponse.DeclarationPaymentDetails.PaymentOrderNumber != null && customResponse.DeclarationPaymentDetails.PaymentOrderNumber!=0)
                 {
                     _MyDeclarationPM.PaymentOrderNumber = customResponse.DeclarationPaymentDetails.PaymentOrderNumber.ToString();
                     paymentOrderNumber = customResponse.DeclarationPaymentDetails.PaymentOrderNumber.ToString(); //Yuval Chalup 10.08.2015 TASK-15472
                 }
-                if (customResponse.DeclarationPaymentDetails.PaymentOrderStatus != null)
+                if (customResponse.DeclarationPaymentDetails.PaymentOrderStatus != null && customResponse.DeclarationPaymentDetails.PaymentOrderStatus!=0)
                 {
                     _MyDeclarationPM.PaymentStatusCode = customResponse.DeclarationPaymentDetails.PaymentOrderStatus.ToString();
                     paymentStatusCode = customResponse.DeclarationPaymentDetails.PaymentOrderStatus.ToString(); //Yuval Chalup 10.08.2015 TASK-15472
@@ -1048,7 +1048,8 @@ namespace Logitude.CustomsMessaging.ResponseServices
                 //<--- Yuval Chalup 09.11.2015 TASK-16498 - Update PaymentOrderNumber in Payment
                 if (customResponse.DeclarationPaymentDetails != null)
                 {
-                    if (customResponse.DeclarationPaymentDetails.PaymentOrderNumber != null || (_IsSubmitDeclarationResponse == true && string.IsNullOrWhiteSpace(paymentOrderNumber) && _MyDeclarationPM.DeclarationStatusTypeCode == "5" && _MyDeclarationPM.TotalTax <= 5))
+                    if(customResponse.DeclarationPaymentDetails.PaymentOrderStatus != null && customResponse.DeclarationPaymentDetails.PaymentOrderNumber != null && customResponse.DeclarationPaymentDetails.PaymentOrderStatus != 0)
+                   // if (customResponse.DeclarationPaymentDetails.PaymentOrderNumber != null || (_IsSubmitDeclarationResponse == true && string.IsNullOrWhiteSpace(paymentOrderNumber) && _MyDeclarationPM.DeclarationStatusTypeCode == "5" && _MyDeclarationPM.TotalTax <= 5))
                     {
                         var unifreightDeclarationPaymentUpdateService = new UnifreightDeclarationPaymentUpdateService(declarationPaymentsPM, _MyDeclarationPM);
                         unifreightDeclarationPaymentUpdateService.Update();
