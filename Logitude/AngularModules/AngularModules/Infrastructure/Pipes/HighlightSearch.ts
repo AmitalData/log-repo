55fab66d3c030927ca2bab347ca8cbabc7520833
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2020-12-02 13:25:41 +0200

    fix 75581
    Related Work Items: #75581

--- Logitude/AngularModules/AngularModules/Infrastructure/Pipes/HighlightSearch.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Pipes/HighlightSearch.ts
@@ -8,7 +8,7 @@ export class HighlightSearch implements PipeTransform {
     transform(value: any, args: any): any {
         if (!args) { return value; }
         var re = new RegExp(args, 'gi'); //'gi' for case insensitive and can use 'g' if you want the search to be case sensitive.
-        if (re) {
+        if (value) {
             return value.replace(re, "<mark>$&</mark>");
         }
         return value;
