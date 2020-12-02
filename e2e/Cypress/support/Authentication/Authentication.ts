e37bc053cf3b151062b220e62774d4af26520336
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2020-12-02 15:01:26 +0200

    fix Merge conflict and improve login Code

--- e2e/Cypress/support/Authentication/Authentication.ts
+++ e2e/Cypress/support/Authentication/Authentication.ts
@@ -2,52 +2,23 @@ declare namespace Cypress {
     interface Chainable {
         Login(): Chainable<Element>
         OpenAndFillChangePasswordPage(newPassword:string,confirmNewPassword:string): Chainable<Element>
-    }
+    } 
+    
 }
 
 Cypress.Commands.add("Login", () => {
     let mode = Cypress.env("Mode")
+    
+    if(mode.toLowerCase() === "development"){ 
 
-    if(mode.toLowerCase() === "development"){
         cy.fixture("Login.json").then((LoginData) => {
-            cy.visit(LoginData.url)
-            cy.get("#Email").clear().type(LoginData.email)
-            cy.get("#Password").clear().type(LoginData.password)
-            cy.get("#cmdLogin").click()
-            if (LoginData.tenant !== null) {
-                cy.get("input[name='cmbTenants_input']").clear().type(LoginData.tenant)
-                cy.wait(500)
-                cy.get("#cmbTenants_listbox").find("li").click()
-            }
-        })
-    }else{
-        let url = Cypress.env("Url")
-        let email = Cypress.env("Email")
-        let password = Cypress.env("Password")
-        let tenant = Cypress.env("Tenant")
-        cy.visit(url)
-        cy.get("#Email").clear().type(email)
-        cy.get("#Password").clear().type(password)
-
-        cy.get("#cmdLogin").click()
-
-        if (LoginData.tenant !== null) {
-            cy.get("input[name='cmbTenants_input']").type(LoginData.tenant)
-            //cy.wait(500)
-            cy.get("#cmbTenants_listbox").children().contains('(' + LoginData.tenant + ')').eq(0).click({force:true})
-
-        }
-
+            CompleteLoginProcess(LoginData.email,LoginData.password,LoginData.url,LoginData.tenant);
+        }) 
     }
+    else{
 
-
-
-    cy.server()
-    cy.route("**/ObjectTableLastUpdate/**").as("LoadDataCompleted")
-    cy.intercept("**/ObjectTableLastUpdate/**").as("LoadDataCompleted")
-    cy.window().then(win => { win.sessionStorage.setItem("ControlledByCypress", "true") })
-    cy.wait("@LoadDataCompleted")
-
+        CompleteLoginProcess(Cypress.env("Email"),Cypress.env("Password"),Cypress.env("Url"),Cypress.env("Tenant")); 
+    }
 })
 
 Cypress.Commands.add("OpenAndFillChangePasswordPage", (newPassword,confirmNewPassword) => {
@@ -58,13 +29,23 @@ Cypress.Commands.add("OpenAndFillChangePasswordPage", (newPassword,confirmNewPas
         cy.FillLogTextBox("#CurrentPassword",LoginData.password); 
         cy.FillLogTextBox('#Password',newPassword);
         cy.FillLogTextBox('#ConfirmPassword',confirmNewPassword);
-    })
+    }) 
+})
 
-    //cy.get("#cmdContinue").click()
+// -------------------------------- functions
+function CompleteLoginProcess(Email:string,Password:string,URL : string,Tenant?:number){
+    cy.visit(URL)
+    cy.get("#Email").clear().type(Email)
+    cy.get("#Password").clear().type(Password)
+    cy.get("#cmdLogin").click()
+    if (Tenant !== null) {
+        cy.get("input[name='cmbTenants_input']").clear().type('(' + Tenant + ')') 
+        cy.get("#cmbTenants_listbox").children().contains('(' + Tenant + ')').eq(0).click({force:true})
+    }
+    cy.get("#cmdContinue").click()
     //cy.server()
     //cy.route("**/ObjectTableLastUpdate/**").as("LoadDataCompleted")
-    //cy.intercept("**/ObjectTableLastUpdate/**").as("LoadDataCompleted")
-    //cy.window().then(win => { win.sessionStorage.setItem("ControlledByCypress", "true") })
-    //cy.wait("@LoadDataCompleted")
-
-})
+    cy.intercept("**/ObjectTableLastUpdate/**").as("LoadDataCompleted")
+    cy.window().then(win => { win.sessionStorage.setItem("ControlledByCypress", "true") })
+    cy.wait("@LoadDataCompleted")
+}
