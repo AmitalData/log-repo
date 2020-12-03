c114b00f646418f810d6a457a934c55f70efb92d
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2020-12-03 13:21:59 +0200

    my commit

--- e2e/cypress/plugins/index.js
+++ e2e/cypress/plugins/index.js
@@ -15,7 +15,10 @@
 /**
  * @type {Cypress.PluginConfig}
  */
+
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  /** the rest of your plugins... **/
+  require('cypress-log-to-output').install(on)
+  // or, if there is already a before:browser:launch handler, use .browserLaunchHandler inside of it
+  // @see https://github.com/flotwig/cypress-log-to-output/issues/5
 }
