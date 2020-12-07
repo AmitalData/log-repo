cdfc9f4caabd47969cc32f02600457c5d15495ca
Islam Shaheen <Islam@logitudeworld.com>
2020-12-07 14:55:45 +0200

    DWH Incremental Process vs Incorrect Fields Data

--- Logitude/WarehouseData/WarehouseScript/Others/Day 17[AbedAddFuncationResolveCustomFieldDateValue].sql
+++ Logitude/WarehouseData/WarehouseScript/Others/Day 17[AbedAddFuncationResolveCustomFieldDateValue].sql
@@ -14,20 +14,23 @@ AS
 BEGIN  
  DECLARE @MyValueOut sql_variant
   DECLARE @Result date
+  DECLARE @DateString as varchar(20)
  if(@DataTypeCode = ''Date'' ) begin  set @Result = ''1-1-1''; end 
 
 if(@DataTypeCode = ''DateTime'' and  LEN(@FieldValue) >= 14)
 begin
-set @Result = SUBSTRING(@FieldValue, 1, 4) + ''-''+ SUBSTRING(@FieldValue, 5, 2) + ''-''+ SUBSTRING(@FieldValue, 7, 2) +'' ''+ SUBSTRING
-(@FieldValue, 9, 2) + '':''+ SUBSTRING(@FieldValue, 11, 2) + '':''+ SUBSTRING(@FieldValue, 13, 2) 
-set @Result = convert(varchar, CAST(@Result AS datetime), 20)
+set @DateString = SUBSTRING(@FieldValue, 1, 4) + ''-''+ SUBSTRING(@FieldValue, 5, 2) + ''-''+ SUBSTRING(@FieldValue, 7, 2) +'' ''+ SUBSTRING(@FieldValue, 9, 2) + '':''+ SUBSTRING(@FieldValue, 11, 2) + '':''+ SUBSTRING(@FieldValue, 13, 2) 
+
+set @Result = convert(varchar, Try_CAST(@DateString AS datetime), 20)
 -- set @Result = CONVERT(DATE, @Result)
+
 end
 
 ELSE if(@DataTypeCode = ''Date''  and  LEN(@FieldValue) >= 8) 
-begin 
-set @Result = SUBSTRING(@FieldValue, 1, 4) + ''-''+ SUBSTRING(@FieldValue, 5, 2) + ''-''+ SUBSTRING(@FieldValue, 7, 2)
-SET @Result =  convert(varchar, CAST(@Result AS datetime), 23) 
+begin
+
+set @DateString = SUBSTRING(@FieldValue, 1, 4) + ''-''+ SUBSTRING(@FieldValue, 5, 2) + ''-''+ SUBSTRING(@FieldValue, 7, 2)
+SET @Result =  convert(varchar, Try_CAST(@DateString AS datetime), 23) 
 set @Result = dbo.GetDateFormateAsNumber(CONVERT(DATE, @Result))
 end
 
