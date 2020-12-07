0372ff8d415df3cd9f24c54e10ff5a7e0627a7ba
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2020-12-07 08:32:13 +0200

    config update

--- e2e/Cypress/RabaiaTeam/Tests/Shipments/NewShipmentTest.ts
+++ e2e/Cypress/RabaiaTeam/Tests/Shipments/NewShipmentTest.ts
@@ -1,4 +1,4 @@
-describe("Create Shipment Tests", () => {
+describe("Create Direct Shipment Tests", () => {
 
     before(() => {
         cy.Login()
@@ -6,76 +6,76 @@ describe("Create Shipment Tests", () => {
         cy.NavigateToWorkSpaceTab("#SHIP")
     })
 
-    // it("Create Direct Export Air Shipment", () => {
+    it("Create Direct Export Air Shipment", () => {
 
-    //     cy.Click("#HelperNotesButton_0_0")
-    //     cy.Click(".LogitudeToggleButtonItem", "Direct")
-    //     cy.ClickRadio("#DirectionRadio_0E")
-    //     cy.ClickRadio("#TransportModeRadio_0A")
-    //     cy.ValidateValue("#Shipment_ShipmentCustomerTypeCode", "Shipper")
-    //     cy.SelectLogLovFirstElement("#Shipment_CustomerId")
-    //     cy.SelectLogLovFirstElement("#Shipment_MainCarriageFromPortId")
-    //     cy.SelectLogLovFirstElement("#Shipment_MainCarriageToPortId")
-    //     //cy.SaveClick("**/shipment", "#ShipmentCreatebtn", "Shipment Saved Successfully")
+        cy.Click("#HelperNotesButton_0_0")
+        cy.Click(".LogitudeToggleButtonItem", "Direct")
+        cy.ClickRadio("#DirectionRadio_0E")
+        cy.ClickRadio("#TransportModeRadio_0A")
+        cy.ValidateValue("#Shipment_ShipmentCustomerTypeCode", "Shipper")
+        cy.SelectLogLovFirstElement("#Shipment_CustomerId")
+        cy.SelectLogLovFirstElement("#Shipment_MainCarriageFromPortId")
+        cy.SelectLogLovFirstElement("#Shipment_MainCarriageToPortId")
+        cy.SaveClick("**/shipment", "#ShipmentCreatebtn", "Shipment Saved Successfully")
 
-    // })
+    })
 
-    // it("Create Direct Import Air Shipment", () => {
+    it("Create Direct Import Air Shipment", () => {
 
-    //     cy.Click("#HelperNotesButton_0_0")
-    //     cy.Click(".LogitudeToggleButtonItem", "Direct")
-    //     cy.ClickRadio("#DirectionRadio_0I")
-    //     cy.ClickRadio("#TransportModeRadio_0A")
-    //     cy.ValidateValue("#Shipment_ShipmentCustomerTypeCode", "Consignee")
-    //     cy.SelectLogLovFirstElement("#Shipment_CustomerId")
-    //     cy.SelectLogLovFirstElement("#Shipment_MainCarriageFromPortId")
-    //     cy.SelectLogLovFirstElement("#Shipment_MainCarriageToPortId")
-    //     //cy.SaveClick("**/shipment", "#ShipmentCreatebtn", "Shipment Saved Successfully")
+        cy.Click("#HelperNotesButton_0_0")
+        cy.Click(".LogitudeToggleButtonItem", "Direct")
+        cy.ClickRadio("#DirectionRadio_0I")
+        cy.ClickRadio("#TransportModeRadio_0A")
+        cy.ValidateValue("#Shipment_ShipmentCustomerTypeCode", "Consignee")
+        cy.SelectLogLovFirstElement("#Shipment_CustomerId")
+        cy.SelectLogLovFirstElement("#Shipment_MainCarriageFromPortId")
+        cy.SelectLogLovFirstElement("#Shipment_MainCarriageToPortId")
+        cy.SaveClick("**/shipment", "#ShipmentCreatebtn", "Shipment Saved Successfully")
 
-    // })
+    })
 
-    // it("Create Direct Domestic Air Shipment", () => {
+    it("Create Direct Domestic Air Shipment", () => {
 
-    //     cy.Click("#HelperNotesButton_0_0")
-    //     cy.Click(".LogitudeToggleButtonItem", "Direct")
-    //     cy.ClickRadio("#DirectionRadio_0D")
-    //     cy.ClickRadio("#TransportModeRadio_0A")
-    //     cy.ValidateValue("#Shipment_ShipmentCustomerTypeCode", "Shipper")
-    //     cy.SelectLogLovFirstElement("#Shipment_CustomerId")
-    //     cy.SelectLogLovFirstElement("#Shipment_MainCarriageFromPortId")
-    //     cy.SelectLogLovFirstElement("#Shipment_MainCarriageToPortId")
-    //     //cy.SaveClick("**/shipment", "#ShipmentCreatebtn", "Shipment Saved Successfully")
+        cy.Click("#HelperNotesButton_0_0")
+        cy.Click(".LogitudeToggleButtonItem", "Direct")
+        cy.ClickRadio("#DirectionRadio_0D")
+        cy.ClickRadio("#TransportModeRadio_0A")
+        cy.ValidateValue("#Shipment_ShipmentCustomerTypeCode", "Shipper")
+        cy.SelectLogLovFirstElement("#Shipment_CustomerId")
+        cy.SelectLogLovFirstElement("#Shipment_MainCarriageFromPortId")
+        cy.SelectLogLovFirstElement("#Shipment_MainCarriageToPortId")
+        cy.SaveClick("**/shipment", "#ShipmentCreatebtn", "Shipment Saved Successfully")
 
-    // })
+    })
 
-    // it("Create Direct Drop Air Shipment", () => {
+    it("Create Direct Drop Air Shipment", () => {
 
-    //     cy.Click("#HelperNotesButton_0_0")
-    //     cy.Click(".LogitudeToggleButtonItem", "Direct")
-    //     cy.ClickRadio("#DirectionRadio_0R")
-    //     cy.ClickRadio("#TransportModeRadio_0A")
-    //     cy.ValidateValue("#Shipment_ShipmentCustomerTypeCode", "Shipper")
-    //     cy.SelectLogLovFirstElement("#Shipment_CustomerId")
-    //     cy.SelectLogLovFirstElement("#Shipment_MainCarriageFromPortId")
-    //     cy.SelectLogLovFirstElement("#Shipment_MainCarriageToPortId")
-    //     //cy.SaveClick("**/shipment", "#ShipmentCreatebtn", "Shipment Saved Successfully")
+        cy.Click("#HelperNotesButton_0_0")
+        cy.Click(".LogitudeToggleButtonItem", "Direct")
+        cy.ClickRadio("#DirectionRadio_0R")
+        cy.ClickRadio("#TransportModeRadio_0A")
+        cy.ValidateValue("#Shipment_ShipmentCustomerTypeCode", "Shipper")
+        cy.SelectLogLovFirstElement("#Shipment_CustomerId")
+        cy.SelectLogLovFirstElement("#Shipment_MainCarriageFromPortId")
+        cy.SelectLogLovFirstElement("#Shipment_MainCarriageToPortId")
+        cy.SaveClick("**/shipment", "#ShipmentCreatebtn", "Shipment Saved Successfully")
 
-    // })
+    })
 
-    // it("Create Direct Export Ocean FCL Shipment", () => {
+    it("Create Direct Export Ocean FCL Shipment", () => {
 
-    //     cy.Click("#HelperNotesButton_0_0")
-    //     cy.Click(".LogitudeToggleButtonItem", "Direct")
-    //     cy.ClickRadio("#DirectionRadio_0E")
-    //     cy.ClickRadio("#TransportModeRadio_0O")
-    //     cy.ClickRadio("#ShipmentTypeRadio_0FCLD")
-    //     cy.ValidateValue("#Shipment_ShipmentCustomerTypeCode", "Shipper")
-    //     cy.SelectLogLovFirstElement("#Shipment_CustomerId")
-    //     cy.SelectLogLovFirstElement("#Shipment_MainCarriageFromPortId")
-    //     cy.SelectLogLovFirstElement("#Shipment_MainCarriageToPortId")
-    //     //cy.SaveClick("**/shipment", "#ShipmentCreatebtn", "Shipment Saved Successfully")
+        cy.Click("#HelperNotesButton_0_0")
+        cy.Click(".LogitudeToggleButtonItem", "Direct")
+        cy.ClickRadio("#DirectionRadio_0E")
+        cy.ClickRadio("#TransportModeRadio_0O")
+        cy.ClickRadio("#ShipmentTypeRadio_0FCLD")
+        cy.ValidateValue("#Shipment_ShipmentCustomerTypeCode", "Shipper")
+        cy.SelectLogLovFirstElement("#Shipment_CustomerId")
+        cy.SelectLogLovFirstElement("#Shipment_MainCarriageFromPortId")
+        cy.SelectLogLovFirstElement("#Shipment_MainCarriageToPortId")
+        cy.SaveClick("**/shipment", "#ShipmentCreatebtn", "Shipment Saved Successfully")
 
-    // })
+    })
 
     it("Create Direct Export Ocean LCL Shipment", () => {
 
@@ -83,7 +83,7 @@ describe("Create Shipment Tests", () => {
         cy.FillShipmentDefaultFields("E","O","LCLD"); 
         cy.ValidateValue("#Shipment_ShipmentCustomerTypeCode", "Shipper")
         
-        //cy.SaveClick("**/shipment", "#ShipmentCreatebtn", "Shipment Saved Successfully")
+        cy.SaveClick("**/shipment", "#ShipmentCreatebtn", "Shipment Saved Successfully")
 
     })
 
