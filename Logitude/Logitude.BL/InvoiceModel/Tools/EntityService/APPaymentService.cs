307c990292f4e535110027b2aa1bed19a159b9d1
Maheera Jazi <Maheera@logitudeworld.com>
2020-12-14 09:03:22 +0200

    75808
    Related Work Items: #75808

--- Logitude/Logitude.BL/InvoiceModel/Tools/EntityService/APPaymentService.cs
+++ Logitude/Logitude.BL/InvoiceModel/Tools/EntityService/APPaymentService.cs
@@ -992,7 +992,7 @@ namespace Logitude.BL.InvoiceModel.Tools.EntityService
 
                         this.UpdateInvoicePaidDate(invoice);
                         APInvoiceService aRInvoiceService = new APInvoiceService(this.objectContext, this.tenant);
-                        aRInvoiceService.Update(invoice);
+                        aRInvoiceService.Update(invoice, true);
                         //invoiceRepository.Update(invoice);
                         #endregion
                     }
