c8a6bb912601ec914a28a6f353103231d7500f4a
elisheva <elisheva@amital.co.il>
2020-12-15 16:43:27 +0200

    #75988 75988

--- Logitude/AngularModules/AngularModules/Customs/Services/StandardLists/AmendmentRequestRejectReasonTypeListService.ts
+++ Logitude/AngularModules/AngularModules/Customs/Services/StandardLists/AmendmentRequestRejectReasonTypeListService.ts
@@ -19,7 +19,7 @@ import {SessionLocator} from '../../../Infrastructure/Utilities/SessionLocator';
 import {SessionInfo} from '../../../Infrastructure/Utilities/SessionInfo';
 import {PerformanceLogger} from '../../../Infrastructure/Utilities/PerformanceLogger';
 import {LocalStorageManager} from '../../../Infrastructure/Utilities/LocalStorageManager';
-import {AmendRequestRejectReasonTypeList} from '../../EntityLists/AmendmentRequestRejectReasonTypeList';
+import {AmendRequestRejectReasonTypeList} from '../../EntityLists/AmendRequestRejectReasonTypeList';
 
 @Injectable()
 
