bd2e96c99cc07847a79e766d1498d095eba8114b
Anas Ziq <anasz@logitudeworld.com>
2020-12-22 09:11:04 +0200

    #74989

--- Logitude/AngularModules/AngularModules/Common/EntityPMs/ShippingLinePM.ts
+++ Logitude/AngularModules/AngularModules/Common/EntityPMs/ShippingLinePM.ts
@@ -356,7 +356,10 @@ export class ShippingLinePM {
     private usoCFDICode: string;
     public get UsoCFDICode() { return this.usoCFDICode; }
     public set UsoCFDICode(newValue: string) { if (this.usoCFDICode != newValue) { this.usoCFDICode = newValue; this.MarkAsDirty("UsoCFDICode"); } }
-       
+
+    private imageDetailId: string;
+    public get ImageDetailId() { return this.imageDetailId; }
+    public set ImageDetailId(newValue: string) { if (this.imageDetailId != newValue) { this.imageDetailId = newValue; this.MarkAsDirty("ImageDetailId"); } }
 	 
     private cBSA: string;
     public get CBSA() { return this.cBSA; }
