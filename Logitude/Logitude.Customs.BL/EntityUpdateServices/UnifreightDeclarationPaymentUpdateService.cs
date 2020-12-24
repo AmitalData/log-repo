9a251455335d5d280ea7b684d5607cff653ff10e
itzik <itzik@amital.co.il>
2020-12-24 08:42:23 +0200

    66816
    Related Work Items: #66816

--- Logitude/Logitude.Customs.BL/EntityUpdateServices/UnifreightDeclarationPaymentUpdateService.cs
+++ Logitude/Logitude.Customs.BL/EntityUpdateServices/UnifreightDeclarationPaymentUpdateService.cs
@@ -56,7 +56,9 @@ namespace Logitude.Customs.BL.EntityUpdateServices
             try
             {
                 //if ((!Environment.MachineName.Equals("itzik-7-new", StringComparison.OrdinalIgnoreCase)) && (!Environment.MachineName.Equals("yuval-7-new", StringComparison.OrdinalIgnoreCase))) return;
-                if (String.IsNullOrWhiteSpace(_DirtyDeclarationPaymentPM.DeclarationId))
+                ///if (String.IsNullOrWhiteSpace(_DirtyDeclarationPaymentPM.DeclarationId))
+                ///all ref 2 _DirtyDeclarationPaymentPM check if null !!!!!!!!
+                if (_DirtyDeclarationPaymentPM== null || String.IsNullOrWhiteSpace(_DirtyDeclarationPaymentPM.DeclarationId))
                 {
                     return;
                 }
