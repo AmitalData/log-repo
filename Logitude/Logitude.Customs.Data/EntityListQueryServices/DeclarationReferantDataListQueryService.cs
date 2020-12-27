afe207844caf00224cfa06960caaa5e3036abf02
elisheva <elisheva@amital.co.il>
2020-12-27 13:15:32 +0200

    Revert "75316"
    This reverts commit bcac1e689c1b04c278fdbfc7546450b2f3486dd0.

--- Logitude/Logitude.Customs.Data/EntityListQueryServices/DeclarationReferantDataListQueryService.cs
+++ Logitude/Logitude.Customs.Data/EntityListQueryServices/DeclarationReferantDataListQueryService.cs
@@ -116,7 +116,6 @@ namespace Logitude.Customs.Data.EntityListQueryServices
                 DeclarationReferantDataCustomFilters filters = new DeclarationReferantDataCustomFilters();
 
                 iQueryable = filters.GetFilteredQuery(iQueryable);
-                iQueryable = filters.GetFreelancerDeclarationReferantDatas(queryOperations, iQueryable, tenant);
             }
             return iQueryable;
 
