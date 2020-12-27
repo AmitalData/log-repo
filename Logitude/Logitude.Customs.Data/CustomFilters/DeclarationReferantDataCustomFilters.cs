b6426d07b5f3955ac1051d5614a728cbba249c85
elisheva <elisheva@amital.co.il>
2020-12-27 13:03:40 +0200

    75316
    Related Work Items: #75316

--- Logitude/Logitude.Customs.Data/CustomFilters/DeclarationReferantDataCustomFilters.cs
+++ Logitude/Logitude.Customs.Data/CustomFilters/DeclarationReferantDataCustomFilters.cs
@@ -1,4 +1,6 @@
 ﻿using Logitude.Customs.Data.EntityPOCOs;
+using Logitude.Customs.Data.Utils;
+using Simplog.Server.Infrastructure.DataContracts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +15,31 @@ namespace Logitude.Customs.Data.CustomFilters
         {
             queryableData = queryableData.Where(d => (d.DeclarationId == null));
             return queryableData; 
+        }
+
+
+        public IQueryable<DeclarationReferantData> GetFreelancerDeclarationReferantDatas(QueryOperations operations, IQueryable<DeclarationReferantData> queryableData, int tenant)
+        {
+            var context = CustomContext.GetContext(tenant);
+
+            FreelancerCustomersUtil frlUtil = new FreelancerCustomersUtil(tenant);
+            if (frlUtil.user.IsFreelancer)
+            {
+                List<string> customersIds = frlUtil.GetConnectedCustomersIds(tenant);
+                if (customersIds.Count > 0)
+                {
+                    queryableData = (from a in queryableData
+                                     join d in context.Declarations
+                                     on a.DeclarationId equals d.Id
+                                     where customersIds.Contains(d.CustomerId)
+                                     select a)
+                                    ;
+
+                    // queryableData = queryableData.Where(d => customersIds.Contains(d.));
+                }
+            }
+
+            return queryableData;
         }
     }
 }
