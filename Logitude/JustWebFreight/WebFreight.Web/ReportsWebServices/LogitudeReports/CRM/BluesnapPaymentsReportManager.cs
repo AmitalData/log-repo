0aef24fe1cc50e93b900307d3ae8a3ae06b958a0
Anas Ziq <anasz@logitudeworld.com>
2020-12-27 12:22:00 +0200

    #75516

--- Logitude/JustWebFreight/WebFreight.Web/ReportsWebServices/LogitudeReports/CRM/BluesnapPaymentsReportManager.cs
+++ Logitude/JustWebFreight/WebFreight.Web/ReportsWebServices/LogitudeReports/CRM/BluesnapPaymentsReportManager.cs
@@ -122,7 +122,7 @@ namespace WebFreight.Web.ReportsWebServices.LogitudeReports.Bluesnap
                                                                          select a);
             iQueryable_BluesnapTransactions = globalObjectContext.BluesnapTransactions;
             iQueryable_BluesnapTransactions = iQueryable_BluesnapTransactions.Where(d => System.Data.Entity.DbFunctions.TruncateTime(d.TransactionDate) >= System.Data.Entity.DbFunctions.TruncateTime(fromDate) && System.Data.Entity.DbFunctions.TruncateTime(d.TransactionDate) <= System.Data.Entity.DbFunctions.TruncateTime(toDate));            
-            List<int> tenantsIds = iQueryable_Tenantmanagements.ToList().Select(e => e.Id).ToList();
+            List<string> tenantsIds = iQueryable_Tenantmanagements.ToList().Select(e => e.Id.ToString()).ToList();
             this.customers_CRM = (from a in commonDataContext.Cards.Include("Customer")
                              where tenantsIds.Contains(a.ReceivablesAccountingCard) && !string.IsNullOrEmpty(a.ReceivablesAccountingCard)
                              select new CustomerCRMData
