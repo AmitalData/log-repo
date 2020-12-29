0ce172c0eb7b9dc00922fcac0a1a70c4dfe782b6
Khawla Ata <khawla@logitudeworld.com>
2020-12-29 14:52:10 +0200

    arpayemnt fixes

--- e2e/Cypress/MohammadsTeam/FullAccounting/ARPayment.spec.ts
+++ e2e/Cypress/MohammadsTeam/FullAccounting/ARPayment.spec.ts
@@ -28,7 +28,7 @@ let R: CreateRandom= new CreateRandom();
     
       cy.get('li[id=FACS]').click();
       cy.get('button[id=NewARPayment]').click();
-      cy.get('input[id=ARPayment_BillToId]').type("Test Customer GLAccountAB36000WD").should("have.value", "Test Customer GLAccountAB36000WD")
+      cy.get('input[id=ARPayment_BillToId]').type("Test Customer GLAccountXU805920XV").should("have.value", "Test Customer GLAccountXU805920XV")
       cy.get('ul[id=mydatalist_ARPayment_BillToId]').contains("Test Customer GLAccountAB36000WD").then(a => {
           a[0].click();
       })
