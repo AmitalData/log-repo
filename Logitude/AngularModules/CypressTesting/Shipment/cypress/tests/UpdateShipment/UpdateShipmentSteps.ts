bbccfd78f5d9c93e342ab35a2a82db1a3e5ec578
Abd Al-Rahman Malakh <Abd@logitudeworld.com>
2021-01-11 13:29:38 +0200

    131311
    Related Work Items: #131311

--- Logitude/AngularModules/CypressTesting/Shipment/cypress/tests/UpdateShipment/UpdateShipmentSteps.ts
+++ Logitude/AngularModules/CypressTesting/Shipment/cypress/tests/UpdateShipment/UpdateShipmentSteps.ts
@@ -13,55 +13,84 @@ Given("Go to shipments workspace", () => {
   cy.Click(Selectors.ShipmentTab, null)
 });
 
-Given("Open the direct Shipment {string}", (shipmentJsonObject) => {
+Given("Open shipment {string}", (shipmentJsonObject) => {
   Actions.OpenShipment(shipmentJsonObject)
 });
 
-Given("The user in the general work space", () => {
+Given("The user in the general tab", () => {
     cy.Click(Selectors.GeneralTab, null)
 });
 
 When("Fill general tab and click save button", () => {
     Actions.FillGeneralTab()
-    cy.Click(Selectors.ShipmentSaveButton, null)
+    Actions.UpdateShipment(Selectors.ShipmentSaveButton)
 });
 
-Given("The user in the Orders work space", () => {
+Given("The user in the Orders tab", () => {
     cy.Click(Selectors.OrdersTab, null)
 });
 
 When("Fill Orders tab and click save button", () => {
     Actions.FillOrdersTab()
-    cy.Click(Selectors.ShipmentSaveButton, null)
+    Actions.UpdateShipment(Selectors.ShipmentSaveButton)
 });
 
-Given("The user in the Partners work space", () => {
+Given("The user in the Partners tab", () => {
     cy.Click(Selectors.PartnersTab, null)
 });
 
 When("Fill Partners tab and click save button", () => {
     Actions.FillPartnersTab("E","A")
-    cy.Click(Selectors.ShipmentSaveButton, null)
+    Actions.UpdateShipment(Selectors.ShipmentSaveButton)
 });
 
-Given("The user in the Packages work space", () => {
+Given("The user in the Packages tab", () => {
     cy.Click(Selectors.PackagesTab, null)
 });
 
 When("Fill Packages tab and click save button", () => {
     Actions.FillPackagesTab("A")
-    cy.Click(Selectors.ShipmentSaveButton, null)
+    Actions.UpdateShipment(Selectors.ShipmentSaveButton)
 });
 
-Given("The user in the Receivables work space", () => {
+Given("The user in the Receivables tab", () => {
     cy.Click(Selectors.ReceivablesTab, null)
 });
 
 When("Fill Receivables tab and click save button", () => {
     Actions.FillReceivablesTab()
-    cy.Click(Selectors.ShipmentSaveButton, null)
+    Actions.UpdateShipment(Selectors.ShipmentSaveButton)
+});
+
+Given("The user in the Routings tab", () => {
+    cy.Click(Selectors.RoutingsTab, null)
+});
+
+When("Add new pickup routing and click save button", () => {
+    Actions.FillPickupRouting()
+    Actions.UpdateShipment(Selectors.SaveClose)
+});
+
+When("Add new delivery routing and click save button", () => {
+    Actions.FillDeliveryRouting()
+    Actions.UpdateShipment(Selectors.SaveClose)
+});
+
+When("Add carriage routings and click save button", () => {
+    Actions.FillPreCarriageRouting("A")
+    Actions.FillOnCarriageRouting("A")
+    Actions.UpdateShipment(Selectors.ShipmentSaveButton)
+});
+
+Given("The user in the Payables tab", () => {
+    cy.Click(Selectors.PayablesTab, null)
+});
+
+When("Add Payables and click save button", () => {
+    Actions.FillPayablesTab()
+    Actions.UpdateShipment(Selectors.ShipmentSaveButton)
 });
 
 Then("The save operation complete successfully", () => {
-    Assertions.SaveOperationCompletedSuccessfully();
+    Assertions.ValidateUpdatedShipment(null);
 });
