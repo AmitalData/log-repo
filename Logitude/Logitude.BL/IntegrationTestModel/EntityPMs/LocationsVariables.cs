a580852ea8609fe5637bcf386c1bc709762fc9af
logitudo <logitudo@live.com>
2021-01-20 12:40:02 +0200

    API test (Done locally)

--- Logitude/Logitude.BL/IntegrationTestModel/EntityPMs/LocationsVariables.cs
+++ Logitude/Logitude.BL/IntegrationTestModel/EntityPMs/LocationsVariables.cs
@@ -14,13 +14,8 @@ namespace Logitude.BL.IntegrationTestModel.EntityPMs
         public string PortMANId { get; set; }
         public string GlobalZoneEUId { get; set; }
         public string StateAKId { get; set; }
-        public string AirlineAAId { get; set; }
-        public string AirlineBAId { get; set; }
         public string CountryUSId { get; set; }
-        public string ShippingLineMSCUId { get; set; }
-        public string ShippingLineMAEUId { get; set; }
-        public string MoveTypeMTAId { get; set; }
-        public string MoveTypeMTOId { get; set; }
-        public string WarehouseId { get; set; }
+
+  
     }
 }
--- Logitude/Logitude.BL/IntegrationTestModel/EntityQueries/PartnersPreparationVariables.cs
+++ Logitude/Logitude.BL/IntegrationTestModel/EntityQueries/PartnersPreparationVariables.cs
@@ -4,6 +4,9 @@ using Logitude.BL.IntegrationTestModel.EntityPMs;
 using Simplog.Data.CommonDataModel;
 using Simplog.Data.CommonDataModel.EntityPOCOs;
 using Simplog.Data.CommonDataModel.Repositories;
+using Simplog.Data.InfrastructureModel;
+using Logitude.BL.CommonDataModel.EntityQueries;
+using Logitude.BL.CommonDataModel.EntityLists;
 
 namespace Logitude.BL.IntegrationTestModel.EntityQueries
 {
@@ -11,14 +14,14 @@ namespace Logitude.BL.IntegrationTestModel.EntityQueries
     {
         PartnersVariables Variables = new PartnersVariables();
         ICommonDataContext commonDataContext;
-
+        IWebFreightContext webFreightContext;
         int tenant;
 
         public PartnersPreparationVariables(int tenant)
         {
             this.tenant = tenant;
             commonDataContext = CommonDataContext.GetContext(tenant);
-
+            webFreightContext = WebFreightContext.GetContext(tenant);
         }
 
         public PartnersVariables GetBasePartnersVaribles()
@@ -30,6 +33,11 @@ namespace Logitude.BL.IntegrationTestModel.EntityQueries
             Variables.CustomAgentId = GetCustomsAgent("InegrationCustomsAgent");
             Variables.ShippingAgentId = GetShippingAgent("IntegrationShippingAgent");
             Variables.CustomerId = GetCustomer("IntegrationCustomer");
+            Variables.AirlineAAId = GetAirline("AA");
+            Variables.AirlineBAId = GetAirline("BA");
+            Variables.ShippingLineMSCUId = GetShippingLine("MSCU");
+            Variables.ShippingLineMAEUId = GetShippingLine("MAEU");   
+            Variables.WarehouseId = GetWarehouse("IntegrationWarehouse", "WR9");
 
             return Variables;
         }
@@ -188,6 +196,93 @@ namespace Logitude.BL.IntegrationTestModel.EntityQueries
             shippingAgentPM.PartnerTypeId = "SG";
             shippingAgentPM.CountryId = Variables.CountryUSId;
             return shippingAgentPM;
+
+        }
+        private string GetAirline(string code)
+        {
+            string airlineId = "";
+            AirlineRepository airlineRepository = new AirlineRepository(commonDataContext);
+            Airline airline = airlineRepository.GetSingleAirlineByCode(code, tenant);
+            if (airline == null)
+            {
+                airlineId = CopyAirlineFromTenantZero(code, airlineRepository);
+            }
+            else
+            {
+                airlineId = airline.Id;
+            }
+            return airlineId;
+        }
+
+        private string CopyAirlineFromTenantZero(string code, AirlineRepository airlineRepository)
+        {
+            Airline tenantZeroAirline = airlineRepository.GetSingleAirlineByCode(code, 0);
+            string copiedAirlineId = "";
+            if (tenantZeroAirline != null)
+            {
+                CardQuery cardQuery = new CardQuery(tenant);
+                CardList copiedAirline = cardQuery.GetCarrierCopyToCurrentTenant(tenantZeroAirline.Id, tenant, null, null, false, null);
+                copiedAirlineId = copiedAirline != null ? copiedAirline.Id : "";
+            }
+            return copiedAirlineId;
+        }
+
+        private string GetShippingLine(string code)
+        {
+            string shippingLineId = "";
+            ShippingLineRepository shippingLineRepository = new ShippingLineRepository(commonDataContext);
+            ShippingLine shippingLine = shippingLineRepository.GetSingleShippingLineByCode(code, tenant);
+            if (shippingLine == null)
+            {
+                shippingLineId = CopyShippingLineFromTenantZero(code, shippingLineRepository);
+            }
+            else
+            {
+                shippingLineId = shippingLine.Id;
+            }
+            return shippingLineId;
+        }
+
+        private string CopyShippingLineFromTenantZero(string code, ShippingLineRepository shippingLineRepository)
+        {
+            ShippingLine tenantZeroShippingLine = shippingLineRepository.GetSingleShippingLineByCode(code, 0);
+            string copiedShippingLineId = "";
+            if (tenantZeroShippingLine != null)
+            {
+                CardQuery cardQuery = new CardQuery(tenant);
+                CardList copiedAirline = cardQuery.GetCarrierCopyToCurrentTenant(tenantZeroShippingLine.Id, tenant, null, null, false, null);
+                copiedShippingLineId = copiedAirline != null ? copiedAirline.Id : "";
+            }
+            return copiedShippingLineId;
+        }
+
+        private string GetWarehouse(string warehouseName, string code)
+        {
+            WarehouseRepository warehouseRepository = new WarehouseRepository(commonDataContext);
+            Warehouse warehouse = warehouseRepository.GetFirstSingleByCode(code, tenant);
+            if (warehouse == null)
+            {
+                InsertNewWarehouse(warehouseName, code);
+                warehouse = warehouseRepository.GetFirstSingleByCode(code, tenant);
+            }
+            return warehouse.Id;
+        }
+        private void InsertNewWarehouse(string warehouseName, string code)
+        {
+            WarehouseService warehouseService = new WarehouseService(commonDataContext, tenant);
+            warehouseService.Create(CreateWarehousePM(warehouseName, code));
+        }
+
+        public WarehousePM CreateWarehousePM(string warehouseName, string code)
+        {
+            WarehousePM WarehousePM = new WarehousePM();
+            WarehousePM.Tenant = tenant;
+            WarehousePM.EnglishName = warehouseName;
+            WarehousePM.CityName = "AKD";
+            WarehousePM.PartnerTypeId = "WH";
+            WarehousePM.CountryId = Variables.CountryUSId;
+            WarehousePM.Code = code;
+            return WarehousePM;
 
         }
     }
