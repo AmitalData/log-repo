addeec4e16b8e88cddb9ada347b7b4d9b108e71b
Mohammad Masoud <Mohammad@logitudeworld.com>
2021-01-24 10:35:32 +0200

    #132494

--- Logitude/AngularModules/AngularModules/Controls/LogitudeHotkeysComponent/LogitudeHotKeysComponent.ts
+++ Logitude/AngularModules/AngularModules/Controls/LogitudeHotkeysComponent/LogitudeHotKeysComponent.ts
@@ -105,7 +105,7 @@ export class LogitudeHotKeysComponent{
     }
 
     DestroyLogitudeHotKeysControl(){
-        this.CurrentSession.RemoveLogitudeHotKeysComponent(this);
+        this.CurrentSession?.RemoveLogitudeHotKeysComponent(this);
         this.CurrentSession=null;
         this.CTRL_S_HotKey=null;
         this.CTRL_Shift_S_HotKey=null;
