48c33087f083e4fdb9d43afa87481d97b26f47ab
moran <moran@amital.co.il>
2021-01-24 17:38:26 +0200

    132051 ms
    Related Work Items: #132051

--- Logitude/Logitude.Customs.BL/EntityUpdateServices/DeclarationUpdateService.amital.cs
+++ Logitude/Logitude.Customs.BL/EntityUpdateServices/DeclarationUpdateService.amital.cs
@@ -1583,6 +1583,10 @@ namespace Logitude.Customs.BL.EntityUpdateServices
 
         public bool CheckFileStatus(DeclarationPM dirtyDeclarationPM, string loggingUserId, string status)
         {
+            if (dirtyDeclarationPM == null || String.IsNullOrWhiteSpace(dirtyDeclarationPM.CustomFileNo) || String.IsNullOrWhiteSpace(status))
+            {
+                throw new Exception("data is missing");
+            }
             var amitalCustomFileCommunicationModel = new Logitude.Customs.BL.Messaging.Amital.AmitalCommunicationModelBase(
                Logitude.Server.Tools.Models.AmitalStandardCommunicationModel.OperationMethod.DataAccess,
                "CWSFLOGIFILE", "DeclarationCheckFileStatus")
@@ -1646,9 +1650,12 @@ namespace Logitude.Customs.BL.EntityUpdateServices
 
         }
 
-
         public bool CheckFileEvent(DeclarationPM dirtyDeclarationPM, string loggingUserId, string CFIEvent)
         {
+            if(dirtyDeclarationPM == null || String.IsNullOrWhiteSpace(dirtyDeclarationPM.CustomFileNo) || String.IsNullOrWhiteSpace(CFIEvent))
+            {
+                throw new Exception("data is missing");
+            }
             var amitalCustomFileCommunicationModel = new Logitude.Customs.BL.Messaging.Amital.AmitalCommunicationModelBase(
                Logitude.Server.Tools.Models.AmitalStandardCommunicationModel.OperationMethod.DataAccess,
                "CWSFLOGIFILE", "DeclarationCheckFileEvent")
@@ -1666,6 +1673,75 @@ namespace Logitude.Customs.BL.EntityUpdateServices
             myLOGIGENREQ.LogitudeGeneralRequest = new LogitudeGeneralRequest[] { new LogitudeGeneralRequest() };
             myLOGIGENREQ.LogitudeGeneralRequest[0].Code = CFIEvent;
             myLOGIGENREQ.LogitudeGeneralRequest[0].Param1 = dirtyDeclarationPM.CustomFileNo;
+            bool myImmediately = true;
+            var myUServerCommunicationService = new Logitude.Customs.BL.Messaging.Amital.UServerCommunicationService
+                <Logitude.Customs.BL.Messaging.Amital.AmitalCommunicationModelBase, LOGIGENREQ>(
+                amitalCustomFileCommunicationModel, myLOGIGENREQ);
+            var info = myUServerCommunicationService.Send(myImmediately);
+            if (String.IsNullOrWhiteSpace(info.ImmediatelyResponse))
+            {
+                throw new Exception("ImmediatelyResponse is null");
+            }
+            var GenericResponse = XmlGenericUtil<GenericResponse>.DeSerializeObject(info.ImmediatelyResponse);
+            var genericResponseObj = GenericResponse.GenericResponseObj.FirstOrDefault();
+            if (genericResponseObj == null)
+            {
+                throw new Exception("GenericResponse.GenericResponseObj is null");
+            }
+
+            if (!String.IsNullOrWhiteSpace(genericResponseObj.Status))
+            {
+                int sts;
+                int.TryParse(genericResponseObj.Status, out sts);
+                if (sts < 0)
+                {
+                    string mess = "Failed To check File Event in Unifreight";
+                    if (!String.IsNullOrWhiteSpace(genericResponseObj.ErrorDescription))
+                    {
+                        mess = mess + Environment.NewLine + genericResponseObj.ErrorDescription;
+                    }
+                    if (!String.IsNullOrWhiteSpace(genericResponseObj.Message))
+                    {
+                        mess = mess + Environment.NewLine + genericResponseObj.Message;
+                    }
+                    LogMessagingUtil.Instance.AppendLine("CheckFileEvent>genericResponseObj>Message= " + mess);
+                    throw new Exception(mess);
+                }
+            }
+
+            if (!String.IsNullOrWhiteSpace(genericResponseObj.Message))
+            {
+                LogMessagingUtil.Instance.AppendLine("CheckFileEvent>genericResponseObj>Message= " + genericResponseObj.Message);
+            }
+            LogMessagingUtil.Instance.AppendLine("CheckFileEvent>genericResponseObj>Status= " + genericResponseObj.Status);
+
+            return (genericResponseObj.Status == "1");
+
+        }
+
+        public bool CheckLeadingFileEvent(CourierMasterPM dirtyCourierMasterPM, string loggingUserId, string CFIEvent)
+        {
+            if (dirtyCourierMasterPM == null || String.IsNullOrWhiteSpace(dirtyCourierMasterPM.UnifreightLeadingFile) || String.IsNullOrWhiteSpace(CFIEvent))
+            {
+                throw new Exception("data is missing");
+            }
+            var amitalCustomFileCommunicationModel = new Logitude.Customs.BL.Messaging.Amital.AmitalCommunicationModelBase(
+               Logitude.Server.Tools.Models.AmitalStandardCommunicationModel.OperationMethod.DataAccess,
+               "CWSFLOGIFILE", "DeclarationCheckFileEvent")
+            {
+                Tenant = dirtyCourierMasterPM.Tenant,
+                objectTableName = "Customs.CourierMaster",
+                CommunicationLoggingEntityReference = dirtyCourierMasterPM.MAWB,
+                EntityId = dirtyCourierMasterPM.Id,
+                UserId = loggingUserId,
+                CommunicationSubject = "Logitude Master Courier check File Event",
+
+            };
+
+            var myLOGIGENREQ = new LOGIGENREQ();
+            myLOGIGENREQ.LogitudeGeneralRequest = new LogitudeGeneralRequest[] { new LogitudeGeneralRequest() };
+            myLOGIGENREQ.LogitudeGeneralRequest[0].Code = CFIEvent;
+            myLOGIGENREQ.LogitudeGeneralRequest[0].Param1 = dirtyCourierMasterPM.UnifreightLeadingFile;
             bool myImmediately = true;
             var myUServerCommunicationService = new Logitude.Customs.BL.Messaging.Amital.UServerCommunicationService
                 <Logitude.Customs.BL.Messaging.Amital.AmitalCommunicationModelBase, LOGIGENREQ>(
