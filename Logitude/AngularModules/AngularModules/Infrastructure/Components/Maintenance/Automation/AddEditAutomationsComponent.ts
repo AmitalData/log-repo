dc4c300bcd67bf0df73113ff76d7d66e900984f4
Heba Mubarak <heba@logitudeworld.com>
2021-01-24 11:57:38 +0200

    Edit validation

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/Maintenance/Automation/AddEditAutomationsComponent.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/Maintenance/Automation/AddEditAutomationsComponent.ts
@@ -1651,14 +1651,14 @@ export class AddEditAutomationsComponent extends BaseComponent implements OnInit
 
         if (this.AutomationCondationAndList.length > 0) {
             this.AutomationCondationAndList.forEach((item) => {
-                if (item.CurrentEntityPM.Value == "" || item.ObjectFieldCode == "") {
+                if (item.CurrentEntityPM.Value == "") {
                     this.ValidationErrorsList.push("Field value is required");
                 }
             })
         }
         if (this.AutomationCondationOrList.length > 0) {
             this.AutomationCondationOrList.forEach((item) => {
-                if (item.CurrentEntityPM.Value == "" || item.ObjectFieldCode == "") {
+                if (item.CurrentEntityPM.Value == "") {
                     this.ValidationErrorsList.push("Field value is required");
                 }
             })
