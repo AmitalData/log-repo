1187a82ab7d2e69e6afddb216ef6f5121be9b881
Fida'a Zuhour <fida@logitudeworld.com>
2021-01-25 17:30:47 +0200

    #132991 External Reco. screen

--- Logitude/AngularModules/AngularModules/Accounting/Components/Others/ReconcileExternalPage/ExternalPagesTabComponent.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/Others/ReconcileExternalPage/ExternalPagesTabComponent.ts
@@ -650,8 +650,8 @@ export class ExternalPagesTabComponent extends BaseComponent implements OnInit, 
         logitudeWindow.Width = 900;
         logitudeWindow.Height = 800;
         logitudeWindow.IsFullScreen = true;
+        var BankName = this.getBankName();
 
-        var BankName = this.DontShowLocal ? this.EntityPM.EnglishName : this.EntityPM.LocalName == null ? this.EntityPM.EnglishName : this.EntityPM.LocalName;
         logitudeWindow.Title = TextCodeTranslator.Translate("Accounting.General.O.ExternalReconcile") + ' - ' + BankName;
 
         logitudeWindow.IsFullScreen = true;
@@ -662,6 +662,13 @@ export class ExternalPagesTabComponent extends BaseComponent implements OnInit, 
             // show alert
             this.CurrentSession.CurrentEditComponent.ReloadEntityPM();
         });
+    }
+
+    getBankName(): string{
+        var isBankNameNotExists = this.EntityPM.EnglishName == null && this.EntityPM.LocalName == null;
+        var BankCode = this.EntityPM.BankCode;
+        var BankName = this.DontShowLocal ? this.EntityPM.EnglishName : this.EntityPM.LocalName == null ? this.EntityPM.EnglishName : this.EntityPM.LocalName;
+        return isBankNameNotExists ? BankCode : BankName+"";
     }
 
 }
