706ba06c3e981071e0eaa0238e98640e99f55f87
Samar Abu Eid <Samar@logitudeworld.com>
2021-01-25 12:52:35 +0200

    131969
    Related Work Items: #131969

--- Logitude/AngularModules/AngularModules/TariffModule/Components/EditTabs/Tariff/UploadedExcelsComponent.ts
+++ Logitude/AngularModules/AngularModules/TariffModule/Components/EditTabs/Tariff/UploadedExcelsComponent.ts
@@ -63,7 +63,7 @@ export class ExcelFileItem {
     get FileName() { return this.EntityPM.FileName; }
     
     DownloadClicked() {
-        DownloadManager.DownloadPage(this.EntityPM.DocumentId);
+        DownloadManager.DownloadPage(this.EntityPM.DocumentId + "*" + this.FileName);
     }
 }
 
