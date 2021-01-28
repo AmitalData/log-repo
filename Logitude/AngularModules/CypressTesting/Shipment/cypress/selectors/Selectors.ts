d364fb9d54d179ab7d66a2f0c349986c90d0340e
logitudo <logitudo@live.com>
2021-01-28 13:37:45 +0200

    132484

--- Logitude/AngularModules/CypressTesting/Shipment/cypress/selectors/Selectors.ts
+++ Logitude/AngularModules/CypressTesting/Shipment/cypress/selectors/Selectors.ts
@@ -4,6 +4,7 @@ export class Selectors
     public static readonly GeneralTab = '#ShipmentTHGeneral';
     public static readonly OrdersTab = '#ShipmentTHOrders';
     public static readonly PartnersTab = '#ShipmentTHPartners';
+    public static readonly ShipmentsTab = '#ShipmentTHConsolidation';
     public static readonly PackagesTab = '#ShipmentTHPackages';
     public static readonly ReceivablesTab = '#ShipmentTHReceivables';
     public static readonly RoutingsTab = '#ShipmentTHRoutings';
@@ -49,4 +50,7 @@ export class Selectors
     public static readonly ShipmentPayable_ChargesTypeId = '#ShipmentPayable_ChargesTypeId';
     public static readonly ShipmentPayable_MeasurementId = '#ShipmentPayable_MeasurementId';
     public static readonly ShipmentPayable_CurrencyId = '#ShipmentPayable_CurrencyId';
+    public static readonly NewAttachedHouse = '#NewHouseBtn';
+    
+
 }
