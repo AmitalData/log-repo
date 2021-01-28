ced5b180787c8b1b736211b529232cc3a8af263e
elisheva <elisheva@amital.co.il>
2021-01-28 12:34:43 +0200

    132405
    Related Work Items: #132405

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsControls/ModuleDeclarations.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsControls/ModuleDeclarations.ts
@@ -7,6 +7,7 @@ import { CustomMessageProgressComponent } from './Components/CustomMessageProgre
 import { NotificationComponent } from './Components/NotificationComponent';
 import { ObjectViewerComponent } from './Components/ObjectViewerComponent';
 import { SendDeclarationTastCaseComponent } from './Components/TestCase/SendDeclarationTastCaseComponent';
+import { RemarksPopUp } from './Components/RemarksPopUp';
 
 export const Components =
     [
@@ -36,6 +37,8 @@ export class ModuleDeclarations {
             case "NotificationComponent": { myResult = NotificationComponent; break; }
             case "ObjectViewerComponent": { myResult = ObjectViewerComponent; break; }
             case "SendDeclarationTastCaseComponent": { myResult = SendDeclarationTastCaseComponent; break; }
+            case "RemarksPopUp": { myResult = RemarksPopUp; break; }
+
         }
 
         return myResult;
