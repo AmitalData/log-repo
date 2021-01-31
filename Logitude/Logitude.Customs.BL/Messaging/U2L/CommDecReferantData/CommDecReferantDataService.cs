997f36cbef6efc5c1f220b5f5ac2ab00d3f92a8b
moran <moran@amital.co.il>
2021-01-31 15:46:07 +0200

    133412 ms
    Related Work Items: #133412

--- Logitude/Logitude.Customs.BL/Messaging/U2L/CommDecReferantData/CommDecReferantDataService.cs
+++ Logitude/Logitude.Customs.BL/Messaging/U2L/CommDecReferantData/CommDecReferantDataService.cs
@@ -150,7 +150,17 @@ namespace Logitude.Customs.BL.Messaging.U2L.CommDecReferantData
                 }
                 if (!string.IsNullOrWhiteSpace(_LogitudeDeclarationReferantData.FollowUpStatus)) _DeclarationReferantDataPM.IsClosedForFollowUp = _LogitudeDeclarationReferantData.FollowUpStatus;
 
-                if (!string.IsNullOrWhiteSpace(_LogitudeDeclarationReferantData.PreClassification)) _DeclarationReferantDataPM.PreClassification = _LogitudeDeclarationReferantData.PreClassification;
+                if (!string.IsNullOrWhiteSpace(_LogitudeDeclarationReferantData.PreClassification))
+                {
+                    if(_LogitudeDeclarationReferantData.PreClassification == "D")
+                    {
+                        _DeclarationReferantDataPM.PreClassification = null;
+                    }
+                    else
+                    {
+                        _DeclarationReferantDataPM.PreClassification = _LogitudeDeclarationReferantData.PreClassification;
+                    }
+                }
                 if (_DeclarationReferantDataPM.Tenant < 1) _DeclarationReferantDataPM.Tenant = ResolvedTenant();
                 if (!string.IsNullOrWhiteSpace(_LogitudeDeclarationReferantData.ClassifiedUserId)) _DeclarationReferantDataPM.ClassifiedUserId = TranslateUser(_LogitudeDeclarationReferantData.ClassifiedUserId);
                 if (!string.IsNullOrWhiteSpace(_LogitudeDeclarationReferantData.ControllerUserId)) _DeclarationReferantDataPM.ControllerUserId = TranslateUser(_LogitudeDeclarationReferantData.ControllerUserId);
