cf6ee3955a530369a8ce90f58ba5da314ca06d41
logitudo <logitudo@live.com>
2021-02-02 15:34:33 +0200

    133038 - Ports

--- Logitude/Logitude.Test.Base/Models/Urls.cs
+++ Logitude/Logitude.Test.Base/Models/Urls.cs
@@ -14,6 +14,8 @@
         public static string APPaymentViewsGetByFilters = "APPaymentViews/GetByFilters";
         public static string ARInvoiceViewsGetByFilters = "ARInvoiceViews/GetByFilters";
         public static string ARPaymentViewsGetByFilters = "ARPaymentViews/GetByFilters";
+        public static string PortViewsGetbyfilters = "PortViews/getbyfilters";
+        public static string PortViewsGetTenantImportByFilters = "PortViews/getTenantImportByFilters";
         public static string IntegrationTestGetBasePartners = "IntegrationTest/GetBasePartners";
         public static string IntegrationTestGetBaseLocations = "IntegrationTest/GetBaseLocations";
         public static string IntegrationTestGetBaseShipment = "IntegrationTest/GetBaseShipment";
@@ -64,5 +66,9 @@
             return "ARPayments/GetSingle?id=" + id;
         }
 
+        public static string CommonDomainGetPortCopyToCurrentTenant(string portId)
+        {
+            return "CommonDomain/GetPortCopyToCurrentTenant?entityId" + portId;
+        }
     }
 }
---
+++ Logitude/Logitude.Test.Base/Services/DataPreparation.cs
@@ -0,0 +1,71 @@
+﻿using Logitude.Test.Base.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logitude.Test.Base.Services
+{
+    public class DataPreparation
+    {
+        public static LocationsVariables GetLocationsVariables()
+        {
+            return new LocationsVariables {
+                PortLHRId = GetPortId("LHR", null),
+            };
+        }
+        public static PartnersVariables GetPartnersVariables()
+        {
+            return null;
+        }
+
+        #region Locations Preparation Variables
+
+        #region Ports
+        private static string GetPortId(string code, string countryCode)
+        {
+            ApiQueryFilters apiQueryFilters = new ApiQueryFilters
+            {
+                PageIndex = 0,
+                PageSize = 1,
+                Filter1Name = "Code",
+                Filter1Operator = "equals",
+                Filter1Value = code,
+                Filter2Name = "CountryCode",
+                Filter2Operator = "contains",
+                Filter2Value = countryCode
+            };
+
+            string UserTenantPortId = GetPortIdFromUserTenant(apiQueryFilters);
+            if (string.IsNullOrEmpty(UserTenantPortId))
+            {
+                string ZeroTenantPortId = GetPortIdFromZeroTenant(apiQueryFilters);
+                UserTenantPortId = GetCopiedPortFromTenantZero(ZeroTenantPortId);
+            }
+            
+            return UserTenantPortId;
+        }
+
+        private static string GetPortIdFromUserTenant(ApiQueryFilters apiQueryFilters)
+        {
+            ApiResponse<IEnumerable<Port>> response = APICaller.CallGetByFilters<IEnumerable<Port>>(Urls.PortViewsGetbyfilters, UserTenant.Token, apiQueryFilters);
+            return response.Data?.FirstOrDefault().Id;
+        }
+        private static string GetPortIdFromZeroTenant(ApiQueryFilters apiQueryFilters)
+        {
+            apiQueryFilters.Tenant = 0;
+            ApiResponse<IEnumerable<Port>> response = APICaller.CallGetByFilters<IEnumerable<Port>>(Urls.PortViewsGetTenantImportByFilters, UserTenant.Token, apiQueryFilters);
+            return response.Data?.FirstOrDefault().Id;
+        }
+
+        private static string GetCopiedPortFromTenantZero(string portId)
+        {
+            ApiResponse<Port> response = APICaller.CallGet<Port>(Urls.CommonDomainGetPortCopyToCurrentTenant(portId), UserTenant.Token);
+            return response.Data?.Id;
+        }
+        #endregion
+
+        #endregion
+    }
+}
