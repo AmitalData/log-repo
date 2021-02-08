fa0b875e25ca04f295f33a03cebf6f86312a3d52
Abd Al-Rahman Malakh <Abd@logitudeworld.com>
2021-02-08 08:58:24 +0200

    133815

--- Logitude/AngularModules/CypressTesting/Shipment/cypress/tests/CreateShipment/CreateShipmentSteps.ts
+++ Logitude/AngularModules/CypressTesting/Shipment/cypress/tests/CreateShipment/CreateShipmentSteps.ts
@@ -8,23 +8,55 @@ import * as BaseAssertion from "../../../../Base/cypress/actions/Assertion"
 let ShipmentData: ShipmentDetails;
 
 Given("the user logged in and navigates to shipments workspace", () => {
-  cy.Login()
-  cy.Click(BaseSelectors.OperationsMenu, null)
-  cy.Click(Selectors.ShipmentTab, null)
+  // cy.Login()
+  // cy.Click(BaseSelectors.OperationsMenu, null)
+  // cy.Click(Selectors.ShipmentTab, null)
 });
 
 Given("a direct shipment with the following details",
   (dataTable) => {
-   let shipmentDetails = dataTable.hashes()[0] as ShipmentDetails;
-   ShipmentData = shipmentDetails;
-   Actions.OpenNewShipmentWizard(ShipmentData.ShipmentLevel);
-   Actions.FillShipmentWizardsFields(ShipmentData);
+  //  let shipmentDetails = dataTable.hashes()[0] as ShipmentDetails;
+  //  ShipmentData = shipmentDetails;
+  //  Actions.OpenNewShipmentWizard(ShipmentData.ShipmentLevel);
+  //  Actions.FillShipmentWizardsFields(ShipmentData);
+
+   //let shipmentDetails = CreateInstance<ShipmentDetails>(dataTable, true);
+   //cy.log(shipmentDetails.Shipper);
+
+   let listOfShipmentDetails = CreateSet<ShipmentDetails>(dataTable);
+   listOfShipmentDetails.forEach(element => {
+     cy.log(element.ShipmentLevel + " - " + element.Direction + " - " + element.TransportMode);
+   });
 });
 
 When("create shipment", () => {
-  Actions.CreateShipment(ShipmentData.ShipmentLevel);
+  //Actions.CreateShipment(ShipmentData.ShipmentLevel);
 });
 
 Then("the shipment should create successfully", () => {
-  BaseAssertion.AssertStatusCode("WaitPostShipmentRequest", 200);
+  //BaseAssertion.AssertStatusCode("WaitPostShipmentRequest", 200);
 });
+
+
+
+
+function CreateInstance<T>(dataTable: any, isVerticalTable?: boolean): T{
+  let dataTableModel: T;
+
+  if(!isVerticalTable){
+    dataTableModel = dataTable.hashes()[0] as T;
+  }else{
+    let tempDataTableModel = {};
+    dataTable.raw().forEach((raw: any[]) => {
+      tempDataTableModel[raw[0]] = raw[1];
+    });
+    dataTableModel = tempDataTableModel as T;
+  }
+
+  return dataTableModel;
+}
+
+
+function CreateSet<T>(dataTable: any): T[]{
+  return dataTable.hashes() as T[];
+}
