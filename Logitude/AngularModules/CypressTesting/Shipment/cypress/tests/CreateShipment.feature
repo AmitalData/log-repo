c6909470ba00ac3f725360ca8a4d1b02af6c2e9d
Abd Al-Rahman Malakh <Abd@logitudeworld.com>
2021-02-08 14:13:18 +0200

    134034
    Related Work Items: #134034

--- Logitude/AngularModules/CypressTesting/Shipment/cypress/tests/CreateShipment.feature
+++ Logitude/AngularModules/CypressTesting/Shipment/cypress/tests/CreateShipment.feature
@@ -1,23 +1,9 @@
 Feature: Create Shipment Tests
 
   Scenario: Create Direct Export Air Shipment
-    Given the user logged in and navigates to shipments workspace
+    Given the user logged in and navigates to shipments workspace 
     And a direct shipment with the following details
       | ShipmentLevel | Direction | TransportMode | Shipper  | MainCarriageFromPort | MainCarriageToPort |
       | Direct        | Export    | Air           | Shipper1 | LHR                  | MIA                |
-      | Direct        | Export    | Air           | Shipper1 | LHR                  | MIA                |
     When create shipment
     Then the shipment should create successfully
-
-
-
-
-
-
-
-# | ShipmentLevel        | Direct   |
-# | Direction            | Export   |
-# | TransportMode        | Air      |
-# | Shipper              | Shipper1 |
-# | MainCarriageFromPort | LHR      |
-# | MainCarriageToPort   | MIA      |
--- Logitude/AngularModules/CypressTesting/Shipment/cypress/tests/CreateShipment/CreateShipmentSteps.ts
+++ Logitude/AngularModules/CypressTesting/Shipment/cypress/tests/CreateShipment/CreateShipmentSteps.ts
@@ -3,60 +3,30 @@ import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
 import { ShipmentDetails } from "../../models/ShipmentDetails";
 import { BaseSelectors } from "../../../../Base/cypress/selectors/BaseSelectors"
 import { Selectors } from "../../selectors/Selectors";
-import * as BaseAssertion from "../../../../Base/cypress/actions/Assertion"
+import * as BaseAssertion from "../../../../Base/cypress/actions/Assertion";
+import { Assists } from "../../../../Base/cypress/assists/Assists";
 
 let ShipmentData: ShipmentDetails;
 
 Given("the user logged in and navigates to shipments workspace", () => {
-  // cy.Login()
-  // cy.Click(BaseSelectors.OperationsMenu, null)
-  // cy.Click(Selectors.ShipmentTab, null)
+  cy.Login()
+  cy.Click(BaseSelectors.OperationsMenu, null)
+  cy.Click(Selectors.ShipmentTab, null)
 });
 
 Given("a direct shipment with the following details",
   (dataTable) => {
-  //  let shipmentDetails = dataTable.hashes()[0] as ShipmentDetails;
-  //  ShipmentData = shipmentDetails;
-  //  Actions.OpenNewShipmentWizard(ShipmentData.ShipmentLevel);
-  //  Actions.FillShipmentWizardsFields(ShipmentData);
-
-   //let shipmentDetails = CreateInstance<ShipmentDetails>(dataTable, true);
-   //cy.log(shipmentDetails.Shipper);
-
-   let listOfShipmentDetails = CreateSet<ShipmentDetails>(dataTable);
-   listOfShipmentDetails.forEach(element => {
-     cy.log(element.ShipmentLevel + " - " + element.Direction + " - " + element.TransportMode);
-   });
+   //let shipmentDetails = dataTable.hashes()[0] as ShipmentDetails;
+   let shipmentDetails = Assists.CreateInstance<ShipmentDetails>(dataTable);
+   ShipmentData = shipmentDetails;
+   Actions.OpenNewShipmentWizard(ShipmentData.ShipmentLevel);
+   Actions.FillShipmentWizardsFields(ShipmentData);
 });
 
 When("create shipment", () => {
-  //Actions.CreateShipment(ShipmentData.ShipmentLevel);
+  Actions.CreateShipment(ShipmentData.ShipmentLevel);
 });
 
 Then("the shipment should create successfully", () => {
-  //BaseAssertion.AssertStatusCode("WaitPostShipmentRequest", 200);
+  BaseAssertion.AssertStatusCode("WaitPostShipmentRequest", 200);
 });
-
-
-
-
-function CreateInstance<T>(dataTable: any, isVerticalTable?: boolean): T{
-  let dataTableModel: T;
-
-  if(!isVerticalTable){
-    dataTableModel = dataTable.hashes()[0] as T;
-  }else{
-    let tempDataTableModel = {};
-    dataTable.raw().forEach((raw: any[]) => {
-      tempDataTableModel[raw[0]] = raw[1];
-    });
-    dataTableModel = tempDataTableModel as T;
-  }
-
-  return dataTableModel;
-}
-
-
-function CreateSet<T>(dataTable: any): T[]{
-  return dataTable.hashes() as T[];
-}
