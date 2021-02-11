591d8c98993b855cf643cb366bfa1dff6ab9d4f5
logitudo <logitudo@live.com>
2021-02-11 13:03:27 +0200

    134019 + 134020
    Related Work Items: #134019, #134020

---
+++ Logitude/AngularModules/CypressTesting/Ticket/cypress/selectors/RegexSelectors.ts
@@ -0,0 +1,7 @@
+export class RegexSelectors {
+   
+   public static readonly Reply= "#Reply";
+   public static readonly InternalNote= "#AddInternalNote";
+   public static readonly SendAsOpenButton= "Button[id^='SendButtom_']";
+
+}
