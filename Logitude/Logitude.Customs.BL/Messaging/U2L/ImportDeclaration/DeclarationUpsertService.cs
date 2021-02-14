6d5d35bff3b2914cc2a7af3b081a15f97fdefb73
moran <moran@amital.co.il>
2021-02-14 14:17:40 +0200

    132882
    Related Work Items: #132882

--- Logitude/Logitude.Customs.BL/Messaging/U2L/ImportDeclaration/DeclarationUpsertService.cs
+++ Logitude/Logitude.Customs.BL/Messaging/U2L/ImportDeclaration/DeclarationUpsertService.cs
@@ -938,6 +938,7 @@ namespace Logitude.Customs.BL.Messaging.U2L.ImportDeclaration
             this._DeclarationReferantDataPM.Team = TranslateTeam(_AmitalCustomsFile.Team);
 
             this._DeclarationReferantDataPM.FileOpenDate = AmitalConvertUtil.GetUnifreightFormatedDate(_AmitalCustomsFile.FileOpenDate, "AmitalCustomsFile.FileOpenDate");
+            this._DeclarationReferantDataPM.FclLcl = _AmitalCustomsFile.FclLcl;
 
             this._DeclarationReferantDataPM.Tenant = ResolvedTenant();
             myDeclarationReferantDataUpdateService.Update(this._DeclarationReferantDataPM, true);
