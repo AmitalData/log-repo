5ede61458efe23edfd0b77ffa102009a7f168def
logitudo <logitudo@live.com>
2021-02-16 17:01:56 +0200

    #134129 (Percent Of Value)

--- Logitude/AngularModules/AngularModules/TariffModule/Components/EditTabs/Tariff/SurchargeVersionTabComponent.ts
+++ Logitude/AngularModules/AngularModules/TariffModule/Components/EditTabs/Tariff/SurchargeVersionTabComponent.ts
@@ -338,7 +338,7 @@ export class SurchargeVersionTabComponent extends BaseComponent implements OnDes
                         isMeasurmentFixed = true;
                     }
 
-                    if (iMeasurement.Code == 'PRFR' ) { 
+                    if (iMeasurement.Code == 'PRFR' || iMeasurement.Code == 'PRVL') { 
                         this['Surcharge' + index + 'PricePercentageVisibility'] = true;
                     } else {
                         this['Surcharge' + index + 'PricePercentageVisibility'] = false;
