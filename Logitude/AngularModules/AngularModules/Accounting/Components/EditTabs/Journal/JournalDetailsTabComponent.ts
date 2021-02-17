8d4b46389d0c076a468c4ca71fb9ca46d676ebe6
Alaa Jamal <Alaa@logitudeworld.com>
2021-02-17 12:05:59 +0200

    #134584 #134585

--- Logitude/AngularModules/AngularModules/Accounting/Components/EditTabs/Journal/JournalDetailsTabComponent.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/EditTabs/Journal/JournalDetailsTabComponent.ts
@@ -905,8 +905,7 @@ class JournalLineModel extends BaseComponent {
             if (!AppTool.IsNullOrEmpty(value) && !AppTool.IsNullOrEmpty(this.parent.currency))
             {
                 if (value != SessionLocator.TenantPM.CurrencyId) {
-                    this.UIProperties.SetEnabled("ForeignAmount", this.ObjectTableName, true);
-                    if (this.parent.currency.Id != value) {
+                    this.UIProperties.SetEnabled("ForeignAmount", this.ObjectTableName, true);                  
                         this.ratesTableExtendedListService.getClosestRate(this.parent.defaultCurrencyId, value).subscribe((myResponse: ServiceResponse) => {
                             if (myResponse != null) {
                                 if (!myResponse.HasError) {
@@ -938,9 +937,7 @@ class JournalLineModel extends BaseComponent {
                                     }
                                 }
                             }
-                        });
-                    }
-                    else this.currencyRate = 1;
+                        });                                    
                 }
                 else
                 {
