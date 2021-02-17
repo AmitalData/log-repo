340f51d45f6b0c955106663472189554f52560ba
Leen Thawabi <leen@logitudeworld.com>
2021-02-17 11:45:58 +0200

    payable fixed

--- Logitude/AngularModules/CypressTesting/Shipment/cypress/actions/Actions.ts
+++ Logitude/AngularModules/CypressTesting/Shipment/cypress/actions/Actions.ts
@@ -262,6 +262,8 @@ export function FillPayablesTab(payableDetails: PayableDetails) {
     cy.FillLogTextBox(ShipmentSelectors.ShipmentPayableQuantity, payableDetails.Quantity.toString());
     cy.FillLogTextBox(ShipmentSelectors.ShipmentPayableUnitPrice, payableDetails.UnitPrice.toString());
     cy.FillLogLov(ShipmentSelectors.ShipmentPayableCurrency, payableDetails.Currency, true)
+    cy.FillLogTextBox(ShipmentSelectors.ShipmentPayableRate,payableDetails.ExchangeRate.toString());
+
     if (payableDetails.Vendor) {
         cy.FillLogLov(ShipmentSelectors.ShipmentPayableVendor, payableDetails.Vendor, true)
         cy.DefineRequestWait(RestAPI.GET, '**/cardviews/**', 'cardviews')
