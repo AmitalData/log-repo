4e0f99f4fbe89ab97a99758920fb0c1519712a43
moran <moran@amital.co.il>
2021-02-22 17:54:35 +0200

    132881
    Related Work Items: #132881

--- Logitude/Logitude.Customs.Data/EntityLists/Generated/DeclarationReferantDataList.Generated.cs
+++ Logitude/Logitude.Customs.Data/EntityLists/Generated/DeclarationReferantDataList.Generated.cs
@@ -150,6 +150,8 @@ namespace Logitude.Customs.Data.EntityLists
        public string ForwarderId  { get; set; }
        [DataMember]
        public string ForwarderName  { get; set; }
+       [DataMember]
+       public string FclLclName  { get; set; }
    }
 
 }
