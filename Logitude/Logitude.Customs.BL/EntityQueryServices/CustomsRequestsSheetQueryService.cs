eaa78ea5b7ac94ea728ef50a2a0f332652e8f55e
itzik <itzik@amital.co.il>
2021-02-24 16:14:22 +0200

    134844
    Related Work Items: #134844

--- Logitude/Logitude.Customs.BL/EntityQueryServices/CustomsRequestsSheetQueryService.cs
+++ Logitude/Logitude.Customs.BL/EntityQueryServices/CustomsRequestsSheetQueryService.cs
@@ -266,7 +266,7 @@ namespace Logitude.Customs.BL.EntityQueryServices
             {
                 throw new Exception("GetRequestInProgress !displayOnlyMode && string.IsNullOrWhiteSpace(InterfaceTypeCode) ");
             }
-
+            requestInProgressParams.InterfaceTypeCode = requestInProgressParams.InterfaceTypeCode.Trim();// angular send " 2750" why  ??
             var intrefaceTypeList =
                 new string[]
                 {
