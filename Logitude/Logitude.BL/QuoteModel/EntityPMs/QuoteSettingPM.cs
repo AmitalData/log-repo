b6abf50bcbdd40a0083bfcaab0c0c0a0edfe7ee0
Samar Abu Eid <Samar@logitudeworld.com>
2021-02-28 12:36:58 +0200

    135074
    Related Work Items: #135074

--- Logitude/Logitude.BL/QuoteModel/EntityPMs/QuoteSettingPM.cs
+++ Logitude/Logitude.BL/QuoteModel/EntityPMs/QuoteSettingPM.cs
@@ -27,5 +27,6 @@ namespace Logitude.BL.QuoteModel.EntityPMs
         public bool IsSaleAsCostCurrency { get; set; }
         public bool CopyExchangeRates { get; set; }
         public int AutomaticallyCloseDays { get; set; }
+        public bool IsMultiCurrency { get; set; }
     }
 }
