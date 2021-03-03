4b5753caa88387e9a6d11ccdc807691ae2ed3a57
Leen Thawabi <leen@logitudeworld.com>
2021-03-03 15:31:04 +0200

    test

--- Logitude/AngularModules/CypressTesting/Accounting/cypress/actions/Actions.ts
+++ Logitude/AngularModules/CypressTesting/Accounting/cypress/actions/Actions.ts
@@ -401,7 +401,7 @@ export function ClickOnRowDependingOnARInvoiceNumber(ARInvoiceNumber: string) {
     BaseActions.ClickOnRowDependingOnValue(ARInvoiceNumber)
 }
 export function ExportARInvoice(ARInvoiceNumber: string) {
-    cy.get(AccountingSelectors.TransferCheckBox(ARInvoiceNumber)).check({force:true});
+    cy.get(AccountingSelectors.TransferCheckBox(ARInvoiceNumber)).check({});
     cy.Click(BaseSelectors.RedButton, AccountingSelectors.ContainExport)
 }
 export function CloseExportingInvoiceTransferWindow() {
