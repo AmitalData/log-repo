a5f49c686174012b5368e5370f453ce03f5c7416
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2021-03-07 17:54:18 +0200

    damage

--- Logitude/AngularModules/CypressTesting/Base/cypress/commands/Elements.ts
+++ Logitude/AngularModules/CypressTesting/Base/cypress/commands/Elements.ts
@@ -83,20 +83,30 @@ Cypress.Commands.add("FillLogTextBox", (selector, value,ValidateInputDone = fals
     }
 
 })
-
+  
 
 Cypress.Commands.add("FillLogLov", (selector, value, fromCache, getByFilters = false) => {
-
+    const itemSelector = value == "{downarrow}" ? "li.DropDownListItem" : "li.DropDownListItem.liItemSelected";
     if (!fromCache) {
         cy.intercept({
             method: RestAPI.GET,
             url: getByFilters ? BaseURLs.GetByFilters : BaseURLs.GetByCompactFilters
         }).as("LOVDataLoaded")
     }
-
+    cy.get('body').find('SessionComponent').eq(0)as('Container')
+    cy.get('@Container')
+        .find(selector)
+        .eq(0)
+        .parents('loglov')
+        .eq(0)
+        .within(() => {
+            cy.get('input').clear().type(value).then(() => {
+                cy.get('ul.DropDownList').find(itemSelector).eq(0).click({ force: true });
+            });
+        });
     //cy.get(selector).clear().type(value)
-    cy.wrap(selector).clear({ force: true })
-    cy.get(selector).type("{selectall}" + value)
+    //cy.wrap(selector).clear({ force: true })
+    //cy.get(selector).type("{selectall}" + value)
 
     if (!fromCache) {
         cy.wait("@LOVDataLoaded")
