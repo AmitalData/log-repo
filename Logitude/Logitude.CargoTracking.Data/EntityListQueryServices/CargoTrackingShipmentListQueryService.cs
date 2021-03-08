ba70fcc837eb03107316c5e0e463759432ad78ff
Fida'a Zuhour <fida@logitudeworld.com>
2021-03-08 10:07:25 +0200

    #135139 display DeliveryOut

--- Logitude/Logitude.CargoTracking.Data/EntityListQueryServices/CargoTrackingShipmentListQueryService.cs
+++ Logitude/Logitude.CargoTracking.Data/EntityListQueryServices/CargoTrackingShipmentListQueryService.cs
@@ -591,12 +591,12 @@ namespace Logitude.CargoTracking.Data.EntityListQueryServices
                 Id = 12,
                 Code = "DeliveryOut",
                 Name = "Delivery Out",
-                //Date = Shipment.de,
-                //EstimationDate = Shipment.DeliveredEstimationDate,
-                //Done = Shipment.DeliveredDone,
-                Notes = null,
+                Date = shipment.DeliveryDate,
+                EstimationDate = shipment.DeliveryEstimationDate,
+                Done = shipment.DeliveryDone,
+                Notes = shipment.DeliveryNotes,
                 IsCurrent = false,
-                //IsEstimation = !Shipment.DeliveredDone
+                IsEstimation = !shipment.DeliveryDone
             });
             milestones.Add(new Milestone()
             {
