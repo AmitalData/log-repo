262b6b2aca79af41023859f258de9bbb1aa43fc1
itzik <itzik@amital.co.il>
2021-03-11 08:03:14 +0200

    133579 already  install in ecargo
    Related Work Items: #133579

--- Logitude/Logitude.SystemLogs/ExceptionHandler.cs
+++ Logitude/Logitude.SystemLogs/ExceptionHandler.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Data.SqlClient;
 using Simplog.Server.Infrastructure;
 using Simplog.Server.Infrastructure.Helpers;
+using System.IO;
 
 namespace Logitude.SystemLogs
 {
@@ -12,98 +13,122 @@ namespace Logitude.SystemLogs
     {
         public static void HandleException(Exception exception, DateTime clientDate, int tenant, string userId, string userName, string ExtraMessage, string ip)
         {
-            if (!DbContextBaseUtil.MaxPoolSizeWasReachedWhileSave.HasValue && exception != null && exception.ToString().Contains("max pool size was reached"))//  The timeout period elapsed prior to obtaining a connection from the pool.  This may have occurred because all pooled connections were in use and max pool size was reached.
+            try
             {
-                DbContextBaseUtil.MaxPoolSizeWasReachedWhileSave = DateTime.Now;
-            }
-            exception = exception ?? new Exception(ExtraMessage??"");
-            if (exception.ToString().Contains("max pool size was reached"))
-            {
-                if (InjectionUtil.Instance.IISManager != null)
+
+
+                if (!DbContextBaseUtil.MaxPoolSizeWasReachedWhileSave.HasValue && exception != null && exception.ToString().Contains("max pool size was reached"))//  The timeout period elapsed prior to obtaining a connection from the pool.  This may have occurred because all pooled connections were in use and max pool size was reached.
                 {
-                    InjectionUtil.Instance.IISManager.RecycleMe();
+                    DbContextBaseUtil.MaxPoolSizeWasReachedWhileSave = DateTime.Now;
                 }
-
-            }
-            Debug.WriteLine(exception.ToString());
-            AmitalDebuggerUtil.Break(AmitalDebuggerLevel.Error);
-            string ErrorMessage = "";
-
-            if (!string.IsNullOrEmpty(ip))
-            {
-                if (ip.StartsWith("150.70"))
+                exception = exception ?? new Exception(ExtraMessage ?? "");
+                if (exception.ToString().Contains("max pool size was reached"))
                 {
-                    return;
-                }
-            }
-
-            if (exception != null)
-            {
-                if (exception.Message.Contains("Sorry you can't update this record right now. It's being updated by another user")
-                    || exception.Message.Contains("WebFreight.Web.Security.AutenticationException")
-                    || exception.Message.Contains("Sorry! this user is not the last signed user!")
-                    || exception.Message.Contains("Can't create payment with future date")
-                    || exception.Message.Contains("is required")
-                    || exception.Message.Contains("already exists")
-                    || exception.Message.Contains("Master field already used in another Shipment")
-                    || exception.Message.Contains("You should have at least 1 invoice line")
-                    || exception.Message.Contains("Some of invoice lines Vat Type Percentage is empty")
-                    || exception.Message.Contains("Invoice line amount field must not be zero")
-                    || exception.Message.Contains("You should have at least 1 invAPInvoice.M.VatTypePercentageEmptyoice line")
-                    || exception.Message.Contains("Sorry! you have no permission to do this operation")
-                      //|| exception.Message.Contains("לא ניתן לעדכן את הרשומה מכיוון שהיא נעולה ע")
-                      )
-
-                {
-                    return;
-                }
-
-                //if (exception.Message.Contains("Physical connection is not usable"))
-                //{
-                //    SqlConnection.ClearAllPools();
-                //}
-
-                //if (exception.Message.Contains("An exception has been raised that is likely due to a transient failure. If you are connecting to a SQL Azure database consider using SqlAzureExecutionStrategy.")
-                //    || exception.Message.Contains("The underlying provider failed on Open"))
-                //{
-                //    WebFreightEntryPoint.CheckConnectionStrategy = true;
-                //    WebFreightEntryPoint.CheckConnectionStartDate = DateTime.UtcNow;
-
-                //}
-
-                if (!string.IsNullOrEmpty(ExtraMessage))
-                {
-                    ErrorMessage = ExtraMessage + Environment.NewLine;
-                }
-
-                ErrorMessage += exception.Message;
-
-                if (exception.InnerException != null)
-                {
-                    ErrorMessage += Environment.NewLine + exception.InnerException.Message;
-
-                    if (exception.InnerException.InnerException != null)
+                    if (InjectionUtil.Instance.IISManager != null)
                     {
-                        ErrorMessage += Environment.NewLine + exception.InnerException.InnerException.Message;
-
-                        if (exception.InnerException.InnerException.InnerException != null)
-                        {
-                            ErrorMessage += Environment.NewLine + exception.InnerException.InnerException.InnerException.Message;
-                        }
+                        InjectionUtil.Instance.IISManager.RecycleMe();
                     }
 
-                    //if (exception.InnerException.Message.Contains("Physical connection is not usable"))
+                }
+                Debug.WriteLine(exception.ToString());
+                AmitalDebuggerUtil.Break(AmitalDebuggerLevel.Error);
+                string ErrorMessage = "";
+
+                if (!string.IsNullOrEmpty(ip))
+                {
+                    if (ip.StartsWith("150.70"))
+                    {
+                        return;
+                    }
+                }
+
+                if (exception != null)
+                {
+                    if (exception.Message.Contains("Sorry you can't update this record right now. It's being updated by another user")
+                        || exception.Message.Contains("WebFreight.Web.Security.AutenticationException")
+                        || exception.Message.Contains("Sorry! this user is not the last signed user!")
+                        || exception.Message.Contains("Can't create payment with future date")
+                        || exception.Message.Contains("is required")
+                        || exception.Message.Contains("already exists")
+                        || exception.Message.Contains("Master field already used in another Shipment")
+                        || exception.Message.Contains("You should have at least 1 invoice line")
+                        || exception.Message.Contains("Some of invoice lines Vat Type Percentage is empty")
+                        || exception.Message.Contains("Invoice line amount field must not be zero")
+                        || exception.Message.Contains("You should have at least 1 invAPInvoice.M.VatTypePercentageEmptyoice line")
+                        || exception.Message.Contains("Sorry! you have no permission to do this operation")
+                          //|| exception.Message.Contains("לא ניתן לעדכן את הרשומה מכיוון שהיא נעולה ע")
+                          )
+
+                    {
+                        return;
+                    }
+
+                    //if (exception.Message.Contains("Physical connection is not usable"))
                     //{
                     //    SqlConnection.ClearAllPools();
                     //}
+
+                    //if (exception.Message.Contains("An exception has been raised that is likely due to a transient failure. If you are connecting to a SQL Azure database consider using SqlAzureExecutionStrategy.")
+                    //    || exception.Message.Contains("The underlying provider failed on Open"))
+                    //{
+                    //    WebFreightEntryPoint.CheckConnectionStrategy = true;
+                    //    WebFreightEntryPoint.CheckConnectionStartDate = DateTime.UtcNow;
+
+                    //}
+
+                    if (!string.IsNullOrEmpty(ExtraMessage))
+                    {
+                        ErrorMessage = ExtraMessage + Environment.NewLine;
+                    }
+
+                    ErrorMessage += exception.Message;
+
+                    if (exception.InnerException != null)
+                    {
+                        ErrorMessage += Environment.NewLine + exception.InnerException.Message;
+
+                        if (exception.InnerException.InnerException != null)
+                        {
+                            ErrorMessage += Environment.NewLine + exception.InnerException.InnerException.Message;
+
+                            if (exception.InnerException.InnerException.InnerException != null)
+                            {
+                                ErrorMessage += Environment.NewLine + exception.InnerException.InnerException.InnerException.Message;
+                            }
+                        }
+
+                        //if (exception.InnerException.Message.Contains("Physical connection is not usable"))
+                        //{
+                        //    SqlConnection.ClearAllPools();
+                        //}
+                    }
+
+                    if (clientDate == null)
+                        clientDate = DateTime.Now;
+
+                    Debug.WriteLine("***HandleException** " + ErrorMessage);//May cause slowness ,But worth - If u Decides to delete ,Please inform itzik !!!!!
+                    AzureLog.SaveLogsInStorage(ErrorMessage, "E", clientDate, exception.Message, exception.StackTrace, tenant, userId, userName, ip, exception);
+
                 }
+            }
+            catch (Exception eee)
+            {
 
-                if (clientDate == null)
-                    clientDate = DateTime.Now;
+                OnExceptionOnDbLogInFile(eee);
+            }
+        }
 
-                Debug.WriteLine("***HandleException** " + ErrorMessage);//May cause slowness ,But worth - If u Decides to delete ,Please inform itzik !!!!!
-                AzureLog.SaveLogsInStorage(ErrorMessage, "E", clientDate, exception.Message, exception.StackTrace, tenant, userId, userName, ip,exception);
-
+        private static void OnExceptionOnDbLogInFile(Exception eee)
+        {
+            try
+            {
+                string fileN = Path.Combine(Path.GetTempPath(), "AmitalCustomsError.Log");
+                File.WriteAllText(fileN, eee.ToString());
+            }
+            catch (Exception)
+            {
+                Debug.WriteLine("Unable to write to File (OnExceptionOnDbLogInFile)");
+                //throw;
             }
         }
 
