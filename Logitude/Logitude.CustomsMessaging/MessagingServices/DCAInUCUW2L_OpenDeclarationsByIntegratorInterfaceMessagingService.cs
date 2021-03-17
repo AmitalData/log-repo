bd1ec62cd4afc3feaff8c4a9f3cfb667b8318051
elisheva <elisheva@amital.co.il>
2021-03-17 16:01:51 +0200

    135237
    Related Work Items: #135237

--- Logitude/Logitude.CustomsMessaging/MessagingServices/DCAInUCUW2L_OpenDeclarationsByIntegratorInterfaceMessagingService.cs
+++ Logitude/Logitude.CustomsMessaging/MessagingServices/DCAInUCUW2L_OpenDeclarationsByIntegratorInterfaceMessagingService.cs
@@ -105,8 +105,7 @@ namespace Logitude.CustomsMessaging.MessagingServices
                 var ourRef = "";
                 using (var trans = TransactionFactory.GetNewTransaction())
                 {
-                    try
-                    {
+                   
                         var InterfaceManagementQS = new InterfaceManagementQueryService(tenant);
                 var InterfaceManagementPM = InterfaceManagementQS.GetSingleInterfaceManagementwithDefinition(
                     this.MainInterfaceCode, tenant);
@@ -118,23 +117,11 @@ namespace Logitude.CustomsMessaging.MessagingServices
 
                 }, xmlESBResponseXmlClass);
 
+
                         trans.Complete();
                         return "SUCCESS";
-                    }
-                    catch (CustomsRequestsSheetDomainModelServiceException myCustomsRequestsSheetServiceException)
-                    {
-                        return ex.Message + " : " + ex.InnerException + " : " + ex.StackTrace;
-
-                        //if (myCustomsRequestsSheetServiceException.Where == CustomsRequestsSheetDomainModelServiceException.WhereEnum.SameRequestInProgress)
-                        //{
-                        //    Logitude.Server.Tools.Helpers.LogMessagingUtil.Instance.AppendLine(" UCBCMSS SameRequestInProgress!! " + myCustomsRequestsSheetServiceException.Message);
-                        //}
-                        //else if (myCustomsRequestsSheetServiceException.Where == CustomsRequestsSheetDomainModelServiceException.WhereEnum.NoAvailableSignServer)
-                        //{
-                        //    Logitude.Server.Tools.Helpers.LogMessagingUtil.Instance.AppendLine("UCBCTML SameRequestInProgress!!  " + myCustomsRequestsSheetServiceException.Message);
-                        //}
-                        //return "קיים מסר זהה בתהליך";
-                    }
+                     
+                  
                 }
 
             }
