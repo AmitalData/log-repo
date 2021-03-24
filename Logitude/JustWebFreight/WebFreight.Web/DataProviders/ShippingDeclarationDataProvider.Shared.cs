730b1e863a4fb656141b74c112f3cabbf49c592f
Samar Abu Eid <Samar@logitudeworld.com>
2021-03-24 12:49:19 +0200

    136756
    Related Work Items: #136756

--- Logitude/JustWebFreight/WebFreight.Web/DataProviders/ShippingDeclarationDataProvider.Shared.cs
+++ Logitude/JustWebFreight/WebFreight.Web/DataProviders/ShippingDeclarationDataProvider.Shared.cs
@@ -307,6 +307,7 @@ namespace WebFreight.Web.DataProviders
         public string Salesman { get; set; }
         public string SalesmanEmail { get; set; }
         public string UserPhoneNumber { get; set; }
+        public string UserMobileNumber { get; set; }
         public double? TotalPayables { get; set; }
         public string CustomsAgent { get; set; }
         public double? TotalPayablesForMainCarriageCarrier { get; set; }
