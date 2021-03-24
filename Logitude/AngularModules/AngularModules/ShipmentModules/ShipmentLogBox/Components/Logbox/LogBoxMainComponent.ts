afcbf7d512381b207b4ba652405fde68fe9d33c3
Wesam Amarneh <wesam@logitudeworld.com>
2021-03-24 10:13:07 +0200

    Fix Private Label Process
    Related Work Items: #133906

--- Logitude/AngularModules/AngularModules/ShipmentModules/ShipmentLogBox/Components/Logbox/LogBoxMainComponent.ts
+++ Logitude/AngularModules/AngularModules/ShipmentModules/ShipmentLogBox/Components/Logbox/LogBoxMainComponent.ts
@@ -95,7 +95,7 @@ export class LogBoxMainComponent implements OnInit, AfterViewInit {
     private handlePrivateLable() {
         if (SessionLocator.PrivateLableSettings) {
             this.isPrivateLabel = true;
-            this.IsDSV = SessionLocator.PrivateLableSettings.PrivateLabelDomain.indexOf("DSV") > -1;
+            this.IsDSV = SessionLocator.PrivateLableSettings.PrivateLabelDomain.toLowerCase().indexOf("dsv") > -1;
             this.AgentShipmentsLabel = SessionLocator.PrivateLableSettings.PrivateLabelShortName + " Shipments";
             this.LogoURL = "data:image/JPEG;base64," + SessionLocator.PrivateLableSettings.MainLogo;
             this.MainColor = SessionLocator.PrivateLableSettings.MainColor;
