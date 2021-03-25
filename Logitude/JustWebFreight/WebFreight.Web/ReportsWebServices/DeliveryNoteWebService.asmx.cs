3d87dbb036f0a626d79abdbf3eccd4083dcfb41a
Abdullah Beitello <abdullahb@logitudeworld.com>
2021-03-25 15:16:10 +0200

    #136883 (change logic)

--- Logitude/JustWebFreight/WebFreight.Web/ReportsWebServices/DeliveryNoteWebService.asmx.cs
+++ Logitude/JustWebFreight/WebFreight.Web/ReportsWebServices/DeliveryNoteWebService.asmx.cs
@@ -329,7 +329,7 @@ namespace WebFreight.Web.ReportsWebServices
         {
             if (!string.IsNullOrEmpty(shipment.ShipperId))
             {
-                dataProvider.ClientReferenceNumber = shipment.CustomerShipmentNumber != null ? shipment.CustomerShipmentNumber : "";
+                dataProvider.ClientReferenceNumber = shipment.CustomerReference1 != null ? shipment.CustomerReference1 : "";
                 dataProvider.ShipperReference2 = shipment.ShipperReference2;
 
                 Card card = CardRepository.GetSingleCard(shipment.ShipperId, tenant, true);
