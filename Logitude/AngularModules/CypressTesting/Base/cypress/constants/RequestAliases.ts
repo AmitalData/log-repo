ac28f9a60fae48f7e6a3706158a5b25251a784c8
Leen Thawabi <leen@logitudeworld.com>
2021-03-28 02:29:05 +0300

    #136653

--- Logitude/AngularModules/CypressTesting/Base/cypress/constants/RequestAliases.ts
+++ Logitude/AngularModules/CypressTesting/Base/cypress/constants/RequestAliases.ts
@@ -101,8 +101,11 @@ export class RequestAliases {
     public static readonly PutQuoteTemplateHeaderFields = "PutQuoteTemplateHeaderFields";
     public static readonly PutQuoteTemplateTextDesignPMs = "PutQuoteTemplateTextDesignPMs";
 
-    public static readonly PostCrossdock = "PostCrossdock";
-    public static readonly PutCrossdock = "PutCrossdock";
+    public static readonly PostCrossdockEntry = "PostCrossdockEntry";
+    public static readonly PutCrossdockEntry = "PutCrossdockEntry";
+
+    public static readonly PostCrossdockRelease="PostCrossdockRelease"
+    public static readonly PutCrossdockRelease = "PutCrossdockRelease";
 
     public static readonly GetSignle = "GetSignle"
     public static readonly GetByFilter = "GetByFilter"
