d60e6d4b3dafb43e7cd29d1eb2ee4e5f30e3a571
Fida'a Zuhour <fida@logitudeworld.com>
2021-03-31 15:20:58 +0300

    #135975: Bank Accounts - Add GLAccount Display Number to Bank Search Fields

--- Logitude/Logitude.Accounting.BL/EntityDataMappings/BankAccountDataMapping.cs
+++ Logitude/Logitude.Accounting.BL/EntityDataMappings/BankAccountDataMapping.cs
@@ -11,6 +11,7 @@ using Logitude.Accounting.Data.EntityPOCOs;
 using Logitude.Accounting.Def.EntityPMs; 
 using Logitude.Accounting.Data;
 using Logitude.Accounting.BL.EntityQueryServices;
+using Logitude.Server.Tools.Helpers;
 
 namespace Logitude.Accounting.BL.EntityDataMappings
 {
@@ -22,6 +23,9 @@ namespace Logitude.Accounting.BL.EntityDataMappings
         {
             BankAccountCustomDataMapping bankAccountCustomDataMapping = new BankAccountCustomDataMapping();
             bankAccountCustomDataMapping.PMToPOCO(entityPM, entityPOCO, this.CustomMappedPOCOProperties);
+            this.CustomMappedPOCOProperties.Add(POCOPropertyNames.SearchFields);
+            BuildSearchFields(entityPM, entityPOCO, entityPM.ChangeSetOp == Simplog.Server.Infrastructure.ChangeSetOperation.Insert);
+            entityPOCO.SearchFields = entityPM.SearchFields;
         }
 
         public void CustomPOCOToPM(BankAccountPM entityPM, BankAccount entityPOCO)
@@ -29,7 +33,35 @@ namespace Logitude.Accounting.BL.EntityDataMappings
             BankAccountCustomDataMapping bankAccountCustomDataMapping = new BankAccountCustomDataMapping();
             bankAccountCustomDataMapping.POCOToPM(entityPM, entityPOCO, this.CustomMappedPMProperties);
         }
-   }
+
+        private void BuildSearchFields(BankAccountPM entityPM, BankAccount entityPOCO, bool p)
+        {
+            string searchFields = "";
+
+            if (!string.IsNullOrEmpty(entityPM.GLAccountId))
+            {
+                MethodHelper.AddToSearchFields(ref searchFields, entityPM.GLAccountNumber);
+            }
+
+            if (!string.IsNullOrEmpty(entityPM.TransferGLAcccountId))
+            {
+                MethodHelper.AddToSearchFields(ref searchFields, entityPM.TransferGLAcccountNumber);
+            }
+
+            if (!string.IsNullOrEmpty(entityPM.DeferredGLAccountId))
+            {
+                MethodHelper.AddToSearchFields(ref searchFields, entityPM.DeferedGLAccountNumber);
+            }
+
+            if (searchFields.Length > 1000)
+            {
+                searchFields = searchFields.Substring(0, 1000);
+            }
+
+            entityPM.SearchFields = searchFields;
+            entityPOCO.SearchFields = searchFields;
+        }
+    }
 
 
 }
