57b27f4410ea70bc931602c57cc2e8eb959ec95c
Atallah Hammad <Atallah@logitudeworld.com>
2021-04-10 17:58:27 +0300

    137454

--- Logitude/CommunicationWorkerRole/CollaborationToolTaskWR.cs
+++ Logitude/CommunicationWorkerRole/CollaborationToolTaskWR.cs
@@ -1,16 +1,14 @@
 ﻿using Logitude.Server.Tools.QueueService;
 using Logitude.SystemLogs;
-using Simplog.Data.CommonDataModel.Repositories;
+using Newtonsoft.Json;
 using Simplog.Data.CommonDataModel.EntityPOCOs;
+using Simplog.Data.CommonDataModel.Repositories;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Configuration;
 using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
 
 namespace CommunicationWorkerRole
 {
@@ -18,7 +16,7 @@ namespace CommunicationWorkerRole
     {
         DbQueueService queueservice;
         string queueName = "CreateTaskCollaborationTool";
-        string URL = "https://localhost:44362/api/Task/PostExternal";
+        string URL = ConfigurationManager.AppSettings["CT_URL"];
 
         public override void Run()
         {
