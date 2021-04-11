b825659521a1d6bfc2e5b4301ec3424af5dca821
moran <moran@amital.co.il>
2021-04-11 14:12:03 +0300

    134838 + 134830
    Related Work Items: #134830, #134838

--- Logitude/Logitude.Customs.BL/Messaging/ILOVS/CourierOVSECTHMessageRequestService.cs
+++ Logitude/Logitude.Customs.BL/Messaging/ILOVS/CourierOVSECTHMessageRequestService.cs
@@ -139,6 +139,12 @@ namespace Logitude.Customs.BL.Messaging.ILOVS
             var rep = new CustomsAirlineRepository(myCourierMasterPM.Tenant);
             var customsAirline = rep.GetSingle(myCourierMasterPM.AirlineId, myCourierMasterPM.Tenant);
 
+            string crateNumber = "";
+            var context = CustomContext.GetContext(myDeclarationPM.Tenant);
+            DeclarationCourierStatusQueryService declarationCourierStatusQueryService = new DeclarationCourierStatusQueryService(context);
+            DeclarationCourierStatusPM currentDeclarationCourierStatusPM = declarationCourierStatusQueryService.GetSingle(myDeclarationPM.Id, true, false);
+            if(currentDeclarationCourierStatusPM != null && !String.IsNullOrWhiteSpace(currentDeclarationCourierStatusPM.CrateNumber))crateNumber = currentDeclarationCourierStatusPM.CrateNumber;
+
             var courierHawbMamanModel = new CourierOVSHAWBRequest()
             {
 
@@ -170,7 +176,8 @@ namespace Logitude.Customs.BL.Messaging.ILOVS
                 CustomsSuspention = myDeclarationPM.CourierSuspentionCode??"",
                 Preclearence = myDeclarationPM.CourierCustomStatusCode== "1"  /*released*/,
 
-
+                ImporterVat = myDeclarationPM.ImporterId ?? myDeclarationPM.ImporterCode ?? "",
+                BoxBarcode = crateNumber,
 
 
             };
@@ -267,18 +274,20 @@ namespace Logitude.Customs.BL.Messaging.ILOVS
         public string DeclarationNumber { get; set; }
         public string CustomsSuspention { get; set; }
         public bool Preclearence { get; set; }
-        
+
         //public string CustomIkuv { get; set; }
         //TAsk 46455.
 
         //public DateTime BaldarMessageTime { get; set; }
-        
-        
+
+
 
         //public int ResponseStatusCode { get; set; }
         //public string ResponseStatusMsg { get; set; }
 
+        public string ImporterVat { get; set; }
 
+        public string BoxBarcode { get; set; }
 
     }
 
