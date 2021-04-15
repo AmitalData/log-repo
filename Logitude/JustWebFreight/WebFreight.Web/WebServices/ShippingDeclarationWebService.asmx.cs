28fde7475269c25f1eb95d4998d5d03dc7eec62d
Wesam Amarneh <wesam@logitudeworld.com>
2021-04-15 10:28:02 +0300

    Integrate with Fourkites - Phase 3
    Related Work Items: #137362

--- Logitude/JustWebFreight/WebFreight.Web/WebServices/ShippingDeclarationWebService.asmx.cs
+++ Logitude/JustWebFreight/WebFreight.Web/WebServices/ShippingDeclarationWebService.asmx.cs
@@ -3777,7 +3777,8 @@ namespace WebFreight.Web.WebServices
 
         private string getLoadNumberPrefix(char legType, int tenant)
         {
-            return tenant + "/" + legType;
+            string loadNumberPrefix = tenant.ToString() + (legType == 'M' ? '/' : '/' + legType);
+            return loadNumberPrefix;
         }
 
         private void FillPickUpDeliveryAddresses(ShipmentPickUpDelivery myFirstPickup, ShipmentPickUpDelivery myLastDelivery, ShippingDeclarationDataProvider myDataProvider)
