54531a79f5a30026404cf71d413aa1327c4f68ef
sohaib <sohaib@amital.co.il>
2021-04-18 11:48:09 +0300

    comments

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/LogGridComponent/LogGridComponentV2.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/LogGridComponent/LogGridComponentV2.ts
@@ -768,6 +768,7 @@ export class LogGridComponentV2 implements OnInit, AfterViewInit, OnChanges, OnD
     pubSubAdvanceQueryFiltersSub: any;
     SearchFieldChanged: boolean = false;
     ngOnInit() {
+        // sohaib -- Task 137412
         if (this.ObjectTable.Name == "Customs.ExportStorge") {
             this.RTL = false;
         }
