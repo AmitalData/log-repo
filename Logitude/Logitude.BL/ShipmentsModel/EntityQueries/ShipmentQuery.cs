47a3577d4fe20300e998df42cbbc836184ec089d
Abdullah Dawoud <Abdullah@logitudeworld.com>
2021-04-18 16:33:52 +0300

    fix error

--- Logitude/Logitude.BL/ShipmentsModel/EntityQueries/ShipmentQuery.cs
+++ Logitude/Logitude.BL/ShipmentsModel/EntityQueries/ShipmentQuery.cs
@@ -13577,13 +13577,6 @@ namespace Logitude.BL.ShipmentsModel.EntityQueries
         }
 
 
-        private List<ShipmentPackagePM> GetPackagesOfShipment(int tenant, string shipmentId)
-        {
-            var shipmentIds = new List<string>() { shipmentId };
-            ShipmentPackageQuery shipmentPackageQuery = new ShipmentPackageQuery(tenant);
-            var shipmentPackages = shipmentPackageQuery.GetShipmentPackages(shipmentIds, tenant);
-            return shipmentPackages;
-        }
     }
 
     public class DeparturesArrivalsDataItem
