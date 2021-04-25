a1cf1034f0c3447b0ae1ea5d8ee83d9e022523e0
sohaib <sohaib@amital.co.il>
2021-04-25 16:14:28 +0300

    fix

--- Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_2754_MSG10004_ImportAmendmentDeclarationResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_2754_MSG10004_ImportAmendmentDeclarationResponseService.cs
@@ -964,19 +964,20 @@ namespace Logitude.CustomsMessaging.ResponseServices
                                     switch (GetValueCodeType(goodsItemAmount.AmountType))
                                     {
                                         case "3":
-                                            {if(!isFromImporter)
-                                                {
-                                                    supplierInvoiceItemPM.ItemPrice = GetValueAmountType(goodsItemAmount.CustomsValueAmount);
-                                                    supplierInvoiceItemPM.ItemPriceCurrencyCode = goodsItemAmount.CustomsValueAmount.currencyID.ToString();
+                                            {
+                                                //if(!isFromImporter)
+                                                //{
+                                                //    supplierInvoiceItemPM.ItemPrice = GetValueAmountType(goodsItemAmount.CustomsValueAmount);
+                                                //    supplierInvoiceItemPM.ItemPriceCurrencyCode = goodsItemAmount.CustomsValueAmount.currencyID.ToString();
 
-                                                }
+                                                //}
                                                 break;
 
                                             }
                                         case "1":
                                             {
-                                                if (isFromImporter)
-                                                {
+                                               // if (isFromImporter)
+                                               // {
                                                 if (item.Invoice!= null && item.Invoice.DMExtensions != null && item.Invoice.DMExtensions.InvoiceAmount!= null )
                                                 {
                                                 if (goodsItemAmount.CustomsValueAmount.currencyID.ToString()== item.Invoice.DMExtensions.InvoiceAmount.currencyID.ToString())
@@ -986,7 +987,7 @@ namespace Logitude.CustomsMessaging.ResponseServices
                                                     }
                                                 }
 
-                                                }
+                                               // }
                                                 break;
 
                                             }
