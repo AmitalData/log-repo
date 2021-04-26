bd45de5d6758edf7df915c3aa4b7d95f05e6d620
Heba Mubarak <heba@logitudeworld.com>
2021-04-26 22:23:00 +0300

    Test

--- Logitude/AngularModules/AngularModules/Quote/Components/MenuButtons/QuoteMenuButtonsHandler.ts
+++ Logitude/AngularModules/AngularModules/Quote/Components/MenuButtons/QuoteMenuButtonsHandler.ts
@@ -754,6 +754,7 @@ export class QuoteMenuButtonsHandler {
 
     IsSetAsSentQuote: boolean = false;
     private SetAsSentToCustomer() {
+        console.log("Test Object reference error -> Set As Sent To Customer");
         this.isLCL = QuoteUtilities.IsLCLQuote(this.EntityPM);
         this.CheckUpdateQuantities();
 
@@ -802,7 +803,7 @@ export class QuoteMenuButtonsHandler {
 
     private CancelQuote() {
         this.Validate();
-
+        console.log("Test Object reference error for canceled quotes");
         if (this.isValid) {
             var myService: ShipmentDomainService = new ShipmentDomainService();
             myService.GetShipmentsCountByQuoteId(this.EntityPM.Id).subscribe((myResult: ServiceResponse) => {
@@ -1211,7 +1212,7 @@ export class QuoteMenuButtonsHandler {
 
     private OnNotesWindowClosed(actionType: string) {
         this.isButtonClicked = false;
-
+        console.log("Test Object reference error for canceled quotes");
         if (actionType == "sent") {
             this.EntityPM.IsClosed = false;
             this.EntityPM.ActionType = "SetAsSentToCustomer";
