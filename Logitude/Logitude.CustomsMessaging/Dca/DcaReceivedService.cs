d5d7f50673448fc6360079c818307f1ad3e67b8a
elisheva Nachaysi <elisheva@amital.co.il>
2021-04-27 09:55:02 +0300

    _EX

--- Logitude/Logitude.CustomsMessaging/Dca/DcaReceivedService.cs
+++ Logitude/Logitude.CustomsMessaging/Dca/DcaReceivedService.cs
@@ -93,7 +93,7 @@ namespace Logitude.CustomsMessaging.Dca
                 
                 string externalId = //GetExternalId(_SelectedFile);
                     _DCAFileModel.OurRefExtrenalId;
-                if (externalId != myESBResponseParser.ResponseHeader.ExternalId && externalId != myESBResponseParser.ResponseHeader.ExternalId +"_E")
+                if (externalId != myESBResponseParser.ResponseHeader.ExternalId && externalId != myESBResponseParser.ResponseHeader.ExternalId +"_EX")
                 {
 
                     this.ErrorMessage = ("Exception:it must be GetExternalId(selectedFile) (" + externalId + ")  == myESBResponseParser.ResponseHeader.ExternalId (" + myESBResponseParser.ResponseHeader.ExternalId + ")") + _DCAFileModel.ErrorMessage;
