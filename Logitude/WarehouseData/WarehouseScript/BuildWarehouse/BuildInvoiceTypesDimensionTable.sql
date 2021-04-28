026fda5cd306dcf40fd62acb8fcfbaaa612bcaf5
Heba Mubarak <heba@logitudeworld.com>
2021-04-28 09:07:39 +0300

    Edit

--- Logitude/WarehouseData/WarehouseScript/BuildWarehouse/BuildInvoiceTypesDimensionTable.sql
+++ Logitude/WarehouseData/WarehouseScript/BuildWarehouse/BuildInvoiceTypesDimensionTable.sql
@@ -1,23 +1,49 @@
-﻿ 
-   declare @Code as varchar(2)
-   declare @Name as varchar(20)
-   declare @SourceTenant int
-   declare @ParentTenant int
-   declare @AutomaticLastUpdateDate as datetime
+﻿
 
+   declare @Code as varchar(2)
+   declare @Name as varchar(20) 
+    
+	
 	DECLARE InvoiceTypesCursor CURSOR READ_ONLY
 	FOR
-	SELECT Code, Name, dw_DWHSettings.Tenant, dw_DWHSettings.ParentTenant, dw_InvoiceTypes.AutomaticLastUpdateDate
-	From dw_InvoiceTypes
-	inner JOIN dw_DWHSettings ON dw_InvoiceTypes.Tenant = dw_DWHSettings.Tenant
-	OPEN InvoiceTypesCursor FETCH NEXT FROMInvoiceTypes INTO  @Code, @Name, @SourceTenant, @ParentTenant, @AutomaticLastUpdateDate
+
+	with InvoiceTypes as(select Code,Name
+	from(
+		select Code, Name
+		from dw_ARInvoiceTypes
+         
+		union all
+
+		select Code, Name
+		from dw_APInvoiceTypes t2
+        
+		where not exists(select Code from dw_ARInvoiceTypes t1 where t2.Code = t1.Code )
+
+		union all
+  
+
+		select 'AP'+ Code, Name
+		from dw_APInvoiceTypes t2
+        where exists ( select Code from dw_ARInvoiceTypes t1 where t2.Code = t1.Code and t2.Name <> t1.Name )
+
+	
+)tt
+)
+
+	SELECT   InvoiceTypes.Code , InvoiceTypes.Name
+ 
+     From InvoiceTypes
+
+OPEN InvoiceTypesCursor FETCH NEXT FROM InvoiceTypesCursor INTO @Code, @Name
 	WHILE @@FETCH_STATUS = 0
 	BEGIN
 	
-    insert into #DIM_InvoiceTypesTemp ( Code,[Name], [Source Tenant],[Parent Tenant], [Automatic Last Update Date]) values(@Code, @Name, @SourceTenant, @ParentTenant, @AutomaticLastUpdateDate)
+    insert into #DIM_ARInvoiceTypesTemp (Code, Name) values (@Code, @Name)
 
-	FETCH NEXT FROM InvoiceTypesCursor  INTO @Code, @Name, @SourceTenant, @ParentTenant, @AutomaticLastUpdateDate
+	FETCH NEXT FROM InvoiceTypesCursor  INTO @Code, @Name
 		End
 	CLOSE InvoiceTypesCursor
 	DEALLOCATE InvoiceTypesCursor
+ 
 
+	
