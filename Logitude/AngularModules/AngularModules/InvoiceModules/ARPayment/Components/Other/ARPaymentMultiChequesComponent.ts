aa8e662fa43ed60539da5bdc7c6c5e7591085481
Fida'a Zuhour <fida@logitudeworld.com>
2021-04-29 09:55:14 +0300

    138276 Multi cheques - Validation Message -R2

--- Logitude/AngularModules/AngularModules/InvoiceModules/ARPayment/Components/Other/ARPaymentMultiChequesComponent.ts
+++ Logitude/AngularModules/AngularModules/InvoiceModules/ARPayment/Components/Other/ARPaymentMultiChequesComponent.ts
@@ -36,7 +36,8 @@ export class ARPaymentMultiChequesComponent extends BaseComponent {
     private CurrentSession = SessionLocator.SelectedSession;
     public ChequesCounter: number;
     public TotalAmount: number;
-   public isLTR: boolean;
+    public isLTR: boolean;
+    ChequeNumbers: string[];
     constructor() {
         super();
         this.ItemsSource = new ObservableCollection([]);
@@ -177,6 +178,9 @@ export class ARPaymentMultiChequesComponent extends BaseComponent {
         if (AppTool.IsNullOrEmpty(cheque.ChequeNumber)) {
             this.ValidationErrorsList.push(this.FIELD_IS_REQUIERD.replace("%FieldName", TextCodeTranslator.Translate("ARPayment.S.Details.ChequeRef")));
         }
+        else {
+            this.ValidateDuplicateChequeNumbers(cheque.ChequeNumber);
+        }
         if (AppTool.IsNullOrEmpty(cheque.BankId)) {
             this.ValidationErrorsList.push(this.FIELD_IS_REQUIERD.replace("%FieldName", TextCodeTranslator.Translate("ARPayment.F.Bank")));
         }
@@ -191,6 +195,16 @@ export class ARPaymentMultiChequesComponent extends BaseComponent {
         }
         if (AppTool.IsNullOrEmpty(cheque.ForeignAmount)) {
             this.ValidationErrorsList.push(this.FIELD_IS_REQUIERD.replace("%FieldName", TextCodeTranslator.Translate("ARPayment.F.AmountInPaymentCurrency")));
+        }
+    }
+
+    ValidateDuplicateChequeNumbers(chequeNumber: string) {
+        var isDuplicateChequeNumber =!AppTool.IsNullOrEmpty(this.ChequeNumbers?.filter(c => c == chequeNumber));
+        if (isDuplicateChequeNumber) {
+            this.ValidationErrorsList.push(TextCodeTranslator.Translate("Accounting.M.MoreThanChequeWithTheSameChequeNumber"))
+        }
+        else {
+            this.ChequeNumbers.push(chequeNumber);
         }
     }
     public SelectedRow: any = null;
