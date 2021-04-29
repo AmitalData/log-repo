787debc03e5ec0c2ae043477d62b9b81260e0062
Heba Mubarak <heba@logitudeworld.com>
2021-04-29 10:27:28 +0300

    DIM_InvoiceStatus

---
+++ Logitude/WarehouseData/WarehouseScript/BuildWarehouse/BuildInvoiceStatusDimensionTable.sql
@@ -0,0 +1,50 @@
+﻿
+
+   declare @Code as varchar(2)
+   declare @Name as varchar(20) 
+   declare @AutomaticLastUpdateDate as datetime
+    
+	
+	DECLARE InvoiceStatusCursor CURSOR READ_ONLY
+	FOR
+
+	with InvoiceStatus as(select Code,Name, AutomaticLastUpdateDate
+	from(
+		select Code, Name, AutomaticLastUpdateDate
+		from dw_ARInvoiceStatus
+         
+		union all
+
+		select Code, Name, AutomaticLastUpdateDate
+		from dw_APInvoiceStatus t2
+        
+		where not exists(select Code from dw_ARInvoiceStatus t1 where t2.Code = t1.Code )
+
+		union all
+  
+
+		select  Code, Name, AutomaticLastUpdateDate
+		from dw_APInvoiceStatus t2
+        where exists ( select Code from dw_ARInvoiceStatus t1 where  t2.Name <> t1.Name )
+
+	
+)tt
+)
+
+	SELECT   InvoiceStatus.Code , InvoiceStatus.Name, InvoiceStatus.AutomaticLastUpdateDate
+ 
+     From InvoiceStatus
+
+OPEN InvoiceStatusCursor FETCH NEXT FROM InvoiceStatusCursor INTO @Code, @Name, @AutomaticLastUpdateDate
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+	
+    insert into #DIM_InvoiceStatusTemp (Code, Name, [Automatic Last Update Date]) values (@Code, @Name, @AutomaticLastUpdateDate)
+
+	FETCH NEXT FROM InvoiceStatusCursor  INTO @Code, @Name, @AutomaticLastUpdateDate
+		End
+	CLOSE InvoiceStatusCursor
+	DEALLOCATE InvoiceStatusCursor
+ 
+
+	
