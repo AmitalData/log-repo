855ffce1929c77b90cdc1c4f62bd7c4cf317d63a
Heba Mubarak <heba@logitudeworld.com>
2021-05-02 12:41:07 +0300

    Change Password Button Color

--- Logitude/AngularModules/LoginModules/Login/Components/DSVChangePasswordComponent.ts
+++ Logitude/AngularModules/LoginModules/Login/Components/DSVChangePasswordComponent.ts
@@ -15,6 +15,7 @@ export class DSVChangePasswordComponent extends ChangePasswordComponent {
     public BackgroundImage: string = "";
     public ForgetPasswordImage: string = "";
     public MainLogo: string = ""; 
+    public MainColor: string = null;
 
     constructor(public ss: PasswordChangeService, public ll: LoginService) {
         super(ss, ll);
@@ -30,5 +31,6 @@ export class DSVChangePasswordComponent extends ChangePasswordComponent {
         this.BackgroundImage = BrandingDataService.GetImage("BackgroundImage");
         this.MainLogo = BrandingDataService.GetImage("MainLogo");
         this.ForgetPasswordImage = BrandingDataService.GetImage("ForgetPasswordImage");
+        this.MainColor = BrandingDataService.GetColor("MainColor");  
     }
 }
