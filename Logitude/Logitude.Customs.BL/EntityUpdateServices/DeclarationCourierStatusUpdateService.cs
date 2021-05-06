01fad00d89a6de8b1c6b19e6f7922083877ed8d4
Moran Segev <moran@amital.co.il>
2021-05-06 09:39:04 +0300

    138868
    Related Work Items: #138868

--- Logitude/Logitude.Customs.BL/EntityUpdateServices/DeclarationCourierStatusUpdateService.cs
+++ Logitude/Logitude.Customs.BL/EntityUpdateServices/DeclarationCourierStatusUpdateService.cs
@@ -80,7 +80,7 @@ namespace Logitude.Customs.BL.EntityUpdateServices
                     courierMasterPM.OpenDeclarations += 1;
                 }
                 courierMasterPM.ChangeSetOp = ChangeSetOperation.Update;
-                if (!entityPM.IsClosedForFollowUp && !courierMasterPM.IsOpen)courierMasterPM.IsOpen = true;
+                //if (!entityPM.IsClosedForFollowUp && !courierMasterPM.IsOpen)courierMasterPM.IsOpen = true;
                 CourierMasterUpdateService.Update(courierMasterPM, true);
             }
 
