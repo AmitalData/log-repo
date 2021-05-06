84f83dda89e37256c9ed1167bf996fce6dc03a2a
Salwa Swelem <salwa@logitudeworld.com>
2021-05-06 11:30:43 +0300

    139131

---
+++ Logitude/Logitude.Test.Base/Models/BillingsPreparation/BillingVariables.cs
@@ -0,0 +1,10 @@
+﻿namespace Logitude.Test.Base.Models.BillingsPreparation
+{
+    public class BillingVariables
+    {
+        public string CurrencyEURId { get; set; }
+        public string MeasurementGRWTId { get; set; }
+        public string ChargeTypeAFTId { get; set; }
+
+    }
+}
