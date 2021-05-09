f96ce33ee15ecfe77d06dd0e67b0060870c8795c
Hadi Jaradat <hadi@logitudeworld.com>
2021-05-09 13:47:06 +0300

    FixPaymentAmount

--- Logitude/AngularModules/AngularModules/InvoiceModules/ARPayment/Components/EditTabs/ARPaymentDetailsFullAccountingTab.ts
+++ Logitude/AngularModules/AngularModules/InvoiceModules/ARPayment/Components/EditTabs/ARPaymentDetailsFullAccountingTab.ts
@@ -2242,13 +2242,16 @@ export class ARPaymentDetailsFullAccountingTab extends BaseComponent implements 
 
     }
     CalculatePaymentTotalAmount() {
-        this.AmountInPaymentCurrency = 0;
+           
+        var total = 0;
         for (let cheque of this.EntityPM.ARPaymentChequeReplicas) {
             if (!AppTool.IsNullOrEmpty(cheque.ForeignAmount)) {
-                this.AmountInPaymentCurrency += cheque.ForeignAmount;
+                total += cheque.ForeignAmount;
 
             }
         }
+        if (this.EntityPM.ARPaymentChequeReplicas.length >0)
+        this.AmountInPaymentCurrency = total;
         this.ComputeLocalAmount();
         this.SetPaymentAmount();
     }
