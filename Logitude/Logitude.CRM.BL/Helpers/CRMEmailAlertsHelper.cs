4000734452e37d037a4cca4f8cd7bdd3182161a8
Wasfi Mahmoud <Wasfi@logitudeworld.com>
2021-05-30 16:59:22 +0300

    fix

--- Logitude/Logitude.CRM.BL/Helpers/CRMEmailAlertsHelper.cs
+++ Logitude/Logitude.CRM.BL/Helpers/CRMEmailAlertsHelper.cs
@@ -21,7 +21,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks 
+using System.Threading.Tasks;
 using System.Transactions;
 
 namespace Logitude.CRM.BL.Helpers
