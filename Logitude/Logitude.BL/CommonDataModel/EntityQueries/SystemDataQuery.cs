ce430e277de17051b168f94445e88d7a3fd22f79
Abed <abed@logitudeworld.com>
2021-05-30 12:18:24 +0300

    140527
    Related Work Items: #140527

--- Logitude/Logitude.BL/CommonDataModel/EntityQueries/SystemDataQuery.cs
+++ Logitude/Logitude.BL/CommonDataModel/EntityQueries/SystemDataQuery.cs
@@ -13,6 +13,7 @@ using System.Transactions;
 using Simplog.Global.Data.GlobalModel.Repositories;
 using Simplog.Global.Data.GlobalModel.EntityPOCOs;
 using Logitude.Server.Tools.Helpers;
+using Simplog.Data.Helpers;
 
 namespace Logitude.BL.CommonDataModel.EntityQueries
 {
@@ -41,7 +42,7 @@ namespace Logitude.BL.CommonDataModel.EntityQueries
             return new SystemDataPM()
             {
                 UserId = user != null ? user.Id : "",
-                Date = DateTime.Now,
+                Date = TenantServerConfigration.GetCurrentDateTime(tenantNum),
                 Signature = user != null ? user.Contact.Signature: null,
                 SignatureHtml = user != null ? user.Contact.SignatureHtml: null,
                 UserName = user!=null ? user.Contact.EnglishName: "",
