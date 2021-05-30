c323934564d4d41d4cc8cdc5cd65fd105386e5f6
Moran Segev <moran@amital.co.il>
2021-05-30 18:17:05 +0300

    140513
    Related Work Items: #140513

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationOthers/Components/DeclarationPayment/DeclarationPaymentComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationOthers/Components/DeclarationPayment/DeclarationPaymentComponent.ts
@@ -2333,9 +2333,9 @@ export class DeclarationPaymentComponent extends BaseComponent implements OnInit
     }
 
     OnlySendPayment(params: CustomFileCreditRequestParams) {
-        var myCustomMessageProgressHelper = new CustomMessageProgressHelper();
-        myCustomMessageProgressHelper.BasicResponse = true;
-        myCustomMessageProgressHelper.StartProgress(params.PBId, 5, true);
+       // var myCustomMessageProgressHelper = new CustomMessageProgressHelper();
+      //  myCustomMessageProgressHelper.BasicResponse = true;
+      //  myCustomMessageProgressHelper.StartProgress(params.PBId, 5, true);
 
         if (AmitalGatewayUtil.Instance.IsDeclarationInUse(this.DeclarationPM.CustomFileNo, this.DeclarationPM.IsConvertedDeclaration, this.DeclarationPM.IsConnectedToUnifreight)) {
             SessionLocator.SelectedSession.StartBusyIndicator(TextCodeTranslator.Translate("Customs.General.O.UnifreightInstSentMehes"));
