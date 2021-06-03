b66f419d944f324312389e660e6c6e0fb80984fa
Fida'a Zuhour <fida@logitudeworld.com>
2021-06-03 14:19:51 +0300

    #140947 Cargo Tracking : No Data issues handling - R2

--- Logitude/Logitude.CargoTracking.Data/EntityListQueryServices/CargoTrackingShipmentListQueryService.cs
+++ Logitude/Logitude.CargoTracking.Data/EntityListQueryServices/CargoTrackingShipmentListQueryService.cs
@@ -318,6 +318,7 @@ namespace Logitude.CargoTracking.Data.EntityListQueryServices
         {
             CargoTrackingShipmentRepository repo = new CargoTrackingShipmentRepository(context);
             CargoTrackingShipment shipment = repo.GetCargoTrackingShipmentByEntityId(shipmentId, tenant);
+            if (shipment == null) throw new Exception("No shipment found");
             return CreateCargoTrackingShipmentListInstanceFromPOCO(shipment);
         }
 
