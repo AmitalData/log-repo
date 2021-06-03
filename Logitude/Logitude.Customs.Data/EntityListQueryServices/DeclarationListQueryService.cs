ab7e896297432c2863740516f46e40dcde464a6a
elisheva Nachaysi <elisheva@amital.co.il>
2021-06-03 11:19:23 +0300

    fix

--- Logitude/Logitude.Customs.Data/EntityListQueryServices/DeclarationListQueryService.cs
+++ Logitude/Logitude.Customs.Data/EntityListQueryServices/DeclarationListQueryService.cs
@@ -275,7 +275,7 @@ namespace Logitude.Customs.Data.EntityListQueryServices
                                IsPendingNotNull = true,
                            });
                 //qMyJoin = Enumerable.Empty<MyDecJoin>().AsQueryable();
-               // q1stConsignments = context.Consignments.Where(r => r.DeclarationId == "-1");
+               q1stConsignments = context.Consignments.Where(r => r.DeclarationId == "-1");
                 //q1stConsignments = Enumerable.Empty<Consignment>().AsQueryable();
             }
 
