041a5b57314642d89cb3551d12447f12b1df0596
Salwa Swelem <salwa@logitudeworld.com>
2021-06-10 14:10:23 +0300

    141355

--- Logitude/Logitude.Server.Tools/Constants/MessageType.cs
+++ Logitude/Logitude.Server.Tools/Constants/MessageType.cs
@@ -5,5 +5,9 @@
         public static long User = 1;
         public static long Task = 2;
         public static long Shipment = 3;
+        public static long Card = 4;
+        public static long Contact = 5;
+        public static long Country = 6;
+        public static long Port = 7;
     }
 }
