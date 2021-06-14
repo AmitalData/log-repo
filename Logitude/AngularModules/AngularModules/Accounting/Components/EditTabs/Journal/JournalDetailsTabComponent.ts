aad36ba1cf0f26e706c53200d8ca0d81e159cf7d
Alaa Jamal <Alaa@logitudeworld.com>
2021-06-14 07:26:25 +0000

    Apply suggestions from code review

--- Logitude/AngularModules/AngularModules/Accounting/Components/EditTabs/Journal/JournalDetailsTabComponent.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/EditTabs/Journal/JournalDetailsTabComponent.ts
@@ -1153,7 +1153,7 @@ class JournalLineModel extends BaseComponent {
 
             }
             else {
-                if (this.ForeignAmount && this.LocalAmount && !this.ChckIfCurrencyIsSameAsTenantCurrency()) {
+                if (this.ForeignAmount && this.LocalAmount && !this.CheckIfCurrencyIsSameAsTenantCurrency()) {
                     this.SetExchangeRateMnualy();
                 }
             }
@@ -1173,7 +1173,7 @@ class JournalLineModel extends BaseComponent {
         this.isLocalEntered = false;
     //    this.isRateManualy = false;
     }
-    ChckIfCurrencyIsSameAsTenantCurrency() {
+    CheckIfCurrencyIsSameAsTenantCurrency() {
         if (this.CurrencyId == SessionLocator.TenantPM.CurrencyId) return true;
     }
     SetExchangeRateMnualy() {
