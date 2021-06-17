2f45ebe360da1494c0ff95363d0c55a1d48388ba
Ahmad Abdalrazeq <a.abdalrazeq@logitudeworld.com>
2021-06-17 11:20:12 +0300

    141754

--- Logitude/Logitude.CommonDataTests/Steps/Security/GetLocalSettingsAccessSteps.cs
+++ Logitude/Logitude.CommonDataTests/Steps/Security/GetLocalSettingsAccessSteps.cs
@@ -2,54 +2,60 @@
 using Logitude.CommonDataTests.DataService;
 using Logitude.CommonTests.Models;
 using Logitude.Test.Base.Context;
+using Logitude.Test.Base.Models.Api;
 using Logitude.Test.Base.Models.Shared;
 using Logitude.Test.Base.Models.UserTenantPreparation;
 using Logitude.Test.Base.Services;
 using System;
+using System.Collections.Generic;
 using TechTalk.SpecFlow;
 
-namespace Logitude.CommonTests.Steps.Security
+namespace Logitude.CommonDataTests.Steps.Security
 {
     [Binding]
-    public class TenantSecurityAccessSteps
+    public class GetLocalSettingsAccessSteps
     {
+
         private SecurityAccessStepsContext<TenantPM> Context;
         private TenantDataService tenantDataService;
 
-        public TenantSecurityAccessSteps( SecurityAccessStepsContext<TenantPM> context)
+        public GetLocalSettingsAccessSteps(SecurityAccessStepsContext<TenantPM> context)
         {
             Context = context;
             tenantDataService = new TenantDataService();
         }
 
+
         #region Step Region
 
-        #region Get information for user's tenant
-        [When(@"get information for user's tenant")]
-        public void WhenGetInformationForUsersTenant()
+        #region Get local settings for user's tenant
+        [When(@"get local settings for user's tenant")]
+        public void WhenGetLocalSettingsForUserSTenant()
         {
             Context.FirstUserPMData = tenantDataService.GetUserTenantByToken(UserTenant.Token);
         }
 
-        [Then(@"tenant information should available")]
-        public void ThenTenantInformationShouldAvailable()
+        [Then(@"local settings should available")]
+        public void ThenLocalSettingsShouldAvailable()
         {
             Context.FirstUserPMData.Should().NotBeNull();
         }
         #endregion
 
-        #region Get information for other tenant
-        [When(@"get information for other tenant")]
-        public void WhenGetInformationForOtherTenant()
+        #region Get local settings for other tenant
+        [When(@"get local settings for other tenant")]
+        public void WhenGetLocalSettingsForOtherTenant()
         {
             Context.act = () => tenantDataService.GetUserTenantByToken(UserOtherTenant.Token);
         }
 
-        [Then(@"should receive error message say not authenticated to view company info")]
-        public void ThenShouldReceiveErrorMessageSayNotAuthenticatedToViewCompanyInfo()
+
+        [Then(@"local settings should not available")]
+        public void ThenLocalSettingsShouldNotAvailable()
         {
             Context.act.Should().ThrowExactly<AggregateException>()
-                .And.InnerExceptions[0].Message.Should().Contain("Sorry you’re not authenticated to view company info");
+                         .And.InnerExceptions[0].Message.Should().Contain("Sorry you’re not authenticated to view company info");
+
         }
         #endregion
 
