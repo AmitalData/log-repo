142cc6a1d29c154c5ced720392919fcbafcb5db4
Maheera Jazi <Maheera@logitudeworld.com>
2021-06-28 11:34:19 +0300

    #141735

--- Logitude/Logitude.BL/InvoiceModel/Tools/EntityService/APPaymentService.cs
+++ Logitude/Logitude.BL/InvoiceModel/Tools/EntityService/APPaymentService.cs
@@ -1015,13 +1015,13 @@ namespace Logitude.BL.InvoiceModel.Tools.EntityService
 
                             else if (invoiceAmountDue < 0 && invoiceAmount > 0)
                             {
-                                throw new Exception("The Amount due is not suitable to the total amount paid");
+                                throw new Exception("The Amount due is not suitable to the total amount paid, for invoice: " + invoice.InvoiceNumber);
                             }
                         }
 
                         else
                         {
-                            throw new Exception("The Amount due is not suitable to the total amount paid");
+                            throw new Exception("The Amount due is not suitable to the total amount paid, for invoice: " + invoice.InvoiceNumber);
                         }
 
 
