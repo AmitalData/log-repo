d8d215204c19a4e3b30bd0196b27b93634947cb3
Moran Segev <moran@amital.co.il>
2021-06-28 12:15:11 +0300

    71538
    Related Work Items: #71538

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/ListComponent/ListComponent.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/ListComponent/ListComponent.ts
@@ -1735,7 +1735,7 @@ else{ this.View = TextCodeTranslator.Translate("General.O.View");}
                     case "Customs.CustomsHouseType":
                     case "Customs.CustomDocumentType":
                     case "Customs.UIMessage":
-                    //case "Customs.CourierPendingReason":
+                    case "Customs.CourierPendingReason":
                     case "Customs.CurrencyType":
                     case "Customs.CustomsCountry":
                     case "Customs.ExceptionReason":
