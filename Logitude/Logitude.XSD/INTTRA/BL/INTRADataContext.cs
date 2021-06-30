9526f7aa92696fd49af0f8ac21c887a83e031620
Abdullah Beitello <AbdullahB@logitudeworld.com>
2021-07-01 00:33:17 +0300

    #141695

--- Logitude/Logitude.XSD/INTTRA/BL/INTRADataContext.cs
+++ Logitude/Logitude.XSD/INTTRA/BL/INTRADataContext.cs
@@ -66,6 +66,7 @@ namespace Logitude.XSD.INTTRA.BL
         private MoveType MoveType;
         private string MoveType_Name;
         private Contact LoggedContact;
+        private ContactRepository contactRepository;
         private Contact BranchContact;
         private Contact EmergencyContact;
         public Country FromPortCountry;
@@ -87,6 +88,7 @@ namespace Logitude.XSD.INTTRA.BL
             this.shipmentMasterDataRepository = new ShipmentMasterDataRepository(shipmentContext);
             this.Shipment = shipmentRepository.GetSingleShipment(ShipmentId, Tenant);
             this.MasterData = shipmentMasterDataRepository.GetSingleMasterData(Shipment.MasterShipmentDataId);
+            this.contactRepository = new ContactRepository(this.CommonContext);
 
 
             if (!string.IsNullOrEmpty(this.Shipment.ShipmentTypeId))
@@ -1440,8 +1442,7 @@ namespace Logitude.XSD.INTTRA.BL
 
                 if (this.Shipper != null)
                 {
-                    ContactRepository contactRepository = new ContactRepository(this.CommonContext);
-                    Contact shipperContact = contactRepository.GetSingleContact(this.Shipment.ShipperContactId,this.Tenant);
+                    Contact shipperContact = this.contactRepository.GetSingleContact(this.Shipment.ShipperContactId,this.Tenant);
 
                     INTTRA_Out.PartnerInformation item = new INTTRA_Out.PartnerInformation()
                     {
@@ -1466,8 +1467,7 @@ namespace Logitude.XSD.INTTRA.BL
                 Card myCard = (from d in CommonContext.Cards where d.Id == this.Shipment.ConsigneeId select d).FirstOrDefault();
                 if (myCard != null)
                 {
-                    ContactRepository contactRepository = new ContactRepository(this.CommonContext);
-                    Contact consigneeContact = contactRepository.GetSingleContact(this.Shipment.ConsigneeContactId, this.Tenant);
+                    Contact consigneeContact = this.contactRepository.GetSingleContact(this.Shipment.ConsigneeContactId, this.Tenant);
 
                     INTTRA_Out.PartnerInformation item = new INTTRA_Out.PartnerInformation()
                     {
