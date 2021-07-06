35c3b507e2adcf27df36b8f282246dccab72921d
Samar Abu Eid <Samar@logitudeworld.com>
2021-07-06 08:17:39 +0300

    142753
    Related Work Items: #142753

--- Logitude/Logitude.BL/CommonDataModel/EntityQueries/ProductItemQuery.cs
+++ Logitude/Logitude.BL/CommonDataModel/EntityQueries/ProductItemQuery.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Simplog.Server.Infrastructure.Helpers;
 
 namespace Logitude.BL.CommonDataModel.EntityQueries
 {
@@ -46,7 +47,11 @@ namespace Logitude.BL.CommonDataModel.EntityQueries
                     Description = entityPoco.Description,
                     Name = entityPoco.Name,
                     SearchFields = entityPoco.SearchFields,
-                    Brand = entityPoco.Brand,                    
+                    Brand = entityPoco.Brand,              
+                    ASIN = entityPoco.ASIN,
+                    UPC = entityPoco.UPC,
+                    OriginCountryId = entityPoco.OriginCountryId,
+                    OriginCountryName = entityPoco.OriginCountry == null ? null : entityPoco.OriginCountry.EnglishName,
                 };
 
                 HTSCodeQuery hTSCodeQuery = new HTSCodeQuery(tenant);
@@ -70,13 +75,17 @@ namespace Logitude.BL.CommonDataModel.EntityQueries
                                                    Description = entity.Description,
                                                    Name = entity.Name,
                                                    SearchFields = entity.SearchFields,
+                                                   ASIN = entity.ASIN,
+                                                   UPC = entity.UPC,
+                                                   OriginCountryId = entity.OriginCountryId,
+                                                   OriginCountryName = entity.OriginCountry == null ? null : entity.OriginCountry.EnglishName,
                                                };
             return result;
         }
 
         public List<ProductItemPM> GetProductItemPMsByCustomerId(string customerId,int tenant)
         {
-            List<ProductItemPM> productItems = (from a in repository.context.ProductItems
+            List<ProductItemPM> productItems = (from a in repository.context.ProductItems.Include("OriginCountry")
                                                 where a.Tenant == tenant && a.CustomerId == customerId
                                                 select new ProductItemPM()
                                                 {
--- Logitude/Logitude.BL/ShipmentsModel/EntityPMs/ShipmentProductItemPM.cs
+++ Logitude/Logitude.BL/ShipmentsModel/EntityPMs/ShipmentProductItemPM.cs
@@ -21,6 +21,10 @@ namespace Logitude.BL.ShipmentsModel.EntityPMs
         public bool ApprovedByCustomer { get; set; }
         public string Brand { get; set; }
         public string Name { get; set; }
+        public string ASIN { get; set; }
+        public string UPC { get; set; }
+        public string OriginCountryId { get; set; }
+        public string OriginCountryName { get; set; }
         public ChangeSetOperation ChangeSetOp { get; set; }
     }
 }
