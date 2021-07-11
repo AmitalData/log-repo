2df597a2cc9d9200a9945d248f330af79df71988
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2021-07-11 18:48:13 +0300

    fix tasks app

--- Logitude/AngularModules/AngularModules/TasksApp/Components/TasksAppComponent.ts
+++ Logitude/AngularModules/AngularModules/TasksApp/Components/TasksAppComponent.ts
@@ -63,6 +63,7 @@ import { Component, OnChanges, AfterViewInit } from "@angular/core";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { LoggedUser } from "collaboration-tool-core";
+import { SessionLocator } from '../../Infrastructure/Utilities/SessionLocator';
 import TasksList from "collaboration-tool-tasks-list";
 
 @Component({
@@ -89,7 +90,7 @@ export class TasksAppComponent extends BaseComponent implements OnChanges, After
         }
 
         const props: any = {
-            loggedUserEmail: LoggedUser.getEmail()
+            loggedUserEmail: SessionLocator.LoggedUserPM.Email
         };
 
         ReactDOM.render(
