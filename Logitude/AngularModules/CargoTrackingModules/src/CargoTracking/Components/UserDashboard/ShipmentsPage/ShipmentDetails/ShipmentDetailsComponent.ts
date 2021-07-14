8391466adba887ec281c9bd037c3e68219b7fb4c
Fida'a Zuhour <fida@logitudeworld.com>
2021-07-14 15:41:26 +0300

    #143452 fix Exception date and when null

--- Logitude/AngularModules/CargoTrackingModules/src/CargoTracking/Components/UserDashboard/ShipmentsPage/ShipmentDetails/ShipmentDetailsComponent.ts
+++ Logitude/AngularModules/CargoTrackingModules/src/CargoTracking/Components/UserDashboard/ShipmentsPage/ShipmentDetails/ShipmentDetailsComponent.ts
@@ -390,7 +390,7 @@ export class ShipmentDetailsComponent implements AfterViewInit
                 newCard.Description = milstone.Notes;
                 newCard.IsDimmed = milstone.IsEstimation && !milstone.Done;
                 newCard.IsActive = milstone.Id + '' == this.Shipment.ShipmentList.CurrentMilestoneCode;
-                newCard.HasWarning = milstone.IsCurrent && CurrentMilestoneExceptions.trim().length > 0;
+                newCard.HasWarning = milstone.IsCurrent && CurrentMilestoneExceptions.trim().length > 1;
                 newCard.WarningMessage = newCard.HasWarning ? CurrentMilestoneExceptions?.split(',')[1] : null;
                 newCard.WarningDate = newCard.HasWarning ? this.datePipe.transform(CurrentMilestoneExceptions?.split(',')[0], 'dd/MM/yyyy, HH:mm'): null;
                 return newCard;
