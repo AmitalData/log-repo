4d9d7dcdf6fb956982046b0e602fc1f4e690f5d2
elisheva Nachaysi <elisheva@amital.co.il>
2021-07-15 18:58:55 +0300

    revrse test

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationOthers/Components/DeclarationPayment/DeclarationPaymentComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationOthers/Components/DeclarationPayment/DeclarationPaymentComponent.ts
@@ -1346,8 +1346,7 @@ export class DeclarationPaymentComponent extends BaseComponent implements OnInit
             item.Line = line;
             item.SequenceNumeric = seq;
             this.paymentPM.AddDeclarationPaymentMethod(item);
-            //&& !(this.sumBtl != null && this.sumBtl > 0)
-            if (this.BetweenMinAndMax && isLoad ) {
+            if (this.BetweenMinAndMax && isLoad && !(this.sumBtl != null && this.sumBtl > 0)) {
                 this.paymentMethodModelMax = new PaymentMethodModel(item, this);
             }
             else {
