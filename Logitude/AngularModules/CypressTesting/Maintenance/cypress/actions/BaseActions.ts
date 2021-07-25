fa7c5bc30cb723a128a086f8034c4f40d1bc4a71
Wasfi Mahmoud <Wasfi@logitudeworld.com>
2021-07-25 17:27:44 +0300

    fix vatTypes error + change tags

--- Logitude/AngularModules/CypressTesting/Maintenance/cypress/actions/BaseActions.ts
+++ Logitude/AngularModules/CypressTesting/Maintenance/cypress/actions/BaseActions.ts
@@ -9,11 +9,9 @@ import { AddressDetails } from 'cypress/models/AddressDetails';
 import { AddressSelectors } from "../selectors/AddressSelectors";
 
 export function Search(searchFieldValue) {
-    cy.wait(3000)
     cy.get("body").then($body => {
-        if ($body.find(BaseSelectors.RedButton).length > 0) {
-            cy.Click(BaseSelectors.RedButton + BaseSelectors.LastElement, null);
-
+        if ($body.find(".MessageWindow").length > 0) {
+            cy.Click(BaseSelectors.MessageErrorRedButton, null);
         }
     });
     DefineViewsGetByFiltersRequest(searchFieldValue);
