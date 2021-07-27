2bf0a4f21d1d3d46b26b2e6ad5f0440d58af732b
Atara Oster <Atara@amital.co.il>
2021-07-27 09:04:51 +0300

    #143273

--- Logitude/Logitude.Customs.Data/EntityMapping/ClientMap.cs
+++ Logitude/Logitude.Customs.Data/EntityMapping/ClientMap.cs
@@ -82,6 +82,8 @@ namespace Logitude.Customs.Data.EntityMapping
             this.Property(t => t.FacilitationTypeCode).HasColumnName("FacilitationTypeCode").HasMaxLength(15).IsUnicode(false);
 
             this.Property(t => t.NationalIdentificationNumber).HasColumnName("NationalIdentificationNumber").HasMaxLength(25).IsUnicode(false);
+
+            this.Property(t => t.IsExportPoaActive).HasColumnName("IsExportPoaActive");
         }
     }
 }
