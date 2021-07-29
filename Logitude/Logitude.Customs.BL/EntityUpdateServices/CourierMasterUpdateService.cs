ae816363fddbce7d806bbb45bb93a1d71d8bb69e
Atara Oster <Atara@amital.co.il>
2021-07-29 15:26:21 +0300

    #144520

--- Logitude/Logitude.Customs.BL/EntityUpdateServices/CourierMasterUpdateService.cs
+++ Logitude/Logitude.Customs.BL/EntityUpdateServices/CourierMasterUpdateService.cs
@@ -356,13 +356,13 @@ namespace Logitude.Customs.BL.EntityUpdateServices
                         GSTRING3 = entityPM.Id,
 
                         DEBUG = "F",
-                        DONEOPERATION = "D",
+                        DONEOPERATION = "A",
                         QUEUEMANAGEMENT = true,
                           GGGQCPMs = new List<GGGQCPM>() {
                                new GGGQCPM(){ 
                                     ChangeSetOp= ChangeSetOperation.Insert,
                                      FIELDID= "TASK_PARAM",
-                                      FIELDVAL= "PROC=A1468"
+                                      FIELDVAL= "PROC=A1468 "+(char)27+"; PRIMARY_NUM="+entityPM.Id
                                }
                           }
                         
