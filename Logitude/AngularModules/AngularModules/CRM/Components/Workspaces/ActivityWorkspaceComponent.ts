5e98d753a7e9632b8d704157ad63ba49ba084896
Wasfi Mahmoud <Wasfi@logitudeworld.com>
2021-08-01 15:54:21 +0300

    add data-cy to CRM

--- Logitude/AngularModules/AngularModules/CRM/Components/Workspaces/ActivityWorkspaceComponent.ts
+++ Logitude/AngularModules/AngularModules/CRM/Components/Workspaces/ActivityWorkspaceComponent.ts
@@ -33,6 +33,7 @@ export class ActivityWorkspaceComponent extends BaseComponent {
     public DataContext = this;
     public QuickSearchItems: ActivityList[] = [];
     private CurrentSession = SessionLocator.SelectedSession;
+    public IsActivityRecentListLoaded: boolean = false;
     constructor() {
         super(); 
         this.ChartID = "ChartID_" + this.CurrentSession.GetChartId();
@@ -211,7 +212,7 @@ export class ActivityWorkspaceComponent extends BaseComponent {
     set SelectedRecordsTypeFilter(value: CodeNameClass) {
         if (this.selectedRecordsTypeFilter != value) {
             this.selectedRecordsTypeFilter = value;
-
+            this.IsActivityRecentListLoaded = false;
             this.RecordsTypeFilterCode = value == null ? "S" : value.Code;
             this.BusinessUnitFilterCode = "M";
             this.CreatedByTypeFilterCode = "M";
@@ -470,6 +471,7 @@ export class ActivityWorkspaceComponent extends BaseComponent {
     public UpcomingActivitiesCount: number = 0;
     public UpcomingActivitiesList: UpcomingActivityItem[];
     public LoadUpcomingEntities() {
+        this.IsActivityRecentListLoaded = false;
         this.myDomainService.GetUpcomigActivities(this.OwnerId, this.BusinessUnitId, this.selectedActivityFilter, this.RecordsTypeFilterCode).subscribe((myResult:any) => {
             if (myResult == null) {
                 this.UpcomingActivitiesList = [];
@@ -498,6 +500,8 @@ export class ActivityWorkspaceComponent extends BaseComponent {
         //    var itemViewModel: UpcomingActivityItem = new UpcomingActivityItem(item, this, null);
         //    this.UpcomingActivitiesList.push(itemViewModel);
         //});
+        this.IsActivityRecentListLoaded = true;
+
     }
 
     // Chart
