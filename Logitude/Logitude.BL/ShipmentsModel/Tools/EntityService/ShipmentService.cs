e4729f55d0090a9378ca70d370ec78498542db24
Wesam Amarneh <wesam@logitudeworld.com>
2021-08-11 12:05:56 +0300

    Issues with Shipment Send Interface
    Related Work Items: #145121

--- Logitude/Logitude.BL/ShipmentsModel/Tools/EntityService/ShipmentService.cs
+++ Logitude/Logitude.BL/ShipmentsModel/Tools/EntityService/ShipmentService.cs
@@ -2356,6 +2356,7 @@ namespace Logitude.BL.ShipmentsModel.Tools.EntityService
                     otherObjectTableName = "Shipment";
                 }
 
+                MapMainCarriageLegsForAutomation(); //temp Solution
                 GeneralEntityChangeService generalEntityChangeService = new GeneralEntityChangeService();
                 object externalEntity = (entityPM.ShipmentLevelCode != "H" && entityMasterData != null) ? this.entityPM : null;
                 if (type == "OnCreate")
@@ -2405,6 +2406,12 @@ namespace Logitude.BL.ShipmentsModel.Tools.EntityService
                     #endregion
                 }
             }
+        }
+
+        private void MapMainCarriageLegsForAutomation()
+        {
+            ShipmentQuery shipmentQuery = new ShipmentQuery(entityPM.Tenant);
+            shipmentQuery.MapMainCarriageLegsForAPI(entityPM);
         }
 
         private ShipmentChangeTracking BuildShipmentChangeTracking()
