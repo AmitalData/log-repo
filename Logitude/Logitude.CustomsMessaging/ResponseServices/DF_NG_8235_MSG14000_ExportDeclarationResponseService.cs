fdbddb9f53c1a3c446a951e18f74171a1be12277
Atara Oster <Atara@amital.co.il>
2021-08-15 16:28:00 +0300

    #145228

--- Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_8235_MSG14000_ExportDeclarationResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_8235_MSG14000_ExportDeclarationResponseService.cs
@@ -1,32 +1,8 @@
-﻿using Logitude.AmitalMessaging.Utils;
-using Logitude.Customs.Def.EntityPMs;
-using Logitude.Customs.BL.EntityQueryServices;
-using Logitude.Customs.BL.EntityUpdateServices;
-using Logitude.Customs.Def.Messaging.Customs;
-using Logitude.Customs.BL.Messaging.LogitudeClient.DeclarationErrorPointer;
-using Logitude.Customs.BL.TraceEvents;
-using Logitude.Customs.Data;
-using Logitude.Customs.Data.EntityKeys;
-using Logitude.Customs.Data.EntityLists;
-using Logitude.Customs.Data.Repsitories;
-using Logitude.CustomsMessaging.Common.Gen;
+﻿
 using Logitude.CustomsMessaging.Common.RequestParams;
 using Logitude.CustomsMessaging.Common.ResponseData;
-using Logitude.CustomsMessaging.MessagingServices;
-using Logitude.CustomsMessaging.ResponseServices.DeclarationErrorPointer;
-using Logitude.Server.Tools.Helpers;
-using Simplog.Data.InfrastructureModel.Repositories;
-using Simplog.Server.Infrastructure;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using UnifreightIIG.Common.MessageLib.Collateral;
-using Logitude.Customs.BL.TraceEvents;
-using Logitude.Customs.BL.Messaging.LogitudeClient.DeclarationErrorPointer.DBWCO;
 using Logitude.Customs.BL.BL;
-using Unifreight.BL.EntityQueryServices;
-using Unifreight.Data.AmitalModel;
 using System.Xml.Serialization;
 using System.Xml.Linq;
 using UnifreightIIG.Common.ExportDeclarationAmendmentRequestMsgRequestServiceReference;
@@ -36,22 +12,6 @@ namespace Logitude.CustomsMessaging.ResponseServices
     public class DF_NG_8235_MSG14000_ExportDeclarationResponseService :
         ResponseServiceBase<INF_MSG_GenericResponseData, DF_NG_8237_MSG14003_ExportDeclarationAmendmentReplyMsg, GenericRequestParams>
     {
-        DeclarationPM _MyDeclarationPM;
-        private bool _FastDelete;
-        //private List<SupplierInvoiceItemsTaxesModPM> _SupplierInvoiceItemsTaxesModificationPMList;
-        //public UnifreightIIG.Common.CommonIIGInterface.IResponseHeaderOrFault _ResponseHeaderExeption;
-        public bool _IsSubmitDeclarationResponse { get; set; }
-        public bool _IsRetrieveDeclarationResponse { get; set; }
-        decimal? totGeneralTaxCalc = 0;
-        decimal? totPurchaseCalc = 0;
-        decimal? totVatCalc = 0;
-        decimal? generalTax = 0;
-        decimal? purchase = 0;
-        decimal? vat = 0;
-
-        DeclarationError _MyDeclarationError;
-        decimal? _TotalBtlCoverageNISSum = 0;
-
         public override void OnRequestFail(DF_NG_8237_MSG14003_ExportDeclarationAmendmentReplyMsg customResponse, GenericRequestParams requestParams)
         {
             if (!String.IsNullOrWhiteSpace(requestParams.AppicationId))
