f8e51e828f7331ba23e7f0d11f84a18a3e454a4f
sohaib <sohaib@amital.co.il>
2021-08-16 17:21:29 +0300

    #145478

--- Logitude/Logitude.Customs.BL/EntityUpdateServices/DeclarationPendingUpdateService.cs
+++ Logitude/Logitude.Customs.BL/EntityUpdateServices/DeclarationPendingUpdateService.cs
@@ -27,6 +27,12 @@ namespace Logitude.Customs.BL.EntityUpdateServices
         public bool IsUpdateComposition { get; set; }
         protected override void OnCreating(DeclarationPendingPM entityPM, DeclarationCourierStatusPM entityParentPM)
         {
+            CourierPendingReasonRepository courierPendingReasonRepositoryRepository = new CourierPendingReasonRepository(entityPM.Tenant);
+            Boolean isActive = courierPendingReasonRepositoryRepository.IsActive(entityPM.CourierPendingReasonCode, entityPM.Tenant);
+            if (!isActive)
+            {
+                throw new Exception($"מיסיבת Pending");
+            }
             if (entityParentPM != null)
             {
                 entityPM.DeclarationID = entityParentPM.DeclarationId;
@@ -57,15 +63,20 @@ namespace Logitude.Customs.BL.EntityUpdateServices
                     _MyDeclarationCourierStatusPM.CourierPendingReasonList = null;
                     foreach (var declarationPending in declarationPendingPMList)
                     {
-                        if (declarationPending.Status == "A")
+                        CourierPendingReasonRepository courierPendingReasonRepositoryRepository = new CourierPendingReasonRepository(entityPM.Tenant);
+                        Boolean isActive = courierPendingReasonRepositoryRepository.IsActive(declarationPending.CourierPendingReasonCode, declarationPending.Tenant);
+                        if (isActive)
                         {
-                            if (_MyDeclarationCourierStatusPM.CourierPendingReasonList == null)
+                            if (declarationPending.Status == "A")
                             {
-                                _MyDeclarationCourierStatusPM.CourierPendingReasonList = declarationPending.CourierPendingReasonCode;
-                            }
-                            else
-                            {
-                                _MyDeclarationCourierStatusPM.CourierPendingReasonList = string.Concat(_MyDeclarationCourierStatusPM.CourierPendingReasonList, ",", declarationPending.CourierPendingReasonCode);
+                                if (_MyDeclarationCourierStatusPM.CourierPendingReasonList == null)
+                                {
+                                    _MyDeclarationCourierStatusPM.CourierPendingReasonList = declarationPending.CourierPendingReasonCode;
+                                }
+                                else
+                                {
+                                    _MyDeclarationCourierStatusPM.CourierPendingReasonList = string.Concat(_MyDeclarationCourierStatusPM.CourierPendingReasonList, ",", declarationPending.CourierPendingReasonCode);
+                                }
                             }
                         }
                     }
