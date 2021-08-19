d0b88780b66adc955335b6af3a8b1f3b47ca646e
itzik M <itzik@amital.co.il>
2021-08-19 13:33:21 +0000

    #144480

--- Logitude/Logitude.Update/SandBox/FormAccountingTester.cs
+++ Logitude/Logitude.Update/SandBox/FormAccountingTester.cs
@@ -118,18 +118,19 @@ namespace Logitude.Update.SandBox
             ///RevaluationBatch revaluationBatch = new RevaluationBatch();
             //revaluationBatch.RunAllOpenRevaluations(28);
             //return;
+            List<string> Last_journalBufferKeys = new List<string>();
+            JournalApproveService.WorkWithoutQueue(62, "1-16309384", ref Last_journalBufferKeys);
+
+            return;
+
+            var myGateWayTester = new GateWayTester();
+            myGateWayTester.ImmediateYearTransferthod(20, 62);
+            return;
 
             YearTest();
             return;
             var myReverseEngineerCashBook = new ReverseEngineerCashBook(69);
             myReverseEngineerCashBook.CheckDbIntegrity();
-            return;
-            var myGateWayTester = new GateWayTester();
-            myGateWayTester.ImmediateYearTransferthod(16, 4);
-            return;
-            List<string> Last_journalBufferKeys = new List<string>();
-            JournalApproveService.WorkWithoutQueue(18, "1-12164556", ref Last_journalBufferKeys);
-
             return;
             GLaccountCreateTester();
 
