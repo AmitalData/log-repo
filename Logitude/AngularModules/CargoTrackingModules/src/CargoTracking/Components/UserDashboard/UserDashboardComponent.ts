c765d9833718eaa85d7e3615965e2ade9213dd26
Abdullah Dawoud <Abdullah@logitudeworld.com>
2021-08-22 12:52:40 +0300

    #145158

--- Logitude/AngularModules/CargoTrackingModules/src/CargoTracking/Components/UserDashboard/UserDashboardComponent.ts
+++ Logitude/AngularModules/CargoTrackingModules/src/CargoTracking/Components/UserDashboard/UserDashboardComponent.ts
@@ -98,7 +98,7 @@ export class UserDashboardComponent implements AfterViewInit
             var splitted = userName.split(" ");
             if (splitted.length == 1)
                 this.UserNameFirstLetters = splitted[0][0];
-            else if (splitted.length == 2)
+            else if (splitted.length >= 2)
                 this.UserNameFirstLetters = splitted[0][0] + splitted[1][0];
             else if (splitted.length == 0)
                 this.UserNameFirstLetters = "Aa";
