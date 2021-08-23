5be9abb778f3df8aede3c81a002c334471e2368f
Ahmad Abdalrazeq <a.abdalrazeq@logitudeworld.com>
2021-08-23 07:58:33 +0300

    conflict

--- Logitude/Logitude.Test.Base/Models/Shared/Urls.cs
+++ Logitude/Logitude.Test.Base/Models/Shared/Urls.cs
@@ -46,7 +46,9 @@
         public static string CRMDomainControllerInserNewTicket = "CRMDomain/InserNewTicket";
         public static string APInvoicesController = "APInvoices";
         public static string ARInvoicesController = "ARInvoices";
+        public static string GetTenantTariffSetting = "TariffDomain/GetTenantTariffSetting";
         public static string CargoTrackingSearchController = "CargoTrackingSearch";
+        public static string Tariffsettings = "tariffsettings";
         public static string TMProjectViewsGetByFilters = "TMProjectViews/GetByFilters";
         public static string TmprojectsController = "Tmprojects";
         public static string TMBudgetViewsGetByFilters = "TMBudgetViews/GetByFilters";
