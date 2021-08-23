855842ec2541d0d829a269a65a09c8ed26cc7c5a
elisheva Nachaysi <elisheva@amital.co.il>
2021-08-23 08:53:02 +0300

    remove var setting = settingService.GetSettingByTenantN

--- Logitude/Logitude.BL/CommonDataModel/EntityQueries/DocumentsFilingQuery.cs
+++ Logitude/Logitude.BL/CommonDataModel/EntityQueries/DocumentsFilingQuery.cs
@@ -2234,14 +2234,14 @@ namespace Logitude.BL.CommonDataModel.EntityQueries
             FollowUpRepository followUpRepository = new FollowUpRepository(tenant);
             List<FollowUp> FollowUps=null;
 
-            CustomsSettingQueryService settingService = new CustomsSettingQueryService(tenant);
-            var setting = settingService.GetSettingByTenantN(tenant) ;
+         //   CustomsSettingQueryService settingService = new CustomsSettingQueryService(tenant);
+          //  var setting = settingService.GetSettingByTenantN(tenant) ;
             var resMode = new { DefaultValue = "" };
-            if (setting.IsConnectedToUniFreight)
-            {
+           // if (setting.IsConnectedToUniFreight)
+           // {
                 FollowUps = followUpRepository.GetFollowUps(tenant).ToList();
 
-            }
+           // }
 
             DocumentsFilingMetaDataValueQuery documentsFilingMetaDataValueQuery = new DocumentsFilingMetaDataValueQuery(tenant);
             //List<DocumentsFilingMetaDataValuePM> documentsFilingMetaDataValuesList = documentsFilingMetaDataValueQuery.GetDocumentsFilingMetaDataValuePMsByTenantAndDocumentIds(tenant, externalDocumentPMs.Select(a => a.Id).ToArray()).ToList();
