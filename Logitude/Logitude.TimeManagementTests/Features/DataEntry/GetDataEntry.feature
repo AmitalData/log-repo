9e7074aad9e79d2371b98b9796930fc1f0cfbd4b
Ahmad Abdalrazeq <a.abdalrazeq@logitudeworld.com>
2021-08-24 08:11:56 +0300

    get data entry

---
+++ Logitude/Logitude.TimeManagementTests/Features/DataEntry/GetDataEntry.feature
@@ -0,0 +1,7 @@
+﻿@Pre-Prepare-DataEntry
+Feature: Get Data Entry
+	We want to get Data Entry.
+
+Scenario: Get data entry
+	When get data entry
+	Then data entry should be avaliable
---
+++ Logitude/Logitude.TimeManagementTests/Services/DataEntryDataPreparation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Logitude.Test.Base.Models.Api;
+using Logitude.Test.Base.Models.Shared;
+using Logitude.Test.Base.Models.UserTenantPreparation;
+using Logitude.Test.Base.Services;
+using Logitude.TimeManagementTests.Models;
+using Logitude.TimeManagementTests.Models.Builders;
+
+namespace Logitude.TimeManagementTests.Services
+{
+    public class DataEntryDataPreparation
+    {
+        public void Prepar()
+        {
+            try
+            {
+                ApiResponse<TimeManagementAPIHelper> response = APICaller.CallPut<TimeManagementAPIHelper>(GetValidDataEntryPM(), Urls.TimeManagementDomainController, UserTenant.Token);
+                GetValidDataEntry(response.Data);
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException("Failed Creating Data Entry Before Feature Run :" + e.InnerException);
+            }
+        }
+
+        private TimeManagementAPIHelper GetValidDataEntryPM()
+        {
+            return new TimeManagementAPIHelperBuilder()
+                .WithDefualtValues()
+                .LocationCode("Office")
+                .ItemsPM(GetTMEmployeeTime())
+                .Build();
+        }
+
+        private TMEmployeeTimePM GetTMEmployeeTime()
+        {
+            return new TMEmployeeTimePMBuilder().WithDefualtValues()
+                .LocationCode("Office")
+                .Description("pre specflow desc")
+                .TimeInMinutes(45)
+                .DateOfWork(DateTime.Now)
+                .Build();
+        }
+
+        private void GetValidDataEntry(TimeManagementAPIHelper dataEntry)
+        {
+            TimeManagementData.DataEntry = dataEntry;
+        }
+    }
+}
