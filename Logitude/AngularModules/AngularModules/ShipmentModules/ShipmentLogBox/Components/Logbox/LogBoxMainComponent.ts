16c0b15f4d91a2ff27cb0cc3bdad119fa4d48da8
Heba Mubarak <heba@logitudeworld.com>
2021-08-24 09:27:40 +0300

    fix

--- Logitude/AngularModules/AngularModules/ShipmentModules/ShipmentLogBox/Components/Logbox/LogBoxMainComponent.ts
+++ Logitude/AngularModules/AngularModules/ShipmentModules/ShipmentLogBox/Components/Logbox/LogBoxMainComponent.ts
@@ -431,7 +431,7 @@ export class LogBoxMainComponent implements OnInit, AfterViewInit {
             this.GetQueryColumn("ShipperName", 'Text', this.isPrivateLabel ? 'Supplier / Consignee' : 'Supplier')
         );
 
-        if (true) { 
+        if (this.isPrivateLabel) { 
             this.DisplayAgentColumn();
         }
 
