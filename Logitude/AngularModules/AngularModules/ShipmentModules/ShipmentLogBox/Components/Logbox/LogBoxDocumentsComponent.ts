918a979b516f188cd8fac18c280e0450d82212fd
Heba Mubarak <heba@logitudeworld.com>
2021-08-29 16:21:47 +0300

    Fix Main Carriage Dates Labels

--- Logitude/AngularModules/AngularModules/ShipmentModules/ShipmentLogBox/Components/Logbox/LogBoxDocumentsComponent.ts
+++ Logitude/AngularModules/AngularModules/ShipmentModules/ShipmentLogBox/Components/Logbox/LogBoxDocumentsComponent.ts
@@ -75,8 +75,8 @@ export class LogBoxDocumentsComponent extends BaseComponent implements OnInit, A
     RefreshTimer: any;
     private CurrentSession = SessionLocator.SelectedSession;
 
-    public MainCarriageTA;
-    public MainCarriageTD;
+    public MainCarriageTA: string = "";
+    public MainCarriageTD: string = "";
     public MainCarriageTALabel: string = "";
     public MainCarriageTDLabel: string = "";
 
@@ -235,6 +235,9 @@ export class LogBoxDocumentsComponent extends BaseComponent implements OnInit, A
         } else if (this.ShipmentPM.MainCarriageETA != null) {
             this.MainCarriageTA = this.ShipmentPM.MainCarriageETA;
             this.MainCarriageTALabel = 'ETA:';
+        } else {
+            this.MainCarriageTA = "";
+            this.MainCarriageTALabel = "";
         }
     }
 
@@ -245,6 +248,9 @@ export class LogBoxDocumentsComponent extends BaseComponent implements OnInit, A
         } else if (this.ShipmentPM.MainCarriageETD != null) {
             this.MainCarriageTD = this.ShipmentPM.MainCarriageETD;
             this.MainCarriageTDLabel = 'ETD:';
+        } else {
+            this.MainCarriageTD = '';
+            this.MainCarriageTDLabel = '';
         }
     }
 
