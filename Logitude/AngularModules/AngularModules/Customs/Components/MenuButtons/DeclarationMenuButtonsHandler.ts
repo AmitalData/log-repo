3c00ced71837b24f59193bc12e27b6df128e2ad0
Atara Oster <Atara@amital.co.il>
2021-08-29 12:09:57 +0300

    #145545

--- Logitude/AngularModules/AngularModules/Customs/Components/MenuButtons/DeclarationMenuButtonsHandler.ts
+++ Logitude/AngularModules/AngularModules/Customs/Components/MenuButtons/DeclarationMenuButtonsHandler.ts
@@ -1266,8 +1266,8 @@ export class DeclarationMenuButtonsHandler implements OnDestroy {
             EntityPM: this.EntityPM,
         };
         var logWindow = new LogitudeWindow();
-        logWindow.Width = 1000;
-        logWindow.Height = 700;
+        logWindow.Width = 770;
+        logWindow.Height = 530;
         logWindow.Title = "סגירת הצהרה";
         logWindow.WindowArgs = args;
         logWindow.ShowCloseButton = true;
