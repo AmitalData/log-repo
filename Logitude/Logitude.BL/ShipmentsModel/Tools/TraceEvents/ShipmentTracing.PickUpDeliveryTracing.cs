1642045bc14203575aced2c73e4cc9d01e870064
Abdullah Beitello <AbdullahB@logitudeworld.com>
2021-09-01 08:55:47 +0300

    #145935

--- Logitude/Logitude.BL/ShipmentsModel/Tools/TraceEvents/ShipmentTracing.PickUpDeliveryTracing.cs
+++ Logitude/Logitude.BL/ShipmentsModel/Tools/TraceEvents/ShipmentTracing.PickUpDeliveryTracing.cs
@@ -702,7 +702,7 @@ namespace Logitude.BL.ShipmentsModel.Tools.TraceEvents
     
         private bool IsETAOrETDEditedAfterDeleteFirstDeilvery(ShipmentDeliveryPM shipmentDeliveryPM, ShipmentPickUpDelivery itemPOCO)
         {
-            bool IsEventNotExist = !IsEventExistInTraceEvents("PCAR", itemPOCO.ETA) && !IsEventExistInTraceEvents("PCAR", itemPOCO.ETD);
+            bool IsEventNotExist = !IsEventExistInTraceEvents("DLAR", itemPOCO.ETA) && !IsEventExistInTraceEvents("DLAR", itemPOCO.ETD);
 
             if (IsETDLegDateEdited(null, shipmentDeliveryPM, itemPOCO) && IsEventNotExist)
                 return true;
@@ -721,7 +721,7 @@ namespace Logitude.BL.ShipmentsModel.Tools.TraceEvents
 
         private bool IsETAOrETDEditedAfterDeleteFirstPickup(ShipmentPickUpPM shipmentPickUpPM, ShipmentPickUpDelivery itemPOCO)
         {
-            bool IsEventNotExist = !IsEventExistInTraceEvents("DLAR", itemPOCO.ETA) && !IsEventExistInTraceEvents("DLAR", itemPOCO.ETD);
+            bool IsEventNotExist = !IsEventExistInTraceEvents("PCAR", itemPOCO.ETA) && !IsEventExistInTraceEvents("PCAR", itemPOCO.ETD);
 
             if (IsETDLegDateEdited(shipmentPickUpPM, null, itemPOCO) && IsEventNotExist)
                 return true;
