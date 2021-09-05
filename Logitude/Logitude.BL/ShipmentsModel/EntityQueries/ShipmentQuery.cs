e18fcc586d33d9b78cfc67eb8aa89a13d7b68e16
Heba Mubarak <heba@logitudeworld.com>
2021-09-05 13:37:02 +0300

    Accounting closed by user for houses shipments

--- Logitude/Logitude.BL/ShipmentsModel/EntityQueries/ShipmentQuery.cs
+++ Logitude/Logitude.BL/ShipmentsModel/EntityQueries/ShipmentQuery.cs
@@ -3971,6 +3971,7 @@ namespace Logitude.BL.ShipmentsModel.EntityQueries
                 shipmentPM.ArrivalNoticeSentDate = entityComputedFields.ArrivalNoticeSent;
                 shipmentPM.T1ReceivedDate = entityComputedFields.T1Received;
                 shipmentPM.FirstPickupATD = entityComputedFields.FirstPickupATD;
+                shipmentPM.AccountingClosedByUserId = entityComputedFields.AccountingClosedByUserId;
             }
         }
 
@@ -4141,12 +4142,17 @@ namespace Logitude.BL.ShipmentsModel.EntityQueries
 
                     ShipmentPM shipmentPM = new ShipmentPM();
 
+
                     shipmentPM = MapShipmentToShipmentPM(shipmentPM, shipment, null, masterData, true);
                     ShipmentPM securedPM = new ShipmentPM();
                     securedPM = SecuredMapping.GetMappedPM(shipmentPM, securedPM, "Shipment", tenant);
 
                     ShipmentPM returnShipment = BranchPermitionsFilter.AddUserBranchRestrictionFilters(new QueryOperations(), securedPM, tenant);//securedPM;
                     returnShipment = ProductPermitionsFilter.AddUserProductRestrictionFilters(new QueryOperations(), securedPM, tenant);
+
+                    
+
+
                     var CLoudData = (from a in repository.context.ShipmentAdditionalCloudDatas
                                      where a.Id == shipment.Id
                                      select a).FirstOrDefault();
@@ -4171,6 +4177,8 @@ namespace Logitude.BL.ShipmentsModel.EntityQueries
                         returnShipment.PaymentDateTime = CLoudData.PaymentDateTime;
                         returnShipment.IsPaymentRequired = CLoudData.IsPaymentRequired;
                     }
+ 
+
 
                     MapShipmentComputedFields(returnShipment);
 
