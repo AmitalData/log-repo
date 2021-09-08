b64cd1cd960c570902460db720d990d9f3f99987
Amer Aldghameen <Amer@logitudeworld.com>
2021-09-08 13:11:10 +0300

    Create Project

---
+++ Logitude/Logitude.FullAccounting/Hooks/BeforeFeatureRun.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace Logitude.FullAccounting.Hooks
+{
+    [Binding]
+    public class BeforeFeatureRun
+    {
+        [BeforeFeature("Pre-Prepare")]
+        public static void SetUpPrepareDataBeforeFeatureRun()
+        {
+
+        }
+    }
+}
