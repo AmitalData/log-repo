9f089c666ee1eb48bec8ca5697064f0561c5f527
Atara Oster <Atara@amital.co.il>
2021-09-09 13:59:30 +0300

    #145613

--- Logitude/AngularModules/AngularModules/Customs/Components/MenuButtons/ContainerizationMenuButtonsHandler.ts
+++ Logitude/AngularModules/AngularModules/Customs/Components/MenuButtons/ContainerizationMenuButtonsHandler.ts
@@ -302,14 +302,26 @@ export class ContainerizationMenuButtonsHandler implements OnDestroy {
         return params
     }
     CancelContainerizationMethod() {
-        if (this.EntityPM.ContainerizationStatus == "4") {
-            let messageWindow = new MessageWindow();
+        if (AppTool.IsNullOrEmpty(this.EntityPM.ContainerizationStatus) || this.EntityPM.ContainerizationStatus == "4") {
+            //let messageWindow = new MessageWindow();
+            let messageWindow = new ConfirmWindow();
             messageWindow.Width = 300;
             messageWindow.Height = 180;
             messageWindow.Title = "שליחת המכלה";
-            messageWindow.RTL = true;
-            messageWindow.ShowErrorIcon = true;
-            messageWindow.Show("המכלה לא קיימת במכס");
+            //messageWindow.RTL = true;
+            //messageWindow.ShowErrorIcon = true;
+            messageWindow.Show("המכלה לא קיימת במכס, האם לשחרר תיקים?");
+            messageWindow.WindowClosed.subscribe((event: any) => {
+                if (messageWindow.Yes)
+                {
+                    this.EntityPM.NotConnectedDeclarations = this.EntityPM.ConnectedDeclarations;
+                    this.EntityPM.ConnectedDeclarations = "";
+                    this.containerizationPMService.update(this.EntityPM).subscribe((response: ServiceResponse) => {
+                        this.CurrentSession.CurrentEditComponent.ReloadEntityPM();
+                        this.CurrentSession.CurrentEditComponent.LoadCompleted.emit(true);
+                    });
+                }
+            });
         }
         else
         {
