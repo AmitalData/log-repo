e14bcfcb378b34c7c30b92cab667ab5b54f03de4
Atara Oster <Atara@amital.co.il>
2021-09-14 17:24:09 +0300

    #145614

--- Logitude/Logitude.Customs.Data/EntityListQueryServices/ContainerizationListQueryService.cs
+++ Logitude/Logitude.Customs.Data/EntityListQueryServices/ContainerizationListQueryService.cs
@@ -63,9 +63,9 @@ namespace Logitude.Customs.Data.EntityListQueryServices
 														  OperationMode = a.OperationMode,
 
 														  ContainerizationStatusName = a.ContainerizationStatusCode != null ? a.ContainerizationStatusCode.Name :null,
-											              ExportFile = a.IsMultiExportFiles ? "List" : ed.dec.FirstOrDefault().ExportFile,
+											              ExportFile = a.IsMultiExportFiles == true ? "List" : ed.dec.FirstOrDefault().ExportFile,
 														  HataraStatusName = a.ContainerizationHataraStatus != null? a.ContainerizationHataraStatus.Name:null,
-														  ImporterName = a.IsMultiCustomers ? "List" : ed.dec.FirstOrDefault().CustomerCard.LocalName,
+														  ImporterName = a.IsMultiCustomers == true ? "List" : ed.dec.FirstOrDefault().CustomerCard.LocalName,
 														  TransportModeForExport = ed.dec.FirstOrDefault().TransportModeId ,
 														  HataraStatusIsNull = a.HataraStatus != null ? false :true
 													  }); ;
