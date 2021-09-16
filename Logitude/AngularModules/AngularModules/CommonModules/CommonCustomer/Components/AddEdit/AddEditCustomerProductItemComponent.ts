84e14597731ccf03a30fd6563a26d33b26383dec
Samar Abu Eid <Samar@logitudeworld.com>
2021-09-16 12:48:17 +0300

    146868
    Related Work Items: #146868

--- Logitude/AngularModules/AngularModules/CommonModules/CommonCustomer/Components/AddEdit/AddEditCustomerProductItemComponent.ts
+++ Logitude/AngularModules/AngularModules/CommonModules/CommonCustomer/Components/AddEdit/AddEditCustomerProductItemComponent.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {BaseComponent} from '../../../../Infrastructure/Components/LogitudeComponents/BaseComponent';
 import {SessionLocator} from '../../../../Infrastructure/Utilities/SessionLocator';
-import {AppTool} from '../../../../Infrastructure/Tools';
+import {AppTool, ArrayTool} from '../../../../Infrastructure/Tools';
 import {Cloner} from '../../../../Infrastructure/Utilities/Cloner';
 import {Validator} from '../../../../Infrastructure/Validators/Validator';
 import { ProductItemPM } from '../../../../Common/EntityPMs/ProductItemPM';
@@ -61,7 +61,7 @@ export class AddEditCustomerProductItemComponent extends BaseComponent {
         }
     }
 
-    CancelButtonClicked() {
+    CancelButtonClicked() {        
         this.RejectChanges();
         this.CurrentSession.CloseCurrentWindow();
     }
@@ -161,5 +161,6 @@ export class AddEditCustomerProductItemComponent extends BaseComponent {
     private RejectChanges() {
         this.DataContext.ResetHTSCodes();
         this.myCloner.RejectChanges();
+        this.DataContext.maxHTSCodesLineNumber = ArrayTool.Max(this.EntityPM.HTSCodes, "LineNumber");
     }
 }
