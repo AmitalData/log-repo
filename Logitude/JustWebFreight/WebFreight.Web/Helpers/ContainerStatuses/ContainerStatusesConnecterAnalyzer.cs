b03fae88a668e2aa4ae2945ef0302204ef13ccf3
Maheera Jazi <Maheera@logitudeworld.com>
2021-09-20 15:05:47 +0300

    #147370

--- Logitude/JustWebFreight/WebFreight.Web/Helpers/ContainerStatuses/ContainerStatusesConnecterAnalyzer.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Helpers/ContainerStatuses/ContainerStatusesConnecterAnalyzer.cs
@@ -828,7 +828,7 @@ namespace WebFreight.Web.Helpers.Analyzers
                         LastResponseDate = TenantServerConfigration.GetCurrentDateTime(tenant),
                         ContainerNumber = container_number,
                         SCACCode = carrier_scac,
-                        Tenant = logitudeTenant,
+                        Tenant = logitudeTenant != null ? logitudeTenant.Value: tenant,
                         CarrierName = GetCarrierName()
                     };
                     logitudeOceanInsightsResponseRepository.Add(logitudeOceanInsightsResponse);
