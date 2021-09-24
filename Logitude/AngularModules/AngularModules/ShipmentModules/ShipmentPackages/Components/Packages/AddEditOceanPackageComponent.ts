7be312a081ecdb60ea7824c880288fdd34bbdb3d
Maheera Jazi <Maheera@logitudeworld.com>
2021-09-24 22:16:53 +0300

    #147437

--- Logitude/AngularModules/AngularModules/ShipmentModules/ShipmentPackages/Components/Packages/AddEditOceanPackageComponent.ts
+++ Logitude/AngularModules/AngularModules/ShipmentModules/ShipmentPackages/Components/Packages/AddEditOceanPackageComponent.ts
@@ -244,11 +244,11 @@ export class AddEditOceanPackageComponent {
     }
 
     GetContainerEntityReferenceVisiblity() {
+        this.IsContainerEntityReferenceVisible = false;
         var featureToggle: FeatureToggleList = SessionLocator.FeatureToggles.filter(d => d.ToggleCode == "OIC")[0];
         if (featureToggle && !AppTool.IsNullOrEmpty(this.EntityPM?.ContainerEntityId)) {
-            this.IsContainerEntityReferenceVisible = true;
-            if (this.DataContext.ShipmentPM.TransportModeId != "O" || this.DataContext.ShipmentPM.ShipmentTypeId.toLowerCase() != "fcl" || this.DataContext.ShipmentPM.ShipmentTypeId.toLowerCase() != "fcld") {
-                this.IsContainerEntityReferenceVisible = false;
+            if (this.DataContext.ShipmentPM.TransportModeId == "O" && (this.DataContext.ShipmentPM.ShipmentTypeId.toLowerCase() == "fcl" || this.DataContext.ShipmentPM.ShipmentTypeId.toLowerCase() == "fcld")) {
+                this.IsContainerEntityReferenceVisible = true;
             }
         }
     }
