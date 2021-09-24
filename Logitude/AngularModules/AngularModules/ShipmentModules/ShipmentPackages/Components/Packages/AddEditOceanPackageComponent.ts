6d1b11b296684748f5352214218c32977d17d008
Maheera Jazi <Maheera@logitudeworld.com>
2021-09-24 19:03:29 +0300

    #147437

--- Logitude/AngularModules/AngularModules/ShipmentModules/ShipmentPackages/Components/Packages/AddEditOceanPackageComponent.ts
+++ Logitude/AngularModules/AngularModules/ShipmentModules/ShipmentPackages/Components/Packages/AddEditOceanPackageComponent.ts
@@ -247,6 +247,9 @@ export class AddEditOceanPackageComponent {
         var featureToggle: FeatureToggleList = SessionLocator.FeatureToggles.filter(d => d.ToggleCode == "OIC")[0];
         if (featureToggle && !AppTool.IsNullOrEmpty(this.EntityPM?.ContainerEntityId)) {
             this.IsContainerEntityReferenceVisible = true;
+            if (this.DataContext.ShipmentPM.TransportModeId != "O" || this.DataContext.ShipmentPM.ShipmentTypeId.toLowerCase() != "fcl" || this.DataContext.ShipmentPM.ShipmentTypeId.toLowerCase() != "fcld") {
+                this.IsContainerEntityReferenceVisible = false;
+            }
         }
     }
 
