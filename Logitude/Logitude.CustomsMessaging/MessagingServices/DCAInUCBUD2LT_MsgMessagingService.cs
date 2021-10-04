2c6b45e583d7a5112b9f076aecd10276b9ca01c0
elisheva Nachaysi <elisheva@amital.co.il>
2021-10-04 14:35:48 +0300

    logs in docs

--- Logitude/Logitude.CustomsMessaging/MessagingServices/DCAInUCBUD2LT_MsgMessagingService.cs
+++ Logitude/Logitude.CustomsMessaging/MessagingServices/DCAInUCBUD2LT_MsgMessagingService.cs
@@ -430,7 +430,7 @@ namespace Logitude.CustomsMessaging.MessagingServices
             catch (Exception E)
             {
 
-                LogitudeSettings.HandleLogMe(E.ToString() + logData, true, "CreateUD2LTService", stopLogAt);
+                LogitudeSettings.HandleLogMe(E.ToString() + E.StackTrace+ logData, true, "CreateUD2LTService", stopLogAt);
                 throw;
             }
             finally
