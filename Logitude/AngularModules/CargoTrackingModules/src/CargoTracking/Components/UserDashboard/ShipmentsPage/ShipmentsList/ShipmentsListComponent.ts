e3a328c9da7621255e1c7462bcc33da98a491175
Ayed Ramadeen <ayed@logitudeworld.com>
2021-10-04 14:55:09 +0300

    revert last change

--- Logitude/AngularModules/CargoTrackingModules/src/CargoTracking/Components/UserDashboard/ShipmentsPage/ShipmentsList/ShipmentsListComponent.ts
+++ Logitude/AngularModules/CargoTrackingModules/src/CargoTracking/Components/UserDashboard/ShipmentsPage/ShipmentsList/ShipmentsListComponent.ts
@@ -172,6 +172,7 @@ export class ShipmentsListComponent implements AfterViewInit, OnInit
     SetValueForSupplierOrClient(shipment: CargoTrackingShipmentList) {
         var value;
         value = this.SetSupplierOrCleintValueByDirection(shipment, value);
+        value = this.SetSupplierOrCleintValueByEntityType(shipment, value);
 
         this.SupplierOrClientValue = value;
     }
@@ -187,6 +188,14 @@ export class ShipmentsListComponent implements AfterViewInit, OnInit
                 value = shipment.ConsigneeName;
                 break;
             }
+        }
+        return value;
+    }
+
+    private SetSupplierOrCleintValueByEntityType(shipment: CargoTrackingShipmentList, value: any) {
+        const EntityType_Customs = "C";
+        if (shipment.EntityType == EntityType_Customs) {
+            value = shipment.ConsigneeName;
         }
         return value;
     }
