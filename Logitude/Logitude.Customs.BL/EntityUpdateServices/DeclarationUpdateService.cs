199fc8685c58978b6b44ed0a038f8679c0ebd47d
elisheva Nachaysi <elisheva@amital.co.il>
2021-10-05 15:22:38 +0300

    #147061

--- Logitude/Logitude.Customs.BL/EntityUpdateServices/DeclarationUpdateService.cs
+++ Logitude/Logitude.Customs.BL/EntityUpdateServices/DeclarationUpdateService.cs
@@ -343,9 +343,9 @@ namespace Logitude.Customs.BL.EntityUpdateServices
                     var entityPMOrg = declarationQueryService.GetSingle(entityPM.AmendmentOriginalDeclartation, true, false);
                     entityPMOrg.CurrentContextTag = eventContextTagModel;
                     entityPMOrg.HatraDate = entityPM.HatraDate;
-                    entityPMOrg.DeclarationNumber = entityPM.DeclarationNumber;
+                  //  entityPMOrg.DeclarationNumber = entityPM.DeclarationNumber;
 
-                    UpdateUnifreight(entityPMOrg);
+                    UpdateUnifreight(entityPMOrg, true);
 
 
                 }
