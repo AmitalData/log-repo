ca0c5b42883d9bb38eb5a87abbc13fc45533a542
Abdullah Dawoud <abdullah@logitudeworld.com>
2021-10-05 11:29:05 +0000

    clean code

--- Logitude/Logitude.CargoTracking.Data/EntityListQueryServices/CargoTrackingShipmentListQueryService.cs
+++ Logitude/Logitude.CargoTracking.Data/EntityListQueryServices/CargoTrackingShipmentListQueryService.cs
@@ -361,7 +361,6 @@ namespace Logitude.CargoTracking.Data.EntityListQueryServices
         {
             CargoTrackingShipmentRepository repo = new CargoTrackingShipmentRepository(context);
             IQueryable<CargoTrackingShipment> shipments = repo.GetFilteredShipmentsByIds(ShipmentIds, tenant);
-            var xxxxx = shipments.ToList();
             List<CargoTrackingShipmentList> shipmetsLists = GetIqueryableList(shipments).ToList();
             return shipmetsLists;
         }
