7d004ab32582c3195a982d230476a9f2a7beeaac
Wesam Amarneh <wesam@logitudeworld.com>
2021-10-06 10:55:39 +0300

    Update the logic of filling the declaration data xml in shipments - R4
    Related Work Items: #148097

--- Logitude/Logitude.BL/ShipmentsModel/Tools/EntityService/ShipmentService.cs
+++ Logitude/Logitude.BL/ShipmentsModel/Tools/EntityService/ShipmentService.cs
@@ -2851,10 +2851,12 @@ namespace Logitude.BL.ShipmentsModel.Tools.EntityService
                             }
                             shipmentAdditionalCloudData.DeclarationXmlData = entityPM.DeclarationXMLData;
                             shipmentAdditionalCloudData.IsImporterApprovalRequried = true;
-                            shipmentAdditionalCloudData.ApprovedByUserName = null;
-                            shipmentAdditionalCloudData.ApproveDateTime = null;
-                            shipmentAdditionalCloudData.DenyReason = null;
-                            shipmentAdditionalCloudData.VersionApproved = null;
+                            if (!shipmentAdditionalCloudData.IsImporterApprovalRequried) {
+                                shipmentAdditionalCloudData.ApprovedByUserName = null;
+                                shipmentAdditionalCloudData.ApproveDateTime = null;
+                                shipmentAdditionalCloudData.DenyReason = null;
+                                shipmentAdditionalCloudData.VersionApproved = null; 
+                            }
                         }
                         else if (entityPM.IsShipmentAdditionalCloudDataChange)
                         {
