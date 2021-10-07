086d3ed4427f78d7d5c7a74bec38ffa2aaa65566
Izik Fridland <IzikFr@amital.co.il>
2021-10-07 16:43:28 +0300

    fixed error message

--- Logitude/AngularModules/AngularModules/QuoteOPM/Components/NewEntity/NewQuoteComponent.ts
+++ Logitude/AngularModules/AngularModules/QuoteOPM/Components/NewEntity/NewQuoteComponent.ts
@@ -2,6 +2,7 @@ import { Component, isDevMode } from "@angular/core";
 import { FormGroup } from "@angular/forms";
 import { add } from "cypress/types/lodash";
 import { SessionLocator } from "Infrastructure/Utilities/SessionLocator";
+import { MessageService } from "primeng/api";
 import { QuoteOPPMInitService } from "QuoteOPM/EntityPMInitServices/QuoteOPPMInitService";
 import { QuoteOPPM } from "QuoteOPM/EntityPMs/QuoteOPPM";
 import { QuoteOPPMService } from "QuoteOPM/Services/StandardPMs/QuoteOPPMService";
@@ -18,26 +19,28 @@ export class NewQuoteComponent {
 
     constructor(
         private newQuoteDataService: NewQuoteDataService,
+        private messageService: MessageService,
     ) { }
 
     async ngOnInit() {
         this.CreateNewQuote();
 
-        if (isDevMode())
-            this.test()
+        // if (isDevMode())
+        //     this.test()
     }
 
     async create() {
-        if(this.formGroup.invalid) return;
-        
+        if (this.formGroup.invalid) return;
+
         SessionLocator.SelectedSession.CurrentWindow.StartBusyIndicator('Create new quote... ');
-        
+
         this.addProperty()
-        await this.newQuoteDataService.creatingNewQuote(this.EntityPM)
-        
-        SessionLocator.SelectedSession.CurrentWindow.StopBusyIndicator();
-        SessionLocator.SelectedSession.CloseCurrentWindowEmit('OK')
-        console.log(this.EntityPM)
+        this.newQuoteDataService.creatingNewQuote(this.EntityPM)
+            .then(() => SessionLocator.SelectedSession.CloseCurrentWindowEmit('OK'))
+            .catch((err: string[]) => this.messageService.add({ severity: 'error', summary: 'Create new quote failed', detail: err.join(', ')}))
+            .finally(() => SessionLocator.SelectedSession.CurrentWindow.StopBusyIndicator())
+
+        // console.log(this.EntityPM)
     }
 
     cancel() {
@@ -65,64 +68,64 @@ export class NewQuoteComponent {
         }
     }
 
-    private test() {
-        console.log('*************  test mode ****************')
-        Object.keys(testData).forEach(propName => this.EntityPM[propName] = testData[propName])
-    }
+    // private test() {
+    //     console.log('*************  test mode ****************')
+    //     Object.keys(testData).forEach(propName => this.EntityPM[propName] = testData[propName])
+    // }
 }
 
 
-const testData = {
-    branchId: "1-29",
-    businessUnitId: "1",
-    chargeableWeight: 1,
-    chargeableWeightUnitCode: "KG",
-    consigneeContactId: "1-6387",
-    consigneeId: "1-4221",
-    consigneeName: "LIRAN TEST",
-    consigneeNote: null,
-    createdByUserId: "1-6390",
-    departmentId: "1-32",
-    dimensionsUnitCode: "Cm",
-    directionId: "E",
-    expirationDate: new Date(),
-    expirationDays: 1,
-    fromAddressCity: "sdfsd",
-    fromAddressCountryId: "1-449",
-    fromAddressZipCode: 1,
-    fromPortId: "AAC",
-    grossWeight: 1,
-    grossWeightUnitCode: "KG",
-    includeDelivery: true,
-    includePickUp: true,    
-    incotermId: "DDU",
-    isClosed: false,
-    moveTypeId: "1-13",
-    numberOfPackages: 1,
-    openDate: new Date(),
-    quoteCharges: [],
-    quoteCostCharges: [],
-    quotePackages: [],
-    quoteSaleCharges: [],
-    quoteSalesTotals: [],
-    quoteTypeCode: "A",
-    ratingCode: "N",
-    saleCurrencyId: "1-3",
-    shipmentTypeId: "FTL",
-    shipperContactId: "1-6387",
-    shipperId: "1-4221",
-    shipperName: "LIRAN TEST",
-    shipperNote: null,
-    specialServiceId: "BAK",
-    startDate: new Date(),
-    tenant: 1,
-    toAddressCity: "fsdf",
-    toAddressCountryId: "1-449",
-    totalVATPerQuote: [],
-    toPortId: "AAC",
-    transportModeId: "O",
-    updateDate: new Date(),
-    updatedByUserId: "1-6390",
-    volume: 1,
-    volumeUnitCode: "CBM",
-}
+// const testData = {
+//     branchId: "1-29",
+//     businessUnitId: "1",
+//     chargeableWeight: 1,
+//     chargeableWeightUnitCode: "KG",
+//     consigneeContactId: "1-6387",
+//     consigneeId: "1-4221",
+//     consigneeName: "LIRAN TEST",
+//     consigneeNote: null,
+//     createdByUserId: "1-6390",
+//     departmentId: "1-32",
+//     dimensionsUnitCode: "Cm",
+//     directionId: "E",
+//     expirationDate: new Date(),
+//     expirationDays: 1,
+//     fromAddressCity: "sdfsd",
+//     fromAddressCountryId: "1-449",
+//     fromAddressZipCode: 1,
+//     fromPortId: "AAC",
+//     grossWeight: 1,
+//     grossWeightUnitCode: "KG",
+//     includeDelivery: true,
+//     includePickUp: true,    
+//     incotermId: "DDU",
+//     isClosed: false,
+//     moveTypeId: "1-13",
+//     numberOfPackages: 1,
+//     openDate: new Date(),
+//     quoteCharges: [],
+//     quoteCostCharges: [],
+//     quotePackages: [],
+//     quoteSaleCharges: [],
+//     quoteSalesTotals: [],
+//     quoteTypeCode: "A",
+//     ratingCode: "N",
+//     saleCurrencyId: "1-3",
+//     shipmentTypeId: "FTL",
+//     shipperContactId: "1-6387",
+//     shipperId: "1-4221",
+//     shipperName: "LIRAN TEST",
+//     shipperNote: null,
+//     specialServiceId: "BAK",
+//     startDate: new Date(),
+//     tenant: 1,
+//     toAddressCity: "fsdf",
+//     toAddressCountryId: "1-449",
+//     totalVATPerQuote: [],
+//     toPortId: "AAC",
+//     transportModeId: "O",
+//     updateDate: new Date(),
+//     updatedByUserId: "1-6390",
+//     volume: 1,
+//     volumeUnitCode: "CBM",
+// }
