9942ca6cecd261b235aa90fe0fc0a2fc58fcc2e3
Wasfi Mahmoud <Wasfi@logitudeworld.com>
2021-10-18 11:09:02 +0300

    E2E: Fix "CopyShipment"

--- Logitude/AngularModules/CypressTesting/Base/cypress/commands/Elements.ts
+++ Logitude/AngularModules/CypressTesting/Base/cypress/commands/Elements.ts
@@ -10,7 +10,7 @@ declare global {
     namespace Cypress {
         interface Chainable {
             FillDate(selector: string, value: string): Chainable<Element>
-            FillLogTextBox(selector: string, value: string,ValidateInputDone? :boolean): Chainable<Element>
+            FillLogTextBox(selector: string, value: string, ValidateInputDone?: boolean): Chainable<Element>
             FillLogLov(selector: string, value: string, fromCache: boolean, getByFilters?: boolean): Chainable<Element>
             FillRandomString(selector: string, length: number, upperCase: boolean): Chainable<Element>
             FillRandomNumber(selector: string, minimum: number, maximum: number): Chainable<Element>
@@ -20,46 +20,46 @@ declare global {
             ClickRadio(selector: string): Chainable<Element>
             ValidateElementColor(selector: string, expectedcolor: string): Chainable<Element>
             SelectQuickSearchFirstElement(quickSearchDetails: QuickSearchDetails): Chainable<Element>
-            BackButton(contains:string): Chainable<Element>
+            BackButton(contains: string): Chainable<Element>
             getAttached(selector: any): Chainable<Element>
 
-            ClickingAfterHovering(LogLovSelector:string,HiddenElementSelector:string): Chainable<Element>
-            SelectCheckBox(Selector:string): Chainable<Element>
-            SelectDropDownListItem(Selector:string,contain:string): Chainable<Element>
-            SelectDropDownListItemNumber(Selector:string,number:number): Chainable<Element>
+            ClickingAfterHovering(LogLovSelector: string, HiddenElementSelector: string): Chainable<Element>
+            SelectCheckBox(Selector: string): Chainable<Element>
+            SelectDropDownListItem(Selector: string, contain: string): Chainable<Element>
+            SelectDropDownListItemNumber(Selector: string, number: number): Chainable<Element>
 
         }
     }
 }
-Cypress.Commands.add("SelectDropDownListItem", (Selector:string,contain:string) => {
+Cypress.Commands.add("SelectDropDownListItem", (Selector: string, contain: string) => {
     cy.get(Selector).find(BaseSelectors.DownArrow).click()
     cy.get(BaseSelectors.DropDownList).find(BaseSelectors.DropDownListItem).contains(contain).click()
 })
-Cypress.Commands.add("SelectDropDownListItemNumber", (Selector:string,number:number) => {
+Cypress.Commands.add("SelectDropDownListItemNumber", (Selector: string, number: number) => {
     cy.get(Selector).find(BaseSelectors.DownArrow).click()
-    cy.get(BaseSelectors.DropDownList).find(BaseSelectors.DropDownListItem).eq(number).click({force:true})
+    cy.get(BaseSelectors.DropDownList).find(BaseSelectors.DropDownListItem).eq(number).click({ force: true })
 })
-Cypress.Commands.add("SelectCheckBox", (Selector:string) => {
+Cypress.Commands.add("SelectCheckBox", (Selector: string) => {
     cy.get(Selector).check({ force: true })
 
-/**
- * getAttached(selector)
- * getAttached(selectorFn)
- *
- * Waits until the selector finds an attached element, then yields it (wrapped).
- * selectorFn, if provided, is passed $(document). Don't use cy methods inside selectorFn.
- */
-Cypress.Commands.add("getAttached", selector => {
-    const getElement = typeof selector === "function" ? selector : $d => $d.find(selector);
-    let $el = null;
-    return cy.document().should($d => {
-      $el = getElement(Cypress.$($d));
-      expect(Cypress.dom.isDetached($el)).to.be.false;
-    }).then(() => cy.wrap($el));
-  });
+    /**
+     * getAttached(selector)
+     * getAttached(selectorFn)
+     *
+     * Waits until the selector finds an attached element, then yields it (wrapped).
+     * selectorFn, if provided, is passed $(document). Don't use cy methods inside selectorFn.
+     */
+    Cypress.Commands.add("getAttached", selector => {
+        const getElement = typeof selector === "function" ? selector : $d => $d.find(selector);
+        let $el = null;
+        return cy.document().should($d => {
+            $el = getElement(Cypress.$($d));
+            expect(Cypress.dom.isDetached($el)).to.be.false;
+        }).then(() => cy.wrap($el));
+    });
 
 })
-Cypress.Commands.add("ClickingAfterHovering", (LogLovSelector:string,HiddenElementSelector:string) => {
+Cypress.Commands.add("ClickingAfterHovering", (LogLovSelector: string, HiddenElementSelector: string) => {
     cy.get(LogLovSelector).trigger(BaseSelectors.MouseoverTrigger).find(HiddenElementSelector).click()
 
 })
@@ -75,17 +75,17 @@ Cypress.Commands.add("FillDate", (selector, value) => {
         cy.get(selector).focus().clear().type("+1{enter}")
     }
     else {
-        cy.get(selector).focus().clear().type(value+'{enter}')
+        cy.get(selector).focus().clear().type(value + '{enter}')
     }
 })
 
 Cypress.Commands.add("FillLogTextBox", (selector, value, ValidateInputDone = false) => {
     if (value) {
-       FillLogTextBoxProcess(selector, value, ValidateInputDone);
+        FillLogTextBoxProcess(selector, value, ValidateInputDone);
     }
 })
 
-function FillLogTextBoxProcess(selector:string, value:string, ValidateInputDone:boolean) {
+function FillLogTextBoxProcess(selector: string, value: string, ValidateInputDone: boolean) {
     if (ValidateInputDone) {
         cy.get(selector).clear().type("{selectall}" + value)//.should('have.value', value)
     }
@@ -105,10 +105,10 @@ Cypress.Commands.add("FillLogLov", (selector, value, fromCache, getByFilters = f
 
     // cy.get('body').find('SessionComponent').eq(0).as('Container')
 
-   if(value){
-      FillLogLovProcess(selector, value, fromCache, getByFilters); 
-   }
-    
+    if (value) {
+        FillLogLovProcess(selector, value, fromCache, getByFilters);
+    }
+
     //cy.get(selector).clear().type(value)
     //cy.wrap(selector).clear({ force: true })
     //cy.get(selector).type("{selectall}" + value)
@@ -120,7 +120,7 @@ Cypress.Commands.add("FillLogLov", (selector, value, fromCache, getByFilters = f
 
 })
 
-function FillLogLovProcess(selector: any, value: string, fromCache: boolean, getByFilters: boolean){
+function FillLogLovProcess(selector: any, value: string, fromCache: boolean, getByFilters: boolean) {
     let requestAlias: string;
     if (!fromCache) {
         requestAlias = "LogLovDataLoad_" + gr.GenerateRandomString(6, false);
@@ -131,14 +131,14 @@ function FillLogLovProcess(selector: any, value: string, fromCache: boolean, get
     }
 
     cy.get(selector).clear({ force: true }).type(value).then($input => {
-        if($input[0].value == value || ($input[0].value == "" && value == "{downarrow}")){
+        if ($input[0].value == value || ($input[0].value == "" && value == "{downarrow}")) {
             if (!fromCache) {
                 cy.wait("@" + requestAlias);
             }
             let itemSelector = value == "{downarrow}" ? "li.DropDownListItem" : "li.DropDownListItem.liItemSelected";
             cy.get("ul.DropDownList").find(itemSelector).eq(0).click({ force: true });
         }
-        else{
+        else {
             FillLogLovProcess(selector, value, fromCache, getByFilters);
         }
     });
@@ -158,22 +158,21 @@ Cypress.Commands.add("Click", (selector, contains, force = false) => {
     //let element = //.should('exist')
     cy.wait(1000);
     if (contains) {
-        cy.get(selector).contains(contains, { matchCase: false }).click({force:force})
+        cy.get(selector).contains(contains, { matchCase: false }).click({ force: force })
     }
-    else{
-        cy.get(selector).click({force:force})
+    else {
+        cy.get(selector).click({ force: force })
     }
 })
 
 Cypress.Commands.add("Navigate", (selector, force = false) => {
     let element = cy.get(selector)
-    element.click({force:force})
+    element.click({ force: force })
 })
 
 Cypress.Commands.add("ClickCheckBox", (selector) => {
-
+    cy.wait(1000);
     cy.get(selector).next("label").click()
-
 })
 
 Cypress.Commands.add("ClickRadio", (selector) => {
@@ -196,5 +195,3 @@ Cypress.Commands.add("SelectQuickSearchFirstElement", (quickSearchDetails: Quick
             })
         })
 })
-
-
