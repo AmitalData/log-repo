8dd54787694c323744138b7182b108cdd0b25ef7
Samar Abu Eid <Samar@logitudeworld.com>
2021-10-24 12:19:37 +0300

    149276
    Related Work Items: #149276

--- Logitude/JustWebFreight/WebFreight.Web/WebServices/ShippingDeclarationWebService.asmx.cs
+++ Logitude/JustWebFreight/WebFreight.Web/WebServices/ShippingDeclarationWebService.asmx.cs
@@ -160,7 +160,9 @@ namespace WebFreight.Web.WebServices
                 myDataProvider.Transhipment2Vessel = shipment.Transshipment2VesselName;
                 myDataProvider.Transhipment3Vessel = shipment.Transshipment3VesselName;
                 myDataProvider.ShipmentSubTypeName = shipment.ShipmentSubTypeName;
-               
+
+                this.FillINTTRADocumentProperties(myDataProvider);                
+                
                 if (shipment.DocumentsClosingDate != null)
                 {
                     myDataProvider.DocumentsClosingDate = shipment.DocumentsClosingDate;
@@ -3827,6 +3829,20 @@ namespace WebFreight.Web.WebServices
             }
 
             return myDataProvider;
+        }
+
+        private void FillINTTRADocumentProperties(ShippingDeclarationDataProvider myDataProvider)
+        {
+            myDataProvider.INTTRADocumentQTY = shipment.INTTRADocumentQTY;
+
+            if (!string.IsNullOrEmpty(shipment.INTTRADocumentTypeCode))
+            {
+                DocumentType documentType = commonContext.DocumentTypes.Where(d => d.Code == shipment.INTTRADocumentTypeCode && d.Tenant == tenant).FirstOrDefault();
+                if (documentType != null)
+                {
+                    myDataProvider.INTTRADocumentTypeName = documentType.Name;
+                }
+            }
         }
 
         private void SetHorseDetails(InsidePackageLine insidePackage, InsideShipmentPackage insideItem)
