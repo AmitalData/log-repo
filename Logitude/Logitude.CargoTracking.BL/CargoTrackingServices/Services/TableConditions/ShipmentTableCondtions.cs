750d06e02ed44acdaa88b09c4f584fbec02e2245
Ayed Ramadeen <ayed@logitudeworld.com>
2021-10-24 15:46:53 +0300

    update query

--- Logitude/Logitude.CargoTracking.BL/CargoTrackingServices/Services/TableConditions/ShipmentTableCondtions.cs
+++ Logitude/Logitude.CargoTracking.BL/CargoTrackingServices/Services/TableConditions/ShipmentTableCondtions.cs
@@ -19,8 +19,8 @@ namespace Logitude.CargoTracking.BL.CargoTrackingServices.Services.TableStructur
             string updatedShipmentFields = shipmentFields.Replace("C.ConsigneeName", "(case when C.DirectionId = 'E' AND C.ConsigneeId IS NOT NULL then ConsigneeCard.EnglishName when" +
                 " C.DirectionId = 'E' AND C.ConsigneeId IS NULL then C.ConsigneeName end) as ConsigneeName");
 
-            updatedShipmentFields = updatedShipmentFields.Replace("C.ShipperName", "(case when C.DirectionId = 'I' AND C.ShipperId IS NOT NULL then ShipperCard.EnglishName when" +
-                " C.DirectionId = 'I' AND C.ShipperId IS NULL then C.ShipperName end) as ShipperName");
+            updatedShipmentFields = updatedShipmentFields.Replace("C.ShipperName", "(case when (C.DirectionId = 'I' OR C.ShipmentLevelCode = 'A') AND C.ShipperId IS NOT NULL then ShipperCard.EnglishName when" +
+                " (C.DirectionId = 'I' OR C.ShipmentLevelCode = 'A') AND C.ShipperId IS NULL then C.ShipperName end) as ShipperName");
 
             var shipmentComputedFields =
                  "com.ContainersNumbers as ContainersNumbers," +
@@ -149,8 +149,8 @@ namespace Logitude.CargoTracking.BL.CargoTrackingServices.Services.TableStructur
             string updatedShipmentFields = shipmentFields.Replace("P.ConsigneeName", "(case when P.DirectionId = 'E' AND P.ConsigneeId IS NOT NULL then ConsigneeCard.EnglishName when" +
                 " P.DirectionId = 'E' AND P.ConsigneeId IS NULL then P.ConsigneeName end) as ConsigneeName");
 
-            updatedShipmentFields = updatedShipmentFields.Replace("P.ShipperName", "(case when P.DirectionId = 'I' AND P.ShipperId IS NOT NULL then ShipperCard.EnglishName when" +
-                " P.DirectionId = 'I' AND P.ShipperId IS NULL then P.ShipperName end) as ShipperName");
+            updatedShipmentFields = updatedShipmentFields.Replace("P.ShipperName", "(case when (P.DirectionId = 'I' OR P.ShipmentLevelCode = 'A') AND P.ShipperId IS NOT NULL then ShipperCard.EnglishName when" +
+                " (P.DirectionId = 'I' OR P.ShipmentLevelCode = 'A') AND P.ShipperId IS NULL then P.ShipperName end) as ShipperName");
 
             var shipmentComputedFields =
                  "com.ContainersNumbers as ContainersNumbers," +
