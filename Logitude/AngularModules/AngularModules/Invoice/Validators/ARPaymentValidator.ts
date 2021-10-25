80437106fa9fa28bd4d10f5d131bf1311d0f02eb
Alaa Jamal <Alaa@logitudeworld.com>
2021-10-25 15:16:38 +0300

    #149406

--- Logitude/AngularModules/AngularModules/Invoice/Validators/ARPaymentValidator.ts
+++ Logitude/AngularModules/AngularModules/Invoice/Validators/ARPaymentValidator.ts
@@ -22,7 +22,7 @@ export class ARPaymentValidator {
       validationResults.push(msg.replace("%FieldName", "Register Date"));
     }
 
-    else if (DateTool.GetDateParts(entityPm.RegisterDate).DateTicks > DateTool.GetCurrentDateAsUtcForAccountingValidation().valueOf()) {
+    else if (DateTool.GetDateParts(entityPm.RegisterDate).DateTicks > DateTool.GetCurrentDateAsUtcForAccountingValidation(SessionLocator.TenantPM.TimeZoneOffset).valueOf()) {
       validationResults.push(TextCodeTranslator.Translate("ARPayment.M.CantSetFutureDatePayment"));
     }
 
@@ -165,7 +165,7 @@ export class ARPaymentValidator {
             errors.push(msg.replace("%FieldName", "Register Date"));
         }
 
-        else if (DateTool.GetDateParts(entityPm.RegisterDate).DateTicks > DateTool.GetCurrentDateAsUtcForAccountingValidation().valueOf()) {
+        else if (DateTool.GetDateParts(entityPm.RegisterDate).DateTicks > DateTool.GetCurrentDateAsUtcForAccountingValidation(SessionLocator.TenantPM.TimeZoneOffset).valueOf()) {
             errors.push(TextCodeTranslator.Translate("ARPayment.M.CantSetFutureDatePayment"));
         }
 
