997c7d0884fb6cbe6a7513c1f1948a10a9e20f35
Abdullah Beitello <AbdullahB@logitudeworld.com>
2021-10-31 10:37:53 +0200

    #149612

--- Logitude/JustWebFreight/WebFreight.Web/ExternalAPIs/V1/DirectController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/ExternalAPIs/V1/DirectController.cs
@@ -1183,6 +1183,7 @@ namespace WebFreight.Web.ExternalAPIs.V1
             }
             return false;
         }
+
         private bool IsAllDimensionsNotNull(ShipmentPackagePM shipmentPackagePM)
         {
            return ((shipmentPackagePM.Width != null && shipmentPackagePM.Height != null && shipmentPackagePM.Length != null) && shipmentPackagePM.Volume != null);
