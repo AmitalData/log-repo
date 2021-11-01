e1acc16050b6431cfa1e9134e5e54b7cc705cf41
elisheva Nachaysi <elisheva@amital.co.il>
2021-11-01 08:42:02 +0200

    Revert "#149466"
    This reverts commit e3cd5ad5f0d4d13e62e26d70698dcc87c23d0872.

--- Logitude/Logitude.CustomsMessaging/ResponseServices/UniCourierBatchSend2755_MsgResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/UniCourierBatchSend2755_MsgResponseService.cs
@@ -176,9 +176,7 @@ namespace Logitude.CustomsMessaging.ResponseServices
                             LoggingUserId = requestParams.LoggingUserId,
                             RequestVIA = SendRequestVIA.WebServiceBatch,
                             UnifreightListOnServerOnly = UnifreightListOnServerOnly_BankeId,
-                          //  TransmitionDateTime = DateTime.Now.AddHours(-2),
-                            FutureSendDateTime = DateTime.Now.AddHours(-2) ,  
-                              
+
                         };
 
                         SBQMessageService.CreateSheetSBQMessage<GenericRequestParams>(requestParams2755, false);
