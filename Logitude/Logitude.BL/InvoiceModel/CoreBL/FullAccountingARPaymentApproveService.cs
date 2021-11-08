d55dbe77ccb07dde4980a69300afa2d1d5cd035b
Ayed Ramadeen <ayed@logitudeworld.com>
2021-11-08 16:27:10 +0200

    Multiple messages appears when changing payment method

--- Logitude/Logitude.BL/InvoiceModel/CoreBL/FullAccountingARPaymentApproveService.cs
+++ Logitude/Logitude.BL/InvoiceModel/CoreBL/FullAccountingARPaymentApproveService.cs
@@ -66,6 +66,10 @@ namespace Logitude.BL.InvoiceModel.CoreBL
 
         public void ApproveARPayment()
         {
+            if (paymentPM.AccountingPaymentMethodCode != "BT" && paymentPM.ARPaymentBankTranfers.Any())
+            {
+                throw new ApplicationException("The payment method should be bank transfer when adding multiple bank transfers");
+            }
             if (PaymentCashbook != null)
                 AddChequesOrCashToCashbook();
             if (IsBankTransferPayment)
