bd8ec4e9623305ddb3f0080677e77413b8709f8a
Noor Shehada <noor@logitudeworld.com>
2021-11-08 20:49:32 +0200

    #add new scenario

--- Logitude/AngularModules/CypressTesting/Shipment/cypress/selectors/Selectors.ts
+++ Logitude/AngularModules/CypressTesting/Shipment/cypress/selectors/Selectors.ts
@@ -173,7 +173,9 @@ export class ShipmentSelectors extends RegexSelectors {
   public static readonly EmptyContainerReturnATADate = '#date_ShipmentPackage_EmptyContainerReturnATA';
   public static readonly PickUpDeliveryATDTime = '#time_ShipmentPickUpDelivery_ATD';
   public static readonly PickUpDeliveryATDDate = '#date_ShipmentPickUpDelivery_ATD';
-
+  public static readonly PickUpDeliveryETADate = '#date_ShipmentPickUpDelivery_ETA';
+  public static readonly PickUpDeliveryATADate = '#date_ShipmentPickUpDelivery_ATA';
+  
   public static readonly ShipmentRatio = '#Shipment_Ratio';
   public static readonly ShipmentGrossWeightUnitCode = '#Shipment_GrossWeightUnitCode';
 
--- Logitude/AngularModules/CypressTesting/Shipment/cypress/tests/StatusInRoutings/StatusInRoutingsSteps.ts
+++ Logitude/AngularModules/CypressTesting/Shipment/cypress/tests/StatusInRoutings/StatusInRoutingsSteps.ts
@@ -8,6 +8,7 @@ import { ReceivableDetails } from "cypress/models/ReceivableDetails"
 import { PackagesDetails } from "cypress/models/PackagesDetails";
 import { RequestAliases } from "../../../../Base/cypress/constants/RequestAliases";
 import * as Assists from "../../../../Base/cypress/assists/Assists";
+import { BaseSelectors } from "../../../../Base/cypress/selectors/BaseSelectors";
 
 //#region variables
 let ShipmentData: ShipmentDetails;
@@ -35,36 +36,92 @@ Given("the user open the shipment and navigate to RoutingsTab workspace", () => 
   cy.Navigate(ShipmentSelectors.RoutingsTab);
 });
 
-
-
-
 Given("add a warehouse with {string} as a terminal", (warehouseLegTerminal) => {
   Actions.AddWarehouseLegPickups(warehouseLegTerminal)
 
 });
 Given("the user add new pickup", () => {
-  
   Actions.FillPickupRouting()
 });
 
 Given("add delivery with {string} as a partner routing", (partner) => {
   Actions.FillDeliveryRouting(partner)
 });
+
 //#endregio
+
+//edit on pickup window
 Given("the user edit pick window",()=>{
 cy.Navigate(ShipmentSelectors.EditPickUp,true)
 })
-Given("add expected departure with {string} as a value",(expectedDeparture)=>{
-cy.FillLogTextBox(ShipmentSelectors.PickUpDeliveryETDDate,'01/10/2021',false)
 
+Given("add expected departure with {string} as a value",(expectedDeparture)=>{
+cy.FillLogTextBox(ShipmentSelectors.PickUpDeliveryETDDate,expectedDeparture,false)
 })
 
+Given("add Actual Departure with {string} as a value",(actualDeparture)=>{
+  cy.FillLogTextBox(ShipmentSelectors.PickUpDeliveryATDDate,actualDeparture,false)
+}) 
+
+Given("add Actual Arrival with {string} as a value",(actualArrival)=>{
+    cy.FillLogTextBox(ShipmentSelectors.PickUpDeliveryATADate,actualArrival,false)
+}) 
+
+ //#endregio
+
+//edit on Warehouse window 
+Given("the user edit Warehouse window",()=>{
+  cy.Navigate(ShipmentSelectors.EditWarehouseLegPickups,true)
+})
+  
+Given("add Actual Entry with {string} as a value",(actualEntry)=>{
+  cy.FillLogTextBox(ShipmentSelectors.WarehouseLegActualEntryDate,actualEntry,false)
+})
+
+Given("add Actual Release with {string} as a value",(actualRelease)=>{
+  cy.FillLogTextBox(ShipmentSelectors.WarehouseLegExpectedReleaseDate,actualRelease,false)
+})
+
+//#endregio
+
+//edit on main carriage window 
+
+Given("the user edit main carriage window",()=>{
+cy.Navigate(ShipmentSelectors.EditRoutingMainCarriage,true)
+})
+
+Given("add ATD with {string} as a value",(aTD)=>{
+cy.FillLogTextBox(ShipmentSelectors.MainCarriageATDDate,aTD,false)
+})
+
+Given("add ATA with {string} as a value",(aTA)=>{
+  cy.FillLogTextBox(ShipmentSelectors.MainCarriageATADate,aTA,false)
+})
+//#endregio
+
+//edit on Delivary window 
+Given("the user edit Delivary window",()=>{
+  cy.Navigate(ShipmentSelectors.EditDelivery,true)
+})
+  
 When("save pickup",(()=>{
     cy.Navigate(ShipmentSelectors.SaveClose,true) 
+    //cy.Navigate(ShipmentSelectors.CloseBtn,true) 
+    //cy.Navigate(ShipmentSelectors.ConfirmWindowYes+BaseSelectors.LastElement) 
 }))
+
+When("create shipment", () => {
+  Actions.CreateShipment(ShipmentData.ShipmentLevel);
+});
+
+When("update shipment", () => {
+  Actions.UpdateShipment(ShipmentSelectors.ShipmentSaveButton)
+});
+
 Then("the status value should be {string}", (statusValue) => {
   BaseAssertion.AssertElementContain(ShipmentSelectors.RoutingStatus, statusValue)
 });
+
 //#endregion
 
 When("create shipment", () => {
@@ -73,6 +130,7 @@ When("create shipment", () => {
 When("update shipment", () => {
   Actions.UpdateShipment(ShipmentSelectors.ShipmentSaveButton)
 });
+
 
 Then("the direct should update successfully", () => {
   BaseAssertion.AssertStatusCode(RequestAliases.ShipmentRequest, 200);
