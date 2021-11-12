286aab966f369623a6c81728e7cab1eb1416dd1a
Alaa Jamal <Alaa@logitudeworld.com>
2021-11-12 23:13:32 +0200

    #150009

--- Logitude/Logitude.Accounting.Data/EntityListQueryServices/LedgerTransactionListQueryService.cs
+++ Logitude/Logitude.Accounting.Data/EntityListQueryServices/LedgerTransactionListQueryService.cs
@@ -314,7 +314,7 @@ namespace Logitude.Accounting.Data.EntityListQueryServices
         private List<string> GetTaxReportLinesJournalIds(TaxReportList taxReport, string inputOrOutput)
         {
           return  (from a in context.TaxReportLines
-             where a.TaxReportId == taxReport.Id && a.Tenant == taxReport.Tenant && a.OutputOrInput == inputOrOutput
+             where a.TaxReportId == taxReport.Id && a.Tenant == taxReport.Tenant && a.OutputOrInput == inputOrOutput && a.TransmitStatusCode !="3"
                    select a.JournalId).ToList();
         }
         private TaxReportList GetTaxReport(string Id, int tenant)
