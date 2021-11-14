524e0c81acd015599816966fae15e85993d7cbba
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2021-11-14 15:37:40 +0200

    fix

--- Logitude/CommunicationWorkerRole/ThreadedRoleEntryPoint.cs
+++ Logitude/CommunicationWorkerRole/ThreadedRoleEntryPoint.cs
@@ -532,8 +532,8 @@ namespace CommunicationWorkerRole
                 }
                 catch(Exception ex)
                 {
-                    EventLog eventLog = new EventLog();
-                    eventLog.WriteEntry("LogitudeBatchServices Exception : " + ex.ToString(), EventLogEntryType.Error);
+                    //EventLog eventLog = new EventLog();
+                    //eventLog.WriteEntry("LogitudeBatchServices Exception : " + ex.ToString(), EventLogEntryType.Error);
                     ExceptionHandler.HandleException(ex, DateTime.UtcNow, 0, "", "WorkerRole", "ThreadedRoleEntryPoint :  Creating Instance for service: "+ Service?.ClassName, null);
                 }
             }
