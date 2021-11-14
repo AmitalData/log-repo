405f886a8b6b0116b89a190d6845adfee80a6b93
Heba Mubarak <heba@logitudeworld.com>
2021-11-14 17:05:52 +0200

    fix

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/ShipmentsModel/ImporterShipmentsController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/ShipmentsModel/ImporterShipmentsController.cs
@@ -1256,10 +1256,11 @@ namespace WebFreight.Web.Controllers.ShipmentsModel
 
         private string GetCardId(ShipmentAM entityAM, CodeProperties card)
         {
-            if (card == null && string.IsNullOrEmpty(card.Code) && string.IsNullOrEmpty(card.Id))
-                return null; 
-          
-                return CardCodePropertiesMapping.GetCardIdFromCardProperties(entityAM.ImporterTenant, card); 
+            if (card == null) return null;
+            if (string.IsNullOrEmpty(card.Code) && string.IsNullOrEmpty(card.Id))
+                return null;
+
+            return CardCodePropertiesMapping.GetCardIdFromCardProperties(entityAM.ImporterTenant, card); 
              
         }
     }
