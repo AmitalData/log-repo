995f25e6d3d46e2b739f60c5487ca31297e1a172
Heba Mubarak <heba@logitudeworld.com>
2021-11-22 09:45:02 +0200

    fix

--- Logitude/Logitude.BL/ShipmentsModel/Tools/EntityService/ExporterShipmentAMMappingService.cs
+++ Logitude/Logitude.BL/ShipmentsModel/Tools/EntityService/ExporterShipmentAMMappingService.cs
@@ -32,7 +32,25 @@ namespace Logitude.BL.ShipmentsModel.Tools.EntityService
             MapShipmentShipper(newAExporterShipmentAM);
             MapShipmentCustomer(newAExporterShipmentAM);
             MapShipmentPorts(shipmentPM, newAExporterShipmentAM);
+            MapPartners(newAExporterShipmentAM);
+
             return newAExporterShipmentAM;
+        }
+
+        private void MapPartners(NewAExporterShipmentAM newAExporterShipmentAM)
+        {
+            newAExporterShipmentAM.Agent = GetCard(shipmentPM.AgentId);
+            newAExporterShipmentAM.Consignee = GetCard(shipmentPM.ConsigneeId);
+        }
+
+        private CodeProperties GetCard(string cardId)
+        {
+            Card card = cardsReporistory.GetSingleCard(cardId, shipmentPM.Tenant);
+            return new CodeProperties()
+            {
+                Code = card != null ? card.Code : "",
+            };
+
         }
 
         private HybridPartnerPM GetHybridPartner()
@@ -52,7 +70,7 @@ namespace Logitude.BL.ShipmentsModel.Tools.EntityService
                 DirectionId = shipmentPM.DirectionId,
                 CustomerShipmentNumber = shipmentPM.ShipmentNumber,
                 ShipmentTypeId = shipmentPM.ShipmentTypeId,
-                ConsigneeName = shipmentPM.ShipperName,
+                ConsigneeName = shipmentPM.ConsigneeName,
                 InvoiceReference = shipmentPM.PrivateLabelInvoiceNumber,
                 CustomerReference = shipmentPM.CustomerReference1,
                 IncludePickup = shipmentPM.PrivateLabelIncludePickup,
