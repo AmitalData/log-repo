5ef3117666ca100033a316a8976811ffb4fcb9e8
Heba Mubarak <heba@logitudeworld.com>
2021-11-22 11:49:32 +0200

    fix

--- Logitude/Logitude.BL/Helpers/DWObjectFieldAdditionalFactService.cs
+++ Logitude/Logitude.BL/Helpers/DWObjectFieldAdditionalFactService.cs
@@ -37,24 +37,33 @@ namespace Logitude.BL.Helpers
         {
             string factCode = !string.IsNullOrEmpty(DwObjectTable.ParentFactCode) ? DwObjectTable.ParentFactCode : factTableCode;
 
+
+
             var factDWObjectFieldPMs = groupedByCategory ? dWObjectFieldQuery.GetDWObjectFieldPMsByDWObjectTabelAndTenantGroupedByCategory(0, factCode, DwObjectTable.RecordType) : dWObjectFieldQuery.GetDWObjectFieldByDWObjectTableCode(0, factTableCode).Where(d => d.DisplayInQueryBuilder == true && (string.IsNullOrEmpty(d.RecordType) || (!string.IsNullOrEmpty(d.RecordType) && d.RecordType.IndexOf(DwObjectTable.RecordType) > -1))).ToList();
             DWObjectFieldPMs = new List<DWObjectFieldPM>();
             if (IsHaveAddAdditionalFactFields)
             {
-                var additionalFactDWObjectFieldPMs = groupedByCategory ? dWObjectFieldQuery.GetDWObjectFieldPMsByDWObjectTabelAndTenantGroupedByCategory(0, DwObjectTable.AdditionalFactCode , DwObjectTable.RecordType) : dWObjectFieldQuery.GetDWObjectFieldByDWObjectTableCode(0, DwObjectTable.AdditionalFactCode).Where(d=> string.IsNullOrEmpty(d.RecordType) || (!string.IsNullOrEmpty(d.RecordType) &&  d.RecordType.IndexOf(DwObjectTable.RecordType) > -1)).ToList();
-                foreach (DWObjectFieldPM additionalFactField in additionalFactDWObjectFieldPMs.Where(d => d.IsMeasurement == false && (d.DisplayInQueryBuilder || d.IsCustom)))
-                { 
+                var additionalFactDWObjectFieldPMs = groupedByCategory ? dWObjectFieldQuery.GetDWObjectFieldPMsByDWObjectTabelAndTenantGroupedByCategory(0, DwObjectTable.AdditionalFactCode, DwObjectTable.RecordType) : dWObjectFieldQuery.GetDWObjectFieldByDWObjectTableCode(0, DwObjectTable.AdditionalFactCode).Where(d => string.IsNullOrEmpty(d.RecordType) || (!string.IsNullOrEmpty(d.RecordType) && d.RecordType.IndexOf(DwObjectTable.RecordType) > -1)).ToList();
+                foreach (DWObjectFieldPM additionalFactField in additionalFactDWObjectFieldPMs.Where(d => d.IsMeasurement == false && (d.DisplayInQueryBuilder || (d.IsCustom && !DwObjectTable.HasCustomFields))))
+                {  
+                    if (additionalFactField.IsCustom && additionalFactField.DWObjectTableCode == "Fact_ARInvoices")
+                    { 
+
+                    } 
+
                     var dwObjectField = !string.IsNullOrEmpty(additionalFactField.OriginalObjectFieldCode) ? factDWObjectFieldPMs.Where(d => d.OriginalObjectFieldCode == additionalFactField.OriginalObjectFieldCode).FirstOrDefault() : null;
                     if (dwObjectField == null)
                     {
                         dwObjectField = !string.IsNullOrEmpty(additionalFactField.Code) ? factDWObjectFieldPMs.Where(d => d.Code == additionalFactField.Code).FirstOrDefault() : null;
                         if (dwObjectField == null && CanAddAdditionalFactField(additionalFactField))
-                        { 
-                                DWObjectFieldPMs.Add(additionalFactField);
+                        {
+                            DWObjectFieldPMs.Add(additionalFactField);
                         }
                     }
                 }
             }
+
+
 
             DWObjectFieldPMs = DWObjectFieldPMs.Concat(factDWObjectFieldPMs).ToList();
         }
