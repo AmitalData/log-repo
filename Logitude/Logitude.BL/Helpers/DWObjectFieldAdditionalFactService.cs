858708f97e4e786538c44aca6b8ae2f85a410b20
Heba Mubarak <heba@logitudeworld.com>
2021-11-22 11:56:44 +0200

    fix

--- Logitude/Logitude.BL/Helpers/DWObjectFieldAdditionalFactService.cs
+++ Logitude/Logitude.BL/Helpers/DWObjectFieldAdditionalFactService.cs
@@ -45,12 +45,7 @@ namespace Logitude.BL.Helpers
             {
                 var additionalFactDWObjectFieldPMs = groupedByCategory ? dWObjectFieldQuery.GetDWObjectFieldPMsByDWObjectTabelAndTenantGroupedByCategory(0, DwObjectTable.AdditionalFactCode, DwObjectTable.RecordType) : dWObjectFieldQuery.GetDWObjectFieldByDWObjectTableCode(0, DwObjectTable.AdditionalFactCode).Where(d => string.IsNullOrEmpty(d.RecordType) || (!string.IsNullOrEmpty(d.RecordType) && d.RecordType.IndexOf(DwObjectTable.RecordType) > -1)).ToList();
                 foreach (DWObjectFieldPM additionalFactField in additionalFactDWObjectFieldPMs.Where(d => d.IsMeasurement == false && (d.DisplayInQueryBuilder || (d.IsCustom && !DwObjectTable.HasCustomFields))))
-                {  
-                    if (additionalFactField.IsCustom && additionalFactField.DWObjectTableCode == "Fact_ARInvoices")
-                    { 
-
-                    } 
-
+                {    
                     var dwObjectField = !string.IsNullOrEmpty(additionalFactField.OriginalObjectFieldCode) ? factDWObjectFieldPMs.Where(d => d.OriginalObjectFieldCode == additionalFactField.OriginalObjectFieldCode).FirstOrDefault() : null;
                     if (dwObjectField == null)
                     {
