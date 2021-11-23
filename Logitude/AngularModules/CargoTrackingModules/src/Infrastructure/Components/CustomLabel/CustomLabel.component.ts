2484323b2cd24d8700dd682aa2f2a49c2912d511
Amer Aldghameen <Amer@logitudeworld.com>
2021-11-23 14:54:50 +0200

    Add custom label component and Handle Long Texts Appearance in Cargo Tracking

---
+++ Logitude/AngularModules/CargoTrackingModules/src/Infrastructure/Components/CustomLabel/CustomLabel.component.ts
@@ -0,0 +1,71 @@
+import { Component, ContentChild, ElementRef, HostListener, Input, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MessageWindowComponent } from '../MessageWindow/MessageWindowComponent';
+
+@Component({
+    selector: 'custom-label',
+    templateUrl: './CustomLabel.component.html',
+    styleUrls: ['./CustomLabel.component.css']
+})
+export class CustomLabelComponent implements OnInit {
+    @ViewChild('container', { static: true }) container: ElementRef;
+    @ViewChild('content', { static: true }) content: ElementRef;
+    @Input('width') width:number;
+    isHasMore: boolean = false;
+    originText: string;
+    availableWidth:number;
+    iconSize = 10;
+    iconMargin = 4;
+    constructor(public dialog: MatDialog) { }
+
+    ngOnInit() {
+
+    }
+    ngAfterContentInit(): void {
+        this.originText = this.content.nativeElement.innerText;
+        this.checkText();
+    }
+    @HostListener('window:resize', ['$event'])
+    onResize(event) {
+        this.checkText();
+    }
+    
+
+    checkText() {
+        this.availableWidth = this.getAvailableWidth();
+        if (!this.checkOverflow())
+            return;
+        this.isHasMore = true;
+        this.content.nativeElement.innerHTML = this.getSliceFromText();
+    }
+    getAvailableWidth(): number {
+        if(this.width)
+            return this.width;
+        return this.container.nativeElement.parentElement.parentElement.offsetWidth;
+    }
+    getSliceFromText(): any {
+        var fitSize = this.getFitSize(this.originText)
+        return this.originText.slice(0, fitSize - 1);
+    }
+    getFitSize(text: string): number {
+        var textWidth = this.content.nativeElement.offsetWidth;
+        var newWidth = this.availableWidth - this.iconSize - this.iconMargin - this.iconMargin ;
+        return (text.length * newWidth) / textWidth;
+    }
+
+    checkOverflow() {
+        if (this.content.nativeElement.offsetWidth <= 0)
+            return false;
+        return  this.availableWidth < this.container.nativeElement.parentElement.offsetWidth;
+    }
+
+    showPopup(event) {
+        event.stopPropagation();
+        this.dialog.open(MessageWindowComponent, {
+            data: {
+                title: '',
+                description: this.originText,
+            }
+        });
+    }
+}
