22eb3df7f58babf5e3789af63bc663d31dbe64a7
Ayed Ramadeen <ayed@logitudeworld.com>
2021-11-28 13:28:28 +0200

    Create Interest Transaction for Adjustments

---
+++ Logitude/Logitude.Accounting.Data/Enums/InterestEntityTypes.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Logitude.Accounting.Data.Enums
+{
+    public struct InterestEntityTypes
+    {
+        public const string ARInvoice = "1";
+        public const string ARPayment = "2";
+        public const string Journal = "3";
+        public const string OpenBalance = "4";
+    }
+}
