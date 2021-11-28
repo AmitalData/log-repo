84ba89997a838bb49cf4f4048ccb7588558d24bb
Maheera Jazi <Maheera@logitudeworld.com>
2021-11-28 08:45:53 +0200

    #151400

--- Logitude/AngularModules/AngularModules/Quote/Components/Templates/FieldTemplateComponent.ts
+++ Logitude/AngularModules/AngularModules/Quote/Components/Templates/FieldTemplateComponent.ts
@@ -1,6 +1,10 @@
-import {Component} from '@angular/core';
 
-@Component({    
+
+import { Component } from '@angular/core';
+import { AppTool } from '../../../Infrastructure/Tools';
+import { CRMTool } from '../../../CRM/Tools';
+
+@Component({
     templateUrl: './FieldTemplateComponent.html',
 })
 
@@ -12,6 +16,7 @@ export class FieldTemplateComponent {
     public SpotlightDataTemplate: string = null;
     public IsSpotLightTemplate: boolean = false;
     public IsInlandDomestic: boolean = false;
+    public ImageSrc: string = null;
     constructor() {
 
     }
@@ -28,6 +33,18 @@ export class FieldTemplateComponent {
 
             if (this.Entity.DirectionId == 'D' && this.Entity.TransportModeId == "I") {
                 this.IsInlandDomestic = true;
+            }
+
+            else if (this.FieldName == "LastActivityTypeCode") {
+                if (!AppTool.IsNullOrEmpty(this.Entity.LastActivityTypeCode)) {
+                    this.ImageSrc = CRMTool.GetActivityImageSrc(this.Entity.LastActivityTypeCode);
+                }
+            }
+
+            else if (this.FieldName == "NextActivityTypeCode") {
+                if (!AppTool.IsNullOrEmpty(this.Entity.NextActivityTypeCode)) {
+                    this.ImageSrc = CRMTool.GetActivityImageSrc(this.Entity.NextActivityTypeCode);
+                }
             }
         }
     }
