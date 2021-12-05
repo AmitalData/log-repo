54a8061b5504ebb30cc0bb42fd32c5e89f2ea973
Izik Fridland <IzikFr@amital.co.il>
2021-12-05 11:08:42 +0200

    #151153 #151138 #151553

--- Logitude/AngularModules/AngularModules/QuoteOPM/Components/NewEntity/Services/new-quote-focus-error/new-quote-focus-error.service.ts
+++ Logitude/AngularModules/AngularModules/QuoteOPM/Components/NewEntity/Services/new-quote-focus-error/new-quote-focus-error.service.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectorRef, ElementRef, Injectable } from '@angular/core';
+import { ChangeDetectorRef, ElementRef, Injectable, isDevMode } from '@angular/core';
 import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { NewQuoteDataShareService } from '../new-quote-data-share/new-quote-data-share.service';
 
 @Injectable()
 export class NewQuoteFocusErrorService {
   newQuoteRef: ElementRef = null as any;
+  controlList: FormGroup["controls"] = null as any;
 
   constructor(
     private dataShareService: NewQuoteDataShareService,
@@ -12,35 +13,45 @@ export class NewQuoteFocusErrorService {
 
   focusError(form: FormGroup, elmRef: ElementRef) {
     this.newQuoteRef = elmRef
-    const controlList: FormGroup["controls"] = form.controls;
+    this.controlList = form.controls;
 
-    const partnerInvalid: string = ['shipper', 'consignee'].find((ctrlName: string) => controlList[ctrlName].invalid);
+    this.focusPartnerError()
+    this.focusPropertiesError()
+    this.focusAllError()
+  }
+
+  private focusPartnerError() {
+    const partnerInvalid: string = ['shipper', 'consignee'].find((ctrlName: string) => this.controlList[ctrlName].invalid);
     if (partnerInvalid) {
-      if((<FormGroup>controlList[partnerInvalid]).controls.partner.valid)
+      if ((<FormGroup>this.controlList[partnerInvalid]).controls.partner.valid)
         this.dataShareService.partnersHidden[partnerInvalid].next(false);
 
       const invalidControl: HTMLElement = this.newQuoteRef.nativeElement.querySelector('app-new-quote-partner')
       return this.scrollToHtmlElm(invalidControl);
     }
+  }
 
-    const i = (controlList.properties as FormArray).controls.findIndex((control) => control.invalid);
+  private focusPropertiesError() {
+    const i = (this.controlList.properties as FormArray).controls.findIndex((control) => control.invalid);
     if (i > - 1) {
       this.dataShareService.indexPropertyTab.next(i)
-      const propertyCtrls: FormGroup["controls"] = ((controlList.properties as FormArray).at(0) as FormGroup).controls;
+      const propertyCtrls: FormGroup["controls"] = ((this.controlList.properties as FormArray).at(0) as FormGroup).controls;
 
       const ctrlName = Object.keys(propertyCtrls).find((ctrlName: string) => propertyCtrls[ctrlName].invalid);
-      setTimeout(() => 
+      setTimeout(() =>
         this.scrollToHtmlElm(this.findControl(ctrlName)), 10);
-      return 
+      return
     }
+  }
 
-    const ctrlName = Object.keys(controlList).find(ctrl => controlList[ctrl] instanceof FormControl && controlList[ctrl].invalid)
+  private focusAllError() {
+    const ctrlName = Object.keys(this.controlList).find(ctrl => this.controlList[ctrl] instanceof FormControl && this.controlList[ctrl].invalid)
     if (!ctrlName)
       return this.scrollToHtmlElm(this.findControl(ctrlName));
   }
 
   private findControl(ctrlName: string): HTMLElement {
-    return this.newQuoteRef.nativeElement.querySelector('[formcontrolname="' + ctrlName + '"], [ng-reflect-name="' + ctrlName + '"]');
+    return this.newQuoteRef.nativeElement.querySelector(`#${ctrlName}, [ng-reflect-name="${ctrlName}"]`);
   }
 
   private scrollToHtmlElm(htmlElm: HTMLElement) {
