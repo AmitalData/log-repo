465e9a939bf2ab4f8e45814c69a1414aa5f0c1ad
Moran Segev <moran@amital.co.il>
2021-12-07 15:31:21 +0200

    151401

--- Logitude/Logitude.CustomsMessaging/U2L/CommDec/CommDecService.cs
+++ Logitude/Logitude.CustomsMessaging/U2L/CommDec/CommDecService.cs
@@ -892,6 +892,7 @@ namespace Logitude.CustomsMessaging.U2L.CommDec
                     {
                         if (currentDeclarationCourierStatusPM.ChangeSetOp != ChangeSetOperation.Update) currentDeclarationCourierStatusPM.ChangeSetOp = ChangeSetOperation.Update;
                         currentDeclarationCourierStatusPM.ShopId = shopId;
+                        this.IsProcedureCurrentCodeChanged = true;
                     }
                 }
             }
