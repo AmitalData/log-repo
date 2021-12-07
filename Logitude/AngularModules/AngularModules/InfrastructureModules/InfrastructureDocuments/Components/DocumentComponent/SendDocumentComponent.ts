e1c359b249c13b258a0a56636fd7cc8b210dbdba
Heba Mubarak <heba@logitudeworld.com>
2021-12-07 12:29:02 +0200

    152251

--- Logitude/AngularModules/AngularModules/InfrastructureModules/InfrastructureDocuments/Components/DocumentComponent/SendDocumentComponent.ts
+++ Logitude/AngularModules/AngularModules/InfrastructureModules/InfrastructureDocuments/Components/DocumentComponent/SendDocumentComponent.ts
@@ -795,7 +795,9 @@ export class SendDocumentComponent implements OnInit, AfterViewInit {
                             this.Subject = this.SelectedInternalDocument.CurrentDocument.DocumentTypeSubject != null ? this.SelectedInternalDocument.CurrentDocument.DocumentTypeSubject : this.SelectedInternalDocument.CurrentDocument.DocumentTypeName;
                         }
 
-
+                        if (this.SelectedInternalDocument && !AppTool.IsNullOrEmpty(this.SelectedInternalDocument.ToSpecificeEmail)) {
+                            this.ToEmail = (this.ToEmail ? this.ToEmail+";" : "") + this.SelectedInternalDocument.ToSpecificeEmail;
+                        }
 
                         this.ReloadFroalaEditor();
 
