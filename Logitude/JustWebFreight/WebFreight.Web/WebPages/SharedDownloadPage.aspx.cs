dbb732400aeb9df589a7391745d869b9a62088f5
Ayed Ramadeen <ayed@logitudeworld.com>
2021-12-08 09:05:01 +0200

    Download Documents Blank Page Problem

--- Logitude/JustWebFreight/WebFreight.Web/WebPages/SharedDownloadPage.aspx.cs
+++ Logitude/JustWebFreight/WebFreight.Web/WebPages/SharedDownloadPage.aspx.cs
@@ -198,7 +198,6 @@ namespace WebFreight.Web.WebPages
                     Dictionary<string, byte[]> CompressedArray = new Dictionary<string, byte[]>();
                     bool DocumentsExistance = false;
                     var ItemNum = 0;
-                    documents = documents.Where(x => x.HasFile && x.DirectionCode == "I").ToList();
                     foreach (DocumentsFilingPM document in documents)
                     {
                         if (document.DirectionCode == "O" && document.DoucmentTypeTemplateFormatCode == "M")
