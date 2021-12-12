c71c5115a37b20c36bbd72fcdc570e37c19b96dc
Samar Abu Eid <Samar@logitudeworld.com>
2021-12-12 13:21:59 +0200

    #151626

--- Logitude/Simplog.Data/ShipmentsModel/Mapping/ShipmentPayableMap.cs
+++ Logitude/Simplog.Data/ShipmentsModel/Mapping/ShipmentPayableMap.cs
@@ -10,7 +10,7 @@ namespace Simplog.Data.ShipmentsModel.Mapping
         {
             this.HasKey(t => t.Id);
             this.Property(t => t.Id).IsRequired().HasMaxLength(15).IsUnicode(false);
-            this.Property(t => t.Notes).HasMaxLength(250).IsUnicode(true);
+            this.Property(t => t.Notes).HasMaxLength(1000).IsUnicode(true);
             this.Property(t => t.ShipmentId).IsRequired().HasMaxLength(15).IsUnicode(false);
             this.Property(t => t.ChargesTypeId).IsRequired().HasMaxLength(15).IsUnicode(false);
             this.Property(t => t.ShipmentPayableLineStatusCode).IsRequired().HasMaxLength(4).IsUnicode(false);
