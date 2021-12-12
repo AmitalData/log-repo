57bef515750cf3ea87f0f93f925d69cc7a2861f1
Ahmad Abdalrazeq <a.abdalrazeq@logitudeworld.com>
2021-12-12 09:15:42 +0200

    Event creation result will always add the event type even if the condition not met
    Related Work Items: #152529

--- Logitude/Logitude.Server.Tools/EntityChanges/AutomationResult/AutomationEventCreationService.cs
+++ Logitude/Logitude.Server.Tools/EntityChanges/AutomationResult/AutomationEventCreationService.cs
@@ -47,7 +47,10 @@ namespace Logitude.Server.Tools.EntityChanges.AutomationResult
             EntityChangeAutomation entityChangesAutomation = CreateEntityChangeAutomation(automation);
             ValidateAutomationResultClass validateResult = CreateValidateAutomationResultClass(automation, entityChangesAutomation);
             if (!validateResult.IsAutomationValid)
+            {
+                HandleAutomationFailure(dateBefore, entityChangesAutomation);
                 return;
+            }
 
             if (validateResult.Type != "Delayed")
             {
@@ -56,6 +59,13 @@ namespace Logitude.Server.Tools.EntityChanges.AutomationResult
             }
             DelaytimeDetails delaytimeDetails = new DelaytimeDetails() { Type = validateResult.Type, Delaytime = validateResult.Delaytime, DelaytimeIndicator = validateResult.DelaytimeIndicator, DelaytimeOp = validateResult.DelaytimeOp, SelectedDelaytimeFieldCode = validateResult.SelectedDelaytimeFieldCode };
             AddAutomationQueue(new AutomationQueueArgs() { EntityChangeId = automationResultArgs.EntityChange.Id, AutomationId = automation.Id, AutomationType = automationResultArgs.EntityChangeArgs.ProcessType, EntityId = automationResultArgs.EntityChange.EntityId, Tenant = automation.Tenant, AutomationDelayTime = GetAutomationDelayTime(delaytimeDetails, automationResultArgs.AutomationFieldLists, automationResultArgs.EntityChange.Tenant) });
+        }
+
+        private void HandleAutomationFailure(DateTime dateBefore, EntityChangeAutomation entityChangesAutomation)
+        {
+            entityChangesAutomation.DoneDate = TenantServerConfigration.GetCurrentDateTime(automationResultArgs.EntityChangeArgs.Tenant);
+            automationResultArgs.MainEntityChangeService.EntityChangesAutomationsFailedList.Add(entityChangesAutomation);
+            entityChangesAutomation.ExecutionTime = (int)((DateTime.Now.Ticks - dateBefore.Ticks) / TimeSpan.TicksPerMillisecond);
         }
 
         public void CreateEvent(AutomationEventCreationArguments automationEventCreationArguments)
