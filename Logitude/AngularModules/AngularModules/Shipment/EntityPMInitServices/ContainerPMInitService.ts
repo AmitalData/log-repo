150b3d56705afec97c151d8df08185c397f35274
Abdullah Beitello <AbdullahB@logitudeworld.com>
2021-12-16 11:16:56 +0200

    #152811

--- Logitude/AngularModules/AngularModules/Shipment/EntityPMInitServices/ContainerPMInitService.ts
+++ Logitude/AngularModules/AngularModules/Shipment/EntityPMInitServices/ContainerPMInitService.ts
@@ -104,7 +104,6 @@ export class ContainerPMInitService {
             entityPM.UIProperties.SetEnabled("IsAutomaticUpdates", "Container", false);
             entityPM.UIProperties.SetEnabled("CancelledDate", "Container", false);
             entityPM.UIProperties.SetEnabled("IsCancelled", "Container", false);
-            entityPM.UIProperties.SetEnabled("TerminalId", "Container", false);
 
         }
     }
