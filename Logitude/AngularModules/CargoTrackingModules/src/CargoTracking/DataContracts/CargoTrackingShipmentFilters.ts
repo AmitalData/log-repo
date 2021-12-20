181b52002f9487ecea1b4a381e8c19c922f5fed0
Amer Aldghameen <Amer@logitudeworld.com>
2021-12-20 12:15:13 +0200

    Cargo Tracking : Filters issues - R5

--- Logitude/AngularModules/CargoTrackingModules/src/CargoTracking/DataContracts/CargoTrackingShipmentFilters.ts
+++ Logitude/AngularModules/CargoTrackingModules/src/CargoTracking/DataContracts/CargoTrackingShipmentFilters.ts
@@ -1,17 +1,20 @@
-export class CargoTrackingShipmentFilters
-{
-    public Tenant: number;
-    public SearchText: string;
-    public CustomersIds: string[] = [];
-    public CustomersIdsString: string;
-    public MilestonesStatus: string;
-    public SelectedMilestonesStatus: any[] = [];
-    public TransportModeCodes: string;
-    public DirectionCodes: string;
-    public SortDescending: string;
-    public SortFieldName: string;
-    public SelectedInvitedCustomers: any[];
+export class MoreFilter{
     public HasException: boolean = false;
     public OrdersOnly: boolean = false;
     public EstimatedArrivalOnly: boolean = false;
 }
+export class CargoTrackingShipmentSearchInput extends MoreFilter
+{
+    public Tenant: number;
+    public SearchText: string;
+    public CustomersIds: string[] = [];
+    public MilestonesCodes: string[] = [];
+    public TransportModeCodes: string[] = [];
+    public DirectionCodes: string[] = [];
+    public SortType: string;
+    public SortFieldName: string;
+    public PageIndex: number;
+    public PageSize: number;
+
+}
+
