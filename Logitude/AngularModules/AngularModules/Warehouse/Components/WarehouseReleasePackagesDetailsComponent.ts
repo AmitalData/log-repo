1cdd09cdd6360fd44fa39e880e75b1663d7d812f
Heba Mubarak <heba@logitudeworld.com>
2021-12-22 11:34:28 +0200

    152864

--- Logitude/AngularModules/AngularModules/Warehouse/Components/WarehouseReleasePackagesDetailsComponent.ts
+++ Logitude/AngularModules/AngularModules/Warehouse/Components/WarehouseReleasePackagesDetailsComponent.ts
@@ -247,7 +247,7 @@ export class WarehouseReleasePackagesDetailsComponent extends BaseComponent impl
         }
         logWindow.Width = 1250;
         logWindow.Height = 550;
-        logWindow.Title = "Choose Packages";
+        logWindow.Title = "Choose " + packageType+"s";
         logWindow.WindowArgs = windowArgs;
         logWindow.Show("./Warehouse/Components/WarehouseReleaseChoosePackagesComponent");
 
