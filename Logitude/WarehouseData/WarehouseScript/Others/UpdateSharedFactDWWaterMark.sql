b4c022df1bede621946381d30eca6e44afc8c538
Abed <abed@logitudeworld.com>
2021-12-22 11:22:39 +0200

    153444
    Related Work Items: #153444

--- Logitude/WarehouseData/WarehouseScript/Others/UpdateSharedFactDWWaterMark.sql
+++ Logitude/WarehouseData/WarehouseScript/Others/UpdateSharedFactDWWaterMark.sql
@@ -1,3 +1,12 @@
+	
+	IF (OBJECT_ID ('dw_ARInvoices', 'U')  IS NOT NULL)
+    BEGIN
 	update dw_WaterMarks set LastUpdateDate =  (select  MAX( AutomaticLastUpdateDate) AutomaticLastUpdateDate from dw_ARInvoices ) where TableName = 'ARInvoice'
+	end
 
+
+	
+IF (OBJECT_ID ('dw_Shipments', 'U')  IS NOT NULL)
+BEGIN
 update dw_WaterMarks set LastUpdateDate = (select  MAX( AutomaticLastUpdateDate) AutomaticLastUpdateDate from dw_Shipments ) where TableName = 'Shipment'
+end
