c73de0fd543f7eb8b52498daf57ff5c819528fcf
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2021-12-22 10:34:01 +0000

    fix Alaa tahkeesh

--- Logitude/Logitude.Accounting.BL/CoreBL/OpenFormatReportService.cs
+++ Logitude/Logitude.Accounting.BL/CoreBL/OpenFormatReportService.cs
@@ -806,13 +806,13 @@ namespace Logitude.Accounting.BL.CoreBL
                     else
                     {
                         myStringBuilder.Append(a);
-                        myStringBuilder.Append(' ', 260);
+                        myStringBuilder.Append(' ', 130);
                     }
                 }
                 else
                 {
                     myStringBuilder.Append(a);
-                    myStringBuilder.Append(' ', 260);
+                    myStringBuilder.Append(' ', 130);
                 }
                 myStringBuilder.Append(a);
                 myStringBuilder.Append(' ', 15);
