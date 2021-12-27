9a1c01607747e24f5bba0b11e9afcb8b89db5005
Abed <abed@logitudeworld.com>
2021-12-27 13:42:32 +0200

    152913
    Related Work Items: #152913

--- Logitude/CommunicationWorkerRole/ImporterShipmentDocumentsBatchWorkerRole.cs
+++ Logitude/CommunicationWorkerRole/ImporterShipmentDocumentsBatchWorkerRole.cs
@@ -732,7 +732,7 @@ namespace CommunicationWorkerRole
                                 catch (Exception ex)
                                 {
                                     #region Exception handling
-                                    ExceptionHandler.HandleException(ex, DateTime.Now, 0, null, "importer shipments worker role start", null, null);
+                                    ExceptionHandler.HandleException(ex, DateTime.Now, 0, null, "importer shipments documents Batch worker role start", null, null);
 
                                     string Status = "F";
                                     if (ex.Message == "EntityNumber is null Or Document has No file" || ex.Message == "Customer Has No Access To send Document")
@@ -807,7 +807,7 @@ namespace CommunicationWorkerRole
                         catch (Exception ex)
                         {
                             ConnectClient();
-                            ExceptionHandler.HandleException(ex, DateTime.Now, 0, null, "importer shipments worker role start", null, null);
+                            ExceptionHandler.HandleException(ex, DateTime.Now, 0, null, "importer shipments documents Batch worker role ", null, null);
                             Thread.Sleep(10000);
                         }
 
@@ -821,7 +821,7 @@ namespace CommunicationWorkerRole
             catch (Exception ex)
             {
                 ConnectClient();
-                ExceptionHandler.HandleException(ex, DateTime.Now, 0, null, "importer shipments worker role start", null, null);
+                ExceptionHandler.HandleException(ex, DateTime.Now, 0, null, "importer shipments documents Batch worker role ", null, null);
                 Thread.Sleep(10000);
             } 
         }
