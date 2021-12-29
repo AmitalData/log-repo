f81dbaaa52e0a444ea9d0c08379fb5b748ef3611
Samar Abu Eid <Samar@logitudeworld.com>
2021-12-29 12:21:04 +0200

    #154099

--- Logitude/Logitude.BL/InvoiceModel/Tools/EntityService/APInvoiceNormalService.cs
+++ Logitude/Logitude.BL/InvoiceModel/Tools/EntityService/APInvoiceNormalService.cs
@@ -606,6 +606,7 @@ namespace Logitude.BL.InvoiceModel.Tools.EntityService
                 if (entityPM.AmountInInvoiceCurrency == 0)
                 {
                     entityPM.StatusCode = "PD";
+                    entityPM.IsClosed = true;
                 }
 
                 else
