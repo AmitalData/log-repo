7ac66b661f1a2c5cd2fc19d6f96f3d61a18a4e6c
Itzik Moshelian <itzik@amital.co.il>
2021-12-30 08:29:07 +0200

    #153770

--- Logitude/Logitude.CustomsMessaging/Dca/WaitingTester.cs
+++ Logitude/Logitude.CustomsMessaging/Dca/WaitingTester.cs
@@ -20,21 +20,32 @@ namespace Logitude.CustomsMessaging.Dca
 
             try
             {
-                var o = new Logitude.CustomsMessaging.MessagingServices.NG_9100_MSG_OutgoingMessageRequestMessagingService();
+                var sYSTBL_NG_9010_MSG_MessageRestoreRequestMessagingService = new Logitude.CustomsMessaging.MessagingServices.SYSTBL_NG_9010_MSG_MessageRestoreRequestMessagingService();
+                var messageRestoreRequestParams= new MessageRestoreRequestParams
+                {
+                    //SuppressSplitWR = true,
+                    Tenant = 1,
+                    FromDate = DateTime.Now.AddHours(-15),
+                    ToDate = DateTime.Now,
+                    InterfaceManagementsCode = "8347",
+                    RequestVIA = Logitude.CustomsMessaging.Common.RequestParams.SendRequestVIA.Default
+                };
+                var res1= sYSTBL_NG_9010_MSG_MessageRestoreRequestMessagingService.Send(messageRestoreRequestParams);
 
-                var res = o.Send(
-                    new MessageWaitingRequestParams
-                    {
-                        //SuppressSplitWR = true,
-                        Tenant = 1,
-                        FromDate = DateTime.Now.AddHours(-15),
-                        ToDate = DateTime.Now,
-                        InterfaceManagementsCode = "8347",
-                        RequestVIA = Logitude.CustomsMessaging.Common.RequestParams.SendRequestVIA.WebServiceBatch
-                    });
+
+                var o = new Logitude.CustomsMessaging.MessagingServices.NG_9100_MSG_OutgoingMessageRequestMessagingService();
+                var messageWaitingRequestParams = new MessageWaitingRequestParams
+                {
+                    //SuppressSplitWR = true,
+                    Tenant = 1,
+                    FromDate = DateTime.Now.AddHours(-15),
+                    ToDate = DateTime.Now,
+                    InterfaceManagementsCode = "8347",
+                    RequestVIA = Logitude.CustomsMessaging.Common.RequestParams.SendRequestVIA.WebServiceBatch
+                };
+                var res = o.Send(messageWaitingRequestParams);
 
             }
-
             finally
             {
 
