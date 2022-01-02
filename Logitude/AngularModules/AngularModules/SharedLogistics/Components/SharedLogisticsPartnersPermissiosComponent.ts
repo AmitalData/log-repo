d10740d566330d1660fee965b95730c19e76647d
Ahmad Abdalrazeq <a.abdalrazeq@logitudeworld.com>
2022-01-02 15:58:09 +0200

    clean code

--- Logitude/AngularModules/AngularModules/SharedLogistics/Components/SharedLogisticsPartnersPermissiosComponent.ts
+++ Logitude/AngularModules/AngularModules/SharedLogistics/Components/SharedLogisticsPartnersPermissiosComponent.ts
@@ -82,6 +82,7 @@ export class SharedLogisticsPartnersPermissiosComponent implements OnInit {
         item1.SuggestedIsChecked = this.TenantZeroEntity.IsShipperShared;
         item1.ChooseIsChecked = this.EntityPM.IsShipperShared;
         item1.IsShowContactTelSMS = this.EntityPM.IsShipperShowContactTS;
+        item1.IsShowContactTelSMSFieldName = "IsShipperShowContactTS";
 
         var item2: PartnerItem = new PartnerItem(this);
         item2.Code = "CO";
@@ -89,6 +90,7 @@ export class SharedLogisticsPartnersPermissiosComponent implements OnInit {
         item2.SuggestedIsChecked = this.TenantZeroEntity.IsConsigneeShared;
         item2.ChooseIsChecked = this.EntityPM.IsConsigneeShared;
         item2.IsShowContactTelSMS = this.EntityPM.IsConsigneeShowContactTS;
+        item2.IsShowContactTelSMSFieldName = "IsConsigneeShowContactTS";
 
         var item3: PartnerItem = new PartnerItem(this);
         item3.Code = "AG";
@@ -96,6 +98,7 @@ export class SharedLogisticsPartnersPermissiosComponent implements OnInit {
         item3.SuggestedIsChecked = this.TenantZeroEntity.IsAgentShared;
         item3.ChooseIsChecked = this.EntityPM.IsAgentShared;
         item3.IsShowContactTelSMS = this.EntityPM.IsAgentShowContactTS;
+        item3.IsShowContactTelSMSFieldName = "IsAgentShowContactTS";
 
         var item4: PartnerItem = new PartnerItem(this);
         item4.Code = "SN";
@@ -103,6 +106,7 @@ export class SharedLogisticsPartnersPermissiosComponent implements OnInit {
         item4.SuggestedIsChecked = this.TenantZeroEntity.IsShipperNotExporterShared;
         item4.ChooseIsChecked = this.EntityPM.IsShipperNotExporterShared;
         item4.IsShowContactTelSMS = this.EntityPM.IsShipperNotExShowContactTS;
+        item4.IsShowContactTelSMSFieldName = "IsShipperNotExShowContactTS";
 
         var item5: PartnerItem = new PartnerItem(this);
         item5.Code = "CN";
@@ -110,6 +114,7 @@ export class SharedLogisticsPartnersPermissiosComponent implements OnInit {
         item5.SuggestedIsChecked = this.TenantZeroEntity.IsConsigneeNotImporterShared;
         item5.ChooseIsChecked = this.EntityPM.IsConsigneeNotImporterShared;
         item5.IsShowContactTelSMS = this.EntityPM.IsConsigneeNotImShowContactTS;
+        item5.IsShowContactTelSMSFieldName = "IsConsigneeNotImShowContactTS";
 
         var item6: PartnerItem = new PartnerItem(this);
         item6.Code = "N1";
@@ -117,6 +122,7 @@ export class SharedLogisticsPartnersPermissiosComponent implements OnInit {
         item6.SuggestedIsChecked = this.TenantZeroEntity.IsNotify1Shared;
         item6.ChooseIsChecked = this.EntityPM.IsNotify1Shared;
         item6.IsShowContactTelSMS = this.EntityPM.IsNotify1ShowContactTS;
+        item6.IsShowContactTelSMSFieldName = "IsNotify1ShowContactTS";
 
         var item7: PartnerItem = new PartnerItem(this);
         item7.Code = "N2";
@@ -124,6 +130,7 @@ export class SharedLogisticsPartnersPermissiosComponent implements OnInit {
         item7.SuggestedIsChecked = this.TenantZeroEntity.IsNotify2Shared;
         item7.ChooseIsChecked = this.EntityPM.IsNotify2Shared;
         item7.IsShowContactTelSMS = this.EntityPM.IsNotify2ShowContactTS;
+        item7.IsShowContactTelSMSFieldName = "IsNotify2ShowContactTS";
 
         var item8: PartnerItem = new PartnerItem(this);
         item8.Code = "FF";
@@ -131,6 +138,7 @@ export class SharedLogisticsPartnersPermissiosComponent implements OnInit {
         item8.SuggestedIsChecked = this.TenantZeroEntity.IsFreightForwarderShared;
         item8.ChooseIsChecked = this.EntityPM.IsFreightForwarderShared;
         item8.IsShowContactTelSMS = this.EntityPM.IsFreightForwardShowContactTS;
+        item8.IsShowContactTelSMSFieldName = "IsFreightForwardShowContactTS";
 
         var item9: PartnerItem = new PartnerItem(this);
         item9.Code = "CL";
@@ -138,6 +146,7 @@ export class SharedLogisticsPartnersPermissiosComponent implements OnInit {
         item9.SuggestedIsChecked = this.TenantZeroEntity.IsColoaderShared;
         item9.ChooseIsChecked = this.EntityPM.IsColoaderShared;
         item9.IsShowContactTelSMS = this.EntityPM.IsColoaderShowContactTS;
+        item9.IsShowContactTelSMSFieldName = "IsColoaderShowContactTS";
 
         var item10: PartnerItem = new PartnerItem(this);
         item10.Code = "CE";
@@ -145,6 +154,7 @@ export class SharedLogisticsPartnersPermissiosComponent implements OnInit {
         item10.SuggestedIsChecked = this.TenantZeroEntity.IsCustomsAgentExportShared;
         item10.ChooseIsChecked = this.EntityPM.IsCustomsAgentExportShared;
         item10.IsShowContactTelSMS = this.EntityPM.IsCustomAgentExShowContactTS;
+        item10.IsShowContactTelSMSFieldName = "IsCustomAgentExShowContactTS";
 
         var item11: PartnerItem = new PartnerItem(this);
         item11.Code = "CI";
@@ -152,6 +162,7 @@ export class SharedLogisticsPartnersPermissiosComponent implements OnInit {
         item11.SuggestedIsChecked = this.TenantZeroEntity.IsCustomsAgentImportShared;
         item11.ChooseIsChecked = this.EntityPM.IsCustomsAgentImportShared;
         item11.IsShowContactTelSMS = this.EntityPM.IsCustomAgentImShowContactTS;
+        item11.IsShowContactTelSMSFieldName = "IsCustomAgentImShowContactTS";
 
         var item12: PartnerItem = new PartnerItem(this);
         item12.Code = "CC";
@@ -159,6 +170,7 @@ export class SharedLogisticsPartnersPermissiosComponent implements OnInit {
         item12.SuggestedIsChecked = this.TenantZeroEntity.IsCustomClearancePoinShared;
         item12.ChooseIsChecked = this.EntityPM.IsCustomClearancePoinShared;
         item12.IsShowContactTelSMS = this.EntityPM.IsCustomCleaPointShowContactTS;
+        item12.IsShowContactTelSMSFieldName = "IsCustomCleaPointShowContactTS";
 
         var item13: PartnerItem = new PartnerItem(this);
         item13.Code = "CD";
@@ -166,6 +178,7 @@ export class SharedLogisticsPartnersPermissiosComponent implements OnInit {
         item13.SuggestedIsChecked = this.TenantZeroEntity.IsConsolidatorShared;
         item13.ChooseIsChecked = this.EntityPM.IsConsolidatorShared;
         item13.IsShowContactTelSMS = this.EntityPM.IsConsolidatorShowContactTS;
+        item13.IsShowContactTelSMSFieldName = "IsConsolidatorShowContactTS";
 
         var item14: PartnerItem = new PartnerItem(this);
         item14.Code = "RL";
@@ -173,6 +186,7 @@ export class SharedLogisticsPartnersPermissiosComponent implements OnInit {
         item14.SuggestedIsChecked = this.TenantZeroEntity.IsReleasingAgentShared;
         item14.ChooseIsChecked = this.EntityPM.IsReleasingAgentShared;
         item14.IsShowContactTelSMS = this.EntityPM.IsReleasingAgentShowContactTS;
+        item14.IsShowContactTelSMSFieldName = "IsReleasingAgentShowContactTS";
 
         var item15: PartnerItem = new PartnerItem(this);
         item15.Code = "IS";
@@ -180,6 +194,7 @@ export class SharedLogisticsPartnersPermissiosComponent implements OnInit {
         item15.SuggestedIsChecked = this.TenantZeroEntity.IsIssuingCarrierAgentShared;
         item15.ChooseIsChecked = this.EntityPM.IsIssuingCarrierAgentShared;
         item15.IsShowContactTelSMS = this.EntityPM.IsIssuingCarAgentShowContactTS;
+        item15.IsShowContactTelSMSFieldName = "IsIssuingCarAgentShowContactTS";
 
         var item16: PartnerItem = new PartnerItem(this);
         item16.Code = "PI";
@@ -273,6 +288,7 @@ export class SharedLogisticsPartnersPermissiosComponent implements OnInit {
 }
 
 export class PartnerItem {
+
     constructor(public father: SharedLogisticsPartnersPermissiosComponent) {
 
     }
@@ -281,6 +297,7 @@ export class PartnerItem {
     public PartnerName: string;
     public SuggestedIsChecked: boolean;
     public IsEnabled: boolean = true;
+    public IsShowContactTelSMSFieldName: string;
     public: boolean;
 
     private chooseIsChecked: boolean;
@@ -297,7 +314,7 @@ export class PartnerItem {
     public set IsShowContactTelSMS(value: boolean) {
         if (this.isShowContactTelSMS != value) {
             this.isShowContactTelSMS = value;
-            this.SetIsShowContactTelSMS(this.IsShowContactTelSMS);
+            this.father.EntityPM[this.IsShowContactTelSMSFieldName] = this.IsShowContactTelSMS;
         }
     }
 
@@ -385,85 +402,6 @@ export class PartnerItem {
 
             case "MC": {
                 this.father.EntityPM.IsMainCarrierShared = this.ChooseIsChecked;
-                break;
-            }
-        }
-    }
-
-    private SetIsShowContactTelSMS(value: boolean) {
-        switch (this.Code) {
-            case "SH": {
-                this.father.EntityPM.IsShipperShowContactTS = value;
-                break;
-            }
-
-            case "CO": {
-                this.father.EntityPM.IsConsigneeShowContactTS = value;
-                break;
-            }
-
-            case "AG": {
-                this.father.EntityPM.IsAgentShowContactTS = value;
-                break;
-            }
-
-            case "SN": {
-                this.father.EntityPM.IsShipperNotExShowContactTS = value;
-                break;
-            }
-
-            case "CN": {
-                this.father.EntityPM.IsConsigneeNotImShowContactTS = value;
-                break;
-            }
-
-            case "N1": {
-                this.father.EntityPM.IsNotify1ShowContactTS = value;
-                break;
-            }
-
-            case "N2": {
-                this.father.EntityPM.IsNotify2ShowContactTS = value;
-                break;
-            }
-
-            case "FF": {
-                this.father.EntityPM.IsFreightForwardShowContactTS = value;
-                break;
-            }
-
-            case "CL": {
-                this.father.EntityPM.IsColoaderShowContactTS = value;
-                break;
-            }
-
-            case "CE": {
-                this.father.EntityPM.IsCustomAgentExShowContactTS = value;
-                break;
-            }
-
-            case "CI": {
-                this.father.EntityPM.IsCustomAgentImShowContactTS = value;
-                break;
-            }
-
-            case "CC": {
-                this.father.EntityPM.IsCustomCleaPointShowContactTS = value;
-                break;
-            }
-
-            case "CD": {
-                this.father.EntityPM.IsConsolidatorShowContactTS = value;
-                break;
-            }
-
-            case "RL": {
-                this.father.EntityPM.IsReleasingAgentShowContactTS = value;
-                break;
-            }
-
-            case "IS": {
-                this.father.EntityPM.IsIssuingCarAgentShowContactTS = value;
                 break;
             }
         }
