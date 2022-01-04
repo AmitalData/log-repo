0e2cf03f6473458ae20c1a1123fa3f317adf2159
Ahmad Abdalrazeq <a.abdalrazeq@logitudeworld.com>
2022-01-04 09:37:55 +0200

    #153071 Map custom fields  for Custom Agents
    Related Work Items: #153071, #154031

--- Logitude/Logitude.BL/CommonDataModel/EntityQueries/CustomAgentQuery.cs
+++ Logitude/Logitude.BL/CommonDataModel/EntityQueries/CustomAgentQuery.cs
@@ -12,6 +12,7 @@ using Simplog.Server.Infrastructure.Helpers;
 using Logitude.Accounting.Def.EntityQueryServicesExt;
 using Logitude.Server.Tools;
 using Microsoft.Practices.Unity;
+using Simplog.Server.Infrastructure.DataContracts;
 
 namespace Logitude.BL.CommonDataModel.EntityQueries
 {
@@ -105,8 +106,26 @@ namespace Logitude.BL.CommonDataModel.EntityQueries
             }
             CustomAgentPM securedPm = new CustomAgentPM();
             SecuredMapping.GetMappedPM(agent, securedPm, "CustomAgent", agent.Tenant);
-
+            if (securedPm != null && agent != null)
+            {
+                CustomAgent entityPoco = (from s in repository.context.CustomAgents where s.Id == securedPm.Id select s).FirstOrDefault();
+                MapCustomFields(securedPm, entityPoco);
+            }
             return securedPm;
+        }
+
+        private void MapCustomFields(CustomAgentPM customAgent, CustomAgent entityPoco)
+        {
+            customAgent.Field1 = new CustomFieldClass("Field1", "CustomAgent", entityPoco.Field1);
+            customAgent.Field2 = new CustomFieldClass("Field2", "CustomAgent", entityPoco.Field2);
+            customAgent.Field3 = new CustomFieldClass("Field3", "CustomAgent", entityPoco.Field3);
+            customAgent.Field4 = new CustomFieldClass("Field4", "CustomAgent", entityPoco.Field4);
+            customAgent.Field5 = new CustomFieldClass("Field5", "CustomAgent", entityPoco.Field5);
+            customAgent.Field6 = new CustomFieldClass("Field6", "CustomAgent", entityPoco.Field6);
+            customAgent.Field7 = new CustomFieldClass("Field7", "CustomAgent", entityPoco.Field7);
+            customAgent.Field8 = new CustomFieldClass("Field8", "CustomAgent", entityPoco.Field8);
+            customAgent.Field9 = new CustomFieldClass("Field9", "CustomAgent", entityPoco.Field9);
+            customAgent.Field10 = new CustomFieldClass("Field10", "CustomAgent", entityPoco.Field10);
         }
 
         public CustomAgentPM GetSinglePM(int tenant, string id)
@@ -179,7 +198,11 @@ namespace Logitude.BL.CommonDataModel.EntityQueries
             }
             CustomAgentPM securedPm = new CustomAgentPM();
             SecuredMapping.GetMappedPM(agent, securedPm, "CustomAgent", tenant);
-
+            if (securedPm != null && agent != null)
+            {
+                CustomAgent entityPoco = (from s in repository.context.CustomAgents where s.Id == securedPm.Id select s).FirstOrDefault();
+                MapCustomFields(securedPm, entityPoco);
+            }
             return securedPm;
         }
 
@@ -258,7 +281,11 @@ namespace Logitude.BL.CommonDataModel.EntityQueries
             }
             CustomAgentPM securedPm = new CustomAgentPM();
             SecuredMapping.GetMappedPM(agent, securedPm, "CustomAgent", tenant);
-
+            if (securedPm != null && agent != null)
+            {
+                CustomAgent entityPoco = (from s in repository.context.CustomAgents where s.Id == securedPm.Id select s).FirstOrDefault();
+                MapCustomFields(securedPm, entityPoco);
+            }
             return securedPm;
         }
 
@@ -435,6 +462,16 @@ namespace Logitude.BL.CommonDataModel.EntityQueries
                                                       PrimaryContactPhone = a.PrimaryContactPhone,
                                                       StateName = a.Card.StateName,
                                                       GLAccountNumber = a.Card.GLAccountDisplayNumber,
+                                                      Field1 = a.Field1,
+                                                      Field2 = a.Field2,
+                                                      Field3 = a.Field3,
+                                                      Field4 = a.Field4,
+                                                      Field5 = a.Field5,
+                                                      Field6 = a.Field6,
+                                                      Field7 = a.Field7,
+                                                      Field8 = a.Field8,
+                                                      Field9 = a.Field9,
+                                                      Field10 = a.Field10,
                                                   });
 
 
