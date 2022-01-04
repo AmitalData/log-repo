099cc06dbd8e5027c7d495b859c0f192677cba17
Heba Mubarak <heba@logitudeworld.com>
2022-01-04 09:22:51 +0000

    fix

--- Logitude/Logitude.BL/InfrastructureModel/Tools/EntityService/PrivateLabelShipmentService.cs
+++ Logitude/Logitude.BL/InfrastructureModel/Tools/EntityService/PrivateLabelShipmentService.cs
@@ -16,37 +16,32 @@ namespace Logitude.BL.InfrastructureModel.Tools.EntityService
         private TenantPM tenantPM;
         private ShipmentPM shipmentPM;
         private CustomerTenantAccessInfo customerTenantAccessInfo;
-         
+
         public PrivateLabelShipmentService(TenantPM tenantPM, ShipmentPM shipmentPM, CustomerTenantAccessInfo customerTenantAccessInfo)
         {
             this.tenantPM = tenantPM;
             this.shipmentPM = shipmentPM;
             this.customerTenantAccessInfo = customerTenantAccessInfo;
-        } 
+        }
         public PrivateLabelShipmentService(ShipmentPM shipmentPM, CustomerTenantAccessInfo customerTenantAccessInfo)
         {
             this.tenantPM = this.GetTenantPM(shipmentPM.Tenant);
             this.shipmentPM = shipmentPM;
             this.customerTenantAccessInfo = customerTenantAccessInfo;
-        } 
+        }
         public bool IsShipmentsAllowedForLogBox()
         {
-            return (IsCustomsShipmentsAllowedForLogBox() || IsExportShipmentsAllowedForLogBox() || IsImportShipmentsAllowedForLogBox());
+            return (IsCustomsShipmentsAllowedForLogBox() || IsExportShipmentsAllowedForLogBox());
         }
-        public bool IsImportShipmentsAllowedForLogBox()
-        {
-            return (tenantPM.CustomerTenantShareImportFile && shipmentPM.DirectionId.ToUpper() == "I");
-        }
-
         public bool IsCustomsShipmentsAllowedForLogBox()
-        { 
+        {
             return(tenantPM.CustomerTenantShareCustomsFile && customerTenantAccessInfo.IsCustomsActivated && shipmentPM.DirectionId.ToUpper() == "C") ;
-        } 
+        }
         public bool IsExportShipmentsAllowedForLogBox()
         {
             return (tenantPM.CustomerTenantShareExportFile && customerTenantAccessInfo.IsExportActivated && shipmentPM.DirectionId.ToUpper() == "E");
-          
-        } 
+
+        }
         private TenantPM GetTenantPM(int tenant)
         {
             TenantPM TenantPM = TenantQuery.GetSingleTenantPM(tenant, false);
