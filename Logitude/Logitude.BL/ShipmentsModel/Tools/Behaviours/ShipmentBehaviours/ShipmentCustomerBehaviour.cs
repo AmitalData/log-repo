7f91781044f83b485387e5ca761f6d23c2cb49ba
Samar Abu Eid <Samar@logitudeworld.com>
2022-01-05 11:16:54 +0200

    #154362

--- Logitude/Logitude.BL/ShipmentsModel/Tools/Behaviours/ShipmentBehaviours/ShipmentCustomerBehaviour.cs
+++ Logitude/Logitude.BL/ShipmentsModel/Tools/Behaviours/ShipmentBehaviours/ShipmentCustomerBehaviour.cs
@@ -66,7 +66,7 @@ namespace Logitude.BL.ShipmentsModel.Tools.Behaviours.ShipmentBehaviours
 
             else
             {
-                this.CheckCustomerValue();
+                //this.CheckCustomerValue();
             }
         }
 
