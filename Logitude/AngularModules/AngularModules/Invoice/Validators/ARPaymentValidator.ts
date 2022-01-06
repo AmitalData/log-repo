940205e7fcfa23f7673733cc6e126aa9836c2fb0
Heba Mubarak <heba@logitudeworld.com>
2022-01-06 12:33:37 +0200

    validation

--- Logitude/AngularModules/AngularModules/Invoice/Validators/ARPaymentValidator.ts
+++ Logitude/AngularModules/AngularModules/Invoice/Validators/ARPaymentValidator.ts
@@ -135,9 +135,8 @@ export class ARPaymentValidator {
     }
 
 
-      if (!SessionLocator.TenantPM.AccountingActivated && entityPm.AccountingPaymentMethodCode != "CA" && entityPm.AccountingPaymentMethodCode != "FS" && entityPm.ValueDate == null) {
-          entityPm.ValueDate = entityPm.UpdateDate;
-//        validationResults.push(msg.replace("%FieldName", TextCodeTranslator.Translate("ARPayment.F.ValueDate")));
+    if (!SessionLocator.TenantPM.AccountingActivated && entityPm.AccountingPaymentMethodCode != "CA" && entityPm.AccountingPaymentMethodCode != "FS" && entityPm.ValueDate == null) {
+      validationResults.push(msg.replace("%FieldName", TextCodeTranslator.Translate("ARPayment.F.ValueDate")));
     }
 
     return validationResults;
