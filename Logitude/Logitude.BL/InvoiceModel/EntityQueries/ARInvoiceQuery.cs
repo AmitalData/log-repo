585a0c62261e957f4777d6c2d0496d0730ad01d3
Heba Mubarak <heba@logitudeworld.com>
2022-01-06 12:42:12 +0200

    InterestReportI

--- Logitude/Logitude.BL/InvoiceModel/EntityQueries/ARInvoiceQuery.cs
+++ Logitude/Logitude.BL/InvoiceModel/EntityQueries/ARInvoiceQuery.cs
@@ -16,13 +16,15 @@ using Simplog.Server.Infrastructure.Helpers;
 using Logitude.Accounting.Data.Repositories;
 using Simplog.Data.InvoiceModel;
 using Simplog.Data.CommonDataModel;
+using Logitude.Accounting.Data.EntityPOCOs;
 
 namespace Logitude.BL.InvoiceModel.EntityQueries
 {
     public class ARInvoiceQuery
     {
         public ARInvoiceRepository repository;
-
+        private Tenant tenantPoco;
+        private const string InterestReportInvoiceTypeCode = "IT";
         public ARInvoiceQuery()
         {
             repository = new ARInvoiceRepository();
@@ -1830,6 +1832,7 @@ namespace Logitude.BL.InvoiceModel.EntityQueries
                 // Include Bill To is not enough to get customer data
                 CardRepository myCardRepository = new CardRepository(myCommonContext);
                 Card myBillTo = myCardRepository.GetSingleCard(entityPOCO.BillToId, tenant);
+                SetInterestReportFields(entityPM);
                 if (myBillTo != null)
                 {
                     entityPM.BillToName = myBillTo.EnglishName;
@@ -2007,6 +2010,27 @@ namespace Logitude.BL.InvoiceModel.EntityQueries
 
             return securedEntityPM;
         }
+        private void SetInterestReportFields(ARInvoicePM invoice)
+        {
+            InterestReport interestReport = new InterestReport();
+            if (invoice.ARInvoiceTypeCode == InterestReportInvoiceTypeCode && IsAccountingActivated(invoice.Tenant))
+            {
+                InterestReportRepository interestReportRepository = new InterestReportRepository(invoice.Tenant);
+                interestReport = interestReportRepository.GetSingleByARInvoiceId(invoice.Id, invoice.Tenant);
+            }
+            if (interestReport != null)
+            {
+                invoice.InterestReportNumber = interestReport.ReportNumber;
+                invoice.InterestReportId = interestReport.Id;
+            }
+        }
+        private bool IsAccountingActivated(int tenant)
+        {
+            TenantRepository tenantRepository = new TenantRepository(tenant);
+            tenantPoco = tenantRepository.GetSingleTenant(tenant);
+            return tenantPoco.AccountingActivated;
+        }
+
         public IQueryable<ARInvoice> GetAllInterestInvoices(DateTime fromDate, DateTime toDate,bool ShowPrintedInvoice, int tenant)
         {
             var result = (from a in repository.context.ARInvoices where a.Tenant == tenant && a.ARInvoiceTypeCode== "IT" && a.InvoiceDate >= fromDate && a.InvoiceDate <= toDate  select a);
