7947caa44f62216d2440bfe453bf49b675a5babb
sohaib <sohaib@amital.co.il>
2022-01-06 12:22:06 +0200

    #154442

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationSupplierInvoice/Components/SupplierInvoices/SupplierInvoiceMoreTabComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationSupplierInvoice/Components/SupplierInvoices/SupplierInvoiceMoreTabComponent.ts
@@ -76,7 +76,11 @@ export class SupplierInvoiceMoreTabComponent extends BaseComponent {
     FillGridData() {
         this.TypeCodeFilterItems = new ApiQueryFilters();
         this.TypeCodeFilterItems.addAdditionalFilter("Code", "I02", null, null, "NotContains", false, false, false, "string", false, true); //Task 36745: Supplier Invoice Modifications - Logic for Code "I02" CALL#302294
-        this.TypeCodeFilterItems.addAdditionalFilter("IsRelevantInvoice", true, null, null, "Equals", false, false, false, "boolean");
+        if(this.declarationPM.Direction == "E"){
+            this.TypeCodeFilterItems.addAdditionalFilter("IsRelevantInvoiceExport", true, null, null, "Equals", false, false, false, "boolean");
+        }else{
+            this.TypeCodeFilterItems.addAdditionalFilter("IsRelevantInvoice", true, null, null, "Equals", false, false, false, "boolean");
+        }
         this.ModificationsList = new ObservableCollection([]);
         for (let item of this.InvoicePM.SupplierInvoiceModifications) {
             if (item.TypeCode != "I02" && item.TypeCode != "67" && item.TypeCode != "144") {
@@ -85,18 +89,13 @@ export class SupplierInvoiceMoreTabComponent extends BaseComponent {
                 }
                 else {
                     this.ModificationsList.Insert(new ModificationItemModel(item, this));
-
                 }
             }
-
-           
-
         }
 
         if (this.declarationPM.Direction == "E") {
             this.TypeCodeFilterItems = new ApiQueryFilters();
             this.TypeCodeFilterItems.addAdditionalFilter("Code", "160", null, null, "NotContains", false, false, false, "string", false, true);
-            this.TypeCodeFilterItems.addAdditionalFilter("IsRelevantInvoice", true, null, null, "Equals", false, false, false, "boolean");
             this.UCRList = new ObservableCollection([]);
             for (let item of this.InvoicePM.SupplierInvoiceUCRs) {
                      this.UCRList.Insert(new UCRItemModel(item));
