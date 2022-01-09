aef91394e3148b1449b7f48164e7814319d21810
Abdullah Beitello <AbdullahB@logitudeworld.com>
2022-01-09 14:13:01 +0200

    #153971

--- Logitude/Logitude.BL/CommonDataModel/APIDataContract/QueryService/VendorQueryService.Generated.cs
+++ Logitude/Logitude.BL/CommonDataModel/APIDataContract/QueryService/VendorQueryService.Generated.cs
@@ -112,7 +112,7 @@ using Simplog.Data.CommonDataModel;
 				   if(MyEntityPM.GLAccountId != null)
 				   {
 					   GLAccountQueryService GLAccountService2 = new GLAccountQueryService(Tenant);
-					   					   temp.GLAccount = GLAccountService2.GLAccountVendorDataMapping(MyEntityPM,Tenant,ComputingPartnerName); 
+					   					   temp.GLAccount = GLAccountService2.GLAccountCustomDataMapping(MyEntityPM.GLAccountId,Tenant,ComputingPartnerName); 
 			       
 					   				   }
 				   
