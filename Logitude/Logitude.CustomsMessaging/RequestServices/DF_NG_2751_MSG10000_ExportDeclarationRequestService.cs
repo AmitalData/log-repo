2b9c44e1e3f481c0099f9d4b087897337533759b
sohaib <sohaib@amital.co.il>
2022-01-11 15:59:25 +0200

    #153528

--- Logitude/Logitude.CustomsMessaging/RequestServices/DF_NG_2751_MSG10000_ExportDeclarationRequestService.cs
+++ Logitude/Logitude.CustomsMessaging/RequestServices/DF_NG_2751_MSG10000_ExportDeclarationRequestService.cs
@@ -1566,7 +1566,10 @@ namespace Logitude.CustomsMessaging.RequestServices
             var DMExtensions = new DeclarationGoodsShipmentGovernmentAgencyGoodsItemCommodityClassificationDMExtensions();
             if (!string.IsNullOrEmpty(supplierInvoiceItemPM.DutyRegimeProtocolCode))
                 DMExtensions.DutyRegimeProtocolCode = new DeclarationGoodsShipmentGovernmentAgencyGoodsItemCommodityClassificationDMExtensionsDutyRegimeProtocolCode() { Value = supplierInvoiceItemPM.DutyRegimeProtocolCode };
-
+            if (!string.IsNullOrEmpty(supplierInvoiceItemPM.TradeAgreementCode))
+            {
+                DMExtensions.DutyRegimeCode = new DutyTaxFeeDutyRegimeCodeType() { Value= supplierInvoiceItemPM.TradeAgreementCode };
+            }
             return DMExtensions;
         }
 
