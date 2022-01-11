70e8ab44583bdc80aa9bbabca5bcbffd1897c856
Abdullah Dawoud <abdullah@logitudeworld.com>
2022-01-11 10:47:44 +0000

    Apply suggestions from code review

--- Logitude/JustWebFreight/WebFreight.Web/WebPages/CorrespondenceDownloadpage.aspx.cs
+++ Logitude/JustWebFreight/WebFreight.Web/WebPages/CorrespondenceDownloadpage.aspx.cs
@@ -78,6 +78,7 @@ namespace WebFreight.Web.WebPages
 
                     Document myDoc = up.GetFileExtensionBySecurityId(securityId, tenant);
                     documentExtension = myDoc.Extension;
+filename = myDoc.FileName;
                     if (filestrings.Length >= 4)
                     {
                         filename = filestrings[3] != null ? filestrings[3] : myDoc.FileName;
