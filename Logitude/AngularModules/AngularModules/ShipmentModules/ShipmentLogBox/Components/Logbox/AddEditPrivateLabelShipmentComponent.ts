2382f144050f5a5185d5cd9f2fa91d77301a4ab6
Wesam Amarneh <wesam@logitudeworld.com>
2022-01-12 08:34:34 +0000

    DSV Digital

--- Logitude/AngularModules/AngularModules/ShipmentModules/ShipmentLogBox/Components/Logbox/AddEditPrivateLabelShipmentComponent.ts
+++ Logitude/AngularModules/AngularModules/ShipmentModules/ShipmentLogBox/Components/Logbox/AddEditPrivateLabelShipmentComponent.ts
@@ -973,12 +973,7 @@ export class AddEditPrivateLabelShipmentComponent extends BaseComponent implemen
                     if (!myResult.HasError) {
                         ServiceLocator.SendTotangoUserActivity("LogBox", "New Shipment");
                         this.CurrentSession.CurrentWindow.StopBusyIndicator();
-                        if (!this.IsDSVTenant) {
-                            //  this.ConnectDocumentsFilings(myResult.Result.Id);
-                            this.CurrentSession.SessionEvent.emit({ Name: "ReloadShipments" });
-                            this.CurrentSession.CurrentWindow.Close("");
-
-                        } else this.CurrentSession.CloseCurrentWindowEmit("MyShipmentAdded");
+                        this.CurrentSession.CloseCurrentWindowEmit("MyShipmentAdded");
 
                     }
                     else {
