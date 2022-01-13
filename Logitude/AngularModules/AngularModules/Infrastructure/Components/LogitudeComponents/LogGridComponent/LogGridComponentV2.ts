fd4c507391e962068a4aa749071ebf239aaaedb8
Wesam Amarneh <wesam@logitudeworld.com>
2022-01-13 12:27:13 +0000

    DSV Digital

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/LogGridComponent/LogGridComponentV2.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/LogGridComponent/LogGridComponentV2.ts
@@ -515,6 +515,10 @@ export class LogGridComponentV2 implements OnInit, AfterViewInit, OnChanges, OnD
             this.onRowSelected(1, 1, row.rowData, row.rowIndex);
         else if (this.selectedRow.$id < firstRowAppear || this.selectedRow.$id > firstRowAppear + this.viewportSize)
             this.onRowSelected(1, 1, row.rowData, row.rowIndex);
+        else if (elem.scrollTop == 0) {
+            this.onRowSelected(1, 1, row.rowData, row.rowIndex);
+        }
+
         return true;
     }
 
