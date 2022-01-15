abfcde48bbc3a85326f66b079c78aa19d5ec6cac
Noor Shehada <noor@logitudeworld.com>
2022-01-15 21:05:20 +0200

    Add new file

--- Logitude/AngularModules/CypressTesting/Shipment/cypress/constants/constants.ts
+++ Logitude/AngularModules/CypressTesting/Shipment/cypress/constants/constants.ts
@@ -9,5 +9,7 @@ export class ShipmentConstants {
     public static readonly AddDelivery = "Add Delivery"
     public static readonly CreateStandaloneShipment = "Create Standalone Shipment"
     public static readonly AddPickUp = "Add Pickup"
+    public static readonly AddContainer = "Add Container"
+    
     
 }
---
+++ Logitude/AngularModules/CypressTesting/Shipment/cypress/tests/StandaloneShipment/CreateStandaloneFromPickupWhichHasManyContainers/CreateStandaloneFromPickupWhichHasManyContainersSteps.ts
@@ -0,0 +1,75 @@
+import * as Actions from "../../../actions/Actions";
+import * as StandaloneAction from "../../../actions/StandaloneAction";
+import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+import { ShipmentDetails } from "../../../models/ShipmentDetails";
+import { PickupDelivaryDetails } from "../../../models/PickupDelivaryDetails";
+import * as BaseAssertion from "../../../../../Base/cypress/actions/Assertion";
+import * as Assists from "../../../../../Base/cypress/assists/Assists";
+import { RequestAliases } from "../../../../../Base/cypress/constants/RequestAliases";
+import { ShipmentSelectors } from "../../../selectors/Selectors"
+import { RestAPI } from '../../../../../Base/cypress/constants/RestAPI';
+import { URLs } from '../../../constants/URLs';
+import { BaseSelectors } from '../../../../../Base/cypress/selectors/BaseSelectors';
+import { ShipmentConstants } from "../../../constants/constants";
+import { PackagesDetails } from "cypress/models/PackagesDetails";
+
+let shipmentDetails: ShipmentDetails;
+let PickupDelivarytData: PickupDelivaryDetails;
+let shipmentNumber: string;
+let containerDetailsList
+Given("the user logged in and navigates to shipments workspace", () => {
+  cy.Login()
+  Actions.NavigatesToShipmentsWorkspace()
+});
+
+Given("a direct shipment with the following details", (dataTable) => {
+  shipmentDetails = Assists.CreateInstance<ShipmentDetails>(dataTable, true);
+  Actions.OpenNewShipmentWizard(shipmentDetails.ShipmentLevel);
+  Actions.FillShipmentWizardsFields(shipmentDetails);
+  cy.FillLogLov(ShipmentSelectors.ShipmentShipper, shipmentDetails.Shipper, true)
+});
+
+When("create shipment", () => {
+  Actions.CreateShipment(shipmentDetails.ShipmentLevel);
+});
+
+Then("the shipment should create successfully", () => {
+  BaseAssertion.AssertStatusCode(RequestAliases.ShipmentRequest, 200).then((interception) => {
+    shipmentNumber = interception.response.body.ShipmentNumber
+  })
+});
+
+//#region Add Containers
+Given("the user open the shipment and navigate to packages workspace", () => {
+  Actions.OpenShipment(shipmentNumber);
+  cy.Navigate(ShipmentSelectors.PackagesTab);
+});
+
+Given("a container with the following details", (dataTable) => {
+  containerDetailsList = Assists.CreateSet<PackagesDetails>(dataTable);
+  Actions.FillPackageTab(shipmentDetails.TransportMode, containerDetailsList, shipmentDetails.ShipmentType);
+});
+
+When("save shipment", () => {
+  Actions.UpdateShipment(ShipmentSelectors.ShipmentSaveButton)
+});
+
+Then("the direct shipment should save successfully", () => {
+  BaseAssertion.AssertStatusCode(RequestAliases.ShipmentRequest, 200);
+});
+//#endregion
+
+Given("the user in shipment routing tab",()=>{
+  cy.Click(ShipmentSelectors.RoutingsTab, null)
+  cy.Click(ShipmentSelectors.AddPickUp, null)
+  cy.Click(BaseSelectors.Button, ShipmentConstants.AddPickUp)
+})
+
+Given("add a new pickup leg with the following details", (dataTable) => {
+  PickupDelivarytData = Assists.CreateInstance<PickupDelivaryDetails>(dataTable, true);
+  StandaloneAction.FillPickUpDelivaryDetails(PickupDelivarytData);
+});
+
+Given("the user in the pickup packages select all container",()=>{
+  cy.Click(BaseSelectors.Button, ShipmentConstants.AddContainer)
+})
