b5ae3745ddcc23dee1c746c839d47ad81207967d
Ahmad Abdalrazeq <a.abdalrazeq@logitudeworld.com>
2022-01-16 11:46:02 +0200

    remove test line

--- Logitude/CommunicationWorkerRole/ThreadedRoleEntryPoint.cs
+++ Logitude/CommunicationWorkerRole/ThreadedRoleEntryPoint.cs
@@ -343,7 +343,7 @@ namespace CommunicationWorkerRole
 
             if (Debugger.IsAttached && LogitudeSettings.WorkerRoleName == "production")
             {
-                //throw new Exception("Production worker role should not be run in Debug mode! To debug the worker role in production please use a custom worker name");// 
+                throw new Exception("Production worker role should not be run in Debug mode! To debug the worker role in production please use a custom worker name");// 
             }
         }
 
