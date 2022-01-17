805ff8c4bf8132e183f3497ce9be80f908cc6fcd
Wesam Amarneh <wesam@logitudeworld.com>
2022-01-17 15:03:34 +0200

    Missed SAT Status Label
    Related Work Items: #155305

--- Logitude/AngularModules/AngularModules/CommonModules/CommonCustomer/Components/EditTabs/CustomerBillingTabComponent.ts
+++ Logitude/AngularModules/AngularModules/CommonModules/CommonCustomer/Components/EditTabs/CustomerBillingTabComponent.ts
@@ -25,6 +25,7 @@ export class CustomerBillingTabComponent extends BaseComponent implements OnInit
     public IsCreditLimitActivated: boolean = false;
     public LocalCurrencyCode: string;
     public IsAccountingActivated: boolean;
+    public SatInterfaceSettingCode: string;
 
     @ViewChild('BillingChild', { read: ViewContainerRef, static: false }) viewContainerRef: ViewContainerRef;
     public DisplaySATSettings: boolean = false;
@@ -46,6 +47,7 @@ export class CustomerBillingTabComponent extends BaseComponent implements OnInit
 
         if (SessionLocator.SATInterfaceSettings.SATInterfaceCode != "NONE") {
             this.DisplaySATSettings = true;
+            this.SatInterfaceSettingCode = SessionLocator.SATInterfaceSettings.SATInterfaceCode;
         }
 
         this.Listen();
