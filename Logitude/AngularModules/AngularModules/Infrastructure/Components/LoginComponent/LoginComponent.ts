c4a17d64caf60fefa0221f6c46a6a2a8f2866f3f
Izik Fridland <IzikFr@amital.co.il>
2022-01-18 13:48:52 +0200

    u

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/LoginComponent/LoginComponent.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/LoginComponent/LoginComponent.ts
@@ -168,19 +168,19 @@ export class LoginComponent implements OnInit {
         } else {
             this.StartLoginProcess();
         }
-        if (isDevMode())
-            this.developerLogin();
+        // if (isDevMode())
+        //     this.developerLogin();
     }
 
-    async developerLogin() {
-        this.Email = 'itzik@amital.co.il'
-        this.Password = 'xhx@word3';
-        this.LoginClicked(); 
-        while (!this.TenantList?.length)
-            await new Promise<void>(resolve => setTimeout(() => resolve(), 100));
-        this.SelectedCompany = this.TenantList.find(d => d.Tenant == 6);
-        this.ContinueClicked()
-    }
+    // async developerLogin() {
+    //     this.Email = 'itzik@amital.co.il'
+    //     this.Password = '';
+    //     this.LoginClicked(); 
+    //     while (!this.TenantList?.length)
+    //         await new Promise<void>(resolve => setTimeout(() => resolve(), 100));
+    //     this.SelectedCompany = this.TenantList.find(d => d.Tenant == 6);
+    //     this.ContinueClicked()
+    // }
 
 
     IsShowLoginForm: boolean = false;
