4f48008700bbe67fc65dab7f67a5256dfe3cf80d
Itzik Moshelian <itzik@amital.co.il>
2022-01-24 15:29:26 +0200

    #152823

--- Logitude/Logitude.Customs.Data/EntityLists/Generated/SupplierInvoiceModificationList.Generated.cs
+++ Logitude/Logitude.Customs.Data/EntityLists/Generated/SupplierInvoiceModificationList.Generated.cs
@@ -36,6 +36,8 @@ namespace Logitude.Customs.Data.EntityLists
        [Key]
        [DataMember]
        public int ModificationCounterKey  { get; set; }
+       [DataMember]
+       public string TypeDesc  { get; set; }
    }
 
 }
