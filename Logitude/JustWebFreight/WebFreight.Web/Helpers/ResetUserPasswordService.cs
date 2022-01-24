f63c638664bdc00a63dd4ea5ff81833f0b84f352
Ahmad Abdalrazeq <a.abdalrazeq@logitudeworld.com>
2022-01-24 12:14:23 +0200

    add validation

--- Logitude/JustWebFreight/WebFreight.Web/Helpers/ResetUserPasswordService.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Helpers/ResetUserPasswordService.cs
@@ -165,7 +165,7 @@ namespace WebFreight.Web.Helpers
             StringBuilder HtmlTemplate = new StringBuilder();
             if (!resetPasswordParameters.IsMobile)
             {
-                if (!string.IsNullOrEmpty(resetPasswordParameters.TemplateName))
+                if (!string.IsNullOrEmpty(resetPasswordParameters.TemplateName) && !string.IsNullOrEmpty(emailBodyArgs.BrandingTenant))
                 {
                     result = new ResetUserPasswordDocumentService(int.Parse(resetPasswordParameters.BrandingTenant)).GetMessageArgsByTemplateName(resetPasswordParameters, emailBodyArgs);
                 }
