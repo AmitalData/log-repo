471aaf955bf0b20635971a7219fec37ec5f6ad47
Heba Mubarak <heba@logitudeworld.com>
2022-01-26 12:18:37 +0200

    Show Warring Message

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/QueryColumnsComponents/QueryColumnsEditComponent.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/QueryColumnsComponents/QueryColumnsEditComponent.ts
@@ -57,6 +57,8 @@ export class QueryColumnsEditComponent {
     public serviceArgs: ServiceArgs;
     private CurrentSession = SessionLocator.SelectedSession;
     public IsViewOnly: boolean = false;
+    public IsShowWarringMessage: boolean = false;
+     
     public LayoutDirection: string = 'ltr';
 
     constructor(private CD: ChangeDetectorRef) {
@@ -208,6 +210,7 @@ export class QueryColumnsEditComponent {
     private SetIsViewOnlyOption(currentQuery: any) {
         if (currentQuery.SharedByUserId && currentQuery.SharedByUserId != SessionLocator.LoggedUserId && currentQuery.IsViewOnly) {
             this.IsViewOnly = true;
+            this.IsShowWarringMessage = true;
         }
     }
 
