ae3ae9ca0df46c2d87bb31f09a8da31ffb37f44e
Itzik Moshelian <itzik@amital.co.il>
2022-01-30 12:28:47 +0200

    #155993 fix merge conflict  my + izikf

--- Logitude/Logitude.Customs.BL/EntityDataMappings/ExportStorageDataMapping.cs
+++ Logitude/Logitude.Customs.BL/EntityDataMappings/ExportStorageDataMapping.cs
@@ -41,12 +41,7 @@ namespace Logitude.Customs.BL.EntityDataMappings
         public void CustomPOCOToPM(ExportStoragePM entityPM, ExportStorage entityPOCO)
         {
             var declarationQueryService = new DeclarationQueryService(entityPOCO.Tenant);
-            DeclarationPM declarationPM = declarationQueryService.GetSingleDeclarationById(entityPOCO.DeclarationId, entityPOCO.Tenant);
-            //DeclarationPM declarationPM = new DeclarationPM();
-            //if (!String.IsNullOrWhiteSpace(entityPOCO.DeclarationId))//fix crush !!!!
-            //{
-            //    declarationPM = declarationQueryService.GetSingleDeclarationById(entityPOCO.DeclarationId, entityPOCO.Tenant);
-            //}
+            DeclarationPM declarationPM = entityPOCO.DeclarationId != null ? declarationQueryService.GetSingleDeclarationById(entityPOCO.DeclarationId, entityPOCO.Tenant) : new DeclarationPM();
 
 
             var cargoTypeQueryService = new CargoTypeQueryService(entityPOCO.Tenant);
