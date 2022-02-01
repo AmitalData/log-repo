627416019003097d3b5685488a0a93ac6d937b53
Alexander Khitrik <alexanderh@amital.co.il>
2022-02-01 10:30:06 +0200

    Alexander: 139380 Interest Trans. bugs

--- Logitude/Logitude.Accounting.BL/CoreBL/InterestTransactionsCSVFlatFileAnalyser.cs
+++ Logitude/Logitude.Accounting.BL/CoreBL/InterestTransactionsCSVFlatFileAnalyser.cs
@@ -60,7 +60,7 @@ namespace Logitude.Accounting.BL.CoreBL
                 accountingContext = AccountingContext.GetContext(tenant);
                 _FullAccountingSettingPM = GetFullAccountingSettings(accountingContext, tenant);
                 ValidateFlatFile(tenant);
-                if (!this._fatal)
+                if (!this._fatal && this._goodCount > 0)
                 {
                     IAccountingContext MyContext = AccountingContext.GetContext(tenant);
 
@@ -76,53 +76,55 @@ namespace Logitude.Accounting.BL.CoreBL
 
                         foreach (InterestTransactionSrcLineDTO itLine in _SrcLinesDTO)
                         {
-                            count++;
-                            bool errors = false;
-
-                            InterestTransactionPM newItPM = new InterestTransactionPM()
+                            if (!itLine.ErrorInLine)
                             {
-                                ChangeSetOp = Simplog.Server.Infrastructure.ChangeSetOperation.Insert,
-                                Tenant = tenant,
+                                count++;
+                                bool errors = false;
 
-                                CreateDateTime = @now,
-                                UpdateDateTime = @now,
+                                InterestTransactionPM newItPM = new InterestTransactionPM()
+                                {
+                                    ChangeSetOp = Simplog.Server.Infrastructure.ChangeSetOperation.Insert,
+                                    Tenant = tenant,
 
-                                GLAccountId = itLine.GLAccountId,
-                                InterestEntityTypeCode = "3", //(Journal)
-                                EntityId = itLine.JournalId,
-                                OriginalEntityLineNumber = itLine.JournalLineNumber,
+                                    CreateDateTime = @now,
+                                    UpdateDateTime = @now,
 
-                                AccountEntityCode = null,
-                                ForeignAmount = itLine.ForeignAmount,
-                                LocalAmount = itLine.LocalAmount,
-                                CurrencyId = itLine.CurrencyId,
+                                    GLAccountId = itLine.GLAccountId,
+                                    InterestEntityTypeCode = "3", //(Journal)
+                                    EntityId = itLine.JournalId,
+                                    OriginalEntityLineNumber = itLine.JournalLineNumber,
 
-                                InterestValueDate = itLine.InterestValueDate,
-                                IsCancelled = false,
-                                IsClosed = false,
-                            };
+                                    AccountEntityCode = null,
+                                    ForeignAmount = itLine.ForeignAmount,
+                                    LocalAmount = itLine.LocalAmount,
+                                    CurrencyId = itLine.CurrencyId,
 
-
-
-
-
+                                    InterestValueDate = itLine.InterestValueDate,
+                                    IsCancelled = false,
+                                    IsClosed = false,
+                                };
 
 
-                            var itSvc = new InterestTransactionUpdateService(accountingContext, new Dictionary<string, Simplog.Server.Infrastructure.IContext>(), tenant);
-                            itSvc.Update(newItPM, true);
+
+
+
+
+
+                                var itSvc = new InterestTransactionUpdateService(accountingContext, new Dictionary<string, Simplog.Server.Infrastructure.IContext>(), tenant);
+                                itSvc.Update(newItPM, true);
+                            }
 
                         }
 
+                        String errorLines = "";
                         if (MyCSVFlatFileLoadResult.ErrorRowList.Count > 0)
                         {
-                            string text_1 = MyCSVFlatFileLoadResult.ErrorRowList.FirstOrDefault();
-                            throw new ApplicationException($"{text_1}");
+                            MyCSVFlatFileLoadResult.ErrorRowList.ForEach(item => errorLines += item.ToString() + "\n");
                         }
-                        //    if (MyFlatFileLoadResult.ExceptionVendorList.Count > 0)
-                        //    {
-                        //        string text = MyFlatFileLoadResult.ExceptionVendorList.FirstOrDefault();
-                        //        throw new ApplicationException($"{text}");
-                        //    }
+                        if (!String.IsNullOrEmpty(errorLines))
+                        {
+                            throw new ApplicationException($"{errorLines}");
+                        }
                         scope.Complete();
 
                     }
@@ -237,7 +239,7 @@ namespace Logitude.Accounting.BL.CoreBL
             if (date == DateTime.MinValue)
             {
                 throw new
-                    Exception($"{fieldname} should be  yyyyMMddHHmm  Substring({pos}) ={txtDateTime}  ");
+                    Exception($"{fieldname} should be in the {@format} format, while Substring({pos}) ={rawLine}  ");
             }
 
             return date;
@@ -361,14 +363,14 @@ namespace Logitude.Accounting.BL.CoreBL
                             foreach (JournalLinePM journalLine in journalPM.JournalLines)
                             {
                                 if ((journalLine.ActionCode == "1" || journalLine.ActionCode == "3") && itLine.CreditAmount != 0m && journalLine.CreditAccountId == itLine.GLAccountId
-                                    && (journalLine.Reference1 == itLine.Rererence || journalLine.Reference2 == itLine.Rererence || String.IsNullOrWhiteSpace(itLine.Rererence)))
+                                    && (journalLine.Reference1 == itLine.Reference || journalLine.Reference2 == itLine.Reference || String.IsNullOrWhiteSpace(itLine.Reference)))
                                 {
                                     lineFound = true;
                                     jlPM = journalLine;
                                     break;
                                 }
                                 else if ((journalLine.ActionCode == "2" || journalLine.ActionCode == "3") && itLine.DebitAmount != 0m && journalLine.DebitAccountId == itLine.GLAccountId
-                                    && (journalLine.Reference1 == itLine.Rererence || journalLine.Reference2 == itLine.Rererence || String.IsNullOrWhiteSpace(itLine.Rererence)))
+                                    && (journalLine.Reference1 == itLine.Reference || journalLine.Reference2 == itLine.Reference || String.IsNullOrWhiteSpace(itLine.Reference)))
                                 {
                                     lineFound = true;
                                     jlPM = journalLine;
@@ -518,7 +520,7 @@ namespace Logitude.Accounting.BL.CoreBL
         public decimal CreditAmount { get; private set; }
         public decimal LocalAmount { get; private set; }
         public decimal ForeignAmount { get; private set; }
-        public string Rererence { get; private set; }
+        public string Reference { get; private set; }
         public string Remarks { get; private set; }
         public string ExternalNumber { get; private set; }
 
@@ -567,7 +569,7 @@ namespace Logitude.Accounting.BL.CoreBL
 
             if (count > 2)
             {
-                txtDateTime = values[2].TrimEnd(' ');
+                txtDateTime = values[2].TrimEnd(' ').Replace('/','.');
                 if (txtDateTime.Length >= 10)
                 {
                     txtDateTime = txtDateTime.Substring(0, 10);
@@ -632,7 +634,7 @@ namespace Logitude.Accounting.BL.CoreBL
 
             if (count > 6)
             {
-                rec.Rererence = values[6];
+                rec.Reference = values[6];
             }
 
             if (count > 7)
@@ -643,7 +645,7 @@ namespace Logitude.Accounting.BL.CoreBL
 
             if (count > 8)
             {
-                txtDateTime = values[8].TrimEnd(' ');
+                txtDateTime = values[8].TrimEnd(' ').Replace('/', '.');
                 if (txtDateTime.Length >= 10)
                 {
                     txtDateTime = txtDateTime.Substring(0, 10);
