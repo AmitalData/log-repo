dd3655303e1044211662c9c16873dfffef4c8aaa
Heba Mubarak <heba@logitudeworld.com>
2022-02-07 11:15:24 +0200

    change custom fields number

--- Logitude/AngularModules/AngularModules/CRM/Services/StandardPMs/OpportunityPMService.ts
+++ Logitude/AngularModules/AngularModules/CRM/Services/StandardPMs/OpportunityPMService.ts
@@ -167,7 +167,7 @@ export class OpportunityPMService {
         }
 
 		var customFields: Array<string> = [];
-        for (var i = 1; i < 11; i++) {
+        for (var i = 1; i < 41; i++) {
             customFields.push("Field" + i);
         }
             var jsonPMKeys = Object.keys(jsonPM);
