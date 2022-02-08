29e1a0f65aa8121b4beb0fe9076842acd10d0b7d
Heba Mubarak <heba@logitudeworld.com>
2022-02-08 11:16:28 +0200

    AR Invoice DWH Feature toggles removal

--- Logitude/Logitude.BL/InfrastructureModel/EntityQueries/DWObjectTableQuery.cs
+++ Logitude/Logitude.BL/InfrastructureModel/EntityQueries/DWObjectTableQuery.cs
@@ -17,7 +17,6 @@ namespace Logitude.BL.InfrastructureModel.EntityQueries
         private readonly Dictionary<string, string> FactTablesFeatureToggle = new Dictionary<string, string>
         {
             {"BIF","Invoices"}, 
-            {"BAF", "ARInvoices"},
             {"IDS", "Inland Domestic Shipments"},
             {"BCF", "Containers"}
         };
