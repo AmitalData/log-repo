972678535d26b4405c131361d30f9afbc2d89b63
Itzik Moshelian <itzik@amital.co.il>
2022-02-09 14:32:52 +0200

    #156104

--- Logitude/Logitude.Customs.BL/Messaging/U2L/ExportStorage/ExportStorageUpsertService.cs
+++ Logitude/Logitude.Customs.BL/Messaging/U2L/ExportStorage/ExportStorageUpsertService.cs
@@ -177,7 +177,8 @@ namespace Logitude.Customs.BL.Messaging.U2L.ExportStorage
                 _DBExportStoragePM.DeclarationId = _UnifreigntExportStorage.DeclarationId;
 
                 _DBExportStoragePM.ExportDealIdentification = _UnifreigntExportStorage.General.ExportDealIdentification;
-                _DBExportStoragePM.ExporterID = _UnifreigntExportStorage.General.ExporterNumber;
+                
+                _DBExportStoragePM.ExporterID = TranslateClient(_DBExportStoragePM.Tenant, _UnifreigntExportStorage.General.ExporterNumber);
                 //UnifreigntExportStorage.General.ExporterFileNumber = "Exp_Ref_INV1";
                 _DBExportStoragePM.ShipCode = _UnifreigntExportStorage.General.ShipCode;
 
@@ -200,7 +201,25 @@ namespace Logitude.Customs.BL.Messaging.U2L.ExportStorage
             }
         }
 
-        
+        private string TranslateClient(int tenant, string exporterNumber)
+        {
+            if (String.IsNullOrWhiteSpace(exporterNumber))
+            {
+                AppendLogLine("ExporterNumber is null");
+                return null;
+            }
+            ClientQueryService clientQueryService = new ClientQueryService(tenant);
+
+            var clientId = clientQueryService.GetIdByCode(exporterNumber, tenant);
+
+            if (clientId == null)
+            {
+                AppendLogLine("ExporterNumber = " + exporterNumber + " could not translate to Logitude Id");
+                return null;
+            }
+            AppendLogLine("ExporterNumber = " + exporterNumber + " Translated to " + clientId);
+            return clientId;
+        }
 
         protected override int ResolvedTenant() 
         {
