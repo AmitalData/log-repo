fb8d14365d08464e9d7577f79990de62e6d0c6fb
Fida'a Zuhour <fida@logitudeworld.com>
2022-02-10 10:03:56 +0200

    #156563 Cargo Tracking : Payment Requested Milestone Adjustment -R1

--- Logitude/Logitude.CargoTracking.BL/CargoTrackingServices/Services/TableConditions/ShipmentTableCondtions.cs
+++ Logitude/Logitude.CargoTracking.BL/CargoTrackingServices/Services/TableConditions/ShipmentTableCondtions.cs
@@ -94,7 +94,6 @@ namespace Logitude.CargoTracking.BL.CargoTrackingServices.Services.TableStructur
              "min(AdditionalData.GoodsClassification) as GoodsClassification, " +
              "min(AdditionalData.DocumentInspection) as DocumentInspection , " +
              "min(AdditionalData.InvoiceIssuedDate) as InvoiceIssuedDate , " +
-             "AdditionalData.IsPaymentRequired as IsPaymentRequired , " +
              "min(AdditionalData.PaymentDateTime) as PaymentDateTime , " +
              "min(AdditionalData.PaymentRequestDateTime) as PaymentRequestDateTime , " +
              "min(AdditionalData.GatepassDocumentsReady) as GatepassDocumentsReady ";
@@ -167,7 +166,6 @@ namespace Logitude.CargoTracking.BL.CargoTrackingServices.Services.TableStructur
 
 
 
-            shipmentFields += ",AdditionalData.IsPaymentRequired";
             var groupByScript =
                 $" GROUP BY {shipmentFields}" +
                 @", ConsigneeCard.EnglishName,
@@ -301,7 +299,6 @@ namespace Logitude.CargoTracking.BL.CargoTrackingServices.Services.TableStructur
              "min(AdditionalData.GoodsClassification) as GoodsClassification, " +
              "min(AdditionalData.DocumentInspection) as DocumentInspection , " +
              "min(AdditionalData.InvoiceIssuedDate) as InvoiceIssuedDate , " +
-             "AdditionalData.IsPaymentRequired as IsPaymentRequired , " +
              "min(AdditionalData.PaymentDateTime) as PaymentDateTime , " +
              "min(AdditionalData.PaymentRequestDateTime) as PaymentRequestDateTime , " +
              "min(AdditionalData.GatepassDocumentsReady) as GatepassDocumentsReady ";
@@ -376,7 +373,6 @@ namespace Logitude.CargoTracking.BL.CargoTrackingServices.Services.TableStructur
 
 
             // Group By
-            shipmentFields += ",AdditionalData.IsPaymentRequired";
             var groupByScript =
                 $" GROUP BY {shipmentFields}" +
                 @", ConsigneeCard.EnglishName,
