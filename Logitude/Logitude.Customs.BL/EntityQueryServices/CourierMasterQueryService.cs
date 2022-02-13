e07b649552eb9808425caef80926f814ec3d0916
sohaib <sohaib@amital.co.il>
2022-02-13 14:36:53 +0200

    #155642

--- Logitude/Logitude.Customs.BL/EntityQueryServices/CourierMasterQueryService.cs
+++ Logitude/Logitude.Customs.BL/EntityQueryServices/CourierMasterQueryService.cs
@@ -461,6 +461,11 @@ namespace Logitude.Customs.BL.EntityQueryServices
             var LastmileReportData = courierMasterRepository.GetAllCourierMasterForLastmileReport(hatraFromDate, hatraToDate, lastMileFromDate, LastMileToDate, airline, trucker, courierHawb,tenant);
             return LastmileReportData;
         }
+        public int CounNoOfCourierHawbwWithoutHatara(string couriermasterid, int tenant)
+        {
+            CourierMasterRepository courierMasterRepository = new CourierMasterRepository(context);
+            return courierMasterRepository.CounNoOfCourierHawbwWithoutHatara(couriermasterid,tenant);
+        }
 
 
 
