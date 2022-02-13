315bbfb2f8fab61e7421e67411ec67bd89abff09
Izik Fridland <IzikFr@amital.co.il>
2022-02-13 18:05:18 +0200

    #156331

--- Logitude/Logitude.Customs.Data/EntityMapping/ConfirmationTypeMap.cs
+++ Logitude/Logitude.Customs.Data/EntityMapping/ConfirmationTypeMap.cs
@@ -25,7 +25,7 @@ namespace Logitude.Customs.Data.EntityMapping
 	 
             this.Property(t => t.Code).HasColumnName("Code").IsRequired().HasMaxLength(4).IsUnicode(false);
 
-            this.Property(t => t.EnglishName).HasColumnName("EnglishName").HasMaxLength(80).IsUnicode(false);
+            this.Property(t => t.EnglishName).HasColumnName("EnglishName").HasMaxLength(200).IsUnicode(false);
 
             this.Property(t => t.LocalName).HasColumnName("LocalName").HasMaxLength(1000).IsUnicode(true);
 
