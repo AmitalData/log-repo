41210e578a539561a2e286fe28219e2eb9407b22
Amer Aldghameen <Amer@logitudeworld.com>
2022-02-14 12:50:24 +0200

    prevent D J

--- Logitude/Logitude.Accounting.Data/EntityListQueryServices/RevaluationListQueryService.cs
+++ Logitude/Logitude.Accounting.Data/EntityListQueryServices/RevaluationListQueryService.cs
@@ -61,7 +61,7 @@ namespace Logitude.Accounting.Data.EntityListQueryServices
         public List<RevaluationList> GetOpenRevaluationList(int tenant)
         {
             IQueryable<Revaluation> revaluationQuery = (from a in context.Revaluations
-                                                    where a.Tenant == tenant && ((a.Status == RevaluationStatusEnum.InProgress.ToString()) && (a.RevaluationDate != null || a.RevaluationDate != DateTime.MinValue))
+                                                    where a.Tenant == tenant && ((a.Status != (int)RevaluationStatusEnum.Done +"" && a.Status != (int)RevaluationStatusEnum.Failed + "") && (a.RevaluationDate != null || a.RevaluationDate != DateTime.MinValue))
                                                     select a);
 
             IQueryable<RevaluationList> revaluationListQuery = this.GetIqueryableList(revaluationQuery);
