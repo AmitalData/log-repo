2cd10615972bf1206b26f9cf1e315556a1fb98eb
sohaib <sohaib@amital.co.il>
2022-02-14 08:44:31 +0200

    #155375

--- Logitude/Logitude.Customs.Data/Repsitories/CustomsItemRepository.cs
+++ Logitude/Logitude.Customs.Data/Repsitories/CustomsItemRepository.cs
@@ -28,6 +28,13 @@ namespace Logitude.Customs.Data.Repsitories
                     select a).FirstOrDefault();
         }
 
+        public List<CustomsItem> GetAllCustomsItemByClassificationCode(string classificationCode)
+        {
+            return (from a in context.CustomsItems
+                    where a.FullClassification == classificationCode
+                    select a).ToList();
+        }
+
     }
 
 }
