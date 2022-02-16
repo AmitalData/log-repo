4cdcce7b939b62a3a4d81eb8d02682dfcd86065b
Ayed Ramadeen <ayed@logitudeworld.com>
2022-02-16 12:49:23 +0200

    856 Tax Deduction Report: Cancelled APPayment Logic R5

--- Logitude/Logitude.Accounting.BL/DataContract/TaxDeductionReportDataProvider.cs
+++ Logitude/Logitude.Accounting.BL/DataContract/TaxDeductionReportDataProvider.cs
@@ -478,7 +478,8 @@ namespace Logitude.Accounting.BL.DataContract
         {
 
             List<APPayment> cancelledPayments = (from a in invoiceContext.APPayments.Include("VendorCard")
-                                                 where a.AccountingCancelationDate >= startDate && a.AccountingCancelationDate < endDate
+                                                 where a.AccountingCancelationDate >= startDate && a.AccountingCancelationDate < endDate &&
+                                                 !(a.RegisterDate >= startDate && a.RegisterDate < endDate)
                                                  && a.Tenant == Tenant
                                                  && (a.StatusCode == "VD" && a.AccountingCancelationDate.Value.Year != a.RegisterDate.Value.Year && a.DontIncludeInDeductionReport == false)
                                                  select a).ToList();
