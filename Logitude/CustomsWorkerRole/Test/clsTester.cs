670bc5d8eb4d2d20e86afaae014fab1d78a44ccf
Itzik Moshelian <itzik@amital.co.il>
2022-02-16 08:05:48 +0200

    #148909

--- Logitude/CustomsWorkerRole/Test/clsTester.cs
+++ Logitude/CustomsWorkerRole/Test/clsTester.cs
@@ -311,7 +311,7 @@ SELECT TOP 1000 [Id]
         public static string CheckWSCourierStatistic(int tenant, bool multiThreard)
         {
             var declarationCourierStatusQueryService = new Logitude.Customs.BL.EntityQueryServices.DeclarationCourierStatusQueryService(tenant);
-            var counts = declarationCourierStatusQueryService.GetQueriesCounts(tenant,multiThreard);
+            var counts = declarationCourierStatusQueryService.GetQueriesCounts(tenant,null,multiThreard);
             var jsonSetting = ProxyUtil.JsonConvertSerialize(counts);
             return jsonSetting;
         }
