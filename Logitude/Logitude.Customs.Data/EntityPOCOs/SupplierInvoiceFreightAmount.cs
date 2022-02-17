e368a002abd8c635c8ab66e8e664c4b7856357f1
Izik Fridland <IzikFr@amital.co.il>
2022-02-17 16:55:29 +0200

    #156330

--- Logitude/Logitude.Customs.Data/EntityPOCOs/SupplierInvoiceFreightAmount.cs
+++ Logitude/Logitude.Customs.Data/EntityPOCOs/SupplierInvoiceFreightAmount.cs
@@ -31,11 +31,8 @@ namespace Logitude.Customs.Data.EntityPOCOs
         [Column("Tenant")]
 	    public int Tenant { get; set; }
      [Key]
-        [ForeignKey("CurrencyType")]
         [Column("CurrencyTypeCode" ,Order = 3)]
 	    public string CurrencyTypeCode { get; set; }
-	      
-        public virtual CurrencyType CurrencyType { get; set; }
         [Column("Amount")]
 	    public decimal? Amount { get; set; }
     }
