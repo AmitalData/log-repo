bd186a3b7b4c26e1d7b58cd63eb482314c490dc1
Ayed Ramadeen <ayed@logitudeworld.com>
2022-02-20 11:55:38 +0200

    T74 - Object reference in tax deduction report

--- Logitude/Logitude.Accounting.BL/DataContract/TaxDeductionReportDataProvider.cs
+++ Logitude/Logitude.Accounting.BL/DataContract/TaxDeductionReportDataProvider.cs
@@ -563,7 +563,7 @@ namespace Logitude.Accounting.BL.DataContract
                 {
                     cardsCodes = cardsCodes + item.Code + ",";
                     Address address = addresses.Where(d => d.CardId == item.Id).FirstOrDefault();
-                    groupedbyVendor.CardAddress1 = address.Address1;
+                    groupedbyVendor.CardAddress1 = address?.Address1;
                     if (groupedbyVendor.VATNumber == null && item.VatNumber != null)
                     {
                         groupedbyVendor.VATNumber = item.VatNumber;
@@ -587,7 +587,7 @@ namespace Logitude.Accounting.BL.DataContract
                 Address address = addresses.Where(d => d.CardId == selectedVendors[0].Id).FirstOrDefault();
                 groupedbyVendor.VendorAddress = address != null ? address.Name : null;
                 groupedbyVendor.VendorCity = address != null ? address.City : null;
-                groupedbyVendor.CardAddress1 = address.Address1;
+                groupedbyVendor.CardAddress1 = address?.Address1;
             }
             if (groupedbyVendor.VATNumber == null)
             {
