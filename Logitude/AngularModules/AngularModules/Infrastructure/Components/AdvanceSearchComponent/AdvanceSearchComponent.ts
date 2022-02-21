1899f7107f39127aacb4eea9f8a3aead366e9e4d
Ayed Ramadeen <ayed@logitudeworld.com>
2022-02-21 10:43:56 +0200

    revert advance search component changes

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/AdvanceSearchComponent/AdvanceSearchComponent.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/AdvanceSearchComponent/AdvanceSearchComponent.ts
@@ -355,13 +355,11 @@ export class AdvanceSearchComponent implements OnInit {
             this.SelectedObjectFields = [];
         }
         var filters = this.AdvancedQueryFilterPMs.filter(d => d.ObjectFieldCode == field.FieldCode);
-        // if (filters != null && filters[0] != null && filters[0].IsPredefined == true) {
-        //     var value = this.AdvancedQueryFilterPMs.filter(d => d.IsPredefined == true && d.ObjectFieldCode == field.FieldCode)[0].PredefinedValue;
-        //     alert(value)
-        //     value = null;
-        //     if (value == "#logged-user") filters[0].PredefinedValue = SessionLocator.LoggedUserPM.DontShowLocal ? SessionInfo.LoggedUserPM.EnglishName : SessionLocator.LoggedUserPM.LocalName;
-        //     this.FieldsValues.SetFieldValue(field.Id, value);
-        // }
+        if (filters != null && filters[0] != null && filters[0].IsPredefined == true) {
+            var value = this.AdvancedQueryFilterPMs.filter(d => d.IsPredefined == true && d.ObjectFieldCode == field.FieldCode)[0].PredefinedValue;
+            if (value == "#logged-user") filters[0].PredefinedValue = SessionLocator.LoggedUserPM.DontShowLocal ? SessionInfo.LoggedUserPM.EnglishName : SessionLocator.LoggedUserPM.LocalName;
+            this.FieldsValues.SetFieldValue(field.Id, value);
+        }
         if (this.SelectedObjectFields.filter(a => a.FieldName == field.FieldName).length == 0) {
             if (field.DataTypeCode != "Constant") {
                 this.SelectedObjectFields.push(new FilterField(field, this.QueryCode, false, filters, this, this.pubSubService));
