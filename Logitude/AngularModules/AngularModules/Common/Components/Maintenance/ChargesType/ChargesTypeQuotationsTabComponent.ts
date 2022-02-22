4fa7200ad5c834f49065bb94cec1e878a84c052e
Maheera Jazi <Maheera@logitudeworld.com>
2022-02-22 11:33:34 +0200

    #157427

--- Logitude/AngularModules/AngularModules/Common/Components/Maintenance/ChargesType/ChargesTypeQuotationsTabComponent.ts
+++ Logitude/AngularModules/AngularModules/Common/Components/Maintenance/ChargesType/ChargesTypeQuotationsTabComponent.ts
@@ -3,17 +3,19 @@ import { BaseComponent } from '../../../../Infrastructure/Components/LogitudeCom
 import { ChargesTypePM } from '../../../EntityPMs/ChargesTypePM';
 import { EntityArgs } from '../../../../Infrastructure/DataContracts/EntityArgs';
 import { FeatureLocator } from '../../../../Infrastructure/Utilities/FeatureLocator';
+import { AppTool } from '../../../../Infrastructure/Tools';
+import { QuoteChargesGroupListService } from '../../../../Infrastructure/Services/StandardLists/QuoteChargesGroupListService';
+import { ServiceResponse } from '../../../../Infrastructure/DataContracts/ServiceResponse';
 
 @Component({
-
     templateUrl: './ChargesTypeQuotationsTabComponent.html',
 })
 
-export class ChargesTypeQuotationsTabComponent extends BaseComponent  {
+export class ChargesTypeQuotationsTabComponent extends BaseComponent {
     public EntityPM: ChargesTypePM;
     public DataContext: ChargesTypeQuotationsTabComponent = this;
     public ObjectTableName: string = "ChargesType";
-    public IsQuotationFeatureOn = false; 
+    public IsQuotationFeatureOn = false;
     constructor(public args: EntityArgs) {
         super();
         this.EntityPM = args.EntityPM;
@@ -24,10 +26,29 @@ export class ChargesTypeQuotationsTabComponent extends BaseComponent  {
             this.IsQuotationFeatureOn = true;
         }
     }
+
     get QuoteChargesGroupId() { return this.EntityPM.QuoteChargesGroupId; }
     set QuoteChargesGroupId(value: string) {
         if (this.EntityPM.QuoteChargesGroupId != value) {
             this.EntityPM.QuoteChargesGroupId = value;
+            if (!AppTool.IsNullOrEmpty(value)) {
+                var myService: QuoteChargesGroupListService = new QuoteChargesGroupListService();
+                myService.getSingleFromCache(this.EntityPM.QuoteChargesGroupId).subscribe((myResponse: ServiceResponse) => {
+                    if (!myResponse.HasError && myResponse.Result) {
+                        this.QuoteChargesGroupCode = myResponse.Result.Code;
+                    }
+                });
+            }
+            else {
+                this.QuoteChargesGroupCode = value;
+            }
+        }
+    }
+
+    get QuoteChargesGroupCode() { return this.EntityPM.QuoteChargesGroupCode; }
+    set QuoteChargesGroupCode(newValue: string) {
+        if (this.EntityPM.QuoteChargesGroupCode != newValue) {
+            this.EntityPM.QuoteChargesGroupCode = newValue;
         }
     }
 }
