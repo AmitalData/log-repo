d754dbac0852602fa077c44ed9b77ee1de982ab7
Amer Aldghameen <Amer@logitudeworld.com>
2022-02-27 13:17:56 +0200

    ++

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/CargoTrackingModel/CargoTrackingShipmentsDeclarationApprovalService.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/CargoTrackingModel/CargoTrackingShipmentsDeclarationApprovalService.cs
@@ -174,7 +174,7 @@ namespace WebFreight.Web.App_Code.AngularJS_App_Code.Generated
 
         private CommunicationsParams CreateCommunicationParametersForApproval(DeclarationApprovalArgs declarationApprovalArgs)
         {
-            ObjectTablePM table = ObjectTableQuery.GetObjectTableByCode("CargoTrackingShipment", 0);
+            ObjectTablePM table = ObjectTableQuery.GetObjectTableByCode("Shipment", 0);
 
             CommunicationsParams comParams = new CommunicationsParams()
             {
@@ -188,6 +188,7 @@ namespace WebFreight.Web.App_Code.AngularJS_App_Code.Generated
                 LoggingObjectTableId = table?.Id,
                 Subject = "Status Update",
                 FolderName = "ExternalTasksQueue",
+                LoggingEntityReference = cargoTrackingShipmentPM.ShipmentNumber,
             };
             return comParams;
         }
