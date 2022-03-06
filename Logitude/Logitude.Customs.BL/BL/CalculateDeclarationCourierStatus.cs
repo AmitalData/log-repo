2eb4864acc3c6841d1819afae09c8facd503b7dc
Atara Oster <Atara@amital.co.il>
2022-03-06 11:45:48 +0200

    #157177

--- Logitude/Logitude.Customs.BL/BL/CalculateDeclarationCourierStatus.cs
+++ Logitude/Logitude.Customs.BL/BL/CalculateDeclarationCourierStatus.cs
@@ -178,6 +178,29 @@ namespace Logitude.Customs.BL.BL
             }
 
         }
+        public void CalcMissingDocumentStatusCode(DeclarationCourierStatusPM myDeclarationCourierStatusPM)
+        {
+            if (myDeclarationCourierStatusPM == null) return;
+            
+            var customContext = CustomContext.GetContext(myDeclarationCourierStatusPM.Tenant);
+            CustomsDocumentsTicketQueryService myCustomsDocumentsTicketQueryService = new CustomsDocumentsTicketQueryService(customContext);
+            List<CustomsDocumentsTicketPM> customsDocumentsTicketPMList = myCustomsDocumentsTicketQueryService.GetCustomsDocumentsTicketPMsByEntityIdAndChilds(declarationPM.Id, "", "", "", myDeclarationCourierStatusPM.Tenant, "Declaration");
+            CustomDocumentTypeQueryService docTypeQuery = new CustomDocumentTypeQueryService(customContext);
+               
+            var existCustomsDocumentTypes = customsDocumentsTicketPMList.Where(d => 
+            (d.DocumentTypeCode == "380" || d.DocumentTypeCode == "ILD") && d.DocumentsFilingId != null && d.CustomsDocId != null)
+                .Select(x=>x.DocumentTypeCode).Distinct().ToList();
+
+            if (existCustomsDocumentTypes == null || existCustomsDocumentTypes.Count == 0)
+            {
+                myDeclarationCourierStatusPM.MissedDocumentStatusCode = "T";
+            }
+            else
+            {
+                if (existCustomsDocumentTypes.Count == 1)
+                    myDeclarationCourierStatusPM.MissedDocumentStatusCode = existCustomsDocumentTypes.Contains("380") ? "C" : "I";
+            }
+        }
 
         public void CalcSpecialActionStatus(DeclarationCourierStatusPM myDeclarationCourierStatusPM)
         {
--- Logitude/Logitude.Customs.BL/Messaging/U2L/DeclarationDocuments/DeclarationDocumentsService.cs
+++ Logitude/Logitude.Customs.BL/Messaging/U2L/DeclarationDocuments/DeclarationDocumentsService.cs
@@ -455,6 +455,7 @@ namespace Logitude.Customs.BL.Messaging.U2L.DeclarationDocuments
                 //LogMessagingUtil.Instance.AppendLine("currentDeclarationCourierStatusPM.DocumentStatusCode: " + currentDeclarationCourierStatusPM.DocumentStatusCode);
                 currentDeclarationCourierStatusPM.DocumentStatusCode = "V";
                 calculateDeclarationCourierStatus.CalcDocumentStatusCode(currentDeclarationCourierStatusPM);//// will change if wrong !!!
+                calculateDeclarationCourierStatus.CalcMissingDocumentStatusCode(currentDeclarationCourierStatusPM);
                 if (currentDeclarationCourierStatusPM.DocumentStatusCode == "V")
                 {
                     LogMessagingUtil.Instance.AppendLine($"currentDeclarationCourierStatusPM.DocumentStatusCode: {currentDeclarationCourierStatusPM.DocumentStatusCode}  change 2 V");
