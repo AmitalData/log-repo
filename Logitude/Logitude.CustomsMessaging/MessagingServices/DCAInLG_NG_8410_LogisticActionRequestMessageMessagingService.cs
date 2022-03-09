df5dc511c2c63dc77eaf4b0ee1a2f486c75b7d71
Izik Fridland <IzikFr@amital.co.il>
2022-03-09 18:23:07 +0200

    #157131

--- Logitude/Logitude.CustomsMessaging/MessagingServices/DCAInLG_NG_8410_LogisticActionRequestMessageMessagingService.cs
+++ Logitude/Logitude.CustomsMessaging/MessagingServices/DCAInLG_NG_8410_LogisticActionRequestMessageMessagingService.cs
@@ -17,34 +17,35 @@ using UnifreightIIG.Common.MessageLib.LogisticActionRequestMessage;
 
 namespace Logitude.CustomsMessaging.MessagingServices
 {// moran 25.1.15 - Task 9967
-    public class DCAInLG_NG_8410_LogisticActionRequestMessageMessagingService : MessagingServiceBase<
-        GenericRequestParams,
-        INF_MSG_GenericResponseData,
-        DCAInCustomRequest,
-        LG_NG_8410_LogisticActionRequestMessage,
-        DCAInCustomRequestService,
-        LG_NG_8410_LogisticActionRequestMessageResponseService, DCAInRequestHeader>
-
+    public class DCAInLG_NG_8410_LogisticActionRequestMessageMessagingService 
+        //: MessagingServiceBase<
+        //GenericRequestParams,
+        //INF_MSG_GenericResponseData,
+        //LG_NG_8410_LogisticActionRequestMessage,
+        //LogisticActionRequestRequestParams,
+        //LG_NG_8410_LogisticActionRequestMessageRequestService,
+        //LG_NG_8411_SendLogisticActionRequestDecisionResponseService, 
+        //DCAInRequestHeader>
     {
 
-        protected override GenericRequestParams CreateDefaultRequestParamsFromCustomsResponse(LG_NG_8410_LogisticActionRequestMessage customsResponse)
-        {
-            var myGenericRequestParams = new GenericRequestParams()
-            {
-                LoggingObjectTableId = ObjectTableRepository.GetObjectTableByName("Customs.LogisticActionRequest"),
-            };
-            return myGenericRequestParams;
+        //protected override GenericRequestParams CreateDefaultRequestParamsFromCustomsResponse(LogisticActionRequestRequestParams customsResponse)
+        //{
+        //    var myGenericRequestParams = new GenericRequestParams()
+        //    {
+        //        LoggingObjectTableId = ObjectTableRepository.GetObjectTableByName("Customs.LogisticActionRequest"),
+        //    };
+        //    return myGenericRequestParams;
 
-        }
+        //}
 
-        protected override LG_NG_8410_LogisticActionRequestMessage CallWS(DCAInCustomRequest customRequest, GenericRequestParams requestParams, out string exceptionMessage)
-        {
-            exceptionMessage = null; // to check
-            var response = new LG_NG_8410_LogisticActionRequestMessage();
-            return response;
-        }
+        //protected override LogisticActionRequestRequestParams CallWS(LG_NG_8410_LogisticActionRequestMessage customRequest, GenericRequestParams requestParams, out string exceptionMessage)
+        //{
+        //    exceptionMessage = null; // to check
+        //    var response = new LogisticActionRequestRequestParams();
+        //    return response;
+        //}
 
-        public override string MainInterfaceCode { get { return "8410"; } }
+        //public override string MainInterfaceCode { get { return "8410"; } }
 
     }
 }
--- Logitude/Logitude.CustomsMessaging/MessagingServices/DCAInLG_NG_8411_SendLogisticActionRequestDecisionMessagingService.cs
+++ Logitude/Logitude.CustomsMessaging/MessagingServices/DCAInLG_NG_8411_SendLogisticActionRequestDecisionMessagingService.cs
@@ -2,42 +2,54 @@
 using Logitude.CustomsMessaging.RequestServices;
 using Logitude.CustomsMessaging.Common.ResponseData;
 using Logitude.CustomsMessaging.ResponseServices;
-
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnifreightIIG.Common.ClientSdk;
+using UnifreightIIG.Common.DeclarationStatusQueryRequestServiceReference;
+using UnifreightIIG.Common.TheGateway;
+using UnifreightIIG.Common.MessageLib.CargoTracking;
 using Simplog.Data.InfrastructureModel.Repositories;
 using UnifreightIIG.Common.LogisticActionRequestMessageDecision;
+//using UnifreightIIG.Common.LogisticActionRequestMessageDecision;
+//using UnifreightIIG.Common.MessageLib.LogisticActionRequestMessage;
 
 namespace Logitude.CustomsMessaging.MessagingServices
-{ //9022
-    public class DCAInLG_NG_8411_SendLogisticActionRequestDecisionMessagingService : 
-    MessagingServiceBase<
-        LogisticActionRequestRequestParams,
-        LogisticActionRequestResponseData,
-        LG_NG_8411_SendLogisticActionRequestDecision,
-        LG_NG_8411_SendLogisticActionRequestDecision,
-        DCAInCustomRequestService,
-        LG_NG_8411_SendLogisticActionRequestDecisionRequestService,
-        RequestHeader
-    >
+{
+    public class DCAInLG_NG_8411_SendLogisticActionRequestDecisionMessagingService 
+        //: MessagingServiceBase<
+        //GenericRequestParams,
+        //INF_MSG_GenericResponseData,
+        //DCAInCustomRequest,
+        //LG_NG_8411_SendLogisticActionRequestDecision,
+        //DCAInCustomRequestService,
+        //LG_NG_8411_SendLogisticActionRequestDecisionResponseService,
+        //DCAInRequestHeader
+        //>
     {
+        //public override string MainInterfaceCode { get { return "8411"; } }
 
-        protected override GenericRequestParams CreateDefaultRequestParamsFromCustomsResponse(LG_NG_8411_SendLogisticActionRequestDecision customsResponse)
-        {
-            var myGenericRequestParams = new GenericRequestParams()
-            {
-                LoggingObjectTableId = ObjectTableRepository.GetObjectTableByName("Customs.Declaration"),
-            };
-            return myGenericRequestParams;
 
-        }
+        //protected override GenericRequestParams CreateDefaultRequestParamsFromCustomsResponse(LG_NG_8411_SendLogisticActionRequestDecision customsResponse)
+        //{
+        //    var myGenericRequestParams = new GenericRequestParams()
+        //    {
+        //        LoggingObjectTableId = ObjectTableRepository.GetObjectTableByName("Customs.LogisticActionRequest"),
+        //    };
+        //    return myGenericRequestParams;
 
-        protected override LG_NG_8411_SendLogisticActionRequestDecision CallWS(DCAInCustomRequest customRequest, GenericRequestParams requestParams, out string exceptionMessage)
-        {
-            exceptionMessage = null; // to check
-            var response = new LG_NG_8411_SendLogisticActionRequestDecision();
-            return response;
-        }
+        //}
 
-        public override string MainInterfaceCode { get { return "8411"; } }
+
+        //protected override LG_NG_8411_SendLogisticActionRequestDecision CallWS(DCAInCustomRequest customRequest, GenericRequestParams requestParams, out string exceptionMessage)
+        //{
+        //    exceptionMessage = null; // to check
+        //    var response = new LG_NG_8411_SendLogisticActionRequestDecision();
+        //    return response;
+        //}
+
 
     }
 }
