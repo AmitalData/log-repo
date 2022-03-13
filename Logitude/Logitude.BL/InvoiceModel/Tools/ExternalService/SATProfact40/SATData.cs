f287ee44cc225ce350eff19216697efef28cdb3f
Wesam Amarneh <wesam@logitudeworld.com>
2022-03-13 13:28:05 +0200

    Managing the details of InformacionGlobal  node

--- Logitude/Logitude.BL/InvoiceModel/Tools/ExternalService/SATProfact40/SATData.cs
+++ Logitude/Logitude.BL/InvoiceModel/Tools/ExternalService/SATProfact40/SATData.cs
@@ -33,5 +33,8 @@ namespace Logitude.BL.InvoiceModel.Tools.ExternalService.SATProfact40
         public static string TransferedSATTransferStatusCode = "TD";
         public static string CanceledSATTransferStatusCode = "CS";
         public static string VoidedInvoiceStatusCode = "VD";
+        public static string BimestralPeriod = "05";
+        public static string IncorporacionFiscalRegimen = "621";
+
     }
 }
