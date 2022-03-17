5f16301b43dbe813fdcc37594257b2190ea84dd5
sohaib <sohaib@amital.co.il>
2022-03-17 11:29:32 +0200

    #158775

--- Logitude/Logitude.Customs.Def/ClosedTable/InterfaceManagementDetails.cs
+++ Logitude/Logitude.Customs.Def/ClosedTable/InterfaceManagementDetails.cs
@@ -515,7 +515,7 @@ namespace Logitude.Customs.Def.ClosedTable
             {
                 Code = "5118",
                 InOut = InOutEnum.I.ToString(),
-                Description = "מענה לבקשת ביטול הצהרת",
+                Description = "מענה לבקשת ביטול הצהרה",
                 DcaPrefixName = "SendDF_MSG5118_DeclarationCancellationReplyMsg_Out.",
                 DefaultSendOptionsCode = null,
                 DefaultPriority = CONST_DefaultPriority,
