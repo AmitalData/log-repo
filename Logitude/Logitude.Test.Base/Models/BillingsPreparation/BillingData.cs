df4df079030bb46af8a937a472588b0f8a35c320
Abed <abed@logitudeworld.com>
2022-03-21 10:29:00 +0200

    158259
    Related Work Items: #158259

--- Logitude/Logitude.Test.Base/Models/BillingsPreparation/BillingData.cs
+++ Logitude/Logitude.Test.Base/Models/BillingsPreparation/BillingData.cs
@@ -1,7 +1,7 @@
 ﻿namespace Logitude.Test.Base.Models.BillingsPreparation
 {
     public static class BillingData
-    {
+    { 
         public static string CurrencyEURId { get; set; }
         public static string MeasurementGRWTId { get; set; }
         public static string ChargeTypeAFTId { get; set; }
