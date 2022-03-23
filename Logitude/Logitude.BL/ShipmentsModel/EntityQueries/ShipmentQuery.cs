95adc9326185646d5a0f635d0225b3164301c977
Atallah Hammad <Atallah@logitudeworld.com>
2022-03-23 18:44:48 +0200

    Solve Shipment list getByFilter request issue

--- Logitude/Logitude.BL/ShipmentsModel/EntityQueries/ShipmentQuery.cs
+++ Logitude/Logitude.BL/ShipmentsModel/EntityQueries/ShipmentQuery.cs
@@ -14245,7 +14245,8 @@ namespace Logitude.BL.ShipmentsModel.EntityQueries
                 var shipmentsMasterDataFields = GetShipmentMasterDataFields(targetedShipmentsQuery);
 
                 var shipmentsAdditionalFields = new List<ShipmentAdditionalFields>();
-                Parallel.ForEach(shipmentIdsList, shipmentId =>
+                var targetedShipmentIds = targetedShipmentsQuery.Select(s => s.Id).ToList();
+                foreach (var shipmentId in targetedShipmentIds)
                 {
                     var shipmentAdditionalFields = new ShipmentAdditionalFields() { ShipmentId = shipmentId };
 
@@ -14259,7 +14260,7 @@ namespace Logitude.BL.ShipmentsModel.EntityQueries
                     MapShipmentMasterDataFields(shipmentAdditionalFields, shipmentMasterDataFields);
 
                     shipmentsAdditionalFields.Add(shipmentAdditionalFields);
-                });
+                }
 
                 return shipmentsAdditionalFields;
             }
@@ -14445,7 +14446,7 @@ namespace Logitude.BL.ShipmentsModel.EntityQueries
 
             if (!string.IsNullOrEmpty(shipmentMasterDataFields.ToPortId))
             {
-                PortPM port = portQuery.GetSinglePM(shipmentMasterDataFields.Transshipment1FromPortId, shipmentMasterDataFields.Tenant);
+                PortPM port = portQuery.GetSinglePM(shipmentMasterDataFields.ToPortId, shipmentMasterDataFields.Tenant);
                 if (port != null)
                 {
                     shipmentAdditionalFields.ToPortId = shipmentMasterDataFields.ToPortId;
