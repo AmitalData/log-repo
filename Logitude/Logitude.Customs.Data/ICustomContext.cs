95d2ae6d1c93162dc7f23e2c041d6ef0a686ea3d
sohaib <sohaib@amital.co.il>
2022-03-28 15:10:07 +0300

    fix tt

--- Logitude/Logitude.Customs.Data/ICustomContext.cs
+++ Logitude/Logitude.Customs.Data/ICustomContext.cs
@@ -148,7 +148,6 @@ namespace Logitude.Customs.Data
 		 IDbSet<CustomsDocumentStatusType> CustomsDocumentStatusTypes { get; }
 		 IDbSet<CustomsDocumentsTicket> CustomsDocumentsTickets { get; }
 		 IDbSet<CustomsDocumentUpload> CustomsDocumentUploads { get; }
-		 IDbSet<CustomsEnvironmentSetting> CustomsEnvironmentSettings { get; }
 		 IDbSet<CustomsEnvoirmentType> CustomsEnvoirmentTypes { get; }
 		 IDbSet<CustomsExchangeRate> CustomsExchangeRates { get; }
 		 IDbSet<CustomsGeneral> CustomsGenerals { get; }
