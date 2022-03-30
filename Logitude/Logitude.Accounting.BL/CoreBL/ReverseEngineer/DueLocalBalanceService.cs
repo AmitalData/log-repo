f4642b6ddcf3ba4d3a2fbd482a911f019f30fa82
Alexander Khitrik <alexanderh@amital.co.il>
2022-03-30 09:46:36 +0300

    Alexander: 159558 IntegrityChecks add. data

--- Logitude/Logitude.Accounting.BL/CoreBL/ReverseEngineer/DueLocalBalanceService.cs
+++ Logitude/Logitude.Accounting.BL/CoreBL/ReverseEngineer/DueLocalBalanceService.cs
@@ -592,6 +592,8 @@ namespace Logitude.Accounting.BL.CoreBL
     public class DueLocalBalanceDiffM
     {
         public string AccountId { get; set; }
+        public string DisplayNumber { get; set; }
+        public string LocalName { get; set; }
 
         public decimal CalcDueInLocal { get; set; }
         public decimal CalcDueInForeign { get; set; }
