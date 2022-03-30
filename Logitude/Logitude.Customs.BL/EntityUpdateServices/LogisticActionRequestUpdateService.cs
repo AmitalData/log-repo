752b4e31fd8282b550788a87e583f52d89eaff73
Moran Segev <moran@amital.co.il>
2022-03-30 15:21:54 +0300

    159210

--- Logitude/Logitude.Customs.BL/EntityUpdateServices/LogisticActionRequestUpdateService.cs
+++ Logitude/Logitude.Customs.BL/EntityUpdateServices/LogisticActionRequestUpdateService.cs
@@ -46,9 +46,8 @@ namespace Logitude.Customs.BL.EntityUpdateServices
             LogisticActionRequestQueryService logisticActionRequestQueryService = new LogisticActionRequestQueryService(context);
             if (!string.IsNullOrWhiteSpace(myLogisticActionRequestPM.Id))
             {
-                LogisticActionRequestPM ExistLogisticActionRequestPM = new LogisticActionRequestPM();
-                ExistLogisticActionRequestPM = logisticActionRequestQueryService.GetExistByCargoKey(myLogisticActionRequestPM.Id, myLogisticActionRequestPM.CargoIdentifierKey1, myLogisticActionRequestPM.CargoIdentifierKey2, myLogisticActionRequestPM.CargoIdentifierKey3, myLogisticActionRequestPM.CargoIdentifierType);
-                if (ExistLogisticActionRequestPM != null) text = "קיימת בקשה לביטול יצוא עם אותם מזהי מטען";
+                var exist = logisticActionRequestQueryService.GetExistByCargoKey(myLogisticActionRequestPM.Id, myLogisticActionRequestPM.CargoIdentifierKey1, myLogisticActionRequestPM.CargoIdentifierKey2, myLogisticActionRequestPM.CargoIdentifierKey3, myLogisticActionRequestPM.CargoIdentifierType);
+                if (exist) text = "קיימת בקשה לביטול יצוא עם אותם מזהי מטען";
             }
 
             if (!string.IsNullOrWhiteSpace(text))
