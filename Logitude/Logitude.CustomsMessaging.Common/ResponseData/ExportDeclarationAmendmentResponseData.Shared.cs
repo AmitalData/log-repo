e64c3153169799927da9784129969ba1c8331c73
Atara Oster <Atara@amital.co.il>
2022-03-30 13:39:06 +0300

    #159773

---
+++ Logitude/Logitude.CustomsMessaging.Common/ResponseData/ExportDeclarationAmendmentResponseData.Shared.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logitude.CustomsMessaging.Common.ResponseData
+{
+    public  class ExportDeclarationAmendmentResponseData : INF_MSG_GenericResponseData
+    {
+        public bool IsExportCloseApprove { get; set; }
+    }
+   
+}
