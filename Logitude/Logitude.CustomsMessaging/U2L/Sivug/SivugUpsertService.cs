1a9d5fde58a5584ed9ae05cf8761bdc454a1c228
Atara Oster <Atara@amital.co.il>
2022-04-07 11:54:28 +0300

    #157787

--- Logitude/Logitude.CustomsMessaging/U2L/Sivug/SivugUpsertService.cs
+++ Logitude/Logitude.CustomsMessaging/U2L/Sivug/SivugUpsertService.cs
@@ -866,7 +866,7 @@ namespace Logitude.CustomsMessaging.U2L.Sivug
                             SupplierInvoiceItemPM.ClassificationCode = invoiceItem.CLASSIFICATIONCODE;
                             SupplierInvoiceItemPM.ChangeSetOp = ChangeSetOperation.Update;
                         }
-                        if (invoiceItem.ITEMORIGINCOUNTRY != SupplierInvoiceItemPM.OriginCountryCode)
+                        if ((invoiceItem.ITEMORIGINCOUNTRY ?? "") != (SupplierInvoiceItemPM.OriginCountryCode ?? ""))
                         {
                             SupplierInvoiceItemPM.OriginCountryCode = invoiceItem.ITEMORIGINCOUNTRY;
                             SupplierInvoiceItemPM.ChangeSetOp = ChangeSetOperation.Update;
