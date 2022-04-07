8cc25a516b10577c9ab8e5ca589abaf7059c3b73
Atara Oster <Atara@amital.co.il>
2022-04-07 16:41:52 +0300

    #158145

--- Logitude/AngularModules/AngularModules/Customs/Services/ExtendedPMs/CustomsRequestSheetExtendedPMService.ts
+++ Logitude/AngularModules/AngularModules/Customs/Services/ExtendedPMs/CustomsRequestSheetExtendedPMService.ts
@@ -113,6 +113,20 @@ export class CustomsRequestSheetExtendedPMService {
         );
 
     }
+
+    GetGeneralRequestInProgress(interfaceTypeCode: string, objectTableId1: string, entityId1: string, tenant: number) {
+
+        return defer(() => {
+            return this._http.get(this._apiUrl + '/GetGeneralRequestInProgress/?' + 'tenant=' + tenant + '&interfaceTypeCode=' + interfaceTypeCode + '&objectTableId1=' + objectTableId1 + '&entityId1=' + entityId1 + '&objectTableId2=' + "" + '&entityId2=' + "" + '&customFileNo=' + "", ServiceHelper.GetHttpHeaders())
+                .pipe(map(response => {
+                    var serviceResponse: ServiceResponse = new ServiceResponse();
+                    var requestSheets = response;
+                    serviceResponse.Result = requestSheets;
+                    return serviceResponse;
+                }), catchError(ServiceHelper.HandleServiceError));
+        });
+    }
+
     MapJsonToEntityPM(jsonPM: any) {
 
         var entityPM: CustomsRequestsSheetPM;
