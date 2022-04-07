d6d1a4a14a4561c570844c0f7a45cc121c2e7e6d
Izik Fridland <IzikFr@amital.co.il>
2022-04-07 09:12:56 +0300

    #160001

--- Logitude/Logitude.Customs.Data/EntityLists/Generated/DeclarationReferantDataList.Generated.cs
+++ Logitude/Logitude.Customs.Data/EntityLists/Generated/DeclarationReferantDataList.Generated.cs
@@ -176,6 +176,10 @@ namespace Logitude.Customs.Data.EntityLists
        public string RemoveInclusiveVisibility  { get; set; }
        [DataMember]
        public string ReferantName  { get; set; }
+       [DataMember]
+       public string Mawb  { get; set; }
+       [DataMember]
+       public string Hawb  { get; set; }
    }
 
 }
