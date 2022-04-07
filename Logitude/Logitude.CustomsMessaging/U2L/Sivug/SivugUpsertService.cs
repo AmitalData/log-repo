1d4a11d7e8432bb6c03997314532a8314879a881
Izik Fridland <IzikFr@amital.co.il>
2022-04-07 14:03:41 +0300

    #154141

--- Logitude/Logitude.CustomsMessaging/U2L/Sivug/SivugUpsertService.cs
+++ Logitude/Logitude.CustomsMessaging/U2L/Sivug/SivugUpsertService.cs
@@ -863,7 +863,7 @@ namespace Logitude.CustomsMessaging.U2L.Sivug
 
                         if (invoiceItem.CLASSIFICATIONCODE != SupplierInvoiceItemPM.ClassificationCode)
                         {
-                            SupplierInvoiceItemPM.ClassificationCode = CalculateCheckDigit.Calc(invoiceItem.CLASSIFICATIONCODE);
+                            SupplierInvoiceItemPM.ClassificationCode =invoiceItem.CLASSIFICATIONCODE;
                             SupplierInvoiceItemPM.ChangeSetOp = ChangeSetOperation.Update;
                         }
                         if (invoiceItem.CLASIFIEDREMARKS != SupplierInvoiceItemPM.ClasifiedRemarks)
@@ -1039,7 +1039,7 @@ namespace Logitude.CustomsMessaging.U2L.Sivug
                     }
                 }
                 SupplierInvoiceItemPM.StatisticQuantityType = TranslateMeasurmentUnit(invoiceItem.StatisticQuantityType);
-                SupplierInvoiceItemPM.TaxExemptCode = invoiceItem.TAXEXEMPTCODE; //TranslateTaxExemptCode(invoiceItem.TAXEXEMPTCODE);
+                SupplierInvoiceItemPM.TaxExemptCode = CalculateCheckDigit.Calc(invoiceItem.TAXEXEMPTCODE); //TranslateTaxExemptCode(invoiceItem.TAXEXEMPTCODE);
 
                 if (!string.IsNullOrWhiteSpace(invoiceItem.OcrPageNumber))
                 {
