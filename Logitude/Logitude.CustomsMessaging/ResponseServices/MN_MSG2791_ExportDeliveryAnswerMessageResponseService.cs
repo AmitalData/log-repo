1f5a563ee7ea767e4775f9dedbf5d9a7e7ed7362
Shoshana Greenwald <shoshana@amital.co.il>
2022-04-11 15:43:28 +0300

    #159610

--- Logitude/Logitude.CustomsMessaging/ResponseServices/MN_MSG2791_ExportDeliveryAnswerMessageResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/MN_MSG2791_ExportDeliveryAnswerMessageResponseService.cs
@@ -106,7 +106,7 @@ namespace Logitude.CustomsMessaging.ResponseServices
 
         }
 
-        public static void RaiseExportStorageStatus(string statusId, string unifrieghtStatus, ExportStoragePM dirtyEntityPM, string FUStatusRemarks)
+        public static void RaiseExportStorageStatus(string statusId, string unifrieghtStatus, ExportStoragePM dirtyEntityPM, string FUStatusRemarks, DateTime? date=null)
         {
             try
             {
@@ -135,7 +135,7 @@ namespace Logitude.CustomsMessaging.ResponseServices
                         status = "new",
                         xml_status = "new",
                         status_id = unifrieghtStatus,
-                        status_DateTime = DateTime.Now,
+                        status_DateTime = date.HasValue?date.GetValueOrDefault():DateTime.Now,
                         //status_save = "no_fail",
                         comments = FUStatusRemarks,
                     };
@@ -148,47 +148,6 @@ namespace Logitude.CustomsMessaging.ResponseServices
                 throw;
             }
         }
-        public static void RaiseExportStorageStatus(string statusId, string unifrieghtStatus, ExportStoragePM dirtyEntityPM, string FUStatusRemarks, DateTime date)
-        {
-            try
-            {
-                string loggingUserId = "";
-                loggingUserId = AuthenticationUtil.ResolveUserId(dirtyEntityPM.Tenant);
-
-                var eventContextTagModel = dirtyEntityPM.CurrentContextTag as EventContextTagModel;
-                var myAmitalEventTracerModel = new Logitude.Customs.BL.TraceEvents.AmitalEventTracerModel()
-                {
-                    Tenant = dirtyEntityPM.Tenant,
-                    objectTableName = "Customs.ExportStorage",
-                    EventCode = statusId,
-                    notes = "DO_NOT_RAISE_EVENT",
-                    CommunicationLoggingEntityReference = dirtyEntityPM.Id.ToString(),
-                    EntityId = dirtyEntityPM.Id,
-                    UserId = loggingUserId,
-                    CommunicationSubject = "FU Status from logitude ",
-                };
-
-                if (!string.IsNullOrWhiteSpace(dirtyEntityPM.StorageNo))
-                {
-                    myAmitalEventTracerModel.MyFUStatus = new AmitalEventTracerModel.FUStatus()
-                    {
-                        entname = "MSCSTORAGE",
-                        primary_number = dirtyEntityPM.StorageNo,
-                        status = "new",
-                        xml_status = "new",
-                        status_id = unifrieghtStatus,
-                        status_DateTime = date,
-                        //status_save = "no_fail",
-                        comments = FUStatusRemarks,
-                    };
-                }
-                AmitalEventTracer.CreateTraceEvent(myAmitalEventTracerModel);
-            }
-            catch (System.Exception)
-            {
-                // TODO: BL Stop Execute or Cuntinue - Ask IHAB
-                throw;
-            }
-        }
+       
     }
 }
