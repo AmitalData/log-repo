8b845b93fc6abc999743e9bd6d5c5b4716b327da
Izik Fridland <IzikFr@amital.co.il>
2022-04-14 18:06:51 +0300

    #159214 #160295

--- Logitude/Logitude.Customs.Data/EntityListQueryServices/ExportLogisticPermitActionListQueryService.cs
+++ Logitude/Logitude.Customs.Data/EntityListQueryServices/ExportLogisticPermitActionListQueryService.cs
@@ -1,4 +1,4 @@
-	using Simplog.Data.InfrastructureModel.EntityPOCOs;
+using Simplog.Data.InfrastructureModel.EntityPOCOs;
 using Simplog.Data.InfrastructureModel.Repositories;
 using Simplog.Server.Infrastructure.DataContracts;
 using Simplog.Server.Infrastructure.Helpers;
@@ -15,30 +15,27 @@ using Logitude.Customs.Data.EntityPOCOs;
 using Logitude.Customs.Data.EntityLists;
 
 namespace Logitude.Customs.Data.EntityListQueryServices
-{ 
+{
 
     public partial class ExportLogisticPermitActionListQueryService
     {
-	    private IQueryable<ExportLogisticPermitActionList> GetIqueryableList(IQueryable<ExportLogisticPermitAction> iQueryable)
+        private IQueryable<ExportLogisticPermitActionList> GetIqueryableList(IQueryable<ExportLogisticPermitAction> iQueryable)
         {
-		IQueryable<ExportLogisticPermitActionList> query = (from a in iQueryable
-                                            select new ExportLogisticPermitActionList()
-											{
-                     
-					                          SearchFields = a.SearchFields,
-					
-					                          Inactive = a.Inactive,
-					
-		                    	            });
+            IQueryable<ExportLogisticPermitActionList> query = (from a in iQueryable
+                                                                select new ExportLogisticPermitActionList()
+                                                                {
+                                                                    Code = a.Code,
+                                                                    EnglishName = a.EnglishName,
+                                                                    LocalName = a.LocalName,
+                                                                    SearchFields = a.SearchFields,
+                                                                    Inactive = a.Inactive,
+                                                                });
             return query;
-		}
+        }
 
-		private IQueryable<ExportLogisticPermitAction> ApplyCustomFilters(QueryOperations queryOperations,IQueryable<ExportLogisticPermitAction> iQueryable)
+        private IQueryable<ExportLogisticPermitAction> ApplyCustomFilters(QueryOperations queryOperations, IQueryable<ExportLogisticPermitAction> iQueryable)
         {
-			throw new NotImplementedException();
-		}
-			}
-
-
+            return iQueryable; //throw new NotImplementedException();
+        }
+    }
 }
-	
