202ff55a664d09b51037976b03e92dd8a5a9f0e6
Ahmad A. Abdalghafoor <ahmada@logitudeworld.com>
2022-04-18 11:04:30 +0000

    Enable new Counter Procedure

--- Logitude/Logitude.Server.Tools/Helpers/TableCounter.cs
+++ Logitude/Logitude.Server.Tools/Helpers/TableCounter.cs
@@ -166,10 +166,10 @@ namespace Logitude.Server.Tools.Helpers
                     {
                         1,42,1330,2653,1688,2095,1526,1686
                     };
-                    //if (tenant <= 400)//newSPTenants.Where(a=>a == tenant).Count() > 0)
-                    //{
-                    //    cmd = new SqlCommand("dbo.usp_GetNextTableNumberValueWithSnapshot", cn);
-                    //}
+                    if (tenant <= 400)//newSPTenants.Where(a=>a == tenant).Count() > 0)
+                    {
+                        cmd = new SqlCommand("dbo.usp_GetNextTableNumberValueWithSnapshot", cn);
+                    }
                     cmd.CommandType = CommandType.StoredProcedure;
 
                     SqlParameter lastValuePar = new SqlParameter("@pLastValue", SqlDbType.Int);
