c47839d5573aeb7cbaade7026822466ca59f25b5
Shoshana Greenwald <shoshana@amital.co.il>
2022-04-25 15:58:15 +0300

    #160446

--- Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/GeneratedUpdate/EntityUpdateClasses/ClientUpdateClass.cs
+++ Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/GeneratedUpdate/EntityUpdateClasses/ClientUpdateClass.cs
@@ -85,7 +85,7 @@ namespace WebFreight.Web.MetaDataUpdate.GeneratedUpdate.EntityUpdateClasses
 {
    public class ClientUpdateClass
    {  		
-		public const string HashString = "52461aab6d3d4a9c566a876814201f7e";
+		public const string HashString = "4a5c8a5c1824a746a381f49f3d49da14";
 	    public void AddObjectTable(Dictionary<string, ObjectTable> objectTables, Dictionary<string, TextCode> textCodes,ObjectTableRepository ObjectTableRepository,TextCodeRepository TextCodeRepository)
         {                     
             
@@ -2627,6 +2627,10 @@ namespace WebFreight.Web.MetaDataUpdate.GeneratedUpdate.EntityUpdateClasses
  		   TextCode ClientTextCode_CustomsClientONewClientSucceeded = AddTextCodes.AddTextCode(new TextCodeDetails() { Code = "Customs.Client.O.NewClientSucceeded", DefaultText = "Create New Client Succeeded",LocalDefaultText = @"לקוח חדש הוקם בהצלחה", ObjectTableId = ClientObjectTable.Id, Tenant = 0, TextCodeTypeCode = "O", IsSpellChecked = false }, TextCodeRepository, TextCodes);
 
  		   TextCode ClientTextCode_CustomsClientNewButton = AddTextCodes.AddTextCode(new TextCodeDetails() { Code = "Customs.Client.NewButton", DefaultText = "New Client",LocalDefaultText = @"לקוח חדש", ObjectTableId = ClientObjectTable.Id, Tenant = 0, TextCodeTypeCode = "O", IsSpellChecked = false }, TextCodeRepository, TextCodes);
+
+ 		   TextCode ClientTextCode_CustomsClientAuthorizationLetters = AddTextCodes.AddTextCode(new TextCodeDetails() { Code = "Customs.Client.AuthorizationLetters", DefaultText = "Authorization Letters",LocalDefaultText = @"שליפת כתבי הרשאה", ObjectTableId = ClientObjectTable.Id, Tenant = 0, TextCodeTypeCode = "O", IsSpellChecked = false }, TextCodeRepository, TextCodes);
+
+ 		   TextCode ClientTextCode_CustomsClientIsSelectAuthorizationLetters = AddTextCodes.AddTextCode(new TextCodeDetails() { Code = "Customs.Client.IsSelectAuthorizationLetters", DefaultText = "Is it possible to retrieve authorization letters for all customers in the system?",LocalDefaultText = @"האם לבצע שליפת כתבי הרשאה לכל הלקוחות במערכת ?", ObjectTableId = ClientObjectTable.Id, Tenant = 0, TextCodeTypeCode = "O", IsSpellChecked = false }, TextCodeRepository, TextCodes);
 
    
 	    
