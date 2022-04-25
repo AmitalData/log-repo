78af4ab6b9108e1656e88e39c58babc1b948e6c5
Libby Hasid <libby@amital.co.il>
2022-04-25 11:13:25 +0300

    #159990

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationOthers/Components/ExportStorageDecleration/ExportStorageDeclerationComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationOthers/Components/ExportStorageDecleration/ExportStorageDeclerationComponent.ts
@@ -360,10 +360,7 @@ export class ExportStorageDeclerationComponent extends BaseComponent {
 
                     }
                 }
-                else {
-
-
-                }
+               
 
             }
         });
