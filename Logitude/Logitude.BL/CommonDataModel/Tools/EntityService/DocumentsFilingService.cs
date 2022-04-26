b8fef13f2355d3252d7196b804dcb80e3fc10409
Maheera Jazi <Maheera@logitudeworld.com>
2022-04-26 11:52:39 +0300

    #160867

--- Logitude/Logitude.BL/CommonDataModel/Tools/EntityService/DocumentsFilingService.cs
+++ Logitude/Logitude.BL/CommonDataModel/Tools/EntityService/DocumentsFilingService.cs
@@ -680,6 +680,8 @@ namespace Logitude.BL.CommonDataModel.Tools.EntityService
 
             entityRepository.Update(Poco);
             entityRepository.SubmitChanges();
+
+            this.OpenPODDocumentUploderQueue(theEntityPm);
             if (!tenantPM.IsDocumentsArchive && LogitudeSettings.DeploymentStage != "Simplog")
             {
                 AddToTasksQueue(theEntityPm, isNewEntity, loggedUserId);
