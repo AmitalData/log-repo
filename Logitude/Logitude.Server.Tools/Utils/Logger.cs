373e2bf77b1fe516091a005cfb55945342f85bfb
Itzik Moshelian <itzik@amital.co.il>
2022-04-26 10:09:49 +0300

    #160850

--- Logitude/Logitude.Server.Tools/Utils/Logger.cs
+++ Logitude/Logitude.Server.Tools/Utils/Logger.cs
@@ -69,6 +69,7 @@ namespace Logitude.Server.Tools.Utils
         {
             try
             {
+                mess = $"[{DateTime.Now.ToString("G")}]{mess}";
                 if (!Error)
                 {
                     if (string.IsNullOrEmpty(suffix)) suffix = "Mess";
