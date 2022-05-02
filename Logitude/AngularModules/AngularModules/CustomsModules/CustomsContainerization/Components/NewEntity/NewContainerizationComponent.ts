fd1796e012edfcbf750155fc89f2a20f789289a0
Libby Hasid <libby@amital.co.il>
2022-05-02 12:22:27 +0300

    #158893

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsContainerization/Components/NewEntity/NewContainerizationComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsContainerization/Components/NewEntity/NewContainerizationComponent.ts
@@ -456,9 +456,9 @@ export class NewContainerizationComponent extends BaseComponent {
 
         if (this.entityPM.Id != null) {
             this.declarationWebService.GetDeclarationByConsignmentParames(ArrayDeclartiosId).subscribe(res => {
-                if (res.Result !=null && res.Result!=0) {
+                if (res.Result !=null) {
                     debugger;
-                    myConfirmWindow.Show(`שים לב ישנם  הצהרות נוספות  בעלות אותם מזהים שלא צורפו להמכלה הנוכחית ${res.Result}`);
+                    myConfirmWindow.Show(`שים לב ישנם הצהרות נוספות  בעלות אותה מזהים שלא צורפה להמכלה הנוכחית ${res.Result} `);
                     myConfirmWindow.WindowClosed.subscribe(event => {
                         if (myConfirmWindow.Yes == true) {
                             SessionLocator.SelectedSession.StartBusyIndicatorLoading();
@@ -482,7 +482,7 @@ export class NewContainerizationComponent extends BaseComponent {
 
 
                 if (res.Result !=null && res.Result!=0) {
-                    myConfirmWindow.Show(`שים לב ישנם ${res.Result} הצהרות נוספות  בעלות אותם מזהים שלא צורפו להמכלה הנוכחית`);
+                    myConfirmWindow.Show(`שים לב ישנם הצהרות נוספות בעלות אותם מזהים שלא צורפו להמכלה הנוכחית   ${res.Result}  `);
                     myConfirmWindow.WindowClosed.subscribe(event => {
 
                         if (myConfirmWindow.Yes == true) {
