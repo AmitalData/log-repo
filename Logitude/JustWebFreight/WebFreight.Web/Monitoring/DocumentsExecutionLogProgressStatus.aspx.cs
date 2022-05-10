8c464b14874fb8d818e2182434f26add1aef2074
Abed <abed@logitudeworld.com>
2022-05-10 12:08:45 +0300

    161395

--- Logitude/JustWebFreight/WebFreight.Web/Monitoring/DocumentsExecutionLogProgressStatus.aspx.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Monitoring/DocumentsExecutionLogProgressStatus.aspx.cs
@@ -31,7 +31,7 @@ namespace WebFreight.Web.Monitoring
             Response.ContentType = "text/xml";
             Response.Write("<pingdom_http_custom_check>");
 
-            if (AnyDocumentsExecutionLogsWaitingStatus())
+            if (AnyDocumentsExecutionLogsProgressStatus())
             {
                 Response.Write("<status>Fail</status>");
             }
@@ -47,27 +47,27 @@ namespace WebFreight.Web.Monitoring
             Response.End();
         }
 
-        private bool AnyDocumentsExecutionLogsWaitingStatus()
+        private bool AnyDocumentsExecutionLogsProgressStatus()
         {
-            bool isWaitingStatus = false;
+            bool isProgressStatus = false;
             using (TransactionScope scope = TransactionFactory.GetNewTransaction())
             {
                 try
                 {
                     DateTime twoDaysBefore = DateTime.Now.AddDays(-2);
                     ICommonDataContext commonDataContext = CommonDataContext.GetContext(0);
-                    isWaitingStatus = (from d in commonDataContext.DocumentsExecutionLogs
+                    isProgressStatus = (from d in commonDataContext.DocumentsExecutionLogs
                                        where d.CreateDate > twoDaysBefore && d.StatusCode == "P" && (EntityFunctions.DiffMinutes(d.CreateDate, DateTime.Now) >= 6)
                                        select d).Any();
                 }
 
                 catch (Exception errorInfo)
                 {
-                    ExceptionHandler.HandleException(errorInfo, DateTime.Now, 0, "", "DocumentsExecutionLogsWaitingStatus", "Bug in AnyWaitingStatus Method : isWaitingStatus = (from a in commonDataContext.DocumentsExecutionLogs ...", null);
+                    ExceptionHandler.HandleException(errorInfo, DateTime.Now, 0, "", "DocumentsExecutionLogProgressStatus", "Bug in AnyProgressStatus Method : isProgressStatus = (from a in commonDataContext.DocumentsExecutionLogs ...", null);
                 }
                 scope.Complete();
             }
-            return isWaitingStatus;
+            return isProgressStatus;
         }
     }
 }
