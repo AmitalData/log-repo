eda0a81aa7447b4eefc4125ecb66c5ce37858582
Samar Abu Eid <Samar@logitudeworld.com>
2022-05-10 14:50:44 +0300

    #161405

--- Logitude/Logitude.BL/ShipmentsModel/Tools/Validating/ShipmentValidating.cs
+++ Logitude/Logitude.BL/ShipmentsModel/Tools/Validating/ShipmentValidating.cs
@@ -78,8 +78,12 @@ namespace Logitude.BL.ShipmentsModel.Tools.Validating
                 ValidateMainCarriageCarrierDueToTransportMode(entityPM);
                 ValidatePartnerTypes(entityPM);
                 ValidateShipmentSubType(entityPM);
-                ValidateShipmentOperationalClose(entityPM, entityPoco);
-                ValidateShipmentAccountingClose(entityPM, entityPoco);
+
+                if (FeatureToggleHelper.HasFeatureToggle("TSV", entityPM.Tenant))
+                {
+                    ValidateShipmentOperationalClose(entityPM, entityPoco);
+                    ValidateShipmentAccountingClose(entityPM, entityPoco);
+                }
             }
         }
         public static string GetCustomerCreditLimitDetails(string customerId, string quoteId, bool isBuildFromQuote, int tenant)
