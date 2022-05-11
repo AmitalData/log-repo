df21407abab138adf3221c371c509b670704cc5c
Ahmad Abdalrazeq <a.abdalrazeq@logitudeworld.com>
2022-05-11 14:52:01 +0300

    #161473 Upload Document Into Shipment Order Using WCF Service

--- Logitude/Logitude.BL/CommonDataModel/Helpers/ShipmentOrderDocumentsQueueService.cs
+++ Logitude/Logitude.BL/CommonDataModel/Helpers/ShipmentOrderDocumentsQueueService.cs
@@ -17,15 +17,22 @@ namespace Logitude.BL.CommonDataModel.Helpers
             var objectTableName = ObjectTableRepository.GetSingleObjectTableById(documentsFiling.ObjectTableId, documentsFiling.Tenant).Name;
             if (objectTableName != "ShipmentOrder") return;
 
-            if (string.IsNullOrEmpty(documentsFiling.EntityId)) return;
-
-            var shipmentOrder = new ShipmentOrderRepository(documentsFiling.Tenant).GetSingleById(documentsFiling.EntityId, documentsFiling.Tenant);
+            ShipmentOrder shipmentOrder = GetShipmentOrder(documentsFiling);
+            if (shipmentOrder == null) return;
             if (!ValidForLogBoxTransfer(shipmentOrder)) return;
 
             IQueueService queueservice = new DbQueueService();
             queueservice.InitializeQueue("ImporterShipmentOrderDocumentsQueue", 0);
             queueservice.Send(new Dictionary<string, string>() { { "ShipmentOrderId", documentsFiling.EntityId }, { "DocumentFilingId", documentsFiling.Id }, { "Tenant", documentsFiling.Tenant.ToString() }, }, documentsFiling.Tenant);
         }
+
+        private ShipmentOrder GetShipmentOrder(DocumentsFilingPM documentsFiling)
+        {
+            if (!string.IsNullOrEmpty(documentsFiling.EntityId)) return new ShipmentOrderRepository(documentsFiling.Tenant).GetSingleById(documentsFiling.EntityId, documentsFiling.Tenant);
+            if (!string.IsNullOrEmpty(documentsFiling.EntityNumber)) return new ShipmentOrderRepository(documentsFiling.Tenant).GetSingleByOrderNumber(documentsFiling.EntityNumber, documentsFiling.Tenant);
+            return null;
+        }
+
         private bool ValidForLogBoxTransfer(ShipmentOrder shipmentOrder)
         {
             return shipmentOrder.TransportModeId == "O" && shipmentOrder.DirectionId == "E" && !string.IsNullOrEmpty(shipmentOrder.CustomerTenantNumber?.ToString()) && !string.IsNullOrEmpty(shipmentOrder.CustomerShipmentNumber) && HaveCustomerAccess(shipmentOrder);
