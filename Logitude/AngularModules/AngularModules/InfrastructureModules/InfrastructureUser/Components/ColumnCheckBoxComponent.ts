5677b000a65b82778bf0369b1fbb6a83ca651cd9
Maheera Jazi <Maheera@logitudeworld.com>
2022-05-15 11:59:07 +0300

    #160273

--- Logitude/AngularModules/AngularModules/InfrastructureModules/InfrastructureUser/Components/ColumnCheckBoxComponent.ts
+++ Logitude/AngularModules/AngularModules/InfrastructureModules/InfrastructureUser/Components/ColumnCheckBoxComponent.ts
@@ -16,20 +16,29 @@ export class ColumnCheckBoxComponent {
     public rowData: UserExtendedList;
     public fieldName: any;
     public packageCode: string;
+    public packageName: string;
     public columnIndex: string;
     public IsEnabled: boolean = true;
+    public LicenseManagementTitle = null;
+
     setVariables(rowData: any, fieldName: string) {
         this.rowData = rowData;
         this.fieldName = fieldName.split(",");
         this.packageCode = this.fieldName[0];
         this.columnIndex = this.fieldName[1];
+        this.packageName = this.fieldName[2];
         
-        this.SetIsChecked();   
-
+        this.SetIsChecked();
+        this.SetLicenseManagementTitle();
+    
         var isDestroyed: boolean = this.CD['destroyed'];
         if (!isDestroyed) {
             this.CD.detectChanges();
         }
+    }
+
+    private SetLicenseManagementTitle() {
+        this.LicenseManagementTitle = "To remove the main package(" + this.packageName +") from a user, please make sure the 'Additional Packages Only' field is checked for that user.";
     }
 
     private SetIsChecked() {
