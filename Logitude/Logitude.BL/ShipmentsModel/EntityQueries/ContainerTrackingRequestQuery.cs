8c77d71b3da8e8537196e24e798ab52dbde4ca77
Amer Aldghameen <Amer@logitudeworld.com>
2022-05-15 16:14:05 +0300

    ContainerTrackingRequests,ContainerTrackingResponses

---
+++ Logitude/Logitude.BL/ShipmentsModel/EntityQueries/ContainerTrackingRequestQuery.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using Simplog.Data.ShipmentsModel.Repositories;
+using Logitude.BL.ShipmentsModel.EntityPMs;
+using Logitude.BL.ShipmentsModel.EntityLists;
+using Simplog.Data.ShipmentsModel.EntityPOCOs;
+
+namespace Logitude.BL.ShipmentsModel.EntityQueries
+{
+    public class ContainerTrackingRequestQuery
+    {
+        ContainerTrackingRequestRepository repository;
+
+        public ContainerTrackingRequestQuery(int tenant)
+        {
+            repository = new ContainerTrackingRequestRepository(tenant);
+        }
+
+        public ContainerTrackingRequestQuery(ContainerTrackingRequestRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public ContainerTrackingRequestPM GetSinglePM(string id,int tenant)
+        {
+            if (string.IsNullOrEmpty(id))
+                return null;
+
+            ContainerTrackingRequestPM entity = (from a in repository.context.ContainerTrackingRequests where a.Id == id && a.Tenant == tenant
+                                                 select new ContainerTrackingRequestPM()
+                                                  {
+                                                      Id = a.Id,
+                                                      RequestId = a.RequestId,
+                                                      SearchFields = a.SearchFields,
+                                                      ContainerNumber = a.ContainerNumber,
+                                                      Provider = a.Provider,
+                                                      Master = a.Master,
+                                                      Tenant = a.Tenant
+                                                  }).FirstOrDefault();
+
+            return entity;
+
+
+        }
+
+        public IQueryable<ContainerTrackingRequestList> GetIQueryableEntityList(IQueryable<ContainerTrackingRequest> iQueryable)
+        {
+            IQueryable<ContainerTrackingRequestList> result = (from a in iQueryable
+                                                                select new ContainerTrackingRequestList()
+                                                                {
+                                                                    Id = a.Id,
+                                                                    RequestId = a.RequestId,
+                                                                    SearchFields = a.SearchFields,
+                                                                    ContainerNumber = a.ContainerNumber,
+                                                                    Provider = a.Provider,
+                                                                    Master = a.Master,
+                                                                    Tenant = a.Tenant
+                                                                });
+            return result;
+        }
+
+
+    }
+}
