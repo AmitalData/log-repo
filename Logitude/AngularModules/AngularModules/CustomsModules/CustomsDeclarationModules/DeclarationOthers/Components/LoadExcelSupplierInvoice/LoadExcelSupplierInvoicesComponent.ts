fab5e7acb71b39ae269e92883a4478ae9bfb0ca0
Atara Oster <Atara@amital.co.il>
2022-05-19 15:07:21 +0300

    #161681

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationOthers/Components/LoadExcelSupplierInvoice/LoadExcelSupplierInvoicesComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationOthers/Components/LoadExcelSupplierInvoice/LoadExcelSupplierInvoicesComponent.ts
@@ -1,4 +1,4 @@
-import { OnInit, Component } from '@angular/core';
+import { OnInit, Component, ViewChild, ElementRef } from '@angular/core';
 import { ExportDeclarationClosingDataPM } from '../../../../../Customs/EntityPMs/ExportDeclarationClosingDataPM';
 import { DeclarationPM } from '../../../../../Customs/EntityPMs/DeclarationPM';
 import { BaseComponent } from '../../../../../Infrastructure/Components/LogitudeComponents/BaseComponent';
@@ -14,27 +14,141 @@ import { CustomMessageProgressComponent, ShowProgressBarParams } from '../../../
 import { DeclarationEditComponentController } from '../../../../../Customs/Controller/DeclarationEditComponentController';
 import { LogitudeWindow } from '../../../../../Controls/Windows/LogitudeWindow';
 import { DeclarationWebService } from '../../../../../Customs/Services/WebServices/DeclarationWebService';
-
+import { Guid } from '../../../../../Infrastructure/Utilities/Guid';
+import { MessageWindow } from '../../../../../Controls/Windows/MessageWindow';
+import { ImageParameter } from '../../../../../Infrastructure/DataContracts/ImageParameter';
+declare var attachmentUploader, ResultAsArray: any;
 @Component({
     selector: 'LoadExcelSupplierInvoicesComponent',
     templateUrl: './LoadExcelSupplierInvoicesComponent.html',
 })
 
 export class LoadExcelSupplierInvoicesComponent extends BaseComponent {
-    
+
+    DataContext: any = this;
+    FileName: string;
+    FileExtension: string;
+    File: any;
+    IsShowProgressBar: boolean = false;
+    UploadButtonIsEnabled: boolean = true;
+    filterImageParameter: ImageParameter;
+    UploadSuccessLabel: boolean
+    Placeholder: any = "";
+    ProgressBarPercentText: string;
+    public UploadFileId: string = Guid.NewRandomString();
+    private CurrentSession = SessionLocator.SelectedSession;
+
     constructor(private EntityResourceService: EntityResourceService) {
         super();
     }
 
-    
+    customerId: string;
+    get CustomerId() { return this.customerId }
+    set CustomerId(value: string) {
+        this.customerId = value;
+    }
 
+    
     SetWindowArgs(args: any) {
         /*this.EntityResourceService.getEntityResourceByTableName(this.ObjectTableName).subscribe((response: any) => {
             //this.DecPM = args.EntityPM;
 
         });*/
     }
+    OpenUpLoadFile() {
+        document.getElementById(this.UploadFileId).click();
+    }
+    public ShowMessage(message: string) {
+        var messageWindow: MessageWindow = new MessageWindow();
+        messageWindow.Show(message);
+    }
 
+    UploadFile(event: any) {
+        var file: any = attachmentUploader(this.UploadFileId);
+        if (file) {
+            var temp = file.name.split('.');
+            this.FileExtension = temp[temp.length - 1];
+            this.FileName = file.name.replace("." + this.FileExtension, "");
+
+            if (this.FileExtension != "csv") {
+                this.ShowMessage("חובה קובץ CSV");
+                return;
+            }
+
+            this.File = file;
+
+            if (this.FileExtension && this.FileExtension.length > 10) {
+                this.ShowMessage("File extension should be less than or equal 10 characters");
+            }
+            else {
+                this.IsShowProgressBar = true;
+                this.UploadButtonIsEnabled = false;
+
+                this.filterImageParameter = new ImageParameter();
+                this.filterImageParameter.Key = Guid.newGuid();
+                this.filterImageParameter.IsFirstTry = true;
+                this.filterImageParameter.Extension = this.FileExtension;
+                this.filterImageParameter.UploadMode = "Block";
+                this.filterImageParameter.FileSize = file.size;
+                this.filterImageParameter.Tenant = SessionLocator.Tenant;
+                this.UploadSuccessLabel = true;
+                this.ArrayBufferToBase64(file, this);
+            }
+        }
+    }
+
+    ArrayBufferToBase64(file: any, viewmodel: any) {
+        var reader: FileReader = new FileReader();
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            var binary = '';
+            var bytes = new Uint8Array(ResultAsArray(e));
+            var len = bytes.byteLength;
+            for (var i = 0; i < len; i++) {
+                binary += String.fromCharCode(bytes[i]);
+            }
+            viewmodel.filterImageParameter.Base64String = window.btoa(binary);
+            viewmodel.IncreaseProgressBar(100);
+        };
+
+        reader.onerror = function (e) {
+            console.log(e);
+        };
+        reader.readAsArrayBuffer(file);
+    }
+
+    IncreaseProgressBar(ProgressBarValue: number) {
+        debugger;
+        var elem = document.getElementById("myBar") as HTMLProgressElement;
+        if (ProgressBarValue == 100) {
+            elem.value = ProgressBarValue;
+            this.ProgressBarPercentText = ProgressBarValue.toString() + ' %';
+            elem.dataset['label'] = this.ProgressBarPercentText;
+        }
+        else {
+            elem.value = ProgressBarValue;
+            this.ProgressBarPercentText = ProgressBarValue.toFixed(2).toString() + ' %';
+            elem.dataset['label'] = this.ProgressBarPercentText;
+        }
+
+    }
+
+    CancelButtonClicked() {
+        this.CurrentSession.CloseCurrentWindow();
+    }
+
+    @ViewChild('myInput')
+    myInputVariable: ElementRef;
+    DeleteFileButtonClicked() {
+        this.myInputVariable.nativeElement.value = "";
+        this.filterImageParameter = null;
+        this.IsShowProgressBar = false;
+        this.UploadButtonIsEnabled = true;
+        this.FileName = "";
+        this.ProgressBarPercentText = "";
+        //this.ErrorsResultList.Clear();
+        this.UploadSuccessLabel = false;
+    }
 }
 
 
--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationOthers/ModuleDeclarations.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationOthers/ModuleDeclarations.ts
@@ -12,6 +12,7 @@ import { SupplierInvoiceSelectionComponent } from './Components/DeclarationPayme
 import { PointersFromInvoicesSelectionComponent } from './Components/Documents/PointersFromInvoicesSelectionComponent';
 import { DeclarationQueryComponent } from './Components/DeclarationQueryComponent';
 import { DeclarationCancellationComponent } from './Components/DeclarationCancellation/DeclarationCancellationComponent';
+import { LoadExcelSupplierInvoicesComponent } from './Components/LoadExcelSupplierInvoice/LoadExcelSupplierInvoicesComponent';
 import { ExportDeclarationClosingDataComponent } from './Components/CloseDeclaration/ExportDeclarationClosingDataComponent';
 import { DeclarationPaymentExportComponent } from './Components/DeclarationPayment/DeclarationPaymentExportComponent';
 
@@ -32,6 +33,7 @@ export const Components =
         DeclarationQueryComponent,
         SendManifestComponent,
         DeclarationCancellationComponent,
+        LoadExcelSupplierInvoicesComponent,
         ExportDeclarationClosingDataComponent,
     ];
 
@@ -54,6 +56,7 @@ export class ModuleDeclarations {
             case "DeclarationQueryComponent": { myResult = DeclarationQueryComponent; break; }
             case "SendManifestComponent": { myResult = SendManifestComponent; break; }
             case "DeclarationCancellationComponent": { myResult = DeclarationCancellationComponent; break; }
+            case "LoadExcelSupplierInvoicesComponent": { myResult = LoadExcelSupplierInvoicesComponent; break; }
             case "ExportDeclarationClosingDataComponent": { myResult = ExportDeclarationClosingDataComponent; break; }
 
         }
