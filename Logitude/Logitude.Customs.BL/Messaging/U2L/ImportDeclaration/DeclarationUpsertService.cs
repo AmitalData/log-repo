748ccbf93e800a8f76da9972f4797adabbf37d36
Izik Fridland <IzikFr@amital.co.il>
2022-05-24 16:03:16 +0300

    #162009

--- Logitude/Logitude.Customs.BL/Messaging/U2L/ImportDeclaration/DeclarationUpsertService.cs
+++ Logitude/Logitude.Customs.BL/Messaging/U2L/ImportDeclaration/DeclarationUpsertService.cs
@@ -1387,7 +1387,7 @@ namespace Logitude.Customs.BL.Messaging.U2L.ImportDeclaration
                     AppendLogLine("Customer Card does not exist for Amital Customer Code " + amitalCustomerCode);
                     MyGenericResponseObj.Message = "Customer Card does not exist for Amital Customer Code " + amitalCustomerCode;
                     
-                    if(!CustomsSettingQueryService.GetSettingByTenant(ResolvedTenant()).IsConnectedToUniFreight)
+                    if(CustomsSettingQueryService.GetSettingByTenant(ResolvedTenant()).IsConnectedToUniFreight)
                         return null;
 
                     AppendLogLine("Open A new Card in the same Transaction Scope ");
