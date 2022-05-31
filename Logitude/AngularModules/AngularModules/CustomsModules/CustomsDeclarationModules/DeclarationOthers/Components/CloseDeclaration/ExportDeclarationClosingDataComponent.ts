acdd0860f6b3bcc7e319abcdcae3e91f4228a87b
Atara Oster <Atara@amital.co.il>
2022-05-31 17:08:19 +0300

    #162334

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationOthers/Components/CloseDeclaration/ExportDeclarationClosingDataComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationOthers/Components/CloseDeclaration/ExportDeclarationClosingDataComponent.ts
@@ -277,7 +277,7 @@ export class ExportDeclarationClosingDataComponent extends BaseComponent {
             });
 
         this.DeclarationService.PostSendDeclarationClosingAmendment(searchParams).subscribe((response: ServiceResponse) => {
-
+            
             if (!AppTool.IsNullOrEmpty(response) && !AppTool.IsNullOrEmpty(response.Result) && !AppTool.IsNullOrEmpty(response.Result.UserMessage) && response.Result.HasException) {
                //this.ValidationErrors.push(response.Result.UserMessage);
                 //this.FillValidationErrors("Errors");
@@ -289,15 +289,22 @@ export class ExportDeclarationClosingDataComponent extends BaseComponent {
                 this.CurrentSession.CurrentEditComponent.PreSelectedTabCode = "DEGC";
                 this.CurrentSession.CurrentEditComponent.SetSelectedTab();
                 this.CurrentSession.CurrentEditComponent.ReloadEntityPM();
+                if (!response.Result.HasException) {
+                    var tab = this.CurrentSession.CurrentEditComponent.TabsItemsSource.filter(d => d.Code == "CloD")[0];
+                    tab.IsDisabled = false;
+                }
+
             }
             //if reject
             else {
                 this.CurrentSession.CurrentEditComponent.PreSelectedTabCode = "CloD";
                 this.CurrentSession.CurrentEditComponent.SetSelectedTab();
                 this.CurrentSession.CurrentEditComponent.ReloadEntityPM();
+                var tab = this.CurrentSession.CurrentEditComponent.TabsItemsSource.filter(d => d.Code == "CloD")[0];
+                tab.IsDisabled = false;
             }
 
-
+           
             SessionLocator.SelectedSession.CloseCurrentWindow();
         });
 
