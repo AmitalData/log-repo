7855936c0b675a5bff84782cfed257ddcc144e08
Shoshana Greenwald <shoshana@amital.co.il>
2022-06-02 09:35:29 +0300

    #162608

---
+++ Logitude/Logitude.Customs.Def/EntityPMs/ItemGovernmentProcedureTypePM.cs
@@ -0,0 +1,19 @@
+﻿using Logitude.Customs.Def.Contracts;
+using Logitude.Customs.Def.Validators;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logitude.Customs.Def.EntityPMs
+{
+    public partial class ItemGovernmentProcedureTypePM : IIIGClosedTable  
+    {
+        [CustomValidation(typeof(CustomsValidationClass), "ValidateClass")]
+        [DataMember]
+        public string LeadingDocumentTypeID { get; set; }
+    }
+}
--- Logitude/Logitude.CustomsMessaging/ResponseServices/SYSTBL_NG_9001_MSG_SystemTablesResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/SYSTBL_NG_9001_MSG_SystemTablesResponseService.cs
@@ -859,6 +859,41 @@ ID List :
                         break;
 
                     }
+                case "1422":
+                case "ItemGovernmentProcedureType":
+                    {
+
+                        var extList = new List<SYSTBL_NG_9001_MSG_SystemTablesResponseTableDataExt>();
+                        Logitude.CustomsMessaging.Helpers.ClosedTable.
+                                                    ManipulateCustomResponse.
+                                                DataSetToTableData(customResponse,
+                                                (newResponseTableData, dr) =>
+                                                {
+                                                    var newExt =
+                                                        SYSTBL_NG_9001_MSG_SystemTablesResponseTableDataExt.CreateNew(newResponseTableData);
+                                                    newExt.MyItemGovernmentProcedureType = new Helpers.ClosedTable.ItemGovernmentProcedureType();
+                                                    if (!writeHighlight)
+                                                    {
+                                                        LogMessagingUtil.Instance.Append(
+                       @"1422:ItemGovernmentProcedureType:Calc=
+ if (dr[""LeadingDocumentTypeID""].ToString() != null)
+       newExt.MyIncotemrsFileValidation.LeadDocumentTypeName = dr[""LeadingDocumentTypeID""].ToString();");
+
+                                                        writeHighlight = true;
+                                                    }
+                                                    
+                                                    if (dr["LeadingDocumentTypeID"].ToString() != null)
+                                                    {
+                                                        LogMessagingUtil.Instance.Append(newResponseTableData.id + ",");
+                                                        newExt.MyIncotemrsFileValidation.LeadDocumentTypeName = dr["LeadingDocumentTypeID"].ToString(); ;
+                                                    }
+
+                                                    extList.Add(newExt);
+                                                });
+                        return extList;
+                        break;
+
+                    }
                 default:
                     break;
             }
