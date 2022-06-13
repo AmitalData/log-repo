c1097aebb51e863f0c9bad1765ce3118d672aff0
Raghad Sous <raghad@logitudeworld.com>
2022-06-13 22:58:09 +0300

    fix Bi

--- Logitude/AngularModules/CypressTesting/Base/cypress/commands/Elements.ts
+++ Logitude/AngularModules/CypressTesting/Base/cypress/commands/Elements.ts
@@ -28,7 +28,7 @@ declare global {
             SelectDropDownListItem(Selector: string, contain: string): Chainable<Element>
             SelectDropDownListItem2(Selector: string, contain: string): Chainable<Element>
             SelectDropDownListItemNumber(Selector: string, number: number): Chainable<Element>
-            SelectComboDropDownListItem(Selector: string, contain: string): Chainable<Element>
+            SelectComboDropDownListItem(Selector: string, contain: string, index: number): Chainable<Element>
 
         }
     }
@@ -37,8 +37,8 @@ Cypress.Commands.add("SelectDropDownListItem", (Selector: string, contain: strin
     cy.get(Selector).find(BaseSelectors.DownArrow).click()
     cy.get(BaseSelectors.DropDownList).find(BaseSelectors.DropDownListItem).contains(contain).click()
 })
-Cypress.Commands.add("SelectComboDropDownListItem", (Selector: string, contain: string) => {
-    cy.get(Selector).find(BaseSelectors.DownArrow).click()
+Cypress.Commands.add("SelectComboDropDownListItem", (Selector: string, contain: string, index: number) => {
+    cy.get(Selector).find(BaseSelectors.DownArrow).eq(index).click()
     cy.get(BaseSelectors.ComboList).find(BaseSelectors.ComboBoxItem).contains(contain).click()
 })
 Cypress.Commands.add("SelectDropDownListItem2", (Selector: string, contain: string) => {
