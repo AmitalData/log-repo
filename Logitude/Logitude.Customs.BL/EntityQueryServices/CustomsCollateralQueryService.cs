67684c1e9990fc0e3eb8610a85e205bceb7d28d2
Izik Fridland <IzikFr@amital.co.il>
2022-06-15 13:47:02 +0300

    #161959 #156336 #58759

--- Logitude/Logitude.Customs.BL/EntityQueryServices/CustomsCollateralQueryService.cs
+++ Logitude/Logitude.Customs.BL/EntityQueryServices/CustomsCollateralQueryService.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Logitude.Customs.BL.EntityUpdateServices;
 using Simplog.Server.Infrastructure;
+using Logitude.Customs.Data.Repsitories;
 
 namespace Logitude.Customs.BL.EntityQueryServices
 {
@@ -74,24 +75,33 @@ namespace Logitude.Customs.BL.EntityQueryServices
 
         public List<CustomsCollateral> UpdateMulti(string[] ids, string declarationId, bool selectAll, CustomsCollateralsAnswerPM customsCollateralsAnswerPM, int tenant)
         {
-            CustomsCollateralUpdateService updateService = new CustomsCollateralUpdateService(context, new Dictionary<string, Simplog.Server.Infrastructure.IContext>(), tenant); 
-
+            CustomsCollateralUpdateService updateService = new CustomsCollateralUpdateService(context, new Dictionary<string, IContext>(), tenant);
+            var ccaRepo = new CustomsCollateralsAnswerRepository(context);
             List<CustomsCollateral> customsCollateralList =
-                selectAll ? 
-                    repository.GetDeclarationCollateralsList(declarationId, tenant).FindAll(x => !ids.Contains(x.Id) && x.IsClosed == false) : 
+                selectAll ?
+                    repository.GetDeclarationCollateralsList(declarationId, tenant).FindAll(x => !ids.Contains(x.Id) && x.IsClosed == false) :
                     repository.GetDeclarationCollateralsList(ids);
 
+            AddCustomsCollateralsAnswerToCustomsCollaterals(customsCollateralsAnswerPM, tenant, updateService, ccaRepo, customsCollateralList);
+
+            return customsCollateralList;
+        }
+
+        private void AddCustomsCollateralsAnswerToCustomsCollaterals(CustomsCollateralsAnswerPM customsCollateralsAnswerPM, int tenant, CustomsCollateralUpdateService updateService, CustomsCollateralsAnswerRepository ccaRepo, List<CustomsCollateral> customsCollateralList)
+        {
             customsCollateralList.ForEach(customsCollateralItem =>
             {
-                CustomsCollateralPM customsCollateralPM = GetEntityPM(customsCollateralItem);
+                CustomsCollateralPM customsCollateralPM = GetEntityPM(customsCollateralItem, true, new CustomsCollateralKeys() { Id = customsCollateralItem.Id });
+                customsCollateralsAnswerPM.CustomsCollateralId = customsCollateralPM.Id;
+                var ccaList = customsCollateralPM.CustomsCollateralsAnswers;
+                customsCollateralsAnswerPM.LineNumber = ccaList.Count == 0 ? 1 : ccaList.Max(m => m.LineNumber) + 1; 
+
                 customsCollateralsAnswerPM.ChangeSetOp = ChangeSetOperation.Insert;
-                customsCollateralPM.CustomsCollateralsAnswers.Add(customsCollateralsAnswerPM);                
+                customsCollateralPM.CustomsCollateralsAnswers.Add(customsCollateralsAnswerPM);
 
                 customsCollateralPM.ChangeSetOp = ChangeSetOperation.Update;
                 updateService.Update(customsCollateralPM, true);
             });
-
-            return customsCollateralList;
         }
     }
 }
