28707a289d1a8eb83c0503b142d1e987b23e2181
Izik Fridland <IzikFr@amital.co.il>
2022-06-20 16:36:01 +0300

    #156336 #58759

--- Logitude/Logitude.Customs.Data/EntityListQueryServices/CustomsCollateralListQueryService.cs
+++ Logitude/Logitude.Customs.Data/EntityListQueryServices/CustomsCollateralListQueryService.cs
@@ -85,7 +85,7 @@ namespace Logitude.Customs.Data.EntityListQueryServices
                     from collateral in iQueryable.Include("Declaration")
                     let d = context.Declarations.Where(x => x.Id == declarationId).Select(x => x.AmendmentOriginalDeclartation).FirstOrDefault()
 
-                    where (collateral.DeclarationId == declarationId || collateral.Declaration.Id == d) && collateral.DeclarationId != null
+                    where (collateral.DeclarationId == declarationId || collateral.Declaration.Id == d || collateral.Declaration.AmendmentOriginalDeclartation == d) && collateral.DeclarationId != null
 
                     select collateral
                  );
