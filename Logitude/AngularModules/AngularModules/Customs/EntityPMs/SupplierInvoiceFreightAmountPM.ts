30f6e8e3ef6df3b709d2baef8afa3bcfb4a08352
Izik Fridland <IzikFr@amital.co.il>
2022-06-21 10:46:45 +0300

    Revert "#156330 not work"
    This reverts commit 77a1ad2e922ab9b121a7c428858aefe0adb62417.

--- Logitude/AngularModules/AngularModules/Customs/EntityPMs/SupplierInvoiceFreightAmountPM.ts
+++ Logitude/AngularModules/AngularModules/Customs/EntityPMs/SupplierInvoiceFreightAmountPM.ts
@@ -57,11 +57,6 @@ export class SupplierInvoiceFreightAmountPM {
     public set CurrencyTypeName(newValue: string) { if (this.currencyTypeName != newValue) { this.currencyTypeName = newValue; this.MarkAsDirty("CurrencyTypeName"); } }
        
 	 
-    private id: string;
-    public get Id() { return this.id; }
-    public set Id(newValue: string) { if (this.id != newValue) { this.id = newValue; this.MarkAsDirty("Id"); } }
-       
-	 
 
     public OldEntityPM: SupplierInvoiceFreightAmountPM;
 	
