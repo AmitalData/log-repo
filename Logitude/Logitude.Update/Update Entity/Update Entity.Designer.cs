e2e24ac924d9dc25b52484efb5e86f15d81bc80d
Wesam Amarneh <wesam@logitudeworld.com>
2022-06-22 12:45:14 +0000

    Form to update A field and run automation

---
+++ Logitude/Logitude.Update/Update Entity/Update Entity.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Logitude.Update.Update_Entity
+{
+    partial class Update_Entity
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UpdateBtn = new System.Windows.Forms.Button();
+            this.FieldCodeText = new System.Windows.Forms.TextBox();
+            this.TenantTxt = new System.Windows.Forms.TextBox();
+            this.FieldCodeLbl = new System.Windows.Forms.Label();
+            this.TenantLbl = new System.Windows.Forms.Label();
+            this.ResultLbl = new System.Windows.Forms.Label();
+            this.NewFieldValueLbl = new System.Windows.Forms.Label();
+            this.NewFieldValueTxt = new System.Windows.Forms.TextBox();
+            this.SourceConnectionLbl = new System.Windows.Forms.Label();
+            this.SourceConnectionTxt = new System.Windows.Forms.TextBox();
+            this.AdditionalScriptLbl = new System.Windows.Forms.Label();
+            this.additionalScriptTxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // UpdateBtn
+            // 
+            this.UpdateBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UpdateBtn.Location = new System.Drawing.Point(63, 254);
+            this.UpdateBtn.Name = "UpdateBtn";
+            this.UpdateBtn.Size = new System.Drawing.Size(134, 37);
+            this.UpdateBtn.TabIndex = 0;
+            this.UpdateBtn.Text = "Update";
+            this.UpdateBtn.UseVisualStyleBackColor = true;
+            this.UpdateBtn.Click += new System.EventHandler(this.UpdateBtn_Click);
+            // 
+            // FieldCodeText
+            // 
+            this.FieldCodeText.Location = new System.Drawing.Point(221, 108);
+            this.FieldCodeText.Name = "FieldCodeText";
+            this.FieldCodeText.Size = new System.Drawing.Size(213, 22);
+            this.FieldCodeText.TabIndex = 7;
+            // 
+            // TenantTxt
+            // 
+            this.TenantTxt.Location = new System.Drawing.Point(221, 68);
+            this.TenantTxt.Name = "TenantTxt";
+            this.TenantTxt.Size = new System.Drawing.Size(213, 22);
+            this.TenantTxt.TabIndex = 6;
+            // 
+            // FieldCodeLbl
+            // 
+            this.FieldCodeLbl.AutoSize = true;
+            this.FieldCodeLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FieldCodeLbl.Location = new System.Drawing.Point(22, 108);
+            this.FieldCodeLbl.Name = "FieldCodeLbl";
+            this.FieldCodeLbl.Size = new System.Drawing.Size(85, 17);
+            this.FieldCodeLbl.TabIndex = 5;
+            this.FieldCodeLbl.Text = "Field Code";
+            // 
+            // TenantLbl
+            // 
+            this.TenantLbl.AutoSize = true;
+            this.TenantLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TenantLbl.Location = new System.Drawing.Point(22, 68);
+            this.TenantLbl.Name = "TenantLbl";
+            this.TenantLbl.Size = new System.Drawing.Size(59, 17);
+            this.TenantLbl.TabIndex = 4;
+            this.TenantLbl.Text = "Tenant";
+            // 
+            // ResultLbl
+            // 
+            this.ResultLbl.AutoSize = true;
+            this.ResultLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultLbl.Location = new System.Drawing.Point(248, 264);
+            this.ResultLbl.Name = "ResultLbl";
+            this.ResultLbl.Size = new System.Drawing.Size(54, 17);
+            this.ResultLbl.TabIndex = 8;
+            this.ResultLbl.Text = "Result";
+            // 
+            // NewFieldValueLbl
+            // 
+            this.NewFieldValueLbl.AutoSize = true;
+            this.NewFieldValueLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NewFieldValueLbl.Location = new System.Drawing.Point(22, 149);
+            this.NewFieldValueLbl.Name = "NewFieldValueLbl";
+            this.NewFieldValueLbl.Size = new System.Drawing.Size(124, 17);
+            this.NewFieldValueLbl.TabIndex = 9;
+            this.NewFieldValueLbl.Text = "New Field Value";
+            // 
+            // NewFieldValueTxt
+            // 
+            this.NewFieldValueTxt.Location = new System.Drawing.Point(221, 149);
+            this.NewFieldValueTxt.Name = "NewFieldValueTxt";
+            this.NewFieldValueTxt.Size = new System.Drawing.Size(213, 22);
+            this.NewFieldValueTxt.TabIndex = 10;
+            // 
+            // SourceConnectionLbl
+            // 
+            this.SourceConnectionLbl.AutoSize = true;
+            this.SourceConnectionLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SourceConnectionLbl.Location = new System.Drawing.Point(22, 24);
+            this.SourceConnectionLbl.Name = "SourceConnectionLbl";
+            this.SourceConnectionLbl.Size = new System.Drawing.Size(145, 17);
+            this.SourceConnectionLbl.TabIndex = 11;
+            this.SourceConnectionLbl.Text = "Source Connection";
+            // 
+            // SourceConnectionTxt
+            // 
+            this.SourceConnectionTxt.Location = new System.Drawing.Point(221, 24);
+            this.SourceConnectionTxt.Name = "SourceConnectionTxt";
+            this.SourceConnectionTxt.Size = new System.Drawing.Size(213, 22);
+            this.SourceConnectionTxt.TabIndex = 12;
+            // 
+            // AdditionalScriptLbl
+            // 
+            this.AdditionalScriptLbl.AutoSize = true;
+            this.AdditionalScriptLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AdditionalScriptLbl.Location = new System.Drawing.Point(22, 193);
+            this.AdditionalScriptLbl.Name = "AdditionalScriptLbl";
+            this.AdditionalScriptLbl.Size = new System.Drawing.Size(159, 21);
+            this.AdditionalScriptLbl.TabIndex = 13;
+            this.AdditionalScriptLbl.Text = "Additional Script";
+            // 
+            // additionalScriptTxt
+            // 
+            this.additionalScriptTxt.Location = new System.Drawing.Point(221, 190);
+            this.additionalScriptTxt.Name = "additionalScriptTxt";
+            this.additionalScriptTxt.Size = new System.Drawing.Size(357, 22);
+            this.additionalScriptTxt.TabIndex = 14;
+            this.additionalScriptTxt.Text = "CreateDate >\'2022-01-01\' and CreateDate <\'2022-06-01\'";
+            // 
+            // Update_Entity
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 314);
+            this.Controls.Add(this.additionalScriptTxt);
+            this.Controls.Add(this.AdditionalScriptLbl);
+            this.Controls.Add(this.SourceConnectionTxt);
+            this.Controls.Add(this.SourceConnectionLbl);
+            this.Controls.Add(this.NewFieldValueTxt);
+            this.Controls.Add(this.NewFieldValueLbl);
+            this.Controls.Add(this.ResultLbl);
+            this.Controls.Add(this.FieldCodeText);
+            this.Controls.Add(this.TenantTxt);
+            this.Controls.Add(this.FieldCodeLbl);
+            this.Controls.Add(this.TenantLbl);
+            this.Controls.Add(this.UpdateBtn);
+            this.Name = "Update_Entity";
+            this.Text = "Update_Entity";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button UpdateBtn;
+        private System.Windows.Forms.TextBox FieldCodeText;
+        private System.Windows.Forms.TextBox TenantTxt;
+        private System.Windows.Forms.Label FieldCodeLbl;
+        private System.Windows.Forms.Label TenantLbl;
+        private System.Windows.Forms.Label ResultLbl;
+        private System.Windows.Forms.Label NewFieldValueLbl;
+        private System.Windows.Forms.TextBox NewFieldValueTxt;
+        private System.Windows.Forms.Label SourceConnectionLbl;
+        private System.Windows.Forms.TextBox SourceConnectionTxt;
+        private System.Windows.Forms.Label AdditionalScriptLbl;
+        private System.Windows.Forms.TextBox additionalScriptTxt;
+    }
+}
