963760f7ed6ed4f52fd4e1d1ce0704399a6f0b69
Maheera Jazi <Maheera@logitudeworld.com>
2022-06-26 12:58:22 +0300

    #163625

--- Logitude/Logitude.BL/ShipmentsModel/CustomFilters/ShipmentCustomFilter.cs
+++ Logitude/Logitude.BL/ShipmentsModel/CustomFilters/ShipmentCustomFilter.cs
@@ -12,6 +12,7 @@ using System.Data.Entity.Core.Objects;
 using Simplog.Data.InfrastructureModel.Repositories;
 using Simplog.Data.InfrastructureModel.EntityPOCOs;
 using Logitude.BL.ShipmentsModel.EntityLists;
+using System.Data.Entity;
 
 namespace Logitude.BL.ShipmentsModel.CustomFilters
 {
@@ -36,6 +37,38 @@ namespace Logitude.BL.ShipmentsModel.CustomFilters
             {
                 if (item.IsCustom)
                 {
+                    if (item.FieldName == "DigitalPortalSearchFields")
+                    {
+                        string digitalPortalSearchFields = item.FieldValue as string;
+                        digitalPortalSearchFields = digitalPortalSearchFields.ToLower();
+                        queryableData = queryableData.AsNoTracking().Where(d =>
+                         d.ShipperReference1.Contains(digitalPortalSearchFields)
+                      || d.ShipperReference2.Contains(digitalPortalSearchFields)
+                      || d.ConsigneeReference1.Contains(digitalPortalSearchFields)
+                      || d.ConsigneeReference2.Contains(digitalPortalSearchFields)
+                      || d.ShipmentNumber.Contains(digitalPortalSearchFields)
+                      || d.MainCarriageCarrierNumber.Contains(digitalPortalSearchFields)
+                      || d.House.Contains(digitalPortalSearchFields)
+                      || d.Master.Contains(digitalPortalSearchFields)
+                      || d.AgentName.ToLower().StartsWith(digitalPortalSearchFields)
+                      || d.CustomAgentImportName.ToLower().StartsWith(digitalPortalSearchFields)
+                      || d.ConsigneeName.ToLower().StartsWith(digitalPortalSearchFields)
+                      || d.ShipperName.ToLower().StartsWith(digitalPortalSearchFields)
+                      || d.CustomerName.ToLower().StartsWith(digitalPortalSearchFields)
+                      || d.Notify1Name.ToLower().StartsWith(digitalPortalSearchFields)
+                      || d.Notify2Name.ToLower().StartsWith(digitalPortalSearchFields)
+                      || d.CustomAgentExportName.ToLower().StartsWith(digitalPortalSearchFields)
+                      || d.ShipperNotExporterName.ToLower().StartsWith(digitalPortalSearchFields)
+                      || d.ConsigneeNotImporterName.ToLower().StartsWith(digitalPortalSearchFields)
+                      || d.FreightForwarderName.ToLower().StartsWith(digitalPortalSearchFields)
+                      || d.ReleasingAgentName.ToLower().StartsWith(digitalPortalSearchFields)
+                      || d.ConsolidatorName.ToLower().StartsWith(digitalPortalSearchFields)
+                      || d.FromPortName.ToLower().StartsWith(digitalPortalSearchFields)
+                      || d.ToPortName.ToLower().StartsWith(digitalPortalSearchFields)
+                      || d.MainCarriageCarrierName.ToLower().StartsWith(digitalPortalSearchFields)
+                       );
+                    }
+
                     if (item.FieldName == "DigitalQuickSearch")
                     {
                         queryableData = DigitalCustomFilter.ApplyDigitalQuickSearchFilter(item, queryableData);
