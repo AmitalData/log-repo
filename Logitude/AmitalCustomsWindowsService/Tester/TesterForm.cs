5293298a28e5f21b6ee277cec03040bd3641a9be
Itzik Moshelian <itzik@amital.co.il>
2022-06-29 14:54:23 +0300

    #163903

--- Logitude/AmitalCustomsWindowsService/Tester/TesterForm.cs
+++ Logitude/AmitalCustomsWindowsService/Tester/TesterForm.cs
@@ -1154,5 +1154,51 @@ namespace AmitalCustomsWindowsService.Tester
 
 
         }
+
+        private void testPooledPublishToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+
+
+               
+
+
+                var message = Encoding.UTF8.GetBytes(Get("CorrelationId", "ExternalId", "body"));
+               
+
+                string InterfaceTypeCode = "ucbud2lt";
+                String rabbitMQCode = RabbitmqHelper.GetRabbitMQCode(1);
+                rabbitMQCode = "itziktest_" + rabbitMQCode;
+
+                var rabbitPublishService = new RabbitPublishService();
+                rabbitPublishService.Publish(message, "communicationLogId", InterfaceTypeCode, rabbitMQCode, 5);
+
+
+
+
+
+        }
+        public string Get(string CorrelationId, string ExternalId, string body)
+        {
+            var xml =
+@"
+<ns0:ESBResponse xmlns:ns0=""http://MalamTeam.Inf.ESB.Schemas.ESBResponse"">
+  <ns1:ResponseHeader xmlns:ns1=""http://MalamTeam.Inf.ESB.Schemas.ResponseHeader"">
+    <ns1:CorrelationId>@CorrelationId@</ns1:CorrelationId>
+    <ns1:Status>Success</ns1:Status>
+    <ns1:ErrorDescription></ns1:ErrorDescription>
+    <ns1:ErrorCode>None</ns1:ErrorCode>
+    <ns1:ExternalId>@ExternalId@</ns1:ExternalId>
+  </ns1:ResponseHeader>
+  <Body>
+@Body@
+  </Body>
+</ns0:ESBResponse>";
+            return xml
+                .Replace("@CorrelationId@", CorrelationId)
+                .Replace("@ExternalId@", ExternalId)
+                .Replace("@Body@", body);
+
+        }
     }
 }
---
+++ Logitude/Logitude.CustomsMessaging/RabbitMQ/PooledRabbitMQPolicy.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Extensions.ObjectPool;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logitude.CustomsMessaging.RabbitMQ
+{
+    public class PooledRabbitMQPolicy : IPooledObjectPolicy<IModel>
+    {
+        //private readonly RabbitMQOptions _options;
+        static int _Counter = 0;
+        private IConnection _connection;
+        //private readonly string _HostName;
+        //private readonly string _UserName;
+        //private readonly string _Password;
+        //private IEnvironmentSettingService _environmentSettingService;
+
+        public PooledRabbitMQPolicy(/*IOptions<RabbitMQOptions>  options*/)
+        {
+            //_environmentSettingService = serviceProvider.GetRequiredService<IEnvironmentSettingService>();
+            _connection = GetConnection();
+
+
+        }
+        public static int GetCounter()
+        {
+            return _Counter;
+        }
+        private IConnection GetConnection()
+        {
+            
+            var factory = RabbitmqHelper.GetConnectionFactory();
+            return factory.CreateConnection();
+        }
+
+        public IModel Create()
+        {
+            _Counter++;
+            return _connection.CreateModel();
+        }
+
+        public bool Return(IModel obj)
+        {
+            if (obj.IsOpen)
+            {
+                return true;
+            }
+            else
+            {
+                obj?.Dispose();
+                return false;
+            }
+        }
+    }
+
+
+    public class PooledRabbitMQPublisher //: IRabbitMQPublisher
+    {
+
+        static PooledRabbitMQPublisher _Instance;
+
+        public static PooledRabbitMQPublisher Instance
+        {
+            get { return PooledRabbitMQPublisher._Instance = PooledRabbitMQPublisher._Instance ?? new PooledRabbitMQPublisher(); }
+
+        }
+
+
+        private readonly DefaultObjectPool<IModel> _objectPool;
+
+        PooledRabbitMQPublisher(/*IPooledObjectPolicy<IModel> objectPolicy*/)
+        {
+            _objectPool = new DefaultObjectPool<IModel>(new PooledRabbitMQPolicy(),
+                Environment.ProcessorCount * 2
+                );
+        }
+         
+
+        public IModel Get()
+        {
+            var channel = _objectPool.Get();
+            return channel;
+        }
+
+        public void Return(IModel channel)
+        {
+            _objectPool.Return(channel);
+        }
+
+
+        
+
+    }
+}
