b43e296fd36a476926d5699a1e443e48a819a540
Abed <abed@logitudeworld.com>
2022-07-06 14:38:00 +0300

    163702

--- Logitude/Logitude.BL/ShipmentsModel/Tools/ExternalService/ShipmentChildEntitiesCustomFieldServices.cs
+++ Logitude/Logitude.BL/ShipmentsModel/Tools/ExternalService/ShipmentChildEntitiesCustomFieldServices.cs
@@ -23,12 +23,17 @@ namespace Logitude.BL.ShipmentsModel.Tools.ExternalService
 
         public void Save()
         {
-            var tasks = BuildTasks();
-            foreach (Task task in tasks)
-            {
-                task.Start();
-            }
-            Task.WaitAll(tasks);
+            Update("ShipmentPackage", ShipmentPackages.Cast<object>().ToList());
+            Update("ShipmentReceivable", ShipmentReceivables.Cast<object>().ToList());
+            Update("ShipmentPayable", ShipmentPayables.Cast<object>().ToList());
+            Update("ShipmentPickUpDelivery", ShipmentPickUps.Cast<object>().ToList());
+            Update("ShipmentPickUpDelivery", ShipmentDeliveries.Cast<object>().ToList());
+            //var tasks = BuildTasks();
+            //foreach (Task task in tasks)
+            //{
+            //    task.Start();
+            //}
+            //Task.WaitAll(tasks);
         }
 
         public Task[] BuildTasks()
