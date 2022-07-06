52811c9f3d202c6fc3c03b4daaad12bd93423447
Samar Abu Eid <Samar@logitudeworld.com>
2022-07-06 07:54:49 +0000

    #164180

--- Logitude/Logitude.BL/CommonDataModel/EntityQueries/FeatureQuery.cs
+++ Logitude/Logitude.BL/CommonDataModel/EntityQueries/FeatureQuery.cs
@@ -447,8 +447,8 @@ namespace Logitude.BL.CommonDataModel.EntityQueries
                         {
                             if (myParentItem != null)
                             {
-                                //allRoleFeatures.Remove(myParentItem);
-                                myParentItem.FeatureAccessLevelCode = "NO";
+                                allRoleFeatures.Remove(myParentItem);
+                                //myParentItem.FeatureAccessLevelCode = "NO";
                             }
                         }
 
