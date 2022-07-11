4dfe9e4a08e99d8eb2855ad67cc338b47b2905e3
Frida Lok <frida@amital.co.il>
2022-07-11 17:07:07 +0300

    revert

--- Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/TenantsUpdateClass.cs
+++ Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/TenantsUpdateClass.cs
@@ -776,7 +776,7 @@ namespace WebFreight.Web.MetaDataUpdate
            
             updateClass.FillPointerLevel();
 
-            updateClass.FillStorageStatusTable();
+           
         }
 
         private static void UpdateTenantData(int tenant)
--- Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/UpdateClasses/CustomUpdate.cs
+++ Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/UpdateClasses/CustomUpdate.cs
@@ -18536,19 +18536,6 @@ namespace WebFreight.Web.MetaDataUpdate.UpdateClasses
         }
 
 
-        public void FillStorageStatusTable()
-        {
-
-            StorageStatusTableRepository storageStatusTableRepository = new StorageStatusTableRepository(0);
-            Dictionary<string, StorageStatusTable> TenantStorageStatusTable = storageStatusTableRepository.GetAll().ToDictionary(d => d.Code, a => a);
-
-            AddClosedTables.AddStorageStatusTable(new StorageStatusTable() { Code = "Open", Name = "Open" }, storageStatusTableRepository);
-            AddClosedTables.AddStorageStatusTable(new StorageStatusTable() { Code = "Close", Name = "Close" }, storageStatusTableRepository);
-            AddClosedTables.AddStorageStatusTable(new StorageStatusTable() { Code = "Cancel", Name = "Cancel" }, storageStatusTableRepository);
-
-            storageStatusTableRepository.SubmitChanges();
-
-        }
     }
 
 
