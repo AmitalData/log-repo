1c1e3e8dac2f5a55dc25914c8ed1e6e4d6804aa3
sohaib <sohaib@amital.co.il>
2022-07-13 15:07:53 +0300

    DELETE DEBUGGER

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsReferant/Components/FiltersMenu/DeclarationReferantDataFiltersMenuComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsReferant/Components/FiltersMenu/DeclarationReferantDataFiltersMenuComponent.ts
@@ -77,7 +77,6 @@ export class DeclarationReferantDataFiltersMenuComponent
 
 
     async SetFiltersMenu(args: any) {
-        debugger;
         this.OpenQueryThruWorkSpace = true;
         this.TransportFilters = new ApiQueryFilters();
         this.UserFilters = new ApiQueryFilters();
