ead39c2e1a5c26f001c6eb5b58902195c9e65281
Izik Fridland <IzikFr@amital.co.il>
2022-07-19 18:04:20 +0300

    #164023

--- Logitude/Logitude.Update/Sivug/CustomsBookImport.cs
+++ Logitude/Logitude.Update/Sivug/CustomsBookImport.cs
@@ -73,7 +73,7 @@ namespace Logitude.Update.Sivug
             {
                 //List<CustomsBook_CustomsItem> 
                  var   mehesCustomsItemRows = contx.CustomsBook_CustomsItem
-                    .Where(rec => rec.CustomsBookTypeID == 1)
+                    .Where(rec => rec.CustomsBookTypeID >= 1 && rec.CustomsBookTypeID <= 3)
                     .Select(rec => new //CustomsBook_CustomsItem()
                     {
                         ID = rec.ID,
