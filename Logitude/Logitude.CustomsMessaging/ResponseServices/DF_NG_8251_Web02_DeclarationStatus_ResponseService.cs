de57a77501b951abd38cbb38d2cadfcc3dc3d39c
Libby Hasid <libby@amital.co.il>
2022-07-21 10:24:57 +0300

    #163971

--- Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_8251_Web02_DeclarationStatus_ResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_8251_Web02_DeclarationStatus_ResponseService.cs
@@ -224,7 +224,7 @@ namespace Logitude.CustomsMessaging.ResponseServices
                                             }
                                         }
                                     }
-                                    if (declarationQuantity == cargoQuantity && declarationQuantity!=0)
+                                    if (declarationQuantity == cargoQuantity &&  declarationQuantity!=0)
                                     {
                                         availableStatus = "SMG";
                                     }
