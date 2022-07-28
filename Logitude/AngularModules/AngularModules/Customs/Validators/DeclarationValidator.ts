53ba86d9121c3e5b532a358f31c6d869c8d54f85
Libby Hasid <libby@amital.co.il>
2022-07-28 15:14:43 +0300

    #164860

--- Logitude/AngularModules/AngularModules/Customs/Validators/DeclarationValidator.ts
+++ Logitude/AngularModules/AngularModules/Customs/Validators/DeclarationValidator.ts
@@ -209,9 +209,9 @@ export class DeclarationValidator {
         var errorMessage: string = "";
 
         if (this._DeclarationPM != null) {
-           
-           
-            if(this._DeclarationPM.IsExportClosed&& this._DeclarationPM.DeclarationStatusTypeCode=="36"){
+
+
+            if (this._DeclarationPM.IsExportClosed && this._DeclarationPM.DeclarationStatusTypeCode == "36") {
                 errorMessage = "Customs.General.O.DeclarationStatClosed";
                 if (!AppTool.IsNullOrEmpty(errorMessage)) {
                     this.ValidationErrorMessageCodes.push(errorMessage);
@@ -230,8 +230,8 @@ export class DeclarationValidator {
                     this.ValidationErrorMessageCodes.push(errorMessage);
                 }
             }
-          
-           
+
+
         }
     }
 
@@ -312,11 +312,25 @@ export class DeclarationValidator {
         //Check if Declaration Paid and there waiting for constraint approval
         if (this._DeclarationPM != null) {
             if (this._DeclarationPM.DeclarationStatusTypeCode == "11") {
+
                 var errorMessage = "Customs.General.O.ConstraintsInProgress";
                 if (!AppTool.IsNullOrEmpty(errorMessage)) {
                     this.ValidationErrorMessageCodes.push(errorMessage);
                 }
             }
+            else {
+
+                if (this._DeclarationPM.DeclarationConstraints.find(dc => dc.ConstraintStatusCode == "5")) {
+                    var errorMessage = "Customs.General.O.HappinessConstraints";
+                    if (!AppTool.IsNullOrEmpty(errorMessage)) {
+                        this.ValidationErrorMessageCodes.push(errorMessage);
+                    }
+                }
+                
+            }
+
+
+
             //Check if there are constraints in progress
             //if (false)//yaron
             //{
@@ -425,7 +439,7 @@ export class DeclarationValidator {
     private CheckConsignmentPackages() {
         var errorMessage = "";
 
-        if (this._DeclarationPM.Consignments == null  || (this._DeclarationPM.Direction === 'E' &&  this._DeclarationPM.ExcludeConsignment)) {
+        if (this._DeclarationPM.Consignments == null || (this._DeclarationPM.Direction === 'E' && this._DeclarationPM.ExcludeConsignment)) {
             return;
         }
 
@@ -623,7 +637,7 @@ export class DeclarationValidator {
     }
 
     public Validate(entityPM: DeclarationPM) {
-
+        
         var result = [];
         this._DeclarationPM = entityPM;
         this.EmptyConsignmentPackageCheck();
--- Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/GeneratedUpdate/EntityUpdateClasses/CustomsGeneralUpdateClass.cs
+++ Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/GeneratedUpdate/EntityUpdateClasses/CustomsGeneralUpdateClass.cs
@@ -85,7 +85,7 @@ namespace WebFreight.Web.MetaDataUpdate.GeneratedUpdate.EntityUpdateClasses
 {
    public class CustomsGeneralUpdateClass
    {  		
-		public const string HashString = "b99ff04b9477bfdf75d285eb07829b97";
+		public const string HashString = "16fec2c50ce6f70cd3da20d896a43c01";
 	    public void AddObjectTable(Dictionary<string, ObjectTable> objectTables, Dictionary<string, TextCode> textCodes,ObjectTableRepository ObjectTableRepository,TextCodeRepository TextCodeRepository)
         {                     
             
@@ -125,7 +125,7 @@ namespace WebFreight.Web.MetaDataUpdate.GeneratedUpdate.EntityUpdateClasses
 			      				    ObjectTableTypeCode =  "MD",
 			      				    MaxNumberOfCustomFields =  0,
 			      				    DefaultText =  "CustomsGeneral",
-			      				    Code =  "ee76",
+			      				    Code =  "fbf3",
 			      				    Name =  "CustomsGeneral",
 			      				    GenerateDomainService =  false,
 			      				    ClientModuleName =  "Customs",
@@ -2095,6 +2095,8 @@ namespace WebFreight.Web.MetaDataUpdate.GeneratedUpdate.EntityUpdateClasses
  		   TextCode CustomsGeneralTextCode_CustomsGeneralODeclarationStatClosed = AddTextCodes.AddTextCode(new TextCodeDetails() { Code = "Customs.General.O.DeclarationStatClosed", DefaultText = "Declaration closed with closed status",LocalDefaultText = @"הצהרה נסגרה עם סטאטוס סגור", ObjectTableId = CustomsGeneralObjectTable.Id, Tenant = 0, TextCodeTypeCode = "O", IsSpellChecked = false }, TextCodeRepository, TextCodes);
 
  		   TextCode CustomsGeneralTextCode_CustomsGeneralORequiredFieldsOrAlert = AddTextCodes.AddTextCode(new TextCodeDetails() { Code = "Customs.General.O.RequiredFieldsOrAlert", DefaultText = "Required Fields Or Alert",LocalDefaultText = @"שדות חובה/התראה", ObjectTableId = CustomsGeneralObjectTable.Id, Tenant = 0, TextCodeTypeCode = "O", IsSpellChecked = false }, TextCodeRepository, TextCodes);
+
+ 		   TextCode CustomsGeneralTextCode_CustomsGeneralOHappinessConstraints = AddTextCodes.AddTextCode(new TextCodeDetails() { Code = "Customs.General.O.HappinessConstraints", DefaultText = "happiness constraint",LocalDefaultText = @"אילוץ אושר", ObjectTableId = CustomsGeneralObjectTable.Id, Tenant = 0, TextCodeTypeCode = "O", IsSpellChecked = false }, TextCodeRepository, TextCodes);
 
    
 	    
