8d01c23f639015fbf2db4c8bd05d0f7fc53b4f74
Libby Hasid <libby@amital.co.il>
2022-08-01 19:41:59 +0300

    #165477

--- Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_2757_MSG10004_ExportAmendmentDeclarationResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_2757_MSG10004_ExportAmendmentDeclarationResponseService.cs
@@ -440,11 +440,25 @@ namespace Logitude.CustomsMessaging.ResponseServices
                                                                                                                                                                     //var mySupplierInvoiceItemsTaxesModificationUpdateService = new SupplierInvoiceItemsTaxesModUpdateService(context, new Dictionary<string, IContext>(), requestParams.Tenant);
             var myDeclarationTaxUpdateService = new DeclarationTaxUpdateService(context, new Dictionary<string, IContext>(), tenant);
 
+            var mySupplierInvoiceItemsPriceUpdateService = new SupplierInvoiceItemsPriceUpdateService(context, new Dictionary<string, IContext>(), tenant); // moran 24.11.15 - Task 17424 
+            var mySupplierInvoiceItemsLevyUpdateService = new SupplierInvoiceItemsLevyUpdateService(context, new Dictionary<string, IContext>(), tenant); // moran 24.11.15 - Task 17424 
+            var mySupplierInvoiceItemsDescriptUpdateService = new SupplierInvoiceItemsDescriptUpdateService(context, new Dictionary<string, IContext>(), tenant); // moran 24.11.15 - Task 17424 
+            var mySupplierInvoiceItemsProdIdentUpdateService = new SupplierInvoiceItemsProdIdentUpdateService(context, new Dictionary<string, IContext>(), tenant); // moran 24.11.15 - Task 17424 
+            var mySupplierInvoiceItemsSerialNumUpdateService = new SupplierInvoiceItemsSerialNumUpdateService(context, new Dictionary<string, IContext>(), tenant); // moran 24.11.15 - Task 17424 
+            var mySuppInvoiceItemsAbachStatementUpdateService = new SuppInvoiceItemsAbachStatementUpdateService(context, new Dictionary<string, IContext>(), tenant); // moran 24.11.15 - Task 17424 
+
+
             var myDeclarationKeys = new DeclarationKeys { Id = declarationPM.Id };
             myDeclarationTaxUpdateService.FastDeleteComposition(myDeclarationKeys);
             mySupplierInvoiceItemVehicleModUpdateService.FastDeleteComposition(myDeclarationKeys);
             mySupplierInvoiceItemsTaxUpdateService.FastDeleteComposition(myDeclarationKeys);
             mySupplierInvoiceItemModVehicleUpdateService.FastDeleteComposition(myDeclarationKeys);
+            mySupplierInvoiceItemsPriceUpdateService.FastDeleteComposition(myDeclarationKeys);
+            mySupplierInvoiceItemsLevyUpdateService.FastDeleteComposition(myDeclarationKeys);
+            mySupplierInvoiceItemsDescriptUpdateService.FastDeleteComposition(myDeclarationKeys);
+            mySupplierInvoiceItemsProdIdentUpdateService.FastDeleteComposition(myDeclarationKeys);
+            mySupplierInvoiceItemsSerialNumUpdateService.FastDeleteComposition(myDeclarationKeys);
+            mySuppInvoiceItemsAbachStatementUpdateService.FastDeleteComposition(myDeclarationKeys);
 
             (context as DbContextBase).SaveChanges();
         }
