24701dff54b0e8721b5d9f297c16708608be4564
Shoshana Greenwald <shoshana@amital.co.il>
2022-08-04 11:37:02 +0300

    #165603 #165602

--- Logitude/AngularModules/AngularModules/Customs/Components/MenuButtons/DeclarationMenuButtonsHandler.ts
+++ Logitude/AngularModules/AngularModules/Customs/Components/MenuButtons/DeclarationMenuButtonsHandler.ts
@@ -1334,7 +1334,8 @@ export class DeclarationMenuButtonsHandler implements OnDestroy {
             logWindow.ShowCloseButton = true;
             logWindow.Show('./CustomsModules/CustomsContainerization/Components/NewEntity/NewContainerizationComponent');
             logWindow.WindowClosed.subscribe(($event: any) => {
-                if($event!="0" && $event!="cancel") {
+
+                if($event!="0" && $event!="cancel"&&$event!=null) {
                    this.OpenScreenContainerizationByFilter($event);
                 }
             });
