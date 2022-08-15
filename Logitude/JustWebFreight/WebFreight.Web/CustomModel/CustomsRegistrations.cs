78e9a4d64ae1a943e9c3244cf00564d17e5f5499
Shoshana Greenwald <shoshana@amital.co.il>
2022-08-15 13:22:20 +0300

    #165965

--- Logitude/JustWebFreight/WebFreight.Web/CustomModel/CustomsRegistrations.cs
+++ Logitude/JustWebFreight/WebFreight.Web/CustomModel/CustomsRegistrations.cs
@@ -37,6 +37,7 @@ namespace WebFreight.Web.CustomModel
             ContainerAccessor.Container.RegisterType<ICustomsCloseCourierMasterService, CloseCourierMasterService>("CloseCourierMasterService", new InjectionFactory(c => new CloseCourierMasterService()));
             ContainerAccessor.Container.RegisterType<ICustomsSendManifestService, SendManifestService>("SendManifestService", new InjectionFactory(c => new SendManifestService()));
             ContainerAccessor.Container.RegisterType<ICustomsSendDeclarationStatus, SendDeclarationStatus>("SendDeclarationStatus", new InjectionFactory(c => new SendDeclarationStatus()));
+            ContainerAccessor.Container.RegisterType<ICustomsExchangeRatesQuery, ExchangeRatesQuery>("ExchangeRatesQuery", new InjectionFactory(c => new ExchangeRatesQuery()));
 
 
             ContainerAccessor.Container.RegisterType<IUpdateOpenDeclarationInCourierMasterService, UpdateOpenDeclarationInCourierMasterService>("UpdateOpenDeclarationInCourierMasterService", new InjectionFactory(c => new UpdateOpenDeclarationInCourierMasterService()));
