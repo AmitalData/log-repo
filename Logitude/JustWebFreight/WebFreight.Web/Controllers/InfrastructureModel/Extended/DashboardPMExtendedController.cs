4b55401b1d2b1c3040d6b1c85a99b294e3c32f7d
Samar Abu Eid <Samar@logitudeworld.com>
2022-08-21 16:12:16 +0300

    #165823

---
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/InfrastructureModel/Extended/DashboardPMExtendedController.cs
@@ -0,0 +1,42 @@
+﻿using Logitude.Infrastructure.BL.EntityPMs;
+using Logitude.Infrastructure.BL.EntityQueryServices;
+using Simplog.Data.CommonDataModel.EntityPOCOs;
+using Simplog.Data.CommonDataModel.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WebFreight.Web.Helpers;
+using WebFreight.Web.Security;
+
+namespace WebFreight.Web.Controllers.InfrastructureModel.Extended
+{
+    public class DashboardPMExtendedController : ApiController
+    {
+        public HttpResponseMessage GetDashboardPMs()
+        {
+            try
+            {
+                string token = HttpContext.Current.Request.Headers["Token"];
+                AuthenticationToken authToken = AuthenticationTokenRepository.GetSingleTokenFromCache(token);
+                int tenant = authToken.Tenant;
+                
+                SecurityUtility.AuthenticationOnTenant(tenant);
+                SecurityUtility.CheckContactFeature("Dashboard", "READ", tenant);
+
+                DashboardQueryService dashboardQueryService = new DashboardQueryService(tenant);
+                IQueryable<DashboardPM> myResult = dashboardQueryService.GetDashboardPMs(tenant);
+
+                return Request.CreateResponse(HttpStatusCode.OK, myResult);
+            }
+
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ApiExceptionBuilder.BuildException(ex));
+            }
+        }
+    }
+}
