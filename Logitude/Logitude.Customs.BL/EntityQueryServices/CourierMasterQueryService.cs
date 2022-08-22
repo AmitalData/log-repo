3468e53da774deeff4841f82727eb8d5c88ff24b
Sohaib Nemer <sohaib@amital.co.il>
2022-08-22 14:27:24 +0300

    #166154

--- Logitude/Logitude.Customs.BL/EntityQueryServices/CourierMasterQueryService.cs
+++ Logitude/Logitude.Customs.BL/EntityQueryServices/CourierMasterQueryService.cs
@@ -214,6 +214,7 @@ namespace Logitude.Customs.BL.EntityQueryServices
             int PAY_R = 0;
             int PAY_I = 0;
             int DecWithoutHaTra = 0;
+            int NotApproved = 0;
 
             var totQ =
             (from dStatus in q
@@ -251,6 +252,7 @@ namespace Logitude.Customs.BL.EntityQueryServices
                  ACC = g.Count(r => (r.StorageSiteStatusCode == "2" || r.SpecialActionStatus == "X")),
                  ACC_W = g.Count(r => (r.StorageSiteStatusCode == "2")),
                  ACC_WS = g.Count(r => (r.SpecialActionStatus == "X")),
+                 NotApproved = g.Count(r=>(r.NotApprovedPendingList != null)),
                  //DecWithoutHaTra = g.Count (r=> (r.Declaration.HatraDate == null))
              });
 
@@ -286,6 +288,7 @@ namespace Logitude.Customs.BL.EntityQueryServices
                 ACC_WS = tot.ACC_WS;
                 PAY_I = tot.pAY_I;
                 PAY_C = tot.pAY_C;
+                NotApproved = tot.NotApproved;
               //  DecWithoutHaTra = tot.DecWithoutHaTra;
             }
 
@@ -318,7 +321,9 @@ namespace Logitude.Customs.BL.EntityQueryServices
             keyValuePairList.Add(new KeyValuePair<string, int>("ACC", ACC));
             keyValuePairList.Add(new KeyValuePair<string, int>("ACC_W", ACC_W));
             keyValuePairList.Add(new KeyValuePair<string, int>("ACC_WS", ACC_WS));
-           // keyValuePairList.Add(new KeyValuePair<string, int>("DecWithoutHaTra", DecWithoutHaTra));
+            keyValuePairList.Add(new KeyValuePair<string, int>("NotApproved", NotApproved));
+
+            // keyValuePairList.Add(new KeyValuePair<string, int>("DecWithoutHaTra", DecWithoutHaTra));
 
         }
 
--- Logitude/Logitude.Customs.BL/EntityQueryServices/CustomsRequestsSheetQueryService.cs
+++ Logitude/Logitude.Customs.BL/EntityQueryServices/CustomsRequestsSheetQueryService.cs
@@ -182,6 +182,7 @@ namespace Logitude.Customs.BL.EntityQueryServices
 "DCAMU", // multi update 
 "UCADPE", // הזנה גורפת PENDING
 "DCACSIFF", // יצירת חשבון ספק מאקסל
+"DCAInUCBApproveAllPending",//אישור PENDING
 
             };
 
@@ -331,6 +332,7 @@ namespace Logitude.Customs.BL.EntityQueryServices
 "DCACCFII",//יצירת אישור לפרטי מכס
 "UCADPE" ,// add multi pending
 "DCACSIFF", // יצירת חשבון ספק מאקסל
+"DCAInUCBApproveAllPending",//אישור PENDING
 
             };
 
@@ -460,6 +462,7 @@ namespace Logitude.Customs.BL.EntityQueryServices
 "DCACCFII",
 "DCACSIFF", // יצירת חשבון ספק מאקסל
 "UCADPE",
+"DCAInUCBApproveAllPending",//אישור PENDING
 //"8302" //בקשה לטופס הצהרה
 };
 
