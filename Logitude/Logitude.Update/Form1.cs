94effa3d2fe19c1cb1dc6c57de3c627694df6f0e
Abed Hamodi <Abed@logitudeworld.com>
2022-08-28 16:54:06 +0300

    166553

--- Logitude/Logitude.Update/Form1.cs
+++ Logitude/Logitude.Update/Form1.cs
@@ -161,7 +161,7 @@ namespace Logitude.Update
             string storageServiceMode = "fs";
             string queueServiceMode = "azure";
             Logitude.Server.Tools.ContainerAccessor.InitContainer();
-            InjectionUtil.Init(null, null, null, () => (new ByteCompressorUtil()) as IByteCompressorUtil, null, null, null, null);
+            InjectionUtil.Init(null, null, null, () => (new ByteCompressorUtil()) as IByteCompressorUtil, null, null, null, null ,null);
             InfraRegistrationHelper.Register();
             CacheManager.CacheWrapper = new CacheWrapper(WorkerEntryPoint.Cache);
         }
