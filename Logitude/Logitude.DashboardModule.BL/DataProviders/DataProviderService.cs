f65960c8bafe8ebf8311d478a4d58771325efd5e
Amer Aldghameen <Amer@logitudeworld.com>
2022-08-30 16:10:42 +0300

    ++

--- Logitude/Logitude.DashboardModule.BL/DataProviders/DataProviderService.cs
+++ Logitude/Logitude.DashboardModule.BL/DataProviders/DataProviderService.cs
@@ -1,4 +1,5 @@
 ﻿using Logitude.DashboardModule.BL.EntityPMs;
+using Logitude.DashboardModule.Data;
 using Logitude.DashboardModule.Data.EntityPOCOs;
 using Logitude.DashboardModule.Data.Repositories;
 using System;
@@ -12,13 +13,29 @@ namespace Logitude.DashboardModule.BL.DataProviders
     public class DataProviderService
     {
 
-        public string GetData(WidgetPM widget)
+        public List<SeriesMeasure> GetData(WidgetPM widget)
         {
             var entity = GetEntity(widget.EntityId);
             var entityFields = GetEntityFields(widget.EntityId).ToDictionary(e=>e.Id,e=>e);
+            var seriesMeasures = new List<SeriesMeasure>();
+            foreach (var measure in widget.WidgetMeasures)
+            {
+                var seriesMeasure = new SeriesMeasure();
+                seriesMeasure.SeriesMeasureVulues = GetSeriesMeasureVulues(widget, entity, entityFields);
+                seriesMeasures.Add(seriesMeasure);
+            }
+
+
+            return seriesMeasures;
+        }
+
+        private List<SeriesMeasureVulue> GetSeriesMeasureVulues(WidgetPM widget, AnalyticsFactsMetaData entity, Dictionary<string, AnalyticsFactsFieldsMetaData> entityFields)
+        {
             var groupBy = entityFields.ContainsKey(widget.GroupById) ? entityFields[widget.GroupById] : throw new Exception($"Meta Data Field '{widget.GroupById}' not found");
             var query = $@"select {groupBy.FieldCode} as GroupName, Value From {entity.TableName} group by {groupBy.FieldCode} ";
-            return null;
+            var conterxt = DashboardContext.GetContext(0);
+            var result = conterxt.GetActiveDbContext().Database.SqlQuery<SeriesMeasureVulue>(query, new object[0]).ToListAsync().Result;
+            return result;
         }
 
         private List<AnalyticsFactsFieldsMetaData> GetEntityFields(string entityId)
---
+++ Logitude/Logitude.DashboardModule.BL/DataProviders/WidgetDataSource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logitude.DashboardModule.BL.DataProviders
+{
+    public class SeriesMeasure
+    {
+        public List<SeriesMeasureVulue> SeriesMeasureVulues = new List<SeriesMeasureVulue>();
+    }
+    public class SeriesMeasureVulue
+    {
+        public string Lable { get; set; }
+        public decimal Value { get; set; }
+    }
+}
