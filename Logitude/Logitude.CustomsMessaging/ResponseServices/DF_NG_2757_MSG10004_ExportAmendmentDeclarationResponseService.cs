0e826f14febe22c8b9d655219f65b2fffca4a8a5
elisheva Nachaysi <elisheva@amital.co.il>
2022-08-31 12:51:42 +0300

    #166839

--- Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_2757_MSG10004_ExportAmendmentDeclarationResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_2757_MSG10004_ExportAmendmentDeclarationResponseService.cs
@@ -401,6 +401,9 @@ namespace Logitude.CustomsMessaging.ResponseServices
                 if (declarationOrg != null && !isUpdateAfterAccept)
                 {
                     List<CustomsDocumentsTicketPM> customsDocumentsTicketPMs = customsDocumentsTicketQuery.GetCustomsDocumentsTicketPMsByEntityIdAndChilds(declarationOrg.Id, "", "", "", tenant, "Declaration");
+                    customsDocumentsTicketPMs.AddRange(customsDocumentsTicketQuery.GetCustomsDocumentsTicketPMsByEntityIdAndChilds(declarationOrg.Id, "", "", "", tenant, parentEntityCode :"ExportDeclarationClosingData"));
+
+
                     CustomsDocumentsTicketUpdateService customsDocumentsTicketUpdateService = new CustomsDocumentsTicketUpdateService(context, new Dictionary<string, IContext>(), declarationPM.Tenant);
 
                     foreach (var customsDocumentsTicketPM in customsDocumentsTicketPMs)
