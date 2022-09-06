88033aea535113ffb88af74e5c14895dac615348
Shoshana Greenwald <shoshana@amital.co.il>
2022-09-06 16:14:45 +0300

    #165513

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsListTemplates/Components/CourierWorksheetListTemplate.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsListTemplates/Components/CourierWorksheetListTemplate.ts
@@ -102,7 +102,8 @@ export class CourierWorksheetListTemplate {
     private currentSession = SessionLocator.SelectedSession;
     IsNotConnectedDeclarationChecked: boolean;
     IsConnectedDeclarationChecked: boolean;
-
+    Isdisabled:boolean=false;
+    CertificateDelay:string="הפקת תעודת עיכוב";
     FirePreventSelect() {
         SessionLocator.SelectedSession.PseventRowSelectEvent.emit("CourierWorksheetListTemplate.SendSplitButton");
     }
@@ -665,6 +666,16 @@ export class CourierWorksheetListTemplate {
                                         this.DelayCertificateDetails = declarationMamanSpecialActionPMItem;
                                         if (declarationMamanSpecialActionPMItem.MamanSpecialActionStatusCode == "1") {
                                             this.IsReceivingDelayCertificate = true;
+                                           
+                                            this.Isdisabled=false;
+                                        }
+                                        else if (declarationMamanSpecialActionPMItem.MamanSpecialActionStatusCode == "2") {
+                                            this.CertificateDelay="שידור תעודת עיכוב";
+                                            this.Isdisabled=false;
+                                        }
+                                        else if (declarationMamanSpecialActionPMItem.MamanSpecialActionStatusCode == null) {
+                                            this.Isdisabled=true;
+                                            this.CertificateDelay="הפקת תעודת עיכוב";
                                         }
                                         break;
                                     }
@@ -689,7 +700,7 @@ export class CourierWorksheetListTemplate {
                                         }
                                         break;
                                     }
-                                }
+                                }                               
                             });
 
                         }
@@ -909,7 +920,7 @@ export class CourierWorksheetListTemplate {
             if (confirm.Yes) {
                 this._IsDropdownMenuFilterReady = false;
                 SessionLocator.SelectedSession.StartBusyIndicatorCreating();
-                if (actionCode == "U") {
+                if (actionCode == "U") {                   
                     if (declarationMamanSpecialActionPM == null) {
                         declarationMamanSpecialActionPM = new DeclarationMamanSpecialActionPM();
                         declarationMamanSpecialActionPM.Tenant = SessionLocator.Tenant;
@@ -926,6 +937,7 @@ export class CourierWorksheetListTemplate {
                         });
                     }
                     else {
+                        this.Isdisabled=true;
                         declarationMamanSpecialActionPM.MamanSpecialActionStatusCode = null;
                         declarationMamanSpecialActionPM.MamanSpecialActionsErrorXml = null;
                         this._DeclarationMamanSpecialActionPMService.update(declarationMamanSpecialActionPM).subscribe((res: any) => {
