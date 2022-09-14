35bc7eb498ec101be3db97d8220c9600ef759812
Maheera Jazi <Maheera@logitudeworld.com>
2022-09-14 15:07:16 +0300

    #167488

--- Logitude/Logitude.BL/ShipmentsModel/CustomFilters/DigitalPortalCustomFilter.cs
+++ Logitude/Logitude.BL/ShipmentsModel/CustomFilters/DigitalPortalCustomFilter.cs
@@ -49,8 +49,35 @@ namespace Logitude.BL.ShipmentsModel.CustomFilters
 
                         List<string> transportModes = transportModesString.Split(',').ToList();
                         List<string> shipmentTypes = shipmentTypesString.Split(',').ToList();
+                        var orderedOccen = new List<string>();
+                        var orderedInlnad = new List<string>();
+                        
+                        foreach (var tm in transportModes)
+                        {
+                            if (tm.Equals("a", StringComparison.InvariantCultureIgnoreCase))
+                            {
+                                queryableData = queryableData.Where(a => tm.Equals(a.TransportModeId, StringComparison.InvariantCultureIgnoreCase));
+                            }
+                            else if (tm.Equals("o", StringComparison.InvariantCultureIgnoreCase))
+                            {
+                                var occenQ = new List<string> { "fcl", "lcl" };
+                                orderedOccen = shipmentTypes.Where(a => occenQ.Contains(a, StringComparer.InvariantCultureIgnoreCase)).ToList();
+                            }
+                            else if (tm.Equals("i", StringComparison.InvariantCultureIgnoreCase))
+                            {
+                                var inlandQ = new List<string> { "ftl", "ltl" };
+                                orderedInlnad = shipmentTypes.Where(a => inlandQ.Contains(a, StringComparer.InvariantCultureIgnoreCase)).ToList();
+                            }
+                        }
 
-                        queryableData = queryableData.Where(d => shipmentTypes.Contains(d.ShipmentTypeId) || transportModes.Contains(d.TransportModeId));
+                        queryableData = queryableData.Where(a => (transportModes.Any(t => t.Equals("a", StringComparison.InvariantCultureIgnoreCase)) 
+                                                                   && "a".Equals(a.TransportModeId, StringComparison.InvariantCultureIgnoreCase))
+                                                                 || (transportModes.Any(t => t.Equals("a", StringComparison.InvariantCultureIgnoreCase))
+                                                                      && "o".Equals(a.TransportModeId, StringComparison.InvariantCultureIgnoreCase) 
+                                                                       && (!orderedOccen.Any() || orderedOccen.Contains(a.ShipmentTypeId)))
+                                                                 || (transportModes.Any(t => t.Equals("a", StringComparison.InvariantCultureIgnoreCase))
+                                                                    && "i".Equals(a.TransportModeId, StringComparison.InvariantCultureIgnoreCase)
+                                                                       && (!orderedInlnad.Any() || orderedInlnad.Contains(a.ShipmentTypeId))));
                     }
                 }
             }
