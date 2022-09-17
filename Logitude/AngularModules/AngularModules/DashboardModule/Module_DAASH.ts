532376bbead9dac2b6817b68c7ca4beb74a59de2
Ahmad Abdalrazeq <a.abdalrazeq@logitudeworld.com>
2022-09-17 13:09:35 +0300

    i

--- Logitude/AngularModules/AngularModules/DashboardModule/Module_DAASH.ts
+++ Logitude/AngularModules/AngularModules/DashboardModule/Module_DAASH.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { InfrastructureModule } from '../Infrastructure/Module_INFR';
 import { Components, ModuleDeclarations } from './ModuleDeclarations';
 import { ModuleProviders } from './ModuleProviders';
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
-    imports: [InfrastructureModule],
+    imports: [InfrastructureModule, AgGridModule],
     declarations: [...Components],
     entryComponents: [...Components],
 })
