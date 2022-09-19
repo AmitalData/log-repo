ff5dce6f74bb0a6cdff61e999fa1e3b320bd9493
Amer Aldghameen <Amer@logitudeworld.com>
2022-09-19 10:47:43 +0300

    ++

--- Logitude/AngularModules/AngularModules/Dashboard/Components/Workspace/CustomDashboardComponent.ts
+++ Logitude/AngularModules/AngularModules/Dashboard/Components/Workspace/CustomDashboardComponent.ts
@@ -2,7 +2,7 @@ import { Component, AfterViewInit, OnInit,Input, OnDestroy } from '@angular/core
 import { SessionLocator } from '../../../Infrastructure/Utilities/SessionLocator';
 import { SessionInfo } from 'Infrastructure/Utilities/SessionInfo';
 import { LogitudeWindow } from '../../../Controls/Windows/LogitudeWindow';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { ServiceResponse } from '../../../Infrastructure/DataContracts/ServiceResponse';
 import { AppTool, DateTool } from '../../../Infrastructure/Tools';
 import { DashboardPM } from '../../../DashboardModule/EntityPMs/DashboardPM';
@@ -15,6 +15,10 @@ import { DashboardSharedUserPM } from '../../../DashboardModule/EntityPMs/Dashbo
 import { BaseComponent } from '../../../Infrastructure/Components/LogitudeComponents/BaseComponent';
 import { ConfirmWindow } from '../../../Controls/Windows/ConfirmWindow';
 import { Cloner } from '../../../Infrastructure/Utilities/Cloner';
+import { ReactWidgetPM } from 'logitude-dashboard-library/dist/types/widget';
+import { DashboardMapping } from 'Dashboard/Services/DashboardMapping';
+import { DashboardDataBinding } from 'logitude-dashboard-library/dist/types/DashboardDataBinding';
+import { Guid } from 'Infrastructure/Utilities/Guid';
 
 @Component({
     templateUrl:'CustomDashboardComponent.html',
@@ -30,6 +34,11 @@ export class CustomDashboardComponent extends BaseComponent implements OnInit, A
     public SelectedDashboard: DashboardPM;
     public SelectedDashboardName: string;
     public DataContext = this;
+
+    public newWidgetWidth = 3;
+    public newWidgetHeight = 5;
+
+    public reactWidgetsLayout:{lg:ReactWidgetPM[]} = {lg:[]};
     @Input('Show') Show;
     constructor() {
         super();
@@ -37,6 +46,15 @@ export class CustomDashboardComponent extends BaseComponent implements OnInit, A
         this.dashboardPMExtendedService = new DashboardPMExtendedService();
         this.SelectedDashboard = new DashboardPM();
         this.Listen();
+    }
+
+    DashboardDataBinding: DashboardDataBinding = {
+        isOnEditLayout: new Subject(),
+        onGetLayouts: new BehaviorSubject({ lg: [] }),
+        widgetUpdated: new Subject(),
+        onAddWidget: new Subject(),
+        onEditWidget: new Subject(),
+
     }
 
     ngOnInit(): void {
@@ -86,11 +104,22 @@ export class CustomDashboardComponent extends BaseComponent implements OnInit, A
             if (!myResponse.HasError) {
                 this.SelectedDashboard = myResponse.Result;
                 if (this.SelectedDashboard) {
+                    this.BindReactWidgets(this.SelectedDashboard.Widgets);
                     this.SelectedDashboardName = this.SelectedDashboard.Name;
                     this.Clone();
                 }
             }           
         });        
+    }
+    private BindReactWidgets(widgets: WidgetPM[]) {
+        var reactWidgets: ReactWidgetPM[] = [];
+
+        widgets.forEach(item => {
+            item.UniqueKey = item.Id;
+            reactWidgets.push(DashboardMapping.GetReactWidget(item));
+        });
+        this.reactWidgetsLayout = {lg:reactWidgets};
+        this.DashboardDataBinding.onGetLayouts.next(this.reactWidgetsLayout);
     }
     
     //GetReactDashboard(dashboard: DashboardPM): ReactDashboardPM {
@@ -222,8 +251,9 @@ export class CustomDashboardComponent extends BaseComponent implements OnInit, A
     AddWidgetClicked() {
         var myWidget: WidgetPM = new WidgetPM(this.SelectedDashboard);
         myWidget.Tenant = SessionInfo.LoggedUserTenant;
-        // myWidget.StartPotistion = widget.StartPotistion;
-        // myWidget.EndPosition = widget.EndPosition;        
+        var position = this.EvaluateNewWidgetPosition();
+        myWidget.StartPotistion = `${position.x},${position.y}`;
+        myWidget.EndPosition = `${position.w},${position.h}`;
 
         var logitudeWindow = new LogitudeWindow();
         logitudeWindow.Title = "Add Widget";
@@ -233,10 +263,41 @@ export class CustomDashboardComponent extends BaseComponent implements OnInit, A
             logitudeWindow.WindowClosed.subscribe(s => {
                 if (s) {
                     this.HasChanges = true;
+                    this.AddWidgetToReactLayout(myWidget);
                 }
             });
         });
     }
+    AddWidgetToReactLayout(myWidget:WidgetPM){
+        myWidget.UniqueKey = Guid.newGuid();
+        var reactWidget = DashboardMapping.GetReactWidget(myWidget);
+        this.reactWidgetsLayout.lg.push(reactWidget);
+
+    }
+    
+
+    EvaluateNewWidgetPosition(){
+        let widgetYPosition = 0;
+        let widgetXPosition = 0;
+        let allLayouts = this.reactWidgetsLayout.lg.map(e => e.Layout);
+        for (let y = 0; true; y++) {
+          widgetYPosition = y;
+          for (let x = 0; x < 10; x++) {
+            widgetXPosition = x;
+    
+            var exisitWidgetInNewPosition = allLayouts.find(e => {
+              return e
+                && widgetXPosition < e.x + e.w && widgetXPosition + this.newWidgetWidth > e.x
+                && widgetYPosition < e.y + e.h && widgetYPosition + this.newWidgetHeight > e.y
+            });
+            if (!exisitWidgetInNewPosition)
+              return { x: widgetXPosition, y: widgetYPosition, w: this.newWidgetWidth, h: this.newWidgetHeight };
+          }
+    
+        }
+    
+      }
+
 
     private isBackButtonClicked: boolean = false;
     BackButtonClicked() {
