4ac5e1b4030b824ce27438412ea9c861d2c84813
Amer Aldghameen <Amer@logitudeworld.com>
2022-09-21 09:48:44 +0300

    Pre Carriage

--- Logitude/JustWebFreight/WebFreight.Web/ContainerTracking/VizionAnalyzer.cs
+++ Logitude/JustWebFreight/WebFreight.Web/ContainerTracking/VizionAnalyzer.cs
@@ -132,8 +132,8 @@ namespace WebFreight.Web.ContainerTracking
 
             if (!string.IsNullOrEmpty(location?.unlocode) &&
                 !string.IsNullOrEmpty(mainLocation?.unlocode) &&
-                visionContainerStatus?.payload?.inland_destination?.unlocode != visionContainerStatus?.payload?.destination_port?.unlocode)
-                return true;
+                location?.unlocode == mainLocation?.unlocode)
+                return false;
 
             if (location.name == mainLocation.name &&
             location.country == mainLocation.country &&
