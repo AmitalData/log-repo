43f8ac2891d9e9e19ecf92b8687eed724e41257d
Sohaib Nemer <sohaib@amital.co.il>
2022-09-29 10:52:35 +0300

    #167885

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/CustomsModel/Extended/CourierMasterController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/CustomsModel/Extended/CourierMasterController.cs
@@ -36,6 +36,7 @@ using Logitude.Customs.BL.Messaging.ILSWS;
 using Logitude.CustomsMessaging.Common.ResponseData;
 using Logitude.Customs.Data.EntityPOCOs;
 using Logitude.Customs.BL.BL;
+using Logitude.BL.CommonDataModel.EntityQueries;
 
 namespace WebFreight.Web.Controllers.CustomsModel.Extended
 {
@@ -771,9 +772,14 @@ namespace WebFreight.Web.Controllers.CustomsModel.Extended
                     var amitalContext = AmitalContext.GetContext(tenant);
                     var myGDFDATAQueryService = new GDFDATAQueryService(amitalContext);
                     var def = myGDFDATAQueryService.GetSingle("ISRAEL", "CGO_CUST_MAMAN", "NON", "NON", false, true);
-
-                    var cancelOldCommunicationLogs = new CancelOldCommunicationLogs();
-                    cancelOldCommunicationLogs.CancelOldECTHRDataMaman(tenant, declarationId);
+                    FeatureQuery featureQuery = new FeatureQuery();
+                    var features = featureQuery.GetAllowedFeaturesForLoggedUser(AuthenticationUtil.ResolveUserId(tenant), tenant);
+                    var feature = features.Features.FirstOrDefault(x => x.Code == "CancelOldCommunication");
+                    if (feature != null)
+                    {
+                        var cancelOldCommunicationLogs = new CancelOldCommunicationLogs();
+                        cancelOldCommunicationLogs.CancelOldECTHRDataMaman(tenant, declarationId);
+                    }
 
                     if (def.DEFDATA.Contains("ILMMN") && declaration.Consignments.FirstOrDefault().StorageSiteCode == "ILMMN") // Maman
                     {
--- Logitude/Logitude.CustomsMessaging/ResponseServices/UniCourierBatchSendUCBCTML_MsgResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/UniCourierBatchSendUCBCTML_MsgResponseService.cs
@@ -1,4 +1,5 @@
 ﻿
+using Logitude.BL.CommonDataModel.EntityQueries;
 using Logitude.Customs.BL.BL;
 using Logitude.Customs.BL.EntityQueryServices;
 using Logitude.Customs.BL.EntityUpdateServices;
@@ -157,8 +158,14 @@ namespace Logitude.CustomsMessaging.ResponseServices
                 string response = "";
                 using (var scope = TransactionFactory.GetNewTransaction())
                 {
-                    var cancelOldCommunicationLogs = new CancelOldCommunicationLogs();
-                    cancelOldCommunicationLogs.CancelOldECTHRDataMaman(requestParams.Tenant, itemDeclarationIdStorageSiteCode.Key);
+                    FeatureQuery featureQuery = new FeatureQuery();
+                    var features = featureQuery.GetAllowedFeaturesForLoggedUser(AuthenticationUtil.ResolveUserId(requestParams.Tenant), requestParams.Tenant);
+                    var feature = features.Features.FirstOrDefault(x => x.Code == "CancelOldCommunication");
+                    if (feature != null)
+                    {
+                        var cancelOldCommunicationLogs = new CancelOldCommunicationLogs();
+                        cancelOldCommunicationLogs.CancelOldECTHRDataMaman(requestParams.Tenant, itemDeclarationIdStorageSiteCode.Key);
+                    }
                     if (def.DEFDATA.Contains("ILMMN") && itemDeclarationIdStorageSiteCode.Value == "ILMMN") // Maman
                     {
                         var courierGWMessageECTHRDataMamanService = new CourierGWMessageECTHRDataMamanRequestService();
