59d387037f8fd9f9d0baf51e4993735b8a521541
Atallah Hammad <Atallah@logitudeworld.com>
2022-10-02 18:24:51 +0300

    Add FlowShipmentController

---
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/WorkflowModel/Models/ApiQueryTreeFilters.cs
@@ -0,0 +1,11 @@
+﻿using Simplog.Server.Infrastructure.DataContracts;
+
+namespace WebFreight.Web.Controllers.WorkflowModel.Models
+{
+    public class ApiQueryTreeFilters
+    {
+        public QueryFilterItem QueryFilterItem { get; set; }
+        public int Tenant { get; set; }
+        public int PageSize { get; set; }
+    }
+}
