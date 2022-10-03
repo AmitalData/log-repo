ab7bd2e6b251bd74a12a3c25cbe462bdf76ebe4a
Shoshana Greenwald <shoshana@amital.co.il>
2022-10-03 10:28:59 +0300

    #167786

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsContainerization/Components/NewEntity/NewContainerizationComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsContainerization/Components/NewEntity/NewContainerizationComponent.ts
@@ -426,6 +426,7 @@ export class NewContainerizationComponent extends BaseComponent {
         this.IsSelected = false;
         this.containerizationExtendedListService.connectedSelectAll = false;
         this.entityPM.ConnectedDeclarations = "";
+        this.entityPM.IsDirty = false;
         this.containerizationExtendedListService.ConnectedDeclarations = "";
         this.containerizationExtendedListService.SelectedDeclarations = false;
         this.LoadConnectedItems();
