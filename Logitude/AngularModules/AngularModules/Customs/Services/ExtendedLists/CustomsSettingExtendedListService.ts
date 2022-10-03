2573d04e4701a2c71982a2d912398610f3e3cb97
Sohaib Nemer <sohaib@amital.co.il>
2022-10-03 09:47:26 +0300

    #168593

--- Logitude/AngularModules/AngularModules/Customs/Services/ExtendedLists/CustomsSettingExtendedListService.ts
+++ Logitude/AngularModules/AngularModules/Customs/Services/ExtendedLists/CustomsSettingExtendedListService.ts
@@ -168,9 +168,9 @@ export class CustomsSettingExtendedListService {
     GetSkipAutoInsurancePromise(customerCode: string, tenant: number) {
         var authHeader = new Headers();
         authHeader.append('Token', SessionInfo.Token);
-
+        debugger;
         return defer(() => {
-            return this._http.get(this._apiUrl + '/GetSkipAutoInsurance/?customerCode=' + customerCode.toString() + '&tenant=' + tenant, ServiceHelper.GetHttpHeaders()).pipe(map(response => {
+            return this._http.get(this._apiUrl + '/GetSkipAutoInsurance/?customerCode=' + customerCode + '&tenant=' + tenant, ServiceHelper.GetHttpHeaders()).pipe(map(response => {
                     var obj = response;
 
                     //var entity: CustomsSettingList;
