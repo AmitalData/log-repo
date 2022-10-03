c1aad4c36cea6b05c45bc15fe462a8af89e9766f
Sohaib Nemer <sohaib@amital.co.il>
2022-10-03 09:51:16 +0300

    delete debugger

--- Logitude/AngularModules/AngularModules/Customs/Services/ExtendedLists/CustomsSettingExtendedListService.ts
+++ Logitude/AngularModules/AngularModules/Customs/Services/ExtendedLists/CustomsSettingExtendedListService.ts
@@ -168,7 +168,6 @@ export class CustomsSettingExtendedListService {
     GetSkipAutoInsurancePromise(customerCode: string, tenant: number) {
         var authHeader = new Headers();
         authHeader.append('Token', SessionInfo.Token);
-        debugger;
         return defer(() => {
             return this._http.get(this._apiUrl + '/GetSkipAutoInsurance/?customerCode=' + customerCode + '&tenant=' + tenant, ServiceHelper.GetHttpHeaders()).pipe(map(response => {
                     var obj = response;
