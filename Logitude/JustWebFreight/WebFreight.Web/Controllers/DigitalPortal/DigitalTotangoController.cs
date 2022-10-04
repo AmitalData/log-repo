8c7119cb41cc148c90ed36a2fd4b0f18aee20669
Maheera Jazi <Maheera@logitudeworld.com>
2022-10-04 09:51:01 +0300

    #168462

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/DigitalPortal/DigitalTotangoController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/DigitalPortal/DigitalTotangoController.cs
@@ -1,4 +1,6 @@
-﻿using Simplog.Data.CommonDataModel.Repositories;
+﻿using Logitude.BL.CommonDataModel.EntityPMs;
+using Logitude.BL.CommonDataModel.EntityQueries;
+using Simplog.Data.CommonDataModel.Repositories;
 using System.Web;
 using System.Web.Http;
 using WebFreight.Web.DataContracts;
@@ -17,10 +19,21 @@ namespace WebFreight.Web.Controllers.DigitalPortal
             SecurityUtility.AuthenticationOnTenant(authToken.Tenant);
             SecurityUtility.CheckDigitalUserAuthentication(authToken.Tenant, activityInfo.CardId);
 
-            var isSharedLogisticsContact = true;
-            ActivityLog.AddContactActivityLog(activityInfo.CardId, activityInfo.PartnerTypeId, activityInfo.ContactId,
-                                              activityInfo.Module, activityInfo.Activity, authToken.Tenant,
-                                              isSharedLogisticsContact, null);
+            var myTenant = GetTenant(authToken.Tenant);
+            activityInfo.IsSharedLogisticsContact = true;
+            activityInfo.OrganizationId = authToken.Tenant.ToString();
+            activityInfo.OrgDisplayName = myTenant.Company + (myTenant.CountryName != null ? ("-" + myTenant.CountryName.Trim()) : "");
+            activityInfo.Tenant = authToken.Tenant;
+            ActivityLog.AddContactActivityWithTotango(activityInfo.OrganizationId, activityInfo.OrgDisplayName, activityInfo.UserName, activityInfo.Module,
+               activityInfo.Activity, activityInfo.ContactId, activityInfo.Tenant, activityInfo.IsSharedLogisticsContact, activityInfo.CardId, activityInfo.PartnerTypeId, null);
+        }
+
+        private TenantPM GetTenant(int tenant)
+        {
+            var tenantQuery = new TenantQuery(tenant);
+            var tenantPM = tenantQuery.GetSinglePM(tenant);
+
+            return tenantPM;
         }
     }
 }
