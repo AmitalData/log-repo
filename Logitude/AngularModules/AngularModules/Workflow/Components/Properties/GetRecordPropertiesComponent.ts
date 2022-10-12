7daf737ee2dd82d6e8d89e96e50045e9ac0685ea
Salwa Swelem <salwa@logitudeworld.com>
2022-10-12 14:25:14 +0300

    #168932

--- Logitude/AngularModules/AngularModules/Workflow/Components/Properties/GetRecordPropertiesComponent.ts
+++ Logitude/AngularModules/AngularModules/Workflow/Components/Properties/GetRecordPropertiesComponent.ts
@@ -6,7 +6,10 @@ import { AppTool } from "Infrastructure/Tools";
 import { SessionLocator } from "Infrastructure/Utilities/SessionLocator";
 import { ConditionOperations } from "Workflow/Constants/ConditionOperations";
 import { GetRecordsLimit } from "Workflow/Constants/GetRecordsLimit";
+import { SortDirections } from "Workflow/Constants/SortDirections";
 import { Condition } from "Workflow/Models/Condition";
+import { ListItem } from "Workflow/Models/ListItem";
+import { SortDirectionList } from "Workflow/Models/SortDirectionList";
 
 @Component({
     templateUrl: "./GetRecordPropertiesComponent.html"
@@ -23,6 +26,8 @@ export class GetRecordPropertiesComponent extends BaseComponent {
     public ReturnedColumns: string[];
     public Conditions: Condition[];
     public ConditionsOperation: string;
+    public OrderBy: string;
+    public SortBy: string;
 
     public ValidationErrorsList: string[];
     public IsValidConditions: boolean = true;
@@ -33,7 +38,8 @@ export class GetRecordPropertiesComponent extends BaseComponent {
     public FlowObjectFields: ObjectFieldPM[];
 
     public CurrentSession = SessionLocator.SelectedSession;
-
+    public SortDirectionListItems = new SortDirectionList().Items;
+    public ListItem = (itemCode: string) => { return new ListItem(itemCode) };
 
     SetWindowArgs(args: any) {
         this.Data = args.Data ? args.Data : {};
@@ -43,10 +49,16 @@ export class GetRecordPropertiesComponent extends BaseComponent {
         this.initialize();
     }
 
+    ngOnChanges() {
+        this.SortDirectionListItems = new SortDirectionList().Items;
+    }
+
     initialize() {
         this.Name = this.Data["name"] || null;
         this.Entity = this.Data["entity"] || null;
         this.RecordsLimit = this.Data["recordsLimit"] ? this.Data["recordsLimit"] : GetRecordsLimit.FirstRecord
+        this.OrderBy = this.Data["orderBy"] ? this.Data["orderBy"] : null;
+        this.SortBy = this.Data["sortBy"] ? this.Data["sortBy"] : null;
 
         this.Conditions = this.Data["conditions"] || [];
         this.ConditionsOperation = this.Data["conditionsOperation"] || ConditionOperations.And;
@@ -58,13 +70,15 @@ export class GetRecordPropertiesComponent extends BaseComponent {
         }
 
         this.ReturnedColumns = this.Data["returnedColumns"] || [];
-        
+
         if (this.ReturnedColumns.length == 0) {
             this.IsValidSelectedCoulmns = false;
             this.ReturnedColumns.push(null);
         }
 
         this.Data["recordsLimit"] = this.RecordsLimit;
+        this.Data["orderBy"] = this.OrderBy;
+        this.Data["sortBy"] = this.SortBy;
 
         this.EntityId = this.getEntityId(this.Entity);
 
@@ -88,6 +102,35 @@ export class GetRecordPropertiesComponent extends BaseComponent {
     updateRecordsLimit(recordsLimit: string) {
         this.Data["recordsLimit"] = recordsLimit;
         this.RecordsLimit = recordsLimit;
+        if (recordsLimit == GetRecordsLimit.FirstRecord) {
+            this.updateOrderBy(null)
+            this.updateSortBy(null)
+        } else {
+            this.updateOrderBy(SortDirections.NotSorted)
+        }
+    }
+
+    updateOrderBy(orderValue: string) {
+        this.Data["orderBy"] = orderValue;
+        this.OrderBy = orderValue;
+        if (!this.isOrderBy()) {
+            this.updateSortBy(null)
+        }
+        this.setUIProperties();
+    }
+
+    updateSortBy(sortValue: ObjectFieldPM) {
+        this.Data["sortBy"] = sortValue ? sortValue.FieldCode : null;
+        this.SortBy = sortValue ? sortValue.FieldCode : null;
+        this.setUIProperties();
+    }
+
+    isAllRecords() {
+        return this.RecordsLimit == GetRecordsLimit.AllRecords
+    }
+
+    isOrderBy() {
+        return this.OrderBy && this.OrderBy != SortDirections.NotSorted
     }
 
     getObjectTablesQueryFilters() {
@@ -103,6 +146,11 @@ export class GetRecordPropertiesComponent extends BaseComponent {
     setUIProperties() {
         this.UIProperties.SetRequired("Name", null, AppTool.IsNullOrEmpty(this.Name));
         this.UIProperties.SetRequired("Object", null, AppTool.IsNullOrEmpty(this.EntityId));
+        if (this.isOrderBy()) {
+            this.UIProperties.SetRequired("SortBy", null, AppTool.IsNullOrEmpty(this.SortBy));
+        }else{
+            this.UIProperties.SetRequired("SortBy", null, false);
+        }
     }
 
     cancelButtonClicked() {
