ef97836570bf7a0df880aea2ecc242b01590dbdf
Salwa Swelem <salwa@logitudeworld.com>
2022-10-16 10:08:20 +0300

    add type

--- Logitude/AngularModules/AngularModules/Workflow/Models/SetValue.ts
+++ Logitude/AngularModules/AngularModules/Workflow/Models/SetValue.ts
@@ -2,6 +2,7 @@ import { ObjectFieldPM } from "Infrastructure/EntityPMs/ObjectFieldPM";
 
 export class SetValue{    
     public field: string;
+    public type: string;
     public operator: string;
     public value: string;
 }
