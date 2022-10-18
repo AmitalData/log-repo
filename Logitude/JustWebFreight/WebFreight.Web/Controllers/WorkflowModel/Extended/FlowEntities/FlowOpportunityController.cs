9540e4504a3a18493d47fb7b06b4a3a151b7f82d
Atallah Hammad <Atallah@logitudeworld.com>
2022-10-18 13:49:02 +0300

    Add PostShipment and PostOpportunity to flow controlles

---
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/WorkflowModel/Extended/FlowEntities/FlowOpportunityController.cs
@@ -0,0 +1,56 @@
+﻿using Logitude.CRM.BL.EntityPMs;
+using Logitude.CRM.BL.EntityUpdateServices;
+using Logitude.CRM.Data;
+using Simplog.Data.CommonDataModel.EntityPOCOs;
+using Simplog.Data.CommonDataModel.Repositories;
+using Simplog.Server.Infrastructure;
+using Simplog.Server.Infrastructure.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Transactions;
+using System.Web;
+using System.Web.Http;
+using WebFreight.Web.Helpers;
+using WebFreight.Web.Security;
+
+namespace WebFreight.Web.Controllers.WorkflowModel.Extended.FlowEntities
+{
+    public class FlowOpportunityController : ApiController
+    {
+        public HttpResponseMessage Post(OpportunityPM entityPM)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    using (TransactionScope scope = TransactionFactory.GetTransaction())
+                    {
+                        string token = HttpContext.Current.Request.Headers["Token"];
+                        AuthenticationToken authToken = AuthenticationTokenRepository.GetSingleTokenFromCache(token);
+                        SecurityUtility.AuthenticationOnTenant(authToken.Tenant);
+                        int tenant = authToken.Tenant;
+
+                        ICRMContext MyContext = CRMContext.GetContext(entityPM.Tenant);
+                        OpportunityUpdateService service = new OpportunityUpdateService(MyContext, new Dictionary<string, IContext>(), entityPM.Tenant);
+                        entityPM.ChangeSetOp = ChangeSetOperation.Insert;
+                        service.Update(entityPM, true);
+
+                        scope.Complete();
+                        return Request.CreateResponse(HttpStatusCode.OK, entityPM);
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, ApiExceptionBuilder.BuildException(ex));
+                }
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ApiExceptionBuilder.BuildModelException(ModelState));
+            }
+        }
+    }
+}
