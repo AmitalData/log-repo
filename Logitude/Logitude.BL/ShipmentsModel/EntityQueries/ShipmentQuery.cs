adfb3b3a4c00aa9be499070c95670f1189334ede
Maheera Jazi <Maheera@logitudeworld.com>
2022-10-24 09:08:49 +0300

    #169635 Bug 169635: Issue in getting customer archived value

--- Logitude/Logitude.BL/ShipmentsModel/EntityQueries/ShipmentQuery.cs
+++ Logitude/Logitude.BL/ShipmentsModel/EntityQueries/ShipmentQuery.cs
@@ -2658,9 +2658,9 @@ namespace Logitude.BL.ShipmentsModel.EntityQueries
                 return;
             }
 
-            shipmentPM.IsCustomerArchived = (from a in repository.context.ShipmentDigitalDataViews
-                                             where a.Id == shipmentPM.Id && a.Tenant == tenant
-                                             select a.IsCustomerArchived).FirstOrDefault();
+            shipmentPM.IsCustomerArchived = repository.context.ShipmentDigitalFields.Where(a=>a.Id == shipmentPM.Id && a.Tenant == shipmentPM.Tenant)
+                                                                                    .Select(a=>a.IsCustomerArchived).FirstOrDefault();
+                
             shipmentPM.IsFullInvoiced = false;
             if (shipmentPM.ShipmentReceivables == null || shipmentPM.ShipmentReceivables?.Count == 0)
             {
