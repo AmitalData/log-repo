3308c4524acf90e975269d0bb73ce027179fb61e
Libby Hasid <libby@amital.co.il>
2022-10-27 17:48:54 +0300

    RUN TT

--- Logitude/AngularModules/AngularModules/Customs/Services/Others/CourierMasterService.ts
+++ Logitude/AngularModules/AngularModules/Customs/Services/Others/CourierMasterService.ts
@@ -738,30 +738,7 @@ export class CourierMasterService {
         });
     }
 
-    PostApproveAllPending(requestParams: PendingRequestParams) {
-
-        return defer(() => {
-            var authHeader = new Headers();
-            authHeader.append('Token', SessionInfo.Token);
-            authHeader.append('Content-Type', 'application/json');
-
-            var serviceResponse: ServiceResponse;
-            serviceResponse = new ServiceResponse();
-
-            return this._http.post(
-                this._apiUrl + '/PostApproveAllPending/', JSON.stringify(requestParams), ServiceHelper.GetHttpHeaders()).pipe(map((res) => {
-                    var messString = res;
-                    var serviceResponse: ServiceResponse;
-                    serviceResponse = new ServiceResponse();
-                    serviceResponse.Result = messString;
-
-                    return serviceResponse;
-                }),catchError(ServiceHelper.HandleServiceError));
-            ;
-
-        });
-    }
-
+    
     sendConnectDeclaration(courierMasterPM: CourierMasterPM) {
         const ajax: Observable<any> = this._http.post(
             this._apiUrl + "/sendConnectDeclaration",
