9b805130ca1fcdd689110e95a843fe3ad9b12982
Libby Hasid <libby@amital.co.il>
2022-10-27 16:00:14 +0300

    #169894

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationTabs/ModuleDeclarations.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationTabs/ModuleDeclarations.ts
@@ -30,6 +30,7 @@ import { DeclarationCargoSealTabComponent } from './Components/CargoSeal/Declara
 import { ExportDeclarationComponent } from './Components/General/ExportDeclarationComponent';
 import { ExportConsigmentContentComponent } from './Components/General/ConsigmentTabContent/ExportConsigmentContentComponent';
 import { ExportDeclarationClosureComponent } from './Components/Closure/ExportDeclarationClosureComponent';
+import { DeclarationExportStorageComponent } from './Components/ExportStorageList/DeclarationExportStorageComponent';
 
 export const Components =
     [
@@ -96,6 +97,7 @@ export class ModuleDeclarations {
             case "DeclarationTapagTabComponent": { myResult = DeclarationTapagTabComponent; break; }
             case "DeclarationCollateralsComponent": { myResult = DeclarationCollateralsComponent; break; }
             case "DeclarationCargoSplitTabComponent": { myResult = DeclarationCargoSplitTabComponent; break; }
+            case "DeclarationExportStorageComponent": { myResult = DeclarationExportStorageComponent; break; }
             case "ImporterDetailsComponent": { myResult = ImporterDetailsComponent; break; }
             case "GuaranteeDataComponent": { myResult = GuaranteeDataComponent; break; }
             case "DeclarationClassificationComponent": { myResult = DeclarationClassificationComponent; break; }
