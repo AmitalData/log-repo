6719933f7871b42e29616c81c1d7550c40ba1e0a
Libby Hasid <libby@amital.co.il>
2022-10-31 15:23:39 +0200

    #169864

--- Logitude/AngularModules/AngularModules/Customs/Controller/DeclarationEditComponentController.ts
+++ Logitude/AngularModules/AngularModules/Customs/Controller/DeclarationEditComponentController.ts
@@ -1,46 +1,54 @@
-import { AmitalGatewayUtil, UnifreightMessageM } from   '../../Infrastructure/Utilities/AmitalGatewayUtil';
+import { AmitalGatewayUtil, UnifreightMessageM } from '../../Infrastructure/Utilities/AmitalGatewayUtil';
 import { AppTool } from '../../Infrastructure/Tools';
 import { SessionLocator } from '../../Infrastructure/Utilities/SessionLocator';
 import { IEditComponentController } from '../../Infrastructure/Components/EditComponent/EditComponent';
 import { DeclarationPM } from '../../Customs/EntityPMs/DeclarationPM';
-import {MenuButtonsEvents, MenuButtonsStateChangedEventArgs} from '../../Infrastructure/Utilities/events/MenuButtonsEvents';
+import { MenuButtonsEvents, MenuButtonsStateChangedEventArgs } from '../../Infrastructure/Utilities/events/MenuButtonsEvents';
 import { FeatureLocator } from '../../Infrastructure/Utilities/FeatureLocator';
 
 export class DeclarationEditComponentController implements IEditComponentController {
     FilterTabs(allTabs: any[]) {
         let currentEntity: DeclarationPM = this.CurrentSession.CurrentEditComponent.EntityPM;
-         var indexOfTab = allTabs.findIndex(t => t.Code == "DCCR");
+        var indexOfTab = allTabs.findIndex(t => t.Code == "DCCR");
         if (!currentEntity.IsAmendment && FeatureLocator.HasFeaturePermession("Customs.Declaration", "DECLARATIONAMENDMENT")) {
             if (indexOfTab > -1) {
                 allTabs.splice(indexOfTab, 1);
             }
         }
-       
 
 
-       else if (!FeatureLocator.HasFeaturePermession("Customs.Declaration", "DECLARATIONAMENDMENT")) {
+
+        else if (!FeatureLocator.HasFeaturePermession("Customs.Declaration", "DECLARATIONAMENDMENT")) {
             allTabs[indexOfTab].IndexOrder = Math.max.apply(Math, allTabs.map(function (o) { return o.IndexOrder; })) + 1;
         }
-       else if (currentEntity.IsAmendment && FeatureLocator.HasFeaturePermession("Customs.Declaration", "DECLARATIONAMENDMENT")) {
+        else if (currentEntity.IsAmendment && FeatureLocator.HasFeaturePermession("Customs.Declaration", "DECLARATIONAMENDMENT")) {
             allTabs[indexOfTab].IndexOrder = -1;
         }
 
-         if (currentEntity.AmendmentDontDisplayInList) {
+        if (currentEntity.AmendmentDontDisplayInList) {
             var indexOfTab = allTabs.findIndex(t => t.Code == "DCDA");
             if (indexOfTab > -1) {
                 allTabs.splice(indexOfTab, 1);
             }
 
         }
-        
+
         var indexOfTab = allTabs.findIndex(t => t.Code == "CloD");
-      
+
         /*if (AppTool.IsNullOrEmpty(currentEntity.ExportClosedErrorXML) && !currentEntity.IsExportClosed ) {*/
-        if (currentEntity.Direction != "E" ) {
+        if (currentEntity.Direction != "E") {
             if (indexOfTab > -1) {
                 allTabs.splice(indexOfTab, 1);
             }
         }
+
+        var indexOfTab = allTabs.findIndex(t => t.Code == "DESL");
+        if (!(currentEntity.Direction == 'E' && currentEntity.TransportModeId == 'O')) {
+            if (indexOfTab > -1) {
+                allTabs.splice(indexOfTab, 1);
+            }
+        }
+
 
         if (currentEntity.IsAmendment || !FeatureLocator.HasFeaturePermession("Customs.Declaration", "DECLARATIONAMENDMENT") || currentEntity.Direction == "E") {
             var indexOfTab = allTabs.findIndex(t => t.Code == "DCCO");
@@ -50,25 +58,25 @@ export class DeclarationEditComponentController implements IEditComponentControl
             }
         }
 
-          if (currentEntity.Direction=="E") {
+        if (currentEntity.Direction == "E") {
             var indexOfTab = allTabs.findIndex(t => t.Code == "DEIN");
-             if (indexOfTab > -1) {
-                 allTabs[indexOfTab].TabNameTextCodeCode = "Customs.Declaration.TH.ExporterInvoices";
-             }
-        
+            if (indexOfTab > -1) {
+                allTabs[indexOfTab].TabNameTextCodeCode = "Customs.Declaration.TH.ExporterInvoices";
+            }
+
 
         }
-         else {
-             var indexOfTab = allTabs.findIndex(t => t.Code == "DEIN");
-             if (indexOfTab > -1) {
-                 allTabs[indexOfTab].TabNameTextCodeCode = "Customs.Declaration.TH.Invoices";
-             }
-               var indexOfTab = allTabs.findIndex(t => t.Code == "DCDI");
-              if (indexOfTab > -1) {
-                  allTabs.splice(indexOfTab, 1);
+        else {
+            var indexOfTab = allTabs.findIndex(t => t.Code == "DEIN");
+            if (indexOfTab > -1) {
+                allTabs[indexOfTab].TabNameTextCodeCode = "Customs.Declaration.TH.Invoices";
+            }
+            var indexOfTab = allTabs.findIndex(t => t.Code == "DCDI");
+            if (indexOfTab > -1) {
+                allTabs.splice(indexOfTab, 1);
 
-              }
-         }
+            }
+        }
 
 
     }
@@ -80,7 +88,7 @@ export class DeclarationEditComponentController implements IEditComponentControl
 
     private _ControllerOn: boolean = false;
     private _UnifaceExclusiveAlreadyLocked: boolean;    ////if true then force Check is loc in every reload
- 
+
     OnFirstTimeAfterSingleDataLoaded(CurrentEntity): Promise<boolean> {
         this._CurrentEntity = CurrentEntity as DeclarationPM;
         this._ControllerOn = true;
@@ -114,65 +122,65 @@ export class DeclarationEditComponentController implements IEditComponentControl
         let sub = AmitalGatewayUtil.Instance.UnifaceRequestArrived
             .subscribe(
 
-            (myUnifreightMessageM: UnifreightMessageM) => {
-                if (
-                    myUnifreightMessageM.LogitudeViewModel == "DeclarationEditComponentController" &&
-                    (myUnifreightMessageM.LogitudeEntity == "Customs.Declaration" || myUnifreightMessageM.LogitudeEntity == "Declaration") &&
-                    myUnifreightMessageM.LogitudeEntityNumber == this._CurrentEntity.Id) {
-                    sub.unsubscribe();
+                (myUnifreightMessageM: UnifreightMessageM) => {
+                    if (
+                        myUnifreightMessageM.LogitudeViewModel == "DeclarationEditComponentController" &&
+                        (myUnifreightMessageM.LogitudeEntity == "Customs.Declaration" || myUnifreightMessageM.LogitudeEntity == "Declaration") &&
+                        myUnifreightMessageM.LogitudeEntityNumber == this._CurrentEntity.Id) {
+                        sub.unsubscribe();
 
-                    //let ResponseInstructionCancel = false;
-                    //let listRes = myUnifreightMessageM.Response.filter(itm =>
-                    //    itm[0] == AmitalGatewayUtil.Instance.DeclarationMessaging.ResponseInstructionCancel);
-                    //if (listRes.length > -1 && !AppTool.IsNullOrEmpty(listRes[0])) {
-                    //    if (!AppTool.IsNullOrEmpty(listRes[0][1])) {
-                    //        ResponseInstructionCancel = (listRes[0][1].toLowerCase() == 'true');
-                    //    }
-                    //}
-                    let ResponseInstructionCancel = this.GetBoolean(myUnifreightMessageM, AmitalGatewayUtil.Instance.DeclarationMessaging.ResponseInstructionCancel)
-                    if (ResponseInstructionCancel) {
-                        this.ToCancell = true;
-                        resolve(this._ControllerOn);
-                        //this.CurrentSession.RealCloseCurrentEditComponent();
-                        return;
-                    }
-                    this._InDisplayModeCFIFILMLockMMessage = "";
-                    let IsAlreadyLock = this.GetBoolean(myUnifreightMessageM, AmitalGatewayUtil.Instance.DeclarationMessaging.ResponseCFIFILMAlreadyLockKey)
-                    if (IsAlreadyLock) {
-                        this._InDisplayModeCFIFILMLockMMessage = "ההצהרה נעולה";
-                        this._UnifaceExclusiveAlreadyLocked = this.InDisplayMode = true;
-                        let responseCFIFILMAlreadyLockMessgae = UnifreightMessageM.GetStringValue(myUnifreightMessageM, AmitalGatewayUtil.Instance.DeclarationMessaging.ResponseCFIFILMAlreadyLockMessgae);
-                        if (!AppTool.IsNullOrEmpty(responseCFIFILMAlreadyLockMessgae)) {
-                            this._InDisplayModeCFIFILMLockMMessage = responseCFIFILMAlreadyLockMessgae;
+                        //let ResponseInstructionCancel = false;
+                        //let listRes = myUnifreightMessageM.Response.filter(itm =>
+                        //    itm[0] == AmitalGatewayUtil.Instance.DeclarationMessaging.ResponseInstructionCancel);
+                        //if (listRes.length > -1 && !AppTool.IsNullOrEmpty(listRes[0])) {
+                        //    if (!AppTool.IsNullOrEmpty(listRes[0][1])) {
+                        //        ResponseInstructionCancel = (listRes[0][1].toLowerCase() == 'true');
+                        //    }
+                        //}
+                        let ResponseInstructionCancel = this.GetBoolean(myUnifreightMessageM, AmitalGatewayUtil.Instance.DeclarationMessaging.ResponseInstructionCancel)
+                        if (ResponseInstructionCancel) {
+                            this.ToCancell = true;
+                            resolve(this._ControllerOn);
+                            //this.CurrentSession.RealCloseCurrentEditComponent();
+                            return;
+                        }
+                        this._InDisplayModeCFIFILMLockMMessage = "";
+                        let IsAlreadyLock = this.GetBoolean(myUnifreightMessageM, AmitalGatewayUtil.Instance.DeclarationMessaging.ResponseCFIFILMAlreadyLockKey)
+                        if (IsAlreadyLock) {
+                            this._InDisplayModeCFIFILMLockMMessage = "ההצהרה נעולה";
+                            this._UnifaceExclusiveAlreadyLocked = this.InDisplayMode = true;
+                            let responseCFIFILMAlreadyLockMessgae = UnifreightMessageM.GetStringValue(myUnifreightMessageM, AmitalGatewayUtil.Instance.DeclarationMessaging.ResponseCFIFILMAlreadyLockMessgae);
+                            if (!AppTool.IsNullOrEmpty(responseCFIFILMAlreadyLockMessgae)) {
+                                this._InDisplayModeCFIFILMLockMMessage = responseCFIFILMAlreadyLockMessgae;
+                            }
+
+                            resolve(this._ControllerOn);
+                            //**********to lock menu buttons and save button --- mohammad bug 30289***************************//
+                            var args: MenuButtonsStateChangedEventArgs = new MenuButtonsStateChangedEventArgs();
+                            args.MenuButtonsStates = {};
+                            args.MenuButtonsStates["SendDeclaration"] = true;
+                            MenuButtonsEvents.MenuButtonsStateChanged.emit(args);
+
+                            if (this.CurrentSession.CurrentEditComponent) {
+                                this.CurrentSession.CurrentEditComponent.IsSaveBtnDisable = true;
+                            }
+                            //**************************************************************************//
+
+                            //this.CurrentSession.RealCloseCurrentEditComponent();
+                            return;
                         }
 
+                        this._UnifaceExclusiveAlreadyLocked = this.InDisplayMode = false;
+                        this._InDisplayModeCFIFILMLockMMessage = "";
                         resolve(this._ControllerOn);
-                        //**********to lock menu buttons and save button --- mohammad bug 30289***************************//
-                        var args: MenuButtonsStateChangedEventArgs = new MenuButtonsStateChangedEventArgs();
-                        args.MenuButtonsStates = {};
-                        args.MenuButtonsStates["SendDeclaration"] = true;
-                        MenuButtonsEvents.MenuButtonsStateChanged.emit(args);
-
-                        if (this.CurrentSession.CurrentEditComponent) {
-                            this.CurrentSession.CurrentEditComponent.IsSaveBtnDisable = true;
-                        }
-                        //**************************************************************************//
-
-                        //this.CurrentSession.RealCloseCurrentEditComponent();
-                        return;
                     }
-                  
-                    this._UnifaceExclusiveAlreadyLocked = this.InDisplayMode = false;
-                    this._InDisplayModeCFIFILMLockMMessage = "";
-                    resolve(this._ControllerOn);
                 }
-            }
             );
         //let unifreightEntity: string = this._CurrentEntity.Direction == "E" ? "BFIFILE" : "CFIFILEM";
         AmitalGatewayUtil.Instance.DeclarationMessaging
             .RaiseCFIFILMLockReturnCFIFILMAlreadyLock(
-            this._CurrentEntity.CustomFileNo, this._CurrentEntity.Id,
-            //this.GetType().Name
+                this._CurrentEntity.CustomFileNo, this._CurrentEntity.Id,
+                //this.GetType().Name
                 "DeclarationEditComponentController",
                 AmitalGatewayUtil.Instance.DeclarationMessaging.UnifreightEntity(this._CurrentEntity.Direction)
 
@@ -191,8 +199,8 @@ export class DeclarationEditComponentController implements IEditComponentControl
                 resolve();
                 return;
             }
-            setTimeout(() => { this.RaiseCFIFILMLockReturnCFIFILMAlreadyLock(resolve);},500)
-            
+            setTimeout(() => { this.RaiseCFIFILMLockReturnCFIFILMAlreadyLock(resolve); }, 500)
+
         });
     }
     OnCloseEditControl(onCallBack?: () => void) {
@@ -207,7 +215,7 @@ export class DeclarationEditComponentController implements IEditComponentControl
             ///&& this._CurrentEntity.IsConvertedDeclaration != true /// yuval +im - not need 
         ) //Yuval Chalup 25.11.2015 TASK-17450 (Add _CurrentEntity.IsConvertedDeclaration != true)
         {
-          
+
 
 
             let sub = AmitalGatewayUtil.Instance.UnifaceRequestArrived
@@ -215,7 +223,7 @@ export class DeclarationEditComponentController implements IEditComponentControl
                     (mess: UnifreightMessageM) => {
                         var IsMatchUnifreightCallbackCommand = (
                             //mess.LogitudeEntity == AmitalGatewayUtil.Instance.DeclarationMessaging.LogitudeEntityDeclaration &&
-                            
+
                             mess.LogitudeEntityNumber == this._CurrentEntity.Id &&
                             mess.UnifreightEntityNumber == this._CurrentEntity.CustomFileNo);
                         if (IsMatchUnifreightCallbackCommand) {
@@ -228,18 +236,18 @@ export class DeclarationEditComponentController implements IEditComponentControl
 
                         }
                     }
-            );
+                );
 
             if (!AppTool.IsNullOrEmpty(onCallBack)) {
                 setTimeout(() => {
                     AmitalGatewayUtil.Instance.DeclarationMessaging.RaiseUnlockCFIFILEM(this._CurrentEntity.CustomFileNo, this._CurrentEntity.Id, this.HaveSaved);
 
-                },300);
+                }, 300);
             } else {
                 AmitalGatewayUtil.Instance.DeclarationMessaging.RaiseUnlockCFIFILEM(this._CurrentEntity.CustomFileNo, this._CurrentEntity.Id, this.HaveSaved);
 
             }
-            
+
 
         } else {
             if (!AppTool.IsNullOrEmpty(onCallBack)) {
@@ -255,7 +263,7 @@ export class DeclarationEditComponentController implements IEditComponentControl
         return myBool;
     }
     HaveSaved: boolean = false;
-    
+
     ToCancell: boolean = false;
 
     private _InDisplayModeCFIFILMLockMMessage: string;
@@ -263,7 +271,7 @@ export class DeclarationEditComponentController implements IEditComponentControl
     public set InDisplayModeMessage(value: string) { this._InDisplayModeCFIFILMLockMMessage = value; }
     public InDisplayMode: boolean = false;
 
-    
+
     public UnifaceStartAsLock(lockMess) {
         this.InDisplayMode = true;
         this.InDisplayModeMessage = lockMess;
@@ -283,13 +291,13 @@ export class DeclarationEditComponentController implements IEditComponentControl
             return true;
         }
         if (itemTabCode != "DCCF") {
-            return false; 
+            return false;
         }
         if (this.ShowDeclarationClassificationComponentTAB) {
             return false;
         }
-        return false; 
-        
+        return false;
+
     }
 
     private _TapagId: string;
