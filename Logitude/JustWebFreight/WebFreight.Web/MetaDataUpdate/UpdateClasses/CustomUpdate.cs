c166a2c1916ddfaefecfa78b34fc0826652afd4b
Sohaib Nemer <sohaib@amital.co.il>
2022-10-31 13:36:34 +0200

    #169751

--- Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/UpdateClasses/CustomUpdate.cs
+++ Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/UpdateClasses/CustomUpdate.cs
@@ -15118,6 +15118,7 @@ namespace WebFreight.Web.MetaDataUpdate.UpdateClasses
             Feature ExceptionReason = tenantFeatures.Where(d => d.Code == "ExceptionReason" && d.FeatureTypeCode == "MENU").FirstOrDefault();
             Feature CustomsReceiptCertificateFromFile = tenantFeatures.Where(d => d.Code == "ReceiptCertificateFromFile" && d.FeatureTypeCode == "MENU").FirstOrDefault();
             Feature CustomsReferantTeam = tenantFeatures.Where(d => d.Code == "ReferantTeam" && d.FeatureTypeCode == "MENU").FirstOrDefault();
+            Feature CustomsSchedulerParam = tenantFeatures.Where(d => d.Code == "SchedulerParam" && d.FeatureTypeCode == "MENU").FirstOrDefault();
 
 
             Feature CustomsPartnerFtpFeature = tenantFeatures.Where(d => d.Code == "CPARTNERFTP" && d.FeatureTypeCode == "MENU").FirstOrDefault();
@@ -15230,6 +15231,7 @@ namespace WebFreight.Web.MetaDataUpdate.UpdateClasses
                 Tenant = 0,
                 MenuTypeCode = "MTC",
                 IndexOfOrder = 62,
+                CustomsSchedulerParam
                 CategoryTypeCode = "CSM",
                 TextCode = "General.MC.Tables.ReAnalysis",
                 HtmlView = "./CustomsModules/CustomsControls/Components/CustomsRequestsSheetsComponent",
@@ -15258,6 +15260,7 @@ namespace WebFreight.Web.MetaDataUpdate.UpdateClasses
             AddMenusTables.AddMenusTable(new MenusTableDetails() { Code = "MCER", Tenant = 0, MenuTypeCode = "MTC", IndexOfOrder = 62, CategoryTypeCode = "CSM", TextCode = "General.MC.Tables.ExceptionReason", Icon = "list", ObjectTableId = tenantObjectTables.Where(o => o.Name == "Customs.ExceptionReason").FirstOrDefault().Id, FeatureId = ExceptionReason.Id, FeatureUniqeCode = ExceptionReason.FeatureUniqeCode }, MenusTablesRepository, tenantMenusTables);
             AddMenusTables.AddMenusTable(new MenusTableDetails() { Code = "MRCF", Tenant = 0, MenuTypeCode = "MTC", IndexOfOrder = 63, CategoryTypeCode = "CSM", TextCode = "General.MC.Tables.ReceiptCertificateFromFile", Icon = "", ObjectTableId = tenantObjectTables.Where(o => o.Name == "Customs.SupplierInvioceItemCertificat").FirstOrDefault().Id, FeatureId = CustomsReceiptCertificateFromFile.Id, FeatureUniqeCode = CustomsReceiptCertificateFromFile.FeatureUniqeCode }, MenusTablesRepository, tenantMenusTables);
             AddMenusTables.AddMenusTable(new MenusTableDetails() { Code = "MRRT", Tenant = 0, MenuTypeCode = "MTC", IndexOfOrder = 64, CategoryTypeCode = "CSM", TextCode = "General.MC.Tables.ReferantTeam", Icon = "", ObjectTableId = tenantObjectTables.Where(o => o.Name == "Customs.ReferantTeam").FirstOrDefault().Id, FeatureId = CustomsReferantTeam.Id, FeatureUniqeCode = CustomsReferantTeam.FeatureUniqeCode }, MenusTablesRepository, tenantMenusTables);
+            AddMenusTables.AddMenusTable(new MenusTableDetails() { Code = "SCPR", Tenant = 0, MenuTypeCode = "MTC", IndexOfOrder = 66, CategoryTypeCode = "CSM", TextCode = "General.MC.Tables.SchedulerParam", Icon = "", ObjectTableId = tenantObjectTables.Where(o => o.Name == "Customs.SchedulerParam").FirstOrDefault().Id, FeatureId = CustomsSchedulerParam.Id, FeatureUniqeCode = CustomsSchedulerParam.FeatureUniqeCode }, MenusTablesRepository, tenantMenusTables);
 
 
 
@@ -15837,6 +15840,7 @@ namespace WebFreight.Web.MetaDataUpdate.UpdateClasses
 
             Feature ReceiptCertificateFromFileFeature = AddRolesAndFeaturesClass.AddFeature(new FeatureDetails() { Code = "ReceiptCertificateFromFile", Packagable = true, ObjectTableId = GeneralObjectTable.Id, Tenant = tenant, NameTextCodeCode = "General.Features.ReceiptCertificateFromFile", NameTextCodeDefaultText = "Receipt Certificate From File", FullLocalDefaultText = "קליטת קובץ אישורים מאיקאה להצהרה", FeatureTypeCode = "MENU" }, FeaturesRepository, textCodeRep, TenantFeatures, TextCodes);
             Feature ReferantTeamFeature = AddRolesAndFeaturesClass.AddFeature(new FeatureDetails() { Code = "ReferantTeam", Packagable = true, ObjectTableId = GeneralObjectTable.Id, Tenant = tenant, NameTextCodeCode = "General.Features.ReferantTeam", NameTextCodeDefaultText = "Referant Teams", FullLocalDefaultText = "צוותים תפעוליים", FeatureTypeCode = "MENU" }, FeaturesRepository, textCodeRep, TenantFeatures, TextCodes);
+            Feature SchedulerParamFeature = AddRolesAndFeaturesClass.AddFeature(new FeatureDetails() { Code = "SchedulerParam", Packagable = true, ObjectTableId = GeneralObjectTable.Id, Tenant = tenant, NameTextCodeCode = "General.Features.SchedulerParam", NameTextCodeDefaultText = "Scheduler Param", FullLocalDefaultText = "Scheduler Params", FeatureTypeCode = "MENU" }, FeaturesRepository, textCodeRep, TenantFeatures, TextCodes);
 
 
 
