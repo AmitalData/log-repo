3ae841c2dee70e40ca142f69349dbad499c6198f
elisheva Nachaysi <elisheva@amital.co.il>
2022-11-04 08:44:59 +0200

    revert test

--- Logitude/Logitude.Server.Tools/StorageService/AzureBlobService.cs
+++ Logitude/Logitude.Server.Tools/StorageService/AzureBlobService.cs
@@ -214,8 +214,7 @@ namespace Logitude.Server.Tools.StorageService
 
         public void Write(byte[] data, BlobFileInfo fileInfo)
         {
-            var y = 0;
-            var x = 3 / y;
+         
             string localPath = null;
             CloudBlobContainer blobContainer = null;
             GetFileBlobContainerInfo(fileInfo, out localPath, out blobContainer);
@@ -234,12 +233,12 @@ namespace Logitude.Server.Tools.StorageService
                         DocumentsFilingRepository documentsFilingRepository = new DocumentsFilingRepository(fileInfo.Tenant);
                         DocumentsFiling documentsFiling = documentsFilingRepository.GetSingleDocumentsFilingByDocumentId(fileInfo.FileName, fileInfo.Tenant);
 
-                        //if (documentsFiling != null)
-                        //{
+                        if (documentsFiling != null)
+                        {
                             KeyValuePair<string, string> metadata = new KeyValuePair<string, string>("Code", documentsFiling.Code);
 
                             blobfile.Metadata.Add(metadata);
-                       // }
+                        }
 
                         AesFunction aesFunction = new AesFunction();
                         data = aesFunction.EncryptData(data, fileInfo.Tenant, fileInfo.AesKey);
