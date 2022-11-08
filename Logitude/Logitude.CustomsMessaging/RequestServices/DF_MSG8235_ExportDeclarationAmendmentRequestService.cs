de8aea9b6799c64b9f4b64c6beb87dafe079e81d
elisheva Nachaysi <elisheva@amital.co.il>
2022-11-08 12:37:40 +0200

    #170610

--- Logitude/Logitude.CustomsMessaging/RequestServices/DF_MSG8235_ExportDeclarationAmendmentRequestService.cs
+++ Logitude/Logitude.CustomsMessaging/RequestServices/DF_MSG8235_ExportDeclarationAmendmentRequestService.cs
@@ -1278,7 +1278,7 @@ namespace Logitude.CustomsMessaging.RequestServices
                 DMExtensions = new DeclarationGoodsShipmentExportConsignmentTransportContractDocumentDMExtensions()
                 {
 
-                    SecondCargoID = SetIDTypeValue<SecondCargoIDType>(consignmentPM.SecondCargoID), // new SecondCargoIDType() { Value = consignmentPM.SecondCargoID },
+                    SecondCargoID = !string.IsNullOrEmpty(consignmentPM.SecondCargoID) ? SetIDTypeValue<SecondCargoIDType>(consignmentPM.SecondCargoID) : new SecondCargoIDType() { Value = "" }, // new SecondCargoIDType() { Value = consignmentPM.SecondCargoID },
                     ThirdCargoID = SetIDTypeValue<ThirdCargoIDType>(consignmentPM.ThirdCargoID) // new ThirdCargoIDType() { Value = consignmentPM.ThirdCargoID }
                 }
             };
@@ -1345,7 +1345,7 @@ namespace Logitude.CustomsMessaging.RequestServices
                 ID = SetIDTypeValue<TransportContractDocumentIdentificationIDType>(consignmentPM.ManifestNumber),
                 DMExtensions = new DeclarationGoodsShipmentImportConsignmentTransportContractDocumentDMExtensions
                 {
-                    SecondCargoID = SetIDTypeValue<SecondCargoIDType>(consignmentPM.SecondCargoID),
+                    SecondCargoID = !string.IsNullOrEmpty(consignmentPM.SecondCargoID) ? SetIDTypeValue<SecondCargoIDType>(consignmentPM.SecondCargoID) : new SecondCargoIDType() { Value = "" },
                     ThirdCargoID = SetIDTypeValue<ThirdCargoIDType>(consignmentPM.ThirdCargoID),
                 }
             };
