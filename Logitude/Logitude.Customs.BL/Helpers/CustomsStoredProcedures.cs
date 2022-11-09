21ed0ce28821a879a4adf9903e1538eafba9b40a
Sohaib Nemer <sohaib@amital.co.il>
2022-11-09 16:34:24 +0200

    delete commandtext

--- Logitude/Logitude.Customs.BL/Helpers/CustomsStoredProcedures.cs
+++ Logitude/Logitude.Customs.BL/Helpers/CustomsStoredProcedures.cs
@@ -466,7 +466,6 @@ AS */
                 {
                     OracleCommand cmd = new OracleCommand();
                     cmd.Connection = cn;
-                    cmd.CommandText =
                     cmd.CommandTimeout = 1000000;
                     DbContextBaseUtil.GetStoredProcedureName("q_Deletecommlogs", LogitudeDBSchema.LOGITUDE_MAIN,
                     cmd.Connection.ConnectionString);
