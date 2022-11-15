8b8034ab50ff8f99eab32ee28883853c369cbbbb
Shoshana Greenwald <shoshana@amital.co.il>
2022-11-15 15:20:44 +0200

    #169746

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationSupplierInvoice/Components/SupplierInvoices/SupplierInvoiceGeneralTabComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationSupplierInvoice/Components/SupplierInvoices/SupplierInvoiceGeneralTabComponent.ts
@@ -2963,10 +2963,14 @@ export class SupplierInvoiceGeneralTabComponent extends BaseComponent implements
                                     if (IsMatchUnifreightCallbackCommand) {
                                         sub.unsubscribe();
                                         SessionLocator.SelectedSession.StopBusyIndicator();
-                                        let sBool = UnifreightMessageM.GetStringValue(mess, AmitalGatewayUtil.Instance.DeclarationMessaging.UnifreightResponseStatus);
+                                        let sDefineToInsure = "aaa"; sDefineToInsure = UnifreightMessageM.GetStringValue(unifreightMessageM, "Response.DefineToInsure");
+                                        let sCurrency = "bbb"; sCurrency = UnifreightMessageM.GetStringValue(unifreightMessageM, "Response.Currency");
+
+                                        //let sBool = UnifreightMessageM.GetStringValue(mess, AmitalGatewayUtil.Instance.DeclarationMessaging.UnifreightResponseStatus);
                                         alert("ApprovalToInsurance");
-                                        alert(sBool);
-                                        this.ActivateInsurance(mess)
+                                        alert(sDefineToInsure);
+                                        alert(sCurrency);
+                                        this.ActivateInsurance(mess,sDefineToInsure)
                                        // SessionLocator.SelectedSession.CurrentListComponent.DoRefresh();
                                     }
                                 }
@@ -2999,9 +3003,9 @@ export class SupplierInvoiceGeneralTabComponent extends BaseComponent implements
             }
     }
 
-    ActivateInsurance(messRes) {
+    ActivateInsurance(messRes,sDefineToInsure) {
         alert("enter ActivateInsurance");
-        alert(messRes.Response["DefineToInsure"].value);
+        alert(sDefineToInsure);
        if(messRes.Response["DefineToInsure"].value=="Yes") {
          var confirm = new ConfirmWindow();
       
