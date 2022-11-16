4ea59b1d297ec7230cae94839395aa1d404ef02d
Frida Lok <frida@amital.co.il>
2022-11-16 17:39:56 +0200

    #169248

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationOthers/Components/CloseDeclaration/ExportDeclarationClosingDataComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationOthers/Components/CloseDeclaration/ExportDeclarationClosingDataComponent.ts
@@ -492,22 +492,22 @@ export class ExportDeclarationClosingDataComponent extends BaseComponent {
         let sub = AmitalGatewayUtil.Instance.UnifaceRequestArrived
             .subscribe(
                 (mess: UnifreightMessageM) => {
-                    alert("subscribe " + mess.LogitudeEntityNumber + ' ' + this.DecPM.Id + ' ' + mess.LogitudeViewModel);
-                    alert("response "+ mess.Response)
                     var IsMatchUnifreightCallbackCommand = (
-                        // mess.LogitudeEntity == AmitalGatewayUtil.Instance.DeclarationMessaging.LogitudeEntityDeclaration &&
                         mess.LogitudeEntityNumber == this.DecPM.Id &&
                         mess.LogitudeViewModel == "ExportDeclarationClosingDataComponent.ts");
                     if (IsMatchUnifreightCallbackCommand) {
                         sub.unsubscribe();
                         SessionLocator.SelectedSession.StopBusyIndicator();
                         let LoadPort = UnifreightMessageM.GetStringValue(mess,"LoadPort");
-                        // this.MainAWB =mess.Response['Mawb'].value;
-                        // this.Smp = mess.Response['Hawb'].value ;
-                        this.ChargingSite = LoadPort ; 
-                        // this.FlightDate =mess.Response['FlightDate'].value ;
-                        alert("sBool");
-                        alert(LoadPort);
+                        let Mawb = UnifreightMessageM.GetStringValue(mess,"Mawb");
+                        let Hawb = UnifreightMessageM.GetStringValue(mess,"Hawb");
+                        let FlightDate = UnifreightMessageM.GetStringValue(mess,"FlightDate");
+
+                        Mawb? this.MainAWB = Mawb : '';
+                        Hawb? this.Smp = Hawb : '';
+                        LoadPort? this.ChargingSite = LoadPort : ''; 
+                        FlightDate? this.FlightDate = new Date(FlightDate) : '';
+
                         SessionLocator.SelectedSession.CurrentListComponent.DoRefresh();
                     }
                 }
