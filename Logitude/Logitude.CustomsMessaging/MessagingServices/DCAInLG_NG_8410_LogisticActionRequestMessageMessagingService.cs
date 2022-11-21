1e0eb5264b084cb7c3364b51684063bb0b02d0b3
Frida Lok <frida@amital.co.il>
2022-11-21 15:09:56 +0200

    #171414

--- Logitude/Logitude.CustomsMessaging/MessagingServices/DCAInLG_NG_8410_LogisticActionRequestMessageMessagingService.cs
+++ Logitude/Logitude.CustomsMessaging/MessagingServices/DCAInLG_NG_8410_LogisticActionRequestMessageMessagingService.cs
@@ -30,6 +30,8 @@ namespace Logitude.CustomsMessaging.MessagingServices
             var logisticActionRequestPM = new LogisticActionRequestQueryService(RequestParams.Tenant).GetSingle(RequestParams.LogisticActionRequestId, false, false);
 
             logisticActionRequestPM.OperationalStatus = "2";
+            logisticActionRequestPM.ResponseStatusCode = "2";
+
             logisticActionRequestPM.ChangeSetOp = ChangeSetOperation.Update;
             new LogisticActionRequestUpdateService(dbContext, new Dictionary<string, IContext>(), RequestParams.Tenant)
                 .Update(logisticActionRequestPM, true);
