1c079c434f55729076fd7c0dba64232bcb62cd7c
Libby Hasid <libby@amital.co.il>
2022-11-27 10:13:28 +0200

    #171245

--- Logitude/Logitude.Customs.Data/EntityLists/Generated/ServersNameList.Generated.cs
+++ Logitude/Logitude.Customs.Data/EntityLists/Generated/ServersNameList.Generated.cs
@@ -24,6 +24,8 @@ namespace Logitude.Customs.Data.EntityLists
        public string ServerName  { get; set; }
        [DataMember]
        public string ServiceName  { get; set; }
+       [DataMember]
+       public bool Delete  { get; set; }
    }
 
 }
