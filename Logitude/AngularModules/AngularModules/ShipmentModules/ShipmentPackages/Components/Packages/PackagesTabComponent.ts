a5ad616ef10243b77512f51f18e4b7ebb83615e0
Wesam Amarneh <wesam@logitudeworld.com>
2022-11-27 14:25:08 +0200

    Cross Docks : Create Cross Docks Releases from  Generating Packages screen
    Related Work Items: #171109

--- Logitude/AngularModules/AngularModules/ShipmentModules/ShipmentPackages/Components/Packages/PackagesTabComponent.ts
+++ Logitude/AngularModules/AngularModules/ShipmentModules/ShipmentPackages/Components/Packages/PackagesTabComponent.ts
@@ -1078,11 +1078,11 @@ export class PackagesTabComponent extends BaseComponent implements OnInit, OnDes
             else {
                 this.GenerateButtonLabel = TextCodeTranslator.Translate("Shipment.O.GenerateFromOrderPackages").replace("%Number", count.toString());
                 if (this.EntityPM.ShipmentTypeId == "FCL" || this.EntityPM.ShipmentTypeId == "FCLD"|| this.EntityPM.ShipmentTypeId == "FTL") {
-                    this.IsGenerateButtonVisible = true;
+                    this.IsGenerateButtonVisible = this.EntityPM.ShipmentOrderPackages.length > 0 ? true : false;
                 } else {
                     this.IsGenerateButtonVisible = true;
                 }
-                this.IsGenerateButtonEnabled = true;
+                this.IsGenerateButtonEnabled = this.EntityPM.BookingNumberOfPackages > 0 ? true : false;
             }
         }
 
