6cffb1244fcf9908cc0a019009767a4fdff53ea6
Wesam Amarneh <wesam@logitudeworld.com>
2022-11-29 15:01:56 +0200

    Connected documents with ocean shipment
    Related Work Items: #171753

--- Logitude/JustWebFreight/WebFreight.Web/App_Code/ImporterShipmentDocumentsController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/App_Code/ImporterShipmentDocumentsController.cs
@@ -753,6 +753,7 @@ namespace WebFreight.Web.App_Code
         private APIException MapEntityAMToEntityPM(DocumentsFilingAM EntityAM, DocumentsFilingPM EntityPM)
         {
             APIException Responce = new APIException();
+            EntityAM.ObjectTableName = EntityAM.ObjectTableName == "ShipmentOrder" ? "Shipment" : EntityAM.ObjectTableName;
             if (!string.IsNullOrEmpty(EntityAM.ObjectTableName))
             {
                 ObjectTableRepository objectTabelRepository = new ObjectTableRepository(EntityAM.ImporterTenant);
