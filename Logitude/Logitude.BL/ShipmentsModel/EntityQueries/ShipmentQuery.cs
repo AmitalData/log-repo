e89b21609d0579e0c6cd730790a77be1826df311
Samar Abu Eid <Samar@logitudeworld.com>
2022-11-30 11:11:45 +0200

    #171901
    Related Work Items: #171815, #171901

--- Logitude/Logitude.BL/ShipmentsModel/EntityQueries/ShipmentQuery.cs
+++ Logitude/Logitude.BL/ShipmentsModel/EntityQueries/ShipmentQuery.cs
@@ -358,6 +358,21 @@ namespace Logitude.BL.ShipmentsModel.EntityQueries
                 {
                     shipmentPM.MainCarriageFromPortId = masterData.MainCarriageFromPortId;
                     shipmentPM.MainCarriageToPortId = masterData.MainCarriageToPortId;
+
+                    if (!string.IsNullOrEmpty(shipmentPM.MainCarriageFromPortId))
+                    {
+                        PortPM mainCarriageFromPort = portQuery.GetSinglePM(shipmentPM.MainCarriageFromPortId, masterData.Tenant);
+                        shipmentPM.MainCarriageFromPortCode = mainCarriageFromPort?.Code;
+                        shipmentPM.MainCarriageFromPortName = mainCarriageFromPort?.EnglishName;
+                    }
+
+                    if (!string.IsNullOrEmpty(shipmentPM.MainCarriageToPortId))
+                    {
+                        PortPM mainCarriageToPort = portQuery.GetSinglePM(shipmentPM.MainCarriageToPortId, masterData.Tenant);
+                        shipmentPM.MainCarriageToPortCode = mainCarriageToPort?.Code;
+                        shipmentPM.MainCarriageToPortName = mainCarriageToPort?.EnglishName;
+                    }
+
                     shipmentPM.MainCarriageFromPartnerId = masterData.MainCarriageFromPartnerId;
                     shipmentPM.MainCarriageFromAddressId = masterData.MainCarriageFromAddressId;
                     shipmentPM.MainCarriageToPartnerId = masterData.MainCarriageToPartnerId;
