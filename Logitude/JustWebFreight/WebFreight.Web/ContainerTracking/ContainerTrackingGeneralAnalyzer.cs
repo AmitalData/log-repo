7d32e0061cdffb557fa518cac60fa14e9775ad09
Samar Abu Eid <Samar@logitudeworld.com>
2022-12-06 15:24:19 +0200

    #172083

--- Logitude/JustWebFreight/WebFreight.Web/ContainerTracking/ContainerTrackingGeneralAnalyzer.cs
+++ Logitude/JustWebFreight/WebFreight.Web/ContainerTracking/ContainerTrackingGeneralAnalyzer.cs
@@ -192,7 +192,7 @@ namespace WebFreight.Web.ContainerTracking
                     manager.SetShipment(shipment);
                     MapContainersExternalData(container);
                     manager.Update(IsUpdateContainerAllowed(container), IsUpdateShipmentAllowed(shipment));
-                    analyz = IsUpdateContainerAllowed(container) && IsUpdateShipmentAllowed(shipment);
+                    analyz = IsUpdateContainerAllowed(container);
                 }
 
                 SetComunicationLogDone(comunicationLog, analyz);
