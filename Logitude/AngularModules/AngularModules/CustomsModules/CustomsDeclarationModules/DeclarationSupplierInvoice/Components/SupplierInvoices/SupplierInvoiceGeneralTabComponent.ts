17f5ddcbe92531c958667b9421449b512df507c3
Shoshana Greenwald <shoshana@amital.co.il>
2022-12-08 17:45:40 +0200

    #171453

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationSupplierInvoice/Components/SupplierInvoices/SupplierInvoiceGeneralTabComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationSupplierInvoice/Components/SupplierInvoices/SupplierInvoiceGeneralTabComponent.ts
@@ -3174,8 +3174,10 @@ export class SupplierInvoiceGeneralTabComponent extends BaseComponent implements
 
 
     InspectionRequest() {
+       
 
-        var IsActivateInsurance = FeatureLocator.HasFeaturePermession("Customs.Declaration", "ISACTIVATEINSURANCE")
+        var IsActivateInsurance = SessionLocator.FeatureToggles.filter(d => d.ToggleCode == "AIN")[0]? true : false;       
+        
         if(!IsActivateInsurance) return;
 
         if (!this.EntityPM.IsDirty) return;
