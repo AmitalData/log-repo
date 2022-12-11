ce509b67e296e01da0ad63696534ca9dc32d2329
Itzik Moshelian <itzikm@amitaldataltd.onmicrosoft.com>
2022-12-11 16:07:50 +0200

    #172097 FIX CONFLICT

--- Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/GeneratedUpdate/EntityUpdateClasses/CustomsSettingUpdateClass.cs
+++ Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/GeneratedUpdate/EntityUpdateClasses/CustomsSettingUpdateClass.cs
@@ -1850,7 +1850,7 @@ namespace WebFreight.Web.MetaDataUpdate.GeneratedUpdate.EntityUpdateClasses
 					  						CanAutomateSetValue =  false,
 					  						DisplayInAutomationAsEnitity =  false,
 					  						DisplayInRequiredFields =  false,
-					  						DisplayInRequiredFields =  false,
+					  						
 					  						HasTemplate =  false,
 					  						IsCustom =  false,
 					  						EnableFullscreenTextBox =  false,
