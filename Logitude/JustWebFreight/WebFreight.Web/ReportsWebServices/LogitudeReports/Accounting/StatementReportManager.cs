d5d0eb7faecc73b8a9f80dee8b52c08a59864d6d
Aseel Zbaidah <aseel@logitudeworld.com>
2022-12-11 14:02:42 +0200

    #171080

--- Logitude/JustWebFreight/WebFreight.Web/ReportsWebServices/LogitudeReports/Accounting/StatementReportManager.cs
+++ Logitude/JustWebFreight/WebFreight.Web/ReportsWebServices/LogitudeReports/Accounting/StatementReportManager.cs
@@ -666,7 +666,12 @@ namespace WebFreight.Web.ReportsWebServices.LogitudeReports.Accounting
                                     IQueryable<ARInvoice> iQueryable_ARInvoice = aRInvoiceRepository.GetUnpaidAndDraftARInvoices(tenant);
                                     IQueryable<ARPayment> iQueryable_ARPayment = aRPaymentRepository.GetOpenedARPayments(tenant);
 
+                                    List <ARInvoice> tt = iQueryable_ARInvoice.ToList();
+
+
+
                                     iQueryable_ARInvoice = iQueryable_ARInvoice.Where(d => !d.IsConstituentInvoice);
+                                    tt = iQueryable_ARInvoice.ToList();
 
                                     if (!includeDraftInvoices)
                                     {
@@ -715,7 +720,9 @@ namespace WebFreight.Web.ReportsWebServices.LogitudeReports.Accounting
 
                                         }
                                     }
-                                
+
+                                    tt = iQueryable_ARInvoice.ToList();
+
                                     list_ARInvoices = this.BuildList_ARInvoice(iQueryable_ARInvoice);
                                     list_ARPayments = this.BuildList_ARPayment(iQueryable_ARPayment);
                                     break;
