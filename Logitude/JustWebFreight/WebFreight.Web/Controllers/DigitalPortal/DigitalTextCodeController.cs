75e2bc62c229f48e723a0ff5c0b3c5bd4c9ea23a
Baraa Deek <baraa@logitudeworld.com>
2022-12-12 09:28:03 +0200

    add defaukt text

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/DigitalPortal/DigitalTextCodeController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/DigitalPortal/DigitalTextCodeController.cs
@@ -55,7 +55,7 @@ namespace WebFreight.Web.Controllers.DigitalPortal
 
                     if (temp != null)
                     {
-                        temp.DisplayText = item.DisplayText;
+                        temp.DefaultText = item.DisplayText;
                     }
                 }
 
--- Logitude/Simplog.Server.Infrastructure/DataContracts/Models/DigitalTextCodeObject.cs
+++ Logitude/Simplog.Server.Infrastructure/DataContracts/Models/DigitalTextCodeObject.cs
@@ -2,6 +2,7 @@
 {
     public class DigitalTextCodeObject
     {
+        public string DefaultText { get; set; }
         public string DisplayText { get; set; }
         public string Code { get; set; }
     }
