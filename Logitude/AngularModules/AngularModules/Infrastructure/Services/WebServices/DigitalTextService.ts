f2b1e1830580d309deb0a570ebc90a97df550572
Maheera Jazi <Maheera@logitudeworld.com>
2022-12-13 11:57:30 +0200

    fix

--- Logitude/AngularModules/AngularModules/Infrastructure/Services/WebServices/DigitalTextService.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Services/WebServices/DigitalTextService.ts
@@ -86,7 +86,7 @@ export class DigitalTextService {
 
     UpdateDigitalTextCodes(labels: DigitalTextCodeUpdateModel) {
         return defer(() => {
-            return this._http.post(this._apiUrl + "/UpdateFeildPermission", JSON.stringify(labels), ServiceHelper.GetHttpHeaders())
+            return this._http.post(this._apiUrl + "/UpdateDigitalTextCodes", JSON.stringify(labels), ServiceHelper.GetHttpHeaders())
                 .pipe(
                     map((response: any) => {
                         return response.body;
