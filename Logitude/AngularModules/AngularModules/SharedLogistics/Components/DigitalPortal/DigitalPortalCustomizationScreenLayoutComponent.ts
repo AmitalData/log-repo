2d051d482e7366fa33e27d9f75aa446d24a397e3
Maheera Jazi <Maheera@logitudeworld.com>
2022-12-15 10:06:06 +0200

    #172540
    Related Work Items: #172540, #172782

--- Logitude/AngularModules/AngularModules/SharedLogistics/Components/DigitalPortal/DigitalPortalCustomizationScreenLayoutComponent.ts
+++ Logitude/AngularModules/AngularModules/SharedLogistics/Components/DigitalPortal/DigitalPortalCustomizationScreenLayoutComponent.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 
 export class DigitalPortalCustomizationScreenLayoutComponent {
 
-    public ScreensC: Array<ScreenItem> = [];
+   
     constructor() {
 
     }
