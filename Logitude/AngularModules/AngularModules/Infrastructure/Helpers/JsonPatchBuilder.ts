7e5e3d1101735bf9cd795ac72bb56b3b5f8d45fc
Abd Al-Rahman Malakh <Abd@logitudeworld.com>
2022-12-18 15:16:57 +0200

    172692
    Related Work Items: #172692

--- Logitude/AngularModules/AngularModules/Infrastructure/Helpers/JsonPatchBuilder.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Helpers/JsonPatchBuilder.ts
@@ -4,7 +4,6 @@ import { CloneEntityPM, CloneObject } from './SafeCloneDeep';
 export class JsonPatchBuilder {
     private LeftObject: any;
     private RightObject: any;
-    private Invertible: boolean;
 
     private ObjectPath: any = require("object-path");
 
@@ -19,14 +18,13 @@ export class JsonPatchBuilder {
         "tEU"
     ];
 
-    constructor(leftObject: any, rightObject: any, invertible: boolean = false) {
-        this.initialize(leftObject, rightObject, invertible);
+    constructor(leftObject: any, rightObject: any) {
+        this.initialize(leftObject, rightObject);
     }
 
-    private initialize(leftObject: any, rightObject: any, invertible: boolean) {
+    private initialize(leftObject: any, rightObject: any) {
         this.LeftObject = leftObject;
         this.RightObject = rightObject;
-        this.Invertible = invertible;
         this.InvalidProperties = this.InvalidProperties.map(p => { return p ? p.toLowerCase() : null; }).filter(p => p !== null);
     }
 
@@ -34,11 +32,12 @@ export class JsonPatchBuilder {
         if (this.LeftObject && this.RightObject) {
             let clonedLeftObject = CloneEntityPM(this.LeftObject);
             let clonedRightObject = CloneEntityPM(this.RightObject);
-            let patch = compare(clonedLeftObject, clonedRightObject, this.Invertible);
+            let patch = compare(clonedLeftObject, clonedRightObject);
             let cleanedPatch = this.cleanPatch(patch);
-            let finalPatch = this.appendTestOperations(cleanedPatch);
-            console.log(finalPatch);
-            return finalPatch;
+            let modifiedPatch = this.modifyAddToListOperations(cleanedPatch);
+            let patchWithTestOperations = this.addTestOperations(modifiedPatch);
+            //console.log(patchWithTestOperations);
+            return patchWithTestOperations;
         }
         return null;
     }
@@ -62,48 +61,82 @@ export class JsonPatchBuilder {
         return false;
     }
 
-    private appendTestOperations(patch: Operation[]) {
+    private modifyAddToListOperations(patch: Operation[]) {
         if (patch && patch.length > 0) {
-
-            let clonedPatch: Operation[] = CloneObject(patch);
-
-            clonedPatch.forEach((operation: Operation, operationIndex: number) => {
-                let pathIndexRegex = /\/(\d+)/;
-                if (operation.op && operation.op === "replace" && operation.path && pathIndexRegex.test(operation.path)) {
-
-                    let testOperations: Operation[] = [];
-
-                    let operationPaths = operation.path.split(pathIndexRegex);
-
-                    operationPaths.forEach((operationPath: string, operationPathIndex: number) => {
-                        if (this.isIntegerNumber(operationPath)) {
-                            let keyField = "id";
-                            let path = operationPaths.slice(0, operationPathIndex + 1).map(p => { return p.replace(/\//g, ""); }).join(".") + "." + keyField;
-                            let pathValue = this.ObjectPath.get(this.LeftObject, path);
-                            let testOperation: Operation = { op: "test", path: ("/" + path.replace(/\./g, "/")), value: (pathValue ? pathValue.toString() : null) };
-                            testOperations.push(testOperation);
-                        }
-                    });
-
-
-                    testOperations.forEach((testOperation: Operation, testOperationIndex: number) => {
-                        patch.splice((operationIndex + testOperationIndex), 0, testOperation);
-                    });
-
+            patch.filter(o => o.op === "add" && o.path).forEach((operation: Operation) => {
+                let operationPathProperties = operation.path.split("/");
+                let lastOperationPathProperty = operationPathProperties[operationPathProperties.length - 1];
+                if (this.isIntegerNumber(lastOperationPathProperty)) {
+                    operation.path = operationPathProperties.slice(0, -1).join("/") + "/-";
                 }
             });
-
         }
-
         return patch;
+    }
+
+    private addTestOperations(patch: Operation[]) {
+        if (patch && patch.length > 0) {
+            let testOperations: Operation[] = this.buildPatchTestOperations(patch);
+            testOperations.forEach((testOperation: Operation, testOperationIndex: number) => {
+                patch.splice(testOperationIndex, 0, testOperation);
+            });
+        }
+        return patch;
+    }
+
+    private buildPatchTestOperations(patch: Operation[]) {
+        if (patch && patch.length > 0) {
+            let testOperations: Operation[] = [];
+            let clonedPatch: Operation[] = CloneObject(patch);
+            clonedPatch.forEach((operation: Operation) => {
+                let pathIndexRegex = /\/(\d+)/;
+                if (operation.path && pathIndexRegex.test(operation.path)) {
+                    let operationPaths = operation.path.split(pathIndexRegex);
+                    operationPaths.forEach((operationPath: string, operationPathIndex: number) => {
+                        if (this.isIntegerNumber(operationPath)) {
+                            let pathProperties = operationPaths.slice(0, operationPathIndex + 1).map(p => { return p.replace(/\//g, ""); });
+                            let pathTestOperations = this.buildPathTestOperations(pathProperties);
+                            pathTestOperations.forEach((testOperation: Operation) => {
+                                if (testOperations.filter(o => o.path === testOperation.path).length === 0) {
+                                    testOperations.push(testOperation);
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+            return testOperations;
+        }
+        return [];
+    }
+
+    private buildPathTestOperations(pathProperties: string[]) {
+        if (pathProperties && pathProperties.length >= 2) {
+            let testOperations: Operation[] = [];
+            let pathProperty = pathProperties[pathProperties.length - 2];
+            let keyFields = this.getPathPropertyKeyFields(pathProperty);
+            keyFields.forEach((keyField: string) => {
+                let path = pathProperties.join(".") + "." + keyField;
+                let pathValue = this.ObjectPath.get(this.LeftObject, path);
+                let testOperation: Operation = { op: "test", path: ("/" + path.replace(/\./g, "/")), value: (pathValue || null) };
+                testOperations.push(testOperation);
+            });
+            return testOperations;
+        }
+        return [];
+    }
+
+    private getPathPropertyKeyFields(pathProperty: string) {
+        switch (pathProperty?.toLowerCase()) {
+            case "shipmentpackageitems": { return ["packageId", "lineNumber"]; }
+            default: { return ["id"]; }
+        }
     }
 
     private isIntegerNumber(value: string | number) {
         if (value && value.toString().includes(".")) {
             return false;
         }
-        return ((value != null) &&
-            (value !== '') &&
-            !isNaN(Number(value.toString())));
+        return (value && value !== "" && !isNaN(Number(value.toString())));
     }
 }
