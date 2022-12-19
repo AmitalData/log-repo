e27f43a24cbd5fa1ac0b497aa708bef5b78707ed
Ahmad A. Abdalghafoor <ahmad.abdulgafoor@gmail.com>
2022-12-19 08:47:05 +0200

    fixing

--- Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/GeneratedUpdate/ShipmentsModel/EntityUpdateClasses/ShipmentStoragePricingUpdateClass.cs
+++ Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/GeneratedUpdate/ShipmentsModel/EntityUpdateClasses/ShipmentStoragePricingUpdateClass.cs
@@ -85,7 +85,7 @@ namespace WebFreight.Web.MetaDataUpdate.GeneratedUpdate.ShipmentsModel.EntityUpd
 {
    public class ShipmentStoragePricingUpdateClass
    {  		
-		public const string HashString = "ed94e27106a7fcf4dc3abbe63ee2aabf";
+		public const string HashString = "ed94e27106a7fcf4dc3abbe63asaaabf";
 	    public void AddObjectTable(Dictionary<string, ObjectTable> objectTables, Dictionary<string, TextCode> textCodes,ObjectTableRepository ObjectTableRepository,TextCodeRepository TextCodeRepository)
         {                     
             
@@ -307,7 +307,7 @@ namespace WebFreight.Web.MetaDataUpdate.GeneratedUpdate.ShipmentsModel.EntityUpd
 					  						InActive =  false,
 					  						DisplayLongName =  false,
 					  						FullFieldLable =  "ShipmentId",
-					  						DefaultText =  "Shipment",
+					  						DefaultText =  "ShipmentId",
 					  						IsForeignKey =  true,
 					  						ForeignEntity =  "Shipment",
 					  						NavigationPropertyName =  "Shipment",
