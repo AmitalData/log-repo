d1bb204c28ceded63b196faacfed8a5f59ca8cab
elisheva Nachaysi <elisheva@amital.co.il>
2022-12-19 14:08:07 +0200

    fix amendment

--- Logitude/Logitude.Customs.Data/Repsitories/DeclarationRepository.cs
+++ Logitude/Logitude.Customs.Data/Repsitories/DeclarationRepository.cs
@@ -530,7 +530,7 @@ namespace Logitude.Customs.Data.Repsitories
 
 
 
-        public Declaration GetDeclarationByFunctionalReferenceID(string functionalReferenceID,string agentFileReferenceID, int tenant)
+        public Declaration GetDeclarationByFunctionalReferenceID(string functionalReferenceID, int tenant)
         {
             //Declaration declarationParent = (from a in context.Declarations
             //                           where declarationNumber == a.DeclarationNumber
@@ -538,7 +538,7 @@ namespace Logitude.Customs.Data.Repsitories
 
 
             Declaration declaration = (from a in context.Declarations
-                                       where functionalReferenceID == a.AmendmentRequestNumber && a.Tenant == tenant && a.CustomFileNo == agentFileReferenceID
+                                       where functionalReferenceID == a.AmendmentRequestNumber && a.Tenant == tenant 
                                        select a).FirstOrDefault();
 
             return declaration;
