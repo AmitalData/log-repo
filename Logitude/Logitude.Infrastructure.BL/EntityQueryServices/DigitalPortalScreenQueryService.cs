4272fcc851f5f99951bd874d297e385e69b1b9df
Baraa Deek <baraa@logitudeworld.com>
2022-12-20 11:29:58 +0200

    add update digital screens

--- Logitude/Logitude.Infrastructure.BL/EntityQueryServices/DigitalPortalScreenQueryService.cs
+++ Logitude/Logitude.Infrastructure.BL/EntityQueryServices/DigitalPortalScreenQueryService.cs
@@ -1,5 +1,9 @@
-﻿using Logitude.Infrastructure.Data.EntityLists;
+﻿using Logitude.Infrastructure.BL.EntityPMs;
+using Logitude.Infrastructure.BL.EntityUpdateServices;
+using Logitude.Infrastructure.Data;
+using Logitude.Infrastructure.Data.EntityLists;
 using Logitude.Infrastructure.Data.Repsitories;
+using Simplog.Server.Infrastructure;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +14,7 @@ namespace Logitude.Infrastructure.BL.EntityQueryServices
 {
     public partial class DigitalPortalScreenQueryService
     {
-        public List<DigitalPortalScreenList> GetDigitalPortalScreenQuery(int tenant, string objectTableId, string screenCode = "")
+        public List<DigitalPortalScreenList> GetDigitalPortalScreensQuery(int tenant, string objectTableId, string screenCode = "")
         {
             var digitalPortalScreenRepository = new DigitalPortalScreenRepository(tenant);
 
@@ -29,6 +33,70 @@ namespace Logitude.Infrastructure.BL.EntityQueryServices
                                                                     })
                                                                     .ToList();
             return digitalPortalScreens;
+        }
+        
+        public DigitalPortalScreenList GetDigitalPortalScreenQuery(int tenant, string objectTableId, string screenCode = "")
+        {
+            var digitalPortalScreenRepository = new DigitalPortalScreenRepository(tenant);
+
+            var digitalPortalScreens = digitalPortalScreenRepository.GetDigitalPortalScreens(tenant, objectTableId, screenCode)
+                                                                    .Select(x => new DigitalPortalScreenList
+                                                                    {
+                                                                        Id = x.Id,
+                                                                        Tenant = x.Tenant,
+                                                                        Name = x.Name,
+                                                                        CreateDate = x.CreateDate,
+                                                                        UpdateDate = x.UpdateDate,
+                                                                        Content = x.Content,
+                                                                        DraftContent = x.DraftContent,
+                                                                        ScreenCode = x.ScreenCode,
+                                                                        ObjectTableId = x.ObjectTableId
+                                                                    })
+                                                                    .FirstOrDefault(a => a.Tenant == tenant);
+            return digitalPortalScreens;
+        }
+
+        public void UpdateDigitalPortalScreen(DigitalPortalScreenList digitalPortalScreenUpdateObject)
+        {
+            if (string.IsNullOrEmpty(digitalPortalScreenUpdateObject.Id))
+            {
+                var entityPm = new DigitalPortalScreenPM
+                {
+                    Name = digitalPortalScreenUpdateObject.Name,
+                    ObjectTableId = digitalPortalScreenUpdateObject.ObjectTableId,
+                    Tenant = digitalPortalScreenUpdateObject.Tenant,
+                    ScreenCode = digitalPortalScreenUpdateObject.ScreenCode,
+                    Content = digitalPortalScreenUpdateObject.Content,
+                    DraftContent = digitalPortalScreenUpdateObject.DraftContent,
+                    CreateDate = digitalPortalScreenUpdateObject.CreateDate,
+                    UpdateDate = digitalPortalScreenUpdateObject.UpdateDate
+                };
+
+                entityPm.ChangeSetOp = ChangeSetOperation.Insert;
+                var contextData = InfrastructureContext.GetContext(entityPm.Tenant);
+                var service = new DigitalPortalScreenUpdateService(contextData, new Dictionary<string, IContext>(), entityPm.Tenant);
+                service.Update(entityPm, true);
+            }
+            else
+            {
+                var entityPm = new DigitalPortalScreenPM
+                {
+                    Id = digitalPortalScreenUpdateObject.Id,
+                    Name = digitalPortalScreenUpdateObject.Name,
+                    ObjectTableId = digitalPortalScreenUpdateObject.ObjectTableId,
+                    Tenant = digitalPortalScreenUpdateObject.Tenant,
+                    Content = digitalPortalScreenUpdateObject.Content,
+                    DraftContent = digitalPortalScreenUpdateObject.DraftContent,               
+                    ScreenCode = digitalPortalScreenUpdateObject.ScreenCode,
+                    CreateDate = digitalPortalScreenUpdateObject.CreateDate,
+                    UpdateDate = digitalPortalScreenUpdateObject.UpdateDate                    
+                };
+
+                var contextData = InfrastructureContext.GetContext(entityPm.Tenant);
+                var service = new DigitalPortalScreenUpdateService(contextData, new Dictionary<string, IContext>(), entityPm.Tenant);
+                entityPm.ChangeSetOp = ChangeSetOperation.Update;
+                service.Update(entityPm, true);
+            }
         }
     }
 }
---
+++ Logitude/Simplog.Server.Infrastructure/DataContracts/Models/DigitalPortalScreenUpdateModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Simplog.Server.Infrastructure.DataContracts.Models
+{
+    public class DigitalPortalScreenUpdateModel
+    {
+        public string Id { get; set; }
+        public int Tenant { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public string ObjectTableId { get; set; }
+        public string ScreenCode { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public string DraftContent { get; set; }
+        public bool IsDraft { get; set; }
+    }
+}
