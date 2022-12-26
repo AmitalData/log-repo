05c47a8a522503e6dfd620224ac7d97abd20e6da
Ahmad A. Abdalghafoor <ahmad.abdulgafoor@gmail.com>
2022-12-26 13:25:17 +0200

    Fixing Insert at cursor - screen layout

--- Logitude/AngularModules/AngularModules/SharedLogistics/Components/DigitalPortal/DigitalPortalCustomizationScreenLayoutComponent.ts
+++ Logitude/AngularModules/AngularModules/SharedLogistics/Components/DigitalPortal/DigitalPortalCustomizationScreenLayoutComponent.ts
@@ -22,7 +22,7 @@ export class DigitalPortalCustomizationScreenLayoutComponent {
     TextAreaInputId: string = Guid.newGuid();
     TextAreaInputCurrentPosition: number = 0;
     IsPreviewChanges: boolean = false;
-    public editorOptions = {theme: '', language: 'html'};
+    public editorOptions = { theme: '', language: 'html' };
     constructor() {
         this.Screens = [];
         this.ModifiedScreenData = new DigitalPortalScreenUpdateModel();
@@ -195,9 +195,31 @@ export class DigitalPortalCustomizationScreenLayoutComponent {
         const start = event.target.selectionStart;
         this.TextAreaInputCurrentPosition = start;
     }
+    myRange : any;
+    myEditor : any;
+    onInit(editor) {
+        editor.onDidBlurEditorText(() => {
+            // const start = editor.getSelection().selectionStartColumn;//.target.selectionStart;
+            // var xx = editor;
+            //var line = editor.getPosition();
+            this.myEditor = editor;
+            this.myRange = editor.getSelection(); 
+        });
+        // editor.onDidChangeCursorPosition((event) => {
+        //     /* column | lineNumber */
+        //     const start = event.position.column;//.target.selectionStart;
+        //     this.TextAreaInputCurrentPosition = start;
+        //     //var xx = editor;
+        // });
+
+    }
 
     attachValue(selectedValue: string) {
-        let patchedValue = this.HTMLEditor.substr(0, this.TextAreaInputCurrentPosition) + selectedValue + this.HTMLEditor.substr(this.TextAreaInputCurrentPosition, this.HTMLEditor.length);
-        this.HTMLEditor = patchedValue;
+        // let patchedValue = this.HTMLEditor.substr(0, this.TextAreaInputCurrentPosition) + selectedValue + this.HTMLEditor.substr(this.TextAreaInputCurrentPosition, this.HTMLEditor.length);
+        // this.HTMLEditor = patchedValue;
+            var id = { major: 1, minor: 1 };
+            var text = selectedValue;
+            var op = { identifier: id, range: this.myRange, text: text, forceMoveMarkers: true };
+            this.myEditor.executeEdits("my-source", [op]);
     }
 }
