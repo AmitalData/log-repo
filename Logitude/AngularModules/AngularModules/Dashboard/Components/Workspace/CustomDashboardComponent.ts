2ce07732076bf731cdbfaac412ba98a7b5c67b22
Samar Abu Eid <Samar@logitudeworld.com>
2022-12-26 13:19:32 +0200

    #172650

--- Logitude/AngularModules/AngularModules/Dashboard/Components/Workspace/CustomDashboardComponent.ts
+++ Logitude/AngularModules/AngularModules/Dashboard/Components/Workspace/CustomDashboardComponent.ts
@@ -141,9 +141,11 @@ export class CustomDashboardComponent extends BaseComponent implements OnDestroy
 
     pinnedDashboards: PinnedDashboard[] = [];
     pinnedDashboardsCount: number = 0;
+    isPinnedDashboardsList:boolean = false;
     private DrawPinnedDashboards(pinnedDashboardsJson: string) {
         this.pinnedDashboards = JSON.parse(pinnedDashboardsJson);
         this.pinnedDashboardsCount = this.pinnedDashboards.length;
+        this.isPinnedDashboardsList = true;
 
         this.dashboardPMEstendedService.GetPinnedDashboards(pinnedDashboardsJson).subscribe((myResponse: ServiceResponse) => {
             if (!myResponse.HasError) {
@@ -195,17 +197,31 @@ export class CustomDashboardComponent extends BaseComponent implements OnDestroy
         }
 
         if (list.length < this.TabsCount) {
-            this.loadedDashboards.forEach(item => {
-                if (!list.find(d => d.Id == item.Id)) {
-                    if (index != this.TabsCount) {
-                        list.push(new DashboardTab(item, index, this));
-                        index++;
+            if (this.isPinnedDashboardsList) {
+                this.pinnedDashboards.forEach(pinnedItem => {
+                    if (!list.find(d => d.Id == pinnedItem.Id)) {
+                        if (index != this.TabsCount) {
+                            var pinnedDashboard: DashboardList = this.loadedDashboards.find(d => d.Id == pinnedItem.Id);
+                            list.push(new DashboardTab(pinnedDashboard, index, this));
+                            index++;
+                        }
                     }
-                }
-            });
+                });
+            }
+
+            else {
+                this.loadedDashboards.forEach(item => {
+                    if (!list.find(d => d.Id == item.Id)) {
+                        if (index != this.TabsCount) {
+                            list.push(new DashboardTab(item, index, this));
+                            index++;
+                        }
+                    }
+                });
+            }
         }
 
-        var pinList: DashboardTab[] = list.filter(d => d.IsPinned).sort(function (a, b) { return a.Order == b.Order ? 0 : a.Order < b.Order ? 1 : -1; });
+        var pinList: DashboardTab[] = list.filter(d => d.IsPinned).sort(function (a, b) { return a.Order == b.Order ? 0 : a.Order > b.Order ? 1 : -1; });
         var unpinList: DashboardTab[] = list.filter(d => !d.IsPinned).sort(d => d.Order);
 
         pinList.forEach((item) => {
@@ -225,25 +241,9 @@ export class CustomDashboardComponent extends BaseComponent implements OnDestroy
         if (this.DashboardsTabs && this.DashboardsTabs.length > 0) {
             if (this.isSelectedDashboardDeleted || !this.SelectedDashboard) {
                 this.isSelectedDashboardDeleted = false;
-                this.selectedDashboardTab = this.DashboardsTabs[0];
                 this.SelectedDashboard = this.DashboardsTabs[0]?.Dashboard;
             }
         }
-    }
-
-    ComputeTabOrder(tab: DashboardTab): number {
-        var order = 0;
-
-        if (tab.IsPinned) {
-            var nextPinOrder: number = ArrayTool.Max(this.pinnedDashboards, "Order");
-            order = nextPinOrder + 1;
-        }
-
-        else {
-
-        }
-
-        return order;
     }
 
     AddDashboardClicked() {
@@ -279,10 +279,8 @@ export class CustomDashboardComponent extends BaseComponent implements OnDestroy
         });
     }
 
-    private selectedDashboardTab: DashboardTab;
     TabSelectionChanged(clickdTab: DashboardTab) {
         if (!clickdTab) return;
-        this.selectedDashboardTab = clickdTab;
         this.ChangeDashboard(clickdTab.Dashboard, false);
     }
 
@@ -412,9 +410,15 @@ export class CustomDashboardComponent extends BaseComponent implements OnDestroy
     PinDashboardTabClicked(item: DashboardTab) {
         console.log("pin tab");
 
+        var tabIndex = this.selectedDashboardsFromLOV.indexOf(item.Dashboard);
+        if (tabIndex > -1) {
+            this.selectedDashboardsFromLOV.splice(tabIndex, 1);
+        }
+
         var pinnedDashboard: PinnedDashboard = new PinnedDashboard();
         pinnedDashboard.Id = item.Dashboard.Id;
         pinnedDashboard.IsPredefined = item.Dashboard.Tenant == 0;
+        pinnedDashboard.Order = ArrayTool.Max(this.pinnedDashboards, "Order");
 
         this.dashboardPMEstendedService.PinDashboard(pinnedDashboard).subscribe((myResponse: ServiceResponse) => {
             if (!myResponse.HasError) {
@@ -445,8 +449,19 @@ export class CustomDashboardComponent extends BaseComponent implements OnDestroy
 
     @HostListener('document:click', ['$event'])
     clickout(event) {
-        if (this.selectedDashboardTab) 
-            this.selectedDashboardTab.IsTabMenuOpened = false;        
+        if (event == this.SelectedDashboard?.Id) {
+            this.DashboardsTabs.filter(d => d.Id != event).forEach(item => {
+                if (item.IsTabMenuOpened)
+                    item.IsTabMenuOpened = false;
+            });
+        }
+
+        else {
+            this.DashboardsTabs.forEach(item => {
+                if (item.IsTabMenuOpened)
+                    item.IsTabMenuOpened = false;
+            });
+        }
     }
 }
 
@@ -488,6 +503,8 @@ class DashboardTab {
     onRightClick(event) {
         console.log("right cick");
         event.preventDefault();
+
+        this.fatherComponent.clickout(this.Id);
 
         if (this.IsTabMenuOpened) {
             this.IsTabMenuOpened = false;
