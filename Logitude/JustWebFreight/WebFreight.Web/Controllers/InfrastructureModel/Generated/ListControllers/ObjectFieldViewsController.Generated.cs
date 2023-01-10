6fdaba444937848e59a50aac1505a2fdf3720e56
Maheera Jazi <Maheera@logitudeworld.com>
2023-01-10 10:46:55 +0200

    #174253  Task 174253: FE: DP Customization - Support Custom Fields Feature

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/InfrastructureModel/Generated/ListControllers/ObjectFieldViewsController.Generated.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/InfrastructureModel/Generated/ListControllers/ObjectFieldViewsController.Generated.cs
@@ -125,7 +125,7 @@ namespace WebFreight.Web.Controllers.InfrastructureModel.Generated.ListControlle
                 string token = HttpContext.Current.Request.Headers["Token"];
                 AuthenticationToken authToken = AuthenticationTokenRepository.GetSingleTokenFromCache(token);
                 SecurityUtility.AuthenticationOnTenant(authToken.Tenant);
-                int tenant = authToken.Tenant;
+                int tenant = 0;
 
                 QueryOperations queryOperations = new QueryOperations()
                 {
