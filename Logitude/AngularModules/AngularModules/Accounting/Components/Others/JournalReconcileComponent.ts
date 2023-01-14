1285fd80d32ff08659c4d802e0d2e032243439bd
Suhayb Yahya <Suhayb@logitudeworld.com>
2023-01-15 00:08:39 +0200

    Change split reconcilation confirmation box messages.

--- Logitude/AngularModules/AngularModules/Accounting/Components/Others/JournalReconcileComponent.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/Others/JournalReconcileComponent.ts
@@ -357,10 +357,12 @@ export class JournalReconcileComponent extends BaseComponent implements OnInit {
         this.BuildReconciliationLines(reconciliationLines);
 
         if (isSplitJournal && AppTool.IsNullOrEmpty(this.AccountingDate)) {
-            const confirmMsg = 'Leaving the accounting date / due date/ reference date/references/note empty will create adjustment journals with dates/references/note taken from the original ones. If you want to continue click ok. If not click cancel and fill the date/references/note fields';
+            const confirmMsg = TextCodeTranslator.Translate('Journal.RE.AccountingDateConfrimation');
             const confirmWindow = new ConfirmWindow();
             confirmWindow.Width = 400;
             confirmWindow.Left = '25%';
+            confirmWindow.YesButtonText = TextCodeTranslator.Translate('Customs.General.B.OK');
+            confirmWindow.NoButtonText = TextCodeTranslator.Translate('Customs.General.B.Cancel');
             confirmWindow.Show(confirmMsg);
             confirmWindow.WindowClosed.subscribe((event: any) => {
                 if (confirmWindow.Yes) {
