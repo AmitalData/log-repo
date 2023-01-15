ebfcbcdd98172f42a3d525ccd35956c5f4ebbabf
Maya Yacoub <maya@logitudeworld.com>
2023-01-15 09:58:35 +0200

    Ocean Export Enhancements (Fix Code) #173888 #174968  #174986

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/ShipmentsModel/ImporterShipmentsController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/ShipmentsModel/ImporterShipmentsController.cs
@@ -581,7 +581,12 @@ namespace WebFreight.Web.Controllers.ShipmentsModel
             entityPM.OrderIsDangerouseGoods = entityAM.IsDangerouseOfGoods;
             entityPM.PrivateLabelAgentName = entityAM.AgentName;
             entityPM.IsShipmentOrder = entityAM.IsShipmentOrder;
-            
+            entityPM.ShippingLine = entityAM.ShippingLine;
+            entityPM.PlaceOfDelivery = entityAM.PlaceOfDelivery;
+            entityPM.PickupPlace = entityAM.PickupPlace;
+            entityPM.SealNo = entityAM.SealNo;
+            entityPM.HSCode = entityAM.HSCode;
+
             if (!string.IsNullOrEmpty(entityAM.ForwardingPartnerTenant))
             {
                 int ForwardingPartnerTenant = int.Parse(entityAM.ForwardingPartnerTenant);
