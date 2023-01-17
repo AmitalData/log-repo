be6d47dc4fc5dc62169139e3e74c28e570684ed7
Maheera Jazi <Maheera@logitudeworld.com>
2023-01-17 12:39:13 +0200

    fix

--- Logitude/AngularModules/AngularModules/SharedLogistics/Components/DigitalPortal/DigitalPortalCustomizationShowHideFieldsComponent.ts
+++ Logitude/AngularModules/AngularModules/SharedLogistics/Components/DigitalPortal/DigitalPortalCustomizationShowHideFieldsComponent.ts
@@ -212,7 +212,7 @@ export class DigitalPortalCustomizationShowHideFieldsComponent extends BaseCompo
         if (this.IsModifiedFields) {
             this.ModifiedFields.ObjectTableId = this.SelectedObjectTableItem.Name;
             this.ModifiedFields.ProfileId = this.SelectedProfileItem.Code;
-            this.ModifiedLables.ProfileCode = this.SelectedProfileItem.LocalName;
+            this.ModifiedFields.ProfileCode = this.SelectedProfileItem.LocalName;
             this.digitalTextService.UpdateDigitalTextCodes(this.ModifiedFields).subscribe((myResult) => {
                 this.customizationEditComponent.IsDirty = false;
                 this.ModifiedFields = new DigitalTextCodeUpdateModel();
