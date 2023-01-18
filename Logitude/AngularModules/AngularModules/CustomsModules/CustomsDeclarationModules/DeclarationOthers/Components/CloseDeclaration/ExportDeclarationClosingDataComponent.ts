25f7de37d8c64e5e2406a782985340c8f5307482
Sohaib Nemer <sohaib@amital.co.il>
2023-01-18 11:14:23 +0200

    #175534

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationOthers/Components/CloseDeclaration/ExportDeclarationClosingDataComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationOthers/Components/CloseDeclaration/ExportDeclarationClosingDataComponent.ts
@@ -85,8 +85,7 @@ export class ExportDeclarationClosingDataComponent extends BaseComponent {
             this.DecPM = args.EntityPM;
             this.GetExportDeclarationClosingData(this.DecPM.Id);
 
-            if (!this.DecPM.IsConnectedToUnifreight && AmitalGatewayUtil.Instance.AmitalBrowserInUse)
-                this.operationalDataFromUnifreight()
+           
             
     
             this.SetUIProperty();
@@ -121,6 +120,8 @@ export class ExportDeclarationClosingDataComponent extends BaseComponent {
         if (id != null) {
 
             this.exportDeclarationClosingDatasExtendPMService.GetSingleWithEFIFILEMData(id).subscribe((response: any) => {
+                if (!this.DecPM.IsConnectedToUnifreight && AmitalGatewayUtil.Instance.AmitalBrowserInUse)
+                this.operationalDataFromUnifreight()
                 this.EntityPM = response.Result;
                 if (this.EntityPM)
 
