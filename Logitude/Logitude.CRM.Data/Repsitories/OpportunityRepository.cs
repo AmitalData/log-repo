7227bd75cd4ab50f949c62df2f137be154b3a1ff
Ahmad Abdalrazeq <a.abdalrazeq@logitudeworld.com>
2023-01-19 17:39:20 +0200

    OpportunityAnalyticTableService

--- Logitude/Logitude.CRM.Data/Repsitories/OpportunityRepository.cs
+++ Logitude/Logitude.CRM.Data/Repsitories/OpportunityRepository.cs
@@ -47,49 +47,6 @@ namespace Logitude.CRM.Data.Repsitories
                    select a;
         }
 
-        partial void OnEntityUpdate(Opportunity entity)
-        {
-            UpdateAnalyticTable(entity);
-        }
-
-
-        partial void OnEntityAdd(Opportunity entity)
-        {
-            AddToAnalyticTable(entity);
-        }
-
-        private void UpdateAnalyticTable(Opportunity entity)
-        {
-            var opportunityAnalytic = Map<OpportunityAnalytic>(entity);
-            if (context.OpportunityAnalytics.Any(e => e.Id == opportunityAnalytic.Id))
-            {
-                context.OpportunityAnalytics.Attach(opportunityAnalytic);
-                context.SetAsModified(opportunityAnalytic);
-            }
-            else AddToAnalyticTable(entity);
-        }
-
-        private void AddToAnalyticTable(Opportunity entity)
-        {
-            var opportunityAnalytic = Map<OpportunityAnalytic>(entity);
-            context.OpportunityAnalytics.Add(opportunityAnalytic);
-        }
-
-        private T Map<T>(Opportunity from) where T : new()
-        {
-            var toPropes = typeof(T).GetProperties();
-            var fromPropes = from.GetType().GetProperties().ToDictionary(e => e.Name, e => e);
-            var to = new T();
-            foreach (var item in toPropes)
-            {
-                if (fromPropes.ContainsKey(item.Name))
-                {
-                    item.SetValue(to, fromPropes[item.Name].GetValue(from));
-                }
-            }
-            return to;
-        }
-
     }
 
 }
