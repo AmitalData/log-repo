9d6fbd0efc3f31457a65728c714d6e5d2cfb0ddc
izik89 <izikfr@amital.co.il>
2023-01-19 10:37:36 +0200

    fix installer update

--- Logitude/Logitude.Update/Program.cs
+++ Logitude/Logitude.Update/Program.cs
@@ -54,6 +54,7 @@ namespace Logitude.Update
                 WebFreight.Web.MetaDataUpdate.TenantsUpdateClass.UpdateDataForTenant(0, "customs");
                 Console.WriteLine("BuildObjectTablesZipFilesData:" + DateTime.Now.ToString());
                 WebFreight.Web.MetaDataUpdate.TenantsUpdateClass.BuildObjectTablesZipFilesData(false, true);
+                WebFreight.Web.MetaDataUpdate.TenantsUpdateClass.BuildObjectTablesZipFilesData(false, false);
                 Console.WriteLine("TableLastUpdateClass.UpdateCacheTableHistory:" + DateTime.Now.ToString());
                 BL.Helpers.TableLastUpdateClass.UpdateCacheTableHistory();
                 Console.WriteLine("TenantsUpdateClass.UpdateTenants:" + DateTime.Now.ToString());
