558266d2aaf04699b9fe7c132cee67d00afbb837
Aseel Zbaidah <aseel@logitudeworld.com>
2023-01-19 16:25:17 +0200

    #174438

--- Logitude/JustWebFreight/WebFreight.Web/ReportsWebServices/LogitudeReportsWebService.asmx.cs
+++ Logitude/JustWebFreight/WebFreight.Web/ReportsWebServices/LogitudeReportsWebService.asmx.cs
@@ -3554,6 +3554,7 @@ namespace WebFreight.Web.ReportsWebServices
                 invoicesRecored.AmountDueInLocalCurrency = apInvoice.AmountDueInLocalCurrency;
                 expenseChargesInLocalCurrency = expenseInvoiceLines.Sum(s => s.LocalCurrencyAmount);
                 invoicesRecored.PaidDate = apInvoice.PaidDate;
+                invoicesRecored.DueDate = apInvoice.DueDate.Value;
 
                 Card vendorCard = CardRepository.GetSingleCard(apInvoice.VendorId, tenant, false);
                 if(vendorCard != null)
