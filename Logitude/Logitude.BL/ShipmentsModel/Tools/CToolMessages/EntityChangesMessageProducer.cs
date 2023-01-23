b2942d86526b5acc3174f01c26e9fede88ab64c7
Ahmad A. Abdalghafoor <ahmad.abdulgafoor@gmail.com>
2023-01-23 06:35:23 +0200

    fix

--- Logitude/Logitude.BL/ShipmentsModel/Tools/CToolMessages/EntityChangesMessageProducer.cs
+++ Logitude/Logitude.BL/ShipmentsModel/Tools/CToolMessages/EntityChangesMessageProducer.cs
@@ -112,7 +112,7 @@ namespace Logitude.Server.Tools.CToolWorkflows
                     {
                         using (var shipmentUpdateMessageProducer = new Producer())
                         {
-                            var result = shipmentUpdateMessageProducer.Produce(KafkaTopics.ShipmentsCreateTopic,
+                            var result = shipmentUpdateMessageProducer.Produce(KafkaTopics.ShipmentsUpdateTopic,
                            KakaMessageTypes.ShipmentCreate, serializedCToolWorkflowMessage, true);
                         }
                     }
