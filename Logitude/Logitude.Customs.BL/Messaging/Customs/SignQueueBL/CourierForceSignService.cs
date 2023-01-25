ccdb86be28214f976abde27ccd0bccd4fb817a9b
Itzik Moshelian <itzikm@amitaldataltd.onmicrosoft.com>
2023-01-25 14:47:14 +0200

    #176106

---
+++ Logitude/Logitude.Customs.BL/Messaging/Customs/SignQueueBL/CourierForceSignService.cs
@@ -0,0 +1,28 @@
+﻿using Logitude.CustomsMessaging.Common.RequestParams;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logitude.Customs.BL.Messaging.Customs.SignQueueBL
+{
+    internal class CourierForceSignService
+    {
+        internal void ApplyForceSign<TRequestParams>(ref TRequestParams requestParams) where TRequestParams : RequestParamsBase
+        {
+            switch (requestParams.InterfaceTypeCode)
+            {
+                case "2715":
+                    requestParams.ForcePersonalSign = true;
+                    break;
+
+                case "2750":
+                    requestParams.ForcePersonalSign = true;//DEFAULT HSM 
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
