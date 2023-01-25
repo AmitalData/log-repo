4e10c7cfb44c05a983b84fc178c074e1abdf8fd1
Maya Yacoub <maya@logitudeworld.com>
2023-01-25 11:44:52 +0200

    Customization: Export customization in Zip file  #175796 #175802

--- Logitude/Logitude.BL/InfrastructureModel/Services/DeploymentPackage/DeploymentPackageZipFileDetailsService.cs
+++ Logitude/Logitude.BL/InfrastructureModel/Services/DeploymentPackage/DeploymentPackageZipFileDetailsService.cs
@@ -2,20 +2,63 @@
 using Logitude.BL.InfrastructureModel.EntityPMs;
 using Logitude.Server.Tools.Counters;
 using System;
+using System.Collections.Generic;
 
 namespace Logitude.BL.InfrastructureModel.Tools.EntityService
 {
     public class DeploymentPackageZipFileDetailsService
     {
-        public DeploymentPackageZipFileDetailsService(int tenant)
+        private Dictionary<string, byte[]> deploymentPackageZipFileDetails;
+        private Dictionary<string, DeploymentPackageDetails> deploymentPackageDetailsList;
+        public DeploymentPackageZipFileDetailsService()
+        {
+            deploymentPackageZipFileDetails = new Dictionary<string, byte[]>();
+            deploymentPackageDetailsList = new Dictionary<string, DeploymentPackageDetails>();
+        }
+
+        public Dictionary<string, byte[]> Build(DeploymentPackageDetails deploymentPackageDetails)
+        {
+
+            SetCustomFields(deploymentPackageDetails.CustomFields);
+            SetCustomPickLists(deploymentPackageDetails.CustomPickLists);
+
+            //AddPackageDescption(dataList , deploymentPackageDetails);
+            return deploymentPackageZipFileDetails;
+        }
+
+        private void SetCustomFields(List<CustomFields> customFields)
+        {
+            customFields.ForEach(customField =>
+            {
+                var deploymentPackageDetails = GetDeploymentPackageDetailsByObjectName(customField.ObjectTableName);
+                deploymentPackageDetails.CustomFields.Add(customField);
+            });
+        }
+        private void SetCustomPickLists(List<CustomPickListItem> customPickLists)
         {
 
         }
-
-        public static DeploymentPackageZipFileDetails Build(DeploymentPackageDetails deploymentPackageDetails)
+        private DeploymentPackageDetails GetDeploymentPackageDetailsByObjectName(string objectTableName)
         {
-            return new DeploymentPackageZipFileDetails();
+            if (string.IsNullOrEmpty(objectTableName)) return null;
+            DeploymentPackageDetails deploymentPackageDetails = null;
+            if (!deploymentPackageDetailsList.TryGetValue(objectTableName,out deploymentPackageDetails)) return null;//need to add it first then return deploymentPackageDetails
+            return deploymentPackageDetails;
         }
+        //public void AddPackageDescption(Dictionary<string, byte[]> dataList , DeploymentPackageDetails deploymentPackageDetails)
+        //{
+        //    var ackageDescption = new PackageDescption() {Name = deploymentPackageDetails.Name };
+        //    ackageDescption.Entites = new List<string>() { "Shipment"};
+
+        //}
     }
+
+    //public class PackageDescption
+    //{
+    //    public string Name { get; set; }
+    //    public string Version { get; set; }
+    //    public List<string> Entites { set; set; }
+
+    //}
 
 }
--- Logitude/Logitude.BL/InfrastructureModel/Services/DeploymentPackage/DeploymentPackageZipFileService.cs
+++ Logitude/Logitude.BL/InfrastructureModel/Services/DeploymentPackage/DeploymentPackageZipFileService.cs
@@ -1,4 +1,6 @@
 ﻿
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip;
 using Logitude.BL.InfrastructureModel.EntityPMs;
 using Logitude.Server.Tools.Counters;
 using Logitude.Server.Tools.Helpers;
@@ -6,6 +8,7 @@ using Simplog.Data.CommonDataModel.EntityPOCOs;
 using Simplog.Data.CommonDataModel.Repositories;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Text.Json;
 
@@ -13,22 +16,21 @@ namespace Logitude.BL.InfrastructureModel.Tools.EntityService
 {
     public class DeploymentPackageZipFileService
     {
-        private DeploymentPackageZipFileDetails deploymentPackageZipFileDetails;
         private Document document;
         private DocumentRepository documentRepository;
         private int tenant;
+        private Dictionary<string, DeploymentPackageDetails> deploymentPackageDetailsList;
         public DeploymentPackageZipFileService(int tenant)
         {
-            deploymentPackageZipFileDetails = new DeploymentPackageZipFileDetails();
+            deploymentPackageDetailsList = new Dictionary<string, DeploymentPackageDetails>();
             this.tenant = tenant;
             documentRepository = new DocumentRepository(tenant);
         }
 
         public string Create(DeploymentPackageDetails deploymentPackageDetails)//for name + desc + code (where to store them)
         {
-            var deploymentPackageZipFileDetails = GetInstanceOfZipFileDetails();
-            var deploymentPackageZipFileDetailsBytes = Encoding.ASCII.GetBytes(JsonSerializer.Serialize(deploymentPackageZipFileDetails));
-            document = GetInstanceOfDocument(deploymentPackageZipFileDetailsBytes);
+            var deploymentPackageZipFileBytes = CompressionData(new Dictionary<string, byte[]>());
+            document = GetInstanceOfDocument(deploymentPackageZipFileBytes);
             documentRepository.Add(document);
             documentRepository.SubmitChanges();
             StorageDataService.WriteFileOnStorage(new StorageDataArgs()
@@ -36,27 +38,56 @@ namespace Logitude.BL.InfrastructureModel.Tools.EntityService
                 FileName = document.Id,
                 FolderName = document.Folder,
                 Tenant = document.Tenant,
-                FileData = deploymentPackageZipFileDetailsBytes,
+                FileData = deploymentPackageZipFileBytes,
                 Extension = document.Extension
             });
 
             return document.Id;
         }
-        private DeploymentPackageZipFileDetails GetInstanceOfZipFileDetails()
+
+        private byte[] CompressionData(Dictionary<string, byte[]> dataList)
         {
-            return new DeploymentPackageZipFileDetails
+            MemoryStream outputMemStream = new MemoryStream();
+            ZipOutputStream zipStream = new ZipOutputStream(outputMemStream);
+
+            zipStream.SetLevel(3);
+            byte[] bytes = null;
+            foreach (string key in dataList.Keys)
             {
-                DeploymentPackageDetailsList = new Dictionary<string, DeploymentPackageDetails>(),
-                FilesNames = new List<string>()
-            };
+                var newEntry = new ZipEntry(key + ".json");
+                newEntry.DateTime = DateTime.Now;
+
+                zipStream.PutNextEntry(newEntry);
+
+                bytes = dataList[key];
+
+                MemoryStream inStream = new MemoryStream(bytes);
+                long inStreamLength = inStream.Length;
+                if (inStreamLength < 200)
+                {
+                    inStreamLength = 200;
+                }
+
+                StreamUtils.Copy(inStream, zipStream, new byte[inStreamLength]);
+                inStream.Close();
+                zipStream.CloseEntry();
+
+            }
+
+            zipStream.IsStreamOwner = false;
+            zipStream.Close();
+            outputMemStream.Position = 0;
+
+            return outputMemStream.ToArray();
+
         }
-        private Document GetInstanceOfDocument(byte[] deploymentPackageZipFileDetailsBytes)
+        private Document GetInstanceOfDocument(byte[] deploymentPackageZipFileBytes)
         {
             return new Document()
             {
                 CreateDate = DateTime.Now,
                 Extension = "zip",
-                FileSize = deploymentPackageZipFileDetailsBytes.Length,
+                FileSize = deploymentPackageZipFileBytes.Length,
                 Tenant = Convert.ToInt32(tenant),
                 Id = IdCounter.GetNumber("Document", tenant),
                 HasFile = true,
@@ -69,8 +100,8 @@ namespace Logitude.BL.InfrastructureModel.Tools.EntityService
         {
             if (deploymentPackagePM.DocumentId == null) return;
             Document document = new DocumentRepository(deploymentPackagePM.Tenant).GetSingleDocument(deploymentPackagePM.Tenant, deploymentPackagePM.DocumentId);
-            var deploymentPackageZipFileDetails = DeploymentPackageZipFileDetailsService.Build(deploymentPackagePM.DeploymentPackageDetails);
-            var deploymentPackageZipFileDetailsBytes = Encoding.ASCII.GetBytes(JsonSerializer.Serialize(deploymentPackageZipFileDetails));
+            var deploymentPackageZipFileDetailsList = new DeploymentPackageZipFileDetailsService().Build(deploymentPackagePM.DeploymentPackageDetails);
+            var deploymentPackageZipFileDetailsBytes = CompressionData(deploymentPackageZipFileDetailsList);
             document.FileSize = deploymentPackageZipFileDetailsBytes.Length;
             documentRepository.Update(document);
             documentRepository.SubmitChanges();
@@ -97,9 +128,4 @@ namespace Logitude.BL.InfrastructureModel.Tools.EntityService
         }
     }
 
-    public class DeploymentPackageZipFileDetails
-    {
-        public Dictionary<string, DeploymentPackageDetails> DeploymentPackageDetailsList;
-        public List<string> FilesNames;
-    }
 }
