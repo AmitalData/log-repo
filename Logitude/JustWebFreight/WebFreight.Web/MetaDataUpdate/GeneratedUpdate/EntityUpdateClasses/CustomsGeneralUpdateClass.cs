b36efdadbb1e54db80d1ebbff134c0fdb83b4906
Shoshana Greenwald <shoshana@amital.co.il>
2023-01-25 12:32:18 +0200

    run tt

--- Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/GeneratedUpdate/EntityUpdateClasses/CustomsGeneralUpdateClass.cs
+++ Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/GeneratedUpdate/EntityUpdateClasses/CustomsGeneralUpdateClass.cs
@@ -85,7 +85,6 @@ namespace WebFreight.Web.MetaDataUpdate.GeneratedUpdate.EntityUpdateClasses
 {
    public class CustomsGeneralUpdateClass
    {  		
-
 		public const string HashString = "1aaede96cf09ad51df697c8f19cceac0";
 	    public void AddObjectTable(Dictionary<string, ObjectTable> objectTables, Dictionary<string, TextCode> textCodes,ObjectTableRepository ObjectTableRepository,TextCodeRepository TextCodeRepository)
         {                     
@@ -126,7 +125,6 @@ namespace WebFreight.Web.MetaDataUpdate.GeneratedUpdate.EntityUpdateClasses
 			      				    ObjectTableTypeCode =  "MD",
 			      				    MaxNumberOfCustomFields =  0,
 			      				    DefaultText =  "CustomsGeneral",
-
 			      				    Code =  "5510",
 			      				    Name =  "CustomsGeneral",
 			      				    GenerateDomainService =  false,
