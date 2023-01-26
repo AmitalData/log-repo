a4d14b4dde8b8d0fdad6ad9ab42e955eeba7d4e3
Atara Oster <Atara@amital.co.il>
2023-01-26 13:35:57 +0200

    #176206 versionId

--- Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_8237_ExportDeclerationAmendmentReplyResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_8237_ExportDeclerationAmendmentReplyResponseService.cs
@@ -611,7 +611,9 @@ namespace Logitude.CustomsMessaging.ResponseServices
                     this._MyDeclarationPM.ChangeSetOp = ChangeSetOperation.Update;
                     myDeclarationUpdateService.Update(this._MyDeclarationPM, true);
                 }
-                if (customResponse.Response.Declaration != null  && (_MyDeclarationPM.AmendmentStatus == "1" || _MyDeclarationPM.AmendmentStatus == "2"))
+                if (customResponse.Response.Declaration != null  && 
+                    (((_MyDeclarationPM.AmendmentStatus == "1" || _MyDeclarationPM.AmendmentStatus == "2") && !isExportClose) 
+                    || MyResponseData.IsExportCloseApprove))
                 {
                     DF_NG_2757_MSG10004_ExportDeclarationResponseService dF_NG_2757_MSG10004_ExportDeclarationResponseService = new DF_NG_2757_MSG10004_ExportDeclarationResponseService();
 
