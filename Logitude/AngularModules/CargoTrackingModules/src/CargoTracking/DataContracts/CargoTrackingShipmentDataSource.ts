5edab3aacc712d2aaad631a4c53df2926e554c97
Suhayb Yahya <Suhayb@logitudeworld.com>
2023-01-29 18:15:20 +0200

    Filtering by customer ids' [CargoTracking]

--- Logitude/AngularModules/CargoTrackingModules/src/CargoTracking/DataContracts/CargoTrackingShipmentDataSource.ts
+++ Logitude/AngularModules/CargoTrackingModules/src/CargoTracking/DataContracts/CargoTrackingShipmentDataSource.ts
@@ -1,35 +1,36 @@
-import { CollectionViewer, DataSource } from '@angular/cdk/collections';
-import { AfterViewInit, ChangeDetectorRef } from '@angular/core';
-import { BehaviorSubject, Observable, Subscription } from 'rxjs';
-import { CargoTrackingSearchService } from 'src/CargoTracking/Services/Others/CargoTrackingSearchService';
-import { ShipmentsListComponent } from '../Components/UserDashboard/ShipmentsPage/ShipmentsList/ShipmentsListComponent';
-import { CargoTrackingShipmentSearchInput } from './CargoTrackingShipmentFilters';
+import {CollectionViewer, DataSource} from '@angular/cdk/collections';
+import {AfterViewInit, ChangeDetectorRef} from '@angular/core';
+import {BehaviorSubject, Observable, Subscription} from 'rxjs';
+import {CargoTrackingSearchService} from 'src/CargoTracking/Services/Others/CargoTrackingSearchService';
+import {ShipmentsListComponent} from '../Components/UserDashboard/ShipmentsPage/ShipmentsList/ShipmentsListComponent';
+import {CargoTrackingShipmentSearchInput} from './CargoTrackingShipmentFilters';
 
-export class ShipmentDataSource extends DataSource<any | undefined>  {
+export class ShipmentDataSource extends DataSource<any | undefined> {
     private pageSize = 50;
-    private cachedShipments = Array.from<any>({ length: this.ShipmentsCount });
+    public cachedShipments = Array.from<any>({length: this.ShipmentsCount});
+    public cachedCustomers = Array.from<Customer>({length: this.ShipmentsCount});
+
     private fetchedPages = new Set<number>();
     private dataStream = new BehaviorSubject<(any | undefined)[]>(this.cachedShipments);
     private subscription = new Subscription();
     timer = null;
+
     constructor(
         public ChangeDetector: ChangeDetectorRef,
         public ShipmentSearchService: CargoTrackingSearchService,
         public ShipmentsFilters: CargoTrackingShipmentSearchInput,
         private parent: ShipmentsListComponent,
         public ShipmentsCount = 1
-    )
-    {
+    ) {
         super();
         this.InitComponent();
 
     }
 
-    private InitComponent()
-    {
+    private InitComponent() {
         this.parent.ShipmentsCount = 0;
         this.parent.noResult = false;
-        this.cachedShipments = Array.from<any>({ length: this.ShipmentsCount || 1 });
+        this.cachedShipments = Array.from<any>({length: this.ShipmentsCount || 1});
         this.fetchedPages = new Set<number>();
         this.FetchPage(0);
 
@@ -37,8 +38,11 @@ export class ShipmentDataSource extends DataSource<any | undefined>  {
         this.ChangeDetector.detectChanges();
     }
 
-    ReloadData(filters)
-    {
+    getShipments() {
+        return this.cachedShipments;
+    }
+
+    ReloadData(filters) {
         this.ShipmentsFilters = filters;
         this.InitComponent();
         this.FetchPage(0);
@@ -46,22 +50,21 @@ export class ShipmentDataSource extends DataSource<any | undefined>  {
         this.ChangeDetector.detectChanges();
         this.dataStream.next(this.cachedShipments);
     }
-    connect(collectionViewer: CollectionViewer): Observable<(any | undefined)[]>
-    {
-        this.subscription.add(collectionViewer.viewChange.subscribe(range =>
-        {
+
+    connect(collectionViewer: CollectionViewer): Observable<(any | undefined)[]> {
+        this.subscription.add(collectionViewer.viewChange.subscribe(range => {
             // the following 4 lines of code added by Rabaia in order to inhance the performance of the CargoTracking.
-            // if you have Problem with it please talk to me --Rabaia 
+            // if you have Problem with it please talk to me --Rabaia
             if (this.timer) {
                 clearTimeout(this.timer);
             }
-            this.timer = setTimeout(() => this.HandleRange(range), 400); 
-            
+            this.timer = setTimeout(() => this.HandleRange(range), 400);
+
         }));
         return this.dataStream;
     }
 
-    HandleRange(range:any): void {
+    HandleRange(range: any): void {
         const startPage = this.GetPageForIndex(range.start);
         const endPage = this.GetPageForIndex(range.end - 1);
         for (let i = startPage; i <= endPage; i++) {
@@ -69,35 +72,31 @@ export class ShipmentDataSource extends DataSource<any | undefined>  {
         }
     }
 
-    disconnect(): void
-    {
+    disconnect(): void {
         this.subscription.unsubscribe();
     }
 
-    private GetPageForIndex(index: number)
-    {
+    private GetPageForIndex(index: number) {
         return Math.floor(index / this.pageSize);
     }
 
-    private FetchPage(pageNumber: number)
-    {
+    private FetchPage(pageNumber: number) {
         if (!this.fetchedPages.has(pageNumber)) {
             this.fetchedPages.add(pageNumber);
             this.GetShipmentsPage(pageNumber);
         }
     }
 
-    private GetShipmentsPage(page: number)
-    {
+    private GetShipmentsPage(page: number) {
         this.ShipmentsFilters.PageIndex = page;
         this.ShipmentsFilters.PageSize = this.pageSize;
-        this.ShipmentSearchService.GetUserShipments( this.ShipmentsFilters)
-            .subscribe((shipmentsResponse: any) =>
-            {
+        this.ShipmentsFilters.CustomersIds = this.ShipmentsFilters.Customers.map(a => a.CustomerId);
+
+        this.ShipmentSearchService.GetUserShipments(this.ShipmentsFilters)
+            .subscribe((shipmentsResponse: any) => {
                 this.parent.ShipmentsLoadingError = "";
                 this.HandleShipmentsResponse(page, shipmentsResponse);
-            }, error =>
-            {
+            }, error => {
                 this.parent.ShipmentsLoadingError = error.statusText;
                 console.error(error);
                 this.ChangeDetector.detectChanges();
@@ -105,21 +104,33 @@ export class ShipmentDataSource extends DataSource<any | undefined>  {
             });
     }
 
-    private HandleShipmentsResponse(page: number, shipmentsResponse: any)
-    {
-        if (page == 0)
+    private HandleShipmentsResponse(page: number, shipmentsResponse: any) {
+        if (page === 0) {
             this.ResetDataSourceVariablesForFirstPage(shipmentsResponse);
+        }
 
         this.CacheShipments(page, shipmentsResponse.Shipments);
+        this.cachedCustomers = shipmentsResponse.Shipments.map(shipment => ({
+            CustomerId: shipment.CustomerId,
+            CustomerName: shipment.CustomerEnglishName
+        } as Customer ));
+
+        this.cachedCustomers = this.cachedCustomers
+            .filter((elem, index, self) => index === self.indexOf(elem));
+
+        this.cachedCustomers = this.cachedCustomers.filter((value, index, self) =>
+            index === self.findIndex((t) => (
+                t.CustomerId === value.CustomerId
+            )));
 
         this.ChangeDetector.detectChanges();
         this.dataStream.next(this.cachedShipments);
     }
 
-    private ResetDataSourceVariablesForFirstPage(shipmentsResponse: any)
-    {
-        if(this.parent.ShipmentsCount != shipmentsResponse.ShipmentsCount)
-            this.parent.ShipmentsCount = shipmentsResponse.ShipmentsCount
+    private ResetDataSourceVariablesForFirstPage(shipmentsResponse: any) {
+        if (this.parent.ShipmentsCount != shipmentsResponse.ShipmentsCount) {
+            this.parent.ShipmentsCount = shipmentsResponse.ShipmentsCount;
+        }
 
         if (shipmentsResponse.ShipmentsCount != this.ShipmentsCount) {
             this.SetShipmentsCount(shipmentsResponse.ShipmentsCount);
@@ -129,24 +140,20 @@ export class ShipmentDataSource extends DataSource<any | undefined>  {
         this.SetNoResultToggle();
     }
 
-    private ResetCachedShipmentsArray(count: number)
-    {
-        this.cachedShipments = Array.from<any>({ length: count });
+    private ResetCachedShipmentsArray(count: number) {
+        this.cachedShipments = Array.from<any>({length: count});
     }
 
-    private SetShipmentsCount(count: any)
-    {
+    private SetShipmentsCount(count: any) {
         this.ShipmentsCount = count;
         this.parent.ShipmentsCount = count;
     }
 
-    private SetNoResultToggle()
-    {
+    private SetNoResultToggle() {
         this.parent.noResult = (this.ShipmentsCount == 0);
     }
 
-    private CacheShipments(pageNumber: number, shipments: any)
-    {
+    private CacheShipments(pageNumber: number, shipments: any) {
         this.cachedShipments.splice(
             pageNumber * this.pageSize,
             this.pageSize,
@@ -154,3 +161,8 @@ export class ShipmentDataSource extends DataSource<any | undefined>  {
     }
 }
 
+export interface Customer
+{
+    CustomerId: string;
+    CustomerName: string;
+}
