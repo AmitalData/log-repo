c7ec437e7a4ea349ef923e8771922fa46d70c8e6
Maheera Jazi <Maheera@logitudeworld.com>
2023-01-29 15:33:33 +0200

    fix

--- Logitude/AngularModules/AngularModules/SharedLogistics/Components/DigitalPortal/DigitalPortalCustomizationShowHideFieldsComponent.ts
+++ Logitude/AngularModules/AngularModules/SharedLogistics/Components/DigitalPortal/DigitalPortalCustomizationShowHideFieldsComponent.ts
@@ -274,9 +274,6 @@ export class ProfileFieldsItem extends BaseComponent {
         this.hasPermission = item.HasPermission;
         this.fieldCode = item.FieldCode;
         var selelectField = father.loadedFieldsResults.filter(a => a['FieldCode'] == this.fieldCode)[0];
-        if (selelectField == null) {
-            var tt = "ccc";
-        }
         this.textCode = selelectField['TextCode'];
         this.defaultText = selelectField['DefaultText'];
         this.displayText = selelectField['DisplayText'];
