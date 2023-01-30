1d3606ea20f7dee4dd7fe54c177f1b9b37a4dfc7
Frida Lok <frida@amital.co.il>
2023-01-30 17:20:54 +0200

    #173080

--- Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_2754_MSG10004_ImportDeclarationResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_2754_MSG10004_ImportDeclarationResponseService.cs
@@ -35,6 +35,7 @@ using Logitude.Customs.BL.Messaging.L2U.CustomFile;
 using Logitude.Customs.BL.Messaging.Customs;
 using Simplog.Server.Infrastructure.Helpers;
 using System.Threading.Tasks;
+using Logitude.Customs.Data.EntityPOCOs;
 
 namespace Logitude.CustomsMessaging.ResponseServices
 {
@@ -992,7 +993,30 @@ namespace Logitude.CustomsMessaging.ResponseServices
                         {
 
                             LogMessagingUtil.Instance.AppendLine("תהליך גביה- במידה ומופעל בדיקה האם להגדיר גבייה = 900");
-                            if (_MyDeclarationPM.SupplierInvoices != null && _MyDeclarationPM.SupplierInvoices.FirstOrDefault().IncotermCode != "DDP" && _MyDeclarationPM.TotalTax > 0 && _MyDeclarationPM.DeclarationStatusTypeCode == "13")
+
+                            bool pendingRequiresPayment = false;
+
+
+                            if (!string.IsNullOrEmpty(_MyDeclarationCourierStatusPM.CourierPendingReasonList))
+                            {
+                                var courierPendingReason = _MyDeclarationCourierStatusPM.CourierPendingReasonList.Split(',');
+                                foreach (var item in courierPendingReason)
+                                {
+                                    if (!pendingRequiresPayment)
+                                    {
+                                        CourierPendingReasonRepository courierPendingReasonRepository = new CourierPendingReasonRepository(_MyDeclarationPM.Tenant);
+                                        CourierPendingReason PendingReason = courierPendingReasonRepository.GetByCode(item, _MyDeclarationPM.Tenant);
+
+                                        if (PendingReason != null && PendingReason.RequiresPayment == true && !PendingReason.Inactive)
+                                            pendingRequiresPayment = true;
+                                    }
+                                    
+
+                                }
+
+                            }
+
+                            if (((_MyDeclarationPM.SupplierInvoices != null && _MyDeclarationPM.SupplierInvoices.FirstOrDefault().IncotermCode != "DDP" && _MyDeclarationPM.TotalTax > 0) || pendingRequiresPayment) && _MyDeclarationPM.DeclarationStatusTypeCode == "13")
                             {
                                 if (declarationPendingPM_900 == null)
                                 {
