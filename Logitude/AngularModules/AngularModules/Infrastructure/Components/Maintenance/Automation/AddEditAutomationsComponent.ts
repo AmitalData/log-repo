e4895dfeb02671aad99dd67421d4ce30d2a05fec
Maya Yacoub <maya@logitudeworld.com>
2023-01-31 09:24:30 +0200

    Cross Dock Release Automation - Metadata

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/Maintenance/Automation/AddEditAutomationsComponent.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/Maintenance/Automation/AddEditAutomationsComponent.ts
@@ -1062,6 +1062,11 @@ export class AddEditAutomationsComponent extends BaseComponent implements OnInit
                 this.ResultCodeList.push(new ResultCode("Docs In F/U Creation", "DOCINFOLLOWUP"));
                 this.ResultCodeList.push(new ResultCode("Set Fields Value", "FIELDSET"));
             }
+
+            //WarehouseEntry
+            if (this.ObjectTableName == "WarehouseEntry") {
+                this.ResultCodeList.push(new ResultCode("Set Fields Value", "FIELDSET"));
+            }
             
             this.ResultCodeSelected = this.ResultCodeList.filter(d => d.Code == this.AutomatedBackupClass.ResultCode)[0];
 
