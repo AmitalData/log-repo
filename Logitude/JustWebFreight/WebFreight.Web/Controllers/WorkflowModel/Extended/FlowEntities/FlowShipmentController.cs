708884cdaf6fc17058ab10877792e039bff6607e
Ahmad A. Abdalghafoor <ahmad.abdulgafoor@gmail.com>
2023-02-06 15:11:26 +0200

    Snapshot Isolation

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/WorkflowModel/Extended/FlowEntities/FlowShipmentController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/WorkflowModel/Extended/FlowEntities/FlowShipmentController.cs
@@ -131,7 +131,7 @@ namespace WebFreight.Web.Controllers.WorkflowModel.Extended.FlowEntities
         {
             try
             {
-                using (TransactionScope transactionScope = TransactionFactory.GetNewReadCommittedTransaction())
+                using (TransactionScope transactionScope = TransactionFactory.GetTransaction())
                 {
                     string token = HttpContext.Current.Request.Headers["Token"];
                     AuthenticationToken authToken = AuthenticationTokenRepository.GetSingleTokenFromCache(token);
