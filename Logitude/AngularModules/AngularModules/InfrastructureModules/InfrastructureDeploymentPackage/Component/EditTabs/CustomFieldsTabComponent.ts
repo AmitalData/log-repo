e39e383516c023ec500c2e02382aa58a6a107ff7
Maya Yacoub <maya@logitudeworld.com>
2023-02-06 09:16:25 +0200

    Deployment package  issues #176851

--- Logitude/AngularModules/AngularModules/InfrastructureModules/InfrastructureDeploymentPackage/Component/EditTabs/CustomFieldsTabComponent.ts
+++ Logitude/AngularModules/AngularModules/InfrastructureModules/InfrastructureDeploymentPackage/Component/EditTabs/CustomFieldsTabComponent.ts
@@ -73,6 +73,7 @@ export class CustomFieldsTabComponent extends BaseComponent {
 
 
     DeleteCustomField(item: CustomFields) {
+        if(this.EntityPM.IsExported) return;
         this.OriginalExportCustomFields = this.OriginalExportCustomFields.filter(customField => customField.FieldCode != item.FieldCode);
         this.ExportCustomFields = this.ExportCustomFields.filter(customField => customField.FieldCode != item.FieldCode);
         this.EntityPM.DeploymentPackageDetails.CustomFields = this.EntityPM.DeploymentPackageDetails.CustomFields.filter(customField => customField.FieldCode != item.FieldCode);
@@ -96,6 +97,7 @@ export class CustomFieldsTabComponent extends BaseComponent {
     }
 
     EditCustomField(item: CustomFields) {
+        if(this.EntityPM.IsExported) return;
         var logWindow = new LogitudeWindow();
         logWindow.Title = "Edit Custom Field";
         logWindow.Width = 720;
