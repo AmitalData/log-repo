f81e460a8a826d146c2182b47bc3447437dc9dca
elisheva Nachaysi <elisheva@amital.co.il>
2023-02-07 09:19:48 +0200

    #176915

--- Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_2757_MSG10004_ExportAmendmentDeclarationResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_2757_MSG10004_ExportAmendmentDeclarationResponseService.cs
@@ -1291,19 +1291,19 @@ namespace Logitude.CustomsMessaging.ResponseServices
                     {
                         SupplierInvoiceItemsPricePM supplierInvoiceItemsPrice = new SupplierInvoiceItemsPricePM();
 
-                        //if (!isFromImporter && AdditionalPriceTypeCodes.Contains(AdditionalPriceTypeCode))
-                        //{
-                        //    supplierInvoiceItemsPrice.ChangeSetOp = ChangeSetOperation.Update;
-                        //}
-                        //else if(isFromImporter)
-                        //{
+                        if (!isFromImporter && AdditionalPriceTypeCodes.Contains(AdditionalPriceTypeCode))
+                        {
+                            supplierInvoiceItemsPrice.ChangeSetOp = ChangeSetOperation.Update;
+                        }
+                       else if (isFromImporter)
+                        {
                             supplierInvoiceItemsPrice.ChangeSetOp = ChangeSetOperation.Insert;
-                  //      }
-                        //else
-                        //{
-                        //    continue;
-                        //}
-                       
+                        }
+                        else
+                        {
+                            continue;
+                        }
+
                         supplierInvoiceItemsPrice.DeclarationId = declarationId;
                        
                         supplierInvoiceItemsPrice.AdditionalPrice = GetValueAmountType(GoodsItemAmount.CustomsValueAmount);
