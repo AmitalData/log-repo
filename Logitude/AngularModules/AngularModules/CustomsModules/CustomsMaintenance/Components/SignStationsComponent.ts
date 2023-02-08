fad1d48cfb3a9228f12a478b356a37462088a55c
Itzik Moshelian <itzikm@amitaldataltd.onmicrosoft.com>
2023-02-08 16:24:00 +0200

    #177194

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsMaintenance/Components/SignStationsComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsMaintenance/Components/SignStationsComponent.ts
@@ -38,7 +38,7 @@ export class SignStationsComponent
     _BadCardSelectedNum: number = 0;
     _OKNum: number = 0;
     private CurrentSession = SessionLocator.SelectedSession;
-    ValidationErrorsList: string[] = [];
+    
     constructor() {
         super();
         this._entityListService = new EntityListService();
