f4d09db52006825e4d36680b4fe14f7bbea3febb
Atara Oster <Atara@amital.co.il>
2023-02-08 13:45:55 +0200

    #176499

--- Logitude/Logitude.CustomsMessaging/ResponseServices/CreateExportSupplierInvoicesFromFileBatchUCB_MsgResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/CreateExportSupplierInvoicesFromFileBatchUCB_MsgResponseService.cs
@@ -78,7 +78,7 @@ namespace Logitude.CustomsMessaging.ResponseServices
                     IncotermCode = invoiceFromDB.IncotermCode,
                     InvoiceNumber = invoiceFromFile.InvoiceNumber,
                     IsPreference = invoiceFromFile.IsPreference,
-                    InvoiceAmount = invoiceFromFile.SupplierInvoiceItems.Sum(x=>x.InvoiceQuentity.GetValueOrDefault()),
+                    InvoiceAmount = invoiceFromFile.SupplierInvoiceItems.Sum(x=>x.ItemPrice.GetValueOrDefault()),
                     BuyerName = invoiceFromDB.BuyerName,
                     BuyerAddress = invoiceFromDB.BuyerAddress,
                     BuyerRoleCode = invoiceFromDB.BuyerRoleCode,
@@ -149,7 +149,7 @@ namespace Logitude.CustomsMessaging.ResponseServices
                     error += invoice.InvoiceNumber + ":some fields is required. \n";
 
                 }
-                var sum = invoiceFromFile.SupplierInvoiceItems.Where(a => a.TransactionNatureCode == "2").Sum(a => a.InvoiceQuentity.GetValueOrDefault());
+                var sum = invoiceFromFile.SupplierInvoiceItems.Where(a => a.TransactionNatureCode == "2").Sum(a => a.ItemPrice.GetValueOrDefault());
                 if (sum > 0)
                 {
                     invoice.SupplierInvoicePayments = new List<SupplierInvoicePaymentPM>
