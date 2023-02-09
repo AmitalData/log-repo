e522e8014781f51ca43f261077d396b4b9162260
Ahmad Abdalrazeq <a.abdalrazeq@logitudeworld.com>
2023-02-09 09:47:09 +0200

    clean

--- Logitude/AngularModules/AngularModules/Invoice/Services/StandardPMs/ARInvoicePMService.ts
+++ Logitude/AngularModules/AngularModules/Invoice/Services/StandardPMs/ARInvoicePMService.ts
@@ -141,9 +141,7 @@ export class ARInvoicePMService {
 			}
 
 			if (errorsArray.length == 0) {
-                console.log(entityPM)
 				var mappedEntity: ARInvoicePM = this.MapJsonToEntityPM(entityPM, false);
-                console.log(mappedEntity)
 				return this._http.put(this._apiUrl, JSON.stringify(mappedEntity), ServiceHelper.GetHttpFullHeaders())
 					.pipe(
 						map((response: HttpResponse<any>) => {
