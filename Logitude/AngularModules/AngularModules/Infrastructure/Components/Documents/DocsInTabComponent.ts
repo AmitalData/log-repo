04126f87d3c1eb02ffde38907c39ddfca4215814
Aisha Marmash <aishamarmash@gmail.com>
2023-02-12 13:19:23 +0200

    edit button availability

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/Documents/DocsInTabComponent.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/Documents/DocsInTabComponent.ts
@@ -302,6 +302,10 @@ export class DocsInTabComponent extends BaseComponent implements OnInit {
     }
 
     CheckApproveDocumentsAvailability() {
+        if (!FeatureLocator.HasFeaturePermession("General", "PendingApprovalDocuments")) {
+            this.IsApprovePendingDocumentsEnabled = false;
+            return;
+        }
         if ((this.ObjectTableName == "Shipment" || ObjectsLocator.GlobalSetting.WorkEnvironment == "Logitude") && SessionLocator.TenantPM.ApproveUploadedDocuments)
             this.IsApprovePendingDocumentsEnabled = true;
         if (this.IsApprovePendingDocumentsEnabled) {
