b96623b4c63564db2b2711450e1ee3d88cdb4c3c
Atallah Hammad <Atallah@logitudeworld.com>
2023-02-13 10:33:31 +0200

    Add WorkflowApiExceptionBuilder To Workflow controllers

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/WorkflowModel/Extended/FlowEntities/FlowOpportunityController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/WorkflowModel/Extended/FlowEntities/FlowOpportunityController.cs
@@ -13,6 +13,7 @@ using System.Transactions;
 using System.Web;
 using System.Web.Http;
 using WebFreight.Web.Helpers;
+using WebFreight.Web.Helpers.Workflow;
 using WebFreight.Web.Security;
 
 namespace WebFreight.Web.Controllers.WorkflowModel.Extended.FlowEntities
@@ -21,6 +22,8 @@ namespace WebFreight.Web.Controllers.WorkflowModel.Extended.FlowEntities
     {
         public HttpResponseMessage Post(OpportunityPM entityPM)
         {
+            int tenant = 0;
+
             if (ModelState.IsValid)
             {
                 try
@@ -30,7 +33,7 @@ namespace WebFreight.Web.Controllers.WorkflowModel.Extended.FlowEntities
                         string token = HttpContext.Current.Request.Headers["Token"];
                         AuthenticationToken authToken = AuthenticationTokenRepository.GetSingleTokenFromCache(token);
                         SecurityUtility.AuthenticationOnTenant(authToken.Tenant);
-                        int tenant = authToken.Tenant;
+                        tenant = authToken.Tenant;
 
                         ICRMContext MyContext = CRMContext.GetContext(entityPM.Tenant);
                         OpportunityUpdateService service = new OpportunityUpdateService(MyContext, new Dictionary<string, IContext>(), entityPM.Tenant);
@@ -44,7 +47,7 @@ namespace WebFreight.Web.Controllers.WorkflowModel.Extended.FlowEntities
 
                 catch (Exception ex)
                 {
-                    return Request.CreateResponse(HttpStatusCode.BadRequest, ApiExceptionBuilder.BuildException(ex));
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, WorkflowApiExceptionBuilder.BuildException(ex, tenant));
                 }
             }
             else
