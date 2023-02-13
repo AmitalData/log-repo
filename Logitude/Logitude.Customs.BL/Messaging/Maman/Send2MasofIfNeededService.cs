a958521f16e5e5aa07e1ada742412dd2e01639f7
Itzik Moshelian <itzikm@amitaldataltd.onmicrosoft.com>
2023-02-13 09:17:39 +0200

    #177370

--- Logitude/Logitude.Customs.BL/Messaging/Maman/Send2MasofIfNeededService.cs
+++ Logitude/Logitude.Customs.BL/Messaging/Maman/Send2MasofIfNeededService.cs
@@ -60,7 +60,7 @@ namespace Logitude.Customs.BL.Messaging.Maman
                     .Where(r => listStorageDefault.Contains(r.StorageSiteCode))
                     .Select(r => r.StorageSiteCode)
                     .FirstOrDefault();
-                sb.Append("myStorageSiteCode={myStorageSiteCode} IS NUL ???");
+                sb.Append($"myStorageSiteCode={myStorageSiteCode} IS NULL ???");
                 if (dbPM==null)
                 {
                     var qs = new DeclarationQueryService(drityEntityPM.Tenant);
