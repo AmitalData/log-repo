8bcc4868f0a0509db21a785621690a6e4347fb56
Libby Hasid <libby@amital.co.il>
2023-02-14 14:02:50 +0200

    #171522

--- Logitude/Logitude.CustomsMessaging/U2L/CommDec/CommDecService.cs
+++ Logitude/Logitude.CustomsMessaging/U2L/CommDec/CommDecService.cs
@@ -655,10 +655,10 @@ namespace Logitude.CustomsMessaging.U2L.CommDec
                     }
                         AppendLogLine("IF " + _MyDeclarationPM.CasualImporterCountry+ "!=IL   905" + _Stopwatch.Elapsed.ToString()); _Stopwatch.Restart();
                         AppendLogLine("IF " + this._AmitalCustomsFile.CreatedByUserId + "==RMXORN  905" + _Stopwatch.Elapsed.ToString()); _Stopwatch.Restart();
-                        if (_MyDeclarationPM.CasualImporterCountry!="IL" && this._AmitalCustomsFile.CreatedByUserId=="RMXORN")
+                        if (this._AmitalCustomsFile.CasualImporterCountry!="IL" && this._AmitalCustomsFile.CreatedByUserId=="RMXORN")
                     {
                             AppendLogLine("זה כן עובד 905" + _Stopwatch.Elapsed.ToString()); _Stopwatch.Restart();
-                            UpdateDeclarationPending("905");
+                          //  UpdateDeclarationPending("905");
                     }
                     if (currentDeclarationCourierStatusPM != null && currentDeclarationCourierStatusPM.CrateNumber != _LogitudeCommDecFile.CrateNumber)
                     {
