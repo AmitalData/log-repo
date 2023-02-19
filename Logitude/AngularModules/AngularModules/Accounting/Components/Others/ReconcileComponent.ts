2476cefb7ab5470ea2ac10ce9e3a798b1c86e22c
Alexander Khitrik <alexanderh@amital.co.il>
2023-02-19 10:09:49 +0200

    Alexander: 177653 Reconcile Screen sfter reconciliation

--- Logitude/AngularModules/AngularModules/Accounting/Components/Others/ReconcileComponent.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/Others/ReconcileComponent.ts
@@ -1500,7 +1500,9 @@ export class ReconcileComponent extends BaseComponent implements OnInit, OnDestr
 
                 this.CurrentSession.StopBusyIndicator();
 
-
+                this.ReloadScreen();
+                //this.SelectedLines.Clear();
+                this.CalculateTotals();
 
             }
 
