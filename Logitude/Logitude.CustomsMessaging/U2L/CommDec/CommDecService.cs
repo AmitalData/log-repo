808331bfa6b12200ad6d5e0092dd9287b58edbfb
Itzik Moshelian <itzikm@amitaldataltd.onmicrosoft.com>
2023-02-22 08:23:53 +0200

    fix merge

--- Logitude/Logitude.CustomsMessaging/U2L/CommDec/CommDecService.cs
+++ Logitude/Logitude.CustomsMessaging/U2L/CommDec/CommDecService.cs
@@ -352,8 +352,8 @@ namespace Logitude.CustomsMessaging.U2L.CommDec
                         DeclarationUpdateService.Update(this._MyDeclarationPM, true);
                         AppendLogLine("Update:MarkToDeleteSupplierInvoice:Took:" + _Stopwatch.Elapsed.ToString()); _Stopwatch.Restart();
                     }
-             
 
+                }
 
                 /////////////////////////////////////////////////////////////
                 _context = CustomContext.GetContext(_tenant);
