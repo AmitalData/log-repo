9074af5ec9f1d6bbfa2916d6bdad3b0b81f3b087
Maya Yacoub <maya@logitudeworld.com>
2023-02-27 12:02:30 +0200

    Customization: Deployment Package New Import (Deploy) #173653
    Related Work Items: #173653, #178030

---
+++ Logitude/CommunicationWorkerRole/DeploymentPackageWorkerRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationWorkerRole
+{
+    public class DeploymentPackageWorkerRole : WorkerEntryPoint
+    {
+        public DeploymentPackageWorkerRole()
+        {
+
+        }
+    }
+}
