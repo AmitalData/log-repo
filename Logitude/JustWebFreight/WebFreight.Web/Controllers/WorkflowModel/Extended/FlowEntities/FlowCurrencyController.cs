5468db0269435408f132e9190556b67bc456bba6
Atallah Hammad <Atallah@logitudeworld.com>
2023-03-01 11:49:18 +0200

    Support new tables as read only

---
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/WorkflowModel/Extended/FlowEntities/FlowCurrencyController.cs
@@ -0,0 +1,66 @@
+﻿using Logitude.BL.CommonDataModel.EntityPMs;
+using Logitude.BL.CommonDataModel.EntityQueries;
+using Logitude.Server.Tools.Helpers;
+using Logitude.Server.Tools.TreeFilterQuery;
+using Simplog.Data.CommonDataModel.EntityPOCOs;
+using Simplog.Data.CommonDataModel.Repositories;
+using Simplog.Server.Infrastructure.DataContracts;
+using System;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+using WebFreight.Web.Controllers.WorkflowModel.Models;
+using WebFreight.Web.DataContracts;
+using WebFreight.Web.Helpers.Workflow;
+using WebFreight.Web.Security;
+
+namespace WebFreight.Web.Controllers.WorkflowModel.Extended.FlowEntities
+{
+    public class FlowCurrencyController : ApiController
+    {
+        public HttpResponseMessage PostByFilterTree(ApiQueryTreeFilters apiQueryTreeFilters)
+        {
+            int tenant = 0;
+
+            try
+            {
+                string token = HttpContext.Current.Request.Headers["Token"];
+                AuthenticationToken authToken = AuthenticationTokenRepository.GetSingleTokenFromCache(token);
+                SecurityUtility.AuthenticationOnTenant(authToken.Tenant);
+                tenant = authToken.Tenant;
+
+                CurrencyQuery currenciesQuery = new CurrencyQuery(tenant);
+                IQueryable<CurrencyPM> currencyQuery = currenciesQuery.GetCurrenciesByTenantPM(tenant);
+
+                JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+                TreeFilterQueryArgs treeFilterQueryArgs = new TreeFilterQueryArgs()
+                {
+                    AdditionalTreeFilter = javaScriptSerializer.Serialize(apiQueryTreeFilters.QueryFilterItem),
+                    ObjectTableName = "Currency",
+                    Tenant = tenant,
+                };
+
+                currencyQuery = new TreeFilterQueryService().Apply(currencyQuery, treeFilterQueryArgs);
+                currencyQuery = currencyQuery.OrderBy(string.IsNullOrEmpty(apiQueryTreeFilters.OrderBy) ? "Id" : apiQueryTreeFilters.OrderBy);
+                currencyQuery = currencyQuery.Skip(0);
+                currencyQuery = currencyQuery.Take(apiQueryTreeFilters.PageSize);
+
+                var currencies = currencyQuery.Select("new { " + apiQueryTreeFilters.ReturnedColumns + " }").ToDynamicList();
+
+                ServiceResponse response = new ServiceResponse();
+                response.Result = currencies;
+                HttpResponseMessage reponseMessage = Request.CreateResponse(HttpStatusCode.OK, response);
+
+                return reponseMessage;
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, WorkflowApiExceptionBuilder.BuildException(ex, tenant));
+            }
+        }
+    }
+}
