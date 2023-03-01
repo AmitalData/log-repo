e911cf04a8b8a5be4a1bb1882d7cbd6199261536
Libby Hasid <libby@amital.co.il>
2023-03-01 13:33:01 +0200

    #177461

--- Logitude/AngularModules/AngularModules/Accounting/Components/Maintenance/FullAccountingSettingsComponent.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/Maintenance/FullAccountingSettingsComponent.ts
@@ -800,6 +800,23 @@ export class FullAccountingSettingsComponent extends BaseComponent implements On
 
 
     }
+    CreateControlAccount(ControlAccountId: string) {
+        this._ControlAccountId = ControlAccountId
+        if (!AppTool.IsNullOrEmpty(this[ControlAccountId])) {
+            var myMessageWindow = new MessageWindow();
+            myMessageWindow.Show("כרטיס מרכז מוגדר");
+            return;
+        }
+
+        let confirmWindow = new ConfirmWindow();
+        confirmWindow.WindowClosed.subscribe((event: any) => {
+            if (confirmWindow.Yes) {
+                this.CurrentSession.StartBusyIndicatorLoading();
+                this.SubmitChanges(ControlAccountId);
+            }
+        });
+        confirmWindow.Show("אנא אשר שמירה והוספה של חשבון מרכז");
+    }
     FullAccountingAddControl(ControlAccountId: string) {
         var windowTitle = TextCodeTranslator.Translate("TaxReport.B.Download");
 
