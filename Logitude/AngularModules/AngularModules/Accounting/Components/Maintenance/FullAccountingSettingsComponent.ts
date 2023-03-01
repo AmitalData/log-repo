913e2a75396a80f64a0c35fb8bd06ccc0d7f55ad
Libby Hasid <libby@amital.co.il>
2023-03-01 19:07:54 +0200

    #177461

--- Logitude/AngularModules/AngularModules/Accounting/Components/Maintenance/FullAccountingSettingsComponent.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/Maintenance/FullAccountingSettingsComponent.ts
@@ -146,6 +146,7 @@ export class FullAccountingSettingsComponent extends BaseComponent implements On
 
                     });
                 }
+                this.listCopyFromTenant0.sort(function(a, b){return (a.Id < b.Id ? -1 : 1)});
 
 
             });
