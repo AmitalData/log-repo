7d1fb31570967ecbfee870101e3d1742c5ef5183
Abd Al-Rahman Malakh <Abd@logitudeworld.com>
2023-03-01 09:30:02 +0200

    177727
    Related Work Items: #177727

--- Logitude/AngularModules/AngularModules/Workflow/Pipes/IsFieldOperatorPipe.ts
+++ Logitude/AngularModules/AngularModules/Workflow/Pipes/IsFieldOperatorPipe.ts
@@ -8,7 +8,7 @@ export class IsFieldOperatorPipe implements PipeTransform {
 
     transform(operatorCode: string) {
         if (operatorCode) {
-            return operatorCode.toString().endsWith("<field>") || operatorCode.toString().endsWith("<collection>") || operatorCode.toString().endsWith("<record>");
+            return operatorCode.toString().endsWith("<field>") || operatorCode.toString().endsWith("<record>");// || operatorCode.toString().endsWith("<collection>");
         }
         return false;
     }
