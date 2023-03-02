109aa5c933353108f5cb5455fd71484b4f751a87
Aisha Marmash <aishamarmash@gmail.com>
2023-03-02 13:41:53 +0200

    Customization: Support the custom fields on the card level #177685

--- Logitude/Logitude.BL/CommonDataModel/EntityQueries/CardQuery.cs
+++ Logitude/Logitude.BL/CommonDataModel/EntityQueries/CardQuery.cs
@@ -31,6 +31,7 @@ using Logitude.Server.Tools.Helpers;
 using Logitude.Server.Tools.QueueService;
 using Logitude.Server.Tools.CustomFields;
 using Simplog.Server.Infrastructure.DataContracts;
+using Logitude.BL.InfrastructureModel.EntityQueries;
 
 namespace Logitude.BL.CommonDataModel.EntityQueries
 {
@@ -1247,7 +1248,11 @@ namespace Logitude.BL.CommonDataModel.EntityQueries
 
         public IQueryable<CardList> GetIQueryableEntityList(IQueryable<Card> iQueryable)
         {
+            string objcetTableId = new ObjectTableQuery(0).GetObjectTableIdByName("Card");
             IQueryable<CardList> myResult = from card in iQueryable
+                                            join customFieldsMainObject in repository.context.CustomFieldsMainObjects.Where(d => d.ObjectTableId == objcetTableId) on card.Id equals customFieldsMainObject.EntityId into customFieldsMainObjectJoin
+                                            from customFieldsMainObject in customFieldsMainObjectJoin.DefaultIfEmpty()
+
                                             select new CardList()
                                             {
                                                 Code = card.Code,
@@ -1321,6 +1326,56 @@ namespace Logitude.BL.CommonDataModel.EntityQueries
                                                 RecordDate = card.UpdateDate != null ? card.UpdateDate : card.CreateDate,
                                                 BillToId = card.BillToId,
                                                 SATCustomerName = card.SATCustomerName,
+                                                Field1 = customFieldsMainObject != null ? customFieldsMainObject.Field1 : null,
+                                                Field2 = customFieldsMainObject != null ? customFieldsMainObject.Field2 : null,
+                                                Field3 = customFieldsMainObject != null ? customFieldsMainObject.Field3 : null,
+                                                Field4 = customFieldsMainObject != null ? customFieldsMainObject.Field4 : null,
+                                                Field5 = customFieldsMainObject != null ? customFieldsMainObject.Field5 : null,
+                                                Field6 = customFieldsMainObject != null ? customFieldsMainObject.Field6 : null,
+                                                Field7 = customFieldsMainObject != null ? customFieldsMainObject.Field7 : null,
+                                                Field8 = customFieldsMainObject != null ? customFieldsMainObject.Field8 : null,
+                                                Field9 = customFieldsMainObject != null ? customFieldsMainObject.Field9 : null,
+                                                Field10 = customFieldsMainObject != null ? customFieldsMainObject.Field10 : null,
+                                                Field11 = customFieldsMainObject != null ? customFieldsMainObject.Field11 : null,
+                                                Field12 = customFieldsMainObject != null ? customFieldsMainObject.Field12 : null,
+                                                Field13 = customFieldsMainObject != null ? customFieldsMainObject.Field13 : null,
+                                                Field14 = customFieldsMainObject != null ? customFieldsMainObject.Field14 : null,
+                                                Field15 = customFieldsMainObject != null ? customFieldsMainObject.Field15 : null,
+                                                Field16 = customFieldsMainObject != null ? customFieldsMainObject.Field16 : null,
+                                                Field17 = customFieldsMainObject != null ? customFieldsMainObject.Field17 : null,
+                                                Field18 = customFieldsMainObject != null ? customFieldsMainObject.Field18 : null,
+                                                Field19 = customFieldsMainObject != null ? customFieldsMainObject.Field19 : null,
+                                                Field20 = customFieldsMainObject != null ? customFieldsMainObject.Field20 : null,
+                                                Field21 = customFieldsMainObject != null ? customFieldsMainObject.Field21 : null,
+                                                Field22 = customFieldsMainObject != null ? customFieldsMainObject.Field22 : null,
+                                                Field23 = customFieldsMainObject != null ? customFieldsMainObject.Field23 : null,
+                                                Field24 = customFieldsMainObject != null ? customFieldsMainObject.Field24 : null,
+                                                Field25 = customFieldsMainObject != null ? customFieldsMainObject.Field25 : null,
+                                                Field26 = customFieldsMainObject != null ? customFieldsMainObject.Field26 : null,
+                                                Field27 = customFieldsMainObject != null ? customFieldsMainObject.Field27 : null,
+                                                Field28 = customFieldsMainObject != null ? customFieldsMainObject.Field28 : null,
+                                                Field29 = customFieldsMainObject != null ? customFieldsMainObject.Field29 : null,
+                                                Field30 = customFieldsMainObject != null ? customFieldsMainObject.Field30 : null,
+                                                Field31 = customFieldsMainObject != null ? customFieldsMainObject.Field31 : null,
+                                                Field32 = customFieldsMainObject != null ? customFieldsMainObject.Field32 : null,
+                                                Field33 = customFieldsMainObject != null ? customFieldsMainObject.Field33 : null,
+                                                Field34 = customFieldsMainObject != null ? customFieldsMainObject.Field34 : null,
+                                                Field35 = customFieldsMainObject != null ? customFieldsMainObject.Field35 : null,
+                                                Field36 = customFieldsMainObject != null ? customFieldsMainObject.Field36 : null,
+                                                Field37 = customFieldsMainObject != null ? customFieldsMainObject.Field37 : null,
+                                                Field38 = customFieldsMainObject != null ? customFieldsMainObject.Field38 : null,
+                                                Field39 = customFieldsMainObject != null ? customFieldsMainObject.Field39 : null,
+                                                Field40 = customFieldsMainObject != null ? customFieldsMainObject.Field40 : null,
+                                                Field41 = customFieldsMainObject != null ? customFieldsMainObject.Field41 : null,
+                                                Field42 = customFieldsMainObject != null ? customFieldsMainObject.Field42 : null,
+                                                Field43 = customFieldsMainObject != null ? customFieldsMainObject.Field43 : null,
+                                                Field44 = customFieldsMainObject != null ? customFieldsMainObject.Field44 : null,
+                                                Field45 = customFieldsMainObject != null ? customFieldsMainObject.Field45 : null,
+                                                Field46 = customFieldsMainObject != null ? customFieldsMainObject.Field46 : null,
+                                                Field47 = customFieldsMainObject != null ? customFieldsMainObject.Field47 : null,
+                                                Field48 = customFieldsMainObject != null ? customFieldsMainObject.Field48 : null,
+                                                Field49 = customFieldsMainObject != null ? customFieldsMainObject.Field49 : null,
+                                                Field50 = customFieldsMainObject != null ? customFieldsMainObject.Field50 : null,
                                             };
 
             if (myResult.Count() > 0)
