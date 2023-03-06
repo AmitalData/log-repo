06e12b39f13a45d4a0e51cb524f03e70e615218d
Frida Lok <frida@amital.co.il>
2023-03-06 13:09:26 +0200

    #178788

--- Logitude/AngularModules/AngularModules/Customs/Components/MenuButtons/DeclarationMenuButtonsHandler.ts
+++ Logitude/AngularModules/AngularModules/Customs/Components/MenuButtons/DeclarationMenuButtonsHandler.ts
@@ -363,17 +363,17 @@ export class DeclarationMenuButtonsHandler implements OnDestroy {
                     if (button.EventCode == "PrintTzrufa") // moran 2.3.16 - Task 19807
                     {
                         //if (!AppTool.IsNullOrEmpty(this.EntityPM.CustomFileNo) && AmitalGatewayUtil.Instance.AmitalBrowserInUse) {
-                        //if (AmitalGatewayUtil.Instance.IsDeclarationInUse(this.EntityPM.CustomFileNo, this.EntityPM.IsConvertedDeclaration, this.EntityPM.IsConnectedToUnifreight)) {
+                       // if (AmitalGatewayUtil.Instance.IsDeclarationInUse(this.EntityPM.CustomFileNo, this.EntityPM.IsConvertedDeclaration, this.EntityPM.IsConnectedToUnifreight)) {
                             //if (!this.EntityPM.IsAccumulated) {
                             //    button.IsDisabled = false;
                             //} else {
                             //    button.IsDisabled = true;
                             //}
-                            //button.IsDisabled = false;
-                        //}
-                        //else {
-                            button.IsDisabled = true;
-                        //}
+                            button.IsDisabled = false;
+                       // }
+                        // else {
+                        //     button.IsDisabled = true;
+                        // }
                     }
 
                     if (button.EventCode == "PrintAccumaltedTzrufa") {
