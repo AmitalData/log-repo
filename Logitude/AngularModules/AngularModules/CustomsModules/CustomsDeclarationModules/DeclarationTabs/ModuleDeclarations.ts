5afc229c99a70744d10afec39d2f4b115077d388
Shoshana Greenwald <shoshana@amital.co.il>
2023-03-12 17:19:23 +0200

    #178466

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationTabs/ModuleDeclarations.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationTabs/ModuleDeclarations.ts
@@ -31,6 +31,8 @@ import { ExportDeclarationComponent } from './Components/General/ExportDeclarati
 import { ExportConsigmentContentComponent } from './Components/General/ConsigmentTabContent/ExportConsigmentContentComponent';
 import { ExportDeclarationClosureComponent } from './Components/Closure/ExportDeclarationClosureComponent';
 import { DeclarationExportStorageComponent } from './Components/ExportStorageList/DeclarationExportStorageComponent';
+import { ComprehensiveUpdateConsignment } from './Components/General/ConsigmentTabContent/ComprehensiveUpdateConsignment';
+
 
 export const Components =
     [
@@ -65,7 +67,8 @@ export const Components =
         DeclarationCargoSealTabComponent,
         ExportDeclarationComponent,
         ExportDeclarationClosureComponent,
-        DeclarationExportStorageComponent
+        DeclarationExportStorageComponent,
+        ComprehensiveUpdateConsignment
 
     ];
 
@@ -109,6 +112,7 @@ export class ModuleDeclarations {
             case "DeclarationCargoSealTabComponent": { myResult = DeclarationCargoSealTabComponent; break; }
             case "ExportDeclarationComponent": { myResult = ExportDeclarationComponent; break; }
             case "ExportDeclarationClosureComponent": { myResult = ExportDeclarationClosureComponent; break; }
+            case "ComprehensiveUpdateConsignment": { myResult = ComprehensiveUpdateConsignment; break; }
 
         }
 
