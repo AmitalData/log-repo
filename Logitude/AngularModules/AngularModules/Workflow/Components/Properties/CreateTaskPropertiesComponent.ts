f83921df13bfcc366d73624b7fa6855a26b30a66
Abd Al-Rahman Malakh <Abd@logitudeworld.com>
2023-03-13 15:27:23 +0200

    178745

--- Logitude/AngularModules/AngularModules/Workflow/Components/Properties/CreateTaskPropertiesComponent.ts
+++ Logitude/AngularModules/AngularModules/Workflow/Components/Properties/CreateTaskPropertiesComponent.ts
@@ -1,13 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { BaseComponent } from "Infrastructure/Components/LogitudeComponents/BaseComponent";
+import { UIProperty } from "Infrastructure/Components/LogitudeComponents/UIProperties";
+import { ApiQueryFilters } from "Infrastructure/DataContracts/ApiQueryFilters";
 import { ObjectFieldList } from "Infrastructure/EntityLists/ObjectFieldList";
 import { AppTool } from "Infrastructure/Tools";
 import { SessionLocator } from "Infrastructure/Utilities/SessionLocator";
+import { ConditionOperations } from "Workflow/Constants/ConditionOperations";
+import { ConditionOperators } from "Workflow/Constants/ConditionOperators";
 import { FieldTypes } from "Workflow/Constants/FieldTypes";
 import { SetValueOperators } from "Workflow/Constants/SetValueOperators";
+import { Condition } from "Workflow/Models/Condition";
 import { SetValue } from "Workflow/Models/SetValue";
+import { TaskEntityField } from "Workflow/Models/TaskEntityField";
 import { TreeSelectItem } from "Workflow/Models/TreeSelectItem";
 import { EditableRecordsTreeList } from "Workflow/TreeLists/EditableRecordsTreeList";
+import { ObjectFieldsTreeList } from "Workflow/TreeLists/ObjectFieldsTreeList";
 import { FlowReader } from "Workflow/Utilities/FlowReader";
 import { ObjectFields } from "Workflow/Utilities/ObjectFields";
 import { ObjectTables } from "Workflow/Utilities/ObjectTables";
@@ -16,25 +23,31 @@ import { ObjectTables } from "Workflow/Utilities/ObjectTables";
     templateUrl: "./CreateTaskPropertiesComponent.html"
 })
 
-export class CreateTaskPropertiesComponent extends BaseComponent {
+export class CreateTaskPropertiesComponent extends BaseComponent implements OnInit {
 
     public DataContext: any = this;
     public Data: any;
     public IsNew: boolean;
+    public TaskEntityId: string;
     public Name: string = null;
     public Record: string;
+    public Entity: string;
+    public IsRecordEntityChanged: boolean = false;
+    public TaskTypeId: string;
     public SetValues: SetValue[];
-    public TaskEntityId: string;
+    public EntityFields: TaskEntityField[];
     public FlowObject: any;
     public CurrentNodeId: string;
     public ValidationErrorsList: string[];
     public IsValidSetValues: boolean = true;
+    public IsValidEntityFields: boolean = true;
     public EditableRecordsTreeItems: TreeSelectItem[];
+    public ObjectFieldsTreeItems: TreeSelectItem[];
+    public TaskTypeQueryFilters: ApiQueryFilters;
     public CurrentSession = SessionLocator.SelectedSession;
 
-    public InitialTaskFields: string[] = [
+    public InitialSetValueTaskFields: string[] = [
         "Subject",
-        "TaskTypeId",
         "OwnerId",
         "DueDate",
         "PriorityId"
@@ -48,8 +61,9 @@ export class CreateTaskPropertiesComponent extends BaseComponent {
 
     ngOnInit() {
         this.initializeWindowEvents();
-        this.initialize();
         this.initializeEditableRecordsTreeItems();
+        this.initialize();
+        this.initializeObjectFieldsTreeItems();
     }
 
     initializeWindowEvents() {
@@ -62,42 +76,67 @@ export class CreateTaskPropertiesComponent extends BaseComponent {
         });
     }
 
-    initialize() {
-        this.IsNew = Object.keys(this.Data).length === 0;
-
-        this.Name = this.Data["label"] || this.Data["name"] || null;
-        this.Record = this.Data["record"] || null;
-        this.SetValues = this.Data["setValues"] || [];
-
-        this.TaskEntityId = ObjectTables.getIdByName("Task");
-
-        this.initializeSetValues();
-        this.setUIProperties();
-    }
-
     initializeEditableRecordsTreeItems() {
         this.EditableRecordsTreeItems = new EditableRecordsTreeList(this.FlowObject, this.CurrentNodeId).Items;
     }
 
+    initialize() {
+        this.IsNew = Object.keys(this.Data).length === 0;
+        this.TaskEntityId = ObjectTables.getIdByName("Task");
+
+        this.Name = this.Data["label"] || this.Data["name"] || null;
+        this.Record = this.Data["record"] || null;
+        this.Entity = this.Data["entity"] || null;
+        this.TaskTypeId = this.Data["taskTypeId"] || null;
+        this.SetValues = this.Data["setValues"] || [];
+        this.EntityFields = this.Data["fields"] || [];
+
+        this.handleRecordEntityChanged();
+        this.setTaskTypeQueryFilters();
+        this.initializeSetValues();
+        this.setUIProperties();
+    }
+
+    initializeObjectFieldsTreeItems() {
+        let entityId = ObjectTables.getIdByName(this.Entity);
+        this.ObjectFieldsTreeItems = new ObjectFieldsTreeList(entityId).Items;
+    }
+
+    handleRecordEntityChanged() {
+        if (this.Record) {
+            let editableRecordItem = this.EditableRecordsTreeItems.filter(i => i.key === this.Record)[0];
+            if (editableRecordItem && editableRecordItem.data && editableRecordItem.data["entity"] !== this.Entity) {
+                this.updateRecord(editableRecordItem);
+            }
+        }
+    }
+
+    setTaskTypeQueryFilters() {
+        let apiQueryFilters = new ApiQueryFilters(false);
+        let entityObjectTableId = ObjectTables.getIdByName(this.Entity);
+        apiQueryFilters.addAdditionalFilter("EntityObjectTableId", entityObjectTableId, null, null, "Equals", false, false, false, "Text");
+        this.TaskTypeQueryFilters = apiQueryFilters;
+    }
+
     initializeSetValues() {
         if (this.SetValues.length === 0) {
-            this.getInitialTaskObjectFields().forEach(objectField => {
+            this.getInitialSetValueTaskObjectFields().forEach(objectField => {
                 this.addSetValue(objectField);
             });
             this.IsValidSetValues = false;
         }
     }
 
-    getInitialTaskObjectFields() {
+    getInitialSetValueTaskObjectFields() {
         return ObjectFields.getByObjectTableId(this.TaskEntityId)
-            .filter(o => this.InitialTaskFields.includes(o.FieldName))
+            .filter(o => this.InitialSetValueTaskFields.includes(o.FieldName))
             .sort((a, b) => this.getTaskObjectFieldOrder(a.FieldName) - this.getTaskObjectFieldOrder(b.FieldName));
     }
 
     getTaskObjectFieldOrder(fieldName: string) {
-        let taskObjectFieldOrder = this.InitialTaskFields.length + 1;
+        let taskObjectFieldOrder = this.InitialSetValueTaskFields.length + 1;
         if (fieldName) {
-            let taskObjectFieldIndex = this.InitialTaskFields.indexOf(fieldName);
+            let taskObjectFieldIndex = this.InitialSetValueTaskFields.indexOf(fieldName);
             return taskObjectFieldIndex === -1 ? taskObjectFieldOrder : taskObjectFieldIndex;
         }
         return taskObjectFieldOrder;
@@ -115,6 +154,27 @@ export class CreateTaskPropertiesComponent extends BaseComponent {
             setValue.disabled = "d,f";
             this.SetValues.push(setValue);
         }
+    }
+
+    addEntityField() {
+        if (this.IsValidEntityFields) {
+            let entityField = new TaskEntityField();
+            this.EntityFields.push(entityField);
+            this.IsValidEntityFields = false;
+        }
+    }
+
+    deleteEntityField(entityFieldIndex: number) {
+        let entityField = this.EntityFields[entityFieldIndex];
+        if (entityField) {
+            this.EntityFields.splice(entityFieldIndex, 1);
+            this.validateEntityFields();
+        }
+    }
+
+    resetEntityFields() {
+        this.EntityFields = [];
+        this.IsValidEntityFields = true;
     }
 
     updateIsValidSetValues(isValidSetValues: boolean) {
@@ -137,13 +197,48 @@ export class CreateTaskPropertiesComponent extends BaseComponent {
         let record = recordItem ? recordItem.key : null;
         let recordUsedFrom = recordItem && recordItem.data ? (recordItem.data["nodeId"] || null) : null;
 
+        let isEntityChanged = this.Data["entity"] !== entity;
+
         this.Record = record;
+        this.Entity = entity;
         this.Data["record"] = record;
         this.Data["entity"] = entity;
         this.Data["isCustomEntity"] = ObjectTables.getIsCustomByName(entity);
         this.Data["recordUsedFrom"] = recordUsedFrom;
 
+        if (isEntityChanged) {
+            this.updateTaskTypeId(null);
+            this.setTaskTypeQueryFilters();
+            this.resetEntityFields();
+            this.initializeObjectFieldsTreeItems();
+            this.IsRecordEntityChanged = !this.IsRecordEntityChanged;
+        }
+
         this.setUIProperties();
+    }
+
+    updateTaskTypeId(taskTypeId: string | null) {
+        this.TaskTypeId = taskTypeId || null;
+        this.Data["taskTypeId"] = taskTypeId || null;
+    }
+
+    updateEntityField(objectFieldItem: TreeSelectItem, entityFieldIndex: number) {
+        let objectField = objectFieldItem ? (objectFieldItem.data["objectField"] || null) : null;
+        this.EntityFields[entityFieldIndex].fieldCode = objectField ? objectField.FieldCode : null;
+        this.EntityFields[entityFieldIndex].field = objectField ? objectField.FieldName : null;
+        this.validateEntityFields();
+    }
+
+    validateEntityFields() {
+        if (this.EntityFields) {
+            this.IsValidEntityFields = this.EntityFields.filter(e => !e.fieldCode).length === 0;
+        } else {
+            this.IsValidEntityFields = true;
+        }
+    }
+
+    updateEntityFieldRequired(isRequired: boolean, entityFieldIndex: number) {
+        this.EntityFields[entityFieldIndex].isRequired = isRequired;
     }
 
     setUIProperties() {
@@ -159,25 +254,73 @@ export class CreateTaskPropertiesComponent extends BaseComponent {
         this.ValidationErrorsList = [];
         let notValidUIProperties = this.UIProperties.UIPropertyList.filter(u => !u.ValidValue);
         let isValidName = !this.IsNew || !FlowReader.isNodeCodeExists(this.FlowObject, this.Name);
-        if (notValidUIProperties.length === 0 && this.IsValidSetValues && isValidName) {
-            this.setValuesData();
-            console.log(this.Data);
-            this.CurrentSession.CurrentWindow.Close(this.Data);
+        let isValidSave = notValidUIProperties.length === 0 && this.IsValidSetValues && this.IsValidEntityFields && isValidName;
+
+        if (isValidSave) {
+            this.completeSave();
         } else {
-            let validationErrors = notValidUIProperties.map(t => { return t.ValidationError; });
-            this.ValidationErrorsList = validationErrors;
-
-            if (!this.IsValidSetValues) {
-                this.ValidationErrorsList.push("Invalid Set Values");
-            }
-
-            if (!isValidName) {
-                this.ValidationErrorsList.push("The Name Should be Unique.");
-            }
+            this.handleSaveValidationErrors(notValidUIProperties, isValidName);
         }
     }
 
-    setValuesData() {
+    completeSave() {
+        this.setSetValuesData();
+        this.setEntityFieldsData();
+        this.setDoneConditionsFromFields();
+        //console.log(this.Data);
+        this.CurrentSession.CurrentWindow.Close(this.Data);
+    }
+
+    handleSaveValidationErrors(notValidUIProperties: UIProperty[], isValidName: boolean) {
+        let validationErrors = notValidUIProperties.map(t => { return t.ValidationError; });
+        this.ValidationErrorsList = validationErrors;
+        if (!this.IsValidSetValues) {
+            this.ValidationErrorsList.push("Invalid Set Values");
+        }
+        if (!this.IsValidEntityFields) {
+            this.ValidationErrorsList.push("Invalid Fields");
+        }
+        if (!isValidName) {
+            this.ValidationErrorsList.push("The Name Should be Unique.");
+        }
+    }
+
+    setSetValuesData() {
         this.Data["setValues"] = this.SetValues;
+    }
+
+    setEntityFieldsData() {
+        this.Data["fields"] = this.EntityFields && this.EntityFields.length > 0 ? this.EntityFields : null;
+    }
+
+    setDoneConditionsFromFields() {
+        let doneConditions = this.getConditionsFromEntityFields(this.EntityFields);
+        this.Data["doneConditions"] = doneConditions && doneConditions.length > 0 ? doneConditions : null;
+        this.Data["doneConditionsOperation"] = doneConditions && doneConditions.length > 0 ? ConditionOperations.And : null;
+    }
+
+    getConditionsFromEntityFields(entityFields: TaskEntityField[]) {
+        if (entityFields && entityFields.length > 0) {
+            return entityFields.filter(entityField => entityField.isRequired)
+                .map(entityField => { return this.getConditionFromEntityField(entityField); })
+                .filter(doneCondition => doneCondition !== null);
+        }
+        return [];
+    }
+
+    getConditionFromEntityField(entityField: TaskEntityField) {
+        let objectField = ObjectFields.getByCode(entityField?.fieldCode);
+        if (entityField && entityField.isRequired && objectField) {
+            let doneCondition = new Condition();
+            doneCondition.fieldCode = objectField.FieldCode;
+            doneCondition.field = objectField.FieldName;
+            doneCondition.type = objectField.DataTypeCode;
+            doneCondition.lookupType = objectField.DataTypeCode === FieldTypes.LookUp ? ObjectTables.getNameById(objectField.LookUpTableId) : null;
+            doneCondition.picklistType = objectField.DataTypeCode === FieldTypes.PickList ? objectField.CustomPickListCode : null;
+            doneCondition.operator = ConditionOperators.IsEmpty;
+            doneCondition.value = "False";
+            return doneCondition;
+        }
+        return null;
     }
 }
---
+++ Logitude/AngularModules/AngularModules/Workflow/Models/TaskEntityField.ts
@@ -0,0 +1,11 @@
+export class TaskEntityField {
+    public field: string;
+    public fieldCode: string;
+    public isRequired: boolean;
+
+    constructor() {
+        this.field = null;
+        this.fieldCode = null;
+        this.isRequired = false;
+    }
+}
