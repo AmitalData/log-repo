91bdb39e05eebe5ef69916a160c4386dfa4dd814
Aseel Zbaidah <aseel@logitudeworld.com>
2023-03-15 15:09:04 +0200

    #178246

--- Logitude/AngularModules/AngularModules/Infrastructure/Pipes/ContainerDateTimeToColorPipe.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Pipes/ContainerDateTimeToColorPipe.ts
@@ -3,40 +3,33 @@ import {AppTool, DateTool, FontTool} from '../Tools';
 
 @Pipe({ name: 'ContainerDateTimeToColorPipe' })
 
-export class ContainerDateTimeToColorPipe {
-    transform(value: Date, defaultColor: string = null, comparisonDate: Date = null): string {
+export class ContainerDateTimeToColorPipe {      
+    transform(value: Date, comparisonDate: Date = null): string {
 
         var myResult = FontTool.Black;
-
-        switch (defaultColor) {
-            case "O": {
-                myResult = FontTool.Orange;
-                break;
-            }
-            case "A": { // ActivityWork
-                myResult = "rgb(110,113,114)";
-                break;
-            }
-            default: {
-                myResult = FontTool.Black;
-                break;
-            }
-        }
-
         if (!AppTool.IsNullOrEmpty(value)) {
             var myDateTicks = DateTool.GetDateParts(value).DateTicks;
 
             var myDateFilterTicks: number;
-            myDateFilterTicks = DateTool.GetDateParts(DateTool.GetCurrentDateAsUtc()).DateTicks;
-            
-            if (myDateTicks < myDateFilterTicks) {
-                myResult = FontTool.Red;
-            }
-            else if (myDateTicks == myDateFilterTicks) {
-                myResult = FontTool.Orange;
-            }
-        }
 
+            if (comparisonDate) {
+                myDateFilterTicks = DateTool.GetDateParts(comparisonDate).DateTicks;
+            }
+
+            else {
+                myDateFilterTicks = DateTool.GetDateParts(DateTool.GetCurrentDateAsUtc()).DateTicks;
+
+                if (myDateTicks < myDateFilterTicks) {
+                    myResult = FontTool.Red;
+                }
+                else if (myDateTicks == myDateFilterTicks) {
+                    myResult = FontTool.Orange;
+                }
+            }
+            
+           
+        
+        }
         return myResult;
     }
 }
