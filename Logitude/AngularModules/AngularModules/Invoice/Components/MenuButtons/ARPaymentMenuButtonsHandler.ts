d1277a1bc41446bc5e7e0dbe26f2eae1d48aeaf5
Ameerah <ameerah@logitudeworld.com>
2023-03-27 09:19:11 +0200

    Just fro full accounting user

--- Logitude/AngularModules/AngularModules/Invoice/Components/MenuButtons/ARPaymentMenuButtonsHandler.ts
+++ Logitude/AngularModules/AngularModules/Invoice/Components/MenuButtons/ARPaymentMenuButtonsHandler.ts
@@ -107,7 +107,7 @@ export class ARPaymentMenuButtonsHandler {
                             if (this.EntityPM.Id == null && this.EntityPM.StatusCode == "VD") {
                                 button.IsDisabled = true;
                             }
-                            else if (this.EntityPM.StatusCode == "DR") {
+                            else if (SessionLocator.TenantPM.AccountingActivated && this.EntityPM.StatusCode == "DR") {
                                 button.IsDisabled = true;
                             }
                             else {
