2ae2de27037e69b0fbd26cb16914c02790681ba1
Atara Oster <Atara@amital.co.il>
2023-03-29 12:29:59 +0300

    #178461

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationOthers/Components/CloseDeclaration/ExportDeclarationClosingDataComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationOthers/Components/CloseDeclaration/ExportDeclarationClosingDataComponent.ts
@@ -717,7 +717,7 @@ export class ExportDeclarationClosingDataComponent extends BaseComponent {
         windowArgs.Warning = this.ValidationErrors;
         windowArgs.ComponentHeight = '328px';
         var windowTitle = title;
-        windowArgs.SaveButtonText = "המשך";
+        windowArgs.SaveButtonText = TextCodeTranslator.Translate("Customs.General.B.OK");
         windowArgs.CancelButtonVisibility = true;
         var logWindow = new LogitudeWindow();
         logWindow.Width = 600;
