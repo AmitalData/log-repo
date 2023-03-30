fab0aec78cdacfc2eb405426525f17ab0a5e4a6c
Ameerah <ameerah@logitudeworld.com>
2023-03-30 14:47:35 +0200

    Logic is implemented in all cards

--- Logitude/Logitude.CargoTracking.BL/CoreBL/CargoTrackingShipmenPartnersCardsBuilder.cs
+++ Logitude/Logitude.CargoTracking.BL/CoreBL/CargoTrackingShipmenPartnersCardsBuilder.cs
@@ -247,9 +247,8 @@ namespace Logitude.CargoTracking.BL.EntityQueryServices
             {
                 var name = GetProperty(shipmentOrderPM, partnerCardMetaData.NameFieldName);
                 var address = GetPartnerAddress(id);
-                if (partnerCardMetaData.SkipIfEmpty &&
-                    (string.IsNullOrEmpty(name) || name.Equals("---")) &&
-                    (string.IsNullOrEmpty(address) || address.Equals(",")))
+                if ( (string.IsNullOrEmpty(name) || name.Equals("---")) &&
+                    (string.IsNullOrEmpty(address) || address.Equals(",")) )
                     return;
                 partnerCards.Add(new PartnerCard()
                 {
