6133d741477cb65eaeae30c73762ff2c3fea273c
Itzik Moshelian <itzikm@amitaldataltd.onmicrosoft.com>
2023-04-10 14:33:30 +0300

    #176673

--- Logitude/AngularModules/AngularModules/Accounting/Components/Others/JournalReconcileComponent.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/Others/JournalReconcileComponent.ts
@@ -19,6 +19,7 @@ import {FullAccountingSettingList} from '../../EntityLists/FullAccountingSetting
 import {GLAccountPMService} from '../../Services/StandardPMs/GLAccountPMService';
 import {MessageWindow} from '../../../Controls/Windows/MessageWindow';
 import {ConfirmWindow} from '../../../Controls/Windows/ConfirmWindow';
+import { promise } from 'selenium-webdriver';
 
 @Component({
 
@@ -342,12 +343,24 @@ export class JournalReconcileComponent extends BaseComponent implements OnInit {
     TotalDifference: any;
     TotalCredit: any;
     TotalDebit: any;
-    SetWindowArgs(winArgs) {
+    SourceGLAccountPM: GLAccountPM;
+    async SetWindowArgs(winArgs) {
         this._SelectedLines = winArgs.SelectedLines;
         this._GLAccountPMId = winArgs.GLAccountPMId;
         this.TotalDifference = winArgs.TotalDifference;
         this.TotalCredit = winArgs.TotalCredit;
         this.TotalDebit = winArgs.TotalDebit;
+
+        await new Promise<void>((resolve) => { 
+        this.gLAccountPMService.get(this._GLAccountPMId).subscribe((myResponse: ServiceResponse) => {
+            if (!myResponse.HasError) {
+                var res = myResponse.Result;
+                this.SourceGLAccountPM = res;
+                resolve();
+            }
+        });
+        });
+
     }
     FillErrors(isSplitJournal: boolean) {
         this.ValidationErrorsList = [];
@@ -382,11 +395,65 @@ export class JournalReconcileComponent extends BaseComponent implements OnInit {
         }
     }
 
-    OkButtonClicked(isSplitJournal: boolean) {
+    async OkButtonClicked(isSplitJournal: boolean) {
         this.FillErrors(isSplitJournal);
-
+        
         if (this.ValidationErrorsList.length > 0) {
             return;
+        }
+
+        if (
+            !AppTool.IsNullOrEmpty(this.SourceGLAccountPM.CurrencyId)
+            && this.GLAccount.IsMultiCurrency) {
+            let pmGLAccount: GLAccountPM = null;
+            this.CurrentSession.StartBusyIndicator("");
+            await new Promise<void>((resolve) => {
+                this.gLAccountPMService.get(this.GLAccountId).subscribe((myResponse: ServiceResponse) => {
+                    this.CurrentSession.StopBusyIndicator();
+                    if (!myResponse.HasError) {
+                        var res = myResponse.Result;
+                        pmGLAccount = res;
+
+                        resolve();
+                    }
+                });
+            });
+            const adjustGLAccountCurrency =
+                pmGLAccount.GLAccountCurrencies.filter(r => r.CurrencyId == this.SourceGLAccountPM.CurrencyId)[0];
+            if (adjustGLAccountCurrency != null) {
+
+                let selectedAccount: string = this.glAccount.LocalName + " " + this.glAccount.DisplayNumber;
+
+                let toContinue: boolean = false;
+                const confirmMsg =
+                    //TextCodeTranslator.Translate('Journal.RE.AccountingDateConfrimation');
+                    `לידיעתך הכרטיס הנבחר-${selectedAccount} שהיינו רב מטבעי
+מקושר לכרטיס-${adjustGLAccountCurrency.GLAccountName}  שמטבעו ${this.SourceGLAccountPM.CurrencyCode}
+ולכן הפקודה תירשם על הכרטיס המקושר
+האם להמשיך
+`;
+                const confirmWindow = new ConfirmWindow();
+                confirmWindow.Width = 400;
+                confirmWindow.Left = '25%';
+                confirmWindow.YesButtonText = TextCodeTranslator.Translate('Customs.General.B.OK');
+                confirmWindow.NoButtonText = TextCodeTranslator.Translate('Customs.General.B.Cancel');
+                confirmWindow.Show(confirmMsg);
+
+                await new Promise<void>((resolve) => {
+                    confirmWindow.WindowClosed.subscribe((event: any) => {
+                        confirmWindow.Close();
+                        if (confirmWindow.Yes) {
+                            toContinue = true;
+                        }
+                        resolve();   
+                    });
+                });
+                if (!toContinue) {
+                    return;
+                }
+
+                
+            }
         }
 
         const reconciliationLines: ReconciliationLinePM[] = [];
