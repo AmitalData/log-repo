03a2e12429c924fe238f041791cd1a5f209b91b7
Sohaib Nemer <sohaib@amital.co.il>
2023-05-03 11:26:44 +0300

    #180896

--- Logitude/Logitude.Customs.BL/Messaging/Customs/SignQueueBL/CreateSignQueueHSMDBService.cs
+++ Logitude/Logitude.Customs.BL/Messaging/Customs/SignQueueBL/CreateSignQueueHSMDBService.cs
@@ -1,4 +1,5 @@
-﻿using Logitude.CustomsMessaging.Common.RequestParams;
+﻿using Logitude.Customs.BL.EntityQueryServices;
+using Logitude.CustomsMessaging.Common.RequestParams;
 using Logitude.Server.Tools.ExternalServices;
 using Logitude.Server.Tools.Helpers;
 using System;
@@ -19,7 +20,8 @@ namespace Logitude.Customs.BL.Messaging.Customs.SignQueueBL
             Dictionary<string, string> messageProperties = new Dictionary<string, string>();
             messageProperties.Add("SignByPersonalId", requestParams.SignByPersonalId);//SignBy
             messageProperties.Add("SignQueueByCompanyOrPersonal", requestParams.SignQueueByCompanyOrPersonal);//SignBy
-            
+            var customsRequestsSheetQueryService = new CustomsRequestsSheetQueryService(requestParams.Tenant);
+            var currCustomsRequestsSheet = customsRequestsSheetQueryService.GetTenantPriorityByEntityID(requestParams.CustomsRequestsSheetId, requestParams.Tenant);
             messageProperties.Add("InterfaceTypeCode", requestParams.InterfaceTypeCode);
             messageProperties.Add("Tenant", requestParams.Tenant.ToString());
             messageProperties.Add("CorrelationId", requestParams.CustomsRequestsSheetId);
@@ -30,6 +32,7 @@ namespace Logitude.Customs.BL.Messaging.Customs.SignQueueBL
             queueSendModel.UseRabbitMQ = false;//never use rabbit !!!!
             queueSendModel.InterfaceTypeCode = requestParams.InterfaceTypeCode;
             queueSendModel.Tenant = requestParams.Tenant;
+            queueSendModel.TenantPriority = currCustomsRequestsSheet?.TenantPriority;
             queueService.Send(messageProperties, requestParams.Tenant, null, queueSendModel);
         }
     }
