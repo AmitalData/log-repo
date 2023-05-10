8772b385092df53dca90a814a6875069441c8d22
Shoshana Greenwald <shoshana@amital.co.il>
2023-05-10 14:10:58 +0300

    #182106

--- Logitude/Logitude.CargoTracking.BL/CoreBL/CargoTrackingShipmenPartnersCardsBuilder.cs
+++ Logitude/Logitude.CargoTracking.BL/CoreBL/CargoTrackingShipmenPartnersCardsBuilder.cs
@@ -296,7 +296,7 @@ namespace Logitude.CargoTracking.BL.EntityQueryServices
             address = city;
             if (string.IsNullOrEmpty(countryName))
                 return address;
-            return address + "," + countryName;
+            return address + ", " + countryName;
 
         }
 
