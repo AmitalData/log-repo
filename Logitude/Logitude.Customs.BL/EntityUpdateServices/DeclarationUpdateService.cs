8b9863db4f5c468c3b46483c9d9529f91eaf6719
elisheva Nachaysi <elisheva@amital.co.il>
2023-05-14 13:44:06 +0300

    #182046

--- Logitude/Logitude.Customs.BL/EntityUpdateServices/DeclarationUpdateService.cs
+++ Logitude/Logitude.Customs.BL/EntityUpdateServices/DeclarationUpdateService.cs
@@ -468,7 +468,7 @@ namespace Logitude.Customs.BL.EntityUpdateServices
                                 var nFromDoubleQuote = nSubStrined.IndexOf("\"");
                                 NewValue = nSubStrined.Substring(0, nFromDoubleQuote);
                             }
-                            if (OldValue != NewValue)
+                            if (OldValue != NewValue && !change.Contains("CreatedByUserId"))
                             {
                                 entityPM.IsChanged = true;
                                 break;
