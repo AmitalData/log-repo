d8e43bd3d6ec362252d7f119a3a7b7abe98134d4
izik89 <izikfr@amital.co.il>
2023-05-28 18:26:59 +0300

    #180072

--- Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_8237_ExportDeclerationAmendmentReplyResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_8237_ExportDeclerationAmendmentReplyResponseService.cs
@@ -58,13 +58,9 @@ namespace Logitude.CustomsMessaging.ResponseServices
                 declarationNumber = customResponse?.Response?.FunctionalReferenceID?.Value;
             }
 
-            IDisposable disposableToken = null;
-            try
+            string key = ProcessLockTableUtil.Instance.GetKey4Declaration(declarationNumber, requestParams.Tenant);
+            using (var disposableToken = ProcessLockTableUtil.Instance.GetProcessLockTableDisposable(requestParams.Tenant, true, key, "DeclarationNumber"))
             {
-
-                string key = ProcessLockTableUtil.Instance.GetKey4Declaration(declarationNumber, requestParams.Tenant);
-                using (disposableToken = ProcessLockTableUtil.Instance.GetProcessLockTableDisposable(requestParams.Tenant, true, key, "DeclarationNumber"))
-                {
 
                     var context = CustomContext.GetContext(requestParams.Tenant);
                     var myDeclarationQueryService = new DeclarationQueryService(context);
@@ -796,19 +792,6 @@ namespace Logitude.CustomsMessaging.ResponseServices
                                                                      //     requestParams.AppicationId = myDeclarationQueryService.GetIdByExternalDeclarationNumber(customResponse.Response.Declaration.DMExtensions.ExternalDeclarationID.Value, requestParams.Tenant);
                                                                      //}
                 }
-            }
-            catch (ProcessLockException processLockException)
-            {
-                LogMessagingUtil.Instance.AppendLine("processLockException wait a minute!! ,the worker Role is proccesing anther response of the same Declaration  ");
-                throw;
-            }
-            finally
-            {
-                if (disposableToken != null)
-                {
-                    disposableToken.Dispose();
-                }
-            }
         }
 
         public void SendManifest(DeclarationPM declarationPM, GenericRequestParams requestParams)  
