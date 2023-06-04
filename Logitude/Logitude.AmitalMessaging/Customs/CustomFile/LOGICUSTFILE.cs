3807cd36f5361d9419522453e1518689ec0c5cf4
Libby Hasid <libby@amital.co.il>
2023-06-04 12:39:47 +0300

    #182334

--- Logitude/Logitude.AmitalMessaging/Customs/CustomFile/LOGICUSTFILE.cs
+++ Logitude/Logitude.AmitalMessaging/Customs/CustomFile/LOGICUSTFILE.cs
@@ -195,7 +195,7 @@ public partial class LogitudeCustomsFile
 
     private string referentHAWBField;
 
-    private ExportInvoice[] invoices;
+    private ExportInvoice[] invoicesField;
 
     /// <remarks/>
     public string CustomFileNo
@@ -1131,15 +1131,18 @@ public partial class LogitudeCustomsFile
         }
     }
 
+
+    [System.Xml.Serialization.XmlElementAttribute("Invoice")]
+
     public ExportInvoice[] Invoices
     {
         get
         {
-            return this.invoices;
+            return this.invoicesField;
         }
         set
         {
-            this.invoices = value;
+            this.invoicesField = value;
         }
     }
 }
@@ -1149,16 +1152,16 @@ public class ExportInvoice
 {
     private  bool isEmpty;
   
-    private string number;
+    private string numberField;
 
-    private string date;
+    private string dateField;
 
     [XmlIgnore]
     public bool IsEmpty
     {
         get
         {
-            return number == null;
+            return numberField == null;
         }
     }
 
@@ -1168,22 +1171,22 @@ public class ExportInvoice
     {
         get
         {
-            return this.number;
+            return this.numberField;
         }
         set
         {
-            this.number = value;
+            this.numberField = value;
         }
     }
     public string Date
     {
         get
         {
-            return this.date;
+            return this.dateField;
         }
         set
         {
-            this.date = value;
+            this.dateField = value;
         }
     }
 }
