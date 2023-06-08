b70b5569032ca4253bb94b57cc732aac13cbb7c2
suhayb <suhayb@amitalvrd-0>
2023-06-08 12:50:04 +0000

    UpdateWhileStreaming() -> GetSingle();

--- Logitude/Logitude.Accounting.Data/Repositories/JournalRepository.cs
+++ Logitude/Logitude.Accounting.Data/Repositories/JournalRepository.cs
@@ -49,19 +49,14 @@ namespace Logitude.Accounting.Data.Repositories
 
         public void UpdateWhileStreaming(int tenant, string journalId,Action<Journal> updatePoco)
         {
-            var myList = LockByJournal_forUpdateNOWAIT(journalId, tenant);// lock it !!!!
-            if (!myList.Any())
+            var poco = GetSingle(journalId, tenant);
+            if (poco == null)
             {
                 var mess = ("JournalApproveService:Failed ... LockByJournal_forUpdateNOWAIT");
                 throw new Exception(mess);
             }
-            var poco = myList.First();
-            //if (poco.QueueId != null)
-            //{
-            //    throw new Exception("JournalApproveService:Failed(poco.QueueId != null) already Streamed !!!");
-            //}
+
             updatePoco(poco);
-            //poco.QueueId = QueueId;
 
             this.Update(poco);
         }
