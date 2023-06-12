b60ee6d9f006443de08a6dc946db7b917d9d3dd5
Libby Hasid <libby@amital.co.il>
2023-06-12 19:27:56 +0300

    #181392

--- Logitude/Logitude.Customs.Data/Repsitories/DeclarationRepository.cs
+++ Logitude/Logitude.Customs.Data/Repsitories/DeclarationRepository.cs
@@ -1381,6 +1381,15 @@ namespace Logitude.Customs.Data.Repsitories
             return decNum;
 
         }
+
+        public int GetTenantByDeclarationId(string id)
+        {
+            return   (from a in context.Declarations
+                                       where a.Id == id 
+                                       select a.Tenant).FirstOrDefault();
+
+         
+        }
     }
 
 
