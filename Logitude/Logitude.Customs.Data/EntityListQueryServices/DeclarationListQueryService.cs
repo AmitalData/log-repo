f0a91960838539e06fabcb83027bb15ab0dbe7d0
Sohaib Nemer <sohaib@amital.co.il>
2023-06-12 12:21:33 +0300

    fix

--- Logitude/Logitude.Customs.Data/EntityListQueryServices/DeclarationListQueryService.cs
+++ Logitude/Logitude.Customs.Data/EntityListQueryServices/DeclarationListQueryService.cs
@@ -433,10 +433,10 @@ namespace Logitude.Customs.Data.EntityListQueryServices
                                                          LoadingPortName = a.ExportLoadingPort.LocalName,
                                                          ExcludeManifest=a.ExcludeManifest,
 
-                                                         MehesFee = MyDeclarationTaxesByTaxTypeCodeViews.MehesFee,
+                                                         /*MehesFee = MyDeclarationTaxesByTaxTypeCodeViews.MehesFee,
                                                          VATReshimonFee = MyDeclarationTaxesByTaxTypeCodeViews.VATReshimonFee,
                                                          SecurityFee = MyDeclarationTaxesByTaxTypeCodeViews.SecurityFee,
-                                                         ComputerFee = MyDeclarationTaxesByTaxTypeCodeViews.SecurityFee,
+                                                         ComputerFee = MyDeclarationTaxesByTaxTypeCodeViews.SecurityFee,*/
 
                                                      });
 
