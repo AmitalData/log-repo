0b3cb17c0077fc552a6a0d86299c09ca212112f6
Frida Lok <frida@amital.co.il>
2023-06-27 20:48:47 +0300

    #185412

--- Logitude/Logitude.Customs.Data/Repsitories/DeclarationCargoSplitRepository.cs
+++ Logitude/Logitude.Customs.Data/Repsitories/DeclarationCargoSplitRepository.cs
@@ -51,13 +51,13 @@ namespace Logitude.Customs.Data.Repsitories
 
         public string GetIdByCargoIdentifiers(string cargoIdentifierKey1, string cargoIdentifierKey2, string cargoIdentifierKey3, int cargoIdentifierType, int tenant)
         {
-            if (String.IsNullOrWhiteSpace(cargoIdentifierKey1) || String.IsNullOrWhiteSpace(cargoIdentifierKey2) || String.IsNullOrWhiteSpace(cargoIdentifierKey3)) return "";
+            if (String.IsNullOrWhiteSpace(cargoIdentifierKey1) || String.IsNullOrWhiteSpace(cargoIdentifierKey2)) return "";
             string cargoTypeCode = null;
             if (cargoIdentifierType > 0) cargoTypeCode = cargoIdentifierType.ToString();
             return
                   (
                   from rec in context.DeclarationCargoSplits
-                  where rec.ManifestNumber == cargoIdentifierKey1 && rec.SecondCargoID == cargoIdentifierKey2 && rec.ThirdCargoID == cargoIdentifierKey3 && rec.CargoTypeCode == cargoTypeCode && rec.Tenant == tenant
+                  where rec.ManifestNumber == cargoIdentifierKey1 && rec.SecondCargoID == cargoIdentifierKey2 && (rec.ThirdCargoID == cargoIdentifierKey3 || string.IsNullOrEmpty(cargoIdentifierKey3)) && rec.CargoTypeCode == cargoTypeCode && rec.Tenant == tenant
                   select rec.Id
                   )
                   .FirstOrDefault();
