b515ffe0a880c4ea77b1c4ed98017f622e9a3c5b
izik89 <izikfr@amital.co.il>
2023-06-28 17:12:39 +0300

    #184029

--- Logitude/Logitude.CargoTracking.Data/CargoTrackingContext.cs
+++ Logitude/Logitude.CargoTracking.Data/CargoTrackingContext.cs
@@ -25,22 +25,6 @@ using System.Data.Entity.Infrastructure.Interception;
 
 namespace Logitude.CargoTracking.Data
 {
-    public class Interceptor : IDbCommandInterceptor
-    {
-        public void ReaderExecuting(DbCommand command, DbCommandInterceptionContext<DbDataReader> interceptionContext)
-        {
-            command.CommandText = "SET ARITHABORT ON; " + command.CommandText;
-        }
-
-        public void NonQueryExecuting(DbCommand command, DbCommandInterceptionContext<int> interceptionContext) { }
-        public void NonQueryExecuted(DbCommand command, DbCommandInterceptionContext<int> interceptionContext) { }
-        public void ReaderExecuted(DbCommand command, DbCommandInterceptionContext<DbDataReader> interceptionContext) { }
-        public void ScalarExecuting(DbCommand command, DbCommandInterceptionContext<object> interceptionContext) { }
-
-        public void ScalarExecuted(DbCommand command, DbCommandInterceptionContext<object> interceptionContext) { }
-    }
-
-
     public  partial  class CargoTrackingContext: DbContextBase, ICargoTrackingContext
     {
         public CargoTrackingContext()
@@ -52,7 +36,7 @@ namespace Logitude.CargoTracking.Data
         public CargoTrackingContext(DbConnection conn)
             : base(conn,true)
         {
-            DbInterception.Add(new Interceptor());
+            DbInterception.Add(new InterceptorArithabort());
             this.Configuration.LazyLoadingEnabled = false;
             this.Configuration.AutoDetectChangesEnabled = false;
             Database.SetInitializer<CargoTrackingContext>(null);
