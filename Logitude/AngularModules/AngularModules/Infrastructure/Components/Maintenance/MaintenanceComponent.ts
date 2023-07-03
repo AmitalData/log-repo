d58a3b0f5276d62956ba0c34fcb9889ed2c26a20
elisheva Nachaysi <elisheva@amital.co.il>
2023-07-03 16:36:55 +0300

    #184406

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/Maintenance/MaintenanceComponent.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/Maintenance/MaintenanceComponent.ts
@@ -1887,7 +1887,11 @@ export class MaintenanceComponent {
             } break;
             case "jokeracctest": {
 
-             
+                if (!SessionLocator.LoggedUserPM.IsCustomerCare) {
+                    var messageWindow = new MessageWindow();
+                    messageWindow.Show("nono NO only Customer Care ");
+                    return;
+                }
                 var logitudeWindow = new LogitudeWindow();
                 logitudeWindow.Width = 1600;
                 logitudeWindow.Height = 1200;
