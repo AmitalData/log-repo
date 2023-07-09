df908083dbf39f861460a40876d14cdabd7fbeb3
Frida Lok <frida@amital.co.il>
2023-07-09 08:06:17 +0300

    #185374

--- Logitude/AngularModules/AngularModules/Accounting/Components/Workspaces/Payable/PayablePageComponent.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/Workspaces/Payable/PayablePageComponent.ts
@@ -349,6 +349,9 @@ export class PayablePageComponent {
                 if (s) {
                     SessionLocator.DynamicLoader.Load('./Infrastructure/Components/EditComponent/EditComponent', this.CurrentSession.SessionLocation.viewContainerRef)
                         .then(cmpRef => {
+
+                            entity.PaymentTermId = SessionLocator.TenantPM.PaymentTermId;
+
                             cmpRef.instance.ComponentRef = cmpRef;
                             cmpRef.instance.Run({ EntityPM: comp.EntityPM, ObjectTableName: 'APInvoice' });
                         });
