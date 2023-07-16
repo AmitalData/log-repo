9cef9915dcf6448728c6e5d2466bb764eb679d14
Izik Fridland <IzikFr@amital.co.il>
2023-07-16 09:38:46 +0000

    Revert "Merged PR 31083: #179457 29291
    #179457 29291
    
    Related work items: #179457"

--- Logitude/Simplog.Data/ShipmentsModel/Mapping/ShipmentAdditionalCloudDataMap.cs
+++ Logitude/Simplog.Data/ShipmentsModel/Mapping/ShipmentAdditionalCloudDataMap.cs
@@ -78,6 +78,7 @@ namespace Simplog.Data.ShipmentsModel.Mapping
 
             this.Property(t => t.PaymentRequestDateTime).HasColumnName("PaymentRequestDateTime");
             this.Property(t => t.InvoiceIssuedDate).HasColumnName("InvoiceIssuedDate");
+            this.Property(t => t.UserAcceptSaveID).HasColumnName("UserAcceptSaveID");
             this.HasRequired(t => t.Shipment);
 
         }
