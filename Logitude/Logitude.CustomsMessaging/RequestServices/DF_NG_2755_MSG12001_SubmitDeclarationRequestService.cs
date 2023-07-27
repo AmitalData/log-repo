0047d128b76bd7d23cffd761d156ab3fd293cb50
elisheva Nachaysi <elisheva@amital.co.il>
2023-07-27 17:53:08 +0300

    #185500

--- Logitude/Logitude.CustomsMessaging/RequestServices/DF_NG_2755_MSG12001_SubmitDeclarationRequestService.cs
+++ Logitude/Logitude.CustomsMessaging/RequestServices/DF_NG_2755_MSG12001_SubmitDeclarationRequestService.cs
@@ -47,11 +47,11 @@ namespace Logitude.CustomsMessaging.RequestServices
             {
                 requestParams.RequestVIA = DefaultMessageController.Via(requestParams.Tenant, requestParams.MainInterfaceCode ?? "2755", requestParams.RequestVIA);
             }
-
-            var srverTime = (new DualQueryService(AmitalContext.GetContext(requestParams.Tenant))).GetServerDateTime();
+            //IMPORTANT - need change to cloud
+    //        var srverTime = (new DualQueryService(AmitalContext.GetContext(requestParams.Tenant))).GetServerDateTime();
             if (
-                declarationPaymentsPM.FuturePaymentDateTime > srverTime &&
-                declarationPaymentsPM.FuturePaymentDateTime.GetValueOrDefault().Subtract(srverTime.GetValueOrDefault()) > TimeSpan.FromMinutes(1)
+                declarationPaymentsPM.FuturePaymentDateTime > DateTime.Now &&
+                declarationPaymentsPM.FuturePaymentDateTime.GetValueOrDefault().Subtract(DateTime.Now) > TimeSpan.FromMinutes(1)
                 )
             {
 
