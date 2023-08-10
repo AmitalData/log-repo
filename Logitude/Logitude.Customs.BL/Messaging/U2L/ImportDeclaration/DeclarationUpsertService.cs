35b44edac2740345c9aba8d00954a73f89f4d7b1
Sohaib Nemer <sohaib@amital.co.il>
2023-08-10 12:52:41 +0300

    #185037

--- Logitude/Logitude.Customs.BL/Messaging/U2L/ImportDeclaration/DeclarationUpsertService.cs
+++ Logitude/Logitude.Customs.BL/Messaging/U2L/ImportDeclaration/DeclarationUpsertService.cs
@@ -903,7 +903,10 @@ namespace Logitude.Customs.BL.Messaging.U2L.ImportDeclaration
                     myDeclarationUpdateService.IsFromU2L = true;
                     myDeclarationUpdateService.TruckerId = truckerId;
                     myDeclarationUpdateService.DistributionArea = _AmitalCustomsFile.DistributionArea;
-                    myDeclarationUpdateService.ImporterCode = _AmitalCustomsFile.ImporterId;
+                    if (_AmitalCustomsFile.ImporterId != null && string.IsNullOrEmpty(this._MyDeclarationPM.ImporterCode))
+                    {
+                        myDeclarationUpdateService.ImporterCode = _AmitalCustomsFile.ImporterId;
+                    }
                     myDeclarationUpdateService.LastMileServiceType = _AmitalCustomsFile.LastMileServiceType;
                     myDeclarationUpdateService.MAWB = _AmitalCustomsFile.MAWB;
                     myDeclarationUpdateService.Update(_MyDeclarationPM, true);
