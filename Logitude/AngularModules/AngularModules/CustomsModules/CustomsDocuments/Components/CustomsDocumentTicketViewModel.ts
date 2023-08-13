3d47ba0bef58564186753dc6e5e82807cc8bb155
Frida Lok <frida@amital.co.il>
2023-08-13 17:29:23 +0300

    #183140

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDocuments/Components/CustomsDocumentTicketViewModel.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDocuments/Components/CustomsDocumentTicketViewModel.ts
@@ -637,21 +637,29 @@ export class CustomsDocumentTicketViewModel {
                 });
             }
             else {
-                
+                debugger
 
                 var ocrStatuses = ['1','3','7','8','9'];
 
                 if(ocrStatuses.includes(relatedDocumentViewModel.documentsFilingPM?.OcrStatusCode))
                 {
                     SessionLocator.SelectedSession.StopBusyIndicator();
-                    var status: string;
-                    switch(relatedDocumentViewModel.documentsFilingPM?.OcrStatusCode)
+                    var status: string = null;
+                    switch (relatedDocumentViewModel.documentsFilingPM?.OcrStatusCode) 
                     {
-                        case "1": 
-                        case "3": status = TextCodeTranslator.Translate("Customs.OcrDocument.O.InPrograss");
-                        case "7": status = TextCodeTranslator.Translate("Customs.OcrDocument.O.Cancelled");
-                        case "8": status = TextCodeTranslator.Translate("Customs.OcrDocument.O.Rejected");
-                        case "9": status =  TextCodeTranslator.Translate("Customs.General.O.Fail");
+                        case "1":
+                        case "3":
+                          status = TextCodeTranslator.Translate("Customs.OcrDocument.O.InPrograss");
+                          break;
+                        case "7":
+                          status = TextCodeTranslator.Translate("Customs.OcrDocument.O.Cancelled");
+                          break;
+                        case "8":
+                          status = TextCodeTranslator.Translate("Customs.OcrDocument.O.Rejected");
+                          break;
+                        case "9":
+                          status = TextCodeTranslator.Translate("Customs.General.O.Fail");
+                          break;
                     }
                     
 
