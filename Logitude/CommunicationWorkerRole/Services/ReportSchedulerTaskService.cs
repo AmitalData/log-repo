f87c4c980069225d2f1256fa7068ac55d6deeac2
Sohaib Nemer <sohaib@amital.co.il>
2023-08-15 12:20:52 +0300

    test

--- Logitude/CommunicationWorkerRole/Services/ReportSchedulerTaskService.cs
+++ Logitude/CommunicationWorkerRole/Services/ReportSchedulerTaskService.cs
@@ -54,6 +54,10 @@ namespace CommunicationWorkerRole.Services
             {"Export pdf report", null},
             {"Stored pdf report in Blob", null},
             {"Send email to reciepents", null},
+            {"test1", null},
+            {"test2", null},
+            {"test3", null},
+
         };
 
         TaskManagerBase currentTask;
