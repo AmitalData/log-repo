bbaa7c75e5206911e0041eca59694087bb48ca38
Libby Hasid <libby@amital.co.il>
2023-08-16 11:52:44 +0300

    #428632

--- Logitude/JustWebFreight/WebFreight.Web/App_Code/AuthenticationController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/App_Code/AuthenticationController.cs
@@ -1409,7 +1409,7 @@ namespace WebFreight.Web
                      customerCare = false;
                     bool distributor = false;
                     User logitudeUser = null;
-                    GlobalContact contact = globalContext.GlobalContacts.Where(d => d.GlobalTenantId == 0 && d.Email.ToLower() == parameters.Email.ToLower()).FirstOrDefault(); //mohammad
+                    GlobalContact contact = globalContext.GlobalContacts.Where(d => d.GlobalTenantId == 0 && d.Email.ToLower() == parameters.Email.ToLower()&& d.InActive==false).FirstOrDefault(); //mohammad
                     ICommonDataContext commonDataContext = CommonDataContext.GetContext(0);
                     if (contact != null)
                     {
