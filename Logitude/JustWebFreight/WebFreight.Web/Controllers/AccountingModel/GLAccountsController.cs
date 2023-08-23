a9d020a0e704684f72968501ab3b358aedaa5910
Ziad Kherallah <Ziad@amital.co.il>
2023-08-23 13:43:28 +0300

    #189110

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/AccountingModel/GLAccountsController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/AccountingModel/GLAccountsController.cs
@@ -303,23 +303,7 @@ namespace WebFreight.Web.App_Code.AngularJS_App_Code.Generated
         }
 
 
-        public HttpResponseMessage GetByGLAccountsDisplayNumber(string displayNumber, int tenant)
-        {
-            try
-            {
-                IAccountingContext MyContext = AccountingContext.GetContext(tenant);
-
-                GLAccountQueryService gLAccountQueryService = new GLAccountQueryService(MyContext);
-
-                GLAccountPM result = gLAccountQueryService.GetByGLAccountsDisplayNumber(displayNumber, tenant);
-
-                return Request.CreateResponse(HttpStatusCode.OK, result);
-            }
-            catch (Exception ex)
-            {
-                return Request.CreateResponse(HttpStatusCode.BadRequest, ApiExceptionBuilder.BuildException(ex));
-            }
-        }
+        
 
 
 
