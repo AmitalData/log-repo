b683559b73fa06840fed1300893f372160211511
izik89 <izikfr@amital.co.il>
2023-08-24 14:21:47 +0300

    #189378

--- Logitude/Logitude.Customs.BL/EntityQueryServices/CustomsRequestsSheetQueryService.cs
+++ Logitude/Logitude.Customs.BL/EntityQueryServices/CustomsRequestsSheetQueryService.cs
@@ -192,7 +192,8 @@ namespace Logitude.Customs.BL.EntityQueryServices
 "8235T", // תיקון שטעון
 "8314",//נתוני פרט מכס
 "8888",// תשובה לנתוני פרט מכס
-"2791"// אחסנה
+"2791",// אחסנה
+"8235", // תיקון הצהרה ברקע
             };
 
             string[] intrefaceTypeListDisplayOnly = GetintrefaceTypeListDisplayOnly();
@@ -354,6 +355,7 @@ namespace Logitude.Customs.BL.EntityQueryServices
 "2791",// אחסנה
 "2755E",// הגשה יצוא
 "8250",// סטטוס הצהרה
+"8235", // תיקון הצהרה ברקע
             };
 
             //var settingPm = CustomsSettingQueryService.GetSettingByTenant(requestInProgressParams.Tenant);
@@ -501,6 +503,7 @@ namespace Logitude.Customs.BL.EntityQueryServices
 "8314", // נתוני פרט מכס
 "8888",//תשובה לנתוני פרט מכס
 "2791",// אחסנה
+"8235", // תיקון הצהרה ברקע
 };
 
 
