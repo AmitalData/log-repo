db8795312e74708bb9c171e5eb1dac27026f567e
OritSH <OritSH@ORITSH-11PC>
2023-09-06 15:27:28 +0300

    #188460

--- Logitude/AngularModules/CypressTesting/FullAccounting/cypress/actions/APInvoiceActions.ts
+++ Logitude/AngularModules/CypressTesting/FullAccounting/cypress/actions/APInvoiceActions.ts
@@ -15,12 +15,14 @@ export function NavigatesAPInvoiceWizerd() {
 
 export function FillAPInvoiceDetails(aPInvoiceDetails: APInvoiceDetails) {
     var generatedInvoiceNumber = "AP" + gr.GenerateRandomNumber(10000, 99999);
-    cy.FillLogLov(APInvoiceSelectors.APInvoiceVendor, aPInvoiceDetails.Vendor, false);
+    cy.FillLogLov(APInvoiceSelectors.APInvoiceVendor, aPInvoiceDetails.Vendor, true);
     cy.FillLogTextBox(APInvoiceSelectors.APInvoiceInvoiceNumber, generatedInvoiceNumber)
     cy.wait(1000)
     cy.FillLogTextBox(APInvoiceSelectors.APInvoiceAmountInInvoice, aPInvoiceDetails.InvoiceAmount);
     cy.FillDate(APInvoiceSelectors.APInvoiceInvoiceDate, aPInvoiceDetails.InvoiceDate)
     cy.FillDate(APInvoiceSelectors.APInvoiceAccountingDate, aPInvoiceDetails.AccountingDate)
+    cy.FillLogLov(APInvoiceSelectors.APInvoicePaymentTerm,aPInvoiceDetails.PaymentTerm, true)
+    cy.FillLogLov(APInvoiceSelectors.APInvoiceBranch,aPInvoiceDetails.BranchId, true)
 }
 
 export function CreateAPInvoice() {
