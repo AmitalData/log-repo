caf438d2ad516b57cd62ebe12ab21722859826f9
Libby Hasid <libby@amital.co.il>
2023-09-07 12:18:57 +0300

    #187029

---
+++ Logitude/Simplog.Data/InvoiceModel/Mapping/ARInvoicesSignedStatusMap.cs
@@ -0,0 +1,40 @@
+﻿using Simplog.Data.InvoiceModel.EntityPOCOs;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplog.Data.InvoiceModel.Mapping
+{
+    public class ARInvoicesSignedStatusMap : EntityTypeConfiguration<ARInvoicesSignedStatus>
+    {
+        public ARInvoicesSignedStatusMap()
+        {
+            // Primary Key
+            this.HasKey(t => t.Code);
+
+            // Properties
+            this.Property(t => t.Code)
+                .IsRequired()
+                .HasMaxLength(1)
+                .IsUnicode(false);
+
+            this.Property(t => t.LocalName)
+                .IsRequired()
+                .HasMaxLength(30)
+                .IsUnicode(false);
+
+            this.Property(t => t.EnglishName)
+                .HasMaxLength(60)
+                .IsUnicode(true);
+
+            // Table & Column Mappings
+            this.ToTable("ARInvoicesSignedStatus");
+            this.Property(t => t.Code).HasColumnName("Code");
+            this.Property(t => t.LocalName).HasColumnName("LocalName");
+            this.Property(t => t.EnglishName).HasColumnName("EnglishName");
+        }
+    }
+}
