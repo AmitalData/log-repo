79b76ae3ecd6828829d7a14c9d5e876d1f550d9c
Elon Hezi <Elon@amital.co.il>
2023-09-10 12:25:13 +0300

    #189580

--- Logitude/Logitude.Customs.Data/EntityListQueryServices/InternalBorderSiteTypeListQueryService.cs
+++ Logitude/Logitude.Customs.Data/EntityListQueryServices/InternalBorderSiteTypeListQueryService.cs
@@ -30,7 +30,7 @@ namespace Logitude.Customs.Data.EntityListQueryServices
                                                           SearchFields = a.SearchFields,
                                                           Inactive = a.Inactive,
                                                           AutonomyRegionTypeCode = a.AutonomyRegionTypeCode,
-                                                          θιμ
+
                                                       });
             return query;
 		}
