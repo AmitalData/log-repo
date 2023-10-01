992ff92c588046b6bd125c0e3c426fdc95030c4b
Sohaib Nemer <sohaib@amital.co.il>
2023-10-01 13:52:01 +0300

    #189409

--- Logitude/AngularModules/AngularModules/Customs/Components/MenuButtons/DeclarationMenuButtonsHandler.ts
+++ Logitude/AngularModules/AngularModules/Customs/Components/MenuButtons/DeclarationMenuButtonsHandler.ts
@@ -1026,9 +1026,9 @@ export class DeclarationMenuButtonsHandler implements OnDestroy {
     ResetDeclarationNumberMethod() {
         this._DeclarationNumberandVersionId = null;//itzik:clear onstart on the house !!!
         if (this.EntityPM.Direction == "E") {
-            this.GetAnyRequestBeforeResetDeclaration("2755E", "לא ניתן לאפס מספר הצהרה ,קיימת בקשה מסוג הגשה")
+            this.GetAnyRequestBeforeResetDeclaration("2755E", "לא ניתן לאפס מספר הצהרה ,קיימת בקשה מסוג הגשה");
         } else {
-            this.GetAnyRequestBeforeResetDeclaration("2755", "לא ניתן לאפס מספר הצהרה ,קיימת בקשה מסוג הגשת תשלום "))
+            this.GetAnyRequestBeforeResetDeclaration("2755", "לא ניתן לאפס מספר הצהרה ,קיימת בקשה מסוג הגשת תשלום ");
         }
     }
     private GetAnyRequestBeforeResetDeclaration(interfaceTypeCode: string, message: string) {
@@ -1062,8 +1062,7 @@ export class DeclarationMenuButtonsHandler implements OnDestroy {
                     confirm.Show(TextCodeTranslator.Translate("Customs.Declaration.O.ResetDeclaration"));
                 }
 
-            }
-            );
+            });
     }
 
     private UpdateResetDeclarationNumber() {
