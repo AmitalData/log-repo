aabe529205937560e7cf1a8975d52558b6698a86
Sohaib Nemer <sohaib@amital.co.il>
2023-10-10 18:35:29 +0300

    #192498

--- Logitude/JustWebFreight/WebFreight.Web/ExternalAPIs/V1/CustomerController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/ExternalAPIs/V1/CustomerController.cs
@@ -210,7 +210,10 @@ namespace WebFreight.Web.ExternalAPIs.V1
                             var partner = computingPartnerQuery.GetSinglePMByCodeAndCheckTenantZero(entity.ComputingPartnerCode, authToken.Tenant);
                             entityPM.CreatedByPartner = (partner != null ? partner.Name : null);
                         }
-
+                        if(entity.GLAccount != null && !string.IsNullOrEmpty(entity.GLAccount.InternalNumber))
+                        {
+                            entityPM.Code = entity.GLAccount.InternalNumber;
+                        }
                         CustomerService service = new CustomerService(MyContext, entityPM);
                         service.Create();
                         service.Submit();
