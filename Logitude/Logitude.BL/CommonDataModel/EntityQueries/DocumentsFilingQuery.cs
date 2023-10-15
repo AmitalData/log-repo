760c8996622d35cc49f9ca7fd4495543ce135f23
elisheva Nachaysi <elisheva@amital.co.il>
2023-10-15 11:15:36 +0300

    fix ocr

--- Logitude/Logitude.BL/CommonDataModel/EntityQueries/DocumentsFilingQuery.cs
+++ Logitude/Logitude.BL/CommonDataModel/EntityQueries/DocumentsFilingQuery.cs
@@ -3461,7 +3461,7 @@ namespace Logitude.BL.CommonDataModel.EntityQueries
                     }
                 }
 
-                CustomsDocumentPM customsDoc = customsDocumentQueryService.GetSingle(extDocPm.Id, false, false, tenant);
+                CustomsDocumentPM customsDoc = customsDocumentQueryService.GetSingleByDocFileId(extDocPm.Id,tenant);
                 if (customsDoc != null)
                 {
                     extDocPm.CustomsDocumentTypeName = customsDoc.DocumentTypeName;
@@ -3469,6 +3469,11 @@ namespace Logitude.BL.CommonDataModel.EntityQueries
                     extDocPm.IsMetaDataReady = customsDoc.IsMetaDataReady;
                     extDocPm.CustomsDocumentStatusCode = customsDoc.DocumentStatusCode;
                     extDocPm.ExternalAttachmentId = customsDoc.ExternalAttachmentId;
+                    extDocPm.OcrStatusCode = customsDoc.OcrStatusCode;
+                    extDocPm.OcrScore = customsDoc.OcrScore;
+                    extDocPm.OcrReference = customsDoc.OcrReference;
+                    extDocPm.OcrNotConnect = customsDoc.OcrNotConnect;
+
                 }
 
                 extDocPm.DocumentsFilingMetaDataValues = documentsFilingMetaDataValuesList;//.Where(d => d.DocumentsFilingId == extDocPm.Id && d.Tenant == extDocPm.Tenant).ToList();
