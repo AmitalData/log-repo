f0ff2a28ba893afbe0e95c79ffacd91eb16eaeb6
Sohaib Nemer <sohaib@amital.co.il>
2023-10-24 11:18:01 +0300

    #193220

--- Logitude/AngularModules/AngularModules/Accounting/Components/NewEntity/NewGLAccountComponent.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/NewEntity/NewGLAccountComponent.ts
@@ -135,21 +135,23 @@ export class NewGLAccountComponent extends BaseComponent {
     set IsMultiCurrency(value: boolean) {
         if (value == true) {
             this.EntityPM.IsMultiCurrency = value;
-            this.ReconcileMethodCode = '0';
             this.CurrencyId = null;
-
             this.UIProperties.SetEnabled("CurrencyId", this.ObjectTableName, false);
             this.UIProperties.SetValidity("CurrencyId", this.ObjectTableName, true, "");
             this.UIProperties.SetRequired("CurrencyId", this.ObjectTableName, false);
-            //this.UIProperties.SetEnabled("ReconcileMethodCode", this.ObjectTableName, false);
-
+            if(!this.isEnableMultiCurrencyWithReconcileMethodCodeEqualOne){
+                this.ReconcileMethodCode = '0';
+                //this.UIProperties.SetEnabled("ReconcileMethodCode", this.ObjectTableName, false);
+            }
             this.CD.detectChanges();
 
         } else if (value == false) {
             this.EntityPM.IsMultiCurrency = value;
             this.UIProperties.SetEnabled("CurrencyId", this.ObjectTableName, true);
             this.UIProperties.SetRequired("CurrencyId", this.ObjectTableName, true);
-            this.ReconcileMethodCode = null;
+            if(!this.isEnableMultiCurrencyWithReconcileMethodCodeEqualOne){
+                this.ReconcileMethodCode = null;
+            }
             this.CD.detectChanges();
         }
     }
