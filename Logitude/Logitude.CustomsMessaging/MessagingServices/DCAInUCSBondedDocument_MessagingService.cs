bed128d96280029cbacda6c1a8574e3bf5e4b156
Shoshana Greenwald <shoshana@amital.co.il>
2023-10-29 16:22:55 +0200

    #fix

--- Logitude/Logitude.CustomsMessaging/MessagingServices/DCAInUCSBondedDocument_MessagingService.cs
+++ Logitude/Logitude.CustomsMessaging/MessagingServices/DCAInUCSBondedDocument_MessagingService.cs
@@ -223,16 +223,7 @@ namespace Logitude.CustomsMessaging.MessagingServices
             using (var trans = TransactionFactory.GetNewTransaction())
             {
                 try
-                {
-					DateTime stopLogAt = DateTime.MinValue;//DateTime stopLogAt = new DateTime(2020, 09, 01);
-					string UntilDateyyyyMMdd = ConfigurationManager.AppSettings["20220227T155633.LogUntilDateyyyyMMdd"];
-					if (!string.IsNullOrWhiteSpace(UntilDateyyyyMMdd))
-					{
-						stopLogAt = DateTime.ParseExact(UntilDateyyyyMMdd,
-															"yyyyMMdd",
-															CultureInfo.InvariantCulture,
-															DateTimeStyles.None);
-					}
+                {					
 
 					_Stopwatch1 = Stopwatch.StartNew();
 
