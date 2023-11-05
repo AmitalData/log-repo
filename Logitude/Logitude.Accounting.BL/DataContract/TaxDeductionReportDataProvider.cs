ea87bd12294d3a1b4d7be1a97d3e17a05647840a
Libby Hasid <libby@amital.co.il>
2023-11-05 17:37:37 +0200

    #193443

--- Logitude/Logitude.Accounting.BL/DataContract/TaxDeductionReportDataProvider.cs
+++ Logitude/Logitude.Accounting.BL/DataContract/TaxDeductionReportDataProvider.cs
@@ -136,7 +136,7 @@ namespace Logitude.Accounting.BL.DataContract
                 taxDeductionReportline.VendorId = payment.VendorCard!= null? payment.VendorCard.GLAccountId: null;
                 if (taxDeductionReportline.VendorId == null)
                 {
-                    throw new ApplicationException("הוראת תשלום מספר :"+payment.PaymentNo+"\n"+ " VendorCard.GLAccountId = null ");
+                    throw new ApplicationException("הוראת תשלום :" + payment.PaymentNo+"\n"+ "הכרטיס התפעולי לא מחובר לכרטיס ההנח\"ש");
 
                 }
                 taxDeductionReportline.MonthOfRegisterDate = cancelled? payment.AccountingCancelationDate.Value.Month : payment.RegisterDate.Value.Month;
