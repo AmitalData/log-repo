847814c478cf5a71040c28ae62b96b2f4ab4ea57
OritSH <OritSH@ORITSH-11PC>
2023-11-16 09:37:47 +0200

    #194162

---
+++ Logitude/AngularModules/CypressTesting/FullAccounting/cypress/selectors/TrailBalanceReportSelectors.ts
@@ -0,0 +1,10 @@
+export class TrailBalanceReportSelectors {
+
+    public static readonly RequestSheet = '#GeneralMHReports';
+    public static readonly Reportbtn ='#ViewTRBRReportbtn'
+    public static readonly FromDate = '#FromDate';
+    public static readonly ToDate = '#ToDate';
+    public static readonly GreenButton = '.greenButton';
+    public static readonly TrailBalanceReporScreen = '.container';
+
+}
