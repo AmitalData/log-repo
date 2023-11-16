21044aad4849d29e14aa7748560dd13eb922d724
Elon Hezi <Elon@amital.co.il>
2023-11-16 17:52:48 +0200

    #194212

--- Logitude/AngularModules/AngularModules/Accounting/Components/EditTabs/GLAccount/GLAccountGeneralTabComponent.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/EditTabs/GLAccount/GLAccountGeneralTabComponent.ts
@@ -77,8 +77,8 @@ export class GLAccountGeneralTabComponent extends BaseComponent {
         //#endregion
 
 
+        debugger
         
-
         this.SetupFiels();
         this.SetUIProperties();
 
