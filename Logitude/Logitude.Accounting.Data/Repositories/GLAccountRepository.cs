5c22a0a405ec78032c8497e49a4f60ff5991be31
Owais Zaid <owais@logitudeworld.com>
2023-11-21 12:09:16 +0200

    When creating a ARpayment/ARinvoice if the card connected to Glaccount that is blocked > do not display it in the DDL  list

--- Logitude/Logitude.Accounting.Data/Repositories/GLAccountRepository.cs
+++ Logitude/Logitude.Accounting.Data/Repositories/GLAccountRepository.cs
@@ -1300,7 +1300,7 @@ namespace Logitude.Accounting.Data.Repositories
         public List<GLAccount> GetAllInActivityAccountsByTenant(int tenant)
         {
             List<GLAccount> accounts = ((from a in context.GLAccounts
-                                         where a.ChartOfAccountsTypeCode == "3" && a.Inactive == true && (tenant == 0 || a.Tenant == tenant)
+                                         where a.Inactive == true && a.Tenant == tenant
                                          select a).ToList());
 
             return accounts;
