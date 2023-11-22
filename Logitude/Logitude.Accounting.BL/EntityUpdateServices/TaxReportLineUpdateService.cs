caca29466a68baa79eb2f06f6493b2ae232ee84e
Sohaib Nemer <sohaib@amital.co.il>
2023-11-22 15:01:50 +0200

    #194947

--- Logitude/Logitude.Accounting.BL/EntityUpdateServices/TaxReportLineUpdateService.cs
+++ Logitude/Logitude.Accounting.BL/EntityUpdateServices/TaxReportLineUpdateService.cs
@@ -383,13 +383,16 @@ namespace Logitude.Accounting.BL.EntityUpdateServices
                 CheckVATAmountInTheRecordIsHigherThanThePercentageOfVATAllowed(entityPM);
 
             }
-            string[] validStatuses = { "C", "H", "K", "P", "R", "T" };
-
-            var _FullAccountingSetting = FullAccountingSettingQueryService.Get(entityPM.Tenant);
-
-            if (validStatuses.Contains(entityPM.LineTypeCode) && entityPM.TotalInvoiceAmount > _FullAccountingSetting.AmountForConfirmationNumber && string.IsNullOrEmpty(entityPM.ConfirmationNumber))
+            
+            if (entityPM.StatusCode != "2" && entityPM.StatusCode != "1")
             {
-                entityPM.StatusCode = "11";
+                string[] validStatuses = { "C", "H", "K", "P", "R", "T" };
+
+                var _FullAccountingSetting = FullAccountingSettingQueryService.Get(entityPM.Tenant);
+                if (validStatuses.Contains(entityPM.LineTypeCode) && entityPM.TotalInvoiceAmount > _FullAccountingSetting.AmountForConfirmationNumber && string.IsNullOrEmpty(entityPM.ConfirmationNumber))
+                {
+                    entityPM.StatusCode = "11";
+                }
             }
 
         }
