ecd5bc452034dd00350acf159f01c02994a0813f
Ayed Ramadeen <ayed@logitudeworld.com>
2023-11-23 14:39:41 +0200

    Product Backlog Item 194753: [Cloud] When trying to send report by email and click on [To] button tenant=47 the screen stuck.

--- Logitude/JustWebFreight/WebFreight.Web/App_Code/AngularJS_App_Code/DocumentOutExtendedController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/App_Code/AngularJS_App_Code/DocumentOutExtendedController.cs
@@ -621,7 +621,7 @@ namespace WebFreight.Web.App_Code.AngularJS_App_Code
                     #endregion
                 }
 
-                else if (objectTableName == "Report")
+                else if (objectTableName == "Report" && !string.IsNullOrWhiteSpace(gLAccountId))
                 {
                     #region Report
 
