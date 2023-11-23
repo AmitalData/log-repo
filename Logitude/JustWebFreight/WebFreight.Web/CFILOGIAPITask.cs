8506ee3f78788b6847d550a22f43cb6cd132d1fa
Moti Marvi <moti@amital.co.il>
2023-11-23 08:31:25 +0200

    #194764

--- Logitude/JustWebFreight/WebFreight.Web/CFILOGIAPITask.cs
+++ Logitude/JustWebFreight/WebFreight.Web/CFILOGIAPITask.cs
@@ -66,7 +66,7 @@ namespace WebFreight.Web
 <DAT name=""REFERENCE"">CENFMAIN.Lp_UpdateCars</DAT>
 <DAT name=""PARAMETERS"">DeclarationId=True&uSEP;VehicleTypeCode=True&uSEP;VehicleChassisNumber=True</DAT>
 <DAT name=""EXAMPLE_SQL"" xml:space='preserve'>select InvoiceCounterKey, InvoiceItemLineNumber from SupplierInvoiceItemVehicles where Tenant='P1' and DeclarationId='P2' and VehicleTypeCode='P3' and VehicleChassisNumber='P4'</DAT>
-<DAT name=""TEMPLATE_SQL"" xml:space='preserve'>select InvoiceCounterKey, InvoiceItemLineNumber from SupplierInvoiceItemVehicles where Tenant=@Tenant' and DeclarationId=@DeclarationId and VehicleTypeCode=@VehicleTypeCode and VehicleChassisNumber=@VehicleChassisNumber</DAT>
+<DAT name=""TEMPLATE_SQL"" xml:space='preserve'>select InvoiceCounterKey, InvoiceItemLineNumber from SupplierInvoiceItemVehicles where Tenant=@Tenant and DeclarationId=@DeclarationId and VehicleTypeCode=@VehicleTypeCode and VehicleChassisNumber=@VehicleChassisNumber</DAT>
 <DAT name=""EXAMPLE_RESULT"" xml:space='preserve'></DAT>
 <DAT name=""LINQ"">true</DAT><DAT name=""HAS_TENANT"">true</DAT></OCC>
 <OCC>
@@ -74,8 +74,8 @@ namespace WebFreight.Web
 <DAT name=""NAME_ENG"" xml:space='preserve'>select ClassificationCode, TradeAgreemenCode, OriginCountryCode, InvoiceQuantityType</DAT>
 <DAT name=""REFERENCE"">CENFMAIN.Lp_UpdateCars</DAT>
 <DAT name=""PARAMETERS"">ClassificationCode=True&uSEP;TradeAgreemenCode=True&uSEP;OriginCountryCode=True&uSEP;InvoiceQuantityType=True</DAT>
-<DAT name=""EXAMPLE_SQL"" xml:space='preserve'>select ClassificationCode, TradeAgreemenCode, OriginCountryCode, InvoiceQuantityType from SupplierInvoiceItems where Tenant='P1' and  DeclarationId='P2' and InvoiceCounterKey='P3' and LineNumber='P4'</DAT>
-<DAT name=""TEMPLATE_SQL"" xml:space='preserve'>select ClassificationCode, TradeAgreemenCode, OriginCountryCode, InvoiceQuantityType from SupplierInvoiceItems where Tenant=@Tenant and  DeclarationId=@DeclarationId and InvoiceCounterKey=@InvoiceCounterKey and LineNumber=@LineNumber</DAT>
+<DAT name=""EXAMPLE_SQL"" xml:space='preserve'>select ClassificationCode, TradeAgreemenCode, OriginCountryCode, InvoiceQuantityType from SupplierInvoiceItems where Tenant='P1' and  DeclarationId='P2' and CounterKey='P3' and LineNumber='P4'</DAT>
+<DAT name=""TEMPLATE_SQL"" xml:space='preserve'>select ClassificationCode, TradeAgreemenCode, OriginCountryCode, InvoiceQuantityType from SupplierInvoiceItems where Tenant=@Tenant and  DeclarationId=@DeclarationId and CounterKey=@CounterKey and LineNumber=@LineNumber</DAT>
 <DAT name=""EXAMPLE_RESULT"" xml:space='preserve'></DAT>
 <DAT name=""LINQ"">true</DAT><DAT name=""HAS_TENANT"">true</DAT></OCC>
 <OCC>
