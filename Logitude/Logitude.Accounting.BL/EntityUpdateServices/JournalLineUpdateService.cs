29ea65e238f5de5d42c2d64fe866639dd6678b65
Elon Hezi <Elon@amital.co.il>
2023-11-23 16:05:03 +0200

    #194212

--- Logitude/Logitude.Accounting.BL/EntityUpdateServices/JournalLineUpdateService.cs
+++ Logitude/Logitude.Accounting.BL/EntityUpdateServices/JournalLineUpdateService.cs
@@ -43,9 +43,7 @@ namespace Logitude.Accounting.BL.EntityUpdateServices
                 throw new ApplicationException("JournalLineUpdateService must be used only from JournalUpdateService(force check Approved Journal Can Only Change To Voided)");
             }
 
-            // check payment terms and add days to due date if needed.
-            ProcessGLAccountPaymentTerms(entityPM, entityParentPM);
-
+            
             base.OnCreating(entityPM, entityParentPM);
         }
 
@@ -65,54 +63,5 @@ namespace Logitude.Accounting.BL.EntityUpdateServices
             base.OnUpdating(entityPM);
         }
 
-        private void ProcessGLAccountPaymentTerms(JournalLinePM entityPM,JournalPM entityParentPM)
-        {
-            
-            JournalPM journal = entityParentPM;
-
-
-            GLAccountQueryService glAccountQueryService = new GLAccountQueryService((MainContext as IAccountingContext));
-            GLAccountPM glAccount = glAccountQueryService.GetGlaAccountByJouranlIdAndJournalLineNumber(entityPM.Tenant, entityPM.JournalId, entityPM.Line);
-
-            // 3. כאשר נוצרת תנועה בזכות מתוך שורת פקודת יומן בזכות  לכרטיס ספק
-            // (שהמקור  שלה הוא פקודת יומן חיצונית(ממערכת יוניפרייט
-            // Journals.ExternalSystem=UNIFREIGHT   >  מקור שלה ביוניפרייט
-            // Journals.AccountingEntityCode = 1 > מסוג פקודת יומן
-            // Journallines.actioncode=1  והתנועה היא מתוך שורת פקודת יומן בזכות
-
-            if (journal.ExternalSystem == "UNIFREIGHT" && journal.AccountingEntityCode == "1" && entityPM.ActionCode == "1")
-            {
-                UpdateDueDate(glAccount, entityPM);
-            }
-
-
-            //4. כאשר נוצרת שורה בזכות מתוך שורת פקודת יומן בזכות  לכרטיס ספק
-            // glaccounts.ChartOfAccountsTypeCode = 4
-            // שהמקור שלה הוא חשבונית ספק
-            // Journals.AccountingEntityCode = 4 > מסוג חשבונית ספק
-            // Journallines.actioncode = 1 > והתנועה היא מתוך שורת פקודת יומן בזכות
-
-
-            if (glAccount != null && glAccount.ChartOfAccountsTypeCode == "4" && journal.AccountingEntityCode == "4" && entityPM.ActionCode == "1" )
-            {
-                UpdateDueDate(glAccount, entityPM);
-            }
-        }
-
-
-        private void UpdateDueDate(GLAccountPM glAccount, JournalLinePM entityPM)
-        {
-            if (glAccount.PaymentTerms != null)
-            {
-                PaymentTermRepository paymentTermQueryService = new PaymentTermRepository((MainContext as ICommonDataContext));
-                int paymentTermDays = paymentTermQueryService.GetSinglePaymentTerm(glAccount.PaymentTerms).Days;
-                if(paymentTermDays != 0)
-                {
-                    LedgerTransactionQueryService ledgerTransactionQueryService = new LedgerTransactionQueryService((MainContext as IAccountingContext));
-                    LedgerTransactionPM ledgerTransaction = ledgerTransactionQueryService.GetLedgerTransactionPMByJournalId(entityPM.JournalId, entityPM.Tenant);
-                    entityPM.DueDate = ledgerTransaction.DocumentDate.AddDays(paymentTermDays);
-                }
-            }
-        }
     }
 }
