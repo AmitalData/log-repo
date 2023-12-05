939a2f1924700ab1cce6a1b492861dea21b4fe7f
Libby Hasid <libby@amital.co.il>
2023-12-05 13:37:47 +0200

    #195879

--- Logitude/AngularModules/AngularModules/InvoiceModules/APPayment/Components/EditTabs/APPaymentDetailsTabComponent.ts
+++ Logitude/AngularModules/AngularModules/InvoiceModules/APPayment/Components/EditTabs/APPaymentDetailsTabComponent.ts
@@ -255,26 +255,25 @@ export class APPaymentDetailsTabComponent extends BaseComponent implements OnIni
         if (this.IsFullAccounting)
         {
            
-
+debugger
                 this.GLAccountWithholdingService.GetDeductionPercentage(this.VendorId, this.EntityPM.RegisterDate).subscribe((myResult:any) => {
                     var myResponse: ServiceResponse = myResult;
                     if (!myResponse.HasError) {
                         if (AppTool.IsNullOrEmpty(this.EntityPM.Id) || this.IsTaxUpdated) {
                             this.IsNoVendorTax = myResponse.Result.IsDefault;
-                            this.TaxDeductionPercentage = myResponse.Result.Percentage;
-                        }
-                        
-                        else {
                             const nonIsraeliVendor = this.vendor ? this.vendor.CountryCode != "IL" : false;
 
-                            if(nonIsraeliVendor){
-                                this.TaxDeductionPercentage = 0;
-
+                            if(!myResponse.Result.IsDefault || !nonIsraeliVendor){
+                               this.TaxDeductionPercentage = myResponse.Result.Percentage;
                             }
                             else{
-                                this.IsNoVendorTax = myResponse.Result.IsDefault;
+                                 this.TaxDeductionPercentage = 0;
+                                }
+                        }
+                        else {
+                             this.IsNoVendorTax = myResponse.Result.IsDefault;
 
-                            }
+                           
                         }
                     }
                     else {
@@ -1297,11 +1296,12 @@ export class APPaymentDetailsTabComponent extends BaseComponent implements OnIni
         return this.EntityPM.TaxDeductionPercentage;
     }
     set TaxDeductionPercentage(value: number) {
+        debugger
         if (this.EntityPM != null) {
             if (this.EntityPM.TaxDeductionPercentage != value) {
 
             const nonIsraeliVendor = this.vendor ? this.vendor.CountryCode != "IL" : false;
-            if(nonIsraeliVendor)
+            if(nonIsraeliVendor && this.IsNoVendorTax)
                 value = 0;
             this.EntityPM.TaxDeductionPercentage = value;
             this.CalculateTaxDeductionLocalAmount();
