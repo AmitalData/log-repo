a922e0783d6f77e02dd8f7e20af5ef93ae875e64
Liron Shaked Hodorov <LironSH@amital.co.il>
2023-12-06 11:54:02 +0200

    #195216

--- Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/GeneratedUpdate/EntityUpdateClasses/ExternalReconciliationUpdateClass.cs
+++ Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/GeneratedUpdate/EntityUpdateClasses/ExternalReconciliationUpdateClass.cs
@@ -85,7 +85,7 @@ namespace WebFreight.Web.MetaDataUpdate.GeneratedUpdate.EntityUpdateClasses
 {
    public class ExternalReconciliationUpdateClass
    {  		
-		public const string HashString = "16878f011551c8c70152fdbf3a928d75";
+		public const string HashString = "7f9c845c123f9314d5292120f8a0c6be";
 	    public void AddObjectTable(Dictionary<string, ObjectTable> objectTables, Dictionary<string, TextCode> textCodes,ObjectTableRepository ObjectTableRepository,TextCodeRepository TextCodeRepository)
         {                     
             
@@ -132,7 +132,7 @@ namespace WebFreight.Web.MetaDataUpdate.GeneratedUpdate.EntityUpdateClasses
 			      				    MaxNumberOfCustomFields =  0,
 			      				    LocalDefaultText =  "התאמה",
 			      				    DefaultText =  "External Reconciliation",
-			      				    Code =  "b172",
+			      				    Code =  "8f4b",
 			      				    Name =  "ExternalReconciliation Query Group",
 			      				    GenerateDomainService =  false,
 			      				    ClientModuleName =  "Accounting",
@@ -1400,6 +1400,10 @@ namespace WebFreight.Web.MetaDataUpdate.GeneratedUpdate.EntityUpdateClasses
  		   TextCode ExternalReconciliationTextCode_ExternalReconciliationOAccoountFilterlabel = AddTextCodes.AddTextCode(new TextCodeDetails() { Code = "ExternalReconciliation.O.AccoountFilter.label", DefaultText = "Display transactions from",LocalDefaultText = @"הצג תנועות מ", ObjectTableId = ExternalReconciliationObjectTable.Id, Tenant = 0, TextCodeTypeCode = "O", IsSpellChecked = false }, TextCodeRepository, TextCodes);
 
  		   TextCode ExternalReconciliationTextCode_ExternalReconciliationOAccoountFilterTransferTransaction = AddTextCodes.AddTextCode(new TextCodeDetails() { Code = "ExternalReconciliation.O.AccoountFilter.TransferTransaction", DefaultText = "Transfer Transaction",LocalDefaultText = @"תנועה מחשבון דחויים", ObjectTableId = ExternalReconciliationObjectTable.Id, Tenant = 0, TextCodeTypeCode = "O", IsSpellChecked = false }, TextCodeRepository, TextCodes);
+
+ 		   TextCode ExternalReconciliationTextCode_ExternalReconciliationOSelectOnePage = AddTextCodes.AddTextCode(new TextCodeDetails() { Code = "ExternalReconciliation.O.SelectOnePage", DefaultText = "to adjust bank fees with Transaction select only one page line",LocalDefaultText = @"לא ניתן לפרוע מספר קבלות בו זמנית , יש לבצע פרעון של כל שורת קבלה בנפרד.", ObjectTableId = ExternalReconciliationObjectTable.Id, Tenant = 0, TextCodeTypeCode = "O", IsSpellChecked = false }, TextCodeRepository, TextCodes);
+
+ 		   TextCode ExternalReconciliationTextCode_ExternalReconciliationOSelectOneOrMore = AddTextCodes.AddTextCode(new TextCodeDetails() { Code = "ExternalReconciliation.O.SelectOneOrMore", DefaultText = "to adjust bank fees, select one or more row External page line",LocalDefaultText = @"כדי ליצור פקודה בגין עמלות יש לבחור בשורת בנק אחת בכל פעם", ObjectTableId = ExternalReconciliationObjectTable.Id, Tenant = 0, TextCodeTypeCode = "O", IsSpellChecked = false }, TextCodeRepository, TextCodes);
 
    
 	    
