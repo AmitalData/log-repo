412cbedc0df57cfd0c48a418d9a27824edfbe869
Sohaib Nemer <sohaib@amital.co.il>
2023-12-10 15:17:19 +0200

    #187990

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsCourier/Components/CourierPendingReason/DeclarationPendingsGeneralComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsCourier/Components/CourierPendingReason/DeclarationPendingsGeneralComponent.ts
@@ -166,6 +166,7 @@ export class DeclarationPendingsGeneralComponent extends BaseComponent {
             item.DeclarationID = this.DeclarationCourierStatus.DeclarationId;
             item.Tenant = this.DeclarationCourierStatus.Tenant;
             item.IsDirty = true;
+            item.WasApproved=false;
             item.Status = "A";
             this.DeclarationCourierStatus.AddDeclarationPending(item);
             //if (!this.DeclarationPendingsList.includes(item)) {
