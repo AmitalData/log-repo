ae70ba0566157c6586ea34884dee4d5d41dcb4ee
Sohaib Nemer <sohaib@amital.co.il>
2023-12-17 13:47:00 +0200

    fix

--- Logitude/CustomsWorkerRole/SendWEBAPIMessage2MamanWR.cs
+++ Logitude/CustomsWorkerRole/SendWEBAPIMessage2MamanWR.cs
@@ -561,7 +561,7 @@ Insert into BATCHSERVICESDEFINITIONMODS (CODE,INACTIVE,NUMBEROFTHREADS) values (
 
                         LogMessagingUtil.Instance.AppendLine($"PostAsync({_CourierHawbMamanCommunicationLogSettings.URIToken}, {content})");
                         var authoTask = client.PostAsync(_CourierHawbMamanCommunicationLogSettings.URIToken, content);
-                        Wait4Finsh(authoTask, TimeOutInMin: 3);
+                        Wait4Finsh(authoTask, 3);
                         myResultString = authoTask.Result.Content.ReadAsStringAsync().Result;
                         //{"access_token":"zkKDt-XnqqM5uoyDwrPxDPHb_vM5hplsUKr7sT5GA2w8Vpsl_HT5eBidUriiyw3Gn-Mne0NIq2LQO7MMT525GdrFutDzIQpRKR6c7oz2GbdSdGdEY3S3nfP0W7svtmShEeUx23SbW8ysLkyAnFP-IdQhvMs2lzxzHIDrnDqm_agwq54x9UiiDa5-9ZkEWBUrN83U4B5qddiYTU0whODGvrxEE9wyrQKoygG3Gi48gwv2_TI4H9yrd2Uys9l_jBivOsRRm1oXtyGsyIq9DwDn7pmcoxUjz-yNwm_hp18Y1qi4aXk1Z8IjeKRQl_8FMUg-","token_type":"bearer","expires_in":35999,"UserName":"F_unitedf","role":"General",".issued":"Mon, 12 Nov 2018 14:48:43 GMT",".expires":"Tue, 13 Nov 2018 00:48:43 GMT"}
                         LogMessagingUtil.Instance.AppendLine($"PostAsyncResult ({myResultString})");
@@ -605,7 +605,7 @@ Insert into BATCHSERVICESDEFINITIONMODS (CODE,INACTIVE,NUMBEROFTHREADS) values (
                         client.DefaultRequestHeaders.Add("Authorization", $"{token_type} {access_token}");
                         LogMessagingUtil.Instance.AppendLine($"URIBaldarCreateECTHRMessgae.PostAsync....");
                         var task = client.PostAsync(_CourierHawbMamanCommunicationLogSettings.URIMethod, content);
-                        Wait4Finsh(task, TimeOutInMin: 3);
+                        Wait4Finsh(task, 3);
                         myResultString = task.Result.Content.ReadAsStringAsync().Result;
                         LogMessagingUtil.Instance.AppendLine($"PostAsyncResult={myResultString}");
                     }
