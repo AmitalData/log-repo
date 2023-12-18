e7561f88881f4cc920dfd1945355b96d1f55881f
Liron Shaked Hodorov <LironSH@amital.co.il>
2023-12-18 13:06:29 +0200

    #196569

--- Logitude/Logitude.CargoTracking.BL/CargoTrackingServices/Services/MainService/CargoTrackingMainService.cs
+++ Logitude/Logitude.CargoTracking.BL/CargoTrackingServices/Services/MainService/CargoTrackingMainService.cs
@@ -933,12 +933,13 @@ namespace Logitude.CargoTracking.BL.CargoTrackingServices.Services
         {
             var cargoTrackingShipmentsService = new CargoTrackingShipmentsService();
             updateCargoTrackingRecords.Milestones = cargoTrackingShipmentsService.GetMilestones(updateCargoTrackingRecords.CargoTrackingUpdateDataBaseArgs.BuildCargoArgs.DestinationConnectionString);
+            //check tenant
             if (updateCargoTrackingRecords.CargoTrackingUpdateDataBaseArgs.CargoTrackingArguments.Tenant == 0)
             {
                 updateCargoTrackingRecords.AllTenantIds = ServiceHelper.GetAllTenants(updateCargoTrackingRecords.CargoTrackingUpdateDataBaseArgs.BuildCargoArgs.SourceConnectionString); ;
             }
             else
-            {
+            {  
                 updateCargoTrackingRecords.AllTenantIds = new List<int> { updateCargoTrackingRecords.CargoTrackingUpdateDataBaseArgs.CargoTrackingArguments.Tenant.Value };
             }
                 updateCargoTrackingRecords.MilestonesNotPermitted = cargoTrackingShipmentsService.GetAllNotPermittedMilestones(updateCargoTrackingRecords.CargoTrackingUpdateDataBaseArgs.BuildCargoArgs.SourceConnectionString);
