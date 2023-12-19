f05eb8b2319cf9556ef0a4e296995cb493d69903
Libby Hasid <libby@amital.co.il>
2023-12-19 16:08:57 +0200

    #fix effectiveFlight

--- Logitude/Logitude.Customs.Data/EntityMapping/DeclarationPaymentMap.cs
+++ Logitude/Logitude.Customs.Data/EntityMapping/DeclarationPaymentMap.cs
@@ -42,7 +42,6 @@ namespace Logitude.Customs.Data.EntityMapping
             this.Property(t => t.FuturePaymentDateTime).HasColumnName("FuturePaymentDateTime");
 
             this.Property(t => t.AutomaticPayment).HasColumnName("AutomaticPayment");
-
         }
     }
 }
