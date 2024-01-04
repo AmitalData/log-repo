01f6fddaeeedf28010ce6ccc407a2a7f94bcc54b
Sohaib Nemer <sohaib@amital.co.il>
2024-01-04 12:37:59 +0200

    add  e.stacktrace for debug purposes

--- Logitude/Logitude.MetadataUpdate/MetadataUpdateService.cs
+++ Logitude/Logitude.MetadataUpdate/MetadataUpdateService.cs
@@ -35,9 +35,11 @@ namespace Logitude.MetadataUpdate
             catch (Exception e)
             {
                 Console.WriteLine("Error: " + e.Message);
+                Console.WriteLine("Error: " + e.StackTrace);
+
                 Console.WriteLine(e.InnerException?.Message);
                 Console.WriteLine("Inner Exception Stack Trace:");
-                Console.WriteLine(e.InnerException.StackTrace);
+                Console.WriteLine(e.InnerException?.StackTrace);
                 Environment.Exit(1);
 
             }
