4b5abc1e1f240f2e296c2ff213d3306aa1a3f8c4
izik89 <izikfr@amital.co.il>
2024-01-07 18:31:21 +0200

    #94793

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/Maintenance/MaintenanceComponent.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/Maintenance/MaintenanceComponent.ts
@@ -66,7 +66,7 @@ export class MaintenanceComponent {
             this.PagesMenu.push(new Menu("CRM", TextCodeTranslator.Translate("General.MC.CRM.CRM")));
         }
         
-        if (FeatureLocator.HasFeaturePermession("General", "SHAAMTOKEN"))
+        if (FeatureLocator.HasFeaturePermession("General", "InvoiceConfirmationNumber"))
             this.PagesMenu.push(new Menu("SHA", TextCodeTranslator.Translate("General.MC.ShaamTokenManagement")));
 
         if (FeatureLocator.HasFeaturePermession("General", "TICKET")) {
@@ -774,7 +774,7 @@ export class MaintenanceComponent {
     }
 
     private BuildShaamTokenManagementMenu() {
-        if (!FeatureLocator.HasFeaturePermession("General", "SHAAMTOKEN")) return;
+        if (!FeatureLocator.HasFeaturePermession("General", "InvoiceConfirmationNumber")) return;
 
         var item = new MenusTablePM();
         item.CategoryTypeCode = "SHA";
