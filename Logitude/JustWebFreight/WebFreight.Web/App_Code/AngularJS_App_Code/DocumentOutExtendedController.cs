653de886d6e24b2204ce7a00729b38de9ec325f6
Libby Hasid <libby@amital.co.il>
2024-01-08 13:56:26 +0200

    #101715

--- Logitude/JustWebFreight/WebFreight.Web/App_Code/AngularJS_App_Code/DocumentOutExtendedController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/App_Code/AngularJS_App_Code/DocumentOutExtendedController.cs
@@ -50,6 +50,7 @@ using Logitude.Accounting.Data;
 using Logitude.Accounting.Data.EntityPOCOs;
 using Customer = Simplog.Data.CommonDataModel.EntityPOCOs.Customer;
 using Microsoft.TeamFoundation.Common;
+using Logitude.Accounting.Data.Repositories;
 
 namespace WebFreight.Web.App_Code.AngularJS_App_Code
 {
@@ -333,27 +334,60 @@ namespace WebFreight.Web.App_Code.AngularJS_App_Code
                 string contactEmail = this.IsSignatureHtmlPresentByBillToId(invocie.BillToId, tenant);
                 if (!string.IsNullOrEmpty(contactEmail))
                 {
-                    this.CreatePdfDoc(documentsFiling, invocie);
+                    this.CreatePdfDoc(documentsFiling, invocie.Id, invocie.Tenant,"ARInvoice");
+                    if(documentHelper.isInterestReport && invocie.ARInvoiceTypeCode == "IT")
+                    {
+                        this.createDocumentInterestReport(invocie.Tenant,invocie.Id);
+                    }
                     documentHelper.StartSignPDFInvoice(invocie, invocie.Tenant, repository, contactEmail,  accountingSettings);
                 }
 
             }
 
         }
-        private void CreatePdfDoc(DocumentsFiling documentsFiling, ARInvoice invocie)
+
+        public void createDocumentInterestReport(int tenant,string arinvocieId)
+        {
+            ICommonDataContext commoncontext = CommonDataContext.GetContext(tenant);
+            DocumentOutQuery documentOutQuery = new DocumentOutQuery(tenant);
+            DocumentRepository documentRepository = new DocumentRepository(commoncontext);
+            DocumentsFilingRepository myDocumentsFilingRepository = new DocumentsFilingRepository(commoncontext);
+            DocumentsFilingQuery myDocumentsFilingQuery = new DocumentsFilingQuery(myDocumentsFilingRepository);
+           DocumentTypeQueryService DocumentTypeService = new DocumentTypeQueryService(tenant);
+            InterestReportRepository interestReportRepository = new InterestReportRepository(tenant);
+            InterestReport interestReport = new InterestReport();
+
+            string documentTypeId = DocumentTypeService.GetDocumentTypeByCode("ITDT", tenant).Id;
+            interestReport = interestReportRepository.GetSingleByARInvoiceId(arinvocieId, tenant);
+            DocumentOutPM documentOutPM = documentOutQuery.GetDocumentOutByDocumentTypeEntityAndChild(interestReport.Id, null, documentTypeId, tenant);
+            if (documentOutPM == null)
+            {
+                var LoggingObjectTableId = ObjectTableRepository.GetObjectTableByName("InterestReport");
+                DocumentHelper documentHelper = new DocumentHelper();
+                documentOutPM = documentHelper.CreateDocumentOut(documentTypeId, interestReport.Id, null, interestReport.ReportNumber, LoggingObjectTableId, tenant, null, null);
+            }
+            var documentsFiling = commoncontext.DocumentsFilings.Where(doc => doc.Id == documentOutPM.Id).FirstOrDefault();
+            DocumentsFilingPM myDocumentFilings = myDocumentsFilingQuery.GetDocumentsFilingPMsByEntityId(interestReport.Id, tenant).FirstOrDefault();
+            if (string.IsNullOrEmpty(documentsFiling.DocumentId))
+            {
+                this.CreatePdfDoc(documentsFiling, interestReport.Id, tenant, "InterestReport");
+            }
+        }
+
+        private void CreatePdfDoc(DocumentsFiling documentsFiling, string Id,int tenant,string objectTableName)
         {
 
             ExportDocumentHelper exportDocumentHelper = new ExportDocumentHelper();
-            ObjectTableRepository objectTableRepository = new ObjectTableRepository(invocie.Tenant);
-            DocumentTypeQuery documentTypeQuery = new DocumentTypeQuery(invocie.Tenant);
-            DocumentTypePM documentTypePM = documentTypeQuery.GetSinglePM(documentsFiling.DocumentTypeId, documentsFiling.Id, invocie.Tenant);
+            ObjectTableRepository objectTableRepository = new ObjectTableRepository(tenant);
+            DocumentTypeQuery documentTypeQuery = new DocumentTypeQuery(tenant);
+            DocumentTypePM documentTypePM = documentTypeQuery.GetSinglePM(documentsFiling.DocumentTypeId, documentsFiling.Id, tenant);
 
-            string resolveLoggingUserId = AuthenticationUtil.ResolveUserIdentityName(invocie.Tenant);
-            var objectTable = objectTableRepository.GetObjectTableByName("ARInvoice", invocie.Tenant, true);
+            string resolveLoggingUserId = AuthenticationUtil.ResolveUserIdentityName(tenant);
+            var objectTable = objectTableRepository.GetObjectTableByName(objectTableName, tenant, true);
 
             documentTypePM.DocumentTypeCopies.ForEach(doc =>
             {
-                exportDocumentHelper.ExportDocument2Pdf(documentsFiling.DocumentTypeId, invocie.Id, objectTable.Id, null, null, documentsFiling.Id, invocie.Tenant, doc.Id, resolveLoggingUserId);
+                exportDocumentHelper.ExportDocument2Pdf(documentsFiling.DocumentTypeId, Id, objectTable.Id, null, null, documentsFiling.Id, tenant, doc.Id, resolveLoggingUserId);
             });
 
         }
