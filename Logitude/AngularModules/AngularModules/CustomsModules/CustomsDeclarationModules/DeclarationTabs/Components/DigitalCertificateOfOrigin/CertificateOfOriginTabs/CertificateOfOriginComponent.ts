d230ed7b1f319e9eaadcc2da49e7dc7f5b0cb17c
Elon Hezi <Elon@amital.co.il>
2024-01-25 19:09:04 +0200

    #102237

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationTabs/Components/DigitalCertificateOfOrigin/CertificateOfOriginTabs/CertificateOfOriginComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationTabs/Components/DigitalCertificateOfOrigin/CertificateOfOriginTabs/CertificateOfOriginComponent.ts
@@ -281,6 +281,8 @@ export class CertificateOfOriginComponent extends BaseRequestsSheetMassaging  {
 
     
     async SendButtonClicked(customSendOptionsArgs:any){
+        this.SaveButtonClicked();  // save before send
+
         if(!this.EntityPM.CooTypeCode || !this.EntityPM.RequestReasonCode) {// manddatory fields
             this.GENERAL.CheckMandatoryFields();
             return;
