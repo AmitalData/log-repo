35c5abff0916b25810767a40ffccdf2db2b6496d
Elon Hezi <Elon@amital.co.il>
2024-01-30 13:02:34 +0200

    #102571

--- Logitude/Logitude.CustomsMessaging/ResponseServices/PC_NG_2280_MSG01_CertificateOfOriginRequestResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/PC_NG_2280_MSG01_CertificateOfOriginRequestResponseService.cs
@@ -67,7 +67,7 @@ namespace Logitude.CustomsMessaging.ResponseServices
 			this.MyRequestSheetParam.ObjectTableId2 = ObjectTableRepository.GetObjectTableByName("Customs.CertificateOfOrigin");
 			this.MyRequestSheetParam.EntityId2 = requestParams.CertificateOfOriginId;
 			this.MyRequestSheetParam.CustomFileNo = requestParams.CustomFileNo;
-			this.MyRequestSheetParam.RequestDescription = (requestParams.RequestReasonCode == 13 ? "משוב לסטטוס תעודת מקור: " : "משוב תעודת מקור: ") + certificateOfOrigin.Counter;
+			this.MyRequestSheetParam.RequestDescription = (requestParams.RequestReasonCode == 13 ? "משוב לסטטוס תעודת מקור: " : "משוב תעודת מקור: ") + certificateOfOriginPM.Counter;
 
 			if (customResponse.ResponseContentHeader.Exception != null)
 			{
