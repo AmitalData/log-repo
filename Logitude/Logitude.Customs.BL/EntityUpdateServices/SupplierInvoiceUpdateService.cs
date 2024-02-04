95d9d6ca968e0bfbff05bc4615c36ae29b4af53d
elisheva Nachaysi <elisheva@amital.co.il>
2024-02-04 15:28:00 +0200

    fix SequenceNumeric

--- Logitude/Logitude.Customs.BL/EntityUpdateServices/SupplierInvoiceUpdateService.cs
+++ Logitude/Logitude.Customs.BL/EntityUpdateServices/SupplierInvoiceUpdateService.cs
@@ -83,7 +83,7 @@ namespace Logitude.Customs.BL.EntityUpdateServices
 
                     DeclarationQueryService declarationQueryService = new DeclarationQueryService(entityPM.Tenant);
                     declarationPM = declarationQueryService.GetSingle(entityPM.DeclarationId, false, false);
-                    if (declarationPM?.Direction == "E" && declarationPM?.IsAmendment == true && entityPM.SequenceNumeric.GetValueOrDefault() == 0)
+                    if  ( declarationPM?.IsAmendment == true && entityPM.SequenceNumeric.GetValueOrDefault() == 0)
                         entityPM.SequenceNumeric = entityPM.InvoiceCounterKey;
                 }
             }
@@ -737,7 +737,7 @@ namespace Logitude.Customs.BL.EntityUpdateServices
                 int index = 0;
                 var my = new SupplierInvoiceKeys() { DeclarationId = entityPM.DeclarationId, InvoiceCounterKey = entityPM.InvoiceCounterKey };
 
-                if (defaultDeclarationPM.IsAmendment != true || defaultDeclarationPM.Direction != "E")
+                if (defaultDeclarationPM.IsAmendment != true )
                 {
 
 
