cf015a6f81283bd24d08e6f57c32aa931f366ae5
Alexander Khitrik <alexanderh@amital.co.il>
2024-02-12 08:36:06 +0200

    Alexander: #103040 AllMonths Fix 2

--- Logitude/Logitude.Accounting.BL/EntityQueryServices/LedgerTransactionQueryService.cs
+++ Logitude/Logitude.Accounting.BL/EntityQueryServices/LedgerTransactionQueryService.cs
@@ -57,6 +57,7 @@ namespace Logitude.Accounting.BL.EntityQueryServices
 
         public DateTime GetFirstDateByDateType(string dateTypeValue, string gLAccointId, int tenant)
         {
+
             DateTime firstDate = DateTime.Today;
             if (!String.IsNullOrEmpty(gLAccointId)) gLAccointId = gLAccointId.Trim();
 
