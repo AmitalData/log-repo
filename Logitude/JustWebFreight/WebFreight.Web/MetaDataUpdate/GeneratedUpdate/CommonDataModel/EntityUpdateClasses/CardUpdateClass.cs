3aa6bb8cc5438acaf4cde8a653020c585f2aee97
Liron Shaked Hodorov <LironSH@amital.co.il>
2024-02-19 15:29:59 +0200

    #103074

--- Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/GeneratedUpdate/CommonDataModel/EntityUpdateClasses/CardUpdateClass.cs
+++ Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/GeneratedUpdate/CommonDataModel/EntityUpdateClasses/CardUpdateClass.cs
@@ -85,7 +85,7 @@ namespace WebFreight.Web.MetaDataUpdate.GeneratedUpdate.CommonDataModel.EntityUp
 {
    public class CardUpdateClass
    {  		
-		public const string HashString = "23985804e018dc2920c39774ebadc199";
+		public const string HashString = "33084d2f972ff6b23f1235351c5be51f";
 	    public void AddObjectTable(Dictionary<string, ObjectTable> objectTables, Dictionary<string, TextCode> textCodes,ObjectTableRepository ObjectTableRepository,TextCodeRepository TextCodeRepository)
         {                     
             
@@ -3027,7 +3027,7 @@ namespace WebFreight.Web.MetaDataUpdate.GeneratedUpdate.CommonDataModel.EntityUp
 					  						FullLocalDefaultText =  "שם הלקוח",
 					  						ListFieldLable =  "LocalNameListLable",
 					  						ListLableDefaultText =  "Local Name",
-					  						ListLocalDefaultText =  "שם מקומי",
+					  						ListLocalDefaultText =  "שם הלקוח",
 					  						ShortFieldLable =  "LocalName",
 					  						ShortFieldLableDefaultText =  "Local Name",
 					  						ShortLocalDefaultText =  "שם הלקוח",
