cb12806df155879d68eed3ca8cad6bdadef6c1e9
Shoshana Greenwald <shoshana@amital.co.il>
2024-02-21 16:58:15 +0200

    #103544

--- Logitude/Logitude.Accounting.BL/CoreBL/TaxReportService.cs
+++ Logitude/Logitude.Accounting.BL/CoreBL/TaxReportService.cs
@@ -224,8 +224,17 @@ namespace Logitude.Accounting.BL.CoreBL
 
                 GLAccountPM account = GetAccountByLedgerTransaction(transaction);
                 card = GetGLAccountCard(account);
+				if (transaction.AccountingEntity == AccountingEntityValues.APInvoice)
+				{
+					aPInvoice = allAPInvoices.Where(d => d.Id == transaction.AccountingEntityId).FirstOrDefault();
 
-                if (account != null)
+					if (aPInvoice != null)
+					{
+						VatNumber = aPInvoice.VATNumber;
+					}
+					
+				}
+				else if (account != null)
                 {
                     if (account.AccountTypeCode == "3" || account.AccountTypeCode == "2" || account.AccountTypeCode == "1")
                     {
