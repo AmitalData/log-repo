ae10e09408f3e2e19bf19f2cb5fb6e6c840a3e74
Alexander Khitrik <alexanderh@amital.co.il>
2024-02-22 11:12:56 +0200

    Alexander: #102390 TaxReportLine - Storno - Status

--- Logitude/Logitude.Accounting.BL/CoreBL/TaxReportService.cs
+++ Logitude/Logitude.Accounting.BL/CoreBL/TaxReportService.cs
@@ -386,7 +386,9 @@ namespace Logitude.Accounting.BL.CoreBL
                     {
                         VatNumber = g.Key.VatNumber,
                         Reference = g.Key.Reference,
-                        LineNumbers = g.OrderBy(x => x.Line).Select(x => x.Line)
+                        LineNumbers = g.OrderBy(x => x.Line).Select(x => x.Line),
+                        JournalIds = g.OrderBy(x => x.Line).Select(x => x.JournalId).ToList(),
+                        IsStornoPair = false,
                     })
                     .Where(r => r.LineNumbers.Count() >= 1).ToList();
                 }
@@ -404,6 +406,32 @@ namespace Logitude.Accounting.BL.CoreBL
                         all_dup_line_nos.AddRange(item.LineNumbers);
                     }
 
+                    List<string> all_dup_line_jIds = new List<string>();
+                    foreach (var item in duplicates)
+                    {
+                        all_dup_line_jIds.AddRange(item.JournalIds);
+                    }
+                    List<int> voidedLineNumbers = new List<int>();
+                    JournalQueryService journalQueryService = new JournalQueryService(taxReportPM.Tenant);
+                    List<JournalPM> jPMs = journalQueryService.GetJournalPMs(all_dup_line_jIds, taxReportPM.Tenant);
+                    if (jPMs != null && jPMs.Count > 0)
+                    {
+                        foreach (var item in duplicates)
+                        {
+                            List<JournalPM> oneDupItemJournalPMs = jPMs.Where(j => item.JournalIds.Contains(j.Id)).OrderByDescending(j => j.StatusCode).ToList();
+                            if (oneDupItemJournalPMs != null && oneDupItemJournalPMs.Count == 2)
+                            {
+                                if ((oneDupItemJournalPMs[0].StatusCode == JournalStatuses.Voided  || (oneDupItemJournalPMs[0].IsVoided.HasValue && oneDupItemJournalPMs[0].IsVoided.Value)) &&
+                                    oneDupItemJournalPMs[1].OriginalJournalId == oneDupItemJournalPMs[0].Id &&
+                                    oneDupItemJournalPMs[0].VoidedByJournalId == oneDupItemJournalPMs[1].Id)
+                                {
+                                    item.IsStornoPair = true;
+                                    voidedLineNumbers.AddRange(item.LineNumbers);
+                                }
+                            }
+                        }
+                    }
+
              //       List<TaxReportLinePM> duplicateLines = new List<TaxReportLinePM>();
 
                     foreach (var oneLine in taxReportLines)
@@ -419,6 +447,10 @@ namespace Logitude.Accounting.BL.CoreBL
                          //   oneLine.ChangeSetOp = ChangeSetOperation.Update;
                             oneLine.StatusCode = "6";
                         //    removeDupLines.Add(oneLine);
+                        }
+                        if (voidedLineNumbers.Contains(oneLine.Line))
+                        {
+                            oneLine.TransmitStatusCode = TaxReportLineTransmitStatusValues.Notfortransmitatall;
                         }
 
                     }
@@ -464,6 +496,8 @@ namespace Logitude.Accounting.BL.CoreBL
             public string VatNumber { get; set; }
             public string Reference { get; set; }
             public IEnumerable<int> LineNumbers { get; set; }
+            public List<string> JournalIds { get; set; } 
+            public bool IsStornoPair { get; set; }
 
         }
         private static string SetTransmitStaus(DateTime invoiceDate, DateTime taxReportMonth, FullAccountingSetting setting)
