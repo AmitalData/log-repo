cb785f9b325bb8c836a55b89afa07a07a71ef813
OritSH <OritSH@ORITSH-11PC>
2024-02-25 12:06:00 +0200

    #103415

---
+++ Logitude/AngularModules/CypressTesting/FullAccounting/cypress/selectors/ExcelDownloadSelectors.ts
@@ -0,0 +1,10 @@
+export class ExcelDownloadSelectors {
+    public static readonly JournalTab = "#FAJournal";
+    public static readonly NewJournalButton = '#NewJournal';
+    public static readonly AllJournal = '.QueryLink:eq(3)';
+    //public static readonly Excelicon = "img[src='/Images/Buttons/excelicon.png']"
+
+    public static readonly Excelicon = '.CenterCenter:eq(2)'
+
+ }
+
