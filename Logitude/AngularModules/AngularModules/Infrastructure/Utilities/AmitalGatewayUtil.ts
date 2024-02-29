d68a3dc5185ea420d44d6f41094c47c2b95943ec
Dror Granot <Dror@amital.co.il>
2024-02-29 13:18:38 +0000

    Updated AmitalGatewayUtil.ts

--- Logitude/AngularModules/AngularModules/Infrastructure/Utilities/AmitalGatewayUtil.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Utilities/AmitalGatewayUtil.ts
@@ -44,7 +44,7 @@ export class AmitalGatewayUtil {
 
     private _AmitalBrowserInUse: boolean = false;
     public get isUnifreightHost(): boolean {  
-        try { return  !!window?.JSBridge || !!window?.parent?.JSBridge; } 
+        try { return !!window?.parent?._JavascriptGateway; } 
         catch  { return false; }
     }
     public get AmitalBrowserInUse(): boolean { return (this._AmitalBrowserInUse === true); }
@@ -1597,5 +1597,3 @@ export class ShowInvoiceFromUrouterReturnCreateInvoiceCommand {
         */
     }  
 }
-
-
