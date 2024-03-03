66fb271a21a31e01acae28578fe60697d4ce73c5
Simon Polishuk <SimonP@amital.co.il>
2024-03-03 11:59:10 +0200

    SatisfactionSurveyComponent

---
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/SatisfactionSurvey/SatisfactionSurveyComponent.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Location } from '@angular/common';
+
+@Component({
+    selector: 'SatisfactionSurveyComponent',
+    templateUrl: './SatisfactionSurveyComponent.html',
+})
+export class SatisfactionSurveyComponent implements OnInit {
+    tabs = [
+        { id: 'tab1', content: 'כלל לא מרוצה' },
+        { id: 'tab2', content: 'במידה מועטה' },
+        { id: 'tab3', content: 'במידה בינונית' },
+        { id: 'tab4', content: 'במידה רבה' },
+        { id: 'tab5', content: 'במידה רבה מאוד' },
+    ];
+
+    activeTab: string = this.tabs[0].content;
+    surveyForm: FormGroup;
+
+    constructor(private location: Location) {
+        this.surveyForm = new FormGroup({
+            feedback: new FormControl(''),
+            activeTab: new FormControl(this.activeTab),
+            currentUrl: new FormControl(this.getCurrentUrl()),
+        });
+    }
+
+    private getCurrentUrl(): string {
+        return this.location.path(); // Return the current URL path
+    }
+
+    ngOnInit() {}
+
+    setActiveTab(tabContent: string) {
+        this.activeTab = tabContent;
+        this.surveyForm.get('activeTab').setValue(this.activeTab);
+    }
+
+    onSubmit() {
+        console.log('Form submitted!');
+        console.log(this.surveyForm.value);
+    }
+}
