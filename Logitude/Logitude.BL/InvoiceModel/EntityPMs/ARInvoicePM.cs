bc27b9d1ae34bfcd0318c0dae457b96b0c481b3a
Libby Hasid <libby@amital.co.il>
2024-03-18 12:10:27 +0200

    #104068

--- Logitude/Logitude.BL/InvoiceModel/EntityPMs/ARInvoicePM.cs
+++ Logitude/Logitude.BL/InvoiceModel/EntityPMs/ARInvoicePM.cs
@@ -448,6 +448,5 @@ namespace Logitude.BL.InvoiceModel.EntityPMs
         public string TransferError_Original { get; set; }
         public bool VatsAmountsManulAdjuested { get; set; }
         public string ConfirmationNumber { get; set; }
-        public string ConfirmationNumberStatus { get; set; }
     }
 }
