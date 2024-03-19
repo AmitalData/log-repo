8f945c397bb1d14e776d813b97bfb2b960042b97
Shoshana Greenwald <shoshana@amital.co.il>
2024-03-19 16:56:30 +0200

    #94162

--- Logitude/AngularModules/AngularModules/Common/EntityPMs/CustomerPM.ts
+++ Logitude/AngularModules/AngularModules/Common/EntityPMs/CustomerPM.ts
@@ -571,11 +571,12 @@ export class CustomerPM extends ObjectCustomFieldPM {
     private emailForSendingSingArinvoice: string;
     public get EmailForSendingSingArinvoice() { return this.emailForSendingSingArinvoice; }
     public set EmailForSendingSingArinvoice(newValue: string) { if (this.emailForSendingSingArinvoice != newValue) { this.emailForSendingSingArinvoice = newValue; this.MarkAsDirty("EmailForSendingSingArinvoice"); } }
-
+       
+	 
     private sendingInterestReport: boolean;
     public get SendingInterestReport() { return this.sendingInterestReport; }
     public set SendingInterestReport(newValue: boolean) { if (this.sendingInterestReport != newValue) { this.sendingInterestReport = newValue; this.MarkAsDirty("SendingInterestReport"); } }
-      
+       
 	 
     private customerStatusName: string;
     public get CustomerStatusName() { return this.customerStatusName; }
@@ -1448,9 +1449,12 @@ export class CustomerPM extends ObjectCustomFieldPM {
     public get IsPotential() { return this.isPotential; }
     public set IsPotential(newValue: boolean) { if (this.isPotential != newValue) { this.isPotential = newValue; this.MarkAsDirty("IsPotential"); } }
        
+	 
     private contactForAccounting: string;
     public get ContactForAccounting() { return this.contactForAccounting; }
-    public set ContactForAccounting(newValue: string) { if (this.contactForAccounting != newValue) { this.contactForAccounting = newValue; this.MarkAsDirty("contactForAccounting"); } }
+    public set ContactForAccounting(newValue: string) { if (this.contactForAccounting != newValue) { this.contactForAccounting = newValue; this.MarkAsDirty("ContactForAccounting"); } }
+       
+	 
 
     public OldEntityPM: CustomerPM;
 		
@@ -1479,4 +1483,4 @@ export class CustomerPM extends ObjectCustomFieldPM {
         ServiceHelper.RejectEntityPMChanges(this);
     }
 
-}
+}
