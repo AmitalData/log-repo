fa46d15be831c73ee31fbac70fc0b2854a30cd20
elisheva Nachaysi <elisheva@amital.co.il>
2024-03-19 15:15:41 +0200

    #103552

--- Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_8251_Web02_DeclarationStatus_ResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_8251_Web02_DeclarationStatus_ResponseService.cs
@@ -818,7 +818,7 @@ namespace Logitude.CustomsMessaging.ResponseServices
             var myDeclarationQueryService = new DeclarationQueryService(dbContext);
             var myDeclarationUpdateService = new DeclarationUpdateService(dbContext, new Dictionary<string, IContext>(), requestParams.Tenant);
             var _declarationPM = myDeclarationQueryService.GetSingle(declarationPM.Id, true, false);
-
+            _declarationPM.MarkAsChanged = false;
             _declarationPM.AvailabilityDate = declarationAvaliabilityDate ?? DateTime.Now;
             _declarationPM.ChangeSetOp = ChangeSetOperation.Update;
             myDeclarationUpdateService.Update(_declarationPM, true);
