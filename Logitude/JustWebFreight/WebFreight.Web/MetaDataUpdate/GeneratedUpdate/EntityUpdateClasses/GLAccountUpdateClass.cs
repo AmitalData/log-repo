2cb7f13d6004c3ab08f293ee2676cfbd30ab0829
Liron Shaked Hodorov <LironSH@amital.co.il>
2024-03-20 17:25:15 +0200

    #103503

--- Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/GeneratedUpdate/EntityUpdateClasses/GLAccountUpdateClass.cs
+++ Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/GeneratedUpdate/EntityUpdateClasses/GLAccountUpdateClass.cs
@@ -85,7 +85,7 @@ namespace WebFreight.Web.MetaDataUpdate.GeneratedUpdate.EntityUpdateClasses
 {
    public class GLAccountUpdateClass
    {  		
-		public const string HashString = "d3b87e1bfd7770153d7b32a439b80e0a";
+		public const string HashString = "a5116a094449827a0e7ec0d6898b4aa4";
 	    public void AddObjectTable(Dictionary<string, ObjectTable> objectTables, Dictionary<string, TextCode> textCodes,ObjectTableRepository ObjectTableRepository,TextCodeRepository TextCodeRepository)
         {                     
             
