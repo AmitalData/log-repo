c8e6e03cafaa76a4843384603a99c589bf4ea28c
Simon Polishuk <SimonP@amital.co.il>
2024-04-07 14:10:38 +0300

    #104815 fix merge

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationSupplierInvoice/Components/SupplierInvoices/SupplierInvoiceGeneralTabComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationSupplierInvoice/Components/SupplierInvoices/SupplierInvoiceGeneralTabComponent.ts
@@ -3873,6 +3873,9 @@ export class SupplierInvoiceItemLine extends BaseComponent {
     public get TradeAgreementCode() { return this.entityPM.TradeAgreementCode; }
     public set TradeAgreementCode(newValue: string) {
         this.entityPM.TradeAgreementCode = newValue;
+        if (this.Parent.declarationPM.Direction == "E") {
+            this.Parent.PreferenceDocumentTypeCode = newValue;
+        }
         this.CheckTariff();
     }
 
@@ -3882,6 +3885,9 @@ export class SupplierInvoiceItemLine extends BaseComponent {
     public get DutyRegimeProtocolCode() { return this.entityPM.DutyRegimeProtocolCode; }
     public set DutyRegimeProtocolCode(newValue: string) {
         this.entityPM.DutyRegimeProtocolCode = newValue;
+        if (this.Parent.declarationPM.Direction == "E") {
+            this.Parent.DutyRegimeProtocolCode = newValue;
+        }
         this.tradeAgreementProtocolListService.getSingle(newValue).subscribe((res) =>
             this.DutyRegimeProtocolLocalName = res?.Result?.LocalName);
     }
