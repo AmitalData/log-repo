651e8d655c48ad1276fa354fd874b810c3120a25
elisheva Nachaysi <elisheva@amital.co.il>
2024-04-09 12:58:44 +0300

    test

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationSupplierInvoice/Components/SupplierInvoices/SupplierInvoiceGeneralTabComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationSupplierInvoice/Components/SupplierInvoices/SupplierInvoiceGeneralTabComponent.ts
@@ -3966,6 +3966,8 @@ export class SupplierInvoiceItemLine extends BaseComponent {
         ItemPriceTextBox.TextValue = this.oldvalue;
     }
 
+
+    
     GetQuantityType(isChangeInvoiceQuantityType: boolean = true, calcInvoiceQuantityType: boolean = false) {
 
         if (this.ClassificationCode != null) {
