d44f0acbd8d0564a2ae18c9fdf0bf64d285e9edb
Elon Hezi <Elon@amital.co.il>
2024-04-11 16:02:20 +0300

    #105359 #104880

--- Logitude/Logitude.CustomsMessaging/ResponseServices/Get_CB_MSG_8314_8888_CustomItemDetailsHeaderResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/Get_CB_MSG_8314_8888_CustomItemDetailsHeaderResponseService.cs
@@ -154,12 +154,12 @@ namespace Logitude.CustomsMessaging.ResponseServices
                         currentDBListCustomsItemRow.CustomsItemHierarchicLocationID = mehesCustomsItemRow.HierarchicLocationID;
                         currentDBListCustomsItemRow.ComputedCheckDigit =mehesCustomsItemRow.fullClassification.Length>10 ?mehesCustomsItemRow.fullClassification.Substring(mehesCustomsItemRow.fullClassification.Length - 1):null ;
                         currentDBListCustomsItemRow.CustomsBookTypeID = requestParams.CustomsBookType;
-                        currentDBListCustomsItemRow.CustomsItemCategoryID = mehesCustomsItemRow.isDiscountCode == false ? 1 : currentDBListCustomsItemRow.CustomsItemCategoryID;
+                        currentDBListCustomsItemRow.CustomsItemCategoryID = mehesCustomsItemRow.isDiscountCode == false && !mehesCustomsItemRow.fullClassification.Contains("-") ? 1 : 2;
                        // currentDBListCustomsItemRow.CustomsItemCategoryID = mehesCustomsItemRow.statisticMeasurementUnitCode?? 0;
 
                         customsItemUpdateService.Update(currentDBListCustomsItemRow, false);
                        
-                            logContext.SaveChanges();
+                        logContext.SaveChanges();
                        
                     }
                     if(doUpdateCustomsItemDetailsHistory && (mehesCustomsItemRow.isDiscountCode == true || mehesCustomsItemRow.fullClassification.Contains("-")))
