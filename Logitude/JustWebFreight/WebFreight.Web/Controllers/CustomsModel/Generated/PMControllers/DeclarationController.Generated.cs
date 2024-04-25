f1d390b5ff1f1b7c5103b8b005778a691ca09932
Elon Hezi <Elon@amital.co.il>
2024-04-25 11:42:20 +0300

    #105747

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/CustomsModel/Generated/PMControllers/DeclarationController.Generated.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/CustomsModel/Generated/PMControllers/DeclarationController.Generated.cs
@@ -42,6 +42,7 @@ using Logitude.Customs.Data.EntityLists;
 using Logitude.Customs.BL.EntityUpdateServices;
 using Logitude.Customs.Data.EntityListQueryServices;
 using Logitude.Customs.BL.EntityQueryServices;
+using Devart.Data.Oracle;
 
 namespace WebFreight.Web.App_Code.AngularJS_App_Code.Generated
 { 
@@ -134,6 +135,26 @@ namespace WebFreight.Web.App_Code.AngularJS_App_Code.Generated
             {
                 try
                 {
+
+                    string dbms = "oracle";
+                    string strConnString = "User Id=v5113;Password=v5113;Direct=True;Data Source=oracle19.amital.co.il;port=1521;sid=amital";
+
+                    if (dbms == "oracle")
+                    {
+
+                        using (OracleConnection con = new OracleConnection(strConnString))
+                        {
+
+                            //string cmd = "SELECT TENANT FROM CCUFILEM WHERE FILE_NO = '10'";
+                            string cmd = "Update CCUFILEM set TENANT=102 where FILE_NO='10'";
+                            OracleCommand oracleCommand = new OracleCommand(cmd, con);
+                            con.Open();
+                            oracleCommand.ExecuteNonQuery();
+                            con.Close();
+                        }
+
+                    }
+
                     using (TransactionScope scope = TransactionFactory.GetTransaction())
                     {
                         string logKey = PerformanceLogger.LogCurrentTime();					                        
