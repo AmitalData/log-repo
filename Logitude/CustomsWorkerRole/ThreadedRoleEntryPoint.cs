2031271b5d3909db6402412d7c9d54419b5b5bc0
elisheva Nachaysi <elisheva@amital.co.il>
2024-05-02 12:48:36 +0300

    merge

--- Logitude/CustomsWorkerRole/ThreadedRoleEntryPoint.cs
+++ Logitude/CustomsWorkerRole/ThreadedRoleEntryPoint.cs
@@ -173,7 +173,7 @@ namespace CustomsWorkerRole
             throw new NotImplementedException();
         }
 
-        public static void StartStatic(Action<bool, bool, bool> BuildObjectTablesZipFilesDataAction=null,string ProductInfo=null)
+        public static void StartStatic(Action<bool, bool> BuildObjectTablesZipFilesDataAction=null,string ProductInfo=null)
         {
             if (string.IsNullOrEmpty(LogitudeSettings.DeploymentStage))
             {
