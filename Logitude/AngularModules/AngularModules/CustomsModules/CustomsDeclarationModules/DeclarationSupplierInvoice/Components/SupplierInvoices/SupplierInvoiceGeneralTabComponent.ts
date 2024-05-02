e11f0781f06f54cfb15cb9e8dd9be1e0a6eab6f8
Simon Polishuk <SimonP@amital.co.il>
2024-05-02 16:45:24 +0300

    #105881

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationSupplierInvoice/Components/SupplierInvoices/SupplierInvoiceGeneralTabComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationSupplierInvoice/Components/SupplierInvoices/SupplierInvoiceGeneralTabComponent.ts
@@ -1672,7 +1672,7 @@ export class SupplierInvoiceGeneralTabComponent extends BaseComponent implements
             }
         }
     }
-    updateProcess(item: SupplierInvoiceItemPM, args) {
+     updateProcess(item: SupplierInvoiceItemPM, args) {
         var updateField = args.UpdateField;
         var value = args[args.UpdateField];
         switch (updateField) {
@@ -1697,7 +1697,7 @@ export class SupplierInvoiceGeneralTabComponent extends BaseComponent implements
             case 'DutyRegimeProtocolCode': {
                 item.DutyRegimeProtocolCode = args.FinalValue.Code;
                 item.DutyRegimeProtocolLocalName = args.FinalValue.LocalName;
-                if (this.Parent.declarationPM.Direction == "E") {
+                if (this.Parent.declarationPM.Direction == "E" && (args.UpdateAll || (args.UpdateItemsWithNoValue && !this.EntityPM.SupplierInvoiceItems.filter(d => !d.IsParent).some(t=>t?.DutyRegimeProtocolCode!=null)))) {
                     this.DutyRegimeProtocolCode = item.DutyRegimeProtocolCode;
                 }
                 break;
