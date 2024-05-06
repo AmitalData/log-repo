3b5818cf238031f577196a755384adba6bc0891e
Sohaib Nemer <sohaib@amital.co.il>
2024-05-06 12:32:01 +0300

    #105473

--- Logitude/Logitude.Accounting.BL/CoreBL/TaxReportService.cs
+++ Logitude/Logitude.Accounting.BL/CoreBL/TaxReportService.cs
@@ -230,9 +230,7 @@ namespace Logitude.Accounting.BL.CoreBL
                 VatNumber = null;
                 InputVatAmount = 0;
                 InputInvoiceAmount = 0;
-                var exist = reportLinesList.Where(d => d.JournalId == transaction.JournalId).Any();
-                if (!exist)
-                {
+                
 
                     bool voidedAPInvoiceTaxMonthTransaction = CheckIfAPInvoiceTaxMonthTransactionIsVoided(taxReport, voidedAPInvoices, transaction);
 
@@ -331,7 +329,7 @@ namespace Logitude.Accounting.BL.CoreBL
                 reportLinesList.Add(inputReportLine);
                 //     UpdateJournalAdditionalDataRecord(inputReportLine, transaction);
 
-                }
+               
             }
 
 
