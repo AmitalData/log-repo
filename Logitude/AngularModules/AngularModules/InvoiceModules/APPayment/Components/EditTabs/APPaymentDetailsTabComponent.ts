b6bec6c74884fc79586ef042c4f770cd3a5b9bd9
OritSH <OritSH@ORITSH-11PC>
2024-05-06 15:56:19 +0300

    #105680

--- Logitude/AngularModules/AngularModules/InvoiceModules/APPayment/Components/EditTabs/APPaymentDetailsTabComponent.ts
+++ Logitude/AngularModules/AngularModules/InvoiceModules/APPayment/Components/EditTabs/APPaymentDetailsTabComponent.ts
@@ -111,8 +111,8 @@ export class APPaymentDetailsTabComponent extends BaseComponent implements OnIni
 
     private InitializeBillToLov() {
         if (this.IsFullAccounting) {
-            this.DisplayFieldsFromList = "Code,CalculatedEnglishName,GLAccountDisplayNumber,CityName,CountryCode";
-            this.DisplayLocalFieldsFromList = "Code,CalculatedLocalName,GLAccountDisplayNumber,CityName,CountryCode";
+            this.DisplayFieldsFromList = "Code,GLAccountDisplayNumber,CalculatedEnglishName,CityName,CountryCode";
+            this.DisplayLocalFieldsFromList = "Code,GLAccountDisplayNumber,CalculatedLocalName,CityName,CountryCode";
             this.VendorLovSizeForFullAccounting = 550;
         }
     }
