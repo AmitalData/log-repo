fd448371cdc6880bab21c296f332c9311ace3006
elisheva Nachaysi <elisheva@amital.co.il>
2024-05-15 13:14:52 +0300

    fix bugs

--- Logitude/AngularModules/AngularModules/Accounting/Components/Others/QueueMessagesServices/QueueMessagesStatistics.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/Others/QueueMessagesServices/QueueMessagesStatistics.ts
@@ -29,6 +29,7 @@ export class QueueMessagesStatistics extends BaseComponent implements OnInit {
     public ToStatusText: string = "Status:  ";
     public ToTenantText: string = "Tenant: ";
     public ToRetryNumberText: string = "Retry Number: ";
+    public BackBtnTitle:string ="";
 
     public WaitingSinceText: string = " waiting since ";
     public Parag1: string = "Incremental last run on: ";
