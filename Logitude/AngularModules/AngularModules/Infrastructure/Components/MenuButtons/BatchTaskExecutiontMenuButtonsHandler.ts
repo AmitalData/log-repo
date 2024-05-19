c68d0c2972bde074589760e4f19c6ab59b56ffc3
Liron Shaked Hodorov <LironSH@amital.co.il>
2024-05-19 14:14:28 +0300

    #106122

---
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/MenuButtons/BatchTaskExecutiontMenuButtonsHandler.ts
@@ -0,0 +1,65 @@
+import { BatchTaskExecutionPM } from '../../EntityPMs/BatchTaskExecutionPM';
+import { MenuButtonPM } from '../../EntityPMs/MenuButtonPM'
+import { EntityArgs } from '../../DataContracts/EntityArgs';
+import { BatchTaskExecutionExtendedPMService } from 'Infrastructure/Services/ExtendedPMs/BatchTaskExecutionExtendedPMService';
+import { SessionLocator } from 'Infrastructure/Utilities/SessionLocator';
+declare var window: any;
+
+export class BatchTaskExecutiontMenuButtonsHandler {
+    BatchTaskExecutionExtendedPMService: BatchTaskExecutionExtendedPMService;
+    public EntityPM: BatchTaskExecutionPM;
+    public entityArgs: EntityArgs;
+    private CurrentSession = SessionLocator.SelectedSession;
+
+    public SetEntityPM(entityArgs: EntityArgs) {
+        this.entityArgs = entityArgs;
+        this.EntityPM = entityArgs.EntityPM;
+        this.BatchTaskExecutionExtendedPMService = new BatchTaskExecutionExtendedPMService();
+    }
+
+    public CheckButtonState(menuButtons: MenuButtonPM[]) {
+        if (this.EntityPM != null) {
+            if (this.entityArgs.EditComponent != null) {
+                menuButtons.forEach(menuButton => {
+                    switch (menuButton.EventCode) {
+                        case "Cancel": {
+                            if (this.EntityPM.StatusCode != "C" && this.EntityPM.StatusCode != "I") {
+                                menuButton.IsDisabled = true;
+                            }
+                            break;
+                        }
+                    }
+                });
+            }
+        }
+
+        return menuButtons;
+    }
+
+    public MenuButtonClick(menuButton: MenuButtonPM) {
+        if (this.EntityPM != null) {
+            if (this.entityArgs.EditComponent != null) {
+                switch (menuButton.EventCode) {
+                    case "Cancel": {
+                        this.CancelButtonClcik();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    CancelButtonClcik() {
+        this.CurrentSession.StartBusyIndicator("Canceling...");
+        this.BatchTaskExecutionExtendedPMService.Cancel(this.EntityPM.Id).subscribe((res: any) => {
+            if (!res.HasError) {
+                this.CurrentSession.CurrentEditComponent.ReloadEntityPM();
+                this.CurrentSession.StopBusyIndicator();
+            }
+            else {
+                this.CurrentSession.StopBusyIndicator();
+                this.CurrentSession.CurrentEditComponent.ValidationErrorsList = res.ErrorsArray;
+            }
+        });
+    }
+}
---
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/InfrastructureModel/Extended/BatchTaskExecutionsExtendedController.cs
@@ -0,0 +1,65 @@
+﻿using Logitude.BL.CommonDataModel.EntityQueries;
+using Logitude.Infrastructure.BL.EntityPMs;
+using Logitude.Infrastructure.BL.EntityQueryServices;
+using Logitude.Infrastructure.BL.EntityUpdateServices;
+using Logitude.Infrastructure.Data;
+using Logitude.Infrastructure.Data.EntityListQueryServices;
+using Logitude.Infrastructure.Data.EntityLists;
+using Logitude.Server.Tools.Helpers;
+using Simplog.Data.CommonDataModel.EntityPOCOs;
+using Simplog.Data.CommonDataModel.Repositories;
+using Simplog.Data.Helpers;
+using Simplog.Data.InfrastructureModel.EntityPOCOs;
+using Simplog.Data.InfrastructureModel.Repositories;
+using Simplog.Server.Infrastructure;
+using Simplog.Server.Infrastructure.DataContracts;
+using Simplog.Server.Infrastructure.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Transactions;
+using System.Web;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+using WebFreight.Web.DataContracts;
+using WebFreight.Web.Helpers;
+using WebFreight.Web.Security;
+
+namespace WebFreight.Web.Controllers.InfrastructureModel.Extended
+{
+    public class BatchTaskExecutionsExtendedController : ApiController
+    {
+        public HttpResponseMessage PostCancel(string batchTaskExecutionId)
+        {
+            try
+            {
+                string logKey = PerformanceLogger.LogCurrentTime();
+                string token = HttpContext.Current.Request.Headers["Token"];
+                AuthenticationToken authToken = AuthenticationTokenRepository.GetSingleTokenFromCache(token);
+                SecurityUtility.AuthenticationOnTenant(authToken.Tenant);
+
+                IInfrastructureContext MyContext = InfrastructureContext.GetContext(authToken.Tenant);
+                BatchTaskExecutionQueryService batchTaskExecutionQuery = new BatchTaskExecutionQueryService(MyContext);
+                batchTaskExecutionQuery.InitializeSettings();
+                BatchTaskExecutionPM batchTaskExecutionPM = batchTaskExecutionQuery.GetSingle(batchTaskExecutionId, true, false);
+                batchTaskExecutionPM.StatusCode = "F";
+                batchTaskExecutionPM.ChangeSetOp = ChangeSetOperation.Update;
+                var batchTaskExecutionUpdateService = new BatchTaskExecutionUpdateService(authToken.Tenant);
+                batchTaskExecutionUpdateService.Update(batchTaskExecutionPM, true);
+
+                return Request.CreateResponse(HttpStatusCode.OK, "OK");
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ApiExceptionBuilder.BuildException(ex));
+            }
+
+        }
+    }
+}
