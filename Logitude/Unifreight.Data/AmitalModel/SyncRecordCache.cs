9a8a23f06c5b9d93a440df6aa417f77651f419c9
elisheva Nachaysi <elisheva@amital.co.il>
2024-05-27 18:15:58 +0300

    remove sync record

--- Logitude/Unifreight.Data/AmitalModel/SyncRecordCache.cs
+++ Logitude/Unifreight.Data/AmitalModel/SyncRecordCache.cs
@@ -7,20 +7,20 @@ namespace Unifreight.Data.AmitalModel
     {
         public static void ClearCacheLasySync(string fileNo, int tenant)
         {
-            string cacheKey = $"SyncRecordQuery.GetLastSyncDate." + fileNo + ";" + tenant;
-            CacheHelper.ClearCache(cacheKey);
+            //string cacheKey = $"SyncRecordQuery.GetLastSyncDate." + fileNo + ";" + tenant;
+            //CacheHelper.ClearCache(cacheKey);
         }
 
         public static void ClearCacheLasySyncByPrimaryNum(string primaryNum, int? tenant)
         {
-            if (!tenant.HasValue || primaryNum == null || !long.TryParse(primaryNum, out long lCUSTOMFILENO))
-                return;
+            //if (!tenant.HasValue || primaryNum == null || !long.TryParse(primaryNum, out long lCUSTOMFILENO))
+            //    return;
 
-            var fileNo = new CCUFILEMRepository(tenant.Value).GetFILENOByCUSTOMFILENO(lCUSTOMFILENO);
-            if (fileNo == null)
-                return;
+            //var fileNo = new CCUFILEMRepository(tenant.Value).GetFILENOByCUSTOMFILENO(lCUSTOMFILENO);
+            //if (fileNo == null)
+            //    return;
 
-            ClearCacheLasySync(fileNo.Value.ToString(), tenant.Value);
+            //ClearCacheLasySync(fileNo.Value.ToString(), tenant.Value);
         }
     }
 }
