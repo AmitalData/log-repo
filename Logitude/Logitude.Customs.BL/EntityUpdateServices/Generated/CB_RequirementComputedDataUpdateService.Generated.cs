e6ec06767d00f582f1a25e3ad5270ba63076c680
Frida Lok <frida@amital.co.il>
2024-05-27 17:54:17 +0300

    #106566

---
+++ Logitude/Logitude.Customs.BL/EntityUpdateServices/Generated/CB_RequirementComputedDataUpdateService.Generated.cs
@@ -0,0 +1,74 @@
+ 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Simplog.Server.Infrastructure;
+using Logitude.Server.Tools;
+using Simplog.Data.Helpers;
+using Logitude.Server.Tools.Counters;
+using Simplog.Data.CommonDataModel.Repositories;
+using Simplog.Data.CommonDataModel.EntityPOCOs;
+using System.Web;
+using Logitude.Customs.Data.EntityPOCOs;
+using Logitude.Customs.Def.EntityPMs;
+using Logitude.Customs.BL.EntityDataMappings;
+using Logitude.Customs.Data.Repsitories;
+using Logitude.Customs.Data.EntityKeys;
+using Logitude.Customs.Data;
+
+namespace Logitude.Customs.BL.EntityUpdateServices
+{ 
+   public partial class CB_RequirementComputedDataUpdateService:EntityUpdateService<CB_RequirementComputedData,CB_RequirementComputedDataPM,EntityPM>
+   {
+   
+        CB_RequirementComputedDataRepository entityRepository;
+        public CB_RequirementComputedDataUpdateService(IContext mainContext,Dictionary<string,IContext> additionalContexts, int tenant)
+            : base(mainContext,additionalContexts, tenant)
+        {
+            ICustomContext  context = mainContext as CustomContext;
+            context = context ??mainContext as ICustomContext ; //Up line is A BUG -and i need it 4 Fakes
+            Mapping = new CB_RequirementComputedDataDataMapping();
+            Repository = new CB_RequirementComputedDataRepository(context);
+        }
+
+       
+        private ICustomContext currentContext;
+        public CB_RequirementComputedDataUpdateService(int tenant)
+        {
+            currentContext = CustomContext.GetContext(tenant);
+        }
+
+        public CB_RequirementComputedDataUpdateService(ICustomContext context)
+        {
+            currentContext = context;
+        }
+
+		
+		protected override EntityKeyFields GetKeys(CB_RequirementComputedDataPM entityPM)
+        {
+            CB_RequirementComputedDataKeys entityKeys = new CB_RequirementComputedDataKeys() { CB_ID = entityPM.CB_ID };
+            return entityKeys;
+        }
+
+		
+		protected override void FillDefaultValuesOnCreate(CB_RequirementComputedDataPM entityPM)
+        {     
+  
+		
+	    }
+        
+		protected override void FillDefaultValuesOnUpdate(CB_RequirementComputedDataPM entityPM)
+        {       
+           
+        }
+		  
+		 
+	 
+   }
+   
+}
+	 
