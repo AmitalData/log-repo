ced562386bf88404da258208e3cf126033e3928b
Frida Lok <frida@amital.co.il>
2024-05-27 16:44:27 +0300

    #106566

---
+++ Logitude/Logitude.Customs.BL/EntityQueryServices/Generated/CB_QuotaComputedDataQueryService.Generated.cs
@@ -0,0 +1,69 @@
+ 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Simplog.Server.Infrastructure;
+using Logitude.Server.Tools;
+using Logitude.Customs.Data.EntityPOCOs;
+using Logitude.Customs.Def.EntityPMs;
+using Logitude.Customs.BL.EntityDataMappings;
+using Logitude.Customs.Data.Repsitories;
+using Logitude.Customs.Data.EntityKeys;
+using Logitude.Customs.Data;
+using Simplog.Server.Infrastructure;
+namespace Logitude.Customs.BL.EntityQueryServices
+{ 
+   public partial class CB_QuotaComputedDataQueryService: EntityQueryService<CB_QuotaComputedData,CB_QuotaComputedDataKeys,CB_QuotaComputedDataPM,object,CB_QuotaComputedDataKeys>
+   {
+   
+        CB_QuotaComputedDataRepository repository;
+		ICustomContext  context;
+        public CB_QuotaComputedDataQueryService(int tenant)
+        {
+		    context = CustomContext.GetContext(tenant);
+            MainContext = context;
+            repository = new CB_QuotaComputedDataRepository(context);
+            Repository = repository;
+            mapping = new CB_QuotaComputedDataDataMapping();
+        }
+
+        public CB_QuotaComputedDataQueryService(CB_QuotaComputedDataRepository repository)
+        {
+            this.repository = repository;
+            Repository = repository;
+            mapping = new CB_QuotaComputedDataDataMapping();
+        }
+
+        public CB_QuotaComputedDataQueryService(ICustomContext context)
+        {
+            this.repository = new CB_QuotaComputedDataRepository(context);
+            this.context = context;
+
+            MainContext = context;
+            Repository = repository;
+            mapping = new CB_QuotaComputedDataDataMapping();
+        }
+		 
+		public  CB_QuotaComputedDataPM GetSingle(string cb_id,bool getComposition, bool getFromCache)
+        {
+             EntityKeys = new CB_QuotaComputedDataKeys(){ CB_ID = cb_id };
+
+			 return base.GetSingle(EntityKeys, getComposition, getFromCache);
+        }
+
+       
+	    protected override EntityKeyFields GetKeys(CB_QuotaComputedData entityPOCO)
+        {
+            CB_QuotaComputedDataKeys entityKeys = new CB_QuotaComputedDataKeys() { CB_ID = entityPOCO.CB_ID,  };
+            return entityKeys;
+        }
+     
+	 
+   }
+   
+}
+	 
---
+++ Logitude/Logitude.Customs.Data/EntityLists/Generated/CB_QuotaComputedDataList.Generated.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization; 
+
+namespace Logitude.Customs.Data.EntityLists
+{
+   [DataContract]
+   public partial class CB_QuotaComputedDataList
+   {
+   
+       [Key]
+       [DataMember]
+       public string CB_ID  { get; set; }
+       [DataMember]
+       public int ID  { get; set; }
+       [DataMember]
+       public int QuotaID  { get; set; }
+       [DataMember]
+       public string Title  { get; set; }
+       [DataMember]
+       public int ValidQuotaDetailsHistoryID  { get; set; }
+       [DataMember]
+       public DateTime StartDate  { get; set; }
+       [DataMember]
+       public DateTime EndDate  { get; set; }
+   }
+
+}
+	 
