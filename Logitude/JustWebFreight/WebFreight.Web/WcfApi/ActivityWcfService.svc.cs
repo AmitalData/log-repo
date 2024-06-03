8c95443c27545a42eff6482cbe1a9989f2768a77
izik89 <izikfr@amital.co.il>
2024-06-03 18:32:20 +0300

    #106071

--- Logitude/JustWebFreight/WebFreight.Web/WcfApi/ActivityWcfService.svc.cs
+++ Logitude/JustWebFreight/WebFreight.Web/WcfApi/ActivityWcfService.svc.cs
@@ -643,7 +643,7 @@ namespace WebFreight.Web.WcfApi
             return response;
         }
 
-        public Response UploadDocumentFileDataFromStorage(int tenant, string blobname, string DocumentId = "")
+        public Response UploadDocumentFileDataFromStorage(int tenant, string blobname, string DocumentId = null)
         {
             Response response = new Response();
             if (string.IsNullOrEmpty(blobname))
