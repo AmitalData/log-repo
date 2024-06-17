79a39ad4ae8a0373342405d89b34793daf9b40ba
Libby Hasid <libby@amital.co.il>
2024-06-17 17:17:01 +0300

    #107073

--- Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_2757_MSG10004_ExportDeclarationResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_2757_MSG10004_ExportDeclarationResponseService.cs
@@ -163,7 +163,7 @@ namespace Logitude.CustomsMessaging.ResponseServices
                     //}
 
                     this._MyDeclarationPM.ChangeSetOp = ChangeSetOperation.Update;
-
+                    myDeclarationUpdateService.Update(_MyDeclarationPM, true);
                 }
                 if(_MyDeclarationPM.Direction == "E" && requestParams.RequestVIA != SendRequestVIA.WebServiceBatch && !setting.IsConnectedToUniFreight && customResponse.ResponseContentHeader?.Exception?.Length > 0 && customResponse.Response?.Declaration == null)
                 {
@@ -183,11 +183,7 @@ namespace Logitude.CustomsMessaging.ResponseServices
                 if (!string.IsNullOrWhiteSpace(requestParams.AppicationId))
                 {   if(_MyDeclarationPM==null)
                          _MyDeclarationPM = myQueryService.GetSingle(requestParams.AppicationId, true, false);
-                    if (_MyDeclarationPM != null)
-                    {
-                       _MyDeclarationPM.ExportClosedErrorXML = mydDclarationErrorPointerService.AnalyzeErrorPionterExport(CastError(customResponse.Response.Error), _MyDeclarationPM, WCOTypeEnum.WCO_EX);
-                                            
-                    }
+                  
                 }
 
                 this.MyResponseData.UserMessage = GetExceptionMsg(customResponse.ResponseContentHeader.Exception[0]);
