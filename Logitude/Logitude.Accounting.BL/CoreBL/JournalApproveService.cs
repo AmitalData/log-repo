cfce7db78961c9b87957493a296902a406169dbb
Libby Hasid <libby@amital.co.il>
2024-06-17 11:54:34 +0300

    #107407

--- Logitude/Logitude.Accounting.BL/CoreBL/JournalApproveService.cs
+++ Logitude/Logitude.Accounting.BL/CoreBL/JournalApproveService.cs
@@ -1653,7 +1653,7 @@ namespace Logitude.Accounting.BL.CoreBL
             static JournalApproveWorker()
             {
                 //_NextDueDoneAt = DateTime.UtcNow.Date.AddDays(1);//tomorrow at 00:00
-                _NextDueDoneAt = DateTime.UtcNow.Date.AddDays(-1);//today already done - do next day =tomorrow at 00:00 ///
+                _NextDueDoneAt = DateTime.UtcNow.Date;//today already done - do next day =tomorrow at 00:00 ///
             }
             public Action<int> LogDoneItemInMemoryAction { get; set; }
             public Action SetLastActivate { get; set; }
