c00038b92bafa7210baf1d3e8a3ec0370281ce2a
Libby Hasid <libby@amital.co.il>
2024-06-30 16:01:17 +0300

    #107821

--- Logitude/Logitude.BL/InvoiceModel/Tools/Validating/APInvoiceValidator.cs
+++ Logitude/Logitude.BL/InvoiceModel/Tools/Validating/APInvoiceValidator.cs
@@ -683,25 +683,8 @@ namespace Logitude.BL.InvoiceModel.Tools.Validating
                 }
                 #endregion
 
-                #region Local Amount
-                double? localAmount = MethodHelper.Round(entityPM.AmountInLocalCurrency, 2);
-                //   double? rate = MethodHelper.Round(entityPM.InvoiceCurrencyExchangeRate, 2);
-                double? localAmount_Computed = MethodHelper.Round(entityPM.AmountInInvoiceCurrency * entityPM.InvoiceCurrencyExchangeRate, 2);
-                if (localAmount != localAmount_Computed)
-                {
-                    double head_amt = localAmount_Computed.HasValue ? localAmount_Computed.Value : 0;
-                    double lines_amt = localAmount.HasValue ? localAmount.Value : 0;
-                    double absdiff = Math.Abs(head_amt - lines_amt);
-                    bool just_one_value = localAmount.HasValue ^ localAmount_Computed.HasValue;
-                    if (absdiff > 0.2 | just_one_value) 
-                    {
-                        decimal head_amt_dec = Convert.ToDecimal(head_amt);
-                        decimal lines_amt_dec = Convert.ToDecimal(lines_amt);
-                        string text = $"Wrong Invoice Local Amount, Head={head_amt_dec} Lines={lines_amt_dec}";
-                        throw new ApplicationException(text);
-                    }
-                }
-                #endregion
+                
+               
             }
         }
 
