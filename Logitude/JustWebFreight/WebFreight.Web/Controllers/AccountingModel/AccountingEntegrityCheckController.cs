6458ef5c507703e137672a58a86df6ff48d5566b
Vladi Behak <vladib@amital.co.il>
2024-07-03 15:29:03 +0300

    test vladi

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/AccountingModel/AccountingEntegrityCheckController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/AccountingModel/AccountingEntegrityCheckController.cs
@@ -23,6 +23,7 @@ namespace WebFreight.Web.Controllers.AccountingModel
         {
             try
             {
+                //test vladi
                 string token = HttpContext.Current.Request.Headers["Token"];
                 AuthenticationToken authToken = AuthenticationTokenRepository.GetSingleTokenFromCache(token);
                 SecurityUtility.AuthenticationOnTenant(authToken.Tenant);
