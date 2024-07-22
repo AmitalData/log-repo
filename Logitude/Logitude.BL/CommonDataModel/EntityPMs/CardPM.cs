378c1a12e7806d6afa6abbccf5a14887877b60fd
Alexander Khitrik <alexanderh@amital.co.il>
2024-07-22 12:37:44 +0300

    Alexander: amital fix 22.07.2024

--- Logitude/Logitude.BL/CommonDataModel/EntityPMs/CardPM.cs
+++ Logitude/Logitude.BL/CommonDataModel/EntityPMs/CardPM.cs
@@ -245,8 +245,7 @@ namespace Logitude.BL.CommonDataModel.EntityPMs
         [CustomValidation(typeof(Validators.ValidationClass), "ValidateClass")]
         [DataMember]
         public bool SendingInterestReport { get; set; }
-        public bool IsFromGlaAccountUpdate { get; set; }
-        public bool IsExcludeCard { get; set; }
+
 
     }
 }
