5d047837a6a2ca7a4f2304d8fef48d7da091ac1a
Sohaib Nemer <sohaib@amital.co.il>
2024-07-23 15:55:34 +0300

    #107450

--- Logitude/AngularModules/AngularModules/ShipmentModules/ShipmentTabs/Components/ShipmentPackages/ShipmentPackagesTabComponent.ts
+++ Logitude/AngularModules/AngularModules/ShipmentModules/ShipmentTabs/Components/ShipmentPackages/ShipmentPackagesTabComponent.ts
@@ -1,6 +1,9 @@
-import {  Component, EventEmitter, OnDestroy, OnInit } from "@angular/core";
+import { Component, EventEmitter, OnDestroy, OnInit } from "@angular/core";
+import { PackageTypeList } from "Common/EntityLists/PackageTypeList";
+import { PackageTypeListService } from "Common/Services/StandardLists/PackageTypeListService";
 import { BaseComponent } from "Infrastructure/Components/LogitudeComponents/BaseComponent";
 import { EntityArgs } from "Infrastructure/DataContracts/EntityArgs";
+import { ServiceResponse } from "Infrastructure/DataContracts/ServiceResponse";
 import { AppTool, FormatTool } from "Infrastructure/Tools";
 import { ObservableCollection } from "Infrastructure/Utilities/ObservableCollection";
 import { SessionLocator } from "Infrastructure/Utilities/SessionLocator";
@@ -8,11 +11,11 @@ import { TextCodeTranslator } from "Infrastructure/Utilities/TextCodeTranslator"
 import { Validator } from "Infrastructure/Validators/Validator";
 import { ShipmentPackagePM } from "Shipment/EntityPMs/ShipmentPackagePM";
 
-@Component({    
+@Component({
     templateUrl: './ShipmentPackagesTabComponent.html',
 })
 
-export class ShipmentPackagesTabComponent extends BaseComponent implements OnInit,OnDestroy {
+export class ShipmentPackagesTabComponent extends BaseComponent implements OnInit, OnDestroy {
     public IsReadOnly: boolean = false;
     public ItemsSource: ObservableCollection;
     public IsDisplayOnly: boolean = false;
@@ -22,7 +25,7 @@ export class ShipmentPackagesTabComponent extends BaseComponent implements OnIni
     ChangeScrollPosition: EventEmitter<any> = new EventEmitter();
     public ObjectTableName = "ShipmentPackages";
 
-    
+
     constructor(public entityArgs: EntityArgs) {
         super();
         this.ItemsSource = new ObservableCollection([]);
@@ -38,14 +41,14 @@ export class ShipmentPackagesTabComponent extends BaseComponent implements OnIni
     }
 
     DisplayOnlyCheck() {
-            if (this.EntityPM.ShipmentTypeId == "LCLD") {
-                this.IsDisplayOnly=true;
-                this.IsDisplayMessage= true;
-                this.DisplayOnlyMessage =  TextCodeTranslator.Translate("ShipmentPackage.O.DisplayOnlyMessage");
-                return;
-            }else if (this.EntityPM.ShipmentTypeId  == "FCLD"){
-                return;
-            }
+        if (this.EntityPM.ShipmentTypeId == "LCLD") {
+            this.IsDisplayOnly = true;
+            this.IsDisplayMessage = true;
+            this.DisplayOnlyMessage = TextCodeTranslator.Translate("ShipmentPackage.O.DisplayOnlyMessage");
+            return;
+        } else if (this.EntityPM.ShipmentTypeId == "FCLD") {
+            return;
+        }
 
 
     }
@@ -55,19 +58,19 @@ export class ShipmentPackagesTabComponent extends BaseComponent implements OnIni
         this.BuildItemsList();
     }
     ngOnDestroy(): void {
-        if(this.ItemsSource){
+        if (this.ItemsSource) {
             this.ItemsSource.Clear();
         }
     }
 
-    Add(){
+    Add() {
         if (this.IsDisplayOnly)
             return;
 
-        var itemPM=new ShipmentPackagePM(this.EntityPM);
+        var itemPM = new ShipmentPackagePM(this.EntityPM);
         itemPM.Tenant = this.EntityPM.Tenant;
         itemPM.ShipmentId = this.EntityPM.ShipmentId;
-        itemPM.Quantity = 1 ;
+        itemPM.Quantity = 1;
         this.EntityPM.IsCustomShipment = true;
         this.EntityPM.ShipmentPackages.push(itemPM);
         this.ItemsSource.Insert(new ShipmentPackageItemLine(itemPM, this, this.ItemsSource.Length + 1));
@@ -77,8 +80,8 @@ export class ShipmentPackagesTabComponent extends BaseComponent implements OnIni
 
     }
 
-    OnRowEnded($event){
-        if(($event) == this.ItemsSource.Length){
+    OnRowEnded($event) {
+        if (($event) == this.ItemsSource.Length) {
             this.Add();
         }
     }
@@ -88,14 +91,14 @@ export class ShipmentPackagesTabComponent extends BaseComponent implements OnIni
         if (this.ItemsSource != null && this.ItemsSource.Length > 0) {
             this.ItemsSource.Clear();
         }
-    
+
         // Check if there are packages to process
         if (this.EntityPM?.ShipmentPackages != null && this.EntityPM.ShipmentPackages.length > 0) {
             // Loop through each package and create a new item line with index
             this.EntityPM.ShipmentPackages.forEach((item, index) => {
                 // Insert the new item into the ObservableCollection with the correct index
                 // Assuming the ShipmentPackageItemLine constructor can accept an index
-                this.ItemsSource.Insert(new ShipmentPackageItemLine(item, this, index+1));
+                this.ItemsSource.Insert(new ShipmentPackageItemLine(item, this, index + 1));
             });
         }
     }
@@ -105,20 +108,20 @@ export class ShipmentPackagesTabComponent extends BaseComponent implements OnIni
             this.OnSelectedItemChanged(Item);
         }
     }
-    
-    
+
+
 }
 
 export class ShipmentPackageItemLine extends BaseComponent {
     public entityPM: ShipmentPackagePM = null;
     public DataContext = this;
     Parent: ShipmentPackagesTabComponent;
-    public index:number;
-    constructor(EntityPM: ShipmentPackagePM, parent: ShipmentPackagesTabComponent,index:number){
+    public index: number;
+    constructor(EntityPM: ShipmentPackagePM, parent: ShipmentPackagesTabComponent, index: number) {
         super();
         this.entityPM = EntityPM;
         this.Parent = parent;
-        this.index = index; 
+        this.index = index;
         this.UIProperties.SetRequired("ContainerNumber", "ShipmentPackages");
 
     }
@@ -127,11 +130,11 @@ export class ShipmentPackageItemLine extends BaseComponent {
         var error = FormatTool.ValidateContainerNumber(this.ContainerNumber);
         if (!AppTool.IsNullOrEmpty(error)) {
             this.UIProperties.SetValidity("ContainerNumber", "ShipmentPackages", false, error);
-            SessionLocator.SustainFocusOnCell = true ;
+            SessionLocator.SustainFocusOnCell = true;
             SessionLocator.SelectedSession.SessionEvent.emit({ FocusNow: true, OuterDivId: logCellTemplate.OuterDivId, LogTextBoxId: containerNumberTextBox.InputId });
-        }else{
-            this.UIProperties.SetValidity("ContainerNumber", "ShipmentPackages", true,'');
-            SessionLocator.SustainFocusOnCell = false ;
+        } else {
+            this.UIProperties.SetValidity("ContainerNumber", "ShipmentPackages", true, '');
+            SessionLocator.SustainFocusOnCell = false;
         }
     }
 
@@ -144,19 +147,42 @@ export class ShipmentPackageItemLine extends BaseComponent {
 
 
     public get ContainerNumber() { return this.entityPM.ContainerNumber; }
-    public set ContainerNumber(value:string) {
-        this.entityPM.ContainerNumber = value; 
+    public set ContainerNumber(value: string) {
+        this.entityPM.ContainerNumber = value;
     }
 
     public get PackageTypeId() { return this.entityPM.PackageTypeId; }
-    public set PackageTypeId(value:string) { this.entityPM.PackageTypeId = value; }
+    public set PackageTypeId(newValue: string) {
+        if (this.entityPM.PackageTypeId != newValue) {
+            this.entityPM.PackageTypeId = newValue;
+            if (AppTool.IsNullOrEmpty(newValue)) {
+                this.entityPM.PackageTypeName = null;
+            }
+
+            else {
+
+                var packageTypeService = new PackageTypeListService();
+                packageTypeService.getSingleFromCache(newValue).subscribe((myResponse: ServiceResponse) => {
+                    if (!myResponse.HasError) {
+                        var myPackageTypeList: PackageTypeList = myResponse.Result;
+                        if (myPackageTypeList != null) {
+                            this.PackageTypeName = myPackageTypeList.LocalName;
+                            
+                        }
+                    }
+                });
+            }
+        }
+    }
 
     public get PackageTypeName() { return this.entityPM.PackageTypeName; }
-    public set PackageTypeName(value:string) { this.entityPM.PackageTypeName = value; }
+    public set PackageTypeName(value: string) { this.entityPM.PackageTypeName = value; }
+
+
 
     public get ShipperSeal() { return this.entityPM.ShipperSeal; }
-    public set ShipperSeal(value:string) { this.entityPM.ShipperSeal = value; }
+    public set ShipperSeal(value: string) { this.entityPM.ShipperSeal = value; }
 
     public get Weight() { return this.entityPM.Weight; }
-    public set Weight(value:number) { this.entityPM.Weight = value; }
+    public set Weight(value: number) { this.entityPM.Weight = value; }
 }
