ceff324ed2e5f3bdff53809d341e288b8ae966bd
Libby Hasid <libby@amital.co.il>
2024-07-24 15:25:14 +0300

    #107541

--- Logitude/Logitude.BL/InvoiceModel/Tools/Validating/APInvoiceValidator.cs
+++ Logitude/Logitude.BL/InvoiceModel/Tools/Validating/APInvoiceValidator.cs
@@ -871,7 +871,7 @@ namespace Logitude.BL.InvoiceModel.Tools.Validating
                 bool useLocal = !(GetLoggedContact(tenant, email).DontShowLocal);
                 IInvoiceContext objectContext=new InvoiceContext();
                 var confirmationNumberDefault = (from a in objectContext.ConfirmationNumberDefaults
-                                                 where a.Tenant == tenant && a.FromDate <= invoiceDate
+                                                 where a.Tenant == tenant && a.FromDate <= invoiceDate &&a.InActive==false
                                                  orderby a.FromDate descending
                                                  select a
                                             ).FirstOrDefault();
