5ecc78efbd2a3bb78e007a9b60c6b0d997ce11a9
Libby Hasid <libby@amital.co.il>
2024-07-31 14:37:01 +0300

    #108871

--- Logitude/AngularModules/AngularModules/InvoiceModules/APPayment/Components/EditTabs/APPaymentDetailsTabComponent.ts
+++ Logitude/AngularModules/AngularModules/InvoiceModules/APPayment/Components/EditTabs/APPaymentDetailsTabComponent.ts
@@ -831,6 +831,7 @@ export class APPaymentDetailsTabComponent extends BaseComponent implements OnIni
         }
     }
     LoadPaymentInvoices(invoiceNumbers) {
+        debugger;
         var filters = new ApiQueryFilters();
         filters.PageIndex = 0;
         filters.PageSize = 1000;
@@ -838,7 +839,7 @@ export class APPaymentDetailsTabComponent extends BaseComponent implements OnIni
         filters.SortDirection = "Descending";
 
         //filters.addAdditionalFilter("VendorId", this.EntityPM.VendorId, null, null, "Equals", false, false, false, "string");
-        filters.addAdditionalFilter("InvoiceNumber", invoiceNumbers, null, null, "InList", false, true, false, "string");
+        filters.addAdditionalFilter("InvoiceNumber", invoiceNumbers, null, null, "InListExact", false, true, false, "string");
 
       
 
