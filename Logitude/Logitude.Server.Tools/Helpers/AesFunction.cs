12560f8516cb70e742dfdb12e08b75793d24ad3b
Alexander Khitrik <alexanderh@amital.co.il>
2024-08-06 14:27:14 +0300

    Alexander: #108999 ReportHeaper and AesFunction bugs

--- Logitude/Logitude.Server.Tools/Helpers/AesFunction.cs
+++ Logitude/Logitude.Server.Tools/Helpers/AesFunction.cs
@@ -42,10 +42,6 @@ namespace Logitude.Server.Tools.Helpers
                         Array.Copy(data, IV, IV.Length);
                         Array.Copy(data, IV.Length, cipherText, 0, cipherText.Length);
 
-                        // Validate ciphertext length
-                        if (cipherText.Length % aesAlg.BlockSize != 0)
-                            throw new Exception("Invalid ciphertext length. Not a multiple of block size.");
-
                         aesAlg.IV = IV;
 
                         aesAlg.Mode = CipherMode.CBC;
