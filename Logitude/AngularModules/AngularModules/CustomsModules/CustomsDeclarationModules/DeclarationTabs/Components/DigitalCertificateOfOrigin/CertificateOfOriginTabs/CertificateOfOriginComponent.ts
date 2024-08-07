70d32bd11908b2869192e7cd0170a8d7710c2ee0
Elon Hezi <Elon@amital.co.il>
2024-08-07 16:40:51 +0300

    #108955

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationTabs/Components/DigitalCertificateOfOrigin/CertificateOfOriginTabs/CertificateOfOriginComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationTabs/Components/DigitalCertificateOfOrigin/CertificateOfOriginTabs/CertificateOfOriginComponent.ts
@@ -353,6 +353,7 @@ export class CertificateOfOriginComponent extends BaseRequestsSheetMassaging {
         this.EntityPM.CertificateOriginInvoiceItems.forEach(invoice =>{
             if(AppTool.IsNullOrEmpty(invoice.DescriptionOfInvoice)){
                 invoice.DescriptionOfInvoice = !AppTool.IsNullOrEmpty(this.DecalarationData.Consignments[0].CargoDescription) ? this.DecalarationData.Consignments[0].CargoDescription : ""; 
+                invoice.DescriptionOfInvoice = AppTool.IsNullOrEmpty(invoice.DescriptionOfInvoice) ? invoice.InvoiceNumber : "";  // if not exist CargoDescription- use InvoiceNumber
             }
         })
     }
