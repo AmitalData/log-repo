84219dea9de8faa1ff1826490e932261ab06a268
Sohaib Nemer <sohaib@amital.co.il>
2024-08-11 00:36:47 +0300

    #108333

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/ListComponent/ListComponent.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/ListComponent/ListComponent.ts
@@ -729,7 +729,7 @@ export class ListComponent implements OnInit, AfterViewInit {
         }
 
         if (this.ObjectTableName != "PortTimeZone") {
-            if (!this.CheckPermissions(this.ObjectTableName, "READ", false)) {
+            if (!this.CheckPermissions(this.ObjectTableName, "READ", false)) { 
                 this.HasPermition = false;
             }
         }
