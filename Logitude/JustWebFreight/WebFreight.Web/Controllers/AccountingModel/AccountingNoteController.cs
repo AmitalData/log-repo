ad340e4902359992fd9dd5072a34175ac30f1722
elisheva Nachaysi <elisheva@amital.co.il>
2024-08-24 20:49:37 +0300

    generate accountng after vladi changes #94307

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/AccountingModel/AccountingNoteController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/AccountingModel/AccountingNoteController.cs
@@ -47,7 +47,7 @@ namespace WebFreight.Web.App_Code.AngularJS_App_Code.Generated
 { 
 
     
-    public partial class AccountingNotesController 
+    public partial class AccountingNotesController : ApiController
     {
 		
 	   public HttpResponseMessage PostDeleteNote(string noteId)
