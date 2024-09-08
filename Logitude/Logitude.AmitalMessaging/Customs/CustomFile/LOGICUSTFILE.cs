705216a277fd5cda987c53c87f40f14e1ce07e4a
Libby Hasid <libby@amital.co.il>
2024-09-08 16:04:26 +0300

    #110261

--- Logitude/Logitude.AmitalMessaging/Customs/CustomFile/LOGICUSTFILE.cs
+++ Logitude/Logitude.AmitalMessaging/Customs/CustomFile/LOGICUSTFILE.cs
@@ -207,6 +207,8 @@ public partial class LogitudeCustomsFile
 
     private ExportClosing closingField;
 
+
+
     private string receiverNameField;
     private string receiverAddressField;
     private string receiverCountryCodeField;
@@ -727,6 +729,8 @@ public partial class LogitudeCustomsFile
 
     public string StorageSiteCode { get; set; }
     /// <remarks/>
+    /// 
+    public string DeliverySiteCode { get; set; }
     public string UnloadDate
     {
         get
@@ -1258,6 +1262,8 @@ public partial class LogitudeCustomsFile
 			this.closingField = value;
 		}
 	}
+
+   
 }
 
 
@@ -1460,6 +1466,7 @@ public class ExportInvoiceItem
     private string classificationDealTypeField;
     private string processTypeField;
     private ExportInvoiceItemCertificats certificatsField;
+    private ConnectedDeclarations[] connectedDeclarationsField;
 
     public string ItemNo
     {
@@ -1583,6 +1590,17 @@ public class ExportInvoiceItem
         set
         {
             this.certificatsField = value;
+        }
+    }
+    public ConnectedDeclarations[] ConnectedDeclarations
+    {
+        get
+        {
+            return this.connectedDeclarationsField;
+        }
+        set
+        {
+            this.connectedDeclarationsField = value;
         }
     }
 }
@@ -1787,6 +1805,98 @@ public class ExportClosing
 			this.freightAmountField = value;
 		}
 	}
+
+}
+
+[System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.8.3928.0")]
+[System.SerializableAttribute()]
+[System.Diagnostics.DebuggerStepThroughAttribute()]
+[System.ComponentModel.DesignerCategoryAttribute("code")]
+[System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://tempuri.org/LOGICUSTFILE")]
+[System.Xml.Serialization.XmlRootAttribute(Namespace = "http://tempuri.org/LOGICUSTFILE", IsNullable = false)]
+public class ConnectedDeclarations
+{
+    [System.Xml.Serialization.XmlElementAttribute("ConnectedDeclarations")]
+
+    private string declarationType;
+
+    private string declarationNo;
+
+    private string invoiceLine;
+
+    private string itemLine;
+
+    private string quantity;
+
+    private string quantityType;
+
+    public string DeclarationType
+    {
+        get
+        {
+            return this.declarationType;
+        }
+        set
+        {
+            this.declarationType = value;
+        }
+    }
+    public string DeclarationNo
+    {
+        get
+        {
+            return this.declarationNo;
+        }
+        set
+        {
+            this.declarationNo = value;
+        }
+    }
+
+    public string InvoiceLine
+    {
+        get
+        {
+            return this.invoiceLine;
+        }
+        set
+        {
+            this.invoiceLine = value;
+        }
+    }
+    public string ItemLine
+    {
+        get
+        {
+            return this.itemLine;
+        }
+        set
+        {
+            this.itemLine = value;
+        }
+    }
+    public string Quantity
+    {
+        get
+        {
+            return this.quantity;
+        }
+        set
+        {
+            this.quantity = value;
+        }
+    }
+    public string QuantityType
+    {
+        get
+        {
+            return this.quantityType;
+        }
+        set
+        {
+            this.quantityType = value;
+        }
+    }
 
 }
 
