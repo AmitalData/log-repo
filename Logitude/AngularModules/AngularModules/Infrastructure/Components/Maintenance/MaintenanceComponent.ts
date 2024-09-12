a210a6906747ee3029259ea96d09087341445877
izik89 <izikfr@amital.co.il>
2024-09-12 17:28:24 +0300

    #109484

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/Maintenance/MaintenanceComponent.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/Maintenance/MaintenanceComponent.ts
@@ -1766,7 +1766,7 @@ export class MaintenanceComponent {
                 case "MTHT": {
                     var windowTitle = "Hybrid Tenant State";
                     var logWindow = new LogitudeWindow();
-                    logWindow.Width = 1100;
+                    logWindow.Width = 1310;
                     logWindow.Height = 550;
                     logWindow.Title = windowTitle;
                     logWindow.Show('./InfrastructureModules/InfrastructureHybrid/Components/HybridTenantState/HybridTenantStateComponent');
