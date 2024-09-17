857add33bd163e187fd2183f4e81f400abacea35
elisheva Nachaysi <elisheva@amital.co.il>
2024-09-17 22:55:28 +0300

    #110562

--- Logitude/Logitude.Accounting.BL/CoreBL/Reports/AgingReportService.cs
+++ Logitude/Logitude.Accounting.BL/CoreBL/Reports/AgingReportService.cs
@@ -643,16 +643,29 @@ namespace Logitude.Accounting.BL.CoreBL.Reports
             }
 
             //_notMultiCurrencyAccountListId = _GLAccounts.Where(rec => !rec.IsMultiCurrency.GetValueOrDefault()).Select(rec => rec.Id).ToList();
-            var _AccountListId_PleaseTake_ForeignAmountList = _AccountListId_PleaseTake_ForeignAmount.ToList();
+            //var _AccountListId_PleaseTake_ForeignAmountList = _AccountListId_PleaseTake_ForeignAmount.ToList();
 
+            //var qTotalByMonthAcc_NOTMultiCurrencySooGet_ForeignAmount = ///GLAccount that is not multi Currency Get Foreign 
+            //        qTotalByMonthAcc
+            //    .Where(rec => _AccountListId_PleaseTake_ForeignAmountList.Contains(rec.AccountId));
+
+
+            //var qTotalByMonthAcc_ISMultiCurrencySooGet_LocalAmount = ///GLAccount that is not multi Currency Get Foreign 
+            //        qTotalByMonthAcc
+            //    .Where(rec => _AccountListId_PleaseTake_ForeignAmountList.Contains(rec.AccountId));
+
+
+            //_notMultiCurrencyAccountListId = _GLAccounts.Where(rec => !rec.IsMultiCurrency.GetValueOrDefault()).Select(rec => rec.Id).ToList();
             var qTotalByMonthAcc_NOTMultiCurrencySooGet_ForeignAmount = ///GLAccount that is not multi Currency Get Foreign 
                     qTotalByMonthAcc
-                .Where(rec => _AccountListId_PleaseTake_ForeignAmountList.Contains(rec.AccountId));
+                .Where(rec => _AccountListId_PleaseTake_ForeignAmount.Contains(rec.AccountId));
 
 
             var qTotalByMonthAcc_ISMultiCurrencySooGet_LocalAmount = ///GLAccount that is not multi Currency Get Foreign 
                     qTotalByMonthAcc
-                .Where(rec => _AccountListId_PleaseTake_ForeignAmountList.Contains(rec.AccountId));
+                .Where(rec => !_AccountListId_PleaseTake_ForeignAmount.Contains(rec.AccountId));
+
+
 
 
 
