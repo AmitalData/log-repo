5c623498c82a44495960f77d8c52418aca583fa5
Sohaib Nemer <sohaib@amital.co.il>
2024-09-18 16:27:55 +0300

    #110373

--- Logitude/Logitude.CustomsMessaging/U2L/Sivug/SivugUpsertService.cs
+++ Logitude/Logitude.CustomsMessaging/U2L/Sivug/SivugUpsertService.cs
@@ -891,7 +891,7 @@ namespace Logitude.CustomsMessaging.U2L.Sivug
                             SupplierInvoiceItemPM.ClassificationCode =invoiceItem.CLASSIFICATIONCODE;
                             SupplierInvoiceItemPM.ChangeSetOp = ChangeSetOperation.Update;
                         }
-                        if ((invoiceItem.ITEMORIGINCOUNTRY ?? "") != (SupplierInvoiceItemPM.OriginCountryCode ?? ""))
+                        if (!string.IsNullOrEmpty(invoiceItem.ITEMORIGINCOUNTRY) && invoiceItem.ITEMORIGINCOUNTRY != (SupplierInvoiceItemPM.OriginCountryCode ?? ""))
                         {
                             SupplierInvoiceItemPM.OriginCountryCode = invoiceItem.ITEMORIGINCOUNTRY;
                             SupplierInvoiceItemPM.ChangeSetOp = ChangeSetOperation.Update;
@@ -919,6 +919,10 @@ namespace Logitude.CustomsMessaging.U2L.Sivug
                                 SupplierInvoiceItemPM.ChangeSetOp = ChangeSetOperation.Update;
                             }
                         }
+                        if (!string.IsNullOrEmpty(invoiceItem.Desc) && invoiceItem.Desc != (SupplierInvoiceItemPM.ItemDescription ?? ""))
+                        {
+                            SupplierInvoiceItemPM.ItemDescription = invoiceItem.Desc;
+                        }
                     }
                 }
             }
@@ -932,7 +936,7 @@ namespace Logitude.CustomsMessaging.U2L.Sivug
             Dictionary<string, string> ClasificationQtyTypes = new Dictionary<string, string>() { };
             CustomsItemQueryService customsItemQueryService = new CustomsItemQueryService(ResolvedTenant());
 			
-			foreach (var invoiceItem in invoice.INVOICEITEMS)
+			foreach (INVOICEITEMS invoiceItem in invoice.INVOICEITEMS)
             {
                 int int1 = 0;
                 decimal decimal1 = 0;
