0ca6c5d2398f1cec5e114a8e6cd429bd090d58d3
Sohaib Nemer <sohaib@amital.co.il>
2024-09-26 13:54:29 +0300

    #110381

---
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/HybridModel/FreelancerGroupTypeHybridController.cs
@@ -0,0 +1,36 @@
+﻿using Logitude.BL.CommonDataModel.EntityPMs;
+using Logitude.Server.Tools;
+using Newtonsoft.Json;
+using Simplog.Data.CommonDataModel.EntityPOCOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebFreight.Web.DataContracts;
+using WebFreight.Web.WcfApi;
+
+namespace WebFreight.Web.Controllers.HybridModel
+{
+    public class FreelancerGroupTypeHybridController : ApiController
+    {
+        [System.Web.Http.HttpPost]
+        public Response Upsert([FromBody] object[] t)//(UserPM entityPM, bool batch)
+        {
+            var jsonSerializerSettings = new JsonSerializerSettings();
+            jsonSerializerSettings.MissingMemberHandling = MissingMemberHandling.Ignore;
+
+
+            FreelancerGroupTypePM freelancerGroupTypePM = JsonConvert.DeserializeObject<FreelancerGroupTypePM>(JsonConvert.SerializeObject(t[0]), jsonSerializerSettings);
+            bool batch = JsonConvert.DeserializeObject<bool>(JsonConvert.SerializeObject(t[1]), jsonSerializerSettings);
+
+
+            FreelancerGroupTypeWcfService freelancerGroupTypeWcfService = new FreelancerGroupTypeWcfService();
+            Response response = freelancerGroupTypeWcfService.Upsert(freelancerGroupTypePM, batch);
+            return response;
+        }
+
+
+    }
+}
