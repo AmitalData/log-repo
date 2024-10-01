bcabcd9036e4ed7bc5bb200bb3f2a2f9e7246cbd
Alexander Khitrik <alexanderh@amital.co.il>
2024-10-01 14:07:43 +0300

    Alexander: #110823 Tax Report Cancel

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/AccountingModel/TaxReportOpController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/AccountingModel/TaxReportOpController.cs
@@ -422,6 +422,32 @@ namespace WebFreight.Web.Controllers.AccountingModel
             }
 
         }
+
+
+        public HttpResponseMessage GetActiveFutureReportsExist(DateTime createDate)
+        {
+            try
+            {
+                string logKey = PerformanceLogger.LogCurrentTime();
+                AuthenticationToken authToken = GetAuthenticationToken();
+                SecurityUtility.CheckContactFeature("TaxReport", "READ", authToken.Tenant);
+                int tenant = authToken.Tenant;
+                bool FutureReportExist = CheckIfActiveFutureReportsExist(createDate, tenant);
+                ServiceResponse response = new ServiceResponse();
+                response.Result = FutureReportExist;
+                HttpResponseMessage reponseMessage = Request.CreateResponse(HttpStatusCode.OK, response);
+                PerformanceLogger.AddServerExecutionTimeHeader(logKey);
+
+                return reponseMessage;
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ApiExceptionBuilder.BuildException(ex));
+            }
+
+        }
+
+
         private bool CheckIfActiveFutureReportsExist(DateTime createDate, int tenant)
         {
             IAccountingContext MyContext = AccountingContext.GetContext(tenant);
