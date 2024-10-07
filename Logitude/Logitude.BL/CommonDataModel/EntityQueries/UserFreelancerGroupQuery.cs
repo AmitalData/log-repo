8414e244b712ba411ffa1da84746ce6d3b971df9
Sohaib Nemer <sohaib@amital.co.il>
2024-10-07 15:17:31 +0300

    #110381

--- Logitude/Logitude.BL/CommonDataModel/EntityQueries/UserFreelancerGroupQuery.cs
+++ Logitude/Logitude.BL/CommonDataModel/EntityQueries/UserFreelancerGroupQuery.cs
@@ -72,6 +72,19 @@ namespace Logitude.BL.CommonDataModel.EntityQueries
                                                      };
             return UserFreelancerGroup;
         }
+        public IQueryable<UserFreelancerGroupPM> GetUserFreelancerGroupsByUserId(string userId, int tenant)
+        {
+            IQueryable<UserFreelancerGroupPM> UserFreelancerGroup = from a in repository.context.UserFreelancerGroups
+                                                     where a.UserId == userId && a.Tenant == tenant
+                                                     select new UserFreelancerGroupPM()
+                                                     {
+                                                         Id = a.Id,
+                                                         Tenant = a.Tenant,
+                                                         GroupID = a.GroupID,
+                                                         UserId = a.UserId,
+                                                     };
+            return UserFreelancerGroup;
+        }
 
     }
 }
