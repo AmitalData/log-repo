1e1cbf1d25f66ca34aea1f923efc3cc5a7cfa698
Elon Hezi <Elon@amital.co.il>
2024-10-08 12:38:16 +0300

    #109207

--- Logitude/AngularModules/CustomsBookModules/src/app/shared/components/accordion/accordion.component.ts
+++ Logitude/AngularModules/CustomsBookModules/src/app/shared/components/accordion/accordion.component.ts
@@ -42,7 +42,7 @@ export class AccordionComponent implements OnInit {
   listenToChanges() {
     this.currentItem.subscribe((data: CB_CustomsItemComputedDataList) => {
       this.customsItemId = data?.CustomsItemID;
-      if(this.customsItemId)
+      if (this.customsItemId)
         this.InitData();
     });
   }
@@ -88,26 +88,36 @@ export class AccordionComponent implements OnInit {
       data: []
     };
 
-    
-    if(!this.currentItem.getValue().PH_MeasurementUnitID) return;
-    // שיעורי מס
-    this.API_MainService.GetCustomsBookAgreementLevelData(this.customsItemId, this.currentItem.getValue().PH_MeasurementUnitID).subscribe((data: any) => {
-      const result: CB_TariffList[] = data.body;
-      if (!result) return; // TODO: add error message
-
-      this.MainEntity.CB_TariffList = result;
-      this.tableData1.data = this.MainEntity.CB_TariffList.filter(x => x.TradeAgreementName != 'מס קניה');
-      this.tableData2.data = this.MainEntity.CB_TariffList.filter(x => x.TradeAgreementName == 'מס קניה');
-    });
-
     // דרישות חוקיות
-    this.API_MainService.GetCustomsBookRegularityRequirementData(this.customsItemId).subscribe((data: any) => {
-      const result: CB_RequirementComputedDataList[] = data.body;
-      if (!result) return; // TODO: add error message
+    this.API_MainService.GetCustomsBookRegularityRequirementData(this.customsItemId).subscribe(
+      (data: any) => {
+        const result: CB_RequirementComputedDataList[] = data.body;
+        if (!result) return; 
 
-      this.MainEntity.CB_RequirementComputedDataList = result;
-      this.tableData3.data = this.MainEntity.CB_RequirementComputedDataList
-    });
+        this.MainEntity.CB_RequirementComputedDataList = result;
+        this.tableData3.data = this.MainEntity.CB_RequirementComputedDataList;
+      },
+      (error) => {
+        console.log(error.message);
+      }
+    );
+
+    // שיעורי מס
+    if (!this.currentItem.getValue().PH_MeasurementUnitID) return;
+    this.API_MainService.GetCustomsBookAgreementLevelData(this.customsItemId, this.currentItem.getValue().PH_MeasurementUnitID).subscribe(
+      (data: any) => {
+        const result: CB_TariffList[] = data.body;
+        if (!result) return; 
+
+        this.MainEntity.CB_TariffList = result;
+        this.tableData1.data = this.MainEntity.CB_TariffList.filter(x => x.TradeAgreementName != 'מס קניה');
+        this.tableData2.data = this.MainEntity.CB_TariffList.filter(x => x.TradeAgreementName == 'מס קניה');
+      },
+      (error) => {
+        console.log(error.message);
+      }
+    );
+
   }
 
 }
