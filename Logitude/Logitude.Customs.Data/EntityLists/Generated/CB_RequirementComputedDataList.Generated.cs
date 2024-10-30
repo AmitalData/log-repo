a4c24fe2d6dc36b99c49e878e32e93d1aba7423c
Elon Hezi <Elon@amital.co.il>
2024-10-30 10:45:10 +0200

    #111449

--- Logitude/Logitude.Customs.Data/EntityLists/Generated/CB_RequirementComputedDataList.Generated.cs
+++ Logitude/Logitude.Customs.Data/EntityLists/Generated/CB_RequirementComputedDataList.Generated.cs
@@ -60,6 +60,8 @@ namespace Logitude.Customs.Data.EntityLists
        public string FromEpisodeDetail  { get; set; }
        [DataMember]
        public string AutonomyRegion  { get; set; }
+       [DataMember]
+       public bool IsVoluntaryOrImporterOfTrust  { get; set; }
    }
 
 }
