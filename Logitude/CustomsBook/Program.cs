68c58794e6fe338cd745eaa7e73ba37e41036a01
izik89 <izikfr@amital.co.il>
2024-10-31 12:25:50 +0200

    #111354 #106554

---
+++ Logitude/CustomsBook/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using NLog;
+
+namespace CustomsBook
+{
+    internal class Program
+    {
+        public static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        static async Task Main(string[] args)
+        {
+            LogManager.Configuration = new NLog.Config.XmlLoggingConfiguration(Path.Combine(AppDomain.CurrentDomain.BaseDirectory , "NLog.config"));
+            
+            logger.Debug("Start TaskScheduler");
+
+            await UpdateCustomsBook.Run();
+
+            await UpdateAzureSearchAIData.Update();
+
+            //Console.ReadLine();
+        }
+    }
+}
