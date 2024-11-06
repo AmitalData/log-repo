6a53a816ed7fc2ecda26f30ed28131579384e104
Libby Hasid <libby@amital.co.il>
2024-11-06 15:36:59 +0200

    #111465

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/LogGridComponent/VirtualRowControllerV2.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/LogGridComponent/VirtualRowControllerV2.ts
@@ -188,9 +188,12 @@ export class VirtualRowControllerV2 extends DataSource<any | undefined> implemen
                     }
                     this.cachedData = this.mycachedData;//[...this.mycachedData]
                     this.dataStream.next(this.cachedData);
+                    this.requestedRowCount.emit(viewResponse.Count);  
+
                     if (this.myMetaData.cd) {
                         this.myMetaData.cd.detectChanges();
                     }
+                 
                 }
                
             });
