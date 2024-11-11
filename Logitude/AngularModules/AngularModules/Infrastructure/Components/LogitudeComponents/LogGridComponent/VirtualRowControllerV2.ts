085110ebbb09e99b2cb110825a83032f0e8393c0
Libby Hasid <libby@amital.co.il>
2024-11-11 14:32:15 +0200

    #112043 ,#112042

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/LogGridComponent/VirtualRowControllerV2.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/LogGridComponent/VirtualRowControllerV2.ts
@@ -160,7 +160,7 @@ export class VirtualRowControllerV2 extends DataSource<any | undefined> implemen
         this.fetchedPages.add(page);
         //this.mycachedData = [];
         //this.pageSize = 17;
-        this.dataSource.getRows(page * this.pageSize, this.pageSize, this.myMetaData.sortingCol, this.myMetaData.sortingDir, false, this.myMetaData.searchFields, this.myMetaData.Filters).then(res => {
+        this.dataSource.getRows(page * this.pageSize, this.pageSize, this.myMetaData.sortingCol, this.myMetaData.sortingDir, true, this.myMetaData.searchFields, this.myMetaData.Filters).then(res => {
             res.subscribe((viewResponse: ServiceResponse) => {
                 if (!viewResponse.HasError) {
                     //if (this.MyCallTime == null || viewResponse.CallTime > this.MyCallTime) {
