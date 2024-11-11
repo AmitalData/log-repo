4583743e665f217f0bd1f18e40e53ade090750d3
Sraya Zeevi <Sraya@amital.co.il>
2024-11-11 15:11:58 +0200

    #111034

--- Logitude/AngularModules/CypressTesting/FullAccounting/cypress/selectors/ExcelDownloadSelectors.ts
+++ Logitude/AngularModules/CypressTesting/FullAccounting/cypress/selectors/ExcelDownloadSelectors.ts
@@ -4,7 +4,7 @@ export class ExcelDownloadSelectors {
     public static readonly AllJournal = '.QueryLink:eq(3)';
     //public static readonly Excelicon = "img[src='/Images/Buttons/excelicon.png']"
 
-    public static readonly Excelicon = '.CenterCenter:eq(2)'
+    public static readonly Excelicon = '[data-cy="ExportJournal"]' ;//'.CenterCenter:eq(2)'
 
  }
 
