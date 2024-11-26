0b32c28ed93f8483e96e761ec55279e0e806122f
Libby Hasid <libby@amital.co.il>
2024-11-26 11:05:00 +0200

    #112258

--- Logitude/AngularModules/AngularModules/Report/ModuleDeclarations.ts
+++ Logitude/AngularModules/AngularModules/Report/ModuleDeclarations.ts
@@ -189,7 +189,6 @@ export const Components =
         SpotRateQuoteReportFilterComponent,
         ControlInvoiceLinesReportFilterComponent,
         ExportDeclarationReportFilterComponent,
-
         MonthlyBalancesReportFilterComponent
 
     ];
