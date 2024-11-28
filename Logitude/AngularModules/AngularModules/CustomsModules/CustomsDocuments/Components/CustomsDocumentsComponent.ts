2e57dd8eb2a880c3eee9cf75b957b46e148c07cf
Elon Hezi <Elon@amital.co.il>
2024-11-28 08:35:25 +0200

    #111815

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDocuments/Components/CustomsDocumentsComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDocuments/Components/CustomsDocumentsComponent.ts
@@ -641,8 +641,7 @@ export class CustomsDocumentsComponent
                         this.CurrentSession.StopBusyIndicator();
                         if (!resp.HasError) {
                             var customsDoc = resp.Result;
-                            if(CustomsDocumentTicketViewModel.IsOcrDocument){
-
+                            if(CustomsDocumentTicketViewModel.IsOcrDocument && this.EntityPM.DeclarationStatusTypeCode != "45"){
                                 this.UpsertSupplierInvioceByOcr(customsDoc.DocumentsFilingId, isThereRequests, customsDocumentsTicket.customsDocumentsTicketPM, customsDoc);
                                 CustomsDocumentTicketViewModel.IsOcrDocument = false;
                             }
