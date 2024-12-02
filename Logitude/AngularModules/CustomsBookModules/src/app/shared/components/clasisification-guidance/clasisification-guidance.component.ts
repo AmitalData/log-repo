e0e0295f9839f84ac7c502087d4ebdc852f8a2b7
Elon Hezi <Elon@amital.co.il>
2024-12-02 16:35:06 +0200

    #112129

--- Logitude/AngularModules/CustomsBookModules/src/app/shared/components/clasisification-guidance/clasisification-guidance.component.ts
+++ Logitude/AngularModules/CustomsBookModules/src/app/shared/components/clasisification-guidance/clasisification-guidance.component.ts
@@ -4,11 +4,12 @@ import { BehaviorSubject } from 'rxjs';
 import { API_MainService } from '../../../core/API_MainService';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { CommonModule, DatePipe, NgIf } from '@angular/common';
+import { SessionInfo } from '../../../core/Infrastructure/Utilities/SessionInfo';
 
 @Component({
   selector: 'app-clasisification-guidance',
   standalone: true,
-  imports: [GenericTableComponent,CommonModule,NgIf],
+  imports: [GenericTableComponent, CommonModule, NgIf],
   templateUrl: './clasisification-guidance.component.html',
   styleUrl: './clasisification-guidance.component.css',
   providers: [DatePipe]
@@ -21,44 +22,41 @@ export class ClasisificationGuidanceComponent implements OnInit {
   tableData: TableData;
 
   ngOnInit(): void {
-    this.buildTable();
     this.listenToChanges();
   }
-  constructor(private api_MainService: API_MainService, private sanitizer: DomSanitizer,private datePipe: DatePipe) { }
+  constructor(private api_MainService: API_MainService, private sanitizer: DomSanitizer, private datePipe: DatePipe) { }
   sanitizeHTML(content: string): SafeHtml {
     return this.sanitizer.bypassSecurityTrustHtml(content) ? this.sanitizer.bypassSecurityTrustHtml(content) : '';
   }
-  
 
   listenToChanges() {
+    let tenant: number = SessionInfo.LoggedUserTenant;
+    if (tenant != 0) this.GetClassifGuidanceFullDetails(tenant);
+    else {
+      this.api_MainService.GetTenantFromCustomsSettings().subscribe(
+        (data: any) => {
+          tenant = data?.body;
+          this.GetClassifGuidanceFullDetails(tenant);
+        },
+        (error) => {
+          console.log(error.message);
+        }
+      );
+    }
+  }
+
+  GetClassifGuidanceFullDetails(tenant: number) {
     this.ClassificationGuidanceId.subscribe((data: any) => {
-      this.data=null;
+      this.data = null;
       if (data) {
-        this.api_MainService.GetClassifGuidanceDetails(this.ClassificationGuidanceId.getValue().toString(), 6).subscribe((data: any) => {
+        this.api_MainService.GetClassifGuidanceDetails(this.ClassificationGuidanceId.getValue().toString(), tenant).subscribe((data: any) => {
           if (!data.body) return; // TODO: add error message
           this.data = data.body;
           console.log(data.body);
-          
+
           this.tableData.data = data.body;
         });
       }
     });
   }
-  buildTable() {
-    this.tableData = {
-      columns: [
-        { key: 'classificationGuidanceNumber', displayName: 'מספר הנחיה', dataType: 'string', visible: true },
-        { key: 'title', displayName: 'שם הנחיה', dataType: 'string', visible: true },
-        { key: 'classificationGuidanceTypeName', displayName: 'סוג הנחיה', dataType: 'string', visible: true },
-        { key: 'fullClassification', displayName: 'חלק/פרק/פרט מכס', dataType: 'string', visible: true },
-        { key: 'openDate', displayName: 'תאריך פתיחה', dataType: 'date', visible: true },
-        { key: 'endDate', displayName: 'תאריך סיום תוקף', dataType: 'date', visible: true },
-        { key: 'publicationDate', displayName: 'תאריך פרסום', dataType: 'date', visible: true },
-        { key: 'relatedGuidance', displayName: 'הנחיה זו מתייחסת', dataType: 'string', visible: true },
-        { key: 'description', displayName: 'תאור הנחיה', dataType: 'string', visible: true },
-      ],
-      data: []
-    };
-  }
-
 }
--- Logitude/AngularModules/CustomsBookModules/src/app/shared/components/rules/rules.component.ts
+++ Logitude/AngularModules/CustomsBookModules/src/app/shared/components/rules/rules.component.ts
@@ -53,6 +53,7 @@ export class RulesComponent implements OnInit, OnChanges {
   // Method to fetch rules data from the API and build the rules hierarchy
   initData(customsItemID: number) {
     this.resetRulesData();
+    this.showRules = false;
     this.API_MainService.GetCustomsBookRulesData(customsItemID).subscribe((data: any) => {
       if (!data.body) return; // TODO: add error message
       // Clean up spaces by replacing multiple &nbsp; with a single space, then condense extra spaces
@@ -60,8 +61,9 @@ export class RulesComponent implements OnInit, OnChanges {
       rules.forEach(rule => {
         rule.Rules = rule.Rules.replace(/(&nbsp;)+/g, ' ').replace(/\s+/g, ' ').trim();
       });
-
+      
       this.allRules = this.buildRulesHierarchy(rules);
+      this.allRules.length > 0 ? this.showRules = true : this.showRules = false;
     });
   }
 
