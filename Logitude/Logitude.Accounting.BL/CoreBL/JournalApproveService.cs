b8fdb94f8c556b2934ab66782741c60dce4f2cc0
Libby Hasid <libby@amital.co.il>
2024-12-02 11:21:36 +0200

    #112596

--- Logitude/Logitude.Accounting.BL/CoreBL/JournalApproveService.cs
+++ Logitude/Logitude.Accounting.BL/CoreBL/JournalApproveService.cs
@@ -563,6 +563,12 @@ namespace Logitude.Accounting.BL.CoreBL
                     scope.Complete();
 
                 }
+                catch (Exception e)
+                {
+                    NetCommonHelper.Logger.DevLog.Instance.WriteError("AccountingStreamingInNewSerializableTransaction! _JournalPM?.Id" + _JournalPM?.Id + " Err:" + e );
+
+
+                }
                 finally
                 {
                     //logger.ToString();
@@ -576,6 +582,8 @@ namespace Logitude.Accounting.BL.CoreBL
 
                     LogMessagingUtil.Instance.AppendLine("AccountingStreamingInNewSerializableTransaction:Took:" + sw.Elapsed.ToString());
                 }
+
+                
             }
         }
         public void CreateInterestTransactionsByDate(DateTime date)
