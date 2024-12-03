0ee081e62db585111d52c5f138f951c1d4e14082
elisheva Nachaysi <elisheva@amital.co.il>
2024-12-03 11:38:00 +0000

    Revert "Merged PR 3556: #112884
    #112884
    
    Related work items: #112884"

--- Logitude/Logitude.Customs.Data/EntityMapping/DeclarationCourierStatusMap.cs
+++ Logitude/Logitude.Customs.Data/EntityMapping/DeclarationCourierStatusMap.cs
@@ -75,7 +75,7 @@ namespace Logitude.Customs.Data.EntityMapping
 
             this.Property(t => t.DistributionArea).HasColumnName("DistributionArea").HasMaxLength(30).IsUnicode(true);
 
-            this.Property(t => t.CrateNumber).HasColumnName("CrateNumber").HasMaxLength(50).IsUnicode(false);
+            this.Property(t => t.CrateNumber).HasColumnName("CrateNumber").HasMaxLength(30).IsUnicode(false);
 
             this.Property(t => t.TerminalReleaseDate).HasColumnName("TerminalReleaseDate");
 
