18bb7be71089d8cb4c6a2ec31c418f60a4017796
Dror Granot <Dror@amital.co.il>
2024-12-04 12:41:11 +0200

    UUUUUDDDAATTEE

--- Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_2470_DF_MSG16001_ReleaseGoodsMessageResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_2470_DF_MSG16001_ReleaseGoodsMessageResponseService.cs
@@ -108,30 +108,30 @@ namespace Logitude.CustomsMessaging.ResponseServices
 
                     if (declarationPM.Direction == "E")
                     {
-                        if (declarationPM.PaymentDate == null)
-                        {
-                            var messagingService = new DF_NG_8373_Web05_RetrieveImportDeclarationMessagingService();
-                            DeclarationRestoreRequestParams = new DeclarationRestoreRequestParams()
-                            {
-                                DeclarationNumber = declarationPM.DeclarationNumber,
-                                CustomFileNo = declarationPM.CustomFileNo,
-                                Tenant = declarationPM.Tenant,
-                                RequestName = "Restore Declaration " + declarationPM.DeclarationNumber,
-                                ResponseName = "Restore Declaration " + declarationPM.DeclarationNumber,
-                                RequestVIA = SendRequestVIA.WebServiceBatch,
-                                InterfaceTypeCode = "8373",
-                                LoggingEntityId = declarationPM.Id,
-                                LoggingObjectTableId = ObjectTableRepository.GetObjectTableByName("Customs.Declaration"),
-                                LoggingUserId = AuthenticationUtil.ResolveUserId(declarationPM.Tenant),
-                            };
-                            responseData = messagingService.Send(requestParams);
-                        }
-
                         declarationPM.ReleaseStatusTypeCode = customResponse.GeneralData?.ReleaseMessageCode.ToString();
                         if (declarationPM.IsSubmitDeclaration == false || declarationPM.IsSubmitDeclaration == null)
                             declarationPM.IsSubmitDeclaration = true;
                     }
 
+                    if (declarationPM.PaymentDate == null && declarationPM.Direction != "E")
+                    {
+
+                        var messagingService = new DF_NG_8373_Web05_RetrieveImportDeclarationMessagingService();
+                        DeclarationRestoreRequestParams =  new DeclarationRestoreRequestParams()
+                        {
+                            DeclarationNumber = declarationPM.DeclarationNumber,
+                            CustomFileNo = declarationPM.CustomFileNo,
+                            Tenant = declarationPM.Tenant,
+                            RequestName = "Restore Declaration " + declarationPM.DeclarationNumber,
+                            ResponseName = "Restore Declaration " + declarationPM.DeclarationNumber,
+                            RequestVIA = SendRequestVIA.WebServiceBatch,
+                            InterfaceTypeCode = "8373",
+                            LoggingEntityId = declarationPM.Id,
+                            LoggingObjectTableId = ObjectTableRepository.GetObjectTableByName("Customs.Declaration"),
+                            LoggingUserId = AuthenticationUtil.ResolveUserId(declarationPM.Tenant),
+                        };
+                        responseData = messagingService.Send(requestParams);
+                    }
 
                     var myCourierMasterQueryService = new CourierMasterQueryService(dbContext);
                     CourierMasterPM _CourierMasterPM = myCourierMasterQueryService.GetByDeclarationId(declarationPM.Id, requestParams.Tenant);
