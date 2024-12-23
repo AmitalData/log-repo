d384ffa35bbdfa367c625072b869e524fc5c5531
Libby Hasid <libby@amital.co.il>
2024-12-23 15:52:54 +0200

    #112986

--- Logitude/AngularModules/AngularModules/Accounting/Components/Others/ReconcileComponent.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/Others/ReconcileComponent.ts
@@ -1186,7 +1186,7 @@ export class ReconcileComponent extends BaseComponent implements OnInit, OnDestr
     }
 
     CancelButtonClicked() {
-        if(this.isMark)
+        if(!this.isMark)
            this.UndoMark();
         this.CurrentSession.CloseCurrentWindow();
     }
