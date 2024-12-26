b9add3cbdcc1e3ea640cf43384935988343656b5
Jeremy Cohen <Jeremy@amital.co.il>
2024-12-26 11:30:20 +0200

    #112417

--- Logitude/Logitude.BL/ShipmentsModel/Tools/EntityService/ShipmentService.cs
+++ Logitude/Logitude.BL/ShipmentsModel/Tools/EntityService/ShipmentService.cs
@@ -1189,7 +1189,7 @@ namespace Logitude.BL.ShipmentsModel.Tools.EntityService
             {
                 List<ShipmentValidating.MessageDetails> messageDetailsList = new List<ShipmentValidating.MessageDetails>();
 
-                using (TransactionScope scope = TransactionFactory.GetNewReadUncommittedTransaction())
+                using (TransactionScope scope = TransactionFactory.GetTransaction())
                 {
                     if (entityPM.CustomerId != entityPoco.CustomerId)
                     {
