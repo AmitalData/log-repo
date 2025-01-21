bcb324b93f6d56c7c21e99f261ee84edcb032b90
Alexander Khitrik <alexanderh@amital.co.il>
2025-01-21 12:36:53 +0200

    Alexander: #114714 CancelClosingJournal reco bugs

--- Logitude/Logitude.Accounting.BL/CoreBL/TaxReportClosingService.cs
+++ Logitude/Logitude.Accounting.BL/CoreBL/TaxReportClosingService.cs
@@ -19,6 +19,7 @@ using System.Threading.Tasks;
 using Logitude.Accounting.Def.EntityUpdateServicesExt;
 using System.Transactions;
 using Simplog.Server.Infrastructure.Helpers;
+using System.Xml.Linq;
 
 namespace Logitude.Accounting.BL.CoreBL
 {
@@ -76,34 +77,99 @@ namespace Logitude.Accounting.BL.CoreBL
         }
         private void EnsureAbilityToCreateClosingJournal()
         {
+            bool skip_other_checks = false;
+            int reco_cancelled = 0;
             TaxReportQueryService taxReportQueryService = new TaxReportQueryService(tenant);
             List<TaxReportLineForErrors> reconciledLines = null;
             var canHaveClosingJournal = taxReportQueryService.CheckIfTaxReportCanHaveClosingJournal(taxReportId, fullAccountingSettings.VATOutputGLAccountId, tenant, ref reconciledLines);
             if (!canHaveClosingJournal)
             {
-                string error_text = TranslateTextsClass.Translate("TaxReport.O.ClosingJournalValidationMessage", tenant);
                 if (reconciledLines != null && reconciledLines.Count > 0)
                 {
-                    reconciledLines = reconciledLines.OrderBy(rl => rl.JournalNumber).ThenBy(rl => rl.Line).ToList();
-                    const int MAX = 5;
-                    if (reconciledLines.Count > MAX)
+
+                    int recocount = 0;
+                    //var taxrepnum = taxReportPM.TaxReportNumber;
+                    JournalQueryService journalQueryService = new JournalQueryService(tenant);
+                    JournalPM journalPM = journalQueryService.GetByAccountingEntityIdAndAccountingEntityCode(taxReportId, "13", tenant); // 13 is tax report;
+                    if (journalPM != null)
                     {
-                        reconciledLines = reconciledLines.Take(MAX).ToList();
+                        List<string> jIds = reconciledLines.Select(rl => rl.JournalId).Distinct().ToList();
+                        if (jIds != null && jIds.Count > 0)
+                        {
+                            foreach (var jrnlId in jIds)
+                            {
+                                IAccountingContext MyContext = AccountingContext.GetContext(tenant);
+                                LedgerTransactionQueryService transactionsQuery = new LedgerTransactionQueryService(tenant);
+                                List<ReconciliationPM> Reconciliations = transactionsQuery.GetReconciliationsByJournalId(jrnlId, tenant);
+                                if (Reconciliations != null && Reconciliations.Count > 0)
+                                {
+                                    recocount = Reconciliations.Count;
+                                    ReconciliationUpdateService service = new ReconciliationUpdateService(MyContext, new Dictionary<string, IContext>(), tenant);
+                                    foreach (var item in Reconciliations)
+                                    {
+                                        if (item != null && !item.IsCancelled)
+                                        {
+                                            try
+                                            {
+                                                List<string> refs = item.SearchFields.Split(',').ToList();
+                                                if (refs != null && refs.Count > 2
+                                                    && refs.ElementAt(1).Trim() == journalPM.JournalNumber
+                                                    && refs.ElementAt(2).Trim() == taxReportPM.TaxReportNumber)
+                                                {
+                                                    item.IsCancelled = true;
+                                                    service.InitializeEntityPM(item);
+                                                    item.ChangeSetOp = ChangeSetOperation.Update;
+                                                    service.Update(item, true);
+                                                    reco_cancelled += 1;
+                                                }
+                                            }
+                                            catch (Exception)
+                                            {
+                                                throw;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        if (reco_cancelled == recocount) skip_other_checks = true;
                     }
-                    string lines = String.Join(",", reconciledLines.Select(rl => rl.Line)); 
 
-                    string journals = String.Join(",", reconciledLines.Select(rl => rl.JournalNumber));
-
-                    string problem = TranslateTextsClass.Translate("Accounting.O.TaxRepProblem", tenant);
-                    if (String.IsNullOrEmpty(problem)) problem = @"הבעיה מצויה בשורה";
-
-                    string problem_2 = TranslateTextsClass.Translate("Accounting.O.TaxRepProblem_2", tenant);
-                    if (String.IsNullOrEmpty(problem)) problem_2 = @"בדוח זה בפקודת יומן מספר";
-
-                    error_text += ". " + problem + " " + lines; 
-                    error_text += " " + problem_2 + " " + journals;
                 }
-                throw new ApplicationException(error_text);
+            }
+            if (!skip_other_checks)
+            {
+                if (reco_cancelled > 0)
+                {
+                    reconciledLines = null;
+                    canHaveClosingJournal = taxReportQueryService.CheckIfTaxReportCanHaveClosingJournal(taxReportId, fullAccountingSettings.VATOutputGLAccountId, tenant, ref reconciledLines);
+                }
+                if (!canHaveClosingJournal)
+                {
+                    string error_text = TranslateTextsClass.Translate("TaxReport.O.ClosingJournalValidationMessage", tenant);
+                    if (reconciledLines != null && reconciledLines.Count > 0)
+                    {
+                        reconciledLines = reconciledLines.OrderBy(rl => rl.JournalNumber).ThenBy(rl => rl.Line).ToList();
+                        const int MAX = 5;
+                        if (reconciledLines.Count > MAX)
+                        {
+                            reconciledLines = reconciledLines.Take(MAX).ToList();
+                        }
+                        string lines = String.Join(",", reconciledLines.Select(rl => rl.Line));
+
+                        string journals = String.Join(",", reconciledLines.Select(rl => rl.JournalNumber));
+
+                        string problem = TranslateTextsClass.Translate("Accounting.O.TaxRepProblem", tenant);
+                        if (String.IsNullOrEmpty(problem)) problem = @"הבעיה מצויה בשורה";
+
+                        string problem_2 = TranslateTextsClass.Translate("Accounting.O.TaxRepProblem_2", tenant);
+                        if (String.IsNullOrEmpty(problem)) problem_2 = @"בדוח זה בפקודת יומן מספר";
+
+                        error_text += ". " + problem + " " + lines;
+                        error_text += " " + problem_2 + " " + journals;
+                    }
+                    throw new ApplicationException(error_text);
+                }
             }
         }
 
@@ -176,10 +242,13 @@ namespace Logitude.Accounting.BL.CoreBL
             var Reconciliations = transactionsQuery.GetReconciliationsByJournalId(journalPM.Id, tenant);
             ReconciliationUpdateService service = new ReconciliationUpdateService(MyContext, new Dictionary<string, IContext>(), tenant);
             foreach (var item in Reconciliations) {
-                item.IsCancelled = true;
-                service.InitializeEntityPM(item);
-                item.ChangeSetOp = ChangeSetOperation.Update;
-                service.Update(item, true);
+                if (item != null && !item.IsCancelled)
+                {
+                    item.IsCancelled = true;
+                    service.InitializeEntityPM(item);
+                    item.ChangeSetOp = ChangeSetOperation.Update;
+                    service.Update(item, true);
+                }
             }
         }
 
