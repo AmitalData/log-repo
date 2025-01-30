999ed30633b4b0660185eb79603443643c804d66
Elon Hezi <Elon@amital.co.il>
2025-01-30 12:47:05 +0200

    #115125

--- Logitude/Logitude.CustomsMessaging/RequestServices/PC_NG_2280_MSG01_CertificateOfOriginRequestRequestService.cs
+++ Logitude/Logitude.CustomsMessaging/RequestServices/PC_NG_2280_MSG01_CertificateOfOriginRequestRequestService.cs
@@ -190,8 +190,8 @@ namespace Logitude.CustomsMessaging.ResponseServices
 					    {
 					    	ItemSerial = Convert.ToInt32(item1.ItemSerial),
 							ItemSerialSpecified = true,
-							// ItemId = item1.ItemId,
-							OriginCriterion = item1.OriginCriterionCodeName,
+                            ItemId = !string.IsNullOrEmpty(item1.ItemId) ? item1.ItemId : "",
+                            OriginCriterion = item1.OriginCriterionCodeName,
 					    	MarksAndNumbers = item1.MarksAndNumbers,
 					    	PackageQuantity = Convert.ToInt32(item1.PackageQuantity),
 							PackageQuantitySpecified = true,
@@ -202,9 +202,6 @@ namespace Logitude.CustomsMessaging.ResponseServices
 					    	MeasureType = item1.MeasureType,
 					    
 					    };
-
-						if (!string.IsNullOrEmpty(item1.ItemId)) CertificateOfOriginRequestItemDetail.ItemId = item1.ItemId;
-						else CertificateOfOriginRequestItemDetail.ItemId = "";
 
 						CertificateOfOriginRequestItemDetailList.Add(CertificateOfOriginRequestItemDetail);
 					}
@@ -259,7 +256,7 @@ namespace Logitude.CustomsMessaging.ResponseServices
 					{
 						ItemSerial = Convert.ToInt32(item1.ItemSerial),
 						ItemSerialSpecified = true,
-						// ItemId = item1.ItemId,
+						ItemId = !string.IsNullOrEmpty(item1.ItemId) ? item1.ItemId : "",
 						OriginCriterion = item1.OriginCriterionCodeName,
 						MarksAndNumbers = item1.MarksAndNumbers,
 						PackageQuantity = Convert.ToInt32(item1.PackageQuantity),
@@ -271,9 +268,6 @@ namespace Logitude.CustomsMessaging.ResponseServices
 						MeasureType = item1.MeasureType,
 
 					};
-
-                    if (!string.IsNullOrEmpty(item1.ItemId)) CertificateOfOriginRequestItemDetail.ItemId = item1.ItemId;
-                    else CertificateOfOriginRequestItemDetail.ItemId = "";
 
                     CertificateOfOriginRequestItemDetailList.Add(CertificateOfOriginRequestItemDetail);
 				}
