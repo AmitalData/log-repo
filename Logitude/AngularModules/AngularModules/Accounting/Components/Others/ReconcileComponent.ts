83535408ca4ca1a5c34965f136163c9f77762215
Frida Lok <frida@amital.co.il>
2025-02-10 11:26:25 +0200

    #113918

--- Logitude/AngularModules/AngularModules/Accounting/Components/Others/ReconcileComponent.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/Others/ReconcileComponent.ts
@@ -915,16 +915,16 @@ export class ReconcileComponent extends BaseComponent implements OnInit, OnDestr
         //     return;
         // }
 
-        if (this.SelectedLines.Length > 0 && this.TotalDifference != 0) {
-            if (this.IsMultiWithReconcileMethodCodeEqualOne) {
-                errors.push(TextCodeTranslator.Translate("Reconciliations.O.ErrorsInMultiWithRecOne"));
-            }
-        }
+        // if (this.SelectedLines.Length > 0 && this.TotalDifference != 0) {
+        //     if (this.IsMultiWithReconcileMethodCodeEqualOne) {
+        //         errors.push(TextCodeTranslator.Translate("Reconciliations.O.ErrorsInMultiWithRecOne"));
+        //     }
+        // }
 
         var ledgerTransactionsPMs = this.GetLedgerTransactionsPMs();
         this.CurrentSession.StartBusyIndicatorSaving();
-            this.CurrentSession.StopBusyIndicator();
-           
+        this.CurrentSession.StopBusyIndicator();
+
                 this.ValidationErrorsList = errors;
                 if (this.ValidationErrorsList.length == 0) {
         
@@ -1154,7 +1154,8 @@ export class ReconcileComponent extends BaseComponent implements OnInit, OnDestr
                             "GLAccountPMId": this.GLAccountPM.Id,
                             TotalDifference: this.TotalDifference,
                             TotalCredit: this.TotalCredit,
-                            TotalDebit: this.TotalDebit
+                            TotalDebit: this.TotalDebit,
+                            IsMultiWithReconcileMethodCodeEqualOne : this.IsMultiWithReconcileMethodCodeEqualOne
                         };
                         logitudeWindow.Show('./Accounting/Components/Others/JournalReconcileComponent');
                         logitudeWindow.WindowClosed.subscribe(($event: any) => {
