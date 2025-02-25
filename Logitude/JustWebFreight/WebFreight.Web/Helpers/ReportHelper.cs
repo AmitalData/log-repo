8637f0b6e66654e93a499bf68367e667244578e7
Jeremy Cohen <Jeremy@amital.co.il>
2025-02-25 15:55:25 +0200

    #115901

--- Logitude/JustWebFreight/WebFreight.Web/Helpers/ReportHelper.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Helpers/ReportHelper.cs
@@ -20,6 +20,7 @@ using Simplog.Data.CommonDataModel.Repositories;
 using Simplog.Data.Helpers;
 using Simplog.Server.Infrastructure;
 using Simplog.Server.Infrastructure.DataContracts;
+using Stimulsoft.Base;
 using Stimulsoft.Base.Json;
 using Stimulsoft.Report;
 using Stimulsoft.Report.Dictionary;
@@ -3412,7 +3413,16 @@ namespace WebFreight.Web.Helpers
 
         }
 
-
+        public static void AddStimulsoftLicenseKey()
+        {
+            StiLicense.Key = "6vJhGtLLLz2GNviWmUTrhSqnOItdDwjBylQzQcAOiHk5LQfMb0Dr1Ze4z6YRXSb7imTiay6/HzKYGUzkd/h3FMt5R7" +
+"uunoM5lX8Vs2voVkSeT6Wv6WI6Jcy4xOeAjjPkTBhC+ivrrxidMQjLaebItqFcnJWqKXBUgoJa0WfmH3soi0IbfEmI" +
+"fQ3ZmMq5BHsjsKoHSdnbzDUPWMXieYRTJZL6tsBC6QRy2ALPnYwg88ZJDGAWgAqMhZ+M0BVM17B3YJN9mu1MfAblN7" +
+"rG1eWrSrR5B53af4aeWs0RmqVNatfenGL8sufvTgOiyEuQmC9J7sHOT6VoQpWOlZthrc7JOl4zbw+qduZHZrpLuK+1" +
+"O3AB8EeDCQ6EgM8TcUesQBZZrUA4ZUFpxsCdvL0n4DQiB1tIof1TGHXCtZ62S1kAfU4XJzEGM/g3MYbKridAK5ckyc" +
+"0xwsK2y46rm9W3EV0m49Na0pcJe+2ZScc6BP1o3tDS9ddHbfkt7hFZpUNTqOxn9BOP0YVoQul+dPckYle4PS4mzXVp" +
+"tMrKV4En69rnW/z658axW0kQ2GxorKwW0IAR";
+        }
 
 
     }
