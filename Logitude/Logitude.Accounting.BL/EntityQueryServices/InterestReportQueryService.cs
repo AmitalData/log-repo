5381a07d299e3f1e2711233b7f554e5f4620d195
Alexander Khitrik <alexanderh@amital.co.il>
2025-03-10 10:45:24 +0200

    Alexander: #113167 Interest Activation Balance  - PR comments

--- Logitude/Logitude.Accounting.BL/EntityQueryServices/InterestReportQueryService.cs
+++ Logitude/Logitude.Accounting.BL/EntityQueryServices/InterestReportQueryService.cs
@@ -22,18 +22,14 @@ namespace Logitude.Accounting.BL.EntityQueryServices
 
         public decimal GetClosedBalanceOfLastInvoicedOrClosedWithoutInvoiceInterestReport(int tenant,string glaccountId)
         {
-            decimal closedBalance = 0;
             InterestReportRepository interestReportRepository = new InterestReportRepository(tenant);
-            closedBalance = interestReportRepository.GetClosedBalanceOfLastInvoicedOrClosedWithoutInvoiceInterestReport(tenant, glaccountId);
-            return closedBalance;
+            return interestReportRepository.GetClosedBalanceOfLastInvoicedOrClosedWithoutInvoiceInterestReport(tenant, glaccountId);
         }
 
         public decimal GetSumOfExReportsOrInterestOpenBalance(int tenant, string glaccountId)
         {
-            decimal closedBalance = 0;
             InterestReportRepository interestReportRepository = new InterestReportRepository(tenant);
-            closedBalance = interestReportRepository.GetSumOfExReportsOrInterestOpenBalance(tenant, glaccountId);
-            return closedBalance;
+            return interestReportRepository.GetSumOfExReportsOrInterestOpenBalance(tenant, glaccountId);
         }
 
         public override void GetComposition(EntityKeyFields entityKeys, InterestReportPM entityPM)
