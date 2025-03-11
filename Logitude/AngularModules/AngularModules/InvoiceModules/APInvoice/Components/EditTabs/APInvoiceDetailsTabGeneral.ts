c910af488af1a2d29c9aec90a8313763fe781cc4
Libby Hasid <libby@amital.co.il>
2025-03-11 13:34:56 +0200

    #116476

--- Logitude/AngularModules/AngularModules/InvoiceModules/APInvoice/Components/EditTabs/APInvoiceDetailsTabGeneral.ts
+++ Logitude/AngularModules/AngularModules/InvoiceModules/APInvoice/Components/EditTabs/APInvoiceDetailsTabGeneral.ts
@@ -269,9 +269,9 @@ export class APInvoiceDetailsTabGeneral extends BaseComponent implements OnDestr
     SetUIProperties_VATNumber() {
         var isRequired = false;
 
-        if (SessionLocator.AccountingSettingPM.IsVatNumberMandatoryInAP && this.EntityPM.VendorCountry === "ISRAEL") {
-            if (AppTool.IsNullOrEmpty(this.VATNumber)) {
-                isRequired = true;
+        if (AppTool.IsNullOrEmpty(this.VATNumber)) {
+                if (SessionLocator.AccountingSettingPM.IsVatNumberMandatoryInAP && (this.EntityPM.VendorCountry === "IL"|| this.EntityPM.VendorCountry === null)) {
+                     isRequired = true;
             }
         }
 
@@ -903,6 +903,7 @@ export class APInvoiceDetailsTabGeneral extends BaseComponent implements OnDestr
     }
     public GLAccountId: string;
     public BillToId: string;
+
     GetCardProperties() {
         this.myCardListService.getSingle(this.EntityPM.VendorId).subscribe((myResponse: ServiceResponse) => {
             if (!myResponse.HasError) {
@@ -917,6 +918,7 @@ export class APInvoiceDetailsTabGeneral extends BaseComponent implements OnDestr
                     this.EntityPM.VendorGLAccountId = null;
                     this.EntityPM.VendorName = null;
                     this.EntityPM.VendorLocalName = null;
+                    this.EntityPM.VendorCountry = null;
                 }
 
                 else {
@@ -925,7 +927,7 @@ export class APInvoiceDetailsTabGeneral extends BaseComponent implements OnDestr
                     this.VATNumber = list.VatNumber;
                     this.EntityPM.VendorName = list.EnglishName;
                     this.EntityPM.VendorLocalName = list.LocalName || list.EnglishName;
-
+                    this.EntityPM.VendorCountry = list.CountryCode;
                     if (!AppTool.IsNullOrEmpty(list.InvoiceCurrencyId)) {
                         this.InvoiceCurrencyId = list.InvoiceCurrencyId;
                     }
@@ -959,6 +961,7 @@ export class APInvoiceDetailsTabGeneral extends BaseComponent implements OnDestr
                     this.GetConnectedGLAccount();
                 else
                     this.GetConnectedBillTo();
+                this.SetUIProperties_VATNumber();
 
 
             }
