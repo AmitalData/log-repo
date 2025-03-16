960216a0f884314c9ef1f27d2f0f5f0605db059d
Elon Hezi <Elon@amital.co.il>
2025-03-16 16:42:32 +0200

    #116494

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationTabs/Components/DigitalCertificateOfOrigin/CertificateOfOriginTabs/CertificateOfOriginComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationTabs/Components/DigitalCertificateOfOrigin/CertificateOfOriginTabs/CertificateOfOriginComponent.ts
@@ -409,8 +409,7 @@ export class CertificateOfOriginComponent extends BaseRequestsSheetMassaging {
                 this.ValidationErrors.push(TextCodeTranslator.Translate("Customs.CertificateOfOrigin.O.OriginCriterionCodeRequired"));
             }
         }).then(() => {
-
-            if (AppTool.IsNullOrEmpty(this.DecalarationData.DeclarationNumber)) {
+            if (AppTool.IsNullOrEmpty(this.DecalarationData.DeclarationNumber) && AppTool.IsNullOrEmpty(this.DecalarationData.IsAmendment)) {
                 this.ValidationErrors.push(TextCodeTranslator.Translate("Customs.CertificateOfOrigin.O.NotDeclaration"));
             }
         }).then(() => {
