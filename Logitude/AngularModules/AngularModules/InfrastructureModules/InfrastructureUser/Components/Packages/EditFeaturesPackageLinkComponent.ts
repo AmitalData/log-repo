438dcf1ba4b06d34025a8e8f9870bb50d67601b6
Tomer Avraham <TomerA@amital.co.il>
2025-03-18 11:54:51 +0200

    Added a search field on the feature list screen

--- Logitude/AngularModules/AngularModules/InfrastructureModules/InfrastructureUser/Components/Packages/EditFeaturesPackageLinkComponent.ts
+++ Logitude/AngularModules/AngularModules/InfrastructureModules/InfrastructureUser/Components/Packages/EditFeaturesPackageLinkComponent.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {SessionLocator} from '../../../../Infrastructure/Utilities/SessionLocator';
 import {PackageFeatureClass} from './EditPackageFeaturesComponent';
 import {Cloner} from '../../../../Infrastructure/Utilities/Cloner';
+import { AppTool } from 'Infrastructure/Tools';
 
 @Component({
     
@@ -10,17 +11,20 @@ import {Cloner} from '../../../../Infrastructure/Utilities/Cloner';
 
 export class EditFeaturesPackageLinkComponent {
     public ItemsSource: PackageFeatureClass[] = [];
+    public ItemsSourceOriginal: PackageFeatureClass[] = [];
     private CurrentSession = SessionLocator.SelectedSession;
+    searchText: string = "";
     constructor() {
 
     }
 
     SetWindowArgs(args: any) {
         this.ItemsSource = args['Items'];
+        this.ItemsSourceOriginal=args['Items'];
         this.Clone();
     }
 
-    CancelButtonClicked() {
+    CancelButtonClicked() {        
         this.RejectChanges();
         this.CurrentSession.CloseCurrentWindow();
     }
@@ -43,4 +47,22 @@ export class EditFeaturesPackageLinkComponent {
             myCloner.RejectChanges();
         });
     }
+
+    SearchFeatures(text: string) {
+        var data = this.ItemsSourceOriginal;
+        if (!AppTool.IsNullOrEmpty(text) && !AppTool.IsNullOrEmpty(data) && data.length > 0) {
+    
+            var tkn = setTimeout(() => {
+    
+                var filteredData = data.filter(d => {
+                    var searchField = d["Name"] ?? ""; 
+                    return searchField.toLowerCase().includes(text.trim().toLowerCase());
+                });    
+                this.ItemsSource = filteredData;    
+            }, 200);
+        } else {
+            this.ItemsSource = this.ItemsSourceOriginal;
+        }
+    }
+    
 }
