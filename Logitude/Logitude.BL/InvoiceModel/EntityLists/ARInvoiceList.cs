7dc69b91b5535b98b6b73b828766a3230cf47ee4
Frida Lok <frida@amital.co.il>
2025-03-19 13:57:54 +0200

    #116793

--- Logitude/Logitude.BL/InvoiceModel/EntityLists/ARInvoiceList.cs
+++ Logitude/Logitude.BL/InvoiceModel/EntityLists/ARInvoiceList.cs
@@ -218,6 +218,9 @@ namespace Logitude.BL.InvoiceModel.EntityLists
         public string ConfirmationNumberStatus { get; set; }
         public string ConfirmationNumberStatusName { get; set; }
         public string APIResponseToConfirmation { get; set; }
+        public string SequenceStatus { get; set; }
+        public string InvoiceNumberPart { get; set; }
+        public string InvoiceSeries { get; set; }
 
     }
 }
