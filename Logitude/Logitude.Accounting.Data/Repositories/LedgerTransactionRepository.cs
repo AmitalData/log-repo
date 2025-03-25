9e32cd4ad6bab3987e193a49529f033d49567e7d
Alexander Khitrik <alexanderh@amital.co.il>
2025-03-25 12:39:33 +0200

    Alexander: #116597 ExcludeFromTaxReport - PR comments

--- Logitude/Logitude.Accounting.Data/Repositories/LedgerTransactionRepository.cs
+++ Logitude/Logitude.Accounting.Data/Repositories/LedgerTransactionRepository.cs
@@ -1718,7 +1718,6 @@ WHERE Mark='true' and AccountId='{0}' and tenant={1} ", gLAccountId, tenant)
         {
             // Single DB query (Include Account and JournalLine to capture all required data)
             var query = context.LedgerTransactions
-                .Include("Account")
                 .Include("JournalLine")
                 .Where(a => journalIds.Contains(a.JournalId) && a.Tenant == tenant)
                 .Select(a => new
