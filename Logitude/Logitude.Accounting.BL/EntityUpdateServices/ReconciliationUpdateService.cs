7743dc42723b0628dfd4d411921de4f9a4fa8782
Alexander Khitrik <alexanderh@amital.co.il>
2025-03-25 15:32:30 +0200

    Alexander: #115264 Voided APInvoice status bug - reco and validation

--- Logitude/Logitude.Accounting.BL/EntityUpdateServices/ReconciliationUpdateService.cs
+++ Logitude/Logitude.Accounting.BL/EntityUpdateServices/ReconciliationUpdateService.cs
@@ -545,7 +545,7 @@ namespace Logitude.Accounting.BL.EntityUpdateServices
                     if (ledgerTransactionPM.OpenAmount == 0)
                     {
                         invoice.IsClosed = true;
-                        invoice.StatusCode = "PD";
+                        if (invoice.StatusCode != "VD") invoice.StatusCode = "PD";
                     }
                     else if (Math.Abs(ledgerTransactionPM.OpenAmount) < transactionAmount)
                     {
