7df44b6376566c802dd17810a7939ae2e0312ad0
Libby Hasid <libby@amital.co.il>
2025-03-26 09:53:17 +0200

    #116591

--- Logitude/AngularModules/AngularModules/Common/Components/Maintenance/RatesHistoryComponent.ts
+++ Logitude/AngularModules/AngularModules/Common/Components/Maintenance/RatesHistoryComponent.ts
@@ -9,6 +9,8 @@ import {CurrencyRatesService, LastRate} from '../../../Common/Services/CurrencyR
 import {ApiQueryFilters} from '../../../Infrastructure/DataContracts/ApiQueryFilters';
 import {RatesTableListService} from '../../../Infrastructure/Services/StandardLists/RatesTableListService';
 import {ServiceArgs} from '../../../Infrastructure/DataContracts/ServiceArgs';
+import { AdditionalCurrencyRateList } from 'Infrastructure/EntityLists/AdditionalCurrencyRateList';
+import { RatesItem } from './RatesMainTabComponent';
 
 @Component({
     
@@ -17,17 +19,21 @@ import {ServiceArgs} from '../../../Infrastructure/DataContracts/ServiceArgs';
 
 export class RatesHistoryComponent extends BaseComponent {
 
-    public ItemsSource: RatesTableList[] = [];
+    public ItemsSource: RatesItem[] = [];
     public LastRate: LastRate = new LastRate();
+    private CurrencyRateTypes: AdditionalCurrencyRateList[];
     private RatesTableListService: RatesTableListService;
     public count: number = 0;
     private CurrentSession = SessionLocator.SelectedSession;
+    currencyRatesService: CurrencyRatesService = new CurrencyRatesService();
+
     constructor() {
         super();
     }
 
-    SetWindowArgs(args: LastRate) {
-        this.LastRate = args;
+    SetWindowArgs(args: any) {
+        this.LastRate = args.LastRate;
+        this.CurrencyRateTypes = args.CurrencyRateTypes;
         this.BuildData();
     }
 
@@ -48,14 +54,9 @@ export class RatesHistoryComponent extends BaseComponent {
         this.QueryPageIndex = 0;
 
         this.LoadData();
-       // this.LoadDateCount();
     }
-
     LoadData() {
-        //this.ItemsSource = [];
-        this.filters.PageIndex = this.QueryPageIndex;
-        this.filters.PageSize = this.PageSize;
-        this.RatesTableListService.getByFilters(this.filters).subscribe((myResult:any) => {
+        this.currencyRatesService.GetCurrenciesExchangeRateByCurrencyId(this.LastRate.ForeignCurrencyId,this.PageSize,this.QueryPageIndex).subscribe((myResult:any) => {
             if (myResult == null) {
                 this.ItemsSource = [];
             }
@@ -85,7 +86,9 @@ export class RatesHistoryComponent extends BaseComponent {
 
         this.SetPagerButtonsStates();
     }
-
+    GetFoundRate(currencyRates: any[], AdditionalCurrencyRateId: number): any {
+        return Array.isArray(currencyRates) ? currencyRates.find(rate => rate.AdditionalCurrencyRateId === AdditionalCurrencyRateId) : undefined;
+    }
     /*Pager & Provider*/
     private queryPageIndex = 0;
     get QueryPageIndex() {
@@ -263,23 +266,8 @@ export class RatesHistoryComponent extends BaseComponent {
         this.LoadData();
     }
 
-    // Props
-    get ForeignCurrencyCode() {
-        return this.LastRate.ForeignCurrencyCode;
-    }
+    
 
-    get ValueDate() {
-        return this.LastRate.ValueDate;
-    }
-
-    get Rate() {
-        return this.LastRate.Rate;
-    }
-    get UpdatedByUserName(){
-        return this.LastRate.UpdatedByUserNameName;
-    }
-
-    // Commands
     public CloseButtonClicked() {
         this.CurrentSession.CloseCurrentWindow();
     }
