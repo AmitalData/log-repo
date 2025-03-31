c9fe3b1fec26caf91551b2bd939f052e8981c7cc
Shoshana Greenwald <shoshana@amital.co.il>
2025-03-31 11:05:09 +0300

    #117009

--- AmitalCloud/AmitalCloud.Shipment.Data/Context/ShipmentContext.cs
+++ AmitalCloud/AmitalCloud.Shipment.Data/Context/ShipmentContext.cs
@@ -41,12 +41,6 @@ namespace AmitalCloud.Shipment.Data.Context
         }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-//		    if (LogitudeSettings.DatabaseManagementSystem == "oracle")
-//            {
-//                var config = Devart.Data.Oracle.Entity.Configuration.OracleEntityProviderConfig.Instance;
-//                config.Workarounds.DisableQuoting = true;
-//                config.Workarounds.IgnoreSchemaName = true;
-//            }
             Database.SetInitializer<ShipmentContext>(null);
             modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
 			            modelBuilder.Configurations.Add(new AccountingInformationIdentifierMap());
@@ -197,8 +191,8 @@ namespace AmitalCloud.Shipment.Data.Context
             modelBuilder.Configurations.Add(new DocumentTypeTemplateMap());
             modelBuilder.Configurations.Add(new DueTypeMap());
             modelBuilder.Configurations.Add(new EntityDateMap());
-            modelBuilder.Configurations.Add(new EntityLastActivityMap());
-            modelBuilder.Configurations.Add(new EntityLastActivityTypeMap());
+			//modelBuilder.Configurations.Add(new EntityLastActivityMap());
+			modelBuilder.Configurations.Add(new EntityLastActivityTypeMap());
             modelBuilder.Configurations.Add(new EntityLastUpdateMap());
             modelBuilder.Configurations.Add(new EntityStatuMap());
             modelBuilder.Configurations.Add(new EventTypeMap());
