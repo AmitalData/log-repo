6a0f1d047e3e6aab4146ffd352c6af8b3abc8459
Izik Fridland <IzikFr@amital.co.il>
2025-04-07 10:49:04 +0300

    #115067

--- Logitude/AngularModules/AngularModules/Common/Services/ExtendedPMs/DocumentTypeMetaDataExtendedService.ts
+++ Logitude/AngularModules/AngularModules/Common/Services/ExtendedPMs/DocumentTypeMetaDataExtendedService.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError, map } from 'rxjs/operators';
-import { defer, of } from 'rxjs';
 import { ServiceHelper } from '../../../Infrastructure/Utilities/ServiceHelper';
 import { ServiceResponse } from '../../../Infrastructure/DataContracts/ServiceResponse';
 
