4e01d378433636cd6e4823492ea04c84818af56b
Sohaib Nemer <sohaib@amital.co.il>
2025-04-08 12:18:48 +0300

    #116497

--- Logitude/Logitude.CustomsMessaging/FakeMessagingServices/Fake_SendDOC_MSG5101_GNMessageToAgent.cs
+++ Logitude/Logitude.CustomsMessaging/FakeMessagingServices/Fake_SendDOC_MSG5101_GNMessageToAgent.cs
@@ -46,6 +46,7 @@ namespace Logitude.CustomsMessaging.FakeMessagingServices
             msg.RelatedEntity = GetRelatedEntity(dec);
             msg.msgCode = msgCode;
             msg.msgString = msgString;
+            msg.SenderName = "Test Sohaib";
             return msg;
         }
         private ConnectedEntity GetRelatedEntity(DeclarationPM dec)
