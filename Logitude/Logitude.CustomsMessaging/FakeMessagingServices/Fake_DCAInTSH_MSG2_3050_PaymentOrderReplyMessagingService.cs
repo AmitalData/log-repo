aa6c80888f388cf3b3b56e44760a622187eaba66
Sohaib Nemer <sohaib@amital.co.il>
2025-04-27 12:45:33 +0300

    #117672

--- Logitude/Logitude.CustomsMessaging/FakeMessagingServices/Fake_DCAInTSH_MSG2_3050_PaymentOrderReplyMessagingService.cs
+++ Logitude/Logitude.CustomsMessaging/FakeMessagingServices/Fake_DCAInTSH_MSG2_3050_PaymentOrderReplyMessagingService.cs
@@ -54,13 +54,15 @@ namespace Logitude.CustomsMessaging.FakeMessagingServices
 
             ConsignmentPM _con = consignmentQueryService.GetSingle(_dec.Id, 1, false, false);
             _paymentOrderReply.PaymentDetails = new PaymentDetails();
-            //_paymentOrderReply.PaymentDetails.paymentID = Convert.ToInt32(_dec.DeclarationNumber.Substring(_dec.DeclarationNumber.Length-4)); // Or Use counter?\
             _paymentOrderReply.PaymentDetails.paymentID = Convert.ToInt32(DateTime.Now.Ticks.ToString().Substring(10, 7));
+            _paymentOrderReply.PaymentDetails.CustomerActivityType = 3;
+
             _paymentOrderReply.customsHouse = 2;
             _paymentOrderReply.paymentProcess = 1;
             _paymentOrderReply.paymentOrderType = 1;
             _paymentOrderReply.paymentStatus = ( paymentStatus != 0 ) ? paymentStatus : 3;
             _paymentOrderReply.paymentProcess = (paymentProcess != 0) ? paymentProcess : 1;
+            _paymentOrderReply.paymentOrderPayDate = DateTime.Now;
             _paymentOrderReply.ConnectedEntity = new ConnectedEntity
             {
                 entityType = 1055,
