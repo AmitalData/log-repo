abe07d7779a27321ac3b1caae729c5a3e793a6d5
Libby Hasid <libby@amital.co.il>
2025-05-11 14:26:07 +0300

    #117183

--- Logitude/AngularModules/AngularModules/Report/Components/FiltersComponent/Accounting/MonthlyBalancesReportFilterComponent.ts
+++ Logitude/AngularModules/AngularModules/Report/Components/FiltersComponent/Accounting/MonthlyBalancesReportFilterComponent.ts
@@ -34,8 +34,8 @@ export class MonthlyBalancesReportFilterComponent extends BaseComponent {
     public TaxReportLists: CodeNameClass[] = [];
     entityResourceService: EntityResourceService = new EntityResourceService();
     public isReady: boolean = false;
+    public ChartOfAccountsComboBoxValue: string ="All";
 
-    //new
     selectedChartOfAccountsTypes: any[] = [];
     chartOfAccountsTypes: any[] = [];
     selectedChartOfAccounts: any[] = [];
@@ -55,6 +55,7 @@ export class MonthlyBalancesReportFilterComponent extends BaseComponent {
                         this.entityResourceService.getEntityResourceByTableName("ARInvoice").subscribe((response: any) => {
                             this.entityResourceService.getEntityResourceByTableName("General").subscribe((response: any) => {
                             this.isReady = true;
+
                     });
                     });
                     });
@@ -64,12 +65,12 @@ export class MonthlyBalancesReportFilterComponent extends BaseComponent {
         this.DataContext.UIProperties.SetRequired("ChartOfAccountsComboBoxValue", this.ObjectTableName, true)
         this.DataContext.UIProperties.SetRequired("NumberOfYear", this.ObjectTableName, true)
 
-        //new 
-        this.getChartOfAccounts();
     }
     InitializeComponent(myReportsPreview: ReportsPreviewComponent) {
         this.ReportsPreview = myReportsPreview;
         this.DetailedForJobs=false
+        this.getChartOfAccounts();
+
     }
 
   
@@ -78,19 +79,6 @@ export class MonthlyBalancesReportFilterComponent extends BaseComponent {
         
     }
 
-      
-   
-   //new
-    private chartOfAccountsComboBoxValue: string;
-    public get ChartOfAccountsComboBoxValue(): string
-    {
-        return this.chartOfAccountsComboBoxValue;
-    }
-    public set ChartOfAccountsComboBoxValue(v: string)
-    {
-        this.chartOfAccountsComboBoxValue = v;
-        this.SetChartOfAccountsFilterProperties();
-    }
 
     private detailedForJobs: boolean;
     get DetailedForJobs() { return this.detailedForJobs; }
@@ -121,6 +109,7 @@ export class MonthlyBalancesReportFilterComponent extends BaseComponent {
               this.selectedChartOfAccounts = this.chartOfAccounts.filter(item=>item.Checked == true);
     }
     
+    AdditionalServiceSelectedValue:string
 
     private getChartOfAccounts()
     {
@@ -132,16 +121,19 @@ export class MonthlyBalancesReportFilterComponent extends BaseComponent {
                 this.chartOfAccounts = arg.Result;                
                 this.chartOfAccounts = this.chartOfAccounts.map(item=> {return {...item,
                     Name: `(${ item.Code }) ${ item.LocalName || item.EnglishName }`,
-                    Checked: this.selectedChartOfAccounts?.some(selectedItem =>this.ChartOfAccountsComboBoxValue=="NotAll" && (selectedItem === item.Id || selectedItem === item.Code))
+                    Checked: this.ChartOfAccountsComboBoxValue == "NotAll" && this.AdditionalServiceSelectedValue.split(',').some(selectedItem =>  selectedItem === item.Code||selectedItem === item.Id)
 
                 }}).sort((a, b) => a.Code - b.Code);            });
+
     }
     OnChartOfAccountsItemClicked(items){
 
         this.selectedChartOfAccounts = this.chartOfAccounts.filter(item=>item.Checked == true);
 
     }
-
+    SelectedItemChanged(item) {
+        this.ChartOfAccountsComboBoxValue = item;
+    }
 
     public RunReportTitle: string = 'Run Report';
     SetRunReportTitle() {
@@ -176,13 +168,10 @@ export class MonthlyBalancesReportFilterComponent extends BaseComponent {
                     this.NumberOfYear = queryFilterItem.FieldValue;
                     break;
                 case "ChartOfAccountsIdList":
-                    {
-                        this.selectedChartOfAccounts = queryFilterItem.FieldValue;
-                        if(this.selectedChartOfAccounts?.length>0 )
-                            this.chartOfAccountsComboBoxValue="NotAll";
-                        else
-                            this.chartOfAccountsComboBoxValue="All";
-                        break;
+                    {  this.ChartOfAccountsComboBoxValue =  queryFilterItem.FieldValue=="All"?"All":"NotAll"; 
+                       this.AdditionalServiceSelectedValue = queryFilterItem.FieldValue;
+                       
+                       break;
     
                     }
                                  
