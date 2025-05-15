746517de0a4d6c2c533dd95337f92ced4bb25809
Sohaib Nemer <sohaib@amital.co.il>
2025-05-15 10:33:23 +0300

    #118332

--- Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_2470_DF_MSG16001_ReleaseGoodsMessageResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_2470_DF_MSG16001_ReleaseGoodsMessageResponseService.cs
@@ -169,7 +169,7 @@ namespace Logitude.CustomsMessaging.ResponseServices
 
                             this.CloseCustomsCollateral(declarationPM);
 
-                           
+
 
                             if (setting.IsConnectedToUniFreight || AmitalEventTracer.UseHybrid_When_NotIsConnectedToUniFreight)
                             {
@@ -180,7 +180,7 @@ namespace Logitude.CustomsMessaging.ResponseServices
                                     SendSoyStatusToUnifreight(declarationPM, user?.Id);
 
                                 }
-                           
+                            }
 
 
 
