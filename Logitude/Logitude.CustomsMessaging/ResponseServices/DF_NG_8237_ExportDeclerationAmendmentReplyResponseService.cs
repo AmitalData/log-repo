02665bd612e651fe68dd2311209702d30c2ecb83
Elon Hezi <Elon@amital.co.il>
2025-05-26 15:24:15 +0300

    #117379

--- Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_8237_ExportDeclerationAmendmentReplyResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_8237_ExportDeclerationAmendmentReplyResponseService.cs
@@ -653,8 +653,6 @@ namespace Logitude.CustomsMessaging.ResponseServices
                 {
                     _MyDeclarationPM.IsExportClosed = false;
                     _MyDeclarationPM.IsClose = false;
-                    this._MyDeclarationPM.ChangeSetOp = ChangeSetOperation.Update;
-                    myDeclarationUpdateService.Update(this._MyDeclarationPM, true);
                 }
 
                 if ((!isExportClose && (_MyDeclarationPM.AmendmentStatus == "1" || _MyDeclarationPM.AmendmentStatus == "2")) || MyResponseData.IsExportCloseApprove)
