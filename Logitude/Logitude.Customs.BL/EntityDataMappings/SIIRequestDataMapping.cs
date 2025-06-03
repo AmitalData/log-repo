ed8d04b490a65b050d1e034839f47a62f5b02c4e
Sohaib Nemer <sohaib@amital.co.il>
2025-06-03 19:49:03 +0300

    #117431

--- Logitude/Logitude.Customs.BL/EntityDataMappings/SIIRequestDataMapping.cs
+++ Logitude/Logitude.Customs.BL/EntityDataMappings/SIIRequestDataMapping.cs
@@ -14,6 +14,8 @@ using Simplog.Server.Infrastructure;
 using Logitude.Customs.BL.EntityQueryServices;
 using Simplog.Data.CommonDataModel.Repositories;
 using Logitude.Customs.Data.Repsitories;
+using Logitude.BL.CommonDataModel.APIDataContract.ApiV1;
+using Logitude.BL.CommonDataModel.EntityQueries;
 
 namespace Logitude.Customs.BL.EntityDataMappings
 {
@@ -65,10 +67,25 @@ namespace Logitude.Customs.BL.EntityDataMappings
                 if (entityPOCO.ContactId == null && agg.CustomerId != null)
                 {
                     DefaultValueQueryService defaultValueQueryService = new DefaultValueQueryService(entityPOCO.Tenant);
+                    UserQuery userQueryService = new UserQuery(entityPOCO.Tenant);
                     var customerCard = CardRepository.GetSingleCard(agg.CustomerId, entityPOCO.Tenant, true);
                     if (customerCard != null)
                     {
                         var defaultContact = defaultValueQueryService.GetDefault("ISRAEL", "CGG_CONT_STDI", "NON", customerCard.Code, entityPOCO.Tenant);
+                        if(defaultContact != null)
+                        {
+                            var userPM = userQueryService.GetSinglePMByCode(defaultContact, entityPOCO.Tenant);
+                            if(userPM != null)
+                            {
+                                entityPM.ContactName = userPM.LocalName;
+                                entityPM.ContactEmail = userPM.Email;
+                                entityPM.ContactCellPhone = userPM.BusinessPhone;
+                                entityPM.ContactFax = userPM.Fax;
+                                entityPM.ContactTel = userPM.BusinessPhone;
+                                entityPM.ContactId = userPM.Id;
+                            }
+
+                        }
                     }
                 }
             }
