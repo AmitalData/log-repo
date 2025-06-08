aa182965a28a5e6cbff7ce4727fceae6859fbd8d
Tomer Avraham <TomerA@amital.co.il>
2025-06-08 12:18:21 +0300

    #120174-Cancellation of changes made in task 113945 and 113942

--- Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_2754_MSG10004_ImportDeclarationResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_2754_MSG10004_ImportDeclarationResponseService.cs
@@ -68,10 +68,7 @@ namespace Logitude.CustomsMessaging.ResponseServices
         {
             if (!String.IsNullOrWhiteSpace(requestParams.AppicationId))
             {
-                CalculateDeclarationCourierStatus.UpdateCourierDeclarationStatusCode(requestParams.Tenant, requestParams.AppicationId);
-
-                // Sending Auto Restore Declaration Request #113942
-                DeclarationRestoreResponseData declarationRestoreResponseData = RestoreDeclarationRequest(requestParams);
+                CalculateDeclarationCourierStatus.UpdateCourierDeclarationStatusCode(requestParams.Tenant, requestParams.AppicationId);                
             }
             base.OnRequestFail(customResponse, requestParams);
         }
@@ -1349,26 +1346,12 @@ namespace Logitude.CustomsMessaging.ResponseServices
                 LogMessagingUtil.Instance.AppendLine("Sending Restore Declaration Request Failed !");
                 NetCommonHelper.Logger.DevLog.Instance.WriteError("Sending Restore Declaration Request Failed !");
                 throw new Exception("Sending Restore Declaration Request Failed !");
-            }
+                LogMessagingUtil.Instance.AppendLine("Sending Restore Declaration Request Failed !");
+        }               
+        private bool CheckFileCredit(DeclarationPM declarationPM, DeclarationPaymentPM declarationPaymentPM, string user)
         }
 
-        private DeclarationRestoreResponseData SendRestoreDeclaration(DeclarationRestoreRequestParams requestParamsData)
-        {
-            DF_NG_8373_Web05_RetrieveImportDeclarationMessagingService messagingService = new DF_NG_8373_Web05_RetrieveImportDeclarationMessagingService();
-            DeclarationRestoreResponseData responseData = messagingService.Send(requestParamsData);
-            if (responseData?.HasException == false && responseData?.Succeeded == true)
-            {
-                NetCommonHelper.Logger.DevLog.Instance.WriteError("Sending Restore Declaration Request success!");
-                return responseData;
-            }
-            else
-            {
-                NetCommonHelper.Logger.DevLog.Instance.WriteError("Sending Restore Declaration Request Failed because of declaration data is not found!");
-                throw new Exception("declaration data is not found !" + " " + responseData?.UserMessage);
-            }
-        }
-
-		private bool CheckFileCredit(DeclarationPM declarationPM, DeclarationPaymentPM declarationPaymentPM, string user, string requestParamsJson)
+        private bool CheckFileCredit(DeclarationPM declarationPM, DeclarationPaymentPM declarationPaymentPM, string user)
         {
             CustomFileCreditRequestParams requestParamsCredit = new CustomFileCreditRequestParams()
             {
