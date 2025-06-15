c390333cf1943e9c1b6336229cbef2e6eef5d19a
Sohaib Nemer <sohaib@amital.co.il>
2025-06-15 16:28:39 +0300

    #120305

--- Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_2754_MSG10004_ImportAmendmentDeclarationResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/DF_NG_2754_MSG10004_ImportAmendmentDeclarationResponseService.cs
@@ -506,9 +506,10 @@ namespace Logitude.CustomsMessaging.ResponseServices
 
                         var courierdeclaration = courierDeclarationQueryService.GetCourierDeclarationByDeclarationId(declarationOrg.Id, tenant);
                         if (courierdeclaration != null)
-                            courierdeclaration.ChangeSetOp = ChangeSetOperation.Delete
-                                ;
-                        courierDeclarationUpdateService.Update(courierdeclaration, true);
+                        {
+                            courierdeclaration.ChangeSetOp = ChangeSetOperation.Delete;
+                            courierDeclarationUpdateService.Update(courierdeclaration, true);
+                        }
 
                         CourierDeclarationPM courierDeclarationPM = new CourierDeclarationPM();
                         courierDeclarationPM.Tenant = courierdeclaration.Tenant;
@@ -580,8 +581,7 @@ namespace Logitude.CustomsMessaging.ResponseServices
             catch (System.Exception ex)
 
             {
-                error = ex.Message + ex.StackTrace;
-                return null;
+                throw new System.Exception("Error in MapResponseToDeclaration: " + ex.Message, ex);
             }
         }
 
