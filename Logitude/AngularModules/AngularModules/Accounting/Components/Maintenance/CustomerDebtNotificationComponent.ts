036e13a1a4a99da02c5b64fe393582efbcc400d0
Shoshana Greenwald <shoshana@amital.co.il>
2025-06-19 12:21:59 +0300

    #118180

--- Logitude/AngularModules/AngularModules/Accounting/Components/Maintenance/CustomerDebtNotificationComponent.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/Maintenance/CustomerDebtNotificationComponent.ts
@@ -129,7 +129,7 @@ export class CustomerDebtNotificationComponent extends BaseComponent implements 
             var myService = new ReportGroupService();
             groupService.getReportGroupListByCode('RACC').subscribe((myResponse: ServiceResponse) => {
             var groupList: ReportGroupList = myResponse.Result;
-            reportService.GetReportListsByGroupId(groupList.Id, SessionLocator.Tenant).subscribe((myResponse: ServiceResponse) => {
+            reportService.GetReportListsByGroupId(groupList.Id).subscribe((myResponse: ServiceResponse) => {
               
             var reportList = myResponse.Result.filter(x => x.Code === 'LTRP')[0];           
             var windowArgs: any = {};
