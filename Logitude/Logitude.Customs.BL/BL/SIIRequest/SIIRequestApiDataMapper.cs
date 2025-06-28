a47e1aa3da601be74ad628b7355f7c305d1ef9d6
Sohaib Nemer <sohaib@amital.co.il>
2025-06-28 21:43:59 +0300

    #117427

--- Logitude/Logitude.Customs.BL/BL/SIIRequest/SIIRequestApiDataMapper.cs
+++ Logitude/Logitude.Customs.BL/BL/SIIRequest/SIIRequestApiDataMapper.cs
@@ -150,12 +150,14 @@ namespace Logitude.Customs.BL.BL.SIIRequest
                 }).ToList();
 
 
-                return new ReleaseRequestApiDto
+                var dto = new ReleaseRequestApiDto
                 {
                     Credentials = credentials,
                     ReleaseRequestForm = form,
                     FormAttachments = attachments
                 };
+
+                SIIRequestValidator.Validate(dto, _tenant); return dto;
             }
             catch (Exception ex)
             {
