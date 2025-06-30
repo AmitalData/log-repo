4b4f55f3489ba54335d199b7776f95b6f97e890a
Jeremy Cohen <Jeremy@amital.co.il>
2025-06-30 16:46:46 +0300

    #121206

--- Logitude/JustWebFreight/WebFreight.Web/Helpers/ExportToExcelHelper.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Helpers/ExportToExcelHelper.cs
@@ -269,17 +269,6 @@ namespace WebFreight.Web.Helpers
 						stop = true;
 					}
 				}
-				if (stop == false)
-				{
-					MethodsInfo = getMethodsInfo("WebFreight.Web.CustomModel.DomainServices.ClaimDomainService", query);
-					if (MethodsInfo != null)
-					{
-						getListMethodInfo = MethodsInfo.ListMethodInfo;
-						getCountMethodInfo = MethodsInfo.CountMethodInfo;
-						context = MethodsInfo.context;
-						stop = true;
-					}
-				}
 
 				if (stop == false)
 				{
@@ -328,7 +317,7 @@ namespace WebFreight.Web.Helpers
 						stop = true;
 					}
 				}
-				if (stop == false)
+                if (stop == false)
 				{
 					MethodsInfo = getMethodsInfo("WebFreight.Web.AccountingModel.DomainServices.TaxReportDomainService", query);
 					if (MethodsInfo != null)
