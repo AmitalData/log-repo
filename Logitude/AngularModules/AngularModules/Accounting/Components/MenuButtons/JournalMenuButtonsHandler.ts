63a661c730a38f1c067a5103577b57888f9d5606
Libby Hasid <libby@amital.co.il>
2025-06-30 12:35:25 +0300

    #121272

--- Logitude/AngularModules/AngularModules/Accounting/Components/MenuButtons/JournalMenuButtonsHandler.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/MenuButtons/JournalMenuButtonsHandler.ts
@@ -114,9 +114,8 @@ export class JournalMenuButtonsHandler {
                         case "JournalPrint":
                             {
                                 button.IsDisabled = false;
-
                                 if (!AppTool.IsNullOrEmpty(SessionLocator.LoggedUserPM.SecurityLevel) && SessionLocator.LoggedUserPM.SecurityLevel <= this.EntityPM.SecurityLevel)
-                               
+                                    button.IsDisabled = true;
                                 break;
                             }
                         case "CopyJournal":
