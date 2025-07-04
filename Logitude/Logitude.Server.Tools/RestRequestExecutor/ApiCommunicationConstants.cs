a3fa1e8410b8e6ad8926c707e3e6a01e033dd440
Sohaib Nemer <sohaib@amital.co.il>
2025-07-04 12:27:09 +0300

    #120872 #121433 #121440

--- Logitude/Logitude.Server.Tools/RestRequestExecutor/ApiCommunicationConstants.cs
+++ Logitude/Logitude.Server.Tools/RestRequestExecutor/ApiCommunicationConstants.cs
@@ -10,10 +10,11 @@ namespace Logitude.Server.Tools.RestRequestExecutor
     public  class ApiCommunicationConstants
     {
         public const char TypeQueue = 'Q';
-        public const char InOut = 'O';        
         public const string DefaultFolder = "ExternalTasksQueue";
         public  string Subject { get; set; }
         public  string EntityId { get; set; }
         public  string ObjectTableId { get; set; }
-    }
+        public  string InOut { get; set; }
+
+}
 }
