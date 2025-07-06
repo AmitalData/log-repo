fdf144a3f2d0dd059326c6679e7ca07614a846cc
Libby Hasid <libby@amital.co.il>
2025-07-06 09:16:34 +0300

    #119772

--- Logitude/JustWebFreight/WebFreight.Web/ExternalAPIs/V1/APInvoiceController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/ExternalAPIs/V1/APInvoiceController.cs
@@ -97,7 +97,7 @@ namespace WebFreight.Web.ExternalAPIs.V1
                 {
                     using (TransactionScope scope = TransactionFactory.GetTransaction())
                     {
-                        GetAndCreateAPInvoice(apinvoice, oldEntity);
+                        ProcessAndCreateAPInvoice(apinvoice, oldEntity);
                         scope.Complete();
 
                         return Request.CreateResponse(HttpStatusCode.OK, apinvoice);
@@ -120,7 +120,7 @@ namespace WebFreight.Web.ExternalAPIs.V1
             return Request.CreateResponse(HttpStatusCode.InternalServerError, "Unknown error");
 
         }
-        public void GetAndCreateAPInvoice(APInvoice apinvoice, APInvoice oldEntity)
+        private void ProcessAndCreateAPInvoice(APInvoice apinvoice, APInvoice oldEntity)
         {
             string token = HttpContext.Current.Request.Headers["Token"];
             AuthenticationToken authToken = AuthenticationTokenRepository.GetSingleTokenFromCache(token);
