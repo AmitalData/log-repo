bfb4af48b279ca417cc0e3b3ab88df24db4882df
Frida <Frida@FRIDA-11PC>
2025-07-08 09:51:07 +0300

    #121668

--- Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/GeneratedUpdate/InvoiceModel/EntityUpdateClasses/ARInvoiceUpdateClass.cs
+++ Logitude/JustWebFreight/WebFreight.Web/MetaDataUpdate/GeneratedUpdate/InvoiceModel/EntityUpdateClasses/ARInvoiceUpdateClass.cs
@@ -25,7 +25,7 @@ namespace WebFreight.Web.MetaDataUpdate.GeneratedUpdate.InvoiceModel.EntityUpdat
 {
    public class ARInvoiceUpdateClass
    {  		
- 		public const string HashString = "9f8b4580bab3c8952ca0c8ud004ce487";
+ 		public const string HashString = "9f8b4580bab38422c89ud004ce487";
 	    public void AddObjectTable(Dictionary<string, ObjectTable> objectTables, Dictionary<string, TextCode> textCodes,ObjectTableRepository ObjectTableRepository,TextCodeRepository TextCodeRepository,int contextTenant=0)
         {                     
             
@@ -17176,11 +17176,12 @@ namespace WebFreight.Web.MetaDataUpdate.GeneratedUpdate.InvoiceModel.EntityUpdat
 
             TextCode ARInvoiceTextCode_ARInvoiceOReconfirm = AddTextCodes.AddTextCode(new TextCodeDetails() { Code = "ARInvoice.O.Reconfirm", DefaultText = "Reconfirm", LocalDefaultText = @"אשר מחדש", ObjectTableId = ARInvoiceObjectTable.Id, Tenant = 0, TextCodeTypeCode = "O", IsSpellChecked = false }, TextCodeRepository, TextCodes, contextTenant);
 
- 		   TextCode ARInvoiceTextCode_ARInvoiceODraftInvoice = AddTextCodes.AddTextCode(new TextCodeDetails() { Code = "ARInvoice.O.DraftInvoice", DefaultText = "Draft Invoice", LocalDefaultText = @"חשבונית טיוטה", ObjectTableId = ARInvoiceObjectTable.Id, Tenant = 0, TextCodeTypeCode = "O", IsSpellChecked = false }, TextCodeRepository, TextCodes);
+ 		   TextCode ARInvoiceTextCode_ARInvoiceODraftInvoice = AddTextCodes.AddTextCode(new TextCodeDetails() { Code = "ARInvoice.O.DraftInvoice", DefaultText = "Draft Invoice", LocalDefaultText = @"חשבונית טיוטה", ObjectTableId = ARInvoiceObjectTable.Id, Tenant = 0, TextCodeTypeCode = "O", IsSpellChecked = false }, TextCodeRepository, TextCodes, contextTenant);
 
- 		   TextCode ARInvoiceTextCode_ARInvoiceOApprovalInvoice = AddTextCodes.AddTextCode(new TextCodeDetails() { Code = "ARInvoice.O.ApprovalInvoice", DefaultText = "This invoice is already approved", LocalDefaultText = @"חשבונית זו כבר אושרה", ObjectTableId = ARInvoiceObjectTable.Id, Tenant = 0, TextCodeTypeCode = "O", IsSpellChecked = false }, TextCodeRepository, TextCodes);
+ 		   TextCode ARInvoiceTextCode_ARInvoiceOApprovalInvoice = AddTextCodes.AddTextCode(new TextCodeDetails() { Code = "ARInvoice.O.ApprovalInvoice", DefaultText = "This invoice is already approved", LocalDefaultText = @"חשבונית זו כבר אושרה", ObjectTableId = ARInvoiceObjectTable.Id, Tenant = 0, TextCodeTypeCode = "O", IsSpellChecked = false }, TextCodeRepository, TextCodes, contextTenant);
 
- 		   TextCode ARInvoiceTextCode_ARInvoiceOInvoiceInProgress = AddTextCodes.AddTextCode(new TextCodeDetails() { Code = "ARInvoice.O.InvoiceInProgress", DefaultText = "The invoice is already in the approval process", LocalDefaultText = @"החשבונית כבר נמצאת בתהליך אישור", ObjectTableId = ARInvoiceObjectTable.Id, Tenant = 0, TextCodeTypeCode = "O", IsSpellChecked = false }, TextCodeRepository, TextCodes);
+ 		   TextCode ARInvoiceTextCode_ARInvoiceOInvoiceInProgress = AddTextCodes.AddTextCode(new TextCodeDetails() { Code = "ARInvoice.O.InvoiceInProgress", DefaultText = "The invoice is already in the approval process", LocalDefaultText = @"החשבונית כבר נמצאת בתהליך אישור", ObjectTableId = ARInvoiceObjectTable.Id, Tenant = 0, TextCodeTypeCode = "O", IsSpellChecked = false }, TextCodeRepository, TextCodes, contextTenant);
+ 		   TextCode ARInvoiceTextCode_ARInvoiceOInvoiceSequence = AddTextCodes.AddTextCode(new TextCodeDetails() { Code = "ARInvoice.O.InvoiceSequence", DefaultText = "Invoice Sequence", LocalDefaultText = @"רצף חשבוניות", ObjectTableId = ARInvoiceObjectTable.Id, Tenant = 0, TextCodeTypeCode = "O", IsSpellChecked = false }, TextCodeRepository, TextCodes, contextTenant);
 
 
         }
