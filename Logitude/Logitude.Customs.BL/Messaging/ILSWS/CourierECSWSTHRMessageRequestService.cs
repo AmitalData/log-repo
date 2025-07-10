0fb1796b25e4682bea6e663af0545364a008d5d0
Elon Hezi <Elon@amital.co.il>
2025-07-10 14:48:22 +0300

    #121752

--- Logitude/Logitude.Customs.BL/Messaging/ILSWS/CourierECSWSTHRMessageRequestService.cs
+++ Logitude/Logitude.Customs.BL/Messaging/ILSWS/CourierECSWSTHRMessageRequestService.cs
@@ -238,7 +238,8 @@ namespace Logitude.Customs.BL.Messaging.ILSWS
 
 
             };
-
+            if (courierHawbMamanModel?.BoxBarcode?.Length > 10)
+                courierHawbMamanModel.BoxBarcode = courierHawbMamanModel.BoxBarcode.Substring(courierHawbMamanModel.BoxBarcode.Length - 10);
 
             if (!string.IsNullOrWhiteSpace(declarationCourierStatusPM.TruckerId))
             {
