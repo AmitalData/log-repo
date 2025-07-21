21f80706c62ed270670c46379899cb0e223c89f5
Dror Granot <dror091@gmail.com>
2025-07-21 16:03:25 +0300

    Round 2

--- Logitude/AngularModules/CypressTesting/Export/cypress/actions/CurrencyRateRunInBatchActions.ts
+++ Logitude/AngularModules/CypressTesting/Export/cypress/actions/CurrencyRateRunInBatchActions.ts
@@ -9,55 +9,59 @@ import { CurrencyRateRunInBatchSelectors } from '../selectors/CurrencyRateRunInB
 
 
 export function NavigatesCurrencyRatesWizerd() {
-    
+
     cy.Click(BaseExportSelectors.CustomsRequest, null)
-    cy.Click(CurrencyRateRunInBatchSelectors.RequestSheet, null) 
+    cy.Click(CurrencyRateRunInBatchSelectors.RequestSheet, null)
 }
 
 export function FillCurrencyRatesDetails(CurrencyRateRunInBatchDetails: CurrencyRateRunInBatchDetails) {
-  
+
     const now = new Date();
-    cy.FillDate(CurrencyRateRunInBatchSelectors.FromDate,now.toLocaleDateString('fr-FR') );
+    cy.FillDate(CurrencyRateRunInBatchSelectors.FromDate, now.toLocaleDateString('fr-FR'));
     cy.FillDate(CurrencyRateRunInBatchSelectors.ToDate, now.toLocaleDateString('fr-FR'));
     cy.FillLogLov(CurrencyRateRunInBatchSelectors.CurrencyTypeId, CurrencyRateRunInBatchDetails.CurrencyTypeId, true);
-    cy.Click(CurrencyRateRunInBatchSelectors.SendOptions,null);
-    cy.Click(CurrencyRateRunInBatchSelectors.RunInBatch,null);
-    cy.Click(CurrencyRateRunInBatchSelectors.Approve,null);
-    cy.Click(CurrencyRateRunInBatchSelectors.Cancel,null);
-    cy.wait(15000);
-    cy.Click(CurrencyRateRunInBatchSelectors.RequestsSheets,null);
+    cy.Click(CurrencyRateRunInBatchSelectors.SendOptions, null);
+    cy.Click(CurrencyRateRunInBatchSelectors.RunInBatch, null);
+    cy.Click(CurrencyRateRunInBatchSelectors.Approve, null);
+    cy.Click(CurrencyRateRunInBatchSelectors.Cancel, null);
+    cy.wait(10000);
+    cy.Click(CurrencyRateRunInBatchSelectors.RequestsSheets, null);
     cy.Click(CurrencyRateRunInBatchSelectors.Search, null);
 
 }
 
 export function FillRequestSheets(currencyRateRunInBatchDetails: CurrencyRateRunInBatchDetails) {
     cy.wait(10000);
-
-       cy.FillLogLov(CurrencyRateRunInBatchSelectors.ManageCustomsRequests,currencyRateRunInBatchDetails.ManageCustomsRequests,true);
-    // cy.get(CurrencyRateRunInBatchSelectors.ManageCustomsRequests).type(currencyRateRunInBatchDetails.ManageCustomsRequests);
-    cy.Click(CurrencyRateRunInBatchSelectors.RequestStatus,null);
-    cy.get(CurrencyRateRunInBatchSelectors.CheckAll).click({force: true});
+    cy.FillLogLov(CurrencyRateRunInBatchSelectors.ManageCustomsRequests, currencyRateRunInBatchDetails.ManageCustomsRequests, true);
+    cy.get(CurrencyRateRunInBatchSelectors.RequestStatus).then($status => {
+        if ($status.length) {
+            cy.get(CurrencyRateRunInBatchSelectors.RequestStatus).click({ force: true });
+        } else {
+            cy.get(CurrencyRateRunInBatchSelectors.RequestStatus1).click({ force: true });
+        }
+    });
+    cy.get(CurrencyRateRunInBatchSelectors.CheckAll).click({ force: true });
     cy.get(CurrencyRateRunInBatchSelectors.Reference).click();
-    
+
 }
 
 
 export function CheckStatusRequest() {
-    
+
     cy.get('#LogGrid_0_12rowtemplate0').then(($elements) => {
         // בדיקת מספר האלמנטים
         expect($elements.length).to.be.greaterThan(0);
-      
+
         // בדיקת טקסט בתוך האלמנט הראשון
-        expect($elements.first().text()).not.to.include('בקשה נכשלה');
-      });
+        expect($elements.first().text()).to.contain('תשובה נותחה');
+    });
 
 }
-   
 
 
 
-  
+
+
 
 
 
