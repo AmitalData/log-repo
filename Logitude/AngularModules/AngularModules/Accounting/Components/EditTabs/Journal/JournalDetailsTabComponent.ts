bb82a1a203267158731fb1e0866c29496c4e4b85
Libby Hasid <libby@amital.co.il>
2025-07-22 10:19:03 +0300

    #122410

--- Logitude/AngularModules/AngularModules/Accounting/Components/EditTabs/Journal/JournalDetailsTabComponent.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/EditTabs/Journal/JournalDetailsTabComponent.ts
@@ -1607,6 +1607,7 @@ class JournalLineModel extends BaseComponent {
                     this.parent.EntityWarningsList.push(TextCodeTranslator.Translate("Journal.O.DifferenceExchangeRate"));
                 }
             })
+            this.AmountChanged('foreign', this.LocalAmount, this.ForeignAmount);
             return userExchageRate && Math.abs(this.currencyRate - userExchageRate) > 0.05;
         }
         else {
