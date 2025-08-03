f970b4eed11fade89fdc93147bfdd117b55899a8
Ori Leibovich <Ori@amital.co.il>
2025-08-04 00:52:43 +0300

    #123134

--- Logitude/Logitude.Customs.BL/TraceEvents/AmitalInsertToQueueService.cs
+++ Logitude/Logitude.Customs.BL/TraceEvents/AmitalInsertToQueueService.cs
@@ -80,11 +80,13 @@ namespace Logitude.Customs.BL.TraceEvents
             Logitude.AmitalMessaging.Infrastructure.FuStatus.LOGICUSTFILE LogistictFile = new Logitude.AmitalMessaging.Infrastructure.FuStatus.LOGICUSTFILE();
 
             bool ifCurrecyEquals = false;
-            if (myDeclaration.SupplierInvoices.Count > 0)
+            var invoices = myDeclaration.SupplierInvoices;
+            var baseCode = invoices[0]?.InvoiceCurrencyTypeCode;
+            if (!string.IsNullOrEmpty(baseCode) && invoices != null && invoices.Count > 0)
             {
-                ifCurrecyEquals = myDeclaration.SupplierInvoices.TrueForAll(s => s.InvoiceCurrencyTypeCode.Equals(myDeclaration.SupplierInvoices[0].InvoiceCurrencyTypeCode));
-
+                ifCurrecyEquals = invoices.All(s => s != null && string.Equals(s.InvoiceCurrencyTypeCode, baseCode, StringComparison.OrdinalIgnoreCase));
             }
+
             LogistictFile.logitudeCustomsFile = new LogitudeCustomsFiles()
             {
                 customFileNo = myDeclaration?.CustomFileNo,
