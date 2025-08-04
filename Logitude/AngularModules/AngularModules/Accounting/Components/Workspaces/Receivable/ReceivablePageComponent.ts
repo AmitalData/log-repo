1ca652f01a2807c19546b353cfd1a9b06bc2d577
Jeremy Cohen <Jeremy@amital.co.il>
2025-08-04 14:01:00 +0300

    #123264

--- Logitude/AngularModules/AngularModules/Accounting/Components/Workspaces/Receivable/ReceivablePageComponent.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Components/Workspaces/Receivable/ReceivablePageComponent.ts
@@ -825,6 +825,10 @@ export class ReceivablePageComponent {
                     if (!myResponse.HasError) {
                         var myResult: GLAccountList[] = myResponse.Result;
 
+                        // sort desc
+                        const sortField = this.SelectedDeptorsFilter.EnglishName === 'Balance Due'? 'LocalBalanceInDue': 'BalanceInLocalCurrency';
+                        myResult.sort((a, b) => b[sortField] - a[sortField]);
+
                         this.TopDebtorsList = myResult;
 
                         console.log("DEPTORS: ", myResult);
