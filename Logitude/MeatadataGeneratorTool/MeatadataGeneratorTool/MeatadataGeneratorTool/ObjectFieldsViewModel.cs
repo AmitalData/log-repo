0e85b2a9b2e890ec51cf471bc809ea64ed635255
Sohaib Nemer <sohaib@amital.co.il>
2025-08-13 15:29:58 +0300

    #123506

--- Logitude/MeatadataGeneratorTool/MeatadataGeneratorTool/MeatadataGeneratorTool/ObjectFieldsViewModel.cs
+++ Logitude/MeatadataGeneratorTool/MeatadataGeneratorTool/MeatadataGeneratorTool/ObjectFieldsViewModel.cs
@@ -1603,7 +1603,8 @@ namespace MeatadataGeneratorTool
         public bool IsSpellCheckedListLocalDefaultText { get; internal set; }
 
         string objectFieldDataMapping;
-       
+        bool objectFieldDBOnly;
+
         string modelName;
         public string ModelName
         {
@@ -1616,6 +1617,15 @@ namespace MeatadataGeneratorTool
             set
             {
                 objectFieldDataMapping = value; FirePropertyChanged("ObjectFieldDataMapping");
+            }
+        }
+        public bool ObjectFieldDBOnly
+        {
+
+            get { return objectFieldDBOnly; }
+            set
+            {
+                objectFieldDBOnly = value; FirePropertyChanged("ObjectFieldDBOnly");
             }
         }
 
