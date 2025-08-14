290441cff71d5b2adac9325612b674ebf1273055
Ori Leibovich <Ori@amital.co.il>
2025-08-14 15:18:34 +0300

    #123676

--- Logitude/Unifreight.Data/AmitalModel/AmitalContext.Template.cs
+++ Logitude/Unifreight.Data/AmitalModel/AmitalContext.Template.cs
@@ -418,12 +418,12 @@ namespace Unifreight.Data.AmitalModel
                 .Property(p => p.DESCOFGOODS2)
                     .HasColumnName(@"DESC_OF_GOODS2")
                     .HasMaxLength(30)
-                    .HasColumnType("varchar2");
+                    .HasColumnType("nvarchar2");
             modelBuilder.Entity<CCUMSHGR>()
                 .Property(p => p.DESCOFGOODS3)
                     .HasColumnName(@"DESC_OF_GOODS3")
                     .HasMaxLength(30)
-                    .HasColumnType("varchar2");
+                    .HasColumnType("nvarchar2");
             modelBuilder.Entity<CCUMSHGR>()
                 .Property(p => p.CARRIERID)
                     .HasColumnName(@"CARRIER_ID")
@@ -11513,12 +11513,12 @@ namespace Unifreight.Data.AmitalModel
                 .Property(p => p.DESCOFGOODS2)
                     .HasColumnName(@"DESC_OF_GOODS2")
                     .HasMaxLength(30)
-                    .HasColumnType("varchar");
+                    .HasColumnType("nvarchar");
             modelBuilder.Entity<CCUMSHGR>()
                 .Property(p => p.DESCOFGOODS3)
                     .HasColumnName(@"DESC_OF_GOODS3")
                     .HasMaxLength(30)
-                    .HasColumnType("varchar");
+                    .HasColumnType("nvarchar");
             modelBuilder.Entity<CCUMSHGR>()
                 .Property(p => p.CARRIERID)
                     .HasColumnName(@"CARRIER_ID")
