171789f26e3da5e2ad3366588f706c4d7a36eec7
Elon Hezi <Elon@amital.co.il>
2025-09-01 20:53:43 +0300

    #123645

--- Logitude/JustWebFreight/WebFreight.Web/WebPages/APILogsDownLoadPage.aspx.cs
+++ Logitude/JustWebFreight/WebFreight.Web/WebPages/APILogsDownLoadPage.aspx.cs
@@ -18,7 +18,7 @@ namespace WebFreight.Web.WebPages
     {
         public bool CheckAvailablityTenantsForEmail(string email, int tenant)
         {
-            UserRepository userRep = new UserRepository(0);
+            UserRepository userRep = new UserRepository(tenant);
             Simplog.Data.CommonDataModel.EntityPOCOs.User user = userRep.GetSingleUserByEmail(email, 0, false);
 
             bool available = true;
