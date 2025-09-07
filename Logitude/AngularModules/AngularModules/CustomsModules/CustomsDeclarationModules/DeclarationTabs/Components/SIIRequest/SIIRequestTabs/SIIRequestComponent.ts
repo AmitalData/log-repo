53d0d945c895c6847e6a2e3d1d57d8fe29891670
Sohaib Nemer <sohaib@amital.co.il>
2025-09-07 22:52:09 +0300

    #123874

--- Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationTabs/Components/SIIRequest/SIIRequestTabs/SIIRequestComponent.ts
+++ Logitude/AngularModules/AngularModules/CustomsModules/CustomsDeclarationModules/DeclarationTabs/Components/SIIRequest/SIIRequestTabs/SIIRequestComponent.ts
@@ -339,7 +339,7 @@ export class SIIRequestComponent extends BaseComponent implements OnInit {
                         const errDlg = new ConfirmWindow();
                         errDlg.YesButtonText = TextCodeTranslator.Translate('General.B.Close');
                         errDlg.ShowNoButton = false;
-                        errDlg.Title = 'Errors Found';
+                        errDlg.Title = TextCodeTranslator.Translate('Customs.General.B.OK');
                         errDlg.IsMultipleMessages = true;
                         errDlg.ShowErorImage = true;
                         errDlg.Show(
@@ -350,8 +350,8 @@ export class SIIRequestComponent extends BaseComponent implements OnInit {
                     (err: HttpErrorResponse) => {
                         const dlg = new ConfirmWindow();
                         dlg.YesButtonText = TextCodeTranslator.Translate('General.B.Close');
+                        dlg.Title = TextCodeTranslator.Translate('Customs.General.B.OK');
                         dlg.ShowNoButton = false;
-                        dlg.Title = 'Errors Found';
                         dlg.IsMultipleMessages = true;
                         dlg.ShowErorImage = true;
                         dlg.Show(extractMessage(err));
