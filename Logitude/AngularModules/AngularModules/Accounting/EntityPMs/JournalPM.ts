a2b434a2e336da6630201b7136a2761c658f3251
Libby Hasid <libby@amital.co.il>
2025-09-15 16:25:08 +0300

    #123448

--- Logitude/AngularModules/AngularModules/Accounting/EntityPMs/JournalPM.ts
+++ Logitude/AngularModules/AngularModules/Accounting/EntityPMs/JournalPM.ts
@@ -321,6 +321,11 @@ export class JournalPM {
     public set IsExternalEntity(newValue: boolean) { if (this.isExternalEntity != newValue) { this.isExternalEntity = newValue; this.MarkAsDirty("IsExternalEntity"); } }
        
 	 
+    private invoicesXml: string;
+    public get InvoicesXml() { return this.invoicesXml; }
+    public set InvoicesXml(newValue: string) { if (this.invoicesXml != newValue) { this.invoicesXml = newValue; this.MarkAsDirty("InvoicesXml"); } }
+       
+	 
 
     public OldEntityPM: JournalPM;
 		
--- Logitude/Logitude.Accounting.Data/EntityMapping/JournalMap.cs
+++ Logitude/Logitude.Accounting.Data/EntityMapping/JournalMap.cs
@@ -87,6 +87,8 @@ namespace Logitude.Accounting.Data.EntityMapping
             this.Property(t => t.CurrencyId).HasColumnName("CurrencyId").HasMaxLength(15).IsUnicode(false);
 
             this.Property(t => t.SecurityLevel).HasColumnName("SecurityLevel");
+
+            this.Property(t => t.InvoicesXml).HasColumnName("InvoicesXml").IsMaxLength().IsUnicode(true);
         }
     }
 }
