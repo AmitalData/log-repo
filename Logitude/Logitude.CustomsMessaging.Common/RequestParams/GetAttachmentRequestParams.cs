546495283ba5d9a061c5160ba1e570dbddf93a45
Elon Hezi <Elon@amital.co.il>
2025-10-05 17:34:19 +0300

    #117235

---
+++ Logitude/Logitude.CustomsMessaging.Common/RequestParams/GetAttachmentRequestParams.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace Logitude.CustomsMessaging.Common.RequestParams
+{
+    public class GetAttachmentRequestParams : RequestParamsBase
+    {
+        public int customsItemId { get; set; }
+        public DateTime validToDate { get; set; }
+        public int languageType { get; set; }
+    }
+}
---
+++ Logitude/Logitude.CustomsMessaging/RequestServices/GetDOC8318_Web_GetAttachmentRequestService.cs
@@ -0,0 +1,32 @@
+﻿using Logitude.CustomsMessaging.Common.RequestParams;
+using UnifreightIIG.Common.CustomItemMekachServiceReference;
+using UnifreightIIG.Common.GetAttachmentServiceReference;
+
+
+namespace Logitude.CustomsMessaging.RequestServices
+{
+    public class GetDOC8318_Web_GetAttachmentRequestService : RequestServiceBase<DOC_Web_GetAttachment_IN, GetAttachmentRequestParams>
+    {
+        public override DOC_Web_GetAttachment_IN GetRequest(GetAttachmentRequestParams requestParams)
+        {
+            DOC_Web_GetAttachment_IN myMsg = new DOC_Web_GetAttachment_IN();
+            
+            // TODO: fix the params to send to the service:
+
+            //myMsg.CIMekachIn = new UnifreightIIG.Common.GetAttachmentServiceReference.CustomsBookItemHeaderIn()
+            //{
+            //    customsItemId = requestParams.customsItemId,
+            //    customsItemIdSpecified = true,
+            //    validToDate = requestParams.validToDate,
+            //    languageType = requestParams.languageType,
+            //};
+
+
+
+            this.MyRequestSheetParam = new RequestSheetParam();
+            this.MyRequestSheetParam.RequestDescription = "תדפיסי חקיקה- קובץ (מקח''ים) - ספר סיווג";
+            return myMsg;
+
+        }
+    }
+}
