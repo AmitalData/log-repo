643039a3eccf2e13a784cf403d2028bde9a93350
Jeremy Cohen <Jeremy@amital.co.il>
2025-10-16 10:21:47 +0300

    #124970

--- Logitude/AngularModules/AngularModules/Report/Components/FilterReportComponent/RevenueExpenseFilterComponent.ts
+++ Logitude/AngularModules/AngularModules/Report/Components/FilterReportComponent/RevenueExpenseFilterComponent.ts
@@ -81,12 +81,11 @@ export class RevenueExpenseFilterComponent extends BaseComponent {
     public BalanceOptionsFilterList: CodeNameClass[];
     private BuildFilterList() {
         this.BalanceOptionsFilterList = [];
-        this.BalanceOptionsFilterList.push(new CodeNameClass(RevenueExpenseFilterComponent.ALL_OPTION,  
+        this.BalanceOptionsFilterList.push(new CodeNameClass(RevenueExpenseFilterComponent.ALL_OPTION, "All Accounts",
             TextCodeTranslator.Translate("Accounting.General.O.AllAccountsOption")));
 
-        this.BalanceOptionsFilterList.push(new CodeNameClass(RevenueExpenseFilterComponent.WITH_OPTION,  
-            TextCodeTranslator.Translate("Accounting.General.O.WithTransactionsOption")
-));
+        this.BalanceOptionsFilterList.push(new CodeNameClass(RevenueExpenseFilterComponent.WITH_OPTION, "Accounts with Transactions",
+            TextCodeTranslator.Translate("Accounting.General.O.WithTransactionsOption")));
         this.SelectedBalanceOptionFilter = this.BalanceOptionsFilterList.filter(d => d.Code === RevenueExpenseFilterComponent.ALL_OPTION)[0];
     }
 
