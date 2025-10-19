bb4b3520e268eac624b79a46cc17a55682c243ec
Elon Hezi <Elon@amital.co.il>
2025-10-19 09:42:19 +0300

    #125089

--- Logitude/Logitude.Customs.Data/EntityMapping/ClientAddressMap.cs
+++ Logitude/Logitude.Customs.Data/EntityMapping/ClientAddressMap.cs
@@ -46,7 +46,7 @@ namespace Logitude.Customs.Data.EntityMapping
 
             this.Property(t => t.IsHebrewAddress).HasColumnName("IsHebrewAddress").IsRequired();
 
-            this.Property(t => t.BranchName).HasColumnName("BranchName").HasMaxLength(35).IsUnicode(true);
+            this.Property(t => t.BranchName).HasColumnName("BranchName").HasMaxLength(255).IsUnicode(true);
 
             this.Property(t => t.ContactIdentifier).HasColumnName("ContactIdentifier").HasMaxLength(9).IsUnicode(false);
 
