9e3f299c555db510cea49b3214c4715052aced4c
Sohaib Nemer <sohaib@amital.co.il>
2025-10-27 17:23:34 +0200

    #125521

--- Logitude/Logitude.Customs.Data/EntityListQueryServices/InterfaceManagementListQueryService.cs
+++ Logitude/Logitude.Customs.Data/EntityListQueryServices/InterfaceManagementListQueryService.cs
@@ -86,6 +86,8 @@ namespace Logitude.Customs.Data.EntityListQueryServices
                                                              SendTime = s.SendTime,
                                                              EntityLockId = a.EntityLockId,
                                                              EntityLockName = a.EntityLock != null ? a.EntityLock.Name : null,
+                                                             IsUnifreight = a.IsUnifreight,
+                                                             IsCustomsFile = a.IsCustomsFile,
 
                                                          });
             return query;
