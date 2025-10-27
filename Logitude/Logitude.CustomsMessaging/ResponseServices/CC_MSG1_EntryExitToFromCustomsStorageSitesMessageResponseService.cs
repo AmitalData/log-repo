9d171df5e48c827186e14b65da862cb923ee1b26
Shoshana Greenwald <shoshana@amital.co.il>
2025-10-27 10:30:07 +0200

    #125412

--- Logitude/Logitude.CustomsMessaging/ResponseServices/CC_MSG1_EntryExitToFromCustomsStorageSitesMessageResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/CC_MSG1_EntryExitToFromCustomsStorageSitesMessageResponseService.cs
@@ -56,7 +56,7 @@ namespace Logitude.CustomsMessaging.ResponseServices
                 var thirdCargoID = customResponse.ReportingDetails.cargoIdentifier.cargoIdentifierKey3;
 
                 var declarationQueryService = new DeclarationQueryService(myDbContext);
-                var declaration = declarationQueryService.GetDeclarationByConsignment(cargoTypeCode.ToString(), manifestNumber, secondCargoID, thirdCargoID);
+                var declaration = declarationQueryService.GetDeclarationByConsignment(requestParams.Tenant, cargoTypeCode.ToString(), manifestNumber, secondCargoID, thirdCargoID);
                 if (declaration != null && declaration.Id != null)
                 {
                     this.MyResponseData.UserMessage = " התקבל מסר יציאה ממסוף " + declaration.CustomFileNo;
