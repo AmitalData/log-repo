5b3eba9e777fee5a594208438fdfa759cac2a08e
Sohaib Nemer <sohaib@amital.co.il>
2025-10-29 15:54:24 +0200

    #125552

--- Logitude/Logitude.Customs.BL/EntityUpdateServices/CustomsDocumentsTicketUpdateService.cs
+++ Logitude/Logitude.Customs.BL/EntityUpdateServices/CustomsDocumentsTicketUpdateService.cs
@@ -117,6 +117,17 @@ namespace Logitude.Customs.BL.EntityUpdateServices
 
                 }
             }
+            if (entityPM?.CustomsDocumentPointers.Count > 1
+                && entityPM.RequestedCustomsDocId != null
+                && entityPM.CustomsDocumentPointers.Any(x => x.ChangeSetOp == ChangeSetOperation.Delete))
+            {
+                var survivor = entityPM.CustomsDocumentPointers
+                       .FirstOrDefault(x => x.ChangeSetOp == ChangeSetOperation.None);
+                if (survivor != null)
+                {
+                    survivor.ChangeSetOp = ChangeSetOperation.Insert;
+                }
+            }
             UpdateNotification(entityPM);
             SetDeclarationAsChanged(entityPM);
 
