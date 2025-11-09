e570af4dc4b486319e885a25f0de95112b6ed637
Dror Granot <dror091@gmail.com>
2025-11-09 15:10:31 +0200

    asd

--- Logitude/AngularModules/AngularModules/Infrastructure/Utilities/TextCodeTranslator.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Utilities/TextCodeTranslator.ts
@@ -122,7 +122,7 @@ export class TextCodeTranslator {
                 window.TextCodesCache.push(translationObject);
             }
             else {
-                if (isNullOrUndefined(translationObject) || translation == "") { console.log("TextCodeTranslator - TranslateCached - Not Found Translation for Code: " + value); };
+              console.log("TextCodeTranslator - TranslateCached - Not Found Translation for Code: " + value); ;
 
                 // if (this.ShowAlertMessage(value)) {
                 //     if (SessionLocator.LoggedUserPM.Email?.includes("logitudeworld.com")) 
