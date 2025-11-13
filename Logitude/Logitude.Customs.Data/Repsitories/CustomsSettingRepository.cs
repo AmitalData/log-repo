e7dc97d016a8fb348da0f8e19e7186614affb73a
Sohaib Nemer <sohaib@amital.co.il>
2025-11-13 17:11:48 +0200

    FIX

--- Logitude/Logitude.Customs.Data/Repsitories/CustomsSettingRepository.cs
+++ Logitude/Logitude.Customs.Data/Repsitories/CustomsSettingRepository.cs
@@ -27,6 +27,7 @@ namespace Logitude.Customs.Data.Repsitories
         public CustomsSetting GetSettingByTenant(int tenant)
         {
             return (from a in context.CustomsSettings
+                    where a.Tenant == tenant
                     select a).FirstOrDefault();
         }
         public DateTime? GetCB_LastUpdateDate(int tenant)
