b2760435cdeba4ac2f3f3b15e7ffaa3c7b73bbcf
Jeremy Cohen <Jeremy@amital.co.il>
2025-11-17 12:31:54 +0200

    #126216

---
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/InfrastructureModel/Extended/DefaultAndConfigurationExtendedController.cs
@@ -0,0 +1,48 @@
+﻿using Logitude.BL.Helpers;
+using Logitude.BL.InfrastructureModel.EntityPMs;
+using Logitude.BL.InfrastructureModel.EntityQueries;
+using Logitude.BL.InfrastructureModel.Tools.EntityService;
+using Simplog.Data.CommonDataModel.EntityPOCOs; using Simplog.Global.Data.GlobalModel.EntityPOCOs;
+using Simplog.Data.CommonDataModel.Repositories;
+using Simplog.Data.InfrastructureModel;
+using Simplog.Data.InfrastructureModel.EntityPOCOs; using Simplog.Global.Data.GlobalModel.EntityPOCOs;
+using Simplog.Data.InfrastructureModel.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WebFreight.Web.Helpers;
+using WebFreight.Web.Security;
+
+namespace WebFreight.Web.Controllers.InfrastructureModel.Extended
+{
+    public class DefaultAndConfigurationExtendedController : ApiController
+    {
+
+        public HttpResponseMessage GetBySetKey(string setKey)
+        {
+            try
+            {
+                string token = HttpContext.Current.Request.Headers["Token"];
+                AuthenticationToken authToken = AuthenticationTokenRepository.GetSingleTokenFromCache(token);
+                int tenant = authToken.Tenant;
+
+                SecurityUtility.AuthenticationOnTenant(tenant);
+
+                DefaultAndConfigurationQuery entityQuery = new DefaultAndConfigurationQuery(tenant);
+                List<DefaultAndConfigurationPM> myResult = entityQuery.GetDefaultAndConfigurationsPMBySetKey(setKey).ToList();
+
+                return Request.CreateResponse(HttpStatusCode.OK, myResult);
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ApiExceptionBuilder.BuildException(ex));
+            }
+        }
+
+    }
+}
