07fe42ce39b15ca4460c55ae91d8313bb9001304
Dror Granot <dror091@gmail.com>
2025-11-17 18:53:57 +0200

    Revert "Merged PR 6944: 118296"
    This reverts commit deb4e37faa0476a4270a4272ad7f0a52642a973d, reversing
    changes made to af553fde82c20f2ded5643c16411786026b75723.

--- Logitude/AngularModules/AngularModules/Accounting/Module_ACCT.ts
+++ Logitude/AngularModules/AngularModules/Accounting/Module_ACCT.ts
@@ -1,11 +1,10 @@
 ﻿import {NgModule} from '@angular/core';
 import {InfrastructureModule} from '../Infrastructure/Module_INFR';
-import {ControlsModule} from '../Controls/Module_CTRL';
 import {Components, ModuleDeclarations} from './ModuleDeclarations';
 import {ModuleProviders} from './ModuleProviders';
 
 @NgModule({
-    imports: [InfrastructureModule, ControlsModule],
+    imports: [InfrastructureModule],
     declarations: [...Components],
     entryComponents: [...Components],
 })
