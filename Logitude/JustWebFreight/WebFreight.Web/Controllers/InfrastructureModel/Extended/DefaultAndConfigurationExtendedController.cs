81f5bece4f8b32e64415b5ca7f145bfc4bc4d97f
Jeremy Cohen <Jeremy@amital.co.il>
2025-11-18 16:08:03 +0200

    #126216

--- Logitude/JustWebFreight/WebFreight.Web/Controllers/InfrastructureModel/Extended/DefaultAndConfigurationExtendedController.cs
+++ Logitude/JustWebFreight/WebFreight.Web/Controllers/InfrastructureModel/Extended/DefaultAndConfigurationExtendedController.cs
@@ -2,11 +2,14 @@
 using Logitude.BL.InfrastructureModel.EntityPMs;
 using Logitude.BL.InfrastructureModel.EntityQueries;
 using Logitude.BL.InfrastructureModel.Tools.EntityService;
-using Simplog.Data.CommonDataModel.EntityPOCOs; using Simplog.Global.Data.GlobalModel.EntityPOCOs;
+using Simplog.Data.CommonDataModel.EntityPOCOs;
+using Simplog.Global.Data.GlobalModel.EntityPOCOs;
 using Simplog.Data.CommonDataModel.Repositories;
 using Simplog.Data.InfrastructureModel;
-using Simplog.Data.InfrastructureModel.EntityPOCOs; using Simplog.Global.Data.GlobalModel.EntityPOCOs;
+using Simplog.Data.InfrastructureModel.EntityPOCOs;
 using Simplog.Data.InfrastructureModel.Repositories;
+using Logitude.BL.GlobalModel.EntityPMs;
+using Logitude.BL.GlobalModel.EntityQueries;
 using System;
 using System.Collections.Generic;
 using System.Linq;
