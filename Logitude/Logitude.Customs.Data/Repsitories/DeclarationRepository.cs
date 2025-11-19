ae30366117896c067b615357c39fac833a2be4bf
elisheva Nachaysi <elisheva@amital.co.il>
2025-11-19 10:53:46 +0200

    #126373

--- Logitude/Logitude.Customs.Data/Repsitories/DeclarationRepository.cs
+++ Logitude/Logitude.Customs.Data/Repsitories/DeclarationRepository.cs
@@ -228,7 +228,7 @@ namespace Logitude.Customs.Data.Repsitories
                     allDecSameFile = qAllCustomFileNo.ToList();
                 }
                 var qGetAcceptDeclarationAmendment = (from a in allDecSameFile
-                                                      where ((a.Id == id && a.AmendmentDontDisplayInList == false && a.DeclarationNumber != null) ||
+                                                      where ((a.Id == id && a.AmendmentDontDisplayInList == false) ||
                             (a.AmendmentOriginalDeclartation == id && a.DeclarationNumber != null && a.AmendmentDontDisplayInList == false))
                             && a.Tenant == tenant
                                                       select a.Id
