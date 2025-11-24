7804fc52846324a0523b97ff142c28c836d18f10
Ariel Partouche <Ariel@amital.co.il>
2025-11-24 11:16:02 +0200

    #125356

--- Logitude/AngularModules/CargoTrackingModules/src/app/app.module.ts
+++ Logitude/AngularModules/CargoTrackingModules/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { CargoTrackingSearchService } from '../CargoTracking/Services/Others/CargoTrackingSearchService';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule  } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CargoTrackingBrandingDataExtendedService } from '../CargoTracking/Services/Others/CargoTrackingBrandingDataExtendedService';// '../../../../../Services/Others/CargoTrackingDataExtendedService';
 import { AppRoutingModule } from './app-routing.module';
@@ -58,6 +58,7 @@ import { SafePipe } from 'src/Infrastructure/Pipes/SafePipe';
 import { StopLoopPipe } from 'src/Infrastructure/Pipes/StopLoopPipe';
 import { TenantManagementService } from 'src/CargoTracking/Services/Others/TenantManagementService';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CookieconsentComponent } from 'src/CargoTracking/Components/cookieconsent/cookieconsent.component';
 
 export function getBaseUrl() {
     return document.getElementsByTagName('base')[0].href;
@@ -94,7 +95,8 @@ export function getBaseUrl() {
         CustomLabelComponent,
         LabelUtilContentDirective,
         LabelUtilElementDirective,
-
+        CookieconsentComponent,
+        
         //Erros
         Error401Component
 
