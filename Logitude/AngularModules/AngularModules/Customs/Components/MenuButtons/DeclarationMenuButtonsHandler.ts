20ed185dfb6b555d5e17649397186c7ade641e85
Sohaib Nemer <sohaib@amital.co.il>
2025-11-30 15:12:27 +0200

    #126883

--- Logitude/AngularModules/AngularModules/Customs/Components/MenuButtons/DeclarationMenuButtonsHandler.ts
+++ Logitude/AngularModules/AngularModules/Customs/Components/MenuButtons/DeclarationMenuButtonsHandler.ts
@@ -1153,27 +1153,22 @@ export class DeclarationMenuButtonsHandler implements OnDestroy {
     ResetDeclarationNumberMethod() {
         this._DeclarationNumberandVersionId = null;//itzik:clear onstart on the house !!!
         if (this.EntityPM.Direction == "E") {
-            this.GetAnyRequestBeforeResetDeclaration("2755E", TextCodeTranslator.Translate("Customs.Declaration.O.CantResetDeclarationNumberSubmissionExsist"));
+            this.GetAnyRequestBeforeResetDeclaration("2755E", TextCodeTranslator.Translate("Customs.Declaration.O.CantResetDeclarationNumberSubmissionExsist"), "9079");
         } else {
-            this.GetAnyRequestBeforeResetDeclaration("2755", TextCodeTranslator.Translate("Customs.Declaration.O.CantResetDeclarationNumberDifferentFromAnalyzed"));
+            this.GetAnyRequestBeforeResetDeclaration("2755", TextCodeTranslator.Translate("Customs.Declaration.O.CantResetDeclarationNumberDifferentFromAnalyzed"), "8373");
         }
     }
-    private GetAnyRequestBeforeResetDeclaration(interfaceTypeCode: string, message: string) {
-        var canResetDeclaration: boolean = this.EntityPM.Direction == 'E' ? true : this.EntityPM.PaymentDate == null;
+    private GetAnyRequestBeforeResetDeclaration(interfaceTypeCode: string, message: string, responseName: string) {
+        const canResetDeclaration: boolean = this.EntityPM.PaymentDate == null;
         if (canResetDeclaration) {
-            if (this.EntityPM.Direction != 'E') {
-                this.RestoreDeclaration(interfaceTypeCode, message, canResetDeclaration)
-            }
-            else {
-                this.CheackIsAnyRequest(interfaceTypeCode, message, canResetDeclaration)
-            }
-
-
+            this.RestoreDeclaration(interfaceTypeCode, message, canResetDeclaration, responseName);
         } else {
-            this.ShowResetDeclarationMessage(TextCodeTranslator.Translate("Customs.Declaration.O.CantResetDeclarationNumberPaymentDate"));
+            this.ShowResetDeclarationMessage(
+                TextCodeTranslator.Translate("Customs.Declaration.O.CantResetDeclarationNumberPaymentDate")
+            );
         }
     }
-    private RestoreDeclaration(interfaceTypeCode, message, canResetDeclaration) {
+    private RestoreDeclaration(interfaceTypeCode, message, canResetDeclaration, responseName: string) {
         SessionLocator.SelectedSession.StartBusyIndicator("checking");
 
         var _DeclarationMessagesService = new DeclarationMessagesService();
@@ -1187,8 +1182,8 @@ export class DeclarationMenuButtonsHandler implements OnDestroy {
         currRequestParams.DeclarationNumber = this.EntityPM.DeclarationNumber;
         currRequestParams.CustomsFile = this.EntityPM.CustomFileNo;
         currRequestParams.RequestVIA = SendRequestVIA.WebServiceInteractive;
-        currRequestParams.ResponseName = "8373"
-        currRequestParams.RequestName = "Restore From ResetDeclaration"
+        currRequestParams.ResponseName = responseName;
+        currRequestParams.RequestName = "Restore From ResetDeclaration";
 
         _DeclarationMessagesService.PostDeclarationRequest(currRequestParams)
             .subscribe((myServiceResponse: ServiceResponse) => {
