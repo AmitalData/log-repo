c53e12ae764b47d406eb1b51ca7529c987612ebd
Sohaib Nemer <sohaib@amital.co.il>
2025-12-22 15:10:25 +0200

    #127685

--- Logitude/Logitude.Customs.BL/BL/AutomatedCourierMessagingService.cs
+++ Logitude/Logitude.Customs.BL/BL/AutomatedCourierMessagingService.cs
@@ -204,6 +204,13 @@ namespace Logitude.Customs.BL.BL
             }
             catch (System.Exception ex)
             {
+                CustomsRequestsSheetDomainModelUtil.ReleaseConcurrentVirtualKey(new GenericRequestParams()
+                {
+                    Tenant = declarationCourierStatusPM.Tenant,
+                    LoggingEntityId = declarationCourierStatusPM.DeclarationId,
+                    LoggingObjectTableId = declarationObjectTableId,
+                    InterfaceTypeCode = "1170",
+                });
                 LogMessagingUtil.Instance.AppendLine($"Exception!!!CreateSheetSBQMessage({declarationCourierStatusPM.DeclarationId}) : {ex.Message}");
             }
         }
@@ -243,6 +250,13 @@ namespace Logitude.Customs.BL.BL
             }
             catch (System.Exception ex)
             {
+                CustomsRequestsSheetDomainModelUtil.ReleaseConcurrentVirtualKey(new GenericRequestParams()
+                {
+                    Tenant = declarationCourierStatusPM.Tenant,
+                    LoggingEntityId = declarationCourierStatusPM.DeclarationId,
+                    LoggingObjectTableId = declarationObjectTableId,
+                    InterfaceTypeCode = "2750",
+                });
                 LogMessagingUtil.Instance.AppendLine($"Exception!!!CreateSheetSBQMessage({declarationCourierStatusPM.DeclarationId}) : {ex.Message}");
             }
         }
@@ -291,6 +305,13 @@ namespace Logitude.Customs.BL.BL
             }
             catch (System.Exception ex)
             {
+                CustomsRequestsSheetDomainModelUtil.ReleaseConcurrentVirtualKey(new GenericRequestParams()
+                {
+                    Tenant = declarationCourierStatusPM.Tenant,
+                    LoggingEntityId = declarationCourierStatusPM.DeclarationId,
+                    LoggingObjectTableId = declarationObjectTableId,
+                    InterfaceTypeCode = "2755",
+                });
                 LogMessagingUtil.Instance.AppendLine($"Exception SendPayment!!!CreateSheetSBQMessage({declarationCourierStatusPM.DeclarationId}) : {ex.Message}");
 
             }
