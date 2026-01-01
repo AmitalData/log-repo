88eda9f65257a3d48451c46890531632114d6baf
Libby Hasid <libby@amital.co.il>
2026-01-01 12:08:10 +0200

    #128083

--- Logitude/Logitude.Accounting.BL/CoreBL/JournalUpdateOnCreating.cs
+++ Logitude/Logitude.Accounting.BL/CoreBL/JournalUpdateOnCreating.cs
@@ -302,8 +302,8 @@ namespace Logitude.Accounting.BL.CoreBL
                 Line = journalLinePM.Line,
                 LedgerTransactionId = transaction.Id,
                 CurrencyId = transaction.OpenAmountCurrencyId,
-                ReconciliationAmount = transaction.OpenAmount,
-                IsPartial =  false
+                ReconciliationAmount = reconciliationAmount,
+                IsPartial = Math.Abs(reconciliationAmount) != Math.Abs(transaction.OpenAmount)
             });
         }
 
