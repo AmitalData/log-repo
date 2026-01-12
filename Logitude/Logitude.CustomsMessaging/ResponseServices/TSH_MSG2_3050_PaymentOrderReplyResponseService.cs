0549f0fea2c6ef4150e042eb4592a5debfc31d32
Sohaib Nemer <sohaib@amital.co.il>
2026-01-12 14:24:19 +0200

    #128453

--- Logitude/Logitude.CustomsMessaging/ResponseServices/TSH_MSG2_3050_PaymentOrderReplyResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/TSH_MSG2_3050_PaymentOrderReplyResponseService.cs
@@ -859,19 +859,7 @@ namespace Logitude.CustomsMessaging.ResponseServices
                 myPaymentOrderLineUpdateService.Update(item, false);
             }
         }
-
-        private void DeletedPaymentOrderConnectionTables(PaymentOrderConnectionTableUpdateService paymentOrderConnectionTableUpdateService, string connectedEntityCode)
-        {
-            foreach (var paymentItem in _PaymentOrderPM.PaymentOrderConnectionTables)
-            {
-                // Delete old connection if it from the same type 
-                if (paymentItem.ConnectedEntityCode == connectedEntityCode)
-                {
-                    paymentItem.ChangeSetOp = ChangeSetOperation.Delete;
-                    paymentOrderConnectionTableUpdateService.Update(paymentItem, false);
-                }
-            }
-        }
+      
     }
 
     public class AnalyzePaymentDocumentManager
