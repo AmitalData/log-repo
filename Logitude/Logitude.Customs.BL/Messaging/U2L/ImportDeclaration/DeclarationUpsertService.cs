85bec889385fdfd0bf5ff601c4b3e5ffa7c2df5f
Elon Hezi <Elon@amital.co.il>
2026-01-27 12:09:50 +0200

    #128277

--- Logitude/Logitude.Customs.BL/Messaging/U2L/ImportDeclaration/DeclarationUpsertService.cs
+++ Logitude/Logitude.Customs.BL/Messaging/U2L/ImportDeclaration/DeclarationUpsertService.cs
@@ -329,8 +329,9 @@ namespace Logitude.Customs.BL.Messaging.U2L.ImportDeclaration
 					this._MyDeclarationPM.TaxationDateTime = DateTime.Now;
 					this._MyDeclarationPM.ExternalDeclarationNumber = (_AmitalCustomsFile.CustomFileNo + DateTime.Today.Year.ToString());
 					this._MyDeclarationPM.SystemConnection = _AmitalCustomsFile.SystemConnection;
-					this._MyDeclarationPM.Consignments[0].ChangeSetOp= ChangeSetOperation.Insert;
-					if (!string.IsNullOrWhiteSpace(_AmitalCustomsFile.CargoTypeCode))
+                    this._MyDeclarationPM.ShipmentId = _AmitalCustomsFile.ShipmentId;
+                    this._MyDeclarationPM.Consignments[0].ChangeSetOp = ChangeSetOperation.Insert;
+                    if (!string.IsNullOrWhiteSpace(_AmitalCustomsFile.CargoTypeCode))
 					{
 						this._MyDeclarationPM.Consignments[0].CargoTypeCode = _AmitalCustomsFile.CargoTypeCode;
 					}
@@ -1085,7 +1086,7 @@ namespace Logitude.Customs.BL.Messaging.U2L.ImportDeclaration
 				MyCommunicationsParams.LoggingEntityId = MyGenericResponseObj.ApplicationId;
 
 				scope.Complete();
-
+				
 			}
 		}
 
