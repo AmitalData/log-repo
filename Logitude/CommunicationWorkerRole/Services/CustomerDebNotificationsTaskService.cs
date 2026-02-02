4b831fd56946b4cb947e570f3e6ef4336abd7fc2
Libby Hasid <libby@amital.co.il>
2026-02-02 10:35:11 +0200

    #129498

--- Logitude/CommunicationWorkerRole/Services/CustomerDebNotificationsTaskService.cs
+++ Logitude/CommunicationWorkerRole/Services/CustomerDebNotificationsTaskService.cs
@@ -47,21 +47,7 @@ namespace CommunicationWorkerRole.Services
     public class CustomerDebNotificationsTaskService
 	{
 
-        int trackerCounter = 0;
-        string[,] trackerLogs = new string[,] //tracker(Step, DateTime)
-        {
-            {"Prepare report scheduler details", null},
-            {"Get report filters", null},
-            {"Build Report Data Provider and get stimul report", null},
-            {"Export pdf report", null},
-            {"Stored pdf report in Blob", null},
-            {"Send email to reciepents", null},
-            {"", null},
-            {"", null},
-            {"", null},
-
-        };
-
+      
         TaskManagerBase currentTask;
 		ReportSchedulerTaskService reportSchedulerTaskService;
 
@@ -140,8 +126,7 @@ namespace CommunicationWorkerRole.Services
                     }
 				}
 				this.currentTask.LogInfo(FTPLogBuilder.BuildLogLine($"Preparing report scheduler details for GLAccount: {glaccount.EnglishName} ({glaccount.DisplayNumber})"));
-				this.trackerLogs[trackerCounter, 1] = DateTime.Now.ToString();
-				this.trackerCounter += 1;
+				
 
 
 				schedulerDetails.ReportDetails.ReportFilterItems.ForEach(filterItem => {
