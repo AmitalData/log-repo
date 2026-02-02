623b7d7434bb2a7e957749568a0471d47436b65a
Sohaib Nemer <sohaib@amital.co.il>
2026-02-02 12:31:14 +0200

    #129191

--- Logitude/AngularModules/AngularModules/Customs/Services/Others/CustomsRequestMenuService.ts
+++ Logitude/AngularModules/AngularModules/Customs/Services/Others/CustomsRequestMenuService.ts
@@ -17,8 +17,9 @@ import { EntityResourceService } from 'Infrastructure/Services/EntityResourceSer
 export class CustomsRequestMenuService {
     private _CustomsRequestMenuItems: CustomsMenuItem[];
     public get CustomsRequestMenuItems() { return this._CustomsRequestMenuItems }
+    private entityResourceService: EntityResourceService = new EntityResourceService();
     private CurrentSession = SessionLocator.SelectedSession;
-    constructor(isReports: any = false,private EntityResourceService: EntityResourceService) {
+    constructor(isReports: any = false) {
         ///alert("CustomsRequestMenuService");
         if (isReports) {
             this.buildReportsList();
@@ -29,7 +30,7 @@ export class CustomsRequestMenuService {
     private buildReportsList() {
         this._CustomsRequestMenuItems = [];
         if (FeatureLocator.HasFeaturePermession("Customs.CustomsVendor", "SettingVehicle")) {
-            this.EntityResourceService.getEntityResourceByTableName("Customs.CustomsVendor").subscribe((response: any) => {
+            this.entityResourceService.getEntityResourceByTableName("Customs.CustomsVendor").subscribe((response: any) => {
                 this._CustomsRequestMenuItems.push(new CustomsMenuItem(TextCodeTranslator.Translate("Customs.Vendor.O.NewClient"), "NewClient", './CustomsModules/CustomsClient/Components/NewClient/NewClientComponent', 850, 500, "Customs.Client")); //3610
             });
         }
