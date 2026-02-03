b014411569fd244ce86b179f7365329a5a2cf051
Daniel Shoshan <Daniel@amital.co.il>
2026-02-03 13:36:59 +0200

    #129512

--- Logitude/Logitude.CustomsMessaging/ResponseServices/TPG_NG_2018_BankAccountToRefundUpdateReplayResponseService.cs
+++ Logitude/Logitude.CustomsMessaging/ResponseServices/TPG_NG_2018_BankAccountToRefundUpdateReplayResponseService.cs
@@ -27,10 +27,14 @@ namespace Logitude.CustomsMessaging.ResponseServices
 
             this.MyResponseData = new INF_MSG_GenericResponseData();
             this.MyResponseData.Succeeded = true;
+            this.MyResponseData.HasException = false;
 
-            if(customResponse?.ResponseContentHeader?.Remark != null)
+            if (!string.IsNullOrWhiteSpace(customResponse?.ResponseContentHeader?.Remark))
             {
+                this.MyResponseData.Succeeded = false;
+                this.MyResponseData.HasException = true;
                 this.MyResponseData.UserMessage = customResponse.ResponseContentHeader.Remark;
+                return;
             }
         }
 
