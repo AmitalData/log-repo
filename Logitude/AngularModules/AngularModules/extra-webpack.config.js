5b9a6bca9a457bff31ba362ff995b947a755b1b1
Dror Granot <dror091@gmail.com>
2026-02-04 21:23:25 +0200

    webp update

--- Logitude/AngularModules/AngularModules/extra-webpack.config.js
+++ Logitude/AngularModules/AngularModules/extra-webpack.config.js
@@ -1,5 +1,22 @@
 module.exports = {
     optimization: {
-       concatenateModules: false
+        concatenateModules: false
+    },
+    module: {
+        rules: [
+            // Transpile react-resizable (uses optional chaining ?.) so Webpack/ES5 build accepts it
+            {
+                test: /\.js$/,
+                include: /node_modules[\\/]react-resizable/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: { ie: '11' }, bugfixes: true }]
+                        ]
+                    }
+                }
+            }
+        ]
     }
 };
