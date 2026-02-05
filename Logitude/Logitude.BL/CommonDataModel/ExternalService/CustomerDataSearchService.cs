205c9f1238e51cffd6b01d719dc5e90d91828a7e
Libby Hasid <libby@amital.co.il>
2026-02-05 10:31:31 +0200

    #129619

--- Logitude/Logitude.BL/CommonDataModel/ExternalService/CustomerDataSearchService.cs
+++ Logitude/Logitude.BL/CommonDataModel/ExternalService/CustomerDataSearchService.cs
@@ -97,7 +97,8 @@ namespace Logitude.BL.CommonDataModel.CustomFilters
 
         private List<CardSearchResult> GetFilteredCardSearchResultList(IQueryable<CardSearch> cardSearches)
         {
-            var cardSearchResultLists = cardDataSearchService.GetCardSearchDataResults(cardSearchAdvanceArgs, cardSearches);
+            var allowedCardIds = allCardSearchResultLists.Select(e => e.CardId).ToList();
+            var cardSearchResultLists = cardDataSearchService.GetCardSearchDataResults(cardSearchAdvanceArgs, cardSearches, allowedCardIds);
             cardSearchResultLists = cardSearchResultLists.Where(d => !allCardSearchResultLists.Select(a => a.CardId).ToList().Contains(d.CardId)).ToList();
             allCardSearchResultLists = allCardSearchResultLists.Concat(cardSearchResultLists).ToList();
             return cardSearchResultLists;
