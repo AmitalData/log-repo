fbc3f84100f144f50e7d948fc46e79987db3d436
Jeremy Cohen <Jeremy@amital.co.il>
2026-02-05 19:08:00 +0200

    #127417

--- Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/AdvancedDatePickerComponent.ts
+++ Logitude/AngularModules/AngularModules/Infrastructure/Components/LogitudeComponents/AdvancedDatePickerComponent.ts
@@ -10,6 +10,7 @@ import { ObjectFieldPM } from "../../EntityPMs/ObjectFieldPM";
 import { UIProperty, } from "./UIProperties";
 import { AppTool } from "../../Tools";
 import { TextCodeTranslator } from '../../Utilities/TextCodeTranslator';
+import { AdvancedDatePickerResolverComponent } from "./AdvancedDatePickerResolverComponent";
 
 @Component({
     selector: "AdvancedDatePicker",
@@ -88,6 +89,9 @@ export class AdvancedDatePickerComponent implements OnInit, OnDestroy {
                         this.selectedPeriodUnit = parts[2];
                         this.IsRelativeperiodDropDownOpen = true;
                     }
+                    else if (newValue?.indexOf("_") > -1) {
+                        this.SetOffsetDateInPopup();
+                    }
                     else {
                         this.DateValueChanged(new Date(newValue));
                     }
@@ -99,6 +103,22 @@ export class AdvancedDatePickerComponent implements OnInit, OnDestroy {
             if (newValue === undefined) newValue = null;
             if (this.selectedDateValue === undefined) this.selectedDateValue = null;
             if (this.selectedDateValue != newValue && !AppTool.IsNullOrEmpty(newValue)) this.DateValueChanged(newValue);
+        }
+    }
+
+    SetOffsetDateInPopup() {
+        // example: BTM_PLUS_2_Weeks
+        const parts = this.selectedDateValue?.split("_");
+        if (parts?.length == 4) {
+            this.Offset = (parts[1]?.toLowerCase() == "minus"? -1: 1) * parseInt(parts[2], 10);
+            this.selectedUnit = parts[3];
+
+            const selectedItem: CodeNameClass = this.DateOptions.filter(date => date.Code == parts[0])[0];
+            if (selectedItem) {
+                this.SelectedItem = selectedItem;
+                this.SelectedItemObject = this.SelectedItem;
+                this.InputValue = selectedItem.Name + (this.Offset > 0? "+": "-") + this.Offset + this.selectedUnit[0]?.toLowerCase();
+            }
         }
     }
 
@@ -135,6 +155,7 @@ export class AdvancedDatePickerComponent implements OnInit, OnDestroy {
         if (!this.eRef.nativeElement.contains(event.target)) {
             this.IsDropDownVisible = false;
             this.IsOpen = false;
+            this.isOffsetOpen = false;
         }
     }
 
@@ -236,7 +257,7 @@ export class AdvancedDatePickerComponent implements OnInit, OnDestroy {
         this.DropDownMouseInArea = false;
     }
 
-    public OnLiMouseOver($event) {
+    public OnLiMouseOver($event, item) {
         this.DropDownMouseInArea = true;
         let active = document.getElementsByClassName("highlighted");
         if (active[0]) {
@@ -254,11 +275,78 @@ export class AdvancedDatePickerComponent implements OnInit, OnDestroy {
         else {
             this.ToggleCalendar(false);
         }
+
+        this.hoveredItem = item;
+    }
+
+    hoveredItem = null;
+    units = ['Days', 'Weeks', 'Months', 'Years'];
+    result = new Date();
+    offset = 1;
+    public get Offset(): number {
+        return this.offset;
+    }
+    public set Offset(value: number) {
+        this.offset = value;
+
+        this.recalculate();
+    }
+    selectedUnit = 'Days';
+    isOffsetOpen = false;
+
+    SetOffsetDate () {
+        const value = this.offset == 0? null: this.SelectedItem.Code + (this.offset < 0? "_MINUS_": "_PLUS_") + this.offset + "_" + this.selectedUnit;
+        console.log("selected offset", value);
+        this.OnDropDownSelected(this.SelectedItem, value);
+        this.isOffsetOpen = false;
+    }
+
+    // changeOffset(delta: number) {
+    //     this.offset += delta;
+    //     this.recalculate();
+    // }
+    recalculate() {
+
+        var advancedDatePickerResolverComponent: AdvancedDatePickerResolverComponent = new AdvancedDatePickerResolverComponent();
+        const date = advancedDatePickerResolverComponent.ResolveDateValue(this.SelectedItem.Code, false);
+
+        switch (this.selectedUnit) {
+        case 'Days':
+            date.setDate(date.getDate() + this.offset);
+            break;
+        case 'Weeks':
+            date.setDate(date.getDate() + this.offset * 7);
+            break;
+        case 'Months':
+            date.setMonth(date.getMonth() + this.offset);
+            break;
+        case 'Years':
+            date.setFullYear(date.getFullYear() + this.offset);
+            break;
+        }
+        this.result = date;
     }
 
     public OnLiMouseLeave($event) {
         this.DropDownMouseInArea = false;
         $event.target.classList.remove("highlighted");
+        this.hoveredItem = null;
+    }
+
+    OnDropDownSelectedMouse($event, item) {
+        setTimeout(() => {
+            if (!this.isOffsetOpen) {
+                console.log("OnDropDownSelected");
+                this.OnDropDownSelected(item);
+            }
+        }, 100);
+    }
+    OpenOffsetPopup(item) {
+        this.isOffsetOpen = true;
+        this.SelectedItem = item;
+
+        this.SetOffsetDateInPopup();
+        this.recalculate();
     }
 
     public OnDropDownSelected(item: any, newValue = null, toggleDropDown = true) {
@@ -273,7 +361,7 @@ export class AdvancedDatePickerComponent implements OnInit, OnDestroy {
         else {
             this.IsRelativeperiodDropDownOpen = false;
         }
-        this.DataContext[this.ObjectFieldName] = this.SelectedItem.Code == "SPD" || this.SelectedItem.Code == "PER" ? selectedValue : this.SelectedItem.Code;
+        this.DataContext[this.ObjectFieldName] = this.SelectedItem.Code == "SPD" || selectedValue?.indexOf("_") > -1 ? selectedValue : this.SelectedItem.Code;
         this.SelectedItemObject = this.SelectedItem;
         this.SelectedItemChanged.emit(this.SelectedItem);
         this.ValidateField();
