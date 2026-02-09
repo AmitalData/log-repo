1cc25ba6de053c1ac6746284e0dbbce23de673ee
Libby Hasid <libby@amital.co.il>
2026-02-09 11:08:07 +0200

    #129619

--- Logitude/Logitude.BL/CommonDataModel/CustomFilters/CardDataSearchService.cs
+++ Logitude/Logitude.BL/CommonDataModel/CustomFilters/CardDataSearchService.cs
@@ -75,7 +75,7 @@ namespace Logitude.BL.CommonDataModel.CustomFilters
             while ((selectedDataCount == take && cardSearchResultLists.Count() < cardSearchAdvanceArgs.Take) || isFirstTime)
             {
                 var cardSearchResultSelectedLists = (from a in cardSearches
-                                               where a.Keyword.StartsWith(cardSearchAdvanceArgs.SeachText) && allowedCardIds.Contains(a.CardId) 
+                                               where a.Keyword.StartsWith(cardSearchAdvanceArgs.SeachText) && (allowedCardIds.Contains(a.CardId)  || allowedCardIds.Count() <1)
                                                select new CardSearchResult()
                                                {
                                                    CardId = a.CardId,
