0d479f553b3b76f122e4704863ee24fd966b1226
Lior Shasha <Lior@amital.co.il>
2026-02-17 08:10:35 +0200

    Fast sync 124777_25R02D

--- Logitude/Logitude.CustomsMessaging.Common/RequestParams/RequestParamsBase.Shared.cs
+++ Logitude/Logitude.CustomsMessaging.Common/RequestParams/RequestParamsBase.Shared.cs
@@ -97,10 +97,6 @@ namespace Logitude.CustomsMessaging.Common.RequestParams
         public bool ForceCompanySign { get; set; }
         public string DeclarationDirection { get; set; } = null;
         public bool ignoreConcurrentKiller { get; set; }
-
-        public bool SendUpdateContainerization { get; set; }
-
-        public string HsmStationContext { get; set; } = null;
     }
 
     public class TestCase
