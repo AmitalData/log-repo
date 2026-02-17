7fa87b35e0efc52b0b9f833a12c294cee45a65a2
Lior Shasha <Lior@amital.co.il>
2026-02-17 08:23:57 +0200

    Fast sync 125890_25r02d

---
+++ AmitalCloud/Core/AmitalCloud.Shipment.Domain/EntityPMs/ShipmentCarrierStatusPM.cs
@@ -0,0 +1,34 @@
+﻿using AmitalCloud.Infrastructure.Domain.BaseClasses;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AmitalCloud.Shipment.Domain.EntityPMs
+{
+    public partial class ShipmentCarrierStatusPM : BaseEntityPM
+    {
+        [Key]
+        public string Id { get; set; }
+        public string ShipmentId { get; set; }
+        public string Status { get; set; }
+        public string FromPortId { get; set; }
+        public string ToPortId { get; set; }
+        public string Details { get; set; }
+        public string RecordHash { get; set; }
+        public int Pieces { get; set; }
+        public double Weight { get; set; }
+        public DateTime ReceivingDate { get; set; }
+        public DateTime? EventDate { get; set; }
+        public bool Partial { get; set; }
+        public string FlightNumber { get; set; }
+        public string Location { get; set; }
+        public string AirlineName { get; set; }
+        public string StatusName { get; set; }
+        public string LocationCode { get; set; }
+        public string LocationName { get; set; }
+
+        public DateTime? DepartureDate { get; set; }
+        public DateTime? ArrivalDate { get; set; }
+        public string TimeOfDepartureInfo { get; set; }
+        public string TimeOfArrivalInfo { get; set; }
+    }
+}
--- Logitude/Logitude.BL/InvoiceModel/APIDataContract/QueryService/ARInvoiceQueryService.Generated.cs
+++ Logitude/Logitude.BL/InvoiceModel/APIDataContract/QueryService/ARInvoiceQueryService.Generated.cs
@@ -25,9 +25,8 @@ using Logitude.BL.InvoiceModel.EntityQueries;
 using Simplog.Data.InvoiceModel;
 using Logitude.Server.Tools;
 using Simplog.Data.CommonDataModel.Repositories;
-using Syncfusion.XlsIO.FormatParser.FormatTokens;
 
-namespace Logitude.BL.InvoiceModel.APIDataContract.ApiV1
+ namespace Logitude.BL.InvoiceModel.APIDataContract.ApiV1
 { 
    public partial class ARInvoiceQueryService
    {
@@ -777,11 +776,6 @@ namespace Logitude.BL.InvoiceModel.APIDataContract.ApiV1
 							int year = myComparativeDate.Value.Year;
 							int month = myComparativeDate.Value.Month;
 							month += myPaymentTerm.NumberOfMonths;
-							if (month > 12)
-							{
-								month -= 12;
-								year += 1;
-							}
 							int daysInMonth = DateTime.DaysInMonth(year, month);
 
 							myComparativeDate = new DateTime(year, month, daysInMonth, 0, 0, 0);
