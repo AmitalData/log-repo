9b9d53e5b6a3cceed03b5123586b3be34ebbb1d7
Lior Shasha <Lior@amital.co.il>
2026-02-17 07:37:29 +0200

    Fast sync 123067_25R02D

---
+++ AmitalCloud/Core/AmitalCloud.Infrastructure.Domain/Interfaces/IBlobService.cs
@@ -0,0 +1,16 @@
+﻿using AmitalCloud.Infrastructure.Domain.Helpers;
+
+namespace AmitalCloud.Infrastructure.Domain.Interfaces
+{
+    public interface IBlobService
+    {
+        byte[] Read(BlobFileInfo fileInfo);
+        void Write(byte[] data, BlobFileInfo fileInfo);
+        void WriteBlock(byte[] buffer, long sentBytes, string[] blockIdsList, int bufferNumber, BlobFileInfo fileInfo);
+        void Delete(BlobFileInfo fileInfo);
+        bool FileExists(BlobFileInfo fileInfo);
+        void AppendText(string text, BlobFileInfo fileInfo);
+        void Dispose();
+        void MoveFromAnotherStorage(string containerSASURI, string fileNameSource, BlobFileInfo destinationFileInfo);
+    }
+}
