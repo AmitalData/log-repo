53406a03ebe66bcb38e4c753e9ac08770c4773da
Lior Shasha <Lior@amital.co.il>
2026-02-19 00:42:07 +0200

    Fast sync 25R02D_124203

---
+++ AmitalCloud/AmitalCloud.Shipment.Domain/EntityPMs/OceanInsightsRequestsCountPM.cs
@@ -0,0 +1,23 @@
+﻿using Logitude.Server.Tools;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace  AmitalCloud.Shipment.Def.EntityPMs
+{
+    public partial class OceanInsightsRequestsCountPM : EntityPM
+    {
+        [Key]
+        public string Id { get; set; }
+        public int Tenant { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string Type { get; set; }
+        public string OceanInsigntId { get; set; }
+        public string BLNumber { get; set; }
+        public string ContainerNumber { get; set; }
+        public string ContainerSubscriptionId { get; set; }
+    }
+}
