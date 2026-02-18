5be8bec75aae85f96c57630e7b1f47c6329229be
Lior Shasha <Lior@amital.co.il>
2026-02-19 00:41:05 +0200

    Fast sync 25R02D_124119

---
+++ AmitalCloud/AmitalCloud.Infrastructure.Model/EntityClasses/Manual/Counter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AmitalCloud.Infrastructure.Domain.EntityPOCOs
+{
+    public class Counter
+    {
+        [Key]
+        public string Id { get; set; }
+
+        public int Tenant { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string ObjectTableId { get; set; }
+
+        public string ChangedByUserId { get; set; }
+        public DateTime? ChangedDate { get; set; }
+
+        //[Include]
+        //[Association("CounterObjectTable", "ObjectTableId", "Id", IsForeignKey = true)]
+
+        [ForeignKey("ObjectTableId")]
+        public ObjectTable ObjectTable { get; set; }
+
+        [ForeignKey("ChangedByUserId")]
+        public User User { get; set; }
+
+        // public List<CounterDefinition> CounterDefinitions { get; set; }
+
+        // public List<CounterStat> CounterStats { get; set; }
+    }
+
+}
