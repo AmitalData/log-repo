f52e783c0a4ff484c256279d2930aa0e4bb8a1ae
Lior Shasha <Lior@amital.co.il>
2026-02-19 01:12:03 +0200

    Fast sync 25R02D_126390

---
+++ AmitalCloud/AmitalCloud.Infrastructure.DomainCore/EntityPMs/Manual/CustomerSalesmanByProductPM.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AmitalCloud.Infrastructure.Domain.EntityPMs
+{
+    public class CustomerSalesmanByProductPM : BaseClasses.BaseEntityPM
+    {
+        [Key]
+        public string ProductTypeCode { get; set; }
+        public string SalesmanUserId { get; set; }
+        [Key]
+        public string CustomerId { get; set; }
+        public string SalesmanUserName { get; set; }
+    }
+}
---
+++ AmitalCloud/AmitalCloud.Infrastructure.Model/EntityClasses/Manual/AdvancedQueryFilter.cs
@@ -0,0 +1,39 @@
+﻿using AmitalCloud.Infrastructure.Domain.BaseClasses;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace AmitalCloud.Infrastructure.Domain.EntityPOCOs
+{
+    public class AdvancedQueryFilter : BaseEntity
+    {
+        [Key]
+        public string Id { get; set; }
+        public string QueryCode { get; set; }
+        public int Tenant { get; set; }
+        public string QueryId { get; set; }
+        public string ObjectFieldId { get; set; }
+        public bool IsPredefined { get; set; }
+        public string PredefinedValue { get; set; }
+        public string PredefinedValue2 { get; set; }
+        public string Operator { get; set; }
+        public int IndexOrder { get; set; }
+        public bool CustomPredefined { get; set; }
+        public string UserId { get; set; }
+        public string ObjectFieldCode { get; set; }
+
+
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+
+
+        //[Include]
+        //[Association("QueryAdvancedQueryFilter","QueryId","Id",IsForeignKey=true)]
+
+        [ForeignKey("QueryId")]
+        public virtual Query Query { get; set; }
+        //[Include]
+        //[Association("ObjectFieldAdvancedQueryFilter","ObjectFieldId","Id",IsForeignKey=true)]
+
+        [ForeignKey("ObjectFieldId")]
+        public virtual ObjectField ObjectField { get; set; }
+    }
+}
