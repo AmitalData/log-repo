8497326b7c541f0468113ad6f3f617d1578dbb39
Lior Shasha <Lior@amital.co.il>
2026-02-19 02:21:41 +0200

    Fast sync 25R02D_with

---
+++ AmitalCloud/AmitalCloud.Infrastructure.Domain/EntityPMs/Manual/QueryGroupPM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AmitalCloud.Infrastructure.Domain.EntityPMs
+{
+    public class QueryGroupPM
+    {
+        [Key]
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public int IndexOrder { get; set; }
+    }
+}
---
+++ AmitalCloud/AmitalCloud.Infrastructure.Domain/Interfaces/IHtmlEditorHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace AmitalCloud.Infrastructure.Domain.Interfaces
+{
+    public interface IHtmlEditorHelper
+    {
+        //string SendEmailOutActivityForEntity(byte[] htmlData, byte[] textData, int tenant, string toEmail, string subject, string cc, string bcc, string userId, string myEntityId, string customerId, string objectTableId, string attachments, string entityReference, string documentTypeCode, string eventTypeCode);
+        string SendHtmlDocument(byte[] htmlData, string internalDocumentId, string externalDocumentId, int tenant, string toEmail, string subject, string cc, string bcc, string userId, string entityId, string objectTableId, string attachments, string entityReference, string from, string replyTo);
+        object GetEntity(string entityName, string entityId, int tenant);
+
+    }
+}
